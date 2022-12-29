827584cadd13fbb2a8bd4b71af278c540b954c35
Sean Leong <70213029+seanleong339@users.noreply.github.com>
2022-12-29 13:56:26 +0800

    .gitignore: Fix ACTUAL.txt -> ACTUAL.TXT  (#73)
    text-ui-test/runtest.bat|sh scripts generate a file ACTUAL.TXT.
    However, .gitignore uses ACTUAL.txt, which means the generated
    file will not be ignored by Git on non-Windows OS.
    
    Let's update .gitignore as ACTUAL.txt -> ACTUAL.TXT

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        String greeting = "Hi, I'm BiuBiu.\nWhat can I do for you?";
+        String exit = "Bye. Have a great day!";
+        System.out.println(greeting + "\n" + exit);
     }
 }
