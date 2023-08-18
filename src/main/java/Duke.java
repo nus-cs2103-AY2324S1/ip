92fdfeac5a690fa75923dbe8d1944b94bb787385
tjch-o <tzejie.c@gmail.com>
2023-08-19 03:14:00 +0800

    add echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,46 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+    private static String divider = "____________________________________________________________";
+    private static String indent = "     ";
+
+    public static void printDivider() {
+        System.out.println(indent + divider);
+    }
+
+    public static void greet() {
         String chatbotName = "Miles";
-        String horizontalLines = "____________________________________________________________";
-        
-        System.out.println(horizontalLines);
-        System.out.println("Hello! I'm " + chatbotName);
-        System.out.println("What can I do for you, my friend?");
-        System.out.println(horizontalLines);
-        System.out.println("Stay safe my friend. Hope to see you again soon man.");
-        System.out.println(horizontalLines);
+
+        printDivider();
+        System.out.println(indent + "Hey! I'm " + chatbotName + "!");
+        System.out.println(indent + "What can I do for you, my friend?");
+        printDivider();
+    }
+
+    public static void echo() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (!input.equals("bye")) {
+                printDivider();
+                System.out.println(indent + " " + input);
+                printDivider();
+            } else {
+                break;
+            }
+        }
+    }
+
+    public static void exit() {
+        printDivider();
+        System.out.println(indent + "Stay safe my friend. Hope to see you again soon man.");
+        printDivider();
+    }
+    public static void main(String[] args) {
+        Duke.greet();
+        Duke.echo();
+        Duke.exit();
     }
 }
