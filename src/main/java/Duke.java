89e8ff0e01b396c3f833d449fa67494484de8333
imkwokyong <darrenkuan@gmail.com>
2023-08-18 23:48:32 +0800

    Add ability to store and display text

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,8 @@ public class Duke {
         String helloGreeting = String.format("Hello! I'm %s\nWhat can I do for you?", name);
         String byeGreeting = "Bye. Hope to see you again soon! o(╥﹏╥)o";
         Scanner sc = new Scanner(System.in);
+        String[] textList = new String[100];
+        int itemCount = 0;
 
         System.out.print(output(helloGreeting));
         while (true) {
@@ -12,8 +14,19 @@ public class Duke {
             if (command.equals("bye")) {
                 System.out.print(output(byeGreeting));
                 break;
+            } else if (command.equals("list")) {
+                StringBuilder msg = new StringBuilder();
+                for (int i = 1; i <= itemCount; i++) {
+                    msg.append(String.format("%d. %s", i, textList[i - 1]));
+                    if (i != itemCount) {
+                        msg.append("\n");
+                    }
+                }
+                System.out.print(output(msg.toString()));
             } else {
-                System.out.print(output(command));
+                textList[itemCount] = command;
+                itemCount++;
+                System.out.print(output("added: " + command));
             }
         }
     }
