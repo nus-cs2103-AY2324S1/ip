8181dc6f94ab8b1beb54748b970dc84411d789f2
Batatprogrammiing <Batatprogrammiing@users.noreply.github.com>
2023-08-18 21:08:57 +0800

    Add Echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
+    private void greet() {
+        System.out.println("    ____________________________________________________________\n");
+        System.out.println("    Hello! I'm Duke\n");
+        System.out.println("    What can I do for you?\n");
+        System.out.println("    ____________________________________________________________\n");
+    }
+
+    private void bye() {
+        System.out.println("    ____________________________________________________________\n");
+        System.out.println("    Bye. Hope to see you again soon!\n");
+        System.out.println("    ____________________________________________________________\n");
+    }
+
+    private void echo() {
+        Scanner scanner = new Scanner(System.in);
+        String command = "";
+
+        while (true) {
+            command = scanner.nextLine();
+            if (command.equals("bye")) {
+                this.bye();
+                break;
+            } else {
+                System.out.println("    ____________________________________________________________\n");
+                System.out.println("    " + command + "\n");
+                System.out.println("    ____________________________________________________________\n");
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n");
-        System.out.println("Hello! I'm Duke\n");
-        System.out.println("What can I do for you?\n");
-        System.out.println("____________________________________________________________\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("____________________________________________________________\n");
+        Duke duke = new Duke();
+        duke.greet();
+        duke.echo();
     }
 }
