487992491afee162ad34394e90c73612a2e94fd1
et-irl <e0958283@u.nus.edu>
2023-08-19 03:21:32 +0800

    Add echo feature and bye to quit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     private static void greet() {
         System.out.println("Hello! I'm Untitled!");
@@ -8,8 +10,19 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
+    private static void echo(String input) {
+        System.out.println(input);
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         Duke.greet();
+        for (String input = scanner.nextLine(); !input.equals("bye"); input = scanner.nextLine()) {
+            Duke.echo(input);
+        }
         Duke.exit();
+
+        scanner.close();
     }
 }
