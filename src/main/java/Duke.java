a1c08f2702d2299ba5a2f4cfcd85f41c9daa5b04
Dick Ong <dickongwd@gmail.com>
2023-08-18 23:10:40 +0800

    Add echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,42 @@
+import java.util.Scanner;
+
 public class Duke {
     public static final String NAME = "Buzu";
-    public static final String DIVIDER = "__________________________________________________";
-    public static void say(String text) {
-        System.out.println(DIVIDER);
-        System.out.println(text);
-        System.out.println(DIVIDER);
+    private static final String DIVIDER = "__________________________________________________";
+
+    /**
+     * Utility function to add indentation when printing line.
+     *
+     * @param line The line to be indented
+     */
+    private static void printLine(String line) {
+        System.out.println("\t" + line);
+    }
+
+    /**
+     * Utility function for chatbot to send a formatted response to the user.
+     *
+     * @param text The text to respond with
+     */
+    private static void say(String... text) {
+        printLine(DIVIDER);
+        for (String line : text) {
+            printLine(line);
+        }
+        printLine(DIVIDER);
         System.out.println();
     }
+
     public static void main(String[] args) {
-        say("Hello! I'm " + NAME + ".\nWhat can I do for you?");
+        say("Hello! I'm " + NAME + ".", "What can I do for you?");
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            say(command);
+        }
         say("Bye! Hope to see you again soon!");
     }
 }
