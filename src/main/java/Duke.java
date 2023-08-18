d1f08fc059af8ddb4ae9541c0031c8f7fe5f203e
tjch-o <tzejie.c@gmail.com>
2023-08-19 02:30:15 +0800

    change chatbot name and add rename, greet, exit feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String chatbotName = "Miles";
+        String horizontalLines = "____________________________________________________________";
+        
+        System.out.println(horizontalLines);
+        System.out.println("Hello! I'm " + chatbotName);
+        System.out.println("What can I do for you, my friend?");
+        System.out.println(horizontalLines);
+        System.out.println("Stay safe my friend. Hope to see you again soon man.");
+        System.out.println(horizontalLines);
     }
 }
