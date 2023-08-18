964e3410712b70b00ee05c0d31f460844db403cf
SimWPEric <e0968924@u.nus.edu>
2023-08-18 17:30:48 +0800

    Added echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,33 @@
+import java.util.Scanner;
 public class Duke {
     private void greet() {
-        String msg = "Hello! I'm Atlas\n"
+        String greetMsg = "Hello! I'm Atlas\n"
                 + "What can I do for you?\n";
-        System.out.println(msg);
+        System.out.println(greetMsg);
     }
 
     private void exit() {
-        String msg = "Bye. Hope to see you again soon!\n";
+        String exitMsg = "Bye. Hope to see you again soon!\n";
+        System.out.println(exitMsg);
+    }
+
+    private void echo(String msg) {
         System.out.println(msg);
+    }
+
+    private void listen() {
+        Scanner sc = new Scanner(System.in);
+        String msg = sc.nextLine();
+        if (msg.equals("bye")) {
+            exit();
+        } else {
+            echo(msg);
+            listen();
+        }
     }
     public static void main(String[] args) {
         Duke duke = new Duke();
         duke.greet();
-        duke.exit();
+        duke.listen();
     }
 }
