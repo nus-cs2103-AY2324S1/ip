bb2627503fccbb043924fe9537630b4767cab311
Xavier <xavieroh05@gmail.com>
2023-08-19 01:35:34 +0800

    Rename ChatBot

---
+++ src/main/java/Horo.java
@@ -0,0 +1,11 @@
+public class Horo {
+    public static void main(String[] args) {
+        String logo = " _    _ \n"
+                    + "| |  | |\n"
+                    + "| |__| | ___  _ __ ___\n"
+                    + "|  __  |/ _ \\| '__/ _ \\\n"
+                    + "| |  | | (_) | | | (_) |\n"
+                    + "|_|  |_|\\___/|_|  \\___/\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
