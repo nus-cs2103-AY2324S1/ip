f69eb1cc9037235ccb5bd0d3c2de89fcca8c6e26
prawnzyy <jamcookies77@gmail.com>
2023-08-18 21:37:36 +0800

    Completed Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        UI ui = new UI();
+        ui.line();
+        System.out.println("Hello, I'm Prawn");
+        System.out.println("What can I do for you?");
+        ui.line();
+        System.out.println("Bye. Hope to see you again soon!");
+        ui.line();
     }
 }
---
+++ src/main/java/UI.java
@@ -0,0 +1,13 @@
+/**
+ * The UI class contains methods that generate UI elements
+ * such as line spaces
+ *
+ * @author Zi Xiang
+ * @version CS2103T AY23/24 Sem 1
+ */
+public class UI {
+    /** Generates a line spacer */
+    public void line(){
+        System.out.println("\n----------------------------------------");
+    }
+}
