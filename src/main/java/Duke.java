69bfec9d35d4e2f235f13630343613e52fb0cf10
jianrong7 <ipwnjianrong@gmail.com>
2023-08-18 21:20:04 +0800

    feat: added ability to echo user unless user says bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void printHorizontalLine() {
-        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("\t------------------------------------------------------------------------------------");
+    }
+    public static void printBotMessage(String msg) {
+        printHorizontalLine();
+        System.out.println("\t" + msg);
+        printHorizontalLine();
     }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = "\n" +
-                "_________ .__            __    __                \n" +
-                "\\_   ___ \\|  |__ _____ _/  |__/  |_  ___________ \n" +
-                "/    \\  \\/|  |  \\\\__  \\\\   __\\   __\\/ __ \\_  __ \\\n" +
-                "\\     \\___|   Y  \\/ __ \\|  |  |  | \\  ___/|  | \\/\n" +
-                " \\______  /___|  (____  /__|  |__|  \\___  >__|   \n" +
-                "        \\/     \\/     \\/                \\/       \n";
-        printHorizontalLine();
-        System.out.println("Hello! I'm" + logo + "\nWhat can I do for you?\n");
-        printHorizontalLine();
-        System.out.println("Bye. Hope to see you again soon!");
+                "\t_________ .__            __    __                \n" +
+                "\t\\_   ___ \\|  |__ _____ _/  |__/  |_  ___________ \n" +
+                "\t/    \\  \\/|  |  \\\\__  \\\\   __\\   __\\/ __ \\_  __ \\\n" +
+                "\t\\     \\___|   Y  \\/ __ \\|  |  |  | \\  ___/|  | \\/\n" +
+                "\t \\______  /___|  (____  /__|  |__|  \\___  >__|   \n" +
+                "\t        \\/     \\/     \\/                \\/       \n";
+        printBotMessage("Hello! I'm" + logo + "\n\t \uD83E\uDD9C What can I do for you?");
+
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                printBotMessage("Bye. Hope to see you again soon! \uD83D\uDD19 \uD83D\uDD1B \uD83D\uDD1D");
+                break;
+            }
+            printBotMessage(input);
+        }
+
     }
 }
