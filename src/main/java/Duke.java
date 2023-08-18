77996488885e00747141118343d1bc8033a8199d
lihongguang00 <lihongguang00@gmail.com>
2023-08-18 14:28:26 +0800

    Level-0a
    Renamed Chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "   _____ _    _          _____   _____ _____ _______ \n" +
+                "  / ____| |  | |   /\\   |  __ \\ / ____|  __ \\__   __|\n" +
+                " | |    | |__| |  /  \\  | |  | | |  __| |__) | | |   \n" +
+                " | |    |  __  | / /\\ \\ | |  | | | |_ |  ___/  | |   \n" +
+                " | |____| |  | |/ ____ \\| |__| | |__| | |      | |   \n" +
+                "  \\_____|_|  |_/_/    \\_\\_____/ \\_____|_|      |_|   \n";
+
+        String horizontal = "______________________________________________________\n";
+
+        System.out.println(horizontal + logo + horizontal);
+        System.out.println("Welcome to CHADGPT, What would you like to do today?");
     }
 }
