9bb786a442e3d73e26927ae147e257abe43a051a
tjch-o <tzejie.c@gmail.com>
2023-08-19 04:42:50 +0800

    implement add, list features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,43 +1,69 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     private static String divider = "____________________________________________________________";
     private static String indent = "     ";
+    private static ArrayList<String> commands = new ArrayList<>();
+
+    public static void formatString(String s) {
+        System.out.println(indent + s);
+    }
 
     public static void printDivider() {
-        System.out.println(indent + divider);
+        formatString(divider);
     }
 
     public static void greet() {
         String chatbotName = "Miles";
 
         printDivider();
-        System.out.println(indent + "Hey! I'm " + chatbotName + "!");
-        System.out.println(indent + "What can I do for you, my friend?");
+        formatString("Hey! I'm " + chatbotName + "!");
+        formatString("What can I do for you, my friend?");
         printDivider();
     }
 
     public static void echo() {
         Scanner scanner = new Scanner(System.in);
+        String add = "added: ";
 
         while (true) {
             String input = scanner.nextLine();
             
-            if (!input.equals("bye")) {
+            if (!input.equals("bye") && !input.equals("list")) {
+                // store each item in the arraylist
+                commands.add(input);
+                String output = " " + add + input;
                 printDivider();
-                System.out.println(indent + " " + input);
+                formatString(output);
                 printDivider();
+            } else if (input.equals("list")) {
+                printList();
             } else {
                 break;
             }
         }
     }
 
-    public static void exit() {
+    public static void printList() {
+        int n = commands.size();
+
         printDivider();
-        System.out.println(indent + "Stay safe my friend. Hope to see you again soon man.");
+        for (int i = 0; i < n; i += 1) {
+            String currentCommand = commands.get(i);
+            int currentNum = i + 1;
+            String output = " " + currentNum + ". " + currentCommand;
+            formatString(output);
+        }
         printDivider();
     }
+
+    public static void exit() {
+        printDivider();
+        formatString("Stay safe my friend. Hope to see you again soon man.");
+        printDivider();
+    }
+
     public static void main(String[] args) {
         Duke.greet();
         Duke.echo();
