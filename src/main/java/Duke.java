9a26ad84aa87e552e490ff190e33e20312b71414
SimWPEric <e0968924@u.nus.edu>
2023-08-18 17:23:56 +0800

    Added greet, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
+    private void greet() {
+        String msg = "Hello! I'm Atlas\n"
+                + "What can I do for you?\n";
+        System.out.println(msg);
+    }
+
+    private void exit() {
+        String msg = "Bye. Hope to see you again soon!\n";
+        System.out.println(msg);
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.greet();
+        duke.exit();
     }
 }
