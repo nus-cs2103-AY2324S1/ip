0b5eff05874c277fc8e812db754a37a42eb253b3
Woo Jia Hao <woojiahao1234@gmail.com>
2023-08-19 14:32:32 +0800

    Implement input storage and listing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Duke {
   private final static String delimiter = "-".repeat(60);
+  private final static int globalIndentation = 4;
 
   private static void printText(String text, int indentation) {
     String frontPadding = " ".repeat(indentation);
@@ -14,14 +19,24 @@ public class Duke {
   }
 
   public static void main(String[] args) {
-    printText("Hello! I'm Cyrus\nWhat can I do for you?", 4);
+    printText("Hello! I'm Cyrus\nWhat can I do for you?", globalIndentation);
     String command = "";
     Scanner sc = new Scanner(System.in);
+    List<String> store = new ArrayList<>();
     while (true) {
       command = sc.nextLine();
       if (command.equals("bye")) break;
-      printText(command, 4);
+      if (command.equals("list")) {
+        String output = IntStream
+            .range(0, store.size())
+            .mapToObj((i) -> String.format("%d. %s", i + 1, store.get(i)))
+            .collect(Collectors.joining("\n"));
+        printText(output, globalIndentation);
+      } else {
+        printText(String.format("added: %s", command), globalIndentation);
+        store.add(command);
+      }
     }
-    printText("Bye. Hope to see you again soon!", 4);
+    printText("Bye. Hope to see you again soon!", globalIndentation);
   }
 }
