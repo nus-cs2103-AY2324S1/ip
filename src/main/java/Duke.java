b65d61b3852ebca2111a8bdfdbf134db03e97161
ong-wei-hong <weihong.ong@outlook.com>
2023-08-19 19:14:38 +0800

    completed level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }
+
+	static String botName = "GOAT";
+
+	public static void main(String[] args) {
+		String logo = "   ______    ___        _    _________ \n"
+				+ " .' ___  | .'   `.     / \\  |  _   _  |\n"
+				+ "/ .'   \\_|/  .-.  \\   / _ \\ |_/ | | \\_|\n"
+				+ "| |   ____| |   | |  / ___ \\    | |    \n"
+				+ "\\ `.___]  \\  `-'  /_/ /   \\ \\_ _| |_   \n"
+				+ " `._____.' `.___.'|____| |____|_____|  \n";
+		System.out.println("Hello from\n" + logo);
+
+		printLine();
+		greet();
+		printLine();
+		sayBye();
+		printLine();
+	}
+
+	static void greet() {
+		System.out.printf("Hello! I'm %s\n", botName);
+		System.out.println("What can I do for you?");
+	}
+
+	static void sayBye() {
+		System.out.println("Bye. Hope to see you again soon!");
+	}
+
+	static void printLine() {
+		String line = "____________________________________________________________";
+		System.out.println(line);
+	}
 }
