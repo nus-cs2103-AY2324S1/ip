0c5151fc46abed3acc6a2da4d70984784eb04e93
sopa301 <96387349+sopa301@users.noreply.github.com>
2023-08-20 00:10:28 +0800

    Changed bot personality to trash gremlin Caelus. Added echo functionality.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Bot {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Bot!\nWhat can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm the trash gremlin Caelus!\nWhat can I do for you?");
+        while (true) {
+            String str = sc.next();
+            if (str.toLowerCase().equals("bye")) {
+                break;
+            }
+            System.out.println(str);
+        }
+        System.out.println("Bye. You can find me at the nearest trash can!");
     }
 }
