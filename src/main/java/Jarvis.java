f20f7b4fb26ced2635329d637037c680c8a6b115
李榕致 <lirongzhi@lirongzhis-MacBook-Air.local>
2023-08-19 11:37:10 +0800

    Increment to Level-1, added echo system

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Jarvis {
     public static void main(String[] args) {
         greet();
+        echo();
         exit();
     }
     public static void greet() {
@@ -11,15 +14,33 @@ public class Jarvis {
                 + "  _| |  / ----- \\  | |\\ \\    \\ \\/ /    _| |_  ____) |    \n"
                 + " |___/ /_/     \\_\\ |_| \\_\\    \\__/    |_____||_____/             \n";
 
-        horizontal_line_printer(50);
+        horizontal_line_printer();
         System.out.println("Hello from Jarvis\n" + logo);
         System.out.println("What can I do for you, sir?");
     }
     public static void exit() {
-        horizontal_line_printer(50);
+        horizontal_line_printer();
         System.out.println("Bye. Hope to see you again soon!");
     }
-    public static void horizontal_line_printer(int lenth) {
-        System.out.println("-".repeat(lenth));
+    public static void echo() {
+        horizontal_line_printer();
+        System.out.println("Echo program started. Exit by entering 'bye' command.");
+        horizontal_line_printer();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Exiting the echo program.");
+                break;
+            }
+            horizontal_line_printer();
+            System.out.println(input);
+            horizontal_line_printer();
+        }
+        sc.close();
+    }
+    public static void horizontal_line_printer() {
+        System.out.println("-".repeat(50));
     }
 }
