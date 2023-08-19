a33f21b8f67688de10d051f4c674a5d565b90c04
Khaleelur <khaleelrrahman2002@gmail.com>
2023-08-19 20:21:05 +0800

    Echo
    Chatbot echos whatever the user inputs and leaves when the user inputs "Bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+    String divider = "------------------------------------\n";
+    String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
 
-        System.out.println("------------------------------------\n" +
-                "Hello! I'm Khaleelur!\n" +
-                "What can I do for you?\n\n " +
-                "-----------------------------------\n" +
-                "Bye. Hope to see you again soon!");
+    String greet = divider +
+            "Hello! I'm Khaleelur!\n" +
+            "What can I do for you?\n " +
+            divider;
+
+    String exit = divider +
+            "Bye. Hope to see you again soon!\n" +
+            divider;
+
+    public String echo(String userInput) {
+        return userInput;
+    }
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        Duke duke = new Duke();
+
+        System.out.println(duke.greet);
+
+        String userInput = obj.nextLine();
+        while(!userInput.equals("bye")) {
+            System.out.println(duke.divider + duke.echo(userInput) + "\n" + duke.divider);
+            userInput = obj.nextLine();
+        }
+        System.out.println(duke.exit);
+
     }
 }
