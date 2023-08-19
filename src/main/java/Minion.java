4d2e67a84d5018387b81d773e7565c85254192d8
joel-foo <joelfootaimeng@gmail.com>
2023-08-20 01:06:38 +0800

    Done Level-1

--- src/main/java/Minion.java
+++ src/main/java/Minion.java
@@ -1,7 +1,38 @@
+import java.util.Scanner;
 public class Minion {
+    public static String line = "\t____________________________________________________________\n";
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        System.out.println(line + "\nHello! I'm Minion.\nWhat can I do for you?\n" + line);
-        System.out.println("Bye. Hope to see you again soon!\n" + line);
+        sayHi();
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String command = sc.nextLine();
+            if(command.equals("bye")) {
+                sayBye();
+                break;
+            }
+            prettyPrint(command);
+        }
+        sc.close();
+    }
+
+    /**
+     * Function to say hi to the user.
+     */
+    public static void sayHi(){
+        prettyPrint("Hello! I'm Minion!\n\tWhat can I do for you?");
+    }
+
+    /**
+     * Function to say bye to the user.
+     */
+    public static void sayBye(){
+        prettyPrint("Bye. Hope to see you again soon!");
+    }
+    /**
+     * Prints the string between two horizontal lines.
+     * @param s String to be printed.
+     */
+    public static void prettyPrint(String s){
+        System.out.println(line + String.format("\t%s\n", s) + line);
     }
 }
