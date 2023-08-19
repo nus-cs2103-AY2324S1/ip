a529d7f7c276ea5483da3b5d72139068706eb979
TiwKangXu <111041537+TiwKangXu@users.noreply.github.com>
2023-08-20 01:21:56 +0800

    Complete Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,22 +8,37 @@ public class Duke {
         System.out.println(text);
         System.out.println("------------------------------------------");
     }
+
     private void greet() {
         System.out.println("------------------------------------------");
-        this.line("Hello! I'm Jokey :). \nWhat can I do for you?");
+        this.line("  Hello! I'm Jokey :) \n  What can I do for you?");
     }
 
+    private boolean isExit(String reply) {
+        return reply.startsWith("bye");
+    }
     private void exit() {
-        this.line(" Bye~ Hope to see you again soon! >w<");
+        this.line("  Bye~ Hope to see you again soon! >w<");
     }
 
-    private void interpretCommand() {
-
+    private void interact() {
+        while(true) {
+            String reply = sc.nextLine();
+            if (isExit(reply)) {
+                exit();
+                break;
+            }
+            this.echo(reply);
+        }
     }
+
+    private void echo(String reply) {
+        this.line("  " + reply);
+    }
+
     public static void main(String[] args) {
         Duke duke = new Duke();
         duke.greet();
-        duke.interpretCommand();
-        duke.exit();
+        duke.interact();
     }
 }
