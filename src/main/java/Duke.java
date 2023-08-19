94c70140eaddd43f2742ce09bf68467b54d93844
B-enguin <e0959432@u.nus.edu>
2023-08-19 22:53:14 +0800

    Implemented Level-1 Requirements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,42 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         greet();
+        while (true) {
+            String userInput = readMessage();
+            if (!Objects.equals(userInput, "bye")) {
+                sendMessage(userInput);
+            } else {
+                break;
+            }
+        }
         exit();
     }
 
     private static void greet() {
-        System.out.println("____________________________________________________________");
-        sendMessage(" Hello! I'm Heimdallr\n" +
-                " What can I do for you?");
+        sendMessage("Hello! I'm Heimdallr\nWhat can I do for you?");
     }
 
     private static void exit() {
         sendMessage(" Bye. Hope to see you again soon!");
     }
 
-    public static void sendMessage(String msg) {
-        System.out.println(msg +
-                "\n____________________________________________________________");
+    public static void sendMessage(String msgs) {
+        printLine();
+        for (String msg: msgs.split("\n")) {
+            System.out.println("\t" + msg);
+        }
+        printLine();
+    }
+
+    public static String readMessage() {
+        return scanner.nextLine();
+    }
+
+    public static void printLine() {
+        System.out.println("\t____________________________________________________________");
     }
 }
