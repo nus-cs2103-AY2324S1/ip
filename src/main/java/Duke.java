55e5d54bba306697324dbfc0e8f8405f4dcbb0ca
yytan25 <tyydapro@gmail.com>
2023-08-19 23:10:59 +0800

    Completed Level-0 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
 public class Duke {
+
+    String name = "Leon";
+    public Duke() {
+        String message = String.format("Hello. I'm your personal assistant, %s." +
+                "\n" +
+                "What can I do for you?",
+                this.name);
+        System.out.println(message + "\n");
+        printHorizontalLine();
+        exit();
+    }
+
+    private void printHorizontalLine() {
+        for (int i = 0; i < 20; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
+    private void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontalLine();
+        System.exit(0);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        new Duke();
     }
 }
