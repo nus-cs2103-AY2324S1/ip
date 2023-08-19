d3162697a8fc1fedbbace3166d67d80edef5e89d
ruishanteo <ruishan.teo03@gmail.com>
2023-08-19 10:29:38 +0800

    completed level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] data = new String[100];
+        Task[] data = new Task[100];
         int index = 0;
 
         String logo =
@@ -26,11 +26,27 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < index; i++) {
-                    System.out.println((i + 1) + "." + " " + data[i]);
+                    System.out.println((i + 1) + "." + data[i]);
                 }
+            } else if (input.contains("unmark")) {
+                String subInput = input.substring(7);
+                int targetIndex = Integer.parseInt(subInput);
+                System.out.println("Ok, I've marked this task as not done yet:");
+                Task marked = data[targetIndex - 1];
+                marked.markAsUndone();
+                System.out.println(marked);
+            } else if (input.contains("mark")) {
+                String subInput = input.substring(5);
+                int targetIndex = Integer.parseInt(subInput);
+                System.out.println("Nice! I've marked this task as done:");
+                Task marked = data[targetIndex - 1];
+                marked.markAsDone();
+                System.out.println(marked);
             } else {
-                data[index] = input;
+                Task t = new Task(input);
+                data[index] = t;
                 index++;
                 System.out.println("added: " + input);
             }
