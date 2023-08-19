d0c1e1bbadd6536eb0e0a576c983792628802fe7
Elijah5399 <chia.elijah@gmail.com>
2023-08-20 02:42:43 +0800

    Added new logo and initial messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " ___  __    __   __  ____   ____    ___\n"
+                    + "|   | | |   \\ \\ / / |  __| |  __|  |   |\n"
+                    + "|   | | |    \\   /   \\ \\    \\ \\    |   |\n"
+                    + "|___| | |     | |     \\ \\    \\ \\   |___|\n"
+                    + "|   | | |___  | |     _\\ \\   _\\ \\  |   |\n"
+                    + "|   | |_____| |_|    |____| |____| |   |\n"
+                ;
+        System.out.println(logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Alyssa!");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
