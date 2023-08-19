3e71c3289a36e834c2a1a9710df8fa799ea0f03f
craigtonlian <108467437+craigtonlian@users.noreply.github.com>
2023-08-19 15:42:24 +0800

    added Level-3: Mark as Done

--- src/main/java/GrumpyGordon.java
+++ src/main/java/GrumpyGordon.java
@@ -6,31 +6,55 @@ public class GrumpyGordon {
         System.out.println("     Why are you bothering me?");
         System.out.println("    ____________________________________________________________");
 
+        int taskIndex;
         int taskCount = 0;
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
+
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
 
         while (!str.equals("bye")) {
-            if (!str.equals("list")) {
-                tasks[taskCount] = str;
-                taskCount++;
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     added: " + str);
-                System.out.println("    ____________________________________________________________");
-                str = sc.nextLine();
-            } else if (str.equals("list") && taskCount == 0) {
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     The list is empty, you donkey!");
-                System.out.println("    ____________________________________________________________");
-                str = sc.nextLine();
-            } else {
-                System.out.println("    ____________________________________________________________");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println("     " + (i + 1) + ". " + tasks[i]);
-                }
-                System.out.println("    ____________________________________________________________");
-                str = sc.nextLine();
+            switch (str.split(" ")[0]) {
+                case "list":
+                    if (taskCount == 0) {
+                        System.out.println("    ____________________________________________________________");
+                        System.out.println("     The list is empty, you donkey!");
+                        System.out.println("    ____________________________________________________________");
+                    } else {
+                        System.out.println("    ____________________________________________________________");
+                        System.out.println("     Stop wasting time, go get it done!");
+                        for (int i = 0; i < taskCount; i++) {
+                            System.out.println("     " + (i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].description);
+                        }
+                        System.out.println("    ____________________________________________________________");
+                    }
+                    str = sc.nextLine();
+                    break;
+                case ("mark"):
+                    taskIndex = Integer.parseInt(str.split(" ")[1]) - 1;
+                    tasks[taskIndex].markAsDone();
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("     Took you long enough!");
+                    System.out.println("       [" + tasks[taskIndex].getStatusIcon() + "] " + tasks[taskIndex].description);
+                    System.out.println("    ____________________________________________________________");
+                    str = sc.nextLine();
+                    break;
+                case ("unmark"):
+                    taskIndex = Integer.parseInt(str.split(" ")[1]) - 1;
+                    tasks[taskIndex].markAsUndone();
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("     My grandmother does it faster than you!");
+                    System.out.println("       [" + tasks[taskIndex].getStatusIcon() + "] " + tasks[taskIndex].description);
+                    System.out.println("    ____________________________________________________________");
+                    str = sc.nextLine();
+                    break;
+                default:
+                    tasks[taskCount] = new Task(str);
+                    taskCount++;
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("     added: " + str);
+                    System.out.println("    ____________________________________________________________");
+                    str = sc.nextLine();
             }
         }
         System.out.println("    ____________________________________________________________");
