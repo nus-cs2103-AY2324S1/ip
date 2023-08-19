c8f508e25b315a4f57cd148d4977b58603d363a7
cbj252 <cbj252@gmail.com>
2023-08-19 16:14:24 +0800

    Duke Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,25 @@
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello I'm iP");
+        String[] tasks = new String[100];
+        int nextTask = 0;
         Scanner input = new Scanner(System.in);
         String response = "";
         while (!Objects.equals(response, "bye")) {
             response = input.next();
             if (Objects.equals(response, "bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
+            } else if (Objects.equals(response, "list")) {
+                for (int i = 0; i < nextTask; i++) {
+                    System.out.println(i + 1 + ". " + tasks[i]);
+                }
             } else {
-                System.out.println(response);
+                tasks[nextTask++] = response;
+                System.out.println("added: " + response);
             }
         }
     }
