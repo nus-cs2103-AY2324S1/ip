e2d4a9524657a9bd2768b52dc2957dcc53cb5023
Song-Mengfei <e1075530@u.nus.edu>
2023-08-19 23:07:01 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Three Wolves B.\n" + "What can I do for you?\n");
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> lst = new ArrayList<>();
         String cmd = sc.nextLine();
         while (!cmd.equals("bye")) {
-            System.out.println(cmd + "\n");
+            if (cmd.equals("list")) {
+                //todo:
+                int counter = 1;
+                lst.forEach(x -> System.out.println(lst.indexOf(x) + 1 + ". " + x + "\n"));
+            } else {
+                lst.add(cmd);
+                System.out.println("added: " + cmd + "\n");
+            }
             cmd = sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
