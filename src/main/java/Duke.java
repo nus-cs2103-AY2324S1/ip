7cb77ef5fcb3c96b239a7600faaff43897126b81
ruishanteo <ruishan.teo03@gmail.com>
2023-08-19 09:58:26 +0800

    completed level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] data = new String[100];
+        int index = 0;
 
         String logo =
                 ".______     ______   .___________.\n" +
@@ -19,13 +21,18 @@ public class Duke {
 
         while (true) {
             String input = scanner.nextLine();
-
             System.out.println("_________________________________________");
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < index; i++) {
+                    System.out.println((i + 1) + "." + " " + data[i]);
+                }
             } else {
-                System.out.println(input);
+                data[index] = input;
+                index++;
+                System.out.println("added: " + input);
             }
             System.out.println("_________________________________________");
         }
