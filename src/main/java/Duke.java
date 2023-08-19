1c9b9524e6134e44475560c2a8e5c5e0f9eae77f
tanshiyu <tanshiyu1999@gmail.com>
2023-08-19 18:10:02 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
 
 
@@ -9,15 +10,40 @@ public class Duke {
         System.out.println("");
     }
 
-    public static void main(String[] args) {
+    public static void echo() {
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        if (command.equals("bye")) {
+            return;
+        }
+        printHorizontalLine();
+        System.out.println(command);
+        printHorizontalLine();
+        echo();
+    }
+
+    public static void introduction() {
         String name = "Donk";
         printHorizontalLine();
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
         printHorizontalLine();
+    }
 
+    public static void conclusion() {
+        printHorizontalLine();
         System.out.println("Bye. Hope to see you soon again soon!");
         printHorizontalLine();
+    }
+
+
+    public static void main(String[] args) {
+        introduction();
+
+        echo();
+
+        conclusion();
+
 
     }
 }
