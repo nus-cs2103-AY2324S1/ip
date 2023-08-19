4f609f2c85ace3355683f7d39512db0abb89ecf5
ItsTYtan <tytan216@gmail.com>
2023-08-19 17:06:24 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String divider = "_____________________________________\n";
         String opening = "Hello: I'm TY's slave\nWhat can I do for you?\n";
         String closing = "Bye. Hope to see you again soon!";
-        System.out.println(divider + opening + divider + closing);
+        System.out.println(divider + opening + divider);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(divider + closing + "\n" + divider);
+                break;
+            } else {
+                System.out.println(divider + input + "\n" + divider);
+            }
+        }
     }
 }
