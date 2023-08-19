25807c414534c7ef62fcd0acf2dbd6a9e1a80036
WX Game <weixiang05@gmail.com>
2023-08-19 16:05:05 +0800

    Initial skeleton

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printIntro();
+        printEnd();
+    }
+
+    private static void printIntro() {
+        printLine();
+        System.out.println("Hello! I'm Roe!\n" + "What can I do for you?\n");
+    }
+
+    private static void printEnd() {
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!\n");
+        printLine();
+    }
+
+    private static void printLine() {
+        int charCount = 50;
+        for (int i = 0; i < charCount; i++) {
+            System.out.print("─");
+        }
+        System.out.print("\n");
     }
 }
