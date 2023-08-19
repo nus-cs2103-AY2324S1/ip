a8e7b81f0e6cc6697f2671851c6629122deec270
AaronJT1 <134244836+AaronJT1@users.noreply.github.com>
2023-08-19 16:18:19 +0800

    Level-0 Increment
    Level-0 Increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
 public class Duke {
+
+    public static void horiLine(){
+        System.out.println("---------------------");
+    }
+    public static void greet() {
+        horiLine();
+        System.out.println("Hello! I'm AJbot\n" +
+                " What can I do for you?");
+        horiLine();
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+        horiLine();
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+
+        greet();
+        exit();
     }
 }
