e8fe6cf623dbd880289671649d2f4887c6310d00
WX Game <weixiang05@gmail.com>
2023-08-19 16:48:42 +0800

    Added echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,47 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static Scanner scannerObj = new Scanner(System.in);
+    public static String userInput;
+
     public static void main(String[] args) {
-        printIntro();
-        printEnd();
-    }
+        PrintIntro();
+        PromptInput();
 
-    private static void printIntro() {
-        printLine();
+        do {
+            EchoString(userInput);
+            PromptInput();
+        } while (!userInput.equals("bye"));
+
+        PrintEnd();
+    }
+    private static void PrintIntro() {
+        PrintLine();
         System.out.println("Hello! I'm Roe!\n" + "What can I do for you?\n");
+        PrintLine();
     }
 
-    private static void printEnd() {
-        printLine();
+    private static void PrintEnd() {
+        PrintLine();
         System.out.println("Bye. Hope to see you again soon!\n");
-        printLine();
+        PrintLine();
     }
 
-    private static void printLine() {
+    private static void EchoString(String input) {
+        PrintLine();
+        System.out.println(input + "\n");
+        PrintLine();
+    }
+
+    private static String PromptInput() {
+        System.out.println();
+        userInput = scannerObj.nextLine();
+        System.out.println();
+        return userInput;
+    }
+
+    private static void PrintLine() {
         int charCount = 50;
         for (int i = 0; i < charCount; i++) {
             System.out.print("─");
