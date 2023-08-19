5e2d46b8a930dbb758aa82dd71044ccaa0884698
Xuan Yi <saltedfishxx@gmail.com>
2023-08-19 15:49:05 +0800

    Complete Level-0: Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String divider = "\n____________________________________________________________";
+        String logo = "       ／ﾌﾌ 　　　　　　ム｀ヽ\n" +
+                "      / ノ)　  A　　A 　　）   ヽ\n" +
+                "      / ｜　　( ´・ω・`)ノ⌒ゝ_ノ\n" +
+                "      /　ﾉ⌒7⌒ヽーく　 ＼　／\n" +
+                "      丶＿ ノ ｡　　 ノ､　　|/\n" +
+                "    　　  `ヽ `ー-'_人`ーﾉ\n" +
+                "    　　　   丶 ￣ _人'彡ﾉ\n" +
+                "   　　　　   ﾉ　r'十ヽ/";
+        System.out.println(logo + divider + "\nHello! I'm MACHO-CATTO! Your personal chat-bot to make your \nday macho!"
+                + "\nWhat can I do for you today?" + divider);
+        System.out.println("Bye. Hope to see you again soon!" + divider);
     }
 }
