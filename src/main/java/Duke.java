0b9264c8a47b2056235f9686b9601d977ae154f2
Kieron Seven Lee <kieronsevenlee@u.nus.edu>
2023-08-19 22:48:27 +0800

    Skeletal version of Oscar with echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,26 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello I'm Oscar");
+        Scanner scanner = new Scanner(System.in);
+
+        // Greet user
+        System.out.println("Hello I'm Oscar! Your friendly chatbot :)");
         System.out.println("What can I do for you?");
         System.out.println();
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            // Obtain command entered by user
+            String userInput = scanner.nextLine();
+            // Exit programme if user enters bye command
+            if (Objects.equals(userInput, "bye")) {
+                System.out.println("Goodbye for now. Hope to see you again soon!");
+                break;
+            }
+            // Echo user's command otherwise
+            System.out.println("Oscar says " + userInput);
+            System.out.println();
+        }
     }
 }
