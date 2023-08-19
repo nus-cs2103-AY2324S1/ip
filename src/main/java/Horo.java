941dcc1616b18a1f4047f8ba646aa185b4a767c0
Xavier <xavieroh05@gmail.com>
2023-08-19 12:07:15 +0800

    Implement echo

--- src/main/java/Horo.java
+++ src/main/java/Horo.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Horo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo = " _    _ \n"
                     + "| |  | |\n"
                     + "| |__| | ___  _ __ ___\n"
@@ -9,6 +13,19 @@ public class Horo {
         System.out.println(logo);
         System.out.println("Hello! I'm Horo");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");        
+
+        System.out.println("Type anything. 'bye' to exit");
+        
+        while(true){
+          System.out.print(">");
+          String input = scanner.nextLine();
+          if(input.compareToIgnoreCase("bye") == 0 ){
+            break;
+          }
+          System.out.println(input);
+        }
+        
+        System.out.println("Bye. Hope to see you again soon!");
+        scanner.close();        
     }
 }
