95818e214a45e93e0443bfa811ff1146eafd96f8
joel-foo <joelfootaimeng@gmail.com>
2023-08-20 00:28:14 +0800

    Edit Minion.java

---
+++ src/main/java/Minion.java
@@ -0,0 +1,7 @@
+public class Minion {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        System.out.println(line + "\nHello! I'm Minion.\nWhat can I do for you?\n" + line);
+        System.out.println("Bye. Hope to see you again soon!\n" + line);
+    }
+}
