cf822b3c89a52637d7f3b319ff768f67065a3e2b
cbj252 <cbj252@gmail.com>
2023-08-19 16:36:35 +0800

    Duke Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,20 +5,33 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello I'm iP");
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int nextTask = 0;
         Scanner input = new Scanner(System.in);
         String response = "";
         while (!Objects.equals(response, "bye")) {
-            response = input.next();
+            response = input.nextLine();
             if (Objects.equals(response, "bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
             } else if (Objects.equals(response, "list")) {
                 for (int i = 0; i < nextTask; i++) {
-                    System.out.println(i + 1 + ". " + tasks[i]);
+                    System.out.println(i + 1 + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
+                }
+            } else if (response.startsWith("mark")) {
+                // Assumption: "mark" is not allowed as a task name & you can mark already done tasks.
+                // Issue: "mark" by itself crashes.
+                String[] array = response.split(" ");
+                String lastVal = array[array.length - 1];
+                int taskToMark = Integer.parseInt(lastVal);
+                if (taskToMark <= nextTask) {
+                    tasks[taskToMark - 1].completeTask();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[" + tasks[taskToMark - 1].getStatusIcon() + "] " + tasks[taskToMark - 1].getDescription());
+                } else {
+                    System.out.println("Invalid Mark Input Found.");
                 }
             } else {
-                tasks[nextTask++] = response;
+                tasks[nextTask++] = new Task(response);
                 System.out.println("added: " + response);
             }
         }
