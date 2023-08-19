c924cca609e7caec326281b07c940c3b1d12bae8
cbj252 <cbj252@gmail.com>
2023-08-19 15:59:38 +0800

    Duke Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,18 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello I'm iP");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner input = new Scanner(System.in);
+        String response = "";
+        while (!Objects.equals(response, "bye")) {
+            response = input.next();
+            if (Objects.equals(response, "bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+            } else {
+                System.out.println(response);
+            }
+        }
     }
 }
