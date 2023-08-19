fc9b1d3b3329628a8144e1a52753e2559463d54b
Song-Mengfei <e1075530@u.nus.edu>
2023-08-19 22:53:31 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Three Wolves B.\n" + "What can I do for you?\n");
+        Scanner sc = new Scanner(System.in);
+        String cmd = sc.nextLine();
+        while (!cmd.equals("bye")) {
+            System.out.println(cmd + "\n");
+            cmd = sc.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
+        sc.close();
     }
 }
