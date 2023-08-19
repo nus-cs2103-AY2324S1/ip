7b75c2b83e6f78952e76cfdf7da6867af04adf42
imkwokyong <darrenkuan@gmail.com>
2023-08-20 00:05:42 +0800

    [Level-3] Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,14 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String name = "ForsakenX";
         String helloGreeting = String.format("Hello! I'm %s\nWhat can I do for you?", name);
         String byeGreeting = "Bye. Hope to see you again soon! o(╥﹏╥)o";
         Scanner sc = new Scanner(System.in);
-        String[] textList = new String[100];
-        int itemCount = 0;
+        ArrayList<Task> taskList = new ArrayList<>();
+        int taskCount = 0;
 
         System.out.print(output(helloGreeting));
         while (true) {
@@ -15,17 +17,31 @@ public class Duke {
                 System.out.print(output(byeGreeting));
                 break;
             } else if (command.equals("list")) {
-                StringBuilder msg = new StringBuilder();
-                for (int i = 1; i <= itemCount; i++) {
-                    msg.append(String.format("%d. %s", i, textList[i - 1]));
-                    if (i != itemCount) {
+                StringBuilder msg = new StringBuilder("Here are the tasks in your list:\n");
+                for (int i = 1; i <= taskCount; i++) {
+                    msg.append(String.format("%d.%s", i, taskList.get(i - 1)));
+                    if (i != taskCount) {
                         msg.append("\n");
                     }
                 }
                 System.out.print(output(msg.toString()));
+            } else if (command.startsWith("mark") && !command.equals("mark")) {
+                int index = Integer.parseInt(command.split(" ")[1]);
+                Task task = taskList.get(index - 1);
+                task.markIsDone();
+                taskList.set(index - 1, task);
+                System.out.print(output(
+                        String.format("Nice! I've marked this task as done:\n\t%s", task)));
+            } else if (command.startsWith("unmark") && !command.equals("unmark")){
+                int index = Integer.parseInt(command.split(" ")[1]);
+                Task task = taskList.get(index - 1);
+                task.markNotDone();
+                taskList.set(index - 1, task);
+                System.out.print(output(
+                        String.format("OK, I've marked this task as not done yet:\n\t%s", task)));
             } else {
-                textList[itemCount] = command;
-                itemCount++;
+                taskList.add(new Task(command));
+                taskCount++;
                 System.out.print(output("added: " + command));
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markIsDone() {
+        this.isDone = true;
+    }
+
+    public void markNotDone() {
+        this.isDone = false;
+    }
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", getStatusIcon(), description);
+    }
+
+}
