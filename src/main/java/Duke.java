9039640f0b7b64b8c13dfb1f6b1ee2367da438c7
jiakai-17 <111171022+jiakai-17@users.noreply.github.com>
2023-08-19 15:59:03 +0800

    feat: Level-2
    Max stores a list of texts entered by the user. Retrieve with "list" command.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,31 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        boolean hasEndedChat = false;
 
         System.out.println("Hello! I'm Max!");
         System.out.println("What can I do for you?");
 
-        while (true) {
+        while (!hasEndedChat) {
             String input = sc.nextLine();
-            if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            } else {
-                System.out.println(input);
+            switch (input) {
+                case "bye":
+                    hasEndedChat = true;
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                case "list":
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println((i + 1) + ". " + list.get(i));
+                    }
+                    break;
+                default:
+                    list.add(input);
+                    System.out.println("added: " + input);
+                    break;
             }
         }
     }
