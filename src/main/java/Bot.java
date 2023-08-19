4b46ff407fb5a48a39c3fcef9684929793f3959e
sopa301 <96387349+sopa301@users.noreply.github.com>
2023-08-19 18:06:59 +0800

    Changed name of bot from Duke to Bot.

---
+++ src/main/java/Bot.java
@@ -0,0 +1,6 @@
+public class Bot {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Bot!\nWhat can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
