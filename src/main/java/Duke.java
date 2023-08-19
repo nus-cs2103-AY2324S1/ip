2d06203125c4137e75de2a773367e308c095bcaa
TiwKangXu <111041537+TiwKangXu@users.noreply.github.com>
2023-08-20 01:40:28 +0800

    Complete Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 public class Duke {
+    String[] tasks;
+    int taskIndex = 0;
     Scanner sc;
     public Duke() {
         this.sc = new Scanner(System.in);
+        this.tasks = new String[100];
     }
     private void line(String text) {
         System.out.println(text);
@@ -21,19 +24,36 @@ public class Duke {
         this.line("  Bye~ Hope to see you again soon! >w<");
     }
 
+    private boolean isListTasks(String reply) {
+        return reply.startsWith("list");
+    }
+
+    private void listOutTasks() {
+        String tasksList = "";
+        for(int i = 0; i < taskIndex; i++) {
+            tasksList += String.format("%d. %s \n", i + 1, tasks[i]);
+        }
+        this.line(tasksList);
+    }
+
     private void interact() {
         while(true) {
             String reply = sc.nextLine();
             if (isExit(reply)) {
                 exit();
                 break;
+            } else if (isListTasks(reply)) {
+                listOutTasks();
+            } else {
+                this.add(reply);
             }
-            this.echo(reply);
         }
     }
 
-    private void echo(String reply) {
-        this.line("  " + reply);
+    private void add(String reply) {
+        tasks[taskIndex] = reply;
+        this.line(String.format("  added: ", taskIndex + 1) + reply);
+        taskIndex += 1;
     }
 
     public static void main(String[] args) {
