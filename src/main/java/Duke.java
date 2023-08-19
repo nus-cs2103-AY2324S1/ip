268d20d4afd0c8f8bb6851f9ffbb01528d99f132
Xuan Yi <saltedfishxx@gmail.com>
2023-08-19 18:29:11 +0800

    Level 1 Completed: Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
 public class Duke {
     public static void main(String[] args) {
         String divider = "\n____________________________________________________________";
@@ -9,8 +14,35 @@ public class Duke {
                 "    　　  `ヽ `ー-'_人`ーﾉ\n" +
                 "    　　　   丶 ￣ _人'彡ﾉ\n" +
                 "   　　　　   ﾉ　r'十ヽ/";
-        System.out.println(logo + divider + "\nHello! I'm MACHO-CATTO! Your personal chat-bot to make your \nday macho!"
-                + "\nWhat can I do for you today?" + divider);
-        System.out.println("Bye. Hope to see you again soon!" + divider);
+
+        String endLogo = "               ＿   ★★EVERYDAY★★\n" +
+                "           ／     j     ★★ IS A  ★★\n" +
+                "        ／     /ｰ'          ★★ MACHO  ★★\n" +
+                "     〈       ヽ               ★★ DAY!!!  ★★\n" +
+                "           ､       ヽ ﾍ⌒ ヽﾌ\n" +
+                "             〉       ´ ･ω )        ,-､、\n" +
+                "           / ノ         ￣⌒ヽ　「　   〉\n" +
+                "          ﾉ       ' L          `ヽ.／   /\n" +
+                "     ／    , '           .ノ＼    ´    /\n" +
+                "    (                ∠_       ヽ､＿,.\n" +
+                "     ＼   (            ヽ ";
+        //Create buffered reader for user input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        //try catch to handle possible IO exceptions
+        try {
+            System.out.println(logo + divider + "\nHello! I'm MACHO-CATTO! Your personal chat-bot to make your \nday macho!"
+                    + "\nWhat can I do for you today?" + divider);
+            String input = br.readLine();
+            //exits the echo commands when input contains 'bye'
+            while (!input.equalsIgnoreCase("bye")) {
+                System.out.println(divider + "\n" + input + divider);
+                input = br.readLine();
+            }
+            System.out.println(divider + "\nBye. Hope to see you again soon!" + divider + "\n" + endLogo);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
