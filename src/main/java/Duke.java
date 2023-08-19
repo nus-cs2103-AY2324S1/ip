34984a70a7042e20ea1ad01156ea09c2af48ed34
Khaleelur <khaleelrrahman2002@gmail.com>
2023-08-19 19:49:38 +0800

    Add salutations.
    Greet the person Hi and goodbye when the person runs the code or exits from it.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,11 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("------------------------------------\n" +
+                "Hello! I'm Khaleelur!\n" +
+                "What can I do for you?\n\n " +
+                "-----------------------------------\n" +
+                "Bye. Hope to see you again soon!");
     }
 }
