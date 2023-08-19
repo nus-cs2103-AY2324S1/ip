99e53b6afb476b32b537f9a957f55eaffe19e906
dishenggg <yeodisheng@u.nus.edu>
2023-08-20 01:14:19 +0800

    Add greet and exit to chat bot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        exit();
+    }
+
+    private static void greet() {
+        System.out.println("Hello! I'm Duke\nWhat can i do for you?\n");
+    }
+
+    private static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
