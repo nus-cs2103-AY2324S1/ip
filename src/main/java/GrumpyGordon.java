bc992638464ec14f4f75535ecb7be851553a40f1
craigtonlian <108467437+craigtonlian@users.noreply.github.com>
2023-08-19 14:58:20 +0800

    added Level-1: Echo

--- src/main/java/GrumpyGordon.java
+++ src/main/java/GrumpyGordon.java
@@ -1,10 +1,23 @@
+import java.util.*;
 public class GrumpyGordon {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Grumpy Gordon.");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Oi! I'm Grumpy Gordon.");
+        System.out.println("    Why are you bothering me?");
+        System.out.println("    ____________________________________________________________");
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        while (!str.equals("bye")) {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    " + str);
+            System.out.println("    ____________________________________________________________");
+            str = sc.nextLine();
+        }
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to never see you again.");
+        System.out.println("    ____________________________________________________________");
     }
 }
