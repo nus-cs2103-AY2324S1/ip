f8d6ae3159fa0b8ecf117007f27f8a2b944f296f
ruishanteo <ruishan.teo03@gmail.com>
2023-08-19 13:46:48 +0800

    completed level 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,59 +20,113 @@ public class Duke {
         System.out.println("_________________________________________");
 
         while (true) {
-            String input = scanner.nextLine();
-            System.out.println("_________________________________________");
-            if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            } else if (input.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < index; i++) {
-                    System.out.println((i + 1) + "." + data[i]);
-                }
-            } else if (input.contains("todo")) {
-                String subInput = input.substring(5);
-                ToDo t = new ToDo(subInput);
-                data[index] = t;
-                index++;
-                System.out.println("Got it. I've added this task:");
-                System.out.println(t);
-                System.out.println("Now you have " + (index) + " tasks in the list.");
+            try {
+                String input = scanner.nextLine();
+                System.out.println("_________________________________________");
+                if (input.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                } else if (input.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < index; i++) {
+                        System.out.println((i + 1) + "." + data[i]);
+                    }
+                } else if (input.contains("todo")) {
+                    String subInput = "";
+                    try {
+                        subInput = input.substring(5);
+                        if (subInput.trim().equals("")) {
+                            throw new DukeException("The description of a todo cannot be empty.");
+                        }
+                    } catch (Exception e) {
+                        throw new DukeException("The description of a todo cannot be empty.");
+                    }
+                    ToDo t = new ToDo(subInput);
+                    data[index] = t;
+                    index++;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(t);
+                    System.out.println("Now you have " + (index) + " tasks in the list.");
 
-            } else if (input.contains("deadline")) {
-                String[] split = input.split(" /by ", 2);
-                Deadline t = new Deadline(split[0].substring(9), split[1]);
-                data[index] = t;
-                index++;
-                System.out.println("Got it. I've added this task:");
-                System.out.println(t);
-                System.out.println("Now you have " + (index) + " tasks in the list.");
-            } else if (input.contains("event")) {
-                String[] split = input.split(" /from ", 2);
-                String description = split[0].substring(6);
-                String[] duration = split[1].split(" /to ", 2);
-                Event t = new Event(description, duration[0], duration[1]);
-                data[index] = t;
-                index++;
-                System.out.println("Got it. I've added this task:");
-                System.out.println(t);
-                System.out.println("Now you have " + (index) + " tasks in the list.");
-            } else if (input.contains("unmark")) {
-                String subInput = input.substring(7);
-                int targetIndex = Integer.parseInt(subInput);
-                System.out.println("Ok, I've marked this task as not done yet:");
-                Task marked = data[targetIndex - 1];
-                marked.markAsUndone();
-                System.out.println(marked);
-            } else if (input.contains("mark")) {
-                String subInput = input.substring(5);
-                int targetIndex = Integer.parseInt(subInput);
-                System.out.println("Nice! I've marked this task as done:");
-                Task marked = data[targetIndex - 1];
-                marked.markAsDone();
-                System.out.println(marked);
-            } else {
-                System.out.println(input + "is invalid");
+                } else if (input.contains("deadline")) {
+                    String[] split = input.split(" /by ", 2);
+                    if (split.length == 1) {
+                        throw new DukeException("Deadlines must have a /by.");
+                    } else if (split[1].trim().equals("")) {
+                        throw new DukeException("/by cannot be empty.");
+                    }
+                    String description = split[0].substring(9);
+                    if (description.trim().equals("")) {
+                        throw new DukeException("The description of a deadline cannot be empty.");
+                    }
+                    Deadline t = new Deadline(description, split[1]);
+                    data[index] = t;
+                    index++;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(t);
+                    System.out.println("Now you have " + (index) + " tasks in the list.");
+                } else if (input.contains("event")) {
+                    String[] split = input.split(" /from ", 2);
+                    if (split.length == 1) {
+                        throw new DukeException("Events must have a /from and /to.");
+                    }
+                    String description = "";
+                    try {
+                        description = split[0].substring(6);
+                        if (description.trim().equals("")) {
+                            throw new DukeException("The description of an event cannot be empty.");
+                        }
+                    } catch (Exception e) {
+                        throw new DukeException("The description of an event cannot be empty.");
+                    }
+
+                    String[] duration = split[1].split(" /to ", 2);
+                    if (duration.length == 1) {
+                        throw new DukeException("Events must have a /from and /to.");
+                    } else if (duration[0].trim().equals("") || duration[1].trim().equals("")) {
+                        throw new DukeException("/from and /to cannot be empty.");
+                    }
+                    Event t = new Event(description, duration[0], duration[1]);
+                    data[index] = t;
+                    index++;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(t);
+                    System.out.println("Now you have " + (index) + " tasks in the list.");
+                } else if (input.contains("unmark")) {
+                    String subInput = input.substring(7);
+                    int targetIndex = -1;
+                    try {
+                        targetIndex = Integer.parseInt(subInput);
+                        if (targetIndex < 1 || targetIndex - 1 > index) {
+                            throw new DukeException("Task number is out of range.");
+                        }
+                    } catch (NumberFormatException e) {
+                        throw new DukeException("Task to be unmarked must be a number.");
+                    }
+                    System.out.println("Ok, I've marked this task as not done yet:");
+                    Task marked = data[targetIndex - 1];
+                    marked.markAsUndone();
+                    System.out.println(marked);
+                } else if (input.contains("mark")) {
+                    String subInput = input.substring(5);
+                    int targetIndex = -1;
+                    try {
+                        targetIndex = Integer.parseInt(subInput);
+                        if (targetIndex < 1 || targetIndex - 1 > index) {
+                            throw new DukeException("Task number is out of range.");
+                        }
+                    } catch (NumberFormatException e) {
+                        throw new DukeException("Task to be marked must be a number.");
+                    }
+                    System.out.println("Nice! I've marked this task as done:");
+                    Task marked = data[targetIndex - 1];
+                    marked.markAsDone();
+                    System.out.println(marked);
+                } else {
+                    throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException exception) {
+                System.out.println(exception.getMessage());
             }
             System.out.println("_________________________________________");
         }
