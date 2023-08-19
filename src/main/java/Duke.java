a603492d9166ad1e6f0b1d5213bc9f98beb32d0e
B-enguin <e0959432@u.nus.edu>
2023-08-19 22:14:34 +0800

    Implemented Level-0 Requirements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        exit();
+    }
+
+    private static void greet() {
+        System.out.println("____________________________________________________________");
+        sendMessage(" Hello! I'm Heimdallr\n" +
+                " What can I do for you?");
+    }
+
+    private static void exit() {
+        sendMessage(" Bye. Hope to see you again soon!");
+    }
+
+    public static void sendMessage(String msg) {
+        System.out.println(msg +
+                "\n____________________________________________________________");
     }
 }
