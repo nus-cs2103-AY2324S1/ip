201290054bcfdb15458a8ec1c92f7d7ed7420d57
Woo Jia Hao <woojiahao1234@gmail.com>
2023-08-19 14:20:49 +0800

    Setup echo of commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,28 @@
-public class Duke {
-    private final static String delimiter = "-".repeat(60);
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        System.out.println(delimiter);
-        System.out.println("Hello! I'm Cyrus\nWhat can I do for you?");
-        System.out.println(delimiter);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(delimiter);
+public class Duke {
+  private final static String delimiter = "-".repeat(60);
+
+  private static void printText(String text, int indentation) {
+    String frontPadding = " ".repeat(indentation);
+    System.out.printf("%s%s\n", frontPadding, delimiter);
+    String[] lines = text.split("\n");
+    for (String line : lines) {
+      System.out.printf("%s%s\n", frontPadding, line);
     }
+    System.out.printf("%s%s\n", frontPadding, delimiter);
+  }
+
+  public static void main(String[] args) {
+    printText("Hello! I'm Cyrus\nWhat can I do for you?", 4);
+    String command = "";
+    Scanner sc = new Scanner(System.in);
+    while (true) {
+      command = sc.nextLine();
+      if (command.equals("bye")) break;
+      printText(command, 4);
+    }
+    ;
+    printText("Bye. Hope to see you again soon!", 4);
+  }
 }
