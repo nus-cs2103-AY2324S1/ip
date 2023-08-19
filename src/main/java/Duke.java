2a08137b15c3f03d808f50a0d370276b7473e8f7
jedkohjk <jedkoh2001@gmail.com>
2023-08-20 03:13:13 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,41 @@
+import java.util.Scanner;
+
 public class Duke {
     private static final int lineLength = 60;
     private static final String horizontalLine = "_".repeat(lineLength);
     private static final String msgIndent = " ";
+    private static final String responseIndent = " ".repeat(4);
     private static final String name = "chatBot";
     private static final String helloMsg = String.format("Hello! I'm %s", name);
     private static final String requestMsg = "What can I do for you?";
     private static final String goodbyeMsg = "Bye. Hope to see you again soon!";
+    private static final Scanner userInput = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println(horizontalLine);
         Duke.printMsg(helloMsg + "\n" + requestMsg);
+        while (true) {
+            String msg = userInput.nextLine();
+            if (msg.equals("bye")) {
+                break;
+            } else {
+                Duke.printMsg(msg);
+            }
+        }
         Duke.printMsg(goodbyeMsg);
     }
 
     private static void printMsg(String msg) {
+        Duke.printResponse(horizontalLine);
         for (String line : msg.split("\n")) {
-            System.out.println(msgIndent + line);
+            Duke.printResponse(msgIndent + line);
         }
-        System.out.println(horizontalLine);
+        Duke.printResponse(horizontalLine);
+        System.out.println();
+    }
+
+    private static void printResponse(String response) {
+        for (String line : response.split("\n")) {
+            System.out.println(responseIndent + line);
+        }
     }
 }
