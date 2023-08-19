3b85c81d54b5f6e87b0ee42a762b89d8cad22eb9
limzhenwy <zwlim38@gmail.com>
2023-08-19 20:32:38 +0800

    Added in the echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String greeting = "Hello! I'm Skog.\n"
-                + "What can I do for you?\n\n"
-                + "Bye. Hope to see you again soon!\n";
+        Scanner sc = new Scanner(System.in);
+        String greeting = "-------------------------------\n"
+                + "Hello! I'm Skog.\n"
+                + "What can I do for you?\n"
+                + "-------------------------------\n";
+
+        String exit = "-------------------------------\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "-------------------------------\n";
+
         System.out.println(greeting);
+
+        while (sc.hasNextLine()) {
+            String str = sc.nextLine();
+            if (!str.equals("bye")) {
+                System.out.println("-------------------------------\n"
+                + str
+                + "\n-------------------------------\n");
+            } else {
+                System.out.println(exit);
+                break;
+            }
+        }
     }
 }
