17366925f94d3f44ec433b04e3714939ad53457c
ItsTYtan <tytan216@gmail.com>
2023-08-19 16:46:30 +0800

    Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String divider = "_____________________________________\n";
+        String opening = "Hello: I'm TY's slave\nWhat can I do for you?\n";
+        String closing = "Bye. Hope to see you again soon!";
+        System.out.println(divider + opening + divider + closing);
     }
 }
