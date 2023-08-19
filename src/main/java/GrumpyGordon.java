7b6f7149ae4dcf0ddb76aa8d9faff3f1617bf6ab
craigtonlian <108467437+craigtonlian@users.noreply.github.com>
2023-08-19 14:43:41 +0800

    added Level-0: Rename, Greet, Exit

---
+++ src/main/java/GrumpyGordon.java
@@ -0,0 +1,10 @@
+public class GrumpyGordon {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Grumpy Gordon.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
