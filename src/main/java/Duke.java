a7f5644976c1cc72463d11133000aad46c610a1d
tjch-o <tzejie.c@gmail.com>
2023-08-19 15:21:08 +0800

    add mark as done and change status back to undone feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Duke {
     private static String divider = "____________________________________________________________";
     private static String indent = "     ";
-    private static ArrayList<String> commands = new ArrayList<>();
+    private static ArrayList<Task> taskList = new ArrayList<>();
 
     public static void formatString(String s) {
         System.out.println(indent + s);
@@ -23,6 +23,48 @@ public class Duke {
         printDivider();
     }
 
+    public static void exit() {
+        printDivider();
+        formatString("Stay safe my friend. Hope to see you again soon man.");
+        printDivider();
+    }
+
+    public static void markTaskAsDone(int taskNum) {
+        Task task = taskList.get(taskNum - 1);
+        task.markAsDone();
+        printDivider();
+        formatString(" Nice! I've marked this task as done:");
+        formatString("  " + task.toString());
+        printDivider();
+    }
+
+    public static void markTaskAsUndone(int taskNum) {
+        Task task = taskList.get(taskNum - 1);
+        task.markAsUndone();
+        printDivider();
+        formatString(" OK, I've marked this task as not done yet:");
+        formatString("  " + task.toString());
+        printDivider();
+    }
+
+    public static int getTaskNumberFromCommand(String command, String input) {
+        String taskNum = input.replace(command + " ", "");
+        return Integer.valueOf(taskNum);
+    }
+
+    public static void printList() {
+        int n = taskList.size();
+
+        printDivider();
+        formatString(" Right now, you have these tasks in your list:");
+        for (int i = 0; i < n; i += 1) {
+            Task currentTask = taskList.get(i);
+            String output = " " + (i + 1) + ". " + currentTask.toString();
+            formatString(output);
+        }
+        printDivider();
+    }
+
     public static void echo() {
         Scanner scanner = new Scanner(System.in);
         String add = "added: ";
@@ -30,38 +72,27 @@ public class Duke {
         while (true) {
             String input = scanner.nextLine();
             
-            if (!input.equals("bye") && !input.equals("list")) {
+            if (input.equals("bye")) {
+                break;
+            } else if (input.equals("list")) {
+                printList();
+            } else if (input.contains("unmark")) {
+                // mark is in the word unmark so this case has to go first
+                int taskNum = getTaskNumberFromCommand("unmark", input);
+                markTaskAsUndone(taskNum);
+            } else if (input.contains("mark")) {
+                int taskNum = getTaskNumberFromCommand("mark", input);
+                markTaskAsDone(taskNum);
+            } else {
                 // store each item in the arraylist
-                commands.add(input);
+                Task newTask = new Task(input);
+                taskList.add(newTask);
                 String output = " " + add + input;
                 printDivider();
                 formatString(output);
                 printDivider();
-            } else if (input.equals("list")) {
-                printList();
-            } else {
-                break;
             }
         }
-    }
-
-    public static void printList() {
-        int n = commands.size();
-
-        printDivider();
-        for (int i = 0; i < n; i += 1) {
-            String currentCommand = commands.get(i);
-            int currentNum = i + 1;
-            String output = " " + currentNum + ". " + currentCommand;
-            formatString(output);
-        }
-        printDivider();
-    }
-
-    public static void exit() {
-        printDivider();
-        formatString("Stay safe my friend. Hope to see you again soon man.");
-        printDivider();
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    private boolean isDone;
+    private String task;
+
+    public Task(String task) {
+        this.isDone = false;
+        this.task = task;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        String status = isDone ? "[X]" : "[ ]";
+        return status + " " + task;
+    }
+}
