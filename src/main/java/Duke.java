d89901cea786845d5dbd1f673278685bef652d12
jedkohjk <jedkoh2001@gmail.com>
2023-08-20 02:49:39 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 public class Duke {
+    private static final int lineLength = 60;
+    private static final String horizontalLine = "_".repeat(lineLength);
+    private static final String msgIndent = " ";
+    private static final String name = "chatBot";
+    private static final String helloMsg = String.format("Hello! I'm %s", name);
+    private static final String requestMsg = "What can I do for you?";
+    private static final String goodbyeMsg = "Bye. Hope to see you again soon!";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(horizontalLine);
+        Duke.printMsg(helloMsg + "\n" + requestMsg);
+        Duke.printMsg(goodbyeMsg);
+    }
+
+    private static void printMsg(String msg) {
+        for (String line : msg.split("\n")) {
+            System.out.println(msgIndent + line);
+        }
+        System.out.println(horizontalLine);
     }
 }
