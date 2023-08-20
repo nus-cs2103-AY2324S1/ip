27cd3ef0ffbd4895cb532f62133add663954ddbc
yytan25 <tan.yang.yi.2001@gmail.com>
2023-08-20 14:41:57 +0800

    Add Level-3 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Duke {
@@ -36,16 +37,26 @@ public class Duke {
     }
 
     public void readInput(String message) {
-        if (message.equals("bye")) {
-            this.isRunning = false;
-        } else if (message.equals("list")) {
-            this.list();
-        } else {
-            tasks.add(new Task(message));
-            numOfTasks++;
-            String output = String.format("Don't expect me to %s for you!", message);
-            System.out.println(output + "\n");
-            printHorizontalLine();
+        switch (message) {
+            case "bye":
+                this.isRunning = false;
+                break;
+            case "list":
+                this.list();
+                break;
+            case "mark":
+                this.markAsComplete();
+                break;
+            case "unmark":
+                this.markAsIncomplete();
+                break;
+            default:
+                tasks.add(new Task(message));
+                numOfTasks++;
+                String output = String.format("Don't expect me to %s for you!", message);
+                System.out.println(output + "\n");
+                printHorizontalLine();
+                break;
         }
     }
 
@@ -58,6 +69,68 @@ public class Duke {
         }
         System.out.println("Don't expect me to remember them for you!");
         printHorizontalLine();
+    }
+
+    public void markAsComplete() {
+        if (this.numOfTasks == 0) {
+            System.out.println("No tasks to mark");
+            printHorizontalLine();
+            return;
+        }
+        int taskNumber = this.launchConfirmationScreen("mark as complete");
+        if (taskNumber != -1) {
+            Task task = tasks.get(taskNumber - 1);
+            if (!task.isCompleted) {
+                task.setCompleted();
+                System.out.printf("Task %d set as complete.%n", taskNumber);
+            } else {
+                System.out.printf("Task %d is already complete." + "%n" +
+                        "Stop wasting my time!", taskNumber);
+            }
+        } else {
+            System.out.println("Request unsuccessful.");
+        }
+        printHorizontalLine();
+    }
+
+    public void markAsIncomplete() {
+        if (this.numOfTasks == 0) {
+            System.out.println("No tasks to mark");
+            printHorizontalLine();
+            return;
+        }
+        int taskNumber = this.launchConfirmationScreen("mark as incomplete");
+        if (taskNumber != -1) {
+            Task task = tasks.get(taskNumber - 1);
+            if (task.isCompleted) {
+                task.setIncomplete();
+                System.out.printf("Task %d set as incomplete.%n", taskNumber);
+            } else {
+                System.out.printf("Task %d is already incomplete." + "%n" +
+                        "Stop wasting my time!", taskNumber);
+            }
+        } else {
+            System.out.println("Request unsuccessful.");
+        }
+        printHorizontalLine();
+    }
+
+    public int launchConfirmationScreen(String message) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Please input the corresponding number of the task you wish to %s.%n",
+                message);
+        printHorizontalLine();
+        for (int i = 0; i < numOfTasks; i++) {
+            System.out.printf("%d. " + tasks.get(i).toString()
+                    + "%n", i+1);
+        }
+        try {
+            int a = sc.nextInt();
+            return (a > numOfTasks ? -1 : a);
+        }
+        catch (InputMismatchException e) {
+            return -1;
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,13 +1,27 @@
 public class Task {
 
-    private final String details;
+    protected String details;
+    protected boolean isCompleted;
 
     public Task(String details) {
         this.details = details;
+        this.isCompleted = false;
+    }
+
+    public void setCompleted() {
+        this.isCompleted = true;
+    }
+
+    public void setIncomplete() {
+        this.isCompleted = false;
     }
 
     @Override
     public String toString() {
-        return this.details;
+        if (this.isCompleted) {
+            return "[X] " + this.details;
+        } else {
+            return "[ ] " + this.details;
+        }
     }
 }
