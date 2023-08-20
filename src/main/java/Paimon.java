c64b90e52502bfba2b4fc5c082fde95eaaa37224
Choonyan02 <choonyan02@gmail.com>
2023-08-20 20:30:54 +0800

    Mark as Done

--- src/main/java/Paimon.java
+++ src/main/java/Paimon.java
@@ -5,12 +5,35 @@ import java.util.Scanner;
 public class Paimon {
 
   private final String name = "Paimon";
-
-  private List<String> tasks = new ArrayList<>();
+  private List<Task> tasks = new ArrayList<>();
 
   public void listTasks() {
-    for (int i = 0; i < tasks.size(); i++) {
-      System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+    System.out.println(" Here are the tasks in your list:");
+    for (int i = 0; i < this.tasks.size(); i++) {
+      System.out.println(" " + (i + 1) +
+              ".[" + this.tasks.get(i).getStatusIcon() +
+              "] " + this.tasks.get(i).toString());
+    }
+  }
+
+  public void readTask(String command, Task t) {
+    if (command.equals("list")) {
+      this.listTasks();
+    } else if (command.startsWith("mark")) {
+      System.out.println(" Nice! I've marked this task as done:");
+      char c = command.charAt(command.length() - 1);
+      int num = c - '0';
+      this.tasks.get(num - 1).markAsDone();
+      System.out.println("   [X] " + this.tasks.get(num - 1).toString());
+    } else if (command.startsWith("unmark")) {
+      System.out.println(" OK, I've marked this task as not done yet:");
+      char c = command.charAt(command.length() - 1);
+      int num = c - '0';
+      this.tasks.get(num - 1).markAsNotDone();
+      System.out.println("   [ ] " + this.tasks.get(num - 1).toString());
+    } else {
+      System.out.println(" added: " + command);
+      this.tasks.add(t);
     }
   }
 
@@ -22,17 +45,14 @@ public class Paimon {
 
     Scanner scanner = new Scanner(System.in);
     String command = scanner.nextLine();
+    Task t = new Task(command);
 
     while (!command.equals("bye")) {
       System.out.println("---------------------------------------------------------------");
-      if (!command.equals("list")) {
-        System.out.println(" added: " + command);
-        this.tasks.add(command);
-      } else {
-        this.listTasks();
-      }
+      this.readTask(command, t);
       System.out.println("---------------------------------------------------------------");
       command = scanner.nextLine();
+      t = new Task(command);
     }
 
     scanner.close();
