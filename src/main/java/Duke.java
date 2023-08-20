5ffa380033bf4fd3537def7eb97a34924ac9cb35
LoMaply <linuslee71@gmail.com>
2023-08-20 14:59:34 +0800

    Add end command, chatbot repeats user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
 public class Duke {
+    /**
+     * Formats chatbot output with borders, first line indentation and new line at the end
+     * @param message message to be displayed to user
+     */
+    public static void output(String message) {
+        String line = "    ____________________________________________________________\n";
+        System.out.println(line + "     " + message + "\n" + line);
+    }
     public static void main(String[] args) {
-        String line = "____________________________________________________________\n";
-        String greet = " Hello! I'm Jyuuni\n What can i do for you?\n";
-        String bye = " Bye. Hope to see you again soon!\n";
-        System.out.println(line + greet + line + bye + line);
+        Duke.output("Hey I'm Jyuuni, your helpful assistant\n     How can I help you?");
+
+        Scanner userInput = new Scanner(System.in);
+        boolean isRun = true;
+
+        // Waits for input until program stops running
+        while (isRun) {
+            String command = userInput.nextLine();
+            if (command.equals("end")) {
+                Duke.output("Come back if you need anything else!");
+                isRun = false;
+            } else {
+                Duke.output(command);
+            }
+        }
     }
 }
