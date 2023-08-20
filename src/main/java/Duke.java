0da65806e8166ca452f483919cb2ba5a5a18792d
Yeo Boh Shin <110957811+YeoBohShin@users.noreply.github.com>
2023-08-21 00:01:01 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 public class Duke {
 
     private boolean active = true;
+    private List<String> list = new ArrayList<>();
 
     private void greet() {
         System.out.println("Hello! I'm Aikent\n" + "What can I do for you?");
@@ -21,6 +24,22 @@ public class Duke {
         }
     }
 
+    private void add(String msg) {
+        switch (msg) {
+            case "bye":
+                exit();
+                break;
+            case "list":
+                for (int index = 0; index < this.list.size(); index++) {
+                    System.out.println(index + ". " + list.get(index));
+                }
+                break;
+            default:
+                this.list.add(msg);
+                System.out.println("added: " + msg);
+        }
+    }
+
     public static void main(String[] args) {
         Duke bot = new Duke();
         Scanner sc = new Scanner(System.in);
@@ -29,7 +48,7 @@ public class Duke {
 
         while (bot.active) {
             String input = sc.nextLine();
-            bot.echo(input);
+            bot.add(input);
         }
     }
 }
