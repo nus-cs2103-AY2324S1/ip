d4624f71ce04dbf6ad9b435ea75a9e13a41a527f
shuenj <60507123+shuenj@users.noreply.github.com>
2023-08-20 15:46:37 +0800

    add Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void markAsDone(boolean done) {
+        isDone = done;
+    }
+
+    @Override
+    public String toString(){
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
