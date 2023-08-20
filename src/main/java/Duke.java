9a32bb5506211c787856fb019183aaab3cdbc9be
tayruxin <tayruxin00@gmail.com>
2023-08-20 15:42:21 +0800

    Implement Level 1: Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         System.out.println("Hello! I'm Muddy\n" + "What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = in.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            if (input.equalsIgnoreCase("hello")) {
+                System.out.println("Hello, what can I help you with?");
+                continue;
+            }
+
+            System.out.println(input);
+        }
+        in.close();
     }
 }
