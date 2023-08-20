0a62e2057853892afd1a65471977ae14d87bf532
DS <yeodisheng@gmail.com>
2023-08-20 15:16:29 +0800

    Added mark as done to chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,13 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Duke {
-    private static TaskList taskList = new TaskList();
+    protected static ArrayList<Task> taskList = new ArrayList<Task>();
+    private static final Pattern markPattern = Pattern.compile("mark\\s(\\d+)\\b$");
+    private static final Pattern unmarkPattern = Pattern.compile("unmark\\s(\\d+)$");
+    private static final Pattern indexPattern = Pattern.compile("(\\d+)$");
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -10,16 +16,27 @@ public class Duke {
             String input = scanner.nextLine();
 
             if (input.equals("bye")) {
-                break;
+                exit();
+                return;
             } else if (input.equals("list")) {
-                System.out.println(taskList.toString());
+                System.out.println(listString());
+            } else if (markPattern.matcher(input).find()) {
+                int idx = parseMark(input);
+                if (idx != 0) {
+                    taskList.get(idx - 1).markAsDone();
+                }
+            } else if (unmarkPattern.matcher(input).find()) {
+                int idx = parseMark(input);
+                if (idx != 0) {
+                    taskList.get(idx - 1).markAsNotDone();
+                }
+            } else if (input.isEmpty()) {
+                System.out.println("No Input");
             } else {
-                taskList.add(input);
+                taskList.add(new Task(input));
                 System.out.println("added: " + input);
             }
-
         }
-        exit();
     }
 
     private static void greet() {
@@ -28,5 +45,32 @@ public class Duke {
 
     private static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static int parseMark(String input) {
+        Matcher m = indexPattern.matcher(input);
+        int res = 0;
+        if (m.find()) {
+            String idx = m.group(1);
+            res = Integer.parseInt(idx);
+        }
+        if (res > taskList.size()) {
+            System.out.println("Task " + res + " does not exist");
+            res = 0;
+        }
+        return res;
+    }
+
+    private static String listString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < taskList.size(); i++) {
+            sb.append(i + 1);
+            sb.append(".[");
+            sb.append(taskList.get(i).getStatusIcon());
+            sb.append("] ");
+            sb.append(taskList.get(i).getDescription());
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon(){
+        return (isDone ? "X" : " ");
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+}
