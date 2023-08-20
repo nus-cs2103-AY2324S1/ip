bc05bab3efcfcbcd2984712664d0d87d340b5520
freddychenyouren2 <freddychenyouren2@gmail.com>
2023-08-21 00:24:34 +0800

    Finished Level-1 increment with some personality modification for my chat bot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,54 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class Duke {
-    static String HORIZONTAL_LINE = "________________________________________";
+    static String HORIZONTAL_LINE = "    ________________________________________"; //40 underscores.
+    static String INDENT = "    "; //4 spaces.
 
     /**
      * Function to greet the User.
-     * @return A Horizontal Line and a greeting statement.
      */
     public static void greet() {
         System.out.println(HORIZONTAL_LINE);
-        System.out.println("Hello! I'm SeeWhyAre Bot!");
-        System.out.println("What can I do for you?");
+        System.out.println("    Hello! I'm SeeWhyAre Bot!");
+        System.out.println("    What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
     }
 
     /**
      * Function to say goodbye to the User and end the program.
-     * @return A Horizontal Line and a farewell statement.
      */
     public static void farewell() {
         System.out.println(HORIZONTAL_LINE);
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("    You are closing the SeeWhyAre chat bot.");
+        System.out.println("    Bye bye. Please use me again soon!");
+        System.out.println(HORIZONTAL_LINE);
     }
+
+    public static void echo(String input) {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(String.format("    %s", input));
+        System.out.println(HORIZONTAL_LINE);
+    }
+
     public static void main(String[] args) {
         greet();
-        farewell();
-        System.out.println(HORIZONTAL_LINE);
+        try (Scanner scanner = new Scanner(System.in)) {
+            Boolean repeatFlag = true;
+            while (repeatFlag) {
+                String userInput = scanner.nextLine();
+                String formattedInput = userInput.toLowerCase();
+                if (formattedInput.equals("bye")) {
+                    farewell();
+                    repeatFlag = false;
+                } else {
+                    echo(userInput);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid command! Please enter valid commands");
+            System.out.println(HORIZONTAL_LINE);
+        }
+
 
     }
 }
