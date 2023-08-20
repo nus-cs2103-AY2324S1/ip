b409b820c166293d2f28782451fdaf718ef1873f
Janssen <janssenlau88@gmail.com>
2023-08-20 20:11:10 +0800

    Added Add and List functionality. Made inputs case insensitive.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,29 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         String greeting = "Hello! I'm Arona, your Virtual Assistant.\n    What can I do for you today?\n";
         String exit = "Goodbye. Hope to see you again soon!";
+        ArrayList<String> tasks = new ArrayList<>();
 
         System.out.println("    " + greeting);
 
         Scanner scanner = new Scanner(System.in);
-        String echo = scanner.nextLine();
+        String echo = scanner.nextLine().toLowerCase().trim();
 
-        while (!echo.toLowerCase().equals("bye")) {
-            if (!echo.trim().isEmpty()) { // Check for non-empty input
-                System.out.println("    " + echo + "\n");
+        while (!echo.equals("bye")) {
+            if (echo.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println("    " +(i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println("");
+            } else if (!echo.isEmpty()) { // Check for non-empty input
+                tasks.add(echo);
+                System.out.println("    added: " +  echo + "\n");
             }
-            echo = scanner.nextLine();
+            echo = scanner.nextLine().toLowerCase().trim();
         }
-
         System.out.println("    " + exit);
     }
 }
