a39775d5b523b469f994558ef618b660940f23a3
Janssen <janssenlau88@gmail.com>
2023-08-20 19:52:37 +0800

    Added Echo functionality.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String greeting = "Hello! I'm Arona.\n" + "What can I do for you?\n";
-        String exit = "Bye. Hope to see you again soon!";
+        String greeting = "Hello! I'm Arona, your Virtual Assistant.\n    What can I do for you today?\n";
+        String exit = "Goodbye. Hope to see you again soon!";
 
-        System.out.println(greeting);
-        System.out.println(exit);
+        System.out.println("    " + greeting);
+
+        Scanner scanner = new Scanner(System.in);
+        String echo = scanner.nextLine();
+
+        while (!echo.toLowerCase().equals("bye")) {
+            if (!echo.trim().isEmpty()) { // Check for non-empty input
+                System.out.println("    " + echo + "\n");
+            }
+            echo = scanner.nextLine();
+        }
+
+        System.out.println("    " + exit);
     }
 }
