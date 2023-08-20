76e343f4a010c3e32350a774fab484e9df622f1f
Loo Mei Lin Zann <>
2023-08-20 20:10:09 +0800

    add TehO.java

---
+++ src/main/java/TehO.java
@@ -0,0 +1,7 @@
+public class TehO {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm TehO \n"
+                + "What can I do for you? \n"
+                + "Bye. Hope to see you again soon!");
+    }
+}
