8ddad67a05fdc80b67367796ad421d2ca70c9985
lshaoqin <74029583+lshaoqin@users.noreply.github.com>
2023-08-20 19:19:38 +0800

    mark as done and undone

--- src/main/java/Juke.java
+++ src/main/java/Juke.java
@@ -7,17 +7,31 @@ public class Juke {
     }
     public static void main(String[] args) {
         class Task {
-            private final String type;
-            private final String desc;
+            protected final String type;
+            protected final String desc;
+            protected boolean isDone;
 
             public Task(String type, String desc) {
                 this.type = type;
                 this.desc = desc;
+                this.isDone = false;
+            }
+
+            public String getStatusIcon() {
+                return (isDone ? "[X] " : "[ ] "); // mark done task with X
+            }
+
+            public void markAsDone() {
+                this.isDone = true;
+            }
+
+            public void markAsUndone() {
+                this.isDone = false;
             }
 
             @Override
             public String toString() {
-                return desc;
+                return this.getStatusIcon() + desc;
             }
         }
         ArrayList<Task> tasks = new ArrayList<Task>();
@@ -44,7 +58,27 @@ public class Juke {
                     count++;
                 }
                 printLine();
+                continue;
             }
+
+            if (input.contains("unmark")) {
+                int index = Integer.parseInt(input.substring(7));
+                Task currTask = tasks.get(index - 1);
+                currTask.markAsUndone();
+                System.out.println("OK, I've marked this task as not done yet: \n" + currTask.toString());
+                printLine();
+                continue;
+            }
+
+            if (input.contains("mark")) {
+                int index = Integer.parseInt(input.substring(5));
+                Task currTask = tasks.get(index - 1);
+                currTask.markAsDone();
+                System.out.println("Nice! I've marked this task as done: \n" + currTask.toString());
+                printLine();
+                continue;
+            }
+
             else {
                 tasks.add(new Task("todo", input));
                 System.out.println("added: " + input);
