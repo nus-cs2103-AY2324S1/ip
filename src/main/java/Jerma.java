8221ba280b7413f44ee79055aef4976f5cee3314
Jason <jiacan.h01@gmail.com>
2023-08-20 15:42:02 +0800

    Initialise scanner

--- src/main/java/Jerma.java
+++ src/main/java/Jerma.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Jerma {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Jerma.");
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
         System.out.println("See ya soon!");
     }
 }
