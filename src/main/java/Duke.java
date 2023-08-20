b5883fd5488aa711bdc8bbf1d2f450f2cd5d1e27
limzhenwy <zwlim38@gmail.com>
2023-08-20 14:34:11 +0800

    List feature etc...

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,34 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    public static Scanner sc = new Scanner(System.in);
+    public static String[] listArr = new String[100];
+    public static void add(String s) {
+        for (int i = 0; i < 100; i++) {
+            if (listArr[i] == null) {
+                listArr[i] = s;
+                System.out.println("-------------------------------\n"
+                        + "added: " + s
+                        + "\n-------------------------------\n");
+                break;
+            }
+        }
+    }
+
+    public static void listOut(String[] arr) {
+        System.out.println("-------------------------------");
+        for (int i = 0; i < 100; i++) {
+            if (arr[i] != null) {
+                int num = i + 1;
+                System.out.println(num + ". " + arr[i]);
+            }
+        }
+        System.out.println("-------------------------------\n");
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
         String greeting = "-------------------------------\n"
                 + "Hello! I'm Skog.\n"
                 + "What can I do for you?\n"
@@ -16,13 +42,12 @@ public class Duke {
 
         while (sc.hasNextLine()) {
             String str = sc.nextLine();
-            if (!str.equals("bye")) {
-                System.out.println("-------------------------------\n"
-                + str
-                + "\n-------------------------------\n");
-            } else {
+            if (str.equals("bye")) {
                 System.out.println(exit);
-                break;
+            } else if (str.equals("list")) {
+                listOut(listArr);
+            } else {
+                add(str);
             }
         }
     }
