dc30400306dfb42444bab243eb3aa236d49eeaec
laurenlim2112 <laurenkoopa@gmail.com>
2023-08-20 16:25:02 +0800

    Add Task class to represent tasks and ability to mark as done/not done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,34 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello, it's Spot!");
-        String goodbye = "Spot's going to take a nap now. Goodnight!";
-        String[] tasks = new String[100];
-        int index = 0;
+        final String HELLO_MESSAGE = "Hello, it's Spot!";
+        final String GOODBYE_MESSAGE = "Spot's going to take a nap now. Goodnight!";
+        System.out.println(HELLO_MESSAGE);
+        ArrayList<Task> tasks = new ArrayList<>();
         Scanner myObj = new Scanner(System.in);
-        String command = myObj.nextLine();  // Read user input
+        String command = myObj.nextLine();
         while (!command.equals("bye")) {
-            if (!command.equals("list")) {
-                tasks[index] = command;
-                System.out.println("added: " + command);
-                index += 1;
-            } else {
-                for (int i = 0; i < index; i++) {
+            String[] keywords = command.split(" ");
+            if (keywords[0].equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
                     int position = i + 1;
-                    System.out.println(position + ". " + tasks[i]);
+                    System.out.println(position + ". " + tasks.get(i).toString());
                 }
+            } else if (keywords[0].equals("mark")) {
+                int position = Integer.parseInt(command.substring(5));
+                tasks.get(position - 1).markAsDone();
+            } else if (keywords[0].equals("unmark")) {
+                int position = Integer.parseInt(command.substring(7));
+                tasks.get(position - 1).markAsNotDone();
+            } else {
+                tasks.add(new Task(command));
+                System.out.println("added: " + command);
             }
             command = myObj.nextLine();
         }
-        System.out.println(goodbye);
+        System.out.println(GOODBYE_MESSAGE);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+    private String description;
+    private boolean done;
+
+    public Task(String description) {
+        this.description = description;
+        this.done = false;
+    }
+
+    public void markAsDone() {
+        this.done = true;
+        System.out.println("Wow! Spot has marked this task as done!");
+        System.out.println(this.toString());
+    }
+
+    public void markAsNotDone() {
+        this.done = false;
+        System.out.println("Spot will mark this task as not done yet then!");
+        System.out.println(this.toString());
+    }
+
+    public String getStatusIcon() {
+        if (this.done) {
+            return "X";
+        } else {
+            return " ";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "]" + " " + this.description;
+    }
+}
