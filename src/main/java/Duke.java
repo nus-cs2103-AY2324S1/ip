cfc802cab7b661ba55591e433a593f324a77bd7d
jingting1412 <e0970352@u.nus.edu>
2023-08-21 00:01:03 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,11 +10,13 @@ public class Duke {
         //this will be the greeting
         String greeting = "        Hello! I'm " + name + "\n        What can I do for you?\n";
         //this will be goodbye
-        String goodbye = "        Bye. Hope to see you again soon! \n";
+        String goodbye = "        Bye. Hope to see you again soon! :D\n";
         //scanner to get user input
         Scanner sc = new Scanner(System.in);
         //this is what the user typed in
         String userCommand;
+        //fixed size array to store the items
+        ArrayList<String> toDo = new ArrayList<>();
 
         //initial greeting
         System.out.println(lines + "\n" + greeting + lines);
@@ -21,7 +24,17 @@ public class Duke {
         userCommand = sc.nextLine();
         //echoes the user input when the command is not "bye"
         while (!userCommand.equals("bye")) {
-            System.out.println(lines + "\n        " + userCommand + "\n" + lines);
+            if (userCommand.equals("list")) {
+                System.out.println(lines);
+                for (int i = 0; i < toDo.size(); i++) {
+                    System.out.println("        " + Integer.toString(i + 1) + ". " + toDo.get(i));
+                }
+                System.out.println(lines);
+            } else {
+                System.out.println(lines + "\n        added: " + userCommand + "\n" + lines);
+                toDo.add(userCommand);
+            }
+
             userCommand = sc.nextLine();
         }
         //when the command is "bye", exit
