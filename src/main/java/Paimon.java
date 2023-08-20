5db8b28c9d9243b6d9538f2a3c71e9bb63053408
Choonyan02 <choonyan02@gmail.com>
2023-08-20 18:25:06 +0800

    Echo

--- src/main/java/Paimon.java
+++ src/main/java/Paimon.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
+
 public class Paimon {
   public final String name = "Paimon";
 
   public void greetAndFarewell() {
     System.out.println("---------------------------------------------------------------");
-    System.out.println("Hello! I'm  " + name);
-    System.out.println("What can I do for you?");
+    System.out.println(" Hello! I'm  " + name);
+    System.out.println(" What can I do for you?");
     System.out.println("---------------------------------------------------------------");
-    System.out.println("Bye. Hope to see you again soon!");
+
+    Scanner scanner = new Scanner(System.in);
+    String command = scanner.nextLine();
+
+    while (!command.equals("bye")) {
+      System.out.println("---------------------------------------------------------------");
+      System.out.println(" " + command);
+      System.out.println("---------------------------------------------------------------");
+      command = scanner.nextLine();
+    }
+
+    scanner.close();
+    System.out.println("---------------------------------------------------------------");
+    System.out.println(" Bye. Hope to see you again soon!");
     System.out.println("---------------------------------------------------------------");
   }
 
