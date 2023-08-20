885b6fd0aa17695d8a07d1a3b5ff4ff25ba564fa
sk2001git <gundamsean22@gmail.com>
2023-08-21 00:52:35 +0800

    Added a storage to store words that are inputted. Has a limit of 100

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] textStorage = new String[100];
+        int storageIndex = 0;
 
         String chatBotName = "Benedit Cucumber Badge";
         System.out.println("Hello! I'm " + chatBotName);
@@ -14,7 +17,14 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 dontTerminate = false;
+            } else if(input.equals("list")) {
+                for (int i = 0; i < storageIndex; i++) {
+                    int itemNumber = i + 1;
+                    System.out.println(itemNumber + ". " + textStorage[i]);
+                }
             } else {
+                textStorage[storageIndex] = input;
+                storageIndex++;
                 System.out.println(input);
             }
         }
