379555c0607b4b0b54d982e4a9e0b5da06a8cb75
markgcera <>
2023-08-21 01:45:41 +0800

    Add Task class. Complete Level 3.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,7 @@ public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input;
-        List<String> inputList = new ArrayList<>();
+        List<Task> taskList = new ArrayList<>();
 
         String initMsg = "____________________________________________________________\n"
                 + " Hello! I'm IRIS\n"
@@ -14,17 +14,32 @@ public class Duke {
         System.out.println(initMsg);
 
         input = scanner.nextLine();
-        int count = 1;
 
         while (!input.equals("bye")) {
             System.out.println("____________________________________________________________");
+            String[] inputParts = input.split(" ", 3);
+            String command = inputParts[0];
+
             if (input.equals("list")) {
-                for (String userInput : inputList) {
-                    System.out.println(userInput);
+                int count = 1;
+                for (Task task : taskList) {
+                    System.out.println(count++ + ". " + task);
                 }
+            } else if (command.equals("mark")) {
+                int index = Integer.parseInt(inputParts[1]) - 1;
+                taskList.get(index).markDone();
+                System.out.println("Nice! I've marked this task as done:\n"
+                + taskList.get(index));
+            } else if (command.equals("unmark")) {
+                int index = Integer.parseInt(inputParts[1]) - 1;
+                taskList.get(index).markUndone();
+                System.out.println("OK, I've marked this task as not done yet:\n"
+                        + taskList.get(index));
             } else {
-                inputList.add(count++ + " " + input);
-                System.out.println("added: " + input);
+                Task newTask = new Task(input);
+                taskList.add(newTask);
+                System.out.println("Understood sir, I've added the task below:\n"
+                + newTask);
             }
             System.out.println("____________________________________________________________");
             input = scanner.nextLine();
---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    public void markUndone() {
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.getDescription();
+    }
+}
