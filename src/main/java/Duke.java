99410bdab96fe00d3cab19663d4dde39138d0990
Inez Kok <kfyinez@gmail.com>
2023-08-20 20:03:59 +0800

    Implement Level-0: Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
 public class Duke {
+    // Constructor for Duke
+    public Duke() {
+
+    }
+
+    // Constants
+    private static final String name = "Beep Boop";
+    private static final String line = "─".repeat(100);
+
+    public void printMessage(String message) {
+        System.out.printf("\t%s\n", message);
+        System.out.println(line);
+    }
+
+    public void greet() {
+        String greeting = String.format("Hello! I'm %s\n\tWhat can I do for you?\n", name);
+        System.out.println(line);
+        printMessage(greeting);
+    }
+
+    public void exit() {
+        String exiting = "Bye. Hope to see you again soon!\n";
+        printMessage(exiting);
+    }
+
+    public void runDuke() {
+        greet();
+        exit();
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.runDuke();
     }
 }
