3d8e74cbd329184248b51d6cd4843abb95efff39
WZWren <115881359+WZWren@users.noreply.github.com>
2023-08-20 21:01:17 +0800

    Renamed the Duke file to TrackerBot.

--- src/main/java/TrackerBot.java
+++ src/main/java/TrackerBot.java
@@ -1,4 +1,12 @@
-public class Duke {
+/**
+ * Main Program for the application. <br>
+ * As of Level-0, this has been renamed from Duke to TrackerBot
+ * as part of the requirements for the iP.
+ *
+ * @author WZWren
+ * @version Level-0
+ */
+public class TrackerBot {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
