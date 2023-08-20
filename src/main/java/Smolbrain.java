eca12aae0f79ae0efe9de1eefbf466619b0d5ee7
pzl111 <phuazailian@gmail.com>
2023-08-20 16:17:39 +0800

    Completed Level-3

--- src/main/java/Smolbrain.java
+++ src/main/java/Smolbrain.java
@@ -9,7 +9,7 @@ public class Smolbrain {
         System.out.println("____________________________________________________________\n");
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> data = new ArrayList<>();
+        ArrayList<Task> data = new ArrayList<>();
 
         while(true) {
             String input = sc.nextLine();
@@ -23,13 +23,29 @@ public class Smolbrain {
 
             switch (input) {
                 case "list":
+                    System.out.println("Here are the tasks in your list: ");
                     for (int i = 0; i < data.size(); i++) {
-                        System.out.println((i+1) + ". " + data.get(i));
+                        System.out.println((i+1) + ". " + data.get(i).display());
                     }
                     break;
 
                 default:
-                    data.add(input);
+                    if (input.contains("unmark")){
+                        int num = Integer.parseInt(input.replace("unmark ", "")) - 1;
+                        System.out.println("OK, I've marked this task as not done yet: ");
+                        data.get(num).unmark();
+                        System.out.println(data.get(num).display());
+                        break;
+                    } else if (input.contains("mark")){
+                        int num = Integer.parseInt(input.replace("mark ", "")) - 1;
+                        System.out.println("Nice! I've marked this task as done: ");
+                        data.get(num).mark();
+                        System.out.println(data.get(num).display());
+                        break;
+                    }
+                    
+                    Task task = new Task(input);
+                    data.add(task);
                     System.out.println("added: " + input);
                     break;
             }
