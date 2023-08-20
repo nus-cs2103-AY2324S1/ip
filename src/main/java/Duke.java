715a36d09dca866bf6c1db2ebb26d1b17605a735
jedkohjk <jedkoh2001@gmail.com>
2023-08-20 17:12:54 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,11 +11,16 @@ public class Duke {
     private static final String name = "chatBot";
     private static final String helloMsg = String.format("Hello! I'm %s", name);
     private static final String requestMsg = "What can I do for you?";
+    private static final String addedMsg = "added: %s";
+    private static final String outOfRangeMsg = "Task %d is out of range. You only have %d tasks.";
+    private static final String markMsg = "Nice! I've marked this task as done:\n  %s";
+    private static final String unmarkMsg = "OK, I've marked this task as not done yet:\n  %s";
     private static final String goodbyeMsg = "Bye. Hope to see you again soon!";
     private static final Scanner userInput = new Scanner(System.in);
+    private static final Pattern markCommand = Pattern.compile("^((un)?)mark [1-9]([0-9]*)$");
 
     public static void main(String[] args) {
-        List<String> tasks = new ArrayList<String>();
+        List<Task> tasks = new ArrayList<Task>();
         Duke.printMsg(helloMsg + "\n" + requestMsg);
         while (true) {
             String msg = userInput.nextLine();
@@ -22,9 +28,24 @@ public class Duke {
                 break;
             } else if (msg.equals("list")) {
                 Duke.printMsg(Duke.stringifyList(tasks));
+            } else if (markCommand.matcher(msg).matches()) {
+                String[] cmd = msg.split(" ");
+                int idx = Integer.parseInt(cmd[1]);
+                if (idx > tasks.size()) {
+                    Duke.printMsg(String.format(outOfRangeMsg, idx, tasks.size()));
+                    continue;
+                }
+                idx--;
+                if (cmd[0].equals("mark")) {
+                    tasks.get(idx).mark();
+                    Duke.printMsg(String.format(markMsg, tasks.get(idx).toString()));
+                } else {
+                    tasks.get(idx).unmark();
+                    Duke.printMsg(String.format(unmarkMsg, tasks.get(idx).toString()));
+                }
             } else {
-                tasks.add(msg);
-                Duke.printMsg(String.format("added: %s", msg));
+                tasks.add(new Task(msg));
+                Duke.printMsg(String.format(addedMsg, msg));
             }
         }
         Duke.printMsg(goodbyeMsg);
