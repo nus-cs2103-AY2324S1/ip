b90841609493b0ca3fe306a21f79d17af1c3ad69
Lim Ngee Yong <e0940691@u.nus.edu>
2023-08-21 01:52:36 +0800

    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import javax.sound.midi.Soundbank;
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
@@ -11,14 +12,15 @@ public class Duke {
 
     public static void main(String[] args) {
         String action;
-        ArrayList<String> history = new ArrayList<>();
+        ArrayList<Task> history = new ArrayList<>();
         Duke.sayHi();
         Scanner reader = new Scanner(System.in);
-        action = reader.next().toString();
-        while (!action.equals(termination_word)) {
-            history.add(action);
-            Duke.actions(action, history);
-            action = reader.next().toString();
+        action = reader.nextLine().toString();
+        String check = action.toUpperCase();
+        while (!check.equals(termination_word)) {
+            history = Duke.actions(action, history);
+            action = reader.nextLine().toString();
+            check = action.toUpperCase();
         }
         Duke.sayBye();
 
@@ -38,8 +40,11 @@ public class Duke {
 
 
 
-    private static void actions(String action, ArrayList history) {
+    private static ArrayList<Task> actions(String action, ArrayList<Task> history) {
         action = action.toUpperCase();
+        String[] parts = action.split(" ",2);
+        action = parts[0];
+        String numberString = parts.length > 1 ? parts[1] : "";
         switch (action) {
             case "BYE":
                 System.out.println();
@@ -52,10 +57,26 @@ public class Duke {
                     System.out.println(i+1 + ": " + history.get(i));
                 }
                 break;
+            case "MARK":
+                try {
+                    Integer number = Integer.parseInt(numberString);
+                    if (number > history.size() || number < 0) {
+                        throw new InvalidParameterException("Wrong Param");
+                    }
+                    Task task = history.get(number-1);
+                    task.markAsDone();
+                    System.out.println("YONG has marked this task as completed! \n" + task.toString());
+                }
+                catch (Exception e) {
+                    System.out.println("Invalid integer input");
+                }
+                break;
             default:
-                action = action.substring(0,1).toUpperCase() + action.substring(1).toLowerCase();
+                Task currAction = new Task(action);
+                history.add(currAction);
                 System.out.println(action);
         }
+        return history;
     }
 
 
