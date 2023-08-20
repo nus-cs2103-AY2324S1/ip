14540e738fb378cd00d89c31a13a5b8f0e94cbe0
ItsTYtan <tytan216@gmail.com>
2023-08-20 20:53:04 +0800

    Delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,43 +8,55 @@ public class Duke {
         System.out.println(Constants.opening);
         while (true) {
             String input = sc.nextLine();
-            if (input.equals("bye")) {
-                System.out.println(Constants.closing);
-                break;
-            } else if (input.startsWith("mark ")) {
-                int idx = Integer.parseInt(input.substring(5));
-                lst.get(idx - 1).markDone();
-                System.out.println("Nice! I've marked this task as done");
-                System.out.println("\t" + lst.get(idx - 1));
-            } else if (input.startsWith("unmark ")) {
-                int idx = Integer.parseInt(input.substring(7));
-                lst.get(idx - 1).unMarkDone();
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("\t" + lst.get(idx - 1));
-            } else if (input.equals("list")) {
-                System.out.println(Constants.divider + "\n" + "Here are the tasks in your list:");
-                    for (int i = 0; i < lst.size(); i++) {
-                        System.out.println((i + 1) + "." + lst.get(i));
-                    }
-                System.out.println(Constants.divider + "\n");
-            } else {
-                String[] arr = input.split(" ", 2);
-                Task task;
-                try {
-                    switch (arr[0]) {
-                        case "todo":
+            String[] arr = input.split(" ", 2);
+            try {
+                switch (arr[0]) {
+                    case "bye":
+                        System.out.println(Constants.closing);
+                        return;
+                    case "mark":
+                        int idx1 = Integer.parseInt(arr[1]);
+                        lst.get(idx1 - 1).markDone();
+                        System.out.println("Nice! I've marked this task as done");
+                        System.out.println("\t" + lst.get(idx1 - 1));
+                        break;
+                    case "unmark":
+                        int idx2 = Integer.parseInt(arr[1]);
+                        lst.get(idx2 - 1).unMarkDone();
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println("\t" + lst.get(idx2 - 1));
+                        break;
+                    case "list":
+                        System.out.println(Constants.divider + "\n" + "Here are the tasks in your list:");
+                        for (int i = 0; i < lst.size(); i++) {
+                            System.out.println((i + 1) + "." + lst.get(i));
+                        }
+                        System.out.println(Constants.divider + "\n");
+                        break;
+                    case "delete":
+                        int idx3 = Integer.parseInt(arr[1]);
+                        Task temp = lst.get(idx3 -1);
+                        lst.remove(idx3 - 1);
+                        String output1 = Constants.divider + "\nNoted. I've removed this task:\n" + "    " + temp
+                                + "\nNow you have " + Integer.toString(lst.size())
+                                + " tasks in the list.\n" + Constants.divider + "\n";
+                        System.out.println(output1);
+                        break;
+                    case "todo":
+                    case "deadline":
+                    case "event":
+                        Task task;
+                        if (arr[0].equals("todo")) {
                             if (arr.length == 1) throw new IncompleteDescriptionException();
                             task = ToDo.create(arr[1]);
                             lst.add(task);
-                            break;
-                        case "deadline":
+                        } else if (arr[0].equals("deadline")) {
                             if (arr.length == 1) throw new IncompleteDescriptionException();
                             String[] arr2 = arr[1].split(" /by ");
                             if (arr2.length == 1) throw new IncompleteDescriptionException();
                             task = Deadline.create(arr2[0], arr2[1]);
                             lst.add(task);
-                            break;
-                        case "event":
+                        } else {
                             if (arr.length == 1) throw new IncompleteDescriptionException();
                             String[] arr3 = arr[1].split(" /from ");
                             if (arr3.length == 1) throw new IncompleteDescriptionException();
@@ -55,24 +67,22 @@ public class Duke {
                             String to = arr4[1];
                             task = Event.create(taskName, from, to);
                             lst.add(task);
-                            break;
-                        default:
-                            throw new UnknownCommandException();
-                    }
-                } catch (IncompleteDescriptionException ex) {
-                    System.out.println(ex);
-                    continue;
-                } catch (UnknownCommandException ex) {
-                    System.out.println(ex);
-                    continue;
+                        }
+                        String output2 = Constants.divider + "\n"
+                                + "Got it. I've added this task:\n"
+                                + "    " + task.toString()
+                                + "\nNow you have " + Integer.toString(lst.size())
+                                + " tasks in the list.\n"
+                                + Constants.divider + "\n";
+                        System.out.println(output2);
+                        break;
+                    default:
+                        throw new UnknownCommandException();
                 }
-                String output = Constants.divider + "\n"
-                        + "Got it. I've added this task:\n"
-                        + "    " + task.toString()
-                        + "\nNow you have " + Integer.toString(lst.size())
-                        + " tasks in the list.\n"
-                        + Constants.divider + "\n";
-                System.out.println(output);
+            } catch (IncompleteDescriptionException ex) {
+                System.out.println(ex);
+            } catch (UnknownCommandException ex) {
+                System.out.println(ex);
             }
         }
     }
