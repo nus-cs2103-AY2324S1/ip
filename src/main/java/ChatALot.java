dd4059529d7d2176779e5ca6e1716327838b2f3c
Chandan8186 <chandananandababu@gmail.com>
2023-08-20 15:56:18 +0800

    Level-1: Echo
    - ChatALot echoes user input
    - minor design changes

--- src/main/java/ChatALot.java
+++ src/main/java/ChatALot.java
@@ -1,11 +1,57 @@
+import java.util.Scanner;
+
 public class ChatALot {
-    public static void main(String[] args) {
-        String message = "________________________________________\n"
-                + "Hello! I'm ChatALot.\n"
-                + "What can I do for you?\n"
-                + "________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "________________________________________\n";
-        System.out.println(message);
+
+    public static String horizontalLine() {
+        return "_____________________________________________________\n";
     }
+
+    public static void intro() {
+        String output = horizontalLine() +
+                " ____  _   _   ____  _____  ____   _     ____  _____ \n" +
+                "/ (__`| |_| | / () \\|_   _|/ () \\ | |__ / () \\|_   _|\n" +
+                "\\____)|_| |_|/__/\\__\\ |_| /__/\\__\\|____|\\____/  |_|  \n\n" +
+                "Hello! I'm ChatALot.\n" +
+                "What can I do for you?\n" +
+                horizontalLine();
+        System.out.println(output);
+    }
+
+    public static String processInput(String userInput) {
+        System.out.print(horizontalLine());
+        String output = echo(userInput);
+        System.out.println(output);
+        System.out.println(horizontalLine());
+        return output;
+    }
+
+    public static String echo(String userInput) {
+        String output = "You have entered: " +
+                userInput;
+        return output;
+    }
+
+    public static String exit() {
+        String outro = horizontalLine()
+                + "Bye. Hope to see you again soon!\n"
+                + horizontalLine();
+        System.out.println(outro);
+        return outro;
+    }
+
+    public static void main(String[] args) {
+        intro();
+
+        Scanner myObj = new Scanner(System.in);
+        String userInput = myObj.nextLine();
+        String bye = "bye";
+
+        while (!userInput.toLowerCase().equals(bye)) {
+            processInput(userInput);
+            userInput = myObj.nextLine();
+        }
+
+        exit();
+    }
+
 }
