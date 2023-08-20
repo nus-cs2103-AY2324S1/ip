ba192488091a49349e954f17247ff36ac10b6a8c
pra-navi <pranavikiran@gmail.com>
2023-08-20 14:40:05 +0800

    add to list

--- src/main/java/Sally.java
+++ src/main/java/Sally.java
@@ -4,14 +4,24 @@ public class Sally {
     public static void main(String[] args) {
         System.out.println("Hey! It's Sally here!\n" + "How can I help you today?\n \n");
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Bye! See you again next time.");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("My List:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
             } else {
-                System.out.println(input);
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added to My List: " + input);
             }
         }
     }
