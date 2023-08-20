0b32d45e0ea90bc9b20fe2ae12ad44fadbe8dee2
teozern1 <75937968+teozern1@users.noreply.github.com>
2023-08-20 16:22:43 +0800

    Added welcome message and exit message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
+
+
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcome = "Hello! I'm Eddie\n" +
+                "What can I do for you?";
+
+        System.out.println(welcome);
+
+        String goodbye = "Bye. Hope to see you again soon!";
+        System.out.println(goodbye);
     }
 }
