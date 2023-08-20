020d045107deef8603fd1dde7c54fc930ada30d1
Jason <jiacan.h01@gmail.com>
2023-08-20 15:44:34 +0800

    Implement echo functionality

--- src/main/java/Jerma.java
+++ src/main/java/Jerma.java
@@ -3,8 +3,17 @@ import java.util.Scanner;
 public class Jerma {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Jerma.");
+
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye"))
+                break;
+            else
+                System.out.println(input);
+        }
         scanner.close();
+
         System.out.println("See ya soon!");
     }
 }
