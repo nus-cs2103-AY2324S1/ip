6dc51ea892c17da4ccddb266d19ca51c0819a9ea
AriellaCallista <arielgit>
2023-08-20 14:22:46 +0800

    Implement add and list functionalities

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static ArrayList<String> tasksList = new ArrayList<>();
+
     public static void main(String[] args) {
         String divider = "   _________________________________ \n";
         String greeting = "   Hello! I'm Sana \n   What can I do for you? \n";
@@ -11,12 +14,23 @@ public class Duke {
 
         while(true) {
             String command = myObj.nextLine();
-            switch(command) {
-                case "bye":
-                    System.out.println(divider + bye + divider);
-                    break;
-                default:
-                    System.out.println(divider + "   " + command + "\n" + divider);
+            if (command.equals("bye")) {
+                System.out.println(divider + "   " + bye + divider);
+                break;
+            }
+
+            switch (command) {
+            case "list":
+                String list = "";
+                for (int i = 0; i < tasksList.size(); i++) {
+                    int id = i + 1;
+                    list += "   " + id + ". " + tasksList.get(i) + "\n";
+                }
+                System.out.println(divider + list + divider);
+                break;
+            default:
+                tasksList.add(command);
+                System.out.println(divider + "   added: " + command + "\n" + divider);
             }
         }
     }
