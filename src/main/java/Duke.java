eb4d5ff8bd8dcf98d123f531be430a6e1a749aab
You Wei <thamyouwei@gmail.com>
2023-08-20 20:00:32 +0800

    Added Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String section = "\n________________________________________";
+        //Print intro
+        System.out.println(section
+                + "\nHello! I'm Evan"
+                + "\nWhat can I do for you?"
+                + section);
+        //Print outro
+        System.out.println("Bye. Hope to see you again soon!" + section);
     }
 }
