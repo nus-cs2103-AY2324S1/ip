64cb410ed17bd85d73d43b64a3a688a17207418d
xavierpok <54708640+xavierpok@users.noreply.github.com>
2023-08-20 23:32:13 +0800

    Adds  ability to store text entered by user and display back when requested
    When a string that isn't interpreted as a command is input, adds it to internal storage. When list is input, displays the list. Note : 'bye' is retained as a command still.
    
    Also adds a new private function to format this task list as a string.

--- src/main/java/Kniaz.java
+++ src/main/java/Kniaz.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Kniaz {
@@ -26,6 +28,11 @@ public class Kniaz {
 
 
     /**
+     * All tasks stored by Kniaz.
+     */
+    private static List<String> taskList = new ArrayList<String>();
+
+    /**
      * Main function of Kniaz, that is intended as entry point into the program.
      * @param args arguments to be entered into Kniaz when it is called from command line
      */
@@ -45,10 +52,30 @@ public class Kniaz {
         System.out.println("What can I do for you?");
         System.out.println(Kniaz.SEPERATOR);
 
-        String next = input.nextLine(); //Initialise the input
+        String next = ""; //Initialise the input
         while (!next.equals(Kniaz.EXITSTRING)) {
-            System.out.println(next);
-            next  = input.nextLine();
+
+            next = input.nextLine();
+
+            if (next.equals(Kniaz.EXITSTRING)){
+
+                break; // this if makes the while condition redundant but is also more explicit
+            }
+
+            if (next.equals("list")){
+                System.out.println(Kniaz.taskListToString((taskList)));
+                // print out if we are asked to list
+            } else {
+                // interpret everything else that isn't special as a task to add
+                taskList.add(next);
+                System.out.println(String.format("added: %s",next));
+            }
+            System.out.println((Kniaz.SEPERATOR));
+
+
+//            System.out.println(next);
+//            next  = input.nextLine();
+
             // Functionality is just to echo -- Print what we got and wait to fetch next input
         }
 
@@ -57,7 +84,23 @@ public class Kniaz {
         System.out.println(Kniaz.SEPERATOR);
     }
 
-
+    /**
+     * Formats a tasklist as a string.
+     * @param taskList the list of tasks to format as a string
+     * @return the list of tasks formatted in the right format
+     */
+    private static String taskListToString(List<? extends  String> taskList){
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < taskList.size(); i++){
+            String currTask = taskList.get(i);
+            String newLine = String.format(
+                    "%d. %s\n", i+1 , currTask);
+            // User probably expects to start counting from 1 instead of 0
+            // so need to add 1 here
+            out.append(newLine);
+        }
+        return out.toString();
+    }
 
 
 
