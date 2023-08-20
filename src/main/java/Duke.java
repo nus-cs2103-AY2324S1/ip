1ac89b87a6a488b3967a60de76d15885ef71a7e9
matochichap <ruijia.chan@gmail.com>
2023-08-20 16:02:03 +0800

    Complete Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String horizontalLine = "__________________________________________________________________\n";
+        String hiMessage = "Hello! I'm ChatGPA 5.0\nWhat can I do for you?\n";
+        String byeMessage = "Bye. Hope to see you again soon!\n";
+        String message = horizontalLine + hiMessage + horizontalLine + byeMessage + horizontalLine;
+        System.out.println(message);
     }
 }
