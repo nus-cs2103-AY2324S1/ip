3f1601330051ef6f352eef4ac08598a1a6f95f84
shuenj <60507123+shuenj@users.noreply.github.com>
2023-08-20 15:30:03 +0800

    Add echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String botName = "Dude";
         System.out.println("Hello! I'm " + botName);
         System.out.println("What can I do for you?");
 
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        do {
+            input = scanner.nextLine();
+            System.out.println("You just said: " + input);
+        } while (!input.equals("bye"));
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
