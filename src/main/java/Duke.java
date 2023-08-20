08880a12c305dc124502a1db875d9af7618950c1
laurenlim2112 <laurenkoopa@gmail.com>
2023-08-20 13:30:27 +0800

    Duke.java: echoes user commmands, exits on 'bye' command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,12 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello, it's Spot!");
         String goodbye = "Spot's going to take a nap now. Goodnight!";
+        Scanner myObj = new Scanner(System.in);
+        String command = myObj.nextLine();  // Read user input
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            command = myObj.nextLine();
+        }
         System.out.println(goodbye);
     }
 }
