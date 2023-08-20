c2c1b3b572a09692001b66a0f6250bf51fdfe84f
vansh284 <vanshnath284@gmail.com>
2023-08-20 19:45:12 +0800

    Make bot echo user commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
 
     public static void main(String[] args) {
-        Duke.botPrintLine("");
-        Duke.botPrintLine("Greetings user, I'm Didier. How can I help you?");
+        Duke.botPrintBr();
+        Duke.botPrintMessage("Greetings user, I'm Didier. How can I help you?");
+        Duke.botPrintBr();
 
-        Duke.botPrintLine("Goodbye! If you need help in the future, don't hesitate to ask me.");
+        Scanner scanner = new Scanner(System.in); // create a Scanner object for user input
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("bye")) {
+            // Echo user input
+            Duke.botPrintMessage(userInput);
+            Duke.botPrintBr();
+            userInput = scanner.nextLine();
+        }
+
+        Duke.botPrintMessage("Goodbye! If you need more help in the future don't hesitate to ask me.");
+        Duke.botPrintBr();
     }
 
     /**
@@ -12,8 +25,15 @@ public class Duke {
      *
      * @param message The message to be printed.
      */
-    private static void botPrintLine(String message) {
+    private static void botPrintMessage(String message) {
+        System.out.println("    " + message);
+    }
+
+    /**
+     * Prints a horizontal line break to the standard output.
+     */
+    private static void botPrintBr() {
         String lineBreak = "---------------------------------------------------------------------";
-        System.out.println(message + "\n" + lineBreak);
+        Duke.botPrintMessage(lineBreak);
     }
 }
