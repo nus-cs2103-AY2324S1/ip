82ebe45d2e86df08b0edd9ddfc04784709364e9b
LoMaply <linuslee71@gmail.com>
2023-08-20 15:43:16 +0800

    Add list command, other user inputs are added to task list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 public class Duke {
+
+    private static final String[] taskList = new String[100];
+    private static int taskTotal = 0;
+
     /**
      * Formats chatbot output with borders, first line indentation and new line at the end
      * @param message message to be displayed to user
@@ -8,8 +12,9 @@ public class Duke {
         String line = "    ____________________________________________________________\n";
         System.out.println(line + "     " + message + "\n" + line);
     }
+
     public static void main(String[] args) {
-        Duke.output("Hey I'm Jyuuni, your helpful assistant\n     How can I help you?");
+        Duke.output("Hey its Jyuuni, your helpful assistant\n     How can I help you?");
 
         Scanner userInput = new Scanner(System.in);
         boolean isRun = true;
@@ -17,11 +22,19 @@ public class Duke {
         // Waits for input until program stops running
         while (isRun) {
             String command = userInput.nextLine();
-            if (command.equals("end")) {
+            if (command.equals("list")) {
+                String line = "    ____________________________________________________________";
+                System.out.println(line);
+                for (int i = 0; i < taskTotal; i++) {
+                    System.out.println("     " + (i + 1) + ". " + taskList[i]);
+                }
+                System.out.println(line + "\n");
+            } else if (command.equals("end")) {
                 Duke.output("Come back if you need anything else!");
                 isRun = false;
             } else {
-                Duke.output(command);
+                taskList[taskTotal++] = command;
+                Duke.output("added: " + command);
             }
         }
     }
