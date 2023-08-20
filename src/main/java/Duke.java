759f9f3d5b933d0b0f6e3bf42246ab5069cc19dc
sk2001git <gundamsean22@gmail.com>
2023-08-20 23:50:42 +0800

    Duke now Echos - allowing user input through scanner, echoing replies unless bye is written

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String chatBotName = "Benedit Cucumber Badge";
-        System.out.printf("Hello! I'm %s\nWhat can I do for you?\nBye. Hope to see you again soon!", chatBotName);
+        System.out.println("Hello! I'm " + chatBotName);
+        System.out.println("What can I do for you?");
+
+        boolean dontTerminate = true;
+
+        while (dontTerminate) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                dontTerminate = false;
+            } else {
+                System.out.println(input);
+            }
+        }
+
+
 
     }
 }
