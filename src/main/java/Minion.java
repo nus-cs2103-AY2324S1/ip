40aa00c77d0dad102e00fe71fe8f41c794ab42be
joel-foo <joelfootaimeng@gmail.com>
2023-08-20 10:37:27 +0800

    Done Level-3

--- src/main/java/Minion.java
+++ src/main/java/Minion.java
@@ -2,33 +2,60 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+
+/**
+ * Represents the Minion chatbot.
+ */
 public class Minion {
     public static String line = "\t____________________________________________________________\n";
-    public static String[] tasks = new String[100];
+    public static Task[] tasks = new Task[100];
     public static void main(String[] args) {
         sayHi();
         Scanner sc = new Scanner(System.in);
         int taskCount= 0;
-        while(true) {
+        while (true) {
             String command = sc.nextLine();
-            if(command.equals("list")){
-                List<String> lst = new ArrayList<>();
-                for(int i = 0; i < taskCount; i++) {
-                    lst.add((i + 1) + ". " + tasks[i]);
-                }
-                prettyPrint(lst);
-                continue;
-            }
-            if(command.equals("bye")) {
+            if (command.equals("bye")) {
                 sayBye();
                 break;
+            } else if (command.equals("list")){
+                List<String> lst = new ArrayList<>();
+                lst.add("Here are the tasks in your list:");
+                for(int i = 0; i < taskCount; i++) {
+                    lst.add((i + 1) + "." + tasks[i].toString());
+                }
+                prettyPrint(lst);
+            } else if (command.startsWith("mark")){
+                Task currTask = getTask(command);
+                currTask.markDone();
+                List<String> lst = new ArrayList<>();
+                lst.add("Nice! I've marked this task as done:");
+                lst.add("\t" + currTask.toString());
+                prettyPrint(lst);
+            } else if (command.startsWith("unmark")){
+                Task currTask = getTask(command);
+                currTask.markUndone();
+                List<String> lst = new ArrayList<>();
+                lst.add("OK, I've marked this task as not done yet:");
+                lst.add("\t" + currTask.toString());
+                prettyPrint(lst);
+            } else {
+                tasks[taskCount++] = new Task(command);
+                prettyPrint(String.format("added %s", command));
             }
-            tasks[taskCount++] = command;
-            prettyPrint(String.format("added %s", command));
         }
         sc.close();
     }
 
+    /**
+     * Retrieves a Task object based on the user's command.
+     * @param command Command given by the user.
+     * @return the Task corresponding to the index requested by the user.
+     */
+    public static Task getTask(String command) {
+        int taskIdx = Integer.valueOf(command.split(" ")[1]);
+        return tasks[taskIdx - 1];
+    }
     /**
      * Function to say hi to the user.
      */
---
+++ src/main/java/Task.java
@@ -0,0 +1,42 @@
+/**
+ * Represents a task created by the user.
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Returns the status icon depending on whether the task is done.
+     * @return the status icon.
+     */
+    protected String getStatusIcon() {
+        return isDone ? "X": "";
+    }
+
+    /**
+     * Marks the task as done.
+     */
+    protected void markDone() {
+        this.isDone = true;
+    }
+
+    /**
+     * Marks the task as undone.
+     */
+    protected void markUndone() {
+        this.isDone = false;
+    }
+
+    /**
+     * Returns the string representation of the task.
+     * @return string representation of the task.
+     */
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
