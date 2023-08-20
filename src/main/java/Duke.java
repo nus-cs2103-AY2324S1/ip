1edd96aa8e3ed595b7d6c3026eba92262d52f6eb
syamfarh <e0939383@u.nus.edu>
2023-08-20 16:35:10 +0800

    Added ability to store user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
 
     /** name of ChatBot */
     private final String name = "Ken";
 
+    /** store user Input in fixed sized array */
+    private ArrayList<String> lists;
+
+    /**
+     * Initialize the fixed sized array.
+     */
+    public Duke() {
+        this.lists = new ArrayList<>();
+    }
     /**
      * Return the name of the Duke ChatBot.
      *
@@ -21,15 +31,40 @@ public class Duke {
      */
     private String replyUser(String input) {
         String output = "";
-        if (input.equals("bye")) {
-            output = "Bye. Hope to see you again soon!";
-        } else if (input.equals("barbie")) {
-            output = "Hi barbie!";
-        }else {
-            output = input;
+        switch (input) {
+            case "bye":
+                output = "Bye. Hope to see you again soon!";
+                break;
+            case "barbie":
+                this.lists.add(input);
+                output = "Hi barbie!";
+                break;
+            case "list":
+                output = outputList();
+                break;
+            default:
+                this.lists.add(input);
+                output = "added: " + input;
+                break;
         }
 
         return output;
+    }
+
+    /**
+     * Output all the user input.
+     *
+     * @return this.lists
+     */
+    private String outputList() {
+        StringBuilder output = new StringBuilder();
+        int i = 1;
+        for (String val : this.lists) {
+            output.append(i).append(". ").append(val).append("\n");
+            i++;
+        }
+
+        return output.toString();
     }
     public static void main(String[] args) {
 
@@ -44,7 +79,7 @@ public class Duke {
         System.out.println(horLine);
 
         while (!userInput.equals("bye")) {
-            userInput = input.next();
+            userInput = input.nextLine();
             System.out.println(horLine);
             System.out.println(chatBot.replyUser(userInput));
             System.out.println(horLine);
