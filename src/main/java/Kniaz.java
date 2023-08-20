8edc3a7d9952ab3327e1fe85148d9f98a7089eed
xavierpok <54708640+xavierpok@users.noreply.github.com>
2023-08-20 22:43:46 +0800

    Implements Level 0 features of the Duke (Re-named to Kniaz for this version) chatbot. This refers to just renaming the chatbot, and implementing a skeletal version that greets the user and exits.
    Adds two new private fields, LOGO and SEPERATOR, to help with this, which hold the logo and horizontal seperator lines respectively (Seperator lines are to help with formatting)

---
+++ src/main/java/Kniaz.java
@@ -0,0 +1,43 @@
+public class Kniaz {
+
+    /**
+     * Seperator to make things a bit prettier
+     */
+    private static final String SEPERATOR = "_".repeat(20) + "\n";
+
+    /**
+     * Logo to use to represent this chatbot in the command line
+     */
+    private static final String LOGO = "KNIAZ";
+    // Placeholder for now, intend to make prettier later
+    // Kniaz is a rough equivalent for Duke in eastern europe - get it?
+
+    /**
+     * Main function of Kniaz, that is intended as entry point into the program.
+     * @param args arguments to be entered into Kniaz when it is called from command line
+     */
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+
+
+        System.out.println("Hello from\n" + Kniaz.LOGO);
+
+        System.out.println(Kniaz.SEPERATOR);
+
+        System.out.println("What can I do for you?");
+        System.out.println(Kniaz.SEPERATOR);
+
+        System.out.println("Bye. Hope to see you again!");
+        System.out.println(Kniaz.SEPERATOR);
+    }
+
+
+
+
+
+
+}
