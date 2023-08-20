0dd0f997012860de77cf11be0efecb3834dcf08e
sushiyade <68842066+sushiyade@users.noreply.github.com>
2023-08-21 00:11:09 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String EXIT_PHRASE = "bye";
         System.out.println(
                 "Hello! I'm Sunacchi\n" +
-                        "What can I do for you?\n" +
-                        "Bye. Hope to see you again soon!"
+                        "What can I do for you?\n"
         );
+
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+
+        String userInput = myObj.nextLine();
+        while (!userInput.equals(EXIT_PHRASE)) {
+            System.out.println(userInput);
+            userInput = myObj.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+
+
+
+
     }
 }
