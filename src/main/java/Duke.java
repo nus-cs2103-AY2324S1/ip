625970b8b95dd5284346a11be4f10c67f02be3e0
Yeo Boh Shin <110957811+YeoBohShin@users.noreply.github.com>
2023-08-20 23:30:26 +0800

    Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
+
+    private void greet() {
+        System.out.println("Hello! I'm Aikent\n" + "What can I do for you?");
+    }
+
+    private void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke bot = new Duke();
+        bot.greet();
+        bot.exit();
     }
 }
