006f214276b6cac94cbae7e0193de3ec3bf1ee75
Inez Kok <kfyinez@gmail.com>
2023-08-20 20:09:18 +0800

    Implement Level-1: Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     // Constructor for Duke
     public Duke() {
@@ -7,6 +9,9 @@ public class Duke {
     // Constants
     private static final String name = "Beep Boop";
     private static final String line = "─".repeat(100);
+
+    // Fields
+    private Scanner sc = new Scanner(System.in);
 
     public void printMessage(String message) {
         System.out.printf("\t%s\n", message);
@@ -20,12 +25,28 @@ public class Duke {
     }
 
     public void exit() {
-        String exiting = "Bye. Hope to see you again soon!\n";
+        String exiting = "Bye. Hope to see you again soon! Beep Boop!\n";
         printMessage(exiting);
+    }
+
+    public void echo(String input) {
+        printMessage(input);
     }
 
     public void runDuke() {
         greet();
+
+        boolean isDone = false;
+        while (!isDone) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                isDone = true;
+            } else {
+                echo(input);
+            }
+        }
+
         exit();
     }
     public static void main(String[] args) {
