c17076b2f6532b38269b821e555875e1c6112739
lamchenghou <lamchenghouu@gmail.com>
2023-08-20 14:06:39 +0800

    Add ui package and Ui.java

---
+++ src/main/java/ui/Ui.java
@@ -0,0 +1,29 @@
+package ui;
+
+public class Ui {
+    public static final String HORIZONTAL_RULE = "_".repeat(60) + "\n";
+
+    private static final String LOGO = " ____          _\n"
+            + "|  _ \\ _____ _| |___\n"
+            + "| | | | | | | | ____|\n"
+            + "| |_| | |_| | | |___\n"
+            + "|____/ \\___/  \\____/\n";
+
+    /**
+     * Displays the welcome message for the user.
+     */
+    public static void welcome() {
+        System.out.printf("%s%sHello! I'm Dot, " +
+                        "let me help you finish your tasks on the dot!\n" +
+                        "What can I do(t) for you?\n%s\n",
+                HORIZONTAL_RULE, LOGO, HORIZONTAL_RULE);
+    }
+
+    /**
+     * Displays the exit message for the user.
+     */
+    public static void goodbye() {
+        System.out.printf("%sBye! DOnT forget to finish your tasks!\n%s",
+                HORIZONTAL_RULE, HORIZONTAL_RULE);
+    }
+}
