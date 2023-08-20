7d09b64d9a9d7f59f7abc1cc2dd9a32eceaeaede
Chandan8186 <chandananandababu@gmail.com>
2023-08-20 13:52:51 +0800

    Level-0: Rename, Greet, Exit
    - Renamed Duke to ChatALot
    - Added greet message (skeletal version)
    - Added *.class to .gitignore

---
+++ src/main/java/ChatALot.java
@@ -0,0 +1,11 @@
+public class ChatALot {
+    public static void main(String[] args) {
+        String message = "________________________________________\n"
+                + "Hello! I'm ChatALot.\n"
+                + "What can I do for you?\n"
+                + "________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "________________________________________\n";
+        System.out.println(message);
+    }
+}
