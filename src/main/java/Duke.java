8d6ed5258cdfa9ec5656f83c82d07f76b5de2951
You Wei <thamyouwei@gmail.com>
2023-08-21 00:04:07 +0800

    Added Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static Todo tasks = new Todo();
+    private static Reply reply = Reply.init();
     public static void main(String[] args) {
         //Start user interaction
         Scanner scanner = new Scanner(System.in);
-        Todo tasks = new Todo();
-        Reply reply = Reply.init();
         while(true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
@@ -13,9 +13,32 @@ public class Duke {
                 return;
             } else if (input.equals("list")) {
                 tasks.printTasks();
+            } else if (input.startsWith("mark")) {
+                markTask(input, true);
+            } else if (input.startsWith("unmark")) {
+                markTask(input, false);
             } else {
                 tasks.addTask(input);
                 reply.printDialog("added: " + input);
+            }
+        }
+    }
+
+    public static void markTask(String input, boolean mark) {
+        String[] split = input.split(" ");
+        if (split.length > 2) {
+            reply.printDialog("Invalid input");
+        } else {
+            try {
+                if (mark) {
+                    tasks.markDone(Integer.parseInt(split[1]));
+                } else {
+                    tasks.unmarkDone(Integer.parseInt(split[1]));
+                }
+            } catch (NumberFormatException e ) {
+                reply.printDialog("Invalid number");
+            } catch (IndexOutOfBoundsException e) {
+                reply.printDialog("Index number does not exist");
             }
         }
     }
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,28 +1,59 @@
 import java.util.ArrayList;
 
 public class Todo {
-    private ArrayList<String> list;
+    private ArrayList<Task> list;
+    private final Reply reply = Reply.init();
 
     public Todo() {
         list = new ArrayList<>();
     }
 
     public void addTask(String task) {
-        list.add(task);
+        list.add(new Task(task));
     }
 
     public void printTasks() {
-        String[] tasks = list.toArray(String[]::new);
-        Reply reply = Reply.init();
-        String dialog = "";
+        Task[] tasks = list.toArray(Task[]::new);
+        StringBuilder dialog = new StringBuilder("Here are the tasks in your list:\n     ");
         for (int i = 0; i < tasks.length; i++) {
             int listIndex = i + 1;
+            Task task = tasks[i];
+            String desc = task.description;
+            dialog.append(listIndex)
+                    .append(".").append("[")
+                    .append(task.getStatusIcon())
+                    .append("] ");
+
             if (i < tasks.length - 1) {
-                dialog = dialog + listIndex + ". " + tasks[i] + "\n     ";
+                dialog.append(desc).append("\n     ");
             } else {
-                dialog = dialog + listIndex + ". " + tasks[i];
+                dialog.append(desc);
             }
         }
-        reply.printDialog(dialog);
+        reply.printDialog(dialog.toString());
+    }
+
+    public void markDone(int index) {
+        StringBuilder dialog = new StringBuilder();
+        Task element = list.get(index - 1);
+        element.markAsDone();
+        dialog.append("Nice! I've marked this task as done:\n")
+                .append("       [")
+                .append(element.getStatusIcon())
+                .append("] ")
+                .append(element.description);
+        reply.printDialog(dialog.toString());
+    }
+
+    public void unmarkDone(int index) {
+        StringBuilder dialog = new StringBuilder();
+        Task element = list.get(index - 1);
+        element.markAsNotDone();
+        dialog.append("OK! I've marked this task as not done yet:\n")
+                .append("       [")
+                .append(element.getStatusIcon())
+                .append("] ")
+                .append(element.description);
+        reply.printDialog(dialog.toString());
     }
 }
