ea6536ef35c17abe2b5af2b2cc14ea0682e5e421
shishirbychapur <shishirbychapur@gmail.com>
2023-08-20 20:51:32 +0800

    Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        String greetings = "Greetings, I am Vision. How may I assist you today? \uD83E\uDD16\uD83D\uDD0D";
+        String exit = "I shall now take my leave. If you require further assistance, \ndo not hesitate to seek my guidance once more. Farewell. \uD83E\uDD16\uD83D\uDC4B";
+        System.out.println(greetings);
+        System.out.println("____________________________________________________________");
+        System.out.println(exit);
+        System.out.println("____________________________________________________________");
     }
 }
