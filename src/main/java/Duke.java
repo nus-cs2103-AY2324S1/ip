9c2a7f2a63b44bc4c6392bb7f32b5d95b09f808f
Lingxi Xing <thelastoreointhejar@gmail.com>
2023-08-21 02:09:09 +0800

    feat: [Level-1] Echo
    - Echo commands entered by user and exit with "bye" command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,42 @@
+import java.util.Scanner;
+
 public class Duke {
 
     private static final String DIVIDER = "____________________________________________________________";
+    private static final String EXIT_COMMAND = "bye";
 
     public static void main(String[] args) {
+        Duke.greet();
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals(Duke.EXIT_COMMAND)) {
+                break;
+            }
+            Duke.echo(input);
+        }
+
+        Duke.exit();
+        scanner.close();
+    }
+
+    public static void greet() {
         System.out.println(DIVIDER);
         System.out.println(" Hello! I'm LilBro!");
         System.out.println(" What can I do for you?");
         System.out.println(DIVIDER);
+    }
+
+    public static void exit() {
+        System.out.println(DIVIDER);
         System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
+    }
+
+    public static void echo(String input) {
+        System.out.println(DIVIDER);
+        System.out.println(input);
         System.out.println(DIVIDER);
     }
 }
