e56e3b55da09557be941633d6b1d04bf6fd841b5
Lim Ngee Yong <e0940691@u.nus.edu>
2023-08-20 23:34:18 +0800

    Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "YONG";
+        System.out.println("Hello I'm " + logo + "\n" +
+                "What can I do for you? \n" +
+                "------------------------------------- \n" +
+                "Bye. Hope to see you again soon! \n" +
+                "-------------------------------------" );
     }
 }
