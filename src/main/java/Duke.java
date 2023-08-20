d837e6435b5830a7bec3dd31921af71da8608005
Geoff <e0772885@u.nus.edu>
2023-08-20 09:04:53 +0800

    level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 public class Duke {
     public static void main(String[] args) {
         String chatBotName = "RatSpeak";
-        System.out.println("Hello from\n" + chatBotName + "\nWhat can I do for you?");
+        System.out.println("Hello from " + chatBotName + "\nWhat can I do for you?");
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
