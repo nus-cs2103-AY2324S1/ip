8169de03a3100de43bec063c3e1a47243a6423ed
Kb-tay <taykaiboon12@gmail.com>
2023-08-20 16:29:18 +0800

    Add echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,24 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String name = "Obi-wan Kenobi";
-
         String line = "_____________________________________";
+        Scanner scanner = new Scanner(System.in);
+        String command;
 
         System.out.println("Hello There! I am " + name);
         System.out.println("What can I do for you?");
         System.out.println(line);
+
+        while (true) {
+            command = scanner.nextLine();
+            if(command.equals("bye")) {
+                break;
+            }
+            System.out.println("\t" + command);
+        }
+
         System.out.println("Bye. May the force be with you!");
     }
-
 
 }
