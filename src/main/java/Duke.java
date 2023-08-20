986d7fee7e258afc88aa95fad298abab933fe977
rionshocker <jaryl591@gmail.com>
2023-08-20 23:22:46 +0800

    Added echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String exitMsg = "Bye. Hope to see you again soon!";
-        System.out.println("Hello! I'm Rion");
-        System.out.println("What can I do for you?");
-        System.out.println(exitMsg);
+        String partition = "--------------------------------------";
+        System.out.println(partition + "\n" + "Hello! I'm Rion");
+        System.out.println("What can I do for you?\n" + partition);
+
+        Scanner sc = new Scanner(System.in);
+        String echo = sc.nextLine();
+        while(!echo.equals("bye")) {
+            System.out.println(partition + "\n   " + echo + "\n" + partition);
+            echo = sc.nextLine();
+        }
+        System.out.println(partition + "\n   " + exitMsg + "\n" + partition);
     }
 }
