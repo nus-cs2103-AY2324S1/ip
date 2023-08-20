df10c53ffbae73e8efe55a84db848289096c1c93
jacobcuison <e0969947@u.nus.edu>
2023-08-20 17:04:40 +0800

    Completed Level-0 tasks

---
+++ src/main/java/Max.java
@@ -0,0 +1,16 @@
+public class Duke {
+    public static void main(String[] args) {
+        String logo = ".___  ___.      ___      ___   ___ \n" +
+                "|   \\/   |     /   \\     \\  \\ /  / \n" +
+                "|  \\  /  |    /  ^  \\     \\  V  /  \n" +
+                "|  |\\/|  |   /  /_\\  \\     >   <   \n" +
+                "|  |  |  |  /  _____  \\   /  .  \\  \n" +
+                "|__|  |__| /__/     \\__\\ /__/ \\__\\ \n" +
+                "                                   ";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Max! \n" + "How may I assist you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye! Please come again!");
+    }
+}
