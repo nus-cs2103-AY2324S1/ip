608440d1d41b95a01efbd2b4c0e7507b46e6f324
lamchenghou <lamchenghouu@gmail.com>
2023-08-20 14:11:18 +0800

    Add Task class

---
+++ src/main/java/tasks/Task.java
@@ -0,0 +1,18 @@
+package tasks;
+
+public class Task {
+
+    private String description;
+
+    protected Task(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
