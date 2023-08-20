d67f149d63ac6af114c0c9416b4a6782035df7fa
Lingxi Xing <thelastoreointhejar@gmail.com>
2023-08-21 00:43:20 +0800

    feat: [Level-0] Rename, Greet, Exit
    - Rename Duke to LilBro
    - Implement initial skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
+
+    private static final String DIVIDER = "____________________________________________________________";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(DIVIDER);
+        System.out.println(" Hello! I'm LilBro!");
+        System.out.println(" What can I do for you?");
+        System.out.println(DIVIDER);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
     }
 }
