3d1ad9eb69ddf7363bf550e3789af2b1227ec56b
pzl111 <phuazailian@gmail.com>
2023-08-20 15:02:28 +0800

    Completed Level-1

--- src/main/java/Smolbrain.java
+++ src/main/java/Smolbrain.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Smolbrain {
     public static void main(String[] args) {
+
+        System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Smolbrain\nWhat can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________\n");
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            String input = sc.next();
+            System.out.println("____________________________________________________________");
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________\n");
+                break;
+            }
+            System.out.println(input);
+            System.out.println("____________________________________________________________\n");
+        }
 
     }
 }
