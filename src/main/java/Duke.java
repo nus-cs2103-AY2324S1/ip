d42b49767c8ca8dc03f9aa2538cfd4621892b60d
ivyy-poison <tan.yuhao.ivan@gmail.com>
2023-08-20 11:22:15 +0800

    Achieve Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,44 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String LOGO = ",------.,--.              ,--. \n"
+                                      + "|  .---\'|  |,-.,--.,--. ,-|  |  \n"
+                                      + "|  `--, |     /|  ||  |' .-. |  \n"
+                                      + "|  `---.|  \\\\  \\\\'  ''  '\\\\ `-' |  \n"
+                                      + "`------'`--'`--'`----'  `---' \n";
+    private static final String LINE = "-".repeat(60);
+
     public static void main(String[] args) {
+        printWelcomeMessage();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                sc.close();
+                break;
+            }
+            echo(input);
+        }
 
-        String logo = ",------.,--.              ,--. \n"
-                + "|  .---'|  |,-.,--.,--. ,-|  |  \n"
-                + "|  `--, |     /|  ||  |' .-. |  \n"
-                + "|  `---.|  \\  \\'  ''  '\\ `-' |  \n"
-                + "`------'`--'`--'`----'  `---' \n";
+        printFarewellMessage();
+    }
 
-        String line = "-".repeat(60);
+    private static void echo(String input) {
+        System.out.println(LINE);
+        System.out.println(input);
+        System.out.println(LINE);
+    }
 
-        System.out.println(line);   
-        System.out.println("Hello! I'm \n");
-        System.out.println(logo);
+    private static void printWelcomeMessage() {
+        System.out.println(LINE);
+        System.out.println("Hello! I'm \\n");
+        System.out.println(LOGO);
         System.out.println("What can I do for you?");
-        System.out.println(line);
+        System.out.println(LINE);
+    }
+
+    private static void printFarewellMessage() {
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(LINE);
     }
 }
