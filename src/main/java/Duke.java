4b4108452d8ccf245a89105d8ecccf101ca06025
jingting1412 <e0970352@u.nus.edu>
2023-08-20 22:51:49 +0800

    implementing a skeleton version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //this is the new name for my chatbot
+        String name = "dukey";
+
+        //this will be the greeting
+        String greeting = "Hello! I'm " + name + "\nWhat can I do for you?\n";
+
+        //this will be goodbye
+        String goodbye = "Bye. Hope to see you again soon!";
+
+        System.out.println(greeting + "\n" + goodbye);
     }
 }
