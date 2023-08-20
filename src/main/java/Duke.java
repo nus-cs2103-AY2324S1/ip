bd304dbb255980847976cf653f77744436851f3d
DonovanJJ <donovanchanjiajun2001@gmail.com>
2023-08-20 14:44:38 +0800

    Added: Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,8 +8,19 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String chatbotName = "notDuke";
         String introMessage = "Hello! I'm " + chatbotName + "\n"
-                        + "What can I do for you?\n"
-                        + "Bye. Hope to see you again soon!\n";
+                        + "What can I do for you?\n";
+        String exitMessage = "Bye. Hope to see you again soon!\n";
         System.out.println(introMessage);
+        Duke.echo();
+        System.out.println(exitMessage);
+    }
+
+    public static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = scanner.nextLine();
+        }
     }
 }
