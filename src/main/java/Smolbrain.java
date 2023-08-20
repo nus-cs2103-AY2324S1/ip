38eda6dcb2bb8a465e1ae52d0bfe56dea0b9b018
pzl111 <phuazailian@gmail.com>
2023-08-20 14:25:02 +0800

    Completed Level-0

---
+++ src/main/java/Smolbrain.java
@@ -0,0 +1,7 @@
+public class Smolbrain {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Smolbrain\nWhat can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+
+    }
+}
