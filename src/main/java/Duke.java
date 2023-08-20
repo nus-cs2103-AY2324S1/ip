504643564e07a4b468bebd48c55b6208ce9c43c6
Blizzeracz <lamkwangzheloysius@gmail.com>
2023-08-20 17:58:07 +0800

    Change the name of the bot and giving an outro

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String intro = "Hi! This is your AI assistant LoyBoy!\n";
+        String question = "What can I do for you today?\n";
+        String outro = "I wish you a pleasant day ahead, goodbye!\n";
+        System.out.println(intro + question + outro);
     }
 }
+
