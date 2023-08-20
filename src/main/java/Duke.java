3f392374f57f3827226763679b59569ff5cafb01
teozern1 <75937968+teozern1@users.noreply.github.com>
2023-08-20 16:51:37 +0800

    Added exit and echo methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,35 @@
-
+import java.util.Scanner;
 
 
 public class Duke {
+    static boolean exited = false;
+
+    public static void exit() {
+        String goodbye = "Bye. Hope to see you again soon!";
+        System.out.println(goodbye);
+        System.exit(0);
+    }
+    public static void echo(String input) {
+        System.out.println(input);
+    }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        exited = false;
         String welcome = "Hello! I'm Eddie\n" +
                 "What can I do for you?";
 
         System.out.println(welcome);
 
-        String goodbye = "Bye. Hope to see you again soon!";
-        System.out.println(goodbye);
+        while (!exited) {
+            String command = sc.nextLine();
+            if (command.equals("exit")) {
+                Duke.exit();
+            } else {
+                Duke.echo(command);
+            }
+        }
+
+
     }
 }
