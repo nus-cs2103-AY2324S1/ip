8a03511f859ffc9212b667fa4af7178c55424841
DonovanJJ <donovanchanjiajun2001@gmail.com>
2023-08-20 15:41:22 +0800

    Added: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
 import java.util.Scanner;
 public class Duke {
-    static String[] storage;
+    static Task[] storage;
     static int storagePointer;
+
+    static public class Task {
+        private boolean isComplete;
+        private String name;
+        public Task(String name, boolean isComplete) {
+            this.isComplete = isComplete;
+            this.name = name;
+        }
+
+        public boolean isComplete() {
+            return this.isComplete;
+        }
+
+        public void markDone() {
+            this.isComplete = true;
+        }
+
+        public void markUndone() {
+            this.isComplete = false;
+        }
+        public String getName() {
+            return this.name;
+        }
+    }
+
     public static void main(String[] args) {
         // Initialise the data storage
-        Duke.storage = new String[100];
+        Duke.storage = new Task[100];
         Duke.storagePointer = 0;
 
         String logo = " ____        _        \n"
@@ -25,18 +50,41 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
         while (!userInput.equals("bye")) {
-            if (userInput.equals("list")) {
-                for (int i = 1; i <= Duke.storagePointer; i ++) {
-                    String item = Duke.storage[i - 1];
-                    System.out.printf("%d. %s\n", i, item);
-                }
-                userInput = scanner.nextLine();
-                continue;
+            Task task = new Task(userInput, false);
+            String command = userInput.split("\\s+")[0];
+            int choice;
+            switch (command) {
+                case "list":
+                    for (int i = 1; i <= Duke.storagePointer; i++) {
+                        Task item = Duke.storage[i - 1];
+                        String itemMarked = " ";
+                        if (item.isComplete()) {
+                            itemMarked = "X";
+                        }
+                        System.out.printf("%d.[%s] %s\n", i, itemMarked, item.getName());
+                    }
+                    userInput = scanner.nextLine();
+                    break;
+                case "mark":
+                    choice = Integer.parseInt(userInput.split("\\s+")[1]);
+                    Duke.storage[choice - 1].markDone();
+                    System.out.printf("Nice! I've marked this task as done:\n" +
+                            "  [X] %s\n", Duke.storage[choice - 1].getName());
+                    userInput = scanner.nextLine();
+                    break;
+                case "unmark":
+                    choice = Integer.parseInt(userInput.split("\\s+")[1]);
+                    Duke.storage[choice - 1].markUndone();
+                    System.out.printf("OK, I've marked this task as not done yet:\n" +
+                            "  [ ] %s\n", Duke.storage[choice - 1].getName());
+                    userInput = scanner.nextLine();
+                    break;
+                default:
+                    Duke.storage[Duke.storagePointer] = task;
+                    Duke.storagePointer ++;
+                    System.out.println("added: " + userInput + "\n");
+                    userInput = scanner.nextLine();
             }
-            Duke.storage[Duke.storagePointer] = userInput;
-            Duke.storagePointer ++;
-            System.out.println("added: " + userInput);
-            userInput = scanner.nextLine();
         }
     }
 }
