66f9511b6a722c5f288b276e157ff06feb8775e1
ivyy-poison <tan.yuhao.ivan@gmail.com>
2023-08-20 10:44:55 +0800

    Achieve Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String logo = ",------.,--.              ,--. \n"
+                + "|  .---'|  |,-.,--.,--. ,-|  |  \n"
+                + "|  `--, |     /|  ||  |' .-. |  \n"
+                + "|  `---.|  \\  \\'  ''  '\\ `-' |  \n"
+                + "`------'`--'`--'`----'  `---' \n";
+
+        String line = "-".repeat(60);
+
+        System.out.println(line);   
+        System.out.println("Hello! I'm \n");
+        System.out.println(logo);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
