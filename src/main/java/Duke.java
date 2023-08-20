9db3807fe3e9c15f50c3a8a341e7ed14f62d7ed6
Yeo Boh Shin <110957811+YeoBohShin@users.noreply.github.com>
2023-08-20 23:47:30 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private boolean active = true;
 
     private void greet() {
         System.out.println("Hello! I'm Aikent\n" + "What can I do for you?");
     }
 
     private void exit() {
+        active = false;
         System.out.println("Bye. Hope to see you again soon!");
     }
+
+    private void echo(String msg) {
+        if (msg.equals("bye")) {
+            exit();
+        } else {
+            System.out.println(msg);
+        }
+    }
+
     public static void main(String[] args) {
         Duke bot = new Duke();
+        Scanner sc = new Scanner(System.in);
+
         bot.greet();
-        bot.exit();
+
+        while (bot.active) {
+            String input = sc.nextLine();
+            bot.echo(input);
+        }
     }
 }
