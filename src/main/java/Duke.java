daa396414eaa0af0569e20e94c2342eb0aedee0a
vansh284 <vanshnath284@gmail.com>
2023-08-20 19:31:19 +0800

    Rename bot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke.botPrintLine("");
+        Duke.botPrintLine("Greetings user, I'm Didier. How can I help you?");
+
+        Duke.botPrintLine("Goodbye! If you need help in the future, don't hesitate to ask me.");
+    }
+
+    /**
+     * Prints the message to the standard output, formatted in a specific way emulate the bot.
+     *
+     * @param message The message to be printed.
+     */
+    private static void botPrintLine(String message) {
+        String lineBreak = "---------------------------------------------------------------------";
+        System.out.println(message + "\n" + lineBreak);
     }
 }
