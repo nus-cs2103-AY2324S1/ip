7c79432a3099a9fe5cd7c873db8f2e93c23cf5f7
Rohan Bahl <Rb@Rb-2.local>
2023-08-21 00:46:25 +0800

    Level-0

---
+++ src/main/java/UI.java
@@ -0,0 +1,14 @@
+public class UI {
+    private void horizontalLines() {
+        System.out.println("____________________________________________________________"); //length taken from sample
+    }
+
+    public void skeletalChatbot() {
+        horizontalLines();
+        System.out.println("Hello! I'm Whelmed.");
+        System.out.println("What can I do for you?");
+        horizontalLines();
+        System.out.println("Bye. Hope to see you again soon!");
+        horizontalLines();
+    }
+}
