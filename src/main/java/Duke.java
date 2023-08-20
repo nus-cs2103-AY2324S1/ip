e1c162186c5ef381015be9c595a5454a3d47de97
sk2001git <gundamsean22@gmail.com>
2023-08-20 23:31:13 +0800

    Duke Level 0 - Rename, Greet and Edit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,7 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String chatBotName = "Benedit Cucumber Badge";
+        System.out.printf("Hello! I'm %s\nWhat can I do for you?\nBye. Hope to see you again soon!", chatBotName);
+
     }
 }
