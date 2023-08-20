2d948054606badaec6532684d90df2762dd1ccf7
leesiyuan <lee.si.yuan.20@gmail.com>
2023-08-20 10:26:14 +0800

    Added Error Handling
    -Added DukeException
    -Added empty description error handling and wrong command error handling
    -Update text-ui-test to test for error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,38 +24,84 @@ public class Duke {
 
             if (input.equals("list")) {
                 tasks.listAllTasks();  
-            } 
-            String[] inputSplit = input.split(" ");
-            
-            switch (inputSplit[0]) {
-                case ("todo"):
-                    String todoDescription = input.substring(5);
-                    TodoTask todoTask = new TodoTask(todoDescription);
-                    tasks.addTask(todoTask);
-                    break;
-                case ("deadline"):
-                    String deadlineDescription = input.substring(9);
-                    String[] deadlineSplit = deadlineDescription.split(" /by ");
-                    DeadlineTask deadlineTask = new DeadlineTask(deadlineSplit[0], deadlineSplit[1]);
-                    tasks.addTask(deadlineTask);
-                    break;
-                case ("event"):
-                    String eventDescription = input.substring(6);
-                    String[] eventSplit = eventDescription.split(" /from ");
-                    String[] eventSplit2 = eventSplit[1].split(" /to ");
-                    EventTask eventTask = new EventTask(eventSplit[0], eventSplit2[0], eventSplit2[1]);
-                    tasks.addTask(eventTask);
-                    break;
-                case ("mark"):
-                    int taskNumber = Integer.parseInt(inputSplit[1]);
-                    tasks.markTaskAsDone(taskNumber);
-                    break;
-                case ("unmark"):
-                    int taskNumber2 = Integer.parseInt(inputSplit[1]);
-                    tasks.markTaskAsUndone(taskNumber2);
-                    break;  
-            }
+            }  else {
+                String[] inputSplit = input.split(" ");
 
+                try {
+                    switch (inputSplit[0]) {
+                        case ("todo"):
+                            try {
+                                String todoDescription = input.substring(5);
+                                TodoTask todoTask = new TodoTask(todoDescription);
+                                tasks.addTask(todoTask);
+                            } catch(StringIndexOutOfBoundsException e) {
+                                throw new DukeException("The description of a todo cannot be empty.");
+                            }
+                            break;
+
+                        case ("deadline"):
+                            try {
+                                String deadlineDescription = input.substring(9);
+                                String[] deadlineSplit = deadlineDescription.split(" /by ");
+                                DeadlineTask deadlineTask = new DeadlineTask(deadlineSplit[0], deadlineSplit[1]);
+                                tasks.addTask(deadlineTask);
+                            } catch(StringIndexOutOfBoundsException e) {
+                                throw new DukeException("The description of a deadline cannot be empty.");
+                            } catch(ArrayIndexOutOfBoundsException e) {
+                                throw new DukeException("The deadline of a deadline cannot be empty.");
+                            }
+                            break;
+
+                        case ("event"):
+                            try {
+                                String eventDescription = input.substring(6);
+                                String[] eventSplit = eventDescription.split(" /from ");
+                                String[] eventSplit2 = eventSplit[1].split(" /to ");
+                                EventTask eventTask = new EventTask(eventSplit[0], eventSplit2[0], eventSplit2[1]);
+                                tasks.addTask(eventTask);
+                            } catch (StringIndexOutOfBoundsException e) {
+                                throw new DukeException("The description of an event cannot be empty.");
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                throw new DukeException("The date of an event cannot be empty.");
+                            }
+                            break;
+                        
+                        case ("mark"):
+                            try {    
+                                int taskNumber = Integer.parseInt(inputSplit[1]);
+                                tasks.markTaskAsDone(taskNumber);
+                            } catch (NumberFormatException e) {
+                                throw new DukeException("Please enter a valid task number.");
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                throw new DukeException("Please enter a task number.");
+                            } catch (IndexOutOfBoundsException e) {
+                                throw new DukeException("Please enter a valid task number.");
+                            } 
+                            break;
+
+                        case ("unmark"):
+                            try {
+                                int taskNumber2 = Integer.parseInt(inputSplit[1]);
+                                tasks.markTaskAsUndone(taskNumber2);
+                            } catch (NumberFormatException e) {
+                                throw new DukeException("Please enter a valid task number.");
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                throw new DukeException("Please enter a task number.");
+                            } catch (IndexOutOfBoundsException e) {
+                                throw new DukeException("Please enter a valid task number.");
+                            } 
+                            break;  
+                        default:
+                            throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                    }
+                } 
+                catch (DukeException e) {
+                    System.out.println(e.toString());
+                } 
+                catch (Exception e) {
+                    System.out.println("☹ OOPS!!! Unknown error occurred.");
+                }
+            }
             input = sc.nextLine();
         }
 
