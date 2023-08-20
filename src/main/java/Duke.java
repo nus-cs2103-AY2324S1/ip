31689104d88001b77e8328ec776fbd8c74ac051e
markgcera <>
2023-08-20 23:48:56 +0800

    Complete Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String command;
+        String input;
+        List<String> inputList = new ArrayList<>();
+
         String initMsg = "____________________________________________________________\n"
                 + " Hello! I'm IRIS\n"
-                + " What can I do for you?\n";
+                + " What can I do for you?\n"
+                + "____________________________________________________________";
         System.out.println(initMsg);
-        command = scanner.nextLine();
 
-        while (!command.equals("bye")) {
-            String echo = "____________________________________________________________\n"
-                    + command + "\n"
-                    + "____________________________________________________________";
-            System.out.println(echo);
-            command = scanner.nextLine();
+        input = scanner.nextLine();
+        int count = 1;
+
+        while (!input.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            if (input.equals("list")) {
+                for (String userInput : inputList) {
+                    System.out.println(userInput);
+                }
+            } else {
+                inputList.add(count++ + " " + input);
+                System.out.println("added: " + input);
+            }
+            System.out.println("____________________________________________________________");
+            input = scanner.nextLine();
         }
 
         scanner.close();
