a0051ea78565ba1ae406a0678cf25352dd0a7eef
lshaoqin <74029583+lshaoqin@users.noreply.github.com>
2023-08-20 15:23:09 +0800

    add ability to echo and exit

--- src/main/java/Juke.java
+++ src/main/java/Juke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Juke {
+    static void printLine() {
+        System.out.println("_______________________________________________________");
+    }
     public static void main(String[] args) {
+
         //Introduce itself to the user
         System.out.println("Hello! I'm Juke!");
         System.out.println("What can I do for you?");
-
-        //Say goodbye
-        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+        while(true) {
+            Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                //Say goodbye
+                System.out.println("Bye. Hope to see you again soon!");
+                printLine();
+                return;
+            }
+            else {
+                System.out.println(input);
+                printLine();
+            }
+        }
     }
 }
