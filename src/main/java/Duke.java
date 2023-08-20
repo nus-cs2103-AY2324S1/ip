639b3f2cba752d9b13b4bb09f39d4eabf05ec0e8
shuenj <60507123+shuenj@users.noreply.github.com>
2023-08-20 16:06:23 +0800

    add mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,20 +8,38 @@ public class Duke {
         System.out.println("What can I do for you?");
 
         //Add the ability to store whatever text entered by the user and display them back to the user when requested.
-        ArrayList<String> userTexts = new ArrayList<String>();
+        ArrayList<Task> userTasks = new ArrayList<Task>();
 
         Scanner scanner = new Scanner(System.in);
         String input;
         do {
             input = scanner.nextLine();
             if (input.equals("list")) {
-                for (int i = 0; i < userTexts.size(); i++) {
-                    System.out.println(i + 1 + ". " + userTexts.get(i));
+                for (int i = 0; i < userTasks.size(); i++) {
+                    System.out.println(i + 1 + ". [" + userTasks.get(i).getStatusIcon() + "] " + userTasks.get(i).getDescription());
                 }
                 continue;
             }
-            userTexts.add(input);
-            System.out.println("added: " + input);
+            // If input has mark followed by an integer, mark the task[i-1] as done.
+            if (input.startsWith("mark")) {
+                int TaskID = Integer.parseInt(input.substring(5)) - 1;
+                userTasks.get(TaskID).markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(userTasks.get(TaskID).toString());
+            }
+            // If input has delete followed by an integer, delete the task[i-1].
+            else if (input.startsWith("unmark")) {
+                int TaskID = Integer.parseInt(input.substring(7)) - 1;
+                userTasks.get(TaskID).markAsUndone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(userTasks.get(TaskID).toString());
+            }
+            else { //input as new task
+                Task task = new Task(input);
+                userTasks.add(task);
+                System.out.println("added: " + input);
+            }
+
         } while (!input.equals("bye"));
         System.out.println("Bye. Hope to see you again soon!");
     }
