02e7d5be389d738f8c6ea3a7022429c6014d0ab9
songgthu <songthu0711@gmail.com>
2023-08-20 15:24:59 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____     ____    _    _\n"
@@ -6,10 +7,26 @@ public class Duke {
                 + "|  |       |  |  | |  | |____\n"
                 + "|__|       |__|  |_|  |______|\n";
         String horLine = "__________________________________________________________________________";
+        String indentation = "     ";
         System.out.println(logo);
-        System.out.println(horLine);
-        System.out.println("Hi there, I'm Mil - your personal chatbot.\nHow can I help you today?");
-        System.out.println(horLine);
-        System.out.println("Have a nice day and see you again soon!");
+        System.out.println(indentation + horLine);
+        System.out.println(indentation + "Hi there, I'm Mil - your personal chatbot.\n     How can I help you today?");
+        System.out.println(indentation + horLine);
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while(scanner.hasNext()) {
+            input = scanner.nextLine();
+            if(input.equals("bye")) {
+                System.out.println(indentation + horLine);
+                System.out.println(indentation + "Have a nice day and see you again soon!");
+                System.out.println(indentation + horLine);
+                break;
+            }
+            System.out.println(indentation + horLine);
+            System.out.println(indentation + input);
+            System.out.println(indentation + horLine);
+        }
+
+
     }
 }
