901797c3bed123431bae3ab412a9ee178c85dfef
jedkohjk <jedkoh2001@gmail.com>
2023-08-21 00:28:55 +0800

    Level 5. Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,62 +14,101 @@ public class Duke {
     private static final String listMsg = "Here are the tasks in your list:\n%s";
     private static final String markMsg = "Nice! I've marked this task as done:\n  %s";
     private static final String unmarkMsg = "OK, I've marked this task as not done yet:\n  %s";
-    private static final String invalidCmdMsg = "Invalid command.";
+    private static final String invalidCmdMsg = "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
+    private static final String emptyFieldMsg = "☹ OOPS!!! The %s of a %s cannot be empty.";
+    private static final String invalidIndexMsg = "☹ OOPS!!! The index should be a valid integer.";
+    private static final String negativeIndexMsg = "☹ OOPS!!! The index should be a positive integer.";
+    private static final String outOfBoundsMsg = "☹ OOPS!!! %d is out of range. The list only has %d item%s.";
+    private static final String invalidByeMsg = "☹ OOPS!!! Did you mean \"bye\" without additional arguments?";
+    private static final String invalidListMsg = "☹ OOPS!!! Did you mean \"list\" without additional arguments?";
+    private static final String invalidMarkMsg = "☹ OOPS!!! mark should have exactly one argument.";
+    private static final String invalidUnmarkMsg = "☹ OOPS!!! unmark should have exactly one argument.";
     private static final String goodbyeMsg = "Bye. Hope to see you again soon!";
     private static final Scanner userInput = new Scanner(System.in);
 
     public static void main(String[] args) {
         List<Task> tasks = new ArrayList<Task>();
-        Duke.printMsg(helloMsg + "\n" + requestMsg);
+        printMsg(helloMsg + "\n" + requestMsg);
         while (true) {
             String[] cmd = userInput.nextLine().split(" ");
             if (cmd.length == 0) {
                 continue;
             }
             int idx;
+            Task newTask;
             switch (cmd[0]) {
                 case "bye":
-                    break;
+                    if (cmd.length > 1) {
+                        printMsg(invalidByeMsg);
+                    } else {
+                        break;
+                    }
+                    continue;
                 case "list":
-                    Duke.printMsg(String.format(listMsg, Duke.stringifyList(tasks)));
+                    if (cmd.length > 1) {
+                        printMsg(invalidListMsg);
+                    } else {
+                        printMsg(String.format(listMsg, stringifyList(tasks)));
+                    }
                     continue;
                 case "mark":
-                    idx = Integer.parseInt(cmd[1]) - 1;
-                    tasks.get(idx).mark();
-                    Duke.printMsg(String.format(markMsg, tasks.get(idx).toString()));
+                    if (cmd.length != 2) {
+                        printMsg(invalidMarkMsg);
+                    } else {
+                        idx = getIndex(cmd[1], tasks.size());
+                        if (idx >= 0) {
+                            tasks.get(idx).mark();
+                            printMsg(String.format(markMsg, tasks.get(idx).toString()));
+                        }
+                    }
                     continue;
                 case "unmark":
-                    idx = Integer.parseInt(cmd[1]) - 1;
-                    tasks.get(idx).unmark();
-                    Duke.printMsg(String.format(unmarkMsg, tasks.get(idx).toString()));
+                    if (cmd.length != 2) {
+                        printMsg(invalidUnmarkMsg);
+                    } else {
+                        idx = getIndex(cmd[1], tasks.size());
+                        if (idx >= 0) {
+                            tasks.get(idx).unmark();
+                            printMsg(String.format(unmarkMsg, tasks.get(idx).toString()));
+                        }
+                    }
                     continue;
                 case "todo":
-                    tasks.add(makeToDo(cmd));
-                    Duke.printLastAdd(tasks);
+                    newTask = makeToDo(cmd);
+                    if (newTask != null) {
+                        tasks.add(newTask);
+                        printLastAdd(tasks);
+                    }
                     continue;
                 case "deadline":
-                    tasks.add(makeDeadline(cmd));
-                    Duke.printLastAdd(tasks);
+                    newTask = makeDeadline(cmd);
+                    if (newTask != null) {
+                        tasks.add(makeToDo(cmd));
+                        printLastAdd(tasks);
+                    }
                     continue;
                 case "event":
-                    tasks.add(makeEvent(cmd));
-                    Duke.printLastAdd(tasks);
+                    newTask = makeEvent(cmd);
+                    if (newTask != null) {
+                        tasks.add(makeToDo(cmd));
+                        printLastAdd(tasks);
+                    }
                     continue;
                 default:
-                    Duke.printMsg(invalidCmdMsg);
+                    printMsg(invalidCmdMsg);
                     continue;
             }
             break;
         }
-        Duke.printMsg(goodbyeMsg);
+        printMsg(goodbyeMsg);
     }
 
     private static void printMsg(String msg) {
-        Duke.printResponse(horizontalLine);
+        printResponse(horizontalLine);
         for (String line : msg.split("\n")) {
-            Duke.printResponse(msgIndent + line);
+            printResponse(msgIndent + line);
         }
-        Duke.printResponse(horizontalLine);
+        printResponse(horizontalLine);
         System.out.println();
     }
 
@@ -88,13 +127,17 @@ public class Duke {
     }
 
     private static <T> void printLastAdd(List<T> arr) {
-        Duke.printMsg(String.format(addedMsg, arr.get(arr.size() - 1).toString(), arr.size(), arr.size() == 1 ? "" : "s"));
+        printMsg(String.format(addedMsg, arr.get(arr.size() - 1).toString(), arr.size(), arr.size() == 1 ? "" : "s"));
     }
 
     private static ToDo makeToDo(String[] arr) {
         List<String> item = new ArrayList<String>();
         for (int i = 1; i < arr.length; i++) {
             item.add(arr[i]);
+        }
+        if (item.size() == 0) {
+            printMsg(String.format(emptyFieldMsg, "description", "todo"));
+            return null;
         }
         return new ToDo(String.join(" ", item));
     }
@@ -111,6 +154,14 @@ public class Duke {
                 default:
                     curr.add(arr[i]);
             }
+        }
+        if (item.size() == 0) {
+            printMsg(String.format(emptyFieldMsg, "description", "deadline"));
+            return null;
+        }
+        if (by.size() == 0) {
+            printMsg(String.format(emptyFieldMsg, "by", "deadline"));
+            return null;
         }
         return new Deadline(String.join(" ", item), String.join(" ", by));
     }
@@ -132,6 +183,37 @@ public class Duke {
                     curr.add(arr[i]);
             }
         }
+        if (item.size() == 0) {
+            printMsg(String.format(emptyFieldMsg, "description", "event"));
+            return null;
+        }
+        if (from.size() == 0) {
+            printMsg(String.format(emptyFieldMsg, "from", "event"));
+            return null;
+        }
+        if (to.size() == 0) {
+            printMsg(String.format(emptyFieldMsg, "to", "event"));
+            return null;
+        }
         return new Event(String.join(" ", item), String.join(" ", from), String.join(" ", to));
+    }
+
+    private static int getIndex(String num, int size) {
+        int idx;
+        try {
+            idx = Integer.parseInt(num);
+        } catch (NumberFormatException e) {
+            printMsg(invalidIndexMsg);
+            return -1;
+        }
+        if (idx <= 0) {
+            printMsg(negativeIndexMsg);
+            return -1;
+        }
+        if (idx > size) {
+            printMsg(String.format(outOfBoundsMsg, idx, size, size == 1 ? "" : "s"));
+            return -1;
+        }
+        return --idx;
     }
 }
