79ec87d4aa9618d55aba7edaaa6c5277abd80e58
pra-navi <pranavikiran@gmail.com>
2023-08-20 16:52:13 +0800

    delete

--- src/main/java/Sally.java
+++ src/main/java/Sally.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Sally {
     public static void main(String[] args) {
-        System.out.println("Hey! It's Sally here!\n" + "How can I help you today?\n \n");
+        System.out.println("Hey! It's Sally here!\n" + "How can I help you today?");
 
-        Task[] tasks = new Task[100];
-        int taskCount = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -16,8 +16,8 @@ public class Sally {
                     break;
                 } else if (input.equals("list")) {
                     System.out.println("My List:");
-                    for (int i = 0; i < taskCount; i++) {
-                        Task task = tasks[i];
+                    for (int i = 0; i < tasks.size(); i++) {
+                        Task task = tasks.get(i);
                         System.out.println((i + 1) + ". " + task);
                     }
                 } else if (input.startsWith("mark")) {
@@ -26,10 +26,10 @@ public class Sally {
                     }
 
                     int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
-                    if (taskIndex < 0 || taskIndex >= taskCount) {
+                    if (taskIndex < 0 || taskIndex >= tasks.size()) {
                         System.out.println("OOPS! Provide a valid task number to mark.");
                     } else {
-                        Task task = tasks[taskIndex];
+                        Task task = tasks.get(taskIndex);
                         task.mark();
                         System.out.println("Nice! I've marked this task as done:");
                         System.out.println(" " + task);
@@ -40,10 +40,10 @@ public class Sally {
                     }
 
                     int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
-                    if (taskIndex < 0 || taskIndex >= taskCount) {
+                    if (taskIndex < 0 || taskIndex >= tasks.size()) {
                         System.out.println("OOPS! Provide a valid task number to unmark.");
                     } else {
-                        Task task = tasks[taskIndex];
+                        Task task = tasks.get(taskIndex);
                         task.unmark();
                         System.out.println("Ok, I've marked this task as not done yet:");
                         System.out.println(" " + task);
@@ -53,33 +53,47 @@ public class Sally {
                         throw new SallyException("OOPS! The description of a todo cannot be empty.");
                     }
 
-                    tasks[taskCount] = new Todo(input.substring(5));
-                    taskCount++;
+                    Task newToDo = new Todo(input.substring(5));
+                    tasks.add(newToDo);
                     System.out.println("Added to My List: ");
-                    System.out.println(" " + tasks[taskCount - 1]);
-                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println(" " + newToDo);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                 } else if (input.startsWith("deadline")) {
                     if (input.length() < 10 || !input.contains(" /by ")) {
                         throw new SallyException("OOPS! The description of a deadline cannot be incomplete, you need a 'by'.");
                     }
 
                     String[] parts = input.split(" /by ");
-                    tasks[taskCount] = new Deadline(parts[0].substring(9), parts[1]);
-                    taskCount++;
+                    Task newDeadline = new Deadline(parts[0].substring(9), parts[1]);
+
+                    tasks.add(newDeadline);
                     System.out.println("Added to My List: ");
-                    System.out.println(" " + tasks[taskCount - 1]);
-                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println(" " + newDeadline);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                 } else if (input.startsWith("event")) {
-                    if (input.length() < 7 || !input.contains(" /from ") || !input.contains(" /to ")) {
+                    if (!input.contains(" /from ") || !input.contains(" /to ")) {
                         throw new SallyException("OOPS! The description of an event cannot be incomplete, you need a '/from' and '/to'.");
                     }
 
                     String[] parts = input.split(" /from | /to ");
-                    tasks[taskCount] = new Event(parts[0].substring(6), parts[1], parts[2]);
-                    taskCount++;
+                    Task newEvent = new Event(parts[0].substring(6), parts[1], parts[2]);
+
+                    tasks.add(newEvent);
                     System.out.println("Added to My List:");
-                    System.out.println("  " + tasks[taskCount - 1]);
-                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println(" " + newEvent);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                } else if (input.startsWith("delete")) {
+                    if (input.length() < 7 || !input.contains(" ")) {
+                        throw new SallyException("OOPS! Provide a valid task number to delete.");
+                    }
+                    int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+                    if (taskIndex < 0 || taskIndex >= tasks.size()) {
+                        throw new SallyException("OOPS! Provide a valid task number to delete.");
+                    }
+                    Task deletedTask = tasks.remove(taskIndex);
+                    System.out.println("Noted. I've removed this task:");
+                    System.out.println(" " + deletedTask);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                 } else {
                     System.out.println("Sorry, I do not understand the command :-(");
                 }
