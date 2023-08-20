c001e995378c008262898a3adeb7bb95bc076e56
Blizzeracz <lamkwangzheloysius@gmail.com>
2023-08-20 23:03:43 +0800

    Add the ability to store whatever text entered by user and display them back to user when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
 public class Duke {
 
+
+
     public static void start() {
         String intro = "Hi! This is your AI assistant LoyBoy!\n";
         String question = "What can I do for you today?";
         System.out.println(intro + question);
         Scanner scanner = new Scanner(System.in);
+        String[] items = new String[100];
+        int position = 0;
 
         while (true) {
             String command = scanner.nextLine();
@@ -13,9 +17,15 @@ public class Duke {
                 String outro = "I wish you a pleasant day ahead, goodbye!\n";
                 System.out.println(outro);
                 break;
+            } else if (command.equalsIgnoreCase("list")) {
+                for (int item = 0; items[item] != null; item++)  {
+                    System.out.println(item + 1 + ". " + items[item]);
+                }
+            } else {
+                items[position] = command;
+                position++;
+                System.out.println("You added '" + command + "' to the list!");
             }
-
-            System.out.println("You just said '" + command + "'!");
 
         }
 
