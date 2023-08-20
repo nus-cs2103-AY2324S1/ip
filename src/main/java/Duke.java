b5370172e5358597b417104d791657f89a9c170f
songgthu <songthu0711@gmail.com>
2023-08-20 14:25:25 +0800

    Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " ____     ____    _    _\n"
+                + "|     \\__/    |  | |  | |\n"
+                + "|  | \\ _ / |  |  | |  | |  \n"
+                + "|  |       |  |  | |  | |____\n"
+                + "|__|       |__|  |_|  |______|\n";
+        String horLine = "__________________________________________________________________________";
+        System.out.println(logo);
+        System.out.println(horLine);
+        System.out.println("Hi there, I'm Mil - your personal chatbot.\nHow can I help you today?");
+        System.out.println(horLine);
+        System.out.println("Have a nice day and see you again soon!");
     }
 }
