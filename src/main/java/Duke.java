fe9edcecc4f15382c0ca224d24a793ff3c513aac
Song-Mengfei <e1075530@u.nus.edu>
2023-08-20 15:38:35 +0800

    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,21 +2,44 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Three Wolves B.\n" + "What can I do for you?\n");
+        System.out.println("Hey summoner! I'm Three Wolves B.\n" + "What do you want me to do?\n");
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> lst = new ArrayList<>();
+        ArrayList<Task> lst = new ArrayList<>();
         String cmd = sc.nextLine();
-        while (!cmd.equals("bye")) {
-            if (cmd.equals("list")) {
-                int counter = 1;
-                lst.forEach(x -> System.out.println(lst.indexOf(x) + 1 + ". " + x + "\n"));
+        while (true) {
+            if (cmd.equals("")) {
+                System.out.println("What happened? You said nothing at all!");
+            } else if (cmd.matches("list")) {
+                System.out.println("Here are the tasks in your list");
+                lst.forEach(x -> System.out.println(lst.indexOf(x) + 1 + ". " + x));
+            } else if (cmd.matches("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else if (cmd.matches("mark \\d*")) {
+                int index = Integer.parseInt(cmd.substring(5)) - 1;
+                Task now = lst.get(index);
+                if (now != null) {
+                    System.out.println("Ok, I will mark this task as done:");
+                    now.mark();
+                } else {
+                    System.out.println("There is no such a task.");
+                }
+            } else if (cmd.matches("unmark \\d*")) {
+                int index = Integer.parseInt(cmd.substring(7)) - 1;
+                Task now = lst.get(index);
+                if (now != null) {
+                    System.out.println("Ok, I will mark this task as not done:");
+                    now.unmark();
+                } else {
+                    System.out.println("There is no such a task.");
+                }
             } else {
-                lst.add(cmd);
-                System.out.println("added: " + cmd + "\n");
+                lst.add(new Task(cmd));
+                System.out.println("added: " + cmd);
             }
+            System.out.println("");
             cmd = sc.nextLine();
         }
-        System.out.println("Bye. Hope to see you again soon!");
         sc.close();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,47 @@
+public class Task {
+    /* The name of this task */
+    String name;
+    /* The state whether the task is done or not*/
+    boolean state;
+
+    /** The constructor of tasks
+     *  The state is not done by default
+     *
+     * @param name The name of this Task
+     * **/
+    public Task(String name) {
+        this.name = name;
+        this.state = false;
+    }
+
+    /** The method to mark the state of this task as done
+     */
+    public void mark() {
+        this.state = true;
+        System.out.println(this);
+    }
+
+    /** The method to unmark the state of this task as not done
+     */
+    public void unmark() {
+        this.state = false;
+        System.out.println(this);
+    }
+
+    /** A private method to show out the state of this task
+     *
+     * @return The visible state of this task
+     */
+    private String getState() {
+        return state ? "[X] " : "[ ] ";
+    }
+
+    /** The method to display this task with its state
+     *
+     * @return The method to display this task with its state
+     */
+    @Override
+    public String toString() {
+        return getState() + name;
+    }
+}
