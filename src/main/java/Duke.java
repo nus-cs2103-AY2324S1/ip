b8b384c0463736bb965466513c89483af1def571
songgthu <songthu0711@gmail.com>
2023-08-20 15:58:29 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____     ____    _    _\n"
@@ -14,6 +16,9 @@ public class Duke {
         System.out.println(indentation + horLine);
         Scanner scanner = new Scanner(System.in);
         String input;
+
+        List<String> taskList = new ArrayList<>();
+
         while(scanner.hasNext()) {
             input = scanner.nextLine();
             if(input.equals("bye")) {
@@ -21,9 +26,19 @@ public class Duke {
                 System.out.println(indentation + "Have a nice day and see you again soon!");
                 System.out.println(indentation + horLine);
                 break;
+            } else if(input.equals("list")) {
+                System.out.println(indentation + horLine);
+                int i = 1;
+                for (String task : taskList) {
+                    System.out.println(indentation + i + ". " + task);
+                    i++;
+                }
+                System.out.println(indentation + horLine);
+                continue;
             }
+            taskList.add(input);
             System.out.println(indentation + horLine);
-            System.out.println(indentation + input);
+            System.out.println(indentation + "added: " + input);
             System.out.println(indentation + horLine);
         }
 
