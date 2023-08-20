a419724898ec42fea351b0d506301e66b7bb2eb6
lshaoqin <74029583+lshaoqin@users.noreply.github.com>
2023-08-20 15:03:05 +0800

    add welcome and goodbye messages

--- src/main/java/Juke.java
+++ src/main/java/Juke.java
@@ -1,5 +1,10 @@
 public class Juke {
     public static void main(String[] args) {
+        //Introduce itself to the user
         System.out.println("Hello! I'm Juke!");
+        System.out.println("What can I do for you?");
+
+        //Say goodbye
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
