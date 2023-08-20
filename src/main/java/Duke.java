1153720efbf2c99a68efe87f10639f88c354cd84
markgcera <>
2023-08-20 20:43:43 +0800

    Complete Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String skeletal = "____________________________________________________________\n"
+        Scanner scanner = new Scanner(System.in);
+        String command;
+        String initMsg = "____________________________________________________________\n"
                 + " Hello! I'm IRIS\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n"
+                + " What can I do for you?\n";
+        System.out.println(initMsg);
+        command = scanner.nextLine();
+
+        while (!command.equals("bye")) {
+            String echo = "____________________________________________________________\n"
+                    + command + "\n"
+                    + "____________________________________________________________";
+            System.out.println(echo);
+            command = scanner.nextLine();
+        }
+
+        scanner.close();
+        String endMsg = "____________________________________________________________\n"
                 + "Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________";
-        System.out.println(skeletal);
+        System.out.println(endMsg);
     }
 }
