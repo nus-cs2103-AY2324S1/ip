e5df12949943e70e3695e9e25a68a3843e5eb17a
Blizzeracz <lamkwangzheloysius@gmail.com>
2023-08-20 19:02:04 +0800

    Change how the bot reacts to certain words, echoing or exiting based off the keywords

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
 public class Duke {
-    public static void main(String[] args) {
+
+    public static void start() {
         String intro = "Hi! This is your AI assistant LoyBoy!\n";
-        String question = "What can I do for you today?\n";
-        String outro = "I wish you a pleasant day ahead, goodbye!\n";
-        System.out.println(intro + question + outro);
+        String question = "What can I do for you today?";
+        System.out.println(intro + question);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equalsIgnoreCase("bye")) {
+                String outro = "I wish you a pleasant day ahead, goodbye!\n";
+                System.out.println(outro);
+                break;
+            }
+
+            System.out.println("You just said '" + command + "'!");
+
+        }
+
+        scanner.close();
+    }
+
+
+
+    public static void main(String[] args) {
+        Duke.start();
+
+
     }
 }
 
