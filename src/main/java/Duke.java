3e9462af150b9217b00f47df1d6ec3d293bb7778
Tanveer Singh <tanveersengh@gmail.com>
2023-08-20 16:38:58 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 public class Duke {
+    private static final String chatBotName = "Cristiano";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +8,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greetUser();
+        exit();
+    }
+
+    public static void greetUser() {
+        printLine();
+        System.out.println("Hello! I'm " + chatBotName + "! SUIIII!!!");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void exit() {
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    public static void printLine() {
+        System.out.println("____________________________________________________________");
     }
 }
