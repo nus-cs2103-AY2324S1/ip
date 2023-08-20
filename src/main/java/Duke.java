89ff14fd31cc29d730b5821405d001367565a296
jingting1412 <e0970352@u.nus.edu>
2023-08-20 23:07:25 +0800

    add indentations and lines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         //this is the new name for my chatbot
         String name = "dukey";
 
+        //these are the lines
+        String lines = "        ____________________________________________________________";
+
         //this will be the greeting
-        String greeting = "Hello! I'm " + name + "\nWhat can I do for you?\n";
+        String greeting = "     Hello! I'm " + name + "\nWhat can I do for you?\n";
 
         //this will be goodbye
-        String goodbye = "Bye. Hope to see you again soon!";
+        String goodbye = "      Bye. Hope to see you again soon!";
 
-        System.out.println(greeting + "\n" + goodbye);
+        Scanner sc = new Scanner(System.in);
+
+        //this is what the user typed in
+        String userCommand = sc.nextLine();
+
+
     }
 }
