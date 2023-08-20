b9d455bc76efcf909ed79655d94c0e9e6a5b9c24
AriellaCallista <arielgit>
2023-08-20 13:58:37 +0800

    Improve skeletal version to echo message and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String divider = "   _________________________________ \n";
         String greeting = "   Hello! I'm Sana \n   What can I do for you? \n";
         String bye = "   Bye. Hope to see you again soon! \n";
 
+        Scanner myObj = new Scanner(System.in);
         System.out.println(divider + greeting + divider);
-        System.out.println(divider + bye + divider);
+
+        while(true) {
+            String command = myObj.nextLine();
+            switch(command) {
+                case "bye":
+                    System.out.println(divider + bye + divider);
+                    break;
+                default:
+                    System.out.println(divider + "   " + command + "\n" + divider);
+            }
+        }
     }
 }
