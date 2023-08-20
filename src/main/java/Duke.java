1b6ac69844a7aee06163657f5ee113a4e46a6913
tayruxin <tayruxin00@gmail.com>
2023-08-20 16:12:00 +0800

    Implement Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Hello! I'm Muddy\n" + "What can I do for you?");
+        int number = 0;
+        String[] list = new String[100];
 
         while (true) {
             String input = in.nextLine();
@@ -10,13 +12,22 @@ public class Duke {
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                if (number == 0) {
+                    System.out.println("List is empty");
+                    continue;
+                } else {
+                    System.out.println("List:");
+                    for (int i = 0; i < number; i++) {
+                        String item = list[i];
+                        System.out.println((i + 1) + ". " + item);
+                    }
+                }
+            } else {
+                list[number] = input;
+                number++;
+                System.out.println("added: " + input);
             }
-            if (input.equalsIgnoreCase("hello")) {
-                System.out.println("Hello, what can I help you with?");
-                continue;
-            }
-
-            System.out.println(input);
         }
         in.close();
     }
