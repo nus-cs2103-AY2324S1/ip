84b4204d32c84f49bddff347edd1647776b0b1be
pra-navi <pranavikiran@gmail.com>
2023-08-20 13:53:17 +0800

    Sally welcomes

---
+++ src/main/java/Sally.java
@@ -0,0 +1,5 @@
+public class Sally {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Sally\n" + "What can I do for you?\n \n" + "Bye. Hope to see you again soon!");
+    }
+}
