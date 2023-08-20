19fe59b4b6c3d7c4270d2f0ca892e352f65ca919
Rohan Bahl <Rb@Rb-2.local>
2023-08-21 01:09:06 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         UI display = new UI();
--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
 public class UI {
     private void horizontalLines() {
         System.out.println("____________________________________________________________"); //length taken from sample
     }
 
     public void skeletalChatbot() {
+        Scanner scanner = new Scanner(System.in);
         horizontalLines();
         System.out.println("Hello! I'm Whelmed.");
         System.out.println("What can I do for you?");
+        horizontalLines();
+        String input = scanner.nextLine();
+        while (!input.toLowerCase().equals("bye")) {
+            horizontalLines();
+            System.out.println(input);
+            horizontalLines();
+            input = scanner.nextLine();
+        }
         horizontalLines();
         System.out.println("Bye. Hope to see you again soon!");
         horizontalLines();
