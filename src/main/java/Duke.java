1d0ba10b375026088c02f1d2208141e3b782928e
ktzy0305 <kevintoh0305@gmail.com>
2023-08-20 23:30:10 +0800

    add echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     private static final int LINE_LENGTH = 100;
     public static void main(String[] args) {
-        renderLine();
-        System.out.println("Hello! I'm Skye.");
-        System.out.println("What can I do for you?");
-        renderLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        renderLine();
+        Scanner scanner = new Scanner(System.in);
+
+        printMessage("Hello! I'm Skye, your personal task assistant. \nWhat can I do for you?");
+
+        while (true) {
+            System.out.print("> ");
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                printMessage("Bye. Hope to see you again soon!");
+                scanner.close();
+                System.exit(0);
+            }
+
+            printMessage(userInput);
+        }
     }
 
-    public static void renderLine() {
+    private static void renderLine() {
         System.out.println("_".repeat(LINE_LENGTH));
+    }
+
+    private static void printMessage(String message) {
+        renderLine();
+        System.out.println(message);
+        renderLine();
     }
 }
