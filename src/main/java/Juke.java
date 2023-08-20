2e6addd9f44c1c1ca89b43641a213a3615287178
lshaoqin <74029583+lshaoqin@users.noreply.github.com>
2023-08-20 18:12:27 +0800

    add and list functions

--- src/main/java/Juke.java
+++ src/main/java/Juke.java
@@ -1,10 +1,26 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Juke {
     static void printLine() {
         System.out.println("_______________________________________________________");
     }
     public static void main(String[] args) {
+        class Task {
+            private final String type;
+            private final String desc;
+
+            public Task(String type, String desc) {
+                this.type = type;
+                this.desc = desc;
+            }
+
+            @Override
+            public String toString() {
+                return desc;
+            }
+        }
+        ArrayList<Task> tasks = new ArrayList<Task>();
 
         //Introduce itself to the user
         System.out.println("Hello! I'm Juke!");
@@ -17,10 +33,21 @@ public class Juke {
                 //Say goodbye
                 System.out.println("Bye. Hope to see you again soon!");
                 printLine();
+                scanner.close();  // Close the scanner before exiting
                 return;
             }
+
+            if (input.equalsIgnoreCase("list")) {
+                int count = 1;
+                for (Task task : tasks) {
+                    System.out.println(count + ": " + task.toString());
+                    count++;
+                }
+                printLine();
+            }
             else {
-                System.out.println(input);
+                tasks.add(new Task("todo", input));
+                System.out.println("added: " + input);
                 printLine();
             }
         }
