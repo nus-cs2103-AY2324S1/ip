fd5fb9e315c205c2ef1af3359d5a74dfda73f2cf
shuenj <60507123+shuenj@users.noreply.github.com>
2023-08-20 21:43:32 +0800

    add ToDos, Events, Deadlines to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,7 +16,8 @@ public class Duke {
             input = scanner.nextLine();
             if (input.equals("list")) {
                 for (int i = 0; i < userTasks.size(); i++) {
-                    System.out.println(i + 1 + ". [" + userTasks.get(i).getStatusIcon() + "] " + userTasks.get(i).getDescription());
+                    // System.out.println(i + 1 + ". [" + userTasks.get(i).getStatusIcon() + "] " + userTasks.get(i).getDescription());
+                    System.out.println(userTasks.get(i).toString());
                 }
                 continue;
             }
@@ -33,6 +34,32 @@ public class Duke {
                 userTasks.get(TaskID).markAsUndone();
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println(userTasks.get(TaskID).toString());
+            }
+            // If input has todo, followed by the task, add the task as new Todo.
+            else if (input.startsWith("todo")) {
+                Task task = new Todo (input.substring(5));
+                userTasks.add(task);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(task.toString());
+                System.out.println("Now you have " + userTasks.size() + " tasks in the list.");
+            }
+            // If input has deadline, followed by the task and the deadline, add the task as new Deadline.
+            else if (input.startsWith("deadline")) {
+                String[] inputSplit = input.split(" /by ");
+                Task task = new Deadline(inputSplit[0].substring(9), inputSplit[1]);
+                userTasks.add(task);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(task.toString());
+                System.out.println("Now you have " + userTasks.size() + " tasks in the list.");
+            }
+            // If input has event, followed by event name and start and end dates, add task as new Event.
+            else if (input.startsWith("event")) {
+                String[] inputSplit = input.split(" /");
+                Task task = new Event(inputSplit[0].substring(6), inputSplit[1].substring(5), inputSplit[2].substring(3));
+                userTasks.add(task);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(task.toString());
+                System.out.println("Now you have " + userTasks.size() + " tasks in the list.");
             }
             else { //input as new task
                 Task task = new Task(input);
