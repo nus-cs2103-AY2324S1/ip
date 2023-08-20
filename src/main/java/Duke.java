1f276dcaf74bdcb7ec0c03f300a4cc69e7573666
Pearlynn <e0968226@u.nus.edu>
2023-08-21 01:11:48 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
+        String line = "\t____________________________________________________________";
         System.out.println(line);
-        System.out.println("Hello! I'm Violet");
-        System.out.println("What can I do for you?");
+        System.out.println("\tHello! I'm Violet");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println(line);
+
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println(line);
+            System.out.println("\t" + command);
+            System.out.println(line);
+            command = sc.nextLine();
+        }
+
         System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
