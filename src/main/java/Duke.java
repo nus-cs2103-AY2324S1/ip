b95620eb2868fa29152aef070a3b4fa3401c38ec
shuenj <60507123+shuenj@users.noreply.github.com>
2023-08-20 15:37:34 +0800

    add list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
@@ -5,11 +7,21 @@ public class Duke {
         System.out.println("Hello! I'm " + botName);
         System.out.println("What can I do for you?");
 
+        //Add the ability to store whatever text entered by the user and display them back to the user when requested.
+        ArrayList<String> userTexts = new ArrayList<String>();
+
         Scanner scanner = new Scanner(System.in);
         String input;
         do {
             input = scanner.nextLine();
-            System.out.println("You just said: " + input);
+            if (input.equals("list")) {
+                for (int i = 0; i < userTexts.size(); i++) {
+                    System.out.println(i + 1 + ". " + userTexts.get(i));
+                }
+                continue;
+            }
+            userTexts.add(input);
+            System.out.println("added: " + input);
         } while (!input.equals("bye"));
         System.out.println("Bye. Hope to see you again soon!");
     }
