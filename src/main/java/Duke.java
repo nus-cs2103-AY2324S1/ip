426e88b8230c62ab5dda4f85b83992f9ccb88a83
Carlin Tan <carlintan2001@gmail.com>
2023-08-22 04:06:21 +0800

    Improve skeletal version by echoing commands by user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    // Function that encapsulates message into a message Card template
+    private static String messageCard(String message) {
+        String horizontalLine = "\t____________________________________________________________\n";
+        return horizontalLine + "\t " + message + "\n" + horizontalLine;
+
+    }
+
+    // Main function
     public static void main(String[] args) {
-        String horizontalLine = "____________________________________________________________\n";
         String CHATBOTNAME = "Carl";
-        System.out.println(horizontalLine + "Hello! I'm " + CHATBOTNAME
-        + "\nWhat can I do for you?\n" + horizontalLine + "Bye. Hope to see you again soon! \n"
-        + horizontalLine);
+        System.out.println(messageCard("Hello! I'm " + CHATBOTNAME
+            + "\n\t What can I do for you?"));
+        while (true) {
+            Scanner SC = new Scanner(System.in);
+            String userInput = SC.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println(messageCard("Bye. Hope to see you again soon!"));
+                break;
+            }
+            System.out.println(messageCard(userInput));
+        }
     }
 }
