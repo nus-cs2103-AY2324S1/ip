acf820df2c080471cb410ff3f5e8d55e08a7f3b3
dylkaw <37286651+dylkaw@users.noreply.github.com>
2023-08-21 16:20:15 +0800

    adding deadline, event, todo classes

---
+++ src/main/java/Event.java
@@ -0,0 +1,14 @@
+public class Event extends Task{
+    private String start;
+    private String end;
+    public Event(String task, String start, String end) {
+        super(task);
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + String.format(" (from: %s to: %s)", start, end);
+    }
+}
