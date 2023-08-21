1aaf7ead658f71048731347e8756601ea6f7acbf
Darren159 <darren_low42@outlook.sg>
2023-08-21 21:16:20 +0800

    Add ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+
         String logo = "    ___    _   ___   ________  __      ____      __________  ____  _   __\n"
                 + "   /   |  / | / / | / / __ \\ \\/ /     / __ \\    /_  __/ __ \\/ __ \\/ | / /\n"
                 + "  / /| | /  |/ /  |/ / / / /\\  /_____/ / / /_____/ / / /_/ / / / /  |/ / \n"
@@ -13,7 +14,7 @@ public class Duke {
         String byeMessage = horizontalLine + "Bye. Hope to see you again soon!\n" + horizontalLine;
 
         Scanner myObj = new Scanner(System.in);
-        List<String> list = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
 
         System.out.println(horizontalLine + logo + "Hello! I'm ANNOY-O-TRON!\nWhat can I do for you?\n"
                 + horizontalLine);
@@ -23,11 +24,23 @@ public class Duke {
             System.out.print(horizontalLine);
             if (userInput.equals("list")) {
                 for (int i = 1; i < list.size() + 1; i++) {
-                    System.out.println(i + ". " + list.get(i - 1));
+                    System.out.println(i + "." + list.get(i - 1));
                 }
+            } else if(userInput.contains("mark")) {
+                int index = userInput.charAt(userInput.length() - 1) - '0';
+                Task currentTask = list.get(index - 1);
+                if (userInput.contains("unmark")) {
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    currentTask.markUndone();
+                } else {
+                    System.out.println("Nice! I've marked this task as done:");
+                    currentTask.markDone();
+                }
+                System.out.println(currentTask);
             } else {
                 System.out.println("added: " + userInput);
-                list.add(userInput);
+                Task newTask = new Task(userInput);
+                list.add(newTask);
             }
             System.out.println(horizontalLine);
             userInput = myObj.nextLine();
