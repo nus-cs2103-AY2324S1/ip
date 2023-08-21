ab3e522522ca202373564111f857905ba8536a84
coderhuang559 <huangyixin1f@gmail.com>
2023-08-21 20:27:24 +0800

    add echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,31 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
+        // Send welcome message
+        System.out.println("____________________________________________________________\n" +
+                "Hello! I'm BbabBBB\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________");
 
-        System.out.println("Hello! I'm BbabBBB\n" +
-                "What can I do for you?\n");
+        // Implement function to read user input via keyboard
+        Scanner scan = new Scanner(System.in);
 
-        System.out.println("Bye. Hope to see you again soon!");
+        // Bot exits when user inputs "bye" and echoes when user inputs anything else
+        while (true) {
+            String userInput = scan.nextLine();
+            if (Objects.equals(userInput, "bye")) {
+                System.out.println("____________________________________________________________\n" +
+                        "Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________\n");
+                break;
+            } else {
+                System.out.println("____________________________________________________________\n" +
+                        userInput +
+                        "\n____________________________________________________________\n");
+            }
+        }
+        scan.close();
     }
 }
