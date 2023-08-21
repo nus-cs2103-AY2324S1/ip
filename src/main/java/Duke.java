99cc515c460de4a89675a93313c8bcef013a84da
lordidiot <0xlordidiot@gmail.com>
2023-08-21 22:55:47 +0800

    add task adding and listing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         boolean running = true;
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Pong");
@@ -13,11 +15,17 @@ public class Duke {
         while (running) {
             System.out.print("You:  ");
             String userInput = scanner.nextLine();
+
             if (userInput.equals("bye")) {
                 running = false;
+            } else if (userInput.equals("list")) {
+                System.out.println("Pong: ");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.printf("    %d. %s\n", i+1, tasks.get(i));
+                }
             } else {
-                System.out.print("Pong: ");
-                System.out.println(userInput);
+                tasks.add(userInput);
+                System.out.printf("Pong: [Added] %s\n", userInput);
             }
         }
 
