43cea3a4303a1de9b16cbfda8279ca9b74474e73
LamJiuFong <jiufonglam@Jius-MacBook-Air.local>
2023-08-21 15:10:48 +0800

    Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        welcome();
+        ending();
     }
+    private static void welcome() {
+        System.out.println("Hello! I'm BoxBox \nWhat can I do for you?");
+    }
+    private static void ending() {
+        System.out.println("Bye. Hope to see you again!");
+    }
+
 }
