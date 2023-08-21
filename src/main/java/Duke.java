387737819f0a9c45b94b05600922cf32840c8449
GSgiansen <giansenrockz@gmail.com>
2023-08-21 13:47:51 +0800

    Completed Level-0 features for MeowBot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo)
+        String lines = "______________________________";
+        System.out.println(lines);
+        System.out.println("Hello! I'm MeowBot!");
+        System.out.println("What can I do for you ?");
+        System.out.println(lines);
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
