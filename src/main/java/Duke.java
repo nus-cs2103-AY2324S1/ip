a0297391659a89ca5e5f18a06d1e426e2be9dea0
CJ-Lee01 <chunjie01@live.com>
2023-08-21 18:33:22 +0800

    Added StoreList class and enum commands. For future use.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,32 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static class StoreList {
+        ArrayList<String> list = new ArrayList<>();
+        StoreList() {
+        }
+
+        String add(String item) {
+            list.add(item);
+            return String.format("added: %s", item);
+        }
+        @Override
+        public String toString() {
+            String result = "";
+            for (int i = 0; i < list.size(); i++) {
+                result += String.format("    %d. %s", i + 1, list.get(i));
+            }
+            return result;
+        }
+    }
+
+    enum commands {
+        list,
+        bye
+    }
+
+    StoreList list = new StoreList();
     public static void main(String[] args) {
         String logo = "Nino!";
         System.out.println("Hello, my name is " + logo);
