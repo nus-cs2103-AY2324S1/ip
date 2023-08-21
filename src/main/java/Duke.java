bc920b8152dfb0695e6e3c0e0c7671aa40203841
seanpzk <seanpzk2@gmail.com>
2023-08-21 15:58:19 +0800

    Added error handling functionality. Code reorganised for error handling.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,9 @@
 import java.util.*;
 public class Duke {
+    protected static String H_LINE = "____________________________________________________________\n";
+    protected static boolean botInUse = true;
+    protected static LinkedList<Task> storage = new LinkedList<>();
+
     /**
      * Converts a LinkedList of Task to a string of Numbered List with the Tasks' description and status.
      * @param linkedList LinkedList with Task object(s).
@@ -9,79 +13,105 @@ public class Duke {
         StringBuilder str = new StringBuilder();
         for (int i = 0; i < linkedList.size(); i++) {
             Task currTask = linkedList.get(i);
-            str.append(String.valueOf(i + 1)).append(".")
+            str.append(i + 1).append(".")
                     .append(currTask.toString())
                     .append("\n");
         }
         return str.toString();
     }
-    public static void main(String[] args) {
-        String horizontal_line = "____________________________________________________________\n";
-        System.out.println(horizontal_line
-                            + "Hello! I'm ChadBob.\n"
-                            + "What can I do for you?\n"
-                            + horizontal_line) ;
-        boolean botInUse = true;
-        LinkedList<Task> storage = new LinkedList<>();
-        while(botInUse) {
-            Scanner sc = new Scanner(System.in);
-            String input = sc.nextLine();
-            if (Objects.equals(input, "bye")) {
-                botInUse=false;
-                System.out.println(horizontal_line
-                                    +  "Bye. Hope to see you again soon!\n"
-                                    + horizontal_line);
-            } else if (Objects.equals(input, "list")) {
-                String outputList = Duke.stringifyList(storage);
-                System.out.println(horizontal_line
-                        + "Here are the tasks in your list:\n"
-                        + outputList
-                        + horizontal_line);
-            } else if (input.contains("unmark")) {
-                int a = Integer.parseInt(input.substring(7));
-                storage.get(a - 1).markAsUndone();
-                System.out.println(horizontal_line
-                        + "OK, I've marked this task as not done yet:\n"
-                        + storage.get(a - 1).toString() + "\n"
-                        + horizontal_line);
-            } else if (input.contains("mark")) {
-                int a = Integer.parseInt(input.substring(5));
-                storage.get(a-1).markAsDone();
-                System.out.println(horizontal_line
-                                    + "Nice! I've marked this task as done:\n"
-                                    + storage.get(a - 1).toString() + "\n"
-                                    + horizontal_line);
-            } else if (input.contains("todo")) {
-                String toDoDescription = input.substring(5);
+    
+    public static void listen(String input) throws InvalidUserInputException {
+        if (input.equals("bye")) {
+            botInUse=false;
+            System.out.println(H_LINE
+                    +  "Bye. Hope to see you again soon!\n"
+                    + H_LINE);
+        } else if (input.equals("list")) {
+            String outputList = Duke.stringifyList(storage);
+            System.out.println(H_LINE
+                    + "Here are the tasks in your list:\n"
+                    + outputList
+                    + H_LINE);
+        } else if (input.contains("unmark")) {
+            int a = Integer.parseInt(input.substring(7));
+            storage.get(a - 1).markAsUndone();
+            System.out.println(H_LINE
+                    + "OK, I've marked this task as not done yet:\n"
+                    + storage.get(a - 1).toString() + "\n"
+                    + H_LINE);
+        } else if (input.contains("mark")) {
+            int a = Integer.parseInt(input.substring(5));
+            storage.get(a-1).markAsDone();
+            System.out.println(H_LINE
+                    + "Nice! I've marked this task as done:\n"
+                    + storage.get(a - 1).toString() + "\n"
+                    + H_LINE);
+        } else if (input.contains("todo")) {
+            try {
+                String toDoDescription = input.split("todo ")[1];
                 ToDo toDoTask = new ToDo(toDoDescription);
                 storage.add(toDoTask);
-                System.out.println(horizontal_line
-                                    + "Got it. I've added this task:\n"
-                                    + toDoTask.toString() + "\n"
-                                    + "Now you have " + storage.size() + ((storage.size() > 1) ? " tasks " : " task ") + "in the list.");
-            } else if (input.contains("deadline")) {
+                System.out.println(H_LINE
+                        + "Got it. I've added this task:\n"
+                        + toDoTask + "\n"
+                        + "Now you have " + storage.size() + ((storage.size() > 1) ? " tasks " : " task ") + "in the list.");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(H_LINE
+                        + "☹ OOPS!!! The description of a todo cannot be empty.\n"
+                        + H_LINE);
+            }
+        } else if (input.contains("deadline")) {
+            try {
                 String by = input.split("/by ")[1];
                 String deadlineDescription = input.split("deadline ")[1].split(" /by")[0];
                 Deadline deadlineTask = new Deadline(deadlineDescription, by);
                 storage.add(deadlineTask);
-                System.out.println(horizontal_line
+                System.out.println(H_LINE
                         + "Got it. I've added this task:\n"
-                        + deadlineTask.toString() + "\n"
+                        + deadlineTask + "\n"
                         + "Now you have " + storage.size() + ((storage.size() > 1) ? " tasks " : " task ") + "in the list.");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(H_LINE
+                        + "☹ OOPS!!! The deadline needs to have a task description and /by .\n"
+                        + H_LINE);
+            }
             } else if (input.contains("event")) {
+            try {
                 String from = input.split("/from ")[1].split(" /to")[0];
                 String to = input.split("/to ")[1];
                 String eventDescription = input.split("event ")[1].split(" /from")[0];
                 Event eventTask = new Event(eventDescription, from, to);
                 storage.add(eventTask);
-                System.out.println(horizontal_line
+                System.out.println(H_LINE
                         + "Got it. I've added this task:\n"
-                        + eventTask.toString() + "\n"
+                        + eventTask + "\n"
                         + "Now you have " + storage.size() + ((storage.size() > 1) ? " tasks " : " task ") + "in the list.");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(H_LINE
+                        + "☹ OOPS!!! The event needs to have a task description, /from and /to.\n"
+                        + H_LINE);
             }
-            else {
-                storage.add(new Task(input));
-                System.out.println(horizontal_line + "added: " + input +"\n" + horizontal_line);
+            } else {
+            throw new InvalidUserInputException();
+        }
+    }
+    public static void main(String[] args) {
+        
+        System.out.println(H_LINE
+                            + "Hello! I'm ChadBob.\n"
+                            + "What can I do for you?\n"
+                            + H_LINE) ;
+
+        
+        while(botInUse) {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            try {
+                listen(input);
+            } catch (InvalidUserInputException e) {
+                System.out.println(H_LINE
+                                    + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n"
+                                    + H_LINE);
             }
         }
 
