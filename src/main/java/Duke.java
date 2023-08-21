e48dc3cd25b48c910d609544b4bdbca7a192333d
m1oojv <97022614+m1oojv@users.noreply.github.com>
2023-08-21 13:53:42 +0800

    Level 1. Echo: echos commands entered by the user, and exits when the user types the command bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,42 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        printHorizontalLine();
-        System.out.println("Hello! I'm Jeffrey, your personal AI chatbot, ready to serve you today\n");
-        System.out.println("What can I do for you?\n");
-        printHorizontalLine();
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("Remember, the universe is vast, but I'm always here for you :D .\n");
-        printHorizontalLine();
+        giveChatbotIntro();
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        do {
+            userInput = scanner.nextLine();
+            printHorizontalLine();
+            processInput(userInput);
+            printHorizontalLine();
+        } while (!userInput.equals("bye"));
     }
 
     public static void printHorizontalLine() {
         System.out.println("____________________________________________________________");
+    }
+
+    public static void giveChatbotIntro() {
+        printHorizontalLine();
+        System.out.println("Hello! I'm Jeffrey, your personal AI chatbot, ready to serve you today\n");
+        System.out.println("What can I do for you?\n");
+        printHorizontalLine();
+    }
+
+    public static void giveChatbotOutro() {
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("Remember, the universe is vast, but I'm always here for you :D\n");
+    }
+
+    public static void processInput(String input) {
+        switch (input) {
+            case "bye":
+                giveChatbotOutro();
+                break;
+            default:
+                System.out.println(input);
+                break;
+        }
     }
 }
