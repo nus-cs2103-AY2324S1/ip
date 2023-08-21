5566a97fb97ac685a2d835559b147daef4f61d21
Jiu Fong Lam <>
2023-08-21 20:09:21 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         welcome();
+        Scanner sc = new Scanner(System.in);
+        String x = sc.nextLine();
+        while (! x.equals("bye")) {
+            repeat(x);
+            x = sc.nextLine();
+        }
         ending();
     }
     private static void welcome() {
@@ -9,5 +16,7 @@ public class Duke {
     private static void ending() {
         System.out.println("Bye. Hope to see you again!");
     }
-
+    private static void repeat(String x) {
+        System.out.println(x);
+    }
 }
