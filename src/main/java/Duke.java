b642bc0f0ebf120ef3db0eafca3ebe24b08e1203
Janssen <janssenlau88@gmail.com>
2023-08-21 19:04:01 +0800

    Added delete functionality.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,6 +13,7 @@ public class Duke {
 
         while (!echo.equals("bye")) {
             try {
+                String[] strings = echo.split(" ");
                 if (!echo.isEmpty()) { // Skips processing when echo is empty.
                     if (echo.equals("list")) { // Lists all current tasks.
                         System.out.println("    " +
@@ -26,10 +27,13 @@ public class Duke {
                         }
                         System.out.println("");
 
-                    } else if (echo.startsWith("mark ")) { // Marks a task.
-                        int index = Integer.valueOf(echo.split(" ")[1]) - 1;
-                        if (index > tasks.size()) {
-                            throw new DukeException("Uh-oh, that task does not exist.");
+                    } else if (strings[0].equals("mark")) { // Marks a task.
+                        if (strings.length == 1) {
+                            throw new DukeException("    Whoops! Don't forget to specify the task.\n");
+                        }
+                        int index = Integer.valueOf(strings[1]) - 1;
+                        if (index < 0 || index >= tasks.size()) {
+                            throw new DukeException("    Uh-oh, that task does not exist.\n");
                         }
                         Task task = tasks.get(index);
                         task.mark();
@@ -37,10 +41,13 @@ public class Duke {
                         System.out.println("    Awesome! I've marked this task as done:");
                         System.out.println("      " + task + "\n");
 
-                    } else if (echo.startsWith("unmark ")) { // Unmarks a task.
-                        int index = Integer.valueOf(echo.split(" ")[1]) - 1;
-                        if (index > tasks.size()) {
-                            throw new DukeException("    Uh-oh, that task does not exist.");
+                    } else if (strings[0].equals("unmark")) { // Unmarks a task.
+                        if (strings.length == 1) {
+                            throw new DukeException("    Whoops! Don't forget to specify the task.\n");
+                        }
+                        int index = Integer.valueOf(strings[1]) - 1;
+                        if (index < 0 || index >= tasks.size()) {
+                            throw new DukeException("    Uh-oh, that task does not exist.\n");
                         }
                         Task task = tasks.get(index);
                         task.unmark();
@@ -48,10 +55,9 @@ public class Duke {
                         System.out.println("    Sure thing! I've marked this task as not done yet:");
                         System.out.println("      " + task + "\n");
 
-                    } else if (echo.startsWith("todo ")) { // Adds a todo task.
-                        String[] strings = echo.split(" ");
+                    } else if (strings[0].equals("todo")) { // Adds a todo task.
                         if (strings.length == 1) {
-                            throw new DukeException("    Oh no! You forgot to specify the task!");
+                            throw new DukeException("    Oh no! You forgot to specify the task!\n");
                         }
                         String description = String.join(" ", Arrays.copyOfRange(strings, 1, strings.length));
                         Todo todo = new Todo(description);
@@ -61,11 +67,13 @@ public class Duke {
                         System.out.println("      " + todo);
                         System.out.println("    Now you have " + tasks.size() + (tasks.size() == 1 ? " task" : " tasks") + " in the list.\n");
 
-                    } else if (echo.startsWith("deadline ")) { // Adds a deadline task.
-                        if (!echo.contains("/")) {
-                            throw new DukeException("Whoopsie! The deadline seems a bit confused. Please use '/by' to set it.");
+                    } else if (strings[0].equals("deadline")) { // Adds a deadline task.
+                        if (strings.length == 1) {
+                            throw new DukeException("    Oh no! You forgot to specify the task!\n");
                         }
-                        String[] strings = echo.split(" ");
+                        if (!echo.contains("/")) {
+                            throw new DukeException("    Whoopsie! The deadline seems a bit confused. Please use '/by' to set it.\n");
+                        }
                         int index = -1;
                         for (int i = 0; i < strings.length; i++) {
                             if (strings[i].charAt(0) == '/') {
@@ -83,7 +91,10 @@ public class Duke {
                         System.out.println("      " + deadline);
                         System.out.println("    Now you have " + tasks.size() + (tasks.size() == 1 ? " task" : " tasks") + " in the list.\n");
 
-                    } else if (echo.startsWith("event ")) { // Adds an event task.
+                    } else if (strings[0].equals("event")) { // Adds an event task.
+                        if (strings.length == 1) {
+                            throw new DukeException("    Oh no! You forgot to specify the event!\n");
+                        }
                         int count = 0;
                         for (int i = 0; i < echo.length(); i++) {
                             if (echo.charAt(i) == '/') {
@@ -91,9 +102,8 @@ public class Duke {
                             }
                         }
                         if (count != 2) { //echo should contain two '/'.
-                            throw new DukeException("    Uh-oh! This event seems to have lost its way. Please use '/from' and '/to' to set it.");
+                            throw new DukeException("    Uh-oh! This event seems to have lost its way. Please use '/from' and '/to' to set it.\n");
                         }
-                        String[] strings = echo.split(" ");
                         int indexStart = -1;
                         int indexEnd = -1;
                         boolean first = true;
@@ -119,6 +129,21 @@ public class Duke {
                         System.out.println("    Got it! I've added this task:");
                         System.out.println("      " + event);
                         System.out.println("    Now you have " + tasks.size() + (tasks.size() == 1 ? " task" : " tasks") + " in the list.\n");
+                    } else if (strings[0].equals("delete")) { // Deletes a task.
+                        if (strings.length == 1) {
+                            throw new DukeException("    Whoops! Don't forget to specify the task to be deleted.\n");
+                        }
+                        int index = Integer.valueOf(strings[1]) - 1;
+                        if (index < 0 || index >= tasks.size()) {
+                            throw new DukeException("    Uh-oh, that task does not exist.\n");
+                        }
+                        Task task = tasks.get(index);
+                        tasks.remove(index);
+
+                        System.out.println("    Sure thing! I've removed this task:");
+                        System.out.println("      " + task + "\n");
+                        System.out.println("    Now you have " + tasks.size() + (tasks.size() == 1 ? " task" : " tasks") + " in the list.\n");
+
                     } else { // Invalid arguments
                         throw new DukeException("    Oops! I'm not quite sure what that means...\n");
                     }
