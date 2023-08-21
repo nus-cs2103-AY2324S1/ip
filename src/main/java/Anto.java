83e48d052431a23724df55337fce38875cd1dbb6
tohpinren <tohpinren@gmail.com>
2023-08-21 16:09:22 +0800

    Level-0: Abstract printing, rename Duke to Anto

---
+++ src/main/java/Anto.java
@@ -0,0 +1,9 @@
+public class Anto {
+    public static void main(String[] args) {
+        Printing.printBlock();
+        Printing.greeting();
+        Printing.printBlock();
+        Printing.bye();
+        Printing.printBlock();
+    }
+}
---
+++ src/main/java/Printing.java
@@ -0,0 +1,13 @@
+public class Printing {
+    public static void printBlock() {
+        System.out.println("████████████████████████████████████████████████");
+    }
+
+    public static void greeting() {
+        System.out.println("Hello I'm Anto\nWhat can I do for you?");;
+    }
+
+    public static void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
