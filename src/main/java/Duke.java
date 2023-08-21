5718e9ccfb40c4b17fa1e35806bbd3e762d5f85d
sushiyade <68842066+sushiyade@users.noreply.github.com>
2023-08-21 13:09:25 +0800

    New level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,13 +7,13 @@ public class Duke {
         String[] toDoList = new String[100];
         int i = 0;
         System.out.println(
-                "Hello! I'm Sunacchi\n" +
+                "Hello! I'm Sunacchi!\n" +
                         "What can I do for you?\n"
         );
 
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 
-        String userInput = myObj.nextLine();
+        String userInput = myObj.next();
         while (i < 100) {
             if (userInput.equals(EXIT_PHRASE)) {
                 System.out.println("Bye. Hope to see you again soon!");
@@ -23,13 +23,17 @@ public class Duke {
                 for (int j = 0; j < i; j++) {
                     System.out.println(j + 1 + "." + " " + toDoList[j]);
                 }
-                userInput = myObj.nextLine();
+                userInput = myObj.next();
+                continue;
+            }
+            if (userInput.equals("")) {
+                userInput = myObj.next();
                 continue;
             }
             toDoList[i] = userInput;
             System.out.println("added: " + userInput);
             i++;
-            userInput = myObj.nextLine();
+            userInput = myObj.next();
         }
     }
 }
