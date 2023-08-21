2d2e4e8067097523f162395af15d98ee0b0131c6
Nam Do Hyun <e0638876@u.nus.edu>
2023-08-21 17:00:56 +0800

    refactor: made functions that receive inputs, check for exit, and echoes.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,33 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void echo() {
+
+    public static void echo(String input) {
+        System.out.println("    " + input);
+        System.out.println("--------------------------------");
+    }
+    public static boolean continueOrNot(String input) {
+        if (input.equals("bye")) {
+            return false;
+        }
+        return true;
+    }
+    public static String input() {
         Scanner myInput = new Scanner(System.in);
         String reply = myInput.nextLine();
-        while (!reply.equals("bye")) {
-            System.out.println(reply + "... ok...");
-            System.out.println("--------------------------------");
-            reply = myInput.nextLine();
-        }
-        System.out.println("Bye..");
         System.out.println("--------------------------------");
+        return reply;
     }
     public static void greeting() {
         System.out.println("Hello.. I'm ekuD..");
         System.out.println("I probably won't be much of a help.. But ask me something..");
         System.out.println("--------------------------------");
-        echo();
+        String input = input();
+        while (continueOrNot(input)) {
+            echo(input);
+            input = input();
+        }
+        System.out.println("    bye...");
     }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
