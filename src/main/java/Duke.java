bbe4ba6777a6e9cdb2adbb896fb61cf629c4f3a7
samuelim01 <samuelim01@gmail.com>
2023-08-21 18:46:15 +0800

    Implemented Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
 
     private static String name = "SoCrates";
     private static String line =
         "\t____________________________________________________________";
+    private static Scanner scanner = new Scanner(System.in);
 
 
     private static void printMessage(String message) {
         System.out.println(line);
         System.out.println("\t" + message);
-        System.out.println("\n" + line);
+        System.out.println("\n" + line + "\n");
     }
 
     private static void printMessage(String[] message) {
@@ -16,7 +19,7 @@ public class Duke {
         for (String messageLine : message) {
             System.out.println("\t" + messageLine);
         }
-        System.out.println("\n" + line);
+        System.out.println("\n" + line + "\n");
     }
 
     public static void printWelcome() {
@@ -31,8 +34,18 @@ public class Duke {
     public static void printExit() {
         printMessage("Bye. Hope to see you again soon!");
     }
+
     public static void main(String[] args) {
         printWelcome();
+
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+
+            printMessage(command);
+        }
 
         printExit();
     }
