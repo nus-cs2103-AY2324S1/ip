18c5329e08bd4a402b6ac0e5a1362529a8340a33
Jweng88 <108978684+Jweng88@users.noreply.github.com>
2023-08-21 18:24:40 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String horizontalLine = "________________________________________________________________";
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println(horizontalLine);
         System.out.println("Hello! I'm Bob");
@@ -12,15 +14,26 @@ public class Duke {
         while (true) {
             String userInput = scanner.nextLine();
             System.out.println(horizontalLine);
-            System.out.println(" " + userInput);
-            System.out.println(horizontalLine);
 
             if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(horizontalLine);
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                taskCount = addTask(tasks, taskCount, userInput);
+                System.out.println("added: " + userInput);
             }
+            System.out.println(horizontalLine);
         }
         scanner.close();
+    }
+
+    public static int addTask(String[] tasks, int count, String task) {
+        tasks[count] = task;
+        return count + 1;
     }
 }
