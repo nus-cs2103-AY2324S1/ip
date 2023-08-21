15204c02961a6973c0dd6ff68efba830ead7534b
Selwyn Ang <selwynang.01@gmail.com>
2023-08-21 11:13:53 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
+    /**
+     * The name of the chatbot
+     */
     private static final String NAME = "Duke Prince";
+
+    /**
+     * The template for spaces within the program
+     */
     private static final String SPACER = "----------------------------------------------------";
+
+    /**
+     * The boolean value for whether program should continue getting user input
+     */
+    private static boolean getInput = true;
+
+    /**
+     * This is the main method to run the program
+     * @param args
+     */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,19 +27,50 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println(SPACER);
         greet();
-        System.out.println(SPACER);
+
+        // Creating scanner object to get user input
+        Scanner scanner = new Scanner(System.in);
+        String response = scanner.nextLine();
+
+        while(getInput) {
+            if (response.equals("bye")) {
+                getInput = false;
+            } else {
+                echo(response);
+                response = scanner.nextLine();
+            }
+        }
+
         bye();
-        System.out.println(SPACER);
     }
 
+    /**
+     * This method greets the user upon starting the program
+     */
     private static void greet() {
+        System.out.println(SPACER);
         System.out.println("Hello! I'm " + NAME);
         System.out.println("What can I do for you?");
+        System.out.println(SPACER);
     }
 
+    /**
+     * This method causes program to echo the user's input
+     * @param userInput
+     */
+    private static void echo(String userInput) {
+        System.out.println(SPACER);
+        System.out.println(userInput);
+        System.out.println(SPACER);
+    }
+
+    /**
+     * This method says bye when user exits the program
+     */
     private static void bye() {
+        System.out.println(SPACER);
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(SPACER);
     }
 }
