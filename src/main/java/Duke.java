23914e0e0ce906e76edfd0aec2eff7a8b926d8ef
Jair Goh <lilozz2>
2023-08-21 18:22:52 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,23 +2,31 @@ import java.util.Scanner;
 public class Duke {
     public static String line = "----------------------------------------------------\n";
     public static String format_response(String response) {
-        return response + "\n" + line;
+        return response + "\n\n" + line;
     }
     public static void greet() {
         System.out.println(line);
         System.out.println(format_response(
                 "Hello I'm Project54\n" +
-                "What can I do for you?\n"
+                "What can I do for you?"
         ));
     }
     public static void bye() {
         System.out.println(format_response(
-                "Bye. Hope to see you again soon\n"
+                "Bye. Hope to see you again soon"
         ));
     }
     public static void main(String[] args) {
 
         greet();
+
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        String response = myObj.nextLine();
+        while (!response.equals("bye")) {
+            System.out.println(format_response(response));  // Output user input
+            response = myObj.nextLine(); // Read user input
+        }
+
         bye();
 
     }
