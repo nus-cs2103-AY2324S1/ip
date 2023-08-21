582ec3d1972dc3bba914375b0e05e0726eaf5bd3
cmHuang777 <e0934116@u.nus.edu>
2023-08-21 09:52:38 +0800

    Modified greetings and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
 public class Duke {
+    private static String CHAT_BOT_NAME = "Genos";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I'm " + Duke.CHAT_BOT_NAME);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
