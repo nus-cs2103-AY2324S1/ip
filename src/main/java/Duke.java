cbf29a3f9a93018c9d5bef3491c3dba27dd8b454
geraldngjx <geraldngjx@gmail.com>
2023-08-21 11:36:18 +0800

    Add feature to store text by user and display storage when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -10,6 +11,7 @@ public class Duke {
 //        System.out.println("Hello from\n" + logo);
 
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> lst = new ArrayList<>();
 
         String line = "\n_____________________________________________________\n";
 
@@ -20,15 +22,22 @@ public class Duke {
 
         while (true) {
             String input = sc.nextLine();
-            if (!input.equals("bye")) {
-                System.out.println(line);
-                System.out.println(input);
-                System.out.println(line);
-            } else {
+            if (input.equals("bye")) {
                 System.out.println(line);
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < lst.size(); i++) {
+                    System.out.println((i + 1) + ". " + lst.get(i) + "\n");
+                }
+                System.out.println(line);
+            } else {
+                System.out.println(line);
+                System.out.println("added: " + input);
+                lst.add(input);
+                System.out.println(line);
             }
         }
     }
