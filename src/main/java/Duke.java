7ac613e2209edf44364cf115179d9cff20739c61
Nam Do Hyun <e0638876@u.nus.edu>
2023-08-21 15:02:05 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void greeting() {
-        System.out.println("Hello! I'm ekuD.");
-        System.out.println("What can I do for you?");
+    public static void echo() {
+        Scanner myInput = new Scanner(System.in);
+        String reply = myInput.nextLine();
+        while (!reply.equals("bye")) {
+            System.out.println(reply + "... ok...");
+            System.out.println("--------------------------------");
+            reply = myInput.nextLine();
+        }
+        System.out.println("Bye..");
         System.out.println("--------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
+    }
+    public static void greeting() {
+        System.out.println("Hello.. I'm ekuD..");
+        System.out.println("I probably won't be much of a help.. But ask me something..");
+        System.out.println("--------------------------------");
+        echo();
     }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
