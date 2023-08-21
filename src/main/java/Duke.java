cce002057d55d086dcd6be9c81879d04c9b08526
songfangyl <e0960536@u.nus.edu>
2023-08-21 14:21:31 +0800

    add mark and unmark features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,45 @@
 import java.util.Objects;
 import java.util.Scanner;
 public class Duke {
+    private static int index = 0;
+    private static Task[] tasks = new Task[100];
+
+    public static void listTasks() {
+        for (int i = 0; i < index; i++) {
+            System.out.println("\t" + (i + 1) + "." + tasks[i].toString());
+        }
+    }
+
+    public static void addTask(String input) {
+        tasks[index++] = new Task(input);
+        System.out.println("\tadded: " + input);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         String LOGO = "   /\\_/\\  \n" +
                 "  ( o.o ) \n" +
                 "   > ^ <\n";
         String GREETING = "Hello(@.@), I'm NiHao \nWhat can I do for you?";
         String EXIT = "Bye(T.T), Hope to see you again soon!";
-        String HORILINE = "_____________________________________________________________";
+
+
         System.out.println(LOGO + GREETING);
-        String[] toDoList = new String[100];
-        int index = 0;
+
         while (true) {
-            String input = sc.nextLine();
+            String input = sc.next();
             if (Objects.equals(input, "exit")) {
                 break;
             } else if (Objects.equals(input, "list")) {
-                for (int i = 0; i < index; i++) {
-                    System.out.println("\t" + (i + 1) + ": " + toDoList[i]);
-                }
+                listTasks();
+            } else if (Objects.equals(input, "mark")) {
+                int i = sc.nextInt();
+                tasks[i - 1].mark();
+            } else if (Objects.equals(input, "unmark")){
+                int i = sc.nextInt();
+                tasks[i - 1].unmark();
             } else {
-                toDoList[index++] = input;
-                System.out.println("\tadded: " + input);
+                addTask(input);
             }
         }
         System.out.println(EXIT);
