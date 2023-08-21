f7403e4cde07bc0e34fa6b7edb057458767223e4
Gabriel4357 <gabrielseethor.jh@gmail.com>
2023-08-21 16:13:17 +0800

    implement initial skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,20 @@
 public class Duke {
-    private String name = "Alfred";
+    private static String name = "Alfred";
+
+    public static void println() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static String  getName() {
+        return Duke.name;
+    }
 
     public static void main(String[] args) {
-
-        System.out.println("Hello from\n");
+        println();
+        System.out.println("Hello I'm " + getName());
+        System.out.println("What can I do for you?");
+        println();
+        System.out.println("Bye. Hope to see you again soon!");
+        println();
     }
 }
