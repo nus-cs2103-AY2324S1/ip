f018e480950f8314c0ce5e01827651c282086ea0
TehOPanas <lzx122603@gmail.com>
2023-08-21 12:43:14 +0800

    Implemented starting and ending messages.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,42 @@
 public class Duke {
+
+    private static char horizontal_line = '\u2500';
+    private static String name = "Chewbacca";
+
     public static void main(String[] args) {
+
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
+
         System.out.println("Hello from\n" + logo);
+
+        start();
+        end();
+    }
+
+    private static String drawLine() {
+        String line = "";
+        for (int i = 0; i < 50; i++)
+            line += horizontal_line;
+        return line;
+    }
+    private static void start() {
+
+        System.out.println(drawLine());
+        System.out.println("Rrrruuuurrr, I am " + name + ", son of Attichitcuk");
+        System.out.println("How can Chewie help?\n");
+
+    }
+
+    private static void end() {
+        System.out.println(drawLine());
+        System.out.println("Chewie is going home now.\nBye bye.\n");
+        System.out.println(drawLine());
+
     }
 }
