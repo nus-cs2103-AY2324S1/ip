a3030c0306d9b8be90f3d13a92dae18e87ae2214
ChongWeiJie29 <chongweijie29@gmail.com>
2023-08-21 11:33:09 +0800

    Complete Level 1: Echo
    Make bot echo command by user.
    Exits only when user says bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     private static String greetingString =  "Hello! I'm ChampionSOS\nWhat can I do for you?";
     private static String exitString =  "Bye. Hope to see you again soon!";
     public static void main(String[] args) {
         System.out.println(Duke.greetingString);
+        Scanner myObj = new Scanner(System.in);
+        String userInput = myObj.nextLine();
+        while (!userInput.equals("bye")) {
+                System.out.println(userInput);
+                userInput = myObj.nextLine();
+        }
         System.out.println(Duke.exitString);
     }
 }
