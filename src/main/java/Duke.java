ca50fc6b00e3b86ff84a0913a83433b897631d66
Say Keong <pohsaykeong@gmail.com>
2023-08-21 15:53:02 +0800

    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> taskList = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -19,21 +19,59 @@ public class Duke {
 
         boolean isBotRunning = true;
         while (isBotRunning) {
-            String command = scanner.nextLine();
-            if (command.equals("bye")) {
-                isBotRunning = false;
-                sayBye();
-            } else if (command.equals("list")) {
-                int numTask = 1;
-                for (String task : taskList) {
-                    System.out.printf("%d. %s\n", numTask, task);
-                    numTask += 1;
+            String[] userIn = scanner.nextLine().split(" ");
+            String command = userIn[0];
+            switch (command) {
+                case "bye":
+                    isBotRunning = false;
+                    sayBye();
+                    break;
+                case "list":
+                    int numTask = 1;
+                    for (Task task : taskList) {
+                        System.out.printf("%d.%s\n", numTask, task);
+                        numTask += 1;
+                    }
+                    printHorizontalLine();
+                    break;
+                case "mark": {
+                    int taskId = Integer.parseInt(userIn[1]) - 1;
+                    if (taskId >= taskList.size()) {
+                        printHorizontalLine();
+                        System.out.println("Task not found!");
+                        printHorizontalLine();
+                        break;
+                    }
+                    Task doneTask = taskList.get(taskId);
+                    doneTask.mark();
+                    printHorizontalLine();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(doneTask);
+                    printHorizontalLine();
+                    break;
                 }
-            } else {
-                printHorizontalLine();
-                taskList.add(command);
-                System.out.printf("added: %s%n", command);
-                printHorizontalLine();
+                case "unmark": {
+                    int taskId = Integer.parseInt(userIn[1]) - 1;
+                    if (taskId >= taskList.size()) {
+                        printHorizontalLine();
+                        System.out.println("Task not found!");
+                        printHorizontalLine();
+                        break;
+                    }
+                    Task unmarkTask = taskList.get(taskId);
+                    unmarkTask.unMark();
+                    printHorizontalLine();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(unmarkTask);
+                    printHorizontalLine();
+                    break;
+                }
+                default:
+                    printHorizontalLine();
+                    taskList.add(new Task(command));
+                    System.out.printf("added: %s%n", command);
+                    printHorizontalLine();
+                    break;
             }
         }
 
