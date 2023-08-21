452735866ed65367eddb8600aabad358b606e835
CelestineTan03 <e0968156@u.nus.edu>
2023-08-21 17:16:18 +0800

    Chatbot now echos user's command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String greetings = "Hello! I'm Botty!\nWhat can I do for you?\n";
-        String farewell = "Bye. Hope to see you again soon!";
-        System.out.println(greetings + farewell);
+        String farewell = "Bye. Hope to see you again soon!\n";
+        System.out.println(greetings);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                System.out.println(farewell);
+                sc.close();
+                break;
+            } else {
+                System.out.println(command + "\n");
+            }
+        }
     }
 }
