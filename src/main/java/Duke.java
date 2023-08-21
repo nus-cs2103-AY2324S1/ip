485d73532209641c87f5c673a670628b9720ab41
Say Keong <pohsaykeong@gmail.com>
2023-08-21 14:45:33 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -10,11 +14,29 @@ public class Duke {
         System.out.println("Hello! I'm Zac");
         System.out.println("What can I do for you?");
         printHorizontalLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        printHorizontalLine();
+
+        boolean isBotRunning = true;
+        while (isBotRunning) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                isBotRunning = false;
+                sayBye();
+            } else {
+                printHorizontalLine();
+                System.out.println(command);
+                printHorizontalLine();
+            }
+        }
+
     }
 
     public static void printHorizontalLine() {
         System.out.println("____________________________________________________________");
+    }
+
+    public static void sayBye() {
+        printHorizontalLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontalLine();
     }
 }
