8184cb422b25c0b60a93f2ad11adc8840af79955
Ryan Ong <59072730+ryanozx@users.noreply.github.com>
2023-08-21 10:00:06 +0800

    Renamed bot and implemented greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
 public class Duke {
+    static String name = "Atlas";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        exit();
+    }
+
+    public static void greet() {
+        String logo =
+                "        _______ _                _____ \n" +
+                        "     /\\|__   __| |        /\\    / ____|\n" +
+                        "    /  \\  | |  | |       /  \\  | (___  \n" +
+                        "   / /\\ \\ | |  | |      / /\\ \\  \\___ \\ \n" +
+                        "  / ____ \\| |  | |____ / ____ \\ ____) |\n" +
+                        " /_/    \\_\\_|  |______/_/    \\_\\_____/ \n";
+        System.out.println(logo);
+        printHorizontalLine();
+        System.out.printf("Hello, I'm %s!%n", Duke.name);
+        System.out.println("What would you like me to do today?");
+        printHorizontalLine();
+    }
+
+    public static void exit() {
+        System.out.println("Goodbye!");
+        printHorizontalLine();
+    }
+
+    public static void printHorizontalLine() {
+        int consoleWidth = 80;
+        String line = "_".repeat(consoleWidth);
+        System.out.println(line);
     }
 }
