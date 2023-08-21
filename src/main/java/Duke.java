404661f216aee5251f99e1a67fa91b197055d20e
dylkaw <37286651+dylkaw@users.noreply.github.com>
2023-08-21 12:52:51 +0800

    renaming bot and creating greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
+    static void greet() {
+        String greeting = "_________________________________________________\n"
+                + " Hello! I'm Glub!\n"
+                + " What can I do for you?\n"
+                + "_________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "_________________________________________________\n";
+        System.out.println(greeting);
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
     }
 }
