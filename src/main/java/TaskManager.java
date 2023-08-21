45dadbed92a4b1983c05fc156e5120c1ce4baca1
seanpzk <seanpzk2@gmail.com>
2023-08-21 17:45:08 +0800

    Added Task Manager class to handle collection of tasks.

---
+++ src/main/java/TaskManager.java
@@ -0,0 +1,38 @@
+import java.util.LinkedList;
+
+public class TaskManager {
+    protected LinkedList<Task> storage;
+
+    /**
+     * Constructor to build a Task Manager.
+     */
+    public TaskManager() {
+        this.storage = new LinkedList<Task>();
+    }
+
+    public void addTask(Task task) {
+        this.storage.add(task);
+    }
+
+    public Task getTask(int taskIndex) {
+        return this.storage.get(taskIndex);
+    }
+
+    public void deleteTask(int taskIndex) {
+        this.storage.remove(taskIndex);
+    }
+
+    public int getSize() {
+        return this.storage.size();
+    }
+    public String outputNumberedList() {
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < this.storage.size(); i++) {
+            Task currTask = this.storage.get(i);
+            str.append(i + 1).append(".")
+                    .append(currTask.toString())
+                    .append("\n");
+        }
+        return str.toString();
+    }
+}
