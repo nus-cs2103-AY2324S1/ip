5578cd026bfd4c20c0de049e9c5aad7006771084
siqirua <lsq6918086@gmail.com>
2023-08-22 01:12:31 +0800

    Add Echo Feature

--- src/main/java/Rua.java
+++ src/main/java/Rua.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Rua {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n" +
-                " Hello! I'm Rua, your chatbot\n" +
+        Scanner sc = new Scanner(System.in);
+
+        String greeting = "____________________________________________________________\n" +
+                " Hello! I'm Rua, your ChatBot\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                "____________________________________________________________\n";
+        String goodbye= "____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
-        System.out.println(logo);
+                "____________________________________________________________\n";
+        System.out.println(greeting);
+
+        String command = sc.nextLine();
+        while (!command.equals("bye")) {
+            String message =  "____________________________________________________________\n" +
+                    command + "\n" +
+                    "____________________________________________________________\n";
+            System.out.println(message);
+            command = sc.nextLine();
+        }
+        System.out.println(goodbye);
+
     }
 }
