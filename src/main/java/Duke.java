59a65ba065b6cbff22924b77da46c3d3568f6cde
LWZ19 <e0985886@u.nus.edu>
2023-08-21 21:28:51 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Duke {
     private static final String NAME = "404";
-    private static final String SPACE = "     ";
+    private static final String INDENT = "     ";
     private static final int STORE_SIZE = 100;
+    private static Task[] tasks = new Task[STORE_SIZE];
+    private static int task_pointer = 0;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Task[] tasks = new Task[STORE_SIZE];
-        int task_pointer = 0;
         printLine();
         String greeting = String.format("%sHello! I'm %s%n%sWhat can I do for you?",
-                                        SPACE, NAME, SPACE);
+                                        INDENT, NAME, INDENT);
         System.out.println(greeting);
         printLine();
         System.out.println();
@@ -22,38 +22,40 @@ public class Duke {
             printLine();
             switch(command) {
                 case "list":
-                    if (task_pointer == 0) {
-                        System.out.printf("%sSorry, there is nothing to list!%n", SPACE);
-                        break;
-                    } else {
-                        System.out.printf("%sHere are the tasks in your list:%n", SPACE);
-                    }
-                    for (int i = 0; i < task_pointer; i++) {
-                        if (tasks[i] == null) {
-                            break;
-                        } else {
-                            String out = String.format("%s%d.%s", SPACE, i + 1, tasks[i]);
-                            System.out.println(out);
-                        }
-                    }
+                    listTask();
                     break;
 
                 case "mark":
-                    int indexMark = Integer.valueOf(split[1]) - 1;
-                    System.out.printf("%sNice! I've marked this task as done:%n%s  %s%n",
-                            SPACE, SPACE, tasks[indexMark].markAsDone());
+                    markAndUnmarkTask(text, true);
                     break;
 
                 case "unmark":
-                    int indexUnmark = Integer.valueOf(split[1]) - 1;
-                    System.out.printf("%sOK, I've marked this task as not done yet:%n%s  %s%n",
-                            SPACE, SPACE, tasks[indexUnmark].markAsNotDone());
+                    markAndUnmarkTask(text, false);
                     break;
 
-                default:
-                    System.out.printf("%sadded: %s%n", SPACE, text);
-                    tasks[task_pointer] = new Task(text);
-                    task_pointer++;
+                case "todo":
+                    String todoTask = text.substring(text.indexOf(" ") + 1);
+                    Task todo = new Todo(todoTask);
+                    addTask(todo);
+                    break;
+
+                case "deadline":
+                    String deadlineTask = text.substring(text.indexOf(" ") + 1,
+                                                         text.indexOf("/by") - 1);
+                    String by = text.substring(text.indexOf("/by") + 4);
+                    Task deadline = new Deadline(deadlineTask, by);
+                    addTask(deadline);
+                    break;
+
+                case "event":
+                    String eventTask = text.substring(text.indexOf(" ") + 1,
+                            text.indexOf("/from") - 1);
+                    String from = text.substring(text.indexOf("/from") + 6,
+                            text.indexOf("/to") - 1);
+                    String to = text.substring(text.indexOf("/to") + 4);
+                    Task event = new Event(eventTask, from, to);
+                    addTask(event);
+                    break;
             }
             printLine();
             System.out.println();
@@ -62,7 +64,7 @@ public class Duke {
             command = split[0];
         }
         printLine();
-        System.out.printf("%sBye. Hope to see you again soon!%n", SPACE);
+        System.out.printf("%sBye. Hope to see you again soon!%n", INDENT);
         printLine();
         sc.close();
     }
@@ -70,5 +72,46 @@ public class Duke {
     private static void printLine() {
         String line =  "    ____________________________________________________________";
         System.out.println(line);
+    }
+
+    private static void addTask(Task task) {
+        tasks[task_pointer] = task;
+        task_pointer++;
+        System.out.printf("%sGot it. I've added this task:%n" +
+                          "%s  %s%n" +
+                          "%sNow you have %d tasks in the list.%n",
+                          INDENT, INDENT, task, INDENT, task_pointer);
+    }
+
+    private static void listTask() {
+        if (task_pointer == 0) {
+            System.out.printf("%sSorry, there is nothing to list!%n", INDENT);
+            return;
+        } else {
+            System.out.printf("%sHere are the tasks in your list:%n", INDENT);
+        }
+
+        for (int i = 0; i < task_pointer; i++) {
+            if (tasks[i] == null) {
+                break;
+            } else {
+                String out = String.format("%s%d.%s", INDENT, i + 1, tasks[i]);
+                System.out.println(out);
+            }
+        }
+    }
+
+    private static void markAndUnmarkTask(String text, boolean mark) {
+        String message;
+        String task;
+        int index = Integer.valueOf(text.substring(text.indexOf(' ') + 1)) - 1;
+        if (mark) {
+            message = "Nice! I've marked this task as done:";
+            task = tasks[index].markAsDone();
+        } else {
+            message = "OK, I've marked this task as not done yet:";
+            task = tasks[index].markAsNotDone();
+        }
+        System.out.printf("%s%s%n%s  %s%n", INDENT, message, INDENT, task);
     }
 }
