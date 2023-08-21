8a0c9d257de3c42803695d95aa50947821fb5ab5
GSgiansen <giansenrockz@gmail.com>
2023-08-21 14:46:16 +0800

    Fixed list indexing to show from 1 onwards instead of 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 public class Duke {
-    String[] Tasklist;
+    Task[] Tasklist;
     int counter;
     String lines;
 
     public Duke() {
-        this.Tasklist = new String[100];
+        this.Tasklist = new Task[100];
         this.counter = 0;
         this.lines = "______________________________";
     }
@@ -27,16 +27,35 @@ public class Duke {
         String command = scan.nextLine();
 
         while (!command.equals("bye")) {
+            // Keywords are "list", "mark" and "unmark"
             if (command.equals("list")) {
                 System.out.println(lines);
-                for (int i = 0; i < meowBot.counter; i++) {
-                    System.out.println(i + ". " + meowBot.Tasklist[i]);
+                for (int i = 1; i < meowBot.counter + 1; i++) {
+                    System.out.println(i + ". " + meowBot.Tasklist[i-1]);
                 }
                 System.out.println(lines);
+            }
+            else if (command.startsWith("mark ")){
+                int tasknumber = Integer.parseInt(command.substring(5));
+                System.out.println("tasknumber is"+ tasknumber);
+                Task wantedtask = meowBot.Tasklist[tasknumber - 1]; //account for 0 indexing
+                wantedtask.markCompleted();
+                System.out.println("Nice! I've meowrked this task as done: ");
+                System.out.println("   " + wantedtask);
+            }
+            else if (command.startsWith("unmark ")) {
+                int tasknumber = Integer.parseInt(command.substring(5));
+//                System.out.println("tasknumber is"+ tasknumber);
+                Task wantedtask = meowBot.Tasklist[tasknumber - 1]; //account for 0 indexing
+                wantedtask.markUncompleted();
+                System.out.println("Ok, get your task done soon, I'll be waiting!");
+                System.out.println("   " + wantedtask);
             }
             else{
                 meowBot.addTask(command);
             }
+
+            // asks for the next keyword
             command = scan.nextLine();
 
         }
@@ -46,7 +65,8 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    void addTask(String task){
+    void addTask(String taskname){
+        Task task = new Task(taskname);
         this.Tasklist[counter] = task;
         counter ++;
         System.out.println(lines);
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    boolean completed;
+    String name;
+    public Task(String name) {
+        this.completed = false;
+        this.name = name;
+    }
+
+    public boolean getComplete() {
+        return this.completed;
+    }
+
+    public void markCompleted() {
+        this.completed = true;
+    }
+
+    public void markUncompleted() {
+        this.completed = false;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+
+    }
+
+
+
+}
