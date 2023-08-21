c184e0da8f22a86760914e54e40443e97b1dd24b
GlendaChong <glendachong04@gmail.com>
2023-08-21 11:22:38 +0800

    Rename chatbot, greet user, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Greet user
+        System.out.println("____________________________________________________________\n"
+                            + "Hello! I'm Glenda!\n"
+                            + "What can I do for you?");
+
+        // Exit
+        System.out.println("____________________________________________________________\n"
+                            + "Bye. Hope to see you again soon!\n"
+                            + "____________________________________________________________\n");
     }
 }
