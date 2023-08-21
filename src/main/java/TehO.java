a0134a82c33662fd544c065fca4b37815fc6e156
Loo Mei Lin Zann <>
2023-08-21 09:41:44 +0800

    Improved TehO by allowing it echo commands entered by user and exit with command bye.

--- src/main/java/TehO.java
+++ src/main/java/TehO.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class TehO {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm TehO \n"
-                + "What can I do for you? \n"
-                + "Bye. Hope to see you again soon!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm TehO \nWhat can I do for you?");
+
+        while (true) {
+            String userCommand = sc.nextLine();
+
+            if (userCommand.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(userCommand);
+            }
+        }
     }
 }
