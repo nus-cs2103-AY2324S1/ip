6ca4aa81e5e143d4099dd02f4c1277f168d32e7d
Jair Goh <lilozz2>
2023-08-21 17:41:33 +0800

    Rename, Greet & Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
 public class Duke {
+    public static String line = "----------------------------------------------------\n";
+    public static String format_response(String response) {
+        return response + "\n" + line;
+    }
+    public static void greet() {
+        System.out.println(line);
+        System.out.println(format_response(
+                "Hello I'm Project54\n" +
+                "What can I do for you?\n"
+        ));
+    }
+    public static void bye() {
+        System.out.println(format_response(
+                "Bye. Hope to see you again soon\n"
+        ));
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        greet();
+        bye();
+
     }
 }
