ee6c33c59eded5e9daac5ef8d79b9525577134a5
Ren Jing <e0958325@u.nus.edu>
2023-08-21 14:12:03 +0800

    Add, List
    Added functionality to store tasks in a list. User may list out tasks with a command "list".

--- src/main/java/Ren.java
+++ src/main/java/Ren.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Ren {
     public static void main(String[] args) {
+        String LS_COMMAND = "list";
+        String EXIT_COMMAND = "bye";
+        List<String> tasks = new ArrayList<String>();
+
         Scanner input = new Scanner(System.in);
         String welcomeMsg = "____________________________________________________________\n" +
                 " Hello! I'm Ren\n" +
@@ -11,12 +17,23 @@ public class Ren {
                 " Bye! Pleasure speaking with you :) \n" +
                 "____________________________________________________________\n";
         System.out.println(welcomeMsg);
-        String inputStr = input.next();
-        while (!inputStr.equals("bye")) {
-            System.out.println("____________________________________________________________\n" +
-                    String.format("%s\n", inputStr) +
-                    "____________________________________________________________\n");
-            inputStr = input.next();
+        String inputStr = input.nextLine();
+        while (!inputStr.equals(EXIT_COMMAND)) {
+            if (inputStr.equals(LS_COMMAND)) {
+                System.out.println("____________________________________________________________\n");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.printf("%d %s\n%n", i + 1, tasks.get(i));
+                }
+                System.out.println("____________________________________________________________\n");
+            } else {
+                tasks.add(inputStr);
+
+                System.out.println("____________________________________________________________\n" +
+                        String.format("Added %s\n", inputStr) +
+                        "____________________________________________________________\n");
+            }
+
+            inputStr = input.nextLine();
         }
 
         System.out.println(goodbyeMsg);
