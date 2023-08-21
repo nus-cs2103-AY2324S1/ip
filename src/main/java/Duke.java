1bf20828f0794e9cfd8badf3b8fa3b10aa0c5df3
Yeo Boh Shin <110957811+YeoBohShin@users.noreply.github.com>
2023-08-21 23:48:34 +0800

    Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,14 +16,6 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    private void echo(String msg) {
-        if (msg.equals("bye")) {
-            exit();
-        } else {
-            System.out.println(msg);
-        }
-    }
-
     private void printListSize() {
         System.out.println("Now you have " + this.list.size() + " tasks in the list.");
     }
@@ -44,44 +36,68 @@ public class Duke {
                 }
                 break;
             case "mark":
-                int index = Integer.parseInt(specifications) - 1;
-                this.list.get(index).markAsDone();
-                System.out.println(this.list.get(index).toString());
-                break;
+                if (specifications.isEmpty()) {
+                    throw new IllegalArgumentException("Please indicate task number.");
+                }
+                try {
+                    int index = Integer.parseInt(specifications) - 1;
+                    this.list.get(index).markAsDone();
+                    System.out.println(this.list.get(index).toString());
+                    break;
+                } catch (IndexOutOfBoundsException error) {
+                    throw new IllegalArgumentException("OOPS!!! I could not find any task in that position.");
+                }
             case "unmark":
-                int i = Integer.parseInt(specifications) - 1;
-                this.list.get(i).markAsUndone();
-                System.out.println(this.list.get(i).toString());
-                break;
+                if (specifications.isEmpty()) {
+                    throw new IllegalArgumentException("Please indicate task number.");
+                }
+                try {
+                    int i = Integer.parseInt(specifications) - 1;
+                    this.list.get(i).markAsUndone();
+                    System.out.println(this.list.get(i).toString());
+                    break;
+                } catch (IndexOutOfBoundsException error) {
+                    throw new IllegalArgumentException("OOPS!!! I could not find any task in that position.");
+                }
             case "todo":
+                if (specifications.isEmpty()) {
+                    throw new IllegalArgumentException("OOPS!!! The description of a todo cannot be empty.");
+                }
                 Task toDoTask = new ToDo(specifications);
                 this.list.add(toDoTask);
                 toDoTask.notice();
                 this.printListSize();
                 break;
             case "deadline":
-                String[] splits = specifications.split("/by", 2);
-                String description = splits[0];
-                String date = splits[1];
-                Task deadlineTask = new Deadline(description, date);
-                this.list.add(deadlineTask);
-                deadlineTask.notice();
-                this.printListSize();
-                break;
+                try {
+                    String[] splits = specifications.split("/by", 2);
+                    String description = splits[0];
+                    String date = splits[1];
+                    Task deadlineTask = new Deadline(description, date);
+                    this.list.add(deadlineTask);
+                    deadlineTask.notice();
+                    this.printListSize();
+                    break;
+                } catch (ArrayIndexOutOfBoundsException error) {
+                    throw new IllegalArgumentException("OOPS!!! The description of a deadline must have <task> /by <time>.");
+                }
             case "event":
-                String[] split = specifications.split("/from", 2);
-                String event = split[0];
-                String[] timings = split[1].split("/to", 2);
-                String start = timings[0];
-                String end = timings[1];
-                Task eventTask = new Event(event, start, end);
-                this.list.add(eventTask);
-                eventTask.notice();
-                this.printListSize();
-                break;
+                try {
+                    String[] split = specifications.split("/from", 2);
+                    String event = split[0];
+                    String[] timings = split[1].split("/to", 2);
+                    String start = timings[0];
+                    String end = timings[1];
+                    Task eventTask = new Event(event, start, end);
+                    this.list.add(eventTask);
+                    eventTask.notice();
+                    this.printListSize();
+                    break;
+                } catch (ArrayIndexOutOfBoundsException error) {
+                    throw new IllegalArgumentException("OOPS!!! The description of an event must have <task> /from <start> /to <end>.");
+                }
             default:
-                this.list.add(new Task(msg));
-                System.out.println("added: " + msg);
+                throw new IllegalArgumentException("OOPS!!! I'm sorry, but I don't know what that means :-(");
         }
     }
 
@@ -93,7 +109,11 @@ public class Duke {
 
         while (bot.active) {
             String input = sc.nextLine();
-            bot.respond(input);
+            try {
+                bot.respond(input);
+            } catch (Exception error) {
+                System.out.println(error.getMessage());
+            }
         }
 
         sc.close();
