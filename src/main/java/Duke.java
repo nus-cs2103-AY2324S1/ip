76606d428a543c2721e38fae6c779948abdbeeaa
You Wei <thamyouwei@gmail.com>
2023-08-22 02:15:51 +0800

    Use Enums

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,35 @@
 import java.util.Scanner;
 
 public class Duke {
+    public enum COMMAND {
+        BYE("bye"),
+        LIST("list"),
+        MARK("mark"),
+        UNMARK("unmark"),
+        DELETE("delete"),
+        DEADLINE("deadline"),
+        TODO("todo"),
+        EVENT("event"),
+        BY("by"),
+        FROM("from"),
+        TO("to");
+
+        private String cmd;
+        private int strLength;
+
+        COMMAND(String cmd) {
+            this.cmd = cmd;
+            this.strLength = cmd.length();
+        }
+
+        public String getCommand() {
+            return this.cmd;
+        }
+
+        public int getCommandStringLength() {
+            return this.strLength;
+        }
+    }
     private static TaskList tasks = new TaskList();
     private static Reply reply = Reply.init();
     public static void main(String[] args) {
@@ -9,22 +38,22 @@ public class Duke {
         while(true) {
             try {
                 String input = scanner.nextLine();
-                if (input.equals("bye")) {
+                if (input.equals(COMMAND.BYE.getCommand())) {
                     reply.printDialog("Bye. Hope to see you again soon!");
                     return;
-                } else if (input.equals("list")) {
+                } else if (input.equals(COMMAND.LIST.getCommand())) {
                     tasks.printTasks();
-                } else if (input.startsWith("mark")) {
+                } else if (input.startsWith(COMMAND.MARK.getCommand())) {
                     markTask(input, true);
-                } else if (input.startsWith("unmark")) {
+                } else if (input.startsWith(COMMAND.UNMARK.getCommand())) {
                     markTask(input, false);
-                } else if (input.startsWith("todo")) {
+                } else if (input.startsWith(COMMAND.TODO.getCommand())) {
                     handleToDo(input);
-                } else if (input.startsWith("deadline")) {
+                } else if (input.startsWith(COMMAND.DEADLINE.getCommand())) {
                     handleDeadline(input);
-                } else if (input.startsWith("event")) {
+                } else if (input.startsWith(COMMAND.EVENT.getCommand())) {
                     handleEvent(input);
-                } else if (input.startsWith("delete")) {
+                } else if (input.startsWith(COMMAND.DELETE.getCommand())) {
                     deleteTask(input);
                 } else {
                     throw new InvalidInputException();
@@ -39,7 +68,7 @@ public class Duke {
     }
 
     public static void markTask(String input, boolean mark) throws InvalidInputException, MissingArgumentException {
-        String command = mark ? "mark" : "unmark";
+        COMMAND command = mark ? COMMAND.MARK : COMMAND.UNMARK;
         String number = getCommandArguments(input, command);
         try {
             if (mark) {
@@ -55,27 +84,27 @@ public class Duke {
     }
 
     private static void handleToDo(String input) throws MissingArgumentException, InvalidInputException {
-        String todo = getCommandArguments(input, "todo");
+        String todo = getCommandArguments(input, COMMAND.TODO);
         if (todo.isEmpty()) {
-            throw new MissingTaskArgumentException("todo");
+            throw new MissingTaskArgumentException(COMMAND.TODO.getCommand());
         }
         tasks.addTask(new ToDo(todo));
     }
 
     private static void handleDeadline(String input) throws InvalidInputException, MissingArgumentException {
-        String deadline = getCommandArguments(input, "deadline");
+        String deadline = getCommandArguments(input, COMMAND.DEADLINE);
         String[] slice = deadline.split("/");
         if (slice.length > 2) {
             throw new InvalidInputException();
         } else if (slice.length == 1) {
-            throw new MissingDateArgumentException("by");
+            throw new MissingDateArgumentException(COMMAND.BY.getCommand());
         } else {
             String desc = slice[0];
             String date;
-            if (slice[1].startsWith("by")) {
-                date = getCommandArguments(slice[1], "by");
+            if (slice[1].startsWith(COMMAND.BY.getCommand())) {
+                date = getCommandArguments(slice[1], COMMAND.BY);
             } else {
-                throw new MissingDateArgumentException("by");
+                throw new MissingDateArgumentException(COMMAND.BY.getCommand());
             }
 
             tasks.addTask(new Deadlines(desc, date));
@@ -83,7 +112,7 @@ public class Duke {
     }
 
     private static void handleEvent(String input) throws InvalidInputException, MissingArgumentException {
-        String event = getCommandArguments(input, "event");
+        String event = getCommandArguments(input, COMMAND.EVENT);
         String[] slice = event.split("/");
 
         if (slice.length > 3) {
@@ -95,23 +124,23 @@ public class Duke {
             String from;
             String to;
 
-            if (slice[1].startsWith("from")) {
-                from = getCommandArguments(slice[1], "from");
+            if (slice[1].startsWith(COMMAND.FROM.getCommand())) {
+                from = getCommandArguments(slice[1], COMMAND.FROM);
             } else {
-                throw new MissingDateArgumentException("from");
+                throw new MissingDateArgumentException(COMMAND.FROM.getCommand());
             }
 
-            if (slice[2].startsWith("to")) {
-                to = getCommandArguments(slice[2], "to");
+            if (slice[2].startsWith(COMMAND.TO.getCommand())) {
+                to = getCommandArguments(slice[2], COMMAND.TO);
             } else {
-                throw new MissingDateArgumentException("to");
+                throw new MissingDateArgumentException(COMMAND.TO.getCommand());
             }
             tasks.addTask(new Events(desc, from, to));
         }
     }
 
     private static void deleteTask(String input) throws InvalidInputException, MissingArgumentException {
-        String number = getCommandArguments(input,"delete");
+        String number = getCommandArguments(input,COMMAND.DELETE);
         try {
             tasks.deleteTask(Integer.parseInt(number));
         } catch (NumberFormatException e ) {
@@ -121,15 +150,22 @@ public class Duke {
         }
     }
 
-    private static String getCommandArguments(String input, String command) throws InvalidInputException, MissingArgumentException {
-        int cmdLength = command.length();
+    private static String getCommandArguments(String input, COMMAND command) throws InvalidInputException, MissingArgumentException {
+        int cmdLength = command.getCommandStringLength();
         String args = input.substring(cmdLength);
 
         if (args.isBlank()) {
-            if (command.equals("todo") || command.equals("event") || command.equals("deadline")) {
-                throw new MissingTaskArgumentException(command);
-            } else {
-                throw new MissingDateArgumentException(command);
+            switch (command) {
+                case TODO:
+                case EVENT:
+                case DEADLINE:
+                    throw new MissingTaskArgumentException(command.getCommand());
+                case BY:
+                case TO:
+                case FROM:
+                    throw new MissingDateArgumentException(command.getCommand());
+                default:
+                    throw new InvalidInputException();
             }
         } else if (args.startsWith(" ")){
             return args.substring(1);
