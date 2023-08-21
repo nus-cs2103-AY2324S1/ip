438506fe459fe397424484cfcc5b22187b8d54a8
Loo Mei Lin Zann <>
2023-08-21 10:15:21 +0800

    added methods addTask and listTask to TehO.java

--- src/main/java/TehO.java
+++ src/main/java/TehO.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class TehO {
+    static int taskCounter = 0;
+    static String[] taskList = new String[100];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm TehO \nWhat can I do for you?");
@@ -11,9 +14,23 @@ public class TehO {
             if (userCommand.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (userCommand.equals("list")) {
+                listTask(taskList);
             } else {
-                System.out.println(userCommand);
+                addTask(taskList, userCommand);
             }
+        }
+    }
+
+    public static void addTask(String[] taskList, String task) {
+        taskList[taskCounter] = task;
+        taskCounter++;
+        System.out.println("added: " + task);
+    }
+
+    public static void listTask(String[] taskList) {
+        for (int i = 0; i < taskCounter; i++) {
+            System.out.println((i+1) + ". " + taskList[i]);
         }
     }
 }
