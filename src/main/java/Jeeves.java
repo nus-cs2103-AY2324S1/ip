1a1e32733ab6026b28671df1fafbc0480aaa600a
JasonLCY-Temp <e0407197@u.nus.edu>
2023-08-21 12:53:10 +0800

    Remove Duke.java and Add Jeeves.java
    Change the name and personality of the chatbot.

---
+++ src/main/java/Jeeves.java
@@ -0,0 +1,12 @@
+public class Jeeves {
+    /**
+     * Main process.
+     * Greets the user and says goodbye.
+     *
+     * @param args Optional command line arguments.
+     */
+    public static void main (String[] args) {
+        System.out.println("Greetings, Master. Jeeves at your service");
+        System.out.println("I bid you farewell Master");
+    }
+}
