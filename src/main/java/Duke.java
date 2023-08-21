841c201595091aa3235988646148148400b65526
Jweng88 <108978684+Jweng88@users.noreply.github.com>
2023-08-22 00:08:12 +0800

    ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,6 +24,51 @@ class Task {
     public String getDescription() {
         return description;
     }
+
+    @Override
+    public String toString() {
+        return this.getStatusIcon() + " " + this.getDescription();
+    }
+}
+
+class ToDo extends Task {
+    public ToDo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Deadline extends Task {
+    protected String by;
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
+
+class Event extends Task {
+    protected String from;
+    protected String to;
+
+    public Event(String description, String from, String to){
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+    }
 }
 
 public class Duke {
@@ -49,33 +94,55 @@ public class Duke {
             } else if (userInput.equalsIgnoreCase("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i].getStatusIcon() + " " + tasks[i].getDescription());
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
                 }
+            } else if (userInput.startsWith("todo")) {
+                String description = userInput.substring(5).trim();
+                taskCount = addTask(tasks, taskCount, new ToDo(description));
+                System.out.println("Got it. I've added this task:");
+                System.out.println(" " + tasks[taskCount - 1]);
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+            } else if (userInput.startsWith("deadline")) {
+                String input = userInput.substring(9).trim();
+                String description = input.split("/by")[0].trim();
+                String by = input.split("/by")[1].trim();
+                taskCount = addTask(tasks, taskCount, new Deadline(description, by));
+                System.out.println("Got it. Ive added this task:");
+                System.out.println(" " + tasks[taskCount - 1]);
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+            } else if (userInput.startsWith("event")) {
+                String input = userInput.substring(6).trim();
+                String description = input.split("/from")[0].trim();
+                String from = input.split("/from")[1].split("/to")[0].trim();
+                String to = input.split("/to")[1].trim();
+                taskCount = addTask(tasks, taskCount, new Event(description, from, to));
+                System.out.println("Got it. Ive added this task:");
+                System.out.println(" " + tasks[taskCount - 1]);
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
             } else if (userInput.startsWith("mark")) {
                 int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
                 if (index >= 0 && index < taskCount) {
                     tasks[index].markAsDone();
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(" " + tasks[index].getStatusIcon() + " " + tasks[index].getDescription());
+                    System.out.println(" " + tasks[index]);
                 }
             } else if (userInput.startsWith("unmark")) {
                 int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
                 if (index >= 0 && index < taskCount) {
                     tasks[index].markAsNotDone();
                     System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println(" " + tasks[index].getStatusIcon() + " " + tasks[index].getDescription());
+                    System.out.println(" " + tasks[index]);
                 }
             } else {
-                taskCount = addTask(tasks, taskCount, userInput);
-                System.out.println("added: " + userInput);
+                System.out.println("Invalid command.");
             }
             System.out.println(horizontalLine);
         }
         scanner.close();
     }
 
-    public static int addTask(Task[] tasks, int count, String taskDescription) {
-        tasks[count] = new Task(taskDescription);
+    public static int addTask(Task[] tasks, int count, Task task) {
+        tasks[count] = task;
         return count + 1;
     }
 }
