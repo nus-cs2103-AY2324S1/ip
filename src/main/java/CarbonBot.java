738a72591f00a53c584f5592841f89639b551f89
aexolate <20797128+aexolate@users.noreply.github.com>
2023-08-21 21:52:18 +0800

    Added "list" command

--- src/main/java/CarbonBot.java
+++ src/main/java/CarbonBot.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class CarbonBot {
     private static String DIVIDER = "____________________________________________________________";
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<String> commandsList = new ArrayList<>();
 
         System.out.println(DIVIDER);
         System.out.println("Hello! I'm CarbonBot");
@@ -13,19 +16,31 @@ public class CarbonBot {
         while(true) {
             String cmd = sc.nextLine();
 
-            if (cmd.equals("bye")) {
-                // Stops reading user inputs if the command was 'bye'
-                break;
-            } else {
-                // Otherwise, echos the command the user entered
-                System.out.println(DIVIDER);
-                System.out.println(cmd);
-                System.out.println(DIVIDER);
+            switch(cmd) {
+                case "bye":
+                    // Stops listening to user input if the command is "bye"
+                    System.out.println(DIVIDER);
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println(DIVIDER);
+                    sc.close();
+                    return;
+                case "list":
+                    // Lists all the commands saved in the arraylist
+                    System.out.println(DIVIDER);
+                    int idx = 1;
+                    for(String c : commandsList) {
+                        System.out.println(String.format("%d. %s", idx, c));
+                        idx++;
+                    }
+                    System.out.println(DIVIDER);
+                    break;
+                default:
+                    // Echos and saves the command given
+                    System.out.println(DIVIDER);
+                    commandsList.add(cmd);
+                    System.out.println("added: " + cmd);
+                    System.out.println(DIVIDER);
             }
         }
-
-        System.out.println(DIVIDER);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(DIVIDER);
     }
 }
