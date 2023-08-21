81b86166e103585dd91e441ba80c734e5c7f999c
newway1814 <e1088145@u.nus.edu>
2023-08-21 22:26:51 +0800

    Completed Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -5,6 +6,8 @@ public class Duke {
 
     private static final String name = "Duck";
     private static final String exitMessage = "bye";
+
+    private static ArrayList<String> list = new ArrayList<>();
 
     public static void welcomeMessage() {
         System.out.println("____________________________________________________________");
@@ -18,19 +21,47 @@ public class Duke {
         System.out.println("____________________________________________________________");
     }
 
+    public static boolean isBye(String str) {
+        return str.equalsIgnoreCase(exitMessage);
+    }
+
+    public static boolean checkList(String str) {
+        return str.equalsIgnoreCase("list");
+    }
+
     public static void typeMessage() {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        while (!(str.equalsIgnoreCase(exitMessage))){
-            echo(str);
+        while (!isBye(str)){
+            //echo(str);
+            if (checkList(str)) {
+                display();
+            } else {
+                echo(str);
+                list.add(str);
+            }
             str = sc.nextLine();
         }
-        exit();
+        if (isBye(str)) {
+            exit();
+        }
+    }
+
+    public static void display() {
+        int count = 0;
+        int serial = 1;
+        System.out.println("____________________________________________________________");
+        while (count < list.size()) {
+            System.out.println(serial + ". " + list.get(count));
+            count++;
+            serial++;
+        }
+        System.out.println("____________________________________________________________");
     }
 
     public static void echo(String str) {
         System.out.println("____________________________________________________________");
-        System.out.println(str);
+        System.out.println("added: " + str);
         System.out.println("____________________________________________________________");
 
     }
