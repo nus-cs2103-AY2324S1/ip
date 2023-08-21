f1b15b406ad16e19a42e77b7b034883871a87272
itssisi <e0969060@u.nus.edu>
2023-08-21 14:45:30 +0800

    Level 1.Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
+
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,12 +10,25 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        String horizontalLine = "____________________________________________________________";
-        System.out.println(horizontalLine);
+        horizontalLine();
         System.out.println("Hello! I'm Snow!");
         System.out.println("What can I do for you?");
-        System.out.println(horizontalLine);
+        horizontalLine();
+
+        Scanner userInput = new Scanner(System.in);  // Create a Scanner object
+        String userOutput = userInput.nextLine();  // Read user input
+        while (!userOutput.equals("bye")) {
+            System.out.println(userOutput);  // Output user input
+            userOutput = userInput.nextLine();  // Read user input
+        }
+        printBye();
+    }
+
+    public static void printBye() {
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+    }
+
+    public static void horizontalLine() {
+        System.out.println("____________________________________________________________");
     }
 }
