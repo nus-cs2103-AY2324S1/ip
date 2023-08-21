53ce34370837c95e03bdf9434ff0173aace837ea
seanpzk <seanpzk2@gmail.com>
2023-08-21 10:02:30 +0800

    Renamed Duke to ChadBob. Now main simply greets user and exits.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
+    public void listen() {
+
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String horizontal_line = "____________________________________________________________\n";
+        System.out.println(horizontal_line
+                            + "Hello! I'm ChadBob.\n"
+                            + "What can I do for you?\n"
+                            + horizontal_line
+                            + "Bye. Hope to see you again soon!\n"
+                            + horizontal_line) ;
     }
 }
