a6a76829232f34c5af14599c0d20fe9226f52f0d
Jason Baek <bkjwjason@gmail.com>
2023-08-21 20:19:52 +0800

    add task listing function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static ArrayList<String> tasks = new ArrayList<String>();
     private static void echo(String input) {
-        System.out.println(input);
+        tasks.add(input);
+        System.out.println("added: " + input);
+    }
+    private static void listTask() {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(i + 1 + ". " + tasks.get(i));
+        }
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -11,6 +19,8 @@ public class Duke {
             String echoInput = scanner.nextLine();
             if (echoInput.equals("bye")) {
                 break;
+            } else if (echoInput.equals("list")) {
+                listTask();
             } else {
                 echo(echoInput);
             }
