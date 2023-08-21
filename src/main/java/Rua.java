33ee8f618bb1c72a4b4f9a7bd7bde8b68828a2ac
siqirua <lsq6918086@gmail.com>
2023-08-22 00:53:28 +0800

    Change Chatbot name and beginning message

---
+++ src/main/java/Rua.java
@@ -0,0 +1,11 @@
+public class Rua {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n" +
+                " Hello! I'm Rua, your chatbot\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(logo);
+    }
+}
