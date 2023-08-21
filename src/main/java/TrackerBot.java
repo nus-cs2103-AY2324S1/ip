c13b0cbda4cc33cb5f114191c8c370663cff7d6c
WZWren <115881359+WZWren@users.noreply.github.com>
2023-08-21 18:04:11 +0800

    Added input handling to TrackerBot.

--- src/main/java/TrackerBot.java
+++ src/main/java/TrackerBot.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Main Program for the application. <br>
  * As of Level-0, this has been renamed from Duke to TrackerBot
@@ -30,13 +32,39 @@ public class TrackerBot {
    * Prints an exit message to the user on logout.
    */
   private static void exit() {
-    System.out.println(FORMAT_LINE);
     System.out.println("Thank you for using " + APP_NAME + ". Goodbye.");
+  }
+
+  /**
+   * Input handler function of the app. <br>
+   * Takes in a user input, and acts upon the input based on what input it gets. <br>
+   * <ul>
+   *   <li>If the input is "bye", exits the program.</li>
+   *   <li>Otherwise, echoes the input back to the console.</li>
+   * </ul>
+   * @param str The input string that is given to the method.
+   */
+  private static void handleInput(String str) {
+    System.out.println(FORMAT_LINE);
+    // switch used for now: to handle future input cases.
+    switch(str) {
+      case "bye":
+        exit();
+        break;
+      default:
+        System.out.println(str);
+    }
     System.out.println(FORMAT_LINE);
   }
 
   public static void main(String[] args) {
     greet();
-    exit();
+    Scanner scanner = new Scanner(System.in);
+    String input;
+    do {
+      // scanner.nextLine() blocks the main thread.
+      input = scanner.nextLine();
+      handleInput(input);
+    } while (!input.equals("bye"));
   }
 }
