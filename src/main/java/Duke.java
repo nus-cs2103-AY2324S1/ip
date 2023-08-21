46f6c15b26629ecbc4eb6a54eb69a9384f871231
zacwong2151 <zacwong2151@gmail.com>
2023-08-21 17:37:04 +0800

    finished Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String horizontalLines = "_______________________________________";
         String name = "zac";
+        Scanner obj = new Scanner(System.in);
 
         System.out.println(horizontalLines);
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
+        System.out.println(horizontalLines);
+
+
+        while (true) {
+            String userInput = obj.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            }
+            System.out.println(horizontalLines);
+            System.out.println(userInput);
+            System.out.println(horizontalLines);
+        }
+
         System.out.println(horizontalLines);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(horizontalLines);
