1d444f1b54e6db183d0ec075dc20f2c0e608f5e2
jinyuan0425 <pdmon123@gmail.com>
2023-08-21 19:29:15 +0800

    Completed Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,31 @@
 import java.util.Scanner;  // Import the Scanner class
 public class Duke {
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        public void setDone() {
+            this.isDone = true;
+        }
+
+        public void setNotDone() {
+            this.isDone = false;
+        }
+        public String toString() {
+            return "[" + getStatusIcon() + "] " + this.description;
+        }
+    }
     public static void main(String[] args) {
-        String[] list = new String[100]; // List to be returned when input is "list"
+        Task[] list = new Task[100]; // List to be returned when input is "list"
         int counter = 0; // Items in list
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 
@@ -11,7 +35,21 @@ public class Duke {
         String echo = myObj.nextLine(); // Reads user input
 
         while (!echo.equals("bye")) {
-            if (echo.equals("list")) {
+            if (echo.startsWith("mark ")) { // Mark task complete
+                int index = Integer.valueOf(echo.substring(5)) - 1; // Index of task
+                list[index].setDone(); // Item mark complete
+                System.out.println("Congratulations on finishing the task. I will now mark it as complete: \n" +
+                        list[index].toString());
+            }
+
+            else if (echo.startsWith("unmark ")) { // Mark task complete
+                int index = Integer.valueOf(echo.substring(7)) - 1; // Index of task
+                list[index].setNotDone(); // Item mark complete
+                System.out.println("No worries. I will now mark it as incomplete: \n" +
+                        list[index].toString());
+            }
+
+            else if (echo.equals("list")) {
                 for (int i = 0; i < list.length; i++) {
                     if (list[i] != null) {
                         System.out.println(i + 1 + ". " + list[i]); // Returns the list
@@ -23,7 +61,8 @@ public class Duke {
                 System.out.println(response);
 
                 // Insert action into list
-                list[counter] = echo;
+                Task add = new Task(echo);
+                list[counter] = add;
                 counter += 1;
             }
             echo = myObj.nextLine();
