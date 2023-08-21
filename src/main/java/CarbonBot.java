45361d0cb02388cb7f09c144f19a0c6e460bb095
aexolate <20797128+aexolate@users.noreply.github.com>
2023-08-21 21:39:36 +0800

    added echoing, bye command

--- src/main/java/CarbonBot.java
+++ src/main/java/CarbonBot.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
+
 public class CarbonBot {
     private static String DIVIDER = "____________________________________________________________";
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println(DIVIDER);
         System.out.println("Hello! I'm CarbonBot");
         System.out.println("What can I do for you?");
+        System.out.println(DIVIDER);
+
+        while(true) {
+            String cmd = sc.nextLine();
+
+            if (cmd.equals("bye")) {
+                // Stops reading user inputs if the command was 'bye'
+                break;
+            } else {
+                // Otherwise, echos the command the user entered
+                System.out.println(DIVIDER);
+                System.out.println(cmd);
+                System.out.println(DIVIDER);
+            }
+        }
+
         System.out.println(DIVIDER);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(DIVIDER);
