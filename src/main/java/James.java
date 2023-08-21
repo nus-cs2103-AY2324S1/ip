b268d39bf00870804f5e87601f0baee951a94031
nreHieW <limweihern@gmail.com>
2023-08-21 15:24:30 +0800

    Rename to James

---
+++ src/main/java/James.java
@@ -0,0 +1,6 @@
+public class James {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm James\n" + "What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
