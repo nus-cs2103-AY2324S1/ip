f00a0e1c7285bb16a9fbab635a7de9abfcc66d58
Tan Teck Fang <tanteckfang@Tans-MacBook-Pro.local>
2023-08-21 22:42:46 +0800

    done level 1

--- src/main/java/Sara.java
+++ src/main/java/Sara.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
+
 public class Sara {
     public static void main(String[] args) {
-        greet();
-    }
-            public static void greet() {
-            String separator = "____________________________________________________________";
-            String greeting = " Hello! I'm Sara\n What can I do for you?";
-            String farewell = " Bye. Hope to see you again soon!";
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.println(separator);
-            System.out.println(greeting);
-            System.out.println(separator);
-            System.out.println(farewell);
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Sara");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            System.out.println("    ____________________________________________________________");
+
+            if ("bye".equalsIgnoreCase(userInput)) {
+                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
+                break;
+            } else {
+                System.out.println("     " + userInput);
+                System.out.println("    ____________________________________________________________");
+            }
         }
+
+        scanner.close();
     }
+}
