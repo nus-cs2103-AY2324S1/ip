eefb66badfca42aac04db0f427dd62c9be6ef758
waseem <mohammedwaseem1000@gmail.com>
2023-08-22 03:04:52 +0800

    Duke A-TextUiTesting: Automated Text UI Testing Test using the I/O redirection technique Use the input/output redirection technique to semi-automate the testing of Duke.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-
 public class Duke {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
