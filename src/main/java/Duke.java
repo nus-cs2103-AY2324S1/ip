fd258dcc96e2b462f6dbd6f13b46fe68eda0daba
jonasongg <jonasong2@gmail.com>
2023-08-21 22:02:12 +0800

    create constant for horizontal line

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,19 @@
 import java.util.Scanner;
 
 public class Duke {
+    static final String HORIZONTAL_LINE = "____________________________________________________________\n";
     public static void main(String[] args) {
-        String greeting =
-                "____________________________________________________________\n" +
-                " Hello! I'm Pixel\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
+        String greeting = HORIZONTAL_LINE + " Hello! I'm Pixel\n What can I do for you?\n" + HORIZONTAL_LINE;
         System.out.println(greeting);
 
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
+                System.out.println(HORIZONTAL_LINE + " Bye. Hope to see you again soon!\n" + HORIZONTAL_LINE);
                 break;
             } else {
-                System.out.println(input);
+                System.out.println(HORIZONTAL_LINE + input + "\n" + HORIZONTAL_LINE);
             }
         }
     }
