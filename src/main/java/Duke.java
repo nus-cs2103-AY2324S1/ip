955376164b003f838172c95cd731ee600d04d2e8
alvinlim277 <alvinlim9815@gmail.com>
2023-08-21 23:57:42 +0800

    Add list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,41 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Duke {
     public static String name = "Dook";
-
+    public static ArrayList<String> taskList = new ArrayList<>();
     public static void main(String[] args) {
 
         GreetUser();
         Scanner sc = new Scanner(System.in);
         String input;
         while (true) {
-            input = sc.next();
+            input = sc.nextLine();
             if (Objects.equals(input, "bye")) {
                 BidFarewell();
                 break;
+            } else if (Objects.equals(input, "list")) {
+                DisplayList();
             } else {
-                PrintMessage(input);
+                PrintMessage("added: " + input);
+                AddToTaskList(input);
             }
         }
     }
+
+    private static void AddToTaskList(String str) {
+        taskList.add(str);
+    }
+
+    private static void DisplayList() {
+        String result = "";
+        for (int i = 0; i < taskList.size(); i++) {
+            result += String.format("%d. %s\n", i + 1, taskList.get(i));
+        }
+        PrintMessage(result);
+    }
+
 
     private static void GreetUser() {
         PrintMessage(String.format("%s here.\nWhat can I do for you?", name));
