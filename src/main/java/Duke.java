cc8b300e9c1d41128db4c524a650193febbb74b8
ktzy0305 <kevintoh0305@gmail.com>
2023-08-21 11:58:55 +0800

    feat: add task and list tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,32 +2,65 @@ import java.util.Scanner;
 
 public class Duke {
     private static final int LINE_LENGTH = 100;
+    private String[] tasks;
+    private int taskCount;
+
+    public Duke() {
+        this.tasks = new String[100];
+        this.taskCount = 0;
+    }
+
     public static void main(String[] args) {
+        Duke program = new Duke();
         Scanner scanner = new Scanner(System.in);
 
-        printMessage("Hello! I'm Skye, your personal task assistant. \nWhat can I do for you?");
+        // Welcome message
+        program.printMessage("Hello! I'm Skye, your personal task assistant. \nWhat can I do for you?");
 
+        // Program only exits when user enters "bye" command
         while (true) {
             System.out.print("> ");
             String userInput = scanner.nextLine();
 
-            if (userInput.equals("bye")) {
-                printMessage("Bye. Hope to see you again soon!");
-                scanner.close();
-                System.exit(0);
-            }
+            // Switch statement to check for command keywords
+            switch (userInput) {
+                case "bye":
+                    program.printMessage("Bye. Hope to see you again soon!");
+                    scanner.close();
+                    System.exit(0);
+                    break;
 
-            printMessage(userInput);
+                case "list":
+                    program.listTasks();
+                    break;
+
+                default:
+                    program.addTask(userInput);
+                    program.printMessage("added: " + userInput);
+            }
         }
     }
 
-    private static void renderLine() {
+    private void renderLine() {
         System.out.println("_".repeat(LINE_LENGTH));
     }
 
-    private static void printMessage(String message) {
+    private void printMessage(String message) {
         renderLine();
         System.out.println(message);
+        renderLine();
+    }
+
+    private void addTask(String task) {
+        this.tasks[taskCount] = task;
+        taskCount++;
+    }
+
+    private void listTasks() {
+        renderLine();
+        for (int i = 0; i < taskCount; i++) {
+            System.out.printf("%d. %s%n", i + 1, tasks[i]);
+        }
         renderLine();
     }
 }
