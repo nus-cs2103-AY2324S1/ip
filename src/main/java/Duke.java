e3cc57a4dcbb1162c2c7aa49c1a973f4a3f002e5
kurt <kurtyjlee@gmail.com>
2023-08-21 13:26:58 +0800

    level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,13 @@
 public class Duke {
+    public static String greetUser() {
+        String greeting = "――――――――――――――――――――――――――――――――――――――――――\n"
+                + "Hello! I'm CringeBot\n"
+                + "What can I do for you?\n"
+                + "――――――――――――――――――――――――――――――――――――――――――\n"
+                + "Bye. Hope to see you again soon!";
+        return greeting;
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +15,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(greetUser());
     }
 }
