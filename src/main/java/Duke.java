b23236221cbde144601e4af95a65d81cb2c6691a
jonasongg <jonasong2@gmail.com>
2023-08-21 22:24:06 +0800

    add Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Duke {
     static final String HORIZONTAL_LINE = "____________________________________________________________\n";
-    static List<String> tasks = new ArrayList<>();
+    static List<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         String greeting = HORIZONTAL_LINE + " Hello! I'm Pixel\n What can I do for you?\n" + HORIZONTAL_LINE;
@@ -17,12 +17,12 @@ public class Duke {
                 System.out.println(HORIZONTAL_LINE + " Bye. Hope to see you again soon!\n" + HORIZONTAL_LINE);
             } else if (input.equals("list")) {
                 System.out.println(HORIZONTAL_LINE);
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i));
+                for (Task task : tasks) {
+                    System.out.println(task.printTask());
                 }
                 System.out.println(HORIZONTAL_LINE);
             } else {
-                tasks.add(input);
+                tasks.add(new Task(input));
                 System.out.println(HORIZONTAL_LINE + "added: " + input + "\n" + HORIZONTAL_LINE);
             }
         }
