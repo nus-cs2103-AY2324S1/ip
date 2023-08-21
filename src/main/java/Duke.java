60a0618b1a4f5c3aac2dee62e4a544ba17a3c53b
Selwyn Ang <selwynang.01@gmail.com>
2023-08-21 10:45:30 +0800

    Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 public class Duke {
+    private static final String NAME = "Duke Prince";
+    private static final String SPACER = "----------------------------------------------------";
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +8,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(SPACER);
+        greet();
+        System.out.println(SPACER);
+        bye();
+        System.out.println(SPACER);
+    }
+
+    private static void greet() {
+        System.out.println("Hello! I'm " + NAME);
+        System.out.println("What can I do for you?");
+    }
+
+    private static void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
