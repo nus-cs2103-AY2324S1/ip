2a981da0366d1899705896931f11e3e49dab1243
JasonLCY-Temp <e0407197@u.nus.edu>
2023-08-21 16:37:43 +0800

    Add Task.java
    No program logic exists for managing tasks.
    
    A Task Object will allow for the management of tasks cleanly.
    
    Let's add the Task class with a default constructor and getter/setters.

---
+++ src/main/java/Task.java
@@ -0,0 +1,42 @@
+/**
+ * Contains logic related to the Task object.
+ */
+public class Task {
+    /** Tracks the number of task objects to guarantee the assigned ID will be unique */
+    private static int taskCount = 0;
+    /** Unique identification number for the task */
+    private final int id;
+    /** The description of the task */
+    private String desc;
+
+    /**
+     * Default constructor for the Task object.
+     * Automatically assigns the instance's ID based on the global task count.
+     *
+     * @param desc Description of the task
+     */
+    public Task(String desc) {
+        // Increments the taskCount and uses the value as the unique ID for the current task
+        Task.taskCount += 1;
+        id = Task.taskCount;
+        this.desc = desc;
+    }
+
+    /**
+     * Getter method for the Task description.
+     *
+     * @return Description of the task
+     */
+    public String getDesc() {
+        return desc;
+    }
+
+    /**
+     * Setter method for the Task description.
+     *
+     * @param desc Description of the task
+     */
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
