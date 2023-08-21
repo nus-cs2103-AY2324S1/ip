2f45a14ce7e1f0656392bb462d239c216d73282d
Jweng88 <108978684+Jweng88@users.noreply.github.com>
2023-08-21 17:49:36 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String horizontalLine = "________________________________________________________________";
@@ -6,7 +7,20 @@ public class Duke {
         System.out.println("Hello! I'm Bob");
         System.out.println("What can I do for you?");
         System.out.println(horizontalLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String userInput = scanner.nextLine();
+            System.out.println(horizontalLine);
+            System.out.println(" " + userInput);
+            System.out.println(horizontalLine);
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(horizontalLine);
+                break;
+            }
+        }
+        scanner.close();
     }
 }
