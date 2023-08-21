3c1db11b7a1c2f5c4d44ed60ff573dd06e18f37c
kurt <kurtyjlee@gmail.com>
2023-08-21 13:47:33 +0800

    level-1, echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static String greetUser() {
+    public static void greetUser() {
         String greeting = "――――――――――――――――――――――――――――――――――――――――――\n"
                 + "Hello! I'm CringeBot\n"
                 + "What can I do for you?\n"
-                + "――――――――――――――――――――――――――――――――――――――――――\n"
-                + "Bye. Hope to see you again soon!";
-        return greeting;
+                + "――――――――――――――――――――――――――――――――――――――――――";
+        System.out.println(greeting);
+    }
+
+    public static void bidFarewel() {
+        String farewell = "――――――――――――――――――――――――――――――――――――――――――\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "――――――――――――――――――――――――――――――――――――――――――";
+        System.out.println(farewell);
+    }
+
+    public static void sayWord(String word) {
+        String wrappedWord = "――――――――――――――――――――――――――――――――――――――――――\n"
+                + word
+                + "\n"
+                + "――――――――――――――――――――――――――――――――――――――――――";
+        System.out.println(wrappedWord);
     }
 
     public static void main(String[] args) {
@@ -15,6 +31,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println(greetUser());
+        Scanner scanObj = new Scanner(System.in);
+        greetUser();
+        while(true) {
+            String nextLine = scanObj.nextLine();
+            if (nextLine.equals("bye")) {
+                break;
+            }
+            sayWord(nextLine);
+        }
+        bidFarewel();
     }
 }
