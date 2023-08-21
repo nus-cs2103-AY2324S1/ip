72a818124969c0156b6560fdeef725ee8170b112
Ren Jing <e0958325@u.nus.edu>
2023-08-21 09:47:22 +0800

    Rename, Greet, Exit
    Changed Duke to Ren. Changed welcome message.

---
+++ src/main/java/Ren.java
@@ -0,0 +1,11 @@
+public class Ren {
+    public static void main(String[] args) {
+        String welcomeMsg = "____________________________________________________________\n" +
+                " Hello! I'm Ren\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(welcomeMsg);
+    }
+}
