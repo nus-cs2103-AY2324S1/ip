7c862be0853d87015956278fccb64c225ff0276d
CelestineTan03 <e0968156@u.nus.edu>
2023-08-21 19:21:43 +0800

    Add the ability to mark and unmark tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int currIndex = 0;
 
         String greetings = "Hello! I'm Botty!\nWhat can I do for you?\n";
@@ -12,16 +12,35 @@ public class Duke {
         // Only exit when user types the command bye
         while (true) {
             String command = sc.nextLine();
-            if (command.equals("bye")) {
+            // Split string into words
+            String[] words = command.split(" ");
+            if (words.length == 1 && command.equals("bye")) {
                 System.out.println(farewell);
                 sc.close();
                 break;
-            } else if (command.equals("list")) {
+                // Display the stored commands
+            } else if (words.length == 1 && command.equals("list")) {
+                System.out.println("Here are the tasks in your list:\n");
                 for (int i = 0; i < currIndex; i++) {
-                    System.out.println(i + 1 + ". " + tasks[i] + "\n");
+                    System.out.println(i + 1 + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].description + "\n");
                 }
+                // Mark task as done
+            } else if (words.length == 2 && words[0].equals("mark")) {
+                System.out.println("Nice! I've marked this task as done:\n");
+                int index = Integer.parseInt(words[1]);
+                Task currTask = tasks[index - 1];
+                currTask.markAsDone();
+                System.out.println(" [X] " + currTask.description + "\n");
+                // Mark task as not done
+            } else if (words.length == 2 && words[0].equals("unmark")) {
+                System.out.println("OK, I've marked this task as not done yet:\n");
+                int index = Integer.parseInt(words[1]);
+                Task currTask = tasks[index - 1];
+                currTask.markAsNotDone();
+                System.out.println(" [ ] " + currTask.description + "\n");
+                // Echo the command
             } else {
-                tasks[currIndex] = command;
+                tasks[currIndex] = new Task(command);
                 currIndex++;
                 System.out.println("added: " + command + "\n");
             }
