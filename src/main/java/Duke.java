74a1837d891cc06ea7bf86cac23797e60d4ceaa1
Carlin Tan <carlintan2001@gmail.com>
2023-08-22 04:29:36 +0800

    Added the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,19 +9,39 @@ public class Duke {
 
     }
 
+    // Displays list of items
+    private static String displayList(String[] list) {
+        String str = "";
+        int count = 0;
+        while (list[count] != null) {
+            str += count + 1 + ". " + list[count] + "\n\t ";
+            count++;
+        }
+
+        return messageCard(str.substring(0, str.length() - 3));
+    }
+
     // Main function
     public static void main(String[] args) {
         String CHATBOTNAME = "Carl";
         System.out.println(messageCard("Hello! I'm " + CHATBOTNAME
             + "\n\t What can I do for you?"));
+
+        String[] arr = new String[100];
+        int count = 0;
         while (true) {
             Scanner SC = new Scanner(System.in);
             String userInput = SC.nextLine();
             if (userInput.equals("bye")) {
                 System.out.println(messageCard("Bye. Hope to see you again soon!"));
                 break;
+            } else if (userInput.equals("list")) {
+                System.out.println(displayList(arr));
+            } else {
+                System.out.println(messageCard("added: " + userInput));
+                arr[count] = userInput;
+                count++;
             }
-            System.out.println(messageCard(userInput));
         }
     }
 }
