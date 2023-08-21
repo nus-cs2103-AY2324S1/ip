8bfdf973f7ba00d85fce23b2fa81302b79945e60
maypfv <maythandarhinn03@gmail.com>
2023-08-22 01:23:50 +0800

    Added: Level 3

--- src/main/java/Blip.java
+++ src/main/java/Blip.java
@@ -1,6 +1,31 @@
 import java.util.Scanner;
 
 public class Blip {
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "[X]" : "[ ]");
+        }
+
+        public void markStatus() {
+            this.isDone = true;
+        }
+
+        public void unmarkStatus() {
+            this.isDone = false;
+        }
+
+        public String getTaskNStatus() {
+            return getStatusIcon() + " " + this.description;
+        }
+    }
     public static void main(String[] args) {
         // Intro message by Blip.
         String intro = "Hello! I'm Blip\n"
@@ -15,8 +40,14 @@ public class Blip {
         // Constant list trigger word to display back stored text.
         String LIST_TRIGGER = "list";
 
+        // Constant mark trigger word to update status of task.
+        String MARK_TRIGGER = "mark";
+
+        // Constant mark trigger word to update status of task.
+        String UNMARK_TRIGGER = "unmark";
+
         // Fixed-size array of tasks to do.
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int index = 0;
 
         // Messages
@@ -29,17 +60,31 @@ public class Blip {
 
         while (!userInput.equals(END_TRIGGER)) {
             if (!userInput.equals(LIST_TRIGGER)) {
-                tasks[index] = userInput;
-                index++;
-                System.out.println("added: " + userInput);
-                userInput = scanIn.nextLine();
+                if (userInput.split(" ")[0].equals(MARK_TRIGGER)) {
+                    int taskNum = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                    Task taskToUpdate = tasks[taskNum];
+                    taskToUpdate.markStatus();
+                    System.out.println("Nice! I've marked this task as done:\n" + taskToUpdate.getTaskNStatus());
+                    userInput = scanIn.nextLine();
+                } else if (userInput.split(" ")[0].equals(UNMARK_TRIGGER)) {
+                    int taskNum = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                    Task taskToUpdate = tasks[taskNum];
+                    taskToUpdate.unmarkStatus();
+                    System.out.println("Ok, I've marked this task as not done yet:\n" + taskToUpdate.getTaskNStatus());
+                    userInput = scanIn.nextLine();
+                } else {
+                    tasks[index] = new Task(userInput);
+                    index++;
+                    System.out.println("added: " + userInput);
+                    userInput = scanIn.nextLine();
+                }
             } else {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < index; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + ". " + tasks[i].getTaskNStatus());
                 }
                 userInput = scanIn.nextLine();
             }
-
         }
 
         System.out.println(outro);
