08f9dad2db8cecf794314737a661764ec7238cda
maypfv <maythandarhinn03@gmail.com>
2023-08-22 00:01:47 +0800

    Added: Level 2

--- src/main/java/Blip.java
+++ src/main/java/Blip.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
+
 public class Blip {
     public static void main(String[] args) {
-        // Separator line.
-        String separator = "\n____________________________________________________________________________\n";
-
         // Intro message by Blip.
-        String intro = separator
-                + "Hello! I'm Blip\n"
-                + "What can I do for you?"
-                + separator;
+        String intro = "Hello! I'm Blip\n"
+                + "What can I do for you?";
 
         // Outro message by Blip.
-        String outro = separator
-                + "Bye. Hope to see you again soon!"
-                + separator;
+        String outro = "Bye. Hope to see you again soon!";
 
         // Constant end trigger word to end the chat with outro.
         String END_TRIGGER = "bye";
+
+        // Constant list trigger word to display back stored text.
+        String LIST_TRIGGER = "list";
+
+        // Fixed-size array of tasks to do.
+        String[] tasks = new String[100];
+        int index = 0;
 
         // Messages
         System.out.println(intro);
@@ -27,12 +28,21 @@ public class Blip {
 
 
         while (!userInput.equals(END_TRIGGER)) {
-            System.out.println(separator + userInput + separator);
-            userInput = scanIn.nextLine();
+            if (!userInput.equals(LIST_TRIGGER)) {
+                tasks[index] = userInput;
+                index++;
+                System.out.println("added: " + userInput);
+                userInput = scanIn.nextLine();
+            } else {
+                for (int i = 0; i < index; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                userInput = scanIn.nextLine();
+            }
+
         }
 
         System.out.println(outro);
-
 
     }
 }
