35e88321179c20fac8245d17852f0f51fd5f0792
Gavino3o <gavinchiam@gmail.com>
2023-08-21 21:58:02 +0800

    Add echo functionality and refactor print messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,44 @@
-public class Duke {
-    private static final String divider = "____________________________________________________________\n";
-    public static void main(String[] args) {
-        String logo = ".-. .-')              .-. .-')   \n" +
-                "\\  ( OO )             \\  ( OO )  \n" +
-                " ;-----.\\  .-'),-----. ;-----.\\  \n" +
-                " | .-.  | ( OO'  .-.  '| .-.  |  \n" +
-                " | '-' /_)/   |  | |  || '-' /_) \n" +
-                " | .-. `. \\_) |  |\\|  || .-. `.  \n" +
-                " | |  \\  |  \\ |  | |  || |  \\  | \n" +
-                " | '--'  /   `'  '-'  '| '--'  / \n" +
-                " `------'      `-----' `------'  ";
-        String welcomeMessage = "Hi, I'm Bob. How can I help you?";
-        String goodbyeMessage = "Goodbye! Bob signing out!";
+import java.util.Scanner;
 
-        System.out.println(divider + logo + "\n" + welcomeMessage);
-        System.out.println(divider + goodbyeMessage + "\n" + divider);
+public class Duke {
+    private static final String divider = "\n____________________________________________________________\n";
+    private static final String logo =
+            ".-. .-')              .-. .-')   \n" +
+            "\\  ( OO )             \\  ( OO )  \n" +
+            " ;-----.\\  .-'),-----. ;-----.\\  \n" +
+            " | .-.  | ( OO'  .-.  '| .-.  |  \n" +
+            " | '-' /_)/   |  | |  || '-' /_) \n" +
+            " | .-. `. \\_) |  |\\|  || .-. `.  \n" +
+            " | |  \\  |  \\ |  | |  || |  \\  | \n" +
+            " | '--'  /   `'  '-'  '| '--'  / \n" +
+            " `------'      `-----' `------'  ";
+
+    private static Boolean isActive = true;
+    public static void main(String[] args) {
+        Duke.printWelcomeMessage();
+        echo();
+    }
+    private static void printWelcomeMessage() {
+        String welcomeMessage = "Hi, I'm Bob. How can I help you?";
+        System.out.println(divider + logo + "\n" + welcomeMessage + divider);
+    }
+
+    private static void printGoodbyeMessage() {
+        String goodbyeMessage = "Goodbye! Bob signing out!";
+        System.out.println(goodbyeMessage + "\n" + divider);
+    }
+
+    private static void echo() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (isActive) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                Duke.printGoodbyeMessage();
+                isActive = false;
+            } else {
+                System.out.println(command + "!" + divider);
+            }
+        }
     }
 }
