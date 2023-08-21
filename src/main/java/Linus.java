bf3d9eef6ffdaff2922fc52e7fcd0ae5ee5f5f1f
joeng03 <yinjoeng2003@gmail.com>
2023-08-21 14:57:45 +0800

    feat: Echo

--- src/main/java/Linus.java
+++ src/main/java/Linus.java
@@ -1,8 +1,38 @@
+import java.util.Scanner;
 public class Linus {
-    public static void main(String[] args) {
+
+    public static void printWelcomeMessage(){
         String name = "LINUS";
-        System.out.println("Hello! I'm " + name + "\n"
-                            + "What can I do for you? \n"
-                            + "Bye. Hope to see you again soon! \n");
+        System.out.println(
+                "____________________________________________________________\n"
+                        + "Hello! I'm " + name + "\n"
+                        + "What can I do for you?\n"
+                        + "____________________________________________________________"
+        );
+    }
+    public static void printExitMessage() {
+        System.out.println(
+                "____________________________________________________________\n"
+                    + "Bye. Hope to see you again soon!\n"
+                    + "____________________________________________________________"
+        );
+    }
+    public static void chat() {
+        Scanner sc = new Scanner(System.in);
+        String input;
+        do {
+            input = sc.nextLine();
+            System.out.println(
+                    "____________________________________________________________\n"
+                            + input + "\n"
+                            + "____________________________________________________________"
+            );
+        }
+        while (!input.equals("bye"));
+    }
+    public static void main(String[] args) {
+        printWelcomeMessage();
+        chat();
+        printExitMessage();
     }
 }
