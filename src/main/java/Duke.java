ae423649a70c14fb1dc30450a92d7f831ab0dfb1
Pearlynn <e0968226@u.nus.edu>
2023-08-21 14:40:34 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,8 +2,16 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static String getNumTasks(int num) {
+        if (num == 1) {
+            return "\t Now you have " + num + " task in the list.";
+        }
+        return "\t Now you have " + num + " tasks in the list.";
+    }
+
     public static void main(String[] args) {
         ArrayList<Task> list = new ArrayList<>();
+        int count = 0;
         boolean exit = false;
         String line = "\t____________________________________________________________";
         System.out.println(line);
@@ -36,9 +44,35 @@ public class Duke {
                 t.markAsUndone();
                 System.out.println("\t OK, I've marked this task as not done yet:");
                 System.out.println("\t\t" + t.toString());
+            } else if (command.startsWith("todo")) {
+                Todo todo = new Todo(command.substring(5));
+                list.add(todo);
+                count++;
+                System.out.println("\t Got it. I've added this task:");
+                System.out.println("\t\t" + todo.toString());
+                System.out.println(getNumTasks(count));
+            } else if (command.startsWith("deadline")) {
+                Deadline deadline = new Deadline(command.substring(9, command.indexOf("/") - 1),
+                        command.substring(command.indexOf("/by") + 4));
+                list.add(deadline);
+                count++;
+                System.out.println("\t Got it. I've added this task:");
+                System.out.println("\t\t" + deadline.toString());
+                System.out.println(getNumTasks(count));
+            } else if (command.startsWith("event")) {
+                Event event = new Event(command.substring(6, command.indexOf("/") - 1),
+                        command.substring(command.indexOf("/from") + 6, command.indexOf("/to") - 1),
+                        command.substring(command.indexOf("/to") + 4));
+                list.add(event);
+                count++;
+                System.out.println("\t Got it. I've added this task:");
+                System.out.println("\t\t" + event.toString());
+                System.out.println(getNumTasks(count));
             } else {
                 list.add(task);
+                count++;
                 System.out.println("\t added: " + command);
+                System.out.println(getNumTasks(count));
             }
             System.out.println(line);
         }
