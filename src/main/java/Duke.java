b83e266af9766c397a9978e0e635dbf9aa204007
Darren159 <darren_low42@outlook.sg>
2023-08-21 20:09:35 +0800

    Add ability to store text entered by user and display them back to the user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
         String logo = "    ___    _   ___   ________  __      ____      __________  ____  _   __\n"
                 + "   /   |  / | / / | / / __ \\ \\/ /     / __ \\    /_  __/ __ \\/ __ \\/ | / /\n"
                 + "  / /| | /  |/ /  |/ / / / /\\  /_____/ / / /_____/ / / /_/ / / / /  |/ / \n"
@@ -11,12 +12,24 @@ public class Duke {
         String horizontalLine = "__________________________________________________________________________\n";
         String byeMessage = horizontalLine + "Bye. Hope to see you again soon!\n" + horizontalLine;
 
+        Scanner myObj = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
+
         System.out.println(horizontalLine + logo + "Hello! I'm ANNOY-O-TRON!\nWhat can I do for you?\n"
                 + horizontalLine);
 
         String userInput = myObj.nextLine();
         while(!userInput.equals("bye")) {
-            System.out.println(horizontalLine + userInput + "\n" + horizontalLine);
+            System.out.print(horizontalLine);
+            if (userInput.equals("list")) {
+                for (int i = 1; i < list.size() + 1; i++) {
+                    System.out.println(i + ". " + list.get(i - 1));
+                }
+            } else {
+                System.out.println("added: " + userInput);
+                list.add(userInput);
+            }
+            System.out.println(horizontalLine);
             userInput = myObj.nextLine();
         }
 
