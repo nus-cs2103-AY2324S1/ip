3e86346bb87782bf357f9e015fdaad70e4c1d4a3
dylkaw <37286651+dylkaw@users.noreply.github.com>
2023-08-21 14:27:58 +0800

    adding echo and exit functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     static void greet() {
         String greeting = "_________________________________________________\n"
@@ -6,7 +9,31 @@ public class Duke {
                 + "_________________________________________________\n";
         System.out.println(greeting);
     }
+
+    static void echo(String input) {
+        String echoMsg = "_________________________________________________\n"
+                + String.format(" %s\n", input)
+                + "_________________________________________________\n";
+        System.out.println(echoMsg);
+    }
+
+    static void exit() {
+        String exitMsg = "_________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "_________________________________________________\n";
+        System.out.println(exitMsg);
+        System.exit(0);
+    }
     public static void main(String[] args) {
         greet();
+        while (true) {
+            Scanner inputScanner = new Scanner(System.in);
+            String input = inputScanner.nextLine();
+            if (Objects.equals(input, "bye")) {
+                exit();
+            } else {
+                echo(input);
+            }
+        }
     }
 }
