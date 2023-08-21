5f577f6370707e6d86a4a8383d5f41cd73b4977d
ji-just-ji <wongjk9999@gmail.com>
2023-08-21 08:43:28 +0800

    Level-1 Add echo functionality Move introduction into separate function

--- src/main/java/Peko.java
+++ src/main/java/Peko.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
+
 public class Peko {
-    static String lineBreak = "------------------------------------------"; //42
-    static String introText = "Konpeko, Konpeko, Konpeko! \n" +
+
+
+    private static final int ECHO = 0;
+    private static String currInput;
+    private static Scanner scanner = new Scanner(System.in);
+    private static String lineBreak = "------------------------------------------"; //42
+    private static String introText = "Konpeko, Konpeko, Konpeko! \n" +
             "Usada Pekora-peko! almondo almondo!";
-    static String exitText = "Otsupeko! Bye bye!";
+    private static String exitText = "Otsupeko! Bye bye!";
     public static void main(String[] args) {
+        String input = "";
+        int responseValue = 0;
+        intro();
+        input = interaction();
+
+        switch (responseValue) {
+            case ECHO:
+                System.out.println(input);
+            default:
+        }
+        exit();
+    }
+
+    public static void intro() {
         String pekoLogo = " _____      _\n"
                 + "|     |___ | | ______\n"
                 + "|  ___/ _ \\| |/ /    \\\n"
@@ -14,9 +35,13 @@ public class Peko {
         System.out.println(lineBreak);
         System.out.println(introText);
         System.out.println(lineBreak);
-        exit();
     }
 
+    public static String interaction() {
+        currInput = scanner.next();
+        System.out.println(lineBreak);
+        return currInput;
+    }
     public static void exit() {
         System.out.println(exitText);
     }
