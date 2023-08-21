d4563e6b83c69665e349d278ebe0cbedde469643
Nixx162 <nicholas.jimmyalden@u.nus.edu>
2023-08-21 23:50:57 +0800

    feat: add echo feature

--- src/main/java/Pardiyem.java
+++ src/main/java/Pardiyem.java
@@ -1,9 +1,12 @@
 import pardiyem.logic.Pardi;
+import java.util.Scanner;
 
 public class Pardiyem {
     public static void main(String[] args) {
         Pardi pardiyem = new Pardi();
+        Scanner scanner = new Scanner(System.in);
         pardiyem.greeting();
+        while (pardiyem.run(scanner.nextLine())) {}
         pardiyem.exit();    
     }
 }
--- src/main/java/pardiyem/logic/Pardi.java
+++ src/main/java/pardiyem/logic/Pardi.java
@@ -1,13 +1,24 @@
 package pardiyem.logic;
 
+import java.util.Objects;
+
 public class Pardi {
-    private final String GREETING = "\nHello, I'm Pardi\nWhat can I do for you?";
-    private final String BYE = "\nCiao! See you again!";
+    private final String GREETING = "\nHello, I'm Pardi\nWhat can I do for you?\n";
+    private final String BYE = "\nCiao! See you again!\n";
     public void greeting() {
         System.out.println(GREETING);
     }
 
     public void exit() {
         System.out.println(BYE);
+    }
+
+    public boolean run(String in) {
+        if (Objects.equals(in, "bye")) {
+            return false;
+        } else {
+            System.out.println("\n" + in + "\n");
+            return true;
+        }
     }
 }
