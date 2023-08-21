1459852702d05148beccc0eeacaaf853197d068b
nreHieW <limweihern@gmail.com>
2023-08-21 15:35:28 +0800

    Added User Input and Echo Functionality

--- src/main/java/James.java
+++ src/main/java/James.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class James {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm James\n" + "What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        String line = "____________________________________________________________";
+        System.out.println(line + "\n" + "Hello! I'm James\n" + "What can I do for you?\n" + line);
+
+        // User Input
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(line + "\n" + input + "\n" + line);
+            input = in.nextLine();
+        }
+
+        System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
+
     }
 }
