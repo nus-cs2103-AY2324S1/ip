f86a948b5b951f11f57e4c70c3abc055dcea5050
aexolate <20797128+aexolate@users.noreply.github.com>
2023-08-21 22:46:32 +0800

    Add Task.java, Implemented "mark", "unmark" commands

--- src/main/java/CarbonBot.java
+++ src/main/java/CarbonBot.java
@@ -6,7 +6,7 @@ public class CarbonBot {
     private static String DIVIDER = "____________________________________________________________";
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<String> commandsList = new ArrayList<>();
+        List<Task> taskList = new ArrayList<>();
 
         System.out.println(DIVIDER);
         System.out.println("Hello! I'm CarbonBot");
@@ -14,33 +14,80 @@ public class CarbonBot {
         System.out.println(DIVIDER);
 
         while(true) {
-            String cmd = sc.nextLine();
+            String input = sc.nextLine();
 
+            // Do not process if the input was empty
+            if (input.isEmpty()) {
+                continue;
+            }
+
+            String cmd = input.split(" ")[0];
+            int taskIdx;
+
+            System.out.println(DIVIDER);
             switch(cmd) {
                 case "bye":
                     // Stops listening to user input if the command is "bye"
-                    System.out.println(DIVIDER);
                     System.out.println("Bye. Hope to see you again soon!");
                     System.out.println(DIVIDER);
                     sc.close();
                     return;
                 case "list":
                     // Lists all the commands saved in the arraylist
-                    System.out.println(DIVIDER);
                     int idx = 1;
-                    for(String c : commandsList) {
-                        System.out.println(String.format("%d. %s", idx, c));
+                    for(Task t : taskList) {
+                        System.out.println(String.format("%d.%s", idx, t));
                         idx++;
                     }
-                    System.out.println(DIVIDER);
+                    break;
+                case "mark":
+                    updateTaskStatus(taskList, input, true);
+                    break;
+                case "unmark":
+                    updateTaskStatus(taskList, input, false);
                     break;
                 default:
-                    // Echos and saves the command given
-                    System.out.println(DIVIDER);
-                    commandsList.add(cmd);
-                    System.out.println("added: " + cmd);
-                    System.out.println(DIVIDER);
+                    // Adds the task to the list
+                    Task task = new Task(input);
+                    taskList.add(task);
+                    System.out.println("added: " + input);
             }
+            System.out.println(DIVIDER);
         }
     }
+
+    private static void updateTaskStatus(List<Task> tasks, String input, boolean isDone) {
+        // Check if the user has specified the index to be updated
+        if (input.split(" ").length < 2) {
+            System.out.println("Please provide the task index to be updated.");
+            return;
+        }
+
+        try {
+            int taskIdx = Integer.parseInt(input.split(" ")[1]);
+            if (taskIdx > 0 && taskIdx <= tasks.size()) {
+                // The -1 is because of the list 0-indexing
+                Task t = tasks.get(taskIdx - 1);
+
+                if (isDone) {
+                    // Mark task as done
+                    t.markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                } else {
+                    // Mark task as not done
+                    t.markAsUndone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                }
+                System.out.println(t);
+            } else {
+                // Do not process the command if the index was out of bounds
+                System.out.println("Index given was out-of-bounds");
+            }
+        } catch (NumberFormatException ex) {
+            // Ensure the index is integer
+            System.out.println("Please provide a valid integer index.");
+        }
+
+    }
+
 }
