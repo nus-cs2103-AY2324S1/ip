0ccae52b8ae9d77a9e0d9f4a7cef5249bb1e0fae
freshcabbage123 <alan@Alans-MacBook-Air.local>
2023-08-21 12:18:04 +0800

    Implement Level-0: Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
 public class Duke {
+
+    /**
+     * Name of the bot.
+     */
+    private static final String NAME = "Cabbage";
+
+    /**
+     * Entry-point for the program. Displays a logo and greeting.
+     * @param args CLI arguments passed into the program.
+     */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        greet();
     }
+
+    /**
+     * Greets the user.
+     */
+    public static void greet() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm " + NAME);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
 }
