e8ccacd566d9c847f0b7e35874c792934a518a01
jonasongg <jonasong2@gmail.com>
2023-08-21 22:19:26 +0800

    add ability to store text and list them when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     static final String HORIZONTAL_LINE = "____________________________________________________________\n";
+    static List<String> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         String greeting = HORIZONTAL_LINE + " Hello! I'm Pixel\n What can I do for you?\n" + HORIZONTAL_LINE;
         System.out.println(greeting);
@@ -11,9 +15,15 @@ public class Duke {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 System.out.println(HORIZONTAL_LINE + " Bye. Hope to see you again soon!\n" + HORIZONTAL_LINE);
-                break;
+            } else if (input.equals("list")) {
+                System.out.println(HORIZONTAL_LINE);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println(HORIZONTAL_LINE);
             } else {
-                System.out.println(HORIZONTAL_LINE + input + "\n" + HORIZONTAL_LINE);
+                tasks.add(input);
+                System.out.println(HORIZONTAL_LINE + "added: " + input + "\n" + HORIZONTAL_LINE);
             }
         }
     }
