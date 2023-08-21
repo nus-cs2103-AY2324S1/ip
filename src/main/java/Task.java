4a80ec24101201928d5cae20dcb4ea02a77b18c0
dylkaw <37286651+dylkaw@users.noreply.github.com>
2023-08-21 14:39:48 +0800

    creating task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    private String task;
+    public Task(String task) {
+        this.task = task;
+    }
+
+    @Override
+    public String toString() {
+        return this.task;
+    }
+}
