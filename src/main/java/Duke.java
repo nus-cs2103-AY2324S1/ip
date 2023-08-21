28972e3dd62ab92400a3e252f576c84353dc5557
zacwong2151 <zacwong2151@gmail.com>
2023-08-21 16:25:45 +0800

    added couple print.ln statements for level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String horizontalLines = "_______________________________________";
+        String name = "zac";
+
+        System.out.println(horizontalLines);
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLines);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLines);
     }
 }
