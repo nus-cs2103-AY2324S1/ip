dd78242c5545e2965455f59f2a2a54e28751bbe4
seantehds <seanteh2001@gmail.com>
2023-08-21 23:42:20 +0800

    Added basic functionality to print output for new chatbot Emiya

---
+++ src/main/java/Emiya.java
@@ -0,0 +1,16 @@
+public class Emiya {
+
+    public static void main(String[] args) {
+        String welcomeMessage = "-----------------------------------------\n"
+                + "Hello! I'm Emiya\n"
+                + "What can I do for you?\n"
+                + "-----------------------------------------\n";
+        String exitMessage = "-----------------------------------------\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "-----------------------------------------\n";
+
+        System.out.println(welcomeMessage);
+        System.out.println(exitMessage);
+
+    }
+}
