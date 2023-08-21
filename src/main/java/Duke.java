cbc3c8be50e99d209df635fc4752a3484a9bffbe
rayyan35p <rayyanis0908@gmail.com>
2023-08-21 21:04:54 +0800

    Integrate Task and TaskList into main code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> list =  new ArrayList<>();
+        TaskList list = new TaskList();
 
         System.out.println("Hello! I'm Bob");
         System.out.println("What can I do for you?");
@@ -14,10 +14,30 @@ public class Duke {
             if (input.equals("bye")) {
                 break;
             }
-            if (input.equals("list")) {
-                printList(list);
-            } else {
-                list.add(input);
+            else if (input.equals("list")) {
+                list.printList();
+            }
+            else if (input.startsWith("mark") || input.startsWith("unmark")) {
+                String[] inputSplit = input.split(" ");
+                if (inputSplit.length == 1) {
+                    System.out.println("Input the task number!");
+                } else if (inputSplit.length > 2) {
+                    System.out.println("Invalid command");
+                } else {
+                    int taskNo = Integer.parseInt(inputSplit[1]) - 1;
+
+                    if (input.startsWith("mark")) {
+                        list.setTaskComplete(taskNo);
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println(list.getTask(taskNo));
+                    } else {
+                        list.setTaskIncomplete(taskNo);
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println(list.getTask(taskNo));
+                    }
+                }
+            }  else {
+                list.addTask(new Task(input));
                 System.out.println("added: " + input);
             }
             input = scanner.nextLine();
@@ -26,9 +46,9 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    public static void printList(ArrayList<String> list) {
+    public static void printList(ArrayList<Task> list) {
         for (int i = 0; i < list.size(); i++) {
-            System.out.println(i + 1 + ". " + list.get(i));
+            System.out.println(i + 1 + "." + list.get(i));
         }
     }
 }
