4305f3de45c13763c58ab81fc3d03353c37d20fa
jinyuan0425 <pdmon123@gmail.com>
2023-08-21 22:24:47 +0800

    Completed Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;  // Import the Scanner class
 public class Duke {
+    int TODO = 0, DEADLINE = 1, EVENT = 2;
     public static class Task {
         protected String description;
         protected boolean isDone;
@@ -24,6 +25,47 @@ public class Duke {
             return "[" + getStatusIcon() + "] " + this.description;
         }
     }
+    public static class Todo extends Task {
+        public Todo(String description) {
+            super(description);
+        }
+
+        @Override
+        public String toString() {
+            return "[T]" + super.toString();
+        }
+    }
+    public static class Deadline extends Task {
+
+        protected String by;
+
+        public Deadline(String description, String by) {
+            super(description);
+            this.by = by;
+        }
+
+        @Override
+        public String toString() {
+            return "[D]" + super.toString() + " (by: " + by + ")";
+        }
+    }
+
+    public static class Event extends Task {
+        protected String from;
+        protected String to;
+
+        public Event(String description, String from, String to) {
+            super(description);
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public String toString() {
+            return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        }
+    }
+
     public static void main(String[] args) {
         Task[] list = new Task[100]; // List to be returned when input is "list"
         int counter = 0; // Items in list
@@ -42,20 +84,65 @@ public class Duke {
                         list[index].toString());
             }
 
-            else if (echo.startsWith("unmark ")) { // Mark task complete
+            else if (echo.startsWith("unmark ")) { // Mark task incomplete
                 int index = Integer.valueOf(echo.substring(7)) - 1; // Index of task
                 list[index].setNotDone(); // Item mark complete
                 System.out.println("No worries. I will now mark it as incomplete: \n" +
                         list[index].toString());
             }
 
-            else if (echo.equals("list")) {
+            else if (echo.equals("list")) { // Returns the list of tasks
                 for (int i = 0; i < list.length; i++) {
                     if (list[i] != null) {
                         System.out.println(i + 1 + ". " + list[i]); // Returns the list
                     }
                 }
-            } else {
+            }
+
+            else if (echo.startsWith("todo ")) { // Task of Todo type
+                // Insert Todo into list
+                String tasking = echo.substring(5);
+                list[counter] = new Todo(tasking);
+                String task = list[counter].toString();
+                counter += 1;
+
+                String response = "Understood, I will add the following task to your list:\n" + task;
+                String listLength = "Please note that there are " + counter + " tasks in the list.";
+                System.out.println(response + "\n" + listLength);
+            }
+
+            else if (echo.startsWith("deadline ")) { // Task of Deadline type
+                // Insert Deadline into list
+                String tasking = echo.substring(9);
+                String[] taskTime = tasking.split(" /by ");
+                list[counter] = new Deadline(taskTime[0], taskTime[1]);
+                String task = list[counter].toString();
+                counter += 1;
+
+                String response = "Understood, I will add the following task to your list:\n" + task;
+                String listLength = "Please note that there are " + counter + " tasks in the list.";
+                System.out.println(response + "\n" + listLength);
+            }
+
+            else if (echo.startsWith("event ")) { // Task of Event type
+                // Insert Event into list
+                String tasking = echo.substring(6);
+                String[] taskTime = tasking.split(" /from ");
+                String event = taskTime[0];
+                String[] startEnd = taskTime[1].split(" /to ");
+                String start = startEnd[0];
+                String end = startEnd[1];
+
+                list[counter] = new Event(event, start, end);
+                String task = list[counter].toString();
+                counter += 1;
+
+                String response = "Understood, I will add the following task to your list:\n" + task;
+                String listLength = "Please note that there are " + counter + " tasks in the list.";
+                System.out.println(response + "\n" + listLength);
+            }
+
+            else {
                 // Echos the input
                 String response = "Understood, I will add \"" + echo + "\" to your to do list.";
                 System.out.println(response);
