813a1363c7173ea82d5c290e247bb7f06e4d86dd
Ren Jing <e0958325@u.nus.edu>
2023-08-21 11:35:40 +0800

    Echo
    Added in custom personality and user input and output functionality.

--- src/main/java/Ren.java
+++ src/main/java/Ren.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class Ren {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         String welcomeMsg = "____________________________________________________________\n" +
                 " Hello! I'm Ren\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        String goodbyeMsg = "____________________________________________________________\n" +
+                " Bye! Pleasure speaking with you :) \n" +
                 "____________________________________________________________\n";
         System.out.println(welcomeMsg);
+        String inputStr = input.next();
+        while (!inputStr.equals("bye")) {
+            System.out.println("____________________________________________________________\n" +
+                    String.format("%s\n", inputStr) +
+                    "____________________________________________________________\n");
+            inputStr = input.next();
+        }
+
+        System.out.println(goodbyeMsg);
     }
 }
