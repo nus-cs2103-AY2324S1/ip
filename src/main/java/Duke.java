b6993bbdd8b8689ce8bd30c063602df1242d8f00
samuelim01 <samuelim01@gmail.com>
2023-08-21 18:28:07 +0800

    Implemented Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
 public class Duke {
+
+    private static String name = "SoCrates";
+    private static String line =
+        "\t____________________________________________________________";
+
+
+    private static void printMessage(String message) {
+        System.out.println(line);
+        System.out.println("\t" + message);
+        System.out.println("\n" + line);
+    }
+
+    private static void printMessage(String[] message) {
+        System.out.println(line);
+        for (String messageLine : message) {
+            System.out.println("\t" + messageLine);
+        }
+        System.out.println("\n" + line);
+    }
+
+    public static void printWelcome() {
+        String[] message = {
+            "Hello! I'm " + name,
+            "What can I do for you?"
+        };
+
+        printMessage(message);
+    }
+
+    public static void printExit() {
+        printMessage("Bye. Hope to see you again soon!");
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printWelcome();
+
+        printExit();
     }
 }
