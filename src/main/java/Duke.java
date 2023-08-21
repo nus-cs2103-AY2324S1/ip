1f313568dc7decf9528fd2b8a896550cf422cb13
richiehx <hsiehrichie246@gmail.com>
2023-08-22 00:43:04 +0800

    Completed Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String horizontalLine = "____________________________________________________________";
+        System.out.println(horizontalLine + "\nHello! I'm Nano\n" +
+                "What can I do for you?\n" + horizontalLine);
+
+        System.out.println(horizontalLine + "\n Bye. Hope to see you again soon!\n" + horizontalLine);
     }
 }
