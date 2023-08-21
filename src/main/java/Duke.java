8b4ff2afb9f8f77df0e615030dd7d24a25fbd20c
weiennn <huangweien01@gmail.com>
2023-08-21 17:34:59 +0800

    Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "Dude";
+        greet(name);
+        bye();
+    }
+    public static void greet(String name) {
+        System.out.println("Hello! I'm " + name + "\n" +
+                "What can I do for you?\n");
+    }
+    public static void bye() {
+        System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
