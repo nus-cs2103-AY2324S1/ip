59351714df0d478b7ec09cfc629f250864bef226
raynertjx <rayner742@gmail.com>
2023-08-21 17:24:45 +0800

    added add, list for tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
     public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<>();
         String welcomeMessage = "____________________________________________________________\n" +
                 " Hello! I'm Bongo!\n" +
                 " What can I do for you?\n" +
@@ -18,8 +21,20 @@ public class Duke {
                 System.out.println(goodbyeMessage);
                 break;
             }
+            if (input.equals("list")) {
+                StringBuilder allTasks = new StringBuilder();
+                for (int i = 0; i < tasks.size(); i++) {
+                    allTasks.append(String.format(" %d. %s\n", i + 1, tasks.get(i)));
+                }
+                String tasksList = "____________________________________________________________\n" +
+                        allTasks +
+                        "____________________________________________________________\n";
+                System.out.println(tasksList);
+                continue;
+            }
+            tasks.add(input);
             String echoMessage = "____________________________________________________________\n" +
-                    String.format(" %s\n", input) +
+                    String.format(" added: %s\n", input) +
                     "____________________________________________________________\n";
             System.out.println(echoMessage);
         }
