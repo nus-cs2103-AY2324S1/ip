9b635f8e7bd68b93cd78b994e2de17348b7e88fa
freshcabbage123 <alan@Alans-MacBook-Air.local>
2023-08-21 19:17:17 +0800

    Implement Level 5: Handle Errors

---
+++ src/main/java/exception/DukeException.java
@@ -0,0 +1,13 @@
+/**
+ * DukeException class is a custom exception class that extends Exception.
+ */
+public class DukeException extends Exception{
+
+    /**
+     * Constructor for DukeException.
+     * @param message The error message.
+     */
+    public DukeException(String message) {
+        super(message);
+    }
+}
