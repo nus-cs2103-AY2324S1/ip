5713522fbc4d4fe8977788db52f803ed8d6fd709
weiennn <huangweien01@gmail.com>
2023-08-21 23:16:30 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ public class Duke {
     public static void main(String[] args) {
         String name = "Botty";
         String tmp = "";
-        String[] taskList = new String[100];
+        Task[] taskList = new Task[100];
         int counter = 0;
         Scanner scanner = new Scanner(System.in);
         greet(name);
@@ -12,13 +12,44 @@ public class Duke {
             if (tmp.equals("bye")) {
                 break;
             } else if (tmp.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < counter; i++) {
-                    tmp = taskList[i];
-                    System.out.println((i + 1) + ". " + tmp);
+                    Task currTask = taskList[i];
+                    String listCounter = String.valueOf(i + 1) + ".";
+                    String checkBox = "[" + currTask.getStatusIcon() + "]";
+                    String taskDescription = currTask.getDescription();
+                    System.out.println(listCounter + checkBox + " " + taskDescription);
                 }
-            } else {
-                System.out.println("added " + tmp);
-                taskList[counter] = tmp;
+                System.out.println("\n");
+            } else if (tmp.contains("unmark")) {
+                String[] stringArray = tmp.split(" ");
+                if (stringArray.length != 2) {
+                    System.out.println("Error unexpected number or arguments");
+                } else {
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    int taskListIndex = Integer.parseInt(stringArray[1]) - 1;
+                    Task currTask = taskList[taskListIndex];
+                    currTask.markUndone();
+                    String checkBox = "[" + currTask.getStatusIcon() + "]";
+                    String taskDescription = currTask.getDescription();
+                    System.out.println( checkBox + " " + taskDescription + "\n");
+                }
+            } else if (tmp.contains("mark")) {
+                String[] stringArray = tmp.split(" ");
+                if (stringArray.length != 2) {
+                    System.out.println("Error unexpected number or arguments");
+                } else {
+                    System.out.println("Nice! I've marked this task as done:");
+                    int taskListIndex = Integer.parseInt(stringArray[1]) - 1;
+                    Task currTask = taskList[taskListIndex];
+                    currTask.markDone();
+                    String checkBox = "[" + currTask.getStatusIcon() + "]";
+                    String taskDescription = currTask.getDescription();
+                    System.out.println( checkBox + " " + taskDescription + "\n");
+                }
+            }  else {
+                System.out.println("added " + tmp + "\n");
+                taskList[counter] = new Task(tmp);
                 counter++;
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+    public String getDescription() {
+        return this.description;
+    }
+    public void markUndone() {
+        this.isDone = false;
+    }
+    public void markDone() {
+        this.isDone = true;
+    }
+
+}
