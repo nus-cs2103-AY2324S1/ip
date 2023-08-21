448e23ac117e9e23cdc158b848b9afd25e31f22d
VanessaMae23 <vanessamae23@gmail.com>
2023-08-21 13:21:36 +0800

    Added level 1 tag

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = "CS2103T";
+        String logo = "Bacon Pancake";
         System.out.println(" Hello from " + logo + "\n What can I do for you? \n" + "---------------------------------- \n"
                 + " Bye. Hope to see you again soon! \n"
                 + "----------------------------------\n ");
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("Enter your input : ");
+            String input = sc.nextLine();
+            System.out.println("Bacon Pancake : \n" + input);
+        }
+
     }
 }
