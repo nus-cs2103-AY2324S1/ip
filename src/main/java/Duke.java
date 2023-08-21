7a9eb67eeed7ccdddf51dab3b01bca8e9018d3a3
ryantzr1 <dreisprung1821@gmail.com>
2023-08-21 16:16:46 +0800

    Level 1: Add Echo function to echo command entered by user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm FRIDAY!\n" +
-                "What can I do for you?\n" +
-                "Bye. Hope to see you again soon!");
+                "What can I do for you?");
+
+        echo();
     }
+
+    public static void echo() {
+        String task = "";
+        while (task != "bye") {
+            Scanner newInput = new Scanner(System.in);
+            task = newInput.nextLine();
+            System.out.println(task);
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+
+    }
+
 }
