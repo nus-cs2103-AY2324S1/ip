90b0048e0e3e956b12c7289936b4bc70b604ac45
Tan Teck Fang <tanteckfang@Tans-MacBook-Pro.local>
2023-08-21 22:08:32 +0800

    Complete Level-0

---
+++ src/main/java/Sara.java
@@ -0,0 +1,15 @@
+public class Sara {
+    public static void main(String[] args) {
+        greet();
+    }
+            public static void greet() {
+            String separator = "____________________________________________________________";
+            String greeting = " Hello! I'm Sara\n What can I do for you?";
+            String farewell = " Bye. Hope to see you again soon!";
+
+            System.out.println(separator);
+            System.out.println(greeting);
+            System.out.println(separator);
+            System.out.println(farewell);
+        }
+    }
