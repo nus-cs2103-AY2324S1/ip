906971659ddfe81607794f4e89bd46692972f870
SynapseProgramming <roald.ong@gmail.com>
2023-08-21 22:06:07 +0800

    Added in echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
 
   public static void main(String[] args) {
 
     String greeting = "Hello! I'm KimochiUsagi (きもち　うさぎ)!\n";
+    String info = "Ask the bunny a question!\n";
     String goodbye = "Bye. See you again! (またね)";
     System.out.println(greeting);
+    System.out.println(info);
+
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+      String inputString = scanner.nextLine();
+      if (inputString.equals("bye")) {
+        break;
+      } else {
+        System.out.println("\t" + inputString);
+      }
+    }
+
     System.out.println(goodbye);
 
   }
