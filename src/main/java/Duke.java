2fb94bce6dd0f4c7261634c2998ef582fba6f33e
jinyuan0425 <pdmon123@gmail.com>
2023-08-21 16:51:17 +0800

    Completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;  // Import the Scanner class
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm JYBot!\n" + "What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Good day to you, I'm ButlerBot.\n" +
+                "How may I be of service to you?\n"); // Greets user
+        String echo = myObj.nextLine(); // Reads user input
+        while (!echo.equals("bye")) {
+            // Echos the input unless input is "bye"
+            String polite = "Pardon me, but I believe you said ";
+            System.out.println(polite + echo + "?");
+            echo = myObj.nextLine();
+        }
+        System.out.println("Goodbye and have a nice day.\n"); // Exits the bot
     }
 }
