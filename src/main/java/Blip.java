1f83e07ff0dacdaff84b6139b5a7030dbd70e89d
maypfv <maythandarhinn03@gmail.com>
2023-08-21 22:04:56 +0800

    Add: Level 0

--- src/main/java/Blip.java
+++ src/main/java/Blip.java
@@ -1,13 +1,4 @@
 public class Blip {
-/*    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }*/
-
     public static void main(String[] args) {
         String intro = "____________________________________________________________________________\n"
                 + "Hello! I'm Blip\n"
@@ -17,5 +8,4 @@ public class Blip {
         String separator = "____________________________________________________________________________\n";
         System.out.println(intro + separator +  outro);
     }
-
 }
