e44a7e5e58f2f5e591a7d40c9141882bd6fd7089
songfangyl <e0960536@u.nus.edu>
2023-08-21 12:46:42 +0800

    add interaction: chatbot now can echo what user input, exit with the keyword "exit"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,23 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String greeting = "Hello(@.@), I'm NiHao \nWhat can I do for you?";
-        String exitText = "Bye(T.T), Hope to see you again soon!";
-        String horizontalLine = "_____________________________________________________________";
-        System.out.println(horizontalLine + "\n" + greeting + "\n" + horizontalLine + "\n" + exitText);
-
+        Scanner sc = new Scanner(System.in);
+        String LOGO = "   /\\_/\\  \n" +
+                "  ( o.o ) \n" +
+                "   > ^ <\n";
+        String GREETING = "Hello(@.@), I'm NiHao \nWhat can I do for you?";
+        String EXIT = "Bye(T.T), Hope to see you again soon!";
+        String HORILINE = "_____________________________________________________________";
+        System.out.println(LOGO + GREETING);
+        while (true) {
+            String input = sc.nextLine();
+            if (Objects.equals(input, "exit") || Objects.equals(input, "Exit") || Objects.equals(input, "EXIT")) {
+                break;
+            } else {
+                System.out.println("\t" +input);
+            }
+        }
+        System.out.println(EXIT);
     }
 }
