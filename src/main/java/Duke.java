b6d7e944e73b1bf7b2bb5ca3e53c2abe11dd6b17
dylkaw <37286651+dylkaw@users.noreply.github.com>
2023-08-21 12:54:32 +0800

    reducing greeting message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,8 +3,6 @@ public class Duke {
         String greeting = "_________________________________________________\n"
                 + " Hello! I'm Glub!\n"
                 + " What can I do for you?\n"
-                + "_________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
                 + "_________________________________________________\n";
         System.out.println(greeting);
     }
