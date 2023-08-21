e8854359fd0bcdb00ff02e6aff17d512684689f9
CelestineTan03 <e0968156@u.nus.edu>
2023-08-21 17:39:19 +0800

    Add list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,29 @@
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        String[] tasks = new String[100];
+        int currIndex = 0;
+
         String greetings = "Hello! I'm Botty!\nWhat can I do for you?\n";
         String farewell = "Bye. Hope to see you again soon!\n";
         System.out.println(greetings);
+
         Scanner sc = new Scanner(System.in);
+        // Only exit when user types the command bye
         while (true) {
             String command = sc.nextLine();
             if (command.equals("bye")) {
                 System.out.println(farewell);
                 sc.close();
                 break;
+            } else if (command.equals("list")) {
+                for (int i = 0; i < currIndex; i++) {
+                    System.out.println(i + 1 + ". " + tasks[i] + "\n");
+                }
             } else {
-                System.out.println(command + "\n");
+                tasks[currIndex] = command;
+                currIndex++;
+                System.out.println("added: " + command + "\n");
             }
         }
     }
