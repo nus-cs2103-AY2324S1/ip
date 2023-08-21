95474419eff6abd9676af2da9b98a2a18e255b0f
dylkaw <37286651+dylkaw@users.noreply.github.com>
2023-08-21 15:08:43 +0800

    adding getDoneIcon, toggleDone methods to Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,20 @@
 public class Task {
     private String task;
+    private boolean isDone;
     public Task(String task) {
         this.task = task;
+        this.isDone = false;
+    }
+
+    public String getDoneIcon() {
+        return isDone ? "X" : " ";
+    }
+    public void toggleDone() {
+        this.isDone = !this.isDone;
     }
 
     @Override
     public String toString() {
-        return this.task;
+        return String.format("[%s] %s", getDoneIcon(), task);
     }
 }
