1005f73546e87efa73c0860a19d824f1e038306a
Shang Yi Ku <kushangyi2001@gmail.com>
2023-08-21 18:33:29 +0800

    Level-0 changes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
+    private static final String HORZ_LINE = "____________________________________________________________";
+    private static final String BOT_NAME = "SHIBA-BOT";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printGreeting();
+        printBye();
+    }
+
+    /** Prints the greeting message. */
+    private static void printGreeting() {
+        System.out.println(HORZ_LINE);
+        System.out.println("Woof! I'm " + BOT_NAME);
+        System.out.println("What can I bark at you?");
+        System.out.println(HORZ_LINE);
+    }
+
+    /** Prints the bye message */
+    private static void printBye() {
+        System.out.println("Woof! Hope to bark at you again soon!");
+        System.out.println(HORZ_LINE);
     }
 }
