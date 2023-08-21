e2df990616a00aafc768625016acc86f24814f1b
sushiyade <68842066+sushiyade@users.noreply.github.com>
2023-08-21 13:05:10 +0800

    level-2 new

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,22 +3,37 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String EXIT_PHRASE = "bye";
+        String LIST_PHRASE = "list";
+        String[] toDoList = new String[100];
+        int i = 0;
         System.out.println(
-                "Hello! I'm Sunacchi\n" +
+                "Hello! I'm Sunacchi!\n" +
                         "What can I do for you?\n"
         );
 
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 
-        String userInput = myObj.nextLine();
-        while (!userInput.equals(EXIT_PHRASE)) {
-            System.out.println(userInput);
-            userInput = myObj.nextLine();
+        String userInput = myObj.next();
+        while (i < 100) {
+            if (userInput.equals(EXIT_PHRASE)) {
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            }
+            if (userInput.equals(LIST_PHRASE)) {
+                for (int j = 0; j < i; j++) {
+                    System.out.println(j + 1 + "." + " " + toDoList[j]);
+                }
+                userInput = myObj.next();
+                continue;
+            }
+            if (userInput.equals("")) {
+                userInput = myObj.next();
+                continue;
+            }
+            toDoList[i] = userInput;
+            System.out.println("added: " + userInput);
+            i++;
+            userInput = myObj.next();
         }
-        System.out.println("Bye. Hope to see you again soon!");
-
-
-
-
     }
 }
