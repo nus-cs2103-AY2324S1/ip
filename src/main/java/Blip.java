93fe6ca6ba8e6663bf0d8b4c1d66bbf14b19ac80
maypfv <maythandarhinn03@gmail.com>
2023-08-21 22:54:32 +0800

    Add: Level 1

--- src/main/java/Blip.java
+++ src/main/java/Blip.java
@@ -1,11 +1,38 @@
+import java.util.Scanner;
 public class Blip {
     public static void main(String[] args) {
-        String intro = "____________________________________________________________________________\n"
+        // Separator line.
+        String separator = "\n____________________________________________________________________________\n";
+
+        // Intro message by Blip.
+        String intro = separator
                 + "Hello! I'm Blip\n"
-                + "What can I do for you?\n";
-        String outro = "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________________________\n";
-        String separator = "____________________________________________________________________________\n";
-        System.out.println(intro + separator +  outro);
+                + "What can I do for you?"
+                + separator;
+
+        // Outro message by Blip.
+        String outro = separator
+                + "Bye. Hope to see you again soon!"
+                + separator;
+
+        // Constant end trigger word to end the chat with outro.
+        String END_TRIGGER = "bye";
+
+        // Messages
+        System.out.println(intro);
+
+        String userInput;
+        Scanner scanIn = new Scanner(System.in);
+        userInput = scanIn.nextLine();
+
+
+        while (!userInput.equals(END_TRIGGER)) {
+            System.out.println(separator + userInput + separator);
+            userInput = scanIn.nextLine();
+        }
+
+        System.out.println(outro);
+
+
     }
 }
