9a2afb14e08f7ef9f9a7e01379b4acc1575fd5b8
KamJiaYue <e0960550@u.nus.edu>
2023-08-22 00:44:08 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,44 @@
+import java.util.Scanner;
+
 public class Duke {
     private String name = "KAM_BOT";
-    private static final String line = "————————————————————————————————————————————————————";
+    private String bye = "bye";
+    private static final String line = "    ————————————————————————————————————————————————————";
 
     public Duke(String name) {
         this.name = name;
     }
 
     public void exit() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(Duke.line);
+        System.out.println("    Bye. Hope to see you again soon!");
         System.out.println(Duke.line);
     }
 
     public void greet() {
         System.out.println(Duke.line);
-        System.out.println("Hello! I'm " + this.name);
-        System.out.println("What can I do for you?");
+        System.out.println("    Hello! I'm " + this.name);
+        System.out.println("    What can I do for you?");
         System.out.println(Duke.line);
+    }
+
+    public void echo() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        if (input.equals(this.bye)) {
+            scanner.close();
+            exit();
+        } else {
+            System.out.println(Duke.line);
+            System.out.println("    " + input);
+            System.out.println(Duke.line);
+            echo();
+        }
     }
 
     public static void main(String[] args) {
         Duke bot = new Duke("Kam_BOT");
         bot.greet();
-        bot.exit();
-
+        bot.echo();
     }
 }
