aa62e89f36ea34e9668e4b6e0cc098b49fc4ffea
maypfv <maythandarhinn03@gmail.com>
2023-08-21 21:44:29 +0800

    Delete: Duke file

---
+++ src/main/java/Blip.java
@@ -0,0 +1,21 @@
+public class Blip {
+/*    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+    }*/
+
+    public static void main(String[] args) {
+        String intro = "____________________________________________________________________________\n"
+                + "Hello! I'm Blip\n"
+                + "What can I do for you?\n";
+        String outro = "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________________________\n";
+        String separator = "____________________________________________________________________________\n";
+        System.out.println(intro + separator +  outro);
+    }
+
+}
