4da38ea9f2c0efcfc3378edf669802922c9f99b8
conradsoon <18553610+conradsoon@users.noreply.github.com>
2023-08-21 19:57:12 +0800

    Added echo command functionality, added some indentations for responses.

--- src/main/java/DeterministicParrot.java
+++ src/main/java/DeterministicParrot.java
@@ -1,10 +1,11 @@
+import java.util.Scanner;
 public class DeterministicParrot {
 
     /**
      * Prints a line
      */
     public static void printDash() {
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
     }
 
     /**
@@ -13,16 +14,32 @@ public class DeterministicParrot {
      */
     public static void greet() {
         printDash();
-        System.out.println("Hello! I'm DeterministicParrot");
-        System.out.println("What can I do for you?");
+        System.out.println("     " + "Hello! I'm DeterministicParrot");
+        System.out.println("     " +"What can I do for you?");
+        printDash();
+    }
+
+    public static void echo(String s) {
+        printDash();
+        System.out.println("     " + s);
         printDash();
     }
     public static void bye() {
-        System.out.println("Bye. Hope to see you again soon!");
+        printDash();
+        System.out.println("      " + "Bye. Hope to see you again soon!");
         printDash();
     }
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         greet();
+        while(true){
+            String input = s.nextLine();
+            if(input.equals("bye")){
+                break;
+            }
+            echo(input);
+        }
+
         bye();
     }
 }
