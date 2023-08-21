3a71f6f7866d792b3cfdd8d4cdaa23d96678ef8d
seantehds <seanteh2001@gmail.com>
2023-08-21 23:53:27 +0800

    Added in basic input recognition from user and displaying their input as an output on the screen

--- src/main/java/Emiya.java
+++ src/main/java/Emiya.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Emiya {
 
     public static void main(String[] args) {
@@ -8,9 +10,43 @@ public class Emiya {
         String exitMessage = "-----------------------------------------\n"
                 + "Bye. Hope to see you again soon!\n"
                 + "-----------------------------------------\n";
+        Scanner myScannerObj = new Scanner(System.in);
 
         System.out.println(welcomeMessage);
+
+        String userCommand = "";
+
+        while (true) {
+            // nextLine is blocking, so can have this here
+            userCommand = myScannerObj.nextLine();
+
+            if (userCommand.equals("bye")) {
+                break;
+            }
+
+            if (userCommand.equals("I am the bone of my sword")) {
+                System.out.println("-----------------------------------------\n"
+                        +"Unknown to death nor known to life"+"\n"
+                        + "-----------------------------------------\n");
+                continue;
+            }
+
+            if (userCommand.equals("dead")) {
+                System.out.println("-----------------------------------------\n"
+                        +"People die if they are killed!"+"\n"
+                        + "-----------------------------------------\n");
+                continue;
+            }
+
+            String outputMessage = "-----------------------------------------\n" +
+                    userCommand+"\n"
+                    +"-----------------------------------------\n";
+            System.out.println(outputMessage);
+
+        }
+
         System.out.println(exitMessage);
 
+        myScannerObj.close();
     }
 }
