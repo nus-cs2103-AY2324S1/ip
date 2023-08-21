8f55f340e45de94b32afa30cb70936ca142b09cc
newway1814 <e1088145@u.nus.edu>
2023-08-21 14:51:59 +0800

    Completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,15 +11,31 @@ public class Duke {
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        exit();
     }
 
     public static void exit() {
-        System.out.println(exitMessage + ". Hope to see you again soon !");
+        System.out.println("Bye. Hope to see you again soon !");
         System.out.println("____________________________________________________________");
     }
 
+    public static void typeMessage() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        while (!(str.equalsIgnoreCase(exitMessage))){
+            echo(str);
+            str = sc.nextLine();
+        }
+        exit();
+    }
+
+    public static void echo(String str) {
+        System.out.println("____________________________________________________________");
+        System.out.println(str);
+        System.out.println("____________________________________________________________");
+
+    }
     public static void main(String[] args) {
         welcomeMessage();
+        typeMessage();
     }
 }
