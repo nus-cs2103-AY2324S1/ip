f0d632ea0522eb0028e0a8fcc84b1b7ce5d49f6f
sushiyade <68842066+sushiyade@users.noreply.github.com>
2023-08-21 15:06:46 +0800

    level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,9 @@ public class Duke {
     public static void main(String[] args) {
         String EXIT_PHRASE = "bye";
         String LIST_PHRASE = "list";
-        String[] toDoList = new String[100];
+
+        int LIMIT = 100;
+        Task[] toDoList = new Task[LIMIT];
         int i = 0;
         System.out.println(
                 "Hello! I'm Sunacchi!\n" +
@@ -14,23 +16,48 @@ public class Duke {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 
         String userInput = myObj.next();
-        while (i < 100) {
+        while (i < LIMIT) {
             if (userInput.equals(EXIT_PHRASE)) {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
             }
+
             if (userInput.equals(LIST_PHRASE)) {
                 for (int j = 0; j < i; j++) {
-                    System.out.println(j + 1 + "." + " " + toDoList[j]);
+                    System.out.println(j + 1 + "." +
+                            "[" + toDoList[j].getStatusIcon() + "]" + " " +
+                            toDoList[j].getDescription());
+
                 }
                 userInput = myObj.next();
                 continue;
             }
+
             if (userInput.equals("")) {
                 userInput = myObj.next();
                 continue;
             }
-            toDoList[i] = userInput;
+
+            if (userInput.equals("mark")) {
+                userInput = myObj.next();
+                Task curr = toDoList[Integer.parseInt(userInput) - 1];
+                curr.mark();
+                System.out.println( "Nice! I've marked this task as done: \n" + "[X] " + curr.getDescription());
+                userInput = myObj.next();
+                continue;
+            }
+
+            if (userInput.equals("unmark")) {
+                userInput = myObj.next();
+                //check int
+                Task curr = toDoList[Integer.parseInt(userInput) - 1];
+                curr.unmark();
+                System.out.println( "OK, I've marked this task as not done yet: \n" + "[ ] " + curr.getDescription());
+                userInput = myObj.next();
+                continue;
+            }
+
+            toDoList[i] = new Task(userInput);
             System.out.println("added: " + userInput);
             i++;
             userInput = myObj.next();
