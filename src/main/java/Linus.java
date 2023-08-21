bc7fb6066924006720a3da5d714419751e3405f6
joeng03 <yinjoeng2003@gmail.com>
2023-08-21 08:51:14 +0800

    feat: rename, greet, exit

---
+++ src/main/java/Linus.java
@@ -0,0 +1,8 @@
+public class Linus {
+    public static void main(String[] args) {
+        String name = "LINUS";
+        System.out.println("Hello! I'm " + name + "\n"
+                            + "What can I do for you? \n"
+                            + "Bye. Hope to see you again soon! \n");
+    }
+}
