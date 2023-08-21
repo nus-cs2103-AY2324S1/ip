1e9af84bcb0288f1d2f6812a64da01145a7b3df7
LoMaply <linuslee71@gmail.com>
2023-08-21 23:55:50 +0800

    Add error handling for wrong user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,20 +5,20 @@ public class Duke {
     private static int taskTotal = 0;
 
     /**
-     * Formats chatbot output with borders, first line indentation and new line at the end
+     * Formats chatbot output with borders, first line indentation and new line character at the end
      * @param message message to be displayed to user
      */
-    public static void output(String message) {
+    private static void output(String message) {
         String line = "    ____________________________________________________________\n";
         String template = line + "     %s\n" + line;
         System.out.printf((template) + "%n", message);
     }
 
     /**
-     * Formats chatbot output for task added messages
+     * Formats chatbot output specifically for task added messages
      * @param task task added to list
      */
-    public static void newTaskOutput(Task task) {
+    private static void newTaskOutput(Task task) {
         String taskAddMessage = "I've added the following task for you:\n       %s\n     There are currently %d tasks in your list.";
         Duke.output(String.format(taskAddMessage, task.toString(), taskTotal));
     }
@@ -39,31 +39,67 @@ public class Duke {
 
             switch (splitInput[0]) {
                 case "mark":
-                    Task item1 = taskList[Integer.parseInt(splitInput[1]) - 1];
-                    item1.markAsDone();
-                    Duke.output("Good job on completing this task:\n       " + item1);
+                    try {
+                        int index = Integer.parseInt(splitInput[1]);
+                        Task item = taskList[index - 1];
+                        item.markAsDone();
+                        Duke.output("Good job on completing this task:\n       " + item);
+                    } catch (NumberFormatException e) {
+                        Duke.output("That ain't a number, ya silly");
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        Duke.output("I can't mark something that doesn't exist...");
+                    } catch (NullPointerException e) {
+                        Duke.output("You don't have that many tasks");
+                    }
                     break;
+
                 case "unmark":
-                    Task item2 = taskList[Integer.parseInt(splitInput[1]) - 1];
-                    item2.markNotDone();
-                    Duke.output("Unmarked task as requested:\n       " + item2);
+                    try {
+                        int index = Integer.parseInt(splitInput[1]);
+                        Task item = taskList[index - 1];
+                        item.markNotDone();
+                        Duke.output("Unmarked task as requested:\n       " + item);
+                    } catch (NumberFormatException e) {
+                        Duke.output("That ain't a number, ya silly");
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        Duke.output("I can't mark something that doesn't exist...");
+                    } catch (NullPointerException e) {
+                        Duke.output("You don't have that many tasks");
+                    }
                     break;
+
                 case "todo":
-                    Task todo = new Todo(splitInput[1]);
-                    taskList[taskTotal++] = todo;
-                    Duke.newTaskOutput(todo);
+                    // Checks for description before creating task
+                    if (splitInput.length != 2) {
+                        Duke.output("Wrong format, try \"todo [DESCRIPTION]\"");
+                    } else {
+                        Task todo = new Todo(splitInput[1]);
+                        taskList[taskTotal++] = todo;
+                        Duke.newTaskOutput(todo);
+                    }
                     break;
                 case "deadline":
-                    String[] deadVar = splitInput[1].split(" /by ", 2);
-                    Task deadline = new Deadline(deadVar[0], deadVar[1]);
-                    taskList[taskTotal++] = deadline;
-                    Duke.newTaskOutput(deadline);
+                    // ArrayIndexOutOfBounds only thrown when the string is not split -> /by not present
+                    try {
+                        String[] deadVar = splitInput[1].split(" /by ", 2);
+                        Task deadline = new Deadline(deadVar[0], deadVar[1]);
+                        taskList[taskTotal++] = deadline;
+                        Duke.newTaskOutput(deadline);
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        Duke.output("Wrong format, try \"deadline [DESCRIPTION] /by [TIME]\"");
+                    }
                     break;
                 case "event":
-                    String[] eventVar = splitInput[1].split(" /(from|to) ", 3);
-                    Task event = new Event(eventVar[0], eventVar[1], eventVar[2]);
-                    taskList[taskTotal++] = event;
-                    Duke.newTaskOutput(event);
+                    // Using only 1 split statement that splits by /(from|to) allows edge cases like "/to x /from x" combinations
+                    try {
+                        String[] eventVar = splitInput[1].split(" /from ", 2);
+                        String[] times = eventVar[1].split(" /to ", 2);
+                        Task event = new Event(eventVar[0], times[0], times[1]);
+                        taskList[taskTotal++] = event;
+                        Duke.newTaskOutput(event);
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        Duke.output("Wrong format, try \"event [DESCRIPTION] /from [START] /to [END]\"");
+                    }
                     break;
                 case "list":
                     StringBuilder allTasks = new StringBuilder("Here are your tasks:");
