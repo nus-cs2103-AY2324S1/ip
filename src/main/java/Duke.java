1911ac7ab30adaa33d94abe1c1ef153b97a4fc78
Nam Do Hyun <e0638876@u.nus.edu>
2023-08-21 13:32:57 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,10 @@
 public class Duke {
+    public static void greeting() {
+        System.out.println("Hello! I'm ekuD.");
+        System.out.println("What can I do for you?");
+        System.out.println("--------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +12,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greeting();
     }
 }
