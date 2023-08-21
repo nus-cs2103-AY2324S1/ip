a7a22c676000321f8c792b07c1a0288b72c4f7f3
GlendaChong <glendachong04@gmail.com>
2023-08-21 22:50:29 +0800

    Add and display tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -16,21 +17,44 @@ public class Duke {
 
     public static void printCommand(String command) {
         System.out.println(horizontalLine
-                            + "      " + command + "\n"
+                            + "      " + "added: " + command + "\n"
                             + horizontalLine);
+    }
+
+    public static void printTasks(ArrayList tasks) {
+        System.out.print(horizontalLine);
+
+        if (tasks.isEmpty()) {
+            // Case where there is no tasks to be displayed
+            System.out.println("      " + "No tasks added. ");
+
+        } else {
+            int taskNumber = 1;
+            for (Object task: tasks) {
+                System.out.println("      " + taskNumber + ". " + task);
+                taskNumber++;
+            }
+        }
+
+        System.out.println(horizontalLine);
     }
 
     public static void main(String[] args) {
         greet();
-
         Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
+        ArrayList<String> tasks = new ArrayList<>();
 
-        while (!command.equals("bye")) {
-            printCommand(command);
-            command = scanner.nextLine();
+        while(true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                exit();
+                return;
+            } else if (command.equals("list")) {
+                printTasks(tasks);
+            } else {
+                printCommand(command);
+                tasks.add(command);
+            }
         }
-
-        exit();
     }
 }
