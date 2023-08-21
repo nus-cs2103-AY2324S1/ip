4a2270a7ba315a1ea8b2f8385f8715f8bafbd5ac
Jason Baek <bkjwjason@gmail.com>
2023-08-21 20:10:17 +0800

    add echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,21 @@
-
+import java.util.Scanner;
 
 public class Duke {
+    private static void echo(String input) {
+        System.out.println(input);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello! I'm BaekBot. \nWhat can I do for you? \nBye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! I'm BaekBot. \nWhat can I do for you?");
+        while (true) {
+            String echoInput = scanner.nextLine();
+            if (echoInput.equals("bye")) {
+                break;
+            } else {
+                echo(echoInput);
+            }
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
