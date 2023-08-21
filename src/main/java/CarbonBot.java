1b326cdaf66de6d849c37453e8652de17316b2b7
aexolate <20797128+aexolate@users.noreply.github.com>
2023-08-21 21:31:33 +0800

    Rename, Greet, Exit

---
+++ src/main/java/CarbonBot.java
@@ -0,0 +1,11 @@
+public class CarbonBot {
+    private static String DIVIDER = "____________________________________________________________";
+    public static void main(String[] args) {
+        System.out.println(DIVIDER);
+        System.out.println("Hello! I'm CarbonBot");
+        System.out.println("What can I do for you?");
+        System.out.println(DIVIDER);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
+    }
+}
