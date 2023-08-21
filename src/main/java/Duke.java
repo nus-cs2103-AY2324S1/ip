4bd90f7552cea0e9b8ba58c4075cedab06f6fc61
kiwibang <kerway.tan@gmail.com>
2023-08-21 21:15:31 +0800

    Add Level-2 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,32 +9,87 @@ import java.util.Scanner;
  * @author Tan Kerway
  */
 public class Duke {
+    // array to store the user's items
+    private final String[] items;
+    // pointer that will always point to the last element of the array
+    private int i;
+
+    /**
+     * Constructs an instance of a chatbot class.
+     *
+     * @author Tan Kerway
+     */
+    public Duke() {
+        this.items = new String[100];
+        this.i = -1;
+    }
+
     /**
      * When called, the chatbot will greet the user.
      *
      * @author Tan Kerway
      */
-    private static void greet() {
+    private void greet() {
         System.out.println("------------------------------------------------------------------------");
         System.out.println("Hello! I'm BongoCatBot!\nWhat can I do for you?");
+        System.out.println("------------------------------------------------------------------------");
     }
 
     /**
-     * When called, awaits user input and echoes the input to the console.
-     * If the input is bye, the function calls the sayGoodBye() function
-     * and terminates.
+     * Prints the current list of tasks.
      *
      * @author Tan Kerway
      */
-    private static void echoCommand() {
+    void listAllTasks() {
+        int index = -1;
+        while(this.items[++index] != null) {
+            System.out.println((index + 1) + ". " + this.items[index]);
+        }
+    }
+
+    /**
+     * Adds a task to the local task list.
+     *
+     * @param task the task to be added to the current list of tasks
+     * @author Tan Kerway
+     */
+    private void addTask(String task) {
+        this.items[++this.i] = task;
+    }
+
+    /**
+     * Echos the task that was added to the task list to
+     * the console.
+     *
+     * @author Tan Kerway
+     * @param task the task to be echoed to the console
+     */
+    void echoTask(String task) {
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("added: " + task);
+        System.out.println("------------------------------------------------------------------------");
+    }
+
+    /**
+     * When called, awaits user input. If the input is "list", the tasks
+     * that the user has added to the list so far is printed to the console.
+     * If the input is "bye" the program will terminate. For other inputs,
+     * the input will be added and saved as a task.
+     *
+     * @author Tan Kerway
+     */
+    private void handleAddTask() {
         Scanner sc = new Scanner(System.in);
         String input = "";
         while (true) {
             input = sc.nextLine();
+            if (input.equals("list")) {
+                listAllTasks();
+                continue;
+            }
             if (input.equals("bye")) { return; }
-            System.out.println("------------------------------------------------------------------------");
-            System.out.println(input);
-            System.out.println("------------------------------------------------------------------------");
+            addTask(input);
+            echoTask(input);
         }
     }
 
@@ -43,7 +98,7 @@ public class Duke {
      *
      * @author Tan Kerway
      */
-    private static void sayGoodBye() {
+    private void sayGoodBye() {
         String logo = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⡷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
                 "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠋⠈⠻⣮⣳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
                 "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⡿⠋⠀⠀⠀⠀⠙⣿⣿⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
@@ -71,6 +126,18 @@ public class Duke {
     }
 
     /**
+     * Function that starts a chat with the user
+     *
+     * @author Kerway
+     *
+     */
+    private void initiateChat() {
+        greet();
+        handleAddTask();
+        sayGoodBye();
+    }
+
+    /**
      * This main function, when run, will cause the Chat Bot
      * to greet the user, and then exit.
      *
@@ -78,8 +145,7 @@ public class Duke {
      * @param args pointer to some array of command-line arguments
      */
     public static void main(String[] args) {
-        greet();
-        echoCommand();
-        sayGoodBye();
+        Duke dukeInstance = new Duke();
+        dukeInstance.initiateChat();
     }
 }
