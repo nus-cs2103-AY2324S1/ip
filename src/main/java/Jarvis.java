07ec1b4cef0792074a40c0a5eed6c84d57dcdb6e
Ting Yong Shen Amos <amostingyongquan@gmail.com>
2023-08-21 16:55:02 +0800

    Level 1. Echo Added logic to echo user commands. If user enters anything other than bye, echo. If user enters bye, reply with goodbye message.

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Jarvis {
     public static void main(String[] args) {
         String name = "Jarvis";
@@ -16,10 +18,24 @@ public class Jarvis {
         String signOff = "Good bye Sir!";
 
         System.out.println(line);
-        System.out.println(greeting + name);
+        System.out.println(greeting + name + "!");
         System.out.println(question);
         System.out.println(line);
-        System.out.println(signOff);
-        System.out.println(line);
+
+        while (true) { // any other command other than bye
+            Scanner userInput = new Scanner(System.in);
+            String request = userInput.nextLine();
+
+            if (!request.equals("bye")) { // if "bye is not entered"
+                System.out.println(line);
+                System.out.println(request);
+                System.out.println(line);
+            } else {
+                System.out.println(line); // if "bye" is entered
+                System.out.println(signOff);
+                System.out.println(line);
+                break;
+            }
+        }
     }
 }
