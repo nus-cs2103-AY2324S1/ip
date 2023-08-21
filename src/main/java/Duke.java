b46faf6e0d755b86dd1468d59ce8534ae83c6f42
Gabriel4357 <gabrielseethor.jh@gmail.com>
2023-08-21 16:05:04 +0800

    changed name, and removed tag

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
 public class Duke {
+    private String name = "Alfred";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello from\n");
     }
 }
