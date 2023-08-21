f8f088e950953d45d101db5a259b55daa6c575be
Gabriel4357 <gabrielseethor.jh@gmail.com>
2023-08-21 16:41:12 +0800

    improved skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     private static String name = "Alfred";
 
@@ -5,16 +6,43 @@ public class Duke {
         System.out.println("____________________________________________________________");
     }
 
-    public static String  getName() {
+    public static String getName() {
         return Duke.name;
     }
 
     public static void main(String[] args) {
+        Scanner scanIn = new Scanner(System.in);
+
         println();
         System.out.println("Hello I'm " + getName());
         System.out.println("What can I do for you?");
         println();
-        System.out.println("Bye. Hope to see you again soon!");
-        println();
+
+
+
+        while (true) {
+
+            String command = scanIn.nextLine();
+            switch (command){
+                case "bye":
+
+                    println();
+                    System.out.println("Bye. Hope to see you again soon!");
+                    println();
+                    break;
+
+                default:
+                    println();
+                    System.out.println(command);
+                    println();
+                    continue;
+
+            }
+            break;
+
+        }
+
     }
 }
+
+
