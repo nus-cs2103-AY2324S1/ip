94e1fb634b7ab0b6838f721722c541564d1b77fc
ktzy0305 <kevintoh0305@gmail.com>
2023-08-21 12:20:44 +0800

    add task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,49 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        setDescription(description);
+        setDone(false);
+    }
+
+    /**
+     * Returns the description of a given task
+     * @return description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Returns the completion status of a given task
+     * @return isDone
+     */
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    /**
+     * Mark completed task with "X"
+     * @return statusIcon
+     */
+    public String getStatusIcon() {
+        return (isDone() ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        setDone(true);
+    }
+
+    public void unmark() {
+        setDone(false);
+    }
+}
