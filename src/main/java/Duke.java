3a7f232f535cb34a573f8e1c6ddeaa8c48351528
GSgiansen <giansenrockz@gmail.com>
2023-08-21 14:25:03 +0800

    Completed Level-2 features where users can add tasks and view task list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,42 @@
 import java.util.Scanner;
 public class Duke {
+    String[] Tasklist;
+    int counter;
+    String lines;
+
+    public Duke() {
+        this.Tasklist = new String[100];
+        this.counter = 0;
+        this.lines = "______________________________";
+    }
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
 //                + "| | | | | | | |/ / _ \\\n"
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo)
         String lines = "______________________________";
         Scanner scan = new Scanner(System.in);
+        Duke meowBot = new Duke();
 
         System.out.println(lines);
         System.out.println("Hello! I'm MeowBot!");
         System.out.println("What can I do for you ?");
+        System.out.println(lines);
 
         String command = scan.nextLine();
 
         while (!command.equals("bye")) {
-            System.out.println(lines);
-            System.out.println(command);
-            System.out.println(lines);
+            if (command.equals("list")) {
+                System.out.println(lines);
+                for (int i = 0; i < meowBot.counter; i++) {
+                    System.out.println(i + ". " + meowBot.Tasklist[i]);
+                }
+                System.out.println(lines);
+            }
+            else{
+                meowBot.addTask(command);
+            }
             command = scan.nextLine();
 
         }
@@ -27,5 +44,13 @@ public class Duke {
 
         System.out.println(lines);
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    void addTask(String task){
+        this.Tasklist[counter] = task;
+        counter ++;
+        System.out.println(lines);
+        System.out.println("added: " + task);
+        System.out.println(lines);
     }
 }
