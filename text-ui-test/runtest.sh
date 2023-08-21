021c7f5a5d54e866f130e2806d63efb7d815aa4b
conradsoon <18553610+conradsoon@users.noreply.github.com>
2023-08-21 22:58:26 +0800

    Added automated testing for MacOS.

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 # create bin directory if it doesn't exist
 if [ ! -d "../bin" ]
@@ -20,7 +20,7 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin DeterministicParrot < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
