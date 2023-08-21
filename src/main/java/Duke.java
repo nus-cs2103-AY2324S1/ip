4770bfde5f1cbdb01d6c988d044179a9e85194e4
kiwibang <kerway.tan@gmail.com>
2023-08-21 20:34:21 +0800

    Add Level-1 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,49 @@
+import java.util.Scanner;
+
 /**
- * This class encapsulates the skeleton of the Duke chatbot.
+ * This class encapsulates the skeleton of the BongoCatBot.
+ * Credits for Bongo Cat:
+ * <a href="https://twitter.com/StrayRogue">...</a>
+ * <a href="https://emojicombos.com/dot-art-editor#">...</a>
  *
  * @author Tan Kerway
  */
 public class Duke {
     /**
-     * This main function, when run, will cause the Chat Bot r
-     * to greet the user, and then exit
-     * Credits for Bongo Cat:
-     * <a href="https://twitter.com/StrayRogue">...</a>
-     * <a href="https://emojicombos.com/dot-art-editor#">...</a>
+     * When called, the chatbot will greet the user.
      *
      * @author Tan Kerway
-     * @param args pointer to some string array
      */
-    public static void main(String[] args) {
+    private static void greet() {
         System.out.println("------------------------------------------------------------------------");
         System.out.println("Hello! I'm BongoCatBot!\nWhat can I do for you?");
+    }
+
+    /**
+     * When called, awaits user input and echoes the input to the console.
+     * If the input is bye, the function calls the sayGoodBye() function
+     * and terminates.
+     *
+     * @author Tan Kerway
+     */
+    private static void echoCommand() {
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        while (true) {
+            input = sc.nextLine();
+            if (input.equals("bye")) { return; }
+            System.out.println("------------------------------------------------------------------------");
+            System.out.println(input);
+            System.out.println("------------------------------------------------------------------------");
+        }
+    }
+
+    /**
+     * When called, will bid the user farewell.
+     *
+     * @author Tan Kerway
+     */
+    private static void sayGoodBye() {
         String logo = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⡷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
                 "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠋⠈⠻⣮⣳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
                 "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⡿⠋⠀⠀⠀⠀⠙⣿⣿⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
@@ -41,5 +68,18 @@ public class Duke {
         System.out.println("------------------------------------------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("------------------------------------------------------------------------");
+    }
+
+    /**
+     * This main function, when run, will cause the Chat Bot
+     * to greet the user, and then exit.
+     *
+     * @author Tan Kerway
+     * @param args pointer to some array of command-line arguments
+     */
+    public static void main(String[] args) {
+        greet();
+        echoCommand();
+        sayGoodBye();
     }
 }
