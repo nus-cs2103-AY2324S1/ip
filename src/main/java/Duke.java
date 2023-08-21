c08b69423babc9fdb3a3eb7c4e28d2cb9c04352b
GSgiansen <giansenrockz@gmail.com>
2023-08-21 13:59:23 +0800

    Completed Level-1 features that Echos whatever the user inputs and exits when bye is executed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -7,9 +8,23 @@ public class Duke {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo)
         String lines = "______________________________";
+        Scanner scan = new Scanner(System.in);
+
         System.out.println(lines);
         System.out.println("Hello! I'm MeowBot!");
         System.out.println("What can I do for you ?");
+
+        String command = scan.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println(lines);
+            System.out.println(command);
+            System.out.println(lines);
+            command = scan.nextLine();
+
+        }
+
+
         System.out.println(lines);
         System.out.println("Bye. Hope to see you again soon!");
     }
