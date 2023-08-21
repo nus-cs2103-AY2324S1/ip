e2912723acb112579e1ee36191fd160b716c4b90
wui-hong <khoowuihong@yahoo.com.sg>
2023-08-21 11:41:10 +0800

    Complete Level 0

---
+++ src/main/java/Hong.java
@@ -0,0 +1,7 @@
+public class Hong {
+    public static void main(String[] args) {
+
+        System.out.println("Hello! I'm Hong\nWhat can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
