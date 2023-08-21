3eea79e9fbfd0407c9124059ceb57d15fa0ddd4f
chiayunrong <chiayunrong@outlook.com>
2023-08-21 13:19:08 +0800

    Implement echoing commands entered by user

--- src/main/java/Alpha.java
+++ src/main/java/Alpha.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class Alpha {
 
     public static void main(String[] args) {
+
         System.out.println("______________________________");
         System.out.println("Hello! I'm Alpha.");
         System.out.println("What can I do for you?");
         System.out.println("______________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("______________________________");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String userInput = sc.nextLine();
+            System.out.println("______________________________");
+
+            if (!userInput.equals("bye")) {
+                System.out.println(userInput);
+            } else {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("______________________________");
+                return;
+            }
+            System.out.println("______________________________");
+
+        }
     }
 }
