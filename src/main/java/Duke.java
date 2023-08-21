19ce3ef99089c8f6952b1627eaed728d163f0f2e
tanshiyu <tanshiyu1999@gmail.com>
2023-08-21 12:49:11 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class Duke {
 
 
@@ -37,47 +39,81 @@ public class Duke {
         printHorizontalLine();
     }
 
+
     public static void toDo() {
         ArrayList<Task> list = new ArrayList<Task>();
         Scanner sc = new Scanner(System.in);
+        Pattern listRegex = Pattern.compile("^list", Pattern.CASE_INSENSITIVE);
+        Pattern markRegex = Pattern.compile("^mark ", Pattern.CASE_INSENSITIVE);
+        Pattern unmarkRegex = Pattern.compile("^unmark ", Pattern.CASE_INSENSITIVE);
+        Pattern deadlineRegex = Pattern.compile("^deadline ", Pattern.CASE_INSENSITIVE);
+        Pattern todoRegex = Pattern.compile("^todo ", Pattern.CASE_INSENSITIVE);
+        Pattern eventRegex = Pattern.compile("^event", Pattern.CASE_INSENSITIVE);
         while(true) {
             String command = sc.nextLine();
             if (command.equals("bye")) {
                 return;
-            } else if (command.equals("list")) {
+            } else if (listRegex.matcher(command).find()) {
                 printHorizontalLine();
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < list.size(); i++) {
                     int printIndex = i + 1;
-                    System.out.println( printIndex + ".[" + list.get(i).getStatusIcon() + "] " + list.get(i).getDescription() );
+                    System.out.println(printIndex + "." + list.get(i).toString());
                 }
                 printHorizontalLine();
                 continue;
-            } else if (command.substring(0,5).equals("mark ")) {
+            } else if (markRegex.matcher(command).find()) {
                 int curr = Integer.parseInt(command.substring(5)) - 1;
                 Task currTask = list.get(curr);
-                if (currTask.isDone) {
-                    continue;
-                } else {
-                    printHorizontalLine();
-                    currTask.isDone = true;
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println( "[" + currTask.getStatusIcon() + "] " + currTask.getDescription() );
-                    printHorizontalLine();
-
-                }
+                currTask.markDone();
                 continue;
-            } else if (command.substring(0,7).equals("unmark ")) {
+            } else if (unmarkRegex.matcher(command).find()) {
                 int curr = Integer.parseInt(command.substring(7)) - 1;
                 Task currTask = list.get(curr);
-                if (currTask.isDone) {
-                    printHorizontalLine();
-                    currTask.isDone = false;
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println( "[" + currTask.getStatusIcon() + "] " + currTask.getDescription() );
-                    printHorizontalLine();
-                }
+                currTask.markUndone();
+                continue;
+            } else if (deadlineRegex.matcher(command).find()) {
+                printHorizontalLine();
+                System.out.println("Got it, I've added this deadline.");
+                String currStr = command.substring(9);
+                int dateIndex = currStr.indexOf("/");
+                String date = currStr.substring(dateIndex + 4);
+                String description = currStr.substring(0, dateIndex);
+                Task deadlineTask = new Deadline(description, date);
+                list.add(deadlineTask);
+                System.out.println(deadlineTask.toString());
+                System.out.println("Now you have " + list.size() + " tasks in the list." );
+                printHorizontalLine();
+                continue;
+            } else if (todoRegex.matcher(command).find()) {
+                printHorizontalLine();
+                System.out.println("Got it, I've added this task.");
+                String description = command.substring(5);
+                Task todoTask = new Todo(description);
+                list.add(todoTask);
+                System.out.println(todoTask.toString());
+                System.out.println("Now you have " + list.size() + " tasks in the list." );
+                printHorizontalLine();
+                continue;
+            } else if (eventRegex.matcher(command).find()) {
+                printHorizontalLine();
+                System.out.println("Got it, I've added this event.");
+                String currStr = command.substring(6);
+                int fromIndex = currStr.indexOf("/");
+                String description = currStr.substring(0, fromIndex);
+                currStr = currStr.substring(fromIndex + 6);
+                int toIndex = currStr.indexOf("/");
+                String from = currStr.substring(0, toIndex);
+                String to = currStr.substring(toIndex + 4);
+                Task eventTask = new Event(description, from, to);
+                list.add(eventTask);
+                System.out.println(eventTask.toString());
+                System.out.println("Now you have " + list.size() + " tasks in the list." );
+                printHorizontalLine();
                 continue;
             }
+
+
             list.add(new Task(command));
             printHorizontalLine();
             System.out.println("added: " + command);
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,12 +7,50 @@ public class Task {
         this.isDone = false;
     }
 
+    public Task() {
+        this.description = "";
+        this.isDone = false;
+    }
+
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
     public String getDescription() {
         return this.description;
+    }
+
+    public void printHorizontalLine() {
+
+        for (int i = 0; i < 50; i++) {
+            System.out.print("_");
+        }
+        System.out.println("");
+    }
+
+    public void markDone() {
+        if (isDone) {
+            return;
+        } else {
+            printHorizontalLine();
+            this.isDone = true;
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println( "[" + this.getStatusIcon() + "] " + this.getDescription() );
+            printHorizontalLine();
+        }
+    }
+
+    public void markUndone() {
+        printHorizontalLine();
+        this.isDone = false;
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println( "[" + this.getStatusIcon() + "] " + this.getDescription() );
+        printHorizontalLine();
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.getDescription();
     }
 
 }
