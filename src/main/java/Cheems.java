de46dfaca761a7a30800a3531b6c01db40c7eeae
hejin <hj827458@gmail.com>
2023-08-21 19:07:12 +0800

    Customised own chatbot

---
+++ src/main/java/Cheems.java
@@ -0,0 +1,8 @@
+public class Cheems {
+    public static void main(String[] args) {
+        String lineBreak = "______________________________________________________________________";
+        String hello = "Hey! I'm Cheems:) Want to get some fries on the pier?";
+        String bye = "Bye:( Let's get the fries next time.";
+        System.out.println(lineBreak + "\n" + hello + "\n" + lineBreak + "\n" + bye + "\n" + lineBreak);
+    }
+}
