de16bd9439abae0ef6705709f9ac798b9396e8ab
chiayunrong <chiayunrong@outlook.com>
2023-08-21 13:05:36 +0800

    Rename chatbot and implement initial skeletal version

---
+++ src/main/java/Alpha.java
@@ -0,0 +1,11 @@
+public class Alpha {
+
+    public static void main(String[] args) {
+        System.out.println("______________________________");
+        System.out.println("Hello! I'm Alpha.");
+        System.out.println("What can I do for you?");
+        System.out.println("______________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("______________________________");
+    }
+}
