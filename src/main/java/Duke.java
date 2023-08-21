75bfcd8d9042f1cd0b8a9ec2480e7d1db6a2f160
alvinlim277 <alvinlim9815@gmail.com>
2023-08-21 21:38:44 +0800

    Add echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,38 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static String name = "Dook";
+
     public static void main(String[] args) {
+
         GreetUser();
-        BidFarewell();
+        Scanner sc = new Scanner(System.in);
+        String input;
+        while (true) {
+            input = sc.next();
+            if (Objects.equals(input, "bye")) {
+                BidFarewell();
+                break;
+            } else {
+                PrintMessage(input);
+            }
+        }
     }
 
     private static void GreetUser() {
-        System.out.println(String.format("%s here.\nWhat can I do for you?", name));
+        PrintMessage(String.format("%s here.\nWhat can I do for you?", name));
+    }
+
+    public static void PrintMessage(String msg) {
+        PrintDivider();
+        System.out.println(msg);
         PrintDivider();
     }
     public static void PrintDivider() {
         System.out.println("_______________________________________");
     }
     private static void BidFarewell() {
-        System.out.println("Goodbye.");
-        PrintDivider();
+        PrintMessage("Goodbye.");
     }
 }
