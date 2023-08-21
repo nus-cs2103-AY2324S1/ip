b5aa68d63fdd4770693967c233c3e10c95b0d034
Jun Yu <lowjy28@gmail.com>
2023-08-22 01:36:20 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,15 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String line = "----------------------------------------------";
+        String greeting = " Hello! I'm Somebodyhaha\n" +
+                " What can I do for you?";
+        String exiting = " Bye. Hope to See you again soon!";
         System.out.println("Hello from\n" + logo);
+        System.out.println(line);
+        System.out.println(greeting);
+        System.out.println(line);
+        System.out.println(exiting);
+        System.out.println(line);
     }
 }
