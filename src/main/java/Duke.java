cfd8af7ff0e45b880113055d18b3e1f49308e821
LWZ19 <e0985886@u.nus.edu>
2023-08-21 18:41:54 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     private static final String NAME = "404";
+    private static final String SPACE = "     ";
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         printLine();
-        System.out.println(" Hello! I'm " + NAME);
-        System.out.println(" What can I do for you?");
+        String greeting = String.format("%sHello! I'm %s%n%sWhat can I do for you?",
+                                        SPACE, NAME, SPACE);
+        System.out.println(greeting);
         printLine();
-        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println();
+        String commands = sc.nextLine();
+        while (!commands.equals("bye")) {
+            printLine();
+            System.out.printf("%s%s%n", SPACE, commands);
+            printLine();
+            System.out.println();
+            commands = sc.nextLine();
+        }
         printLine();
+        System.out.printf("%sBye. Hope to see you again soon!%n", SPACE);
+        printLine();
+        sc.close();
     }
 
     private static void printLine() {
-        String line =  "____________________________________________________________";
+        String line =  "    ____________________________________________________________";
         System.out.println(line);
     }
 }
