9b0ab0a314cab2af37375cc60c4c71ddbe59c9fc
JasonLCY-Temp <e0407197@u.nus.edu>
2023-08-21 15:10:25 +0800

    Jeeves class, main method: Update to be responsive to user input
    The program only starts and ends instantly.
    
    There is a need for it to be able to recognize user input and respond accordingly.
    
    Let's add the ability to read and recognize user input.
    Specifically, Jeeves can recognize the 'bye' command and terminate appropriately.
    All other commands will simply be echoed back.

--- src/main/java/Jeeves.java
+++ src/main/java/Jeeves.java
@@ -1,12 +1,35 @@
+import java.util.Scanner;
+
 public class Jeeves {
+
     /**
      * Main process.
-     * Greets the user and says goodbye.
+     * Greets the user and waits for user input.
      *
      * @param args Optional command line arguments.
      */
     public static void main (String[] args) {
         System.out.println("Greetings, Master. Jeeves at your service");
-        System.out.println("I bid you farewell Master");
+        System.out.println("How may I serve you today?\n");
+
+        // Waits for user input and process it accordingly
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            // Reads the user input
+            String currentCommand = sc.nextLine();
+            // Performs a different action depending on the input received
+            // By default the program always waits for another command unless
+            // the termination command is received.
+            switch (currentCommand) {
+            case "bye":
+                // Displays the farewell message and terminates the application
+                System.out.println("I bid you farewell, Master");
+                System.exit(0);
+            default :
+                // By default, echo the command then wait for the next command
+                System.out.println(currentCommand + "\n");
+                break;
+            }
+        }
     }
 }
