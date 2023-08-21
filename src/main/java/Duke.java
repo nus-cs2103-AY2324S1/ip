0d68dfef1b2065d7672fc28c28e946407f154350
Ruizhi2001 <70838461+Ruizhi2001@users.noreply.github.com>
2023-08-22 00:57:37 +0800

    Level-1: Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String greet = "____________________________________________________________\n" +
                 " Hello! I'm Ruiz\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
+                "____________________________________________________________\n";
         System.out.println(greet);
+        Scanner inputObj = new Scanner(System.in);
+        while(true){
+            String input = inputObj.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(
+                        "____________________________________________________________\n" +
+                        "Bye! Comeback soon!\n" +
+                        "____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________\n" +
+                    input +
+                    "\n" +
+                    "____________________________________________________________\n");
+        }
     }
 }
