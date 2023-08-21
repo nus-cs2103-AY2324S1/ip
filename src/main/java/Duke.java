826fc362666f8cced54d7cc999b69aabfdfe6d8e
kurt <kurtyjlee@gmail.com>
2023-08-21 14:10:47 +0800

    level-2, Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,42 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    private static ArrayList<String> items = new ArrayList<>();
+    private static String bar = "――――――――――――――――――――――――――――――――――――――――――";
+
     public static void greetUser() {
-        String greeting = "――――――――――――――――――――――――――――――――――――――――――\n"
-                + "Hello! I'm CringeBot\n"
-                + "What can I do for you?\n"
-                + "――――――――――――――――――――――――――――――――――――――――――";
-        System.out.println(greeting);
+        String greeting = "Hello! I'm CringeBot\n"
+                + "What can I do for you?";
+        String wrappedGreeting = wrapWord(greeting);
+        System.out.println(wrappedGreeting);
     }
 
     public static void bidFarewel() {
-        String farewell = "――――――――――――――――――――――――――――――――――――――――――\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "――――――――――――――――――――――――――――――――――――――――――";
+        String farewell = wrapWord("Bye. Hope to see you again soon!");
         System.out.println(farewell);
     }
 
     public static void sayWord(String word) {
-        String wrappedWord = "――――――――――――――――――――――――――――――――――――――――――\n"
-                + word
-                + "\n"
-                + "――――――――――――――――――――――――――――――――――――――――――";
+        String wrappedWord = wrapWord(word);
         System.out.println(wrappedWord);
+    }
+
+    public static String wrapWord(String word) {
+        return String.format("%s\n%s\n%s", Duke.bar, word, Duke.bar);
+    }
+
+    public static void addItem(String item) {
+        Duke.items.add(item);
+        sayWord("added:" + item);
+    }
+
+    public static void printItems() {
+        System.out.println(Duke.bar);
+        for (int i = 0; i < Duke.items.size(); i++) {
+            System.out.println(String.format("%d. %s", i + 1, Duke.items.get(i)));
+        }
+        System.out.println(Duke.bar);
     }
 
     public static void main(String[] args) {
@@ -37,8 +52,11 @@ public class Duke {
             String nextLine = scanObj.nextLine();
             if (nextLine.equals("bye")) {
                 break;
+            } else if (nextLine.equals("list")) {
+                printItems();
+            } else {
+                addItem(nextLine);
             }
-            sayWord(nextLine);
         }
         bidFarewel();
     }
