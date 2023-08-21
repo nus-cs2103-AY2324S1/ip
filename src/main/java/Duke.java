702b16df0826014e34cc9c102182a11e4f8fc986
ryanongwx <ryanongwx@gmail.com>
2023-08-22 00:50:55 +0800

    Level-0 Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = "____________________________________________________________\n"
+                + "Hello! I'm yourChatBot\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n";
+        System.out.println(greeting);
+
+        try {
+            // Future Inputs
+        } finally {
+            System.out.println("Bye. Hope to see you again soon!\n"
+                    + "____________________________________________________________");
+        }
     }
 }
