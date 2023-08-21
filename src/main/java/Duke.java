b3cd12de7d1667992b04662829a860d2bbdf60d2
jonasongg <jonasong2@gmail.com>
2023-08-21 21:34:59 +0800

    add scanner for entering commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String greeting = "____________________________________________________________\n" +
+        String greeting =
+                "____________________________________________________________\n" +
                 " Hello! I'm Pixel\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
         System.out.println(greeting);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
