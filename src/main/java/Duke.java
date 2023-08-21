ee3829cce0135e0b5430aaadcffb3910386c1b53
cmHuang777 <e0934116@u.nus.edu>
2023-08-21 10:59:12 +0800

    Added function to store text and list them

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,45 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     private static String CHAT_BOT_NAME = "Genos";
+    private ArrayList<String> list;
+
+    public Duke() {
+        this.list = new ArrayList<String>();
+    }
 
     private static void greet() {
         System.out.println("Hello I'm " + Duke.CHAT_BOT_NAME);
         System.out.println("What can I do for you?");
-        System.out.println("(Please type your command below, I will repeat what you said)");
+        System.out.println("Please type your command below, I will store what you said");
+        System.out.println("Usage: \"list\" to see the list of text stored, \"bye\" to exit");
     }
 
-    private static void converse() {
+    private void converse() {
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
         while (! command.equals("bye")) {
-            System.out.println("    I heard you said: " + command);
+            if (command.equals("list")) {
+                this.listCommands();
+            } else {
+                System.out.println("    Added: " + command);
+                this.list.add(command);
+            }
             command = sc.nextLine();
         }
         System.out.println("    Goodbye, Hope to see you again soon.");
     }
 
+    private void listCommands() {
+        for (int i = 0; i < this.list.size(); i++) {
+            System.out.println("    " + (i + 1) + ". " + this.list.get(i));
+        }
+    }
+
     public static void main(String[] args) {
-        Duke.greet();
-        Duke.converse();
+        Duke bot = new Duke();
+        bot.greet();
+        bot.converse();
     }
 }
