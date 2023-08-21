b32328a84872fd5e6b226966317f9c468a7a948c
ChongWeiJie29 <chongweijie29@gmail.com>
2023-08-21 10:37:52 +0800

    Complete Level 0: Rename, Greet, Exit
    Rename chatbot.
    Add in feature for bot to greet and exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
 public class Duke {
+    private static String greetingString =  "Hello! I'm ChampionSOS\nWhat can I do for you?";
+    private static String exitString =  "Bye. Hope to see you again soon!";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(Duke.greetingString);
+        System.out.println(Duke.exitString);
     }
 }
