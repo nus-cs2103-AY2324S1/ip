15d1f84f689949eb01b82698f273ac38ac914657
waseem <mohammedwaseem1000@gmail.com>
2023-08-22 01:17:04 +0800

    Level 3. Mark as Done Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done.
    Created a new class called Task to represent tasks.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,7 +9,7 @@ public class Duke {
         System.out.println("____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         while (true) {
             String userInput = scanner.nextLine();
@@ -20,21 +20,50 @@ public class Duke {
 
             if ("list".equalsIgnoreCase(userInput)) {
                 System.out.println("____________________________________________________________");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i));
+                    Task task = tasks.get(i);
+                    System.out.println((i + 1) + ".[" + task.getStatusIcon() + "] " + task.description);
                 }
                 System.out.println("____________________________________________________________");
+            } else if (userInput.startsWith("mark ")) {
+                processTask(userInput, tasks, true);
+            } else if (userInput.startsWith("unmark ")) {
+                processTask(userInput, tasks, false);
             } else {
                 System.out.println("____________________________________________________________");
                 System.out.println("added: " + userInput);
                 System.out.println("____________________________________________________________");
-                tasks.add(userInput);
+                tasks.add(new Task(userInput));
             }
         }
 
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+    }
+
+    public static void processTask(String userInput, ArrayList<Task> tasks, boolean mark) {
+        int taskNumber;
+        try {
+            taskNumber = Integer.parseInt(userInput.split(" ")[1]);
+            Task task = tasks.get(taskNumber - 1);
+            if (mark) {
+                task.markAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println("Nice! I've marked this task as done:");
+            } else {
+                task.unmarkAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println("OK, I've marked this task as not done yet:");
+            }
+            System.out.println("  [" + task.getStatusIcon() + "] " + task.description);
+            System.out.println("____________________________________________________________");
+        } catch (NumberFormatException e) {
+            System.out.println("Please provide a valid task number.");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("That task doesn't exist!");
+        }
     }
 }
 
