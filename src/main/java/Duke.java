6c756f81a39596c1cd4990e7de457ae3d364ce92
Janssen <janssenlau88@gmail.com>
2023-08-21 17:57:25 +0800

    Implemented Automated Testing using Input/Output Redirection.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,7 @@ public class Duke {
     public static void main(String[] args) {
         ArrayList<Task> tasks = new ArrayList<>();
 
-        System.out.println("    Hello! \uD83C\uDF1F I'm Arona, your Virtual Assistant.\n    What can I do for you today? \uD83D\uDCAC\n");
+        System.out.println("    Hello! I'm Arona, your Virtual Assistant.\n    What can I do for you today?\n");
 
         Scanner scanner = new Scanner(System.in);
         String echo = scanner.nextLine().toLowerCase().trim();
@@ -14,7 +14,13 @@ public class Duke {
         while (!echo.equals("bye")) {
             if (echo.equals("list")) { // Lists all current tasks.
 
-                System.out.println("    " + (tasks.size() == 1 ? "Yay! \uD83C\uDF1F You only have one task:" : "Here are your current tasks \uD83D\uDE0A: "));
+                System.out.println("    " +
+                        (tasks.size() == 0
+                                ? "Great job! No tasks right now, enjoy your day!"
+                                : (tasks.size() == 1
+                                ? "Good news! You only have one task:"
+                                : "Hello! Here is your list of tasks:")));
+
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println("    " + (i + 1) + ". " + tasks.get(i));
                 }
@@ -25,16 +31,16 @@ public class Duke {
                 Task task = tasks.get(index);
                 task.mark();
 
-                System.out.println("    Awesome! \uD83C\uDF89 I've marked this task as done:");
-                System.out.println("    " + task + "\n");
+                System.out.println("    Awesome! I've marked this task as done:");
+                System.out.println("      " + task + "\n");
 
             } else if (echo.startsWith("unmark ")) { // Unmarks a task.
                 int index = Integer.valueOf(echo.split(" ")[1]) - 1;
                 Task task = tasks.get(index);
                 task.unmark();
 
-                System.out.println("    Sure thing! \uD83D\uDC4C I've marked this task as not done yet:");
-                System.out.println("    " + task + "\n");
+                System.out.println("    Sure thing! I've marked this task as not done yet:");
+                System.out.println("      " + task + "\n");
 
             } else if (echo.startsWith("todo ")) { // Adds a todo task.
                 String[] strings = echo.split(" ");
@@ -42,7 +48,7 @@ public class Duke {
                 Todo todo = new Todo(description);
                 tasks.add(todo);
 
-                System.out.println("    Got it! \uD83D\uDE04 I've added this task:");
+                System.out.println("    Got it! I've added this task:");
                 System.out.println("      " + todo);
                 System.out.println("    Now you have " + tasks.size() + (tasks.size() == 1 ? " task" : " tasks") + " in the list.\n");
 
@@ -63,7 +69,7 @@ public class Duke {
                 Deadline deadline = new Deadline(description, by);
                 tasks.add(deadline);
 
-                System.out.println("    Got it! \uD83D\uDE04 I've added this task:");
+                System.out.println("    Got it! I've added this task:");
                 System.out.println("      " + deadline);
                 System.out.println("    Now you have " + tasks.size() + (tasks.size() == 1 ? " task" : " tasks") + " in the list.\n");
 
@@ -93,13 +99,13 @@ public class Duke {
                 Event event = new Event(description, from, to);
                 tasks.add(event);
 
-                System.out.println("    Got it! \uD83D\uDE04 I've added this task:");
+                System.out.println("    Got it! I've added this task:");
                 System.out.println("      " + event);
                 System.out.println("    Now you have " + tasks.size() + (tasks.size() == 1 ? " task" : " tasks") + " in the list.\n");
             }
             echo = scanner.nextLine().toLowerCase().trim(); // Reads next line
         }
-        System.out.println("    Goodbye. See you soon! \uD83D\uDC3E"); // Exits the programme.
+        System.out.println("    Goodbye. See you soon!"); // Exits the programme.
     }
 }
 
