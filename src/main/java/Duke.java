4c9e6a0d765d2edc51e517b31c5c4ff1af33cb07
Darren159 <darren_low42@outlook.sg>
2023-08-21 19:51:06 +0800

    Add command echoing and exiting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
         String logo = "    ___    _   ___   ________  __      ____      __________  ____  _   __\n"
                 + "   /   |  / | / / | / / __ \\ \\/ /     / __ \\    /_  __/ __ \\/ __ \\/ | / /\n"
                 + "  / /| | /  |/ /  |/ / / / /\\  /_____/ / / /_____/ / / /_/ / / / /  |/ / \n"
                 + " / ___ |/ /|  / /|  / /_/ / / /_____/ /_/ /_____/ / / _, _/ /_/ / /|  /  \n"
                 + "/_/  |_/_/ |_/_/ |_/\\____/ /_/      \\____/     /_/ /_/ |_|\\____/_/ |_/   \n";
         String horizontalLine = "__________________________________________________________________________\n";
-        System.out.print(horizontalLine + logo + "Hello! I'm ANNOY-O-TRON!\nWhat can I do for you?\n"
+        String byeMessage = horizontalLine + "Bye. Hope to see you again soon!\n" + horizontalLine;
+
+        System.out.println(horizontalLine + logo + "Hello! I'm ANNOY-O-TRON!\nWhat can I do for you?\n"
                 + horizontalLine);
-        System.out.println("Bye. Hope to see you again soon!\n" + horizontalLine);
+
+        String userInput = myObj.nextLine();
+        while(!userInput.equals("bye")) {
+            System.out.println(horizontalLine + userInput + "\n" + horizontalLine);
+            userInput = myObj.nextLine();
+        }
+
+        System.out.println(byeMessage);
     }
 }
