5f41e1b68ff5ae3058e42795605756ef868f7c42
jellywaiyan <jellywaiyan@gmail.com>
2023-08-23 01:50:32 +0800

    Level-0

---
+++ src/main/java/Jelly.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Jelly {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm Jelly :)");
+        System.out.println("What can I do for you?");
+        while (sc.hasNext()) {
+            String temp = sc.next();
+            if (temp.equals("bye")) {
+                System.out.println("Bye, have a nice day!");
+                return;
+            } else {
+                System.out.println(temp);
+            }
+        }
+    }
+}
