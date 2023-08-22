5a897ac200136d5f3e9e3f3af5f7473f3ad0c276
BEATRICE <beatricechen2003@gmail.com>
2023-08-22 20:00:06 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,6 +9,20 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello! I'm froggie! ");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        boolean notEnd = true;
+
+        while (notEnd) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                notEnd = false;
+                System.out.println("Bye. Hope to see you again soon!");
+            } else {
+                System.out.println(input);
+            }
+        }
+
+        scanner.close();
     }
 }
