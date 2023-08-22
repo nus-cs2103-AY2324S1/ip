af757b8861ea020dc8e32efb3ca7d41b6f08619b
Qiming <huangqiming@r-30-101-25-172.comp.nus.edu.sg>
2023-08-22 13:02:30 +0800

    Update event loop to echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     private static final String NAME = "Joi";
+    private boolean isRuning = true;
+    private Scanner sc = new Scanner(System.in);
 
+    // the event loop
     private void run() {
         this.greeting();
+
+        while (this.isRuning) {
+            String input = this.getUserInput();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                this.isRuning = false;
+            }
+            else {
+                System.out.println(input + "\n");
+            }
+        }
     }
 
     private void greeting() {
         System.out.println("Hello! I'm " + NAME + "\nWhat can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private String getUserInput() {
+        return this.sc.nextLine();
     }
 
     public static void main(String[] args) {
