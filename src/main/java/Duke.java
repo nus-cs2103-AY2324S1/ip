ec70cf1a791561ec15700b932154bae5fce3d113
Sasmik23 <mikhilanand@gmail.com>
2023-08-22 23:40:34 +0800

    Level2 - add,list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,6 +6,8 @@ public class Duke {
 
         System.out.println("Hello! I'm Mikhil" + '\n' + "What can I do for you");
         boolean flag = true;
+        ArrayList<String> items = new ArrayList<>();
+
         while (flag == true) {
             Scanner userInput = new Scanner(System.in);
             String input = userInput.nextLine();
@@ -13,8 +15,15 @@ public class Duke {
             if (input.equals("bye")) {
                 flag = false;
                 System.out.println("Bye. Hope to see you again soon!");
-            } else {
-                System.out.println(input);
+            }
+            else if (input.equals("list")) {
+                for (int i = 0; i < items.size(); i++) {
+                    System.out.println(i + 1 + ". " + items.get(i));
+                }
+            }
+            else {
+                items.add(input);
+                System.out.println("added: " + input);
             }
         }
     }
