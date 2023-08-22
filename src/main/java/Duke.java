567284e90422eca47c68ba86f78136e9fb96c305
Gary <garylow2001@gmail.com>
2023-08-23 00:57:05 +0800

    Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,7 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "I'm Chewy\n" +
+                " What can I do for you?\n";
+        System.out.println("Hello! \n" + logo);
     }
 }
