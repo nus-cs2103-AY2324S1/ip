b0f6d7e80342a6d4b38072f72faabb6a8f695870
lihongguang00 <lihongguang00@gmail.com>
2023-08-22 19:54:46 +0800

    Level-0b
    Reformatted horizontal lines, added goodbye statement

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = "   _____ _    _          _____   _____ _____ _______ \n" +
+        String logo = "\n   _____ _    _          _____   _____ _____ _______ \n" +
                 "  / ____| |  | |   /\\   |  __ \\ / ____|  __ \\__   __|\n" +
                 " | |    | |__| |  /  \\  | |  | | |  __| |__) | | |   \n" +
                 " | |    |  __  | / /\\ \\ | |  | | | |_ |  ___/  | |   \n" +
                 " | |____| |  | |/ ____ \\| |__| | |__| | |      | |   \n" +
                 "  \\_____|_|  |_/_/    \\_\\_____/ \\_____|_|      |_|   \n";
 
-        String horizontal = "______________________________________________________\n";
+        String horizontal = "--------------------------------------------------------";
 
         System.out.println(horizontal + logo + horizontal);
-        System.out.println("Welcome to CHADGPT, What would you like to do today?");
+        System.out.println("Welcome to CHADGPT, What would you like to do today?\n" + horizontal +
+                "\nBye. Hope to see you again soon!\n" + horizontal);
     }
 }
