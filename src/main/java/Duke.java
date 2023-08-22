fa1b6b26209e2da2d755558b570436fb5e3d3626
darrentfy <>
2023-08-23 03:16:27 +0800

    added subclasses of the Task class, Deadline, Event and Todo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,114 +1,106 @@
-import java.text.NumberFormat;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         ArrayList<Task> tasks = new ArrayList<>();
-        String welcome = " ──────────────────────────────────────── \n"
-                + "  Hello! I'm Handsome \n"
-                + "  What can I do for you? \n"
-                + " ──────────────────────────────────────── ";
-        String goodbye = " ──────────────────────────────────────── \n"
-                + "  Bye. Hope to see you again soon! \n"
-                + " ──────────────────────────────────────── \n";
+        String welcome = " ────────────────────────────────────────\n"
+                + "  Hello! I'm Handsome\n"
+                + "  What can I do for you?\n"
+                + " ────────────────────────────────────────";
+        String goodbye = " ────────────────────────────────────────\n"
+                + "  Bye. Hope to see you again soon!\n"
+                + " ────────────────────────────────────────";
         System.out.println(welcome);
         while (true) {
-            String command = input.nextLine();
-            if (command.equals("bye")) {
+            String input = scan.nextLine();
+            if (input.equals("bye")) {
                 break;
             }
-            if (command.equals("list")) {
+            if (input.equals("list")) {
                 int index = 1;
-                System.out.println(" ──────────────────────────────────────── \n"
+                System.out.println(" ────────────────────────────────────────\n"
                         + "Here are the tasks in your list:");
                 for (Task task : tasks) {
-                    System.out.println(String.format("  %d. %s ", index, task.toString()));
+                    System.out.println(String.format(" %d. %s ", index, task.toString()));
                     index++;
                 }
-                System.out.println(" ──────────────────────────────────────── ");
-            } else if (command.startsWith("mark")) {
-                try {
-                    String[] substrings = command.split(" ", 2);
-                    int taskId = Integer.parseInt(substrings[1]) - 1;
-                    if (taskId >= 0 && taskId < tasks.size()) {
-                        tasks.get(taskId).markAsDone();
-                    } else {
-                        String message = tasks.size() > 0
-                                ? "No such task! Please enter a task ID between 1 and " + tasks.size()
-                                : "You have no tasks! Please add some tasks first";
-                        System.out.println(" ──────────────────────────────────────── \n"
-                                + message
-                                + "\n ──────────────────────────────────────── ");
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.println(" ──────────────────────────────────────── \n"
-                            + "Invalid command! Please enter only one valid task ID\n"
-                            + " ──────────────────────────────────────── ");
-                }
-            } else if (command.startsWith("unmark")) {
-                try {
-                    String[] substrings = command.split(" ", 2);
-                    int taskId = Integer.parseInt(substrings[1]) - 1;
-                    if (taskId >= 0 && taskId < tasks.size()) {
-                        tasks.get(taskId).markAsUndone();
-                    } else {
-                        String message = tasks.size() > 0
-                                ? "No such task! Please enter a task ID between 1 and " + tasks.size()
-                                : "You have no tasks! Please add some tasks first";
-                        System.out.println(" ──────────────────────────────────────── \n"
-                                + message
-                                + "\n ──────────────────────────────────────── ");
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.println(" ──────────────────────────────────────── \n"
-                            + "Invalid command! Please enter only one valid task ID\n"
-                            + " ──────────────────────────────────────── ");
-                }
+                System.out.println(" ────────────────────────────────────────");
             } else {
-                tasks.add(new Task(command));
-                String echo = " ──────────────────────────────────────── \n"
-                        + "  added: " + command + " \n"
-                        + " ──────────────────────────────────────── ";
-                System.out.println(echo);
+                try {
+                String[] substrings = input.split(" ", 2);
+                String command = substrings[0];
+                    switch (command) {
+                        case "mark":
+                            int markTaskId = Integer.parseInt(substrings[1]) - 1;
+                            if (markTaskId >= 0 && markTaskId < tasks.size()) {
+                                tasks.get(markTaskId).markAsDone();
+                            } else {
+                                String message = tasks.size() > 0
+                                        ? "No such task! Please enter a task ID between 1 and " + tasks.size()
+                                        : "You have no tasks! Please add some tasks first";
+                                System.out.println(" ────────────────────────────────────────\n"
+                                        + message
+                                        + "\n ────────────────────────────────────────");
+                            }
+                            break;
+                        case "unmark":
+                            int unmarkTaskId = Integer.parseInt(substrings[1]) - 1;
+                            if (unmarkTaskId >= 0 && unmarkTaskId < tasks.size()) {
+                                tasks.get(unmarkTaskId).markAsUndone();
+                            } else {
+                                String message = tasks.size() > 0
+                                        ? "No such task! Please enter a task ID between 1 and " + tasks.size()
+                                        : "You have no tasks! Please add some tasks first";
+                                System.out.println(" ────────────────────────────────────────\n"
+                                        + message
+                                        + "\n ────────────────────────────────────────");
+                            }
+                            break;
+                        case "todo":
+                            String todoDesc = substrings[1];
+                            Todo todo = new Todo(todoDesc);
+                            tasks.add(todo);
+                            System.out.println(" ────────────────────────────────────────\n"
+                                    + " Got it. I've added this task:\n"
+                                    + "     " + todo
+                                    + "\n Now you have " + tasks.size() + " tasks in the list.\n"
+                                    + " ────────────────────────────────────────");
+                            break;
+                        case "deadline":
+                            String[] details = substrings[1].split("/by", 2);
+                            Deadline deadline = new Deadline(details[0], details[1]);
+                            tasks.add(deadline);
+                            System.out.println(" ────────────────────────────────────────\n"
+                                    + " Got it. I've added this task:\n"
+                                    + "     " + deadline
+                                    + "\n Now you have " + tasks.size() + " tasks in the list.\n"
+                                    + " ────────────────────────────────────────");
+                            break;
+                        case "event":
+                            String[] eventDetails = substrings[1].split("/from", 2);
+                            String[] eventTimings = eventDetails[1].split("/to", 2);
+                            Event event = new Event(eventDetails[0], eventTimings[0], eventTimings[1]);
+                            tasks.add(event);
+                            System.out.println(" ────────────────────────────────────────\n"
+                                    + " Got it. I've added this task:\n"
+                                    + "     " + event
+                                    + "\n Now you have " + tasks.size() + " tasks in the list.\n"
+                                    + " ────────────────────────────────────────");
+                            break;
+                    }
+                } catch (NumberFormatException e) { // invalid argument is entered for mark and unmark eg. "mark a"
+                    System.out.println(" ────────────────────────────────────────\n"
+                            + "Invalid command! Please enter only one valid task ID\n"
+                            + " ────────────────────────────────────────");
+                } catch (IndexOutOfBoundsException e) { // when user only inputs the command eg "mark" or "deadline"
+                    System.out.println(" ────────────────────────────────────────\n"
+                            + "Invalid command! Please enter valid task details or a valid task ID\n"
+                            + " ────────────────────────────────────────");
+                }
             }
         }
         System.out.println(goodbye);
-    }
-}
-
-class Task {
-    protected String description;
-    protected boolean isDone;
-
-    public Task(String description) {
-        this.description = description;
-        this.isDone = false;
-    }
-
-    public String getStatusIcon() {
-        return (this.isDone ? "X" : " "); // mark done task with X
-    }
-
-    public void markAsDone() {
-        this.isDone = true;
-        System.out.println(" ──────────────────────────────────────── \n"
-                + "  Nice! I've marked this task as done:\n"
-                + "     " + this
-                + "\n ────────────────────────────────────────");
-    }
-
-    public void markAsUndone() {
-        this.isDone = false;
-        System.out.println(" ──────────────────────────────────────── \n"
-                + "  OK, I've marked this task as not done yet:\n"
-                + "     " + this
-                + "\n ────────────────────────────────────────");
-    }
-
-    @Override
-    public String toString() {
-        return "[" + this.getStatusIcon() + "] " + this.description;
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+abstract class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+        System.out.println(" ──────────────────────────────────────── \n"
+                + "  Nice! I've marked this task as done:\n"
+                + "     " + this
+                + "\n ────────────────────────────────────────");
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+        System.out.println(" ──────────────────────────────────────── \n"
+                + "  OK, I've marked this task as not done yet:\n"
+                + "     " + this
+                + "\n ────────────────────────────────────────");
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
