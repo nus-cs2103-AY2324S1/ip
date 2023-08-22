83884e9f6eee85f1328c2ba206996200f0399357
kohkaijie <kohkaijie21@gmail.com>
2023-08-22 14:52:49 +0800

    level 1

--- src/main/java/CR7.java
+++ src/main/java/CR7.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
 public class CR7 {
+    // Function to print a horizontal line
+    public static void printHorizontalLine(int length) {
+        for (int i = 0; i < length; i++) {
+            System.out.print("-");
+        }
+        System.out.println(); // Move to the next line
+    }
+
     public static void main(String[] args) {
+        printHorizontalLine(40);
         System.out.println("Hello! I'm CR7\n" + "What can I do for you?\n");
-        System.out.println("Bye! Hope to see you again soon!");
+        String input = "";
+        while (!input.equals("bye")) {
+            Scanner myObj = new Scanner(System.in);
+            input = myObj.nextLine();
+            if (input.equals("bye")) {
+                printHorizontalLine(40);
+                System.out.println("Bye! Hope to see you again soon! SIUUUU\n");
+                printHorizontalLine(40);
+                break;
+            }
+            printHorizontalLine(40);
+            System.out.println(input + "\n");
+            printHorizontalLine(40);
+        }
     }
 }
