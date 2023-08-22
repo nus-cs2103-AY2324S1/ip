d90d0544cf68a30c201cffe16320411f15ad3c43
mounilsankar <mounil974@gmail.com>
2023-08-23 00:48:25 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         String logo = "       .__               \n"
                 + "  ____ |__| ____   ____  \n"
@@ -16,21 +18,31 @@ public class Duke {
         System.out.println(HORIZONTAL);
         while(true) {
             String command = scanner.nextLine();
-            evaluate(command);
+            evaluate(command, tasks);
             if(command.equals("bye")){
+                tasks.clear();
                 break;
             }
         }
     }
-    public static void evaluate(String command) {
+    public static void evaluate(String command, ArrayList<String> tasks) {
         final String HORIZONTAL = "_____________________________________________________________";
         if(command.equals("bye")) {
             System.out.println(HORIZONTAL);
             System.out.println("Bye. Hope to see you again soon!");
             System.out.println(HORIZONTAL);
-        } else {
+        } else if(command.equals("list")) {
+            int length = tasks.size();
             System.out.println(HORIZONTAL);
-            System.out.println(command);
+            for(int i = 0;i < length; i++) {
+                int task_number = i + 1;
+                System.out.println(task_number + ". " + tasks.get(i));
+            }
+            System.out.println(HORIZONTAL);
+        } else {
+            tasks.add(command);
+            System.out.println(HORIZONTAL);
+            System.out.println("added: " + command);
             System.out.println(HORIZONTAL);
         }
     }
