99caea2a04f5a95049084adde6404f8ebf1a12e3
Jun Yu <lowjy28@gmail.com>
2023-08-22 12:29:11 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,27 +2,36 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        int count = 0;
+        String[] strlst = new String[100];
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
-        String greeting = " Hello! I'm Somebodyhaha\n" +
-                " What can I do for you?";
-        String exiting = " Bye. Hope to See you again soon!";
 
         System.out.println("Hello from\n" + logo);
-        Printer.print(greeting);
+        Printer.printGreeting();
 
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
         while(!input.equals("bye")) {
-            Printer.print(input);
-            input = sc.nextLine();
+            if (input.equals("list")) {
+                Printer.printList(strlst);
+                input = sc.nextLine();
+            }
+            else {
+                Printer.print("added: " + input);
+                strlst[count] = input;
+                input = sc.nextLine();
+                count++;
+            }
+
         }
 
-        Printer.print(exiting);
+        Printer.printExit();
     }
 }
--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -4,9 +4,35 @@ public class Printer {
 
     static String line = "---------------------";
 
+    static String greeting = " Hello! I'm Somebodyhaha\n" +
+            " What can I do for you?";
+    static String exiting = " Bye. Hope to See you again soon!";
+
+
+    public static void printGreeting(){
+        print(greeting);
+    }
+
+    public static void printExit() {
+        print(exiting);
+    }
     public static void print(String input) {
         System.out.println(line);
         System.out.println(input);
+        System.out.println(line);
+    }
+
+    public static void printList(String[] lst){
+        int count = 1;
+        System.out.println(line);
+        if(lst != null) {
+            for (String item: lst) {
+                if(item == null) {break;}
+                String str = count + ". " + item;
+                System.out.println(str);
+                count++;
+            }
+        }
         System.out.println(line);
     }
 }
