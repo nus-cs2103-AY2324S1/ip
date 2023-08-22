36a4bcaf1aea813aa82587acd5ef86eac2ec24f9
Jonyxzx <jonykhozhixiong@gmail.com>
2023-08-22 22:29:30 +0800

    Level 2 added

--- src/main/java/Ace.java
+++ src/main/java/Ace.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 
 public class Ace {
+    private static String[] toDoList = new String[100];
+    private static int tracker = 0;
+
     private String addLine(String message) {
         String horizontal = "_____________________________________________________\n";
         return horizontal + message + "\n" + horizontal;
@@ -14,14 +17,32 @@ public class Ace {
         return "Bye. Hope to see you again soon!";
     }
 
+    private String printList() {
+        String output = "";
+        for (int i = 0; i < tracker; i++) {
+            if (!toDoList[i].isEmpty()) {
+                output += (i + 1) + ". " + toDoList[i] + "\n";
+            }
+        }
+        return output;
+    }
+
+    private String addTask(String task) {
+        toDoList[tracker] = task;
+        tracker++;
+        return "added: " + task;
+    }
+
     public String sendMessage(String keyWord) {
         switch(keyWord) {
             case "start":
                 return addLine(greet());
             case "bye":
                 return addLine(goodbye());
+            case "list":
+                return addLine(printList());
             default:
-                return addLine((keyWord));
+                return addLine(addTask(keyWord));
         }
     }
 }
