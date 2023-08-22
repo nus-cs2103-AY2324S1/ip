4267d6d4f5751178bb63250f2a06001cf5ff22aa
shyanyong <wshyanyong123@gmail.com>
2023-08-22 20:19:20 +0800

    Level 1. Echo Improve the skeletal version of Duke so that it echos commands entered by the user, and exits when the user types the command bye.

--- src/main/java/Brotherman.java
+++ src/main/java/Brotherman.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Brotherman {
+import java.util.Scanner;
+
+public class Brotherman {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(
+                "___________________________________________________________\n"
+                + "Hello! I'm Brotherman\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n");
+        String userCommand = input.nextLine();
+
+        while (!userCommand.equals("bye")) {
+            System.out.println(userCommand);
+            userCommand = input.nextLine();
+        }
+
+        System.out.println(
+                "___________________________________________________________\n"
+                        + "Bye, see you again Brotherman!\n"
+                        + "___________________________________________________________\n"
+
+        );
+
+    }
 }
