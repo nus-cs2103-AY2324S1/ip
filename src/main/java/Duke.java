456541837118f522890ebd2e9796455992bd29ae
Sher Yew <tansheryew@gmail.com>
2023-08-22 14:32:18 +0800

    Echo
    Echo user commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,22 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     private static final String chatbot = "War Room";
     public static void main(String[] args) {
         System.out.println("Hello! I'm " + chatbot);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String user_input = scanner.nextLine();
+            if (Objects.equals(user_input, "bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                scanner.close();
+                break;
+            } else {
+                System.out.println(user_input);
+            }
+        }
     }
 }
