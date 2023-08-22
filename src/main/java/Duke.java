398a7c8dc6a0aeb0feb17cf3e97c130a0ff367de
STEVEN\Steven <stevenliudw@gmail.com>
2023-08-22 21:34:31 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,13 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello from\n" + logo);
+        System.out.println("_____________________________________\n");
+        System.out.println("Hello! I 'm Jarvis.\n");
+        System.out.println("What can I do for you?\n");
+        System.out.println("_____________________________________\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("_____________________________________\n");
     }
 }
