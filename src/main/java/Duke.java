583656cb5f0a3e33abfd86cd35accdd252bc97d4
Zhetao <quzhetao2001@gmail.com>
2023-08-22 16:42:14 +0800

    Added bye control flow logic

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         printLine();
-        System.out.println("Hello! I'm FUNNY.\nWhat can I do for you?");
+        System.out.println("\tHello! I'm FUNNY.\n\tWhat can I do for you?");
         printLine();
-        System.out.println("Bye. Hope to see you again soon!");
+        String input = scanner.nextLine().trim();
+        while (!input.equals("bye")) {
+            printLine();
+            System.out.println("\t" + input);
+            printLine();
+            input = scanner.nextLine().trim();
+        }
+        printLine();
+        System.out.println("\tBye. Hope to see you again soon!");
+        printLine();
     }
-    
+
 
     public static void printLine() {
+        System.out.print("\t");
         for (int i = 0; i < 80; i++) {
-            System.out.print("-");
+            System.out.print("─");
         }
         System.out.println();
     }
