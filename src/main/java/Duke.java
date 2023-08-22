a0aed2dde62a7f11c8f50c407cb1f3b959f01a0a
tiif <pekyuan@gmail.com>
2023-08-22 20:27:20 +0800

    complete level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import java.io.*;
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-    }
+         */
+        String start = "Hello! I'm ChatBot" + "\n" + "What can I do for you?" + "\n";
+        System.out.println(start);
+        System.out.println("Bye. Hope to see you again soon!");
+
+        }
 }
