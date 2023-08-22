5fd772a81b82f76f518667a986b44cc116ed49da
SynapseProgramming <roald.ong@gmail.com>
2023-08-22 14:14:49 +0800

    Added in formatting for Deadline objects

--- src/main/java/UiFormatter.java
+++ src/main/java/UiFormatter.java
@@ -6,6 +6,13 @@ public class UiFormatter {
 
     if (task instanceof Todo) {
       answer = "[" + Todo.taskType + "]" + "[" + task.getDoneIcon() + "] " + task.description;
+    } else if (task instanceof Deadline) {
+      Deadline deadTask = (Deadline) task;
+
+      answer =
+          "[" + Deadline.taskType + "]" + "[" + task.getDoneIcon() + "] " + task.description + "("
+              + deadTask.getDeadDate() + ")";
+
     }
 
     return answer;
