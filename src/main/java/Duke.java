66b1e0c3fb75e5035ae11b52ffaf371589a10988
kristayeo <kristayeo@u.nus.edu>
2023-08-22 19:15:26 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,25 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello! I'm Meep.\nWhat can I do for you?");
 
-        System.out.println("Bye! Hope to see you again soon!");
+        while (true) {
+            String userCommand = scanner.nextLine();
+            if (userCommand.equalsIgnoreCase("bye")) {
+                System.out.println("Bye! Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(userCommand);
+            }
+        }
 
     }
 }
