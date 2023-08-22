49ec4fca52e2d74ba90a950038f95bfcbd4b98f2
ruiyangzh <zhao.ruiyang90@gmail.com>
2023-08-23 00:10:41 +0800

    Level 2. Added list functionality (implemented using an array of String).

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,8 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        String[] list = new String[100];
+        int counter = 0;
         String logo = " _           _        \n"
                     + "| |    _   _| | _____ \n"
                     + "| |   | | | | |/ / _ \\\n"
@@ -11,10 +13,18 @@ public class Duke {
 
         while (true) {
             String input = reader.nextLine();
-            if (input.equals("bye")){
+            if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < counter; i++) {
+                    System.out.println((i+1)+". " + list[i]);
+
+                }
+            } else {
+                System.out.println("added: " + input);
+                list[counter] = input;
+                counter += 1;
             }
-            System.out.println(input);
         }
         //System.out.println("What's up?\n");
         System.out.println("Ok byeee\n");
