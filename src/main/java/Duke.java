fc06ec5c6dfd30cfd762b36a822b5fbf6800552d
revdrag <94689523+revdrag@users.noreply.github.com>
2023-08-22 17:16:10 +0800

    Implemented Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
@@ -8,8 +9,16 @@ public class Duke {
                 + "        ┗━━┻━┓┏┻━━┛\n"
                 + "        ╋╋╋┏━┛┃\n"
                 + "        ╋╋╋┗━━┛\n";
+        System.out.println(line + "\nHello! I'm \n" + logo);
+        System.out.println("How can I help you? \n" + line + "\n");
+        Scanner userInput = new Scanner(System.in);
+        String input = userInput.nextLine();
 
-        System.out.println(line + "\nHello! I'm \n" + logo + line);
+        while (!input.equals("bye")) {
+            System.out.println(input + "\n" + line);
+            input = userInput.nextLine();
+        }
+
         System.out.println("Bye (actually hehe). Hope to see you again!\n" + line);
     }
 }
