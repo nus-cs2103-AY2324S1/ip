d681a52cd5f80224e6948d3840c005b4c496dfa6
lihongguang00 <lihongguang00@gmail.com>
2023-08-22 20:53:53 +0800

    Level-2
    Added list feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Duke {
     public static void main(String[] args) {
@@ -8,8 +10,9 @@ public class Duke {
                 " | |    |  __  | / /\\ \\ | |  | | | |_ |  ___/  | |   \n" +
                 " | |____| |  | |/ ____ \\| |__| | |__| | |      | |   \n" +
                 "  \\_____|_|  |_/_/    \\_\\_____/ \\_____|_|      |_|   \n";
-
         final String horizontal = "-------------------------------------------------------------------";
+
+        List<String> taskList = new ArrayList<String>();
 
         System.out.println(horizontal + logo + horizontal);
         System.out.println("ChadGPT: Welcome to ChadGPT, What would you like to do today?\n" + horizontal);
@@ -17,12 +20,23 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         System.out.print("User: ");
         while (!sc.hasNext("bye")) {
-            System.out.println("ChadGPT: " + sc.nextLine() + "\n" + horizontal);
-            System.out.print("User: ");
+            String task = sc.nextLine();
+            if (task.equals("list")) {
+                int counter = 1;
+                System.out.println("ChadGPT: Here are your tasks: ");
+                for (String t : taskList) {
+                    System.out.println(counter + ". " + t);
+                    counter++;
+                }
+            } else {
+                System.out.println("ChadGPT: added task '" + task + "'");
+                taskList.add(task);
+            }
+            System.out.print(horizontal + "\nUser: ");
         }
 
         sc.close();
 
-        System.out.println("ChadGPT: Bye. Hope to see you again soon!\n" + horizontal);
+        System.out.print("ChadGPT: Bye. Hope to see you again soon!\n" + horizontal);
     }
 }
