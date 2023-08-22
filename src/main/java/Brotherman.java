4cf5cfcc9bee956c0130baf7a2f4f89d19c4a6a2
shyanyong <wshyanyong123@gmail.com>
2023-08-22 22:01:00 +0800

    Level 3. Mark as Done Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done.

--- src/main/java/Brotherman.java
+++ src/main/java/Brotherman.java
@@ -7,7 +7,7 @@ public class Brotherman {
 
         Scanner input = new Scanner(System.in);
 
-        ArrayList<String> userList = new ArrayList<String>();
+        ArrayList<Task> userList = new ArrayList<Task>();
 
 
         System.out.println(
@@ -15,17 +15,50 @@ public class Brotherman {
                 + "Hello! I'm Brotherman\n"
                 + "What can I do for you?\n"
                 + "____________________________________________________________\n");
+
         String userCommand = input.nextLine();
 
         while (!userCommand.equals("bye")) {
+
             if (userCommand.equals("list")) {
                 int start = 1;
-                for (String listItems : userList) {
-                    System.out.println(start + ". " + listItems);
+                for (Task listItems : userList) {
+                    System.out.println(start + ". " + listItems.toString());
                     start++;
                 }
+            } else if (userCommand.split(" ")[0].equals("mark")) {
+                int task = Integer.parseInt(userCommand.split(" ")[1]) - 1;
+
+                if (task < 0 || task >= userList.size()) {
+                    System.out.println("Brotherman the value you put in wrong.  Try again.");
+                } else {
+                    userList.get(task).markAsDone();
+                    System.out.println(
+                            "___________________________________________________________\n"
+                                    + "The task is done Brotherman \n"
+                                    + userList.get(task).toString()
+                                    + " \n"
+                                    + "___________________________________________________________\n"
+                    );
+                }
+            } else if (userCommand.split(" ")[0].equals("unmark")) {
+                int task = Integer.parseInt(userCommand.split(" ")[1]) - 1;
+
+                if (task < 0 || task >= userList.size()) {
+                    System.out.println("Brotherman the value you put in wrong.  Try again.");
+                } else {
+                    userList.get(task).unmarkAsDone();
+                    System.out.println(
+                            "___________________________________________________________\n"
+                                    + "The task is now undone Brotherman \n"
+                                    + userList.get(task).toString()
+                                    + " \n"
+                                    + "___________________________________________________________\n"
+                    );
+                }
+
             } else {
-                userList.add(userCommand);
+                userList.add(new Task(userCommand));
             }
             userCommand = input.nextLine();
         }
