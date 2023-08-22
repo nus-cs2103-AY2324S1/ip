706f66747315532f2aca7d53b671afd4db6f97a4
darrentfy <>
2023-08-22 15:41:21 +0800

    added feature to echo commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         String welcome = " ──────────────────────────────────────── \n"
                 + "  Hello! I'm Handsome \n"
                 + "  What can I do for you? \n"
-                + " ──────────────────────────────────────── \n";
-
-        String goodbye = "  Bye. Hope to see you again soon! \n" +
+                + " ──────────────────────────────────────── ";
+        String goodbye = " ──────────────────────────────────────── \n" +
+                "  Bye. Hope to see you again soon! \n" +
                 " ──────────────────────────────────────── \n";
-        System.out.println(welcome + goodbye);
+        System.out.println(welcome);
+        while (true) {
+            String command = input.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            String echo = " ──────────────────────────────────────── \n" +
+                    "  " + command + " \n" +
+                    " ──────────────────────────────────────── ";
+            System.out.println(echo);
+        }
+        System.out.println(goodbye);
     }
 }
