27de53388cc446210c7d2d008bff074518debdb4
Nathanael M. Tan <nathanaelmark.tan@gmail.com>
2023-08-22 13:05:59 +0800

    Created Task class Created mark and unmark functionality Error handling

--- src/main/java/EpochMind.java
+++ src/main/java/EpochMind.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class EpochMind {
     public static String logo = "\n" +
             "                                                                                                    \n" +
@@ -21,10 +23,9 @@ public class EpochMind {
             "`----'      `---`               `----'             '---'            ---`-'                `----'    \n" +
             "                                                                                                    \n";
     public static String[] exitMessage = {"May you seek the truth.", "May the truth prevail.", "May you yet again thirst for wisdom.", "May your quest for wisdom be unwavering.", "May your pursuit of knowledge illuminate your path."};
-
+    public static List<Task> tasks = new ArrayList<>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<String> userInput = new ArrayList<>();
 
         System.out.println(logo + "___________________________________________________________________________________________________________\n" +
                 " Greetings! I'm EpochMind, Seer of the Cosmos, Keeper of Knowledge, Pantheon of Wisdom, the Eternal Truth\n" +
@@ -32,30 +33,75 @@ public class EpochMind {
                 "___________________________________________________________________________________________________________\n");
         while (sc.hasNextLine()) {
             String command = sc.nextLine();
-            if (command.trim().toLowerCase().equals("bye")) {
-                System.out.println("___________________________________________________________________________________________________________\n" +
-                        " May you seek the truth\n" +
-                        "___________________________________________________________________________________________________________\n");
-            } else if (command.trim().toLowerCase().equals("list")) {
-                list(userInput);
-            } else {
-                userInput.add(command);
-                System.out.println("___________________________________________________________________________________________________________\n" +
-                        "added: " + command + "\n"+"___________________________________________________________________________________________________________\n");
+            String[] commandList = command.trim().toLowerCase().split(" ");
+            switch (commandList[0]) {
+                case "bye":
+                    System.out.println("___________________________________________________________________________________________________________\n" +
+                            " May you seek the truth\n" +
+                            "___________________________________________________________________________________________________________\n");
+                    break;
+                case "list":
+                    list();
+                    break;
+                case "mark":
+                    if (commandList.length == 1) {
+                        System.out.println("Thou hast forgotten to specify thine index");
+                    } else {
+                        int index = parseInt(commandList[1]);
+                        if (tasks.size() < index) {
+                            System.out.println("Thou hast specified an index larger than thine list");
+                        } else {
+                            mark(parseInt(commandList[1]));
+                        }
+                    }
+                    break;
+                case "unmark":
+                    if (commandList.length == 1) {
+                        System.out.println("Thou hast forgotten to specify thine index");
+                    } else {
+                        int index = parseInt(commandList[1]);
+                        if (tasks.size() < index) {
+                            System.out.println("Thou hast specified an index larger than thine list");
+                        } else {
+                            unmark(parseInt(commandList[1]));
+                        }
+                    }
+                    break;
+                default:
+                    tasks.add(new Task(command));
+                    System.out.println("___________________________________________________________________________________________________________\n" +
+                            "added: " + command + "\n" + "___________________________________________________________________________________________________________\n");
+                    break;
             }
         }
     }
 
-    public static void list(List<String> userInput) {
+    public static void list() {
         System.out.println("___________________________________________________________________________________________________________\n");
-        for (int i = 0; i < userInput.size(); i++) {
+        for (int i = 0; i < tasks.size(); i++) {
             StringBuilder sb = new StringBuilder();
             sb.append(i+1);
-            sb.append(".");
-            sb.append(userInput.get(i));
-            System.out.println(sb.toString());
+            sb.append(". ");
+            sb.append("[" + tasks.get(i).getStatusIcon() + "]");
+            sb.append(tasks.get(i).description);
+            System.out.println(sb);
         }
         System.out.println("___________________________________________________________________________________________________________\n");
     }
+
+    public static void mark(int index) {
+        System.out.println("___________________________________________________________________________________________________________\n" + "The Mind sees that this task is completed \n" +
+                "[X] " + tasks.get(index - 1).description +
+                "\n___________________________________________________________________________________________________________\n");
+        tasks.get(index - 1).mark();
+    }
+
+    public static void unmark(int index) {
+        System.out.println("___________________________________________________________________________________________________________\n" + "The Mind sees that this task is not yet completed \n" +
+"[ ] " + tasks.get(index - 1).description +
+"\n___________________________________________________________________________________________________________\n");
+        tasks.get(index - 1).unmark();
+    }
+
 
 }
