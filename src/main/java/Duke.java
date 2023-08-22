5ed4defb0a2f1b8cc3c3095d671654a914fcc1fe
Victor <vpeng36@gmail.com>
2023-08-22 21:56:16 +0800

    complete level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String chatbotName = "Albatross\n";
         System.out.print("Hello! I'm " + chatbotName);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        // scanner to read user response
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a command");
+
+        String userResponse = scanner.nextLine();
+        while (!userResponse.equals("bye")) {
+            System.out.println(userResponse);
+            userResponse = scanner.nextLine();
+        }
+
+        System.out.println("Bye! Hope to see you again soon!");
     }
 }
