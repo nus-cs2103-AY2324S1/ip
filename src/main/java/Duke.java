e595aed22a9ea92d3a0036fd5956f0418419b6fc
jianyangg <jianyanglim01@gmail.com>
2023-08-22 18:14:03 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,20 +2,24 @@ import java.util.Scanner;
 
 public class Duke {
     private final String DIVIDER = "\t____________________________________________________________";
-    private final String LOGO = " ____        _        \n"
-                                    + "|  _ \\ _   _| | _____ \n"
-                                    + "| | | | | | | |/ / _ \\\n"
-                                    + "| |_| | |_| |   <  __/\n"
-                                    + "|____/ \\__,_|_|\\_\\___|\n";
+    private final String LOGO = 
+                            "\t░█████╗░██╗░░██╗░█████╗░████████╗████████╗██╗░░░██╗\n" +
+                            "\t██╔══██╗██║░░██║██╔══██╗╚══██╔══╝╚══██╔══╝╚██╗░██╔╝\n" +
+                            "\t██║░░╚═╝███████║███████║░░░██║░░░░░░██║░░░░╚████╔╝░\n" +
+                            "\t██║░░██╗██╔══██║██╔══██║░░░██║░░░░░░██║░░░░░╚██╔╝░░\n" +
+                            "\t╚█████╔╝██║░░██║██║░░██║░░░██║░░░░░░██║░░░░░░██║░░░\n" +
+                            "\t░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░░░╚═╝░░░░░░╚═╝░░░\n";
     private final String GREETING = "\tHello! I'm Chatty.\n\tWhat can I do for you?";
     private final String FAREWELL = "\tBye. Have \"fun\" in school!";
+    private String[] textList = new String[100];
+    private int textListSize = 0;
 
     public static void main(String[] args) {
         // get new duke instance
         Duke duke = new Duke();
 
         // introduction
-        System.out.println("\nWelcome to Chatty.\n" + duke.LOGO);
+        System.out.println("\n\tWelcome to Chatty.\n" + duke.LOGO);
         duke.sendGreeting();
 
         try (Scanner sc = new Scanner(System.in)) {
@@ -23,7 +27,7 @@ public class Duke {
             String input = sc.nextLine().trim();
 
             while (!input.equals("bye")) {
-                duke.echoAndDisplayDivider(input);
+                duke.handleTextInput(input);
                 System.out.println();
                 input = sc.nextLine().trim();
             }
@@ -32,15 +36,31 @@ public class Duke {
         duke.sendFarewell();
     }
 
+    private void addToTextList(String text) {
+        if (textListSize < textList.length) {
+            textList[textListSize] = text;
+            textListSize++;
+        } else {
+            System.out.println("Error: List is full.");
+        }
+    }
+
     private void sendGreeting() {
         System.out.println(DIVIDER);
         System.out.println(GREETING);
         System.out.println(DIVIDER);
     }
 
-    private void echoAndDisplayDivider(String message) {
+    private void handleTextInput(String inputString) {
         System.out.println(DIVIDER);
-        System.out.println('\t' + message);
+        if (inputString.equals("list")) {
+            for (int i = 0; i < textListSize; i++) {
+                System.out.println("\t" + (i + 1) + ". " + textList[i]);
+            }
+        } else {
+            System.out.println("\tadded: " + inputString);
+            addToTextList(inputString);
+        }
         System.out.println(DIVIDER);
     }
 
