046a8e4f85445069c01e808045291d6df75fadac
junnengsoo <soojunneng01@gmail.com>
2023-08-22 16:58:27 +0800

    Complete level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,13 +13,24 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
         String input;
+        String[] list = new String[100];
+        int listIndex = 0;
 
         while (true) {
             input = scanner.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                System.out.print(line + "\n");
+                for (int i = 0; i < listIndex; i++) {
+                    System.out.println((i + 1) + ". " + list[i]);
+                }
+                System.out.print(line + "\n");
+            } else {
+                list[listIndex] = input;
+                listIndex++;
+                System.out.println(line + "added: " + input + "\n" + line);
             }
-            System.out.println(line + input + "\n" + line);
         }
 
         System.out.println(line +
