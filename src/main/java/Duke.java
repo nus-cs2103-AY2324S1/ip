5c1f10ae2d42fa1529cb766df483e5e9e7e25020
Yu Han Zhang <yuhan@zhyuhan.me>
2023-08-22 10:59:35 +0800

    feat: add greeting and farewell

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
+    private static final String NAME = "Jimmy";
+
+    private static void greet() {
+        System.out.printf("Hello! I'm %s\nWhat can I do for you?%n", NAME);
+    }
+
+    private static void farewell() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+
+        farewell();
     }
 }
