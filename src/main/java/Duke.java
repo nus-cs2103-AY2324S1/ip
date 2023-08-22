b9ea60eb2d45e2bcfdfc1b4c99412a8b78dff2c1
chonguschonguschongus <>
2023-08-22 11:20:05 +0800

    Level 1 changes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -8,6 +12,17 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I am Dukey.\n"
                 + "What can I do for you?");
-        System.out.println("Bye. Hope to see you again!");
+
+        while (true) {
+            String response = scanner.nextLine();
+
+            if (response.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again!");
+                break;
+            } else {
+                System.out.println(response);
+            }
+        }
+        scanner.close();
     }
 }
