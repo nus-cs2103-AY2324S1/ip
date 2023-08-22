e2a46ad83416748e13e91777d1ec096ded9448ea
Mahidharah <mahidharah@gmail.com>
2023-08-22 23:15:46 +0800

    1. Echos commands entered by the user, and exits when the user types the command bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,16 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm IPSVIJAYKUMARAAKOODAIRRUKALAM");
         System.out.println("What can I do for you?");
+        String userinput;
+        Scanner scan = new Scanner( System.in );
+
+        userinput = scan.nextLine();
+
+        while (!userinput.equalsIgnoreCase("bye")) {
+            System.out.println(userinput);
+            userinput = scan.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
