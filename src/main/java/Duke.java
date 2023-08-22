2407c18bfb6b55fd018154d62c8d3fb9bdcb9db2
Mohammed-Faizzzz <110959467+Mohammed-Faizzzz@users.noreply.github.com>
2023-08-22 10:09:39 +0800

    Completed Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "Faiz";
+        String line = "------------------------\n";
+        String greeting = line + "Hello! I'm " + name + "\n" + "What can I do for you?" + "\n";
+        String goodbye = line + "Bye. Hope to see you again soon!" + "\n" + line;
+        String logo = greeting + goodbye;
+        System.out.println(logo);
     }
 }
