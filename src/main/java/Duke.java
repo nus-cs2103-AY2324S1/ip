9e1829228c4f38f7f93531cef7baefe35e114de6
samuelmui8 <samuelmui8@gmail.com>
2023-08-23 00:46:12 +0800

    added ability to mark and unmark tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,22 +9,59 @@ public class Duke {
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t-----------------------------------------------");
         Scanner scanner = new Scanner(System.in);
-        List<String> list = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
 
         while (true) {
             String input = scanner.nextLine();
+            String[] words = input.split(" ");
 
             if (input.equals("bye")) {
+                // exit program
                 break;
             } else if (input.equals("list")) {
+                // display current list
                 System.out.println("\t-----------------------------------------------");
+                System.out.println("\tHere are the tasks in your list:");
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println("\t" + (i + 1) + ". " + list.get(i));
+                    System.out.println("\t" + (i + 1) + ".[" + list.get(i).getStatusIcon() + "] " + list.get(i));
                 }
                 System.out.println("\t-----------------------------------------------");
 
+            } else if (words[0].equals("mark")) {
+                // mark task as done
+                int index = Integer.parseInt(words[1]) - 1;
+
+                if (index < 0 || index >= list.size()) {
+                    // handle invalid input
+                    System.out.println("Invalid number ");
+                } else {
+                    list.get(index).markAsDone();
+                    System.out.println("\t-----------------------------------------------");
+                    System.out.println("\tNice! I've marked this task as done: ");
+                    System.out.println("\t[" + list.get(index).getStatusIcon() + "] " + list.get(index));
+                    System.out.println("\t-----------------------------------------------");
+                }
+
+
+            } else if (words[0].equals("unmark")) {
+                // mark task as undone
+                int index = Integer.parseInt(words[1]) - 1;
+
+                if (index < 0 || index >= list.size()) {
+                    // handle invalid input
+                    System.out.println("Invalid number ");
+                } else {
+                    list.get(index).unMark();
+                    System.out.println("\t-----------------------------------------------");
+                    System.out.println("\tNice! I've marked this task as not done yet: ");
+                    System.out.println("\t[" + list.get(index).getStatusIcon() + "] " + list.get(index));
+                    System.out.println("\t-----------------------------------------------");
+                }
+
             } else {
-                list.add(input);
+                // add task
+                Task task = new Task(input);
+                list.add(task);
                 System.out.println("\t-----------------------------------------------");
                 System.out.println("\tadded: " + input);
                 System.out.println("\t-----------------------------------------------");
