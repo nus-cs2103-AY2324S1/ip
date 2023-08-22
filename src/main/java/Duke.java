ded50efe5b4f0c166f6fc5c1a241a3f4bcfdc81a
justin soong <91204066+juzzztinsoong@users.noreply.github.com>
2023-08-22 10:54:37 +0800

    renamed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = 
+    "―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――\n";
+        String logo = 
+    "     ______     _                     ______          __     ____        __ \n"+
+    "    / ____/____(_)___  ____ ____     /_  __/__  _  __/ /_   / __ )____  / /_\n"+
+    "   / /   / ___/ / __ \\/ __ `/ _ \\     / / / _ \\| |/_/ __/  / __  / __ \\/ __/\n"+
+    "  / /___/ /  / / / / / /_/ /  __/    / / /  __/>  </ /_   / /_/ / /_/ / /_  \n"+
+    "  \\____/_/  /_/_/ /_/\\__, /\\___/    /_/  \\___/_/|_|\\__/  /_____/\\____/\\__/  \n"+
+    "                    /____/                                                  \n";
+        System.out.println(line + "Hi, I am a\n" + logo + "How can I help you hehe.. (° ͜ʖ °)\n\n" + line + "Aw goodbye.. ಠ_ಠ\n\n" + line);
+/*
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            String temp = scanner.nextLine();
+            if (temp == null) {
+                break;
+            }
+            System.out.println(temp);
+        }
+*/
     }
 }
