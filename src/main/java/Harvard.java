11c53bce3fd62405b2b80c48e0bae9402af21aaa
Kota Sudarshan Karanth <sudarshan2402@gmail.com>
2023-08-22 16:11:21 +0800

    Added logic to "Harvard.java" in order to be able to scan inputs for commands and output the same command back. Scanner closes upon "bye" command.

--- src/main/java/Harvard.java
+++ src/main/java/Harvard.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
 public class Harvard {
     public static void main(String[] args) {
         String line = "----------------------------------------";
         System.out.println(line);
         System.out.println("Hello! I'm Harvard\nWhat can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String command = in.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            } else {
+                System.out.println(line);
+                System.out.println(command);
+                System.out.println(line);
+            }
+        }
+        in.close();
     }
 }
