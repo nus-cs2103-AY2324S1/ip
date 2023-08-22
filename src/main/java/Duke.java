01cafddf47bde9f1fa29d35cb023d88b1d240325
jianyangg <jianyanglim01@gmail.com>
2023-08-22 17:02:48 +0800

    add Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,15 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Welcome to Chatty.\n" + logo);
+
+        String divider = "____________________________________________________________";
+        String greeting = "Hello! I'm Chatty.\nWhat can I do for you?";
+        String bye = "Bye. Hope to see you again soon!";
+        System.out.println(divider);
+        System.out.println(greeting);
+        System.out.println(divider);
+        System.out.println(bye);
+        System.out.println(divider);
     }
 }
