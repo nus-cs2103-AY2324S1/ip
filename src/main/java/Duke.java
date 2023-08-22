f5fe0526277534677302406d7ecc608af0da1099
GlendaChong <glendachong04@gmail.com>
2023-08-22 10:53:50 +0800

    Added mark task as done or undone feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -27,33 +27,81 @@ public class Duke {
         if (tasks.isEmpty()) {
             // Case where there is no tasks to be displayed
             System.out.println("      " + "No tasks added. ");
-
         } else {
-            int taskNumber = 1;
-            for (Object task: tasks) {
-                System.out.println("      " + taskNumber + ". " + task);
-                taskNumber++;
+            System.out.println("      " + "Here are the tasks in your list: ");
+
+            for (Object taskObj: tasks) {
+                if (taskObj instanceof Task) {
+                    Task task = (Task) taskObj;
+                    System.out.println("      " + (tasks.indexOf(task) + 1) + ". " + task.getTask());
+                }
             }
         }
-
         System.out.println(horizontalLine);
+    }
+
+    public static void markTaskAsDone(Task task) {
+        System.out.print(horizontalLine);
+        System.out.println("      " + "Great! I've completed this task!");
+        task.markAsDone();
+        System.out.println("      " + task.getTask());
+        System.out.println(horizontalLine);
+    }
+
+    public static void markTaskAsUnDone(Task task) {
+        System.out.print(horizontalLine);
+        System.out.println("      " + "Okay, I have not yet completed this task.");
+        task.markAsUndone();
+        System.out.println("      " + task.getTask());
+        System.out.println(horizontalLine);
+    }
+
+    public static void invalidCommand() {
+        System.out.println(horizontalLine
+                            + "      " + "Invalid command!\n"
+                            + horizontalLine);
     }
 
     public static void main(String[] args) {
         greet();
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         while(true) {
             String command = scanner.nextLine();
+
             if (command.equals("bye")) {
                 exit();
                 return;
+
             } else if (command.equals("list")) {
+                // List out all the tasks added
                 printTasks(tasks);
+
+            } else if (command.contains(" ") && command.split(" ", 2)[0].equals("mark")) {
+                // Mark tasks as done
+                int taskNumber = Integer.parseInt(command.split(" ", 2)[1]);
+
+                if (taskNumber > tasks.size()) {
+                    invalidCommand();
+                } else {
+                    markTaskAsDone(tasks.get(taskNumber - 1));
+                }
+
+            } else if (command.contains(" ") && command.split(" ", 2)[0].equals("unmark")) {
+                // Mark tasks as undone
+                int taskNumber = Integer.parseInt(command.split(" ", 2)[1]);
+
+                if (taskNumber > tasks.size()) {
+                    invalidCommand();
+                } else {
+                    markTaskAsUnDone(tasks.get(taskNumber - 1));
+                }
+
             } else {
+                // Add task into task list
                 printCommand(command);
-                tasks.add(command);
+                tasks.add(new Task(command));
             }
         }
     }
