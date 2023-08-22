0bec87b75f9a9829472956103406371b15ddc321
LicongHuang <alexliconghuang@gmail.com>
2023-08-22 11:02:55 +0800

    Improved skeletal code for chatbot

--- src/main/java/Cheese.java
+++ src/main/java/Cheese.java
@@ -1,13 +1,24 @@
-
+import java.util.Scanner;
 
 public class Cheese {
   public void speak() {
-    System.out.println("-----------------------------------------");
-    System.out.println("Hello, I'm Cheese");
-    System.out.println("What can I do for you?");
-    System.out.println("------------------------------------------");
-    System.out.println("Bye. Hope to see you again soon!");
-    System.out.println("-----------------------------------------");
+    Scanner myObj = new Scanner(System.in);
+    String echoedInput = "";
+    System.out.println("\t-----------------------------------------");
+    System.out.println("\tHello, I'm Cheese");
+    System.out.println("\tWhat can I do for you?");
+    System.out.println("\t------------------------------------------");
+    while (true) {
+      echoedInput = myObj.nextLine();
+      System.out.println("\t-----------------------------------------");
+      System.out.println("\t" + echoedInput);
+      System.out.println("\t-----------------------------------------");
+      if (echoedInput.equals("bye")) {
+        break;
+      }
+    }
+    System.out.println("\tBye. Hope to see you again soon!");
+    System.out.println("\t-----------------------------------------");
   }
 
   public static void main(String[] args) {
