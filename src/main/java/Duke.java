b492127a3c254dd61a796ea4120db6e3f5a0038e
Yarn Meng <e0969262@u.nus.edu>
2023-08-22 19:03:35 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 public class Duke {
     private static String GREETINGS = "Hello! I'm AChatBot\n" +
             "What can I do for you?";
     private static String FAREWELL = "Bye. Hope to see you again soon!";
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         System.out.println(GREETINGS);
+        while (true) {
+            String input = s.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
         System.out.println(FAREWELL);
+        s.close();
     }
 }
