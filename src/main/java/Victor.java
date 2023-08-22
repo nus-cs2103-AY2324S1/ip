23eb48ace2f0e63d3575503a48b1a86ddc276cfb
victorlaiyeeteng <vlai69863@gmail.com>
2023-08-22 10:44:50 +0800

    Level-0

--- src/main/java/Victor.java
+++ src/main/java/Victor.java
@@ -1,10 +1,17 @@
-public class Duke {
+import java.util.Scanner;
+
+public class Victor {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Victor\n" +
+                "What can I do for you?\n" +
+                "-----------------------\n" +
+                "Bye. Hope to see you again soon!");
+
     }
 }
