fac9d53ac96598d33a9fac3c9ae88e3745b62606
wjayee <wong.jayee@gmail.com>
2023-08-22 23:06:09 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -5,6 +6,8 @@ public class Duke {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        List<String> tasks = new ArrayList<>();
 
         String greeting = "Hello! I'm Toothless. \n" +
                 "What can I do for you today? \n" +
@@ -16,15 +19,23 @@ public class Duke {
         System.out.println(greeting);
 
         String bye = "bye";
+        String taskList = "list";
 
         while (true) {
 
-            String nextInput = scanner.nextLine();
+            String nextInput = scanner.nextLine().trim();
 
             if (nextInput.equals(bye)) {
                 break;
+            } else if (nextInput.equals(taskList)) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ": " + tasks.get(i));
+                }
+            } else {
+                tasks.add(nextInput);
+                System.out.println("Task " + nextInput + " has been added");
             }
-            System.out.println(nextInput + "\n---------------------------------");
+            System.out.println("\n---------------------------------");
         }
 
         System.out.println(farewell);
