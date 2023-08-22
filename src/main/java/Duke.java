c05c6afb07b60ceeb20aa4db34c75587b627fd8c
yyyaohhh <yyyaohhh@gmail.com>
2023-08-22 23:01:03 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,11 +10,22 @@ public class Duke {
 
         System.out.println("____________________________________________________________");
         System.out.println("Ko...ko...ko..nichi...wa!!! I... I am Gotoh... Hitori desu! Σ(っ °Д °;)っ");
-        System.out.println("You... can call me... Bocchi. Bocchi chan...");
+        System.out.println("You... can call me... Bocchi. They usually... call me Bocchi chan...");
         System.out.println("What can... can I do for you? (°□°;) ");
+        System.out.println("____________________________________________________________");
+        Scanner echoObject = new Scanner(System.in);
+        String echo = echoObject.nextLine();
+        while(!echo.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(echo);
+            System.out.println("____________________________________________________________");
+            Scanner nextEchoObject = new Scanner(System.in);
+            echo = nextEchoObject.nextLine();
+        }
         System.out.println("____________________________________________________________");
         System.out.println("B... b...bye bye!... And ... see you! (〃ω〃)");
         System.out.println("____________________________________________________________");
+
 
     }
 }
