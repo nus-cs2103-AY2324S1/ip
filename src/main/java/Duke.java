75d094220a91d141542b35b45f51a95370989515
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2023-08-22 23:59:39 +0800

    Level 2
    Add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,40 +1,69 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     private static String horizontalLine = "____________________________________________________________";
+    private String myName = "Quack-NKN";
+    private Scanner scanner = new Scanner(System.in);
+    private ArrayList<Task> taskList = new ArrayList<>();
 
-    public static void main(String[] args) {
-        System.out.println(horizontalLine);
-        String myName = "Quack-NKN";
+    public Duke() {
+        interact();
+    }
+
+    private void start() {
+        System.out.println(Duke.horizontalLine);
         System.out.print("Hello from ");
-        System.out.println(myName);
+        System.out.println(this.myName);
         System.out.println("What can I do for you?");
-        System.out.println(horizontalLine);
+        System.out.println(Duke.horizontalLine);
+    }
 
-        boolean isExit = false;
-        Scanner scanner = new Scanner(System.in);
-        while (!isExit) {
+    private void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(Duke.horizontalLine);
+    }
+
+    private void interact() {
+        this.start();
+        while (true) {
             // receive input
-            String command = scanner.nextLine();
-            System.out.println(horizontalLine);
+            String command = this.scanner.nextLine();
+            System.out.println(Duke.horizontalLine);
 
             // exit
             if (command.equals("bye")) {
-                isExit = true;
+                break;
             }
 
-            // echo
+            // show list
+            if (command.equals("list")) {
+                this.showList();
+            }
+
+            // add to list
             else {
-                echo(command);
+                this.addToList(command);
             }
-        }
 
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+            System.out.println(Duke.horizontalLine);
+        }
+        this.exit();
     }
 
-    private static void echo(String text) {
-        System.out.println(text);
-        System.out.println(horizontalLine);
+    private void addToList(String text) {
+        Task newTask = new Task(text);
+        this.taskList.add(newTask);
+        System.out.println("added: " + newTask);
+    }
+
+    private void showList() {
+        for (int i = 0; i < this.taskList.size(); i++) {
+            System.out.println((i + 1) + ". " + this.taskList.get(i));
+        }
+    }
+
+    public static void main(String[] args) {
+        new Duke();
     }
 }
