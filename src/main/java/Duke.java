16c3d8032871c0e93353253b340c08d1d4deb286
chonguschonguschongus <>
2023-08-22 10:50:52 +0800

    Level 0 changes1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I am Dukey.\n"
+                + "What can I do for you?");
+        System.out.println("Bye. Hope to see you again!");
     }
 }
