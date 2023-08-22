de120df366273e67ce78bcc66b5989f3f967b266
NgChunMan <chunman03@gmail.com>
2023-08-22 22:08:04 +0800

    Add: Level 0

---
+++ src/main/java/Evo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Evo {
+    public static void main(String[] args) {
+        String logo = " _____\n"
+                + "|  ___| \n"
+                + "| |___ __    __  _____ \n"
+                + "|  ___|\\ \\  / / |     | \n"
+                + "| |___  \\ \\/ /  |     |  \n"
+                + "|_____|  \\__/   |_____|  \n";
+        System.out.println("Hello from\n" + logo);
+
+        String welcomeMsg = "Hello! I'm Evo.\n" + "What can I do for you?\n";
+        String byeMsg = "Bye. Hope to see you again soon!";
+
+        System.out.println(welcomeMsg);
+        System.out.println(byeMsg);
+
+    }
+}
