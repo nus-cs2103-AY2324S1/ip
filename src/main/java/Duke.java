a013eee63f09fca8447bada8227113459b74015a
Yarn Meng <e0969262@u.nus.edu>
2023-08-22 23:05:42 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
     private static String GREETINGS = "Hello! I'm AChatBot\n" +
             "What can I do for you?";
@@ -31,6 +34,8 @@ public class Duke {
      * Abstracts a list. Note that items in the list cannot be removed as yet.
      */
     private static class ChatBotList {
+        private ArrayList<Item> list;
+
         /**
          * Encapsulates an item in the list
          */
@@ -141,23 +146,21 @@ public class Duke {
             }
         }
 
-        private Item[] list;
-        private int counter;
 
         public ChatBotList() {
-            this.list = new Item[100];
-            this.counter = 0;
+            this.list = new ArrayList<Item>();
         }
 
         public int getLength() {
-            return counter;
+            return this.list.size();
         }
         /**
          * Adds Item to List. Able to specify type.
          *
          * @param s    Substring of the input string, with the type removed.
          *             e.g. The user inputs "event a /from 2 /to 2". s will be "a /from 2 to 2"
-         * @param type The type of item
+         * @param type The type of item*
+         * @return The toString() of the Item added.
          */
         public String addToList(String s, String type) {
             switch (type) {
@@ -174,8 +177,7 @@ public class Duke {
                                 "Please use this syntax:\n" +
                                 "event <desc> /from <start> /to <end>");
                     }
-                    this.list[this.counter] = new Event(firstSplit[0], secondSplit[0], secondSplit[1]);
-                    this.counter++;
+                    this.list.add( new Event(firstSplit[0], secondSplit[0], secondSplit[1]));
                     break;
                 case "deadline":
                     String[] splitInput = s.split(" +/by +", 2);
@@ -184,65 +186,70 @@ public class Duke {
                                 "Please use this syntax:\n" +
                                 "deadline <desc> /by <deadline>");
                     }
-                    this.list[this.counter] = new Deadline(splitInput[0], splitInput[1]);
-                    this.counter++;
+                    this.list.add(new Deadline(splitInput[0], splitInput[1]));
                     break;
                 case "todo":
-                    this.list[this.counter] = new Todo(s);
-                    this.counter++;
+                    this.list.add(new Todo(s));
                     break;
                 default:
                     break;
             }
-            return this.list[this.counter-1].toString();
+            return this.list.get(this.list.size()-1).toString();
         }
 
         /**
          * Marks item at index given by user as completed.
          *
          * @param index Raw index given by user.
+         * @return The toString() of the item marked.
          */
-        public void markItem(int index) {
-            if (index <= -1 || index >= counter + 1 || counter == 0) {
+        public String markItem(int index) {
+            if (index <= 0 || index >= this.list.size() + 1 || this.list.size() == 0) {
                 throw new NotInChatBotListException();
             }
-            this.list[index - 1].markCompleted();
+            this.list.get(index - 1).markCompleted();
+            return this.list.get(index - 1).toString();
         }
 
         /**
          * Removes "completed" mark of item at index given by user.
          *
          * @param index Raw index given by user.
+         * @return The toString() of the item marked.
          */
-        public void unmarkItem(int index) {
-            if (index < -1 || index >= counter + 1) {
+        public String unmarkItem(int index) {
+            if (index <= 0 || index >= this.list.size() + 1) {
                 throw new NotInChatBotListException();
             }
-            this.list[index - 1].unmarkCompleted();
+            this.list.get(index - 1).unmarkCompleted();
+            return this.list.get(index - 1).toString();
         }
 
         /**
-         * Views item at index given by user.
+         * Deletes the item at index given by user.
          *
-         * @param index Raw index given by user.
+         * @param index Raw index given by use
+         * @return The toString() of the Item deleted.
          */
-        public String viewItem(int index) {
-            if (index < -1 || index >= counter + 1) {
+        public String deleteItem(int index) {
+            if (index <= 0 || index >= this.list.size() + 1) {
                 throw new NotInChatBotListException();
             }
-            return this.list[index - 1].toString();
+            Item itemToRemove = this.list.get(index - 1);
+            this.list.remove(index - 1);
+            return itemToRemove.toString();
         }
 
         @Override
         public String toString() {
             String rtnVal = "";
-            if (counter == 0) {
+            if (this.list.size() == 0) {
                 rtnVal = "List is empty.";
             } else {
-                for (int i = 0; i < this.counter - 1; i++) {
-                    rtnVal += (i + 1 + ".") + this.list[i].toString() + "\n";
+                for (int i = 0; i < this.list.size() - 1; i++) {
+                    rtnVal += (i + 1 + ".") + this.list.get(i).toString() + "\n";
                 }
-                rtnVal += (counter + ".") + this.list[counter - 1];
+                rtnVal += (this.list.size() + ".") + this.list.get(this.list.size() - 1);
             }
             return rtnVal;
         }
@@ -264,27 +271,33 @@ public class Duke {
                 System.out.println(list);
             } else {
                 String[] splitInput = input.split(" +", 2);
-                if (splitInput.length == 2 && splitInput[0].equals("mark") || splitInput[0].equals("unmark")) {
-                    try {
-                        int idx = Integer.parseInt(splitInput[1]);
-                        if (splitInput[0].equals("mark")) {
-                            list.markItem(idx);
-                            System.out.println("Nice! I've marked this task as done:");
-                        } else {
-                            list.unmarkItem(idx);
-                            System.out.println("OK, I've marked this task as not done yet:");
+                if (splitInput[0].equals("mark") || splitInput[0].equals("unmark") || splitInput[0].equals("delete")) {
+                    if (splitInput.length == 1) {
+                        System.out.println("☹ OOPS!!! You did not specify which task you want to " + splitInput[0] + ".\nPlease use this syntax:\n" +
+                                splitInput[0] + " <index>");
+                    } else {
+                        try {
+                            int idx = Integer.parseInt(splitInput[1]);
+                            if (splitInput[0].equals("mark")) {
+                                String res = list.markItem(idx);
+                                System.out.println("Nice! I've marked this task as done:\n" + "  " + res);
+                            } else if (splitInput[0].equals("unmark")) {
+                                String res = list.unmarkItem(idx);
+                                System.out.println("OK, I've marked this task as not done yet:\n" + "  " + res);
+                            } else {
+                                String res = list.deleteItem(idx);
+                                System.out.println("Noted. I've removed this task:\n" + "  " + res);
+                            }
+                        } catch (NumberFormatException e) {
+                            System.out.println("Please input a valid number for your index!");
+                        } catch (NotInChatBotListException e) {
+                            System.out.println("Make sure your item is in the list!\n" +
+                                    "You may check using the \"list\" command.");
                         }
-                        System.out.println("  " + list.viewItem(idx));
-                    } catch (NumberFormatException e) {
-                        System.out.println("Please input a valid number for your index!");
-                    }
-                    catch (NotInChatBotListException e) {
-                        System.out.println("Make sure your item is in the list!\n" +
-                                "You may check using the \"list\" command.");
                     }
                 } else if (splitInput[0].equals("event")) {
                     if (splitInput.length < 2) {
-                        System.out.println("☹ OOPS!!! The description of a event cannot be empty.\n" +
+                        System.out.println("☹ OOPS!!! The description of a event cannot be empty.\nPlease use this syntax:\n" +
                                 "event <desc> /from <start> /to <end>");
                     } else {
                         try {
@@ -297,7 +310,7 @@ public class Duke {
                     }
                 } else if (splitInput[0].equals("deadline")) {
                     if (splitInput.length < 2) {
-                        System.out.println("☹ OOPS!!! The description of a deadline cannot be empty.\n"+
+                        System.out.println("☹ OOPS!!! The description of a deadline cannot be empty.\nPlease use this syntax:\n"+
                                 "deadline <desc> /by <deadline>");
                     } else {
                         try {
@@ -317,7 +330,6 @@ public class Duke {
                         System.out.println("Now you have " + list.getLength() + " tasks in the list.");
                     }
                 } else {
-                    list.addToList(input, "todo");
                     System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
             }
