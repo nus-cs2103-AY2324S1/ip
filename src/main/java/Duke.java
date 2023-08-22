64021382219dff8ffd410c036933872cbe0b4be1
junnengsoo <soojunneng01@gmail.com>
2023-08-22 16:28:56 +0800

    Complete level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = ">. <\n";
+        String name = "your father";
+        String line = "_________________________\n";
+        System.out.println(logo +
+                            line +
+                            "Hello! I'm " + name + "\n" +
+                            "What can I do for you?\n" +
+                            line +
+                            "Bye. Hope to see you again soon!\n" +
+                            line);
     }
 }
