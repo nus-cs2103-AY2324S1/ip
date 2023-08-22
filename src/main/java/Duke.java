15a2322a8616a46e3fd602bc08561e434dfe2aab
butteredyakiimo <nicoleiscolder@gmail.com>
2023-08-23 00:39:45 +0800

    Store user input, list out all tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,33 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        String line = "\n---------------------------------------\n";
+        String line = "---------------------------------------\n";
         String intro = "Hey there! I'm BUTTER.\n" +
-                "How can I help you today?";
-        String bye = "Signing off, see you later!";
+                "How can I help you today?\n";
+        String bye = "Signing off, see you later!\n";
 
         System.out.println(line + intro + line); //greeting
         String task = "";
+        ArrayList<String> list = new ArrayList<>();
+
         while (true) {
             System.out.println("Add a task: ");
             Scanner scanner = new Scanner(System.in); //create Scanner object
             task = scanner.nextLine();
             if (task.equals("bye")) {
                 break;
+            } else if (task.equals("list")) {
+                String result = "";
+                for (int i = 0; i < list.size(); i++) {
+                    int index = i + 1;
+                    result += index + ". " + list.get(i) + "\n";
+                }
+                System.out.println(result + line);
             } else {
-                System.out.println("New task added: " + task + line);
+                list.add(task);
+                System.out.println("New task added: " + task + "\n" + line);
             }
         }
         System.out.println(line + bye + line);
