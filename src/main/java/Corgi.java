dbcee84638adaf96d2aa75ca457371ef7363c3a8
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-08-22 18:14:47 +0800

    rename, greet, exit

---
+++ src/main/java/Corgi.java
@@ -0,0 +1,13 @@
+public class Corgi {
+    public static void main(String[] args) {
+        String logo = "  ____ ___  ____   ____ ___\n"
+                + " / ___/ _ \\|  _ \\ / ___|_ _|\n"
+                + "| |  | | | | |_) | |  _ | |\n"
+                + "| |__| |_| |  _ <| |_| || |\n"
+                + " \\____\\___/|_| \\_\\\\____|___|\n";
+        System.out.println(logo);
+        System.out.println("Hello! I'm Corgi!");
+        System.out.println("What can I do for you, hooman?");
+        System.out.println("Bye, take care and see you soon! *tail wags*");
+    }
+}
