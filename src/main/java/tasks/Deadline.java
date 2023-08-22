33bb53b43a9ac1f064296e54b8ec6a7a5c329191
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-08-22 20:12:19 +0800

    Add Deadline class

---
+++ src/main/java/tasks/Deadline.java
@@ -0,0 +1,15 @@
+package tasks;
+
+public class Deadline extends Task{
+    private String by;
+
+    public Deadline(String desc, String by) {
+        super(desc);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
