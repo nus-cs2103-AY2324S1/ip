c3477fe85481913482de66b49cca8dbf54bada82
emzm2023 <meeselevels@elizabeths-MacBook-Air-3.local>
2023-08-22 19:04:44 +0800

    implemented echo and readInput methods.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     /** The name of the chatbot. */
     private static final String NAME = "Bro";
+
+    /** The scanner for the chatbot. */
+    private Scanner scanner;
+
+    /** Constructor for the chatbot. */
+    public Duke() {
+        this.scanner = new Scanner(System.in);
+    }
 
     /** Greets the user. */
     public void greet() {
@@ -12,9 +22,29 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
+    /** Echoes commands entered by user.
+     *
+     * @param input The command entered by the user.
+     */
+    public void echo(String input) {
+        System.out.println(input + "\n");
+    }
+
+    /** Reads user input. Exits if user input is "bye", else, echoes commands. */
+    public void readInput() {
+        System.out.print("> ");
+        String userInput = this.scanner.nextLine();
+
+        if (userInput.equals("bye")) {
+            this.exit();
+        } else {
+            this.echo(userInput);
+            this.readInput();
+        }
+    }
+
     public static void main(String[] args) {
         Duke chatbot = new Duke();
-        chatbot.greet();
-        chatbot.exit();
+        chatbot.readInput();
     }
 }
