1d6b8c3269f731717bd598fcf67cd134473f6a91
s0ngyang <keesongyang4@gmail.com>
2023-08-22 22:06:44 +0800

    Renamed Chatbot

---
+++ src/main/java/Nexus.java
@@ -0,0 +1,7 @@
+public class Nexus {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm NEXUS");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
