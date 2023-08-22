242ea3e5af24d27d6deffa316e81ea10b3d5a6b2
LicongHuang <alexliconghuang@gmail.com>
2023-08-22 23:34:36 +0800

    Refactored code to be Object Oriented and complete Task 5

--- src/main/java/Cheese.java
+++ src/main/java/Cheese.java
@@ -3,72 +3,170 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Cheese {
-  private List<String> cheeseStack = new ArrayList<String>();
+    private List<Task> CheeseStack = new ArrayList<Task>();
 
-  public void speak() {
-    Scanner myObj = new Scanner(System.in);
-    String echoedInput = "";
-    System.out.println("\t-----------------------------------------");
-    System.out.println("\tHello, I'm Cheese");
-    System.out.println("\tWhat can I do for you?");
-    System.out.println("\t------------------------------------------");
-    while (true) {
-      echoedInput = myObj.nextLine();
-      commandResponse(echoedInput);
-      if (echoedInput.equals("bye")) {
-        break;
-      }
-    }
-    System.out.println("\t-----------------------------------------");
-  }
 
-  private void addToList(String input) {
-    cheeseStack.add("[ ] " + input);
-  }
-
-  private void markItem(int index) {
-    String input = cheeseStack.get(index);
-    String[] inputArray = new String[2];
-    inputArray = input.split("]");
-    inputArray[0] = "[X] ";
-    input = inputArray[0] + inputArray[1];
-    cheeseStack.set(index, input);
-  }
-
-  private void commandResponse(String input) {
-    String inputSplit[] = input.split(" ");
-    switch(inputSplit[0]) {
-      case "mark":
-        markItem(Integer.parseInt(inputSplit[1]) - 1);
+    public void speak() {
+        Scanner myObj = new Scanner(System.in);
+        String echoedInput = "";
         System.out.println("\t-----------------------------------------");
-        System.out.println("\tNice! I've marked this task as done:");
-        System.out.println("\t" + cheeseStack.get(Integer.parseInt(inputSplit[1]) - 1));
-        System.out.println("\t-----------------------------------------");
-        break;
-      case "bye":
-        System.out.println("\tBye. Hope to see you again soon!");
-        break;
-      case "list":
-        System.out.println("\t-----------------------------------------");
-        System.out.println("\tHere are the tasks in your list:");
-        for (int i = 0; i < cheeseStack.size(); i++) {
-          System.out.println("\t" + (i + 1) + ". " + cheeseStack.get(i));
+        System.out.println("\tHello, I'm Cheese");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t------------------------------------------");
+        while (true) {
+            echoedInput = myObj.nextLine();
+            commandResponse(echoedInput);
+            if (echoedInput.equals("bye")) {
+                break;
+            }
         }
         System.out.println("\t-----------------------------------------");
-        break;
-      default:
-        addToList(input);
-        System.out.println("\t-----------------------------------------");
-        System.out.println("\tadded: " + input);
-        System.out.println("\t-----------------------------------------");
-        break;
-
     }
-  }
 
-  public static void main(String[] args) {
-    Cheese cheese = new Cheese();
-    cheese.speak();
-  }
+    private boolean isCommand(String input) {
+        String[] inputSplit = input.split(" ");
+        switch(inputSplit[0]) {
+            case "mark":
+            case "bye":
+            case "list":
+            case "todo":
+            case "deadline":
+            case "event":
+                return true;
+            default:
+                return false;
+        }
+    }
 
+    private void addToList(String input) {
+        String[] inputSplit = input.split(" ", 2);
+        String command = inputSplit[0];
+
+        try {
+            if (inputSplit.length < 2 && isCommand(input))  {
+                throw new IllegalArgumentException("The description of a " + command + " cannot be empty.");
+            } else if (inputSplit.length < 2 && !isCommand(input)) {
+                throw new IllegalArgumentException("OOPS!!! I'm sorry, but I don't know what that means :-(");
+            }
+
+            String taskDescription = inputSplit[1];
+
+            Task newTask = null;
+
+            switch (command) {
+                case "todo":
+                    newTask = new Task('T', taskDescription);
+                    break;
+                case "deadline":
+                    String[] deadlineInfo = taskDescription.split("/by", 2);
+                    newTask = new Task('D', deadlineInfo[0].trim(), deadlineInfo[1].trim());
+                    break;
+                case "event":
+                    String[] eventInfo = taskDescription.split("/from", 2);
+                    String[] eventTime = eventInfo[1].split("/to", 2);
+                    newTask = new Task('E', eventInfo[0].trim(), eventTime[0].trim(), eventTime[1].trim());
+                    break;
+                default:
+                    throw new IllegalArgumentException("OOPS!!! I'm sorry, but I don't know what that means :-(");
+            }
+
+            if (newTask != null) {
+                CheeseStack.add(newTask);
+                System.out.println("\t-----------------------------------------");
+                System.out.println("\tGot it. I've added this task:");
+                System.out.println("\t  " + newTask.toString());
+                System.out.println("\tNow you have " + CheeseStack.size() + " tasks in the list.");
+                System.out.println("\t-----------------------------------------");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("\t-----------------------------------------");
+            System.out.println("\t☹ " + e.getMessage());
+            System.out.println("\t-----------------------------------------");
+        }
+    }
+    private void commandResponse(String input) {
+        String inputSplit[] = input.split(" ");
+        switch(inputSplit[0]) {
+            case "mark":
+                markItem(Integer.parseInt(inputSplit[1]) - 1);
+                System.out.println("\t-----------------------------------------");
+                System.out.println("\tNice! I've marked this task as done:");
+                System.out.println("\t" + CheeseStack.get(Integer.parseInt(inputSplit[1]) - 1));
+                System.out.println("\t-----------------------------------------");
+                break;
+            case "bye":
+                System.out.println("\tBye. Hope to see you again soon!");
+                break;
+            case "list":
+                System.out.println("\t-----------------------------------------");
+                System.out.println("\tHere are the tasks in your list:");
+                for (int i = 0; i < CheeseStack.size(); i++) {
+                    System.out.println("\t" + (i + 1) + ". " + CheeseStack.get(i));
+                }
+                System.out.println("\t-----------------------------------------");
+                break;
+            default:
+                addToList(input);
+                break;
+        }
+    }
+
+    private void markItem(int index) {
+        if (index >= 0 && index < CheeseStack.size()) {
+            Task task = CheeseStack.get(index);
+            task.markAsDone();
+        }
+    }
+
+    public static void main(String[] args) {
+        Cheese Cheese = new Cheese();
+        Cheese.speak();
+    }
+
+    private class Task {
+        private char type;
+        private String description;
+        private String dateTime1;
+        private String dateTime2;
+        private boolean isDone;
+
+        public Task(char type, String description) {
+            this.type = type;
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public Task(char type, String description, String dateTime1) {
+            this.type = type;
+            this.description = description;
+            this.dateTime1 = dateTime1;
+            this.isDone = false;
+        }
+
+        public Task(char type, String description, String dateTime1, String dateTime2) {
+            this.type = type;
+            this.description = description;
+            this.dateTime1 = dateTime1;
+            this.dateTime2 = dateTime2;
+            this.isDone = false;
+        }
+
+        public void markAsDone() {
+            this.isDone = true;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("[").append(type).append("][").append(isDone ? "X" : " ").append("] ").append(description);
+            if (dateTime1 != null) {
+                sb.append(" (from: ").append(dateTime1);
+                if (dateTime2 != null) {
+                    sb.append(" to: ").append(dateTime2);
+                }
+                sb.append(")");
+            }
+            return sb.toString();
+        }
+    }
 }
