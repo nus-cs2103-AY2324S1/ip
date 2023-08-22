1fc6b36af98777ce5cb1deea854ab1533e77f65c
NicholasTng010601 <nicholastng@hotmail.com>
2023-08-22 21:54:35 +0800

    Created hello and goodbye functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,22 @@
 public class Duke {
+    static String name = "Nichbot";
+
+    // Function to say introduce the chatbot
+    public static void sayHello() {
+        String greet = String.format(
+                "____________________________________________________________\n" +
+                        " Hello! I'm %s\n" +
+                        " What can I do for you?\n" +
+                        "____________________________________________________________\n", name);
+        System.out.println(greet);
+    }
+
+    // Function to say goodbye
+    public static void sayGoodBye() {
+        String bye =  ("Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________");
+        System.out.println(bye);
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +24,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        sayHello();
+        sayGoodBye();
     }
 }
