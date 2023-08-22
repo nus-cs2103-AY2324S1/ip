c623a4815e53db241d2c0d16bc10e2c2b7b65234
Zhetao <quzhetao2001@gmail.com>
2023-08-23 00:26:26 +0800

    Add Event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,17 @@
+public class Event extends Task {
+
+    private String from;
+    private String to;
+
+    public Event(String task, String from, String to) {
+        super(task);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + "(from: " + this.from + "to: " + this.to + ")";
+    }
+
+}
