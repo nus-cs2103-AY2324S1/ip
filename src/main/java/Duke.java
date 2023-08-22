048f0e7d73fdcb7011feb97ea7205e806af67c3e
Nicholas <nicholaschiazhijie2001@yahoo.com>
2023-08-22 16:30:15 +0800

    amended level 1 submission

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,34 +2,38 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // String logo = " ____ _ \n"
+        // + "| _ \\ _ _| | _____ \n"
+        // + "| | | | | | | |/ / _ \\\n"
+        // + "| |_| | |_| | < __/\n"
+        // + "|____/ \\__,_|_|\\_\\___|\n";
+        // System.out.println("Hello from\n" + logo);
 
+        // Print greeting
         String greeting = "____________________________________________________________\n"
                 + "Hello! I'm Lorem\n"
                 + "What can I do for you?\n"
                 + "____________________________________________________________\n";
 
         System.out.println(greeting);
+
+        // initialise scanner to detect user input
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
 
-        String ending = "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
-
-        System.out.println(greeting);
+        // while loop to continuously take in inputs until user types bye
         while (!userInput.equals("bye")) {
             System.out.println("____________________________________________________________");
             System.out.println(userInput);
             System.out.println("____________________________________________________________");
             userInput = sc.nextLine();
         }
+
+        // terminate program: close scanner, print ending message
         sc.close();
+        String ending = "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
         System.out.println(ending);
     }
 }
