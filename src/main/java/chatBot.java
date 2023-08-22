ca8df770c0aec1cd2a7b17d021ec0f40a979a315
Rayshawn <rayshawntan124@gmail.com>
2023-08-22 23:34:54 +0800

    Change Duke class to chatBot class; Change output message to skeletal version of greetings and bot name

---
+++ src/main/java/chatBot.java
@@ -0,0 +1,10 @@
+public class chatBot {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n" +
+                " Hello! I'm Desolute\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
+    }
+}
