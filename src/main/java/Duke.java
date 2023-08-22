d00f3ef35d5c55e69302869edd37ccec2ec87c81
Jie Xiang <n.jiexiang@gmail.com>
2023-08-22 20:56:52 +0800

    Add echo and exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,26 @@
-public class Duke {
-    public static void main(String[] args) {
-        String greeting = "____________________________________________________________\n"
-                        + "Hello! I'm EnPassant\n"
-                        + "What can I do for you?\n"
-                        + "____________________________________________________________\n"
-                        + "Bye. Hope to see you again soon!\n"
-                        + "____________________________________________________________\n";
+import java.util.Scanner;
 
-        System.out.println(greeting);
+public class Duke {
+    private static final String line = "____________________________________________________________\n";
+    private static final String greetingMessage = "Hello! I'm EnPassant\n"
+                                                + "What can I do for you?\n";
+    private static final String exitMessage = "Great heavens! Hope to see you again soon!\n";
+
+    private static void printWithLines(String message) {
+        System.out.print(line + message + line);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        printWithLines(greetingMessage);
+        while (true) {
+            String str = sc.nextLine();
+            if (!str.equals("bye")) {
+                printWithLines(str + '\n');
+            } else {
+                printWithLines(exitMessage);
+                break;
+            }
+        }
     }
 }
