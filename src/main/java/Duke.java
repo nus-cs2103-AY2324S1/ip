747d1e9e8a0b75b3bfe5d2d2eac560887a78b761
zhiruo <e0967827@u.nus.edu>
2023-08-22 23:04:33 +0800

    completed level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -8,19 +9,25 @@ public class Duke {
         String exit = " Bye. Hope to see you again soon!\n";
         System.out.println(introduction);
 
-        //Level 1
+        //Level 2
         String input;
         Scanner scan = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
 
         while (true) {
             input = scan.nextLine();
             if (input.equals("bye")) {
                 System.out.println(exit);
                 break;
+            } else if (input.equals("list")) {
+                list.forEach(item -> {
+                    System.out.println((list.indexOf(item) + 1) + ". " + item);
+                });
             } else {
-                System.out.println(input);
+                System.out.println("added: " + input);
+                list.add(input);
             }
         }
-    }
 
+    }
 }
