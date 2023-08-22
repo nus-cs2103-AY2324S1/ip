786589942e0dae06f0cbe6141dc585e67ad95111
Yuheng <guoyuheng1year@gmail.com>
2023-08-22 20:09:33 +0800

    "Level 0"

---
+++ src/main/java/Mimi.java
@@ -0,0 +1,21 @@
+
+public class Mimi {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+
+        String LINE = "_________________________________________________\n";
+
+        System.out.println(
+                LINE
+                + "Hello! I'm Mimi.\n"
+                + "What can I do for you?\n"
+                + LINE
+                + "Bye. Hope to see you again soon!\n"
+                + LINE
+        );
+    }
+}
