79f0ee3f9a8ef1fc41493eab0f112813ca17ee95
Andre Sim Jing Xiang <e0968908@u.nus.edu>
2023-08-22 15:10:34 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.*;
+
 public class Duke {
+    public static String separator = "____________________________________________________________";
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "| |_| | | | | |/ / _ \\\n"
+                + "| ___/| |_| |    | __/\n"
+                + "| |    \\__,_|_|\\_\\___|\n"
+                + "|_|";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Salutations! I am\n" + logo + "\nAn exceedingly verbose conversation simulation program");
+        System.out.println(separator);
+
+        System.out.println("It appears that the user has decided to close the program as indicated by the command of which this is the function being issued and therefore I shall bid thee farewell and wish thee great fortune in your future endeavors");
     }
+
+
 }
