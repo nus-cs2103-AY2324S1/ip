e0996cd204fab82226411230af35778930ea3b57
wui-hong <khoowuihong@yahoo.com.sg>
2023-08-22 09:57:05 +0800

    Complete Level 1 Echo

--- src/main/java/Hong.java
+++ src/main/java/Hong.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Hong {
     public static void main(String[] args) {
 
-        System.out.println("Hello! I'm Hong\nWhat can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+        String line = "---------------------------------------------------------\n";
+        String firstMessage = line + "Hello! I'm Hong\nWhat can I do for you?\n" + line;
+        System.out.println(firstMessage);
+        Scanner myObj = new Scanner(System.in);
+        while (true) {
+            String userInput = myObj.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            } else {
+                String currentMessage = line + userInput + "\n" + line;
+                System.out.println(currentMessage);
+            }
+        }
+        String exitMessage = line + "Bye. Hope to see you again soon!\n" + line;
+        System.out.println(exitMessage);
     }
 }
