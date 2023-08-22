6a57b9669c2a7e908148cd7e06b9fa40d147c4b1
feifeiraindrops <sunyufei.sylvia@gmail.com>
2023-08-23 00:47:52 +0800

    "Renamed the bot and changed greeting msg"

---
+++ src/main/java/BenBen.java
@@ -0,0 +1,18 @@
+public class BenBen {
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+
+        String line ="_______________________________________\n";
+        System.out.println(line);
+        System.out.println("Hello! I'm BenBen.\n" +
+                "What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
