eb43445f23690f14c016dbdf2b21788c43aa9160
alyssaongyxE <ongyixian@gmail.com>
2023-08-23 06:34:06 +0800

    Increment to Level-0

--- src/main/java/Ally.java
+++ src/main/java/Ally.java
@@ -11,11 +11,13 @@ public class Ally {
         System.out.println(line);
     }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
         Ally ally = new Ally();
+        ally.start();
+
     }
 }
