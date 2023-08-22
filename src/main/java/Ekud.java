2ac2e306eb38013a80f2a6d79f2f3e12d5c57ac3
J-hta-n <e0969121@u.nus.edu>
2023-08-23 02:31:14 +0800

    Change echo to user text list functionality

--- src/main/java/Ekud.java
+++ src/main/java/Ekud.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Ekud {
     String horizontalLine = "##***~-~-~-~-~-~--~-~-~-~-~-~***##";
     String intro = "Hello there! I'm Ekud.\nWhat can I do for you? :O";
     String outro = "Goodbye, have a nice day! :p";
+    List<String> userTextList = new ArrayList<>();
     public void intro() {
         this.echo(this.intro);
     }
@@ -15,15 +18,31 @@ public class Ekud {
                 message,
                 this.horizontalLine));
     }
+    public void addToList(String text) {
+        userTextList.add(text);
+        this.echo("added: " + text);
+    }
+    public void showList() {
+        System.out.println(horizontalLine);
+        int len = userTextList.size();
+        for (int i = 0; i < len; i++) {
+            System.out.println(String.format("%d. %s", i + 1, userTextList.get(i)));
+        }
+        System.out.println(horizontalLine);
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Ekud chatbot = new Ekud();
         chatbot.intro();
-        String userCommand = scanner.next();
+        String userCommand = scanner.nextLine();
         while (!userCommand.equals("end")) {
-            chatbot.echo(userCommand);
-            userCommand = scanner.next();
+            if (userCommand.equals("list")) {
+                chatbot.showList();
+            } else {
+                chatbot.addToList(userCommand);
+            }
+            userCommand = scanner.nextLine();
         }
         chatbot.outro();
     }
