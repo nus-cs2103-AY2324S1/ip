16b59df9219462b0353fc741d36dc88d61fb245f
Kota Sudarshan Karanth <sudarshan2402@gmail.com>
2023-08-22 15:45:31 +0800

    Created new file "Harvard.java" in order to print skeletal greeting for chatbot.

---
+++ src/main/java/Harvard.java
@@ -0,0 +1,10 @@
+public class Harvard {
+    public static void main(String[] args) {
+        String line = "----------------------------------------";
+        System.out.println(line);
+        System.out.println("Hello! I'm Harvard\nWhat can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
