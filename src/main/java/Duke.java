b0cd9d27ada17767d1f8f927bfb4e702fbcf00bc
Nguyen Cao Duy <nguyen.cao.duy.0303@gmail.com>
2023-08-22 11:54:12 +0800

    Inital commit with basic greetings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
 public class Duke {
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+
+    public static final String LOGO = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    public static final String NAME = "Duke";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        hello();
+        bye();
+    }
+
+    public static void hello() {
+        printLine();
+        System.out.println(LOGO);
+        System.out.println("Hello! I'm " + NAME);
+        System.out.println("What can I do for you?");
+        printLine();
+    }
+
+    public static void bye() {
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    private static void printLine() {
+        System.out.println(HORIZONTAL_LINE);
     }
 }
