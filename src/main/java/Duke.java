25bad7eeaafeda224e51f6140f2bcde7986441dc
Tan Boon Khong <tanboonkhong246@gmail.com>
2023-08-22 14:58:45 +0800

    Implement Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.printLine();
+        duke.printGreeting();
+        duke.printLine();
+        duke.printBye();
+        duke.printLine();
+    }
+
+    public void printGreeting() {
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+    }
+
+    public void printBye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public void printLine() {
+        System.out.println("____________________________________________________________");
     }
 }
