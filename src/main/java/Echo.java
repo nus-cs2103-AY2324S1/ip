a6fb4787cebb3ea538d8c9ae6e31eb82df5df261
JasonRay168 <e0985893@u.nus.edu>
2023-08-22 20:23:19 +0800

    Echo

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * The main class for Echo Chatbot
+ *
+ * @author Jason Ray
+ */
 public class Echo {
     public static void main(String[] args) {
-        String LINE_BREAK = "____________________________________________________________";
+        Scanner sc = new Scanner(System.in);
+
+        String LINE_BREAK = "----------------------------------------------------------";
         System.out.println(LINE_BREAK);
         System.out.println("Welcome. My name is Echo");
         System.out.println("What can I do for you?");
-        System.out.println(LINE_BREAK);
-        System.out.println("I hope you enjoy my service.");
-        System.out.println(LINE_BREAK);
+        System.out.println(LINE_BREAK + "\n");
+        System.out.println("Type 'bye' to exit\n");
+
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(LINE_BREAK);
+            System.out.println(input);
+            System.out.println(LINE_BREAK + "\n");
+            input = sc.nextLine();
+        }
+        if (input.equals("bye")) {
+            System.out.println(LINE_BREAK);
+            System.out.println("I hope you enjoy my service. Thank you and goodbye");
+            System.out.println(LINE_BREAK);
+        }
     }
 }
