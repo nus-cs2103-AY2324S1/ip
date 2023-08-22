07b560564a74323963bc620c5edf3b968abfc497
Hanjoo Kim <hachookimhanjoo@gmail.com>
2023-08-22 22:22:44 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String horizontalLine = "____________________________________________________";
+        String horizontalLine = "\t____________________________________________________";
         System.out.println(horizontalLine);
-        System.out.println("Hello! I'm Ari.");
-        System.out.println("What can I do for you?");
+        System.out.println("\tHello! I'm Ari.");
+        System.out.println("\tWhat can I do for you?");
         System.out.println(horizontalLine);
-        System.out.println("Bye. Hope to see you again soon!");
+        echo();
+        System.out.println(horizontalLine);
+        System.out.println("\tBye. Hope to see you again soon!");
         System.out.println(horizontalLine);
     }
+
+    public static void echo() {
+        String horizontalLine = "\t____________________________________________________";
+
+        Scanner input = new Scanner(System.in);
+        String commands = input.nextLine();
+
+        if (commands.equals("bye")) {
+            return;
+        }
+
+        boolean inLoop = true;
+
+        while (inLoop) {
+            System.out.println(horizontalLine);
+            System.out.println("\t" + commands);
+            System.out.println(horizontalLine);
+            commands = input.nextLine();
+            if (commands.equals("bye")) {
+                inLoop = false;
+            }
+        }
+    }
+
 }
