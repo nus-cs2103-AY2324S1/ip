cc4f941d0e7e992b6cb5d3070d2ae35a23ed4e24
Nicole Ng <nicol3slaw@gmail.com>
2023-08-22 20:09:13 +0800

    feat: echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -8,6 +10,17 @@ public class Duke {
 //        System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Sara");
         System.out.println("What can I do for you?");
+        Scanner scan = new Scanner(System.in);
+
+        while(true) {
+            String userInput = scan.nextLine();
+            System.out.println(userInput);
+
+            if(userInput.equalsIgnoreCase("bye")){
+                break;
+            }
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
