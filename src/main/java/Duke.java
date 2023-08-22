017bd11ca0542609defddc34257ab3b71a83c3bd
Sasmik23 <mikhilanand@gmail.com>
2023-08-22 23:17:06 +0800

    Level1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,21 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-
-
 public class Duke {
     public static void main(String[] args) {
 
         System.out.println("Hello! I'm Mikhil" + '\n' + "What can I do for you");
-        System.out.println("Bye. Hope to see you again soon!");
+        boolean flag = true;
+        while (flag == true) {
+            Scanner userInput = new Scanner(System.in);
+            String input = userInput.nextLine();
+
+            if (input.equals("bye")) {
+                flag = false;
+                System.out.println("Bye. Hope to see you again soon!");
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
