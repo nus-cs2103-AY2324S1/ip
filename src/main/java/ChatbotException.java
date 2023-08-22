34158bb883bf2d793184fde88f998f2c79ae4219
Wern <dev@wern.cc>
2023-08-23 01:26:32 +0800

    add new chatbot exception classes

---
+++ src/main/java/ChatbotException.java
@@ -0,0 +1,5 @@
+public class ChatbotException extends Exception {
+    ChatbotException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ChatbotRuntimeException.java
@@ -0,0 +1,5 @@
+public class ChatbotRuntimeException extends RuntimeException {
+    ChatbotRuntimeException(String message) {
+        super(message);
+    }
+}
