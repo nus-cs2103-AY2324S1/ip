30da4222710c1fa10c841f1c3a46385c9837db9a
Julius Bryan <67666355+juliusgambe@users.noreply.github.com>
2023-08-22 11:01:44 +0800

    feat: echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    static String chatbotName = "Gobble Gobble";
+    static String lineSeparator = "____________________________________________________________";
+
     public static void main(String[] args) {
-        // Greet
-        String greetString = "____________________________________________________________\n" +
-                " Hello! I'm GobbleGobble \n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
-        System.out.println(greetString);
+        System.out.println(Duke.lineSeparator + "\n" + "Hello! I'm " + Duke.chatbotName + "\n" +
+                "What can I do for you?" + "\n" + Duke.lineSeparator);
+
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            String userInput = scanner.nextLine().trim(); // trim() removes leading and trailing spaces
+
+            if (userInput.equals("bye")) {
+                System.out.println(Duke.lineSeparator + "\n" + "Bye. Hope to see you again soon!" + "\n" + Duke.lineSeparator);
+                break;
+            } else {
+                System.out.println(Duke.lineSeparator + "\n" + userInput + "\n" + Duke.lineSeparator);
+            }
+        }
+
+        scanner.close();
     }
 }
