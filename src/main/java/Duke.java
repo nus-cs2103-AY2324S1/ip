0789b26a2224c5be3dde8e3cce68d650525a4ae8
Jackie <jackieguo126@gmail.com>
2023-08-22 20:56:32 +0800

    incr: level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -26,16 +26,34 @@ public class Duke {
         return stringFormat(new String[]{input});
     }
 
-    private static String addToList(String taskName) {
-        Task taskObj = new Task(taskName);
+    private static String addToList(String description) {
+        // Split by the first " " into type, and task details
+        String taskType = description.split(" ", 2)[0];
+        String taskDetails = description.split(" ", 2)[1];
+        Task taskObj;
+        if (taskType.equals("deadline")) {
+            taskObj = new Deadline(taskDetails);
+        } else if (taskType.equals("event")) {
+            taskObj = new Event(taskDetails);
+        } else {
+            taskObj = new Todo(taskDetails);
+        }
+
         lst.add(taskObj);
-        return stringFormat(new String[]{"added: " + taskName});
+        return stringFormat(new String[]{"New task added: ", "\t" + taskObj.toString(),
+                "You now have " + lst.size() + (lst.size() == 1 ? " task!" : " tasks!")});
     }
 
     private static String displayList() {
-        String[] tasks = new String[lst.size()];
+        String[] tasks = new String[lst.size() + 1];
+        if (lst.isEmpty()) {
+            tasks[0] = "You currently have no tasks.";
+        } else {
+            tasks[0] = "Here are your tasks!";
+        }
+
         for (int i = 0; i < lst.size(); i++) {
-            tasks[i] = (i + 1) + ". " + lst.get(i).toString();
+            tasks[i + 1] = (i + 1) + ". " + lst.get(i).toString();
         }
         return stringFormat(tasks);
     }
@@ -43,7 +61,7 @@ public class Duke {
     private static String markDoneOrNot(int index, boolean doneOrNot) {
         lst.get(index - 1).SetDoneOrNot(doneOrNot);
         String statement = doneOrNot ? "Nice! You completed a task!" : "... This is now undone.";
-        return stringFormat(new String[]{statement, lst.get(index - 1).toString()});
+        return stringFormat(new String[]{statement, "\t" + lst.get(index - 1).toString()});
     }
 
     public static void main(String[] args) {
@@ -62,7 +80,7 @@ public class Duke {
             if (nextLine.equals("list")) {
                 System.out.println(displayList());
 
-            } else if (nextLine.contains("mark")) {
+            } else if (nextLine.contains("mark")) { // if command is to mark or unmark
                 String[] markIndex = nextLine.split(" ");
                 int index = Integer.parseInt(markIndex[1]);
                 boolean doneOrNot = true;
@@ -76,7 +94,7 @@ public class Duke {
                 sc.close();
                 break;
 
-            } else {
+            } else { // if command is to add tasks
                 System.out.println(addToList(nextLine));
             }
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,8 +1,9 @@
+import javax.swing.plaf.synth.SynthRadioButtonMenuItemUI;
 import java.util.*;
 
 public class Task {
-    private String name;
-    private boolean done;
+    public String name;
+    public boolean done;
 
     public Task(String name) {
         this.name = name;
@@ -12,10 +13,48 @@ public class Task {
     public void SetDoneOrNot(boolean doneOrNot) {
         this.done = doneOrNot;
     }
+}
+
+class Todo extends Task {
+    public Todo(String name) {
+        super(name);
+    }
 
     @Override
     public String toString() {
         String done = this.done ? "[X]" : "[ ]";
-        return done + " " + this.name;
+        return "[T]" + done + " " + this.name;
+    }
+}
+
+class Deadline extends Task {
+    String due;
+    public Deadline(String description) {
+        super(description.split(" /by ")[0]);
+        this.due = description.split(" /by ")[1];
+    }
+
+    @Override
+    public String toString() {
+        String done = this.done ? "[X]" : "[ ]";
+        return "[D]" + done + " " + this.name
+                + " (by: " + this.due + ")";
+    }
+}
+
+class Event extends Task {
+    String start;
+    String end;
+    public Event(String description) {
+        super(description.split(" /from ")[0]);
+        this.start = description.split(" /from ")[1].split(" /to ")[0];
+        this.end = description.split(" /from ")[1].split(" /to ")[1];
+    }
+
+    @Override
+    public String toString() {
+        String done = this.done ? "[X]" : "[ ]";
+        return "[E]" + done + " " + this.name
+                + " (from: " + this.start + " to: " + this.end + ")";
     }
 }
