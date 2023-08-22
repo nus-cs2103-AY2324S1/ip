e3344433c3af71d6b498edff54d212433aaf334a
wjayee <wong.jayee@gmail.com>
2023-08-22 19:40:32 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+
+        String greeting = "Hello! I'm Toothless. \n" +
+                "What can I do for you today? \n" +
+                "---------------------------------";
+        String farewell = "Goodbye. Hope to see you soon!\n" +
+                "---------------------------------";
+        String name = "Toothless";
+
+        System.out.println(greeting);
+
+        System.out.println(farewell);
     }
 }
