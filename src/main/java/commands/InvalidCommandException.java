6e7312c562644c63891c8deba6d7462ece707120
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-08-23 04:47:40 +0800

    Add JavaDoc for InvalidCommandException class

--- src/main/java/commands/InvalidCommandException.java
+++ src/main/java/commands/InvalidCommandException.java
@@ -1,6 +1,13 @@
 package commands;
 
+/**
+ * Exception thrown when an invalid command is provided.
+ */
 public class InvalidCommandException extends Exception{
+
+    /**
+     * Constructs a new InvalidCommandException with a default error message.
+     */
     public InvalidCommandException() {
         super("Invalid command is provided!");
     }
