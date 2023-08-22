bf5bfb53fc96a42298b76c6bac8a3d7cd6636398
Zhetao <quzhetao2001@gmail.com>
2023-08-22 12:58:25 +0800

    Added printLine function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printLine();
+    }
+
+
+    public static void printLine() {
+        for (int i = 0; i < 80; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 }
