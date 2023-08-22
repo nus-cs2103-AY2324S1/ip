865fcca9c0287434e0e3fa75192e600fab746a3c
Sher Yew <tansheryew@gmail.com>
2023-08-22 14:44:03 +0800

    Level 2
    Add the ability to store whatever text entered by the user and display them back

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class Duke {
     private static final String chatbot = "War Room";
+    private static String[] userData = new String[100];
+    private static int index = 0;
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm " + chatbot);
         System.out.println("What can I do for you?");
@@ -14,8 +17,14 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 scanner.close();
                 break;
+            } else if (Objects.equals(user_input, "list")) {
+                for (int i = 0; i < index; i++) {
+                    System.out.println((i+1) + "." + " " + userData[i]);
+                }
             } else {
-                System.out.println(user_input);
+                userData[index] = user_input;
+                index++;
+                System.out.println("added: " + user_input);
             }
         }
     }
