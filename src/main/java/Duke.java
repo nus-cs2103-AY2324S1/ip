280551acb54bb1dad70d46028e0aca35c9766ec6
lordidiot <0xlordidiot@gmail.com>
2023-08-22 12:03:34 +0800

    implement input error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,42 +16,70 @@ public class Duke {
             System.out.print("You:  ");
             String userInput = scanner.nextLine();
 
-            if (userInput.equals("bye")) {
-                running = false;
-            } else if (userInput.equals("list")) {
-                System.out.println("Pong: ");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.printf("    %d. %s\n", i+1, tasks.get(i));
-                }
-            } else if (userInput.startsWith("mark")) {
-                String userIndex = userInput.substring(5);
-                int index = Integer.parseInt(userIndex) - 1;
-                tasks.get(index).markDone();
-                System.out.println("Pong: I've marked this task as done.");
-                System.out.printf("    %s\n", tasks.get(index));
-            } else if (userInput.startsWith("unmark")) {
-                String userIndex = userInput.substring(7);
-                int index = Integer.parseInt(userIndex) - 1;
-                tasks.get(index).unmarkDone();
-                System.out.println("Pong: I've marked this task as not done.");
-                System.out.printf("    %s\n", tasks.get(index));
-            } else if (userInput.startsWith("todo")) {
-                tasks.add(new Todo(userInput.substring(5)));
-                System.out.printf("Pong: [Added] %s\n", tasks.get(tasks.size() - 1));
-            } else if (userInput.startsWith("deadline")) {
-                String[] tokens = userInput.substring(9).split(" /by ");
-                tasks.add(new Deadline(tokens[0], tokens[1]));
-                System.out.printf("Pong: [Added] %s\n", tasks.get(tasks.size() - 1));
-            } else if (userInput.startsWith("event")) {
-                String[] tokens = userInput.substring(6).split(" /from ");
-                String[] duration = tokens[1].split(" /to ");
-                tasks.add(new Event(tokens[0], duration[0], duration[1]));
-                System.out.printf("Pong: [Added] %s\n", tasks.get(tasks.size() - 1));
+            try {
+                running = Duke.handleInput(userInput, tasks);
+            } catch (DukeException e) {
+                System.out.printf("[!] %s\n", e.getMessage());
             }
         }
 
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+    }
+
+    private static boolean handleInput(String userInput, ArrayList<Task> tasks) throws DukeException {
+        if (userInput.equals("bye")) {
+            return false;
+        } else if (userInput.equals("list")) {
+            System.out.println("Pong: ");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.printf("    %d. %s\n", i+1, tasks.get(i));
+            }
+        } else if (userInput.startsWith("mark")) {
+            String userIndex = userInput.substring(5);
+            int index = Integer.parseInt(userIndex) - 1;
+            tasks.get(index).markDone();
+            System.out.println("Pong: I've marked this task as done.");
+            System.out.printf("    %s\n", tasks.get(index));
+        } else if (userInput.startsWith("unmark")) {
+            String userIndex = userInput.substring(7);
+            int index = Integer.parseInt(userIndex) - 1;
+            tasks.get(index).unmarkDone();
+            System.out.println("Pong: I've marked this task as not done.");
+            System.out.printf("    %s\n", tasks.get(index));
+        } else if (userInput.startsWith("todo")) {
+            try {
+                tasks.add(new Todo(userInput.substring(5)));
+            } catch (StringIndexOutOfBoundsException e) {
+                throw new DukeException("Todo name cannot be empty.");
+            }
+            System.out.printf("Pong: [Added] %s\n", tasks.get(tasks.size() - 1));
+        } else if (userInput.startsWith("deadline")) {
+            String[] tokens;
+            try {
+                tokens = userInput.substring(9).split(" /by ");
+                tasks.add(new Deadline(tokens[0], tokens[1]));
+            } catch (StringIndexOutOfBoundsException e) {
+                throw new DukeException("Deadline name cannot be empty.");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new DukeException("Use /by to specify deadline date.");
+            }
+            System.out.printf("Pong: [Added] %s\n", tasks.get(tasks.size() - 1));
+        } else if (userInput.startsWith("event")) {
+            String[] tokens;
+            try {
+                tokens = userInput.substring(6).split(" /from ");
+                String[] duration = tokens[1].split(" /to ");
+                tasks.add(new Event(tokens[0], duration[0], duration[1]));
+            } catch (StringIndexOutOfBoundsException e) {
+                throw new DukeException("Event name cannot be empty.");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new DukeException("Use /from and /to to specify event duration.");
+            }
+            System.out.printf("Pong: [Added] %s\n", tasks.get(tasks.size() - 1));
+        }
+
+        return true;
     }
 }
