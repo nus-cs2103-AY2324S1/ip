5e916ca266b8c3712644041727767471d7f585b5
Derek Tan <derek.tan.jing.xu@gmail.com>
2023-08-22 10:49:38 +0800

    completed level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String name = "Derek";
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String line = in.nextLine();
+            if (line.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(line);
+        }
     }
 }
