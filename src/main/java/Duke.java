a3c38ff643778833be7ae376722bb2c6ba379df1
ChuanXinNg <ngcx0103@gmail.com>
2023-08-22 16:23:40 +0800

    Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = " ____                \n"
+                + "|  _ \\  _____  _____ \n"
+                + "| |/ / |  _  ||  _  |\n"
+                + "| |\\ \\ | |_| || |_| |\n"
+                + "|_| \\_\\|_____||_____|";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Roo!");
+        System.out.println("What can I do for you ah?\n");
+
+        String bye = "Ciao! Hope to see you soon yorr!!";
+        System.out.println(bye);
     }
 }
