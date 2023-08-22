5437d9a251d312b0c589135871c0925aad4ed482
kokseng1 <ngkokseng02@gmail.com>
2023-08-23 02:31:53 +0800

    lvl0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "Chaty";
+        System.out.println("Hello! I'm " + name + "\n" +
+                "What can I do for you?" + "\n\n" +
+                "Bye. Hope to see you again soon!");
     }
 }
