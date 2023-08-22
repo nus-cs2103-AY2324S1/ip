c52bfa92c03e67edcec9e046a7357ce2d82ea74e
ChuanXinNg <ngcx0103@gmail.com>
2023-08-22 17:23:15 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,39 +1,53 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
-    public String greet() {
+    private final ArrayList<String> data = new ArrayList<>();
+
+    public void greet() {
         String logo = " ____                \n"
                 + "|  _ \\  _____  _____ \n"
                 + "| |/ / |  _  ||  _  |\n"
                 + "| |\\ \\ | |_| || |_| |\n"
                 + "|_| \\_\\|_____||_____|\n";
         String hello = "Hello! I am Roo!!\n" + "What can I do for you ah?\n";
-        return "Hello from\n" + logo + hello;
+        System.out.println("Hello from\n" + logo + hello);
     }
 
-    public String bye() {
-        return "Ciao! Hope to see you soon yorr!!";
+    public void bye() {
+        System.out.println("Ciao! Hope to see you soon yorr!!");
     }
 
-    public String echo(String s) {
-        return s + "\n";
+    public void add(String s) {
+        data.add(s);
+        System.out.println("Added \"" + s + "\" lerr :)\n");
+    }
+
+    public void list() {
+        for (int i = 0; i < data.size(); i++) {
+            System.out.println((i + 1) + ". " + data.get(i));
+        }
+        System.out.println(" ");
     }
 
     public static void main(String[] args) {
         Duke roo = new Duke();
-        System.out.println(roo.greet());
+        roo.greet();
 
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
         while (true) {
-            if (!input.equals("bye")) {
-                System.out.println(roo.echo(input));
-                input = sc.next();
+            if (input.equals("list")) {
+                roo.list();
+                input = sc.nextLine();
+            } else if (!input.equals("bye")) {
+                roo.add(input);
+                input = sc.nextLine();
             } else {
                 sc.close();
-                System.out.println(roo.bye());
+                roo.bye();
                 break;
             }
         }
