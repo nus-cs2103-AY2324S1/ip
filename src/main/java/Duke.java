0a5f2b6697f2b988d6416d5957e44b98a315097f
Lee Seng Kitt <87774484+bobscodedump@users.noreply.github.com>
2023-08-22 20:20:59 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "Hello! I am Bob\n"
                 + "What can I do for you?\n"
-                + "----------------------------\n"
-                + "Bye! Hope to see you again soon!\n";
+                + "----------------------------\n";
+        String bye = "Bye! Hope to see you again soon!\n";
         System.out.println(logo);
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            String output = scanner.nextLine();
+            if (output.equals("bye")) {
+                System.out.println(bye);
+                break;
+            }
+            System.out.println(output);
+        }
     }
 }
