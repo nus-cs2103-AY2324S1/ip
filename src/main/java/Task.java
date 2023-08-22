6fdd367be688c85c11491b9f31911098eda13bc6
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-08-22 19:26:56 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String desc;
+    protected boolean status;
+
+    public Task(String desc) {
+        this.desc = desc;
+        this.status = false;
+    }
+
+    public void markAsDone() {
+        this.status = true;
+    }
+
+    public void markAsNotDone() {
+        this.status = false;
+    }
+
+    public String getStatusIcon() {
+        return (this.status ? "X" : " ");
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.desc;
+    }
+}
