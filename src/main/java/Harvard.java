816639aa043c8b5980dc9cbff3bf43234c68409d
Kota Sudarshan Karanth <sudarshan2402@gmail.com>
2023-08-22 22:54:26 +0800

    Added DukeException and checks to throw and handle errors for Todo, Deadline and Event.

--- src/main/java/Harvard.java
+++ src/main/java/Harvard.java
@@ -12,74 +12,111 @@ public class Harvard {
 
         while (true) {
             String command = in.nextLine();
-            if (command.equals("bye")) {
-                System.out.println(line);
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(line);
-                break;
-            } else if (command.startsWith("todo")) {
-                String description = command.substring(5);
-                Todo todo = new Todo(description);
-                tasks[taskCount] = todo;
-                taskCount++;
-                System.out.println(line);
-                System.out.println("Got it. I've added this task:\n" + todo +
-                        "\nNow you have " + taskCount + " tasks in the list.");
-            } else if (command.startsWith("deadline")) {
-                String[] split = command.split(" /by ");
-                String description = split[0].substring(9);
-                String by = split[1];
-                Deadline deadline = new Deadline(description, by);
-                tasks[taskCount] = deadline;
-                taskCount++;
-                System.out.println(line);
-                System.out.println("Got it. I've added this task:\n" + deadline +
-                        "\nNow you have " + taskCount + " tasks in the list.");
-            } else if (command.startsWith("event")) {
-                String[] split = command.split("/from ");
-                String[] split2 = split[1].split(" /to ");
-                String description = split[0].substring(6);
-                String from = split2[0];
-                String to = split2[1];
-                Event event = new Event(description, from, to);
-                tasks[taskCount] = event;
-                taskCount++;
-                System.out.println(line);
-                System.out.println("Got it. I've added this task:\n" + event +
-                        "\nNow you have " + taskCount + " tasks in the list.");
-            } else if (command.equals("list")) {
-                System.out.println(line);
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
-                }
-            } else if (command.startsWith("mark")) {
-                String[] split = command.split(" ");
-                int index = Integer.parseInt(split[1]) - 1;
-                if (index >= taskCount || index < 0) {
+            try {
+                if (command.equals("bye")) {
                     System.out.println(line);
-                    System.out.println("Invalid index!");
+                    System.out.println("Bye. Hope to see you again soon!");
                     System.out.println(line);
-                    continue;
+                    break;
+                } else if (command.startsWith("todo")) {
+                    if (command.length() < 5) {
+                        throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                    }
+                    String description = command.substring(5);
+                    if (description.isEmpty()) {
+                        throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                    }
+                    Todo todo = new Todo(description);
+                    tasks[taskCount] = todo;
+                    taskCount++;
+                    System.out.println(line);
+                    System.out.println("Got it. I've added this task:\n" + todo +
+                            "\nNow you have " + taskCount + " tasks in the list.");
+                } else if (command.startsWith("deadline")) {
+                    if (command.length() < 9) {
+                        throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                    }
+                    if (!command.contains(" /by ")) {
+                        throw new DukeException("☹ OOPS!!! The deadline must be specified.");
+                    }
+                    String[] split = command.split(" /by ");
+                    if (split[0].length() < 9) {
+                        throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                    }
+                    if (split.length != 2) {
+                        throw new DukeException("☹ OOPS!!! The deadline must be specified.");
+                    }
+                    String description = split[0].substring(9);
+                    if (description.isEmpty()) {
+                        throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                    }
+                    String by = split[1];
+                    Deadline deadline = new Deadline(description, by);
+                    tasks[taskCount] = deadline;
+                    taskCount++;
+                    System.out.println(line);
+                    System.out.println("Got it. I've added this task:\n" + deadline +
+                            "\nNow you have " + taskCount + " tasks in the list.");
+                } else if (command.startsWith("event")) {
+                    if (command.length() < 6) {
+                        throw new DukeException("☹ OOPS!!! The description of an event cannot be empty.");
+                    }
+                    if (!command.contains(" /from ") || !command.contains(" /to ")) {
+                        throw new DukeException("☹ OOPS!!! The event time must be specified.");
+                    }
+
+                    String[] split = command.split("/from ");
+                    if (split[0].length() < 6) {
+                        throw new DukeException("☹ OOPS!!! The description of an event cannot be empty.");
+                    }
+                    String[] split2 = split[1].split(" /to ");
+                    if (split2.length != 2) {
+                        throw new DukeException("☹ OOPS!!! The event time must be specified.");
+                    }
+                    String description = split[0].substring(6);
+                    if (description.isEmpty()) {
+                        throw new DukeException("☹ OOPS!!! The description of an event cannot be empty.");
+                    }
+                    String from = split2[0];
+                    String to = split2[1];
+                    Event event = new Event(description, from, to);
+                    tasks[taskCount] = event;
+                    taskCount++;
+                    System.out.println(line);
+                    System.out.println("Got it. I've added this task:\n" + event +
+                            "\nNow you have " + taskCount + " tasks in the list.");
+                } else if (command.equals("list")) {
+                    System.out.println(line);
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                } else if (command.startsWith("mark")) {
+                    String[] split = command.split(" ");
+                    int index = Integer.parseInt(split[1]) - 1;
+                    if (index >= taskCount || index < 0) {
+                        System.out.println(line);
+                        System.out.println("Invalid index!");
+                        System.out.println(line);
+                        continue;
+                    }
+                    tasks[index].markAsDone();
+                    System.out.println(line);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(tasks[index]);
+                } else if (command.startsWith("unmark")) {
+                    String[] split = command.split(" ");
+                    int index = Integer.parseInt(split[1]) - 1;
+                    tasks[index].markAsUndone();
+                    System.out.println(line);
+                    System.out.println("Ok! I've marked this task as not done yet:");
+                    System.out.println(tasks[index]);
+                } else {
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-                tasks[index].markAsDone();
+            } catch (DukeException e) {
                 System.out.println(line);
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(tasks[index]);
-            } else if (command.startsWith("unmark")) {
-                String[] split = command.split(" ");
-                int index = Integer.parseInt(split[1]) - 1;
-                tasks[index].markAsUndone();
-                System.out.println(line);
-                System.out.println("Ok! I've marked this task as not done yet:");
-                System.out.println(tasks[index]);
-            } else {
-                Task task = new Task(command);
-                tasks[taskCount] = task;
-                taskCount++;
-                System.out.println(line);
-                System.out.println("Got it! I've added this task: " + task);
-                System.out.println("Now you have " + taskCount + " tasks in the list.");
+                System.out.println(e.getMessage());
             }
             System.out.println(line);
         }
