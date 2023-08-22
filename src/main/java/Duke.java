cc91929afff542da3ecea88c65e65c9f3df37269
A1WAYSD <yujl2004@163.com>
2023-08-22 15:01:09 +0800

    Add mark feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.regex.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -12,6 +13,10 @@ public class Duke {
         System.out.println("Hi! This is your intelligent friend L.\n" + "What can I do for you?");
         while(scanner.hasNextLine()) {
             String repeat = scanner.nextLine();
+            Pattern markPattern = Pattern.compile("mark (\\d+)");
+            Matcher markMatcher = markPattern.matcher(repeat);
+            Pattern unmarkPattern = Pattern.compile("unmark (\\d+)");
+            Matcher unmarkMatcher = unmarkPattern.matcher(repeat);
             if (repeat.equals("")){
                 System.out.println("Empty message. Please tell me more.");
             }
@@ -19,10 +24,19 @@ public class Duke {
                 System.out.println("Bye. See you next time!");
                 break;
             }
+            else if(markMatcher.matches()){
+                int taskIndex = Integer.parseInt(markMatcher.group(1));
+                System.out.println(storage.markTask(taskIndex,true));
+            }
+            //TODO: allow multiple marks in one line
+            else if (unmarkMatcher.matches()){
+                int taskIndex = Integer.parseInt(unmarkMatcher.group(1));
+                System.out.println(storage.markTask(taskIndex,false));
+            }
             else if (repeat.contains("list") || repeat.contains("List")) {
                 System.out.println(storage.getTasks());
             } else {
-                storage.addTask(repeat);
+                storage.addTask(new Task(repeat));
                 System.out.println("Task recorded: " + repeat);
             }
         }
--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,23 +1,32 @@
 public class Storage {
-    private String[] tasks;
+    private Task[] tasks;
     private int taskCount;
 
     public Storage() {
-        this.tasks = new String[100];
+        this.tasks = new Task[100];
         this.taskCount = 0;
     }
 
-    public void addTask(String task) {
+    public void addTask(Task task) {
         this.tasks[this.taskCount] = task;
         this.taskCount++;
     }
 
     public String getTasks(){
-        String result =  "";
+        String result =  "Here are your tasks!\n";
         for (int i = 0; i < taskCount; i++) {
-            result+=(i+1)+" "+tasks[i];
+            result+=(i+1)+" "+tasks[i].getStatusIcon()+" "+tasks[i].getDescription();
             if (i != taskCount-1) result+="\n";
         }
         return result;
+    }
+
+    public String markTask(int taskIndex, boolean isMark){
+        if (taskIndex - 1 > taskCount){
+            return "Oops! Cannot find the task.";
+        }
+        else {
+            return tasks[taskIndex - 1].markAsDone(isMark);
+        }
     }
 }
