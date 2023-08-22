019de4473bf4d0cccdfc844778e726520cb09501
jingjie88 <tanjingjie88@gmail.com>
2023-08-23 01:56:42 +0800

    Add echo

--- src/main/java/Monke.java
+++ src/main/java/Monke.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class Monke {
+
+    public static void speak(String msg) {
+        System.out.println("\t" + msg);
+    }
     public static void greet() {
-        System.out.println("Hello, I'm Monke.");
-        System.out.println("What can I do for you?");
+        Monke.speak("Hello, I'm Monke. OOGA BOOGA!");
+        Monke.speak("What can I do for you?");
+        Monke.printHorizontalLine();
     }
 
-    public static void bye() {
-        System.out.println("Bye. Hope to see you again soon!");
+    public static void exit() {
+        Monke.speak("Bye. Hope to see you again soon! OOGA BOOGA!");
+        Monke.printHorizontalLine();
     }
 
     public static void printHorizontalLine() {
@@ -15,10 +23,25 @@ public class Monke {
         System.out.println();
     }
 
+    public static void echo() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.next();
+            if (input.equals("bye")) {
+                break;
+            }
+            Monke.printHorizontalLine();
+            Monke.speak(input);
+            Monke.printHorizontalLine();
+        }
+        Monke.printHorizontalLine();
+        sc.close();
+    }
+
     public static void main(String[] args) {
         Monke.printHorizontalLine();
         Monke.greet();
-        Monke.printHorizontalLine();
-        Monke.bye();
+        Monke.echo();
+        Monke.exit();
     }
 }
