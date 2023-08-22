ac6eff5f562bb5fef20929f48e8a39596f465d53
Ravern Koh <ravernkoh@gmail.com>
2023-08-22 12:44:17 +0800

    Rename chatbot to Ekud

---
+++ src/main/java/Ekud.java
@@ -0,0 +1,9 @@
+public class Ekud {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Ekud!\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________");
+    }
+}
