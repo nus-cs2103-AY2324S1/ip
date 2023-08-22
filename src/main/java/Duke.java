942ac94c590f5edaf1c6b62ed8b82ba429c75707
A1WAYSD <yujl2004@163.com>
2023-08-22 13:12:26 +0800

    Add echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo = " _ \n"
                 + "| |\n"
                 + "| |\n"
                 + "| |___\n"
                 + "|_____|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm L\n" + "What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Greetings from\n" + logo);
+        System.out.println("Hi! This is your intelligent friend L.\n" + "What can I do for you?");
+        while(scanner.hasNextLine()) {
+            String repeat = scanner.nextLine();
+            if (repeat.contains("bye") || repeat.contains("Bye")) {
+                System.out.println("Bye. See you next time!");
+                break;
+            } else {
+                System.out.println(repeat);
+            }
+        }
     }
 }
