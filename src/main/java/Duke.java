a3d66263dd00d5349a420e73fec6f4dd679750e3
Qiming <huangqiming@r-30-101-25-172.comp.nus.edu.sg>
2023-08-22 13:14:14 +0800

    Add list function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,37 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     private static final String NAME = "Joi";
-    private boolean isRuning = true;
-    private Scanner sc = new Scanner(System.in);
+    private boolean isRunning;
+    private final Scanner sc;
+    private final ArrayList<String> itemList;
+
+    // constructor for Duke
+    public Duke() {
+        this.isRunning = true;
+        this.sc = new Scanner(System.in);
+        this.itemList = new ArrayList<>();
+    }
 
     // the event loop
     private void run() {
         this.greeting();
 
-        while (this.isRuning) {
+        while (this.isRunning) {
             String input = this.getUserInput();
 
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
-                this.isRuning = false;
-            }
-            else {
-                System.out.println(input + "\n");
+                this.isRunning = false;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < this.itemList.size(); i++) {
+                    System.out.println((i+1) + ". " + this.itemList.get(i));
+                }
+                System.out.println();
+            } else {
+                this.itemList.add(input);
+                System.out.println("added: " + input + "\n");
             }
         }
     }
