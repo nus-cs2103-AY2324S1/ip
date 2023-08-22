db36ba53d7d4c1cf2d0204c2ba477aa63f4ffa04
Matthew Sung <SungMatt@SungMatt.com>
2023-08-22 18:31:28 +0800

    Modify name of DukeBot

---
+++ src/main/java/MattBot.java
@@ -0,0 +1,17 @@
+public class MattBot {
+    private static String NAME = "MattBot";
+    public static void main(String[] args) {
+        /*String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);*/
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm " + NAME);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
