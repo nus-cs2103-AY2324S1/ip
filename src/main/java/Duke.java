0b9beaabe4b68aec3a06dd8e6d1bd1401bab8e9a
Carlin Tan <carlintan2001@gmail.com>
2023-08-22 17:59:22 +0800

    Added support for deleting tasks from the list.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
     // Array storing the tasks
-    static Task[] taskArr = new Task[100];
-    static int count = 0;
+    static ArrayList<Task> taskArr = new ArrayList<Task>();
 
     // Function that encapsulates message into a message Card template
     private static String messageCard(String message) {
@@ -13,11 +13,10 @@ public class Duke {
     }
 
     // Displays list of items
-    private static String displayList(Task[] list) {
+    private static String displayList(ArrayList<? extends Task> list) {
         String str = "Here are the tasks in your list:\n\t ";
         int count = 0;
-        while (list[count] != null) {
-            Task task = list[count];
+        for (Task task : taskArr) {
             str += count + 1 + "." + task.toString() + "\n\t ";
             count++;
         }
@@ -25,30 +24,30 @@ public class Duke {
     }
 
     private static void markTaskAsDone(int index) {
-        taskArr[index].markAsDone();
-        System.out.println(messageCard("Nice! I've marked this task as done:\n\t\t" + taskArr[index]));
+        taskArr.get(index).markAsDone();
+        System.out.println(messageCard("Nice! I've marked this task as done:\n\t\t" + taskArr.get(index)));
     }
 
     private static void markTaskAsUndone(int index) {
-        taskArr[index].markAsUndone();
-        System.out.println(messageCard("OK, I've marked this task as not done yet:\n\t\t" + taskArr[index]));
+        taskArr.get(index).markAsUndone();
+        System.out.println(messageCard("OK, I've marked this task as not done yet:\n\t\t" + taskArr.get(index)));
     }
 
     private static void addToDo(String description) {
-        taskArr[count] = new Todo(description);
+        Todo todo = new Todo(description);
+        taskArr.add(todo);
         System.out.println(messageCard("Got it. I've added this task:\n\t\t"
-                + taskArr[count]
-                + "\n\tNow you have " + (count + 1) + " tasks in the list."));
-        count++;
+                + todo
+                + "\n\tNow you have " + taskArr.size() + " tasks in the list."));
     }
 
     private static void addDeadline(String description) {
-        taskArr[count] = new Deadline(description.substring(0, description.indexOf("/by")),
+        Deadline deadline = new Deadline(description.substring(0, description.indexOf("/by")),
                 description.substring(description.indexOf("/by") + 4));
+        taskArr.add(deadline);
         System.out.println(messageCard("Got it. I've added this task:\n\t\t"
-                + taskArr[count]
-                + "\n\tNow you have " + (count + 1) + " tasks in the list."));
-        count++;
+                + deadline
+                + "\n\tNow you have " + taskArr.size() + " tasks in the list."));
     }
 
     private static void addEvent(String description) {
@@ -60,11 +59,19 @@ public class Duke {
         String startTime = description.substring(indexFrom + "/from".length(), indexTo).trim();
         String endTime = description.substring(indexTo + "/to".length()).trim();
 
-        taskArr[count] = new Event(event, startTime, endTime);
+        Event eventTask = new Event(event, startTime, endTime);
+        taskArr.add(eventTask);
         System.out.println(messageCard("Got it. I've added this task:\n\t\t"
-                + taskArr[count]
-                + "\n\tNow you have " + (count + 1) + " tasks in the list."));
-        count++;
+                + eventTask
+                + "\n\tNow you have " + taskArr.size() + " tasks in the list."));
+    }
+
+    private static void deleteTask(int index) {
+        Task task = taskArr.get(index);
+        taskArr.remove(task);
+        System.out.println(messageCard("Noted. I've removed this task:\n\t\t"
+                + task
+                + "\n\tNow you have " + taskArr.size() + " tasks in the list."));
     }
 
 
@@ -86,24 +93,35 @@ public class Duke {
                 } else if (userInput.contains("mark") && userInput.substring(0, 4).equals("mark")) {
                     if (!userInput.equals("mark")) {
                         int index = Integer.parseInt(userInput.substring(5)) - 1;
-                        if (index >= 100 || index < 0 || taskArr[index] == null) {
-                            System.out.println("Invalid mark task");
+                        if (index >= taskArr.size() || index < 0) {
+                            System.out.println(messageCard("Invalid mark task"));
                         } else {
                             markTaskAsDone(index);
                         }
                     } else {
-                        System.out.println("Invalid mark task");
+                        System.out.println(messageCard("Invalid mark task"));
                     }
-                } else if (userInput.contains("mark") && userInput.substring(0, 6).equals("unmark")) {
-                    if (!userInput.equals("mark")) {
+                } else if (userInput.contains("unmark") && userInput.substring(0, 6).equals("unmark")) {
+                    if (!userInput.equals("unmark")) {
                         int index = Integer.parseInt(userInput.substring(7)) - 1;
-                        if (index >= 100 || index < 0 || taskArr[index] == null) {
-                            System.out.println("Invalid mark task");
+                        if (index >= taskArr.size() || index < 0) {
+                            System.out.println(messageCard("Invalid unmark task"));
                         } else {
                             markTaskAsUndone(index);
                         }
                     } else {
-                        System.out.println("Invalid mark task");
+                        System.out.println(messageCard("Invalid unmark task"));
+                    }
+                } else if (userInput.contains("delete") && userInput.substring(0, 6).equals("delete")) {
+                    if (!userInput.equals("delete")) {
+                        int index = Integer.parseInt(userInput.substring(7)) - 1;
+                        if (index >= taskArr.size() || index < 0) {
+                            System.out.println(messageCard("Invalid task index to be deleted"));
+                        } else {
+                            deleteTask(index);
+                        }
+                    } else {
+                        System.out.println(messageCard("Invalid task index to be deleted"));
                     }
                 } else {
                     if (userInput.contains("todo") && userInput.substring(0, 4).equals("todo")) {
@@ -115,7 +133,7 @@ public class Duke {
                         }
                     } else if (userInput.contains("deadline") && userInput.substring(0, 8).equals("deadline")) {
                         // Add a deadline
-                        if (userInput.equals("deadline")) { // checks if description is empty
+                        if (userInput.equals("deadline") || !userInput.contains("/by")) { // checks if description is invalid
                             throw new DukeException("deadline");
                         } else {
                             addDeadline(userInput.substring(9));
@@ -123,7 +141,7 @@ public class Duke {
 
                     } else if (userInput.contains("event") && userInput.substring(0, 5).equals("event")) {
                         // Add an event
-                        if (userInput.equals("event")) { // checks if description is empty
+                        if (userInput.equals("event") || !userInput.contains("/from") || !userInput.contains("/to")) { // checks if description is invalid
                             throw new DukeException("event");
                         } else {
                             addEvent(userInput.substring(6));
