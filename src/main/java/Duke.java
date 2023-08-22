97e1b209f2ae35da113b4c2bda7494532e8d297c
kenvynKwek <kenvynkwek@gmail.com>
2023-08-22 15:30:54 +0800

    Level-2: Add, List - store tasks & output the list of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,12 +24,26 @@ public class Duke {
 
         System.out.println(logo + "\nHi ! I'm DogeBot \nHow may I help you today ?\n");
 
+        String[] tasks = new String[100];
+        int tasksCounter = 0;
         Scanner in = new Scanner(System.in);
 
         while (true) {
             String s = in.nextLine();
             if (s.equals("bye")) break;
-            System.out.println(s);
+
+            if (s.equals("list")) {
+                int i = 1;
+                for (String task : tasks) {
+                    if (task == null) break;
+                    System.out.println(i++ + ". " + task);
+                }
+            } else {
+                System.out.println("added: " + s); // echo command
+
+                tasks[tasksCounter] = s; // store into 'tasks'
+                tasksCounter++;
+            }
         }
 
         System.out.println("Bye~ See you again");
