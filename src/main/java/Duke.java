4f89c68383946986a3253ba4dec1d09d10e49a76
Gary <garylow2001@gmail.com>
2023-08-23 01:24:54 +0800

    Added scanner to read input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "I'm Chewy,\n" +
                 "What can I do for you?\n";
         System.out.println("Hello! \n" + logo);
+
+        // Create a scanner object to read commands entered by the user
+        Scanner scanner = new Scanner(System.in);
+
+        // Start command loop
+        while (true) {
+            // Read the next line of input
+            String userInput = scanner.nextLine();
+
+            // Exit if user types "bye"
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            // Echo the user's input
+            System.out.println(userInput);
+        }
     }
 }
