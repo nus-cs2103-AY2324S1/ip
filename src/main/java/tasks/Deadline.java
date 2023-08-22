2d2ee993daba9c8063a674847c994a020c17a3f3
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-08-23 02:34:24 +0800

    Add JavaDoc to Deadline class

--- src/main/java/tasks/Deadline.java
+++ src/main/java/tasks/Deadline.java
@@ -1,13 +1,26 @@
 package tasks;
-
+/**
+ * Deadline task, a type of task that need to be done before a specific date/time.
+ */
 public class Deadline extends Task{
     private String by;
 
+    /**
+     * Initializes a new deadline task with the given description and deadline. 
+     *
+     * @param desc The description of the task
+     * @param by The deadline of the task
+     */
     public Deadline(String desc, String by) {
         super(desc);
         this.by = by;
     }
 
+    /**
+     * Returns a string representation of the deadline task.
+     *
+     * @return Task type, status icon, description and deadline of the task
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + by + ")";
