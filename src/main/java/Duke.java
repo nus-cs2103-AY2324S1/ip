3412191908e136c741ed26f41da125166af7a5e2
jamz903 <jamietohhl@gmail.com>
2023-08-22 14:10:50 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        exit();
+    }
+
+    public static void greet() {
+        System.out.println("Hello! I'm Oranges");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
