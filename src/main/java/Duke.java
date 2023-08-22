c7fc61ae686a153dbd6968f8f44d722b01c12236
AlyssaPng <e0959598@u.nus.edu>
2023-08-22 15:54:30 +0800

    Level-1: Added in userinput

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Chatty\n" + "What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!\n");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userinput = scanner.nextLine();
+            if (userinput.equals("bye")) {
+                break;
+            } else {
+                System.out.println(userinput);
+            }
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
