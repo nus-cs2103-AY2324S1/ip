3caa25dfb3c8ca1071cfad287ea22faf58795141
victorlaiyeeteng <vlai69863@gmail.com>
2023-08-22 11:37:34 +0800

    Level-3

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X": " ");
+    }
+    public String getDescription() {
+        return this.description;
+    }
+    public void markDone() {
+        this.isDone = true;
+    }
+    public void unMarkDone() {
+        this.isDone = false;
+    }
+}
--- src/main/java/Victor.java
+++ src/main/java/Victor.java
@@ -11,22 +11,39 @@ public class Victor {
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
         Scanner reader = new Scanner(System.in);
-        List list = new ArrayList(100);
+        List<Task> list = new ArrayList<>(100);
         System.out.println("Hello! I'm Victor\n" +
                 "What can I do for you?\n----------\n");
 
         while (true) {
             String input = reader.nextLine();
+            String[] formattedInput = input.split(" ");
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (input.equals("list")) {
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.printf("%d. %s\n", i + 1, list.get(i));
+                    Task task = list.get(i);
+                    String taskDescription = task.getDescription();
+                    String taskStatusIcon = task.getStatusIcon();
+                    System.out.printf("%d.[%s] %s\n", i + 1, taskStatusIcon, taskDescription);
                 }
                 System.out.println("----------\n");
+            } else if (formattedInput[0].equals("mark")) {
+                Task task = list.get(Integer.parseInt(formattedInput[1]) - 1);
+                task.markDone();
+                System.out.printf("Nice! I've marked this task as done: \n" +
+                        "[%s] %s\n" +
+                        "----------\n", task.getStatusIcon(), task.getDescription());
+            } else if (formattedInput[0].equals("unmark")) {
+                Task task = list.get(Integer.parseInt(formattedInput[1]) - 1);
+                task.unMarkDone();
+                System.out.printf("OK, I've marked this task as not done yet: \n" +
+                        "[%s] %s\n" +
+                        "----------\n", task.getStatusIcon(), task.getDescription());
             } else {
-                list.add(input);
+                Task task = new Task(input);
+                list.add(task);
                 System.out.printf("added: %s\n----------\n", input);
             }
         }
