b9258ee4cc5d8372649921d84312b600c60d4b21
kristayeo <kristayeo@u.nus.edu>
2023-08-22 19:34:13 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 public class Duke {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        List<String> userList = new ArrayList<>();
 
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -16,8 +20,13 @@ public class Duke {
             if (userCommand.equalsIgnoreCase("bye")) {
                 System.out.println("Bye! Hope to see you again soon!");
                 break;
+            } if (userCommand.equalsIgnoreCase("list")) {
+                for (int i = 0; i < userList.size(); i++) {
+                    System.out.println((i + 1) + ". " + userList.get(i));
+                }
             } else {
-                System.out.println(userCommand);
+                userList.add(userCommand);
+                System.out.println("added: " + userCommand);
             }
         }
 
