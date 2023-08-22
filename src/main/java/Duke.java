70125559ea89c78e8c01420fa60e4fda03734699
GlendaChong <glendachong04@gmail.com>
2023-08-22 23:36:26 +0800

    Added error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -57,10 +57,35 @@ public class Duke {
         System.out.println(horizontalLine);
     }
 
-    public static void invalidCommand() {
+    public static void invalidCommand(String errorMessage) {
         System.out.println(horizontalLine
-                            + "        " + "Invalid command!\n"
+                            + "        " + errorMessage + "\n"
                             + horizontalLine);
+    }
+
+    public static void validateTask(String taskDescription, int numberOfTasks) throws DukeException {
+        if (taskDescription.contains("event") && !taskDescription.matches("event .*/from .* /to .*")) {
+            // Validate event task format
+            throw new DukeException("☹ OOPS!!! The format of an event task is " +
+                    "event TASK_DESCRIPTION /from START /to END");
+
+        } else if (taskDescription.contains("deadline") && !taskDescription.matches("deadline .*/by .*")) {
+            // Validate deadline task format
+            throw new DukeException("☹ OOPS!!! The format of a deadline task is " +
+                    "deadline TASK_DESCRIPTION /by DEADLINE");
+
+        } else if (taskDescription.contains("todo") && !taskDescription.matches("todo .*")) {
+            // Validate todo task format
+            throw new DukeException(" ☹ OOPS!!! The description of a todo cannot be empty.");
+
+        } else if ((taskDescription.contains("mark") && taskDescription.matches("mark \\d+")) ||
+                    (taskDescription.contains("unmark") && taskDescription.matches("unmark \\d+"))) {
+            // Validate mark task format
+            int taskNumber = Integer.parseInt(taskDescription.split(" ")[1]);
+            if (taskNumber > numberOfTasks) {
+                throw new DukeException("☹ OOPS!!! Task " + taskNumber + " does not exist.");
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -82,47 +107,48 @@ public class Duke {
                 // List out all the tasks added
                 printTasks(tasks);
 
-            } else if (command.contains(" ")) {
-                String[] splitCommand = command.split(" ", 2);
+            } else {
+                try {
+                    validateTask(command, numberOfTasks);
 
-                if (command.contains("/")) {
-
+                    String[] splitCommand = command.split(" ", 2);
                     if (splitCommand[0].equals("event")) {
-                        // Case for event task
-                        String[] taskParts = splitCommand[1].split(" /from | /to ");
+                        // Add event task into task list
+                        String[] taskParts = splitCommand[1].split("/");
                         tasks[numberOfTasks] = new Event(taskParts[0], taskParts[1], taskParts[2]);
+                        printCommand(tasks[numberOfTasks++], numberOfTasks);
 
                     } else if (splitCommand[0].equals("deadline")) {
-                        // Case for deadline task
+                        // Add deadline task into tasklist
                         String[] taskParts = splitCommand[1].split(" /by ");
                         tasks[numberOfTasks] = new Deadline(taskParts[0], taskParts[1]);
-                    }
-                    printCommand(tasks[numberOfTasks++], numberOfTasks);
+                        printCommand(tasks[numberOfTasks++], numberOfTasks);
 
-                } else if (splitCommand[0].equals("mark")) {
-                    // Mark tasks as done
-                    int taskNumber = Integer.parseInt(command.split(" ", 2)[1]);
+                    } else if (splitCommand[0].equals("todo")) {
+                        // Add to-do task into tasklist
+                        tasks[numberOfTasks] = new ToDo(command.split(" ", 2)[1]);
+                        printCommand(tasks[numberOfTasks++], numberOfTasks);
 
-                    if (tasks[taskNumber - 1] == null) {
-                        invalidCommand();
-                    } else {
+                    } else if (splitCommand[0].equals("mark")) {
+                        // Mark tasks as done
+                        int taskNumber = Integer.parseInt(command.split(" ", 2)[1]);
                         markTaskAsDone(tasks[taskNumber - 1]);
-                    }
 
-                } else if (splitCommand[0].equals("unmark")) {
-                    // Mark tasks as undone
-                    int taskNumber = Integer.parseInt(command.split(" ", 2)[1]);
-
-                    if (tasks[taskNumber - 1] == null) {
-                        invalidCommand();
-                    } else {
+                    } else if (splitCommand[0].equals("unmark")) {
+                        // Mark tasks as undone
+                        int taskNumber = Integer.parseInt(command.split(" ", 2)[1]);
                         markTaskAsUnDone(tasks[taskNumber - 1]);
+
+                    } else {
+                        // Non-existent task functions
+                        throw new DukeException();
                     }
 
-                } else if (splitCommand[0].equals("todo")) {
-                    // Case for toDo task
-                    tasks[numberOfTasks] = new ToDo(command.split(" ", 2)[1]);
-                    printCommand(tasks[numberOfTasks++], numberOfTasks);
+                } catch (DukeException error) {
+                    invalidCommand(error.getMessage());
+
+                } catch (Exception error) {
+                    invalidCommand(error.getMessage());
                 }
             }
         }
