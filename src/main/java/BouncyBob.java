c3e7be767062fc91ce6d6f673b2993a27e48c94e
JCSnap <e0970167@u.nus.edu>
2023-08-23 00:25:22 +0800

    changed Duke to BouncyBob with a cooler greeting

---
+++ src/main/java/BouncyBob.java
@@ -0,0 +1,16 @@
+public class BouncyBob {
+
+    private static final String TOP_BORDER = "╔══════════════════════════════════════════════╗";
+    private static final String MIDDLE_BORDER = "║                                              ║";
+    private static final String BOTTOM_BORDER = "╚══════════════════════════════════════════════╝";
+
+    public static void main(String[] args) {
+        System.out.println(TOP_BORDER);
+        System.out.println(MIDDLE_BORDER);
+        System.out.println("║                   Hey there! 🎈              ║");
+        System.out.println("║      I'm BouncyBob, your bubbly chatbot!     ║");
+        System.out.println("║ Wow you look very round today, like a ball!  ║");
+        System.out.println(MIDDLE_BORDER);
+        System.out.println(BOTTOM_BORDER);
+    }
+}
