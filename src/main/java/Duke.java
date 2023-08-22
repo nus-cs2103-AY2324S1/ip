f397555b7f2a711a99d47555dd52cbb34cee04ac
justin soong <91204066+juzzztinsoong@users.noreply.github.com>
2023-08-22 13:21:50 +0800

    echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,27 +2,37 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static void main(String[] args) {
-        String line = 
+    public static String line = 
     "―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――\n";
-        String logo = 
+
+    public static String logo = 
     "     ______     _                     ______          __     ____        __ \n"+
     "    / ____/____(_)___  ____ ____     /_  __/__  _  __/ /_   / __ )____  / /_\n"+
     "   / /   / ___/ / __ \\/ __ `/ _ \\     / / / _ \\| |/_/ __/  / __  / __ \\/ __/\n"+
     "  / /___/ /  / / / / / /_/ /  __/    / / /  __/>  </ /_   / /_/ / /_/ / /_  \n"+
     "  \\____/_/  /_/_/ /_/\\__, /\\___/    /_/  \\___/_/|_|\\__/  /_____/\\____/\\__/  \n"+
     "                    /____/                                                  \n";
-        System.out.println(line + "Hi, I am a\n" + logo + "How can I help you hehe.. (° ͜ʖ °)\n\n" + line + "Aw goodbye.. ಠ_ಠ\n\n" + line);
-/*
+   
+    private static String StringHandler(String tempString) {
+        return (tempString + "\n" + line);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(line + "Hi, I am a\n" + logo + "\nHow can I help you hehe.. (° ͜ʖ °)\n" + line);
         Scanner scanner = new Scanner(System.in);
 
         while(true) {
-            String temp = scanner.nextLine();
-            if (temp == null) {
+            String command = scanner.nextLine();
+            System.out.println(command + "\n" + line);
+            if (command.equals("bye")) {
+                System.out.println("Aw goodbye.. ಠ_ಠ\n" + line);
+                scanner.close();
                 break;
             }
-            System.out.println(temp);
+            String output = StringHandler(command);
+            System.out.println(output);
         }
-*/
+
     }
 }
