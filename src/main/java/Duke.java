ba6ba95781cc9aa0edfbd44932f3c74ae65ab64e
samuelmui8 <samuelmui8@gmail.com>
2023-08-22 17:47:34 +0800

    added echo and exit behaviour

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("-----------------------------------------------");
-        System.out.println("Hello! I'm Brobot");
+        System.out.println("Sup bro! I'm Brobot");
         System.out.println("What can I do for you?");
+        System.out.println("-----------------------------------------------");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println("-----------------------------------------------");
+            System.out.println(input);
+            System.out.println("-----------------------------------------------");
+        }
+
         System.out.println("-----------------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("-----------------------------------------------");
