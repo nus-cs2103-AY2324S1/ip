05bde19cd1df5ec1a33f1ac3cb8afe5835536fc5
Loo Mei Lin Zann <>
2023-08-22 08:41:38 +0800

    added new methods to mark and unmark tasks in TehO.java

--- src/main/java/TehO.java
+++ src/main/java/TehO.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class TehO {
     static int taskCounter = 0;
-    static String[] taskList = new String[100];
+    static Task[] taskList = new Task[100];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -16,21 +18,39 @@ public class TehO {
                 break;
             } else if (userCommand.equals("list")) {
                 listTask(taskList);
+            } else if (userCommand.startsWith("mark")) {
+                //note that split returns a String[]
+                //parseInt returns the integer value which is represented by the argument
+                int taskNumber = parseInt(userCommand.split(" ")[1]) - 1; //counting from 0
+                Task task = taskList[taskNumber];
+                task.markAsDone(taskNumber);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(task.getStatus());
+            } else if (userCommand.startsWith("unmark")) {
+                int taskNumber = parseInt(userCommand.split(" ")[1]) - 1; //counting from 0
+                Task task = taskList[taskNumber];
+                task.markAsNotDone(taskNumber);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(task.getStatus());
             } else {
-                addTask(taskList, userCommand);
+                Task task = new Task(userCommand);
+                addTask(task);
             }
         }
     }
 
-    public static void addTask(String[] taskList, String task) {
-        taskList[taskCounter] = task;
+
+    public static void addTask(Task newTask) {
+        taskList[taskCounter] = newTask;
         taskCounter++;
-        System.out.println("added: " + task);
+        System.out.println("added: " + newTask.description);
     }
 
-    public static void listTask(String[] taskList) {
+    public static void listTask(Task[] taskList) {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < taskCounter; i++) {
-            System.out.println((i+1) + ". " + taskList[i]);
+            Task task = taskList[i];
+            System.out.println((i+1) + ". " + task.getStatus());
         }
     }
 }
