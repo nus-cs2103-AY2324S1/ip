28952d148579d33488559a5dca9ab9a44a09066f
Jackie <jackieguo126@gmail.com>
2023-08-22 19:08:54 +0800

    incr: level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,16 @@
 public class Duke {
+    private static String Greet() {
+        return "_______________________________________________\n"
+                + "Hi there! I'm Bob.\nHow can I help?\n"
+                + "_______________________________________________";
+    }
+
+    private static String Exit() {
+        return "_______________________________________________\n"
+                + "See you soon!\n"
+                + "_______________________________________________";
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +18,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(Greet());
+        System.out.println(Exit());
     }
 }
