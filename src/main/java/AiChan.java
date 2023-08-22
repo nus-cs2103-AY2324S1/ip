8184735d84e3da696cf174c2b39bf16f301ea152
KumChaiYin <kumchaiyin@gmail.com>
2023-08-22 17:53:17 +0800

    Rename, Greet, Exit

---
+++ src/main/java/AiChan.java
@@ -0,0 +1,10 @@
+public class AiChan {
+    public static void main(String[] args) {
+        String line = "_______________________________________________________________________\n";
+        String greet = "Hiya! I'm Ai-chan~ ✧◝(⁰▿⁰)◜✧\n" +
+                "Hey there, dear viewer, what's on your mind?\n" +
+                "Is there anything I can do to sprinkle some magic into your day?\n";
+        String bye = "Ta-da! It's time to go~ Keep smiling till we reunite!\n";
+        System.out.println(line + greet + line + bye + line);
+    }
+}
