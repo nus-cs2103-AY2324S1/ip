8d06adccfe3b7a9ad284e2d47fa2539643c6105d
Tan Boon Khong <tanboonkhong246@gmail.com>
2023-08-23 00:33:20 +0800

    Implement Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,63 +21,79 @@ public class Duke {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 isExit = true;
-                printLine();
                 printBye();
-                printLine();
             } else if (input.equals("list")) {
-                printLine();
                 printList();
-                printLine();
             } else if (input.startsWith("mark ")) {
                 int index = Integer.parseInt(input.substring(5));
                 list.get(index - 1).markAsDone();
-                printLine();
                 printDone(index);
-                printLine();
             } else if (input.startsWith("unmark ")) {
                 int index = Integer.parseInt(input.substring(7));
                 list.get(index - 1).markAsUndone();
-                printLine();
                 printUndone(index);
-                printLine();
             } else {
-                list.add(new Task(input));
-                printLine();
-                printAdded(input);
-                printLine();
+                addTask(input);
             }
         }
     }
-    public void printEcho(String input) {
-        System.out.println(input);
-    }
 
     public void printDone(int index) {
+        printLine();
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(list.get(index - 1));
+        printLine();
     }
 
     public void printUndone(int index) {
+        printLine();
         System.out.println("OK, I've marked this task as not done yet:");
         System.out.println(list.get(index - 1));
+        printLine();
     }
 
-    public void printAdded(String input) {
-        System.out.println("added: " + input);
+    public void printAdded(Task task) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println(task);
+        System.out.println("Now you have " + list.size() + " tasks in the list.");
+    }
+
+    public void addTask(String input) {
+        printLine();
+        if (input.startsWith("todo ")) {
+            Task task = new Todo (input.substring(5));
+            list.add(task);
+            printAdded(task);
+        } else if (input.startsWith("deadline ")) {
+            String[] arr = input.substring(9).split(" /by ");
+            Task task = new Deadline(arr[0], arr[1]);
+            list.add(task);
+            printAdded(task);
+        } else if (input.startsWith("event ")) {
+            String[] arr = input.split("\\s*/from\\s*|\\s*/to\\s*");
+            Task task = new Event(arr[0], arr[1], arr[2]);
+            list.add(task);
+            printAdded(task);
+        }
+        printLine();
     }
 
     public void printList() {
+        printLine();
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < list.size(); i++) {
             System.out.println(i + 1 + ". " + list.get(i));
         }
+        printLine();
     }
     public void printGreeting() {
         System.out.println("Hello! I'm Max\n" + "What can I do for you?");
     }
 
     public void printBye() {
+        printLine();
         System.out.println("Bye. Hope to see you again soon!");
+        printLine();
     }
 
     public void printLine() {
