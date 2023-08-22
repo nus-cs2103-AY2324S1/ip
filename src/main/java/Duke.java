cb56f7ab8b4b8d7a4e0685eafda9c176beb55ddb
kenvynKwek <kenvynkwek@gmail.com>
2023-08-22 19:27:12 +0800

    Level-3: Mark as Done - added mark task feature, created 'Task' class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,28 +24,41 @@ public class Duke {
 
         System.out.println(logo + "\nHi ! I'm DogeBot \nHow may I help you today ?\n");
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int tasksCounter = 0;
-        Scanner in = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         while (true) {
-            String s = in.nextLine();
+            String s = sc.next();
             if (s.equals("bye")) break;
 
-            if (s.equals("list")) {
+            if (s.equals("list")) { // view the list of tasks
+                System.out.println("Stuff to do:");
                 int i = 1;
-                for (String task : tasks) {
+                for (Task task : tasks) {
                     if (task == null) break;
-                    System.out.println(i++ + ". " + task);
+                    System.out.println(i++ + "." + task.toString());
                 }
-            } else {
-                System.out.println("added: " + s); // echo command
+            } else if (s.equals("mark")) { // mark tasks
+                int temp = sc.nextInt() - 1;
+                tasks[temp].markTask(true);
+                System.out.println("Good job on completing a task ! You deserve a cookie C:");
+                System.out.println("\t" + tasks[temp].toString());
+            } else if (s.equals("unmark")) { // unmark tasks
+                int temp = sc.nextInt() - 1;
+                tasks[temp].markTask(false);
+                System.out.println("Oh nyo, did someone make a mistake ?");
+                System.out.println("\t" + tasks[temp].toString());
+            } else { // adding tasks
+                String temp = s + sc.nextLine(); // sc.nextLine() to add the remaining words
+                System.out.println("added: " + temp); // echo command
 
-                tasks[tasksCounter] = s; // store into 'tasks'
+                tasks[tasksCounter] = new Task(temp); // store into 'tasks'
                 tasksCounter++;
             }
         }
 
         System.out.println("Bye~ See you again");
+        sc.close();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,18 @@
+public class Task {
+    // abstraction: information-hiding principle
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String toString() { // encapsulation principle
+        return "[" + (isDone ? "X" : " ") + "] " + description;
+    }
+
+    public void markTask(boolean mark) { // tell-don't-ask principle
+        isDone = (mark) ? true : false;
+    }
+}
