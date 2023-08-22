1053f207bc65c976dd543199abb2c3c6312dccfb
Andre Sim Jing Xiang <e0968908@u.nus.edu>
2023-08-22 15:57:14 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Duke {
     public static String separator = "____________________________________________________________";
+    public static ArrayList<String> list = new ArrayList<String>(100);
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -17,11 +18,22 @@ public class Duke {
             System.out.println(separator);
             if (command.equals("bye")) {
                 break;
+            } else if (command.equals("list")) {
+                int i = 1;
+                for (String s:list) {
+                    System.out.println(String.format("%d. %s", i, s));
+                    i++;
+                }
+                System.out.println(separator);
+                continue;
+            } else {
+                list.add(command);
+                System.out.println(String.format("added: %s", command));
+                System.out.println(separator);
             }
-            System.out.println(command);
-            System.out.println(separator);
         }
         System.out.println("It appears that the user has decided to close the program as indicated by the command of which this is the function being issued and therefore,\n" +
                 "I shall bid thee farewell and wish thee great fortune in your future endeavors");
     }
 }
+
