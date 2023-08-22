85761343757ef14f2f9b9ca717e8c1bd4374e8cb
Zjinnnn <zongjin.2@gmail.com>
2023-08-22 09:58:42 +0800

    feat: implement level-1 increment

--- src/main/java/Pooh.java
+++ src/main/java/Pooh.java
@@ -1,23 +1,39 @@
+import java.util.Scanner;
+
 public class Pooh {
     public static void main(String[] args) {
-        String logo = " .----------------.  .----------------.  .----------------.  .----------------. \n" +
-                "| .--------------. || .--------------. || .--------------. || .--------------. |\n" +
-                "| |   ______     | || |     ____     | || |     ____     | || |  ____  ____  | |\n" +
-                "| |  |_   __ \\   | || |   .'    `.   | || |   .'    `.   | || | |_   ||   _| | |\n" +
-                "| |    | |__) |  | || |  /  .--.  \\  | || |  /  .--.  \\  | || |   | |__| |   | |\n" +
-                "| |    |  ___/   | || |  | |    | |  | || |  | |    | |  | || |   |  __  |   | |\n" +
-                "| |   _| |_      | || |  \\  `--'  /  | || |  \\  `--'  /  | || |  _| |  | |_  | |\n" +
-                "| |  |_____|     | || |   `.____.'   | || |   `.____.'   | || | |____||____| | |\n" +
-                "| |              | || |              | || |              | || |              | |\n" +
-                "| '--------------' || '--------------' || '--------------' || '--------------' |\n" +
-                " '----------------'  '----------------'  '----------------'  '----------------' ";
-        String horizontalLine = "____________________________________________________________";
+        String logo = "      .----------------.  .----------------.  .----------------.  .----------------. \n" +
+                "      | .--------------. || .--------------. || .--------------. || .--------------. |\n" +
+                "      | |   ______     | || |     ____     | || |     ____     | || |  ____  ____  | |\n" +
+                "      | |  |_   __ \\   | || |   .'    `.   | || |   .'    `.   | || | |_   ||   _| | |\n" +
+                "      | |    | |__) |  | || |  /  .--.  \\  | || |  /  .--.  \\  | || |   | |__| |   | |\n" +
+                "      | |    |  ___/   | || |  | |    | |  | || |  | |    | |  | || |   |  __  |   | |\n" +
+                "      | |   _| |_      | || |  \\  `--'  /  | || |  \\  `--'  /  | || |  _| |  | |_  | |\n" +
+                "      | |  |_____|     | || |   `.____.'   | || |   `.____.'   | || | |____||____| | |\n" +
+                "      | |              | || |              | || |              | || |              | |\n" +
+                "      | '--------------' || '--------------' || '--------------' || '--------------' |\n" +
+                "       '----------------'  '----------------'  '----------------'  '----------------' ";
+        String horizontalLine = "      _______________________________________________________________________________";
         System.out.println(logo);
         System.out.println(horizontalLine);
-        System.out.println("Hello! I'm Pooh!\nWhat can I do for you?");
+        System.out.println("      Hi there! Good to see you! I'm Pooh!\n      What can I do for you?");
         System.out.println(horizontalLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+
+        Scanner userInput = new Scanner(System.in);
+        while  (userInput.hasNextLine()) {
+            String userCmd = userInput.nextLine();
+            if (userCmd.equalsIgnoreCase("bye")) {
+                System.out.println(horizontalLine);
+                System.out.println("      How lucky I am to have something that makes saying goodbye so hard. Bye!");
+                System.out.println(horizontalLine);
+                userInput.close();
+                System.exit(0);
+            } else {
+                System.out.println(horizontalLine);
+                System.out.println("      " + userCmd);
+                System.out.println(horizontalLine);
+            }
+        }
     }
 }
 
