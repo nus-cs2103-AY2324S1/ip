9bc996611a871506a77562fe6315187664281285
kohkaijie <kohkaijie21@gmail.com>
2023-08-22 22:45:55 +0800

    level 3

--- src/main/java/CR7.java
+++ src/main/java/CR7.java
@@ -8,11 +8,38 @@ public class CR7 {
         System.out.println(); // Move to the next line
     }
 
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        public String getDescription() {
+            return this.description;
+        }
+
+        public void markAsDone() {
+            isDone = true;
+        }
+
+        public void unmarkAsDone() {
+            isDone = false;
+        }
+    }
+
+
     public static void main(String[] args) {
         printHorizontalLine(40);
         System.out.println("Hello! I'm CR7\n" + "What can I do for you?\n");
         String input = "";
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int counter = 0;
         while (!input.equals("bye")) {
             Scanner myObj = new Scanner(System.in);
@@ -25,13 +52,30 @@ public class CR7 {
             }
             if (input.equals("list")) {
                 printHorizontalLine(40);
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 1; i < counter + 1; i++) {
-                    System.out.println(i + ". " + tasks[i-1]);
+                    Task x = tasks[i-1];
+                    System.out.println(i + ".[" + x.getStatusIcon() + "] " + x.getDescription());
                 }
                 System.out.println();
                 printHorizontalLine(40);
+            } else if (input.startsWith("mark ")) {
+                int s = Integer.valueOf(input.substring(5));
+                Task k = tasks[s-1];
+                k.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [X] " + k.getDescription() + "\n");
+                printHorizontalLine(40);
+            } else if (input.startsWith("unmark ")) {
+                int s = Integer.valueOf(input.substring(7));
+                Task k = tasks[s-1];
+                k.unmarkAsDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [ ] " + k.getDescription() + "\n");
+                printHorizontalLine(40);
             } else {
-                tasks[counter] = input;
+                Task t = new Task(input);
+                tasks[counter] = t;
                 counter++;
                 printHorizontalLine(40);
                 System.out.println("added: " + input + "\n");
