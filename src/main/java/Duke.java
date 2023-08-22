a147b6a42042be0f6674dcd218763d722e4f17bb
darrentfy <>
2023-08-22 16:29:27 +0800

    added feature to add and display tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,38 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
         String welcome = " ──────────────────────────────────────── \n"
                 + "  Hello! I'm Handsome \n"
                 + "  What can I do for you? \n"
                 + " ──────────────────────────────────────── ";
-        String goodbye = " ──────────────────────────────────────── \n" +
-                "  Bye. Hope to see you again soon! \n" +
-                " ──────────────────────────────────────── \n";
+        String goodbye = " ──────────────────────────────────────── \n"
+                + "  Bye. Hope to see you again soon! \n"
+                + " ──────────────────────────────────────── \n";
         System.out.println(welcome);
         while (true) {
             String command = input.nextLine();
             if (command.equals("bye")) {
                 break;
             }
-            String echo = " ──────────────────────────────────────── \n" +
-                    "  " + command + " \n" +
-                    " ──────────────────────────────────────── ";
-            System.out.println(echo);
+            if (command.equals("list")) {
+                int index = 1;
+                System.out.println(" ──────────────────────────────────────── ");
+                for (String task : tasks) {
+                    System.out.println(String.format("  %d. %s ", index, task));
+                    index++;
+                }
+                System.out.println(" ──────────────────────────────────────── ");
+            } else {
+                tasks.add(command);
+                String echo = " ──────────────────────────────────────── \n"
+                        + "  added: " + command + " \n"
+                        + " ──────────────────────────────────────── ";
+                System.out.println(echo);
+            }
         }
         System.out.println(goodbye);
     }
