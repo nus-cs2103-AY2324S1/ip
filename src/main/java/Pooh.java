022f5c3fb867fb6d24c798e0fec8f7f6bf547494
Zjinnnn <zongjin.2@gmail.com>
2023-08-22 10:56:37 +0800

    feat: implement level-2 increment

--- src/main/java/Pooh.java
+++ src/main/java/Pooh.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Pooh {
-    public static void main(String[] args) {
+
+    public static void welcomeMsg() {
         String logo = "      .----------------.  .----------------.  .----------------.  .----------------. \n" +
                 "      | .--------------. || .--------------. || .--------------. || .--------------. |\n" +
                 "      | |   ______     | || |     ____     | || |     ____     | || |  ____  ____  | |\n" +
@@ -13,27 +16,62 @@ public class Pooh {
                 "      | |              | || |              | || |              | || |              | |\n" +
                 "      | '--------------' || '--------------' || '--------------' || '--------------' |\n" +
                 "       '----------------'  '----------------'  '----------------'  '----------------' ";
+
+        String greetings = "      Hi there! Good to see you! I'm Pooh!\n      What can I do for you?";
         String horizontalLine = "      _______________________________________________________________________________";
         System.out.println(logo);
         System.out.println(horizontalLine);
-        System.out.println("      Hi there! Good to see you! I'm Pooh!\n      What can I do for you?");
+        System.out.println(greetings);
         System.out.println(horizontalLine);
+    }
 
+    public static void exitMsg() {
+        String horizontalLine = "      _______________________________________________________________________________";
+        String byeMessage = "      How lucky I am to have something that makes saying goodbye so hard. Pooh says Bye!";
+        System.out.println(horizontalLine);
+        System.out.println(byeMessage);
+        System.out.println(horizontalLine);
+    }
+
+    public static void generalRespond(String message) {
+        String horizontalLine = "      _______________________________________________________________________________";
+        System.out.println(horizontalLine);
+        System.out.println(message);
+        System.out.println(horizontalLine);
+    }
+
+    public static void printTasks(List<String> tasks) {
+        StringBuilder todoListString = new StringBuilder();
+        for (int i = 0; i < tasks.size(); i++) {
+            String task = String.format("      %d. ", i) + tasks.get(i) + "\n";
+            todoListString.append(task);
+        }
+        String horizontalLine = "      _______________________________________________________________________________";
+        System.out.println(horizontalLine);
+        System.out.println(todoListString.toString().stripTrailing());
+        System.out.println(horizontalLine);
+    }
+
+    public static void main(String[] args) {
+        welcomeMsg();
+        List<String> todoList = new ArrayList<String>();
         Scanner userInput = new Scanner(System.in);
         while  (userInput.hasNextLine()) {
             String userCmd = userInput.nextLine();
             if (userCmd.equalsIgnoreCase("bye")) {
-                System.out.println(horizontalLine);
-                System.out.println("      How lucky I am to have something that makes saying goodbye so hard. Bye!");
-                System.out.println(horizontalLine);
+                exitMsg();
                 userInput.close();
                 System.exit(0);
+            } else if (userCmd.equalsIgnoreCase("list")){
+                if (todoList.isEmpty()) {
+                    generalRespond("      No tasks added. Add one now!");
+                } else {
+                    printTasks(todoList);
+                }
             } else {
-                System.out.println(horizontalLine);
-                System.out.println("      " + userCmd);
-                System.out.println(horizontalLine);
+                todoList.add(userCmd);
+                generalRespond("      added: " + userCmd);
             }
         }
     }
 }
-
