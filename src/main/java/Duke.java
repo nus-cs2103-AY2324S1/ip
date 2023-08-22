eae61baa0c23712ff8ac5f39c631ab3751ef48fb
Julius Bryan <67666355+juliusgambe@users.noreply.github.com>
2023-08-22 10:48:42 +0800

    feat: rename, greet, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Greet
+        String greetString = "____________________________________________________________\n" +
+                " Hello! I'm GobbleGobble \n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(greetString);
     }
 }
