80d674859e462f478a2c5e625e51fb42bc7e7356
JCSnap <e0970167@u.nus.edu>
2023-08-23 00:37:56 +0800

    added feature where chatbot repeats after user and terminates after bye

--- src/main/java/BouncyBob.java
+++ src/main/java/BouncyBob.java
@@ -1,10 +1,12 @@
-public class BouncyBob {
+import java.util.Scanner;
 
+public class BouncyBob {
     private static final String TOP_BORDER = "╔══════════════════════════════════════════════╗";
     private static final String MIDDLE_BORDER = "║                                              ║";
     private static final String BOTTOM_BORDER = "╚══════════════════════════════════════════════╝";
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println(TOP_BORDER);
         System.out.println(MIDDLE_BORDER);
         System.out.println("║                   Hey there! 🎈              ║");
@@ -12,5 +14,21 @@ public class BouncyBob {
         System.out.println("║ Wow you look very round today, like a ball!  ║");
         System.out.println(MIDDLE_BORDER);
         System.out.println(BOTTOM_BORDER);
+
+        while (true) {
+            System.out.println("Enter something: ");
+            String userInput = scanner.nextLine();
+            System.out.println(TOP_BORDER);
+            if (userInput.equals("bye")) {
+                System.out.println(MIDDLE_BORDER);
+                System.out.println("║        BouncyBob: Bye! Bounce back soon!     ║");
+                System.out.println(MIDDLE_BORDER);
+                System.out.println(BOTTOM_BORDER);
+                break;
+            } else {
+                System.out.println(userInput);
+                System.out.println(BOTTOM_BORDER);
+            }
+        }
     }
 }
