b0f433fae49fc1bbea4684e1a646c63df5c2ae51
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-08-23 02:30:20 +0800

    Add JavaDoc to ToDo class

--- src/main/java/tasks/ToDo.java
+++ src/main/java/tasks/ToDo.java
@@ -1,11 +1,24 @@
 package tasks;
 
+/**
+ * Todo task, a type of task without any date/time attached to it.
+ */
 public class ToDo extends Task {
     
+    /**
+     * Initializes a new todo task with the given description. The task's initial status is set to not done.
+     *
+     * @param desc The description of the todo task
+     */
     public ToDo(String desc) {
         super(desc);
     }
 
+    /**
+     * Returns a string representation of the todo task.
+     *
+     * @return Task type, status icon and description of the task
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
