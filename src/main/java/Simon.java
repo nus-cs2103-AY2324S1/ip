677483b900af62e97301877fd53ed923c0037f63
L1MJH <lolflgc@icloud.com>
2023-08-22 17:06:30 +0800

    Update Simon.java

---
+++ src/main/java/Simon.java
@@ -0,0 +1,12 @@
+public class Simon {
+    public static void main(String[] args) {
+        String output = "____________________________________________________________\n" +
+                "Hello! I'm Simon\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+
+        System.out.println(output);
+    }
+}
