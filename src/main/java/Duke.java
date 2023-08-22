78057ccbc9a908a3187d5521f2a9dac0580d8370
Jair Goh <lilozz2>
2023-08-22 22:17:54 +0800

    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,7 +14,22 @@ public class Duke {
         ));
     }
 
-    public static void printList(List<String> list){
+    public static String getCommand(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            return null; // Return null for empty input or whitespace
+        }
+
+        input = input.trim(); // Remove leading and trailing whitespace
+        int spaceIndex = input.indexOf(' '); // Find the index of the first space
+
+        if (spaceIndex == -1) {
+            return input; // If no space found, the whole string is the first word
+        } else {
+            return input.substring(0, spaceIndex); // Extract the first word using substring
+        }
+    }
+
+    public static void printList(List<Task> list){
         String response = "";
         for (int i = 0; i < list.size(); i++) {
             response += String.format("%d. %s\n", i+1, list.get(i));
@@ -25,15 +40,26 @@ public class Duke {
     public static void reply() {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         String response = myObj.nextLine();
-        List<String> list = new ArrayList<String>();
+        String command = getCommand(response);
+        List<Task> list = new ArrayList<Task>();
         while (!response.equals("bye")) {
-            switch(response) {
+            switch(response.split(" ")[0]) {
                 case "list":
                     printList(list);
                     response = myObj.nextLine();
                     break;
+                case "mark":
+                    list.get(Integer.parseInt(response.split(" ")[1])-1).setDone();
+                    printList(list);
+                    response = myObj.nextLine();
+                    break;
+                case "unmark":
+                    list.get(Integer.parseInt(response.split(" ")[1])-1).setNotDone();
+                    printList(list);
+                    response = myObj.nextLine();
+                    break;
                 default: {
-                    list.add(response);
+                    list.add(new Task(response));
                     System.out.println(format_response("added: " + response));  // Output user input
                     response = myObj.nextLine(); // Read user input
                 }
