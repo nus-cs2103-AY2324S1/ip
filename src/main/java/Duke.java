260d44c87d150baaf68318371ca4b66d4f1820df
jamz903 <jamietohhl@gmail.com>
2023-08-22 14:56:38 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static Scanner prompt = new Scanner(System.in);
     public static void main(String[] args) {
         greet();
-        exit();
     }
 
     public static void greet() {
-        System.out.println("Hello! I'm Oranges");
+        System.out.println("Hello! I'm Oranges.");
         System.out.println("What can I do for you?");
+        echo();
+    }
+
+    public static void echo() {
+        String promptText = prompt.nextLine();
+        if (promptText.equals("bye")) {
+            exit();
+        }
+        System.out.println("\t" + promptText);
+        echo();
     }
 
     public static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
+        System.exit(0);
     }
 }
