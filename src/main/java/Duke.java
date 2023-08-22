99d3c805f9532384e48fd0f877fdf0ad3cfd0ef3
nicholascher <nicholascherkwokhui@gmail.com>
2023-08-22 23:35:21 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "nic";
+        String exit = "Bye. Hope to see you again soon!";
+        String line = "___________________________________\n";
+        System.out.println(line + "Hello from " + name + "\nWhat can i do for you?\n" + line + exit );
     }
 }
