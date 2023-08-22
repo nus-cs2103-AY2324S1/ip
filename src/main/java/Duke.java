ddca1fbad49c0a33a66d869859a866b9d9c2ec04
LAPTOP-SN5EU1FD\Miguel <e1024867@u.nus.edu>
2023-08-22 18:24:02 +0900

    Level 1 of the iP + Added a messageHandler method that merely echoes the user's input messages.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
 public class Duke {
+
+
     public static void main(String[] args) {
+        Duke lati = new Duke();
+
         System.out.println("____________________________________________________________\n" +
                 " Hellooooooooooo! I'm Lati!\n" +
                 " What can I do for you? :3\n" +
                 "____________________________________________________________");
+
+        Scanner scan = new Scanner(System.in);
+        String comd = scan.nextLine();
+
+        while (!comd.equals("bye")) {
+            lati.messageHandler(comd);
+            comd = scan.nextLine();
+        }
+
         System.out.println(" Byeeeeee. Hope to see you again soon~~\n" +
                 "____________________________________________________________");
     }
+
+    //In-class method to manage messages, currently set to just echo the message input of the user.
+    public void messageHandler(String tally) {
+
+        System.out.println(tally + "\n" +
+                "____________________________________________________________");
+
+        
+    }
+
+
+
+
 }
