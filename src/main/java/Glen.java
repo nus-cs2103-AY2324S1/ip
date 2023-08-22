8443030e1d4eb3635d40d616e547eba6b2243eba
tayian <90464339+tayian@users.noreply.github.com>
2023-08-23 01:29:28 +0800

    Update and rename Duke.java to Glen.java

---
+++ src/main/java/Glen.java
@@ -0,0 +1,11 @@
+public class Glen {
+    public static void main(String[] args) {
+        String logo = "  _____ _            \n"
+                + " / ____| |           \n"
+                + "| |  __| | ___ _ __  \n"
+                + "| | |_ | |/ _ \ '_ \ \n"
+                + "| |__| | |  __/ | | |\n"
+                + " \_____|_|\___|_| |_|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
