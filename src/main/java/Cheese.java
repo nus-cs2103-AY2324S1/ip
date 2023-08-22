3f784d58419e5512cb9c26cc4077ce633835646e
LicongHuang <alexliconghuang@gmail.com>
2023-08-22 14:29:04 +0800

    Added list functionality and complete task 2

--- src/main/java/Cheese.java
+++ src/main/java/Cheese.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Cheese {
+  private List<String> cheeseStack = new ArrayList<String>();
+
   public void speak() {
     Scanner myObj = new Scanner(System.in);
     String echoedInput = "";
@@ -10,15 +14,39 @@ public class Cheese {
     System.out.println("\t------------------------------------------");
     while (true) {
       echoedInput = myObj.nextLine();
-      System.out.println("\t-----------------------------------------");
-      System.out.println("\t" + echoedInput);
-      System.out.println("\t-----------------------------------------");
+      commandResponse(echoedInput);
       if (echoedInput.equals("bye")) {
         break;
       }
     }
-    System.out.println("\tBye. Hope to see you again soon!");
     System.out.println("\t-----------------------------------------");
+  }
+
+  private void addToList(String input) {
+    cheeseStack.add(input);
+  }
+
+  private void commandResponse(String input) {
+    switch(input) {
+      case "bye":
+        System.out.println("\tBye. Hope to see you again soon!");
+        break;
+      case "list":
+        System.out.println("\t-----------------------------------------");
+        System.out.println("\tHere are the tasks in your list:");
+        for (int i = 0; i < cheeseStack.size(); i++) {
+          System.out.println("\t" + (i + 1) + ". " + cheeseStack.get(i));
+        }
+        System.out.println("\t-----------------------------------------");
+        break;
+      default:
+        addToList(input);
+        System.out.println("\t-----------------------------------------");
+        System.out.println("\tadded: " + input);
+        System.out.println("\t-----------------------------------------");
+        break;
+
+    }
   }
 
   public static void main(String[] args) {
