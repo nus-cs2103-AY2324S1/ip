2540b5c6d70af3986109a26b73612e2acba7c399
J-hta-n <e0969121@u.nus.edu>
2023-08-23 02:15:01 +0800

    Add echo functionality

--- src/main/java/Ekud.java
+++ src/main/java/Ekud.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
 public class Ekud {
+    String horizontalLine = "##***~-~-~-~-~-~--~-~-~-~-~-~***##";
+    String intro = "Hello there! I'm Ekud.\nWhat can I do for you? :O";
+    String outro = "Goodbye, have a nice day! :p";
+    public void intro() {
+        this.echo(this.intro);
+    }
+    public void outro() {
+        this.echo(this.outro);
+    }
+    public void echo(String message) {
+        System.out.println(String.format("%s\n%s\n%s",
+                this.horizontalLine,
+                message,
+                this.horizontalLine));
+    }
+
     public static void main(String[] args) {
-        String horizontalLine = "------------------------------------";
-        String intro = "Hello there! I'm Ekud.\nWhat can I do for you?";
-        String outro = "Hope to see you again soon, goodbye!";
-        System.out.println(
-                String.format("%s\n%s\n%s\n%s\n%s",
-                        horizontalLine,
-                        intro,
-                        horizontalLine,
-                        outro,
-                        horizontalLine)
-        );
+        Scanner scanner = new Scanner(System.in);
+        Ekud chatbot = new Ekud();
+        chatbot.intro();
+        String userCommand = scanner.next();
+        while (!userCommand.equals("end")) {
+            chatbot.echo(userCommand);
+            userCommand = scanner.next();
+        }
+        chatbot.outro();
     }
 }
