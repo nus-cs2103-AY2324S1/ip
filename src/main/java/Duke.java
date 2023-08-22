ca9759d45a80bce233f434ff6eeb0749e61de765
Qiming <huangqiming@r-30-101-25-172.comp.nus.edu.sg>
2023-08-22 12:24:16 +0800

    Rename chatbot to Joi

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
+    private static final String NAME = "Joi";
+
+    private void run() {
+        this.greeting();
+    }
+
+    private void greeting() {
+        System.out.println("Hello! I'm " + NAME + "\nWhat can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke joi = new Duke();
+        joi.run();
     }
 }
