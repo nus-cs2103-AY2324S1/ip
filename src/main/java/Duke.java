9f287431db1ead9b0789c3f807f12c47f0208799
zhiruo <e0967827@u.nus.edu>
2023-08-22 21:02:11 +0800

    completed level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,10 +2,25 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        //Level 0
         String botName = "Pau";
         String introduction = " Hello! I'm " + botName + "\n" + " What can I do for you?";
         String exit = " Bye. Hope to see you again soon!\n";
         System.out.println(introduction);
-        System.out.println(exit);
+
+        //Level 1
+        String input;
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            input = scan.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(exit);
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
     }
 }
