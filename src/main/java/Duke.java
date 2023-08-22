5f302d979f3a53a17b19445a741138cd9aebb995
Song-Mengfei <e1075530@u.nus.edu>
2023-08-22 13:15:22 +0800

    Automated Text UI Testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        System.out.println("Hey summoner! I'm Three Wolves B.\n" + "What do you want me to do?\n");
         IBot myBot = new IBot();
         boolean state = true;
         Scanner sc = new Scanner(System.in);
