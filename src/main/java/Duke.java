bfc4994f0b7d26b98f3ce6922fcb9d777c4cb803
emzm2023 <meeselevels@elizabeths-MacBook-Air-3.local>
2023-08-22 18:05:32 +0800

    renamed chatbot, implemented greet and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
+    /** The name of the chatbot. */
+    private static final String NAME = "Bro";
+
+    /** Greets the user. */
+    public void greet() {
+        System.out.println("Hello! I'm " + NAME + "\n" + "What can I do for you? \n");
+    }
+
+    /** Causes chatbot to exit. */
+    public void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke chatbot = new Duke();
+        chatbot.greet();
+        chatbot.exit();
     }
 }
