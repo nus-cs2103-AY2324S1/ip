f34816bebcc57470332ac4650ec09a5959439b3f
zhanyang01 <ngzhanyang01@gmail.com>
2023-08-22 12:22:05 +0800

    Add level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "DukeKing";
+        String welcome = "Hello! I'm " + logo + "\nWhat can I do for you?";
+        String bye = "Bye. Hope to see you again soon!";
+        System.out.println(welcome + "\n" + bye);
     }
 }
