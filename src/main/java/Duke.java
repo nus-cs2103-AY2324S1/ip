fbf82b10c95a6488832c8db1c5d720fc834c9e13
yyyaohhh <yyyaohhh@gmail.com>
2023-08-22 22:10:06 +0800

    level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Ko...ko...ko..nichi...wa!!! I... I am Gotoh... Hitori desu! Σ(っ °Д °;)っ");
+        System.out.println("You... can call me... Bocchi. Bocchi chan...");
+        System.out.println("What can... can I do for you? (°□°;) ");
+        System.out.println("____________________________________________________________");
+        System.out.println("B... b...bye bye!... And ... see you! (〃ω〃)");
+        System.out.println("____________________________________________________________");
+
     }
 }
