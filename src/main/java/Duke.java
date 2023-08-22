c5956f76a26182900487522678124b72664e78bd
Marcus Soh <macussoh38@gmail.com>
2023-08-23 01:32:35 +0800

    incr: lvl-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
+    private static final String SEPARATOR_LINE = "____________________________________________________________";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        System.out.println(SEPARATOR_LINE);
+        String entranceMsg = "Hello! I'm Elon Musk.\n" +
+                "What can I do for you?";
+        System.out.println(entranceMsg);
+        System.out.println(SEPARATOR_LINE);
+
+        String exitMsg = "Bye! Hope to see you again soon.";
+        System.out.println(exitMsg);
+        System.out.println(SEPARATOR_LINE);
+
+
+
     }
 }
