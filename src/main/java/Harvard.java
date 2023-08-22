5001bd676ca0da1dc66e99cf952eb3ffee9bb01c
Kota Sudarshan Karanth <sudarshan2402@gmail.com>
2023-08-22 16:24:03 +0800

    Updated formatting of output with new horizontal lines

--- src/main/java/Harvard.java
+++ src/main/java/Harvard.java
@@ -5,19 +5,20 @@ public class Harvard {
         System.out.println(line);
         System.out.println("Hello! I'm Harvard\nWhat can I do for you?");
         System.out.println(line);
-        Scanner in = new Scanner(System.in);
 
+        Scanner in = new Scanner(System.in);
         String[] tasks = new String[100];
         int taskCount = 0;
+
         while (true) {
             String command = in.nextLine();
             if (command.equals("bye")) {
-
+                System.out.println(line);
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
             } else if (command.equals("list")) {
-
+                System.out.println(line);
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
@@ -25,7 +26,7 @@ public class Harvard {
             } else {
                 tasks[taskCount] = command;
                 taskCount++;
-
+                System.out.println(line);
                 System.out.println("added: " + command);
             }
             System.out.println(line);
