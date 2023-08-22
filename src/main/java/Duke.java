e41e6cc5747b910630e33e3f31804a0796d333c4
raynertjx <rayner742@gmail.com>
2023-08-22 18:14:05 +0800

    added delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -33,33 +33,41 @@ public class Duke {
                             allTasks +
                             "____________________________________________________________";
                     System.out.println(tasksList);
-                    continue;
-                } else if (input[0].equals("mark") || input[0].equals("unmark")) {
+                } else if (input[0].equals("mark") || input[0].equals("unmark") || input[0].equals("delete")) {
                     if (tasks.size() == 0) throw new DukeException("OOPS!!! There are currently no tasks.");
                     if (input.length < 2) throw new DukeException("OOPS!!! Please include the task index.");
-                    int taskIndex = Integer.parseInt(input[1]);
-                    if (taskIndex < 0 || taskIndex > tasks.size()) throw new DukeException("OOPS!!! Task does not exist.");
-                    Task chosenTask = tasks.get(taskIndex - 1);
-                    String taskStatusMessage;
+                    int taskIndex = Integer.parseInt(input[1]) - 1;
+                    if (taskIndex < 0 || taskIndex > tasks.size())
+                        throw new DukeException("OOPS!!! Task does not exist.");
+                    Task chosenTask = tasks.get(taskIndex);
+                    String taskStatusMessage = null;
                     if (input[0].equals("mark")) {
                         chosenTask.markAsDone();
-                        taskStatusMessage = " Nice! I've marked this task as done:\n";
-                    } else {
+                        taskStatusMessage = " Nice! I've marked this task as done:\n" +
+                                String.format("  %s\n", chosenTask);
+                    } else if (input[0].equals("unmark")) {
                         chosenTask.markAsUndone();
-                        taskStatusMessage = " OK, I've marked this task as not done yet:\n";
+                        taskStatusMessage = " OK, I've marked this task as not done yet:\n" +
+                                String.format("  %s\n", chosenTask);
+                    } else {
+                        tasks.remove(taskIndex);
+                        taskStatusMessage = " Noted. I've removed this task:\n" +
+                                String.format("  %s\n", chosenTask) +
+                                String.format(" Now you have %d tasks in the list.\n", tasks.size());
                     }
                     String finalMessage = "____________________________________________________________\n" +
                             taskStatusMessage +
-                            String.format("  %s\n", chosenTask) +
                             "____________________________________________________________";
                     System.out.println(finalMessage);
                     continue;
                 } else if (input[0].equals("todo")) {
-                    if (input.length <= 1) throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
+                    if (input.length <= 1)
+                        throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
                     String newTaskDesc = String.join(" ", input).substring(input[0].length() + 1);
                     newTask = new Todo(newTaskDesc);
                 } else if (input[0].equals("deadline")) {
-                    if (input.length <= 1) throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+                    if (input.length <= 1)
+                        throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
                     String taskInput = String.join(" ", input).substring(input[0].length() + 1);
                     int index = taskInput.indexOf("/by");
                     if (index == -1) throw new DukeException("OOPS!!! Please include the deadline.");
@@ -67,11 +75,13 @@ public class Duke {
                     String taskDeadline = taskInput.substring(index + 4);
                     newTask = new Deadline(taskDesc, taskDeadline);
                 } else if (input[0].equals("event")) {
-                    if (input.length <= 1) throw new DukeException("OOPS!!! The description of an event cannot be empty.");
+                    if (input.length <= 1)
+                        throw new DukeException("OOPS!!! The description of an event cannot be empty.");
                     String taskInput = String.join(" ", input).substring(input[0].length() + 1);
                     int fromIndex = taskInput.indexOf("/from");
                     int toIndex = taskInput.indexOf("/to");
-                    if (fromIndex == -1 || toIndex == -1) throw new DukeException("OOPS!!! Please include the to and from date/time of the event.");
+                    if (fromIndex == -1 || toIndex == -1)
+                        throw new DukeException("OOPS!!! Please include the to and from date/time of the event.");
                     String taskDesc = taskInput.substring(0, fromIndex - 1);
                     String taskFrom = taskInput.substring(fromIndex + 6, toIndex - 1);
                     String taskTo = taskInput.substring(toIndex + 4);
