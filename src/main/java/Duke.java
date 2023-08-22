439f39c64cc6dadabfbef52a9e3ecf885f5c8de0
butteredyakiimo <nicoleiscolder@gmail.com>
2023-08-23 02:02:52 +0800

    Add a Task class, users can mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,25 +9,41 @@ public class Duke {
         String bye = "Signing off, see you later!\n";
 
         System.out.println(line + intro + line); //greeting
-        String task = "";
-        ArrayList<String> list = new ArrayList<>();
+        String input = "";
+        String command = "";
+        ArrayList<Task> list = new ArrayList<>();
 
         while (true) {
-            System.out.println("Add a task: ");
             Scanner scanner = new Scanner(System.in); //create Scanner object
-            task = scanner.nextLine();
-            if (task.equals("bye")) {
+            input = scanner.nextLine(); //get user input
+            command = input.split(" ", 2)[0]; //gets the first word of input
+
+            if (input.equals("bye")) {
                 break;
-            } else if (task.equals("list")) {
+
+            } else if (input.equals("list")) {
                 String result = "";
                 for (int i = 0; i < list.size(); i++) {
                     int index = i + 1;
-                    result += index + ". " + list.get(i) + "\n";
+                    Task task = list.get(i);
+                    result += index + ". " + task.toString() + "\n";
                 }
+                System.out.println("Here's your todo list: ");
                 System.out.println(result + line);
+
+            } else if (command.equals("mark")) {
+                int taskID = Integer.parseInt(input.split(" ", 2)[1]) - 1; //because index starts from 0
+                list.get(taskID).mark();
+                System.out.println(line);
+
+            } else if (command.equals("unmark")) {
+                int taskID = Integer.parseInt(input.split(" ", 2)[1]) - 1; //because index starts from 0
+                list.get(taskID).unmark();
+                System.out.println(line);
+                
             } else {
-                list.add(task);
-                System.out.println("New task added: " + task + "\n" + line);
+                list.add(new Task(0, input));
+                System.out.println("New task added: " + input + "\n" + line);
             }
         }
         System.out.println(line + bye + line);
---
+++ src/main/java/Task.java
@@ -0,0 +1,35 @@
+public class Task {
+    int status; //0 for uncompleted, 1 (or any other number) for completed
+    String task;
+
+    public Task(int status, String task) {
+        this.status = status;
+        this.task = task;
+    }
+
+    public void mark() {
+        if (this.status == 0) {
+            this.status = 1;
+            System.out.println("Yay! This task is completed: \n" + this.toString() + "\n");
+        } else {
+            System.out.println("This task is already marked as completed!");
+        }
+    }
+
+    public void unmark() {
+        if (this.status != 0) {
+            this.status = 0;
+            System.out.println("All righty, I've marked this task as uncompleted: \n" + this.toString() + "\n");
+        } else {
+            System.out.println("This task is already marked as uncompleted!");
+        }
+    }
+    @Override
+    public String toString() {
+        if (status == 0) {
+            return "[ ] " + task;
+        } else {
+            return "[X] " + task;
+        }
+    }
+}
