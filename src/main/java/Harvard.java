d1861b257abe62a81679255d6c2995cc070746ad
Kota Sudarshan Karanth <sudarshan2402@gmail.com>
2023-08-22 16:22:57 +0800

    Added the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Harvard.java
+++ src/main/java/Harvard.java
@@ -6,17 +6,29 @@ public class Harvard {
         System.out.println("Hello! I'm Harvard\nWhat can I do for you?");
         System.out.println(line);
         Scanner in = new Scanner(System.in);
+
+        String[] tasks = new String[100];
+        int taskCount = 0;
         while (true) {
             String command = in.nextLine();
             if (command.equals("bye")) {
+
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
+            } else if (command.equals("list")) {
+
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
             } else {
-                System.out.println(line);
-                System.out.println(command);
-                System.out.println(line);
+                tasks[taskCount] = command;
+                taskCount++;
+
+                System.out.println("added: " + command);
             }
+            System.out.println(line);
         }
         in.close();
     }
