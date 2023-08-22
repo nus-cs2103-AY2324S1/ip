d9cd649f09d6464fe8b7261c3cb3c613aa3f40f6
L1MJH <lolflgc@icloud.com>
2023-08-22 23:42:03 +0800

    Added task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,17 @@
+public class Task {
+    public String taskName;
+    public boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+}
