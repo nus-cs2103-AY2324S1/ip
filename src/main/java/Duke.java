4e5bac8fba4ecc60d5798eb2ddabdd3f6c40dd4c
Mahidharah <mahidharah@gmail.com>
2023-08-23 00:07:40 +0800

    1. Store whatever text entered by the user 2. Display them back to the user when requested with "list" command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -5,13 +6,24 @@ public class Duke {
         System.out.println("Hello! I'm IPSVIJAYKUMARAAKOODAIRRUKALAM");
         System.out.println("What can I do for you?");
         String userinput;
+        ArrayList<String> list = new ArrayList<>();
         Scanner scan = new Scanner( System.in );
 
         userinput = scan.nextLine();
 
         while (!userinput.equalsIgnoreCase("bye")) {
-            System.out.println(userinput);
-            userinput = scan.nextLine();
+            if (userinput.equalsIgnoreCase("list")) {
+
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(i + 1 + ". " + list.get(i));
+                }
+                userinput = scan.nextLine();
+
+            } else {
+                System.out.println("added: " + userinput);
+                list.add(userinput);
+                userinput = scan.nextLine();
+            }
         }
 
         System.out.println("Bye. Hope to see you again soon!");
