d451f02cd57932803cd7390f0d88ce3949f66a22
Yarn Meng <e0969262@u.nus.edu>
2023-08-22 18:43:28 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
+    private static String GREETINGS = "Hello! I'm AChatBot\n" +
+            "What can I do for you?";
+    private static String FAREWELL = "Bye. Hope to see you again soon!";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(GREETINGS);
+        System.out.println(FAREWELL);
     }
 }
