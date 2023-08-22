1a8875165b3c206310e9b77b5b87294eb3d4c8d1
Mohammed-Faizzzz <110959467+Mohammed-Faizzzz@users.noreply.github.com>
2023-08-22 11:14:55 +0800

    Level 1 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
 public class Duke {
+    private static final String name = "Faiz";
+    private static final String line = "__________________________________\n";
     public static void main(String[] args) {
-        String name = "Faiz";
-        String line = "------------------------\n";
-        String greeting = line + "Hello! I'm " + name + "\n" + "What can I do for you?" + "\n";
+
+        greet();
+        startChat();
+    }
+
+    private static void greet() {
+        String greeting = line + "Hello! I'm " + name + "\n" + "What can I do for you?" + "\n" + line;
+        System.out.println(greeting);
+    }
+
+    private static void startChat() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput= scanner.nextLine();
         String goodbye = line + "Bye. Hope to see you again soon!" + "\n" + line;
-        String logo = greeting + goodbye;
-        System.out.println(logo);
+
+        while (!userInput.equals("bye")){
+            echo(userInput);
+            userInput = scanner.nextLine();
+        }
+        System.out.println(goodbye);
+        scanner.close();
+    }
+
+    private static void echo(String command) {
+        String echo = line + command + "\n" + line;
+        System.out.println(echo);
     }
 }
