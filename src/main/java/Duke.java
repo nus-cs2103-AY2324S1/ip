85aa774d1907edb91cdfdff4b2e9204aa7d0ba9a
Delton Cheng <chengxuzhi@gmail.com>
2023-08-23 03:34:31 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 public class Duke {
 
+    String[] taskList = new String[100];
+    int taskCount = 0;
+
     String line = "~~*~~*~~*~~*~~*~~*~~*~~*~~*~~\n";
         public void chadGreet() {
             System.out.println(line);
@@ -19,6 +22,28 @@ public class Duke {
             System.out.println(input + "\n");
             System.out.println(line);
         }
+        public void chadAddList(String input) {
+            taskList[taskCount] = input;
+            taskCount++;
+            System.out.println(line);
+            System.out.println(input + " has been added to yo list!\n");
+            System.out.println(line);
+        }
+        public void chadListTask() {
+            if (taskCount == 0) {
+                System.out.println("Your task list is EMPTY!");
+            } else {
+                System.out.println(line);
+                System.out.println("Your outstanding tasks are...");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("Task " + (i + 1) + ") " + taskList[i]);
+                }
+                System.out.println("\n" + "Get to work NOW!\n");
+                System.out.println(line);
+            }
+        }
+
+
     public static void main(String[] args) {
         Duke chad = new Duke();
         chad.chadGreet();
@@ -31,7 +56,12 @@ public class Duke {
                 chad.chadBye();
                 break;
             }
-            chad.chadOutput(input);
+            else if (input.equals("list")) {
+                chad.chadListTask();
+            }
+            else {
+                chad.chadAddList(input);
+            }
         }
         scanObj.close();
 
