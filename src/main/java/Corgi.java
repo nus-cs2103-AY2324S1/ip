00b51995d26b0deb64d0a6571231241a81a9c2ca
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-08-22 18:33:59 +0800

    echo

--- src/main/java/Corgi.java
+++ src/main/java/Corgi.java
@@ -1,13 +1,36 @@
+import java.util.Scanner;
+
 public class Corgi {
     public static void main(String[] args) {
+        Corgi bot = new Corgi();
+        bot.start();
+    }
+
+    public void start() {
         String logo = "  ____ ___  ____   ____ ___\n"
                 + " / ___/ _ \\|  _ \\ / ___|_ _|\n"
                 + "| |  | | | | |_) | |  _ | |\n"
                 + "| |__| |_| |  _ <| |_| || |\n"
                 + " \\____\\___/|_| \\_\\\\____|___|\n";
         System.out.println(logo);
-        System.out.println("Hello! I'm Corgi!");
-        System.out.println("What can I do for you, hooman?");
-        System.out.println("Bye, take care and see you soon! *tail wags*");
+        System.out.println("Woof! I'm Corgi!");
+        System.out.println("What can I do for you, hooman?\n");
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            String userInput = sc.nextLine();
+
+            if (!userInput.toLowerCase().equals("bye")) {
+                System.out.println(userInput);
+            } else {
+                 System.out.println("Bye, take care and see you soon! *tail wags*");
+                 break;
+            }
+
+            System.out.println("------------------------------------------------------------");
+        }
+
+        sc.close();
     }
 }
