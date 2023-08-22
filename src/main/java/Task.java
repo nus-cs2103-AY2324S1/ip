fa8d6fa781af0d1ed1859f3c42790ebac1dc9a50
Loo Mei Lin Zann <>
2023-08-22 09:45:02 +0800

    add Task.java (for lvl3) and separated toString() method and getStatusIcon() method

---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() { //change to seperate it with the description
+        return "[" + (isDone ? "X" : " ") + "]"; // mark done task with X
+    }
+
+    @Override
+    public String toString() {
+        return this.getStatusIcon() + " " + this.description;
+    }
+
+    public void markAsDone(int task) {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone(int task) {
+        this.isDone = false;
+    }
+}
--- src/main/java/TehO.java
+++ src/main/java/TehO.java
@@ -25,13 +25,13 @@ public class TehO {
                 Task task = taskList[taskNumber];
                 task.markAsDone(taskNumber);
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println(task.getStatus());
+                System.out.println(task.toString());
             } else if (userCommand.startsWith("unmark")) {
                 int taskNumber = parseInt(userCommand.split(" ")[1]) - 1; //counting from 0
                 Task task = taskList[taskNumber];
                 task.markAsNotDone(taskNumber);
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(task.getStatus());
+                System.out.println(task.toString());
             } else {
                 Task task = new Task(userCommand);
                 addTask(task);
@@ -50,7 +50,7 @@ public class TehO {
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < taskCounter; i++) {
             Task task = taskList[i];
-            System.out.println((i+1) + ". " + task.getStatus());
+            System.out.println((i+1) + ". " + task.toString());
         }
     }
 }
