8bf452611252e8fdfe9c95f10c2c3f89c636529a
CelestineTan03 <e0968156@u.nus.edu>
2023-08-22 15:26:13 +0800

    Test using the I/O redirection technique

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -51,58 +51,61 @@ public class Duke {
         // Only exit when user types the command bye
         label:
         while (true) {
-            String command = sc.nextLine();
-            // Split string into first word and remaining words
-            String[] words = command.split(" ", 2);
-            String firstWord = words[0];
-            switch (firstWord) {
-                case "bye":
-                    System.out.println(farewell);
-                    sc.close();
-                    break label;
-                // Display the stored commands
-                case "list":
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < chatbot.numberOfTasks; i++) {
-                        System.out.println(i + 1 + "." + chatbot.tasks[i].toString());
-                    }
-                    break;
-                // Add task
-                case "todo":
-                    chatbot.addTask(words, "todo");
-                    break;
-                case "deadline":
-                    if (words[1].contains("/by")) {
-                        String[] task = words[1].split(" /by ");
-                        chatbot.addTask(task, "deadline");
-                    }
-                    break;
-                case "event":
-                    if (words[1].contains("/from") && words[1].contains("/to")) {
-                        String[] task = words[1].split(" /from ");
-                        chatbot.addTask(task, "event");
-                    }
-                    break;
-                // Mark task as done
-                case "mark":
-                    System.out.println("Nice! I've marked this task as done:");
-                    int markIndex = Integer.parseInt(words[1]);
-                    Task markTask = chatbot.tasks[markIndex - 1];
-                    markTask.markAsDone();
-                    System.out.println(markTask.toString());
-                    break;
-                // Mark task as not done
-                case "unmark":
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    int unmarkIndex = Integer.parseInt(words[1]);
-                    Task unmarkTask = chatbot.tasks[unmarkIndex - 1];
-                    unmarkTask.markAsNotDone();
-                    System.out.println(unmarkTask.toString());
-                    break;
-                // Invalid command
-                default:
-                    System.out.println("Invalid command.");
-                    break;
+            if (sc.hasNextLine()) {
+                String command = sc.nextLine();
+                // Split string into first word and remaining words
+                String[] words = command.split(" ", 2);
+                String firstWord = words[0];
+
+                switch (firstWord) {
+                    case "bye":
+                        System.out.println(farewell);
+                        sc.close();
+                        break label;
+                    // Display the stored commands
+                    case "list":
+                        System.out.println("Here are the tasks in your list:");
+                        for (int i = 0; i < chatbot.numberOfTasks; i++) {
+                            System.out.println(i + 1 + "." + chatbot.tasks[i].toString());
+                        }
+                        break;
+                    // Add task
+                    case "todo":
+                        chatbot.addTask(words, "todo");
+                        break;
+                    case "deadline":
+                        if (words[1].contains("/by")) {
+                            String[] task = words[1].split(" /by ");
+                            chatbot.addTask(task, "deadline");
+                        }
+                        break;
+                    case "event":
+                        if (words[1].contains("/from") && words[1].contains("/to")) {
+                            String[] task = words[1].split(" /from ");
+                            chatbot.addTask(task, "event");
+                        }
+                        break;
+                    // Mark task as done
+                    case "mark":
+                        System.out.println("Nice! I've marked this task as done:");
+                        int markIndex = Integer.parseInt(words[1]);
+                        Task markTask = chatbot.tasks[markIndex - 1];
+                        markTask.markAsDone();
+                        System.out.println(markTask.toString());
+                        break;
+                    // Mark task as not done
+                    case "unmark":
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        int unmarkIndex = Integer.parseInt(words[1]);
+                        Task unmarkTask = chatbot.tasks[unmarkIndex - 1];
+                        unmarkTask.markAsNotDone();
+                        System.out.println(unmarkTask.toString());
+                        break;
+                    // Invalid command
+                    default:
+                        System.out.println("Invalid command.");
+                        break;
+                }
             }
         }
     }
