f7cbc1a436cd8f609331dc37b504ee4d8458ecef
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2023-08-22 23:00:34 +0800

    Level 0
    Rename, greet and exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        String horizontalLine = "____________________________________________________________";
+        System.out.println(horizontalLine);
+        String myName = "Quack-NKN";
+        System.out.print("Hello from ");
+        System.out.println(myName);
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
     }
 }
