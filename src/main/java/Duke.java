4cf7ee367cb37d07db1a55e5604682dbc5a9946d
Dioclei <jasonepic0801@gmail.com>
2023-08-22 15:40:52 +0800

    Renamed Duke to Fluke. Added skeletal code.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
 public class Duke {
+    private final static String CHATBOT_NAME = "Fluke";
+    private final static String LOGO =
+            "    ________      __      \n" +
+            "   / ____/ /_  __/ /_____ \n" +
+            "  / /_  / / / / / //_/ _ \\\n" +
+            " / __/ / / /_/ / ,< /  __/\n" +
+            "/_/   /_/\\__,_/_/|_|\\___/ \n" +
+            "                          ";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(LOGO);
+        addHorizontalLine();
+        greet();
+        sayBye();
+    }
+
+    private static void greet() {
+        System.out.println(
+                "Hello! I'm " + CHATBOT_NAME + "\n" +
+                "What can I do for you?"
+        );
+        addHorizontalLine();
+    }
+
+    private static void sayBye() {
+        System.out.println("Bye. Hope to see you again soon!");
+        addHorizontalLine();
+    }
+
+    private static void addHorizontalLine() {
+        System.out.println("____________________________________________________________");
     }
 }
