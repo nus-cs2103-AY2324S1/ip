e2c2900e8fd532ec117d03dc72cd54c5047eb1c3
Nidhish Krishnan <nid.nk45@gmail.com>
2023-08-22 17:58:36 +0800

    Hello added Scanner to receive input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,15 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Maggie " +
-                "\nWhat can I do for you?" +
-                "\nBye. Hope to see you again soon!");
+                "\nWhat can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        while (!s.equals("bye")) {
+            System.out.println(s);
+            s = sc.next();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
