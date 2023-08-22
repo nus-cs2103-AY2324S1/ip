52010eb46a7d119b70d4176994f8893b26f55d50
kohkaijie <kohkaijie21@gmail.com>
2023-08-22 13:46:39 +0800

    level 0

---
+++ src/main/java/CR7.java
@@ -0,0 +1,6 @@
+public class CR7 {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm CR7\n" + "What can I do for you?\n");
+        System.out.println("Bye! Hope to see you again soon!");
+    }
+}
