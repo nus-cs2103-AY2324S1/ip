3e09bf67254ad68420dea6c76ecdf485c769382e
Nixx162 <nicholas.jimmyalden@u.nus.edu>
2023-08-22 10:07:13 +0800

    feat: add tasklist feature

--- src/main/java/pardiyem/logic/Pardi.java
+++ src/main/java/pardiyem/logic/Pardi.java
@@ -1,10 +1,15 @@
 package pardiyem.logic;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class Pardi {
     private final String GREETING = "\nHello, I'm Pardi\nWhat can I do for you?\n";
     private final String BYE = "\nCiao! See you again!\n";
+    private ArrayList<String> tasklist;
+    public Pardi() {
+        this.tasklist = new ArrayList<String>();
+    }
     public void greeting() {
         System.out.println(GREETING);
     }
@@ -17,7 +22,14 @@ public class Pardi {
         if (Objects.equals(in, "bye")) {
             return false;
         } else {
-            System.out.println("\n" + in + "\n");
+            if (Objects.equals(in, "list")) {
+                for (int i = 1; i <= this.tasklist.size(); i++) {
+                    System.out.printf("\n%d. %s", i, this.tasklist.get(i-1));
+                }
+            } else {
+                this.tasklist.add(in);
+                System.out.println("\nadded: " + in + "\n");
+            }
             return true;
         }
     }
