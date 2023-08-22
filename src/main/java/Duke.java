0ccc33a6f9b70d7d6ef38c7cd429f46325838a16
Li <e0985877@u.nus.edu>
2023-08-22 19:02:16 +0800

    Implement Level-1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
 public class Duke {
+    static Scanner scanner = new Scanner(System.in);
     private String name;
 
     public Duke(String name) {
         this.name = name;
     }
 
+    public void echo(String line) {
+        formatPrintMessage(line);
+    }
+
     public void greet() {
-        System.out.println();
         formatPrintMessage("Hello! I'm " + this.name + "\nWhat can I do for you?");
     }
     public void exit() {
@@ -14,12 +19,18 @@ public class Duke {
     }
 
     public void formatPrintMessage(String message) {
+        System.out.println();
         System.out.println(message);
         System.out.println();
     }
 
     public void run() {
         greet();
+        String line = scanner.nextLine();
+        while (!line.equals("bye")) {
+            echo(line);
+            line = scanner.nextLine();
+        }
         exit();
     }
 
@@ -30,4 +41,3 @@ public class Duke {
     }
 
 }
-
