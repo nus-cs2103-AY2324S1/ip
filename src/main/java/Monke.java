374994e0f3e8adfd7d8644ef2f7c4844c5a0c26c
jingjie88 <tanjingjie88@gmail.com>
2023-08-23 02:30:41 +0800

    Add list

--- src/main/java/Monke.java
+++ src/main/java/Monke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Monke {
+    private static String[] list = new String[100];
+    private static int index = 0;
 
     public static void speak(String msg) {
         System.out.println("\t" + msg);
@@ -23,25 +25,43 @@ public class Monke {
         System.out.println();
     }
 
-    public static void echo() {
+    public static void listen() {
         Scanner sc = new Scanner(System.in);
         while (true) {
-            String input = sc.next();
+            String input = sc.nextLine();
             if (input.equals("bye")) {
                 break;
             }
+            if (input.equals("list")) {
+                Monke.displayList();
+                continue;
+            }
             Monke.printHorizontalLine();
-            Monke.speak(input);
+            Monke.speak("added: " + input);
+            Monke.addToList(input);
             Monke.printHorizontalLine();
         }
         Monke.printHorizontalLine();
         sc.close();
     }
 
+    public static void addToList(String item) {
+        Monke.list[index] = item;
+        Monke.index++;
+    }
+
+    public static void displayList() {
+        Monke.printHorizontalLine();
+        for (int i = 0; i < Monke.index; i++) {
+            Monke.speak((i + 1) + ". " + Monke.list[i]);
+        }
+        Monke.printHorizontalLine();
+    }
+
     public static void main(String[] args) {
         Monke.printHorizontalLine();
         Monke.greet();
-        Monke.echo();
+        Monke.listen();
         Monke.exit();
     }
 }
