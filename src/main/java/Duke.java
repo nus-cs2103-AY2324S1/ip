b4458ab8cf67e8f5b16062c58dd3d87bac07a37f
ChuanXinNg <ngcx0103@gmail.com>
2023-08-22 16:57:35 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,42 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+
+    public String greet() {
         String logo = " ____                \n"
                 + "|  _ \\  _____  _____ \n"
                 + "| |/ / |  _  ||  _  |\n"
                 + "| |\\ \\ | |_| || |_| |\n"
-                + "|_| \\_\\|_____||_____|";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Roo!");
-        System.out.println("What can I do for you ah?\n");
+                + "|_| \\_\\|_____||_____|\n";
+        String hello = "Hello! I am Roo!!\n" + "What can I do for you ah?\n";
+        return "Hello from\n" + logo + hello;
+    }
 
-        String bye = "Ciao! Hope to see you soon yorr!!";
-        System.out.println(bye);
+    public String bye() {
+        return "Ciao! Hope to see you soon yorr!!";
+    }
+
+    public String echo(String s) {
+        return s + "\n";
+    }
+
+    public static void main(String[] args) {
+        Duke roo = new Duke();
+        System.out.println(roo.greet());
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        while (true) {
+            if (!input.equals("bye")) {
+                System.out.println(roo.echo(input));
+                input = sc.next();
+            } else {
+                sc.close();
+                System.out.println(roo.bye());
+                break;
+            }
+        }
+
     }
 }
