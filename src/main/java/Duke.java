e5f4449e8912e9185e0e33f6d78b57979adad64e
tiif <pekyuan@gmail.com>
2023-08-22 20:42:34 +0800

    complete level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) throws IOException{
         /*
@@ -11,15 +12,23 @@ public class Duke {
          */
         String start = "Hello! I'm ChatBot" + "\n" + "What can I do for you?" + "\n";
         System.out.println(start);
+        ArrayList<String> tdlist = new ArrayList<String>();
         while (true) {
             BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
             String str = bf.readLine();
-            if (str.equals("bye")) {
+            if (str.equals("list")) {
+                int size = tdlist.size();
+                for (int i = 0; i < size; i++) {
+                    System.out.println((i + 1) + ". " + tdlist.get(i));
+                }
+            } else if (str.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else {
+                tdlist.add(str);
+                System.out.println("added: " + str);
             }
-            System.out.println(str);
         }
 
-        }
+    }
 }
