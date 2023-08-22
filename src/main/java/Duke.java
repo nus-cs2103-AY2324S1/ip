e78ec9c584e5124f2c17568c73af1adefa60873f
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2023-08-22 23:15:26 +0800

    Level 1
    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String horizontalLine = "____________________________________________________________";
+
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-        String horizontalLine = "____________________________________________________________";
         System.out.println(horizontalLine);
         String myName = "Quack-NKN";
         System.out.print("Hello from ");
         System.out.println(myName);
         System.out.println("What can I do for you?");
         System.out.println(horizontalLine);
+
+        boolean isExit = false;
+        Scanner scanner = new Scanner(System.in);
+        while (!isExit) {
+            // receive input
+            String command = scanner.nextLine();
+            System.out.println(horizontalLine);
+
+            // exit
+            if (command.equals("bye")) {
+                isExit = true;
+            }
+
+            // echo
+            else {
+                echo(command);
+            }
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+
+    private static void echo(String text) {
+        System.out.println(text);
         System.out.println(horizontalLine);
     }
 }
