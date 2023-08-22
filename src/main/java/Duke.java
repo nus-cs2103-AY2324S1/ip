dcbe7f82cdb49cb77d0ad70e8ccf822bb284e181
vijay-shankaranand <shankaranand48@gmail.com>
2023-08-23 02:00:52 +0800

    add level-1 changes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,17 @@
+
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello, I'm JavAI.\nWhat can I do for you?\n");
+        String output = input.nextLine();
+        while (!output.equals("bye")) {
+            System.out.println(output);
+            output = input.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
 
-        System.out.println("Hello, I'm JavAI.\nWhat can I do for you?\nBye. Hope to see you again soon!");
+
+
     }
 }
