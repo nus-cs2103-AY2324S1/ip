8dc38c193fd0f17e8b301d8d78dc54a9eacc141c
Mohammed-Faizzzz <110959467+Mohammed-Faizzzz@users.noreply.github.com>
2023-08-22 12:14:40 +0800

    Completed Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,20 +14,33 @@ public class Duke {
     }
 
     private static void startChat() {
+        String[] tasks = new String[100];
+        int taskId = 1;
         Scanner scanner = new Scanner(System.in);
         String userInput= scanner.nextLine();
         String goodbye = line + "Bye. Hope to see you again soon!" + "\n" + line;
 
         while (!userInput.equals("bye")){
-            echo(userInput);
+            if (userInput.equals("list")){
+                System.out.println(line);
+                for(int i = 1; i < taskId; i++){
+                    System.out.println(i + ". " + tasks[i-1]);
+                }
+                System.out.println(line + "\n");
+            }
+            else {
+                addToList(userInput, tasks, taskId);
+                taskId++;
+            }
             userInput = scanner.nextLine();
         }
         System.out.println(goodbye);
         scanner.close();
     }
 
-    private static void echo(String command) {
-        String echo = line + command + "\n" + line;
-        System.out.println(echo);
+    private static void addToList(String command, String[] tasks, int numTasks) {
+        String task = line + "added: " + command + "\n" + line;
+        tasks[numTasks - 1] = command;
+        System.out.println(task);
     }
 }
