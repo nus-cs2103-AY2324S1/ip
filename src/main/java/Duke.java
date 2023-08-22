8357c1646c25356ababcedc0f5008bab8a4875b3
BEATRICE <beatricechen2003@gmail.com>
2023-08-22 18:15:22 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,8 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm froggie! ");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
