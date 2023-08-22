455b70de90223b55464bb5c8ae14f13aa301054e
nicholascher <nicholascherkwokhui@gmail.com>
2023-08-22 23:54:06 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+
         String name = "nic";
-        String exit = "Bye. Hope to see you again soon!";
         String line = "___________________________________\n";
-        System.out.println(line + "Hello from " + name + "\nWhat can i do for you?\n" + line + exit );
+        String exit = line + "Bye Bye. Hope to see you again soon!\n" + line;
+
+        System.out.println(line + "Hello I'm " + name + "\nWhat can i do for you?\n" + line);
+
+        Scanner scan = new Scanner(System.in);
+
+
+        while (true) {
+            String input = scan.nextLine();
+            if ("bye".equals(input)) {
+                System.out.println(exit);
+                break;
+            } else {
+                System.out.println(line + input + "\n" + line);
+            }
+
+        }
+
+
     }
 }
