ab44ef8ac3483d7bb7f1f996d18507d6845e8cf7
raynertjx <rayner742@gmail.com>
2023-08-22 11:03:16 +0800

    added mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Duke {
 
     public static void main(String[] args) {
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         String welcomeMessage = "____________________________________________________________\n" +
                 " Hello! I'm Bongo!\n" +
                 " What can I do for you?\n" +
@@ -13,18 +13,18 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
 
         while (sc.hasNext()) {
-            String input = sc.nextLine();
-            if (input.equals("bye")) {
+            String[] input = sc.nextLine().split(" ");
+            if (input[0].equals("bye")) {
                 String goodbyeMessage = "____________________________________________________________\n" +
                         " Bye. Hope to see you again soon!\n" +
                         "____________________________________________________________\n";
                 System.out.println(goodbyeMessage);
                 break;
             }
-            if (input.equals("list")) {
+            if (input[0].equals("list")) {
                 StringBuilder allTasks = new StringBuilder();
                 for (int i = 0; i < tasks.size(); i++) {
-                    allTasks.append(String.format(" %d. %s\n", i + 1, tasks.get(i)));
+                    allTasks.append(String.format("%d.%s\n", i + 1, tasks.get(i)));
                 }
                 String tasksList = "____________________________________________________________\n" +
                         allTasks +
@@ -32,9 +32,28 @@ public class Duke {
                 System.out.println(tasksList);
                 continue;
             }
-            tasks.add(input);
+            if (input.length >= 2 && (input[0].equals("mark") || input[0].equals("unmark"))) {
+                int taskIndex = Integer.parseInt(input[1]);
+                Task chosenTask = tasks.get(taskIndex - 1);
+                String taskStatusMessage;
+                if (input[0].equals("mark")) {
+                    chosenTask.markAsDone();
+                    taskStatusMessage = " Nice! I've marked this task as done:\n";
+                } else {
+                    chosenTask.markAsUndone();
+                    taskStatusMessage = " OK, I've marked this task as not done yet:\n";
+                }
+                String finalMessage = "____________________________________________________________\n" +
+                            taskStatusMessage +
+                            String.format("  %s\n", chosenTask) +
+                            "____________________________________________________________\n";
+                System.out.println(finalMessage);
+                continue;
+            }
+            String newTaskDesc = String.join(" ", input);
+            tasks.add(new Task(newTaskDesc));
             String echoMessage = "____________________________________________________________\n" +
-                    String.format(" added: %s\n", input) +
+                    String.format(" added: %s\n", newTaskDesc) +
                     "____________________________________________________________\n";
             System.out.println(echoMessage);
         }
