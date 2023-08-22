69d08b9f7033b5cde974fea5bd9cab8a490fc998
JCSnap <e0970167@u.nus.edu>
2023-08-23 01:31:40 +0800

    added feature to set a task as done or undone

--- src/main/java/BouncyBob.java
+++ src/main/java/BouncyBob.java
@@ -4,6 +4,14 @@ public class BouncyBob {
     private static final String MIDDLE_BORDER = "║                                              ║";
     private static final String BOTTOM_BORDER = "╚══════════════════════════════════════════════╝";
 
+    public static void printTaskStatus(Task task) {
+        String marking = " ";
+        if (task.isDone()) {
+            marking = "X";
+        }
+        System.out.println("[" + marking + "]" + " " + task.getName());
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Task[] database = new Task[100];
@@ -19,19 +27,42 @@ public class BouncyBob {
         while (true) {
             System.out.println("Enter something: ");
             String userInput = scanner.nextLine();
-            System.out.println(TOP_BORDER);
+            String[] parts = userInput.split(" ");
             if (userInput.equals("bye")) {
+                System.out.println(TOP_BORDER);
                 System.out.println(MIDDLE_BORDER);
                 System.out.println("║        BouncyBob: Bye! Bounce back soon!     ║");
                 System.out.println(MIDDLE_BORDER);
                 System.out.println(BOTTOM_BORDER);
                 break;
             } else if (userInput.equals("list")) {
+                System.out.println(TOP_BORDER);
+                System.out.println("Here are your tasks! ");
                 for (int i = 0; i < pointer; i++) {
-                    System.out.println(database[i].getName());
+                    Task curTask = database[i];
+                    printTaskStatus(curTask);
+                }
+                System.out.println(BOTTOM_BORDER);
+            } else if (parts.length == 2) {
+                String action = parts[0];
+                int index = Integer.parseInt(parts[1]); // This assumes that second part contains an int
+                Task task = database[index];
+                System.out.println(TOP_BORDER);
+                switch(action) {
+                    case "mark":
+                        System.out.println("You've done it, very bouncy!");
+                        task.setDone();
+                        printTaskStatus(task);
+                        break;
+                    case "unmark":
+                        System.out.println("Gotta pump for air! It's unmarked! ");
+                        task.setUnDone();
+                        printTaskStatus(task);
+                        break;
                 }
                 System.out.println(BOTTOM_BORDER);
             } else {
+                System.out.println(TOP_BORDER);
                 System.out.println("Added to database: " + userInput);
                 database[pointer] = new Task(userInput);
                 pointer++;
