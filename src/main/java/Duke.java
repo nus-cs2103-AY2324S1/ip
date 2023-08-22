7da6369378af7a096ef1e1521bca3039e583b90a
Zhetao <quzhetao2001@gmail.com>
2023-08-22 12:58:50 +0800

    Completed skeletal version Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
         printLine();
+        System.out.println("Hello! I'm FUNNY.\nWhat can I do for you?");
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
     }
-
+    
 
     public static void printLine() {
         for (int i = 0; i < 80; i++) {
