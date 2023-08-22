722564f98e9caf9997275283a1a09edee90174db
Jonyxzx <jonykhozhixiong@gmail.com>
2023-08-22 22:10:51 +0800

    Level 1 added

--- src/main/java/Ace.java
+++ src/main/java/Ace.java
@@ -1,20 +1,27 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Ace {
-    private String line() {
-        return "_____________________________________________________\n";
+    private String addLine(String message) {
+        String horizontal = "_____________________________________________________\n";
+        return horizontal + message + "\n" + horizontal;
     }
 
     private String greet() {
-        return "Hello! I'm Ace\nWhat can I do for you?\n";
+        return "Hello! I'm Ace\nWhat can I do for you?";
     }
 
     private String goodbye() {
-        return "Bye. Hope to see you again soon!\n";
+        return "Bye. Hope to see you again soon!";
     }
 
-    public String sendMessage() {
-        return line() + greet() + line() + goodbye() + line();
+    public String sendMessage(String keyWord) {
+        switch(keyWord) {
+            case "start":
+                return addLine(greet());
+            case "bye":
+                return addLine(goodbye());
+            default:
+                return addLine((keyWord));
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Ace chatBot = new Ace();
-        System.out.println(chatBot.sendMessage());
+        Scanner scanner = new Scanner(System.in);
+        String keyWord = "start";
+
+        while (true) {
+            if (keyWord.equals("bye")) {
+                System.out.println(chatBot.sendMessage("bye"));
+                scanner.close();
+                break;
+            } else {
+                System.out.println((chatBot.sendMessage(keyWord)));
+                keyWord = scanner.nextLine();
+            }
+        }
     }
 }
