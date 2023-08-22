16ff2bc32a7499e1f508d310c934e780dce0a0d7
Ting Yong Shen Amos <amostingyongquan@gmail.com>
2023-08-22 16:08:18 +0800

    Level 3. Mark as Done. Added Task.java. Added logic for "mark" and "unmark"

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -17,34 +17,62 @@ public class Jarvis {
         String greeting = "Good day Sir! I'm ";
         String question = "How can I help you today Sir?";
         String signOff = "Good bye Sir!";
+        String listInforming = "Here are the task in your list Sir:";
+        String markInforming = "Roger that Sir, I've marked this task as done:";
+        String unmarkInforming = "Noted Sir, I've marked this task as NOT done yet:";
 
         System.out.println(line);
         System.out.println(greeting + name + "!");
         System.out.println(question);
         System.out.println(line);
 
-        ArrayList<String> taskList = new ArrayList<String>(); // list of tasks
+        ArrayList<Task> taskList = new ArrayList<Task>(); // list of Tasks
 
         while (true) {
             Scanner userInput = new Scanner(System.in);
-            String task = userInput.nextLine();
+            String taskDescription = userInput.nextLine();
 
-            if (task.equals("list")) { // if "list" is entered
+            if (taskDescription.equals("list")) { // if "list" is entered
                 System.out.println(line);
-                for (int i = 0; i < taskList.size(); i++) {
+                System.out.println(listInforming);
+                for (int i = 0; i < taskList.size(); i++) { // listing out the current task
                     int count = i + 1;
-                    System.out.println(count + ". " + taskList.get(i));
+                    Task currentTask = taskList.get(i);
+                    System.out.println(count + "." + currentTask.getTaskString());
                 }
                 System.out.println(line);
-            } else if (task.equals("bye")) { // if "bye" is entered
+
+            } else if (taskDescription.equals("bye")) { // if "bye" is entered
                 System.out.println(line);
                 System.out.println(signOff);
                 System.out.println(line);
                 break;
+
+            } else if (taskDescription.contains("mark") || taskDescription.contains("unmark")) { // if "mark" or "unmark" is entered
+                int taskNum = Integer.parseInt(taskDescription.substring(taskDescription.length() - 1));
+                if (taskDescription.contains("unmark")) { // if "unmark" is entered
+                    Task currentTask = taskList.get(taskNum - 1);
+                    currentTask.setDone(false);
+
+                    System.out.println(line);
+                    System.out.println(unmarkInforming);
+                    System.out.println(currentTask.getTaskString());
+                    System.out.println(line);
+                } else { // if "mark" is entered
+                    Task currentTask = taskList.get(taskNum - 1);
+                    currentTask.setDone(true);
+
+                    System.out.println(line);
+                    System.out.println(markInforming);
+                    System.out.println(currentTask.getTaskString());
+                    System.out.println(line);
+                }
+
             } else { // if any other request
+                Task task = new Task(taskDescription);
                 taskList.add(task); // add new task to the end of task list
                 System.out.println(line);
-                System.out.println("added: " + task);
+                System.out.println("added: " + task.description);
                 System.out.println(line);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void setDone(boolean done) {
+        this.isDone = done;
+    }
+
+    public String getTaskString() { // generates the string of marking and task
+        String marking = "";
+        if (this.isDone) {
+            marking = "[✓]";
+        } else {
+            marking = "[✕]";
+        }
+        return marking + " " + this.description;
+    }
+}
