ebc09162728c1d5fa8b5da37715c908f52015b08
jean-cq <mary84060117@gmail.com>
2023-08-22 16:33:51 +0800

    A-TextUiTesting

--- src/main/java/URChatBot.java
+++ src/main/java/URChatBot.java
@@ -36,7 +36,7 @@ public class URChatBot {
             } else if (command.toUpperCase().startsWith("DELETE")){
                 int value = Integer.parseInt(command.replaceAll("[^0-9]", ""));
                 if (tasks.size() < 1 || tasks.size() < value) {
-                    throw new URChatBotException("☹ OOPS!!! No Task to delete!");
+                    throw new URChatBotException("OOPS!!! No Task to delete!");
                 }
                 Task deletedTask = tasks.get(value - 1);
                 tasks.remove(value - 1);
@@ -47,7 +47,7 @@ public class URChatBot {
                 }
             } else if (command.toUpperCase().startsWith("TODO")){
                 if (command.length() <= 5) {
-                    throw new URChatBotException("☹ OOPS!!! The description of a todo cannot be empty.");
+                    throw new URChatBotException("OOPS!!! The description of a todo cannot be empty.");
                 }
                 String task = command.substring(command.indexOf("todo") + 5);
                 Task newTask = new ToDo(task);
@@ -59,7 +59,10 @@ public class URChatBot {
                 }
             } else if (command.toUpperCase().startsWith("DEADLINE")){
                 if (command.length() <= 5) {
-                    throw new URChatBotException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                    throw new URChatBotException("OOPS!!! The description of a deadline cannot be empty.");
+                }
+                if (!command.contains("/by") || command.substring(command.indexOf("/by") + 3).trim().length() < 1) {
+                    throw new URChatBotException("OOPS!!! The deadline cannot be empty.");
                 }
                 String task = command.substring(command.indexOf("deadline") + 9, command.indexOf("/by") - 1);
                 String by = command.substring(command.indexOf("/by") + 4);
@@ -72,7 +75,10 @@ public class URChatBot {
                 }
             } else if (command.toUpperCase().startsWith("EVENT")){
                 if (command.length() <= 5) {
-                    throw new URChatBotException("☹ OOPS!!! The description of a todo cannot be empty.");
+                    throw new URChatBotException("OOPS!!! The description of a todo cannot be empty.");
+                }
+                if (!command.contains("/from") || !command.contains("/to") || command.substring(command.indexOf("/from") + 5, command.indexOf("/to") - 1).trim().length() < 1|| command.substring(command.indexOf("/to") + 3).trim().length() < 1) {
+                    throw new URChatBotException("OOPS!!! The from or/and to cannot be empty.");
                 }
                 String task = command.substring(command.indexOf("event") + 6, command.indexOf("/from") - 1);
                 String from = command.substring(command.indexOf("/from") + 6, command.indexOf("/to") - 1);
@@ -85,7 +91,7 @@ public class URChatBot {
                     System.out.println("Got it. I've added this task:\n  " + newTask.toString() + "\nNow you have " + tasks.size()  + " tasks in the list.");
                 }
             } else{
-                throw new URChatBotException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                throw new URChatBotException("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
             }catch (URChatBotException e) {
                 System.out.println(" " + e.getMessage());
