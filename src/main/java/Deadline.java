bea1467e751046db5011edbc6b440dbcfdc122bc
Zhetao <quzhetao2001@gmail.com>
2023-08-23 00:20:50 +0800

    Added Deadline class

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,14 @@
+public class Deadline extends Task {
+    private String due;
+
+    public Deadline(String task, String due) {
+        super(task);
+        this.due = due;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + "(by: " + due + ")";
+    }
+
+}
