d2dbc0433262d8ae693a4c00f8c8a8dba246b1e1
cmHuang777 <e0934116@u.nus.edu>
2023-08-22 23:00:50 +0800

    Added Error Handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,15 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Duke {
     private static final String CHAT_BOT_NAME = "Genos";
+    private static final HashMap<String, Command> commandMap = new HashMap<>(
+            Map.of("todo", Command.TODO, "event", Command.EVENT, "mark", Command.MARK,
+                    "unmark", Command.UNMARK, "list", Command.LIST, "bye", Command.EXIT,
+                    "deadline", Command.DEADLINE)
+    );
     private ArrayList<Task> tasks;
     private enum Command {
         MARK, UNMARK, LIST, EXIT, TODO, DEADLINE, EVENT, INVALID
@@ -16,14 +23,21 @@ public class Duke {
         System.out.println("Hello I'm " + Duke.CHAT_BOT_NAME);
         System.out.println("What can I do for you?");
         System.out.println("Please type your command below, I will store what you said");
-        System.out.println("Usage: \"list\" to see the list of text stored, \"bye\" to exit\n" +
-                "\"mark (number)\" to mark task no. (number) to be done, \"unmark (number)\" to mark it as undone");
+        System.out.println("Usage: \"list\" to see the list of text stored, \"bye\" to exit");
+        System.out.println("\"mark [number]\" to mark task no. [number] to be done, "
+                + "\"unmark [number]\" to mark it as undone");
+        System.out.println("\"todo [description]\" to add todo, \"event [description] /from [date] /to[date]\""
+                + " to add event, \"deadline [description] /by [date]\" to add deadline");
     }
 
     private void converse() {
         Scanner sc = new Scanner(System.in);
         while (true) {
             String text = sc.nextLine();
+            if (text.isEmpty()) {
+                System.out.println("Sorry, empty command is not supported");
+                continue;
+            }
             Command cmd = textToCommand(text);
 
             // non tasks command.
@@ -33,62 +47,84 @@ public class Duke {
             } else if (cmd == Command.LIST) {
                 this.listTasks();
                 continue;
+            } else if (cmd == Command.INVALID) {
+                System.out.println("    Command given is invalid! You must start with a "
+                        + "todo/event/deadline to add tasks, "
+                        + "or list/mark/unmark/bye for other purposes");
+                continue; // retype commands
             }
 
-            // adding tasks
-            Task task;
-            switch (cmd) {
-                case MARK:
-                    int num = Integer.parseInt(text.substring(5));
-                    if (num > this.tasks.size() || num <= 0) {
-                        System.out.println("The task number you are trying to mark does not exist yet.");
-                        continue;
-                    } else {
+            // adding tasks or marking tasks
+            try {
+                if (text.split(" ").length == 1) {
+                    throw new RuntimeException("    Description cannot be empty!");
+                }
+                Task task;
+                switch (cmd) {
+                    case MARK:
+                        int num = Integer.parseInt(text.substring(5));
+                        // index out of bounds error will be caught below
                         task = this.tasks.get(num-1);
                         task.doTask();
                         System.out.println("    I've marked this task as done:");
-                    }
-                    break;
-                case UNMARK:
-                    int num2 = Integer.parseInt(text.substring(7));
-                    if (num2 > this.tasks.size() || num2 <= 0) {
-                        System.out.println("The task number you are trying to unmark does not exist yet.");
-                        continue;
-                    } else {
+                        break;
+                    case UNMARK:
+                        int num2 = Integer.parseInt(text.substring(7));
+                        // index out of bounds error will be caught below
                         task = this.tasks.get(num2-1);
                         task.undoTask();
                         System.out.println("    I've marked this task as not done yet:");
-                    }
-                    break;
-                case TODO:
-                    System.out.println("    Added Todo to the list of tasks:");
-                    task = new Todo(text.substring(5));
-                    this.tasks.add(task);
-                    break;
-                case EVENT:
-                    System.out.println("    Added Event to the list of tasks:" );
-                    String[] parts = (text.substring(6)).split("/");
-                    String fromTime = parts[1].substring(5); // remove the "from "
-                    String toTime = parts[2].substring(3);  // remove the "to "
-                    task = new Event(parts[0], fromTime, toTime);
-                    this.tasks.add(task);
-                    break;
-                case DEADLINE:
-                    System.out.println("    Added Deadline to the list of tasks:");
-                    String[] parts2 = (text.substring(9)).split("/");
-                    String by = parts2[1].substring(3); // remove the "by "
-                    task = new Deadline(parts2[0], by);
-                    this.tasks.add(task);
-                    break;
-                default:
-                    System.out.println("    There seems to be some error here");
-                    continue;
+                        break;
+                    case TODO:
+                        String description = text.substring(5);
+                        if (description.isEmpty()) {
+                            throw new RuntimeException("Description of todo cannot be empty");
+                        }
+                        task = new Todo(description);
+                        this.tasks.add(task);
+                        System.out.println("    Added Todo to the list of tasks:");
+                        break;
+                    case EVENT:
+                        String[] parts = (text.substring(6)).split("/");
+                        if (parts.length != 3 || !parts[1].startsWith("from ") ||
+                            !parts[2].startsWith("to ")) {
+                            throw new RuntimeException("    Invalid format for adding an event! "
+                            + "Please enter in this format:\n"
+                            + "event [description] /from [date] /to [date]");
+                        }
+                        String fromTime = parts[1].substring(5); // remove the "from "
+                        String toTime = parts[2].substring(3);  // remove the "to "
+                        task = new Event(parts[0], fromTime, toTime);
+                        this.tasks.add(task);
+                        System.out.println("    Added Event to the list of tasks:" );
+                        break;
+                    case DEADLINE:
+                        String[] parts2 = (text.substring(9)).split("/");
+                        if (parts2.length != 2 || !parts2[1].startsWith("by ")) {
+                            throw new RuntimeException("    Invalid format for adding a deadline! "
+                                    + "Please enter in this format:\n"
+                                    + "deadline [description] /by [date]");
+                        }
+                        String by = parts2[1].substring(3); // remove the "by "
+                        task = new Deadline(parts2[0], by);
+                        this.tasks.add(task);
+                        System.out.println("    Added Deadline to the list of tasks:");
+                        break;
+
+                    default:
+                        throw new RuntimeException("    There seems to be some error here");
+                }
+                System.out.println("    " + task.toString());
+                System.out.println("    You currently have " + this.tasks.size() + " tasks in the list.");
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid task number. Please enter a valid number to mark/unmark");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("The task number you are trying to mark/unmark "
+                        + "does not exist yet.");
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
             }
-            System.out.println("    " + task.toString());
-            System.out.println("    You currently have " + this.tasks.size() + " tasks in the list.");
-
         }
-
     }
 
     private void listTasks() {
@@ -98,28 +134,8 @@ public class Duke {
     }
 
     private Command textToCommand(String text) {
-        if (text.equals("bye")) {
-            return Command.EXIT;
-        }
-        if (text.equals("list")) {
-            return Command.LIST;
-        }
-        if (text.substring(0, Math.min(text.length(), 4)).equals("mark")) {
-            return Command.MARK;
-        }
-        if (text.substring(0, Math.min(text.length(), 4)).equals("todo")) {
-            return Command.TODO;
-        }
-        if (text.substring(0, Math.min(text.length(), 5)).equals("event")) {
-            return Command.EVENT;
-        }
-        if (text.substring(0, Math.min(text.length(), 6)).equals("unmark")) {
-            return Command.UNMARK;
-        }
-        if (text.substring(0, Math.min(text.length(), 8)).equals("deadline")) {
-            return Command.DEADLINE;
-        }
-        return Command.INVALID;
+        String cmd = text.split(" ")[0];
+        return Duke.commandMap.getOrDefault(cmd, Command.INVALID);
     }
 
     public static void main(String[] args) {
