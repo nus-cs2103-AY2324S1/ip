712ed9441a90db370e5ad5f87036ef66288b6017
Ravern Koh <ravernkoh@gmail.com>
2023-08-22 14:01:24 +0800

    Add echo functionality

--- src/main/java/Ekud.java
+++ src/main/java/Ekud.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Ekud {
+    public static final String GREETING = "Hello! I'm Ekud!\nWhat can I do for you?";
+    public static final String FAREWELL = "Bye. Hope to see you again soon!";
+    public static final String PROMPT = "> ";
+
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Ekud!\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(GREETING);
+
+        String line;
+        while (true) {
+            System.out.print(PROMPT);
+            line = scanner.nextLine().trim();
+            if (line.equals("bye")) {
+                break;
+            } else {
+                System.out.println(line);
+            }
+        }
+
+        System.out.println(FAREWELL);
+
+        scanner.close();
     }
 }
