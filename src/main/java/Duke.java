a6e44e87053c73a5a7f7debf578cbe6a6d635e53
LAPTOP-SN5EU1FD\Miguel <e1024867@u.nus.edu>
2023-08-22 15:07:49 +0900

    Level 0 of the iP + Added greeting and exit messages as a skeletal programme

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________\n" +
+                " Hellooooooooooo! I'm Lati!\n" +
+                " What can I do for you? :3\n" +
+                "____________________________________________________________");
+        System.out.println(" Byeeeeee. Hope to see you again soon~~\n" +
+                "____________________________________________________________");
     }
 }
