d6cc28cb1eeac3dead773f0f1da59a7d4d564947
samuelmui8 <samuelmui8@gmail.com>
2023-08-22 18:15:48 +0800

    added list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("-----------------------------------------------");
-        System.out.println("Sup bro! I'm Brobot");
-        System.out.println("What can I do for you?");
-        System.out.println("-----------------------------------------------");
+        System.out.println("\t-----------------------------------------------");
+        System.out.println("\tSup bro! I'm Brobot");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t-----------------------------------------------");
         Scanner scanner = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
 
         while (true) {
             String input = scanner.nextLine();
+
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("\t-----------------------------------------------");
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println("\t" + (i + 1) + ". " + list.get(i));
+                }
+                System.out.println("\t-----------------------------------------------");
+
+            } else {
+                list.add(input);
+                System.out.println("\t-----------------------------------------------");
+                System.out.println("\tadded: " + input);
+                System.out.println("\t-----------------------------------------------");
             }
-            System.out.println("-----------------------------------------------");
-            System.out.println(input);
-            System.out.println("-----------------------------------------------");
         }
 
-        System.out.println("-----------------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("-----------------------------------------------");
+        System.out.println("\t-----------------------------------------------");
+        System.out.println("\tBye. Hope to see you again soon bro!");
+        System.out.println("\t-----------------------------------------------");
     }
 }
