49880b09d60fcc0fe8a4b156cba713ac94efd2f6
junnengsoo <soojunneng01@gmail.com>
2023-08-22 16:49:02 +0800

    Complete Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = ">. <\n";
@@ -7,8 +9,22 @@ public class Duke {
                             line +
                             "Hello! I'm " + name + "\n" +
                             "What can I do for you?\n" +
-                            line +
-                            "Bye. Hope to see you again soon!\n" +
                             line);
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(line + input + "\n" + line);
+        }
+
+        System.out.println(line +
+                "Bye. Hope to see you again soon!\n" +
+                line);
+        scanner.close();
     }
 }
