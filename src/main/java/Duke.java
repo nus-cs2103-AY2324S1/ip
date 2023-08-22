2f3e3c9075f99ef420021b93eea2174a4f8699ae
lihongguang00 <lihongguang00@gmail.com>
2023-08-22 20:33:06 +0800

    Level-1
    ChadGPT echoes commands entered by users, until user enters 'bye'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = "\n   _____ _    _          _____   _____ _____ _______ \n" +
+        final String logo = "\n   _____ _    _          _____   _____ _____ _______ \n" +
                 "  / ____| |  | |   /\\   |  __ \\ / ____|  __ \\__   __|\n" +
                 " | |    | |__| |  /  \\  | |  | | |  __| |__) | | |   \n" +
                 " | |    |  __  | / /\\ \\ | |  | | | |_ |  ___/  | |   \n" +
                 " | |____| |  | |/ ____ \\| |__| | |__| | |      | |   \n" +
                 "  \\_____|_|  |_/_/    \\_\\_____/ \\_____|_|      |_|   \n";
 
-        String horizontal = "--------------------------------------------------------";
+        final String horizontal = "-------------------------------------------------------------------";
 
         System.out.println(horizontal + logo + horizontal);
-        System.out.println("Welcome to CHADGPT, What would you like to do today?\n" + horizontal +
-                "\nBye. Hope to see you again soon!\n" + horizontal);
+        System.out.println("ChadGPT: Welcome to ChadGPT, What would you like to do today?\n" + horizontal);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("User: ");
+        while (!sc.hasNext("bye")) {
+            System.out.println("ChadGPT: " + sc.nextLine() + "\n" + horizontal);
+            System.out.print("User: ");
+        }
+
+        sc.close();
+
+        System.out.println("ChadGPT: Bye. Hope to see you again soon!\n" + horizontal);
     }
 }
