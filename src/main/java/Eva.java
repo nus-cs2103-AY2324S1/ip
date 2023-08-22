12442f1a396fe0db997c21c49c314361d8bb4e79
bhnuka <01bhanukaekanayake@gmail.com>
2023-08-23 00:18:50 +0800

    > Improved the skeletal version of Duke so that it echos commands entered by the user, and exits when the user types the command bye.

--- src/main/java/Eva.java
+++ src/main/java/Eva.java
@@ -9,12 +9,25 @@ public class Eva {
                 + " |______\\_/ \\__,_|\n";
         System.out.println("Hello from\n" + logo);
         Scanner scanner = new Scanner(System.in);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Eva");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t Hello! I'm Eva.");
+        System.out.println("\t What can I do for you?");
+        System.out.println("\t____________________________________________________________\n");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("\t____________________________________________________________");
+                System.out.println("\t Bye. Hope to see you again soon!");
+                System.out.println("\t____________________________________________________________");
+                break;
+            }
+
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\t" + input);
+            System.out.println("\t____________________________________________________________");
+        }
         scanner.close();
     }
 }
