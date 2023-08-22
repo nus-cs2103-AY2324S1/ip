80929c062a1b6a2b11c979c886ddd4b0e02d630e
Julius Bryan <67666355+juliusgambe@users.noreply.github.com>
2023-08-22 11:12:20 +0800

    feat: add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,15 +10,35 @@ public class Duke {
                 "What can I do for you?" + "\n" + Duke.lineSeparator);
 
         Scanner scanner = new Scanner(System.in);
-        
+
+        // Data
+        String[] answerList = new String[100];
+        int counter = 0;
+
         while (true) {
             String userInput = scanner.nextLine().trim(); // trim() removes leading and trailing spaces
 
+            // Different responses for different inputs
             if (userInput.equals("bye")) {
+                // Display goodbye message
                 System.out.println(Duke.lineSeparator + "\n" + "Bye. Hope to see you again soon!" + "\n" + Duke.lineSeparator);
                 break;
+            } else if (userInput.equals("list")) {
+                // Display Ordered list
+                System.out.println(Duke.lineSeparator);
+                for (int i = 0; i < counter; i++) {
+                    System.out.println((i + 1) + ". " + answerList[i]);
+                }
+                System.out.println(Duke.lineSeparator);
             } else {
-                System.out.println(Duke.lineSeparator + "\n" + userInput + "\n" + Duke.lineSeparator);
+                // Default input
+
+                // Save user input
+                answerList[counter] = userInput;
+
+                // Display user input has been added
+                System.out.println(Duke.lineSeparator + "\n" + "added:" + userInput + "\n" + Duke.lineSeparator);
+                counter++;
             }
         }
 
