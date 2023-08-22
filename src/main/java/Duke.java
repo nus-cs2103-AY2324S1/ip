1c9fe6066ad7621f57270f8dd9e78b693e8e75c7
AlyssaPng <e0959598@u.nus.edu>
2023-08-22 16:24:19 +0800

    Level-2: Added List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,13 +3,22 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Chatty\n" + "What can I do for you?");
         Scanner scanner = new Scanner(System.in);
+        String[] ls = new String[100];
+        int i = 0;
 
         while (true) {
             String userinput = scanner.nextLine();
-            if (userinput.equals("bye")) {
+            if (userinput.equals("list")) {
+                for (int j = 0; j < i; j++) {
+                    int index = j + 1;
+                    System.out.println(index + ". " + ls[j]);
+                }
+            } else if (userinput.equals("bye")) {
                 break;
             } else {
-                System.out.println(userinput);
+                ls[i] = userinput;
+                i++;
+                System.out.println("added: " + userinput);
             }
         }
 
