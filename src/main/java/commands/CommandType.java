5956114eb284eefc1423937226a3ab31dadaf78a
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-08-23 04:43:17 +0800

    Add JavaDoc for CommandType enum

--- src/main/java/commands/CommandType.java
+++ src/main/java/commands/CommandType.java
@@ -1,8 +1,18 @@
 package commands;
 
+/**
+ * Types of commands that can be given to the Corgi chat bot.
+ */
 public enum CommandType {
     MARK, UNMARK, TODO, DEADLINE, EVENT, BYE, LIST;
 
+    /**
+     * Retrieves the corresponding CommandType enum value based on the given command string.
+     *
+     * @param commandStr The command string to match
+     * @return The matching CommandType enum value
+     * @throws InvalidCommandException if the command string is not recognized
+     */
     public static CommandType getCommandType(String commandStr) throws InvalidCommandException{
         switch (commandStr.toLowerCase()) {
             case "mark":
