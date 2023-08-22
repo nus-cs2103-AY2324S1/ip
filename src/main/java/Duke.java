e6146b9d29ca7be68b90ee98aa4a7539c620bd83
Nicholas <nicholaschiazhijie2001@yahoo.com>
2023-08-22 15:55:41 +0800

    completed level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String greeting = "____________________________________________________________\n"
+                + "Hello! I'm Lorem\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+        System.out.println(greeting);
     }
 }
