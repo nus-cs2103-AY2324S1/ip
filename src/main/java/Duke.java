9400b64b4b63a7dd332b2b299e34302b4d558ac1
butteredyakiimo <nicoleiscolder@gmail.com>
2023-08-23 00:20:18 +0800

    Add user input, chatbot echos command, exits with bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String line = "---------------------------------------\n";
+        String line = "\n---------------------------------------\n";
         String intro = "Hey there! I'm BUTTER.\n" +
-                "How can I help you today?\n";
-        String bye = "Signing off, see you later!\n";
+                "How can I help you today?";
+        String bye = "Signing off, see you later!";
 
-        System.out.println(line + intro + line + bye + line);
+        System.out.println(line + intro + line); //greeting
+        String task = "";
+        while (true) {
+            System.out.println("Add a task: ");
+            Scanner scanner = new Scanner(System.in); //create Scanner object
+            task = scanner.nextLine();
+            if (task.equals("bye")) {
+                break;
+            } else {
+                System.out.println("New task added: " + task + line);
+            }
+        }
+        System.out.println(line + bye + line);
     }
 }
