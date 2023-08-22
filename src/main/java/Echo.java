2fe8ea4515a1094286c9c5a82cd5c7ba98199827
JasonRay168 <e0985893@u.nus.edu>
2023-08-22 19:32:07 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Echo.java
@@ -0,0 +1,11 @@
+public class Echo {
+    public static void main(String[] args) {
+        String LINE_BREAK = "____________________________________________________________";
+        System.out.println(LINE_BREAK);
+        System.out.println("Welcome. My name is Echo");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE_BREAK);
+        System.out.println("I hope you enjoy my service.");
+        System.out.println(LINE_BREAK);
+    }
+}
