2f434dfa43f812394c55b5ec0c66ab05de6bd3d7
nicrandomlee <leejpnicholas@gmail.com>
2023-08-22 20:19:29 +0800

    Improve the skeletal version of Duke so that it echos commands entered by the user, and exits when the user types the command bye.

--- src/main/java/Cupid.java
+++ src/main/java/Cupid.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Cupid {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, I'm Cupid.");
         System.out.println("What can I do for you?");
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.toLowerCase().equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
