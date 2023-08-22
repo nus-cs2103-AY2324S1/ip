2aa5192c67cb3d5f5219eff55aea6508339e1617
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-08-23 02:27:54 +0800

    Add JavaDoc to Task class

--- src/main/java/tasks/Task.java
+++ src/main/java/tasks/Task.java
@@ -1,26 +1,50 @@
 package tasks;
 
+/**
+ * Task class storing description and status.
+ */
 public class Task {
     protected String desc;
     protected boolean status;
 
+    /**
+     * Initializes a new task with its description. The task's initial status is set to not done.
+     *
+     * @param desc The description of the task.
+     */
     public Task(String desc) {
         this.desc = desc;
         this.status = false;
     }
 
+    /**
+     * Mark task as done.
+     */
     public void markAsDone() {
         this.status = true;
     }
 
+    /**
+     * Mark task as not done.
+     */
     public void markAsNotDone() {
         this.status = false;
     }
 
+    /**
+     * Returns an icon representing the status of the task.
+     * 
+     * @return An icon ("X" for done, " " for not done).
+     */
     public String getStatusIcon() {
         return (this.status ? "X" : " ");
     }
 
+    /**
+     * Returns a string representation of the task.
+     *
+     * @return Status icon and description of the task.
+     */
     @Override
     public String toString() {
         return "[" + this.getStatusIcon() + "] " + this.desc;
