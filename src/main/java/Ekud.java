fda2240e0300bed7389f105fd37973f6e8b2abcd
Ravern Koh <ravernkoh@gmail.com>
2023-08-22 14:35:02 +0800

    Add adding and listing functionality

--- src/main/java/Ekud.java
+++ src/main/java/Ekud.java
@@ -1,28 +1,52 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ekud {
     public static final String GREETING = "Hello! I'm Ekud!\nWhat can I do for you?";
     public static final String FAREWELL = "Bye. Hope to see you again soon!";
+
     public static final String PROMPT = "> ";
 
+    public ArrayList<Task> tasks = new ArrayList<>();
+
+    public boolean processLine(String line) {
+        if (line.equals("list")) {
+            if (tasks.isEmpty()) {
+                System.out.println("No tasks yet. Add one!");
+                return false;
+            }
+            for (int index = 0; index < tasks.size(); index++) {
+                // Add padding to align single-digit numbers if we'll render
+                // two-digit numbers later on.
+                if (tasks.size() > 9 && index < 9) {
+                    System.out.print(" ");
+                }
+                System.out.print(index + 1);
+                Task task = tasks.get(index);
+                System.out.println(". " + task.getTitle());
+            }
+        } else if (line.equals("bye")) {
+            return true;
+        } else if (!line.isEmpty()) {
+            Task task = new Task(line);
+            tasks.add(task);
+            System.out.println("added: " + task.getTitle());
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
+        Ekud program = new Ekud();
         Scanner scanner = new Scanner(System.in);
-
         System.out.println(GREETING);
-
-        String line;
         while (true) {
             System.out.print(PROMPT);
-            line = scanner.nextLine().trim();
-            if (line.equals("bye")) {
+            String line = scanner.nextLine().trim();
+            if (program.processLine(line)) {
                 break;
-            } else {
-                System.out.println(line);
             }
         }
-
         System.out.println(FAREWELL);
-
         scanner.close();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    private final String title;
+
+    public Task(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
