98012a3b0a1de56d96339159aadf3da2b2f35960
revdrag <94689523+revdrag@users.noreply.github.com>
2023-08-22 16:45:23 +0800

    Implemented Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "____________________________________________________________";
+        String logo = "        ┏┓\n"
+                + "        ┃┃\n"
+                + "        ┃┗━┳┓╋┏┳━━┓\n"
+                + "        ┃┏┓┃┃╋┃┃┃━┫\n"
+                + "        ┗━━┻━┓┏┻━━┛\n"
+                + "        ╋╋╋┏━┛┃\n"
+                + "        ╋╋╋┗━━┛\n";
+
+        System.out.println(line + "\nHello! I'm \n" + logo + line);
+        System.out.println("Bye (actually hehe). Hope to see you again!\n" + line);
     }
 }
