db24df90d449213910c319549fd0d80d767a4b78
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-08-22 20:10:10 +0800

    Add ToDo class

---
+++ src/main/java/tasks/ToDo.java
@@ -0,0 +1,13 @@
+package tasks;
+
+public class ToDo extends Task {
+    
+    public ToDo(String desc) {
+        super(desc);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
