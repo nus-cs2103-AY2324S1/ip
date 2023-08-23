a0b2704e711258f82760676da617a1c7de170c6e
brendanneojw <e0726880@u.nus.edu>
2023-08-23 18:44:43 +0800

    Implemented Level 0: Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printHoriLine();
+        System.out.println("WEEWOOWEEWOO WELCOME! I'm Siren");
+        System.out.println("What can I do for you?");
+        printHoriLine();
+        System.out.println("WEEEWOOWEEWOO GOODBYE! Hope to see you again soon!");
+        printHoriLine();
+    }
+
+    public static void printHoriLine() {
+        System.out.println("____________________________________________________________");
     }
 }
