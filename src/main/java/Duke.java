efc1b55747638f300d19689f64554b3b39bda481
Sher Yew <tansheryew@gmail.com>
2023-08-23 18:04:31 +0800

    A-TextUiTesting
    Automated Text UI Testing
    -- Removed the code for initialising new scanner from the while loop

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,9 +9,9 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm " + chatbot);
         System.out.println("What can I do for you?");
+        Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            Scanner scanner = new Scanner(System.in);
             String user_input = scanner.nextLine();
             String[] words = user_input.split(" ");
             /* This is the check for deadline */
