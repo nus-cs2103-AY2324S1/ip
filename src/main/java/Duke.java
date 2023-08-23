a0753cdeed3dc2fc8b4973c61d9764df5196c44f
Debbie Hii Wenxin <e0959749@u.nus.edu>
2023-08-24 00:41:56 +0800

    Level-2 Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        ArrayList<String> array = new ArrayList<>();
         String horizontalLine = "   ------------------------\n";
         System.out.println(horizontalLine
                 + "     GREETINGS HUMAN! I AM QLATZ! □ \n"
-                + "     I AM NOW A PARROT\n\n"
+                + "     I AM NOW A LISTMAKER\n"
                 + horizontalLine);
 
         Scanner scanner = new Scanner(System.in);
@@ -14,8 +16,17 @@ public class Duke {
             String text = scanner.nextLine();
             if (text.equalsIgnoreCase("bye"))
                 break;
-            text = "     " + text;
-            System.out.println("\n" + horizontalLine + text + "\n" + horizontalLine);
+            else if(text.equalsIgnoreCase("list")) {
+                System.out.println("\n" + horizontalLine);
+                for (int i = 1; i <= array.size(); i++) {
+                    System.out.println("     " + i + ". " + array.get(i - 1));
+                }
+                System.out.println("\n" + horizontalLine);
+            } else {
+                array.add(text);
+                text = "     " + text;
+                System.out.println("\n" + horizontalLine + text + "\n" + horizontalLine);
+            }
         }
 
         System.out.println("     BYE!\n" + horizontalLine);
