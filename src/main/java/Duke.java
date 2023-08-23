97013d9e2d011c8936ee56a91c839d9271a32b34
WeeMingQing <e0725595@u.nus.edu>
2023-08-23 14:33:48 +0800

    Add echo.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,15 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        Scanner jonBird = new Scanner(System.in);
+        String input = "";
         System.out.println("Hello! I'm JonBird.\nWhat can I do for you?\n");
+        while (true) {
+            input = jonBird.nextLine();
+            if (input.equalsIgnoreCase("bye")) break;
+            System.out.println("\t" + input);
+        }
+        jonBird.close();
         System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
