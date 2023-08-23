aabbbff1c4ba3480a6414d59fe396b165f16c419
Seonlo99 <Zheng_chenglong1999@hotmail.com>
2023-08-23 22:35:27 +0800

    Add ability to store and display

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Siri");
         System.out.println("What can I do for you?");
+
+        String[] store = new String[100];
+        int size = 0;
+        size = echo(store, size);
         exit();
+    }
+
+    private static int echo(String[] store, int size) {
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String word = sc.nextLine();
+            if (word.equals("bye")) {
+                break;
+            } else  if (word.equals("list")) {
+                for (int i = 0; i < size; ++i) {
+                    System.out.println(i + 1 + ". " + store[i]);
+                }
+            } else {
+                store[size++] = word;
+                System.out.println("added: " + word);
+            }
+        }
+        return size;
     }
 
     private static void exit(){
