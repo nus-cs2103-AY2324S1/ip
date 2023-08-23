2e7e379d7cb1af24d5e6233b3d959611ee825ca3
suryanshkushwaha <e0985892@u.nus.edu>
2023-08-24 00:49:58 +0800

    echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        String lines = "____________________________________________________________\n";
-        String intro = "Hello! I'm sillyBot\nWhat can I do for you?\n";
-        String exit = "Bye. Hope to see you again soon!\n";
-        System.out.println(lines + intro + lines + exit + lines);
+        Duke duke = new Duke();
+        duke.intro();
+        duke.run();
+        duke.exit();
+    }
+
+    public void lines() {
+        System.out.println("_".repeat(50));
+    }
+    public void intro() {
+        lines();
+        System.out.println("Hello! I'm sillyBot\nWhat can I do for you?");
+        lines();
+    }
+
+    public void exit() {
+        lines();
+        System.out.println("Bye. Hope to see you again soon!");
+        lines();
+    }
+
+    public void run() {
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            lines();
+            System.out.println(input);
+            lines();
+            input = sc.nextLine();
+        }
     }
 }
