60cbb228698c137deadace9f17c67bc15e6fab1c
WeeMingQing <e0725595@u.nus.edu>
2023-08-23 16:03:55 +0800

    Add Task class.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,32 +1,64 @@
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        boolean listen = true;
         int index = 0;
         /** Captures user input*/
         Scanner jonBird = new Scanner(System.in);
         /** Stores user input*/
-        String[] inputList =  new String[100];
+        Task[] inputList =  new Task[100];
         /** User input*/
         String input = "";
         System.out.println("Hello! I'm JonBird.\nWhat can I do for you?\n");
-        while (true) {
+        while (listen) {
             input = jonBird.nextLine();
-            if (input.equalsIgnoreCase("bye")) break;
-            if (input.equalsIgnoreCase("list")) {
-                printList(inputList, index);
-            } else {
-                inputList[index] = input;
-                index += 1;
-                System.out.println("\tadded: " + input);
+            String[] inp = input.split("\\s+");
+            int taskIndex = 0;
+            if (inp[0].equalsIgnoreCase("mark") || inp[0].equalsIgnoreCase("unmark")) {
+                if (inp.length == 2) {
+                    try {
+                        taskIndex = Integer.parseInt(inp[1]);
+                        input = inp[0];
+                    } catch (NumberFormatException e) {
+                    }
+                    ;
+                }
+            }
+
+            switch (input) {
+                case "":
+                    if (inp.length == 0) {
+                        System.out.println("Please enter something!");
+                    } else {
+                        System.out.println("Unknown command!");
+                    }
+
+                case "bye":
+                    listen = false;
+                    break;
+                case "list":
+                    printList(inputList, index);
+                    break;
+                case "unmark":
+                    inputList[taskIndex-1].markAsUndone();
+                    break;
+                case "mark":
+                    inputList[taskIndex-1].markAsDone();
+                    break;
+                default:
+                    inputList[index] = new Task(input);
+                    index += 1;
+                    System.out.println("\tadded: " + input);
             }
         }
         jonBird.close();
-        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 
-    public static void printList(String[] list, int index) {
+    public static void printList(Task[] list, int index) {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < index; i++) {
-            System.out.println("\t"+ (i+1) + ". " + list[i]);
+            System.out.println("\t"+ (i+1) + ". " + list[i].printTask());
         }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,38 @@
+/** Class to represent the task set by user*/
+public class Task {
+    /** Description of the task*/
+    protected String description;
+    /** Track whether the task is done or not*/
+    protected boolean isDone;
+
+    /** Cosntructor for the class*/
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /** Method to mark the task as done*/
+    public void markAsDone() {
+        this.isDone = true;
+        System.out.println("\tNice! I've marked this task as done:");
+        System.out.println("\t\t" +this.printTask());
+    }
+
+    /** Method to mark the task as undone*/
+    public void markAsUndone() {
+        this.isDone = false;
+        System.out.println("\tOK, I've marked this task as not done yet:");
+        System.out.println("\t\t" + this.printTask());
+    }
+
+    /** Return icon based on whether task is done or undone*/
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    /** Method to print the task*/
+    public String printTask() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+
+}
