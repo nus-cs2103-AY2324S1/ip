3c0d92d285e426c3427eca3c8061c0c188a44587
emzm2023 <meeselevels@elizabeths-MacBook-Air-3.local>
2023-08-23 10:39:47 +0800

    implemented add and listTasks functionality.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -7,9 +9,13 @@ public class Duke {
     /** The scanner for the chatbot. */
     private Scanner scanner;
 
+    /** The taskList for the chatbot. */
+    private List<String> taskList;
+
     /** Constructor for the chatbot. */
     public Duke() {
         this.scanner = new Scanner(System.in);
+        this.taskList = new ArrayList<>();
     }
 
     /** Greets the user. */
@@ -20,6 +26,7 @@ public class Duke {
     /** Causes chatbot to exit. */
     public void exit() {
         System.out.println("Bye. Hope to see you again soon!");
+        this.scanner.close();
     }
 
     /** Echoes commands entered by user.
@@ -30,16 +37,42 @@ public class Duke {
         System.out.println(input + "\n");
     }
 
-    /** Reads user input. Exits if user input is "bye", else, echoes commands. */
+    /** Adds task inputted by user to taskList. Prints out confirmation.
+     *
+     * @param input The task inputted by the user.
+     */
+    public void add(String input) {
+        this.taskList.add(input);
+        System.out.println("added: " + input +"\n");
+    }
+
+    /** Lists out all the tasks in taskList. */
+    public void listTasks() {
+        for (int i = 0; i < taskList.size(); i++) {
+            int num = i + 1;
+            System.out.println(num +". " + taskList.get(i));
+        }
+
+        System.out.print("\n");
+    }
+
+    /** Reads user input. Exits if user input is "bye",
+     * lists tasks if input is "list", else, echoes commands. */
     public void readInput() {
         System.out.print("> ");
         String userInput = this.scanner.nextLine();
 
-        if (userInput.equals("bye")) {
-            this.exit();
-        } else {
-            this.echo(userInput);
-            this.readInput();
+        switch (userInput) {
+            case "list":
+                this.listTasks();
+                this.readInput();
+                break;
+            case "bye":
+                this.exit();
+                break;
+            default:
+                this.add(userInput);
+                this.readInput();
         }
     }
 
