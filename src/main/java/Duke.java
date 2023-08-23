4de06649fca07b774e918ce2a7d08577c15532db
zacwong2151 <zacwong2151@gmail.com>
2023-08-23 14:39:55 +0800

    implemented handle exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,11 @@ public class Duke {
     static String horizontalLines = indent  + "__________________________________________";
     static Task taskArray[] = new Task[100];
     static int count = 0;
+    public static void printWithIndent(String string) {
+        System.out.println(horizontalLines);
+        System.out.println(indent + string);
+        System.out.println(horizontalLines);
+    }
     public static void displayList() {
         System.out.println(horizontalLines);
         System.out.println(indent + "Here are the tasks in your list:");
@@ -15,26 +20,26 @@ public class Duke {
         }
         System.out.println(horizontalLines);
     }
-
     public static void whichTask(String letter, String string) {
-        if (letter == "T") {
-            taskArray[count] = new ToDo(string);
+        try {
+            if (letter == "T") {
+                taskArray[count] = new ToDo(string);
+            }
+            if (letter == "D") {
+                taskArray[count] = new Deadline(getDescription(string), getBy(string));
+            }
+            if (letter == "E") {
+                taskArray[count] = new Event(getDescription(string), getFrom(string), getTo(string));
+            }
+            System.out.println(horizontalLines);
+            System.out.println(indent + "Got it. I've added this task:");
+            System.out.println(megaIndent + taskArray[count].toString());
+            count++;
+            System.out.println(indent + "Now you have " + count + " tasks in the list.");
+            System.out.println(horizontalLines);
+        } catch (DukeException e) {
+            printWithIndent(e.getMessage());
         }
-        if (letter == "D") {
-            taskArray[count] = new Deadline(getDescription(string), getBy(string));
-        }
-        if (letter == "E") {
-            taskArray[count] = new Event(getDescription(string), getFrom(string), getTo(string));
-        }
-
-        System.out.println(horizontalLines);
-        System.out.println(indent + "Got it. I've added this task:");
-        System.out.println(megaIndent + taskArray[count].toS
-                tring());
-        count++;
-
-        System.out.println(indent + "Now you have " + count + " tasks in the list.");
-        System.out.println(horizontalLines);
     }
 
     public static String getDescription(String string) {
@@ -49,40 +54,59 @@ public class Duke {
         return string.substring(0, count);
     }
 
-    public static String getBy(String string) {
-        String by = "by";
-        int i = string.indexOf(by) + 3;
-        return string.substring(i); // returns "Sunday"
+    public static String getBy(String string) throws DukeException {
+        String slash = "/";
+        int first = string.indexOf(slash);
+        int second = first + 3;
+        if (first == -1 || !string.substring(first, second).equals("/by")) {
+            throw new DukeException("You need to add a by timing!");
+        }
+        return string.substring(first + 4); // returns "Sunday"
     }
 
-    public static String getFrom(String string) {
-        String from = "from";
-        int first = string.indexOf(from) + 5;
-        String to = "to";
-        int second = string.indexOf(to) - 2;
-        return string.substring(first, second);
+    public static String getFrom(String string) throws DukeException {
+        String slash = "/";
+        int firstSlash = string.indexOf(slash);
+        int secondSlash = string.indexOf(slash, firstSlash + 1);
+
+        if (firstSlash == -1 || secondSlash == -1
+                || !string.substring(firstSlash, firstSlash + 5).equals("/from")) {
+            throw new DukeException("You need to add a /from and /to for events");
+        }
+
+        return string.substring(firstSlash + 6, secondSlash - 1);
     }
-    public static String getTo(String string) {
-        String to = "to";
-        int num = string.indexOf(to) + 3;
-        return string.substring(num);
+    public static String getTo(String string) throws DukeException {
+        String slash = "/";
+        int firstSlash = string.indexOf(slash);
+        int secondSlash = string.indexOf(slash, firstSlash + 1);
+
+        if (!string.substring(secondSlash, secondSlash + 3).equals("/to")) {
+            throw new DukeException("You need to add a /to for events");
+        }
+        return string.substring(secondSlash + 4);
     }
 
     public static void markDescription(String string) {
-        String clean = string.replaceAll("\\D+",""); //remove non-digits
-        int pos = Integer.parseInt(clean) - 1;
-        Task curr = taskArray[pos];
+        try {
+            String clean = string.replaceAll("\\D+", ""); //remove non-digits
+            int pos = Integer.parseInt(clean) - 1;
+            Task curr = taskArray[pos];
 
-        System.out.println(horizontalLines);
-        if (string.contains("unmark")) {
-            curr.markAsUnDone();
-            System.out.println(indent + "OK, I've marked this task as not done yet:");
-        } else if (string.contains("mark")) {
-            curr.markAsDone();
-            System.out.println(indent + "Nice! I've marked this task as done:");
+            if (string.contains("unmark")) {
+                curr.markAsUnDone();
+                System.out.println(horizontalLines);
+                System.out.println(indent + "OK, I've marked this task as not done yet:");
+            } else if (string.contains("mark")) {
+                curr.markAsDone();
+                System.out.println(horizontalLines);
+                System.out.println(indent + "Nice! I've marked this task as done:");
+            }
+            System.out.println(megaIndent + curr.getStatusIcon() + " " + curr.description);
+            System.out.println(horizontalLines);
+        } catch (NullPointerException e) {
+            printWithIndent("You are trying to access a Task that does not exist!");
         }
-        System.out.println(megaIndent + curr.getStatusIcon() + " " + curr.description);
-        System.out.println(horizontalLines);
     }
 
 
@@ -100,33 +124,41 @@ public class Duke {
             if (userInput.equals("list")) {
                 displayList();
                 continue;
-            }
-            if (userInput.contains("unmark")) {
-                markDescription(userInput);
-                continue;
-            }
-            if (userInput.contains("mark")) {
-                markDescription(userInput);
-                continue;
-            }
-            if (userInput.contains("todo")) {
-                whichTask("T", userInput.substring(5));
-                continue;
-            }
-            if (userInput.contains("deadline")) {
-                whichTask("D", userInput.substring(9));
-                continue;
-            }
-            if (userInput.contains("event")) {
-                whichTask("E", userInput.substring(6));
-                continue;
-            }
-
-            if (userInput.equals("bye")) {
-                System.out.println(horizontalLines);
-                System.out.println(indent + "Bye. Hope to see you again soon!");
-                System.out.println(horizontalLines);
+            } else if (userInput.equals("bye")) {
+                printWithIndent("Bye. Hope to see you again soon!");
                 break;
+            } else if (userInput.contains("unmark")) {
+                markDescription(userInput);
+                continue;
+            } else if (userInput.contains("mark")) {
+                markDescription(userInput);
+                continue;
+            } else if (userInput.contains("todo")) {
+                try {
+                    whichTask("T", userInput.substring(5));
+                } catch (StringIndexOutOfBoundsException e) {
+                    printWithIndent("OOPS!!! The description of a todo cannot be empty.");
+                } finally {
+                    continue;
+                }
+            } else if (userInput.contains("deadline")) {
+                try {
+                    whichTask("D", userInput.substring(9));
+                } catch (StringIndexOutOfBoundsException e) {
+                    printWithIndent("OOPS!!! The description of a deadline cannot be empty.");
+                } finally {
+                    continue;
+                }
+            } else if (userInput.contains("event")) {
+                try {
+                    whichTask("E", userInput.substring(6));
+                } catch (StringIndexOutOfBoundsException e) {
+                    printWithIndent("OOPS!!! The description of a deadline cannot be empty.");
+                } finally {
+                    continue;
+                }
+            } else {
+                printWithIndent("OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
         }
     }
