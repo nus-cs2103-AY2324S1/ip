cdf4007c8d72be40f30a8e93cb448ba80247d628
brendanneojw <e0726880@u.nus.edu>
2023-08-23 18:47:43 +0800

    Implemented Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,19 +13,35 @@ public class Duke {
     }
 
     public static void takeInput() {
+        String[] taskArray = new String[100];
         Scanner sc = new Scanner(System.in);
+        int count = 0;
+
         while (true) {
             String input = sc.nextLine();
             printHoriLine();
-            if (!input.equals("bye")) {
-                System.out.println(input);
-                printHoriLine();
+
+            if (input.equals("list")) {
+                if (count == 0) {
+                    System.out.println("HEYYYYYYYY! There's nothing to show here!");
+                }
+                else {
+                    for (int i = 0; i < count; i++) {
+                        System.out.println(i + 1 + "." + taskArray[i]);
+                    }
+                }
+            }
+            else if (!input.equals("bye")) {
+                System.out.println("WAAAAAAOOOOO! Added: " + input);
+                taskArray[count] = input;
+                count++;
             }
             else {
                 System.out.println("WEEEWOOWEEWOO GOODBYE! Hope to see you again soon!");
                 printHoriLine();
                 break;
             }
+            printHoriLine();
         }
     }
 }
