0abb929481dc1330d14ce939bdafdf16d08044fb
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2023-08-23 10:46:20 +0800

    Rename bot to Ax and added divider line

---
+++ src/main/java/Ax.java
@@ -0,0 +1,35 @@
+public class Ax {
+
+    private static void hoLine() {
+        System.out.println("=^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=    =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=    ");
+    }
+
+    public static void greet() {
+        System.out.println("\uD83D\uDC4B Greetings!\n" +
+                "\n" +
+                "Ax at your service! I'm not just any chatbot; I'm Ax – your knowledgeable and engaging virtual companion. Whether you're seeking answers, a friendly chat, or a bit of fun, I'm here to make your experience enjoyable and insightful. Don't hesitate to share your thoughts or questions with me. Let's dive in and start our conversation! How can I assist you today, my friend?");
+        hoLine();
+    }
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        String newLogo = "                \n" +
+                "                \n" +
+                "                \n" +
+                "    ##          \n" +
+                "   ###          \n" +
+                "  ## ##  ##  ## \n" +
+                "  ## ##   ####  \n" +
+                " ##  ##    ##   \n" +
+                " ######   ###   \n" +
+                "##   ##  ## ##  \n" +
+                "##   ## ##  ##  \n" +
+                "                \n" +
+                "                \n";
+        System.out.println("Hello from\n" + newLogo);
+        greet();
+    }
+}
