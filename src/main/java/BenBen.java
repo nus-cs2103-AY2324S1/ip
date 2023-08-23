b7efbc7834b7a3fa102ddae512fb22004e7372a0
feifeiraindrops <sunyufei.sylvia@gmail.com>
2023-08-23 10:56:50 +0800

    echo

--- src/main/java/BenBen.java
+++ src/main/java/BenBen.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class BenBen {
     public static void main(String[] args) {
+        //initial
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
 //                + "| | | | | | | |/ / _ \\\n"
@@ -7,12 +10,26 @@ public class BenBen {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
 
+        //level 0
         String line ="_______________________________________\n";
         System.out.println(line);
         System.out.println("Hello! I'm BenBen.\n" +
                 "What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+//        System.out.println(line);
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            String next = sc.nextLine();
+
+            if (next.equals("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!\n" + line);
+                break;
+            }
+            System.out.println(line);
+            System.out.println(next);
+            System.out.println(line);
+        }
+        System.exit(0);
     }
 }
