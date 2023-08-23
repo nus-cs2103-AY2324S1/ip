f1bc936e09ab9675ebb6530d30dfbaeeb25694a4
Nabonita Sen <nabonita.sonali.sen@gmail.com>
2023-08-23 15:07:38 +0800

    Rename, greet, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String lineSpacer = "________________________________________";
+        String logo = "   / \\__\n"
+                + "  (    @\\___\n"
+                + "  /          O\n"
+                + " /   (_____/\n"
+                + "/_____/   \n";
+
+        System.out.println(lineSpacer);
+        System.out.println("Hello I'm Barkley\n" + logo);
+        System.out.println("Howl can I help you?");
+        System.out.println(lineSpacer);
+        System.out.println("Goodbye! Have a paw-some day :-)");
+        System.out.println(lineSpacer);
     }
 }
