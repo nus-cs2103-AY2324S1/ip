2f16bcfcb77a8bb8ac93bf4d0832a60f59aac9ad
RSXIX <rohansapra19@gmail.com>
2023-08-24 04:10:17 +0800

    Completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " _______________ \n"
-                + "Hello! I'm Scarlet \n"
-                + "What can I do for you? \n\n"
-                + " _______________ \n"
-                + "Bye. Hope to see you again soon! \n"
+                + "Eh, I'm Scarlet \n"
+                + "No, I don't really want to know who you are \n\n"
+                + " _______________ \n";
+        System.out.println(logo);
+        Duke duke = new Duke();
+        duke.repeat();
+    }
+
+    public void repeat() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String entry = sc.nextLine();
+
+            if (entry.equals("bye")) {
+                break;
+            }
+            String message = " _______________ \n\n"
+                    + String.format("%s \n", entry)
+                    + " _______________ \n";
+            System.out.println(message);
+        }
+        String logo = " _______________ \n\n"
+                + "finally. \n"
                 + " _______________ \n";
         System.out.println(logo);
     }
+
 }
