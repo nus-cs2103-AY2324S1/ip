8ba2c8b938a169b59d53ba1af26201a541f5cfde
Jingya <e0968387@u.nus.edu>
2023-08-24 00:09:27 +0800

    Added echo functionality to ChatBot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class Duke {
     private static String NAME = "Moira";
     private static String SPACER = "--------------------------------------------------------------------------";
+    private static boolean IS_RECEIVING_INPUT = false;
 
     public static void main(String[] args) {
         greet();
+        while (IS_RECEIVING_INPUT) {
+            getUserInput();
+        }
         exit();
     }
 
@@ -12,11 +18,25 @@ public class Duke {
         System.out.println("Howdy, I'm + " + NAME + ", your friendly personal assistant!");
         System.out.println("What can I do for you today?");
         System.out.println(SPACER);
+        IS_RECEIVING_INPUT = true;
     }
 
     private static void exit() {
+        IS_RECEIVING_INPUT = false;
         System.out.println(SPACER);
         System.out.println("See ya later, alligator! I'm waiting here if you need anything :>");
         System.out.println(SPACER);
+    }
+
+    private static void getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        if (!userInput.equals("bye")) {
+            System.out.println(SPACER);
+            System.out.println(userInput);
+            System.out.println(SPACER);
+        } else {
+            IS_RECEIVING_INPUT = false;
+        }
     }
 }
