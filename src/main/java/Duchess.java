4155919ebf425530e076246265a888bd926bdbaf
Evelyne Law <lawruixi@gmail.com>
2023-08-23 14:28:05 +0800

    added echoing functionality

--- src/main/java/Duchess.java
+++ src/main/java/Duchess.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duchess {
     /**
      * Name for the Duchess AI.
@@ -14,21 +16,48 @@ public class Duchess {
     }
 
     /**
+     * Returns true if the command would cause Duchess to exit operations.
+     *
+     * @param s - the command to check for exit operations
+     */
+    private static boolean isExitCommand(String s) {
+        return s.toLowerCase().equals("bye");
+    }
+
+    /**
      * Prints farewell/exit message.
      */
     private static void printFarewell() {
         System.out.println("Bye bye!! Hope to see you again (＾▽＾)");
     }
 
+    /**
+     * Outputs the same string parameter that was passed in.
+     *
+     * @param s - the string to be echoed.
+     */
+    private static void echo(String s) {
+        System.out.println(s);
+    }
+
     public static void main(String[] args) {
-        // String logo = " ____        _        \n"
-        // + "|  _ \\ _   _| | _____ \n"
-        // + "| | | | | | | |/ / _ \\\n"
-        // + "| |_| | |_| |   <  __/\n"
-        // + "|____/ \\__,_|_|\\_\\___|\n";
-        
         Duchess.printGreeting();
-        Duchess.printFarewell();
+
+        Scanner sc = new Scanner(System.in);
+        String userInput = "";
+
+        while(!Duchess.isExitCommand(userInput)) {
+            userInput = sc.nextLine();
+
+            // Check if this command is an exit.
+            if(Duchess.isExitCommand(userInput)) {
+                break;
+            }
+
+            // Otherwise, echo the command back.
+            Duchess.echo(userInput);
+        }
         
+        Duchess.printFarewell();
     }
 }
