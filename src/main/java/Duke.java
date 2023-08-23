ea831f6b359662ae38f63be419907161481057da
evanyan13e <weidongy201@gmail.comm>
2023-08-23 16:59:46 +0800

    First Commit for Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
+    public static final String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+    public static void main(String[] args) {
+        printIntro();
+        printBye();
+        
+    }
+
+    private static String breakLine() {
+        System.out.println("____________________________________________________________ \n");
+        return null;
+    } 
+
+    private static String printIntro() {
+        breakLine();
+        System.out.println("Hi Master! I'm your personal assistant: JARVIS! \n" + 
+                            "How can I serve you today? \n");
+        breakLine();
+        return null;
+    }
+
+    private static String printBye() {
+        System.out.println("Bye Master. It has been my honour to serve you!\n");
+        breakLine();
+        return null;
     }
 }
