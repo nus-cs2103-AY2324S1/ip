effbd4d7f1d49c58fec075fec059a5394b440fe8
bhnuka <01bhanukaekanayake@gmail.com>
2023-08-23 12:29:53 +0800

    > Added the ability to mark tasks as done and to change the status back to not done. > Used a Task class to represent tasks.

--- src/main/java/Eva.java
+++ src/main/java/Eva.java
@@ -14,7 +14,7 @@ public class Eva {
         System.out.println("\t What can I do for you?");
         System.out.println("\t____________________________________________________________");
 
-        String[] tasks = new String[100]; // Array to store tasks
+        Task[] tasks = new Task[100]; // Array to store tasks
         int taskCount = 0; // Counter for tasks
 
         while (true) {
@@ -25,18 +25,41 @@ public class Eva {
                 System.out.println("\t Bye. Hope to see you again soon!");
                 System.out.println("\t____________________________________________________________");
                 break;
-            } else if (input.equalsIgnoreCase("list")) {
+            } else if (input.equals("list")) {
                 System.out.println("\t____________________________________________________________");
                 if (taskCount == 0) {
                     System.out.println("\t No tasks added yet.");
                 } else {
+                    System.out.println("\t Here are the tasks in your list:");
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println("\t " + (i + 1) + ". " + tasks[i]);
+                        System.out.println("\t " + (i + 1) + "." + tasks[i]);
                     }
                 }
                 System.out.println("\t____________________________________________________________");
+            } else if (input.startsWith("mark")) {
+                int taskIndex = Integer.parseInt(input.substring(5)) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].markDone();
+                    System.out.println("\t____________________________________________________________");
+                    System.out.println("\t Nice! I've marked this task as done:");
+                    System.out.println("\t   " + tasks[taskIndex]);
+                    System.out.println("\t____________________________________________________________");
+                } else {
+                    System.out.println("\t Task not found.");
+                }
+            } else if (input.startsWith("unmark")) {
+                int taskIndex = Integer.parseInt(input.substring(7)) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].markUndone();
+                    System.out.println("\t____________________________________________________________");
+                    System.out.println("\t OK, I've marked this task as not done yet:");
+                    System.out.println("\t   " + tasks[taskIndex]);
+                    System.out.println("\t____________________________________________________________");
+                } else {
+                    System.out.println("\t Task not found.");
+                }
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("\t____________________________________________________________");
                 System.out.println("\t added: " + input);
@@ -44,5 +67,28 @@ public class Eva {
             }
         }
         scanner.close();
+    }
+
+    static class Task {
+        private String description;
+        private boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public void markDone() {
+            isDone = true;
+        }
+
+        public void markUndone() {
+            isDone = false;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + (isDone ? "X" : " ") + "] " + description;
+        }
     }
 }
