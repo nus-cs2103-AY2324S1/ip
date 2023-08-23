f8d396fb8c8171283f68ba68cf219f1aa0b5fd1b
Saezenn <teng.hxa@gmail.com>
2023-08-23 23:08:32 +0800

    Level-0, Rename, Greet, Exit

---
+++ src/main/java/Sae.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Sae {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Sae\nWhat can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
