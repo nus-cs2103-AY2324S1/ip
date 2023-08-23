9c8a7362dbfbb14fe666ed4a5a274f9790e25484
ryam <ryanngohenrui@gmail.com>
2023-08-23 15:09:04 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+	static ArrayList<String> list = new ArrayList<>();
+
 	public static void main(String[] args) {
 		printLine();
 		System.out.println("Hello I'm RyamBot");
@@ -13,31 +16,37 @@ public class Duke {
 		System.out.println("____________________________________________________________");
 	}
 
+	static void addToList(String item) {
+		printLine();
+		list.add(item);
+		System.out.println("added: " + item);
+		printLine();
+	}
+
+	static void printList() {
+		printLine();
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(i + 1 + ". " + list.get(i));
+		}
+		printLine();
+	}
+
+	static void exit() {
+		printLine();
+		System.out.println("Bye. Hope to see you again soon!");
+		printLine();
+	}
+
 	static void queryBot() {
 		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 		String query = myObj.nextLine();  // Read user input
-//		  case "list": {
-//				printLine();
-//				System.out.println("list");
-//				printLine();
-//				queryBot();
-//				break;
-//			}
-//			case "blah": {
-//				printLine();
-//				System.out.println("blah");
-//				printLine();
-//				queryBot();
-//				break;
-//			}
 		if (query.equals("bye")) {
-			printLine();
-			System.out.println("Bye. Hope to see you again soon!");
-			printLine();
+			exit();
+		} else if (query.equals("list")) {
+			printList();
+			queryBot();
 		} else {
-			printLine();
-			System.out.println(query);
-			printLine();
+			addToList(query);
 			queryBot();
 		}
 	}
