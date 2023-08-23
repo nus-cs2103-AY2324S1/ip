b1072be813e2b3069535f3eac7d4ff08f3d0b650
Debbie Hii Wenxin <e0959749@u.nus.edu>
2023-08-24 00:13:40 +0800

    Level-0 created greetings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         String horizontalLine = "------------------------\n";
-        System.out.println("Hello from\n" + logo);
         System.out.println(horizontalLine
-                + "GREETINGS HUMAN! I AM LE QLATZ! \n"
-                + "HOW MAY I BE OF SERVICE??\n\n"
+                + "GREETINGS HUMAN! I AM QLATZ! □ \n"
+                + "HOW MAY I BE OF SERVICE?\n\n"
                 + horizontalLine);
 
-        System.out.println("BYE!" + horizontalLine);
+        System.out.println("BYE!\n" + horizontalLine);
     }
 }
