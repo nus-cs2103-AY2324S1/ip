49eea838dcf8895eca645d084fbf83a18364e879
cyaoxuan <chuayaoxuan@gmail.com>
2023-08-23 13:23:45 +0800

    Level-2 imeplemented

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,19 +9,35 @@ public class Duke {
         System.out.println("Hello! I'm Eepy\nWhat can I do for you?");
         System.out.println(line);
 
-        // Get input and echo
+        // Get input and store it
         Scanner in = new Scanner(System.in);
+        String[] items = new String[100];
+        int itemsCount = 0;
+
         while (true) {
             String s = in.nextLine();
             System.out.println("    " + line);
 
             if (s.equals("bye")) {
+                // Exit
                 System.out.println("    Bye. Hope to see you again soon!");
                 System.out.println("    " + line);
                 break;
+            } else if (s.equals("list")) {
+                // List out all items
+                for (int i = 0; i < itemsCount; i++) {
+                    int index = i + 1;
+                    System.out.println("    " + index + ". " + items[i]);
+                }
+                System.out.println("    " + line);
+            } else {
+                // Add item
+                items[itemsCount] = s;
+                itemsCount++;
+
+                System.out.println("    added: " + s);
+                System.out.println("    " + line);
             }
-            System.out.println("    " + s);
-            System.out.println("    " + line);
         }
     }
 }
