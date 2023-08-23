8538f65f679f5d9e7b54848e026e17ae02f24731
YangHeng Tang <yanghengtang@gmail.com>
2023-08-23 22:23:35 +0800

    Add Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        String status = "[" + (this.isDone ? "X" : " ") + "]";
+        return status + " " + this.description;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+}
