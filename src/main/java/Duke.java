862fb703da1aada0c6de0b5fd81495db5d505e53
tiong jjyi <tiongjjyi@gmail.com>
2023-08-23 18:12:07 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,32 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Duke {
+
 
     public static void main(String[] args) {
         System.out.println("Hello! I'm JJ\n" +
                 "What can I do for you?\n" + "\n");
         Scanner sc = new Scanner(System.in);
         String cmd = sc.nextLine();
+        List<String> ls = new ArrayList<>();
+
         while (true) {
             if (cmd.equals("bye")) {
                 sc.close();
                 System.out.println("Bye. Hope to see you again soon!\n");
                 System.exit(0);
+            } else if (cmd.equals("list")) {
+                int size = ls.size();
+                for(int i = 0; i < size;i++) {
+                    int j = i + 1;
+                    System.out.println(j + ". " + ls.get(i));
+                }
+                cmd = sc.nextLine();
             } else {
-                System.out.println(cmd + "\n");
+                ls.add(cmd);
+                System.out.println("added: " + cmd + "\n");
                 cmd = sc.nextLine();
             }
         }
