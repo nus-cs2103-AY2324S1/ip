9b3c718f37b6b0816133a950155fdb64e2b7b46d
javinchua <javin.chua@gmail.com>
2023-08-23 16:19:36 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
 public class Duke {
 
     public static void main(String[] args) {
         String name = "CodeBuddy";
         String horizontal = "----------------------------------------";
+        Scanner inputScanner = new Scanner(System.in);
         System.out.println(horizontal);
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
         System.out.println(horizontal);
-        System.out.println("Bye. Hope to see you again soon !");
-        System.out.println(horizontal);
+
+        while(true) {
+            String input = inputScanner.nextLine();
+            System.out.println(horizontal);
+            if(input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon !");
+                System.out.println(horizontal);
+                break;
+            }
+            System.out.println(input);
+            System.out.println(horizontal);
+        }
     }
 }
