927f03f3821a19e1d5309af267380cd43df25c37
xxiaoweii <e0967899@u.nus.edu>
2023-08-23 17:40:58 +0800

    level 0 - rename, greet, exit

---
+++ src/main/java/Messages.java
@@ -0,0 +1,5 @@
+public class Messages {
+
+    public static final String GREET = "Hello! I'm Chatty!\nWhat can I do for you?";
+    public static final String EXIT = "Bye. Hope to see you again soon!";
+}
