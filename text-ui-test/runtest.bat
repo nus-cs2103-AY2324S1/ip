1d84fe905e538a6d2f902370d952fee1808dab21
Yuheng <guoyuheng1year@gmail.com>
2023-08-23 11:08:21 +0800

    "Level-TextUITesting"

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -15,7 +15,7 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from task.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Mimi < input.txt > EXPECTED.TXT
+java -classpath ..\bin Mimi < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
