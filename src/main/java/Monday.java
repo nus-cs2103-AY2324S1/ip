07fb9ca31b45fe44fbeb3a12528697dd50050c9c
nubnubyas <e0970089@u.nus.edu>
2023-08-23 18:46:50 +0800

    Completed Level-0

--- src/main/java/Monday.java
+++ src/main/java/Monday.java
@@ -6,7 +6,6 @@ public class Monday {
         System.out.println("Hello! I'm " + chatBotName);
         System.out.println("What can I do for you?");
         printSeparator();
-
         System.out.println("Bye. Hope to see you again soon!");
         printSeparator();
     }
