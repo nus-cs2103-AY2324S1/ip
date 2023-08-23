5e2603efc4d9cbe29b57ae4dc0d4104bb0c9f21e
Andre Chua <110965035+andrechuakj@users.noreply.github.com>
2023-08-23 20:58:48 +0800

    Renamed bot, added greet and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " ___  _ _                  ___  _          _    ___        _    ___ \n"
+                + "|  _|| | | ___  _ _  _ _  |  _>| |_  ___ _| |_ | . > ___ _| |_ |_  |\n"
+                + "| |  \\   // . \\| | || '_> | <__| . |<_> | | |  | . \\/ . \\ | |    | |\n"
+                + "| |_  |_| \\___/`___||_|   `___/|_|_|<___| |_|  |___/\\___/ |_|   _| |\n"
+                + "|___|                                                          |___|\n";
+
+        String greet = " Hello! I'm\n" + logo + "What can I do for you?\n";
+        System.out.println(greet);
+
+        String sendOff = "Bye. Hope to see you again soon!";
+        System.out.println(sendOff);
     }
 }
