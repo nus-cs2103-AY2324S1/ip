31dbabb9274b99c556d8918e3b1cecf3dfdf6e6a
AlainS87 <1950834052@qq.com>
2023-08-23 16:17:20 +0800

    level1 completed

--- src/main/java/ChatbotAlain.java
+++ src/main/java/ChatbotAlain.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ChatbotAlain {
     public static void main(String[] args) {
         String renameAndGreeting = "____________________________________________________________\n"
@@ -6,5 +8,22 @@ public class ChatbotAlain {
                 + "____________________________________________________________\n"
                 + " Bye. Hope to see you again soon!\n";
         System.out.println(renameAndGreeting);
+        while (true) {
+            Scanner s = new Scanner(System.in);
+            String text = new String();
+            text = s.nextLine();
+            if (text.equals("bye")) {
+                break;
+            } else {
+                String output = "____________________________________________________________\n"
+                        + " " + text + "\n"
+                        + "____________________________________________________________\n";
+                System.out.println(output);
+            }
+        }
+        String endingLine = "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(endingLine);
     }
 }
