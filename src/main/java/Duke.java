5e8695955e97d167b6ad020761fd92a88503a944
kokseng1 <ngkokseng02@gmail.com>
2023-08-23 18:43:05 +0800

    lvl1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         String name = "Chaty";
         System.out.println("Hello! I'm " + name + "\n" +
-                "What can I do for you?" + "\n\n" +
-                "Bye. Hope to see you again soon!");
+                "What can I do for you?" + "\n\n");
+        String echo = scan.nextLine();
+        while (!echo.equals("bye")) {
+            System.out.println(echo);
+            echo = scan.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
