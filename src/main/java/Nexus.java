9ef6d477e07c814075ac182cc49ad38185979e3b
s0ngyang <keesongyang4@gmail.com>
2023-08-23 23:22:11 +0800

    Added Mark as done

--- src/main/java/Nexus.java
+++ src/main/java/Nexus.java
@@ -8,20 +8,39 @@ public class Nexus {
         System.out.println("Hello! I'm NEXUS");
         System.out.println("What can I do for you?");
         Scanner scanner = new Scanner(System.in);
-        List<String> list = new ArrayList<>();
-        while (true) {
-            String input = scanner.nextLine();
-            if (Objects.equals(input, "bye")) break;
-            if (Objects.equals(input, "list")) {
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.print(i + 1);
-                    System.out.println(". " + list.get(i));
-                }
-            } else {
-                list.add(input);
-                System.out.println("added: " + input);
+        List<Task> list = new ArrayList<>();
+        int index;
+        boolean isBye = false;
+
+        while (!isBye) {
+            String input = scanner.next();
+            switch (input) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    isBye = true;
+                    break;
+                case "list":
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.print(i + 1);
+                        System.out.println("." + list.get(i));
+                    }
+                    break;
+                case "mark":
+                    index = Integer.parseInt(scanner.next()) - 1;
+                    list.get(index).setDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(list.get(index));
+                    break;
+                case "unmark":
+                    index = Integer.parseInt(scanner.next()) - 1;
+                    list.get(index).setUndone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(list.get(index));
+                    break;
+                default:
+                    list.add(new Task(input));
+                    System.out.println("added: " + input);
             }
         }
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public boolean getDone() {
+        return this.isDone;
+    }
+
+    public void setDone() {
+        this.isDone = true;
+    }
+
+    public void setUndone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
