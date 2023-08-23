1d2028dc31060418ea90c0b67820d799a68c49d8
WeeMingQing <e0725595@u.nus.edu>
2023-08-23 12:22:55 +0800

    Rename, Greet, Exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,7 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm JonBird.\nWhat can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
