283a7fa92cb2ee01699562fe02ab4fb2b67bce4a
AlfredBeNoel <alfredbenoel@gmail.com>
2023-08-23 14:22:15 +0800

    Update Noel.java

--- src/main/java/Noel.java
+++ src/main/java/Noel.java
@@ -1,10 +1,8 @@
-public class Duke {
+public class Noel {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String filler = "____________________________________________________________\n";
+        String helloMsg = " Hello! I'm Noel!\nWhat can I do for you?\n";
+        String byeMsg = " Bye. Hope to see you again soon!\n";
+        System.out.println(filler + helloMsg + filler + byeMsg);
     }
 }
