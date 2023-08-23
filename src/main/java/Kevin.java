76faac895c5323abb711978e37940d9e99d65c92
aliciamichellew <aliciamichellew@gmail.com>
2023-08-23 15:15:59 +0800

    Abstract out horizontal lines

--- src/main/java/Kevin.java
+++ src/main/java/Kevin.java
@@ -8,13 +8,28 @@ public class Kevin {
             "| |\\  \\| |___\\ \\_/ /_| |_| |\\  |\n" +
             "\\_| \\_/\\____/ \\___/ \\___/\\_| \\_/\n";
 
+    public static void wrapInHorizontalLines(String str) {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(str);
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void echo(String command) {
+        wrapInHorizontalLines(command);
+    }
+
+    public static void hello() {
+        String welcomeMessage = "Hello! I'm" + BOT_NAME + "\n" + "What can I do for you?";
+        wrapInHorizontalLines(welcomeMessage);
+    }
+
+    public static void bye() {
+        String goodbyeMessage = "Bye. Hope to see you again soon!";
+        wrapInHorizontalLines(goodbyeMessage);
+    }
     public static void main(String[] args) {
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println("Hello! I'm" + BOT_NAME);
-        System.out.println("What can I do for you?");
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(HORIZONTAL_LINE);
+        hello();
+        bye();
     }
 }
 
