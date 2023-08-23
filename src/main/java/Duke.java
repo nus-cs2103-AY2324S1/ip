f6bcc368353482bb923a600fb857b23f4929497b
chuababyy <altheachuajiaxuan@gmail.com>
2023-08-24 00:28:59 +0800

    Level-3 Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,7 @@ public class Duke {
     private static String line = "------------------------------------";
 
     public static void main(String[] args) {
-        ArrayList fullList = new ArrayList();
+        ArrayList<Task> fullList = new ArrayList();
 
         System.out.println(line);
         System.out.println("Hello! I'm " + chatbot);
@@ -16,19 +16,25 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            String userInput = scanner.nextLine();
+            String userInput = scanner.nextLine().trim();
 
+            // Closes chatbot when user says bye
             if (userInput.equals("bye")) {
                 break;
             }
 
+            // List out all items when user says list
             if (userInput.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 1; i <= fullList.size(); i++) {
-                    System.out.println(i + ". " + fullList.get(i - 1));
+                    int index = i - 1;
+                    System.out.println(i + ". " + fullList.get(index).getStatusIcon() +
+                            " " + fullList.get(index).getDescription());
                 }
                 continue;
             }
 
+            // Error message when user enters empty response
             if (userInput.equals("")) {
                 System.out.println(line);
                 System.out.println("Item to be added cannot be empty");
@@ -36,15 +42,45 @@ public class Duke {
                 continue;
             }
 
-            char lastChar = userInput.charAt(userInput.length() - 1);
-            if (lastChar == ' ') {
-                System.out.println(line);
-                System.out.println("Last character should not be an empty space");
-                System.out.println(line);
-                continue;
+            if (userInput.startsWith("mark")) {
+                int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                if (index >= 0 && index < fullList.size()) {
+                    Task curr = fullList.get(index);
+                    curr.markDone();
+
+                    System.out.println(line);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(curr.getStatusIcon() + " " + curr.getDescription());
+                    System.out.println(line);
+                    continue;
+                } else {
+                    System.out.println(line);
+                    System.out.println("No such item exists");
+                    System.out.println(line);
+                    continue;
+                }
             }
 
-            fullList.add(userInput);
+            if (userInput.startsWith("unmark")) {
+                int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                if (index >= 0 && index < fullList.size()) {
+                    Task curr = fullList.get(index);
+                    curr.markNotDone();
+
+                    System.out.println(line);
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(curr.getStatusIcon() + " " + curr.getDescription());
+                    System.out.println(line);
+                    continue;
+                } else {
+                    System.out.println(line);
+                    System.out.println("No such item exists");
+                    System.out.println(line);
+                    continue;
+                }
+            }
+
+            fullList.add(new Task(userInput));
             System.out.println(line);
             System.out.println("added: " + userInput);
             System.out.println(line);
