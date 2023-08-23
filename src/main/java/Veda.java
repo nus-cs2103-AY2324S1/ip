895ba0beda2df1ca501282f88268ac9d025a4d97
Sebastian Tay <taysebastian95@gmail.com>
2023-08-23 14:27:51 +0800

    echo feature

--- src/main/java/Veda.java
+++ src/main/java/Veda.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Veda {
 
     private final static String NAME = "Veda";
@@ -5,12 +7,24 @@ public class Veda {
     public static void main(String[] args) {
         //Greet users upon initialisation
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm " + NAME);
-        System.out.println("What can I do for you?");
+        System.out.println(NAME + " initialised. How may I help you?");
         System.out.println("____________________________________________________________");
+
+        Scanner inScanner = new Scanner(System.in);
+
+        while (true) {
+            String input = inScanner.nextLine();
+
+            //User wishes to exit the program
+            if (input.toLowerCase().equals("bye")) {
+                break;
+            }
+
+            System.out.println(input);
+        }
 
 
         //Exits the program
-        System.out.println("Bye. Hope to see you soon!");
+        System.out.println("Bye. All the best for your mission!");
     }
 }
