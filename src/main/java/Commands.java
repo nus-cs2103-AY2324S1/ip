0e60b3e9531f67998f4b2e9ea06b0d35ae502e91
NicholasTng010601 <nicholastng@hotmail.com>
2023-08-23 12:16:57 +0800

    Created delete function

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -1,39 +1,45 @@
+import Errors.DukeException;
 import Errors.InvalidTaskInput;
 import Tasks.Deadlines;
 import Tasks.Events;
 import Tasks.Task;
 import Tasks.ToDos;
 
+import java.util.ArrayList;
+
 public class Commands {
     static String name = "Nichbot";
 
-    //    static String[] tasks = new String[100];
     static int count = 0;
-    // Assuming there will not be more than 100 tasks
-    static Task[] tasks = new Task[100];
+
+    // task list to store tasks
+    static ArrayList<Task> tasks = new ArrayList<>();
 
     // Level-0, Function to say introduce the chatbot
     public static void sayHello() {
         String greet = String.format(
-                """
-                        ____________________________________________________________
-                         Hello! I'm %s, a task planning bot that will help you record your tasks.
-                         I currently can record 3 types of tasks. tasks to do, tasks with deadlines and events
-                         Here is how you can record down your events.
-                         To record tasks to do, simply begin your command with "todo" followed by a space and the task you need to do.\s
-                         To record tasks with deadlines, simply begin your command with "deadlines" followed by the task and a / then the due date.\s
-                         To record events, simply begin your command with "events" followed by the event, / and start time , then / with the end time
-                         To view your list of events, type list. Lastly, to exit the chatbot, type "bye" ____________________________________________________________
-                        """, name);
+
+                        "____________________________________________________________\n" +
+                         "Hello! I'm %s, a task planning bot that will help you record your tasks.\n" +
+                         "I currently can record 3 types of tasks. tasks to do, tasks with deadlines and events \n" +
+                         "Here is how you can record down your events.\n" +
+                         "To record tasks to do, simply begin your command with \"todo\" followed by a space and the task you need to do.\n" +
+                         "To record tasks with deadlines, simply begin your command with \"deadlines\" followed by the task and a / then the due date.\n" +
+                         "To record events, simply begin your command with \"events\" followed by the event, / and start time , then / with the end time" +
+                         "To view your list of events, type list. \n" + "To mark your events as done or undone, type in mark / unmark , followed by the index of the task \n"+
+                                "To delete tasks from the list, type delete task number, such as delete 2 \n" +
+                                        "Lastly, to exit the chatbot, type \"bye\" \n" +
+                                 "____________________________________________________________"
+                         , name);
         System.out.println(greet);
     }
 
     // Level-0, Function to say goodbye
     public static void sayGoodBye() {
-        String bye =  ("""
-                ____________________________________________________________
-                Bye. Hope to see you again soon!
-                ____________________________________________________________""");
+        String bye =  (
+                "____________________________________________________________ \n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
         System.out.println(bye);
     }
 
@@ -108,7 +114,7 @@ public class Commands {
         }
 
         if (newTask == null) newTask = new Task(input);
-        tasks[count++] = newTask;
+        tasks.add(count++, newTask);
         System.out.println("____________________________________________________________\n" +
                 "I have added this task to your list\n" +
                 String.format("[%s]",newTask.getTaskType()) + "[ ]" + newTask.getDescription() + "\n" +
@@ -120,31 +126,35 @@ public class Commands {
         System.out.println("____________________________________________________________");
         for (int i = 0; i < count; i++) {
             // for each task in task[], prints out a simple line describing the task
-            Character taskType = tasks[i].getTaskType();
-            String current = String.format("%d: [%c][%s] %s ", i+1, taskType, tasks[i].getStatusIcon(),tasks[i]);
+            Character taskType = tasks.get(i).getTaskType();
+            String current = String.format("%d: [%c][%s] %s ", i+1, taskType, tasks.get(i).getStatusIcon(),tasks.get(i));
 
-            if (tasks[i] instanceof Deadlines) {
-                current += (String.format("(%s)", ((Deadlines) tasks[i]).getDeadline()));
-            } else if (tasks[i] instanceof Events) {
-                current += (String.format("(%s, %s)", ((Events) tasks[i]).getStartTime(), ((Events) tasks[i]).getEndTime()));
+            if (tasks.get(i) instanceof Deadlines) {
+                current += (String.format("(%s)", ((Deadlines) tasks.get(i)).getDeadline()));
+            } else if (tasks.get(i) instanceof Events) {
+                current += (String.format("(%s, %s)", ((Events) tasks.get(i)).getStartTime(), ((Events) tasks.get(i)).getEndTime()));
             }
 
             System.out.println(current);
         }
-        System.out.print("\n____________________________________________________________\n");
+        System.out.print("____________________________________________________________\n");
     }
 
     // function to mark task as done or undone
     public static void markDoneOrUndone(int task, boolean done) {
-        if (task < 1 || task > 100) {
+        if (task < 1 || task > tasks.size()) {
+            System.out.print("____________________________________________________________\n");
             System.out.println("Invalid Input written.");
+            System.out.print("____________________________________________________________\n");
             return;
         }
         if (task > count + 1) {
-            System.out.printf("Tasks.Task %d has not been recorded%n",task);
+            System.out.print("____________________________________________________________\n");
+            System.out.printf("Task %d has not been recorded%n",task);
+            System.out.print("____________________________________________________________\n");
             return;
         }
-        Task current = tasks[task - 1];
+        Task current = tasks.get(task - 1);
         if (done) {
             current.setDone();
             System.out.println("____________________________________________________________\n");
@@ -158,11 +168,36 @@ public class Commands {
         }
     }
 
+    //level 6. Delete tasks
+    public static void deleteTasks(int taskNumber) throws  DukeException{
+        if (count < taskNumber) throw new DukeException("Invalid input added");
+        Task removed = tasks.remove(taskNumber - 1);
+        count--;
+        System.out.println("____________________________________________________________\n");
+        System.out.println("Noted, I have deleted this task:");
+        System.out.println(String.format("  [%c][%s] %s ", removed.getTaskType(), removed.getStatusIcon(), removed));
+        System.out.println(String.format("You now have %d tasks in your list.", taskNumber));
+        System.out.println("____________________________________________________________\n");
+    }
+
     // overall input handler which determines which function to run
-    public static void handleInput(String userInput) throws InvalidTaskInput{
+    public static void handleInput(String userInput) throws InvalidTaskInput, DukeException{
             if (userInput.equalsIgnoreCase("bye")) return;
             if (userInput.equalsIgnoreCase("list")) {
                 printList();
+            } else if (userInput.length() > 7 && userInput.substring(0,6).equalsIgnoreCase("delete")) {
+                // delete takes up first 5, 6 index takes up space, so 7 onwards should be numbers
+                String index = userInput.substring(7);
+                while (index.charAt(0) == ' ') index = index.substring(1);
+                while (index.charAt(index.length() - 1) == ' ') index = index.substring(0, index.length()-1);
+                Integer value = null;
+                try {
+                    value = Integer.parseInt(index);
+                } catch (NumberFormatException nfe) {
+                    System.out.println("Invalid input added");
+                    return;
+                }
+                if (value != null ) deleteTasks(value);
             } else if (userInput.length() > 5 && userInput.substring(0,4).equalsIgnoreCase("mark")) {
                 markDoneOrUndone(Integer.parseInt(userInput.substring(5)), true);
             } else if (userInput.length() > 7 && userInput.substring(0,6).equalsIgnoreCase("unmark")) {
