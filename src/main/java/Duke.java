6754bdb695a6a302631fdc6632636c23b68130f7
YangHeng Tang <yanghengtang@gmail.com>
2023-08-23 16:21:08 +0800

    Implement Echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String HORIZONTAL_LINE = "____________________________________________________________ \n";
 
-        String message = HORIZONTAL_LINE
+        String entryMessage = HORIZONTAL_LINE
                 + "Hello! I'm Chad \n"
                 + "What can I do for you? \n"
-                + HORIZONTAL_LINE
+                + HORIZONTAL_LINE;
+
+        String exitMessage = HORIZONTAL_LINE
                 + "Bye. Hope to see you again soon! \n"
                 + HORIZONTAL_LINE;
 
-        System.out.println(message);
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        System.out.println(entryMessage);
+        while(!input.equals("bye")) {
+            input = scanner.nextLine();
+            if(!input.equals("bye")) {
+                System.out.println(HORIZONTAL_LINE + input + "\n" + HORIZONTAL_LINE);
+            }
+        }
+        scanner.close();
+        System.out.println(exitMessage);
     }
 }
