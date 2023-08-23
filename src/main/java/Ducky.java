a36ff062f69ee8b26511876724d488fb12e94ea6
chew01 <chewjh19@gmail.com>
2023-08-23 18:51:50 +0800

    skeletal version of ducky

---
+++ src/main/java/Ducky.java
@@ -0,0 +1,6 @@
+public class Ducky {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Ducky\nWhat can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
