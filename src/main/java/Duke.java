58c7925917c409aa354df89a0022c41565ae8050
GohTengFong <gohtengfong@Gohs-MacBook-Pro-4.local>
2023-08-23 15:08:58 +0800

    Added enums

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
@@ -36,57 +35,74 @@ public class Duke {
         System.out.println(line);
     }
 
+    public String handleTodo(String task) throws DukeException {
+        String[] preprocessedTask = task.split("todo ");
+
+        if (preprocessedTask.length <= 1) {
+            throw new DukeException("Please enter a valid todo description.");
+        }
+
+
+        Todo nextTodo = new Todo(preprocessedTask[1]);
+        this.tasks.add(nextTodo);
+        return nextTodo.toString();
+    }
+
+    public String handleDeadline(String task) throws DukeException {
+        String[] temp = task.split("deadline ");
+
+        if (temp.length <= 1) {
+            throw new DukeException("You are missing both a valid deadline description and a deadline. "
+                    + "Please enter a valid deadline description and deadline.");
+        }
+
+        String[] preprocessedTask = preprocessTask(temp[1]);
+
+        if (preprocessedTask.length <= 1) {
+            throw new DukeException("You are missing either a valid deadline description or deadline. "
+                    + "Please enter a valid deadline description or deadline.");
+        }
+
+        Deadline nextDeadline = new Deadline(preprocessedTask[0], preprocessedTask[1]);
+        this.tasks.add(nextDeadline);
+        return nextDeadline.toString();
+    }
+
+    public String handleEvent(String task) throws DukeException {
+        String[] temp = task.split("event ");
+
+        if (temp.length <= 1) {
+            throw new DukeException("You are missing both a valid event description and a start and end time. "
+                    + "Please enter a valid event description and start and end time.");
+        }
+
+        String[] preprocessedTask = preprocessTask(temp[1]);
+
+        if (preprocessedTask.length <= 2) {
+            throw new DukeException("You are missing either a valid event description or start and end time. "
+                    + "Please enter a valid event description or start and end time.");
+        }
+
+        Event nextEvent = new Event(preprocessedTask[0], preprocessedTask[1], preprocessedTask[2]);
+        this.tasks.add(nextEvent);
+        return nextEvent.toString();
+    }
+
+    public void handleInvalid() {
+        System.out.println(line);
+        System.out.println("You did not enter a valid task. Please enter either a todo, deadline or event.");
+        System.out.println(line);
+    }
+
     public void handleTask(String task) throws DukeException {
         String nextTaskString = null;
 
         if (task.startsWith("todo")) {
-            String[] preprocessedTask = task.split("todo ");
-
-            if (preprocessedTask.length <= 1) {
-                throw new DukeException("Please enter a valid todo description.");
-            }
-
-
-            Todo nextTodo = new Todo(preprocessedTask[1]);
-            this.tasks.add(nextTodo);
-            nextTaskString = nextTodo.toString();
+            nextTaskString = handleTodo(task);
         } else if (task.startsWith("deadline")) {
-            String[] temp = task.split("deadline ");
-
-            if (temp.length <= 1) {
-                throw new DukeException("You are missing both a valid deadline description and a deadline. Please enter a valid deadline description and deadline.");
-            }
-
-            String[] preprocessedTask = preprocessTask(temp[1]);
-
-            if (preprocessedTask.length <= 1) {
-                throw new DukeException("You are missing either a valid deadline description or deadline. Please enter a valid deadline description or deadline.");
-            }
-
-            Deadline nextDeadline = new Deadline(preprocessedTask[0], preprocessedTask[1]);
-            this.tasks.add(nextDeadline);
-            nextTaskString = nextDeadline.toString();
+            nextTaskString = handleDeadline(task);
         } else if (task.startsWith("event")) {
-            String[] temp = task.split("event ");
-
-            if (temp.length <= 1) {
-                throw new DukeException("You are missing both a valid event description and a start and end time. Please enter a valid event description and start and end time.");
-            }
-
-            String[] preprocessedTask = preprocessTask(temp[1]);
-
-            if (preprocessedTask.length <= 2) {
-                throw new DukeException("You are missing either a valid event description or start and end time. Please enter a valid event description or start and end time.");
-            }
-
-            Event nextEvent = new Event(preprocessedTask[0], preprocessedTask[1], preprocessedTask[2]);
-            this.tasks.add(nextEvent);
-            nextTaskString = nextEvent.toString();
-        } else if (task.startsWith("delete")) {
-            handleDelete(task);
-            return;
-        } else {
-            throw new DukeException("You did not enter a valid task. Please enter either a todo, deadline or event.");
+            nextTaskString = handleEvent(task);
         }
 
         System.out.println(line);
@@ -96,7 +112,7 @@ public class Duke {
         System.out.println(line);
     }
 
-    private String[] preprocessTask(String temp) throws DukeException {
+    private String[] preprocessTask(String temp) {
         String[] preprocessedTask = temp.split(" /by | /from | /to ");
 
         return preprocessedTask;
@@ -114,7 +130,7 @@ public class Duke {
         int taskIndex = -1;
 
         try {
-            taskIndex = Integer.parseInt(index) - 1; // .parseInt throws a NumberFormatExeception if index does not contain a parsable integer
+            taskIndex = Integer.parseInt(index) - 1;
         } catch (NumberFormatException e) {
             throw new DukeException("You did not enter a valid index. Please enter a valid index to delete.");
         }
@@ -146,7 +162,7 @@ public class Duke {
         int taskIndex = -1;
 
         try {
-            taskIndex = Integer.parseInt(index) - 1; // .parseInt throws a NumberFormatExeception if index does not contain a parsable integer
+            taskIndex = Integer.parseInt(index) - 1;
         } catch (NumberFormatException e) {
             throw new DukeException("You did not enter a valid index. Please enter a valid index to mark or unmark.");
         }
@@ -181,34 +197,54 @@ public class Duke {
         System.out.println(line);
     }
 
+    // CHECKSTYLE.OFF: Indentation
     public void acceptTasks() {
         Scanner scanner = new Scanner(System.in);
         String nextTask = scanner.nextLine();
+        CommandEnum taskEnum = CommandEnum.assignEnum(nextTask);
 
-        while (!nextTask.equals("bye")) {
-            if (nextTask.equals("list")) {
-                this.printTasks();
-            } else if (nextTask.startsWith("mark") || nextTask.startsWith("unmark")) {
-                try {
-                    this.handleMark(nextTask);
-                } catch (DukeException e) {
-                    this.explainException(e);
-                }
-            } else {
-                try {
-                    this.handleTask(nextTask);
-                } catch (DukeException e) {
-                    this.explainException(e);
-                }
+        while (!taskEnum.equals(CommandEnum.BYE)) {
+            switch (taskEnum) {
+                case LIST:
+                    this.printTasks();
+                    break;
+                case MARK:
+                case UNMARK:
+                    try {
+                        this.handleMark(nextTask);
+                    } catch (DukeException e) {
+                        this.explainException(e);
+                    }
+                    break;
+                case TODO:
+                case DEADLINE:
+                case EVENT:
+                    try {
+                        this.handleTask(nextTask);
+                    } catch (DukeException e) {
+                        this.explainException(e);
+                    }
+                    break;
+                case DELETE:
+                    try {
+                        this.handleDelete(nextTask);
+                    } catch (DukeException e) {
+                        this.explainException(e);
+                    }
+                    break;
+                default:
+                    this.handleInvalid();
             }
 
             nextTask = scanner.nextLine();
+            taskEnum = CommandEnum.assignEnum(nextTask);
         }
 
         scanner.close();
 
         this.bye();
     }
+    // CHECKSTYLE.ON: Indentation
 
     public static void main(String[] args) {
         Duke chatBot = new Duke();
--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -2,9 +2,4 @@ public class DukeException extends Exception {
     public DukeException(String message) {
         super(message);
     }
-
-    @Override
-    public String toString() {
-        return " ☹ OOPS!!! " + super.getMessage();
-    }
 }
