7fb10af7792cf4e68ed90e7f365f0293d0060ee5
AlagappanRa <96157054+AlagappanRa@users.noreply.github.com>
2023-08-23 15:48:35 +0800

    Tasks Completed: Level 0. Rename, Greet, Exit Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,44 @@
+import java.util.Scanner;
 public class Duke {
+    private static String chatBotName = "CHAD CCP";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+
+        Duke dukeInstance = new Duke();
+        Scanner scanner = new Scanner(System.in);
+        dukeInstance.greetUser();
+
+        while(true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                dukeInstance.goodBye();
+                break;
+            } else {
+                dukeInstance.echoUser(command);
+            }
+        }
+    }
+
+    public void greetUser() {
+        System.out.println("________________________________");
+        System.out.println("Hello! I'm " + Duke.chatBotName);
+        System.out.println("What can I do for you?");
+        System.out.println("________________________________");
+    }
+
+    public void echoUser(String command) {
+        System.out.println("________________________________");
+        System.out.println(command);
+        System.out.println("________________________________");
+    }
+
+    public void goodBye() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________");
     }
 }
