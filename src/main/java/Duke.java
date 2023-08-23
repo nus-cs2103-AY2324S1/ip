d3a170072c64d563ea8f9153eabb5c11f649ca7d
Sheeepen <chengxi1020@gmail.com>
2023-08-23 21:56:43 +0800

    added welcome and exit message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        String welcomeMessage = "──────────────────────────────────── \n" +
+                                "Hello! I'm BIGMODE \n" +
+                                "What can I do for you? \n" +
+                                "────────────────────────────────────";
+
+        String exitMessage = "Bye! Hope to see you again soon! \n" +
+                             "────────────────────────────────────";
+
+        System.out.println(welcomeMessage);
+        System.out.println(exitMessage);
     }
 }
