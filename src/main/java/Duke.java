7b424ebbc5d8d1ec2a3de3c8e8b7bd4676ec34ad
Kailash201 <109354852+Kailash201@users.noreply.github.com>
2023-08-23 17:44:32 +0800

    Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String Start = "Hello! I'm Red \nWhat can I do for you?\n\n";
+        String End = "Bye. Hope to see you again soon!\n";
+
+        System.out.println(Start + End);
     }
 }
