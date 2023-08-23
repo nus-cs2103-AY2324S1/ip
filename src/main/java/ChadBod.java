7d9be1ea5e7c67c3965a59d533b354f58f523bc4
awhb <awhangbin@gmail.com>
2023-08-23 13:48:34 +0800

    Week 2 - Duke Level-1

--- src/main/java/ChadBod.java
+++ src/main/java/ChadBod.java
@@ -1,9 +1,20 @@
 package main.java;
+import java.util.Scanner;
 
 public class ChadBod {
     public static void main(String[] args) {
         System.out.println("Hello! I'm ChadBod.\n");
         System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
