e74546b752c3c3bfac8a37aacd2a6230973b2e03
feifeiraindrops <sunyufei.sylvia@gmail.com>
2023-08-23 12:18:49 +0800

    list

--- src/main/java/BenBen.java
+++ src/main/java/BenBen.java
@@ -1,34 +1,54 @@
 import java.util.Scanner;
 
 public class BenBen {
-    public static void main(String[] args) {
-        //initial
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
+    private static String[] arr;
+    private static String line ="_______________________________________\n";
+    private static int counter = 0;
 
-        //level 0
-        String line ="_______________________________________\n";
+    public static void exit() {
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!\n" + line);
+        System.exit(0);
+    }
+
+    public static void add(String task) {
+        arr[counter] = task;
+        counter++;
+        System.out.println(line);
+        System.out.println("added: " + task);
+        System.out.println(line);
+    }
+
+    public static void iterList() {
+        System.out.println(line);
+      for(int i = 0; i < counter; i++) {
+          System.out.println(i + ". " + arr[i]);
+      }
+        System.out.println(line);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        arr = new String[100];
         System.out.println(line);
         System.out.println("Hello! I'm BenBen.\n" +
                 "What can I do for you?");
-        System.out.println(line);
-//        System.out.println(line);
-        Scanner sc = new Scanner(System.in);
+        System.out.println(line);;
+
         while(sc.hasNext()) {
             String next = sc.nextLine();
 
             if (next.equals("bye")) {
-                System.out.println(line);
-                System.out.println("Bye. Hope to see you again soon!\n" + line);
+                exit();
                 break;
             }
-            System.out.println(line);
-            System.out.println(next);
-            System.out.println(line);
+
+            if (next.equals("list")) {
+                iterList();
+                continue;
+            }
+
+            add(next);
         }
         System.exit(0);
     }
