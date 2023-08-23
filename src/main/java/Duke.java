71e0509b75b2ba41267aac3221dd13d290a83af7
Sasmik23 <mikhilanand@gmail.com>
2023-08-24 01:41:14 +0800

    level-5 error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,8 +3,14 @@ import java.util.ArrayList;
 
 public class Duke {
 
+    public static void inputChecker(String[] str, String task) throws DukeException{
 
-    public static void main(String[] args) {
+        if (str.length <2 ) {
+            throw new DukeException("☹ OOPS!!! The description of a " +task+ " cannot be empty.");
+        }
+    }
+    public static void main(String[] args) throws DukeException {
+
 
         System.out.println("Hello! I'm Mikhil" + '\n' + "What can I do for you");
         boolean flag = true;
@@ -28,33 +34,58 @@ public class Duke {
                 }
             }
             else if (splitStr[0].equals("todo")){
+                try{
+                    inputChecker(splitStr, "todo");
+                    Todo t = new Todo(input.substring(5));
+                    int totalTasks = Task.getSize();
+                    tasks[totalTasks-1] = t;
+                }
+                catch (DukeException e){
+                    System.out.println(e.getMessage());
+                }
 
-                Todo t = new Todo(input.substring(5));
-                int totalTasks = Task.getSize();
-                tasks[totalTasks-1] = t;
             }
             else if (splitStr[0].equals("deadline")){
+                try{
+                    inputChecker(splitStr, "deadline");
+                    String[] deadline = input.split("/");
+                    inputChecker(deadline, "deadline");
+                    Deadline d = new Deadline(deadline[0].substring(9), deadline[1].substring(3));
+                    int totalTasks = Task.getSize();
+                    tasks[totalTasks-1] = d;
+                }
+                catch (DukeException e){
+                    System.out.println(e.getMessage());
+                }
 
-                String[] deadline = input.split("/");
-                Deadline d = new Deadline(deadline[0].substring(9), deadline[1].substring(3));
-                int totalTasks = Task.getSize();
-                tasks[totalTasks-1] = d;
 
             }
             else if (splitStr[0].equals("event")){
+                try{
+                    inputChecker(splitStr, "event");
+                    String[] event = input.split("/");
+                    inputChecker(event, "deadline");
 
-                String[] event = input.split("/");
-                Event e = new Event(event[0].substring(6),event[1].substring(5), event[2].substring(3));
-                int totalTasks = Task.getSize();
-                tasks[totalTasks-1] = e;
+                    Event e = new Event(event[0].substring(6),event[1].substring(5), event[2].substring(3));
+                    int totalTasks = Task.getSize();
+                    tasks[totalTasks-1] = e;
+                }
+                catch (DukeException e){
+                    System.out.println(e.getMessage());
+                }
+
 
             }
             else if(splitStr[0].equals("mark") || splitStr[0].equals("unmark")){
+
                 int index = Integer.parseInt(splitStr[1]);
                 Task item = tasks[index-1];
                 item.setAction(splitStr[0]);
 
 
+            }
+            else{
+                System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
             System.out.println("----------------------------------------");
         }
