da66b3e26bc48e45e50fb5ba6f8fa9fa32684e74
nicleongyj <e0968256@u.nus.edu>
2023-08-23 18:18:11 +0800

    level 6 - delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,138 +1,173 @@
+import java.lang.reflect.Array;
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Duke {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Task> taskList = new ArrayList<>();
-        int number = 0;
+    private static ArrayList<Task> taskList = new ArrayList<>();
 
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
+    private static int number = 0;
+    private static boolean activated = true;
+    private static final String line = "____________________________________________________________";
+
+    public static void printHello() {
+        System.out.println(line);
         System.out.println("Hello! I am TaskMaster!");
         System.out.println("What can I do for you today?");
-        System.out.println("____________________________________________________________\n");
+        System.out.println(line);
+    }
+    public static void printGoodbye() {
+        System.out.println(line);
+        System.out.println("Bye! Hope to see you again!");
+        System.out.println(line);
+    }
 
-        boolean loggedIn = true;
+    public static void printList(ArrayList<Task> list) {
+        System.out.println(line);
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < Duke.number; i ++ ) {
 
-        while (loggedIn) {
+            System.out.println("Task " + (i + 1)+ ": " + list.get(i));
+        }
+        System.out.println(line);
+    }
+
+    public static void toggleMark(String mark, ArrayList<Task> taskList, String userInput) {
+        String[] parts = userInput.split(" ");
+        if (parts.length == 2) {
+            int taskIndex = Integer.parseInt(parts[1]) - 1;
+            if (taskIndex >= 0 && taskIndex < Duke.number) {
+                if (mark == "unmark") {
+                    taskList.get(taskIndex).markAsNotDone();
+                    System.out.println(line);
+                    System.out.println("OK, I have marked this as undone:");
+                    System.out.println("  " + taskList.get(taskIndex));
+                    System.out.println(line);
+                } else {
+                    taskList.get(taskIndex).markAsDone();
+                    System.out.println(line);
+                    System.out.println("Good job! I have marked this task as completed:");
+                    System.out.println("  " + taskList.get(taskIndex));
+                    System.out.println(line);
+                }
+            } else {
+                System.out.println("Sorry! you have input an invalid task!");
+            }
+        }
+    }
+
+    public static void deleteTask(int taskIndex) {
+        if (taskIndex >= 0 && taskIndex < Duke.taskList.size()) {
+            Task removedTask = Duke.taskList.remove(taskIndex);
+            Duke.number--;
+            System.out.println(line);
+            System.out.println("Noted. I've removed this task:");
+            System.out.println("  " + removedTask);
+            System.out.println("Now you have " + Duke.taskList.size() + " tasks in the list.");
+            System.out.println(line);
+        } else {
+            System.out.println("Sorry! You have entered an invalid task number.");
+        }
+    }
+
+    public static void addTask(String taskType, String userInput) throws DukeException{
+        if (taskType == "todo") {
+            String description = userInput.substring(5).trim();
+            if (description.isEmpty()) {
+                throw new DukeException("The description of a todo cannot be empty.");
+            }
+            Duke.taskList.add(new Todo(description));
+            Duke.number++;
+            System.out.println(line);
+            System.out.println("Got it. I've added this to-do task:");
+            System.out.println("  " + Duke.taskList.get(Duke.taskList.size() - 1));
+            System.out.println("Now you have " + taskList.size() + " tasks in the list.");
+            System.out.println(line);
+        } else if (taskType == "event") {
+            boolean wrongInput = false;
+            String input = userInput.substring(5);
+            String[] parts = input.split("/from");
+            if (parts.length == 2) {
+                String description = parts[0].trim();
+                String[] timeParts = parts[1].split("/to");
+                if (timeParts.length == 2) {
+                    String from = timeParts[0].trim();
+                    String end = timeParts[1].trim();
+                    Duke.taskList.add(new Event(description, from, end));
+                    Duke.number++;
+                } else {
+                    wrongInput = true;
+                }
+            } else {
+                wrongInput = true;
+            }
+            if (wrongInput == true) {
+                System.out.println("Please input a valid task");
+            } else {
+                System.out.println(line);
+                System.out.println("Got it. I've added this event:");
+                System.out.println("  " + Duke.taskList.get(Duke.taskList.size() - 1));
+                System.out.println("Now you have " + Duke.taskList.size() + " tasks in the list.");
+                System.out.println(line);
+            }
+        } else if (taskType == "deadline") {
+            boolean wrongInput = false;
+            String input = userInput.substring(8);
+            String[] parts = input.split("/by");
+            if (parts.length == 2) {
+                String description = parts[0].trim();
+                String by = parts[1].trim();
+                Duke.taskList.add(new Deadline(description, by));
+                Duke.number++;
+
+            } else {
+                wrongInput = true;
+            }
+            if (wrongInput == true) {
+                System.out.println("Please input a valid task");
+            } else {
+                System.out.println(line);
+                System.out.println("Got it. I've added this deadline:");
+                System.out.println("  " + Duke.taskList.get(Duke.taskList.size() - 1));
+                System.out.println("Now you have " + taskList.size() + " tasks in the list.");
+                System.out.println(line);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws DukeException {
+        Scanner scanner = new Scanner(System.in);
+        printHello();
+
+        while (Duke.activated) {
             String userInput = scanner.nextLine();
 
             if (userInput.equalsIgnoreCase("bye")) {
-                loggedIn = false;
+                Duke.activated = false;
                 break;
-            }
-
-            if (userInput.equalsIgnoreCase("list")) {
-                System.out.println("____________________________________________________________");
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < number; i ++ ) {
-
-                    System.out.println("Task " + (i + 1)+ ": " + taskList.get(i));
-                }
-                System.out.println("____________________________________________________________\n");
+            } else if (userInput.equalsIgnoreCase("list")) {
+                printList(Duke.taskList);
             } else if (userInput.startsWith("todo")) {
-                String description = userInput.substring(5);
-                taskList.add(new Todo(description));
-                number++;
-
-                System.out.println("____________________________________________________________");
-                System.out.println("Got it. I've added this to-do task:");
-                System.out.println("  " + taskList.get(taskList.size() - 1));
-                System.out.println("Now you have " + taskList.size() + " tasks in the list.");
-                System.out.println("____________________________________________________________\n");
-
+                addTask("todo", userInput);
             } else if (userInput.startsWith("event")) {
-                boolean wrongInput = false;
-                String input = userInput.substring(6);
-                String[] parts = input.split("/from");
-                if (parts.length == 2) {
-                    String description = parts[0].trim();
-                    String[] timeParts = parts[1].split("/to");
-                    if (timeParts.length == 2) {
-                        String from = timeParts[0].trim();
-                        String end = timeParts[1].trim();
-                        taskList.add(new Event(description, from, end));
-                        number++;
-                    } else {
-                        wrongInput = true;
-                    }
-                } else {
-                    wrongInput = true;
-                }
-
-                if (wrongInput == true) {
-                    System.out.println("Please input a valid task");
-                } else {
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Got it. I've added this event:");
-                    System.out.println("  " + taskList.get(taskList.size() - 1));
-                    System.out.println("Now you have " + taskList.size() + " tasks in the list.");
-                    System.out.println("____________________________________________________________\n");
-                }
+                addTask("event", userInput);
             } else if (userInput.startsWith("deadline")) {
-                boolean wrongInput = false;
-                String input = userInput.substring(9);
-                String[] parts = input.split("/by");
-                if (parts.length == 2) {
-                    String description = parts[0].trim();
-                    String by = parts[1].trim();
-                    taskList.add(new Deadline(description, by));
-                    number++;
-
-                } else {
-                    wrongInput = true;
-                }
-
-                if (wrongInput == true) {
-                    System.out.println("Please input a valid task");
-                } else {
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Got it. I've added this deadline:");
-                    System.out.println("  " + taskList.get(taskList.size() - 1));
-                    System.out.println("Now you have " + taskList.size() + " tasks in the list.");
-                    System.out.println("____________________________________________________________\n");
-                }
+                addTask("deadline", userInput);
             } else if (userInput.startsWith("mark")) {
-                String[] parts = userInput.split(" ");
-                if (parts.length == 2) {
-                    int taskIndex = Integer.parseInt(parts[1]) - 1;
-                    if (taskIndex >= 0 && taskIndex < number) {
-                        taskList.get(taskIndex).markAsDone();
-                        System.out.println("____________________________________________________________");
-                        System.out.println("Good job! I have marked this task as completed:");
-                        System.out.println("  " + taskList.get(taskIndex));
-                        System.out.println("____________________________________________________________\n");
-                    } else {
-                        System.out.println("Sorry! you have input an invalid task!");
-                    }
-                }
+                toggleMark("mark", Duke.taskList, userInput);
             } else if (userInput.startsWith("unmark")) {
+                toggleMark("unmark", Duke.taskList, userInput);
+            } else if (userInput.startsWith("delete")) {
                 String[] parts = userInput.split(" ");
                 if (parts.length == 2) {
                     int taskIndex = Integer.parseInt(parts[1]) - 1;
-                    if (taskIndex >= 0 && taskIndex < number) {
-                        taskList.get(taskIndex).markAsNotDone();
-                        System.out.println("____________________________________________________________");
-                        System.out.println("OK, I have marked this as undone:");
-                        System.out.println("  " + taskList.get(taskIndex));
-                        System.out.println("____________________________________________________________\n");
-                    } else {
-                        System.out.println("Sorry! you have input an invalid task!");
-                    }
+                    deleteTask(taskIndex);
+                } else {
+                    System.out.println("Please specify the task number to delete.");
                 }
             } else {
-                System.out.println("Please input valid command");
+                throw new DukeException("Please enter a valid command!");
             }
         }
-
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye! Hope to see you again!");
-        System.out.println("____________________________________________________________");
+        printGoodbye();
     }
 }
 
