aaa1fa69313c4dce4fc4b2d21fd2fab64d0ff01d
jannnice <>
2023-08-23 16:45:08 +0800

    test 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,4 @@
-public class Duke {
+ublic class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
