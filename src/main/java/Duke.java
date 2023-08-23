3f2274998099033a5d4009c04e021d63904640b8
TyrusLye <Lyetyrus@gmail.com>
2023-08-24 00:22:49 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -13,20 +14,30 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        // Start an infinite loop to continuously read and echo commands
+        // Create an ArrayList to store user commands
+        ArrayList<String> commands = new ArrayList<>(100);
+
+        // Start an infinite loop to continuously read and process commands
         while (true) {
             String command = scanner.nextLine();
-            // Check if the user entered "bye" to exit the loop
+            System.out.println("____________________________________________________________");
+
             if (command.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (command.equalsIgnoreCase("list")) {
+                System.out.println("Your commands:");
+                for (int i = 0; i < commands.size(); i++) {
+                    System.out.println((i + 1) + ". " + commands.get(i));
+                }
+            } else {
+                commands.add("added: " + command);
+                System.out.println("added: " + command);
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(command);
+
             System.out.println("____________________________________________________________");
         }
 
-        // Close the Scanner object
         scanner.close();
     }
 }
