527c0c1892942e1204e6f8457d5f1ee7bd519a22
Chow Yuan Jing <zcyuanjing@gmail.com>
2023-08-23 17:39:20 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String linesep = "-------------------------------";
+        System.out.println(linesep);
+        System.out.println("Hello! I'm YJ's Chatbot");
+        System.out.println("What can I do for you?\n" + linesep);
+        System.out.println("Bye. Hope to see you soon!\n" + linesep);
     }
 }
