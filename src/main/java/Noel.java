3e5e9d54685054615b29d16e44f294ee67199dd5
AlfredBeNoel <alfredbenoel@gmail.com>
2023-08-23 14:35:20 +0800

    Update Noel.java

--- src/main/java/Noel.java
+++ src/main/java/Noel.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class Noel {
+
+    public static void printFunction(String message){
+        String filler = "____________________________________________________________";
+        System.out.println(filler + "\n" + message + "\n" + filler);
+    }
     public static void main(String[] args) {
-        String filler = "____________________________________________________________\n";
-        String helloMsg = " Hello! I'm Noel!\nWhat can I do for you?\n";
-        String byeMsg = " Bye. Hope to see you again soon!\n";
-        System.out.println(filler + helloMsg + filler + byeMsg);
+
+        String helloMsg = " Hello! I'm Noel!\nWhat can I do for you?";
+        String byeMsg = " Bye. Hope to see you again soon!";
+
+        printFunction(helloMsg);
+
+        Scanner in = new Scanner(System.in);
+        String nextLine = in.nextLine();
+
+        while (!nextLine.equals("bye")){
+            printFunction(nextLine);
+            nextLine = in.nextLine();
+        }
+
+        printFunction(byeMsg);
     }
 }
