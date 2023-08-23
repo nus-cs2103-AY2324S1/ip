2225d9647daaed152f9b91389f46bc057c4e9e3d
tiong jjyi <tiongjjyi@gmail.com>
2023-08-23 17:43:22 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,8 +3,19 @@ public class Duke {
 
     public static void main(String[] args) {
         System.out.println("Hello! I'm JJ\n" +
-                "What can I do for you?\n" + "\n"+"Bye. Hope to see you again soon!");
-
+                "What can I do for you?\n" + "\n");
+        Scanner sc = new Scanner(System.in);
+        String cmd = sc.nextLine();
+        while (true) {
+            if (cmd.equals("bye")) {
+                sc.close();
+                System.out.println("Bye. Hope to see you again soon!\n");
+                System.exit(0);
+            } else {
+                System.out.println(cmd + "\n");
+                cmd = sc.nextLine();
+            }
+        }
     }
 }
 //    String logo = " ____        _        \n"
