8848c8f75f5e45c6abd171c81ffcc59fef632b18
L1MJH <lolflgc@icloud.com>
2023-08-23 16:29:20 +0800

    Added a lot more test cases

--- src/main/java/Simon.java
+++ src/main/java/Simon.java
@@ -86,21 +86,46 @@ public class Simon {
                 if (deadlineParts.length <= 1 || !inData.contains(" /by ")) {
                     throw new SimonException("☹ OOPS!!! The format for deadline is incorrect. Expected format: 'deadline [task description] /by [deadline]'.");
                 }
-                String name = deadlineParts[1].split(" /by ")[0];
-                String endDate = deadlineParts[1].split(" /by ")[1];
-                task = new Deadline(name, endDate);
+                String nameDeadline = deadlineParts.length > 1 ? deadlineParts[1].split("/by ")[0] : "";
+                if (nameDeadline.trim().isEmpty()) {
+                    throw new SimonException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                }
+                String endDate = deadlineParts[1].split(" /by ").length > 1 ? deadlineParts[1].split(" /by ")[1] : "";
+                if (endDate.trim().isEmpty()) {
+                    throw new SimonException("☹ OOPS!!! The deadline date cannot be empty.");
+                }
+                task = new Deadline(nameDeadline, endDate);
                 break;
+
+
 
             case EVENT:
                 String[] eventParts = inData.split("event ");
-                if (eventParts.length <= 1 || !inData.contains(" /from ") || !inData.contains(" /to ")) {
+                if (eventParts.length <= 1 || !inData.contains("/from ") || !inData.contains("/to ")) {
                     throw new SimonException("☹ OOPS!!! The format for event is incorrect. Expected format: 'event [event description] /from [start date] /to [end date]'.");
                 }
-                String eventName = eventParts[1].split(" /from ")[0];
-                String startDate = eventParts[1].split(" /from ")[1].split(" /to ")[0];
-                String endDateEvent = eventParts[1].split(" /from ")[1].split(" /to ")[1];
+
+                String[] fromToParts = eventParts[1].split("/from ");
+                if (fromToParts.length < 2) {
+                    throw new SimonException("☹ OOPS!!! The format for event is missing 'from' information.");
+                }
+
+                String eventName = fromToParts[0].trim();
+                if (eventName.isEmpty()) {
+                    throw new SimonException("☹ OOPS!!! The description of an event cannot be empty.");
+                }
+
+                String[] toParts = fromToParts[1].split("er/to ");
+                if (toParts.length < 2) {
+                    throw new SimonException("☹ OOPS!!! The format for event is missing 'to' information.");
+                }
+
+                String startDate = toParts[0].trim();
+                String endDateEvent = toParts[1].trim();
+
                 task = new Event(eventName, startDate, endDateEvent);
                 break;
+
 
             default:
                 throw new SimonException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
