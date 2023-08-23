e7892094d67cdf40955b069c5f5d552750d326e9
remuslum <57741494+remuslum@users.noreply.github.com>
2023-08-23 16:24:29 +0800

    Added Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,10 +7,30 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
+        //Initiate Scanner to handle user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Introduction Message
         System.out.println(logo);
         System.out.println("Hello! I'm Chad");
         System.out.println("What can I do for you?");
         System.out.println();
-        System.out.println("Bye. Hope to see you again");
+
+        // Echoing User Input
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            // If user types bye, goodbye message is printed without echoing the command
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again");
+                break;
+            }
+            else {
+                // Command is echoed if command is not "bye"
+                System.out.println(userInput);
+            }
+        }
+        scanner.close();
     }
 }
