bcf49751ffacdefd16bc497748f1aa036472f8d8
Timothy <timothyleow12@gmail.com>
2023-08-23 10:56:08 +0800

    Level-1 Bot, echoes input until bye is said

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
 public class Duke {
     private static String name = "WallE";
+    public static void printDivider(boolean isIndented) {
+        if (isIndented)
+            System.out.println('\t' + "_________________________________________");
+        else
+            System.out.println("_________________________________________");
+    }
     public static void main(String[] args) {
-        System.out.println("_________________________________________");
-        System.out.println("Hello! I'm " + name + "!");
-        System.out.println("What can I do for you?");
-        System.out.println("_________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        printDivider(true);
+        System.out.println("\tHello! I'm " + name + "!");
+        System.out.println("\tWhat can I do for you?");
+        printDivider(true);
+        String input;
+        do {
+            input = scanner.nextLine();
+            if (!input.equals("bye")) {
+                printDivider(true);
+                System.out.println('\t' + input);
+                printDivider(true);
+            } else {
+                break;
+            }
+        } while (true);
+
+        printDivider(true);
+        System.out.println("\tBye. Hope to see you again soon!");
+        printDivider(true);
     }
 }
