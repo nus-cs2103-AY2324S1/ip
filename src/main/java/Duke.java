b8beb55918337818df99ebb1c29c19480a822b7b
kokseng1 <ngkokseng02@gmail.com>
2023-08-23 22:12:04 +0800

    lvl4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -29,11 +29,51 @@ public class Duke {
             }
         }
 
+        class Todo extends Task {
+            public Todo (String desc) {
+                super(desc.substring(5));
+            }
+
+            @Override
+            public String toString() {
+                return "[T]" + super.toString();
+            }
+        }
+
+        class Deadline extends Task {
+            final String by;
+            public Deadline(String desc) {
+                super(desc.substring(9, desc.indexOf("/by")));
+                this.by = desc.substring(desc.indexOf("/by") + 4);
+            }
+
+            @Override
+            public String toString() {
+                return "[D]" + super.toString() + "(by: " + this.by + ")";
+            }
+        }
+
+        class Event extends Task {
+            final String from;
+            final String to;
+            public Event(String desc) {
+                super(desc.substring(6, desc.indexOf("/from")));
+                int fromIndex = desc.indexOf("/from");
+                int toIndex = desc.indexOf("/to");
+                this.from = desc.substring(fromIndex + 6, toIndex);
+                this.to = desc.substring(toIndex + 4);
+            }
+
+            @Override
+            public String toString() {
+                return "[E]" + super.toString() + "(from: " + this.from + "to: " + this.to +")";
+            }
+        }
+
         Scanner scan = new Scanner(System.in);
         Task[] tasks = new Task[100];
         String name = "Chaty";
         int taskNo = 0;
-
         System.out.println("Hello! I'm " + name + "\n" +
                 "What can I do for you?" + "\n\n");
         String next = scan.nextLine();
@@ -50,11 +90,21 @@ public class Duke {
                 int tasknum = Integer.parseInt(next.split(" ")[1]) - 1;
                 tasks[tasknum].unmark();
                 System.out.println(" OK, I've marked this task as not done yet:\n" + tasks[tasknum]);
-            } else {
-                    Task nextTask = new Task(next);
+            } else if (next.startsWith("deadline")){
+                    Task nextTask = new Deadline(next);
                     tasks[taskNo] = nextTask;
                     taskNo++;
-                    System.out.println("added: " + next);
+                    System.out.println("Got it. I've added this task: \n" + nextTask + "\nnow you have " + taskNo + " tasks in the list");
+            } else if (next.startsWith("event")){
+                Task nextTask = new Event(next);
+                tasks[taskNo] = nextTask;
+                taskNo++;
+                System.out.println("Got it. I've added this task: \n" + nextTask + "\nnow you have " + taskNo + " tasks in the list");
+            } else if (next.startsWith("todo")){
+                Task nextTask = new Todo(next);
+                tasks[taskNo] = nextTask;
+                taskNo++;
+                System.out.println("Got it. I've added this task: \n" + nextTask + "\nnow you have " + taskNo + " tasks in the list");
             }
             next = scan.nextLine();
         }
