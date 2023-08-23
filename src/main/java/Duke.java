a70d23f2299849c24ff01304402e9ab78687c87c
yezkez <e1032428@u.nus.edu>
2023-08-23 09:35:02 +0800

    Do Level-3. why did it take me 5 hours?? But finally clutched it

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public class Duke {
     private static int id = 1;
     private static ArrayList<Task> taskList = new ArrayList<>(); //universal task list in memory
+
     public static void main(String[] args) {
         String logo = " __          _        \n"
                 + "| |     _   _| | _____ \n"
@@ -11,12 +12,13 @@ public class Duke {
                 + "| |___ | |_| |   <  __/\n"
                 + "|____/ \\__,__|_|\\_\\___|\n";
         System.out.println("Hello I'm\n" + logo);
-        System.out.println( "What can I do for you?\n");
+        System.out.println("What can I do for you?\n");
         Scanner scanner = new Scanner(System.in);
         String input;
 
-        while(true) {
+        while (true) {
             input = scanner.nextLine();
+            Task newTask = new Task(input);
 
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
@@ -33,9 +35,26 @@ public class Duke {
                 continue;
             }
 
-            Task newTask = new Task(input);
+            String[] parts = input.split(" ");
+            if (parts.length >= 2) {
+                String cmd = parts[0];
+                int taskID = Integer.parseInt(parts[1]) - 1;
+                Task taskChanged = taskList.get(taskID);
+                if (cmd.equals("mark")) {
+                    taskChanged.markDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + taskChanged);
+                } else {
+                    taskChanged.markUndone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  " + taskChanged);
+                }
+                continue;
+            }
+
             taskList.add(newTask);
             System.out.println("added: " + input);
+
         }
         scanner.close();
     }
