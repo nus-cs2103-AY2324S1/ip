81fc52365fd1a4533512df35f9b2a424d4c05e59
brendan <brenstack62@gmail.com>
2023-08-23 21:24:43 +0800

    Add echo function for Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void echo(String message) {
+        String echoed = "\t____________________________________________________________\n"
+                + "\t " + message + "\n"
+                + "\t____________________________________________________________\n";
+        System.out.println(echoed);
+    }
+
     public static void main(String[] args) {
         /**
         String logo = " ____        _        \n"
@@ -8,16 +18,25 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
          */
-        String intro_message = "____________________________________________________________\n"
-                + " Hello! I'm Bob the Chatbot!\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n";
+        String intro_message = "\t____________________________________________________________\n"
+                + "\t Hello! I'm Bob the Chatbot!\n"
+                + "\t What can I do for you?\n"
+                + "\t____________________________________________________________\n";
 
-        String bye_message = " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
+        String bye_message = "\t____________________________________________________________\n"
+                + "\t Bye. Hope to see you again soon!\n"
+                + "\t____________________________________________________________";
 
         System.out.println(intro_message);
-        System.out.println(bye_message);
+        Scanner sc = new Scanner(System.in);
+        String message = "";
 
+        while (true) {
+            message = sc.nextLine();
+            if (message.equals("bye")) break;
+            echo(message);
+        }
+
+        System.out.println(bye_message);
     }
 }
