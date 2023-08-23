20327e71b207efe08c151112b8bcbb38c2ac7ad7
yanghengtang <yanghengtang@gmail.com>
2023-08-23 14:02:21 +0800

    Implement initial skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String HORIZONTAL_LINE = "____________________________________________________________ \n";
+
+        String message = HORIZONTAL_LINE
+                + "Hello! I'm Chad \n"
+                + "What can I do for you? \n"
+                + HORIZONTAL_LINE
+                + "Bye. Hope to see you again soon! \n"
+                + HORIZONTAL_LINE;
+
+        System.out.println(message);
     }
 }
