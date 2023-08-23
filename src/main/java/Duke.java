62059fe6e261c9be0452cf58cbb25ce8c2d51927
sunzihan23 <e0970553@u.nus.edu>
2023-08-23 18:55:13 +0800

    Renamed chatbot, added greet and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
+    private static void greet() {
+        System.out.println("–––––––––––––––––––––––––––––––––––––––––");
+        System.out.println("Hello! I'm Bot");
+        System.out.println("What can I do for you?");
+    }
+
+    private static void exit() {
+        System.out.println("–––––––––––––––––––––––––––––––––––––––––");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("–––––––––––––––––––––––––––––––––––––––––");
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke.greet();
+        Duke.exit();
     }
 }
