be790b0b5a8662742103ea027706491d22bdd0ae
marcellaantania <marcellaantaniatan@gmail.com>
2023-08-23 17:27:04 +0800

    Name change from Duke to Thea, changed greeting and exiting message

---
+++ src/main/java/Thea.java
@@ -0,0 +1,12 @@
+public class Thea {
+    public static void main(String[] args) {
+        greet();
+        exit();
+    }
+    public static void greet() {
+        System.out.println("Hello! I'm Thea •ᴗ•\nHow can I help you?\n");
+    }
+    public static void exit() {
+        System.out.println("I hope I made your day easier with my service. See you again! >ᴗ<");
+    }
+}
