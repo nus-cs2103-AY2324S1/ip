fa89b8b3ab4e905753e3cabcb6ad33cc1669e254
rocketninja7 <andrewyapp@gmail.com>
2023-08-23 17:27:09 +0800

    Added ability to store text and display them

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -14,16 +16,30 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
 
+        List<String> list = new ArrayList<>();
+
         while(true) {
-            String command = scanner.nextLine();
-            if(command.strip().equalsIgnoreCase("bye")){
+            String command = scanner.nextLine().strip();
+            if(command.equalsIgnoreCase("bye")){
                 break;
             }
             System.out.println(HORIZONTAL_LINE);
-            System.out.println(command);
+            if(command.isEmpty()){
+                System.out.println("Nothing happened!");
+            }
+            else if(command.equalsIgnoreCase("list")){
+                for(int index=0;index<list.size();index++) {
+                    System.out.printf("%d. %s\n", index+1, list.get(index));
+                }
+            }
+            else {
+                list.add(command);
+                System.out.println("Added: " + command);
+            }
             System.out.println(HORIZONTAL_LINE);
         }
 
+        System.out.println(HORIZONTAL_LINE);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(HORIZONTAL_LINE);
     }
