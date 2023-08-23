d512299eae4510c24fed6782e4c97d92fc4b24b4
elaineshijie <lohelaineshijie.183@gmail.com>
2023-08-23 11:36:04 +0800

    Level-2 Lemon

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int counter = 0;
         String logo =
                   "\n" +
                           "                                                     \n" +
@@ -14,7 +16,15 @@ public class Duke {
         System.out.println("Hello! I'm " + logo + "! \nWhat can I do for you?");
         String input = scanner.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(input);
+            if (input.equals("list")) {
+                for (int i = 0; i < counter; i++ ){
+                    System.out.println(i + 1 + ". " + tasks[i]);
+                }
+            } else {
+                System.out.println("added: " + input);
+                tasks[counter] = input;
+                counter++;
+            }
             input = scanner.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
