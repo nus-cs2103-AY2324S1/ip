3d8a9cb7abb5649c7ae6fa5604f49a2425bc8b24
chuababyy <altheachuajiaxuan@gmail.com>
2023-08-23 23:04:21 +0800

    Level-1 Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,24 +3,26 @@ public class Duke {
     private static String chatbot = "chuababyy chatbot";
     private static String line = "------------------------";
 
-    public static void hiGreeting() {
+    public static void main(String[] args) {
         System.out.println(line);
         System.out.println("Hello! I'm " + chatbot);
         System.out.println("What can I do for you?");
         System.out.println(line);
-    }
-
-    public static void byeGreeting() {
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again!");
-        System.out.println(line);
-    }
-    public static void main(String[] args) {
-        hiGreeting();
 
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
 
-        byeGreeting();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = scanner.nextLine();
+        }
+
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again!");
+        System.out.println(line);
+
+
+        scanner.close();
+
     }
 }
