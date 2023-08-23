2c64fc6ebb90d155c03e8ac116f01f9da23d2608
awhb <awhangbin@gmail.com>
2023-08-23 15:46:05 +0800

    Week 2 - Duke Level-3

--- src/main/java/ChadBod.java
+++ src/main/java/ChadBod.java
@@ -1,29 +1,64 @@
-package main.java;
-import java.util.Scanner;
-import java.util.ArrayList;
+    package main.java;
+    import java.util.Scanner;
+    import java.util.ArrayList;
 
-public class ChadBod {
 
-    private static final ArrayList<String> tasks = new ArrayList<>();
+    public class ChadBod {
 
-    public static void main(String[] args) {
-        System.out.println("Hello! I'm ChadBod.\n");
-        System.out.println("What can I do for you?\n");
-        Scanner sc = new Scanner(System.in);
+        public static void main(String[] args) {
+            System.out.println("Hello! I'm ChadBod.\n");
+            System.out.println("What can I do for you?\n");
+            ArrayList<Task> tasks = new ArrayList<>();
+            Scanner sc = new Scanner(System.in);
+            boolean shouldExit = false;
 
-        while (true) {
-            String input = sc.nextLine();
-            if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!\n");
-                break;
-            } else if (input.equals("list")) {
-                for (int i = 0; i < ChadBod.tasks.size(); i ++) {
-                    System.out.printf("%d: %s\n", i + 1, ChadBod.tasks.get(i));
+            while (!shouldExit) {
+                String input = sc.nextLine();
+                String[] command = input.split(" ", 2);
+                switch (command[0]) {
+                    case "bye":
+                        System.out.println("Bye. Hope to see you again soon!\n");
+                        shouldExit = true;
+                        break;
+                    case "list":
+                        for (int i = 0; i < tasks.size(); i ++) {
+                            System.out.printf("%d: %s\n", i + 1, tasks.get(i));
+                        }
+                        break;
+                    case "mark":
+                        try {
+                            int taskNumber = Integer.parseInt(command[1]);
+                            if (taskNumber >= 1 && taskNumber <= tasks.size()) {
+                                Task task = tasks.get(taskNumber - 1);
+                                task.markDone();
+                                System.out.println("Nice! I've marked this task as done:\n");
+                                System.out.printf("%s\n", task);
+                            } else {
+                                System.out.println("Invalid task index.\n");
+                            }
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid input for marking task.\n");
+                        }
+                        break;
+                    case "unmark":
+                        try {
+                            int taskNumber = Integer.parseInt(command[1]);
+                            if (taskNumber >= 1 && taskNumber <= tasks.size()) {
+                                Task task = tasks.get(taskNumber - 1);
+                                task.markUndone();
+                                System.out.println("OK, I've marked this task as not done yet:\n");
+                                System.out.printf("%s\n", task);
+                            } else {
+                                System.out.println("Invalid task index.\n");
+                            }
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid input for unmarking task.\n");
+                        }
+                        break;
+                    default:
+                        tasks.add(new Task(input));
+                        System.out.printf("added: %s\n", input);
                 }
-            } else {
-                ChadBod.tasks.add(input);
-                System.out.printf("added: %s\n", input);
             }
         }
     }
-}
---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+package main.java;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markUndone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+}
