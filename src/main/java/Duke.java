ef95ab7924e8bcb8b87163eb2757fcebb502cfc0
howenc <xhowenx@gmail.com>
2023-08-23 19:38:45 +0800

    Added nextCommand and echo method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -10,15 +12,41 @@ public class Duke {
     }
 
     private static void greet() {
-        System.out.println("Hello. I am Luxion. \nWhat can I do for you?\n");
-        exit();
+        System.out.println("_______________________________\n" +
+                "Hello. I am Luxion. \n" +
+                "What can I do for you?\n" +
+                "_______________________________\n");
+        Scanner scanObj = new Scanner(System.in);
+        String command = scanObj.nextLine();
+        nextCommand(command);
     }
 
     private static void exit() {
-        System.out.println("Bye. See you soon.");
+        System.out.println("_______________________________\n" +
+                "Bye. See you soon.\n" +
+                "_______________________________\n");
+    }
+
+    private static void echo(String command) {
+        System.out.println("_______________________________\n" +
+                command+
+                "\n_______________________________\n");
     }
 
     private static void nextCommand(String command) {
-        
+        String[] commandArraycommand = command.split(" ");
+        switch(commandArraycommand[0]) {
+
+            case ("bye"):
+                exit();
+                break;
+
+            case ("list"):
+                //list();
+                break;
+
+            default:
+                echo(command);
+        }
     }
 }
