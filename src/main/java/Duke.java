4630f7536b4a46b79b8fe199ea0fd229c5295650
jannnice <>
2023-08-23 20:38:01 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
-ublic class Duke {
+public class Duke {
+    public static final String horizontalLine = "    ____________________________________________________________\n";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(horizontalLine
+                + "     Hello! I'm POPOOH\n"
+                + "     What can I do for you?\n"
+                + horizontalLine);
+        System.out.println("     Bye. Hope to see you again soon!\n"
+                + horizontalLine);
     }
 }
