a6f13d3c9e8d57d81c0b91954d960e55bd88cbf8
vivienherq <heruoqingvivien@gmail.com>
2023-08-23 13:54:26 +0800

    greet & exit

--- src/main/java/Dude.java
+++ src/main/java/Dude.java
@@ -1,10 +1,14 @@
 public class Dude {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        String greeting = "Hello, I'm Dude! \n" +
+                "What can I do for you?";
+        String exit = "Bye. Hope to see you again soon!";
+        System.out.println(greeting);
+        System.out.println(exit);
     }
 }
