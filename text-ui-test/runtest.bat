946b8037dea23940ec999b981e0076b02d36a960
TohLiYuan <e0960481@u.nus.edu>
2023-08-23 20:23:13 +0800

    Automated Test

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -2,12 +2,14 @@
 
 REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
+if not exist ..\bin\task mkdir ..\bin\task
 
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+javac -classpath ..\src\main\java\task -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
