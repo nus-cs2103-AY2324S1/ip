38d903e1ad7c608628f3e98f7f2afb9b0651d121
peiran18 <e0968798@u.nus.edu>
2023-08-23 23:59:43 +0800

    Echoes user commands until bye, added horizontal lines

--- src/main/java/Potato.java
+++ src/main/java/Potato.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 public class Potato {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Potato\n" + "What can I do for you?\n" + " \n" + "Bye. Hope to see you again soon!"
-);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("-----------------------------------------\n" +
+                "Hello! I'm Potato\n" + "What can I do for you?\n" +
+                "-----------------------------------------\n");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("-----------------------------------------\n" +
+                        "Bye. Hope to see you again soon!\n" +
+                        "-----------------------------------------\n");
+                break;
+            } else {
+                System.out.println("-----------------------------------------\n" +
+                        input + "\n" +
+                        "-----------------------------------------\n");
+                continue;
+            }
+        }
     }
 }
