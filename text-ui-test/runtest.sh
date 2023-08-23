bd633d1f3f7f5cf8a632779964d609271ae30bad
LicongHuang <alexliconghuang@gmail.com>
2023-08-23 19:21:10 +0800

    Added test cases for text-ui-test

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -7,6 +7,7 @@ then
 fi
 
 # delete output from previous run
+
 if [ -e "./ACTUAL.TXT" ]
 then
     rm ACTUAL.TXT
@@ -20,7 +21,7 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin Cheese < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
