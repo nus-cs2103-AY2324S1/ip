c7313997e4c0e361cad9c064f654ba4c51f30652
billieboy7 <smkbillkirankumar@gmail.com>
2023-08-24 00:22:16 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from the other side\n" + logo);
+        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+
+        duke.line();
+        System.out.println(" Hello! I'm JARVIS");
+        System.out.println("What can I do for you?");
+        duke.line();
+        System.out.println(" Bye. Hope to see you again soon!");
+        duke.line();
+    }
+    public void line() {
+        String line = "____________________________________________________________";
+        System.out.println(line);
     }
 }
