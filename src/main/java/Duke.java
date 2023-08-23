e8c93ffe24ee1a181f1993bf009da715e324d496
Teng Xin Zhuan <>
2023-08-23 15:59:16 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        String botName = "Aaronbot"; // Rename your chatbot here
+
+        System.out.println("Hello! I'm " + botName);
+        System.out.println("What can I do for you?");
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
