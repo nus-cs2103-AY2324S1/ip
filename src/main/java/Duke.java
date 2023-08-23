2fab45ce53f2a20988454e234b0921c1d446d27f
lerxuann <lerxuan@gmail.com>
2023-08-23 15:15:07 +0800

    Level 1: Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,32 @@
+import java.util.*;
+
 public class Duke {
+    static String greeting = "______________________________________\n"
+            + "Hi, I'm Chatty\n"
+            + "What do you need to do today?\n"
+            + "______________________________________";
+    static String goodbye = "______________________________________\n"
+            + "Bye. Don't come back!\n"
+            + "______________________________________";
+
+    public static String echo(String str) {
+        String returnLine = "______________________________________\n"
+                + str
+                + "\n______________________________________\n";
+        return returnLine;
+    }
+    static void awaitCommand() {
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        if (command.equals("bye")) {
+            System.out.println(goodbye);
+        } else {
+            System.out.println(echo(command));
+            awaitCommand();
+        }
+    }
     public static void main(String[] args) {
-        String greeting = "______________________________________\n"
-                + "Hi, I'm Chatty\n"
-                + "What do you need to do today?\n"
-                + "______________________________________";
-        String goodbye = "______________________________________\n"
-                + "Bye. Don't come back!\n"
-                + "______________________________________";
         System.out.println(greeting);
-        System.out.println(goodbye);
+        awaitCommand();
     }
 }
