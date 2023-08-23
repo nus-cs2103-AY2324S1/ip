d75443037ba6b6d24f141cc861a7bc283d650dee
peiran18 <e0968798@u.nus.edu>
2023-08-23 23:36:35 +0800

    Potato greets user and exits

---
+++ src/main/java/Potato.java
@@ -0,0 +1,6 @@
+public class Potato {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Potato\n" + "What can I do for you?\n" + " \n" + "Bye. Hope to see you again soon!"
+);
+    }
+}
