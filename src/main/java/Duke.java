a57d3d522f518326d3629be350f6d1a398226551
Timothy <timothyleow12@gmail.com>
2023-08-23 11:58:00 +0800

    Can mark tasks as done and undone

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,38 @@
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 public class Duke {
     private static String name = "WallE";
+    private static class Task {
+        private String name;
+        private boolean isDone;
+        public Task(String name, boolean isDone) {
+            this.name = name;
+            this.isDone = isDone;
+        }
+        public String getName() {
+            return this.name;
+        }
+        public boolean isDone() {
+            return this.isDone;
+        }
+        public void markAsDone() {
+            this.isDone = true;
+        }
+        public void markAsUndone() {
+            this.isDone = false;
+        }
+        public String getStatusIcon() {
+            return this.isDone() ? "[X]" : "[]";
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s %s", this.getStatusIcon(), this.getName());
+
+        }
+    }
+    private static List<Task> tasks = new ArrayList<>();
     public static void printDivider(boolean isIndented) {
         if (isIndented)
             System.out.println('\t' + "_________________________________________");
@@ -16,18 +45,38 @@ public class Duke {
         System.out.println("\tHello! I'm " + name + "!");
         System.out.println("\tWhat can I do for you?");
         printDivider(true);
-        List<String> inputs = new ArrayList<>();
         String input;
         do {
             input = scanner.nextLine();
             if (!input.equals("bye")) {
                 printDivider(true);
                 if (!input.equals("list")) {
-                    inputs.add(input);
-                    System.out.println("\tadded: " + input);
+                    String[] inputWords = input.split(" ");
+                    if (inputWords[0].equals("mark") || inputWords[0].equals("unmark")) {
+                        try {
+                            int id = Integer.valueOf(inputWords[1]) - 1;
+                            switch (inputWords[0]) {
+                                case "mark":
+                                    tasks.get(id).markAsDone();
+                                    System.out.println("\tNice! I've marked this task as done:");
+                                    System.out.println("\t\t" + tasks.get(id).toString());
+                                    break;
+                                case "unmark":
+                                    tasks.get(id).markAsUndone();
+                                    System.out.println("\tOk, I've marked this task as not done yet:");
+                                    System.out.println("\t\t" + tasks.get(id).toString());
+                            }
+                        } catch (Exception e) {
+                            throw e;
+                        }
+                    } else {
+                        tasks.add(new Task(input, false));
+                        System.out.println("\tadded: " + input);
+                    }
                 } else {
-                    for (int i = 0; i < inputs.size(); i++) {
-                        System.out.println(String.format("\t%d. %s", i + 1, inputs.get(i)));
+                    for (int i = 0; i < tasks.size(); i++) {
+                        Task task = tasks.get(i);
+                        System.out.println(String.format("\t%d.%s", i + 1, task.toString()));
                     }
                 }
                 printDivider(true);
