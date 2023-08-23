9773c0b12a384ad6dc1dafa1dcdfe558e08dc542
Dominic Ng Jun Yao <e0958779@u.nus.edu>
2023-08-23 23:40:56 +0800

    Add echo command and exit command for chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String line = "_".repeat(60);
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(line + "\nHello! I'm Dommi\nWhat can I do for you?\n" + line);
-        System.out.println("Bye. Hope to see you again soon!\n" + line);
+        while (true) {
+            String userInput = scanner.nextLine();  // Read user input
+            if (userInput.equals("bye")) {
+                System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
+                break;
+            }
+            System.out.println(line + "\n" + userInput + "\n" + line);
+        }
     }
 }
