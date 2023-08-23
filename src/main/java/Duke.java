b0a9ad1b32a7d3ff9e24a4be64d9653fd86cf85f
dhruvir29 <dhruvirathod29@gmail.com>
2023-08-24 03:03:11 +0800

    echo user's input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
 public class Duke {
 
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         printHorizontalLine();
-        System.out.println("Hello! I'm Buddy! \n" +
-                "What can I do for you? ");
+        System.out.println("\t " + "Hello! I'm Buddy! \n" +
+               "\t " + "What can I do for you? ");
         printHorizontalLine();
-        System.out.println("Bye! Hope to see you again soon! ");
-        printHorizontalLine();
+
+        while(true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("\t " + "Bye! Hope to see you again soon! ");
+                printHorizontalLine();
+                break;
+            }
+            System.out.println("\t " + input);
+            printHorizontalLine();
+        }
+        scanner.close();
+
     }
 
     public static void printHorizontalLine() {
-        System.out.println("________________________________________________");
+        System.out.println("    ________________________________________________");
     }
 }
