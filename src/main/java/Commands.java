4f75af8457384ab330bdc9c4665f8abe9ff04aeb
Marcus Soh <macussoh38@gmail.com>
2023-08-23 13:42:44 +0800

    addn: enums

---
+++ src/main/java/Commands.java
@@ -0,0 +1,22 @@
+public enum Commands {
+    BYE,
+    LIST,
+    MARK,
+    UNMARK,
+    DELETE,
+    TODO,
+    DEADLINE,
+    EVENT;
+
+    public static boolean contains(String test) {
+
+        for (Commands c : Commands.values()) {
+            if (c.name().equals(test)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -37,111 +37,129 @@ public class Duke {
 
         Scanner keyboard = new Scanner(System.in);
 
+        loop:
         while (true) {
             try {
                 inputString = keyboard.nextLine();
 
-                if (Objects.equals(inputString, "bye")) {
-                    break;
-                }
-//            System.out.println(inputString);
-                System.out.println(SEPARATOR_LINE);
 
-                if (inputString.equals("list")) {
-                    // output the list
-                    System.out.println(listContainer.toString());
-                } else if (inputString.startsWith("mark ")) {
-                    // mark the index
-                    int index = Integer.parseInt(inputString.split(" ")[1]);
-                    listContainer.markAsDone(index);
+                String inputCommandString = (inputString.split(" ")[0].toUpperCase());
 
-                } else if (inputString.startsWith("unmark ")) {
-                    // unmark the index
-                    int index = Integer.parseInt(inputString.split(" ")[1]);
-                    listContainer.markAsUnDone(index);
-                } else if (inputString.startsWith("delete ")) {
-                    int index = Integer.parseInt(inputString.split(" ")[1]);
-                    listContainer.removeFromList(index);
-                } else if (inputString.startsWith("todo ")) {
-                    // add a todo
-                    String itemName = inputString.replace("todo ", "");
-
-                    if (itemName.isEmpty()) {
-                        // no item name
-                        throw new DukeException(NAME_EMPTY);
-                    }
-
-
-                    TodoTask todoTask = new TodoTask(itemName);
-
-                    listContainer.addToList(todoTask);
-                } else if (inputString.startsWith("deadline ")) {
-                    // TODO: potential error handling here
-                    // format of entry: "deadline return book /by Sunday"
-                    String itemName = inputString.replace("deadline ", "").split("/by ")[0];
-
-                    if (itemName.isEmpty()) {
-                        // no item name
-                        throw new DukeException(NAME_EMPTY);
-                    }
-
-                    String[] inputArgs = inputString.replace("deadline ", "").split("/by ");
-                    if (inputArgs.length < 2) {
-                        // missing deadline
-                        throw new DukeException(DEADLINE_EMPTY);
-                    }
-                    String deadline = inputArgs[1];
-
-                    if (deadline.isEmpty()) {
-                        // no item name
-                        throw new DukeException(DEADLINE_EMPTY);
-                    }
-                    DeadlineTask deadlineTask = new DeadlineTask(itemName, deadline);
-
-                    listContainer.addToList(deadlineTask);
-                } else if (inputString.startsWith("event ")) {
-                    String inputArgs = inputString.replace("event ", "");
-
-                    // sample format: event project meeting /from Mon 2pm /to 4pm
-                    // get the name
-                    String itemName = inputArgs.split("/from ")[0];
-
-                    if (itemName.isEmpty()) {
-                        // no item name
-                        throw new DukeException(NAME_EMPTY);
-                    }
-
-                    // get the 'from...to'
-                    // @see https://stackoverflow.com/questions/4662215/how-to-extract-a-substring-using-regex
-                    Pattern patternFrom = Pattern.compile("(/from )(.*?)( /to)");
-                    Matcher matcherFrom = patternFrom.matcher(inputArgs);
-
-                    String from = "";
-                    if (matcherFrom.find()) {
-                        // yes, formatted correctly
-                        from = matcherFrom.group(2);
-                    } else {
-//                        System.out.println("ERROR: no pattern found");
-                        throw new DukeException(TIME_FORMAT_ERROR);
-                    }
-
-                    // get the to...
-                    String to = inputArgs.split("/to ")[1];
-
-                    if (to.isEmpty()) {
-                        throw new DukeException(TO_EMPTY);
-                    }
-
-                    EventTask eventTask = new EventTask(itemName, from, to);
-
-                    listContainer.addToList(eventTask);
-
-
-                } else {
+                if (!Commands.contains(inputCommandString)) {
                     throw new DukeException(UNKNOWN_COMMAND);
                 }
 
-//                listContainer.addToList();
+                Commands inputCommand = Commands.valueOf(inputCommandString);
+
+                System.out.println(SEPARATOR_LINE);
+                switch (inputCommand) {
+                    case BYE:
+                        break loop;
+                    case LIST: {
+                        System.out.println(listContainer.toString());
+                        break;
+                    }
+                    case MARK: {
+                        int index = Integer.parseInt(inputString.split(" ")[1]);
+                        listContainer.markAsDone(index);
+                        break;
+                    }
+                    case UNMARK: {
+                        int index = Integer.parseInt(inputString.split(" ")[1]);
+                        listContainer.markAsUnDone(index);
+                        break;
+                    }
+                    case DELETE: {
+                        int index = Integer.parseInt(inputString.split(" ")[1]);
+                        listContainer.removeFromList(index);
+                        break;
+                    }
+                    case TODO: {
+                        // add a todo
+                        String itemName = inputString.replace("todo ", "");
+
+                        if (itemName.isEmpty()) {
+                            // no item name
+                            throw new DukeException(NAME_EMPTY);
+                        }
+
+
+                        TodoTask todoTask = new TodoTask(itemName);
+
+                        listContainer.addToList(todoTask);
+                        break;
+                    }
+                    case DEADLINE: {
+                        // format of entry: "deadline return book /by Sunday"
+                        String itemName = inputString.replace("deadline ", "").split("/by ")[0];
+
+                        if (itemName.isEmpty()) {
+                            // no item name
+                            throw new DukeException(NAME_EMPTY);
+                        }
+
+                        String[] inputArgs = inputString.replace("deadline ", "").split("/by ");
+                        if (inputArgs.length < 2) {
+                            // missing deadline
+                            throw new DukeException(DEADLINE_EMPTY);
+                        }
+                        String deadline = inputArgs[1];
+
+                        if (deadline.isEmpty()) {
+                            // no item name
+                            throw new DukeException(DEADLINE_EMPTY);
+                        }
+                        DeadlineTask deadlineTask = new DeadlineTask(itemName, deadline);
+
+                        listContainer.addToList(deadlineTask);
+                        break;
+                    }
+                    case EVENT: {
+                        String inputArgs = inputString.replace("event ", "");
+
+                        // sample format: event project meeting /from Mon 2pm /to 4pm
+                        // get the name
+                        String itemName = inputArgs.split("/from ")[0];
+
+                        if (itemName.isEmpty()) {
+                            // no item name
+                            throw new DukeException(NAME_EMPTY);
+                        }
+
+                        // get the 'from...to'
+                        // @see https://stackoverflow.com/questions/4662215/how-to-extract-a-substring-using-regex
+                        Pattern patternFrom = Pattern.compile("(/from )(.*?)( /to)");
+                        Matcher matcherFrom = patternFrom.matcher(inputArgs);
+
+                        String from = "";
+                        if (matcherFrom.find()) {
+                            // yes, formatted correctly
+                            from = matcherFrom.group(2);
+                        } else {
+//                        System.out.println("ERROR: no pattern found");
+                            throw new DukeException(TIME_FORMAT_ERROR);
+                        }
+
+                        // get the to...
+                        String to = inputArgs.split("/to ")[1];
+
+                        if (to.isEmpty()) {
+                            throw new DukeException(TO_EMPTY);
+                        }
+
+                        EventTask eventTask = new EventTask(itemName, from, to);
+
+                        listContainer.addToList(eventTask);
+                        break;
+                    }
+
+                    default:
+                        throw new DukeException(UNKNOWN_COMMAND);
+                }
+                System.out.println(SEPARATOR_LINE);
+
+//            System.out.println(inputString);
+
 
             } catch (DukeException e) {
                 System.out.println(e.printError());
@@ -149,10 +167,9 @@ public class Duke {
                 System.out.println("some other exception " + e.getMessage());
             }
 
-            System.out.println(SEPARATOR_LINE);
+
         }
 
-        System.out.println(SEPARATOR_LINE);
         String exitMsg = "Bye! Hope to see you again soon.";
         System.out.println(exitMsg);
         System.out.println(SEPARATOR_LINE);
