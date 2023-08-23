62bebaa3ccd11951e62eafecb0875d2d933e5235
Saezenn <teng.hxa@gmail.com>
2023-08-24 00:25:00 +0800

    Level-2, Add, List

--- src/main/java/Sae.java
+++ src/main/java/Sae.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Sae {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Sae\nWhat can I do for you?");
+
+        ArrayList<String> store = new ArrayList<>();
 
         while (true) {
             Scanner input = new Scanner(System.in);
@@ -9,8 +12,15 @@ public class Sae {
             if (str.equals("bye")) {
                 break;
             }
+            else if (str.equals("list")) {
+                int len = store.size();
+                for (int i = 1; i < len + 1; i++) {
+                    System.out.println(i + ". " + store.get(i - 1));
+                }
+            }
             else {
-                System.out.println(str);
+                store.add(str);
+                System.out.println("added: " + str);
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
