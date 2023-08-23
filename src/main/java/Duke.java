e53ff2b413a2bef5b7be22a54a51eb3d0e2a58a1
nananakx-x <nanettekx.x3020@gmail.com>
2023-08-24 02:32:20 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello friend :> My name is John, nice to meet you!\n\n" +
-                "Bye for now, hope to see you soon.");
+        System.out.println("Hello friend :> My name is John, nice to meet you!\n" +
+                "Feel free to ask me anything.\n");
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Reply John: ");
+            String command = sc.nextLine(); // using .next() is wrong - only reads first word
+
+            if (command.equals("bye")) {
+                System.out.println("Bye for now, hope to see you soon.");
+                sc.close();
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
+
     }
 }
 
