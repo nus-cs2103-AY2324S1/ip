6df7fd8369b5ce232be21ba7ee6162b16e5feed2
Seonlo99 <Zheng_chenglong1999@hotmail.com>
2023-08-24 00:22:14 +0800

    Add mark and  unmark task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,29 +4,60 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Siri");
         System.out.println("What can I do for you?");
-
-        String[] store = new String[100];
-        int size = 0;
-        size = echo(store, size);
+        program();
         exit();
     }
 
-    private static int echo(String[] store, int size) {
+    private static class Task {
+        private final String taskName;
+        private boolean isComplete = false;
+
+        public Task(String taskName) {
+            this.taskName = taskName;
+        }
+
+        public void markComplete() {
+            this.isComplete = true;
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println(this.toString());
+        }
+
+        public void markIncomplete() {
+            this.isComplete = false;
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println(this.toString());
+        }
+
+        @Override
+        public String toString() {
+            char mark = isComplete ? 'X' : ' ';
+            return "[" + mark + "] " + this.taskName;
+        }
+    }
+
+    private static void program() {
+        Task[] store = new Task[100];
+        int size = 0;
         while (true) {
             Scanner sc = new Scanner(System.in);
             String word = sc.nextLine();
             if (word.equals("bye")) {
                 break;
-            } else  if (word.equals("list")) {
+            } else if (word.equals("list")) {
                 for (int i = 0; i < size; ++i) {
                     System.out.println(i + 1 + ". " + store[i]);
                 }
+            } else if (word.startsWith("mark ")) {
+                int target = Integer.parseInt(word.substring(5)) - 1;
+                store[target].markComplete();
+            } else if (word.startsWith("unmark ")) {
+                int target = Integer.parseInt(word.substring(7)) - 1;
+                store[target].markIncomplete();
             } else {
-                store[size++] = word;
+                store[size++] = new Task(word);
                 System.out.println("added: " + word);
             }
         }
-        return size;
     }
 
     private static void exit(){
