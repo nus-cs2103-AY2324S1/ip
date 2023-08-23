696208839f8f7f007aa496aa88792b149e35cd65
chewjh1234 <111339807+chewjh1234@users.noreply.github.com>
2023-08-23 22:52:12 +0800

    Add Level-4

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -19,4 +19,8 @@ public class Task {
     public void markAsUndone() {
         this.isDone = false;
     }
+
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
 }
--- src/main/java/Tired.java
+++ src/main/java/Tired.java
@@ -19,34 +19,57 @@ public class Tired {
        while (!input.equals("bye")) {
            System.out.println(horizontalLine);
            if (input.equals("list")) {
+               System.out.println("Here are the tasks in your list: ");
                for (int i = 0; i < pos; i++) {
-                   System.out.println((i + 1) + "."
-                           + "[" + list[i].getStatusIcon() + "] " + list[i].description);
+                   System.out.println((i + 1) + "." + list[i].toString());
                }
            } else if (input.startsWith("mark")) {
                int taskIndex = Integer.parseInt(input.substring(5).trim()) - 1;
                if (taskIndex >= 0 && taskIndex < pos) {
                    list[taskIndex].markAsDone();
                    System.out.println("Nice! I've marked this task as done:");
-                   System.out.println("[" + list[taskIndex].getStatusIcon() + "] " + list[taskIndex].description);
+                   System.out.println(list[taskIndex].toString());
                }
            } else if (input.startsWith("unmark")) {
                int taskIndex = Integer.parseInt(input.substring(7).trim()) - 1;
                if (taskIndex >= 0 && taskIndex < pos) {
                    list[taskIndex].markAsUndone();
                    System.out.println("OK, I've marked this task as not done yet:");
-                   System.out.println("[" + list[taskIndex].getStatusIcon() + "] " + list[taskIndex].description);
+                   System.out.println(list[taskIndex].toString());
                }
            } else {
-               Task t = new Task(input);
-               list[pos] = t;
-               System.out.println("added: " + input);
-               pos += 1;
-           }
+               if (input.startsWith("todo")) {
+                   String s = input.substring(5).trim();
+                   Task t = new ToDo(s);
+                   list[pos] = t;
+                   pos += 1;
+                   System.out.println("Got it. I've added this task:");
+                   System.out.println(t.toString());
+               } else if (input.startsWith("deadline")) {
+                   String[] parts = input.split("/");
+                   String task = parts[0].substring(9).trim();
+                   String date = parts[1].substring(3).trim();
+                   Task t = new Deadline(task, date);
+                   list[pos] = t;
+                   pos += 1;
+                   System.out.println("Got it. I've added this task:");
+                   System.out.println(t.toString());
+               } else if ((input.startsWith("event"))){
+                   String[] parts = input.split("/");
+                   String task = parts[0].substring(6).trim();
+                   String start = parts[1].substring(5).trim();
+                   String end = parts[2].substring(3).trim();
+                   Task t = new Event(task, start, end);
+                   list[pos] = t;
+                   pos += 1;
+                   System.out.println("Got it. I've added this task:");
+                   System.out.println(t.toString());
+               }
+               System.out.println("Now you have " + (pos) + " task(s) in the list.");
+       }
            System.out.println(horizontalLine + "\n");
            input = sc.nextLine();
        }
-
        System.out.println(horizontalLine);
        System.out.println("Bye. Hope to see you again soon!");
        System.out.println(horizontalLine);
