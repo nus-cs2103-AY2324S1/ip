15dea815972e60a37bef109177abf6f84d65a9e1
AlainS87 <1950834052@qq.com>
2023-08-23 18:32:29 +0800

    level 5 completed

---
+++ src/main/java/AlainException.java
@@ -0,0 +1,5 @@
+public class AlainException extends Exception{
+        public AlainException(String message) {
+            super(message);
+        }
+}
--- src/main/java/ChatbotAlain.java
+++ src/main/java/ChatbotAlain.java
@@ -2,103 +2,117 @@ import java.lang.reflect.Array;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class ChatbotAlain {
-    public static void main(String[] args) {
-        Task[] list = new Task[200];
-        int pos = 0;
-        String renameAndGreeting = "____________________________________________________________\n"
-                + " Hello! I'm Alain\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n";
-        System.out.println(renameAndGreeting);
-        while (true) {
-            Scanner s = new Scanner(System.in);
-            String text = new String();
-            text = s.nextLine();
-            boolean isMatchMark = Pattern.matches("mark\\d+", text);
-            boolean isMatchUnmark = Pattern.matches("unmark\\d+", text);
-            boolean isDeadline = Pattern.matches("deadline .+", text);
-            boolean isToDo = Pattern.matches("todo .+", text);
-            boolean isEvent = Pattern.matches("event .+", text);
-            if (isToDo) {
-                String mission = text.substring(4);
-                list[pos] = new ToDos(mission);
-                pos ++;
-                String output = "____________________________________________________________\n"
-                        + "  Got it. I've added this task:\n"
-                        + "   " + list[pos - 1] + "\n"
-                        + "Now you have " + pos + " tasks in the list.\n"
-                        + "____________________________________________________________\n";
-                System.out.println(output);
-                continue;
-            }
-            if (isDeadline) {
-                String mission = text.substring(8);
-                String[] parts = mission.split("/");
-                list[pos] = new Deadlines(parts[0], parts[1]);
-                pos ++;
-                String output = "____________________________________________________________\n"
-                        + "  Got it. I've added this task:\n"
-                        + "   " + list[pos - 1] + "\n"
-                        + "Now you have " + pos + " tasks in the list.\n"
-                        + "____________________________________________________________\n";
-                System.out.println(output);
-                continue;
-            }
-            if (isEvent) {
-                String mission = text.substring(5);
-                String[] parts = mission.split("/");
-                list[pos] = new Events(parts[0], parts[1], parts[2]);
-                pos ++;
-                String output = "____________________________________________________________\n"
-                        + "  Got it. I've added this task:\n"
-                        + "   " + list[pos - 1] + "\n"
-                        + "Now you have " + pos + " tasks in the list.\n"
-                        + "____________________________________________________________\n";
-                System.out.println(output);
-                continue;
-            }
-            if (text.equals("bye")) {
-                break;
-            } else if (isMatchMark) {
-                String numericPart = text.substring(4);
-                list[Integer.parseInt(numericPart) -1].markAsDone();
-                String output = "____________________________________________________________\n"
-                        + " Nice! I've marked this task as done:\n"
-                        + "   " + list[Integer.parseInt(numericPart) - 1] + "\n"
-                        + "____________________________________________________________\n";
-                System.out.println(output);
-            }else if (isMatchUnmark) {
-                String numericPart = text.substring(6);
-                list[Integer.parseInt(numericPart) - 1].markAsUndone();
-                String output = "____________________________________________________________\n"
-                        + " OK, I've marked this task as not done yet:\n"
-                        + "   " + list[Integer.parseInt(numericPart) - 1] + "\n"
-                        + "____________________________________________________________\n";
-                System.out.println(output);
-            }else if (! text.equals("list")){
-                list[pos] = new Task(text);
-                pos ++;
-                String output = "____________________________________________________________\n"
-                        + " " + text + "\n"
-                        + "____________________________________________________________\n";
-                System.out.println(output);
-
-            } else {
-                String output = "";
-                output += "____________________________________________________________\n"
-                 + "Here are the tasks in your list:\n";
-                for (int i = 0; i < pos; i++ ) {
-                    output += " " + (i + 1) + ". " + list[i] + "\n";
+public class ChatbotAlain{
+    public static void main(String[] args) throws AlainException {
+        try {
+            Task[] list = new Task[200];
+            int pos = 0;
+            String renameAndGreeting = "____________________________________________________________\n"
+                    + " Hello! I'm Alain\n"
+                    + " What can I do for you?\n"
+                    + "____________________________________________________________\n";
+            System.out.println(renameAndGreeting);
+            while (true) {
+                Scanner s = new Scanner(System.in);
+                String text = new String();
+                text = s.nextLine();
+                boolean isMatchMark = Pattern.matches("mark\\d+", text);
+                boolean isMatchUnmark = Pattern.matches("unmark\\d+", text);
+                boolean isDeadline = Pattern.matches("deadline .+", text);
+                boolean isToDo = Pattern.matches("todo .+", text);
+                boolean isEvent = Pattern.matches("event .+", text);
+                if (isToDo) {
+                    String mission = text.substring(4);
+                    if (mission.length() == 0) {
+                        throw new AlainException(" ☹ OOPS!!! The description of a Todo cannot be empty.");
+                    }
+                    list[pos] = new ToDos(mission);
+                    pos++;
+                    String output = "____________________________________________________________\n"
+                            + "  Got it. I've added this task:\n"
+                            + "   " + list[pos - 1] + "\n"
+                            + "Now you have " + pos + " tasks in the list.\n"
+                            + "____________________________________________________________\n";
+                    System.out.println(output);
+                    continue;
                 }
-                output += "____________________________________________________________\n";
-                System.out.println(output);
-            }
-        }
-        String endingLine = "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
+                if (isDeadline) {
+                    String mission = text.substring(8);
+                    if (mission.length() == 0) {
+                        throw new AlainException(" ☹ OOPS!!! The description of a Deadline cannot be empty.");
+                    }
+                    String[] parts = mission.split("/");
+                    if (parts.length != 2) {
+                        throw new AlainException(" ☹ OOPS!!! The description of a Deadline is invalid");
+                    }
+                    list[pos] = new Deadlines(parts[0], parts[1]);
+                    pos++;
+                    String output = "____________________________________________________________\n"
+                            + "  Got it. I've added this task:\n"
+                            + "   " + list[pos - 1] + "\n"
+                            + "Now you have " + pos + " tasks in the list.\n"
+                            + "____________________________________________________________\n";
+                    System.out.println(output);
+                    continue;
+                }
+                if (isEvent) {
+                    String mission = text.substring(5);
+                    if (mission.length() == 0) {
+                        throw new AlainException(" ☹ OOPS!!! The description of a Event cannot be empty.");
+                    }
+                    String[] parts = mission.split("/");
+                    if (parts.length != 3) {
+                        throw new AlainException(" ☹ OOPS!!! The description of a Event is invalid");
+                    }
+                    list[pos] = new Events(parts[0], parts[1], parts[2]);
+                    pos++;
+                    String output = "____________________________________________________________\n"
+                            + "  Got it. I've added this task:\n"
+                            + "   " + list[pos - 1] + "\n"
+                            + "Now you have " + pos + " tasks in the list.\n"
+                            + "____________________________________________________________\n";
+                    System.out.println(output);
+                    continue;
+                }
+                if (text.equals("bye")) {
+                    break;
+                } else if (isMatchMark) {
+                    String numericPart = text.substring(4);
+                    list[Integer.parseInt(numericPart) - 1].markAsDone();
+                    String output = "____________________________________________________________\n"
+                            + " Nice! I've marked this task as done:\n"
+                            + "   " + list[Integer.parseInt(numericPart) - 1] + "\n"
+                            + "____________________________________________________________\n";
+                    System.out.println(output);
+                } else if (isMatchUnmark) {
+                    String numericPart = text.substring(6);
+                    list[Integer.parseInt(numericPart) - 1].markAsUndone();
+                    String output = "____________________________________________________________\n"
+                            + " OK, I've marked this task as not done yet:\n"
+                            + "   " + list[Integer.parseInt(numericPart) - 1] + "\n"
+                            + "____________________________________________________________\n";
+                    System.out.println(output);
+                } else if (text.equals("list")) {
+                    String output = "";
+                    output += "____________________________________________________________\n"
+                            + "Here are the tasks in your list:\n";
+                    for (int i = 0; i < pos; i++) {
+                        output += " " + (i + 1) + ". " + list[i] + "\n";
+                    }
+                    output += "____________________________________________________________\n";
+                    System.out.println(output);
 
-        System.out.println(endingLine);
+                }
+                throw new AlainException(" ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+            }
+            String endingLine = "____________________________________________________________\n"
+                    + " Bye. Hope to see you again soon!\n"
+                    + "____________________________________________________________\n";
+            System.out.println(endingLine);
+        } catch (AlainException e) {
+            System.out.println("____________________________________________________________\n");
+            System.out.println(e.getMessage());
+            System.out.println("____________________________________________________________\n");
+        }
     }
 }
