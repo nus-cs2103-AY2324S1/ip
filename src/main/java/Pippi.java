851b8259458eade32a54235940f2c3ebb6b2e4c2
ph-nathan <px5099@gmail.com>
2023-08-23 14:59:19 +0800

    Add logic for adding tasks and listing for Level 2: Add, list

--- src/main/java/Pippi.java
+++ src/main/java/Pippi.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Pippi {
-    private static boolean inPokeball = false;
+
     public static void wrapText(String content) {
         String line = "_____________________________________\n";
         System.out.println(line);
-        System.out.println(content + "\n");
+        System.out.println(content);
         System.out.println(line);
     }
     public static void main(String[] args) {
+        boolean inPokeball = false;
+        ArrayList<String> tasks = new ArrayList<String>();
         String pippi =  "⣿⡿⠷⣶⣤⣄⡀⠀⠀⠀⢀⣤⣶⣿⣿⣿⣿⣶⣤⡀⠀⠀⠀⢀⣠⣤⣶⠾⢿⣿\n" +
                         "⣿⡇⠀⠀⣨⣿⣿⣷⠀⢠⣿⣿⠿⠋⢉⡉⠙⠿⣿⣿⣄⣤⣾⣿⣿⣅⠀⠀⢸⣿\n" +
                         "⢸⣧⠀⣰⣿⣿⣿⡏⢀⣿⣿⡏⢠⣾⣿⣿⣷⡄⢹⣿⣿⣿⣿⣿⣿⣿⣆⠀⣼⡇\n" +
@@ -30,8 +34,16 @@ public class Pippi {
             if (userMessage.equalsIgnoreCase("bye")) {
                 wrapText("Bye. Hope to see you again soon!");
                 inPokeball = true;
+            } else if (userMessage.equalsIgnoreCase("list")) {
+                String all = "";
+                for (int i = 0; i < tasks.size(); i++) {
+                    all = all + String.valueOf(i + 1) +". " + tasks.get(i) + "\n";
+                }
+                wrapText(all);
+
             } else {
-                wrapText(userMessage);
+                tasks.add(userMessage);
+                wrapText("added " + userMessage);
             }
         }
     }
