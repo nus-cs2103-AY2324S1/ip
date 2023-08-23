892b284458060f9cb03faed33c75fc0098ef2501
TyrusLye <Lyetyrus@gmail.com>
2023-08-24 00:05:59 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo = "██╗░░░██╗██████╗░██████╗░░█████╗░██╗\n"
                     + "██║░░░██║██╔══██╗██╔══██╗██╔══██╗██║\n"
                     + "██║░░░██║██████╔╝██████╦╝██║░░██║██║\n"
@@ -7,9 +10,23 @@ public class Duke {
                     + "╚██████╔╝██║░░██║██████╦╝╚█████╔╝██║\n"
                     + "░╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("What can I do for you? \n");
-        System.out.println("____________________________________________________________\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("____________________________________________________________\n");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Start an infinite loop to continuously read and echo commands
+        while (true) {
+            String command = scanner.nextLine();
+            // Check if the user entered "bye" to exit the loop
+            if (command.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(command);
+            System.out.println("____________________________________________________________");
+        }
+
+        // Close the Scanner object
+        scanner.close();
     }
 }
