f4f0bcc8b4a1d1bbb1f8176b61465d4e68a445b5
chewjh1234 <111339807+chewjh1234@users.noreply.github.com>
2023-08-23 20:42:06 +0800

    Add Level-3

--- src/main/java/Tired.java
+++ src/main/java/Tired.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Tired {
    public static void main(String[] args) {
 
-       String[] list = new String[100];
+       Task[] list = new Task[100];
        int pos = 0;
 
        String name = "Tired";
@@ -20,10 +20,26 @@ public class Tired {
            System.out.println(horizontalLine);
            if (input.equals("list")) {
                for (int i = 0; i < pos; i++) {
-                   System.out.println((i + 1) + "." + list[i]);
+                   System.out.println((i + 1) + "."
+                           + "[" + list[i].getStatusIcon() + "] " + list[i].description);
+               }
+           } else if (input.startsWith("mark")) {
+               int taskIndex = Integer.parseInt(input.substring(5).trim()) - 1;
+               if (taskIndex >= 0 && taskIndex < pos) {
+                   list[taskIndex].markAsDone();
+                   System.out.println("Nice! I've marked this task as done:");
+                   System.out.println("[" + list[taskIndex].getStatusIcon() + "] " + list[taskIndex].description);
+               }
+           } else if (input.startsWith("unmark")) {
+               int taskIndex = Integer.parseInt(input.substring(7).trim()) - 1;
+               if (taskIndex >= 0 && taskIndex < pos) {
+                   list[taskIndex].markAsUndone();
+                   System.out.println("OK, I've marked this task as not done yet:");
+                   System.out.println("[" + list[taskIndex].getStatusIcon() + "] " + list[taskIndex].description);
                }
            } else {
-               list[pos] = input;
+               Task t = new Task(input);
+               list[pos] = t;
                System.out.println("added: " + input);
                pos += 1;
            }
