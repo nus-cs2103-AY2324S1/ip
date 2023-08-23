54e1c5a54931607684676c5cc0baa3ddc341ad7f
RiyaMehta2211 <>
2023-08-23 22:35:54 +0800

    ip - Riya Mehta

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,15 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String name = "Harry Potter";
         String question = "What can I do for you?";
         System.out.println("Hello! I'm " + name + "\n" + question);
-        System.out.println("\n" + "Bye. Hope to see you again soon!");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        while (!str.equals("bye")) {
+            System.out.println("\t" + str);
+            str = sc.nextLine();
+        }
+        System.out.println("\t" + "Bye. Hope to see you again soon!");
     }
 }
