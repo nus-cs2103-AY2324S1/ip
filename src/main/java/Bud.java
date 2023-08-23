5f9846356c89c10b332cf3cbf8dead16d530cf5b
ylyma <amylingyuhui@gmail.com>
2023-08-23 15:37:41 +0800

    rename, greet, exit

---
+++ src/main/java/Bud.java
@@ -0,0 +1,17 @@
+public class Bud {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        String name = "Bud";
+        String line = "---------------------------\n";
+        System.out.println(line
+                + "Hello! I'm your buddy, " + name + "!\n" 
+                + "How can I help?\n"
+                + line
+                + "Bye. Check in again soon!\n"
+                + line);
+    }
+}
