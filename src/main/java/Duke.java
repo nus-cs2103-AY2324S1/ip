a6c7963aa9ff7066c13aa3c9e403c8a060188feb
Jingya <e0968387@u.nus.edu>
2023-08-24 00:42:54 +0800

    Added Task class and mark as done functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,24 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Duke {
     private static String NAME = "Moira";
     private static String SPACER = "--------------------------------------------------------------------------";
     private static boolean IS_RECEIVING_INPUT = false;
-    private static ArrayList<String> ALL_USER_INPUTS;
+    private static ArrayList<Task> TASK_LIST;
+    private static Scanner SCANNER;
 
     public static void main(String[] args) {
-        ALL_USER_INPUTS = new ArrayList<>();
+        SCANNER = new Scanner(System.in);
+        TASK_LIST = new ArrayList<>();
         greet();
         while (IS_RECEIVING_INPUT) {
             getUserInput();
         }
         exit();
+        SCANNER.close();
     }
 
     private static void greet() {
@@ -32,30 +37,71 @@ public class Duke {
     }
 
     private static void getUserInput() {
-        Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        if (userInput.equals("bye")) {
-            IS_RECEIVING_INPUT = false;
-        } else if (userInput.equals("list")) {
-            System.out.println(SPACER);
-            printAllUserInputs();
-            System.out.println(SPACER);
-        } else {
-            addInputToList(userInput);
-            System.out.println(SPACER);
-            System.out.println("Added: " + userInput);
-            System.out.println(SPACER);
-        }
+        String prefix = SCANNER.next();
+
+            switch (prefix) {
+                case "bye":
+                    IS_RECEIVING_INPUT = false;
+                    break;
+                case "list":
+                    System.out.println(SPACER);
+                    printTaskList();
+                    System.out.println(SPACER);
+                    break;
+                case "unmark":
+                case "mark":
+                    try {
+                        int index = SCANNER.nextInt() - 1;
+                        if (prefix.equals("mark")) {
+                            markTaskInListAsDone(index, true);
+                            System.out.println(SPACER);
+                            System.out.println("Good job on completing this task! You are an awesome possum!!");
+                            System.out.println(TASK_LIST.get(index).toString());
+                            System.out.println(SPACER);
+                        } else if (prefix.equals("unmark")) {
+                            markTaskInListAsDone(index, false);
+                            System.out.println(SPACER);
+                            System.out.println("Man, you've got this extra task to do now...");
+                            System.out.println(TASK_LIST.get(index).toString());
+                            System.out.println(SPACER);
+                        }
+                    } catch (InputMismatchException e) {
+                        SCANNER.nextLine();
+                        System.out.println(SPACER);
+                        System.out.println("HOLD UP! Invalid input for mark command. Input must be a positive non-zero integer.");
+                        System.out.println(SPACER);
+                    } catch (IndexOutOfBoundsException e) {
+                        SCANNER.nextLine();
+                        System.out.println(SPACER);
+                        System.out.println("HOLD UP! There is no such task in your list!");
+                        System.out.println(SPACER);
+                    } finally {
+                        break;
+                    }
+                default:
+                    String newTaskDescription = prefix + SCANNER.nextLine();
+                    addTaskToList(newTaskDescription);
+                    System.out.println(SPACER);
+                    System.out.println("Added: " + newTaskDescription);
+                    System.out.println(SPACER);
+                    break;
+            }
     }
 
-    private static void addInputToList(String newInput) {
-        ALL_USER_INPUTS.add(newInput);
+    private static void addTaskToList(String description) {
+        Task newTask = new Task(description);
+        TASK_LIST.add(newTask);
     }
 
-    private static void printAllUserInputs() {
+    private static void markTaskInListAsDone(int index, boolean done) {
+        TASK_LIST.get(index).markTaskCompleted(done);
+    }
+
+    private static void printTaskList() {
+        System.out.println("Hey buddy, here's your TODO list:");
         int index = 1;
-        for (String input : ALL_USER_INPUTS) {
-            System.out.println(index + ". " + input);
+        for (Task task : TASK_LIST) {
+            System.out.println(index + "." + task.toString());
             index++;
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    private String description;
+    private boolean done;
+
+    public Task(String description) {
+        this.description = description;
+        this.done = false;
+    }
+    public void markTaskCompleted(boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public String toString() {
+        String checkbox = "";
+        if (this.done) {
+            checkbox = "[X]";
+        } else {
+            checkbox = "[ ]";
+        }
+        return checkbox + " " + this.description;
+    }
+}
