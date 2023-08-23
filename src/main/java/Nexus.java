e2cecdee821ea0e1507cdbd7b52427fe05ffa671
s0ngyang <keesongyang4@gmail.com>
2023-08-23 21:04:32 +0800

    Added chatbot echo

--- src/main/java/Nexus.java
+++ src/main/java/Nexus.java
@@ -1,7 +1,16 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Nexus {
     public static void main(String[] args) {
         System.out.println("Hello! I'm NEXUS");
         System.out.println("What can I do for you?");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (Objects.equals(input, "bye")) break;
+            System.out.println(input);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
