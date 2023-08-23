8c01edb381794a18a37bbc7fbe27ad976b28bc33
andytoh1 <e0773488@u.nus.edu>
2023-08-24 07:34:20 +0800

    no message

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Chatbot {
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
         System.out.println("Hello! I'm Chatbot");
         System.out.println("What can I do for you?");
+        while (true) {
+            String userMessage = userInput.nextLine();
+            if (userMessage.equalsIgnoreCase("bye")) break;
+            System.out.println(userMessage);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
