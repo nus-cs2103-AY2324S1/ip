9025138549979a46dce670656049480b0085965c
vishnu <vishnuprasath639@gmail.com>
2023-08-23 15:43:39 +0800

    * Make bot echo user input * Make bot terminate with "bye" command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,42 @@
+import java.util.Scanner;
+
 public class Duke {
 
     /**
      * Draws a line separating each conversation.
+     *
      */
     public static void drawLine() {
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
+    }
+
+
+    /**
+     * Handles input from the user accordingly.
+     *
+     */
+    public static void handleUserInput() {
+        String userInput;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        userInput = scanner.nextLine();
+        drawLine();
+        while (!userInput.equals("bye")) {
+            System.out.println("\t" + userInput);
+            drawLine();
+            System.out.println();
+            userInput = scanner.nextLine();
+            drawLine();
+        }
     }
 
     public static void main(String[] args) {
         drawLine();
-        System.out.println("Hello I am Vishnu.");
-        System.out.println("What can I do for you? \n");
+        System.out.println("\tHello I am Vishnu.");
+        System.out.println("\tWhat can I do for you?");
         drawLine();
-        System.out.println("Bye. Hope to see you again soon! \n");
+        handleUserInput();
+        System.out.println("\tBye. Hope to see you again soon!");
         drawLine();
     }
 }
