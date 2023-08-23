a30f24799c4c6d693870f21abaf874445aa5907e
Evelyne Law <lawruixi@gmail.com>
2023-08-23 14:15:05 +0800

    Fixed bug relating to Java unfamiliarity and also added the greetings functions

--- src/main/java/Duchess.java
+++ src/main/java/Duchess.java
@@ -2,7 +2,23 @@ public class Duchess {
     /**
      * Name for the Duchess AI.
      */
-    public static final NAME = "Duchess";
+    public static final String NAME = "Duchess";
+
+    /**
+     * Prints greeting to the user and returns.
+     */
+    private static void printGreeting() {
+        System.out.println("(^O^)／ Heya~");
+        System.out.println("I am ~~Duchess~~!!");
+        System.out.println("What can I do for you!! o_O");
+    }
+
+    /**
+     * Prints farewell/exit message.
+     */
+    private static void printFarewell() {
+        System.out.println("Bye bye!! Hope to see you again (＾▽＾)");
+    }
 
     public static void main(String[] args) {
         // String logo = " ____        _        \n"
@@ -10,6 +26,9 @@ public class Duchess {
         // + "| | | | | | | |/ / _ \\\n"
         // + "| |_| | |_| |   <  __/\n"
         // + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + Duchess.NAME);
+        
+        Duchess.printGreeting();
+        Duchess.printFarewell();
+        
     }
 }
