1f3d1ceb36e46d49c34f35d3941ca036edd58d82
tayian <90464339+tayian@users.noreply.github.com>
2023-08-24 00:19:56 +0800

    Implement feature to greet user then exit

--- src/main/java/Glen.java
+++ src/main/java/Glen.java
@@ -1,11 +1,24 @@
 public class Glen {
+    static final String HORLINE = "____________________________________________________________\n";    
     public static void main(String[] args) {
-        String logo = "  _____ _            \n"
-                + " / ____| |           \n"
-                + "| |  __| | ___ _ __  \n"
-                + "| | |_ | |/ _ \ '_ \ \n"
-                + "| |__| | |  __/ | | |\n"
-                + " \_____|_|\___|_| |_|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.print(intro());
+        System.out.println(exit());
+    }
+
+    static String intro() {
+        String logo = "  _____ _            \n" +
+                      " / ____| |           \n" +
+                      "| |  __| | ___ _ __  \n" +
+                      "| | |_ | |/ _ \\  _ \\ \n" +
+                      "| |__| | |  __/ | | |\n" +
+                      " \\_____|_|\\___|_| |_|\n\n";
+        String introText = "Hello, I'm Glen!\n" + 
+                           "What can I do for you?\n";
+        return HORLINE + logo + introText + HORLINE;
+    }
+
+    static String exit() {
+        String exitTxt = "Bye. Hope to see you again soon!\n";
+        return exitTxt + HORLINE;
     }
 }
