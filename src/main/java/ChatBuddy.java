77c936ed3640f5aa2db09fecdd866d532cff02ce
Goh Li Ting <e0970558@u.nus.edu>
2023-08-23 15:56:07 +0800

    Add ability to echo commands

--- src/main/java/ChatBuddy.java
+++ src/main/java/ChatBuddy.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
 public class ChatBuddy {
     private static void printHorizontalLine() {
-        String horizontalLine = "____________________________________________________________";
+        String horizontalLine = "    ____________________________________________________________";
         System.out.println(horizontalLine);
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         printHorizontalLine();
-        System.out.println("Hello! I'm Chat Buddy!");
-        System.out.println("What can I do for you?");
+        System.out.println("    Hello! I'm Chat Buddy!");
+        System.out.println("    What can I do for you?");
         printHorizontalLine();
-        System.out.println("Bye. Hope to see you again soon!");
+
+        String userInput = scanner.nextLine();
+
+        while (!userInput.equals("bye")) {
+            printHorizontalLine();
+            System.out.println("    " + userInput);
+            printHorizontalLine();
+            userInput = scanner.nextLine();
+        }
+
+        printHorizontalLine();
+        System.out.println("    Bye. Hope to see you again soon!");
         printHorizontalLine();
     }
 }
