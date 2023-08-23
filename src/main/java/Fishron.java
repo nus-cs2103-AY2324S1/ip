916a81fda29c76314ccdbeaec2d0e135f7d121b0
jingyu987 <jingyu2987@gmail.com>
2023-08-23 19:40:03 +0800

    Level 3. Mark as Done Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done.

--- src/main/java/Fishron.java
+++ src/main/java/Fishron.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Fishron {
     private String name;
-    private String[] storage = new String[100];
+    private Task[] storage = new Task[100];
     private int taskCount = 0;
 
     public Fishron(String name) {
@@ -18,20 +18,48 @@ public class Fishron {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    public void addTask(String task) {
+    public void addTask(Task task) {
         storage[taskCount] = task;
         taskCount++;
         System.out.println("____________________________________________________________");
-        System.out.println("added: " + task);
+        System.out.println("added: " + task.getDescription());
         System.out.println("____________________________________________________________");
     }
 
     public void listTasks() {
         System.out.println("____________________________________________________________");
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < taskCount; i++) {
-            System.out.println((i + 1) + ". " + storage[i]);
+            Task task = storage[i];
+            System.out.println((i + 1) + ". " + task.getStatusIcon() + " " + task.getDescription());
         }
         System.out.println("____________________________________________________________");
+    }
+
+    public void markTaskAsDone(int Index) {
+        if (Index >= 1 && Index <= taskCount) {
+            Task task = storage[Index - 1];
+            task.markAsDone();
+            System.out.println("____________________________________________________________");
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("  " + task.getStatusIcon() + " " + task.getDescription());
+            System.out.println("____________________________________________________________");
+        } else {
+            System.out.println("Invalid task number.");
+        }
+    }
+
+    public void unmarkTask(int Index) {
+        if (Index >= 1 && Index <= taskCount) {
+            Task task = storage[Index - 1];
+            task.markAsUndone();
+            System.out.println("____________________________________________________________");
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println("  " + task.getStatusIcon() + " " + task.getDescription());
+            System.out.println("____________________________________________________________");
+        } else {
+            System.out.println("Invalid task number.");
+        }
     }
 
     public static void main(String[] args) {
@@ -47,8 +75,14 @@ public class Fishron {
             input = scanner.nextLine();
             if (input.equalsIgnoreCase("list")) {
                 chatBot.listTasks();
+            } else if (input.toLowerCase().startsWith("mark")) {
+                int taskIndex = Integer.parseInt(input.split(" ")[1]);
+                chatBot.markTaskAsDone(taskIndex);
+            } else if (input.toLowerCase().startsWith("unmark")) {
+                int taskIndex = Integer.parseInt(input.split(" ")[1]);
+                chatBot.unmarkTask(taskIndex);
             } else if (!input.equalsIgnoreCase("bye")) {
-                chatBot.addTask(input);
+                chatBot.addTask(new Task(input));
             }
         } while (!input.equalsIgnoreCase("bye"));
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsUndone() {
+        isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // mark done task with X
+    }
+
+}
