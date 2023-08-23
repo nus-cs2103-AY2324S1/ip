51a9ed294f76d758b0492c4d5ec3e7650cbd0d14
Goh Li Ting <e0970558@u.nus.edu>
2023-08-23 15:30:57 +0800

    Add greet and exit

---
+++ src/main/java/ChatBuddy.java
@@ -0,0 +1,15 @@
+public class ChatBuddy {
+    private static void printHorizontalLine() {
+        String horizontalLine = "____________________________________________________________";
+        System.out.println(horizontalLine);
+    }
+
+    public static void main(String[] args) {
+        printHorizontalLine();
+        System.out.println("Hello! I'm Chat Buddy!");
+        System.out.println("What can I do for you?");
+        printHorizontalLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontalLine();
+    }
+}
