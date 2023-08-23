ccf2bde821633288b984f10e6005594fe004d27d
Richie <hsiehrichie246@gmail.com>
2023-08-23 15:33:22 +0800

    Completed Level 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,56 +24,69 @@ public class Duke {
         return str;
     }
 
-    private static void handleMarking(int index, String status) {
+    private static void handleMarking(int index, String status) throws DukeException {
         if (index > store.size() || index < 0) {
-            System.out.println(formatOutput("Invalid Index!"));
-        } else {
-            Task selectedTask = store.get(index);
-            if (status.equals("mark")) {
-                selectedTask.markTask();
-                System.out.println(formatOutput("Nice! I've marked the task as done:\n   " +
-                        INDENTATION + selectedTask));
-            } else if (status.equals("unmark")) {
-                selectedTask.unmarkTask();
-                System.out.println(formatOutput("OK, I've marked this task as not done yet:\n   " +
-                        INDENTATION + selectedTask));
-            }
+            throw new DukeException("Invalid Task Index provided!");
         }
+        Task selectedTask = store.get(index);
+        if (status.equals("mark")) {
+            selectedTask.markTask();
+            System.out.println(formatOutput("Nice! I've marked the task as done:\n   " +
+                    INDENTATION + selectedTask));
+        } else if (status.equals("unmark")) {
+            selectedTask.unmarkTask();
+            System.out.println(formatOutput("OK, I've marked this task as not done yet:\n   " +
+                    INDENTATION + selectedTask));
+        }
+
     }
 
-    private static void handleToDo(String task) {
+    private static void handleToDo(String task) throws DukeException {
+
+        if (task.length() == 0) {
+            throw new DukeException("Hey! The task description cannot be empty!");
+        }
         ToDo item = new ToDo(task);
         store.add(item);
         System.out.println(formatOutput("Got it. I've added this task:\n   " +
                 INDENTATION + item + "\n " + INDENTATION + "Now you have " +
-                store.size() +  " tasks in the list."));
+                store.size() + " tasks in the list."));
     }
 
-    private static void handleDeadline(String task) {
+    private static void handleDeadline(String task) throws DukeException {
+        if (task.length() == 0) {
+            throw new DukeException("Hey! The task description cannot be empty!");
+        }
         String[] arr = task.split(" /by ");
         if (arr.length != 2) {
-            System.out.println(formatOutput("Invalid Deadline!"));
-            return;
+            throw new DukeException("Hey, the Deadline given is Invalid! " +
+                    "Make sure that you follow this format:\n" +
+                    INDENTATION + "'taskDescription /by time'");
         }
         Deadline item = new Deadline(arr[0], arr[1]);
         store.add(item);
         System.out.println(formatOutput("Got it. I've added this task:\n   " +
                 INDENTATION + item + "\n " + INDENTATION + "Now you have " +
-                store.size() +  " tasks in the list."));
+                store.size() + " tasks in the list."));
     }
 
-    private static void handleEvent(String task) {
+    private static void handleEvent(String task) throws DukeException {
+        if (task.length() == 0) {
+            throw new DukeException("Hey! The task description cannot be empty!");
+        }
         String[] arr = task.split(" /from ");
         if (arr.length != 2) {
-            System.out.println(formatOutput("Invalid Event!"));
-            return;
+            throw new DukeException("Hey, the Event given is Invalid!" +
+                    " Make sure that you follow this format:\n" +
+                    INDENTATION + " 'eventDescription /from startTime /to endTime'");
         }
         String desc = arr[0];
 
         String[] startEnd = arr[1].split(" /to ");
         if (startEnd.length != 2) {
-            System.out.println("Invalid Start / End!");
-            return;
+            throw new DukeException("Hey, the Event given is Invalid!" +
+                    " Make sure that you follow this format:\n" +
+                    INDENTATION + " 'eventDescription /from startTime /to endTime'");
         }
         String start = startEnd[0];
         String end = startEnd[1];
@@ -82,7 +95,13 @@ public class Duke {
         store.add(item);
         System.out.println(formatOutput("Got it. I've added this task:\n   " +
                 INDENTATION + item + "\n " + INDENTATION + "Now you have " +
-                store.size() +  " tasks in the list."));
+                store.size() + " tasks in the list."));
+    }
+
+    private static void checkCommandArguments(String[] commandArr) throws DukeException {
+        if (commandArr.length == 1) {
+            throw new DukeException("Hey, the description of a " + commandArr[0] + " cannot be empty!");
+        }
     }
 
     private static void handleCommand() {
@@ -93,27 +112,46 @@ public class Duke {
 
         while (true) {
             commandString = sc.nextLine();
+
+            if (commandString.length() == 0) {
+                System.out.println(formatOutput("Hey, Type Something!"));
+                continue;
+            }
+
             commandArray = commandString.split(" ", 2);
             command = commandArray[0];
 
-            if (command.equals("bye")) {
-                System.out.println(formatOutput("Bye. Hope to see you again soon!"));
-                break;
-            } else if (command.equals("list")) {
-                System.out.println(formatOutput(formatList(store)));
-            } else if (command.equals("mark") || command.equals("unmark")) {
-                commandIndex = Integer.parseInt(commandArray[1]) - 1;
-                handleMarking(commandIndex, command);
-            } else if (command.equals("todo")) {
-                handleToDo(commandArray[1]);
-            } else if (command.equals("deadline")) {
-                handleDeadline(commandArray[1]);
-            } else if (command.equals("event")) {
-                handleEvent(commandArray[1]);
-            }
-            else {
-                store.add(new Task(commandString));
-                System.out.println(formatOutput("added: " + commandString));
+            try {
+                if (command.equals("bye")) {
+                    System.out.println(formatOutput("Bye. Hope to see you again soon!"));
+                    break;
+                } else if (command.equals("list")) {
+                    System.out.println(formatOutput(formatList(store)));
+                    continue;
+                }
+
+                if (command.equals("mark") || command.equals("unmark")) {
+                    checkCommandArguments(commandArray);
+                    commandIndex = Integer.parseInt(commandArray[1]) - 1;
+                    handleMarking(commandIndex, command);
+                } else if (command.equals("todo")) {
+                    checkCommandArguments(commandArray);
+                    handleToDo(commandArray[1]);
+                } else if (command.equals("deadline")) {
+                    checkCommandArguments(commandArray);
+                    handleDeadline(commandArray[1]);
+                } else if (command.equals("event")) {
+                    checkCommandArguments(commandArray);
+                    handleEvent(commandArray[1]);
+                } else if (command.equals("blah")) {
+                    System.out.println(formatOutput("I don't understand what you're saying."));
+                } else {
+                    store.add(new Task(commandString));
+                    System.out.println(formatOutput("added: " + commandString));
+                }
+
+            } catch (DukeException e) {
+                System.out.println(formatOutput(e.getMessage()));
             }
         }
 
