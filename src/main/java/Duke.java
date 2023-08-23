abea017d205deecdd55d392c6c10721037de941c
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-08-23 13:26:37 +0800

    Rename bot and add basic greetings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        final String NAME = "Sisyphus";
+        final String HORIZONTAL_LINE = "_________________________________";
+        System.out.println("Hello! I'm " + NAME);
+        System.out.println("What can I do for you");
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Bye! Hope to see you again soon!");
+
     }
 }
