60aaf1084aa401f278285929c4c37bd59f8bcd6e
junnengsoo <soojunneng01@gmail.com>
2023-08-23 11:25:29 +0800

    Add delete function and modify array to ArrayList

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,18 +5,20 @@ import Task.Todo;
 import Exception.UnknownCommandException;
 import Exception.EmptyDescriptionException;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void filterInput(String[] words) throws UnknownCommandException, EmptyDescriptionException {
         // Only allows commands that are listed below
         String command = words[0];
-        boolean validCommand = command.equals("bye") || command.equals("todo") || command.equals("deadline") || command.equals("event")
-                || command.equals("list") || command.equals("mark") || command.equals("unmark");
+        boolean validCommand2Words = command.equals("todo") || command.equals("deadline") || command.equals("event") ||
+                command.equals("mark") || command.equals("unmark");
+        boolean validCommand1Word = command.equals("bye") || command.equals("list") || command.equals("delete");
 
-        if (!validCommand) {
+        if (!validCommand2Words && !validCommand1Word) {
             throw new UnknownCommandException();
-        } else if (words.length < 2) {
+        } else if (validCommand2Words && words.length < 2) {
             throw new EmptyDescriptionException();
         }
     }
@@ -34,7 +36,7 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
         String input;
 
-        Task[] tasks = new Task[100];
+        ArrayList<Task> tasks = new ArrayList<>();
         int taskIndex = 0;
 
         while (true) {
@@ -50,33 +52,46 @@ public class Duke {
 
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("delete")) {
+                int index = Integer.parseInt(words[1]) - 1;
+                Task removedTask = tasks.remove(index); // Removes and retrieves the task from the list
+                System.out.println(line +
+                        "Noted. I've removed this task:\n  " +
+                        removedTask +
+                        "\nNow you have " +
+                        tasks.size() +
+                        " tasks in the list.\n" +
+                        line);
             } else if (words[0].equals("todo")) {
-                tasks[taskIndex] = new Todo(words[1]);
+                Task newTask = new Todo(words[1]);
+                tasks.add(newTask);
                 taskIndex++;
                 System.out.println(line + "Got it. I've added this task:\n  " +
-                        tasks[taskIndex - 1] +
+                        newTask +
                         "\nNow you have " +
                         taskIndex +
                         " tasks in the list.\n" +
                         line);
             } else if (words[0].equals("deadline")) {
                 String[] parts = words[1].split(" /by ");
-                tasks[taskIndex] = new Deadline(parts[0], parts[1]);
+                Task newTask = new Deadline(parts[0], parts[1]);
+                tasks.add(newTask);
                 taskIndex++;
                 System.out.println(line +
                         "Got it. I've added this task:\n  " +
-                        tasks[taskIndex - 1] +
+                        newTask +
                         "\nNow you have " +
                         taskIndex +
                         " tasks in the list.\n" + line);
             } else if (words[0].equals("event")) {
                 String[] parts = words[1].split(" /from "); // second part will consist the timings
                 String[] times = parts[1].split(" /to ");
-                tasks[taskIndex] = new Event(parts[0], times[0], times[1]);
+                Task newTask = new Event(parts[0], times[0], times[1]);
+                tasks.add(newTask);
                 taskIndex++;
                 System.out.println(line +
                         "Got it. I've added this task:\n  " +
-                        tasks[taskIndex - 1] +
+                        newTask +
                         "\nNow you have " +
                         taskIndex +
                         " tasks in the list.\n" +
@@ -84,28 +99,28 @@ public class Duke {
             } else if (input.equals("list")) {
                 System.out.println(line + "\nHere are the tasks in your list:");
                 for (int i = 0; i < taskIndex; i++) {
-                    System.out.println((i + 1) + "." + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks.get(i));
                 }
                 System.out.println(line);
             } else if (words[0].equals("mark") && words.length > 1) {
                 int index = Integer.parseInt(words[1]) - 1; // string -> int
-                tasks[index].markDone();
+                Task taskToMark = tasks.get(index); // Get the task from the list
+                taskToMark.markDone(); // Mark the task as done
+                tasks.set(index, taskToMark); // Update the task in the list
                 System.out.println(line + "\nNice! I've marked this task as done:\n  " +
-                        tasks[index] +
+                        taskToMark +
                         "\n" +
                         line);
             } else if (words[0].equals("unmark") && words.length > 1) {
                 int index = Integer.parseInt(words[1]) - 1; // string -> int
-                tasks[index].unmarkDone();
+                Task taskToUnmark = tasks.get(index); // Get the task from the list
+                taskToUnmark.unmarkDone(); // Mark the task as done
+                tasks.set(index, taskToUnmark); // Update the task in the list
                 System.out.println(line +
                         "\nOK, I've marked this task as not done yet:\n  " +
-                        tasks[index] +
+                        taskToUnmark +
                         "\n" +
                         line);
-            } else {
-                tasks[taskIndex] = new Task(input);
-                taskIndex++;
-                System.out.println(line + "added: " + input + "\n" + line);
             }
         }
 
