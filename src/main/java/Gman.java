f97f9559aac1d5eb836bcc5e75eeaadb429201a5
glenngnng <glennng12@gmail.com>
2023-08-23 18:05:38 +0800

    Added ability to store whatever text entered by user and display them back to user when requested.

--- src/main/java/Gman.java
+++ src/main/java/Gman.java
@@ -1,17 +1,27 @@
 import java.util.Scanner;
 public class Gman {
     public static String userInput;
+    public static String inputList[] = new String[100];
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Hello! I'm Gman! \nWhat can I do for you?");
-        userInput = myScanner.nextLine();
         String exitWord = "bye";
+        userInput = myScanner.nextLine();
 
-        while (!userInput.equals(exitWord)) {
-            System.out.println("    " + userInput);
+        int counter = 0;
+        while (!userInput.equals(exitWord) && counter < 100) {
+            if (userInput.equals("list") && counter != 0) {
+                for (int i = 0; i < counter; i++) {
+                    System.out.println(inputList[i]);
+                }
+            } else {
+                String toAdd = String.valueOf(counter + 1) + ". " + userInput;
+                inputList[counter] = toAdd;
+                System.out.println("    added: " + userInput);
+                counter++;
+            }
             userInput = myScanner.nextLine();
         }
-
         System.out.println("    Bye. Hope to see you again soon!");
     }
 }
