003cdc15cf717852d30316f8cc7f4ea572027bd3
thienmy0 <111404222+thienmy0@users.noreply.github.com>
2023-08-23 12:09:49 +0800

    Level 2

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Bob {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Bob");
         System.out.println("What can I do for you?");
+
+        ArrayList<String> list = new ArrayList<String>();
 
         while (true) {
             Scanner obj = new Scanner(System.in);
@@ -12,8 +15,13 @@ public class Bob {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
+            } else if (input.equals("list")) {
+                for (int i = 1; i <= list.size(); i++) {
+                    System.out.println(i + ". " + list.get(i - 1));
+                }
             } else {
-                System.out.println(input);
+                list.add(input);
+                System.out.println("added: " + input);
             }
         }
     }
