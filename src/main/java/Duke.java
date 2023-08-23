945d4f64184723d499abb1c1b84d4f9178781118
Jie Xiang <n.jiexiang@gmail.com>
2023-08-23 21:22:09 +0800

    Add delete Task feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,12 +14,13 @@ public class Duke {
             + "  bye\n"
             + "  list\n"
             + "  mark|unmark <index>\n"
+            + "  delete <index>\n"
             + "  todo <description>\n"
             + "  deadline <description> /by <date>\n"
             + "  event <description> /from <start> /to <end>\n";
     private static final String invalidMarkMessage = "Great heavens! The index of mark cannot be empty!\n"
             + "Usage: mark <index>\n";
-    private static final String invalidUnmarkMessage = "Great heavens! The index of mark cannot be empty!\n"
+    private static final String invalidUnmarkMessage = "Great heavens! The index of unmark cannot be empty!\n"
             + "Usage: unmark <index>\n";
     private static final String invalidTodoMessage = "Great heavens! The description of todo cannot be empty!\n"
             + "Usage: todo <description>\n";
@@ -27,14 +28,17 @@ public class Duke {
             + "Usage: deadline <description> /by <date>\n";
     private static final String invalidEventMessage = "Great heavens! Invalid usage of event!\n"
             + "Usage: event <description> /from <start> /to <end>\n";
+    private static final String invalidDeleteMessage = "Great heavens! The index of delete cannot be wmpty!\n"
+            + "Usage: delete <index>\n";
     private static final String markDoneMessage = "Great success! I have marked this task as done:\n";
     private static final String markUndoneMessage = "Very nice! I have marked this task as not done yet:\n";
     private static final String listMessage = "Here are the tasks in your list:\n";
-    private static final String regexPattern = "(bye|list|unmark|mark|todo|deadline|event)\\s*" // match command
+    private static final String regexPattern = "\\b(bye|list|unmark|mark|todo|deadline|event|delete)\\s*" // match command
             + "([^/]*[^/\\s])?\\s*"                     // match chars that are not / after command, trimming whitespace
             + "(?:(/by|/from)\\s+([^/]*[^/\\s]))?\\s*"  // match /by or /from command and argument, trimming whitespace
             + "(?:(/to)\\s+([^/]*[^/\\s]))?\\s*";       // match /to command and argument, trimming whitespace
     private static final String newTaskAddedMessage = "New task just dropped!\n";
+    private static final String taskDeletedMessage = "Task went on vacation, never came back.\n";
     private static final String totalTaskCountMessage = "You now have %d tasks in the list!\n";
 
     private static void printWithLines(String message) {
@@ -51,6 +55,13 @@ public class Duke {
 
     private static void printTaskAdded(Task task, int totalTasks) {
         System.out.print(line + newTaskAddedMessage);
+        System.out.print("  " + task + "\n");
+        System.out.printf(totalTaskCountMessage, totalTasks);
+        System.out.print(line);
+    }
+
+    private static void printTaskDeleted(Task task, int totalTasks) {
+        System.out.print(line + taskDeletedMessage);
         System.out.print("  " + task + "\n");
         System.out.printf(totalTaskCountMessage, totalTasks);
         System.out.print(line);
@@ -146,6 +157,20 @@ public class Duke {
                         newTask = new Event(matcher.group(2), matcher.group(4), matcher.group(6));
                         tasks.add(newTask);
                         printTaskAdded(newTask, tasks.size());
+                        break;
+                    case "delete":
+                        if (matcher.group(2) == null) {
+                            throw new DukeException(line + invalidDeleteMessage + line);
+                        }
+
+                        index = Integer.parseInt(matcher.group(2));
+
+                        if (index < 1 || index > tasks.size()) {
+                            throw new DukeException(line + invalidIndexMessage + line);
+                        }
+
+                        Task removed = tasks.remove(index - 1);
+                        printTaskDeleted(removed, tasks.size());
                         break;
                     default:
                         throw new DukeException(line + invalidInputMessage + line);
