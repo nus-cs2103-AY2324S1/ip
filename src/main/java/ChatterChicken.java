e19899ff10e632fa289211f8d8e938135c841a6a
tllshan <shantsou03@gmail.com>
2023-08-24 00:11:24 +0800

    Implemented Level 0. Rename, Greet, Exit
    Renamed Duke to ChatterChicken.
    Added greeting and exit.

---
+++ src/main/java/ChatterChicken.java
@@ -0,0 +1,10 @@
+public class ChatterChicken {
+    public static void main(String[] args) {
+        String line = "_____________________________________________________________________________";
+        System.out.println(line
+            + "\nHello! I'm ChatterChicken!\nWhat can I do for you?\n"
+            + line
+            + "\nBye. Hope to see you again soon!\n"
+            + line);
+    }
+}
