49bc09335020e4072e49de4ac39c600fe17d0307
jannnice <>
2023-08-23 21:00:43 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,35 @@
+package main.java;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static final String horizontalLine = "    ____________________________________________________________\n";
-    public static void main(String[] args) {
+    public static void greet() {
         System.out.println(horizontalLine
                 + "     Hello! I'm POPOOH\n"
                 + "     What can I do for you?\n"
                 + horizontalLine);
+    }
+    public static void exit() {
         System.out.println("     Bye. Hope to see you again soon!\n"
                 + horizontalLine);
+    }
+    public static void printCommand(String command) {
+        if (Objects.equals(command, "bye")) {
+            exit();
+        } else {
+            System.out.println(horizontalLine
+                    + "     " + command + "\n"
+                    + horizontalLine);
+        }
+    }
+    public static void main(String[] args) {
+        greet();
+        Scanner input = new Scanner(System.in);
+
+        while (input.hasNext()) {
+            String command = input.nextLine();
+            printCommand(command);
+        }
     }
 }
