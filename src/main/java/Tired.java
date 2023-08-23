a1a0df876fa0213058cc8dfab37bb4207e7fcc5d
chewjh1234 <111339807+chewjh1234@users.noreply.github.com>
2023-08-23 17:17:53 +0800

    Add Level-1

--- src/main/java/Tired.java
+++ src/main/java/Tired.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Tired {
    public static void main(String[] args) {
 
@@ -7,8 +8,19 @@ public class Tired {
        System.out.println(horizontalLine);
        System.out.println("Hello! I'm " + name);
        System.out.println("What can I do for you?");
-       System.out.println(horizontalLine);
+       System.out.println(horizontalLine + "\n");
 
+       Scanner sc = new Scanner(System.in);
+       String input = sc.nextLine();
+
+       while (!input.equals("bye")) {
+           System.out.println(horizontalLine);
+           System.out.println(input);
+           System.out.println(horizontalLine + "\n");
+           input = sc.nextLine();
+       }
+
+       System.out.println(horizontalLine);
        System.out.println("Bye. Hope to see you again soon!");
        System.out.println(horizontalLine);
    }
