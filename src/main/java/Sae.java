91e1dc3cdcacd81188327b6f73ff42c06f1e98f8
Saezenn <teng.hxa@gmail.com>
2023-08-24 01:39:33 +0800

    Level-3, Mark as Done

--- src/main/java/Sae.java
+++ src/main/java/Sae.java
@@ -1,28 +1,91 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
+/**
+ * The Sae class represents an interactive task manager.
+ * Users can add, list, mark, and unmark tasks using this program.
+ */
 public class Sae {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Sae\nWhat can I do for you?");
 
-        ArrayList<String> store = new ArrayList<>();
+        ArrayList<Task> store = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
 
         while (true) {
-            Scanner input = new Scanner(System.in);
             String str = input.nextLine();
-            if (str.equals("bye")) {
+            String[] commandTask = str.split(" ", 2);
+
+            String command = commandTask[0];
+
+            if (command.equals("bye")) {
                 break;
-            }
-            else if (str.equals("list")) {
-                int len = store.size();
-                for (int i = 1; i < len + 1; i++) {
-                    System.out.println(i + ". " + store.get(i - 1));
-                }
-            }
-            else {
-                store.add(str);
-                System.out.println("added: " + str);
+            } else if (command.equals("list")) {
+                listTasks(store);
+            } else if (command.equals("mark")) {
+                markTask(store, commandTask);
+            } else if (command.equals("unmark")) {
+                unmarkTask(store, commandTask);
+            } else {
+                addTask(store, str);
             }
         }
+
+        input.close();
+
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    /**
+     * Lists all tasks in the store along with their statuses.
+     *
+     * @param store The ArrayList containing the tasks.
+     */
+    private static void listTasks(ArrayList<Task> store) {
+        int len = store.size();
+        for (int i = 0; i < len; i++) {
+            Task curr = store.get(i);
+            System.out.println((i + 1) + ".[" + curr.getStatusIcon() + "] " + curr.getDescription());
+        }
+    }
+
+    /**
+     * Marks a task as done based on the user's input.
+     *
+     * @param store        The ArrayList containing the tasks.
+     * @param commandTask  The user's input split into command and content.
+     */
+    private static void markTask(ArrayList<Task> store, String[] commandTask) {
+        int number = Integer.parseInt(commandTask[1]);
+        Task curr = store.get(number - 1);
+        curr.markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("[" + curr.getStatusIcon() + "] " + curr.getDescription());
+    }
+
+    /**
+     * Unmarks a task as done based on the user's input.
+     *
+     * @param store        The ArrayList containing the tasks.
+     * @param commandTask  The user's input split into command and content.
+     */
+    private static void unmarkTask(ArrayList<Task> store, String[] commandTask) {
+        int number = Integer.parseInt(commandTask[1]);
+        Task curr = store.get(number - 1);
+        curr.unmarkAsDone();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("[" + curr.getStatusIcon() + "] " + curr.getDescription());
+    }
+
+    /**
+     * Adds a new task to the storage.
+     *
+     * @param store The ArrayList containing the tasks.
+     * @param str   The user's input representing the task.
+     */
+    private static void addTask(ArrayList<Task> store, String str) {
+        Task curr = new Task(str);
+        store.add(curr);
+        System.out.println("added: " + str);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,49 @@
+/**
+ * The Task class represents a task with a description and a status indicating whether it's done or not.
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Constructs a new task with the given description. By default, the task is marked as not done.
+     *
+     * @param description The description of the task.
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Returns the status icon of the task. The icon is "X" if the task is done, or a space if it's not done.
+     *
+     * @return The status icon of the task.
+     */
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    /**
+     * Returns the description of the task.
+     *
+     * @return The description of the task.
+     */
+    public String getDescription() {
+        return this.description;
+    }
+
+    /**
+     * Marks the task as done by setting its status to true.
+     */
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    /**
+     * Unmarks the task as done by setting its status to false.
+     */
+    public void unmarkAsDone() {
+        this.isDone = false;
+    }
+}
