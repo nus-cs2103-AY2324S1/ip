5b3bc2f71f48e0d6a77b4ca9b99de8d6c3596be8
YangHeng Tang <yanghengtang@gmail.com>
2023-08-24 00:47:43 +0800

    Add ToDo.java

---
+++ src/main/java/ToDo.java
@@ -0,0 +1,11 @@
+public class ToDo extends Task {
+    public ToDo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        String status = "[" + (super.isDone ? "X" : " ") + "]";
+        return "[T]" + status + " " + super.description;
+    }
+}
