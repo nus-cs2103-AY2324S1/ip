2fd606edb722856f84c2610ac80b8d0fda74eb64
s0ngyang <keesongyang4@gmail.com>
2023-08-23 22:11:50 +0800

    Added list

--- src/main/java/Nexus.java
+++ src/main/java/Nexus.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -6,10 +8,19 @@ public class Nexus {
         System.out.println("Hello! I'm NEXUS");
         System.out.println("What can I do for you?");
         Scanner scanner = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
         while (true) {
             String input = scanner.nextLine();
             if (Objects.equals(input, "bye")) break;
-            System.out.println(input);
+            if (Objects.equals(input, "list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.print(i + 1);
+                    System.out.println(". " + list.get(i));
+                }
+            } else {
+                list.add(input);
+                System.out.println("added: " + input);
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
