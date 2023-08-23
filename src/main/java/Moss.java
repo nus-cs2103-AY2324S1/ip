def15047cbfd90dcf8973cbd5fe6511fc1328213
heran lang <namedx9@gmail.com>
2023-08-24 01:54:18 +0800

    add Moss.java, remove Duke.java

---
+++ src/main/java/Moss.java
@@ -0,0 +1,11 @@
+public class Moss {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+                + "Hello! I'm Moss \n"
+                + "What can I do for you? \n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(logo);
+    }
+}
