5e7db755627af945303b5a36167afa5402cac3f8
kokseng1 <ngkokseng02@gmail.com>
2023-08-23 19:01:01 +0800

    lvl2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,13 +3,23 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String[] tasks = new String[100];
         String name = "Chaty";
+        int taskNo = 0;
+
         System.out.println("Hello! I'm " + name + "\n" +
                 "What can I do for you?" + "\n\n");
-        String echo = scan.nextLine();
-        while (!echo.equals("bye")) {
-            System.out.println(echo);
-            echo = scan.nextLine();
+        String next = scan.nextLine();
+        while (!next.equals("bye")) {
+            if (next.equals("list")) {
+                for (int i = 0; i < taskNo ; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }} else {
+                    tasks[taskNo] = next;
+                    taskNo++;
+                    System.out.println("added: " + next);
+            }
+            next = scan.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
