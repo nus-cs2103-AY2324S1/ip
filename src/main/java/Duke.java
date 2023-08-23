804e070822564e664b1cf78bae26f8806125e76d
adhigop13 <adhigop13@gmail.com>
2023-08-23 12:03:46 +0800

    Added echo function lvl1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;  // Import the Scanner class
 public class Duke {
     public static void main(String[] args) {
+        String endMessage = "Bye. Hope to see you again soon!";
+        String endVal = "bye";
+        String horizontalLine = "-".repeat(22);
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm Tom!" + "\n"  +  "What can I do for you?" + "\n");
+        System.out.println(horizontalLine);
 
-        System.out.println("Hello! I'm Tom!" + "\n"  +  "What can I do for you?" + "\n" + "\n" + "Bye. Hope to see you again soon!");
+        while (true) {
+            Scanner userInputObject = new Scanner(System.in);
+            String userInput = userInputObject.nextLine();
+            if (userInput.equals(endVal)) {
+                System.out.println("\n" + endMessage);
+                break;
+            } else {
+                System.out.println(horizontalLine);
+                System.out.println(userInput);
+                System.out.println(horizontalLine);
+
+            }
+        }
     }
 }
