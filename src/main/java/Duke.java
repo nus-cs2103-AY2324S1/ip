786f20edc99613ae08f46ac0362177338273fd6e
brendan <brenstack62@gmail.com>
2023-08-23 21:04:04 +0800

    Add skeleton for Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
     public static void main(String[] args) {
+        /**
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+         */
+        String intro_message = "____________________________________________________________\n"
+                + " Hello! I'm Bob the Chatbot!\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n";
+
+        String bye_message = " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+
+        System.out.println(intro_message);
+        System.out.println(bye_message);
+
     }
 }
