ef7b798314c2a8a54e206f52b8ffdb4403a491b8
techjay-c <>
2023-08-23 18:51:06 +0800

    Duke Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
 
 // Solution below inspired by https://stackoverflow.com/questions/47150081/while-loop-for-multiple-inputs
-// Solution below inspired by ChatGPT, to solve the issue in the else block of incrementing the num_items counter to add the new item subsequently.
-/* Solution below inspired by ChatGPT, to solve the issue for the command list, to show the separators only at the start and end, by moving
+// Solution below inspired by ChatGPT, to solve the issue in the else block of incrementing the num_items counter to add
+// the new item subsequently.
+/* Solution below inspired by ChatGPT, to solve the issue for the command list, to show the separators only at the start
+and end, by moving
 the statement outside the for loop
+ */
+// Solution inspired by ChatGPT, regarding the method .contains("mark"), as it might be a bug if the input contains mark,
+// without a corresponding integer.
+// Solution below inspired by https://stackoverflow.com/questions/12973871/read-multiple-user-input-words-and-split-them
+// Solution below inspired by ChatGPT, employed its help to solve my indexing problem. (ie mark 1 and unmark 1 refers to
+// diff items)
+/* Solution below inspired by ChatGPT, employed its help to solve the list not updating and showing the items with their
+correct status icon, by creating a new task array of tasks instead of a string array.
  */
 
 public class Duke {
@@ -17,7 +27,7 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String user_text = sc.nextLine();
-        String[] stringArray = new String[100];
+        Task[] tasks = new Task[100];
         int num_items = 0;
 
         while (!user_text.isEmpty()) {
@@ -26,12 +36,39 @@ public class Duke {
                 break;
             } else if (user_text.equals("list")) {
                 System.out.println(separators);
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < num_items; i++) {
-                    System.out.println((i + 1) + ". " + " " + stringArray[i]);
+                    System.out.println((i + 1) + "." + "[" + tasks[i].getStatusIcon() + "] " + tasks[i].description);
                 }
                 System.out.println(separators);
+            } else if (user_text.contains("mark")) {
+                String[] split_commands = user_text.split(" ");
+
+                if (split_commands.length == 2) {
+                    int index_toChange = 1;
+                    if (split_commands[0].equals("mark")) {
+                        index_toChange = Integer.parseInt(split_commands[1]);
+                        tasks[index_toChange - 1].markAsDone();
+                        System.out.println(separators);
+                        System.out.println("Nice! I've marked this task as done:" + "\n" + "[" +
+                                tasks[index_toChange - 1].getStatusIcon() + "] " + tasks[index_toChange - 1].description);
+                        System.out.println(separators);
+                    } else {
+                        index_toChange = Integer.parseInt(split_commands[1]);
+                        tasks[index_toChange - 1].markAsNotDone();
+                        System.out.println(separators);
+                        System.out.println("OK, I've marked this task as not done yet:" + "\n" + "[" +
+                                tasks[index_toChange - 1].getStatusIcon() + "] " + tasks[index_toChange - 1].description);
+                        System.out.println(separators);
+                    }
+                } else {
+                    // only contains the word mark
+                    tasks[num_items] = new Task(user_text);
+                    num_items++;
+                    System.out.println(separators + "\n" + "added: " + user_text + "\n" + separators + "\n");
+                }
             } else {
-                stringArray[num_items] = user_text;
+                tasks[num_items] = new Task(user_text);
                 num_items++;
                 System.out.println(separators + "\n" + "added: " + user_text + "\n" + separators + "\n");
             }
