57e062d13b3a4eb9eaa50095ae507a7c8818dffe
techjay-c <>
2023-08-23 15:04:55 +0800

    Duke Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String text1 = " Hello! I'm Novo\n"
-                +  " What can I do for you?";
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n";
         String text2 = " Bye. Hope to see you again soon!";
         System.out.println("____________________________________________________________\n"
-                + text1 + "\n"
-                + "____________________________________________________________\n"
-                + text2 + "\n"
-                + "____________________________________________________________\n"
-        );
+                + text1);
+
+        Scanner sc = new Scanner(System.in);
+        String user_text = sc.nextLine();
+
+        while (!user_text.isEmpty()) {
+            if (user_text.equals("bye")) {
+                System.out.println("____________________________________________________________\n"
+                        + text2 + "\n"
+                        + "____________________________________________________________\n");
+                break;
+            } else {
+                System.out.println("____________________________________________________________\n"
+                        + user_text + "\n"
+                        + "____________________________________________________________\n");
+            }
+            user_text = sc.nextLine();
+        }
     }
 }
