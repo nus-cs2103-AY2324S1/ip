505812c237e94be4865ee300c42042a81ce045d2
VN-Hao <111359733+VN-Hao@users.noreply.github.com>
2023-08-23 23:19:10 +0800

    echo commands

--- src/main/java/Qi.java
+++ src/main/java/Qi.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
 public class Qi {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Qi");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Qi");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        System.out.println();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String str = sc.nextLine();
+            if (str.equals("bye")) {
+                break;
+            }
+            System.out.println("    ____________________________________________________________");
+            System.out.println("     " + str);
+            System.out.println("    ____________________________________________________________");
+            System.out.println();
+        }
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 }
