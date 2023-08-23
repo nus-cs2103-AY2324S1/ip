5cfb43f4c06869ecf8326f13acbbde06df2e541d
javinchua <javin.chua@gmail.com>
2023-08-23 16:25:43 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
 
     public static void main(String[] args) {
+        ArrayList<String> store = new ArrayList<>();
         String name = "CodeBuddy";
         String horizontal = "----------------------------------------";
         Scanner inputScanner = new Scanner(System.in);
@@ -17,8 +19,16 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon !");
                 System.out.println(horizontal);
                 break;
+            } else if(input.equals("list")) {
+                int counter = 1;
+                for(String text: store) {
+                    System.out.println(counter + ". " + text);
+                    counter++;
+                }
+            } else {
+                System.out.println("added: " + input);
+                store.add(input);
             }
-            System.out.println(input);
             System.out.println(horizontal);
         }
     }
