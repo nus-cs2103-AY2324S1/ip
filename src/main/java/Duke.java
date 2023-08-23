0a2faf577151d0af6325ca8474916a3fa5ec04cc
saraozn <saraongzining@gmail.com>
2023-08-23 15:43:52 +0800

    feat: Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -6,8 +7,17 @@ public class Duke {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
+        String inData = "";
+        Scanner scan = new Scanner(System.in);
         System.out.println("Hello! I'm Nicole");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon");
+        while (!inData.equals("bye")) {
+            inData = scan.nextLine();
+            if (inData.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon");
+                break;
+            }
+            System.out.println(inData);
+        }
     }
 }
