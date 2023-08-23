459db77a1012750d247be084a6fd7a813de38325
winson8222 <winson8222@hotmail.com>
2023-08-23 17:12:13 +0800

    Level0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = "Hello! I'm chatbot\nWhat can I do for you?";
+        String bye = "Bye. Hope to see you again soon!";
+        System.out.println(greeting);
+        System.out.println(bye);
+
     }
 }
