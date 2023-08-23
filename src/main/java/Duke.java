558b8d0cac3fae088bc045d919385b9414427ca8
brendan <brenstack62@gmail.com>
2023-08-23 21:39:13 +0800

    Add list functionality for Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,24 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
-    public static void echo(String message) {
-        String echoed = "\t____________________________________________________________\n"
-                + "\t " + message + "\n"
+    public static ArrayList<String> messages = new ArrayList<>();
+
+    public static void addToList(String message) {
+        String addMessage = "\t____________________________________________________________\n"
+                + "\t Added: " + message + "\n"
                 + "\t____________________________________________________________\n";
-        System.out.println(echoed);
+        messages.add(message);
+        System.out.println(addMessage);
+    }
+
+    public static void printList() {
+        System.out.println("\t____________________________________________________________");
+        for (int i = 0; i < messages.size(); i++) {
+            System.out.println("\t " + (i + 1) + ". " + messages.get(i));
+        }
+        System.out.println("\t____________________________________________________________");
     }
 
     public static void main(String[] args) {
@@ -18,6 +30,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
          */
+
         String intro_message = "\t____________________________________________________________\n"
                 + "\t Hello! I'm Bob the Chatbot!\n"
                 + "\t What can I do for you?\n"
@@ -34,7 +47,12 @@ public class Duke {
         while (true) {
             message = sc.nextLine();
             if (message.equals("bye")) break;
-            echo(message);
+
+            if (message.equals("list")) {
+                printList();
+            } else {
+                addToList(message);
+            }
         }
 
         System.out.println(bye_message);
