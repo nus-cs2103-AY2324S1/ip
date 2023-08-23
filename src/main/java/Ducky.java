a389cc6894b54ff79eea701dfe29de7616e5357b
chew01 <chewjh19@gmail.com>
2023-08-24 01:32:45 +0800

    update storage to use arraylist

--- src/main/java/Ducky.java
+++ src/main/java/Ducky.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Ducky {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Storage store = new Storage(100);
+        Storage store = new Storage();
         CommandParser parser = new CommandParser();
         UserInterface ui = new UserInterface(sc, store, parser);
         ui.start();
--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,45 +1,44 @@
+import java.util.ArrayList;
+
 public class Storage {
-    private final Task[] arr;
-    private int idx;
-    public Storage(int size) {
-        this.arr = new Task[size];
-        this.idx = 0;
+    private final ArrayList<Task> tasks;
+    public Storage() {
+        this.tasks = new ArrayList<>();
     }
 
     public void add(Task t) {
-        arr[idx] = t;
-        this.idx++;
+        tasks.add(t);
     }
 
     public void mark(int index) throws InvalidTaskIndexException {
-        if (index < 0 || index >= this.idx) {
-            throw new InvalidTaskIndexException(index + 1, this.idx);
+        if (index < 0 || index >= this.tasks.size()) {
+            throw new InvalidTaskIndexException(index + 1, this.tasks.size());
         }
-        this.arr[index].complete();
+        this.tasks.get(index).complete();
     }
 
     public void unmark(int index) throws InvalidTaskIndexException {
-        if (index < 0 || index >= this.idx) {
-            throw new InvalidTaskIndexException(index + 1, this.idx);
+        if (index < 0 || index >= this.tasks.size()) {
+            throw new InvalidTaskIndexException(index + 1, this.tasks.size());
         }
-        this.arr[index].incomplete();
+        this.tasks.get(index).incomplete();
     }
 
     public String getTaskDescription(int index) {
-        return this.arr[index].toString();
+        return this.tasks.get(index).toString();
     }
 
     public int getStorageSize() {
-        return idx;
+        return this.tasks.size();
     }
 
     public String list() {
         StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < idx; i++) {
-            if (i == idx - 1) {
-                builder.append(String.format("%d.%s", i + 1, this.arr[i]));
+        for (int i = 0; i < tasks.size(); i++) {
+            if (i == tasks.size() - 1) {
+                builder.append(String.format("%d.%s", i + 1, this.tasks.get(i)));
             } else {
-                builder.append(String.format("%d.%s\n", i + 1, this.arr[i]));
+                builder.append(String.format("%d.%s\n", i + 1, this.tasks.get(i)));
             }
         }
         return builder.toString();
