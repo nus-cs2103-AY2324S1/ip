1937758873255ed90ed658f8b96f5cf5edac4047
Nabonita Sen <nabonita.sonali.sen@gmail.com>
2023-08-23 18:40:10 +0800

    Add, List function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> taskList = new ArrayList<>();
+
         String lineSpacer = "________________________________________";
         String logo = "   / \\__\n"
                 + "  (    @\\___\n"
@@ -22,9 +25,17 @@ public class Duke {
                 System.out.println("Goodbye! Have a paw-some day :-)");
                 System.out.println(lineSpacer);
                 break;
+            } else if (userInput.equals("list")) {
+                System.out.println(lineSpacer);
+                for (int i = 0; i < taskList.size(); i++ ) {
+                    int index = i + 1;
+                    System.out.println((i + 1) + ". " + taskList.get(i));
+                }
+                System.out.println(lineSpacer);
             } else {
                 System.out.println(lineSpacer);
-                System.out.println(userInput);
+                taskList.add(userInput);
+                System.out.println("added: " + userInput);
                 System.out.println(lineSpacer);
             }
         }
