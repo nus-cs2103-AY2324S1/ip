02362df3ee1bd96d425091d823cb48a39c26db89
Daphne789 <daphneshaine933@gmail.com>
2023-08-23 19:14:55 +0800

    Completed Level-0

---
+++ src/main/java/EchoBot.java
@@ -0,0 +1,13 @@
+public class EchoBot {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        String horizontalLine = "_____________________________________________\n";
+        System.out.println(horizontalLine + "Hello! I'm EchoBot\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine + "Bye. Hope to see you again soon!\n" + horizontalLine);
+    }
+}
