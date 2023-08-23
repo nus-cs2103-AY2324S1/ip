e3c248c809987c4488f6c9d74abe2ff31c882eec
awhb <awhangbin@gmail.com>
2023-08-23 13:04:37 +0800

    Week 2 - Duke Level-0

---
+++ src/main/java/ChadBod.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class ChadBod {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm ChadBod.\n");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
