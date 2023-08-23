6ebd049836439efb6a8e40121e79d3f79a3da7b4
JCSnap <e0970167@u.nus.edu>
2023-08-23 13:06:41 +0800

    added Events class

---
+++ src/main/java/Events.java
@@ -0,0 +1,27 @@
+public class Events extends Task {
+    private String from;
+    private String to;
+    public Events(String name, String from, String to) {
+        super(name);
+        this.from = from;
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return this.from;
+    }
+
+    public String getTo() {
+        return this.to;
+    }
+
+    @Override
+    public String getSymbol() {
+        return "T";
+    }
+
+    @Override
+    public String getDescription() {
+        String description = String.format("%s (from: %s to: %s)", super.getName(), from, to);
+    }
+}
