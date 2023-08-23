81ca101a5f7372caaa593571808352ad7359498c
Jie Xiang <n.jiexiang@gmail.com>
2023-08-23 21:06:10 +0800

    Add exceptions handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,13 +9,24 @@ public class Duke {
                                                 + "What can I do for you?\n";
     private static final String exitMessage = "Bye! Hope to see you again soon!\n";
     private static final String invalidIndexMessage = "Great heavens! You have entered an invalid index!\n";
-    private static final String invalidInputMessage = "Great heavens! You have entered an invalid input! Valid commands:\n"
-            + "bye\n"
-            + "list\n"
-            + "mark|unmark <index>\n"
-            + "todo <name>\n"
-            + "deadline <name> /by <date>\n"
-            + "event <name> /from <start> /to <end>\n";
+    private static final String invalidInputMessage = "Great heavens! You have entered an invalid input!\n"
+            + "Here are the valid commands:\n"
+            + "  bye\n"
+            + "  list\n"
+            + "  mark|unmark <index>\n"
+            + "  todo <description>\n"
+            + "  deadline <description> /by <date>\n"
+            + "  event <description> /from <start> /to <end>\n";
+    private static final String invalidMarkMessage = "Great heavens! The index of mark cannot be empty!\n"
+            + "Usage: mark <index>\n";
+    private static final String invalidUnmarkMessage = "Great heavens! The index of mark cannot be empty!\n"
+            + "Usage: unmark <index>\n";
+    private static final String invalidTodoMessage = "Great heavens! The description of todo cannot be empty!\n"
+            + "Usage: todo <description>\n";
+    private static final String invalidDeadlineMessage = "Great heavens! Invalid usage of deadline!\n"
+            + "Usage: deadline <description> /by <date>\n";
+    private static final String invalidEventMessage = "Great heavens! Invalid usage of event!\n"
+            + "Usage: event <description> /from <start> /to <end>\n";
     private static final String markDoneMessage = "Great success! I have marked this task as done:\n";
     private static final String markUndoneMessage = "Very nice! I have marked this task as not done yet:\n";
     private static final String listMessage = "Here are the tasks in your list:\n";
@@ -52,100 +63,95 @@ public class Duke {
         printWithLines(greetingMessage);
 
         while (true) {
-            String str = sc.nextLine();
-            Matcher matcher = pattern.matcher(str);
-            int index;
-            Task newTask;
+            try {
+                String str = sc.nextLine();
+                Matcher matcher = pattern.matcher(str);
+                int index;
+                Task newTask;
 
-            if (!matcher.matches()) {
-                // no match means input is not valid
-                printWithLines(invalidInputMessage);
-                continue;
-            }
+                if (!matcher.matches()) {
+                    // no match means input is not valid
+                    throw new DukeException(line + invalidInputMessage + line);
+                }
 
-            if (matcher.group(1).equals("bye")) {
-                printWithLines(exitMessage);
-                break;
-            }
-
-            switch (matcher.group(1)) {
-                case "list":
-                    printList(tasks);
+                if (matcher.group(1).equals("bye")) {
+                    printWithLines(exitMessage);
                     break;
-                case "mark":
-                    if (matcher.group(2) == null) {
-                        printWithLines(invalidInputMessage);
+                }
+
+                switch (matcher.group(1)) {
+                    case "list":
+                        printList(tasks);
                         break;
-                    }
+                    case "mark":
+                        if (matcher.group(2) == null) {
+                            throw new DukeException(line + invalidMarkMessage + line);
+                        }
 
-                    index = Integer.parseInt(matcher.group(2));
+                        index = Integer.parseInt(matcher.group(2));
 
-                    if (index < 1 || index > tasks.size()) {
-                        printWithLines(invalidIndexMessage);
+                        if (index < 1 || index > tasks.size()) {
+                            throw new DukeException(line + invalidIndexMessage + line);
+                        }
+
+                        tasks.get(index - 1).markAsDone();
+                        printWithLines(markDoneMessage + "  " + tasks.get(index - 1) + '\n');
                         break;
-                    }
+                    case "unmark":
+                        if (matcher.group(2) == null) {
+                            throw new DukeException(line + invalidUnmarkMessage + line);
+                        }
 
-                    tasks.get(index - 1).markAsDone();
-                    printWithLines(markDoneMessage + "  " + tasks.get(index - 1) + '\n');
-                    break;
-                case "unmark":
-                    if (matcher.group(2) == null) {
-                        printWithLines(invalidInputMessage);
+                        index = Integer.parseInt(matcher.group(2));
+
+                        if (index < 1 || index > tasks.size()) {
+                            throw new DukeException(line + invalidIndexMessage + line);
+                        }
+
+                        tasks.get(index - 1).markAsUndone();
+                        printWithLines(markUndoneMessage + "  " + tasks.get(index - 1) + '\n');
                         break;
-                    }
+                    case "todo":
+                        if (matcher.group(2) == null) {
+                            throw new DukeException(line + invalidTodoMessage + line);
+                        }
 
-                    index = Integer.parseInt(matcher.group(2));
-
-                    if (index < 1 || index > tasks.size()) {
-                        printWithLines(invalidIndexMessage);
+                        newTask = new Todo(matcher.group(2));
+                        tasks.add(newTask);
+                        printTaskAdded(newTask, tasks.size());
                         break;
-                    }
+                    case "deadline":
+                        if (matcher.group(2) == null
+                                || matcher.group(3) == null
+                                || !matcher.group(3).equals("/by")
+                                || matcher.group(4) == null) {
+                            throw new DukeException(line + invalidDeadlineMessage + line);
+                        }
 
-                    tasks.get(index - 1).markAsUndone();
-                    printWithLines(markUndoneMessage + "  " + tasks.get(index - 1) + '\n');
-                    break;
-                case "todo":
-                    if (matcher.group(2) == null) {
-                        printWithLines(invalidInputMessage);
+                        newTask = new Deadline(matcher.group(2), matcher.group(4));
+                        tasks.add(newTask);
+                        printTaskAdded(newTask, tasks.size());
                         break;
-                    }
+                    case "event":
+                        if (matcher.group(2) == null
+                                || matcher.group(3) == null
+                                || !matcher.group(3).equals("/from")
+                                || matcher.group(4) == null
+                                || matcher.group(5) == null
+                                || !matcher.group(5).equals("/to")
+                                || matcher.group(6) == null) {
+                            throw new DukeException(line + invalidEventMessage + line);
+                        }
 
-                    newTask = new Todo(matcher.group(2));
-                    tasks.add(newTask);
-                    printTaskAdded(newTask, tasks.size());
-                    break;
-                case "deadline":
-                    if (matcher.group(2) == null
-                            || matcher.group(3) == null
-                            || !matcher.group(3).equals("/by")
-                            || matcher.group(4) == null) {
-                        printWithLines(invalidInputMessage);
+                        newTask = new Event(matcher.group(2), matcher.group(4), matcher.group(6));
+                        tasks.add(newTask);
+                        printTaskAdded(newTask, tasks.size());
                         break;
-                    }
-
-                    newTask = new Deadline(matcher.group(2), matcher.group(4));
-                    tasks.add(newTask);
-                    printTaskAdded(newTask, tasks.size());
-                    break;
-                case "event":
-                    if (matcher.group(2) == null
-                            || matcher.group(3) == null
-                            || !matcher.group(3).equals("/from")
-                            || matcher.group(4) == null
-                            || matcher.group(5) == null
-                            || !matcher.group(5).equals("/to")
-                            || matcher.group(6) == null) {
-                        printWithLines(invalidInputMessage);
-                        break;
-                    }
-
-                    newTask = new Event(matcher.group(2), matcher.group(4), matcher.group(6));
-                    tasks.add(newTask);
-                    printTaskAdded(newTask, tasks.size());
-                    break;
-                default:
-                    printWithLines(invalidInputMessage);
-                    break;
+                    default:
+                        throw new DukeException(line + invalidInputMessage + line);
+                }
+            } catch (DukeException e) {
+                System.out.print(e.getMessage());
             }
         }
     }
