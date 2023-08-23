2c81870e09f0f6325b6ecd3eb021faf6cfa2d8fc
AlainS87 <1950834052@qq.com>
2023-08-23 17:32:33 +0800

    level3 completed

--- src/main/java/ChatbotAlain.java
+++ src/main/java/ChatbotAlain.java
@@ -1,9 +1,10 @@
 import java.lang.reflect.Array;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class ChatbotAlain {
     public static void main(String[] args) {
-        String[] list = new String[200];
+        Task[] list = new Task[200];
         int pos = 0;
         String renameAndGreeting = "____________________________________________________________\n"
                 + " Hello! I'm Alain\n"
@@ -14,10 +15,28 @@ public class ChatbotAlain {
             Scanner s = new Scanner(System.in);
             String text = new String();
             text = s.nextLine();
+            boolean isMatchMark = Pattern.matches("mark\\d+", text);
+            boolean isMatchUnmark = Pattern.matches("unmark\\d+", text);
             if (text.equals("bye")) {
                 break;
-            } else if (! text.equals("list")){
-                list[pos] = text;
+            } else if (isMatchMark) {
+                String numericPart = text.substring(4);
+                list[Integer.parseInt(numericPart) -1].markAsDone();
+                String output = "____________________________________________________________\n"
+                        + " Nice! I've marked this task as done:\n"
+                        + "   [X]" +  list[Integer.parseInt(numericPart) - 1].getName() + "\n"
+                        + "____________________________________________________________\n";
+                System.out.println(output);
+            }else if (isMatchUnmark) {
+                String numericPart = text.substring(6);
+                list[Integer.parseInt(numericPart) - 1].markAsUndone();
+                String output = "____________________________________________________________\n"
+                        + " OK, I've marked this task as not done yet:\n"
+                        + "   [ ]" +  list[Integer.parseInt(numericPart) - 1].getName() + "\n"
+                        + "____________________________________________________________\n";
+                System.out.println(output);
+            }else if (! text.equals("list")){
+                list[pos] = new Task(text);
                 pos ++;
                 String output = "____________________________________________________________\n"
                         + " " + text + "\n"
@@ -26,9 +45,10 @@ public class ChatbotAlain {
 
             } else {
                 String output = "";
-                output += "____________________________________________________________\n";
+                output += "____________________________________________________________\n"
+                 + "Here are the tasks in your list:\n";
                 for (int i = 0; i < pos; i++ ) {
-                    output += " " + (i + 1) + ". " + list[i] + "\n";
+                    output += " " + (i + 1) + ". " + list[i].getStatusIcon() + list[i].getName() + "\n";
                 }
                 output += "____________________________________________________________\n";
                 System.out.println(output);
@@ -37,7 +57,7 @@ public class ChatbotAlain {
         String endingLine = "____________________________________________________________\n"
                 + " Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
-        
+
         System.out.println(endingLine);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    protected String name;
+    protected boolean isDone;
+
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // mark done task with X
+    }
+
+    public void markAsDone(){
+        this.isDone = true;
+    }
+
+    public void markAsUndone(){
+        this.isDone = false;
+    }
+}
