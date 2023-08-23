0d995481b3f3f443836d5cf30451545b5c80a9bf
chew01 <chewjh19@gmail.com>
2023-08-23 19:03:46 +0800

    add std input loop

--- src/main/java/Ducky.java
+++ src/main/java/Ducky.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Ducky {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Ducky\nWhat can I do for you?");
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
