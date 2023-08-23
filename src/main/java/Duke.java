9ce6f4151155a97500d288de9ec3a035380e1ab6
mingyu <wanmingyu0318@gmail.com>
2023-08-23 17:50:17 +0800

    A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Duke  {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
 
         MYBot MYBot = new MYBot("MYBot");
         MYBot.openGreeting();
