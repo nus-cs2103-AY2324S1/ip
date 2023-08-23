fcf9b5e6786ec4fba445ea84d4c91da4c4dd2f30
JasonLCY-Temp <e0407197@u.nus.edu>
2023-08-23 21:41:09 +0800

    Update Jeeves class: Change taskList scope
    To avoid unnecessary code duplication when implementing task marking
    and unmarking, the task printing function has been moved to a separate
    method.
    
    The scope of taskList has also been changed to be a class-level member
    in order for the program to properly function.

--- src/main/java/Jeeves.java
+++ src/main/java/Jeeves.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
  * Contains the main method and primary logic for Jeeves.
  */
 public class Jeeves {
+    // Due to how the taskCount variable is used as the id and
+    // array access position, index 0 will always be unused.
+    // taskList is effectively 1-indexed
+    private static final Task[] taskList = new Task[100];
 
     /**
      * Main process.
@@ -16,11 +20,6 @@ public class Jeeves {
         System.out.println("Greetings, Master. Jeeves at your service");
         System.out.println("How may I serve you today?\n");
 
-        // Due to how the taskCount variable is used as the id and
-        // array access position, index 0 will always be unused.
-        // taskList is effectively 1-indexed
-        Task[] taskList = new Task[100];
-
         // Waits for user input and process it accordingly
         while (true) {
             Scanner sc = new Scanner(System.in);
@@ -31,11 +30,12 @@ public class Jeeves {
             // a specific pre-defined command is received.
             switch (currentCommand) {
             case "list":
-                // Displays the current list of tasks tracked
+                // Displays the current list of tasks tracked and their status
                 for (int i = 1; i <= Task.getTaskCount(); i++) {
-                    System.out.println(taskList[i].getId() + ". "
-                        + taskList[i].getDesc());
+                    Jeeves.displayTask(i);
                 }
+                // Prints an empty line for aesthetic reasons
+                System.out.print("\n");
                 break;
             case "bye":
                 // Displays the farewell message and terminates the application
@@ -48,6 +48,20 @@ public class Jeeves {
                 System.out.println("Task added: " + currentCommand + "\n");
                 break;
             }
+        }
+    }
+
+    private static void displayTask(int id) {
+        // Checks if a task has its status marked as complete
+        // and displays the appropriate visual
+        if (taskList[id].getStatus()) {
+            System.out.println(id + "."
+                    + "[X] "
+                    + taskList[id].getDesc());
+        } else {
+            System.out.println(id + "."
+                    + "[ ] "
+                    + taskList[id].getDesc());
         }
     }
 }
