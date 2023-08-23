9202336c69ac0b832451398417d8c1341ee3e01a
emzm2023 <meeselevels@elizabeths-MacBook-Air-3.local>
2023-08-23 16:13:23 +0800

    edited code in Chatbot.java and implemented semi-automated testing.

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -20,11 +20,12 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin Chatbot < input.txt > ACTUAL.TXT
 
-# convert to UNIX format
+## convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+dos2unix ACTUAL.TXT
+dos2unix EXPECTED-UNIX.TXT
 
 # compare the output to the expected output
 diff ACTUAL.TXT EXPECTED-UNIX.TXT
