073e653030bed8d15b1c00da608e656c459c375e
rocketninja7 <andrewyapp@gmail.com>
2023-08-23 23:01:42 +0800

    Refactored code using enums

---
+++ src/main/java/Command.java
@@ -0,0 +1,26 @@
+public enum Command {
+    BYE ("bye"),
+    LIST ("list"),
+    MARK ("mark"),
+    UNMARK ("unmark"),
+    TODO ("todo"),
+    DEADLINE ("deadline"),
+    EVENT ("event"),
+    DELETE ("delete"),
+    INVALID ("");
+
+    private final String command;
+
+    Command(String command) {
+        this.command = command;
+    }
+
+    public static Command commandEnum(String command) {
+        for(Command currCmd: values()){
+            if(currCmd.command.equals(command)){
+                return currCmd;
+            }
+        }
+        return null;
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -31,9 +31,6 @@ public class Duke {
 
         while(true) {
             String command = scanner.nextLine().strip();
-            if(command.equalsIgnoreCase("bye")){
-                break;
-            }
             System.out.println(HORIZONTAL_LINE);
             try {
                 if (command.isEmpty()) {
@@ -43,97 +40,115 @@ public class Duke {
                     while (commandEndIndex < command.length() && command.charAt(commandEndIndex) != ' ') {
                         commandEndIndex++;
                     }
-                    String commandName = command.substring(0, commandEndIndex).toLowerCase();
-                    String commandArgs = command.substring(commandEndIndex).strip();
-                    if (commandName.equals("list")) {
-                        if (!commandArgs.isEmpty()) {
-                            throw new CommandArgumentException("list does not take in arguments!");
+                    String commandNameStr = command.substring(0, commandEndIndex).toLowerCase();
+                    Command commandName = Command.commandEnum(commandNameStr);
+                    if(commandName == null){
+                        System.out.println("Error: " + commandNameStr + " is not a valid command!");
+                    }
+                    else {
+                        String commandArgs = command.substring(commandEndIndex).strip();
+                        if (command.equalsIgnoreCase("bye")) {
+                            System.out.println("Bye. Hope to see you again soon!");
+                            System.out.println(HORIZONTAL_LINE);
+                            break;
                         }
-                        for (int index = 0; index < list.size(); index++) {
-                            System.out.printf("%d. %s\n", index + 1, list.get(index).toString());
+                        String desc;
+                        switch (commandName) {
+                            case LIST:
+                                if (!commandArgs.isEmpty()) {
+                                    throw new CommandArgumentException("list does not take in arguments!");
+                                }
+                                for (int index = 0; index < list.size(); index++) {
+                                    System.out.printf("%d. %s\n", index + 1, list.get(index).toString());
+                                }
+                                break;
+                            case MARK:
+                                int markIndex = checkIndexArg(commandArgs, list.size());
+                                if (markIndex == -1) {
+                                    throw new CommandArgumentException("Invalid task to mark: " + commandArgs);
+                                }
+                                boolean wasNotMarked = list.get(markIndex).markDone();
+                                if (wasNotMarked) {
+                                    System.out.println("Marking task as done: " + list.get(markIndex).toString());
+                                } else {
+                                    System.out.println("Task was already marked as done: " + list.get(markIndex).toString());
+                                }
+                                break;
+                            case UNMARK:
+                                int unmarkIndex = checkIndexArg(commandArgs, list.size());
+                                if (unmarkIndex == -1) {
+                                    throw new CommandArgumentException("Invalid task to unmark: " + commandArgs);
+                                }
+                                boolean wasNotUnmarked = list.get(unmarkIndex).unmarkDone();
+                                if (wasNotUnmarked) {
+                                    System.out.println("Marking task as undone: " + list.get(unmarkIndex).toString());
+                                } else {
+                                    System.out.println("Task was already marked as undone: " + list.get(unmarkIndex).toString());
+                                }
+                                break;
+                            case TODO:
+                                if (commandArgs.isEmpty()) {
+                                    throw new CommandArgumentException("Task description cannot be empty!");
+                                }
+                                list.add(new ToDo(commandArgs));
+                                System.out.println("Added todo item: " + list.get(list.size() - 1));
+                                break;
+                            case DEADLINE:
+                                int byIndex = commandArgs.indexOf("/by");
+                                if (byIndex == -1) {
+                                    throw new CommandArgumentException("Deadline missing a /by argument!");
+                                }
+                                desc = commandArgs.substring(0, byIndex).strip();
+                                String by = commandArgs.substring(byIndex + "/by".length()).strip();
+                                if (desc.isEmpty()) {
+                                    throw new CommandArgumentException("Task description cannot be empty!");
+                                }
+                                if (by.isEmpty()) {
+                                    throw new CommandArgumentException("/by argument cannot be empty!");
+                                }
+                                list.add(new Deadline(desc, by));
+                                System.out.println("Added deadline item: " + list.get(list.size() - 1));
+                                break;
+                            case EVENT:
+                                int fromIndex = commandArgs.indexOf("/from");
+                                int toIndex = commandArgs.indexOf("/to");
+                                if (fromIndex == -1) {
+                                    throw new CommandArgumentException("Deadline missing a /from argument!");
+                                }
+                                if (toIndex == -1) {
+                                    throw new CommandArgumentException("Deadline missing a /to argument!");
+                                }
+                                String from, to;
+                                if (fromIndex < toIndex) {
+                                    desc = commandArgs.substring(0, fromIndex).strip();
+                                    from = commandArgs.substring(fromIndex + "/from".length(), toIndex).strip();
+                                    to = commandArgs.substring(toIndex + "/to".length()).strip();
+                                } else {
+                                    desc = commandArgs.substring(0, toIndex).strip();
+                                    from = commandArgs.substring(fromIndex + "/from".length()).strip();
+                                    to = commandArgs.substring(toIndex + "/to".length(), fromIndex).strip();
+                                }
+                                if (desc.isEmpty()) {
+                                    throw new CommandArgumentException("Task description cannot be empty!");
+                                }
+                                if (from.isEmpty()) {
+                                    throw new CommandArgumentException("/from argument cannot be empty!");
+                                }
+                                if (to.isEmpty()) {
+                                    throw new CommandArgumentException("/to argument cannot be empty!");
+                                }
+                                list.add(new Event(desc, from, to));
+                                System.out.println("Added event item: " + list.get(list.size() - 1));
+                                break;
+                            case DELETE:
+                                int deleteIndex = checkIndexArg(commandArgs, list.size());
+                                if (deleteIndex == -1) {
+                                    throw new CommandArgumentException("Invalid task to delete: " + commandArgs);
+                                }
+                                Task task = list.remove(deleteIndex);
+                                System.out.println("Deleted item: " + task.toString());
+                                break;
                         }
-                    } else if (commandName.equals("mark")) {
-                        int markIndex = checkIndexArg(commandArgs, list.size());
-                        if(markIndex == -1) {
-                            throw new CommandArgumentException("Invalid task to mark: " + commandArgs);
-                        }
-                        boolean wasNotMarked = list.get(markIndex).markDone();
-                        if (wasNotMarked) {
-                            System.out.println("Marking task as done: " + list.get(markIndex).toString());
-                        } else {
-                            System.out.println("Task was already marked as done: " + list.get(markIndex).toString());
-                        }
-                    } else if (commandName.equals("unmark")) {
-                        int unmarkIndex = checkIndexArg(commandArgs, list.size());
-                        if(unmarkIndex == -1) {
-                            throw new CommandArgumentException("Invalid task to unmark: " + commandArgs);
-                        }
-                        boolean wasNotMarked = list.get(unmarkIndex).unmarkDone();
-                        if (wasNotMarked) {
-                            System.out.println("Marking task as undone: " + list.get(unmarkIndex).toString());
-                        } else {
-                            System.out.println("Task was already marked as undone: " + list.get(unmarkIndex).toString());
-                        }
-                    } else if (commandName.equals("todo")) {
-                        if(commandArgs.isEmpty()) {
-                            throw new CommandArgumentException("Task description cannot be empty!");
-                        }
-                        list.add(new ToDo(commandArgs));
-                        System.out.println("Added todo item: " + list.get(list.size() - 1));
-                    } else if (commandName.equals("deadline")) {
-                        int byIndex = commandArgs.indexOf("/by");
-                        if (byIndex == -1) {
-                            throw new CommandArgumentException("Deadline missing a /by argument!");
-                        }
-                        String desc = commandArgs.substring(0, byIndex).strip();
-                        String by = commandArgs.substring(byIndex + "/by".length()).strip();
-                        if(desc.isEmpty()) {
-                            throw new CommandArgumentException("Task description cannot be empty!");
-                        }
-                        if(by.isEmpty()) {
-                            throw new CommandArgumentException("/by argument cannot be empty!");
-                        }
-                        list.add(new Deadline(desc, by));
-                        System.out.println("Added deadline item: " + list.get(list.size() - 1));
-                    } else if (commandName.equals("event")) {
-                        int fromIndex = commandArgs.indexOf("/from");
-                        int toIndex = commandArgs.indexOf("/to");
-                        if (fromIndex == -1) {
-                            throw new CommandArgumentException("Deadline missing a /from argument!");
-                        }
-                        if (toIndex == -1) {
-                            throw new CommandArgumentException("Deadline missing a /to argument!");
-                        }
-                        String desc, from, to;
-                        if (fromIndex < toIndex) {
-                            desc = commandArgs.substring(0, fromIndex).strip();
-                            from = commandArgs.substring(fromIndex + "/from".length(), toIndex).strip();
-                            to = commandArgs.substring(toIndex + "/to".length()).strip();
-                        } else {
-                            desc = commandArgs.substring(0, toIndex).strip();
-                            from = commandArgs.substring(fromIndex + "/from".length()).strip();
-                            to = commandArgs.substring(toIndex + "/to".length(), fromIndex).strip();
-                        }
-                        if(desc.isEmpty()) {
-                            throw new CommandArgumentException("Task description cannot be empty!");
-                        }
-                        if(from.isEmpty()) {
-                            throw new CommandArgumentException("/from argument cannot be empty!");
-                        }
-                        if(to.isEmpty()) {
-                            throw new CommandArgumentException("/to argument cannot be empty!");
-                        }
-                        list.add(new Event(desc, from, to));
-                        System.out.println("Added event item: " + list.get(list.size() - 1));
-                    } else if(commandName.equals("delete")) {
-                        int deleteIndex = checkIndexArg(commandArgs, list.size());
-                        if(deleteIndex == -1) {
-                            throw new CommandArgumentException("Invalid task to delete: " + commandArgs);
-                        }
-                        Task task = list.remove(deleteIndex);
-                        System.out.println("Deleted item: " + task.toString());
-                    } else {
-                        System.out.println("Error: " + commandName + " is not a valid command!");
                     }
                 }
             }
@@ -142,9 +157,5 @@ public class Duke {
             }
             System.out.println(HORIZONTAL_LINE);
         }
-
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(HORIZONTAL_LINE);
     }
 }
