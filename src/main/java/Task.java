fba0f96c0fc0f6f4d7d04e16a27bb0858da16a0b
Rayshawn <rayshawntan124@gmail.com>
2023-08-23 10:29:41 +0800

    Modified ChatBot.java to check for mark and unmark commands; Added instructions to Task.java to include mark and unmark

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,13 +1,43 @@
 public class Task {
     private String task;
+    private boolean done = false;
 
     public Task(String task) {
         this.task = task;
     }
 
+    public void markDone() {
+        if (done) {
+            System.out.println("____________________________________________________________\n" +
+                    " This task is already marked done!\n" +
+                    "____________________________________________________________\n");
+        } else {
+            this.done = true;
+            System.out.println("____________________________________________________________\n" +
+                    " Nice! I've marked this task as done:\n " +
+                    this.toString() +
+                    "\n____________________________________________________________\n");
+        }
+    }
+
+    public void unmarkDone() {
+        if (!done) {
+            System.out.println("____________________________________________________________\n" +
+                    " This task is not done yet!\n" +
+                    "____________________________________________________________\n");
+        } else {
+            this.done = false;
+            System.out.println("____________________________________________________________\n" +
+                    " OK, I've marked this task not done yet:\n " +
+                    this.toString() +
+                    "\n____________________________________________________________\n");
+        }
+        this.done = false;
+    }
+
     @Override
     public String toString() {
-        return this.task;
+        return String.format("[%s] %s", done ? "X" : " ", this.task);
     }
 
 }
--- src/main/java/chatBot.java
+++ src/main/java/chatBot.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
 
-public class chatBot {
+public class ChatBot {
     private static List<Task> list = new ArrayList<>();
 
     public static void main(String[] args) {
@@ -35,14 +35,38 @@ public class chatBot {
                         "____________________________________________________________\n");
                 break;
             default:
-                // Adds ability to store text
-                Task curr = new Task(str);
-                list.add(curr);
-                System.out.println("____________________________________________________________\n" +
-                        " added: " +
-                        str +
-                        "\n" +
-                        "____________________________________________________________\n");
+                if (str.startsWith("mark ")){
+                    String[] temp = str.split(" ");
+                    try {
+                        int num = Integer.parseInt(temp[1]) - 1;
+                        Task curr = list.get(num);
+                        curr.markDone();
+                    } catch(IndexOutOfBoundsException e) {
+                        System.out.println("This task number is not available. Please try again.");
+                    } catch(NumberFormatException e) {
+                        System.out.println("The task number you have keyed in is not an integer, please try again");
+                    }
+                } else if (str.startsWith("unmark ")) {
+                    String[] temp = str.split(" ");
+                    try {
+                        int num = Integer.parseInt(temp[1]) - 1;
+                        Task curr = list.get(num);
+                        curr.unmarkDone();
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println("This task number is not available. Please try again.");
+                    } catch (NumberFormatException e) {
+                        System.out.println("The task number you have keyed in is not an integer, please try again");
+                    }
+                } else {
+                    // Adds ability to store text
+                    Task curr = new Task(str);
+                    list.add(curr);
+                    System.out.println("____________________________________________________________\n" +
+                            " added: " +
+                            str +
+                            "\n" +
+                            "____________________________________________________________\n");
+                }
         }
     }
 
@@ -50,7 +74,7 @@ public class chatBot {
         System.out.println("____________________________________________________________");
         for (int i = 0; i < list.size(); i++) {
             Task temp = list.get(i);
-            String current = String.format(" %d. %s", i + 1, temp.toString());
+            String current = String.format(" %d.%s", i + 1, temp.toString());
             System.out.println(current);
         }
         System.out.println("____________________________________________________________\n");
