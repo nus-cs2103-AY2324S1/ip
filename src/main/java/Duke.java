aab5c4f9b4265ccca8181cbd5e3edefa35442617
jonasongg <jonasong2@gmail.com>
2023-08-23 13:41:57 +0800

    change to use switch case instead of if else

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,27 +13,36 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             String input = scanner.nextLine();
-            if (input.equals("bye")) {
-                System.out.println(HORIZONTAL_LINE + " Bye. Hope to see you again soon!\n" + HORIZONTAL_LINE);
-            } else if (input.equals("list")) {
-                System.out.println(HORIZONTAL_LINE);
-                for (Task task : tasks) {
-                    System.out.println(task.printTask());
+            String command = input.split(" ", 2)[0];
+            switch (command) {
+                case "bye":
+                    System.out.println(HORIZONTAL_LINE + " Bye. Hope to see you again soon!\n" + HORIZONTAL_LINE);
+                    break;
+                case "list":
+                    System.out.println(HORIZONTAL_LINE);
+                    for (Task task : tasks) {
+                        System.out.println(task.printTask());
+                    }
+                    System.out.println(HORIZONTAL_LINE);
+                    break;
+                case "mark": {
+                    Task task = tasks.get(Integer.parseInt(input.split(" ", 2)[1]) - 1);
+                    task.markAsDone();
+                    System.out.println(HORIZONTAL_LINE + " Nice! I've marked this task as done:\n" +
+                            task.printTask() + "\n" + HORIZONTAL_LINE);
+                    break;
                 }
-                System.out.println(HORIZONTAL_LINE);
-            } else if (input.split(" ", 2)[0].equals("mark")) {
-                Task task = tasks.get(Integer.parseInt(input.split(" ", 2)[1]) - 1);
-                task.markAsDone();
-                System.out.println(HORIZONTAL_LINE + " Nice! I've marked this task as done:\n" +
-                        task.printTask() + "\n" + HORIZONTAL_LINE);
-            } else if (input.split(" ", 2)[0].equals("unmark")) {
-                Task task = tasks.get(Integer.parseInt(input.split(" ", 2)[1]) - 1);
-                task.markAsUndone();
-                System.out.println(HORIZONTAL_LINE + " OK, I've marked this task as not done yet:\n" +
-                        task.printTask() + "\n" + HORIZONTAL_LINE);
-            } else {
-                tasks.add(new Task(input));
-                System.out.println(HORIZONTAL_LINE + "added: " + input + "\n" + HORIZONTAL_LINE);
+                case "unmark": {
+                    Task task = tasks.get(Integer.parseInt(input.split(" ", 2)[1]) - 1);
+                    task.markAsUndone();
+                    System.out.println(HORIZONTAL_LINE + " OK, I've marked this task as not done yet:\n" +
+                            task.printTask() + "\n" + HORIZONTAL_LINE);
+                    break;
+                }
+                default:
+                    tasks.add(new Task(input));
+                    System.out.println(HORIZONTAL_LINE + "added: " + input + "\n" + HORIZONTAL_LINE);
+                    break;
             }
         }
     }
