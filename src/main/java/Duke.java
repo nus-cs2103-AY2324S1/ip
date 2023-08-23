23fdff342a74226d6a16155b876c6be2de50d40c
nicholascher <nicholascherkwokhui@gmail.com>
2023-08-23 14:46:32 +0800

    Level-4: Todo, event and deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,9 +2,14 @@ import java.util.Scanner;
 
 public class Duke {
 
+    public static void taskCounter(int counter) {
+        System.out.println("Now you have " + (counter + 1) + " tasks in the list.");
+        System.out.println("___________________________________\n");
+    }
+
     public static void main(String[] args) {
 
-        String name = "Duke";
+        String name = "Duke!!!!!";
         String line = "___________________________________\n";
         String exit = line + "Bye Bye. Hope to see you again soon!\n" + line;
 
@@ -18,22 +23,49 @@ public class Duke {
 
             String input = scan.nextLine();
             String[] arr = input.split(" ", 2);
+            String type = arr[0];
 
             if ("bye".equals(input)) {
                 System.out.println(exit);
                 break;
 
-            } else if (arr[0].equals("mark")) {
+            } else if (type.equals("mark")) {
                 int amt = Integer.parseInt(arr[1].strip()) - 1;
                 System.out.println(line);
                 list[amt].mark();
                 System.out.println(line);
 
-            } else if (arr[0].equals("unmark")) {
+            } else if (type.equals("unmark")) {
                 int amt = Integer.parseInt(arr[1].strip()) - 1;
                 System.out.println(line);
                 list[amt].unMark();
                 System.out.println(line);
+
+            } else if (type.equals("deadline")) {
+                String[] deadline = arr[1].split("/by", 2);
+                Task newTask = new Deadline(deadline[0], deadline[1]);
+                list[counter] = newTask;
+                System.out.println(line);
+                System.out.println("Got it. I've added the Deadline:\n\t" + newTask.toString());
+                taskCounter(counter);
+                counter++;
+
+            } else if (type.equals("event")) {
+                String[] event = arr[1].split("/from |/to ");
+                Task newTask = new Event(event[0], event[1], event[2]);
+                list[counter] = newTask;
+                System.out.println(line);
+                System.out.println("Got it. I've added the Event:\n\t" + newTask.toString());
+                taskCounter(counter);
+                counter++;
+
+            } else if (type.equals("todo")) {
+                Task newTask = new ToDo(arr[1]);
+                list[counter] = newTask;
+                System.out.println(line);
+                System.out.println("Okay! I added a new TODO:\n\t" + newTask.toString());
+                taskCounter(counter);
+                counter++;
 
             } else if ("list".equals(input.strip())) {
                 System.out.println(line + "Here are the tasks in your list:");
@@ -43,10 +75,9 @@ public class Duke {
                 System.out.println(line);
 
             } else {
-                Task newTask = new Task(input);
-                list[counter] = newTask;
-                counter++;
-                System.out.println(line + "added: " + input + "\n" + line);
+                System.out.println(line);
+                System.out.println("That's not a valid action! Try again");
+                System.out.println(line);
             }
         }
     }
