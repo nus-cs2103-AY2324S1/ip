4b5c28f09b96dfafb388d0393945b89644536de7
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2023-08-23 10:48:50 +0800

    Added a new bye msg

--- src/main/java/Ax.java
+++ src/main/java/Ax.java
@@ -1,15 +1,22 @@
 public class Ax {
 
     private static void hoLine() {
+        System.out.println();
         System.out.println("=^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=    =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=    ");
     }
 
-    public static void greet() {
+    private static void greet() {
         System.out.println("\uD83D\uDC4B Greetings!\n" +
                 "\n" +
                 "Ax at your service! I'm not just any chatbot; I'm Ax – your knowledgeable and engaging virtual companion. Whether you're seeking answers, a friendly chat, or a bit of fun, I'm here to make your experience enjoyable and insightful. Don't hesitate to share your thoughts or questions with me. Let's dive in and start our conversation! How can I assist you today, my friend?");
         hoLine();
     }
+
+    private static void bye() {
+        System.out.println("Thank you for your time and I hope you found what you needed!! 🥰");
+        hoLine();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -31,5 +38,6 @@ public class Ax {
                 "                \n";
         System.out.println("Hello from\n" + newLogo);
         greet();
+        bye();
     }
 }
