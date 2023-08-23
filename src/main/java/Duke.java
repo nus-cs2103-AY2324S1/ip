672ce29b4ec1e634bdcf649e45bf8a6aca54d32b
dhruvir29 <dhruvirathod29@gmail.com>
2023-08-24 01:10:04 +0800

    greet and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printHorizontalLine();
+        System.out.println("Hello! I'm Buddy! \n" +
+                "What can I do for you? ");
+        printHorizontalLine();
+        System.out.println("Bye! Hope to see you again soon! ");
+        printHorizontalLine();
+    }
+
+    public static void printHorizontalLine() {
+        System.out.println("________________________________________________");
     }
 }
