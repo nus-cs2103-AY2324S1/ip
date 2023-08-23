235c93c60d1777bcb486c9ffd7c33575d14fe244
marcellaantania <marcellaantaniatan@gmail.com>
2023-08-23 19:25:03 +0800

    integrate Thea with Task class, add mark/unmark function

--- src/main/java/Thea.java
+++ src/main/java/Thea.java
@@ -6,19 +6,30 @@ public class Thea {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        List<String> tasks =  new ArrayList<>();
+        ArrayList<Task> tasks =  new ArrayList<>();
         greet();
-        String command = input.nextLine();
+        String userInput = input.nextLine();
         while (true) {
+            String[] commandWords = userInput.split(" ");
+            String command = commandWords[0];
             if (command.equals("bye")) {
                 exit();
                 break;
             } else if (command.equals("list")) {
                 printList(tasks);
-            } else {
-                add(command, tasks);
+            } else if (command.equals("mark")) {
+                int index = Integer.parseInt(commandWords[1]) - 1;
+                tasks.get(index).markAsDone();
+                System.out.printf("Great job! I've marked this task as done:\n  %s\n", tasks.get(index));
+            } else if (command.equals("unmark")) {
+                int index = Integer.parseInt(commandWords[1]) - 1;
+                tasks.get(index).unmarkAsDone();
+                System.out.printf("Okay, I've marked this task as not done yet:\n  %s\n", tasks.get(index));
             }
-            command = input.nextLine();
+            else {
+                add(new Task(userInput), tasks);
+            }
+            userInput = input.nextLine();
         }
     }
     public static void greet() {
@@ -27,11 +38,11 @@ public class Thea {
     public static void exit() {
         System.out.println("I hope I made your day easier with my service. See you again! >ᴗ<");
     }
-    public static void add(String task, List<String> tasks) {
+    public static void add(Task task, ArrayList<Task> tasks) {
         tasks.add(task);
-        System.out.println("added: " + task);
+        System.out.println("added: " + task.getTaskName());
     }
-    public static void printList(List<String> tasks) {
+    public static void printList(ArrayList<Task> tasks) {
         for (int i = 0; i < tasks.size(); i++) {
             System.out.printf("%d. %s%n", i + 1, tasks.get(i));
         }
