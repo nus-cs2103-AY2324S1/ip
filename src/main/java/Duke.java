62bfa2db44ff2c2ea0a02d77d257544ef9199398
cyaoxuan <chuayaoxuan@gmail.com>
2023-08-23 12:35:10 +0800

    Level-0 implemented. Renamed to Eepy

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "____________________________________________________________";
+        String greet = "Hello! I'm Eepy\nWhat can I do for you?";
+        String exit = "Bye. Hope to see you again soon!";
+
+        System.out.println(line);
+        System.out.println(greet);
+        System.out.println(line);
+        System.out.println(exit);
+        System.out.println(line);
     }
 }
