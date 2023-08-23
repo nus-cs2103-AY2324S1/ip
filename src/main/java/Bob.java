c742a4bf1c973cd90d205f8ab319bbdda04f6ed2
thienmy0 <111404222+thienmy0@users.noreply.github.com>
2023-08-23 12:00:50 +0800

    Level 1

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,12 +1,21 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";*/
         System.out.println("Hello! I'm Bob");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            Scanner obj = new Scanner(System.in);
+            String input = obj.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
+
 }
