7eed96b90c0b0c4d6b9e32b6969ec1a4bfb8bdf9
tllshan <shantsou03@gmail.com>
2023-08-24 00:26:53 +0800

    Implement Level 1. Echo
    Added indentation to lines printed.
    Added scanner to echo user input.
    Exits on "bye" input.

--- src/main/java/ChatterChicken.java
+++ src/main/java/ChatterChicken.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class ChatterChicken {
     public static void main(String[] args) {
-        String line = "_____________________________________________________________________________";
-        System.out.println(line
-            + "\nHello! I'm ChatterChicken!\nWhat can I do for you?\n"
-            + line
-            + "\nBye. Hope to see you again soon!\n"
-            + line);
+        String line = "\n    _____________________________________________________________________________\n    ";
+        System.out.println(line + "Hello! I'm ChatterChicken!\n    What can I do for you?" + line);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while(!input.equals("bye")) {
+            System.out.println(line + input + line);
+            input = sc.nextLine();
+        }
+        System.out.println(line + "Bye. Hope to see you again soon!" + line);
     }
 }
