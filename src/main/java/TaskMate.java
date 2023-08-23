d4d35d4cf3ec230273143ebcccddb3038a15aca4
elhy1999 <ernestliu64@gmail.com>
2023-08-23 17:19:35 +0800

    Changed Chatbot name from Duke to TaskMate. Greets user and exits

---
+++ src/main/java/TaskMate.java
@@ -0,0 +1,12 @@
+public class TaskMate {
+    public static void main(String[] args) {
+        String chatbotName = "TaskMate";
+        String horizontalLine = "--------------------";
+        System.out.println(horizontalLine);
+        System.out.println("Hello I'm " + chatbotName);
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
