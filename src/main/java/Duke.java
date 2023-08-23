254bf9f5119a6a7ede9c6c1901ba5543d84d0dc0
yuxunn <teeyuxun@gmail.com>
2023-08-24 01:30:34 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,18 +2,31 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-            String logo = "____________________________________________________________\n Hello! I'm bob \n What can I do for you? \n ____________________________________________________________";
-            System.out.println(logo);
-            Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0;
+        int listCount = 1;
+        String[] action = new String[100];
+        String logo = "____________________________________________________________\n Hello! I'm bob \n What can I do for you?\n ____________________________________________________________";
+        System.out.println(logo);
+        while (true) {
             String input = scanner.nextLine();
-            while (!input.equals("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println(" " + input);
-                System.out.println("____________________________________________________________");
-                input = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+            if (input.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon! \n ____________________________________________________________");
+                break;
+            } else if (input.equals("list")) {
+                System.out.println(" Here are the tasks in your list:");
+                for (int i = 0; i < counter; i++) {
+                    System.out.println(" " + listCount + ". " + action[i]);
+                    listCount ++;
+                }
+            } else {
+                action[counter] = input;
+                counter++;
+                System.out.println(" added: " + input);
             }
-            System.out.println(" Bye. Hope to see you again soon!");
+
             System.out.println("____________________________________________________________");
         }
+    }
 }
-
