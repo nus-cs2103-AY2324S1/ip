0a8f1fa96e573ff7d3e4e5fa8b314b19c3c56897
jellywaiyan <jellywaiyan@gmail.com>
2023-08-23 10:20:42 +0800

    Level-2

--- src/main/java/Jelly.java
+++ src/main/java/Jelly.java
@@ -1,18 +1,27 @@
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Jelly {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] storage = new String[100];
+        int index = 0;
         System.out.println("Hello! I'm Jelly");
         System.out.println("What can I do for you?");
-        while (sc.hasNext()) {
-            String temp = sc.next();
+        while (sc.hasNextLine()) {
+            String temp = sc.nextLine();
             if (temp.equals("bye")) {
                 System.out.println("Bye, have a nice day!");
                 return;
+            } else if (temp.equals("list")) {
+                for (int i = 0; i < index; i++) {
+                    System.out.println((i + 1) + "." + storage[i]);
+                }
             } else {
-                System.out.println(temp);
+                    storage[index] = temp;
+                    index++;
+                    System.out.println("added: " + temp);
+                }
             }
         }
-    }
 }
