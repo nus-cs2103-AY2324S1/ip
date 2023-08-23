579ac138c877dea80a1998c39eebb3fa023cd7be
chuababyy <altheachuajiaxuan@gmail.com>
2023-08-23 23:27:41 +0800

    Level-2 Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,43 @@
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 public class Duke {
     private static String chatbot = "chuababyy chatbot";
     private static String line = "------------------------";
 
     public static void main(String[] args) {
+        ArrayList fullList = new ArrayList();
+
         System.out.println(line);
         System.out.println("Hello! I'm " + chatbot);
         System.out.println("What can I do for you?");
         System.out.println(line);
 
         Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
 
-        while (!userInput.equals("bye")) {
-            System.out.println(userInput);
-            userInput = scanner.nextLine();
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                break;
+            }
+
+            if (userInput.equals("list")) {
+                for (int i = 1; i <= fullList.size(); i++) {
+                    System.out.println(i + ". " + fullList.get(i - 1));
+                }
+                continue;
+            }
+
+            fullList.add(userInput);
+            System.out.println(line);
+            System.out.println("added: " + userInput);
+            System.out.println(line);
         }
 
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again!");
+        System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
-
 
         scanner.close();
 
