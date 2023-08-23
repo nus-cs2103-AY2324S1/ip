c18c3412730bd68e4f1d8b4104fc2c5951721e9e
VN-Hao <111359733+VN-Hao@users.noreply.github.com>
2023-08-23 22:43:08 +0800

    New chatbot with simple greetings

---
+++ src/main/java/Qi.java
@@ -0,0 +1,10 @@
+public class Qi {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Qi");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
