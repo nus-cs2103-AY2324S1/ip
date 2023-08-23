7c0c5f9f337535c8ad5b1444747ade38a0ee04d8
hcs1203 <solomonrajhannah@gmail.com>
2023-08-24 01:18:41 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("____________________________________________________________\n" +
                 "Hello! I'm CarrotCake\n" +
                 "What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                "____________________________________________________________\n");
+
+        String input = scanner.nextLine();
+
+        while (!input.toLowerCase().equals("bye")) {
+            System.out.println("____________________________________________________________\n" +
+                    input +
+                    "\n____________________________________________________________\n");
+
+            input = scanner.nextLine();
+        }
+
+        System.out.println("____________________________________________________________\n" +
                 "Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n");
     }
