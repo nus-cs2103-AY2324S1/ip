19798a3508fba4addc1fabb7f9d6d6e318a0f662
heran lang <namedx9@gmail.com>
2023-08-24 03:04:10 +0800

    add  Add, List make it a little notebook

--- src/main/java/Moss.java
+++ src/main/java/Moss.java
@@ -1,26 +1,39 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Moss {
+    static ArrayList<String> things = new ArrayList<>();
     public static void main(String[] args) {
+
         String greet = "____________________________________________________________\n"
                 + "Hello! I'm Moss \n"
                 + "What can I do for you? \n"
                 + "____________________________________________________________\n";
         System.out.println(greet);
         Scanner sc = new Scanner(System.in);
-        String message = sc.next();
+        String message = sc.nextLine();
         while (!message.equals("bye")) {
-            echo(message);
-            message = sc.next();
+            if (message.equals("list")){
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < things.size(); i++) {
+                    System.out.println(i+1 + ". " + things.get(i));
+                }
+                System.out.println("____________________________________________________________");
+            }
+            else {
+                add(message);
+            }
+            message = sc.nextLine();
         }
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 
-    public static void echo(String message) {
+    public static void add(String message) {
+        things.add(message);
         System.out.println("____________________________________________________________");
-        System.out.println(message);
+        System.out.println("added: " + message);
         System.out.println("____________________________________________________________");
     }
 }
