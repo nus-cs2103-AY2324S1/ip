7ad0c356efe786fd4220a55a1dc1b786a2d85c17
howenc <xhowenx@gmail.com>
2023-08-23 10:39:59 +0800

    Added greeting and exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
+    }
+
+    private static void greet() {
+        System.out.println("Hello. I am Luxion. \nWhat can I do for you?\n");
+        exit();
+    }
+
+    private static void exit() {
+        System.out.println("Bye. See you soon.");
+    }
+
+    private static void nextCommand(String command) {
+        
     }
 }
