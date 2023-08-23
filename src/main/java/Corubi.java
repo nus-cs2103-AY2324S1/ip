a0b602a58f4356126f7ab9779b18607cb748cba1
lululwtv <edrickhiewlol@gmail.com>
2023-08-24 03:02:44 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Corubi.java
@@ -0,0 +1,11 @@
+public class Corubi {
+    public static void main(String[] args) {
+        String name = "Corubi";
+        String divider = "---------------------------------------------------";
+        System.out.println(divider);
+        System.out.println("Hello! I am " + name + ". \nWhat can I do for you?");
+        System.out.println(divider);
+        System.out.println("Bye!");
+        System.out.println(divider);
+    }
+}
