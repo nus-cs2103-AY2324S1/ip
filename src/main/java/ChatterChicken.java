ab00251ac4651b3e23b59222deb5574aa463da57
tllshan <shantsou03@gmail.com>
2023-08-24 01:11:32 +0800

    Implement Level 2. Add, List
    Added Task and List classes.
    Adds user inputs as tasks to list.
    Prints list on "list" input.

--- src/main/java/ChatterChicken.java
+++ src/main/java/ChatterChicken.java
@@ -1,13 +1,21 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ChatterChicken {
     public static void main(String[] args) {
-        String line = "\n    _____________________________________________________________________________\n    ";
-        System.out.println(line + "Hello! I'm ChatterChicken!\n    What can I do for you?" + line);
+        String line = "\n    _____________________________________________________________________________\n      ";
         Scanner sc = new Scanner(System.in);
+        List list = new List();
+
+        System.out.println(line + "Hello! I'm ChatterChicken!\n      What can I do for you?" + line);
         String input = sc.nextLine();
+
         while(!input.equals("bye")) {
-            System.out.println(line + input + line);
+            if (input.equals("list")) {
+                list.printList();
+            } else {
+                list.addTask(new Task(input));
+            }
             input = sc.nextLine();
         }
         System.out.println(line + "Bye. Hope to see you again soon!" + line);
