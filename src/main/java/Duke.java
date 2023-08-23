6e2942cb4409951f169e39e4f05f494cc8ae8f74
tiongMax <maxtiongcs16@gmail.com>
2023-08-23 21:52:20 +0800

    Attempt on level 1.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+
+    public static String dash = "\t-------------------------------------------------------------";
+
+    public static void welomeMessage() {
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -7,13 +13,38 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
         System.out.println("Hello from\n" + logo);
+        System.out.println(dash);
+        System.out.println("\t Hello! I'm YOLO \n\t What can I do for you? \n");
+        System.out.println(dash);
+        System.out.println();
+    }
 
-        String dash = "-------------------------------------------------------------";
+    public static void farewell() {
 
         System.out.println(dash);
-        System.out.println("Hello! I'm YOLO \nWhat can I do for you? \n");
+        System.out.println("\t Bye. Hope to see you again soon! \n");
         System.out.println(dash);
-        System.out.println("Bye. Hope to see you again soon! \n");
+    }
+
+    public static void echo(String message) {
+
         System.out.println(dash);
+        System.out.println("\t" + message + "\n");
+        System.out.println(dash);
+    }
+
+    public static void main(String[] args) {
+
+        welomeMessage();
+        Scanner sc = new Scanner(System.in);
+        String message = sc.nextLine();
+
+        while (!message.equals("bye")) {
+
+            echo(message);
+            message = sc.nextLine();
+        }
+
+        farewell();
     }
 }
