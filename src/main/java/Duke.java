b606ef65a3fa1280a368e4462b41addc162b49c2
wj331 <e0389091@u.nus.edu>
2023-08-23 22:10:54 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
+    public static String byeGreetings = "Bye. Hope to see you again soon!";
+    public static String name = " ____    __        __       \n"
+            + "|  _ \\   | |  ____ | | __\n"
+            + "| | | |  | | |     | |/ /\n"
+            + "| |_| |  | | |     |   < \n"
+            + "|____/   ___  ____ |_|\\_\\\n";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(greet());
+
+        System.out.println(Duke.byeGreetings);
+    }
+
+    public static String greet() {
+        return "Hello from \n " + Duke.name + "What can I do for you? \n";
     }
 }
