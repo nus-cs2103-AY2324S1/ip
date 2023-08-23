6fd3d98f20d053fbcc8cb750cc3b707c1c7a7b1a
zhengyup <zhengyu@Zhengs-MacBook-Air.local>
2023-08-23 21:32:41 +0800

    implemented skeletal version, changed duke to HEAD and changed logo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "       ___          \n" +
+                "    . -^   `--,      \n" +
+                "   /# =========`-_   \n" +
+                "  /# (--====___====\\ \n" +
+                " /#   .- --.  . --.| \n" +
+                "/##   |  * ) (   * ),\n" +
+                "|##   \\    /\\ \\   / |\n" +
+                "|###   ---   \\ ---  |\n" +
+                "|####      ___)    #|\n" +
+                "|######           ##|\n" +
+                " \\##### ---------- / \n" +
+                "  \\####           (  \n" +
+                "   `\\###          |  \n" +
+                "     \\###         |  \n" +
+                "      \\##        |   \n" +
+                "       \\###.    .)   \n" +
+                "        `======/     ";
+
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I'm HEAD");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
