134905e2bffc8257dbd9ee39f68396aa529d055e
seraphimstreets <edisonsiowxiong@gmail.com>
2023-08-23 19:00:11 +0800

    added deadline functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,48 @@
 import java.util.Scanner;
+
 public class Duke {
+
+    enum TaskType {
+        ToDo ("T"),
+        Deadline ("D"),
+        Event ("E");
+        private final String sr;
+        TaskType(String sr){
+            this.sr = sr;
+        }
+
+    }
+
+    static Scanner sc = new Scanner(System.in);
+    static String[] store = new String[100];
+    static boolean[] completed = new boolean[100];
+    static TaskType[] tt = new TaskType[100];
+    static String[] dl = new String[100];
+    static int counter = 0;
+
+    public static String getString(String[] a, int x, int y){
+        String res = "";
+        for(int i=x;i<y;i++){
+            if(a[i].charAt(0) == '/') res += a[i].substring(1) + ":";
+            else res += a[i];
+            if(i!=y-1) res += " ";
+        }
+        return res;
+    }
+    public static String formatItem(int x){
+        String cBox = "[" + (completed[x] ? "X" : " ") + "] ";
+        String tBox = "[" + tt[x].sr + "]";
+        String taco = dl[x] == null ? "" : " (" + dl[x] + ")";
+        return  tBox + cBox + " " + store[x] + taco;
+    }
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("─────────────────────");
         System.out.println("Hello I'm Robot!");
         System.out.println("What can I do for you?");
         System.out.println("─────────────────────");
 
-        Scanner sc = new Scanner(System.in);
-        String[] store = new String[100];
-        boolean[] completed = new boolean[100];
-        int counter = 0;
+
         while(true){
             String userInput = sc.nextLine();
             String[] splitStr = userInput.split("\\s+");
@@ -24,23 +52,51 @@ public class Duke {
             if(userInput.equals("list")){
                 System.out.println("Here are the tasks in your list:");
                 for(int i=0;i<counter;i++){
-                    System.out.println( Integer.toString(i+1)  + "." + "[" + (completed[i] ? "X" : " ") + "] " + store[i]);
+                     System.out.println(Integer.toString(i+1)  + "." + formatItem(i));
                 }
             }else if(splitStr.length == 2 && splitStr[0].equals("mark")){
-                System.out.println("Nice! I've marked this task as done:");
                 int x = Integer.parseInt(splitStr[1])-1;
                 completed[x] = true;
-                System.out.println( splitStr[1]  + "." + "[" + (completed[x] ? "X" : " ") + "] " + store[x]);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(formatItem(x));
 
             }else if(splitStr.length == 2 && splitStr[0].equals("unmark")){
-                System.out.println("Ok, I've marked this task as not done yet:");
+
                 int x = Integer.parseInt(splitStr[1])-1;
                 completed[x] = false;
-                System.out.println( splitStr[1]  + "." + "[" + (completed[x] ? "X" : " ") + "] " + store[x]);
+                System.out.println("Ok, I've marked this task as not done yet:");
+                System.out.println(formatItem(x));
 
-            }else{
-                store[counter++] = userInput;
-                System.out.println("added: " + userInput);
+            }else if(splitStr[0].equals("todo")){
+                tt[counter] = TaskType.ToDo;
+                store[counter++] = getString(splitStr, 1, splitStr.length);;
+                System.out.println("Got it, I've added this task:");
+                System.out.println(formatItem(counter-1));
+                System.out.println("Now you have " + counter + " tasks in the list.");
+
+            }else if(splitStr[0].equals("deadline")){
+                for(int i=1;i<splitStr.length;i++){
+                    if(splitStr[i].equals("/by")) {
+                        tt[counter] = TaskType.Deadline;
+                        dl[counter] = getString(splitStr, i, splitStr.length);
+                        store[counter++] = getString(splitStr, 1, i);
+                        break;
+                    }
+                }
+                System.out.println(formatItem(counter-1));
+                System.out.println("Now you have " + counter + " tasks in the list.");
+
+            }else if(splitStr[0].equals("event")){
+                for(int i=1;i<splitStr.length;i++){
+                    if(splitStr[i].equals("/from")) {
+                        tt[counter] = TaskType.Event;
+                        dl[counter] = getString(splitStr, i, splitStr.length);
+                        store[counter++] = getString(splitStr, 1, i);
+                        break;
+                    }
+                }
+                System.out.println(formatItem(counter-1));
+                System.out.println("Now you have " + counter + " tasks in the list.");
             }
             System.out.println("─────────────────────");
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    enum TaskType {
+        ToDo,
+        Deadline,
+        Event;
+    }
+    String task;
+    TaskType tt;
+    boolean comp;
+
+}
