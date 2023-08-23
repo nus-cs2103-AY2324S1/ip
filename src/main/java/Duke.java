df306c174739a081d9dda53d195bc94f902eb138
Leb14 <83680341+Leb14@users.noreply.github.com>
2023-08-23 12:19:33 +0800

    Level - 0
    Level - 0 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo  =   "____    ____  ________   ___    ___   __________    _____\n"
+                     + "\\   \\  /   / |  ____  |  |  |   |  |  |  _____  |  / ____|\n"
+                     + " \\   \\/   /  | |    | |  |  |   |  |  |  |___|  |  | (___\n"
+                      + "  \\      /   | |    | |  |  |   |  |  |   ______|  \\ ___ \\ \n"
+                       + "   |    |    | |    | |  |  |   |  |  |  \\  \\           | |\n"
+                       + "   |    |    | |____| |  |  |   |  |  |  | \\  \\     ____) |\n"
+                       + "   |____|    |________|  \\_________/  |__|   \\__\\  |_____/     \n";
+
+        String name = "Yours";
+
+
+        System.out.println("Hello! I'm " + name);
+        System.out.println(logo);
+        System.out.println("____________________________________________________________________________________");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
