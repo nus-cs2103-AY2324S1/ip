b65eb2f445606d9c0627f4f0752c8dcf93b2a08f
thienmy0 <111404222+thienmy0@users.noreply.github.com>
2023-08-23 11:48:30 +0800

    Level 0

---
+++ src/main/java/Bob.java
@@ -0,0 +1,12 @@
+public class Bob {
+    public static void main(String[] args) {
+        /*String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";*/
+        System.out.println("Hello! I'm Bob");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
