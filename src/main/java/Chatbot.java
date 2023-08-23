be54eb62370c7bf4c5f42ebaa63560004397b72e
andytoh1 <e0773488@u.nus.edu>
2023-08-24 07:18:24 +0800

    no message

---
+++ src/main/java/Chatbot.java
@@ -0,0 +1,7 @@
+public class Chatbot {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Chatbot");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
