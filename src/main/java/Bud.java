01d58971dcd33b83bff8d576b000c2d50b919b53
ylyma <amylingyuhui@gmail.com>
2023-08-23 18:21:45 +0800

    echo

--- src/main/java/Bud.java
+++ src/main/java/Bud.java
@@ -1,12 +1,39 @@
+
 public class Bud {
     public static void main(String[] args) {
         String name = "Bud";
-        String line = "---------------------------\n";
-        System.out.println(line
-                + "Hey! It's your buddy, " + name + "!\n" 
-                + "What's up?\n"
+        String line = "-------------------------------------\n";
+        String hello = "    " 
                 + line
+                + "    " 
+                + "Hey! It's your buddy, " + name + "!\n" 
+                + "    " 
+                + "What's up?\n"
+                + "    " 
+                + line;
+        String bye =  "    "  
+                + line
+                + "    " 
                 + "See ya!\n"
-                + line);
+                + "    " 
+                + line;
+        System.out.println(hello);
+        
+        while (true) {
+            String input = System.console().readLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println("    " 
+                        + line
+                        + "    " 
+                        + input
+                        + "\n"
+                        + "    " 
+                        + line);
+        }
+        System.out.println(bye);
+        
+        
     }
 }
