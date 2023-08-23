46c0b8dbd5f63d9f8905921424c9d08841df6ae8
glenngnng <glennng12@gmail.com>
2023-08-23 16:55:43 +0800

    Ranamed, added greeting and exit.

---
+++ src/main/java/Gman.java
@@ -0,0 +1,11 @@
+public class Gman {
+    public static void main(String[] args) {
+        /*String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";*/
+        System.out.println("Hello! I'm Gman! \nWhat can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
