71f36c58af4a6046c4418d36acab76139bdbcf49
Bearypop <looyl2k@gmail.com>
2023-08-24 00:06:07 +0800

    Added Add and List feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         String name = "Beary";
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
 
         System.out.println(String.format("Hello! I'm %s\nWhat can I do for you?", name));
         printLine();
@@ -16,7 +18,17 @@ public class Duke {
                 printLine();
                 break;
             }
-            System.out.println(command);
+
+            if (command.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+                printLine();
+                continue;
+            }
+
+            System.out.println("added: " + command);
+            tasks.add(command);
             printLine();
         }
     }
