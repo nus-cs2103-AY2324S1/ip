71650f6703a1f11b25bf97a39f25a2b214c7a54d
Bearypop <looyl2k@gmail.com>
2023-08-23 23:44:19 +0800

    Added Echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String name = "Beary";
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(String.format("Hello! I'm %s\nWhat can I do for you?", name));
         printLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        printLine();
+
+        while (true) {
+            String command = scanner.nextLine();
+            printLine();
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                printLine();
+                break;
+            }
+            System.out.println(command);
+            printLine();
+        }
     }
+
 
     public static void printLine() {
         System.out.println("____________________________________________________________");
