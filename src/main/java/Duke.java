422af47e2bb190699a84b05f8f48728612ff0d1d
Dominic Ng Jun Yao <e0958779@u.nus.edu>
2023-08-24 00:06:50 +0800

    Allow chatbot to store user input and display them

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,16 +2,26 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String line = "_".repeat(60);
+        String[] tasks = new String[100];
+        int noTasks = 0;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println(line + "\nHello! I'm Dommi\nWhat can I do for you?\n" + line);
         while (true) {
             String userInput = scanner.nextLine();  // Read user input
+            System.out.println(line);
             if (userInput.equals("bye")) {
-                System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
+                System.out.println("Bye. Hope to see you again soon!\n" + line);
                 break;
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < noTasks; i++)
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                System.out.println(line);
+            } else {
+                tasks[noTasks] = userInput;
+                noTasks += 1;
+                System.out.println("added: " + userInput + "\n" + line);
             }
-            System.out.println(line + "\n" + userInput + "\n" + line);
         }
     }
 }
