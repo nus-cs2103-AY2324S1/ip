567dc8023f74a8fae2f3c74e4deeae04c6396c75
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2023-08-23 10:50:45 +0800

    remove old logo

--- src/main/java/Ax.java
+++ src/main/java/Ax.java
@@ -18,11 +18,6 @@ public class Ax {
     }
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         String newLogo = "                \n" +
                 "                \n" +
                 "                \n" +
