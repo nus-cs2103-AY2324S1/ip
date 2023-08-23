d0115ace57eea95eabb13d52f51760e7276adc44
Chow Yuan Jing <zcyuanjing@gmail.com>
2023-08-23 18:02:15 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String linesep = "-------------------------------";
-        System.out.println(linesep);
+        String lineSep = "-------------------------------";
+        System.out.println(lineSep);
         System.out.println("Hello! I'm YJ's Chatbot");
-        System.out.println("What can I do for you?\n" + linesep);
-        System.out.println("Bye. Hope to see you soon!\n" + linesep);
+        System.out.println("What can I do for you?\n" + lineSep);
+        Scanner scanner = new Scanner(System.in);
+        String cmd = readCmd(scanner);
+
+        while (!cmd.equals("bye")) {
+            System.out.println(cmd + "\n" + lineSep);
+            cmd = readCmd(scanner);
+        }
+
+        // Close scanner, must be done here because if I try to close in readCmd it'll close the System.in stream.
+        scanner.close();
+
+        System.out.println("Bye. Hope to see you soon!\n" + lineSep);
+    }
+
+    public static String readCmd(Scanner scanner) {
+        // Read user input
+        String cmd = scanner.nextLine();
+        return cmd;
     }
 }
