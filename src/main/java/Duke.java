cdbebcd85fb98fe24810c499d0fe97247841756f
revdrag <94689523+revdrag@users.noreply.github.com>
2023-08-23 19:33:04 +0800

    Implemented Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,12 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
+    final static String LIST = "list";
+    final static String MARK = "mark";
+    final static String UNMARK = "unmark";
+    final static String TODO = "todo";
+    final static String DEADLINE = "deadline";
+    final static String EVENT = "event";
     public static void main(String[] args) {
         ArrayList<Task> storedList = new ArrayList<>();
         String line = "____________________________________________________________";
@@ -12,41 +17,59 @@ public class Duke {
                 + "        ┗━━┻━┓┏┻━━┛\n"
                 + "        ╋╋╋┏━┛┃\n"
                 + "        ╋╋╋┗━━┛\n";
-        System.out.println(line + "\nHello! I'm \n" + logo);
+        System.out.println("\nHello! I'm \n" + logo);
         System.out.println("How can I help you? \n" + line);
         Scanner userInput = new Scanner(System.in);
         String input = userInput.nextLine();
+        String[] inputArr = input.split(" ",2);
 
         while (!input.equals("bye")) {
-            if (input.equals("list")) {
-                System.out.print("Here are the tasks in your list: \n");
-                for (int i = 0; i < storedList.size(); i++) {
-                    int index = i + 1;
-                    Task t = storedList.get(i);
-                    System.out.println(index + "." + t.getStatusIcon() + t.description);
-                }
-                System.out.print(line + "\n");
-            } else if (input.contains("unmark")) {
-                int index = input.charAt(7) - 49;
-                Task t = storedList.get(index);
-                storedList.get(index).markAsUndone();
-                System.out.println("Nice! I've marked this task as not done yet:");
-                System.out.println(t.getStatusIcon() + t.description);
-                System.out.print(line);
-            } else if (input.contains("mark")) {
-                int index = input.charAt(5) - 49;
-                Task t = storedList.get(index);
-                storedList.get(index).markAsDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(t.getStatusIcon() + t.description);
-                System.out.print(line);
-            } else {
-                Task task = new Task(input);
-                storedList.add(task);
-                System.out.println("added: "+ input + "\n" + line);
+            switch (inputArr[0]) {
+                case LIST:
+                    System.out.print("Here are the tasks in your list: \n");
+                    for (int i = 0; i < storedList.size(); i++) {
+                        int index = i + 1;
+                        Task t = storedList.get(i);
+                        System.out.println(index + "." + t.toString());
+                    }
+                    System.out.print(line + "\n");
+                    break;
+                case MARK:
+                    int mIndex = input.charAt(5) - 49;
+                    Task t = storedList.get(mIndex);
+                    storedList.get(mIndex).markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(t.toString() + "\n" + line);
+                    break;
+                case UNMARK:
+                    int umIndex = input.charAt(7) - 49;
+                    Task umTask = storedList.get(umIndex);
+                    storedList.get(umIndex).markAsUndone();
+                    System.out.println("Nice! I've marked this task as not done yet:");
+                    System.out.println(umTask.toString() + "\n" + line);
+                    break;
+                case DEADLINE:
+                    String[] deadlineArr = inputArr[1].split("/by ", 2);
+                    Task deadline = new Deadline(deadlineArr[0], deadlineArr[1]);
+                    storedList.add(deadline);
+                    System.out.println(deadline.toString() + "\n" + line);
+                    break;
+                case EVENT:
+                    String[] eventArr1 = inputArr[1].split("/from ", 2);
+                    String[] eventArr2 = eventArr1[1].split("/to ", 2);
+                    Task event = new Event(eventArr1[0], eventArr2[0], eventArr2[1]);
+                    storedList.add(event);
+                    System.out.println(event.toString() + "\n" + line);
+                    break;
+                case TODO:
+                    Task todo = new Todo(inputArr[1]);
+                    storedList.add(todo);
+                    System.out.println(todo.toString() + "\n" + line);
+                    break;
             }
 
             input = userInput.nextLine();
+            inputArr = input.split(" ",2);
         }
 
         System.out.println("Bye (actually hehe). Hope to see you again!\n" + line);
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -18,4 +18,9 @@ public class Task {
     public void markAsUndone() {
         isDone = false;
     }
+
+    @Override
+    public String toString() {
+        return this.getStatusIcon() + this.description;
+    }
 }
