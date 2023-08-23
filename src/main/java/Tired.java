877b90309570bc9d8f9c1c4ed11632371daabdc0
chewjh1234 <111339807+chewjh1234@users.noreply.github.com>
2023-08-23 16:14:59 +0800

    Add Level-0

---
+++ src/main/java/Tired.java
@@ -0,0 +1,15 @@
+public class Tired {
+   public static void main(String[] args) {
+
+       String name = "Tired";
+       String horizontalLine = "____________________________________________________________";
+
+       System.out.println(horizontalLine);
+       System.out.println("Hello! I'm " + name);
+       System.out.println("What can I do for you?");
+       System.out.println(horizontalLine);
+
+       System.out.println("Bye. Hope to see you again soon!");
+       System.out.println(horizontalLine);
+   }
+}
