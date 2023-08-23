869ed743ac364eec931ff664bcbbaf000b0167e3
Ruth Lim <>
2023-08-23 15:45:27 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -10,7 +12,21 @@ public class Duke {
         System.out.println("Hello! I'm Bot.");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String userCommand = scanner.nextLine();
+            if (userCommand.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(userCommand);
+                System.out.println("____________________________________________________________");
+            }
+
+        }
     }
 }
