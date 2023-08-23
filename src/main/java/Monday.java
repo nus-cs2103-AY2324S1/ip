ba0d375257caefb04722b6a7d955554cc230d472
nubnubyas <e0970089@u.nus.edu>
2023-08-23 19:48:14 +0800

    Completed Level-1 Echo

--- src/main/java/Monday.java
+++ src/main/java/Monday.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Monday {
     public static void main(String[] args) {
         String chatBotName = "Monday";
+        Scanner scanner = new Scanner(System.in);
 
+        greet(chatBotName);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                exit();
+                break;
+            }
+            echo(userInput);
+        }
+    }
+
+    private static void echo(String word) {
+        printSeparator();
+        System.out.println(word);
+        printSeparator();
+    }
+
+    private static void greet(String chatBotName) {
         printSeparator();
         System.out.println("Hello! I'm " + chatBotName);
         System.out.println("What can I do for you?");
+        printSeparator();
+    }
+
+    private static void exit() {
         printSeparator();
         System.out.println("Bye. Hope to see you again soon!");
         printSeparator();
