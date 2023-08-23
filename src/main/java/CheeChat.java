c7194789107829aaced84fd3111123e62291f8d4
Chee Teng <>
2023-08-24 01:02:48 +0800

    Level 2

--- src/main/java/CheeChat.java
+++ src/main/java/CheeChat.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class CheeChat {
+
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -11,9 +13,22 @@ public class CheeChat {
         System.out.println("Hello! I'm CheeChat");
         System.out.println("What can I do for you?");
         Scanner input = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
         String cmd = input.nextLine();
+        int counter = 0;
         while(!cmd.equals("bye")){
-            System.out.println(cmd);
+
+
+            if (cmd.equals("list")) {
+                for (int x = 0; x < counter; x++) {
+                    int index = x + 1;
+                    System.out.println(index + ". " + tasks.get(x));
+                    }
+            } else {
+                System.out.println("added: " + cmd);
+                tasks.add(cmd);
+                counter++;
+            }
             cmd = input.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
