4cba055916a1d92c0987cb10e11bda7066b2cb69
shashah <shashaminchok@gmail.com>
2023-08-23 20:10:23 +0800

    echo command back and exit when bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] tasks = new String[10];
+        String horizontal_line =  "____________________________________________________________";
+        int count  = 0;
         String welcomeMessage = "____________________________________________________________\n" +
                 " Hello! I'm Blob\n" +
                 " What can I do for you?\n" +
@@ -14,14 +17,22 @@ public class Duke {
 
             if ("bye".equals(userCommand)) {
                 break;
+            } else if ("list".equals(userCommand)) {
+                System.out.println(horizontal_line);
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(horizontal_line);
+            } else {
+                System.out.println(horizontal_line);
+                System.out.println("added: " + userCommand);
+                System.out.println(horizontal_line);
+                tasks[count] = userCommand;
+                count++;
             }
-
-            System.out.println("____________________________________________________________");
-            System.out.println(userCommand);
-            System.out.println("____________________________________________________________");
         }
-
+        System.out.println(horizontal_line);
         System.out.println(" Bye. Come talk to Blob again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(horizontal_line);
     }
 }
