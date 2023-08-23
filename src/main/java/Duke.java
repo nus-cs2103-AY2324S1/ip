ce9378f7040c171e89c686f3b3095137bb62edd5
tiongMax <maxtiongcs16@gmail.com>
2023-08-23 21:25:39 +0800

    Attempt on level 0.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,15 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello from\n" + logo);
+
+        String dash = "-------------------------------------------------------------";
+
+        System.out.println(dash);
+        System.out.println("Hello! I'm YOLO \nWhat can I do for you? \n");
+        System.out.println(dash);
+        System.out.println("Bye. Hope to see you again soon! \n");
+        System.out.println(dash);
     }
 }
