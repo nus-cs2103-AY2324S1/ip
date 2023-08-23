f14d6fe2740b5b39d0f1c53bb4ae341bff961774
marcellaantania <marcellaantaniatan@gmail.com>
2023-08-23 18:23:48 +0800

    add, list function

--- src/main/java/Thea.java
+++ src/main/java/Thea.java
@@ -1,20 +1,39 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Thea {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        List<String> tasks =  new ArrayList<>();
         greet();
         String command = input.nextLine();
-        while(!command.equals("bye")) {
-            System.out.println(command);
+        while (true) {
+            if (command.equals("bye")) {
+                exit();
+                break;
+            } else if (command.equals("list")) {
+                printList(tasks);
+            } else {
+                add(command, tasks);
+            }
             command = input.nextLine();
         }
-        exit();
     }
     public static void greet() {
         System.out.println("Hello! I'm Thea •ᴗ•\nHow can I help you?\n");
     }
     public static void exit() {
         System.out.println("I hope I made your day easier with my service. See you again! >ᴗ<");
+    }
+    public static void add(String task, List<String> tasks) {
+        tasks.add(task);
+        System.out.println("added: " + task);
+    }
+    public static void printList(List<String> tasks) {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.printf("%d. %s%n", i + 1, tasks.get(i));
+        }
     }
 }
