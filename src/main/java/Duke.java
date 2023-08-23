b6d19c4af013b59eaea2d07c9bcf70e94a8b129a
YangHeng Tang <yanghengtang@gmail.com>
2023-08-23 23:40:02 +0800

    Update Duke.java to use Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,7 +16,7 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
         String input = "";
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         System.out.println(entryMessage);
         while (!input.equals("bye")) {
@@ -24,12 +24,29 @@ public class Duke {
             if (input.equals("list")) {
                 System.out.print(HORIZONTAL_LINE);
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println((i + 1) + ". "+ list.get(i));
+                    System.out.println((i + 1) + ". "+ list.get(i).toString());
                 }
                 System.out.println(HORIZONTAL_LINE);
-            } else if (!input.equals("bye")) {
+            } else if (input.startsWith("mark")) {
+                Task task = list.get(Integer.valueOf(input.substring(5)) - 1);
+                task.markAsDone();
+
                 System.out.print(HORIZONTAL_LINE);
-                list.add(input);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(task);
+                System.out.println(HORIZONTAL_LINE);
+            } else if (input.startsWith("unmark")) {
+                Task task = list.get(Integer.valueOf(input.substring(7)) - 1);
+                task.markAsNotDone();
+
+                System.out.print(HORIZONTAL_LINE);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(task);
+                System.out.println(HORIZONTAL_LINE);
+            } else if (!input.equals("bye")) {
+                list.add(new Task(input));
+                
+                System.out.print(HORIZONTAL_LINE);
                 System.out.println("added: " + input);
                 System.out.println(HORIZONTAL_LINE);
             }
