9e71e15ea7a2140d0a8952b731b749afeb1b197d
RSXIX <rohansapra19@gmail.com>
2023-08-24 01:41:33 +0800

    Completed Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " _______________ \n"
+                + "Hello! I'm Scarlet \n"
+                + "What can I do for you? \n\n"
+                + " _______________ \n"
+                + "Bye. Hope to see you again soon! \n"
+                + " _______________ \n";
+        System.out.println(logo);
     }
 }
