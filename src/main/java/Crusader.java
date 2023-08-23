254a81bcd9b6a590f8ad1236ebaf2dbeb7df4312
Kyriel Abad <abadkyriel@gmail.com>
2023-08-23 15:41:02 +0800

    Personalise chatbot with new name

---
+++ src/main/java/Crusader.java
@@ -0,0 +1,18 @@
+public class Crusader {
+    /* Logo generated from https://patorjk.com/software/taag */
+    private static final String logo =
+              "   _____                          _\n"
+            + "  / ____|                        | |\n"
+            + " | |     _ __ _   _ ___  __ _  __| | ___ _ __\n"
+            + " | |    | '__| | | / __|/ _` |/ _` |/ _ \\ '__|\n"
+            + " | |____| |  | |_| \\__ \\ (_| | (_| |  __/ |\n"
+            + "  \\_____|_|   \\__,_|___/\\__,_|\\__,_|\\___|_|\n";
+
+    private static void showLogo() {
+        System.out.println(logo);
+    }
+
+    public static void main(String[] args) {
+        showLogo();
+    }
+}
