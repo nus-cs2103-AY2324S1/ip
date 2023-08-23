9b36f3efca7833183292ba14c0e64f5110d89d92
xxiaoweii <e0967899@u.nus.edu>
2023-08-23 18:20:07 +0800

    level 1

--- src/main/java/Messages.java
+++ src/main/java/Messages.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Messages {
 
     public static final String GREET = "Hello! I'm Chatty!\nWhat can I do for you?";
     public static final String EXIT = "Bye. Hope to see you again soon!";
+
+    public static void startChatting() {
+        String exitCommand = "bye";
+
+        Scanner scanner = new Scanner(System.in);
+
+        String userCommands;
+        do {
+            userCommands = scanner.nextLine();
+            if (!userCommands.equals(exitCommand)) {
+                System.out.println(userCommands);
+            }
+        } while (!userCommands.equals(exitCommand));
+
+        System.out.println(EXIT);
+    }
 }
