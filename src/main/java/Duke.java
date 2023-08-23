acd7eedea50c71501e96c20e247a2915f0ddab05
teeheehoo\jierongu <jiarongchoo@gmail.com>
2023-08-24 03:06:54 +0800

    added ability to store history in a list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<String> inputHistory = new ArrayList<>();
 
         System.out.println("Hello! I'm Gideon");
         System.out.println("What can I do for you?");
@@ -12,8 +14,13 @@ public class Duke {
             if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+                for(int i = 0; i < inputHistory.size(); i++) {
+                    System.out.println((i + 1) + ". " + inputHistory.get(i));
+                }
             } else {
-                System.out.println(userInput);
+                System.out.println("added: " + userInput);
+                inputHistory.add(userInput);
             }
         }
         scanner.close();
