6c864b49e77fbed61d2127b7941ba1c5a48c34eb
Shu Yang <shuyangk@gmail.com>
2023-08-24 00:10:23 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Charlie\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________\n" +
+                        " Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________\n");
+                break;
+            }
+
+            System.out.println("____________________________________________________________\n" +
+                    input +
+                    "\n____________________________________________________________\n");
+        }
+
+        scanner.close();
+
     }
 }
