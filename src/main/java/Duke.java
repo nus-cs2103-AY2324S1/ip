db59517507011c92429a30de97105623670d3def
jordankanghm <jordankanghm@gmail.com>
2023-08-23 21:33:21 +0800

    Level 2 Commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,16 +7,26 @@ public class Duke {
                 "____________________________________________________________";
         System.out.println(intro);
 
+        int counter = 1;
+        String list = "";
+
         Scanner sc = new Scanner(System.in);
         // Get user command
         String cmd = sc.nextLine();
 
         // Exit if command is "bye"
         while (!cmd.equals("bye")) {
-            String echo = "____________________________________________________________\n" +
-                    " " + cmd + "\n" +
-                    "____________________________________________________________";
-            System.out.println(echo);
+            // If cmd is "list", list items and wait for next command
+            if (cmd.equals("list")) {
+                System.out.println(list);
+            } else {    // else, echo command and add to the list
+                String echo = "____________________________________________________________\n" +
+                        " added: " + cmd + "\n" +
+                        "____________________________________________________________";
+                System.out.println(echo);
+                list += counter + ". " + cmd + "\n";
+                counter++;
+            }
             cmd = sc.nextLine();
         }
 
