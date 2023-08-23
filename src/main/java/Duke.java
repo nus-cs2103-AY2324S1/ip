901b651448a5e83dd0f81db04cdbb954d305b2b5
Kailash201 <109354852+Kailash201@users.noreply.github.com>
2023-08-23 18:34:00 +0800

    Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,11 +8,25 @@ public class Duke {
         String Start = "Hello! I'm Red \nWhat can I do for you?";
         System.out.println(Start);
 
-        boolean run = true;
-        while (run) {
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
+        while (true) {
             String input = scanner.nextLine();
-            System.out.println(input);
-            run = !input.equals("bye");
+            if(input.equals("bye"))
+                break;
+
+            if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(i + 1 + ". " + tasks[i]);
+                }
+                continue;
+            }
+
+            tasks[taskCount] = input;
+            taskCount++;
+            System.out.println("added: " + input);
+
         }
 
         String End = "Bye. Hope to see you again soon!\n";
