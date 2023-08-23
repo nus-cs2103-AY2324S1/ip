d8a9636ebe928c04259652ea927acefb4ce03d31
Wern <dev@wern.cc>
2023-08-23 15:10:57 +0800

    fix functional interface

--- src/main/java/EventListener.java
+++ src/main/java/EventListener.java
@@ -2,10 +2,11 @@
  * An interface capable of listening to certain events.
  * @param <T> The type of the event.
  */
+@FunctionalInterface
 public interface EventListener<T> {
     /**
      * A method to process an event that has been fired.
      * @param event Fired event.
      */
-    public void eventFired(T event);
+    void eventFired(T event);
 }
