ea168f27804fd5b9fe6d239317cd182acc2d0438
Teng Xin Zhuan <>
2023-08-23 16:21:28 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,11 +3,22 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String botName = "Aaronbot"; // Rename your chatbot here
+        String botName = "Aaronbot";
 
         System.out.println("Hello! I'm " + botName);
         System.out.println("What can I do for you?");
 
-        System.out.println("Bye. Hope to see you again soon!");
+        while (true) {
+            String userInput = scanner.nextLine();
+            
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(userInput); 
+            }
+        }
+        
+        scanner.close();
     }
 }
