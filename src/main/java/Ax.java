e9b8b5f1ed88dc4449baa3a90d80dd2e9ab9ca70
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2023-08-23 11:22:13 +0800

    make the list work

--- src/main/java/Ax.java
+++ src/main/java/Ax.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Ax {
+    private static List<String> listItems = new ArrayList<String>();
 
     private static void hoLine() {
         System.out.println();
@@ -19,12 +22,27 @@ public class Ax {
         hoLine();
     }
 
+    private static void listTheList() {
+        for (int i = 0; i < listItems.size(); i++) {
+            System.out.println(String.format("%d. %s", i, listItems.get(i)));
+        }
+    }
+
     private static boolean getInput(Scanner scanner) {
-        String input = scanner.nextLine();
-        System.out.println(input);
+        String input = scanner.nextLine(); // get next input
+        System.out.println(input); // repeat the input
         System.out.println();
         hoLine();
-        return input.equals("bye");
+        if (input.equals("bye")) { // check if it is bye, then return true so it will exit the loop
+            return true;
+        } else if (input.equals("list")) {
+            // call the list function
+            listTheList();
+        } else {
+            listItems.add(input);
+        }
+
+        return false;
     }
 
     public static void main(String[] args) {
