4d9644d0f1d9fab6d5f9c8a340fcf167037d111a
jordankanghm <jordankanghm@gmail.com>
2023-08-23 21:21:02 +0800

    Level 1 Commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String intro = "____________________________________________________________\n" +
                 " Hello! I'm Dookie\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
+                "____________________________________________________________";
         System.out.println(intro);
+
+        Scanner sc = new Scanner(System.in);
+        // Get user command
+        String cmd = sc.nextLine();
+
+        // Exit if command is "bye"
+        while (!cmd.equals("bye")) {
+            String echo = "____________________________________________________________\n" +
+                    " " + cmd + "\n" +
+                    "____________________________________________________________";
+            System.out.println(echo);
+            cmd = sc.nextLine();
+        }
+
+        String exitMessage = "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(exitMessage);
     }
 }
