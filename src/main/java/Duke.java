81e6df3dd9dc2fb1645eb0846df57a56c57dfd41
Originalidk <ryanpang321@gmail.com>
2023-08-23 23:23:57 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,32 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("    ____________________________________________________________");
-        System.out.println("     Hello! I'm ChatGP0");
-        System.out.println("     What can I do for you?");
-        System.out.println("    ____________________________________________________________\n");
         Scanner scan = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<String>();
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     " + "Hello! I'm ChatGP0");
+        System.out.println("     " + "What can I do for you?");
+        System.out.println("    ____________________________________________________________\n");
         String input = scan.nextLine();
         while (!input.equals("bye")) {
-            System.out.println("    ____________________________________________________________");
-            System.out.println("     " + input);
-            System.out.println("    ____________________________________________________________\n");
+            if (input.equals("list")) {
+                System.out.println("    ____________________________________________________________");
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println("     " + (i + 1) + ". " + list.get(i));
+                }
+                System.out.println("    ____________________________________________________________");
+            } else {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     " + "added: " + input);
+                System.out.println("    ____________________________________________________________\n");
+                list.add(input);
+            }
             input = scan.nextLine();
         }
         System.out.println("    ____________________________________________________________");
-        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("     " + "Bye. Hope to see you again soon!");
         System.out.println("    ____________________________________________________________");
     }
 }
