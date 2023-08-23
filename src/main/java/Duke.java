137c1492c0933a5fdbb4a04f2512ac023e8b8879
erohsikivar <ravikishore1809@gmail.com>
2023-08-24 01:00:25 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
         System.out.println("Hello! I am Nila");
         System.out.println("What can I do for you?");
-        System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
-        System.out.println("\nHope to see you again soon!");
+        System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -\n");
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        while (!str.equals("bye") && !str.equals("Bye")) {
+            System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+            System.out.println(str);
+            System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+            str = sc.nextLine();
+        }
+
+        sc.close();
+        System.out.println("\nBye. Hope to see you again soon!");
         System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
     }
 }
