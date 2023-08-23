80d8ade706834ce0f51814e8023cd9b220dfa908
Kota Sudarshan Karanth <sudarshan2402@gmail.com>
2023-08-23 13:41:05 +0800

    Added delete functionality and updated tasks to be an ArrayList

--- src/main/java/Harvard.java
+++ src/main/java/Harvard.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Harvard {
     public static void main(String[] args) {
@@ -7,7 +8,7 @@ public class Harvard {
         System.out.println(line);
 
         Scanner in = new Scanner(System.in);
-        Task[] tasks = new Task[100];
+        ArrayList<Task> tasks = new ArrayList<>();
         int taskCount = 0;
 
         while (true) {
@@ -27,7 +28,7 @@ public class Harvard {
                         throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
                     }
                     Todo todo = new Todo(description);
-                    tasks[taskCount] = todo;
+                    tasks.add(todo);
                     taskCount++;
                     System.out.println(line);
                     System.out.println("Got it. I've added this task:\n" + todo +
@@ -52,7 +53,7 @@ public class Harvard {
                     }
                     String by = split[1];
                     Deadline deadline = new Deadline(description, by);
-                    tasks[taskCount] = deadline;
+                    tasks.add(deadline);
                     taskCount++;
                     System.out.println(line);
                     System.out.println("Got it. I've added this task:\n" + deadline +
@@ -80,7 +81,7 @@ public class Harvard {
                     String from = split2[0];
                     String to = split2[1];
                     Event event = new Event(description, from, to);
-                    tasks[taskCount] = event;
+                    tasks.add(event);
                     taskCount++;
                     System.out.println(line);
                     System.out.println("Got it. I've added this task:\n" + event +
@@ -89,9 +90,29 @@ public class Harvard {
                     System.out.println(line);
                     System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println((i + 1) + ". " + tasks[i]);
+                        System.out.println((i + 1) + ". " + tasks.get(i));
                     }
-                } else if (command.startsWith("mark")) {
+
+                } else if(command.startsWith("delete")) {
+                    String[] split = command.split(" ");
+                    if (split.length != 2) {
+                        throw new DukeException("☹ OOPS!!! The index must be specified.");
+                    }
+                    int index = Integer.parseInt(split[1]) - 1;
+                    if (index >= taskCount || index < 0) {
+                        throw new DukeException("☹ OOPS!!! The index is invalid.");
+                    }
+                    Task task = tasks.get(index);
+                    tasks.remove(index);
+                    taskCount--;
+                    System.out.println(line);
+                    System.out.println("Noted. I've removed this task:");
+                    System.out.println(task);
+                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+
+
+
+                }else if (command.startsWith("mark")) {
                     String[] split = command.split(" ");
                     int index = Integer.parseInt(split[1]) - 1;
                     if (index >= taskCount || index < 0) {
@@ -100,17 +121,17 @@ public class Harvard {
                         System.out.println(line);
                         continue;
                     }
-                    tasks[index].markAsDone();
+                    tasks.get(index).markAsDone();
                     System.out.println(line);
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(tasks[index]);
+                    System.out.println(tasks.get(index));
                 } else if (command.startsWith("unmark")) {
                     String[] split = command.split(" ");
                     int index = Integer.parseInt(split[1]) - 1;
-                    tasks[index].markAsUndone();
+                    tasks.get(index).markAsUndone();
                     System.out.println(line);
                     System.out.println("Ok! I've marked this task as not done yet:");
-                    System.out.println(tasks[index]);
+                    System.out.println(tasks.get(index));
                 } else {
                     throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
