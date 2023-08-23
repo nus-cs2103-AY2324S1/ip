9dfb3cab7d39f9f244577e15f7170a0bce27d5b0
samuelmui8 <samuelmui8@gmail.com>
2023-08-23 16:44:24 +0800

    added error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -32,101 +32,108 @@ public class Duke {
 
             } else if (words[0].equals("mark")) {
                 // mark task as done
-                int index = Integer.parseInt(words[1]) - 1;
-
-                if (index < 0 || index >= list.size()) {
-                    // handle invalid input
-                    System.out.println("Invalid number ");
-                } else {
+                try {
+                    int index = Integer.parseInt(words[1]) - 1;
                     list.get(index).markAsDone();
                     System.out.println("\t-----------------------------------------------");
                     System.out.println("\tNice! I've marked this task as done:");
                     System.out.println("\t" + list.get(index));
+                    System.out.println("\t-----------------------------------------------");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("\t-----------------------------------------------");
+                    System.out.println("\tInvalid number");
+                    System.out.println("\t-----------------------------------------------");
+                } catch (NumberFormatException e) {
+                    System.out.println("\t-----------------------------------------------");
+                    System.out.println("\tPlease key in a number");
                     System.out.println("\t-----------------------------------------------");
                 }
 
 
             } else if (words[0].equals("unmark")) {
                 // mark task as undone
-                int index = Integer.parseInt(words[1]) - 1;
-
-                if (index < 0 || index >= list.size()) {
-                    // handle invalid input
-                    System.out.println("Invalid number ");
-                } else {
+                try {
+                    int index = Integer.parseInt(words[1]) - 1;
                     list.get(index).unMark();
                     System.out.println("\t-----------------------------------------------");
                     System.out.println("\tNice! I've marked this task as not done yet:");
                     System.out.println("\t" + list.get(index));
                     System.out.println("\t-----------------------------------------------");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("\t-----------------------------------------------");
+                    System.out.println("Invalid number");
+                    System.out.println("\t-----------------------------------------------");
+                } catch (NumberFormatException e) {
+                    System.out.println("\t-----------------------------------------------");
+                    System.out.println("Please key in a number");
+                    System.out.println("\t-----------------------------------------------");
                 }
 
-            } else if (taskType.equals("todo") || taskType.equals("deadline") || taskType.equals("event")) {
-                // add task
-                String description;
-                Task task;
-                if (taskType.equals("todo")) {
-                    // Define regular expressions for pattern matching for todo
-                    Pattern todoPattern = Pattern.compile("todo\\s+(.*?)$");
+            } else {// add task
+                try {
+                    String description;
+                    Task task;
+                    if (taskType.equals("todo")) {
+                        // Define regular expressions for pattern matching for todo
+                        Pattern todoPattern = Pattern.compile("todo\\s+(.*?)$");
 
-                    // Match the input string with the pattern
-                    Matcher matcher = todoPattern.matcher(input);
+                        // Match the input string with the pattern
+                        Matcher matcher = todoPattern.matcher(input);
 
-                    // Check if the input string matches the pattern
-                    if (matcher.matches()) {
-                        description = matcher.group(1); // Extract task name
-                        task = new Todo(description);
+                        // Check if the input string matches the pattern
+                        if (matcher.matches()) {
+                            description = matcher.group(1); // Extract task name
+                            task = new Todo(description);
+                        } else {
+                            throw new DukeException("\t☹ OOPS!!! The description of a todo cannot be empty."
+                                    + "\n\ttodo ...");
+                        }
+
+                    } else if (taskType.equals("deadline")) {
+                        // Define regular expressions for pattern matching for deadline
+                        Pattern deadlinePattern = Pattern.compile("deadline\\s+(.*?)\\s+/by\\s+(.*?)$");
+
+                        // Match the input string with the pattern
+                        Matcher matcher = deadlinePattern.matcher(input);
+                        // Check if the input string matches the pattern
+                        if (matcher.matches()) {
+                            description = matcher.group(1); // Extract task name
+                            String dueDate = matcher.group(2);  // Extract due date
+                            task = new Deadline(dueDate, description);
+                        } else {
+                            throw new DukeException("\tInput for deadline doesn't match the expected format."
+                                    + "\n\tdeadline ... /by ...");
+                        }
+
+                    } else if (taskType.equals("event")){
+                        // Define regular expressions for pattern matching for event
+                        Pattern eventPattern = Pattern.compile("event\\s+(.*?)\\s+/from\\s+(.*?)\\s+/to\\s+(.*?)$");
+                        Matcher matcher = eventPattern.matcher(input);
+                        if (matcher.matches()) {
+                            description = matcher.group(1); // Extract event name
+                            String startTime = matcher.group(2); // Extract start time
+                            String endTime = matcher.group(3);   // Extract end time
+                            task = new Event(startTime, endTime, description);
+                        } else {
+                            throw new DukeException("\tInput for event doesn't match the expected format."
+                                    + "\n\tevent ... /from ... /to ...");
+                        }
                     } else {
-                        System.out.println("\t-----------------------------------------------");
-                        System.out.println("\tInput doesn't match the expected format.");
-                        System.out.println("\t-----------------------------------------------");
-                        continue;
+                        // invalid input
+                        throw new DukeException("\t☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                     }
+                    list.add(task);
+                    System.out.println("\t-----------------------------------------------");
+                    System.out.println("\tGot it bro! I've added this task:\n\t\t" + task);
+                    System.out.println("\tNow you have " + list.size() + " tasks in the list");
+                    System.out.println("\t-----------------------------------------------");
 
-                } else if (taskType.equals("deadline")) {
-                    // Define regular expressions for pattern matching for deadline
-                    Pattern deadlinePattern = Pattern.compile("deadline\\s+(.*?)\\s+/by\\s+(.*?)$");
-
-                    // Match the input string with the pattern
-                    Matcher matcher = deadlinePattern.matcher(input);
-                    // Check if the input string matches the pattern
-                    if (matcher.matches()) {
-                        description = matcher.group(1); // Extract task name
-                        String dueDate = matcher.group(2);  // Extract due date
-                        task = new Deadline(dueDate, description);
-                    } else {
-                        System.out.println("\t-----------------------------------------------");
-                        System.out.println("\tInput doesn't match the expected format.");
-                        System.out.println("\t-----------------------------------------------");
-                        continue;
-                    }
-
-                } else {
-                    // Define regular expressions for pattern matching for event
-                    Pattern eventPattern = Pattern.compile("event\\s+(.*?)\\s+/from\\s+(.*?)\\s+/to\\s+(.*?)$");
-                    Matcher matcher = eventPattern.matcher(input);
-                    if (matcher.matches()) {
-                        description = matcher.group(1); // Extract event name
-                        String startTime = matcher.group(2); // Extract start time
-                        String endTime = matcher.group(3);   // Extract end time
-                        task = new Event(startTime, endTime, description);
-                    } else {
-                        System.out.println("\t-----------------------------------------------");
-                        System.out.println("\tInput doesn't match the expected format.");
-                        System.out.println("\t-----------------------------------------------");
-                        continue;
-                    }
+                } catch (DukeException e) {
+                    System.out.println("\t-----------------------------------------------");
+                    System.out.println(e.getMessage());
+                    System.out.println("\t-----------------------------------------------");
                 }
-                list.add(task);
-                System.out.println("\t-----------------------------------------------");
-                System.out.println("\tGot it bro! I've added this task:\n\t\t" + task);
-                System.out.println("\tNow you have " + list.size() + " tasks in the list");
-                System.out.println("\t-----------------------------------------------");
-            } else {
-                // invalid input
-                System.out.println("\t-----------------------------------------------");
-                System.out.println("\t☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                System.out.println("\t-----------------------------------------------");
+
             }
         }
 
