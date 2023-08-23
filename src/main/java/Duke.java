da63b56490b6265a4aacca35946ba2d1bfa0750f
ruo-x <e0970274@u.nus.edu>
2023-08-23 20:01:30 +0800

    Greeting and exit message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What's your name?");
+
+        // Take in inputs from keyboard and show them on screen
+        Scanner sc = new Scanner(System.in);
+        String answer = sc.nextLine();
+
+        // Bot reply
+        System.out.println("Hi" + answer);
     }
 }
---
+++ src/main/java/Homepage.java
@@ -0,0 +1,12 @@
+public class Homepage {
+    public static void main(String[] args) {
+        System.out.println(
+                "____________________________________________________________\n"
+                + "Hello! I'm Bobi >.< , your trusted companion.\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye! Hope you have a good day today :)\n"
+                + "____________________________________________________________\n"
+        );
+    }
+}
