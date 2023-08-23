2a8a0356f79c5e7089bfc627a55a5f3894efb071
pzl111 <phuazailian@gmail.com>
2023-08-23 16:17:07 +0800

    Level-6 completed

--- src/main/java/Smolbrain.java
+++ src/main/java/Smolbrain.java
@@ -18,6 +18,12 @@ public class Smolbrain {
             String[] words = input.split(" ");
             String descr = "";
 
+//            try {
+//
+//            } catch {
+//
+//            }
+
             switch (words[0]) {
                 case "list":
                     System.out.println("Here are the tasks in your list: ");
@@ -27,6 +33,10 @@ public class Smolbrain {
                     break;
 
                 case "todo":
+                    if (words.length < 2) {
+                        System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                        break;
+                    }
                     for (int i = 1; i < words.length; i++) {
                         descr = descr + words[i] + " ";
                     }
@@ -51,6 +61,13 @@ public class Smolbrain {
                         } else {
                             descr = descr + words[i] + " ";
                         }
+                    }
+                    if (descr.equals("")) {
+                        System.out.println("☹ OOPS!!! The description of a deadline cannot be empty.");
+                        break;
+                    } else if (by_text.equals("")) {
+                        System.out.println("Please provide a time for deadline");
+                        break;
                     }
                     descr = descr.substring(0, descr.length()-1);
                     by_text = by_text.substring(0, by_text.length()-1);
@@ -82,6 +99,16 @@ public class Smolbrain {
                             descr = descr + words[i] + " ";
                         }
                     }
+                    if (descr.equals("")) {
+                        System.out.println("☹ OOPS!!! The description of a event cannot be empty.");
+                        break;
+                    } else if (from_text.equals("")) {
+                        System.out.println("Please provide a start time for event");
+                        break;
+                    } else if (to_text.equals("")) {
+                        System.out.println("Please provide a end time for event");
+                        break;
+                    }
                     descr = descr.substring(0, descr.length()-1);
                     from_text = from_text.substring(0, from_text.length()-1);
                     to_text = to_text.substring(0, to_text.length()-1);
@@ -93,18 +120,66 @@ public class Smolbrain {
                     break;
 
                 case "mark":
-                    int marknum = Integer.parseInt(words[1]);
-                    data.get(marknum-1).mark();
-                    System.out.println("Nice! I've marked this task as done: ");
-                    System.out.println(data.get(marknum-1));
-                    break;
+                    if (words.length < 2) {
+                        System.out.println("Please give the number of the task to mark.");
+                        break;
+                    }
+                    int marknum;
+                    try {
+                        marknum = Integer.parseInt(words[1]);
+                        data.get(marknum-1).mark();
+                        System.out.println("Nice! I've marked this task as done: ");
+                        System.out.println(data.get(marknum-1));
+                        break;
+                    } catch (NumberFormatException e) {
+                        System.out.println("Please provide a valid number of the task to mark.");
+                        break;
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println("Please provide a valid number within the range.");
+                        break;
+                    }
 
                 case "unmark":
-                    int unmarknum = Integer.parseInt(words[1]);
-                    data.get(unmarknum-1).unmark();
-                    System.out.println("OK, I've marked this task as not done yet: ");
-                    System.out.println(data.get(unmarknum-1));
-                    break;
+                    if (words.length < 2) {
+                        System.out.println("Please give the number of the task to unmark.");
+                        break;
+                    }
+                    int unmarknum;
+                    try {
+                        unmarknum = Integer.parseInt(words[1]);
+                        data.get(unmarknum-1).unmark();
+                        System.out.println("OK, I've marked this task as not done yet: ");
+                        System.out.println(data.get(unmarknum-1));
+                        break;
+                    } catch (NumberFormatException ex) {
+                        System.out.println("Please provide a valid number of the task to unmark.");
+                        break;
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println("Please provide a valid number within the range.");
+                        break;
+                    }
+
+                case "delete":
+                    if (words.length < 2) {
+                        System.out.println("Please give the number of the task to delete.");
+                        break;
+                    }
+                    int delnum;
+                    try {
+                        delnum = Integer.parseInt(words[1]);
+                        String task = data.get(delnum-1).toString();
+                        data.remove(delnum-1);
+                        System.out.println("Noted. I've removed this task: ");
+                        System.out.println(task);
+                        System.out.println("Now you have " + data.size() +" tasks in the list.");
+                        break;
+                    } catch (NumberFormatException ex) {
+                        System.out.println("Please provide a valid number of the task to delete.");
+                        break;
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println("Please provide a valid number within the range.");
+                        break;
+                    }
 
                 case "bye":
                     System.out.println("Bye. Hope to see you again soon!");
