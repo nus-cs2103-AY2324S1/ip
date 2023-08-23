39a9af9fb8e44682b171519a6d98bab33a9f156f
raydenlim <jiansonglim@gmail.com>
2023-08-23 20:29:03 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,36 +1,70 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    private Scanner scanner;
+    private String[] tasks;
+    private int counter;
 
-        String logo =
-                "██████╗░██╗░░░░░░█████╗░░█████╗░██████╗░\n" +
-                "██╔══██╗██║░░░░░██╔══██╗██╔══██╗██╔══██╗\n" +
-                "██████╦╝██║░░░░░██║░░██║██║░░██║██████╔╝\n" +
-                "██╔══██╗██║░░░░░██║░░██║██║░░██║██╔═══╝░\n" +
-                "██████╦╝███████╗╚█████╔╝╚█████╔╝██║░░░░░\n" +
-                "╚═════╝░╚══════╝░╚════╝░░╚════╝░╚═╝░░░░░";
+    private static String logo =
+            "██████╗░██╗░░░░░░█████╗░░█████╗░██████╗░\n" +
+                    "██╔══██╗██║░░░░░██╔══██╗██╔══██╗██╔══██╗\n" +
+                    "██████╦╝██║░░░░░██║░░██║██║░░██║██████╔╝\n" +
+                    "██╔══██╗██║░░░░░██║░░██║██║░░██║██╔═══╝░\n" +
+                    "██████╦╝███████╗╚█████╔╝╚█████╔╝██║░░░░░\n" +
+                    "╚═════╝░╚══════╝░╚════╝░░╚════╝░╚═╝░░░░░";
 
-        boolean isEcho = true;
+    public Duke() {
+        scanner = new Scanner(System.in);
+        tasks = new String[100];
+        counter = 0;
+    }
 
+    public void start() {
         System.out.println("Hello from\n" + logo);
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm BloopBot");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
+        boolean isEcho = true;
+
+
         while (isEcho) {
             String strInput = scanner.nextLine();
             System.out.println("____________________________________________________________");
-            System.out.println(strInput);
-            System.out.println("____________________________________________________________");
-
             if (strInput.equalsIgnoreCase("bye")) {
                 isEcho = false;
+                System.out.println(" Bye. Hope to see you again soon!");
+            } else if (strInput.equalsIgnoreCase("list")) {
+                displayTasks();
+            } else {
+                addTask(strInput);
+                System.out.println(" added: " + strInput);
+            }
+            System.out.println("____________________________________________________________");
+        }
+
+    }
+
+    private void addTask(String task) {
+        tasks[counter] = task;
+        counter++;
+    }
+
+    private void displayTasks() {
+        if (counter == 0) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" No tasks in list.");
+            System.out.println("____________________________________________________________");
+        } else {
+            for (int i = 0; i < counter; i++) {
+                System.out.println(" " + (i + 1) + ". " + tasks[i]);
             }
         }
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        Duke bloopBot = new Duke();
+        bloopBot.start();
     }
 }
