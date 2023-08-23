f35b6b4f9c131a2b6bea7e8e0a6df89a85225956
LordSaumya <68504323+LordSaumya@users.noreply.github.com>
2023-08-23 15:36:59 +0800

    Complete Level 6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -42,55 +42,111 @@ public class Duke {
                 // Mark a task as done
                 System.out.println(divider);
                 int taskNum = Integer.parseInt(command.substring(5));
-                list.get(taskNum - 1).changeStatus(true);
-                System.out.println(
-                        "Astonishingly enough, you have managed to triumph over this mind-bogglingly simple task:\n");
-                System.out.println(list.get(taskNum - 1).toString());
+                try {
+                    list.get(taskNum - 1).changeStatus(true);
+                    System.out.println(
+                            "Astonishingly enough, you have managed to triumph over this mind-bogglingly simple task:\n");
+                    System.out.println(list.get(taskNum - 1).toString());
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("You have no such task, mortal.");
+                }
                 System.out.println(divider);
             } else if (command.startsWith("unmark")) {
                 // Mark a task as not done
                 System.out.println(divider);
                 int taskNum = Integer.parseInt(command.substring(7));
-                list.get(taskNum - 1).changeStatus(false);
-                System.out.println(
-                        "In a stunning twist, this task remains untouched by the hands of progress:\n");
-                System.out.println(list.get(taskNum - 1).toString());
-                System.out.println(divider);
+                try {
+                    list.get(taskNum - 1).changeStatus(false);
+                    System.out.println(
+                            "In a stunning twist, this task remains untouched by the hands of progress:\n");
+                    System.out.println(list.get(taskNum - 1).toString());
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("You have no such task, mortal.");
+                } finally {
+                    System.out.println(divider);
+                }
             } else if (command.startsWith("todo")) {
                 // Add a todo task
                 System.out.println(divider);
-                System.out.println("This task has been reluctantly bestowed upon your ever-growing list:\n");
-                list.add(new ToDo(command.substring(5)));
-                System.out.println(list.get(list.size() - 1).toString());
-                System.out
-                        .println("Congratulations, your pile of tasks has swelled to a whopping " + list.size() + ".");
-                System.out.println(divider);
+                try {
+                    System.out.println("This task has been reluctantly bestowed upon your ever-growing list:\n");
+                    if (command.substring(5).equals("")) {
+                        throw new Exception("The description of a todo cannot be empty.");
+                    }
+                    System.out.println(list.get(list.size() - 1).toString());
+                    System.out
+                            .println("Congratulations, your pile of tasks has swelled to a whopping " + list.size()
+                                    + ".");
+                    list.add(new ToDo(command.substring(5)));
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                } finally {
+                    System.out.println(divider);
+                }
             } else if (command.startsWith("deadline")) {
                 // Add a deadline task
                 System.out.println(divider);
-                String[] deadlineTaskComponents = command.substring(9).split(" /by ");
-                System.out.println(
-                        "With your constant mediocrity, it is entirely unlikely that you will be able to meet this deadline I have just added: \n");
-                list.add(new Deadline(deadlineTaskComponents[0], deadlineTaskComponents[1]));
-                System.out.println(list.get(list.size() - 1).toString());
-                System.out
-                        .println("Congratulations, your pile of tasks has swelled to a whopping " + list.size() + ".");
-                System.out.println(divider);
+                try {
+                    String[] deadlineTaskComponents = command.substring(9).split(" /by ");
+
+                    if (deadlineTaskComponents[0].equals("")) {
+                        throw new Exception("The description of a deadline cannot be empty.");
+                    } else if (deadlineTaskComponents.length == 1) {
+                        throw new Exception("The deadline of a deadline cannot be empty.");
+                    } else if (deadlineTaskComponents.length > 2) {
+                        throw new Exception("The deadline of a deadline cannot contain more than one date.");
+                    } else if (deadlineTaskComponents[1].equals("")) {
+                        throw new Exception("The deadline of a deadline cannot be empty.");
+                    }
+
+                    System.out.println(
+                            "With your constant mediocrity, it is entirely unlikely that you will be able to meet this deadline I have just added: \n");
+                    list.add(new Deadline(deadlineTaskComponents[0], deadlineTaskComponents[1]));
+                    System.out.println(list.get(list.size() - 1).toString());
+                    System.out
+                            .println("Congratulations, your pile of tasks has swelled to a whopping " + list.size()
+                                    + ".");
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                } finally {
+                    System.out.println(divider);
+                }
             } else if (command.startsWith("event")) {
                 // Add an event task
                 System.out.println(divider);
-                String[] eventTaskComponents = command.substring(6).split(" /from ");
-                String eventDescription = eventTaskComponents[0];
-                String[] eventTaskDates = eventTaskComponents[1].split(" /to ");
-                System.out.println(
-                        "Looks like I will have to slow time down myself if you wish to make it to this event I just added:\n");
-                list.add(new Event(eventDescription, eventTaskDates[0], eventTaskDates[1]));
-                System.out.println(list.get(list.size() - 1).toString());
-                System.out
-                        .println("Congratulations, your pile of tasks has swelled to a whopping " + list.size() + ".");
-                System.out.println(divider);
+                try {
+                    String[] eventTaskComponents = command.substring(6).split(" /from ");
+                    String eventDescription = eventTaskComponents[0];
+                    if (eventDescription.equals("")) {
+                        throw new Exception("The description of an event cannot be empty.");
+                    } else if (eventTaskComponents.length == 1) {
+                        throw new Exception("The start time of an event cannot be empty.");
+                    } else if (eventTaskComponents.length > 2) {
+                        throw new Exception("The start time of an event cannot contain more than one date.");
+                    }
+
+                    String[] eventTaskDates = eventTaskComponents[1].split(" /to ");
+
+                    if (eventTaskDates[1].equals("")) {
+                        throw new Exception("The end time of an event cannot be empty.");
+                    } else if (eventTaskDates.length > 2) {
+                        throw new Exception("The end time of an event cannot contain more than one date.");
+                    }
+
+                    System.out.println(
+                            "Looks like I will have to slow time down myself if you wish to make it to this event I just added:\n");
+                    list.add(new Event(eventDescription, eventTaskDates[0], eventTaskDates[1]));
+                    System.out.println(list.get(list.size() - 1).toString());
+                    System.out
+                            .println("Congratulations, your pile of tasks has swelled to a whopping " + list.size()
+                                    + ".");
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                } finally {
+                    System.out.println(divider);
+                }
             } else {
-                // Error message (command unclear)
+                // Error message (unknown command)
                 System.out.println(divider);
                 System.out.println("Do not test my patience, mortal. Speak clearly.");
                 System.out.println(divider);
