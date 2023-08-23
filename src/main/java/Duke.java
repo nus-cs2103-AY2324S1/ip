3eddb1804e730081ee3ac88934b20ae2c593c362
beatkoin <beatkoin458@gmail.com>
2023-08-23 19:49:18 +0800

    “Level-1”

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo =
                 "██████╗░██╗░░░░░░█████╗░░█████╗░██████╗░\n" +
                 "██╔══██╗██║░░░░░██╔══██╗██╔══██╗██╔══██╗\n" +
@@ -7,11 +11,25 @@ public class Duke {
                 "██╔══██╗██║░░░░░██║░░██║██║░░██║██╔═══╝░\n" +
                 "██████╦╝███████╗╚█████╔╝╚█████╔╝██║░░░░░\n" +
                 "╚═════╝░╚══════╝░╚════╝░░╚════╝░╚═╝░░░░░";
+
+        boolean isEcho = true;
+
         System.out.println("Hello from\n" + logo);
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm BloopBot");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
+
+        while (isEcho) {
+            String strInput = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+            System.out.println(strInput);
+            System.out.println("____________________________________________________________");
+
+            if (strInput.equalsIgnoreCase("bye")) {
+                isEcho = false;
+            }
+        }
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
