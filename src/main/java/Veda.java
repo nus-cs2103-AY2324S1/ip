78649d5f247dd30c1823567b412fbcd6e7e05e70
Sebastian Tay <taysebastian95@gmail.com>
2023-08-23 10:08:22 +0800

    Implemented Level 0

---
+++ src/main/java/Veda.java
@@ -0,0 +1,16 @@
+public class Veda {
+
+    private final static String NAME = "Veda";
+
+    public static void main(String[] args) {
+        //Greet users upon initialisation
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm " + NAME);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+
+        //Exits the program
+        System.out.println("Bye. Hope to see you soon!");
+    }
+}
