c799dda454b500e319a672c41f0d2f7baa800090
Debbie Hii Wenxin <e0959749@u.nus.edu>
2023-08-24 00:25:15 +0800

    Level-1 Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String horizontalLine = "------------------------\n";
+        String horizontalLine = "   ------------------------\n";
         System.out.println(horizontalLine
-                + "GREETINGS HUMAN! I AM QLATZ! □ \n"
-                + "HOW MAY I BE OF SERVICE?\n\n"
+                + "     GREETINGS HUMAN! I AM QLATZ! □ \n"
+                + "     I AM NOW A PARROT\n\n"
                 + horizontalLine);
 
-        System.out.println("BYE!\n" + horizontalLine);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String text = scanner.nextLine();
+            if (text.equalsIgnoreCase("bye"))
+                break;
+            text = "     " + text;
+            System.out.println("\n" + horizontalLine + text + "\n" + horizontalLine);
+        }
+
+        System.out.println("     BYE!\n" + horizontalLine);
     }
 }
