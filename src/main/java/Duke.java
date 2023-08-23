701c9ee35174d3a44310a758c9253bf03b02ef7b
LordSaumya <68504323+LordSaumya@users.noreply.github.com>
2023-08-23 13:46:36 +0800

    Complete Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,10 +17,10 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
 
         String command = "";
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<Task> list = new ArrayList<Task>();
 
         System.out.println("Greetings, puny mortal. This is \n" + logo
-                + "\nThe Lord of Time. \nWhat inconsequential errand do you seek to accomplish with my immense powers?");
+                + "\nThe Lord of Time. \nWhat foolish errand do you seek to accomplish with my immense powers?");
         System.out.println(divider);
 
         while (true) {
@@ -29,14 +29,33 @@ public class Duke {
                 break;
             } else if (command.equals("list")) {
                 System.out.println(divider);
+                System.out.println(
+                        "You have somehow found the audacity to conjure up this laughable list of inconsequential endeavours:\n");
+                System.out.println(divider);
                 for (int i = 1; i <= list.size(); i++) {
                     System.out.println(i + ". " + list.get(i - 1));
                 }
                 System.out.println(divider);
+            } else if (command.startsWith("mark")) {
+                System.out.println(divider);
+                int taskNum = Integer.parseInt(command.substring(5));
+                list.get(taskNum - 1).changeStatus(true);
+                System.out.println(
+                        "Astonishingly enough, you have managed to triumph over this mind-bogglingly simple task:\n");
+                System.out.println(list.get(taskNum - 1).toString());
+                System.out.println(divider);
+            } else if (command.startsWith("unmark")) {
+                System.out.println(divider);
+                int taskNum = Integer.parseInt(command.substring(7));
+                list.get(taskNum - 1).changeStatus(false);
+                System.out.println(
+                        "In a stunning twist, this task remains untouched by the hands of progress:\n");
+                System.out.println(list.get(taskNum - 1).toString());
+                System.out.println(divider);
             } else {
                 System.out.println(divider);
                 System.out.println("added: " + command);
-                list.add(command);
+                list.add(new Task(command));
                 System.out.println(divider);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    private String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+
+    public void changeStatus(boolean isDone) {
+        this.isDone = isDone;
+    }
+}
