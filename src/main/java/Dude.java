4b12bd630068eddc46a497eb0bacd4eb5b6cd30c
vivienherq <heruoqingvivien@gmail.com>
2023-08-23 13:50:52 +0800

    rename

--- src/main/java/Dude.java
+++ src/main/java/Dude.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class Dude {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
