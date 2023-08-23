ccbc12118e44a95cab2ca1d7929b81f07df83cba
Gary <garylow2001@gmail.com>
2023-08-23 14:31:04 +0800

    Added test cases and expected results

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,7 @@ public class Duke {
     public static void main(String[] args) {
         String logo = "I'm Chewy,\n" +
                 "What can I do for you?\n";
-        System.out.println("Hello! \n" + logo);
+        System.out.println("Hello!\n" + logo);
 
         // Create a scanner object to read commands entered by the user
         Scanner scanner = new Scanner(System.in);
