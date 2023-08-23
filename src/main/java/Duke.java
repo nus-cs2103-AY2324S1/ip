75cb2dfa1c3ec740b53d010874c49c20ca799116
kiatkat <kiatkat123@gmail.com>
2023-08-23 20:46:31 +0800

    Level 1 Implemented - Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,41 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
+
+    static String softbreak = "--------------------------------------------------";
+    static String hardbreak = "——————————————————————————————————————————————————";
+
+
+    private static void greet() {
+        System.out.println(hardbreak +
+                "\n——Fingerprint match found. Verification complete. Welcome home.\n" +
+                "PRTS, at your service. What would you like to do today?\n" + hardbreak);
+    }
+
+    private static void exit() {
+        System.out.println(hardbreak + "\nFarewell. See you again soon.\n" + hardbreak);
+    }
+
     public static void main(String[] args) {
 
-        String linebreak = "——————————————————————————————————————————————————————————";
+        Scanner scanner = new Scanner(System.in);
 
-        String greeting = linebreak +
-                "\n——Fingerprint match found. Verification complete. Welcome home.\n" +
-                "PRTS, at your service. What would you like to do today?\n" + linebreak;
+        String input;
 
-        String farewell = "Farewell. See you again soon.\n" + linebreak;
+        greet();
 
-        System.out.println(greeting);
-        System.out.println(farewell);
+        while (true) {
+            input = scanner.next();
+            if (Objects.equals(input, "bye")) {
+                break;
+            }
+            System.out.println(softbreak);
+            System.out.println(input);
+            System.out.println(hardbreak);
+        }
+
+        exit();
+
     }
 }
