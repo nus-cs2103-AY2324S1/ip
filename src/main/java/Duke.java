f2f34cd198af8a35c7c484ace536f05f0f3e0106
nananakx-x <nanettekx.x3020@gmail.com>
2023-08-24 01:45:01 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
+        System.out.println("Hello friend :> My name is John, nice to meet you!\n\n" +
+                "Bye for now, hope to see you soon.");
+    }
+}
+
+/*
+public class Duke {
+    public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -8,3 +16,5 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
     }
 }
+ */
+
