84cd062b797ff6928af840bbb4ec20e68d59e8c7
aslam341 <aslam.singapore@gmail.com>
2023-08-24 04:48:30 +0800

    Implement initial skeleton version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,10 @@
 public class Duke {
     public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm David.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
