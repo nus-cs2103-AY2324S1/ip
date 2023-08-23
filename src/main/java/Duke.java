a984dead9d85943a92ca116d4dccefc3c5d97357
winson8222 <winson8222@hotmail.com>
2023-08-23 17:34:24 +0800

    Added echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,26 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
+        String linebreak = "____________________________________________________________";
+        System.out.println(linebreak);
         String greeting = "Hello! I'm chatbot\nWhat can I do for you?";
         String bye = "Bye. Hope to see you again soon!";
         System.out.println(greeting);
+        System.out.println(linebreak);
+        Scanner sc = new Scanner(System.in);
+        do {
+            String echo = sc.next();
+            if (echo.equals("bye")) {
+                break;
+            } else {
+                System.out.println(linebreak);
+                System.out.println(echo);
+                System.out.println(linebreak);
+            }
+        } while (true);
+        System.out.println(linebreak);
         System.out.println(bye);
+        System.out.println(linebreak);
 
     }
 }
