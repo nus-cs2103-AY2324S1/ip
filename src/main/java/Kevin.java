75311bd6e968c7111f7cd57d4be0fc6a4ed307e5
aliciamichellew <aliciamichellew@gmail.com>
2023-08-23 14:52:12 +0800

    Change bot name and add greetings

---
+++ src/main/java/Kevin.java
@@ -0,0 +1,22 @@
+public class Kevin {
+    public static final String HORIZONTAL_LINE = "_".repeat(70);
+    public static final String BOT_NAME = "\n" +
+            " _   __ _____ _   _ _____ _   _ \n" +
+            "| | / /|  ___| | | |_   _| \\ | |\n" +
+            "| |/ / | |__ | | | | | | |  \\| |\n" +
+            "|    \\ |  __|| | | | | | | . ` |\n" +
+            "| |\\  \\| |___\\ \\_/ /_| |_| |\\  |\n" +
+            "\\_| \\_/\\____/ \\___/ \\___/\\_| \\_/\n";
+
+    public static void main(String[] args) {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Hello! I'm" + BOT_NAME);
+        System.out.println("What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE);
+    }
+}
+
+
+
