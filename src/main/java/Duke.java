08381011c67cd1d4cdd0067e566b8a550666ef01
HEEaZ <do.u.noe.imm@gmail.com>
2023-08-23 22:16:14 +0800

    Added echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,54 @@
+import java.util.Scanner;
+
 public class Duke {
-    private static final String LINEBREAK = "____________________________________________________________________";
+    private static final String LINEBREAK = "    ＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿";
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        greet();
+
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            displayMessage(input);
+            input = sc.nextLine();
+        }
+
+        bye();
+    }
+
+    private static void bye() {
+        String byeMsg = "Bye. Hope to see you again soon!";
+
+        displayMessage(byeMsg);
+    }
+
+    private static void greet() {
         String logo = " _           _        \n"
                     + "| |    _   _| | _____ \n"
                     + "| |   | | | | |/ / _ \\\n"
                     + "| |__ | |_| |   <  __/\n"
                     + "|____| \\__,_|_|\\_\\___|\n";
+        String greetingMsg = "Hello! I'm Luke \n"
+                        + "What can I do for you?\n";
+
         System.out.println("Hello from\n" + logo);
 
-        String greetingMsg = "Hello! I'm Luke \n"
-                        + "What can I do for you?";
-        System.out.println(LINEBREAK);
-        System.out.println(greetingMsg);
-
-        String byeMsg = "Bye. Hope to see you again soon!";
-        System.out.println(LINEBREAK);
-        System.out.println(byeMsg);
-        System.out.println(LINEBREAK);
-
-
-
-
+        displayMessage(greetingMsg);
     }
+
+    /**
+     * Displays the given message between horizontal lines
+     *
+     * @param msg String to output
+     */
+    private static void displayMessage(String msg) {
+        System.out.println(LINEBREAK);
+        System.out.println(indent(msg));
+        System.out.println(LINEBREAK);
+    }
+
+    private static String indent(String msg) {
+        return msg.replaceAll("(?m)^", "\t");
+    }
+
 }
