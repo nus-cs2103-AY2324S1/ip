902371b5c96c0f21a2687209d40bfc8e01c7bc60
heran lang <namedx9@gmail.com>
2023-08-24 02:27:02 +0800

    add echo, make it a parrot

--- src/main/java/Moss.java
+++ src/main/java/Moss.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Moss {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
+        String greet = "____________________________________________________________\n"
                 + "Hello! I'm Moss \n"
                 + "What can I do for you? \n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
-        System.out.println(logo);
+        System.out.println(greet);
+        Scanner sc = new Scanner(System.in);
+        String message = sc.next();
+        while (!message.equals("bye")) {
+            echo(message);
+            message = sc.next();
+        }
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void echo(String message) {
+        System.out.println("____________________________________________________________");
+        System.out.println(message);
+        System.out.println("____________________________________________________________");
     }
 }
