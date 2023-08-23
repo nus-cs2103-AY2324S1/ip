136582f6481ba9c7d396676bfc3447744b79529d
nicleongyj <e0968256@u.nus.edu>
2023-08-23 15:56:45 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -10,6 +12,16 @@ public class Duke {
         System.out.println("Hello! I am TaskMaster!");
         System.out.println("What can I do for you today?");
         System.out.println("____________________________________________________________");
+
+        boolean loggedIn = true;
+
+        while (loggedIn) {
+            String userInput = scanner.nextLine();
+            System.out.println("Taskmaster: " + userInput + "\n");
+            if (userInput.equals("bye")) {
+                loggedIn = false;
+            }
+        }
 
         System.out.println("____________________________________________________________");
         System.out.println("Bye! Hope to see you again!");
