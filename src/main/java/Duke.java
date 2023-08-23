25de10bef57e88f45213fc37d68ec76a5b343ff7
wesho1107 <wesho1107@gmail.com>
2023-08-23 17:02:48 +0800

    added echo method and abstraction

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,42 @@
+import java.util.Scanner;
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+    static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
 
-        String logo_bird = "(• >       (• >       (• >       (• >       (• >       (• >\n"
-                        +  "/))        /))        /))        /))        /))        /))\n"
-                        +  " ``         ``         ``         ``         ``         ``\n";
+    static String divider = "    ____________________________________________________________\n";
 
-        String greet = "____________________________________________________________\n"
-                + "Hello! I'm Birdy\n"
-                + "Chirp chirp! How can I help?\n"
-                + "____________________________________________________________\n"
-                + "Chirp! See you around!\n"
-                + "____________________________________________________________\n";
-        System.out.println("Chirp chirp!\n" + logo_bird + greet);
+    static String logo_bird = "    (• >       (• >       (• >       (• >       (• >       (• >\n"
+            +  "    /))        /))        /))        /))        /))        /))\n"
+            +  "     ``         ``         ``         ``         ``         ``\n"
+            + divider;
+
+    static String greet = "    Hello! I'm Birdy\n"
+            + "    chirp chirp! How can I help?\n"
+            + divider;
+
+    static String parting = divider + "    chirp! See you around!\n"
+            + divider;
+    public static void echo() {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println(divider + "    " + input + "\n" + divider);
+            }
+        }
     }
+    public static void main(String[] args) {
+        System.out.println("    chirp chirp!\n" + logo_bird + greet);
+
+        echo();
+
+        System.out.println(parting);
+    }
+
+
 }
