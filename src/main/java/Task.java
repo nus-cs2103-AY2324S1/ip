24ba7c1cad011455db2c8c5318a94794d3b33d52
tiongMax <maxtiongcs16@gmail.com>
2023-08-24 00:11:22 +0800

    Attempt on level 3.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+
+        String result = "[" + getStatusIcon() + "] " + this.description;
+        return result;
+    }
 }
+
+
