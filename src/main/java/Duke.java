7eac7bbc64f5889ce2d4657a3fcb670bad5073dc
teeheehoo\jierongu <jiarongchoo@gmail.com>
2023-08-23 23:19:45 +0800

    getting the bot to echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,21 @@
+
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Gideon");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(userInput);
+            }
+        }
+        scanner.close();
     }
 }
