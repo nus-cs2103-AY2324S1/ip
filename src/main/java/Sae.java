2ef1c24da91e4a1186457075c6070e9d739b8a15
Saezenn <teng.hxa@gmail.com>
2023-08-23 23:18:01 +0800

    Level-1, Echo

--- src/main/java/Sae.java
+++ src/main/java/Sae.java
@@ -2,6 +2,17 @@ import java.util.Scanner;
 public class Sae {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Sae\nWhat can I do for you?");
+
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            String str = input.nextLine();
+            if (str.equals("bye")) {
+                break;
+            }
+            else {
+                System.out.println(str);
+            }
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
