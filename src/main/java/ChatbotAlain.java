fb7901d77f2888e3cdf174b38779d702c406ade6
AlainS87 <1950834052@qq.com>
2023-08-23 16:01:07 +0800

    Level0 completed

---
+++ src/main/java/ChatbotAlain.java
@@ -0,0 +1,10 @@
+public class ChatbotAlain {
+    public static void main(String[] args) {
+        String renameAndGreeting = "____________________________________________________________\n"
+                + " Hello! I'm Alain\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n";
+        System.out.println(renameAndGreeting);
+    }
+}
