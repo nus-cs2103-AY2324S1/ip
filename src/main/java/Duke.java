29929c8c2cd4fe5fce65e8c2226f83393e7f32ad
WangCheng0116 <e1088136@u.nus.edu>
2023-08-23 23:53:12 +0800

    Rename bot and initialize skeleton

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
+    private static final String botName = "cc";
+
+    private static void greet() {
+        System.out.println("Hello! I'm " + botName);
+        System.out.println("What can I do for you?");
+    }
+
+    private static void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke.greet();
+        Duke.bye();
     }
 }
