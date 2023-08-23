a6d70dcf551d026cb352004a7071594422cf6508
Ruth Lim <>
2023-08-23 16:02:33 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,16 +14,29 @@ public class Duke {
         System.out.println("____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         while (true) {
             String userCommand = scanner.nextLine();
+
             if (userCommand.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            } else {
+            } else if (userCommand.equals("list")) {
                 System.out.println("____________________________________________________________");
-                System.out.println(userCommand);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = userCommand;
+                taskCount++;
+
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + userCommand);
                 System.out.println("____________________________________________________________");
             }
 
