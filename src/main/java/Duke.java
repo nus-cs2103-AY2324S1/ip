e1fa0d3c153280418f9cd53d130cbe4035209218
brendanneojw <e0726880@u.nus.edu>
2023-08-23 18:46:07 +0800

    Implemented Level 1: Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,31 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         printHoriLine();
         System.out.println("WEEWOOWEEWOO WELCOME! I'm Siren");
         System.out.println("What can I do for you?");
         printHoriLine();
-        System.out.println("WEEEWOOWEEWOO GOODBYE! Hope to see you again soon!");
-        printHoriLine();
+        takeInput();
     }
 
     public static void printHoriLine() {
         System.out.println("____________________________________________________________");
+    }
+
+    public static void takeInput() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            printHoriLine();
+            if (!input.equals("bye")) {
+                System.out.println(input);
+                printHoriLine();
+            }
+            else {
+                System.out.println("WEEEWOOWEEWOO GOODBYE! Hope to see you again soon!");
+                printHoriLine();
+                break;
+            }
+        }
     }
 }
