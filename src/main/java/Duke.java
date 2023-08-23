3f3c680b29d067d40fe355f0edeecd6ed9e4ba1b
vishnu <vishnuprasath639@gmail.com>
2023-08-23 17:10:05 +0800

    * Make bot add tasks to list * Make bot list out tasks with "list" command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+
+    /** An ArrayList to hold tasks entered by the User. */
+    private static ArrayList<String> tasks;
 
     /**
      * Draws a line separating each conversation.
@@ -10,20 +14,52 @@ public class Duke {
         System.out.println("\t____________________________________________________________");
     }
 
+    /**
+     * Adds the given task to the tasks ArrayList.
+     * Prints that the task has been added.
+     *
+     * @param task The task to add.
+     *
+     */
+    public static void addTask(String task) {
+        tasks.add(task);
+        System.out.println("\tadded: " + task);
+        drawLine();
+    }
+
+    /**
+     * Lists the tasks present in tasks ArrayList.
+     */
+    public static void listTasks() {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.print("\t");
+            System.out.print(i + 1);
+            System.out.print(". " + tasks.get(i) + "\n");
+        }
+        drawLine();
+    }
+
 
     /**
      * Handles input from the user accordingly.
      *
      */
     public static void handleUserInput() {
-        String userInput;
+
         Scanner scanner = new Scanner(System.in);
+        tasks = new ArrayList<String>();
         System.out.println();
+        String userInput;
         userInput = scanner.nextLine();
         drawLine();
+
         while (!userInput.equals("bye")) {
-            System.out.println("\t" + userInput);
-            drawLine();
+            if (userInput.equals("list")) {
+                listTasks();
+            } else {
+                addTask(userInput);
+            }
+
             System.out.println();
             userInput = scanner.nextLine();
             drawLine();
