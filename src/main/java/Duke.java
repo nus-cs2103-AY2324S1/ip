859bb2ee5b847d6b4d1542c98338fb24ab85b77d
Andre Chua <110965035+andrechuakj@users.noreply.github.com>
2023-08-23 23:12:32 +0800

    Added echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,32 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        String line = "────────────────────────────────────────────────────────────────────\n";
         String logo = " ___  _ _                  ___  _          _    ___        _    ___ \n"
                 + "|  _|| | | ___  _ _  _ _  |  _>| |_  ___ _| |_ | . > ___ _| |_ |_  |\n"
                 + "| |  \\   // . \\| | || '_> | <__| . |<_> | | |  | . \\/ . \\ | |    | |\n"
                 + "| |_  |_| \\___/`___||_|   `___/|_|_|<___| |_|  |___/\\___/ |_|   _| |\n"
                 + "|___|                                                          |___|\n";
 
-        String greet = " Hello! I'm\n" + logo + "What can I do for you?\n";
+        String greet = line +
+                "Hello! I'm\n"
+                + logo
+                + "What can I do for you?\n"
+                + line;
         System.out.println(greet);
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            String userInput = input.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            } else {
+                System.out.println(line + userInput + "\n"+ line);
+            }
+        }
 
-        String sendOff = "Bye. Hope to see you again soon!";
+        String sendOff = line
+                + "Bye. Hope to see you again soon!\n"
+                + line;
         System.out.println(sendOff);
     }
 }
