23756dcf08b1aecc5ae7a3cdd00e2b96612af458
ryam <ryanngohenrui@gmail.com>
2023-08-23 14:52:11 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,44 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello I'm RyamBot");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
-    }
+	public static void main(String[] args) {
+		printLine();
+		System.out.println("Hello I'm RyamBot");
+		System.out.println("What can I do for you?");
+		printLine();
+		queryBot();
+	}
+
+	static void printLine() {
+		System.out.println("____________________________________________________________");
+	}
+
+	static void queryBot() {
+		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+		String query = myObj.nextLine();  // Read user input
+//		  case "list": {
+//				printLine();
+//				System.out.println("list");
+//				printLine();
+//				queryBot();
+//				break;
+//			}
+//			case "blah": {
+//				printLine();
+//				System.out.println("blah");
+//				printLine();
+//				queryBot();
+//				break;
+//			}
+		if (query.equals("bye")) {
+			printLine();
+			System.out.println("Bye. Hope to see you again soon!");
+			printLine();
+		} else {
+			printLine();
+			System.out.println(query);
+			printLine();
+			queryBot();
+		}
+	}
 }
