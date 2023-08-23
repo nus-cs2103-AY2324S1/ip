30c8bdadaa7c2e9da80911bd2d0ad155bdb26c2b
HEEaZ <do.u.noe.imm@gmail.com>
2023-08-23 21:55:00 +0800

    Add greeting and bye msg

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 public class Duke {
+    private static final String LINEBREAK = "____________________________________________________________________";
     public static void main(String[] args) {
         String logo = " _           _        \n"
                     + "| |    _   _| | _____ \n"
@@ -6,5 +7,19 @@ public class Duke {
                     + "| |__ | |_| |   <  __/\n"
                     + "|____| \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String greetingMsg = "Hello! I'm Luke \n"
+                        + "What can I do for you?";
+        System.out.println(LINEBREAK);
+        System.out.println(greetingMsg);
+
+        String byeMsg = "Bye. Hope to see you again soon!";
+        System.out.println(LINEBREAK);
+        System.out.println(byeMsg);
+        System.out.println(LINEBREAK);
+
+
+
+
     }
 }
