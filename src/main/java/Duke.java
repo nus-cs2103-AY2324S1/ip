9c5b72e5428ad65cd0d8a55c8134c73b5f3ff7dd
kiatkat <kiatkat123@gmail.com>
2023-08-24 03:04:33 +0800

    Level 5 Implemented. Code restructured for future extensibility.
    Much of the input validation already existed prior to this commit, but
    have been consolidated via the DukeException exception.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,11 +7,12 @@ public class Duke {
     static String softbreak = "--------------------------------------------------";
     static String hardbreak = "——————————————————————————————————————————————————";
 
+    ArrayList<Task> tasks = new ArrayList<>();
 
     /**
      * Greets the user with a predefined message.
      */
-    private static void greet() {
+    private void greet() {
         System.out.println(hardbreak +
                 "\n...Fingerprint match found. Verification complete. Welcome home.\n" +
                 "PRTS, at your service. What would you like to do today?\n" + hardbreak);
@@ -20,7 +21,7 @@ public class Duke {
     /**
      * Bids the user farewell with a predefined message.
      */
-    private static void exit() {
+    private void exit() {
         System.out.println(hardbreak + "\nFarewell. See you again soon.\n" + hardbreak);
     }
 
@@ -29,7 +30,7 @@ public class Duke {
      * Displays a list of the user's past tasks.
      * @param tasks An array containing the user's tasks.
      */
-    private static void list(ArrayList<Task> tasks) {
+    private void list(ArrayList<Task> tasks) {
 
         System.out.println("Here are your tasks for today.");
 
@@ -46,13 +47,138 @@ public class Duke {
 
     }
 
+    /**
+     * Takes the user's input and parses it to execute actions.
+     * @param input The user's input read in from the Scanner.
+     */
+    private void parseInput(String input) throws DukeException {
+
+        String[] splits = input.strip().split(" ", 2);
+        String command = splits[0].toLowerCase();
+        String detail = splits.length == 2 ? splits[1].strip() : "";
+
+        switch (command) {
+
+            case "todo":
+
+                if (detail.isBlank())
+                    throw new DukeException("I can't read your mind. Do add a description.");
+                System.out.println("Understood. No rest for the weary, eh?");
+                tasks.add(new Todo(detail));
+                System.out.println(tasks.get(tasks.size() - 1));
+                break;
+
+            case "deadline":
+
+                if (detail.isBlank())
+                    throw new DukeException("I can't read your mind. Do add a description.");
+                try {
+                    String[] deadlineSplits = detail.split("/by", 2);
+                    String deadlineDescription = deadlineSplits[0].strip();
+                    String deadline = deadlineSplits[1].strip();
+                    if (deadline.isBlank() || deadlineDescription.isBlank())
+                        throw new DukeException("I can't read your mind. Do add more details.");
+                    System.out.println("Alright. I'll make sure you don't forget it.");
+                    tasks.add(new Deadline(deadlineDescription, deadline));
+                    System.out.println(tasks.get(tasks.size() - 1));
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    throw new DukeException("Please format your description properly.");
+                }
+                break;
+
+            case "event":
+
+                if (detail.isBlank())
+                    throw new DukeException("I can't read your mind. Do add a description.");
+                try {
+                    String[] eventSplit1 = detail.split("/from", 2);
+                    String[] eventSplit2 = eventSplit1[1].split("/to", 2);
+                    String eventDescription = eventSplit1[0].strip();
+                    String startDate = eventSplit2[0].strip();
+                    String endDate = eventSplit2[1].strip();
+                    if (eventDescription.isBlank() || startDate.isBlank() || endDate.isBlank())
+                        throw new DukeException("I can't read your mind. Do add more details.");
+                    System.out.println("Roger that. Preparations will be underway.");
+                    tasks.add(new Event(eventDescription, startDate, endDate));
+                    System.out.println(tasks.get(tasks.size() - 1));
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    throw new DukeException("Please format your description properly.");
+                }
+                break;
+
+            case "mark":
+
+                if (detail.isBlank())
+                    throw new DukeException("I can't read your mind. Provide a suitable index.");
+                try {
+                    int index = Integer.parseInt(detail) - 1;
+                    Task task = tasks.get(index);
+                    task.markDone();
+                    System.out.println("Mission accomplished.");
+                    System.out.println(task);
+                } catch (NumberFormatException e) {
+                    throw new DukeException("I can't understand that number.");
+                } catch (IndexOutOfBoundsException e) {
+                    throw new DukeException("Provide a number within range, please.");
+                }
+                break;
+
+            case "unmark":
+
+                if (detail.isBlank())
+                    throw new DukeException("I can't read your mind. Provide a suitable index.");
+                try {
+                    int index = Integer.parseInt(detail) - 1;
+                    Task task = tasks.get(index);
+                    task.markUndone();
+                    System.out.println("Uncharacteristic of you. More work has been added to the pile.");
+                    System.out.println(task);
+                } catch (NumberFormatException e) {
+                    throw new DukeException("I can't understand that number.");
+                } catch (IndexOutOfBoundsException e) {
+                    throw new DukeException("Provide a number within range, please.");
+                }
+                break;
+
+            case "list":
+
+                list(tasks);
+                break;
+
+            case "thanks":
+
+                System.out.println("...No problem.");
+                break;
+
+            case "zzz":
+
+                if (!tasks.isEmpty()) {
+                    System.out.println("There's still lots of work that needs to be done. " +
+                            "We can't afford to have you resting.");
+                } else {
+                    System.out.println("...have a good rest.");
+                }
+                break;
+
+            case "hi":
+
+                System.out.println("Greetings.");
+                break;
+
+            default:
+
+                System.out.println("Sorry, I didn't quite catch that. Are you having enough sleep?");
+
+        }
+
+    }
+
     public static void main(String[] args) {
 
+        Duke prts = new Duke();
         Scanner scanner = new Scanner(System.in);
 
-        ArrayList<Task> tasks = new ArrayList<>();
-
-        greet();
+        prts.greet();
 
         while (true) {
 
@@ -64,87 +190,17 @@ public class Duke {
 
             System.out.println(softbreak);
 
-            if (input.startsWith("todo ")) {
-
-                System.out.println("Understood. No rest for the weary, eh?");
-                tasks.add(new Todo(input.substring(5)));
-                System.out.println(tasks.get(tasks.size() - 1));
-
-            } else if (input.startsWith("deadline ")) {
-
-                String[] parts = input.split("/");
-                if (parts.length != 2) {
-                    System.out.println("Please enter your request properly.\n" + hardbreak);
-                    continue;
-                }
-                System.out.println("Alright. I'll make sure you don't forget it.");
-                tasks.add(new Deadline(parts[0].substring(9), parts[1].substring(3)));
-                System.out.println(tasks.get(tasks.size() - 1));
-
-            } else if (input.startsWith("event ")) {
-
-                String[] parts = input.split("/");
-                if (parts.length != 3) {
-                    System.out.println("Please enter your request properly.\n" + hardbreak);
-                    continue;
-                }
-                System.out.println("Roger that. Preparations will be underway.");
-                tasks.add(new Event(parts[0].substring(6), parts[1].substring(5),
-                        parts[2].substring(3)));
-                System.out.println(tasks.get(tasks.size() - 1));
-
-            } else if (input.startsWith("mark ")) {
-
-                int index = Integer.parseInt(input.substring(5)) - 1;
-                if (tasks.size() <= index || index < 0) {
-                    System.out.println("That isn't a valid input. Please try again.\n" + hardbreak);
-                    continue;
-                }
-                Task task = tasks.get(index);
-                task.markDone();
-                System.out.println("Mission accomplished.");
-                System.out.println(task);
-
-            } else if (input.startsWith("unmark ")) {
-
-                int index = Integer.parseInt(input.substring(7)) - 1;
-                if (tasks.size() <= index || index < 0) {
-                    System.out.println("That isn't a valid input. Please try again.\n" + hardbreak);
-                    continue;
-                }
-                Task task = tasks.get(index);
-                task.markUndone();
-                System.out.println("Uncharacteristic of you. More work has been added to the pile.");
-                System.out.println(task);
-
-            } else if (input.equals("list")) {
-
-                list(tasks);
-
-            } else if (input.equals("thanks")) {
-
-                System.out.println("...No problem.");
-
-            } else if (input.equals("zzz")) {
-
-                if (!tasks.isEmpty()) {
-                    System.out.println("There's still lots of work that needs to be done." +
-                            "We can't afford to have you resting.");
-                } else {
-                    System.out.println("...have a good rest.");
-                }
-
-            } else {
-
-                System.out.println("Sorry, I didn't quite catch that. Are you having enough sleep?");
-
+            try {
+                prts.parseInput(input);
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
+            } finally {
+                System.out.println(hardbreak);
             }
-
-            System.out.println(hardbreak);
 
         }
 
-        exit();
+        prts.exit();
 
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -8,15 +8,19 @@ public class Task {
         this.isDone = false;
     }
 
-    public void markDone() {
+    public void markDone() throws DukeException{
         if (!this.isDone) {
             this.isDone = true;
+        } else {
+            throw new DukeException("It's already done.");
         }
     }
 
-    public void markUndone() {
+    public void markUndone() throws DukeException {
         if (this.isDone) {
             this.isDone = false;
+        } else {
+            throw new DukeException("This hasn't been done yet...");
         }
     }
 
