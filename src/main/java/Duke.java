fc84eaaa609f3ecdceb2f3eca7ab923506788849
WinSheng1 <33998278+WinSheng1@users.noreply.github.com>
2023-08-23 21:33:40 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String horizontalline = "______________________________________________";
+
+        System.out.println(horizontalline +
+                "\nHello i'm ChatterBuddy\n" +
+                "Is there anything I can assist you with today?\n" +
+                horizontalline +
+                "\nGoodbye.");
     }
 }
