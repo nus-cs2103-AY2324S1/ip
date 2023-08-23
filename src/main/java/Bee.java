8beb47c592ce2b7bd5152eed03ebb4638082374f
leezhengjing <zhengjing.lee86@gmail.com>
2023-08-24 00:24:41 +0800

    Completed Level-0
    - Changed the name of chatbot to Bee
    - Replaced ASCII art to Bee
    - Added new print statements following the example code

---
+++ src/main/java/Bee.java
@@ -0,0 +1,14 @@
+public class Bee {
+    public static void main(String[] args) {
+        String logo = " \n" +
+                "__________               \n" +
+                "\\______   \\ ____   ____  \n" +
+                " |    |  _// __ \\_/ __ \\ \n" +
+                " |    |   \\  ___/\\  ___/ \n" +
+                " |______  /\\___  >\\___  >\n" +
+                "        \\/     \\/     \\/ \n";
+        System.out.println("Hello! I'm " + logo + ".\n");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again!");
+    }
+}
