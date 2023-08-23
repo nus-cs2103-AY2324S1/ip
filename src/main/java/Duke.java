3e0959f7d6893d2b8e1b2e8fa84958251fc306fd
tiongMax <maxtiongcs16@gmail.com>
2023-08-24 00:00:03 +0800

    Attempt on level 3.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Duke {
 
     public static String dash = "\t-------------------------------------------------------------";
-    public static ArrayList<String> taskList = new ArrayList<>();
+    public static ArrayList<Task> taskList = new ArrayList<>();
 
     public static void welomeMessage() {
 
@@ -34,20 +34,42 @@ public class Duke {
         System.out.println(dash);
         System.out.println("\t added: " + message + "\n");
         System.out.println(dash);
-        taskList.add(message);
+        taskList.add(new Task(message));
     }
 
     public static void listAllTask() {
 
         System.out.println(dash);
+        System.out.println("\tHere are the tasks in your list: ");
         if (taskList.size() > 0) {
             for (int i = 1; i < taskList.size() + 1; i++) {
-                System.out.println("\t" + i + ". " + taskList.get(i - 1));
+                System.out.println("\t" + i + "." + taskList.get(i - 1).toString());
             }
         }
         System.out.println();
         System.out.println(dash);
     }
+
+    public static void mark(int i) {
+
+        taskList.get(i - 1).mark();
+        System.out.println(dash);
+        System.out.println(" \tNice! I've marked this task as done:");
+        System.out.println("\t  " + taskList.get(i - 1).toString());
+        System.out.println();
+        System.out.println(dash);
+    }
+
+    public static void unmark(int i) {
+
+        taskList.get(i - 1).unmark();
+        System.out.println(dash);
+        System.out.println(" \tOk! I've marked this task as not done yet:");
+        System.out.println("\t  " + taskList.get(i - 1).toString());
+        System.out.println();
+        System.out.println(dash);
+    }
+
     public static void main(String[] args) {
 
         welomeMessage();
@@ -58,6 +80,18 @@ public class Duke {
 
             if (message.equals("list")) {
                 listAllTask();
+            } else if (message.length() >= 4 && message.substring(0, 4).equals("mark")) {
+
+                int index = Integer.parseInt(message.substring(message.length() - 1));
+                if (index > 0 && index <= taskList.size()) {
+                    mark(Integer.parseInt(message.substring(message.length() - 1)));
+                }
+            } else if (message.length() >= 6 && message.substring(0, 6).equals("unmark")) {
+
+                int index = Integer.parseInt(message.substring(message.length() - 1));
+                if (index > 0 && index <= taskList.size()) {
+                    unmark(Integer.parseInt(message.substring(message.length() - 1)));
+                }
             } else {
                 addTask(message);
             }
