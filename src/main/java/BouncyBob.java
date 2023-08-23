7ec69ec3dc2838c1b833d2981164b29a38f7d62a
JCSnap <e0970167@u.nus.edu>
2023-08-23 13:22:44 +0800

    refractored command blocks into individual functions

--- src/main/java/BouncyBob.java
+++ src/main/java/BouncyBob.java
@@ -12,6 +12,50 @@ public class BouncyBob {
         System.out.println("[" + marking + "]" + " " + task.getName());
     }
 
+    public static void printBye() {
+        System.out.println(TOP_BORDER);
+        System.out.println(MIDDLE_BORDER);
+        System.out.println("║        BouncyBob: Bye! Bounce back soon!     ║");
+        System.out.println(MIDDLE_BORDER);
+        System.out.println(BOTTOM_BORDER);
+    }
+
+    public static void printDatabase(Task[] database, int pointer) {
+        System.out.println(TOP_BORDER);
+        System.out.println("Here are your tasks! ");
+        for (int i = 0; i < pointer; i++) {
+            Task curTask = database[i];
+            printTaskStatus(curTask);
+        }
+        System.out.println(BOTTOM_BORDER);
+    }
+
+    public static void modifyTask(String[] parts, Task[] database) {
+        String action = parts[0];
+        int index = Integer.parseInt(parts[1]); // This assumes that second part contains an int
+        Task task = database[index];
+        System.out.println(TOP_BORDER);
+        switch(action) {
+            case "mark":
+                System.out.println("You've done it, very bouncy!");
+                task.setDone();
+                printTaskStatus(task);
+                break;
+            case "unmark":
+                System.out.println("Gotta pump for air! It's unmarked! ");
+                task.setUnDone();
+                printTaskStatus(task);
+                break;
+        }
+        System.out.println(BOTTOM_BORDER);
+    }
+
+    public static void printTaskAdded(String userInput, Task[] database, int pointer) {
+        System.out.println(TOP_BORDER);
+        System.out.println("Added to database: " + userInput);
+        database[pointer] = new Task(userInput);
+        System.out.println(BOTTOM_BORDER);
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Task[] database = new Task[100];
@@ -29,44 +73,15 @@ public class BouncyBob {
             String userInput = scanner.nextLine();
             String[] parts = userInput.split(" ");
             if (userInput.equals("bye")) {
-                System.out.println(TOP_BORDER);
-                System.out.println(MIDDLE_BORDER);
-                System.out.println("║        BouncyBob: Bye! Bounce back soon!     ║");
-                System.out.println(MIDDLE_BORDER);
-                System.out.println(BOTTOM_BORDER);
+                printBye();
                 break;
             } else if (userInput.equals("list")) {
-                System.out.println(TOP_BORDER);
-                System.out.println("Here are your tasks! ");
-                for (int i = 0; i < pointer; i++) {
-                    Task curTask = database[i];
-                    printTaskStatus(curTask);
-                }
-                System.out.println(BOTTOM_BORDER);
+                printDatabase(database, pointer);
             } else if (parts.length == 2) {
-                String action = parts[0];
-                int index = Integer.parseInt(parts[1]); // This assumes that second part contains an int
-                Task task = database[index];
-                System.out.println(TOP_BORDER);
-                switch(action) {
-                    case "mark":
-                        System.out.println("You've done it, very bouncy!");
-                        task.setDone();
-                        printTaskStatus(task);
-                        break;
-                    case "unmark":
-                        System.out.println("Gotta pump for air! It's unmarked! ");
-                        task.setUnDone();
-                        printTaskStatus(task);
-                        break;
-                }
-                System.out.println(BOTTOM_BORDER);
+                modifyTask(parts, database);
             } else {
-                System.out.println(TOP_BORDER);
-                System.out.println("Added to database: " + userInput);
-                database[pointer] = new Task(userInput);
+                printTaskAdded(userInput, database, pointer);
                 pointer++;
-                System.out.println(BOTTOM_BORDER);
             }
         }
     }
