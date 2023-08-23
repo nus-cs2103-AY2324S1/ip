385cee9485f0b439a1a24a60d7128a9014b09b55
Sebastian Tay <taysebastian95@gmail.com>
2023-08-23 15:08:37 +0800

    Add, List feature

--- src/main/java/Veda.java
+++ src/main/java/Veda.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class Veda {
 
     private final static String NAME = "Veda";
+    private static ArrayList<String> tasks = new ArrayList<String>(100);
+
+    private static void addTask(String task) {
+        if (tasks.add(task)) {
+            System.out.println("added in mission: " + task);
+        } else {
+            System.out.println("System is unable to accommodate the new mission");
+        }
+    }
 
     public static void main(String[] args) {
         //Greet users upon initialisation
@@ -15,12 +26,18 @@ public class Veda {
         while (true) {
             String input = inScanner.nextLine();
 
-            //User wishes to exit the program
             if (input.toLowerCase().equals("bye")) {
+                //User wishes to exit the program
                 break;
+            } else if (input.toLowerCase().equals("list")) {
+                //User wishes to see his listed missions
+                tasks.forEach( task -> System.out.println(tasks.indexOf(task) + ". " + task));
+
+                continue;
             }
 
-            System.out.println(input);
+            //Add tasks
+            addTask(input);
         }
 
 
