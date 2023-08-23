e33f377b656de95a2dd596a5c803330aae3aedfd
sunzihan23 <e0970553@u.nus.edu>
2023-08-23 19:13:21 +0800

    Added echo feature and exit on bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class Duke {
     private static void greet() {
         System.out.println("–––––––––––––––––––––––––––––––––––––––––");
         System.out.println("Hello! I'm Bot");
         System.out.println("What can I do for you?");
+        System.out.println("–––––––––––––––––––––––––––––––––––––––––");
     }
 
     private static void exit() {
@@ -10,8 +13,21 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("–––––––––––––––––––––––––––––––––––––––––");
     }
+
+    private static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println("–––––––––––––––––––––––––––––––––––––––––");
+            System.out.println(input);
+            System.out.println("–––––––––––––––––––––––––––––––––––––––––");
+            input = scanner.nextLine();
+        }
+        Duke.exit();
+    }
     public static void main(String[] args) {
         Duke.greet();
-        Duke.exit();
+        Duke.echo();
     }
 }
