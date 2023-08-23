029ef0a5e7db2e9286638e9457a0649a37a5b3b1
Daphne789 <daphneshaine933@gmail.com>
2023-08-23 20:01:38 +0800

    Completed Level-1

--- src/main/java/EchoBot.java
+++ src/main/java/EchoBot.java
@@ -1,13 +1,27 @@
+import java.util.Scanner;
 public class EchoBot {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        String horizontalLine = "_____________________________________________\n";
-        System.out.println(horizontalLine + "Hello! I'm EchoBot\n" + logo);
-        System.out.println("What can I do for you?");
-        System.out.println(horizontalLine + "Bye. Hope to see you again soon!\n" + horizontalLine);
+        Scanner scanner = new Scanner(System.in);
+        String logo = "    ____        _        \n"
+                + "   |  _ \\ _   _| | _____ \n"
+                + "   | | | | | | | |/ / _ \\\n"
+                + "   | |_| | |_| |   <  __/\n"
+                + "   |____/ \\__,_|_|\\_\\___|\n";
+        String horizontalLine = "   _____________________________________________\n";
+        System.out.println(horizontalLine + "   Hello! I'm EchoBot\n" + logo);
+        System.out.println("   What can I do for you?\n" + horizontalLine);
+
+        while(true) {
+            // Read the user input
+            String userInput = scanner.nextLine().trim();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println(horizontalLine + "   Bye. Hope to see you again soon!\n" + horizontalLine);
+                break;
+            }
+
+            //Echo the user input
+            System.out.println(horizontalLine + "   " + userInput + "\n" + horizontalLine);
+        }
     }
 }
