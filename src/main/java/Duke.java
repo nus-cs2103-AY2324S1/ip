e61fab800a7eeefe3885d7eb99ac64921e8d9fb8
remuslum <57741494+remuslum@users.noreply.github.com>
2023-08-23 16:01:57 +0800

    Added Rename, Greet and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,10 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        System.out.println("Hello! I'm Chad");
+        System.out.println("What can I do for you?");
+        System.out.println();
+        System.out.println("Bye. Hope to see you again");
     }
 }
