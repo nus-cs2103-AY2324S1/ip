961a09137fa7feb60d023480ba64220bcffd39a1
ShashahChk <shashah@Shashamins-MacBook-Pro.local>
2023-08-23 20:01:34 +0800

    echo command back and exit when bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String welcomeMessage = "____________________________________________________________\n" +
                 " Hello! I'm Blob\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
         System.out.println(welcomeMessage);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String userCommand = scanner.nextLine();
+
+            if ("bye".equals(userCommand)) {
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(userCommand);
+            System.out.println("____________________________________________________________");
+        }
+
+        System.out.println(" Bye. Come talk to Blob again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
