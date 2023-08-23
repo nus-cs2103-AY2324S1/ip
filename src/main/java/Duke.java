88875172a69214f1c9e4439bbe25037d11fca212
Jingya <e0968387@u.nus.edu>
2023-08-24 00:24:54 +0800

    Added add-to-list functionality and listing command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,14 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     private static String NAME = "Moira";
     private static String SPACER = "--------------------------------------------------------------------------";
     private static boolean IS_RECEIVING_INPUT = false;
+    private static ArrayList<String> ALL_USER_INPUTS;
 
     public static void main(String[] args) {
+        ALL_USER_INPUTS = new ArrayList<>();
         greet();
         while (IS_RECEIVING_INPUT) {
             getUserInput();
@@ -31,12 +34,29 @@ public class Duke {
     private static void getUserInput() {
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
-        if (!userInput.equals("bye")) {
+        if (userInput.equals("bye")) {
+            IS_RECEIVING_INPUT = false;
+        } else if (userInput.equals("list")) {
             System.out.println(SPACER);
-            System.out.println(userInput);
+            printAllUserInputs();
             System.out.println(SPACER);
         } else {
-            IS_RECEIVING_INPUT = false;
+            addInputToList(userInput);
+            System.out.println(SPACER);
+            System.out.println("Added: " + userInput);
+            System.out.println(SPACER);
+        }
+    }
+
+    private static void addInputToList(String newInput) {
+        ALL_USER_INPUTS.add(newInput);
+    }
+
+    private static void printAllUserInputs() {
+        int index = 1;
+        for (String input : ALL_USER_INPUTS) {
+            System.out.println(index + ". " + input);
+            index++;
         }
     }
 }
