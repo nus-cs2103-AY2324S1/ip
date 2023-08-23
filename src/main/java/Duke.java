577dfff0e9c9dde5127d822038d45a6210c9a874
wj331 <e0389091@u.nus.edu>
2023-08-23 22:16:59 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void main(String[] args) {
+        System.out.println(greet());
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                sc.close();
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+        System.out.println(Duke.byeGreetings);
+    }
+
     public static String byeGreetings = "Bye. Hope to see you again soon!";
     public static String name = " ____    __        __       \n"
             + "|  _ \\   | |  ____ | | __\n"
             + "| | | |  | | |     | |/ /\n"
             + "| |_| |  | | |     |   < \n"
             + "|____/   ___  ____ |_|\\_\\\n";
-    public static void main(String[] args) {
-        System.out.println(greet());
-
-        System.out.println(Duke.byeGreetings);
-    }
-
     public static String greet() {
-        return "Hello from \n " + Duke.name + "What can I do for you? \n";
+        return "Hello from \n " + Duke.name + "What can I do for you? ";
     }
 }
