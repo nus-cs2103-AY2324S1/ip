5dd114660853f46d3cb795b4778556f1f43ecff5
mingyu <wanmingyu0318@gmail.com>
2023-08-23 21:07:04 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,7 @@
 import java.util.Scanner;
 
 public class Duke  {
-    public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);*/
+    public static void main(String[] args) throws MYBotExceptions {
 
         MYBot MYBot = new MYBot("MYBot");
         MYBot.openGreeting();
@@ -15,19 +9,11 @@ public class Duke  {
         Scanner scanner = new Scanner(System.in);
         while(true){
             String input = scanner.nextLine();
-            if(input.equals("bye")){
+            if(input.equals("bye")) {
                 MYBot.closeGreeting();
                 break;
-            } else if(input.equals("list")){
-                MYBot.listTasks();
-            } else if(input.startsWith("mark ")) {
-                int task_index = Integer.parseInt(input.substring(5));
-                MYBot.markTasks(task_index);
-            } else if(input.startsWith("unmark ")) {
-                int task_index = Integer.parseInt(input.substring(7));
-                MYBot.unmarkTasks(task_index);
             } else {
-                MYBot.addTask(input);
+                MYBot.analyseInput(input);
             }
         }
     }
--- src/main/java/MYBot.java
+++ src/main/java/MYBot.java
@@ -21,33 +21,93 @@ public class MYBot {
         System.out.println("____________________________________________________________");
     }
 
-    public void addTask(String input){
+    public void analyseInput(String input) {
+
+        try {
+            if (input.equals("list")) {
+                listTasks();
+            } else if (input.startsWith("mark ")) {
+                int task_index = Integer.parseInt(input.substring(5));
+                markTasks(task_index);
+            } else if (input.startsWith("unmark ")) {
+                int task_index = Integer.parseInt(input.substring(7));
+                unmarkTasks(task_index);
+            } else if (input.isEmpty()) {
+                throw new MYBotExceptions.UnknownCommandException();
+            } else if (!input.startsWith("todo ") && !input.startsWith("deadline ") && !input.startsWith("event ")) {
+                throw new MYBotExceptions.InvalidTaskException();
+            } else {
+                addTask(input);
+            }
+        } catch (MYBotExceptions e) {
+            System.out.println("____________________________________________________________");
+            System.out.println(e.getMessage());
+            System.out.println("____________________________________________________________");
+        }
+    }
+
+    public void addTask(String input) {
+
         System.out.println("____________________________________________________________");
-        System.out.println("Got it. I've added this task:");
 
-        if(input.startsWith("todo ")){
+        try {
+            if (input.startsWith("todo ")) {
 
-            String description = input.substring(5);
-            Task task = new Todo(description);
-            task_List.addTask(task);
-            System.out.println(task.toString());
+                String description = input.substring(5);
+                if (!description.isBlank()) {
+                    Task task = new Todo(description);
+                    task_List.addTask(task);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(task.toString());
+                } else {
+                    throw new MYBotExceptions.EmptyDetailsException("description", "todo");
+                }
 
-        } else if (input.startsWith("deadline ")){
+            } else if (input.startsWith("deadline ")) {
+                if(!input.contains(" /by")) {
+                    throw new MYBotExceptions.InvalidInputException("deadline", "duedate");
+                }
 
-            String description = input.substring(9,input.indexOf(" /by "));
-            String by = input.substring(input.indexOf(" /by ") + 4);
-            Task task = new Deadline(description, by);
-            task_List.addTask(task);
-            System.out.println(task.toString());
+                String description = input.substring(9, input.indexOf(" /by "));
+                String by = input.substring(input.indexOf(" /by ") + 4);
 
-        } else if ((input.startsWith("event "))) {
+                if (description.isBlank()) {
+                    throw new MYBotExceptions.EmptyDetailsException("description", "deadline");
+                } else if (by.isBlank()) {
+                    throw new MYBotExceptions.EmptyDetailsException("duedate", "deadline");
+                } else {
+                    Task task = new Deadline(description, by);
+                    task_List.addTask(task);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(task.toString());
+                }
+            } else if ((input.startsWith("event "))) {
 
-            String description = input.substring(6,input.indexOf(" /from "));
-            String from = input.substring(input.indexOf(" /from ") + 6, input.indexOf(" /to "));
-            String to = input.substring(input.indexOf(" /to ") + 4);
-            Task task = new Event(description, from, to);
-            task_List.addTask(task);
-            System.out.println(task.toString());
+                if(!input.contains(" /from")) {
+                    throw new MYBotExceptions.InvalidInputException("event", "start time");
+                } else if (!input.contains(" /to")) {
+                    throw new MYBotExceptions.InvalidInputException("event", "end time");
+                }
+
+                String description = input.substring(6, input.indexOf(" /from "));
+                String from = input.substring(input.indexOf(" /from ") + 6, input.indexOf(" /to"));
+                String to = input.substring(input.indexOf(" /to ") + 4);
+
+                if (description.isBlank()) {
+                    throw new MYBotExceptions.EmptyDetailsException("description", "event");
+                } else if (from.isBlank()) {
+                    throw new MYBotExceptions.EmptyDetailsException("start time", "event");
+                } else if (to.isBlank()) {
+                    throw new MYBotExceptions.EmptyDetailsException("end time", "event");
+                } else {
+                    Task task = new Event(description, from, to);
+                    task_List.addTask(task);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(task.toString());
+                }
+            }
+        } catch (MYBotExceptions e) {
+            System.out.println(e.getMessage());
         }
 
         System.out.println("Now you have " + task_List.getTask_Count() + " tasks in the list.");
@@ -69,14 +129,18 @@ public class MYBot {
     public void markTasks(int task_number) {
         System.out.println("____________________________________________________________");
 
-        if (task_number > 0 && task_List.getTask(task_number) != null) {
-            Task taskTobeMarked = task_List.getTask(task_number);
-            taskTobeMarked.taskDone();
+        try {
+            if (task_number > 0 && task_List.getTask(task_number) != null) {
+                Task taskTobeMarked = task_List.getTask(task_number);
+                taskTobeMarked.taskDone();
 
-            System.out.println("Good job completing! I've marked these task as done:):");
-            System.out.println(taskTobeMarked.toString());
-        } else {
-            System.out.println("Oops! Task not found:(");
+                System.out.println("Good job completing! I've marked these task as done:):");
+                System.out.println(taskTobeMarked.toString());
+            } else {
+                throw new MYBotExceptions.NoSuchTaskException();
+            }
+        } catch (MYBotExceptions e) {
+            System.out.println(e.getMessage());
         }
 
         System.out.println("____________________________________________________________");
@@ -86,14 +150,18 @@ public class MYBot {
     public void unmarkTasks(int task_number){
         System.out.println("____________________________________________________________");
 
-        if (task_number > 0 && task_List.getTask(task_number) != null) {
-            Task taskTobeMarked = task_List.getTask(task_number);
-            taskTobeMarked.undoTask();
+        try {
+            if (task_number > 0 && task_List.getTask(task_number) != null) {
+                Task taskTobeMarked = task_List.getTask(task_number);
+                taskTobeMarked.undoTask();
 
-            System.out.println("OK, I've marked this task as not done yet:");
-            System.out.println(taskTobeMarked.toString());
-        } else {
-            System.out.println("Oops! Task not found:(");
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(taskTobeMarked.toString());
+            } else {
+                throw new MYBotExceptions.NoSuchTaskException();
+            }
+        } catch (MYBotExceptions e) {
+            System.out.println(e.getMessage());
         }
 
         System.out.println("____________________________________________________________");
