c3f82d433065944648d3f66103db25bab3264b33
Li <e0985877@u.nus.edu>
2023-08-23 11:38:27 +0800

    Implement Level-5: Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -27,46 +27,104 @@ public class Duke {
         System.out.println();
     }
 
-    public void handleInput(String input) {
-        if (input.isEmpty()) {
-            formatPrintMessage("Task cannot be empty");
-            return;
-        }
-
+    public void handleInput(String input){
         String[] inputArr = input.split(" ");
         String command = inputArr[0];
 
-        switch (command) {
-            case "list":
-                this.storage.showAllTasks();
-                break;
-            case "mark":
-                int taskNumber = Integer.parseInt(inputArr[1]);
-                this.storage.markTaskAsDone(taskNumber);
-                break;
-            case "unmark":
-                int taskNumber2 = Integer.parseInt(inputArr[1]);
-                this.storage.unmarkTaskAsDone(taskNumber2);
-                break;
-            case "todo":
-                String description = input.replace("todo", "");
-                this.storage.addTask(description);
-                break;
-            case "deadline":
-                String description2 = input.replace("deadline", "");
-                this.storage.addTask(description2);
-                break;
-            case "event":
-                String description3 = input.replace("event", "");
-                this.storage.addTask(description3);
-                break;
-            default:
-                formatPrintMessage("Invalid command");
-                break;
+        try {
+            switch (command) {
+                case "list":
+                    this.storage.showAllTasks();
+                    break;
+                case "mark":
+                    int taskNumber = Integer.parseInt(inputArr[1]);
+                    this.storage.markTaskAsDone(taskNumber);
+                    break;
+                case "unmark":
+                    int taskNumber2 = Integer.parseInt(inputArr[1]);
+                    this.storage.unmarkTaskAsDone(taskNumber2);
+                    break;
+                case "todo":
+                    String description = validateToDoCommand(input);
+                    this.storage.addTask(description);
+                    break;
+                case "deadline":
+                    String description2 = validateDeadlineCommand(input);
+                    this.storage.addTask(description2);
+                    break;
+                case "event":
+                    String description3 = validateEventCommand(input);
+                    this.storage.addTask(description3);
+                    break;
+                default:
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+            }
+        } catch (DukeException e) {
+            System.out.println();
+            formatPrintMessage(e.getMessage());
         }
     }
 
-    public void run() {
+    public String validateToDoCommand(String input) throws DukeException {
+        String[] inputArr = input.split(" ");
+        if (inputArr.length == 1) {
+            throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+        }
+        return input.replace("todo", "");
+    }
+
+    public String validateDeadlineCommand(String input) throws DukeException {
+        String[] inputArr = input.split(" ");
+        if (inputArr.length == 1) {
+            throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+        }
+
+        String[] description = input.replace("deadline", "").trim().split(" ");
+        int index = -1;
+        if (description.length > 2) {
+            for (int i = 0; i < description.length; i++) {
+                if (description[i].equals("/by")) {
+                    index = i;
+                    break;
+                }
+            }
+        }
+
+        if (index == -1 || index == 0 || index == description.length - 1) {
+            throw new DukeException("☹ OOPS!!! The format of a deadline is invalid. Format: deadline <task name> /by <date>");
+        }
+
+        return input.replace("deadline", "");
+    }
+
+    public String validateEventCommand(String input) throws DukeException {
+        String[] inputArr = input.split(" ");
+        if (inputArr.length == 1) {
+            throw new DukeException("☹ OOPS!!! The description of a event cannot be empty.");
+        }
+
+        String[] description = input.replace("event", "").trim().split(" ");
+        int fromIndex = -1;
+        int toIndex = -1;
+        if (description.length > 2) {
+            for (int i = 0; i < description.length; i++) {
+                if (description[i].equals("/from")) {
+                    fromIndex = i;
+                }
+                if (description[i].equals("/to")) {
+                    toIndex = i;
+                }
+            }
+        }
+
+        if (fromIndex == -1 || toIndex == -1 || fromIndex == 0 || toIndex == 0 || fromIndex >= toIndex || fromIndex == description.length - 1 || toIndex == description.length - 1) {
+            throw new DukeException("☹ OOPS!!! The format of a event is invalid. Format: event <task name> /from <date> /to <date>");
+        }
+
+        return input.replace("event", "");
+    }
+
+    public void run(){
         greet();
         String line = scanner.nextLine();
         while (!line.equals("bye")) {
