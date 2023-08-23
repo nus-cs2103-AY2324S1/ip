96c44b651c203be359e758eb1405bea3d1eeb2d7
Teng Xin Zhuan <>
2023-08-23 17:02:40 +0800

    Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,28 +6,68 @@ public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String botName = "Aaronbot";
-        List<String> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
 
         System.out.println("Hello! I'm " + botName);
         System.out.println("What can I do for you?");
 
         while (true) {
             String userInput = scanner.nextLine();
-            
-            if (userInput.equals("bye")) {
+            String[] inputParts = userInput.split(" ");
+            String command = inputParts[0];
+
+            if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (userInput.equals("list")) {
+            } else if (command.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println((i + 1) + ". " + tasks.get(i));
                 }
+            } else if (command.equals("mark") || command.equals("unmark")) {
+                int taskIndex = Integer.parseInt(inputParts[1]) - 1;
+                if (taskIndex < tasks.size()) {
+                    Task task = tasks.get(taskIndex);
+                    if (command.equals("mark")) {
+                        task.markAsDone();
+                        System.out.println("Nice! I've marked this task as done:");
+                    } else {
+                        task.markAsNotDone();
+                        System.out.println("OK, I've marked this task as not done yet:");
+                    }
+                    System.out.println("  " + task);
+                } else {
+                    System.out.println("Index you gave is out of range.");
+                }
             } else {
-                tasks.add(userInput);
+                tasks.add(new Task(userInput));
                 System.out.println("added: " + userInput);
             }
         }
-        
+
         scanner.close();
+    }
+}
+
+class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + (isDone ? "X" : " ") + "] " + description;
     }
 }
