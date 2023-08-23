b8e638dbd9b7a449cc34fb5de11cff5008b5dcf2
suryanshkushwaha <e0985892@u.nus.edu>
2023-08-24 00:28:57 +0800

    new name

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String lines = "____________________________________________________________\n";
+        String intro = "Hello! I'm sillyBot\nWhat can I do for you?\n";
+        String exit = "Bye. Hope to see you again soon!\n";
+        System.out.println(lines + intro + lines + exit + lines);
     }
 }
