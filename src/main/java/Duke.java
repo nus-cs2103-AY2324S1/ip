e6ea6dc8966cda93d1c27acea70d7e3a4296c4e0
kokseng1 <ngkokseng02@gmail.com>
2023-08-23 19:30:36 +0800

    lvl3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,8 +2,35 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        class Task {
+            final protected String description;
+            protected boolean isDone;
+
+            public Task(String description) {
+                this.description = description;
+                this.isDone = false;
+            }
+
+            public void mark() {
+                this.isDone = true;
+            }
+
+            public void unmark() {
+                this.isDone = false;
+            }
+
+            private String getStatusIcon() {
+                return (isDone ? "X" : " "); // mark done task with X
+            }
+
+            @Override
+            public String toString() {
+                return ("[" + this.getStatusIcon() + "] " + this.description);
+            }
+        }
+
         Scanner scan = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         String name = "Chaty";
         int taskNo = 0;
 
@@ -13,9 +40,19 @@ public class Duke {
         while (!next.equals("bye")) {
             if (next.equals("list")) {
                 for (int i = 0; i < taskNo ; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
-                }} else {
-                    tasks[taskNo] = next;
+                    System.out.println((i + 1) + "." + tasks[i]);
+                }
+            } else if (next.startsWith("mark")) {
+                int tasknum = Integer.parseInt(next.split(" ")[1]) - 1;
+                tasks[tasknum].mark();
+                System.out.println("Nice! I've marked this task as done:\n" + tasks[tasknum]);
+            } else if (next.startsWith("unmark")) {
+                int tasknum = Integer.parseInt(next.split(" ")[1]) - 1;
+                tasks[tasknum].unmark();
+                System.out.println(" OK, I've marked this task as not done yet:\n" + tasks[tasknum]);
+            } else {
+                    Task nextTask = new Task(next);
+                    tasks[taskNo] = nextTask;
                     taskNo++;
                     System.out.println("added: " + next);
             }
