de627bc98dbade88a0eeff16911a0f9129aa9570
sheng <chengzhisheng2001@gmail.com>
2023-08-23 21:27:49 +0800

    Add the ability to check fof tasks

--- src/main/java/Pogo.java
+++ src/main/java/Pogo.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 public class Pogo {
     private static ArrayList<Task> tasks = new ArrayList<>();
 
+    private static int parseTaskIndex(String input) {
+        return Integer.parseInt(input.split(" ")[1]) - 1;
+    }
+
+    private static void printTasks() {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(i + 1 + ". " + tasks.get(i).getStatusMessage());
+        }
+    }
+
     public static void main(String[] args) {
         String horizontalLine = "____________________________________________________________";
 
@@ -18,21 +29,28 @@ public class Pogo {
             String input = scanner.nextLine();
             System.out.println(horizontalLine);
 
-            switch (input) {
-                case "bye":
-                    System.out.println(quitMessage);
-                    break;
-                case "list":
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println(i + 1 + ". " + tasks.get(i));
-                    }
-                    break;
-                default:
-                    tasks.add(new Task(input));
-                    System.out.println("added: " + input);
-                    break;
+            // Convert switch statement to if-else statement
+            if (input.equals("bye")) {
+                System.out.println(quitMessage);
+                break;
+            } else if (input.equals("list")) {
+                Pogo.printTasks();
+            } else if (input.startsWith("mark")) {
+                int index = Pogo.parseTaskIndex(input);
+                tasks.get(index).markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks.get(index).getStatusMessage());
+            } else if (input.startsWith("unmark")) {
+                int index = Pogo.parseTaskIndex(input);
+                tasks.get(index).markAsUndone();
+                System.out.println("Ok, I've marked this task as not done yet:");
+                System.out.println(tasks.get(index).getStatusMessage());
             }
+            else {
+                tasks.add(new Task(input));
+                System.out.println("added: " + input);
+            }
+
             System.out.println(horizontalLine);
 
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,8 +1,26 @@
 public class Task {
     protected String description;
+    protected boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String getStatusMessage() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
     }
 
     public String toString() {
