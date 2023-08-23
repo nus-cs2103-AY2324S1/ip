2e599c0486e1f63956c39832048f81c31d9ca642
sheng <chengzhisheng2001@gmail.com>
2023-08-23 20:55:53 +0800

    Rename chatbot

---
+++ src/main/java/Pogo.java
@@ -0,0 +1,11 @@
+public class Pogo {
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
+
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm Pogo\nWhat can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
