4b85d6579fbeb3136aec7adb3d61616184d1921b
Clin-lyx <110957868+Clin-lyx@users.noreply.github.com>
2023-08-23 15:52:19 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,20 @@
 public class Duke {
+
+    // An greeting display everytime entering the program
+    private static void OnEnter () {
+        System.out.println("____________________________________________");
+        System.out.println("Hello! I am YOU");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________");
+    }
+
+    // AN exit display everytime exits the program
+    private static void OnExit() {
+        System.out.println("____________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +22,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        OnEnter();
+        OnExit();
     }
 }
