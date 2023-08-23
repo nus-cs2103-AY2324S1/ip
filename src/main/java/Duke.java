4f633e765cad57b46d441785e6997fe8e10fe023
tiif <pekyuan@gmail.com>
2023-08-23 18:39:55 +0800

    add exception

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,22 +17,39 @@ public class Duke {
             BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
             String str = bf.readLine();
             String parsed_str[] = str.split(" ");
-            if (str.equals("list")) {
+            if (parsed_str[0].equals("list")) {
                 //print out the list
                 int size = tdlist.size();
                 for (int i = 0; i < size; i++) {
                     Task curr = tdlist.get(i);
                     System.out.println((i + 1) + ". " + curr);
                 }
+                if (parsed_str.length > 1) {
+                    //error handling
+                   System.out.println("Do not input another argument beside list");
+                }
             } else if (parsed_str[0].equals("mark")) {
-                //0-index
+                if (parsed_str.length < 2) {
+                    //error handling
+                    System.out.println("Please specify the index of the task");
+                    continue;
+                } else if (parsed_str.length > 2) {
+                    System.out.println("Extra argument detected!");
+                    continue;
+                }
                 System.out.println("Nice! I've marked this task as done:");
                 int index = Integer.parseInt(parsed_str[1]) - 1;
                 Task curr = tdlist.get(index);
                 curr.markAsDone();
                 System.out.println((index + 1) + ". " + curr);
             } else if (parsed_str[0].equals("unmark")) {
-                //0-index
+                if (parsed_str.length < 2) {
+                    System.out.println("Please specify the index of the task");
+                    continue;
+                } else if (parsed_str.length > 2) {
+                    System.out.println("Extra argument detected!");
+                    continue;
+                }
                 System.out.println("Ok, I've marked this task as not done yet:");
                 int index = Integer.parseInt(parsed_str[1]) - 1;
                 Task curr = tdlist.get(index);
@@ -42,13 +59,16 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (parsed_str[0].equals("todo")) {
-                //for todo
                 int size = parsed_str.length;
+                if (size < 2) {
+                    //error handling
+                    System.out.println("You do not specify the todo name");
+                    continue;
+                }
                 String description = "";
                 for (int i = 1; i < size; i++) {
                     description += parsed_str[i] + " ";
                 }
-                System.out.println(description);
                 System.out.println("Got it. I've added this task:");
                 Todo curr = new Todo(description);
                 tdlist.add(curr);
@@ -70,6 +90,14 @@ public class Duke {
                     } else {
                         description += parsed_str[i] + " ";
                     }
+                }
+                //error handling
+                if (description.equals("")) {
+                    System.out.println("Please input the name of the deadline task");
+                    continue;
+                } else if (by_date.equals("")) {
+                    System.out.println("Please specify when is the deadline");
+                    continue;
                 }
                 System.out.println("Got it. I've added this task:");
                 Deadline curr = new Deadline(description, by_date);
@@ -99,6 +127,16 @@ public class Duke {
                        toDate += parsed_str[i] + " ";
                     }
                 }
+                if (description.equals("")) {
+                    System.out.println("Please input the name of the deadline task");
+                    continue;
+                } else if (fromDate.equals("")) {
+                    System.out.println("Please specify start time");
+                    continue;
+                } else if (toDate.equals("")) {
+                    System.out.println("Please specify the end time");
+                    continue;
+                }
                 System.out.println("Got it. I've added this task:");
                 //todo here
                 Event curr = new Event(description, fromDate, toDate);
@@ -106,6 +144,9 @@ public class Duke {
                 System.out.println(curr);
                 System.out.println("Now you have " + tdlist.size() + " tasks in the list.");
 
+            } else {
+                //no command found
+                System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
         }
 
