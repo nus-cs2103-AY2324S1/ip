0dbf37b58d04286786745f5294533c8b78f9c893
RiyaMehta2211 <>
2023-08-23 22:13:05 +0800

    commit message 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,12 +4,6 @@ public class Duke {
         String name = "Harry Potter";
         String question = "What can I do for you?";
         System.out.println("Hello! I'm " + name + "\n" + question);
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        while (!str.equals("bye")) {
-            System.out.println("\t" + str);
-            str = sc.nextLine();
-        }
-        System.out.println("\t" + "Bye. Hope to see you again soon!");
+        System.out.println("\n" + "Bye. Hope to see you again soon!");
     }
 }
