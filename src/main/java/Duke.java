020940783469bf6fca6354ad0814950d0d2fedb5
RiyaMehta2211 <>
2023-08-23 23:18:34 +0800

    Committing Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,29 @@
-import java.util.Scanner;
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         String name = "Harry Potter";
-        String qn = "What can I do for you?";
-        System.out.println("Hello! I'm " + name + "\n" + qn);
+        String question = "What can I do for you?";
+        System.out.println("Hello! I'm " + name + "\n" + question);
+
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+        String output = "";
+        Integer i = 1;
+        List<String> lst = new ArrayList<>();
+
         while (!str.equals("bye")) {
-            System.out.println("\t" + str);
+            if (!str.equals("list")) {
+                lst.add(str);
+                System.out.println("\t" + "added: " + str);
+            } else {
+                for (String el : lst) {
+                    output += i.toString() + ". " + el + "\n" + "\t";
+                    i++;
+                }
+                System.out.println("\t" + output);
+                output = ""; //reset
+                i = 1; //reset
+            }
             str = sc.nextLine();
         }
         System.out.println("\t" + "Bye. Hope to see you again soon!");
