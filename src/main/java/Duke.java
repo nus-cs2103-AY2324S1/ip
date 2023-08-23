236774f05ecd366202e89780ff28460247045a67
Bearypop <looyl2k@gmail.com>
2023-08-23 23:20:31 +0800

    Added Rename, Greet, Exit feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "Beary";
+        System.out.println(String.format("Hello! I'm %s\nWhat can I do for you?", name));
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    public static void printLine() {
+        System.out.println("____________________________________________________________");
     }
 }
+
