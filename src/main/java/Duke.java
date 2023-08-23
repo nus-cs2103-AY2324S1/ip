011248689e2f5a60b3085268b4a492a78f9d1308
dloh2236 <daniel.loh@live.com.sg>
2023-08-23 18:43:25 +0800

    Added ability to add text and display back when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,15 +15,48 @@ public class Duke {
         String exit = line + "     I will be sad to see you go! bye!\n" + line;
 
         String reply = "";
+        String[] list = new String[100];
+        int numberOfTasks = 0;
 
         System.out.println(greet);  // print greet message
         while (true) {
             Scanner userReply = new Scanner(System.in);
             reply = userReply.nextLine();
-            if (reply.equals("bye")) break;
-            String echo = line + "     " + reply + "\n" + line;
 
-            System.out.println(echo);
+            if (reply.equals("bye")) {
+                break;
+            } else if (reply.equals("list")) {
+                if (numberOfTasks == 0) {
+                    System.out.println(line
+                            + "     "
+                            + "looks empty to me!");
+                } else {
+                    if (numberOfTasks > 10) {
+                        System.out.println(line
+                                + "     "
+                                + "seems like you have a lot of things to do...\n"
+                                + "     "
+                                + "remember to play with me after :D");
+                    } else {
+                        System.out.println(line
+                                + "     "
+                                + "here are the things you told me to keep track of:");
+                    }
+                    for (int i = 0; i < numberOfTasks; i++) {
+                        System.out.println("     " + (i + 1) + ". "
+                                + list[i]);
+                    }
+                }
+                System.out.println(line);
+                continue;
+            } else {
+                list[numberOfTasks] = reply;
+                numberOfTasks++;
+                String echo = line + "     " + "gotchu! noted down... "
+                        + reply + "\n" + line;
+
+                System.out.println(echo);
+            }
         }
         System.out.println(exit);
     }
