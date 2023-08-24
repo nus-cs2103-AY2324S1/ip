01f74e27fa1c9df4bdccacfcf66b324e783fee91
Adammangzijun <117640074+adammangzijun@users.noreply.github.com>
2023-08-24 17:05:28 +0800

    Feature 1: added user input and cases

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,6 +9,29 @@ public class Duke {
         System.out.println("Hello! I'm "+ name);
         System.out.println("What can I do for you?");
         System.out.println(space);
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+        userInput = scanner.nextLine().trim();// remove trailing spaces and get use input
+
+        //prompt for user input if input is not "bye"
+        while(!userInput.equalsIgnoreCase("bye")) {
+            if (userInput.equals("list")) {
+                System.out.println(space);
+                System.out.println("list");
+                System.out.println(space);
+
+            } else {
+
+                System.out.println(space);
+                System.out.println(userInput);
+                System.out.println(space);
+
+            }
+            userInput = scanner.nextLine().trim();
+        }
+
+        System.out.println(space);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(space);
     }
