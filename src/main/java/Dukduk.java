a8b6dbfec7d81e5feb24759eb17cd110b6700cce
wnchan <e0970469@u.nus.edu>
2023-08-24 16:07:25 +0800

    Level-0, Rename, Greet, Exit

---
+++ src/main/java/Dukduk.java
@@ -0,0 +1,14 @@
+public class Dukduk {
+    public static void main(String[] args) {
+	printLine();
+	System.out.println(" Hello! I'm Dukduk");
+	System.out.println(" What can I do for you?");
+	printLine();
+	System.out.println(" Bye. Hope to see you again soon!");
+	printLine();	
+    }
+
+    public static void printLine() {
+	    System.out.println("____________________________________________________________");
+    }
+}
