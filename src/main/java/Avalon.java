5dbae6174ddeb44f5987f86ffb2273381f88f17d
LHeng1 <limhengyi1@gmail.com>
2023-08-24 08:58:53 +0800

    Level-0

---
+++ src/main/java/Avalon.java
@@ -0,0 +1,10 @@
+public class Avalon {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n" +
+                " Hello! I'm Arthur Pendragon.\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________");
+    }
+}
