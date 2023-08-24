ffeb0be49f7bd6f1fc411b87a37f895f658c02b8
oeggy03 <oeggy03>
2023-08-25 00:47:57 +0800

    Level 2 done: Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,16 +3,21 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         String introduction = "____________________________________________________________\n" +
                 " Hello! I'm eggbot\n" +
-                " What can I do for you?\n" +
+                " Please add a task!\n\n" +
+                " To view tasks, type 'list' \n" +
+                " To exit, type 'bye' \n" +
                 "____________________________________________________________\n";
 
         System.out.println(introduction);
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("Type something for me to echo!");
+            System.out.println("Please add a task, or type list to show tasks.");
 
             String input = scanner.nextLine();  // Read input
 
@@ -23,9 +28,32 @@ public class Duke {
 
                 System.out.println(output);
                 break;
+            } else if (Objects.equals(input, "list")) {
+
+                if (taskCount == 0) {
+                    System.out.println("You have no tasks!");
+
+                    continue;
+                }
+                String output = "____________________________________________________________\n" +
+                        "Your tasks: \n";
+
+                System.out.println(output);
+
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(tasks[i]);
+                }
+
+                System.out.println("____________________________________________________________\n");
+
+                continue;
             }
+
+            tasks[taskCount] = input;
+            taskCount += 1;
+
             String output = "____________________________________________________________\n" +
-                    input + "\n" +
+                    "added: " + input + "\n" +
                     "____________________________________________________________\n";
 
             System.out.println(output);
