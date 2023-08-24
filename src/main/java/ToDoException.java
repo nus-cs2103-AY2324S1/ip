a0bdad0c27209592058347d5c30e1598167397d5
YangHeng Tang <yanghengtang@gmail.com>
2023-08-24 19:36:10 +0800

    Add ToDoException.java

---
+++ src/main/java/ToDoException.java
@@ -0,0 +1,9 @@
+public class ToDoException extends Exception {
+    private static String HORIZONTAL_LINE = "____________________________________________________________\n";
+
+    public ToDoException() {
+        super(HORIZONTAL_LINE + "Oops! The description of a todo cannot be empty.\n"
+                + "Valid Format: todo (description)\n"
+                + HORIZONTAL_LINE);
+    }
+}
