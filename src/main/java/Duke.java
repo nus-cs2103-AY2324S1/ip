196e076f9e255b4a7ddc7cd076f5b36d71014e50
nananakx-x <nanettekx.x3020@gmail.com>
2023-08-24 15:55:23 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,54 +2,29 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static class Task {
-        protected String description;
-        protected boolean isDone;
-
-        public Task(String description) {
-            this.description = description;
-            this.isDone = false;
-        }
-
-        public String getStatusIcon() {
-            return (isDone ? "X" : " "); // mark done task with X
-        }
-
-        public void markAsDone() {
-            if (!this.isDone) {
-                this.isDone = true;
-            }
-        }
-
-        public void unmark() {
-            if (this.isDone) {
-                this.isDone = false;
-            }
-        }
-
-    }
-
+    // substring: begIndex (inclusive) up to the endIndex (exclusive)
     public static void main(String[] args) {
         int itemsAdded = 0;
         Task[] taskList = new Task[100]; // assume there will be <= 100 tasks
-        System.out.println("Hello friend :> My name is John, nice to meet you!\n" +
-                "What do you have to do today?\n");
+        System.out.println("Hello friend :> My name is John, nice to meet you! " +
+                "What do you have to do today?");
+
+        Scanner sc = new Scanner(System.in); // do not put this in the while loop
 
         while (true) {
-            Scanner sc = new Scanner(System.in);
             System.out.print("Reply John: ");
             String command = sc.nextLine(); // using .next() is wrong - only reads first word
 
             if (command.equals("bye")) {
                 System.out.println("Bye for now, hope to see you soon.");
-                sc.close();
                 break;
             } else if (command.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
-                
+
                 for (int i = 1; i <= itemsAdded; i++) {
                     Task task = taskList[i - 1];
-                    System.out.println(i + ". " + "[" + task.getStatusIcon() + "] " + task.description);
+                    System.out.println(i + ". " + taskList[i - 1].toString());
+                    // adding toString() to use the overridden one in Task, etc.
                 }
             } else if (command.startsWith("mark ")) { // space behind is needed!
                 int taskPos = Integer.parseInt(command.substring(5)) - 1; // convert substring to int
@@ -62,7 +37,7 @@ public class Duke {
                     System.out.println("Invalid.");
                 }
 
-            } else if (command.startsWith("unmark ")) { // space behind is needed!
+            } else if (command.startsWith("unmark ")) {
                 int taskPos = Integer.parseInt(command.substring(7)) - 1; // convert substring to int
 
                 // only numbers >= 0 and < total number are valid
@@ -73,6 +48,42 @@ public class Duke {
                     System.out.println("Invalid.");
                 }
 
+            } else if (command.startsWith("todo ")) { // description starting index = 5
+                String description = command.substring(5);
+
+                taskList[itemsAdded] = new ToDo(description); // add new command
+
+                System.out.println("Got it. I've added this task:\n" + "  " + taskList[itemsAdded]
+                        + "\nNow you have " + (itemsAdded + 1) + " tasks in the list.");
+
+                itemsAdded++; // increment number of items
+
+            } else if (command.startsWith("deadline ")) { // description starting index = 9
+                // indexOf: searches for the substring and returns the index of the first character
+                String description = command.substring(9, command.indexOf(" /by "));
+                String by = command.substring(command.indexOf(" /by ") + 5); // from " " to the specified date is 5
+
+                taskList[itemsAdded] = new Deadline(description, by); // add new command
+
+                System.out.println("Got it. I've added this task:\n" + "  " + taskList[itemsAdded]
+                        + "\nNow you have " + (itemsAdded + 1) + " tasks in the list.");
+
+                itemsAdded++; // increment number of items
+
+            } else if (command.startsWith("event ")) { // description starting index = 6
+                String description = command.substring(6, command.indexOf(" /from "));
+                // from " " to 'from' date is 7
+                String from = command.substring(command.indexOf(" /from ") + 7, command.indexOf(" /to "));
+                // from " " to 'to' date is 5
+                String to = command.substring(command.indexOf(" /to ") + 5);
+
+                taskList[itemsAdded] = new Event(description, from, to); // add new command
+
+                System.out.println("Got it. I've added this task:\n" + "  " + taskList[itemsAdded]
+                        + "\nNow you have " + (itemsAdded + 1) + " tasks in the list.");
+
+                itemsAdded++; // increment number of items
+
             } else {
                 taskList[itemsAdded] = new Task(command); // add new command
                 System.out.println("added: " + command); // show user that it is added
@@ -80,19 +91,9 @@ public class Duke {
             }
         }
 
+        sc.close();
+
     }
 }
 
-/*
-public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }
-}
- */
 
