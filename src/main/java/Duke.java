91d4bf3b25770be8bf6f8a49ee870eec3ba55bdf
Timothy Putra Prasetio <timothypp02@gmail.com>
2023-08-24 10:34:25 +0800

    Add Echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
 public class Duke {
+    final static String LINE = "____________________________________________________________\n";
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n" +
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(LINE +
                 " Hello! I'm Anya Forger\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                LINE);
+
+        String command = sc.nextLine();
+
+        while (!command.equals("bye")) {
+            echo(command);
+            command = sc.nextLine();
+        }
+
+        System.out.println("____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n");
+    }
+    public static void echo(String s) {
+        System.out.println(LINE + " " + s + "\n" + LINE);
     }
 }
