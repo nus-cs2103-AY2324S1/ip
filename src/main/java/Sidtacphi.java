a5d1d84dac110d2eda77a663f0d2d5ef0dd4953a
Nauxe <51425614+Nauxe@users.noreply.github.com>
2023-08-24 23:29:17 +0800

    Added javadoc and echo

--- src/main/java/Sidtacphi.java
+++ src/main/java/Sidtacphi.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Sidtacphi is the main class for the Sidtacphi bot.
+ * 
+ * @author Jeffry Lum
+ * @author Damith C. Rajapakse
+ * @author Sean Leong
+ * @author Liow Jia Cheng
+ * @author Yu Lexuan
+ */
 public class Sidtacphi {
     public static void main(String[] args) {
         String logo = " _______  ___   ______   _______  _______  _______  _______  __   __  ___  \n"
@@ -11,7 +22,20 @@ public class Sidtacphi {
         System.out.println("Hello from\n" + logo + "\n");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("\nYou: ");
+        String input = "";
+        while (!scan.hasNext("bye")) {
+            input = scan.nextLine();
+            System.out.print("\nSidtacphi: " + input + "\n");
+            System.out.print("\nYou: ");
+        }
+        scan.close();
+
         System.out.println("____________________________________________________________");
+        System.out.println("\nBye. Hope to see you again soon!");
+
     }
 }
