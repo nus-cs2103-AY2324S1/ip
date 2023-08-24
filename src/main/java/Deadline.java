1fc3f7e3ae99949b1e23f9c4debd2446c8b15d53
iantsaii <ianfourr@gmail.com>
2023-08-24 22:40:32 +0800

    Implement testing

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,3 +1,4 @@
+//Solution below adapted from: https://nus-cs2103-ay2324s1.github.io/website/schedule/week2/project.html#:~:text=Extension%3A%20A%2D-,Classes,-A%2DClasses
 public class Deadline extends Task {
     protected String by;
 
