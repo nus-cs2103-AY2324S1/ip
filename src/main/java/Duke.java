6ae63d88d058d01a766c2332067df9112d995e8b
neyapraveen <neyapraveen@gmail.com>
2023-08-25 00:18:20 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,15 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         String horizontalLine = "_".repeat(35) + "\n";
-        System.out.println(horizontalLine
-            + "Hello! I'm Bot\n" + "What can I do for you?\n"
-            + horizontalLine + "Bye. Hope to see you again soon!\n" + horizontalLine);
+        Scanner sc = new Scanner(System.in);
+        String task = "";
+        System.out.println("Hello! I'm Bot\n" + "What can I do for you?\n" + horizontalLine);
+        while (!task.equals("bye")) {
+            task = sc.nextLine();
+            System.out.println(horizontalLine + task + "\n" + horizontalLine);
+        }
+        System.out.println("Bye. Hope to see you again soon!\n" + horizontalLine);
     }
 }
