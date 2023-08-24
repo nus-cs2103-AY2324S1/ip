597ff62874b1e58587f5571aac1b516b52b4d249
coderhuang559 <huangyixin1f@gmail.com>
2023-08-24 09:33:05 +0800

    Compartmentalise main into methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,7 +17,6 @@ public class Duke {
 
         int inputNum = 0;
 
-        // Bot exits when user inputs "bye" and echoes when user inputs anything else
         while (true) {
             try {
                 String userInput = scan.nextLine().trim();
@@ -29,104 +28,43 @@ public class Duke {
                     break;
 
                 } else if (Objects.equals(userInput, "list")) {
-                    String inputArrayString = "";
-                    for (int i = 0; i < taskArray.length; i++) {
-                        Task currentTask = taskArray[i];
-                        if (taskArray[i] != null) {
-                            String taskName = taskArray[i].getName();
-                            boolean taskStatus = taskArray[i].getStatus();
-                            int num = i + 1;
-                            inputArrayString += "     " + num + ". " + currentTask.toString() + "\n";
-                        } else {
-                            break;
-                        }
-                    }
-                    System.out.println(
-                            "    ____________________________________________________________\n" +
-                                    "     Here are the tasks in your list:\n" +
-                                    inputArrayString +
-                                    "    ____________________________________________________________\n");
+                    listTasks(taskArray);
 
                 } else if (userInput.startsWith("mark")) {
-                    if (userInput.equals("mark")) {
-                        throw new EmptyTaskException("mark");
-                    }
-                    String[] parts = userInput.split("\\s+");
-                    int taskIndex = Integer.parseInt(parts[1]) - 1;
-                    if (taskIndex >= inputNum || taskIndex < 0 || taskArray[taskIndex] == null) {
-                        throw new OutOfRangeException("Mark");
-                    }
-                    Task currentTask = taskArray[taskIndex];
-                    currentTask.markDone();
-                    System.out.println(
-                            "    ____________________________________________________________\n" +
-                                    "     Nice! I've marked this task as done:\n" +
-                                    "       " + currentTask.toString() + "\n" +
-                                    "    ____________________________________________________________\n");
+                    markTask(userInput, inputNum, taskArray);
 
                 } else if (userInput.startsWith("unmark")) {
-                    if (userInput.equals("unmark")) {
-                        throw new EmptyTaskException("unmark");
-                    }
-                    String[] parts = userInput.split("\\s+");
-                    int taskIndex = Integer.parseInt(parts[1]) - 1;
-                    if (taskIndex >= inputNum || taskIndex < 0 || taskArray[taskIndex] == null) {
-                        throw new OutOfRangeException("Unmark");
-                    }
-                    Task currentTask = taskArray[taskIndex];
-                    currentTask.unmarkDone();
-                    System.out.println(
-                            "    ____________________________________________________________\n" +
-                                    "     OK, I've marked this task as not done yet:\n" +
-                                    "       " + currentTask.toString() + "\n" +
-                                    "    ____________________________________________________________\n");
+                    unmarkTask(userInput, inputNum, taskArray);
+
                 } else if (userInput.startsWith("todo")) {
                     if (userInput.equals("todo")) {
                         throw new EmptyTaskException("todo");
                     }
                     String taskName = userInput.substring("todo".length()).trim();
-                    taskArray[inputNum] = new ToDo(taskName);
-                    System.out.println(
-                            "    ____________________________________________________________\n" +
-                                    "     Got it. I've added this task:\n" +
-                                    "       " + taskArray[inputNum].toString() + "\n" +
-                                    "     Now you have " + (inputNum + 1) + " task(s) in the list.\n" +
-                                    "    ____________________________________________________________\n");
+                    makeToDo(taskName, inputNum, taskArray);
                     inputNum++;
 
                 } else if (userInput.startsWith("deadline")) {
-                    String[] parts = getPartsDeadline(userInput);
-                    String taskName = parts[0].trim();
-                    String by = parts[1].trim();
-                    taskArray[inputNum] = new Deadline(taskName, by);
-                    System.out.println(
-                            "    ____________________________________________________________\n" +
-                                    "     Got it. I've added this task:\n" +
-                                    "       " + taskArray[inputNum].toString() + "\n" +
-                                    "     Now you have " + (inputNum + 1) + " task(s) in the list.\n" +
-                                    "    ____________________________________________________________\n");
+                    if (userInput.equals("deadline")) {
+                        throw new EmptyTaskException("deadline");
+                    } else if (userInput.endsWith("/by")) {
+                        throw new EmptyDateException("deadline");
+                    }
+                    String[] deadlineParts = getDeadlineParts(userInput);
+                    String taskName = deadlineParts[0];
+                    String by = deadlineParts[1];
+                    makeDeadline(taskName, by, inputNum, taskArray);
                     inputNum++;
 
                 } else if (userInput.startsWith("event")) {
                     if (userInput.equals("event")) {
                         throw new EmptyTaskException("event");
                     }
-                    String description = userInput.substring("event".length()).trim();
-                    String[] partsA = description.split("/from");
-                    String taskName = partsA[0].trim();
-                    String[] partsB = partsA[1].split("/to");
-                    if (partsB.length == 1 || partsB[0].trim().isEmpty() || partsB[1].trim().isEmpty()) {
-                        throw new EmptyDateException("event");
-                    }
-                    String start = partsB[0].trim();
-                    String end = partsB[1].trim();
-                    taskArray[inputNum] = new Event(taskName, start, end);
-                    System.out.println(
-                            "    ____________________________________________________________\n" +
-                                    "     Got it. I've added this task:\n" +
-                                    "       " + taskArray[inputNum].toString() + "\n" +
-                                    "     Now you have " + (inputNum + 1) + " task(s) in the list.\n" +
-                                    "    ____________________________________________________________\n");
+                    String[] eventParts = getEventParts(userInput);
+                    String taskName = eventParts[0];
+                    String start = eventParts[1];
+                    String end = eventParts[2];
+                    makeEvent(taskName, start, end, inputNum, taskArray);
                     inputNum++;
 
                 } else {
@@ -139,17 +77,110 @@ public class Duke {
         scan.close();
     }
 
-    private static String[] getPartsDeadline(String userInput) throws EmptyTaskException, EmptyDateException {
-        if (userInput.equals("deadline")) {
-            throw new EmptyTaskException("deadline");
-        } else if (userInput.endsWith("/by")) {
-            throw new EmptyDateException("deadline");
+    private static void listTasks(Task[] taskArray) {
+        String inputArrayString = "";
+        for (int i = 0; i < taskArray.length; i++) {
+            Task currentTask = taskArray[i];
+            if (taskArray[i] != null) {
+                int num = i + 1;
+                inputArrayString += "     " + num + ". " + currentTask.toString() + "\n";
+            } else {
+                break;
+            }
         }
+        System.out.println(
+                "    ____________________________________________________________\n" +
+                        "     Here are the tasks in your list:\n" +
+                        inputArrayString +
+                        "    ____________________________________________________________\n");
+
+    }
+
+    private static void markTask(String userInput, int inputNum, Task[] taskArray) throws EmptyTaskException, OutOfRangeException {
+        if (userInput.equals("mark")) {
+            throw new EmptyTaskException("mark");
+        }
+        String[] parts = userInput.split("\\s+");
+        int taskIndex = Integer.parseInt(parts[1]) - 1;
+        if (taskIndex >= inputNum || taskIndex < 0 || taskArray[taskIndex] == null) {
+            throw new OutOfRangeException("Mark");
+        }
+        Task currentTask = taskArray[taskIndex];
+        currentTask.markDone();
+        System.out.println(
+                "    ____________________________________________________________\n" +
+                        "     Nice! I've marked this task as done:\n" +
+                        "       " + currentTask.toString() + "\n" +
+                        "    ____________________________________________________________\n");
+    }
+
+    private static void unmarkTask(String userInput, int inputNum, Task[] taskArray) throws EmptyTaskException, OutOfRangeException {
+        if (userInput.equals("unmark")) {
+            throw new EmptyTaskException("unmark");
+        }
+        String[] parts = userInput.split("\\s+");
+        int taskIndex = Integer.parseInt(parts[1]) - 1;
+        if (taskIndex >= inputNum || taskIndex < 0 || taskArray[taskIndex] == null) {
+            throw new OutOfRangeException("Unmark");
+        }
+        Task currentTask = taskArray[taskIndex];
+        currentTask.unmarkDone();
+        System.out.println(
+                "    ____________________________________________________________\n" +
+                        "     OK, I've marked this task as not done yet:\n" +
+                        "       " + currentTask.toString() + "\n" +
+                        "    ____________________________________________________________\n");
+    }
+
+    private static void makeToDo(String taskName, int inputNum, Task[] taskArray) {
+        taskArray[inputNum] = new ToDo(taskName);
+        System.out.println(
+                "    ____________________________________________________________\n" +
+                        "     Got it. I've added this task:\n" +
+                        "       " + taskArray[inputNum].toString() + "\n" +
+                        "     Now you have " + (inputNum + 1) + " task(s) in the list.\n" +
+                        "    ____________________________________________________________\n");
+    }
+
+    private static String[] getDeadlineParts(String userInput) throws EmptyDateException {
         String description = userInput.substring("deadline".length()).trim();
         String[] parts = description.split("/by");
         if (parts.length == 1) {
             throw new EmptyDateException("deadline");
         }
-        return parts;
+        return new String[]{parts[0].trim(), parts[1].trim()};
+    }
+
+    private static void makeDeadline(String taskName, String by, int inputNum, Task[] taskArray) {
+        taskArray[inputNum] = new Deadline(taskName, by);
+        System.out.println(
+                "    ____________________________________________________________\n" +
+                        "     Got it. I've added this task:\n" +
+                        "       " + taskArray[inputNum].toString() + "\n" +
+                        "     Now you have " + (inputNum + 1) + " task(s) in the list.\n" +
+                        "    ____________________________________________________________\n");
+    }
+
+    private static void makeEvent(String taskName, String start, String end, int inputNum, Task[] taskArray) {
+        taskArray[inputNum] = new Event(taskName, start, end);
+        System.out.println(
+                "    ____________________________________________________________\n" +
+                        "     Got it. I've added this task:\n" +
+                        "       " + taskArray[inputNum].toString() + "\n" +
+                        "     Now you have " + (inputNum + 1) + " task(s) in the list.\n" +
+                        "    ____________________________________________________________\n");
+    }
+
+    private static String[] getEventParts(String userInput) throws EmptyDateException {
+        String description = userInput.substring("event".length()).trim();
+        String[] partsA = description.split("/from");
+        String taskName = partsA[0].trim();
+        String[] partsB = partsA[1].split("/to");
+        if (partsB.length == 1 || partsB[0].trim().isEmpty() || partsB[1].trim().isEmpty()) {
+            throw new EmptyDateException("event");
+        }
+        String start = partsB[0].trim();
+        String end = partsB[1].trim();
+        return new String[]{taskName, start, end};
     }
 }
