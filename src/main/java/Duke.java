73dd793bf13329eceffa2e441857679ac5793876
lerxuann <lerxuan@gmail.com>
2023-08-24 15:46:14 +0800

    Level 6: Delete & Handled more exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,6 +11,20 @@ public class Duke {
             + "Bye. Don't come back!\n"
             + "______________________________________";
 
+    static void removeFromList(int index) throws DukeException {
+        if (index < 0 || index > taskList.size()-1) {
+            throw new DukeException("Wow, deleting a nonexistent task? Check your tasks again with 'list'.");
+        }
+        Task t = taskList.get(index);
+        taskList.remove(t);
+        String returnLine = "______________________________________\n"
+                + "Looks like you have more time to sleep now. Deleted this for you:\n"
+                + t.toString()
+                + "\nYou now have " + taskList.size() + " things to do.\n"
+                + "______________________________________\n";
+        System.out.println(returnLine);
+    }
+
     static void addToList(String str) throws DukeException {
         Task t = null;
         if (str.startsWith("todo")) {
@@ -33,6 +47,12 @@ public class Duke {
             }
             int indexBy = str.lastIndexOf("/by");
             t = new Deadline(str.substring(9, indexBy-1), str.substring(indexBy+4));
+        } else if (str.startsWith("mark")) {
+            throw new DukeException("Specify the task number after the word 'mark', please. I can't read minds.");
+        } else if (str.startsWith("unmark")) {
+            throw new DukeException("Specify the task number after the word 'unmark', please. I can't read minds.");
+        } else if (str.startsWith("delete")) {
+            throw new DukeException("Specify the task number after the word 'delete', please. I can't read minds.");
         }
         if (t == null) {
             throw new DukeException("Uncivilised speech. Please try again with words I can understand.");
@@ -62,12 +82,21 @@ public class Duke {
                 isEnd = true;
             } else if (command.equals("list")) {
                 listTasks();
-            } else if (command.startsWith("mark")) {
+            } else if (command.startsWith("mark") && command.length() > 5) {
                 int index = Integer.parseInt(command.substring(5));
+                if (index <= 0 || index > taskList.size()) {
+                    throw new DukeException("Wow, that's a nonexistent task. Check your tasks again with 'list'.");
+                }
                 taskList.get(index-1).markAsDone();
-            } else if (command.startsWith("unmark")) {
+            } else if (command.startsWith("unmark") && command.length() > 7) {
                 int index = Integer.parseInt(command.substring(7));
+                if (index <= 0 || index > taskList.size()) {
+                    throw new DukeException("Wow, that's a nonexistent task. Check your tasks again with 'list'.");
+                }
                 taskList.get(index-1).markAsUndone();
+            } else if (command.startsWith("delete") && command.length() > 7) {
+                int index = Integer.parseInt(command.substring(7));
+                removeFromList(index-1);
             } else {
                 addToList(command);
             }
