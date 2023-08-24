aa49ad72382cee54092d2fca2316b0a20bc5a22b
dinde2004 <doanquocthinh2004@gmail.com>
2023-08-24 10:33:45 +0800

    Add new name to chatbot. Implement greetings and exits.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("_________________________________________________________________________________________");
+        System.out.println("Hello! I'm Thinh's chatbot\nWhat can I do for you?");
+        System.out.println("_________________________________________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_________________________________________________________________________________________");
     }
 }
