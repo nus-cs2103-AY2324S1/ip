171d6d669a55a1c927e9a85c91ca95309acd5a6d
Singa-Pirate <E0969244@u.nus.edu>
2023-08-24 18:23:16 +0800

    Level-0: rename, greeting, exit

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,10 +1,13 @@
-public class Duke {
+public class ChatBot {
+    static final String line = "";
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "4F5DA2";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
     }
 }
