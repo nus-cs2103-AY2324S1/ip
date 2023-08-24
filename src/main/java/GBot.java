ae963bc67b66d15a26ef11b282b85991bfec1e51
gongg21 <gallen.ong21@gmail.com>
2023-08-25 00:32:16 +0800

    Level-2

--- src/main/java/GBot.java
+++ src/main/java/GBot.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GBot {
@@ -5,13 +6,23 @@ public class GBot {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello I'm GBot\n");
         System.out.println("What can I do for you today?\n");
+
+        ArrayList<String> list = new ArrayList<>();
+
         while (true) {
             String message = scanner.nextLine();
-            if (message.equals("bye")) {
+            if (message.equals("list")) {
+                int len = list.size();
+                for (int i = 0; i < len; i++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
+            } else if (message.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!\n");
                 return;
+            } else {
+                list.add(message);
+                System.out.println("added: " + message);
             }
-            System.out.println(message);
         }
     }
 }
