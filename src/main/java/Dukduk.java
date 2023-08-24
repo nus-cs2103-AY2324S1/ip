40e35795377fbf1e3c8fc8662120c19b117c44ba
wnchan <e0970469@u.nus.edu>
2023-08-24 16:45:58 +0800

    Level-2: Add, List

--- src/main/java/Dukduk.java
+++ src/main/java/Dukduk.java
@@ -7,19 +7,32 @@ public class Dukduk {
         System.out.println(" What can I do for you?");
         printLine();
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print(" ");
             String input = scanner.nextLine();
-            printLine();
-            System.out.println(" " + input);
             printLine();
 
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println(" Bye. Hope to see you again soon!");
                 printLine();
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                if (taskCount == 0) {
+                    System.out.println(" No tasks added yet.");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    }
+                }
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println(" added: " + input);
             }
+            printLine();
         }
         scanner.close();
     }
