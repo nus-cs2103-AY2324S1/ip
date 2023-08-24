20e2fba2a04df3e5b0e666fbbbdf0e11e9975891
suryanshkushwaha <e0985892@u.nus.edu>
2023-08-24 19:57:43 +0800

    mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-
     Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         Duke duke = new Duke();
@@ -27,23 +26,71 @@ public class Duke {
     }
 
     public void run() {
+        ArrayList<Task> tasks = new ArrayList<>();
         String input = sc.nextLine();
-        ArrayList<String> store = new ArrayList<>();
+
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 lines();
-                for (int i = 0; i < store.size(); i++) {
-                    System.out.println(i + 1 + ". " + store.get(i));
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
                 }
                 lines();
-                input = sc.nextLine();
-                continue;
+            } else if (input.contains("unmark")) {
+                lines();
+                String[] split = input.split(" ");
+                int index = Integer.parseInt(split[1]) - 1;
+                tasks.get(index).unmark();
+                System.out.println(tasks.get(index));
+                lines();
+            } else if (input.contains("mark")) {
+                lines();
+                String[] split = input.split(" ");
+                int index = Integer.parseInt(split[1]) - 1;
+                tasks.get(index).mark();
+                System.out.println(tasks.get(index));
+                lines();
+            } else {
+                lines();
+                System.out.println("added: " + input);
+                tasks.add(new Task(input));
+                lines();
             }
-            lines();
-            store.add(input);
-            System.out.println("added: " + input);
-            lines();
             input = sc.nextLine();
+        }
+    }
+
+    public class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); //return tick or X symbols
+        }
+
+        public void mark() {
+            this.isDone = true;
+            System.out.println("Nice! I've marked this task as done:");
+        }
+
+        public void unmark() {
+            this.isDone = false;
+            System.out.println("OK, I've marked this task as not done yet:");
+        }
+
+        public String getDescription() {
+            return this.description;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + this.getStatusIcon() + "] " + this.getDescription();
         }
     }
 }
