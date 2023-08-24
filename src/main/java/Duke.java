e0b0cbca29debe06cad118530d4188cce0e9c330
AnnabelTing <tsy.annabel@gmail.com>
2023-08-24 20:31:40 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,24 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    static ArrayList<String> taskList = new ArrayList<>();
+    static int counter = 0;
+
     private static void greet() {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+    }
+
+    private static void addTask(String userCommand) {
+        taskList.add(userCommand);
+        counter++;
+        System.out.println("added: " + userCommand);
+    }
+
+    private static void listTask() {
+        for (int i = 1; i < taskList.size() + 1; i++) {
+            System.out.println(i + ". " + taskList.get(i - 1));
+        }
     }
 
     private static void exit() {
@@ -12,10 +28,14 @@ public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         greet();
-        String userCommand = sc.next();
-        while(!userCommand.equals("bye")){
-            System.out.println(userCommand);
-            userCommand = sc.next();
+        String userCommand = sc.nextLine();
+        while(!userCommand.equals("bye")) {
+            if(userCommand.equals("list")) {
+                listTask();
+            } else {
+                addTask(userCommand);
+            }
+            userCommand = sc.nextLine();
         }
         exit();
     }
