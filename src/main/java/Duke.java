fab27d5a89bee867003c95bd026381378e82d158
lynnlow175 <lowhanlynn@Lows-MacBook-Air.local>
2023-08-24 17:44:51 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,25 @@
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm lippy the wombat\n" + "What can I do for you?\n");
         Scanner scanner = new Scanner(System.in);
+        List<String> tasks = new ArrayList<>();
         while (true) {
             String input = scanner.nextLine();
             if (Objects.equals(input, "bye")) {
                 break;
+            }
+            if (Objects.equals(input, "list")) {
+                for (int i = 0; i < tasks.size(); i ++) {
+                    int num = i + 1;
+                    System.out.println(num + ". " + tasks.get(i));
+                }
             } else {
-                System.out.println(input);
+                tasks.add(input);
+                System.out.println("added: " + input);
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
