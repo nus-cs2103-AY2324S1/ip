cfaa5a99f4bc2c962c9cc1c0dad8c1c97adccb0a
josepholim <joseph.o.lim@gmail.com>
2023-08-24 13:29:43 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,13 +3,30 @@ import java.util.Scanner;
 public class Duke {
     private static final String HORIZONTAL_LINE = "____________________________________________________________";
     private Scanner sc = new Scanner(System.in);
+    private String[] tasks = new String[100];
+    private int countTasks = 0;
+
     public static void main(String[] args) {
         Duke duke = new Duke();
         duke.run();
     }
 
-    public void printWithLines(String s){
+    public void printWithLines(String s) {
         System.out.println(HORIZONTAL_LINE + "\n" + s + "\n" + HORIZONTAL_LINE);
+    }
+
+    public void addTask(String s) {
+        tasks[countTasks] = s;
+        countTasks++;
+        printWithLines("added: " + s);
+    }
+
+    public void printList() {
+        System.out.println(HORIZONTAL_LINE);
+        for (int i = 0; i < countTasks; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
+        System.out.println(HORIZONTAL_LINE);
     }
 
     public void run() {
@@ -20,8 +37,10 @@ public class Duke {
             if (input.equals("bye")) {
                 printWithLines("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                printList();
             } else {
-                printWithLines(input);
+                addTask(input);
             }
         }
     }
