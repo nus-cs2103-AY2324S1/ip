1d108c8490cc73afa349ee906dd714451be8b34b
fuyiqiao <yiqiao_fu@hotmail.com>
2023-08-24 11:50:52 +0800

    Level 0

---
+++ src/main/java/Ballsorter.java
@@ -0,0 +1,9 @@
+public class Ballsorter {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Ballsorter\nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
