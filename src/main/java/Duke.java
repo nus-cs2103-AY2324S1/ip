e9888a8a521b9a1b41fe7889d35680e632c90289
Wee Hung <cwhung1804@gmail.com>
2023-08-24 18:22:58 +0800

    Level-1: Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
+
 public class Duke {
+
+    //function to retrieve string that the user input
+    public static String getInput() {
+        Scanner scanner = new Scanner(System.in);
+        //to mimic chatBot
+        System.out.print(" ");
+        return scanner.nextLine();
+    }
+
     public static void main(String[] args) {
         String horizontalLine = "   ---------------------------------------------------";
         String intro = "    Hello! I'm iPbot \n    What can I do for you?";
@@ -6,6 +18,20 @@ public class Duke {
 
         System.out.println(horizontalLine);
         System.out.println(intro);
+        System.out.println(horizontalLine);
+
+        while (true) {
+            String input = getInput();
+            if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println(horizontalLine);
+            System.out.println("     " + input);
+            System.out.println(horizontalLine);
+
+        }
+
         System.out.println(horizontalLine);
         System.out.println(outro);
         System.out.println(horizontalLine);
