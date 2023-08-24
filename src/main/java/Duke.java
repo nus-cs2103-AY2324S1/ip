115332139829ea8a56052f890d01fd69d84564e7
erohsikivar <ravikishore1809@gmail.com>
2023-08-24 11:06:50 +0800

    Mark As Done (new class Task added)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,24 +10,43 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        String[] list = new String[100];
+        Task[] task = new Task[100];
         int i = 0;
 
         while (!str.equals("bye") && !str.equals("Bye")) {
-            if (!str.equals("list") && !str.equals("List")) {
+            if (str.equals("list")){
                 System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
-                System.out.println("Added: " + str);
-                list[i] = str;
-                i++;
-                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
-                str = sc.nextLine();
-            } else {
-                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                System.out .println("Here are the tasks in your list:");
                 for (int j = 0; j < i; j++){
-                    System.out.println(j+1 +". " + list[j]);
+                    System.out.println(j+1 +"." + task[j].getStatusIcon());
                 }
                 System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
                 str = sc.nextLine();
+            } else if (str.equals("mark")) {
+                int k = sc.nextInt() - 1;
+                task[k].markAsDone();
+                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                System.out.println("Nice! I've marked this task as done:\n" + task[k].getStatusIcon());
+                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                str = sc.nextLine();
+            } else if (str.equals("unmark")) {
+                int k = sc.nextInt() - 1;
+                task[k].unmark();
+                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                System.out.println("OK, I've marked this task as not done yet:\n" + task[k].getStatusIcon());
+                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                str = sc.nextLine();
+            } else {
+                if (str.equals("")) {
+                    str = sc.nextLine();
+                } else {
+                    System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                    System.out.println("Added: " + str);
+                    task[i] = new Task(str);
+                    i++;
+                    System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                    str = sc.nextLine();
+                }
             }
         }
 
