c175134a4bdb289b916e6f81dcadcec925fa5133
MathanC <ncmathan98@gmail.com>
2023-08-24 18:11:24 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,10 +10,23 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
+        ArrayList<String> list = new ArrayList<>();
         while (!str.equals("bye")){
-            System.out.println("____________________________________________________________\n" +
-                    str+"\n" +
-                    "____________________________________________________________");
+            if(str.equals("list")){
+                System.out.println("____________________________________________________________");
+                for(int i=0;i<list.size();i++){
+                    int j=i+1;
+                    System.out.println(j+". "+list.get(i)+"");
+                }
+                System.out.println("____________________________________________________________");
+            }
+            else {
+                list.add(str);
+
+                System.out.println("____________________________________________________________\n" +
+                        str + "\n" +
+                        "____________________________________________________________");
+            }
             str = scanner.nextLine();
         }
         System.out.println("____________________________________________________________\n" +
