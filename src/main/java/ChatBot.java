87514659cab4f21fcc0ef9fd977ffec0cbf40852
Singa-Pirate <E0969244@u.nus.edu>
2023-08-24 18:45:41 +0800

    Level-1: echo and bye

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,13 +1,42 @@
+import java.util.Scanner;
+
 public class ChatBot {
-    static final String line = "";
+    static final String name = "4F5DA2";
+    static final String line = "\t____________________________________________________________";
+    static final Scanner scanner = new Scanner(System.in);
+
+    public static void greet() {
+        System.out.println(line);
+        System.out.println("\tWelcome back, human!");
+        System.out.println("\tI'm your personal ChatBot, " + name + ".");
+        System.out.println("\tWhat can I do for you today?");
+        System.out.println(line);
+    }
+
+    public static void exit() {
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+
+    public static void output(String output) {
+        System.out.println(line);
+        System.out.println(output);
+        System.out.println(line);
+    }
+
+    public static void nextCommand() {
+        String command = scanner.nextLine();
+        if (command.equals("bye")) {
+            exit();
+        } else {
+            output("\t" + command);
+            nextCommand();
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        String name = "4F5DA2";
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
+        greet();
+        nextCommand();
+
     }
 }
