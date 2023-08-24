cbaecf5f973c3ff82239a9255392be1e6e63b657
hcs1203 <solomonrajhannah@gmail.com>
2023-08-24 22:55:01 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String div = "____________________________________________________________\n";
         class Task {
             protected String name;
             protected boolean isDone = false;
@@ -12,14 +13,12 @@ public class Duke {
 
             public void mark(){
                 isDone = true;
-                System.out.println("Nice! I've marked this task as done:\n [X] " + name +"\n");
-                System.out.println("____________________________________________________________\n");
+                System.out.println("Nice! I've marked this task as done:\n [X] " + name +"\n" + div);
             }
 
             public void unmark(){
                 isDone = false;
-                System.out.println("OK, I've marked this task as not done yet:\n [ ] " + name + "\n");
-                System.out.println("____________________________________________________________\n");
+                System.out.println("OK, I've marked this task as not done yet:\n [ ] " + name + "\n" + div);
             }
 
             public String printTask() {
@@ -27,25 +26,63 @@ public class Duke {
             }
         }
 
+        class ToDos extends Task {
+            public ToDos(String name) {
+                super(name);
+            }
+
+            @Override
+            public String printTask() {
+                return (isDone) ? "[T] [X] " + name + "\n": "[T] [ ] " + name + "\n";
+            }
+        }
+
+        class Deadline extends Task {
+            String due;
+            public Deadline(String name, String due) {
+                super(name);
+                this.due = due;
+            }
+
+            @Override
+            public String printTask() {
+                return (isDone) ? "[D] [X] " + name + "(" + due + ") \n"
+                        : "[D] [ ] " + name + "(" + due + ") \n";
+            }
+        }
+
+        class Events extends Task {
+            String start;
+            String end;
+            public Events(String name, String start, String end) {
+                super(name);
+                this.start = start;
+                this.end = end;
+            }
+
+            @Override
+            public String printTask() {
+                return (isDone) ? "[E] [X] " + name + "(" + start + " " + end + ") \n"
+                        : "[E] [ ] " + name + "(" + start + " " + end + ") \n";
+            }
+        }
+
         Scanner scanner = new Scanner(System.in);
         Task tasks[] = new Task[100];
 
-        System.out.println("____________________________________________________________\n" +
-                "Hello! I'm CarrotCake\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n");
+        System.out.println(div + "Hello! I'm CarrotCake\nWhat can I do for you?\n" + div);
 
         String input = scanner.nextLine();
         int count = 0;
 
         while (!input.toLowerCase().equals("bye")) {
-            System.out.println("____________________________________________________________\n");
+            System.out.println(div);
             if (input.toLowerCase().equals("list")) {
                 //Print tasks
                 for (int i = 0; i < count; i++) {
                     System.out.print(Integer.toString(i+1) + ". " + tasks[i].printTask());
                 }
-                System.out.println("____________________________________________________________\n");
+                System.out.println(div);
                 input = scanner.nextLine();
                 continue;
             }
@@ -64,17 +101,34 @@ public class Duke {
                 continue;
             }
 
+            if (input.toLowerCase().startsWith("todo ")) {
+                input = input.substring(4, input.length());
+                System.out.println("Got it. I've  added this task: \n[T] []" + input +
+                        "\nNow you have "+ (count+1)+ " tasks in the list.\n" +div);
+                tasks[count] = new ToDos(input);
+            }
+            else if (input.toLowerCase().startsWith("deadline ")) {
+                String[] parts = input.split("/");
+                String due = parts[1];
+                input = parts[0].substring(8, parts[0].length());
+                System.out.println("Got it. I've  added this task: \n[D] []" + input + "(" + due + ")" +
+                        "\nNow you have "+ (count+1) + " tasks in the list.\n" +div);
+                tasks[count] = new Deadline(input, due);
+            }
+            else if (input.toLowerCase().startsWith("event ")) {
+                String[] parts = input.split("/");
+                String start = parts[1];
+                String end = parts[2];
+                input = parts[0].substring(5, parts[0].length());
+                System.out.println("Got it. I've  added this task: \n[E] []" + input + "(" + start + " " + end + ")" +
+                        "\nNow you have "+ (count + 1) + " tasks in the list.\n" +div);
+                tasks[count] = new Events(input, start, end);
+            }
 
-            System.out.println("added: " + input +
-                    "\n____________________________________________________________\n");
-
-            tasks[count] = new Task(input);
             input = scanner.nextLine();
             count++;
         }
 
-        System.out.println("____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+        System.out.println(div + "Bye. Hope to see you again soon!\n" + div);
     }
 }
