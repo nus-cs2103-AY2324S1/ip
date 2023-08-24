f4b7097a1088f89bf73516fb9b56fb828177ee96
erohsikivar <ravikishore1809@gmail.com>
2023-08-24 17:25:38 +0800

    Delete tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,8 +11,7 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        Task[] task = new Task[100];
-        int i = 0;
+        ArrayList<Task> tasklist = new ArrayList<Task>();
 
         while (!str.equals("bye")) {
             String[] input = str.split(" ", 2);
@@ -19,75 +19,80 @@ public class Duke {
             try {
                 if (str.equals("todo") || str.equals("deadline") || str.equals("event")) {
                     throw new DukeException("OOPS!!! The description of a " + str + " cannot be empty.");
-                } else if (str.equals("mark") || str.equals("unmark")) {
-                    throw new DukeException("OOPS!!! Please specify which task you want to mark or unmark.");
-                } else if (!str.equals("bye") && !str.equals("list")) {
+                } else if (str.equals("mark") || str.equals("unmark") || str.equals("delete")) {
+                    throw new DukeException("OOPS!!! Please specify which task you want to mark, unmark or delete .");
+                }
+
+                else if (input[0].equals("list")){
+                    System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+
+                    System.out .println(" Here are the tasks in your list:");
+                    int x = 1;
+                    for (Task task : tasklist) {
+                        System.out.println(x + "." + task.getStatusIcon());
+                        x++;
+                    }
+                    System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                    str = sc.nextLine();
+                } else if (input[0].equals("mark")) {
+                    int index = Integer.parseInt(input[1]) - 1;
+                    tasklist.get(index).markAsDone();
+                    System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                    System.out.println(" Nice! I've marked this task as done:\n" + "   " + tasklist.get(index).getStatusIcon());
+                    System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                    str = sc.nextLine();
+                } else if (input[0].equals("unmark")) {
+                    int index = Integer.parseInt(input[1]) - 1;
+                    tasklist.get(index).unmark();
+                    System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                    System.out.println(" OK, I've marked this task as not done yet:\n" + "   " + tasklist.get(index).getStatusIcon());
+                    System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                    str = sc.nextLine();
+                } else if (input[0].equals("todo")) {
+                    String description = input[1];
+                    tasklist.add(new Todo(description));
+                    System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                    System.out.println(" Got it. I've added this task:\n" + "   " + tasklist.get(tasklist.size() - 1).getStatusIcon());
+                    System.out.println(" Now you have " + tasklist.size() +" tasks in the list.");
+                    System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                    str = sc.nextLine();
+                } else if (input[0].equals("deadline")) {
+                    String[] full_desc = input[1].split(" /by ");
+                    String description = full_desc[0];
+                    String by = full_desc[1];
+                    tasklist.add(new Deadline(description, by));
+                    System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                    System.out.println(" Got it. I've added this task:\n" + "   " + tasklist.get(tasklist.size() - 1).getStatusIcon());
+                    System.out.println(" Now you have " + tasklist.size() +" tasks in the list.");
+                    System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                    str = sc.nextLine();
+                } else if (input[0].equals("event")) {
+                    String[] full_desc = input[1].split(" /from | /to ");
+                    String description = full_desc[0];
+                    String from = full_desc[1];
+                    String to = full_desc[2];
+                    tasklist.add(new Event(description, from, to));
+                    System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                    System.out.println(" Got it. I've added this task:\n" + "   " + tasklist.get(tasklist.size() - 1).getStatusIcon());
+                    System.out.println(" Now you have " + tasklist.size() +" tasks in the list.");
+                    System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                    str = sc.nextLine();
+                } else if (input[0].equals("delete")) {
+                    int index = Integer.parseInt(input[1]) - 1;
+                    System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                    System.out.println(" Noted. I've removed this task:\n" + "   " + tasklist.get(index).getStatusIcon());
+                    tasklist.remove(index);
+                    System.out.println(" Now you have " + tasklist.size() + " tasks in the list.");
+                    System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
+                    str = sc.nextLine();
+                }
+
+                else {
                     throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
             } catch (DukeException e) {
                 System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
                 System.out.println(e.getMessage());
-                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
-                str = sc.nextLine();
-                continue;
-            }
-
-            if (input[0].equals("list")){
-                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
-
-                System.out .println(" Here are the tasks in your list:");
-                if (i == 0) {
-                    System.out.println("You have 0 tasks to be addressed.");
-                }
-                for (int j = 0; j < i; j++){
-                    System.out.println(j+1 +"." + task[j].getStatusIcon());
-                }
-                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
-                str = sc.nextLine();
-            } else if (input[0].equals("mark")) {
-                int index = Integer.parseInt(input[1]) - 1;
-                task[index].markAsDone();
-                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
-                System.out.println(" Nice! I've marked this task as done:\n" + "   " + task[index].getStatusIcon());
-                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
-                str = sc.nextLine();
-            } else if (input[0].equals("unmark")) {
-                int index = Integer.parseInt(input[1]) - 1;
-                task[index].unmark();
-                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
-                System.out.println(" OK, I've marked this task as not done yet:\n" + "   " + task[index].getStatusIcon());
-                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
-                str = sc.nextLine();
-            } else if (input[0].equals("todo")) {
-                String description = input[1];
-                task[i] = new Todo(description);
-                i++;
-                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
-                System.out.println(" Got it. I've added this task:\n" + "   " + task[i-1].getStatusIcon());
-                System.out.println(" Now you have " + i +" tasks in the list.");
-                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
-                str = sc.nextLine();
-            } else if (input[0].equals("deadline")) {
-                String[] full_desc = input[1].split(" /by ");
-                String description = full_desc[0];
-                String by = full_desc[1];
-                task[i] = new Deadline(description, by);
-                i++;
-                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
-                System.out.println(" Got it. I've added this task:\n" + "   " + task[i-1].getStatusIcon());
-                System.out.println(" Now you have " + i +" tasks in the list.");
-                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
-                str = sc.nextLine();
-            } else if (input[0].equals("event")) {
-                String[] full_desc = input[1].split(" /from | /to ");
-                String description = full_desc[0];
-                String from = full_desc[1];
-                String to = full_desc[2];
-                task[i] = new Event(description, from, to);
-                i++;
-                System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
-                System.out.println(" Got it. I've added this task:\n" + "   " + task[i-1].getStatusIcon());
-                System.out.println(" Now you have " + i +" tasks in the list.");
                 System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
                 str = sc.nextLine();
             }
