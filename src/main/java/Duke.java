41ec3644fe4eeece8d96437e7c34b205806bbb24
Taanish Bhardwaj <btaanish07@gmail.com>
2023-08-24 11:54:44 +0800

    Completing Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,13 @@ public class Duke {
         String line = "____________________________________________________________\n";
         System.out.println(line + " Hello! I'm Alcazar\n" +
                 " What can I do for you?\n" +
-                line +
+                line);
+        String prompt = Duke.inputText();
+        while (!prompt.equals("bye")) {
+            System.out.println(line + prompt + "\n" + line);
+            prompt = Duke.inputText();
+        }
+        System.out.println(line +
                 " Bye. Hope to see you again soon!\n" +
                 line);
     }
