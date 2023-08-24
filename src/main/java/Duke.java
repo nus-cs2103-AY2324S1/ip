0732b2b1445dc9a43cbb2edf9e5ebedcddf8589a
Vedant Sinha <vedantsinha2003@gmail.com>
2023-08-24 20:49:31 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         // Greeting message
         System.out.println("Hello! I'm Sivraj");
         System.out.println("What can I do for you?");
 
-        // Farewell message
-        System.out.println("Bye. Hope to see you again soon!");
+        while (true) {
+            String echo = scanner.nextLine();
+
+            if (echo.equals("bye")) {
+                // Farewell message
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(" ----------------------------------------");
+                System.out.println("    " + echo);
+                System.out.println(" ----------------------------------------");
+            }
+        }
+
+        scanner.close();
+
     }
 }
