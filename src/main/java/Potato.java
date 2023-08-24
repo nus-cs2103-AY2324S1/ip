fb38a1abbd68d4601a79b4565f273d731ebeb596
peiran18 <e0968798@u.nus.edu>
2023-08-24 08:11:17 +0800

    Store and display user input

--- src/main/java/Potato.java
+++ src/main/java/Potato.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 public class Potato {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] store = new String[100];
+        int size = 0;
+
         System.out.println("-----------------------------------------\n" +
                 "Hello! I'm Potato\n" + "What can I do for you?\n" +
                 "-----------------------------------------\n");
@@ -13,11 +16,21 @@ public class Potato {
                         "Bye. Hope to see you again soon!\n" +
                         "-----------------------------------------\n");
                 break;
+            } else if (input.equals("list")) {
+                int count = 0;
+                System.out.println("-----------------------------------------");
+                for (String s : store) {
+                    if (s == null) break;
+                    count ++;
+                    System.out.println(String.valueOf(count) + ". " + s);
+                }
+                System.out.println("-----------------------------------------\n");
             } else {
+                store[size] = input;
+                size++;
                 System.out.println("-----------------------------------------\n" +
-                        input + "\n" +
+                        "added: " + input + "\n" +
                         "-----------------------------------------\n");
-                continue;
             }
         }
     }
