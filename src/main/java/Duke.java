d78465d27414e474e455515bec92b815d4a39f1e
aslam341 <aslam.singapore@gmail.com>
2023-08-24 12:02:12 +0800

    Echoes user commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String LINE_SEPARATOR = "____________________________________________________________";
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE_SEPARATOR);
         System.out.println("Hello! I'm David.");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE_SEPARATOR);
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                printWithSeparator("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                printWithSeparator(input);
+            }
+        }
+    }
+
+    private static void printWithSeparator(String message) {
+        System.out.println(LINE_SEPARATOR);
+        System.out.println(message);
+        System.out.println(LINE_SEPARATOR);
     }
 }
