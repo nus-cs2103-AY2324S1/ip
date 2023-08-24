e852d40b21f02c8d6238e05e8d553bd96005f613
yongning0310 <85162884+yongning0310@users.noreply.github.com>
2023-08-24 14:22:10 +0800

    keeps track of list and prints out when required

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -13,14 +14,21 @@ public class Duke {
         System.out.println("What can I do for you?\n");
 
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<String> inputs = new ArrayList<>();
         while (true) {
             String input = scanner.nextLine();
             if ("bye".equals(input)) {
                 System.out.println("Bye. Hope to see you again soon!\n");
                 break; // This will exit the loop and end the program
             } else {
-                System.out.println(input);
+                if ("list".equals(input)) {
+                    for (int i = 1; i < inputs.size()+1; i++) {
+                        System.out.println(i + ". " + inputs.get(i-1));
+                    }
+                } else {
+                    inputs.add(input);
+                    System.out.println("added: " + input);
+                }
             }
         }
 
