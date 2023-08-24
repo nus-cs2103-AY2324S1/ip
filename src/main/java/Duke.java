69316f7eade964abb61796382883b7aa12bf4ca3
Eola-Z <sxplayer123@gmail.com>
2023-08-25 01:14:00 +0800

    Personalized the chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+/**
+ * The main class controlling the chatbot's actions.
+ * This class should be able to keep track of tasks, and
+ * send messages to the user.
+ */
 public class Duke {
+    /**
+     * Common text elements to be reused by the chatbot, such as the chatbot name,
+     * and the horizontal line element.
+     */
+    private static final String name = "JOHNATHAN CENATOR\n";
+    private static final String horizontalLine = "-------------------------------\n";
+
+    /**
+     * Sends a greeting message on startup of the chatbot.
+     */
+    private static void greet() {
+        System.out.println(horizontalLine +
+                "YOU DIDN'T SEE\n" +
+                name +
+                "COMING DID YOU!?\n" +
+                horizontalLine);
+    }
+
+    /**
+     * Sends a departing message on chatbot shutdown.
+     */
+    private static void exit() {
+        System.out.println("NOW GET OUTTA HERE!\n" +
+                "RESPECTFULLY,\n" +
+                name +
+                horizontalLine);
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        exit();
     }
 }
