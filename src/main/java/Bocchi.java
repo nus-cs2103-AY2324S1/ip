2096afc76419d50e45c6e7c77a9a5697a68f3671
Jun Hong <junhongloww@gmail.com>
2023-08-24 19:42:19 +0800

    Add private methods for greeting and exit

--- src/main/java/Bocchi.java
+++ src/main/java/Bocchi.java
@@ -2,15 +2,29 @@ public class Bocchi {
     private static final String LINE_BREAK = "___________________________________________________";
 
     /**
-     * Main method.
+     * Outputs greeting message.
      */
-    public static void main(String[] args) {
-
+    private static void greet() {
         System.out.println(LINE_BREAK);
         System.out.println("Hello! I'm Bocchi");
         System.out.println("What can I do for you?");
         System.out.println(LINE_BREAK);
+    }
+
+    /**
+     * Outputs exit message.
+     */
+    private static void exit() {
+        System.out.println(LINE_BREAK);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(LINE_BREAK);
+    }
+    /**
+    /**
+     * Main method.
+     */
+    public static void main(String[] args) {
+        greet();
+        exit();
     }
 }
