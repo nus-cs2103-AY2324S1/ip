0b518f9158f2db6885310a6ad05c0ef217a238ce
jovkusuma <jovankusuma1@gmail.com>
2023-08-24 14:36:20 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-
+        String farewell = " Bye. Hope to see you again soon!";
         String message = "____________________________________________________________\n"
                 + " Hello! I'm [YOUR CHATBOT NAME]\n"
                 + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
 
         System.out.println(message);
 
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            String input = scanner.nextLine();
+            System.out.println("----------------------------------------------");
+            System.out.println(input);
+            System.out.println("----------------------------------------------");
+
+            if(input.equalsIgnoreCase("bye")) {
+                System.out.println("----------------------------------------------");
+                System.out.println(farewell);
+                System.out.println("----------------------------------------------");
+                break;
+            }
+        }
     }
 }
