234a4fa883b8f371b2fe44ac442808124b5574e0
Tim-Siu <e1088140@u.nus.edu>
2023-08-24 11:29:50 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
 public class Duke {
+    private static final String name = "Kevin";
+    private static final int splitterLength = 50;
+    private static void lineSplitter() {
+        for (int i = 0; i < Duke.splitterLength; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
+    private static void greet() {
+        lineSplitter();
+        System.out.println("Hello! I'm " + Duke.name + "\n" + "What can I do for you?\n");
+    }
+
+    private static void bye() {
+        lineSplitter();
+        System.out.println("Bye. Hope to see you again soon!\n");
+        lineSplitter();
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        bye();
     }
 }
