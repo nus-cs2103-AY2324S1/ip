18a18476dd23377e5d0119ddc2bd188f589a1699
LIN <peterway6933@gmail.com>
2023-08-24 17:14:29 +0800

    Echo

--- src/main/java/Alex.java
+++ src/main/java/Alex.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class Alex {
     public static void main(String[] args) {
         String greeting = "_____________________________________________________________\n"
-                        + "Hello! I'm your personal task assistant, Alex\n"
-                        + "What can I do for you today?\n\n"
-                        + "_____________________________________________________________\n"
-                        + "Bye. Hope to see you again soon!\n"
-                        + "_____________________________________________________________\n";
+                + "Hello! I'm your personal task assistant, Alex\n"
+                + "What can I do for you today?\n\n"
+                + "_____________________________________________________________\n";
+
+        String bye = "_____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "_____________________________________________________________\n";
+
         System.out.println(greeting);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println(bye);
+                break;
+            } else {
+                System.out.println("_____________________________________________________________\n"
+                        + userInput
+                        + "\n"
+                        + "_____________________________________________________________\n"
+                );
+            }
+        }
     }
 }
