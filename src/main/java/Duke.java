1ec27984e367bee3238a427b73ca19452274dd20
ChuanXinNg <ngcx0103@gmail.com>
2023-08-24 17:22:58 +0800

    Use Enums

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,10 @@ public class Duke {
 
     private final ArrayList<Task> data = new ArrayList<>();
 
+    private enum Command {
+        LIST, UNMARK, MARK, DELETE, TODO, DEADLINE, EVENT, END, UNKNOWN
+    }
+
     public void greet() {
         String logo = " ____                \n"
                 + "|  _ \\  _____  _____ \n"
@@ -50,108 +54,150 @@ public class Duke {
         System.out.println("You still have " + data.size() + " tasks in the list\n");
     }
 
-    public static void print(String input, Duke roo, Scanner sc) {
-        while (true) {
+    public void print(String input, Duke roo, Scanner sc) {
+        boolean loop = true;
+
+        while (loop) {
+            Command c;
             if (input.equals("list")) {
-                roo.list();
-                input = sc.nextLine();
+                c = Command.LIST;
             } else if (input.startsWith("unmark")) {
-                try {
-                    if (input.length() < 8) {
-                        throw new DukeException("Please unmark your task using this format: \"unmark [serial number]\"\n");
-                    }
-                    int t = Integer.parseInt(input.substring(7));
-                    if (t > roo.data.size()) {
-                        throw new DukeException("We dunhave so many task lah =_=\n");
-                    } else if (!roo.data.get(t - 1).done()) {
-                        throw new DukeException("Weihh... It's unmark ehhh\n");
-                    }
-                    roo.markUndone(t - 1);
-                } catch (DukeException e) {
-                    System.err.println(e);
-                } finally {
-                    input = sc.nextLine();
-                }
+                c = Command.UNMARK;
             } else if (input.startsWith("mark")) {
-                try {
-                    if (input.length() < 6) {
-                        throw new DukeException("Please mark your task using this format: \"mark [serial number]\"\n");
-                    }
-                    int t = Integer.parseInt(input.substring(5));
-                    if (t > roo.data.size()) {
-                        throw new DukeException("We dunhave so many task lah =_=\nq");
-                    } else if (roo.data.get(t - 1).done()) {
-                        throw new DukeException("Weihh... It's already mark ehhh\n");
-                    }
-                    roo.markDone(t - 1);
-                } catch (DukeException e) {
-                    System.err.println(e);
-                } finally {
-                    input = sc.nextLine();
-                }
+                c = Command.MARK;
             } else if (input.startsWith("delete")) {
-                try {
-                    if (input.length() < 8) {
-                        throw new DukeException("Please delete your task using this format: \"delete [serial number]\"\n");
-                    }
-                    int t = Integer.parseInt(input.substring(7));
-                    if (t > roo.data.size()) {
-                        throw new DukeException("We dunhave so many task lah =_=\n");
-                    }
-                    roo.delete(t - 1);
-                } catch (DukeException e) {
-                    System.err.println(e);
-                } finally {
-                    input = sc.nextLine();
-                }
+                c = Command.DELETE;
             } else if (input.startsWith("todo")) {
-                String tsk = input.substring(4);
-                try {
-                    roo.add(new Todo(tsk));
-                } catch (DukeException e) {
-                    System.err.println(e);
-                } finally {
-                    input = sc.nextLine();
-                }
+                c = Command.TODO;
             } else if (input.startsWith("deadline")) {
-                try {
-                    if (!input.contains("/by")) {
-                        throw new DukeException("Please enter your task with this format: \"deadline task_description /by deadline\"\n");
-                    }
-                    String tsk = input.substring(8, input.indexOf("/") - 1);
-                    String date = input.substring(input.indexOf("/by") + 3);
-                    roo.add(new Deadline(tsk, date));
-                } catch (DukeException e) {
-                    System.err.println(e);
-                } finally {
-                    input = sc.nextLine();
-                }
+                c = Command.DEADLINE;
             } else if (input.startsWith("event")) {
-                try {
-                    if (!input.contains("/from") || !input.contains("/to")) {
-                        throw new DukeException("Please enter your task with this format: \"event task_description /from start /to end\"\n");
-                    }
-                    String tsk = input.substring(5, input.indexOf("/from") - 1);
-                    String start = input.substring(input.indexOf("/from") + 5, input.indexOf("/to") - 1);
-                    String end = input.substring(input.indexOf("/to") + 3);
-                    roo.add(new Event(tsk, start, end));
-                } catch (DukeException e) {
-                    System.err.println(e);
-                } finally {
-                    input = sc.nextLine();
-                }
+                c = Command.EVENT;
             } else if (input.startsWith("end")) {
-                sc.close();
-                roo.bye();
-                break;
+                c = Command.END;
             } else {
-                try {
-                    throw new DukeException("I dunno what u mean!!!\n");
-                } catch (DukeException e) {
-                    System.err.println(e);
-                    input = sc.nextLine();
-                }
+                c = Command.UNKNOWN;
             }
+
+            switch (c) {
+                case LIST:
+                    roo.list();
+                    input = sc.nextLine();
+                    break;
+
+                case UNMARK:
+                    try {
+                        if (input.length() < 8) {
+                            throw new DukeException("Please unmark your task using this format: \"unmark [serial number]\"\n");
+                        }
+                        int t = Integer.parseInt(input.substring(7));
+                        if (t > roo.data.size()) {
+                            throw new DukeException("We dunhave so many task lah =_=\n");
+                        } else if (!roo.data.get(t - 1).done()) {
+                            throw new DukeException("Weihh... It's unmark ehhh\n");
+                        }
+                        roo.markUndone(t - 1);
+                    } catch (DukeException e) {
+                        System.err.println(e);
+                    } finally {
+                        input = sc.nextLine();
+                    }
+                    break;
+
+                case MARK:
+                    try {
+                        if (input.length() < 6) {
+                            throw new DukeException("Please mark your task using this format: \"mark [serial number]\"\n");
+                        }
+                        int t = Integer.parseInt(input.substring(5));
+                        if (t > roo.data.size()) {
+                            throw new DukeException("We dunhave so many task lah =_=\nq");
+                        } else if (roo.data.get(t - 1).done()) {
+                            throw new DukeException("Weihh... It's already mark ehhh\n");
+                        }
+                        roo.markDone(t - 1);
+                    } catch (DukeException e) {
+                        System.err.println(e);
+                    } finally {
+                        input = sc.nextLine();
+                    }
+                    break;
+
+                case DELETE:
+                    try {
+                        if (input.length() < 8) {
+                            throw new DukeException("Please delete your task using this format: \"delete [serial number]\"\n");
+                        }
+                        int t = Integer.parseInt(input.substring(7));
+                        if (t > roo.data.size()) {
+                            throw new DukeException("We dunhave so many task lah =_=\n");
+                        }
+                        roo.delete(t - 1);
+                    } catch (DukeException e) {
+                        System.err.println(e);
+                    } finally {
+                        input = sc.nextLine();
+                    }
+                    break;
+
+                case TODO:
+                    String tsk = input.substring(4);
+                    try {
+                        roo.add(new Todo(tsk));
+                    } catch (DukeException e) {
+                        System.err.println(e);
+                    } finally {
+                        input = sc.nextLine();
+                    }
+                    break;
+
+                case DEADLINE:
+                    try {
+                        if (!input.contains("/by")) {
+                            throw new DukeException("Please enter your task with this format: \"deadline task_description /by deadline\"\n");
+                        }
+                        String tk = input.substring(8, input.indexOf("/") - 1);
+                        String date = input.substring(input.indexOf("/by") + 3);
+                        roo.add(new Deadline(tk, date));
+                    } catch (DukeException e) {
+                        System.err.println(e);
+                    } finally {
+                        input = sc.nextLine();
+                    }
+                    break;
+
+                case EVENT:
+                    try {
+                        if (!input.contains("/from") || !input.contains("/to")) {
+                            throw new DukeException("Please enter your task with this format: \"event task_description /from start /to end\"\n");
+                        }
+                        String ts = input.substring(5, input.indexOf("/from") - 1);
+                        String start = input.substring(input.indexOf("/from") + 5, input.indexOf("/to") - 1);
+                        String end = input.substring(input.indexOf("/to") + 3);
+                        roo.add(new Event(ts, start, end));
+                    } catch (DukeException e) {
+                        System.err.println(e);
+                    } finally {
+                        input = sc.nextLine();
+                    }
+                    break;
+
+                case END:
+                    loop = false;
+                    sc.close();
+                    roo.bye();
+                    break;
+
+                case UNKNOWN:
+                    try {
+                        throw new DukeException("I dunno what u mean!!!\n");
+                    } catch (DukeException e) {
+                        System.err.println(e);
+                        input = sc.nextLine();
+                    }
+                    break;
+            }
+
         }
     }
 
@@ -162,6 +208,6 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
-        print(input, roo, sc);
+        roo.print(input, roo, sc);
     }
 }
