3eebf8900464b005463f0f71782c15f3ff7664cb
tllshan <shantsou03@gmail.com>
2023-08-24 21:49:23 +0800

    Level 5. Handle Errors
    Implemented error handling for faulty inputs.
    Changed Task from interface to abstract class.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,30 +1,13 @@
-public class Deadline implements Task {
-    private final String name;
+public class Deadline extends Task {
     private final String end;
-    private boolean isDone;
 
-    public Deadline(String input) {
-        int slash = input.indexOf('/');
-        this.name = input.substring("deadline ".length(), slash - 1);
-        this.end = input.substring(slash + " by ".length());
-        this.isDone = false;
+    public Deadline(String name, String end) {
+        super(name);
+        this.end = end;
     }
 
+    @Override
     public String getTask() {
-        return String.format("[%s][D] %s (by: %s)", checkDone(), name, end);
-    }
-
-    public void markDone() {
-        isDone = true;
-        System.out.println(ChatterChicken.INDENT_BIG + this.getTask());
-    }
-
-    public void unmarkDone() {
-        isDone = false;
-        System.out.println(ChatterChicken.INDENT_BIG + this.getTask());
-    }
-
-    private String checkDone() {
-        return isDone ? "X" : " ";
+        return String.format("[%s][D] %s (by: %s)", super.checkDone(), super.getName(), end);
     }
 }
