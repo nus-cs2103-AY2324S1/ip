51a3755e4f9b32a8214cd7abdab28d0077421ce3
Ting Yong Shen Amos <amostingyongquan@gmail.com>
2023-08-24 17:25:20 +0800

    Level 6. Delete. Added logic to handle delete command. Added error handling for delete command. Organised error handling to become more modular and added regex matching.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -10,6 +10,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + "to: " + to + ")";
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 }
--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Jarvis {
     private static ArrayList<Task> taskList = new ArrayList<>(); // list of Tasks
@@ -24,6 +26,7 @@ public class Jarvis {
     private static final String markInforming = "Roger that Sir, I've marked this task as done:";
     private static final String uncheckInforming = "Noted Sir, I've marked this task as NOT done yet:";
     private static final String taskInforming = "As you please Sir, I've added the task:";
+    private static final String deleteInforming = "Alright Sir, I've removed this task";
 
     private static void printGreeting() {
         System.out.println(line);
@@ -71,34 +74,16 @@ public class Jarvis {
         System.out.println(line);
     }
 
-    private static void printError(String inputCommand) {
-        // check if command is one of the valid keywords
-        boolean isValidCommand = false;
-        String validInputCommand = "";
-        for (String validCommand: validCommands) {
-            if (inputCommand.contains(validCommand)) {
-                isValidCommand = true;
-                validInputCommand = validCommand;
-                break;
-            }
-        }
+    private static void printDelete(Task currentTask) {
+        System.out.println(line);
+        System.out.println(deleteInforming);
+        System.out.println(currentTask.toString());
+        System.out.println("You have now " + taskList.size() + " tasks in the list Sir.");
+        System.out.println(line);
+    }
 
-        if (!isValidCommand) {
-            try {
-                throw new IncorrectJarvisCommandException("Apologies Sir, the command " + inputCommand + " is not a valid command.");
-            } catch (IncorrectJarvisCommandException e) {
-                System.out.println(line);
-                System.out.println(e.getMessage());
-                System.out.println("    This is the list of valid commands:");
-                for (int i = 0; i < validCommands.size(); i++) {
-                    int numbering = i + 1;
-                    System.out.println("    " + numbering + ". " + validCommands.get(i));
-                }
-                System.out.println(line);
-                return;
-            }
-        }
-
+    // identifies which command has wrong formatting and prints feedback to user
+    private static void printIncorrectFormat(String inputCommand, String validInputCommand) {
         // since command is valid, check if formatting of the command is correct
         boolean markMatch = inputCommand.matches("mark \\d+");
         boolean uncheckMatch = inputCommand.matches("unmark \\d+");
@@ -117,7 +102,7 @@ public class Jarvis {
                 System.out.println(line);
                 return;
             }
-        } else if (validInputCommand.equals("unmark") && !uncheckMatch) {
+        } else if (validInputCommand.equals("uncheck") && !uncheckMatch) {
             try {
                 throw new WrongJarvisCommandFormatException("Apologies Sir, the format of the uncheck command you provided is incorrect.");
             } catch (WrongJarvisCommandFormatException e) {
@@ -165,6 +150,54 @@ public class Jarvis {
 
     }
 
+    // checks if command is valid and throws IncorrectJarvisCommandException
+    private static String isValidCommand(String inputCommand) {
+        // check if command is one of the valid keywords
+        boolean isValidCommand = false;
+        String validInputCommand = "";
+
+        for (String validCommand: validCommands) {
+            if (inputCommand.contains(validCommand)) {
+                isValidCommand = true;
+                validInputCommand = validCommand;
+                break;
+            }
+        }
+
+        if (!isValidCommand) {
+            try {
+                throw new IncorrectJarvisCommandException("Apologies Sir, the command " + inputCommand + " is not a valid command.");
+            } catch (IncorrectJarvisCommandException e) {
+                System.out.println(line);
+                System.out.println(e.getMessage());
+                System.out.println("    This is the list of valid commands:");
+                for (int i = 0; i < validCommands.size(); i++) {
+                    int numbering = i + 1;
+                    System.out.println("    " + numbering + ". " + validCommands.get(i));
+                }
+                System.out.println(line);
+            }
+        }
+        return validInputCommand;
+    }
+
+    // function check if task number is valid and throws InvalidTaskNumberException
+    private static boolean isValidTaskNumber(int taskNum) {
+        boolean isValid = true;
+        if (taskNum < 0 || taskNum > taskList.size()) { // check if task number is of valid range
+            try {
+                throw new InvalidTaskNumberException("Apologies Sir, the task number you provided is out of range.");
+            } catch (InvalidTaskNumberException e) {
+                System.out.println(line);
+                System.out.println(e.getMessage());
+                System.out.println("    There are currently " + taskList.size() + " tasks in the list.");
+                System.out.println(line);
+                isValid = false;
+            }
+        }
+        return isValid;
+    }
+
     public static void main(String[] args) {
 
         addValidCommands(); // adding the list of valid commands to the validCommands arraylist
@@ -173,68 +206,88 @@ public class Jarvis {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String command = scanner.nextLine().trim();
+            String nameOfValidCommand = isValidCommand(command);
+            if (nameOfValidCommand.equals("")) {continue;}
 
-            if (command.equals("list")) { // if "list" is entered
+            // if "list" is entered
+            if (command.matches("list")) {
                 printList();
+                continue;
+            }
 
-            } else if (command.equals("bye")) { // if "bye" is entered
+            // if "bye" is entered
+            if (command.matches("bye")) {
                 printBye();
                 break;
+            }
 
-            } else if (command.matches("mark \\d+") ||
-                    command.matches("uncheck \\d+")) { // if "mark" or "uncheck" is entered
+            // if "mark" or "uncheck" is entered
+            if (command.matches("mark \\d+") ||
+                    command.matches("uncheck \\d+")) {
+
                 int taskNum = Integer.parseInt(command.substring(command.length() - 1));
-                if (taskNum < 0 || taskNum > taskList.size()) { // check if task number is of valid range
-                    try {
-                        throw new InvalidTaskNumberException("Apologies Sir, the task number you provided is out of range.");
-                    } catch (InvalidTaskNumberException e) {
-                        System.out.println(line);
-                        System.out.println(e.getMessage());
-                        System.out.println("    There are currently " + taskList.size() + " tasks in the list.");
-                        System.out.println(line);
-                        continue;
-                    }
-                }
+                if (!isValidTaskNumber(taskNum)) {continue;}
+
+                Task currentTask = taskList.get(taskNum - 1);
                 if (command.matches("uncheck \\d+")) { // if "uncheck" is entered
-                    Task currentTask = taskList.get(taskNum - 1);
                     currentTask.setDone(false);
                     printUncheck(currentTask);
+                    continue;
                 } else { // if "mark" is entered
-                    Task currentTask = taskList.get(taskNum - 1);
                     currentTask.setDone(true);
                     printMark(currentTask);
+                    continue;
                 }
 
-            } else if (command.matches("todo .+") ||
-                    command.matches("deadline .+ /.+") ||
-                    command.matches("event .+ /.+ /.+")) { // if task command is entered
-                if (command.matches("todo .+")) { // if "todo" is entered
-                    String taskDescription = command.substring(5);
+            }
+
+            // if task command is entered
+            Pattern todoPattern = Pattern.compile("(todo) (.+)");
+            Pattern deadlinePattern = Pattern.compile("(deadline) (.+) /by (.+)");
+            Pattern eventPattern = Pattern.compile("(event) (.+) /from (.+) /to (.+)");
+            Matcher todoMatcher = todoPattern.matcher(command);
+            Matcher deadlineMatcher = deadlinePattern.matcher(command);
+            Matcher eventMatcher = eventPattern.matcher(command);
+            if (todoMatcher.matches() || deadlineMatcher.matches() || eventMatcher.matches()) {
+                if (todoMatcher.matches()) { // if "todo" is entered
+                    String taskDescription = todoMatcher.group(2);
                     ToDo todo = new ToDo(taskDescription);
                     taskList.add(todo);
                     printTask(todo);
-                } else if (command.matches("deadline .+ /.+")) { // if "deadline" is entered
-                    String tempString = command.substring(9);
-                    String[] parts = tempString.split("/");
-                    String taskDescription = parts[0];
-                    String by = parts[1].substring(3);
+                    continue;
+                } else if (deadlineMatcher.matches()) { // if "deadline" is entered
+                    String taskDescription = deadlineMatcher.group(2);
+                    String by = deadlineMatcher.group(3);
                     Deadline deadline = new Deadline(taskDescription, by);
                     taskList.add(deadline);
                     printTask(deadline);
+                    continue;
                 } else { // if "event" is entered
-                    String tempString = command.substring(6);
-                    String[] parts = tempString.split("/");
-                    String taskDescription = parts[0];
-                    String from = parts[1].substring(5);
-                    String to = parts[2].substring(3);
+                    String taskDescription = eventMatcher.group(2);
+                    String from = eventMatcher.group(3);
+                    String to = eventMatcher.group(4);
                     Event event = new Event(taskDescription, from, to);
                     taskList.add(event);
                     printTask(event);
+                    continue;
                 }
-
-            } else { // if command given is invalid
-                printError(command);
             }
+
+            // if delete is entered
+            Pattern deletePattern = Pattern.compile("(delete) (\\d+)");
+            Matcher deleteMatcher = deletePattern.matcher(command);
+            if (deleteMatcher.matches()) {
+                int taskNum = Integer.parseInt(deleteMatcher.group(2));
+                if (isValidTaskNumber(taskNum)) {
+                    Task deletedTask = taskList.get(taskNum - 1);
+                    taskList.remove(taskNum - 1);
+                    printDelete(deletedTask);
+                }
+                continue;
+            }
+
+            // if none of the above commands go through
+            printIncorrectFormat(command, nameOfValidCommand);
         }
         scanner.close(); // closing the user input scanner
     }
