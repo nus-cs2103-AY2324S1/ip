f479ba922a00f207e1dba22309bc935a50174f3b
Catherine Liang <catherineliang1101@gmail.com>
2023-08-24 14:17:40 +0800

    Level-3 changes: Added Task class, userList now stores Task instances, user can also mark and unmark tasks.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,52 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 public class Duke {
+    /** Divider constant */
     private static final String DIVIDER = "      ____________________________________________________________";
+
+    /**
+     * Method that marks a given task in a list
+     * @param list List containing user inputs.
+     * @param index Index of the given task.
+     */
+    public static void markAsDone(List<Task> list, int index) {
+        if (index >= 0 && index <= list.size()) {
+            Task task = list.get(index);
+            task.markAsDone();
+
+            System.out.println(DIVIDER);
+            System.out.println("        Nice! I've marked this task as done:");
+            System.out.println("            " + task);
+            System.out.println(DIVIDER);
+
+        } else {
+            System.out.println(DIVIDER);
+            System.out.println("        Oops! Task not found!");
+            System.out.println(DIVIDER);
+        }
+    }
+
+    /**
+     * Method that unmark a given task in a list
+     * @param list List containing user inputs.
+     * @param index Index of the given task.
+     */
+    public static void markAsUndone(List<Task> list, int index) {
+        if (index >= 0 && index <= list.size()) {
+            Task task = list.get(index);
+            task.markAsUndone();
+
+            System.out.println(DIVIDER);
+            System.out.println("         OK! I've marked this task as not done yet:");
+            System.out.println("            " + task);
+            System.out.println(DIVIDER);
+
+        } else {
+            System.out.println(DIVIDER);
+            System.out.println("        Oops! Task not found!");
+            System.out.println(DIVIDER);
+        }
+    }
 
     public static void main(String[] args) {
         // Opening lines
@@ -13,7 +58,7 @@ public class Duke {
 
         // Check user input
         Scanner sc = new Scanner(System.in);
-        List<String> userList = new ArrayList<>();
+        List<Task> userList = new ArrayList<>();
 
         while (true) {
             String userInput = sc.nextLine();
@@ -35,9 +80,48 @@ public class Duke {
                 }
                 System.out.println(DIVIDER);
 
+            } else if (userInput.startsWith("mark")) {
+                // If user wants to mark something
+                String[] parts = userInput.split(" ");
+                if (parts.length == 2) {
+                    try {
+                        int index = Integer.parseInt(parts[1]) - 1;
+                        markAsDone(userList, index);
+                    } catch (NumberFormatException e) {
+                        System.out.println(DIVIDER);
+                        System.out.println("        Invalid task number!");
+                        System.out.println(DIVIDER);
+                    }
+                } else {
+                    // Invalid command format
+                    System.out.println(DIVIDER);
+                    System.out.println("        Oops! Invalid command format!");
+                    System.out.println(DIVIDER);
+                }
+
+            } else if (userInput.startsWith("unmark")) {
+                // If user wants to unmark something
+                String[] parts = userInput.split(" ");
+                if (parts.length == 2) {
+                    try {
+                        int index = Integer.parseInt(parts[1]) - 1;
+                        markAsUndone(userList, index);
+                    } catch (NumberFormatException e) {
+                        System.out.println(DIVIDER);
+                        System.out.println("        Invalid task number!");
+                        System.out.println(DIVIDER);
+                    }
+                } else {
+                    // Invalid command format
+                    System.out.println(DIVIDER);
+                    System.out.println("        Oops! Invalid command format!");
+                    System.out.println(DIVIDER);
+                }
+
             } else {
                 // Other inputs
-                userList.add(userInput);
+                Task task = new Task(userInput);
+                userList.add(task);
 
                 System.out.println(DIVIDER);
                 System.out.println("        Added: " + userInput);
---
+++ src/main/java/Task.java
@@ -0,0 +1,50 @@
+/**
+ * A class for Tasks.
+ */
+public class Task {
+    /** Class field description that describes the task */
+    protected String description;
+    /** Class field isDone that tells whether the task is marked */
+    protected boolean isDone;
+
+    /**
+     * Constructor to initialize the Task class
+     * @param description Describes the task.
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Method to get the status icon of the task.
+     * @return String "X" if task's isDone is true, " " otherwise
+     */
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    /**
+     * Method that sets the task's isDone to true
+     */
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    /**
+     * Method that sets the task's isDone to false
+     */
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+    /**
+     * Method that overrides default toString.
+     * @return String representation of Task.
+     */
+    @Override
+    public String toString() {
+
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+}
