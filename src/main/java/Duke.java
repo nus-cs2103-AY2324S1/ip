5d7e5dc25b52e91c0982c8c024a28861177eb071
rayyan35p <rayyanis0908@gmail.com>
2023-08-24 22:09:19 +0800

    Modify main code to catch exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,47 +11,62 @@ public class Duke {
         String input = scanner.nextLine();
 
         while(true) {
-            if (input.equals("bye")) {
-                break;
-            }
-            else if (input.equals("list")) {
-                list.printList();
-            }
-            else if (input.startsWith("mark") || input.startsWith("unmark")) {
-                String[] inputSplit = input.split(" ");
-                if (inputSplit.length == 1) {
-                    System.out.println("Input the task number!");
-                } else if (inputSplit.length > 2) {
-                    System.out.println("Invalid command");
-                } else {
-                    int taskNo = Integer.parseInt(inputSplit[1]) - 1;
-                    if (input.startsWith("mark")) {
-                        list.setTaskComplete(taskNo);
+            try {
+                if (input.equals("bye")) {
+                    break;
+                } else if (input.equals("list")) {
+                    list.printList();
+                } else if (input.startsWith("mark") || input.startsWith("unmark")) {
+                    String[] inputSplit = input.split(" ");
+                    if (inputSplit.length == 1) {
+                        throw new DukeException("Input the task number");
+                    } else if (inputSplit.length > 2) {
+                        throw new DukeException("Invalid command");
                     } else {
-                        list.setTaskIncomplete(taskNo);
+                        int taskNo = Integer.parseInt(inputSplit[1]) - 1;
+                        if (input.startsWith("mark")) {
+                            list.setTaskComplete(taskNo);
+                        } else {
+                            list.setTaskIncomplete(taskNo);
+                        }
+                    }
+                } else {
+                    if (input.startsWith("todo")) {
+                        if (input.length() == 4 || input.equals("todo ")) {
+                            throw new DukeException("Description of a todo cannot be empty");
+                        }
+                        input = input.substring(5);
+                        list.addTask(new ToDoTask(input));
+                    } else if (input.startsWith("deadline")) {
+                        if (input.length() == 8 || input.equals("deadline ")) {
+                            throw new DukeException("Description of a deadline cannot be empty");
+                        }
+                        input = input.substring(9);
+                        String[] inputArr;
+                        inputArr = input.split(" /by ");
+                        list.addTask(new DeadlineTask(inputArr[0], inputArr[1]));
+                    } else if (input.startsWith("event")) {
+                        if (input.length() == 5 || input.equals("event ")) {
+                            throw new DukeException("Description of an event cannot be empty");
+                        }
+                        input = input.substring(6);
+                        String[] inputArr;
+                        inputArr = input.split(" /from ");
+                        String description = inputArr[0];
+                        inputArr = inputArr[1].split(" /to ");
+                        list.addTask(new EventTask(description, inputArr[0], inputArr[1]));
+                    } else {
+                        throw new DukeException("I don't know what that means");
                     }
                 }
-            } else {
-                if (input.startsWith("todo")) {
-                    input = input.substring(5);
-                    list.addTask(new ToDoTask(input));
-                } else if (input.startsWith("deadline")) {
-                    input = input.substring(9);
-                    String[] inputArr;
-                    inputArr = input.split(" /by ");
-                    list.addTask(new DeadlineTask(inputArr[0], inputArr[1]));
-                } else if (input.startsWith("event")) {
-                    input = input.substring(6);
-                    String[] inputArr;
-                    inputArr = input.split(" /from ");
-                    String description = inputArr[0];
-                    inputArr = inputArr[1].split(" /to ");
-                    list.addTask(new EventTask(description, inputArr[0], inputArr[1]));
-                } else {
-                    list.addTask(new Task(input));
-                }
+                input = scanner.nextLine();
+            } catch (DukeException e) {
+                System.out.println(e);
+                input = scanner.nextLine();
+            } catch (NumberFormatException e) {
+                System.out.println("Please key in a valid index");
+                input = scanner.nextLine();
             }
-            input = scanner.nextLine();
         }
 
         System.out.println("Bye. Hope to see you again soon!");
