70d92cd6d5a1dbdfe9118202378d07b015f56d52
KT <tkt2001@hotmail.com>
2023-08-24 16:41:02 +0800

    l1 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -5,25 +6,41 @@ public class Duke {
     private static final String greeting = "Hello I'm " + name + "\nWhat can I do for you?";
     private static final String end = "Bye bye";
 
-    public static void main(String[] args) {
-        System.out.println(greeting);
-        Scanner s = new Scanner(System.in);
-        while (s.hasNextLine()) {
-            String t = s.nextLine();
-            if (t.equals("bye")) {
-                exit();
-                return;
-            } else {
-                echo(t);
-            }
-        }
+    private ArrayList<String> todolist;
+
+    private Duke() {
+        this.todolist = new ArrayList<>();
+        greet();
     }
 
+    private static void greet() {
+        System.out.println(greeting);
+    }
     private static void echo(String s) {
         System.out.println(s);
     }
 
     private static void exit() {
         System.out.println(end);
+    }
+
+    private static boolean respond(String s) {
+        if (s.equals("bye")) {
+            exit();
+            return false;
+        } else {
+            echo(s);
+            return true;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Duke d = new Duke();
+        while (s.hasNextLine()) {
+            String t = s.nextLine();
+            if (respond(t)) {
+                break;
+            }
+        }
     }
 }
