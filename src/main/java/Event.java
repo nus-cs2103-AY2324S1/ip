a590e8fc873d2b02c60fbf2166edab151d2eb6cc
EricXiong420 <E0958551@u.nus.edu>
2023-08-25 03:27:34 +0800

    Adds three types of tasks which each have their representation

---
+++ src/main/java/Event.java
@@ -0,0 +1,14 @@
+public class Event extends Task {
+    private String startTime;
+    private String endTime;
+    public Event(String taskName, String startTime, String endTime) {
+        super(taskName);
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[E]%s (from: %s to: %s)", super.toString(), startTime, endTime);
+    }
+}
--- src/main/java/Hachi.java
+++ src/main/java/Hachi.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Hachi {
@@ -11,41 +12,97 @@ public class Hachi {
         line();
         Scanner sc = new Scanner(System.in);
         while (true) {
-            String command = sc.nextLine();
-            String[] words = command.split(" ");
+            String input = sc.nextLine();
+            String[] words = input.split(" ");
+            String command = words[0];
+            String[] arguments = Arrays.copyOfRange(words, 1, words.length);
+
             line();
-            if (command.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            } else if (command.equals("list")) {
-                for (int i = 0; i < currIndex; i++) {
-                    int num = i + 1;
-                    System.out.println(num + ". " + tasks[i]);
+            if (words.length == 1) {
+                if (command.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                } else if (command.equals("list")) {
+                    for (int i = 0; i < currIndex; i++) {
+                        int num = i + 1;
+                        System.out.println(num + ". " + tasks[i]);
+                    }
+                } else {
+                    System.out.println("Invalid command " + command);
                 }
-            } else if (words[0].equals("mark")) {
-                try {
-                    int number = Integer.parseInt(words[1]);
-                    int i = number - 1;
-                    tasks[i].mark();
-                    System.out.println("Nice! I've marked this task as done");
-                    System.out.println("   " + tasks[i]);
-                } catch (NumberFormatException e) {
-                    System.out.println("Invalid argument for command \"mark\"");
+            } else {
+                if (command.equals("mark")) {
+                    try {
+                        int number = Integer.parseInt(words[1]);
+                        int i = number - 1;
+                        tasks[i].mark();
+                        System.out.println("Nice! I've marked this task as done");
+                        System.out.println("   " + tasks[i]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid argument for command \"mark\"");
+                    }
+                } else if (command.equals("unmark")) {
+                    try {
+                        int number = Integer.parseInt(words[1]);
+                        int i = number - 1;
+                        tasks[i].unmark();
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println("   " + tasks[i]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid argument for command \"unmark\"");
+                    }
+                } else if (command.equals("todo")) {
+                    String todoTask = String.join(" ", arguments);
+                    System.out.println("Got it. I've added this task:");
+                    tasks[currIndex] = new Todo(todoTask);
+                    System.out.println("   " + tasks[currIndex]);
+                    currIndex++;
+                    System.out.println(String.format("Now you have %d tasks in the list.", currIndex));
+                } else if (command.equals("deadline")) {
+                    System.out.println("Got it. I've added this task:");
+                    int byIndex = -1;
+                    for (int i = 0; i < arguments.length; i++) {
+                        if (arguments[i].equals("/by")) {
+                            byIndex = i;
+                            break;
+                        }
+                    }
+                    if (byIndex != -1) {
+                        String deadlineTask = String.join(" ",
+                                Arrays.copyOfRange(arguments, 0, byIndex));
+                        String deadlineDate = String.join(" ",
+                                Arrays.copyOfRange(arguments, byIndex + 1, arguments.length));
+                        tasks[currIndex] = new Deadline(deadlineTask, deadlineDate);
+                        System.out.println("   " + tasks[currIndex]);
+                        currIndex++;
+                        System.out.println(String.format("Now you have %d tasks in the list.", currIndex));
+                    }
+                } else if (command.equals("event")) {
+                    System.out.println("Got it. I've added this task:");
+                    int fromIndex = -1;
+                    int toIndex = -1;
+                    for (int i = 0; i < arguments.length; i++) {
+                        if (arguments[i].equals("/from")) {
+                            fromIndex = i;
+                        } else if (arguments[i].equals("/to")) {
+                            toIndex = i;
+                        }
+                    }
+                    if (fromIndex != -1 && toIndex != -1) {
+                        String eventTask = String.join(" ",
+                                Arrays.copyOfRange(arguments, 0, fromIndex));
+                        String eventStartDate = String.join(" ",
+                                Arrays.copyOfRange(arguments, fromIndex + 1, toIndex));
+                        String eventEndDate = String.join(" ",
+                                Arrays.copyOfRange(arguments, toIndex + 1, arguments.length));
+                        tasks[currIndex] = new Event(eventTask, eventStartDate, eventEndDate);
+                        System.out.println("   " + tasks[currIndex]);
+                        currIndex++;
+                        System.out.println(String.format("Now you have %d tasks in the list.", currIndex));
+                    }
+                } else {
+                    System.out.println("Invalid command " + words[0]);
                 }
-            } else if (words[0].equals("unmark")) {
-                try {
-                    int number = Integer.parseInt(words[1]);
-                    int i = number - 1;
-                    tasks[i].unmark();
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println("   " + tasks[i]);
-                } catch (NumberFormatException e) {
-                    System.out.println("Invalid argument for command \"unmark\"");
-                }
-            }
-            else {
-                System.out.println("added: " + command);
-                tasks[currIndex++] = new Task(command);
             }
             line();
         }
@@ -57,4 +114,5 @@ public class Hachi {
     public static void line() {
         System.out.println("____________________________________________________________");
     }
+
 }
