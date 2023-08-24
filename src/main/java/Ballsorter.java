81e18e5ef34d5748414aef90404a0e4769825e36
fuyiqiao <yiqiao_fu@hotmail.com>
2023-08-24 12:21:55 +0800

    Level 2

--- src/main/java/Ballsorter.java
+++ src/main/java/Ballsorter.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Ballsorter {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
+        String[] tasks = new String[100];
+        int numberOfTasks = 0;
 
         System.out.println(line);
         System.out.println("Hello! I'm Ballsorter\nWhat can I do for you?");
@@ -13,9 +15,19 @@ public class Ballsorter {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < numberOfTasks; i++) {
+                    int temp = i + 1;
+                    System.out.println(temp + ". " + tasks[i]);
+                }
+                System.out.println(line);
+            } else {
+                tasks[numberOfTasks] = input;
+                numberOfTasks++;
+
+                System.out.println("added: " + input);
+                System.out.println(line);
             }
-            System.out.println(input);
-            System.out.println(line);
         }
 
         System.out.println("Bye. Hope to see you again soon!");
