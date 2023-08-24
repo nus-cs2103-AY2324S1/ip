754e6fa38ca6732130d10c53a348bd4d2348cd00
wasjoe1 <joechua247@gmail.com>
2023-08-24 15:49:19 +0800

    Level-0 commit by joe : 1st ever commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in); // makes it such that the scanner takes in inputs from the console
+
+        String openingStr =
+                "____________________________________________________________\n" +
+                        " Hello! I'm JEOE\n" +
+                        " What can I do for you?\n" +
+                        "____________________________________________________________\n";
+        System.out.println(openingStr);
+
+        String closingStr =
+                "____________________________________________________________\n" +
+                        " Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________";
+        System.out.println(closingStr);
     }
 }
