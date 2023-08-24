d1bdd434393f3fa6e84a6bdddf7985f9433d5e5b
qyaner <>
2023-08-24 17:17:04 +0800

    separated classes into different files and added some comments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,9 +8,7 @@ public class Duke {
         System.out.println("What can I do for you?");
 
         Scanner scanner = new Scanner(System.in);
-        //Task[] list = new Task[100];
         ArrayList<Task> list = new ArrayList<>();
-        //int count = 0;
 
         while (true) {
 
@@ -22,34 +20,48 @@ public class Duke {
                 } catch (DukeException e) {
                     System.out.println(e.getMessage());
                 }
+
+            // exits program
             } else if (command.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+
+            // delete task
             } else if (command.toLowerCase().startsWith("delete")) {
                 int number = Integer.parseInt(command.split(" ")[1]) - 1;
                 Task removedTask = list.remove(number);
+
+                // print
                 System.out.println("Noted. I've removed this task:");
                 System.out.println(removedTask.toString());
                 System.out.println("Now you have " + list.size() + " tasks in the list.");
 
+            // list tasks
             } else if (command.equalsIgnoreCase("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < list.size(); i++) {
                     System.out.println((i + 1) + ". " + list.get(i).toString());
                 }
 
+            // mark task as done
             } else if (command.toLowerCase().startsWith("mark")) {
                 int number = Integer.parseInt(command.split(" ")[1]) - 1;
                 list.get(number).markAsDone();
+
+                // print
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println(list.get(number).toString());
 
+            // unmark task
             } else if (command.toLowerCase().startsWith("unmark")) {
                 int number = Integer.parseInt(command.split(" ")[1]) - 1;
                 list.get(number).markAsNotDone();
+
+                // print
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println(list.get(number).toString());
 
+            // add todo
             } else if (command.toLowerCase().startsWith("todo")) {
                 String todo = command.substring(4).trim();
                 if (todo.isEmpty()) {
@@ -61,11 +73,14 @@ public class Duke {
                 } else {
                     Todo newTodo = new Todo(todo);
                     list.add(newTodo);
+
+                    //print
                     System.out.println("Got it. I've added this task:");
                     System.out.println(newTodo.toString());
                     System.out.println("Now you have " + list.size() + " tasks in the list.");
                 }
 
+            // add deadline
             } else if (command.toLowerCase().startsWith("deadline")) {
                 String deadline = command.substring(8).trim();
 
@@ -76,7 +91,7 @@ public class Duke {
                         System.out.println(e.getMessage());
                     }
                 } else {
-
+                    // separate the task and its deadline
                     String[] sub = deadline.split("/by");
 
                     String description = sub[0].trim();
@@ -84,11 +99,14 @@ public class Duke {
 
                     Deadline newDeadline = new Deadline(description, by);
                     list.add(newDeadline);
+
+                    // print
                     System.out.println("Got it. I've added this task:");
                     System.out.println(newDeadline.toString());
                     System.out.println("Now you have " + list.size() + " tasks in the list.");
                 }
 
+            // add event
             } else if (command.toLowerCase().startsWith("event")) {
                 String event = command.substring(5).trim();
 
@@ -99,123 +117,32 @@ public class Duke {
                         System.out.println(e.getMessage());
                     }
                 } else {
-
+                    // separate event and timing
                     String[] sub = event.split("/from");
-
                     String description = sub[0].trim();
                     String timing = sub[1].trim();
 
+                    // separate start time and end time
                     String[] fromTo = timing.split("/to");
                     String from = fromTo[0].trim();
                     String to = fromTo[1].trim();
 
                     Event newEvent = new Event(description, from, to);
                     list.add(newEvent);
+
+                    // print
                     System.out.println("Got it. I've added this task:");
                     System.out.println(newEvent.toString());
                     System.out.println("Now you have " + list.size() + " tasks in the list.");
                 }
+
             } else {
-//                Task task = new Task(command);
-//                list[count] = task;
-//                count++;
-//                System.out.println("Got it. I've added this task:");
-//                System.out.println(task.toString());
-//                System.out.println("Now you have " + count + " tasks in the list.");
                 try {
                     throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 } catch (DukeException e) {
                     System.out.println(e.getMessage());
                 }
             }
-
         }
     }
-
-    public static class Task {
-        protected String description;
-        protected boolean isDone;
-
-        public Task(String description) {
-            this.description = description;
-            this.isDone = false;
-        }
-
-        public String getStatusIcon() {
-            return (isDone ? "[X]" : "[ ]"); // mark done task with X
-        }
-
-        public String getDescription() {
-            return this.description;
-        }
-
-        public void markAsDone() {
-            this.isDone = true;
-        }
-
-        public void markAsNotDone() {
-            this.isDone = false;
-        }
-
-        public String toString() {
-            return this.getStatusIcon() + " " + this.description;
-        }
-
-
-    }
-
-    public static class Deadline extends Task {
-
-        protected String by;
-
-        public Deadline(String description, String by) {
-            super(description);
-            this.by = by;
-        }
-
-        @Override
-        public String toString() {
-            return "[D]" + super.toString() + " (by: " + by + ")";
-        }
-    }
-
-    public static class Todo extends Task {
-
-        protected String by;
-
-        public Todo(String description) {
-            super(description);
-        }
-
-        @Override
-        public String toString() {
-            return "[T]" + super.toString();
-        }
-    }
-
-    public static class Event extends Task {
-
-        protected String from;
-        protected String to;
-
-        public Event(String description, String from, String to) {
-            super(description);
-            this.from = from;
-            this.to = to;
-        }
-
-        @Override
-        public String toString() {
-            return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
-        }
-    }
-
-    public static class DukeException extends Exception {
-        public DukeException(String message) {
-            super(message);
-        }
-    }
-
-
-
 }
