20d0fa2f28826d5babf209c95ea3e24d1b5fea99
DESKTOP-N8BNBE3\Juanpa <juanpaabola+coding@gmail.com>
2023-08-24 11:58:57 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -11,7 +15,20 @@ public class Duke {
         System.out.println("Hey! I'm " + name + "!");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye!");
-        System.out.println(line);
+
+        boolean running = true;
+        while (running) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                System.out.println(line);
+                System.out.println("Bye!");
+                System.out.println(line);
+                running = false;
+            } else {
+                System.out.println(line);
+                System.out.println(command);
+                System.out.println(line);
+            }
+        }
     }
 }
