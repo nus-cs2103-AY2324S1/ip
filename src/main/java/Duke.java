f368cd6db6f88dc65ef165f81de1fef16fc77897
lynnlow175 <lowhanlynn@Lows-MacBook-Air.local>
2023-08-24 17:33:55 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,17 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm lippy the wombat\n" + "What can I do for you?\n"
-        +  "Bye. Hope to see you again soon!");
+        System.out.println("Hello! I'm lippy the wombat\n" + "What can I do for you?\n");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (Objects.equals(input, "bye")) {
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
