12a042e973582c8215737fd7040dd366a4732101
p-xp <panxinping10@gmail.com>
2023-08-24 23:28:30 +0800

    feat: implement Level-3

--- src/main/java/Remy.java
+++ src/main/java/Remy.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+
 public class Remy {
 
     public static void main(String[] args) {
         String welcomeMessage = "____________________________________________________________\n" +
-                " Hello! I'm Remy.\n" +
-                " What can I do for you?\n" +
+                "I'm Remy, and it is NOT nice to see you.\n" +
+                "Faster tell me what you want and go away.\n" +
                 "____________________________________________________________\n";
-        String exitMessage = " Bye. Hope to see you again soon!\n" +
-        "____________________________________________________________\n";
+        String exitMessage = "Hope to never see you again!\n" +
+                "____________________________________________________________\n";
 
-        ArrayList<String> taskList = new ArrayList(100);
+        ArrayList<Task> taskList = new ArrayList(100);
+
         System.out.println(welcomeMessage);
         Scanner scanner = new Scanner(System.in);
         String input;
@@ -26,8 +28,37 @@ public class Remy {
                     System.out.println(" " + (i + 1) + ". " + taskList.get(i));
                 }
                 System.out.println("__________");
+            } else if (input.toLowerCase().startsWith("mark ")) {
+                // Marks item as done
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                if (index >= 0 && index < taskList.size()) {
+                    taskList.get(index).markAsDone();
+                    System.out.println("__________");
+                    System.out.println("Done. You happy?");
+                    System.out.println(taskList.get(index).toString());
+                    System.out.println("__________");
+                } else {
+                    System.out.println("__________");
+                    System.out.println("No such item lah.");
+                    System.out.println("__________");
+                }
+            } else if (input.toLowerCase().startsWith("unmark ")) {
+                // Marks item as undone
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                if (index >= 0 && index < taskList.size()) {
+                    taskList.get(index).markAsUndone();
+                    System.out.println("__________");
+                    System.out.println("Done. You happy?");
+                    System.out.println(taskList.get(index).toString());
+                    System.out.println("__________");
+                } else {
+                    System.out.println("__________");
+                    System.out.println("No such item lah.");
+                    System.out.println("__________");
+                }
             } else {
-                taskList.add(input);
+                Task temp = new Task(input);
+                taskList.add(temp);
                 System.out.println("__________");
                 System.out.println(" added: " + input);
                 System.out.println("__________");
