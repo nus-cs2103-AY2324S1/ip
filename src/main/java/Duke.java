b97016054aea72048996b27adeff5460498ff16c
peiran <she_peiran@u.nus.edu>
2023-08-25 00:32:17 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n" +
+        Scanner io = new Scanner(System.in);
+        System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Husky\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
-        System.out.println(logo);
+                "____________________________________________________________\n");
+        while (true) {
+            String echo = io.nextLine();
+            if (echo.equals("bye")) {
+                System.out.println("____________________________________________________________\n" +
+                        " Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________\n");
+                break;
+            } else {
+                System.out.println("____________________________________________________________\n" + echo + "\n" +
+                        "____________________________________________________________");
+            }
+        }
     }
 }
+
