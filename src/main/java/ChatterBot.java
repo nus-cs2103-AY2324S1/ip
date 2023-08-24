339265e4c1b14a595e8ece7f6bad7b565c25340e
hyc17003 <e0970589@u.nus.edu>
2023-08-24 16:50:37 +0800

    Add, List ChatterBot

--- src/main/java/ChatterBot.java
+++ src/main/java/ChatterBot.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ChatterBot {
@@ -6,6 +7,8 @@ public class ChatterBot {
         String logo = "ChatterBot";
         System.out.println("Hello! I'm " + logo + "\nWhat can I do for you?");
 
+        ArrayList<String> list = new ArrayList<>();
+
         while (true) {
             Scanner scanner = new Scanner(System.in);
             String userMessage = scanner.nextLine();
@@ -13,8 +16,13 @@ public class ChatterBot {
             if (userMessage.toLowerCase().equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (userMessage.toLowerCase().equals("list")) {
+                for (String str : list) {
+                    System.out.println(list.indexOf(str) + 1 + ". " + str);
+                }
             } else {
-                System.out.println(userMessage);
+                list.add(userMessage);
+                System.out.println("added " + userMessage);
             }
         }
     }
