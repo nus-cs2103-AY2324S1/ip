1a49bc6704d98d8e1c196c6de5d852ced8ec2028
Catherine Liang <catherineliang1101@gmail.com>
2023-08-24 13:18:20 +0800

    Level-0 changes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
+    private static final String DIVIDER = "      ____________________________________________________________";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Opening lines
+        System.out.println(DIVIDER);
+        System.out.println("        Hello! I'm Valerie!");
+        System.out.println("        What can I do for you?");
+        System.out.println(DIVIDER);
+
+        // If user wants to quit
+        System.out.println("        Bye ~ Hope to see you again soon ~");
+        System.out.println(DIVIDER);
     }
 }
