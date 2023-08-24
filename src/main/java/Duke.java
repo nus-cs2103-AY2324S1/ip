0328312e51f08993e4c223d8ca39b450c5f05903
Nabonita Sen <nabonita.sonali.sen@gmail.com>
2023-08-24 17:12:44 +0800

    Mark/Unmark as done function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> taskList = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
 
         String lineSpacer = "________________________________________";
         String logo = "   / \\__\n"
@@ -28,14 +28,30 @@ public class Duke {
             } else if (userInput.equals("list")) {
                 System.out.println(lineSpacer);
                 for (int i = 0; i < taskList.size(); i++ ) {
-                    int index = i + 1;
-                    System.out.println((i + 1) + ". " + taskList.get(i));
+                    System.out.println((i + 1) + ". " + taskList.get(i).toString());
                 }
+                System.out.println(lineSpacer);
+            } else if (userInput.contains("unmark")) {
+                System.out.println(lineSpacer);
+                String[] unparsedTaskIndex = userInput.split(" ");
+                int taskIndex = Integer.parseInt(unparsedTaskIndex[1]);
+                taskList.get(taskIndex - 1).markAsUndone();
+                System.out.println("That's ruff! I've unmarked this task:  \n" +
+                        taskList.get(taskIndex - 1).toString());
+                System.out.println(lineSpacer);
+            } else if (userInput.contains("mark")) {
+                System.out.println(lineSpacer);
+                String[] unparsedTaskIndex = userInput.split(" ");
+                int taskIndex = Integer.parseInt(unparsedTaskIndex[1]);
+                taskList.get(taskIndex - 1).markAsDone();
+                System.out.println("Furtastic job completing this task: \n" +
+                        taskList.get(taskIndex - 1).toString());
                 System.out.println(lineSpacer);
             } else {
                 System.out.println(lineSpacer);
-                taskList.add(userInput);
-                System.out.println("added: " + userInput);
+                Task task = new Task(userInput);
+                taskList.add(task);
+                System.out.println("Woof luck with your new task: " + userInput);
                 System.out.println(lineSpacer);
             }
         }
