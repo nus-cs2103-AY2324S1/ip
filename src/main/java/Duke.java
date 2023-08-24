c37c3c1975666e4aa778f1104cbf4ecf6972d258
dinde2004 <doanquocthinh2004@gmail.com>
2023-08-24 11:10:49 +0800

    Add the ability to echo commands to chatbot.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("_________________________________________________________________________________________");
-        System.out.println("Hello! I'm Thinh's chatbot\nWhat can I do for you?");
-        System.out.println("_________________________________________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_________________________________________________________________________________________");
+        System.out.println("    _________________________________________________________________________________");
+        System.out.println("    Hello! I'm Thinh's chatbot\n    What can I do for you?");
+        System.out.println("    _________________________________________________________________________________");
+        Scanner in = new Scanner(System.in);
+        String inputStr;
+        do {
+            inputStr = in.nextLine();
+            if (inputStr.equals("bye")) {
+                System.out.println("    Bye. Hope to see you again soon!");
+            } else {
+                System.out.println("    " + inputStr);
+            }
+            System.out.println("    _________________________________________________________________________________");
+        } while (!inputStr.equals("bye"));
     }
 }
