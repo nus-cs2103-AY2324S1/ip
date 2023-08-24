cc499ed6b93fe65834ccdcf7e80851a2b17e9caf
leezhengjing <zhengjing.lee86@gmail.com>
2023-08-24 17:58:39 +0800

    Completed Level-5 Increment - Created BeeException class which extends exception - Generates a bee ascii art text along with the error message in the   case of incorrect outputs

--- src/main/java/Bee.java
+++ src/main/java/Bee.java
@@ -19,45 +19,92 @@ public class Bee {
         while (isRunning) {
             String userInput = scanner.nextLine();
             String[] splitInput = userInput.split(" ");
-            // If user enters "bye", ends the program and says goodbye to the user.
-            if (splitInput[0].equals("bye")) {
-                System.out.println("By-ee!. Hope to see you soon! ~Bzzz~");
-                break;
-            } else if (splitInput[0].equals("list")) {
-                listOfTasks.listAllTasks();
-            } else if (splitInput[0].equals("todo")) {
-                // Take the input after "todo "
-                String editedInput = userInput.substring(5);
-                Todo todo = new Todo(editedInput);
-                listOfTasks.addTask(todo);
-            } else if (splitInput[0].equals("deadline")) {
-                // Take the input after "deadline "
-                String editedInput = userInput.substring(9);
-                String[] splitEditedInput = editedInput.split(" /by ");
-                String deadlineDescription = splitEditedInput[0];
-                String deadlineDate = splitEditedInput[1];
-                Deadline deadlineTask = new Deadline(deadlineDescription, deadlineDate);
-                listOfTasks.addTask(deadlineTask);
-            } else if (splitInput[0].equals("event")) {
-                // Take the input after "event "
-                String editedInput = userInput.substring(6);
-                String[] splitEditedInput = editedInput.split(" /from ");
-                String[] splitEditedInput2 = splitEditedInput[1].split(" /to ");
-                String eventDescription = splitEditedInput[0];
-                String eventStartDate = splitEditedInput2[0];
-                String eventEndDate = splitEditedInput2[1];
-                Event event = new Event(eventDescription, eventStartDate, eventEndDate);
-                listOfTasks.addTask(event);
-            }
-            else if (splitInput[0].startsWith("mark")) {
-                int taskIndex = Integer.parseInt(userInput.split(" ")[1]);
-                listOfTasks.setTaskDone(taskIndex);
-            } else if (splitInput[0].startsWith("unmark")) {
-                int taskIndex = Integer.parseInt(userInput.split(" ")[1]);
-                listOfTasks.setTaskNotDone(taskIndex);
-            } else {
-                // Else, echo back the user input and add to list
-                listOfTasks.addTask(new Task(userInput));
+            try {
+                // If user enters "bye", ends the program and says goodbye to the user.
+                if (splitInput[0].equals("bye")) {
+                    System.out.println("By-ee!. Hope to see you soon! ~Bzzz~");
+                    break;
+                } else if (splitInput[0].equals("list")) {
+                    listOfTasks.listAllTasks();
+                } else if (splitInput[0].equals("todo")) {
+                    // Take the input after "todo "
+                    try {
+                        String editedInput = userInput.substring(5);
+                        if (editedInput.isEmpty()) {
+                            throw new BeeException("OOPS!! The description of a todo cannot be empty.");
+                        }
+                        Todo todo = new Todo(editedInput);
+                        listOfTasks.addTask(todo);
+                    } catch (StringIndexOutOfBoundsException e) {
+                        throw new BeeException("OOPS!! The description of a todo cannot be empty.");
+                    }
+                } else if (splitInput[0].equals("deadline")) {
+                    // Take the input after "deadline "
+                    try {
+                        String editedInput = userInput.substring(9);
+                        if (editedInput.isEmpty()) {
+                            throw new BeeException("OOPS!! The description of a deadline cannot be empty.");
+                        }
+                        String[] splitEditedInput = editedInput.split(" /by ");
+                        String deadlineDescription = splitEditedInput[0];
+                        String deadlineDate = splitEditedInput[1];
+                        Deadline deadlineTask = new Deadline(deadlineDescription, deadlineDate);
+                        listOfTasks.addTask(deadlineTask);
+                    } catch (StringIndexOutOfBoundsException e) {
+                        throw new BeeException("OOPS!! The description of a deadline cannot be empty.");
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        throw new BeeException("OOPS!! The date of the deadline cannot be empty.");
+                    }
+                } else if (splitInput[0].equals("event")) {
+                    // Take the input after "event "
+                    try {
+                        String editedInput = userInput.substring(6);
+                        if (editedInput.isEmpty()) {
+                            throw new BeeException("OOPS!! The description of an event cannot be empty.");
+                        }
+                        String[] splitEditedInput = editedInput.split(" /from ");
+                        String[] splitEditedInput2 = splitEditedInput[1].split(" /to ");
+                        String eventDescription = splitEditedInput[0];
+                        String eventStartDate = splitEditedInput2[0];
+                        String eventEndDate = splitEditedInput2[1];
+                        Event event = new Event(eventDescription, eventStartDate, eventEndDate);
+                        listOfTasks.addTask(event);
+                    } catch (StringIndexOutOfBoundsException e) {
+                        throw new BeeException("OOPS!! The description of an event cannot be empty.");
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        throw new BeeException("OOPS!! The date of an event cannot be empty.");
+                    }
+                }
+                else if (splitInput[0].startsWith("mark")) {
+                    try {
+                        int taskIndex = Integer.parseInt(userInput.split(" ")[1]);
+                        listOfTasks.setTaskDone(taskIndex);
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        throw new BeeException("OOPS!! The task number cannot be empty.");
+                    } catch (IndexOutOfBoundsException e) {
+                        throw new BeeException("OOPS!! You must have entered an invalid task number");
+                    }
+                } else if (splitInput[0].startsWith("unmark")) {
+                    try {
+                        int taskIndex = Integer.parseInt(userInput.split(" ")[1]);
+                        listOfTasks.setTaskNotDone(taskIndex);
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        throw new BeeException("OOPS!! The task number cannot be empty.");
+                    } catch (IndexOutOfBoundsException e) {
+                        throw new BeeException("OOPSS!! You must have entered an invalid task number");
+                    }
+                } else {
+                    // Else, echo back the user input and add to list
+                    listOfTasks.addTask(new Task(userInput));
+                }
+            } catch (BeeException e) {
+                System.out.println(e);
+            } catch (Exception e) {
+                System.out.println("    _  _\n" +
+                        "   | )/ )\n" +
+                        "\\\\ |//,' __\n" +
+                        "(\")(_)-\"()))=-\n" +
+                        "   (\\\\ BZZZZZZZ!!!! Something went very wrong!!");
             }
         }
     }
---
+++ src/main/java/BeeException.java
@@ -0,0 +1,14 @@
+public class BeeException extends Exception {
+    public BeeException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String toString() {
+        return "    _  _\n" +
+                "   | )/ )\n" +
+                "\\\\ |//,' __\n" +
+                "(\")(_)-\"()))=-\n" +
+                "   (\\\\ " + super.getMessage();
+    }
+}
