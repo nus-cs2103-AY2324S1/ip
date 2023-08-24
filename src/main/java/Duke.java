9895ab7a3b158283fffd65fc8e00e6fd3191354d
raydenlim <jiansonglim@gmail.com>
2023-08-24 10:26:36 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,7 +9,7 @@ public class Duke {
         taskList = new TaskList();
     }
 
-    private Task parseTaskInput(String input) {
+    private Task parseTaskInput(String input) throws DukeException {
         String[] words = input.split(" ");
         String taskType = words[0].toLowerCase();
 
@@ -17,33 +17,51 @@ public class Duke {
 
         switch (taskType) {
             case "add":
+                if (taskDescription.isEmpty()) {
+                    throw new DukeException("What should I add in? Pleas add in a description :)");
+                }
                 return new Add(taskDescription);
             case "todo":
+                if (taskDescription.isEmpty()) {
+                    throw new DukeException("Task Description cannot be EMPTY. Please add in a description :)");
+                }
                 return new ToDo(taskDescription);
             case "deadline":
                 String[] deadlineParts = taskDescription.split("/by", 2);
-                if (deadlineParts.length == 2) {
-                    return new DeadLine(deadlineParts[0].trim(), deadlineParts[1].trim());
+                String description = deadlineParts[0].trim();
+                if (description.isEmpty()) {
+                    throw new DukeException("Please provide a task description.");
                 }
-                break;
+                if (deadlineParts.length < 2 || deadlineParts[0].trim().isEmpty()) {
+                    throw new DukeException("When is " + deadlineParts[0] + " due? use /by: (date)");
+                }
+                return new DeadLine(deadlineParts[0].trim(), deadlineParts[1].trim());
             case "event":
                 String[] eventParts = taskDescription.split("/from", 2);
-                if (eventParts.length == 2) {
+                String desc = eventParts[0].trim();
+                if (desc.isEmpty() || desc.equals("/from") || desc.equals("/to") ) {
+                    throw new DukeException("Please provide a task description.");
+                } else if (eventParts.length < 2) {
+                    throw new DukeException("When is " + desc + "? use /from: (date) /to: (date)");
+                } else {
                     String[] toParts = eventParts[1].split("/to", 2);
-                    if (toParts.length == 2) {
-                        return new Event(eventParts[0].trim(), toParts[0].trim(), toParts[1].trim());
+                    if (toParts.length < 2 || toParts[1].trim().isEmpty()) {
+                        throw new DukeException("When is " + eventParts[0] + "? use /from: (date) /to: (date)");
+                    } else {
+                        return new Event(desc, toParts[0].trim(), toParts[1].trim());
                     }
                 }
-                break;
         }
         return null;
     }
 
-    public void start() {
+    public void start() throws DukeException {
         System.out.println("Hello from\n" + "Bloooooooop");
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm BloopBot");
         System.out.println(" What can I do for you?");
+        System.out.println(" Bloop will echo by default, without any keywords");
+        System.out.println(" Keywords: add, deadline, event, todo, bye");
         System.out.println("\n____________________________________________________________");
 
         boolean isEcho = true;
@@ -56,42 +74,50 @@ public class Duke {
             String[] words = strInput.split(" ");
             String firstWord = words[0].toLowerCase();
 
-            if (firstWord.equals("bye")) {
-                isEcho = false;
-                System.out.println(" Bye. Hope to see you again soon!");
-            } else if (firstWord.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                taskList.displayTasks();
-            } else if (firstWord.equals("mark")){
-                int taskIndex = Integer.parseInt(words[1]) - 1;
-                if (taskList.isMarked(taskIndex)) {
-                    System.out.println("Task already marked");
+            try {
+                if (firstWord.equals("bye")) {
+                    isEcho = false;
+                    System.out.println(" Bye. Hope to see you again soon!");
+                } else if (firstWord.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    taskList.displayTasks();
+                } else if (firstWord.equals("mark")) {
+                    int taskIndex = Integer.parseInt(words[1]) - 1;
+                    if (taskList.isMarked(taskIndex)) {
+                        throw new DukeException("Task is already marked");
+                    } else {
+                        taskList.doneAndDusted(taskIndex);
+                    }
+                } else if (firstWord.equals("unmark")) {
+                    int taskIndex = Integer.parseInt(words[1]) - 1;
+                    if (taskList.isMarked(taskIndex)) {
+                        taskList.notDoneNotDusted(taskIndex);
+                    } else {
+                        throw new DukeException("Task is already unmarked");
+                    }
+                } else if (firstWord.equals("add") || firstWord.equals("todo") ||
+                        firstWord.equals("deadline") || firstWord.equals("event")) {
+                    Task newTasks = parseTaskInput(strInput);
+                    if (newTasks != null) {
+                        taskList.addTask(newTasks);
+                    }
+                } else if (firstWord.equals("delete")) {
+                    if (words.length == 1) {
+                        throw new DukeException("Please provide a task number to delete");
+                    }
+                    int taskIndex = Integer.parseInt(words[1]) - 1;
+                    taskList.deleteTask(taskIndex);
                 } else {
-                    taskList.doneAndDusted(taskIndex);
+                    System.out.println(" Echo!! " + strInput);
                 }
-            } else if (firstWord.equals("unmark")) {
-                int taskIndex = Integer.parseInt(words[1]) - 1;
-                if (taskList.isMarked(taskIndex)) {
-                    taskList.notDoneNotDusted(taskIndex);
-                } else {
-                    System.out.println("Task already unmarked");
-                }
-            } else if (firstWord.equals("add") || firstWord.equals("todo") ||
-                    firstWord.equals("deadline") || firstWord.equals("event")){
-                Task newTasks = parseTaskInput(strInput);
-                if (newTasks != null) {
-                    taskList.addTask(newTasks);
-                } else {
-                    System.out.println("Invalid command or task input.");
-                }
-            } else {
-                System.out.println(" Echo!! " + strInput);
+            } catch (DukeException e) {
+                System.out.println("Error: " + e.getMessage());
             }
             System.out.println("\n____________________________________________________________");
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
         Duke bloopBot = new Duke();
         bloopBot.start();
     }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,20 +1,19 @@
+import java.util.ArrayList;
+
 public class TaskList {
-    private Task[] tasksDescArray;
-    private int counter;
+    private ArrayList<Task> taskList;
 
 
-    TaskList() {
-        tasksDescArray = new Task[100];
-        counter = 0;
+    public TaskList() {
+        taskList = new ArrayList<>();
     }
 
-    void addTask(Task taskDesc) {
-        if (counter < tasksDescArray.length) {
-            tasksDescArray[counter] = taskDesc;
-            counter++;
+    public void addTask(Task taskDesc) {
+        if (taskList.size() < 100) {
+           taskList.add(taskDesc);
             System.out.println(" Got it. I've added this task:");
             System.out.println("   " + taskDesc);
-            System.out.println(" Now you have " + counter + " tasks in the list.");
+            System.out.println(" Now you have " + taskList.size() + " tasks in the list.");
         } else {
             System.out.println(" 100/100 Task limit reached.");
             System.out.println(" Pay to upgrade your account.");
@@ -22,47 +21,52 @@ public class TaskList {
     }
 
     public void displayTasks() {
-        if (counter == 0) {
+        if (taskList.isEmpty()) {
             System.out.println("No task present in list");
-        } if (counter < 0) {
-            System.out.println("Error: magician, you have negative task.");
         } else {
-            for (int i = 0; i < counter; i++) {
-                System.out.println(" " + (i + 1) + "." + tasksDescArray[i]);
+            for (int i = 0; i < taskList.size(); i++) {
+                System.out.println(" " + (i + 1) + "." + taskList.get(i));
             }
         }
     }
 
     public void doneAndDusted(int taskNum) {
-        if (taskNum >= 0 && taskNum < counter) {
-            tasksDescArray[taskNum].isCompleted();
+        if (taskNum >= 0 && taskNum < taskList.size()) {
+            taskList.get(taskNum).isCompleted();
             System.out.println(" Nice! I've marked this task as done:");
-            System.out.println("   " + tasksDescArray[taskNum]);
+            System.out.println("   " + taskList.get(taskNum));
         } else {
             System.out.println(" Error: No such Task Number");
         }
     }
 
     public void notDoneNotDusted(int taskNum) {
-        if (taskNum >= 0 && taskNum < counter) {
-            tasksDescArray[taskNum].isNotCompleted();
+        if (taskNum >= 0 && taskNum < taskList.size()) {
+            taskList.get(taskNum).isNotCompleted();
             System.out.println(" OK, I've marked this task as not done yet:");
-            System.out.println("   " + tasksDescArray[taskNum]);
+            System.out.println("   " + taskList.get(taskNum));
         } else {
             System.out.println(" Error: No such Task Number");
         }
     }
 
-    public int getCounter() {
-        return counter;
-    }
-
     public boolean isMarked(int taskNum) {
-        if (taskNum >= 0 && taskNum < counter) {
-            return tasksDescArray[taskNum].isDone;
+        if (taskNum >= 0 && taskNum < taskList.size()) {
+            return taskList.get(taskNum).isDone;
         } else {
             System.out.println("Error: No such Task Number");
             return false;
+        }
+    }
+
+    public void deleteTask(int taskNum) {
+        if (taskNum >= 0 && taskNum < taskList.size()) {
+            Task removedTask = taskList.remove(taskNum);
+            System.out.println("Noted. I've removed this task:");
+            System.out.println("   " + removedTask);
+            System.out.println(" Now you have " + taskList.size() + " tasks in the list.");
+        } else {
+            System.out.println("Error: No such Task Number");
         }
     }
 
