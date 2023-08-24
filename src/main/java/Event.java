b37037343afa901ed56841b2ad78bef85ede68be
ZENN\User <miljy123@gmail.com>
2023-08-24 20:15:38 +0800

    Create Deadline, Event, Todo classes

---
+++ src/main/java/Event.java
@@ -0,0 +1,10 @@
+public class Event extends Task {
+    protected String start;
+    protected String end;
+
+    public Event(String description, String start, String end) {
+        super(description);
+        this.start = start;
+        this.end = end;
+    }
+}
