33741ba9d211d693c8ad656b2ad0e1f9333aa5a4
alientian <e1062165@u.nus.edu>
2023-08-24 23:00:16 +0800

    Add Delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
-    private static final Task[] lst = new Task[100];
-    private static int index = 0;
+    private static final ArrayList<Task> lst = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -13,6 +13,7 @@ public class Duke {
         );
 
         String input = scanner.nextLine();
+
         while (!input.equals("bye")) {
             checkInput(input);
             input = scanner.nextLine();
@@ -25,53 +26,65 @@ public class Duke {
     }
 
     public static void checkInput(String str) {
-        if (str.startsWith("mark")) {
-            String num = str.substring(5);
-            int i = Integer.parseInt(num);
-            Task t = lst[i - 1];
-            t.markAsDone();
-            System.out.println("____________________________________________________________\n"
-                    + "Nice! I've marked this task as done:\n"
-                    + t + "\n"
-                    + "____________________________________________________________\n"
-            );
-        } else if (str.startsWith("unmark")) {
-            String num = str.substring(7);
-            int i = Integer.parseInt(num);
-            Task t = lst[i - 1];
-            t.markAsNotDone();
-            System.out.println("____________________________________________________________\n"
-                    + "OK, I've marked this task as not done yet:\n"
-                    + t + "\n"
-                    + "____________________________________________________________\n"
-            );
-        } if (str.equals("list")) {
-            System.out.println("____________________________________________________________\n"
-                    + "Here are the tasks in your list:");
-            for (int i = 1; i <= index; i++) {
-                Task t = lst[i - 1];
-                System.out.println(i + ". " + t.toString());
-            }
-            System.out.println("____________________________________________________________");
-        } else {
-            try {
+        try {
+            if (str.startsWith("mark")) {
+                String num = str.substring(5);
+                int i = Integer.parseInt(num);
+                Task t = lst.get(i - 1);
+                t.markAsDone();
+                System.out.println("____________________________________________________________\n"
+                        + "Nice! I've marked this task as done:\n"
+                        + t + "\n"
+                        + "____________________________________________________________\n"
+                );
+            } else if (str.startsWith("unmark")) {
+                String num = str.substring(7);
+                int i = Integer.parseInt(num);
+                Task t = lst.get(i - 1);
+                t.markAsNotDone();
+                System.out.println("____________________________________________________________\n"
+                        + "OK, I've marked this task as not done yet:\n"
+                        + t + "\n"
+                        + "____________________________________________________________\n"
+                );
+            } else if (str.equals("list")) {
+                System.out.println("____________________________________________________________\n"
+                        + "Here are the tasks in your list:");
+                for (int i = 1; i <= lst.size(); i++) {
+                    Task t = lst.get(i - 1);
+                    System.out.println(i + ". " + t.toString());
+                }
+                System.out.println("____________________________________________________________");
+            } else if (str.startsWith("delete")) {
+                String num = str.substring(7);
+                int i = Integer.parseInt(num);
+                Task t = lst.get(i - 1);
+                lst.remove(i - 1);
+                System.out.println("____________________________________________________________\n"
+                        + "Noted. I've removed this task:\n"
+                        + t + "\n"
+                        + "Now you have " + lst.size() + " tasks in the list.\n"
+                        + "____________________________________________________________\n"
+                );
+            } else {
+                System.out.println("im addlist");
                 addList(str);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
             }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
+
     public static void addList(String str) throws InvalidInputException, IncompleteInputException {
          if (str.startsWith("todo ")) {
              if (str.length() < 6) throw new IncompleteInputException("todo");
             String des = str.substring(5);
             Todo todo = new Todo(des);
-            lst[index] = todo ;
-            index++;
+            lst.add(todo);
             System.out.println("____________________________________________________________\n"
                     + "Got it. I've added this task:\n"
-                    + lst[index - 1].toString() + "\n"
-                    + "Now you have " + index + " tasks in the list.\n"
+                    + todo + "\n"
+                    + "Now you have " + lst.size() + " tasks in the list.\n"
                     + "____________________________________________________________\n"
             );
 
@@ -83,12 +96,11 @@ public class Duke {
             String des = words[0].substring(9);
             String by = words[1].substring(3);
             Deadline dl = new Deadline(des, by);
-            lst[index] = dl ;
-            index++;
+            lst.add(dl);
             System.out.println("____________________________________________________________\n"
                     + "Got it. I've added this task:\n"
-                    + lst[index - 1].toString() + "\n"
-                    + "Now you have " + index + " tasks in the list.\n"
+                    + dl + "\n"
+                    + "Now you have " + lst.size() + " tasks in the list.\n"
                     + "____________________________________________________________\n"
             );
 
@@ -101,12 +113,11 @@ public class Duke {
             String from = words[1].substring(5);
             String to = words[2].substring(3);
             Event event = new Event(des, from, to);
-            lst[index] = event ;
-            index++;
+            lst.add(event);
             System.out.println("____________________________________________________________\n"
                     + "Got it. I've added this task:\n"
-                    + lst[index - 1].toString() + "\n"
-                    + "Now you have " + index + " tasks in the list.\n"
+                    + event + "\n"
+                    + "Now you have " + lst.size() + " tasks in the list.\n"
                     + "____________________________________________________________\n"
             );
         } else {
