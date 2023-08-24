ba34232309db078e5475351cd4b8d28e0d5ba25e
JCSnap <e0970167@u.nus.edu>
2023-08-24 17:34:38 +0800

    added logic for deadlines and events

--- src/main/java/BouncyBob.java
+++ src/main/java/BouncyBob.java
@@ -1,4 +1,7 @@
+import jdk.jfr.Event;
+
 import java.util.Scanner;
+import java.util.regex.*;
 public class BouncyBob {
     private static final String TOP_BORDER = "╔══════════════════════════════════════════════╗";
     private static final String MIDDLE_BORDER = "║                                              ║";
@@ -9,7 +12,7 @@ public class BouncyBob {
         if (task.isDone()) {
             marking = "X";
         }
-        System.out.println("[" + marking + "]" + " " + task.getName());
+        System.out.println("[" + task.getSymbol() + "]" + "[" + marking + "]" + " " + task.getDescription());
     }
 
     public static void printBye() {
@@ -50,11 +53,69 @@ public class BouncyBob {
         System.out.println(BOTTOM_BORDER);
     }
 
-    public static void printTaskAdded(String userInput, Task[] database, int pointer) {
+    public static String getTaskEvent(String input) {
+        String task = input.split("/from")[0].trim();
+        return task;
+    }
+
+    public static String getTaskDeadline(String input) {
+        String task = input.split("/by")[0].trim();
+        return task;
+    }
+
+    public static void addTaskAndPrint(String[] parts, Task[] database, int pointer) {
+        String taskType = parts[0];
+        String taskName = "";
+        switch (taskType) {
+            case "todo":
+                taskName = arrayToString(parts, 1); // we skip the string in pos -, which is the command
+                System.out.println(taskName);
+                database[pointer] = new ToDos(taskName);
+                break;
+            case "deadline":
+                taskName = arrayToString(parts, 1);
+                String datetime = extractDatetime(taskName);
+                taskName = getTaskDeadline(taskName);
+                database[pointer] = new Deadlines(taskName, datetime);
+                break;
+            case "event":
+                taskName = arrayToString(parts, 1);
+                String[] fromTo = extractFromTo(taskName);
+                taskName = getTaskEvent(taskName);
+                database[pointer] = new Events(taskName, fromTo[0], fromTo[1]);
+                break;
+        }
         System.out.println(TOP_BORDER);
-        System.out.println("Added to database: " + userInput);
-        database[pointer] = new Task(userInput);
+        System.out.println("Added to database: " + database[pointer].getDescription());
         System.out.println(BOTTOM_BORDER);
+    }
+
+    private static String arrayToString(String[] arr, int index) {
+        String combinedString = "";
+        for (int i = index; i < arr.length; i++) {
+            combinedString = combinedString + arr[i] + " ";
+        }
+        return combinedString;
+    }
+
+    private static String extractDatetime(String input) {
+        String[] parts = input.split("/by", 2);
+        if (parts.length > 1) {
+            return parts[1].trim();
+        }
+        return "";
+    }
+
+    private static String[] extractFromTo(String string) {
+        Pattern pattern = Pattern.compile("/from\\s+(.*?)\\s+/to\\s+(.*)");
+        Matcher matcher = pattern.matcher(string);
+        String[] fromTo = new String[2];
+
+        if (matcher.find()) {
+            fromTo[0] = matcher.group(1).trim();
+            fromTo[1] = matcher.group(2).trim();
+        }
+        return fromTo;
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -77,10 +138,10 @@ public class BouncyBob {
                 break;
             } else if (userInput.equals("list")) {
                 printDatabase(database, pointer);
-            } else if (parts.length == 2) {
+            } else if (parts[0].equals("mark") || parts[0].equals("unmark")) {
                 modifyTask(parts, database);
             } else {
-                printTaskAdded(userInput, database, pointer);
+                addTaskAndPrint(parts, database, pointer);
                 pointer++;
             }
         }
