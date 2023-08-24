b9951deeab6f842968684657d746899f6952a4a2
Debbie Hii Wenxin <e0959749@u.nus.edu>
2023-08-24 22:55:14 +0800

    A-Enums

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,7 +7,7 @@ import Exception.MissingTextException;
 public class Duke {
     // obligatory enums here
     enum Test {
-        HI, YOU, LOOK, AMAZING, JUST
+        HI, YOU, LOOK, AMAZING
     }
     final static ArrayList<Task> tasks = new ArrayList<>();
     final static String horizontalLine = "   ------------------------\n";
