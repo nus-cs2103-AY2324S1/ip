cfb90cfbae124a326783fd04ff5f7be3fb5213fd
lunaroddity <e0969600@u.nus.edu>
2023-08-24 12:46:09 +0800

    Rename chatbot to Bareum, add greetings

---
+++ src/main/java/Bareum.java
@@ -0,0 +1,12 @@
+public class Bareum {
+
+    public static void main(String[] args) {
+        String line = "------------------------------------";
+        System.out.println(line);
+        System.out.println("Hello! I'm Bareum!");
+        System.out.println("\nWhat can I do for you? ^^");
+        System.out.println(line);
+        System.out.println("\nBye! Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
