c1b3632ea7f0436c2f7ccf063a5a493ee1612dcc
fuyiqiao <yiqiao_fu@hotmail.com>
2023-08-24 12:09:28 +0800

    Level 1

--- src/main/java/Ballsorter.java
+++ src/main/java/Ballsorter.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class Ballsorter {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
         System.out.println("Hello! I'm Ballsorter\nWhat can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println(line);
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+            System.out.println(line);
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(line);
     }
 }
