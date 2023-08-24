6f191606b51b7d02c265310712191fa488b243c9
andrefoo <76435197+andrefoo@users.noreply.github.com>
2023-08-24 15:36:24 +0800

    Level-2 Add, List

--- src/main/java/Dre.java
+++ src/main/java/Dre.java
@@ -1,30 +1,51 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Dre {
-    public static void greet() {
+    ArrayList<String> list;
+
+    public Dre() {
+        list = new ArrayList<>();
+    }
+    public void greet() {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Dre");
         System.out.println("What can I do for you?");
     }
 
-    public static void exit() {
+    public void exit() {
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 
-    public static void echo() {
+    public void echo() {
         Scanner sc = new Scanner(System.in);
         String next = sc.nextLine();
         while (!next.equals("bye")) {
-            System.out.println(next);
+            if (next.equals("list")) {
+                list();
+            } else {
+                System.out.println("added: " + next);
+                list.add(next);
+            }
             next = sc.nextLine();
         }
+        sc.close();
         exit();
     }
 
+    public void list() {
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + ". " + list.get(i));
+        }
+    }
+
+
+
     public static void main(String[] args) {
-        greet();
-        echo();
+        Dre dre = new Dre();
+        dre.greet();
+        dre.echo();
     }
 }
