1491e56be50498fdff0aab05574cfecf8fa65598
Andre Chua <110965035+andrechuakj@users.noreply.github.com>
2023-08-24 14:41:47 +0800

    Added Todos, events, deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,63 +1,117 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class Duke {
     public static void main(String[] args) {
-        String line = "────────────────────────────────────────────────────────────────────\n";
+        String line = "────────────────────────────────────────────────────────────────────";
         String logo = " ___  _ _                  ___  _          _    ___        _    ___ \n"
                 + "|  _|| | | ___  _ _  _ _  |  _>| |_  ___ _| |_ | . > ___ _| |_ |_  |\n"
                 + "| |  \\   // . \\| | || '_> | <__| . |<_> | | |  | . \\/ . \\ | |    | |\n"
                 + "| |_  |_| \\___/`___||_|   `___/|_|_|<___| |_|  |___/\\___/ |_|   _| |\n"
                 + "|___|                                                          |___|\n";
 
-        String greet = line +
-                "Hello! I'm\n"
+        String greet = line
+                + "\n"
+                + "Hello! I'm\n"
                 + logo
                 + "What can I do for you?\n"
-                + line;
+                + line
+                + "\n";
         System.out.println(greet);
         Scanner input = new Scanner(System.in);
 
-        Task[] list = new Task[100];
-        int index = 0;
+        ArrayList<Task> list = new ArrayList<>();
         while (true) {
             String userInput = input.nextLine();
-            if (userInput.split(" ", 2)[0].equals("mark")) {
-                int i = Integer.parseInt(userInput.split(" ", 2)[1]);
-                list[i - 1].markAsDone();
-                System.out.println(line
-                        + "Nice! I've marked this task as done: \n"
-                        + list[i - 1]
-                        + "\n"
-                        + line);
-            } else if (userInput.split(" ", 2)[0].equals("unmark")) {
-                int i = Integer.parseInt(userInput.split(" ", 2)[1]);
-                list[i - 1].markAsNotDone();
-                System.out.println(line
-                        + "OK, I've marked this task as not done yet: \n"
-                        + list[i - 1]
-                        + "\n"
-                        + line);
-            } else if (userInput.equals("bye")) {
-                break;
-            } else if (userInput.equals("list")) {
-                for (int i = 0; i < index; i++) {
-                    System.out.println(Integer.toString(i + 1)
-                            + ". "
-                            + list[i]);
+
+            int spaceIndex = userInput.indexOf(" ");
+            if (spaceIndex == -1) {
+                if (userInput.equals("list")) {
+                    System.out.println(line);
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println(Integer.toString(i + 1)
+                                + ". "
+                                + list.get(i));
+                    }
+                    System.out.println(line + "\n");
+                } else if (userInput.equals("bye")) {
+                    break;
                 }
             } else {
-                list[index] = new Task(userInput);
-                index++;
-                System.out.println(line
-                        + "added: "
-                        + userInput
-                        + "\n" + line);
+                switch (userInput.substring(0, spaceIndex)) {
+                    case "todo":
+                        list.add(new Todo(userInput.substring(spaceIndex + 1)));
+                        System.out.println(line
+                                + "\n"
+                                + "Got it. I've added this task: \n    "
+                                + list.get(list.size() - 1)
+                                + "\n"
+                                + "Now you have " + list.size() + " tasks in the list.\n"
+                                + line
+                                + "\n");
+                        break;
+                    case "event":
+                        int fromIndex = userInput.indexOf("/from");
+                        int toIndex = userInput.indexOf("/to");
+                        String eventDesc = userInput.substring(spaceIndex + 1, fromIndex - 1);
+                        String from = userInput.substring(fromIndex + 6, toIndex - 1);
+                        String to = userInput.substring(toIndex + 4);
+                        list.add(new Event(eventDesc, from, to));
+                        System.out.println(line
+                                + "\n"
+                                + "Got it. I've added this task: \n    "
+                                + list.get(list.size() - 1)
+                                + "\n"
+                                + "Now you have " + list.size() + " tasks in the list.\n"
+                                + line
+                                + "\n");
+                        break;
+                    case "deadline":
+                        int byIndex = userInput.indexOf("/by");
+                        String DeadlineDesc = userInput.substring(spaceIndex + 1, byIndex - 1);
+                        String by = userInput.substring(byIndex + 4);
+                        list.add(new Deadline(DeadlineDesc, by));
+                        System.out.println(line
+                                + "\n"
+                                + "Got it. I've added this task: \n    "
+                                + list.get(list.size() - 1)
+                                + "\n"
+                                + "Now you have " + list.size() + " tasks in the list.\n"
+                                + line
+                                + "\n");
+                        break;
+                    case "mark":
+                        int i = Integer.parseInt(userInput.split(" ", 2)[1]);
+                        list.get(i - 1).markAsDone();
+                        System.out.println(line
+                                + "\n"
+                                + "Nice! I've marked this task as done: \n"
+                                + list.get(i - 1)
+                                + "\n"
+                                + line
+                                + "\n");
+                        break;
+                    case "unmark":
+                        int j = Integer.parseInt(userInput.split(" ", 2)[1]);
+                        list.get(j - 1).markAsNotDone();
+                        System.out.println(line + "\n"
+                                + "OK, I've marked this task as not done yet: \n"
+                                + list.get(j - 1)
+                                + "\n"
+                                + line);
+                        break;
+                }
             }
+
+
         }
 
         String sendOff = line
+                + "\n"
                 + "Bye. Hope to see you again soon!\n"
-                + line;
+                + line
+                + "\n";
         System.out.println(sendOff);
     }
 }
