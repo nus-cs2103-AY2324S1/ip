27a23fb7b3171119e2bb508a6e1d248d66123253
gongg21 <gallen.ong21@gmail.com>
2023-08-25 00:24:13 +0800

    Level-1

--- src/main/java/GBot.java
+++ src/main/java/GBot.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class GBot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello I'm GBot\n");
         System.out.println("What can I do for you today?\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
+        while (true) {
+            String message = scanner.nextLine();
+            if (message.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n");
+                return;
+            }
+            System.out.println(message);
+        }
     }
 }
 
