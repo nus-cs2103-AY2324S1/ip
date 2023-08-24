e635b63f30086ee72d4780a097661ab59746e9ad
Kyriel Abad <abadkyriel@gmail.com>
2023-08-24 14:09:27 +0800

    Add greeter and goodbye to bot

--- src/main/java/Crusader.java
+++ src/main/java/Crusader.java
@@ -6,21 +6,43 @@ public class Crusader {
             + " | |     _ __ _   _ ___  __ _  __| | ___ _ __\n"
             + " | |    | '__| | | / __|/ _` |/ _` |/ _ \\ '__|\n"
             + " | |____| |  | |_| \\__ \\ (_| | (_| |  __/ |\n"
-            + "  \\_____|_|   \\__,_|___/\\__,_|\\__,_|\\___|_|\n";
+            + "  \\_____|_|   \\__,_|___/\\__,_|\\__,_|\\___|_|";
 
+    /**
+     * Displays the logo for the chatbot.
+     */
     private static void showLogo() {
         System.out.println(logo);
+        addDivider();
     }
 
     /**
      * Generates a horizontal line to divide parts of the conversation.
      */
     private static void addDivider() {
-        System.out.println("____________________________________________________________\n");
+        System.out.println("____________________________________________________________");
     }
+
+    /**
+     * Greets the user.
+     */
+    private static void greet() {
+        System.out.println("Hi, I am CRUSADER\nWhat can I do for you?");
+        addDivider();
+    }
+
+    /**
+     * Says goodbye to the user.
+     */
+    private static void farewell() {
+        System.out.println("Bye!\nHave a great day!");
+        addDivider();
+    }
+
     public static void main(String[] args) {
         addDivider();
         showLogo();
-        addDivider();
+        greet();
+        farewell();
     }
 }
