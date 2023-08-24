b47d291beaf5965a021aa88bbb235b1e525fc9f6
Jeffrey <jeffreyjianyujie@gmail.com>
2023-08-24 12:52:16 +0800

    Add list function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,6 +12,8 @@ public class Duke {
     public static void main(String[] args) {
         String greeting = "Hello! I'm JED, your personal chat-bot!\nWhat can I do for you?";
         String goodbye = "Bye. Hope to see you again soon!";
+        String[] items = new String[100];
+        int count = 0;
 
         Scanner sc = new Scanner(System.in);
 
@@ -21,8 +23,17 @@ public class Duke {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                String list = "";
+                for (int i = 0; i < count; i++) {
+                    list += "  " + (i + 1) + ". " + items[i] + "\n";
+                }
+                talk(list);
+            } else {
+                items[count] = input;
+                count++;
+                talk("added: " + input);
             }
-            talk(input);
         }
         talk(goodbye);
     }
