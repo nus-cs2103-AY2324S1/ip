3d4a50f077fc7964084a065ae0efbb6d815da0dc
QianChangru <changru_qian@u.nus.edu>
2023-08-24 23:37:59 +0800

    Finish Level-1: Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
 public class Duke {
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Jarvis");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Jarvis");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        String command = userInput.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    " + command);
+            System.out.println("    ____________________________________________________________");
+            command = userInput.nextLine();
+        }
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 }
