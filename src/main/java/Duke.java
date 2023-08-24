a4dcac727f1aa096f7ffbf6e645bf8851cd35235
kayabuttertoastt <e0970063@u.nus.edu>
2023-08-24 19:51:10 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm ChatBot.\n" +
-                "What can I do for you?\n" +
-                "Bye. Hope to see you again soon!"
-        );
+              "What can I do for you?\n" );
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = scanner.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
