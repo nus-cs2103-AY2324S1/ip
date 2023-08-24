b5c7a1c40d3b2026a3d27b68bdab398c4d010cb6
p-xp <panxinping10@gmail.com>
2023-08-25 00:15:04 +0800

    feat: implement Level-4

---
+++ src/main/java/Event.java
@@ -0,0 +1,16 @@
+public class Event extends Task {
+
+    protected String startTime;
+    protected String endTime;
+
+    public Event(String description, String startTime, String endTime) {
+        super(description);
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (" + this.startTime + " to " + this.endTime + ")";
+    }
+}
--- src/main/java/Remy.java
+++ src/main/java/Remy.java
@@ -3,68 +3,122 @@ import java.util.ArrayList;
 
 public class Remy {
 
+    private static String divider = "____________________________________________________________\n";
+    private static String shortDivider = "_____________";
+
     public static void main(String[] args) {
-        String welcomeMessage = "____________________________________________________________\n" +
+
+        String welcomeContent =
                 "I'm Remy, and it is NOT nice to see you.\n" +
-                "Faster tell me what you want and go away.\n" +
-                "____________________________________________________________\n";
-        String exitMessage = "Hope to never see you again!\n" +
-                "____________________________________________________________\n";
+                        "Faster tell me what you want and go away.";
+
+        String exitMessage = "Hope to never see you again!\n" + divider;
+
 
         ArrayList<Task> taskList = new ArrayList(100);
 
-        System.out.println(welcomeMessage);
+        printSandwichContent(welcomeContent, "long");
         Scanner scanner = new Scanner(System.in);
         String input;
 
         while (true) {
             input = scanner.nextLine();
-            if (input.equals("bye")) {
+            String taskType = parseTaskType(input);
+            if (taskType.equals("bye")) {
                 System.out.println(exitMessage);
                 break;
-            } else if (input.equalsIgnoreCase("list")) {
-                System.out.println("__________");
+            } else if (taskType.equals("list")) {
+                System.out.println(shortDivider);
                 for (int i = 0; i < taskList.size(); i++) {
                     System.out.println(" " + (i + 1) + ". " + taskList.get(i));
                 }
-                System.out.println("__________");
-            } else if (input.toLowerCase().startsWith("mark ")) {
+                System.out.println(shortDivider);
+            } else if (taskType.equals("mark")) {
                 // Marks item as done
                 int index = Integer.parseInt(input.substring(5)) - 1;
                 if (index >= 0 && index < taskList.size()) {
                     taskList.get(index).markAsDone();
-                    System.out.println("__________");
-                    System.out.println("Done. You happy?");
-                    System.out.println(taskList.get(index).toString());
-                    System.out.println("__________");
+                    String content = "Done. You happy?\n" + taskList.get(index).toString();
+                    printSandwichContent(content, "short");
                 } else {
-                    System.out.println("__________");
-                    System.out.println("No such item lah.");
-                    System.out.println("__________");
+                    printSandwichContent("No such item lah", "short");
                 }
-            } else if (input.toLowerCase().startsWith("unmark ")) {
+            } else if (taskType.equals("unmark")) {
                 // Marks item as undone
                 int index = Integer.parseInt(input.substring(5)) - 1;
                 if (index >= 0 && index < taskList.size()) {
                     taskList.get(index).markAsUndone();
-                    System.out.println("__________");
-                    System.out.println("Done. You happy?");
-                    System.out.println(taskList.get(index).toString());
-                    System.out.println("__________");
+                    String content = "Done. You happy?\n" + taskList.get(index).toString();
+                    printSandwichContent(content, "short");
                 } else {
-                    System.out.println("__________");
-                    System.out.println("No such item lah.");
-                    System.out.println("__________");
+                    printSandwichContent("No such item lah", "short");
+                }
+            } else if (taskType.equals("todo")) {
+                String description = input.substring(5);
+                Todo temp = new Todo(description);
+                taskList.add(temp);
+                addTask(temp, taskList.size());
+            } else if (taskType.equals("deadline")) {
+                String[] parts = input.substring(9).split(" /by ");
+                if (parts.length == 2) {
+                    Deadline temp = new Deadline(parts[0], parts[1]);
+                    taskList.add(temp);
+                    addTask(temp, taskList.size());
+                } else {
+                    printSandwichContent("Wrong format lah.", "short");
+                }
+            } else if (taskType.equals("event")) {
+                String[] parts = input.substring(6).split(" /from | /to ");
+                if (parts.length == 3) {
+                    Event temp = new Event(parts[0], parts[1], parts[2]);
+                    taskList.add(temp);
+                    addTask(temp, taskList.size());
+                } else {
+                    printSandwichContent("Wrong format lah.", "short");
                 }
             } else {
-                Task temp = new Task(input);
-                taskList.add(temp);
-                System.out.println("__________");
-                System.out.println(" added: " + input);
-                System.out.println("__________");
+                printSandwichContent("No such command lah.", "short");
             }
-
-
         }
+    }
+
+    public static String parseTaskType(String input) {
+        if (input.equalsIgnoreCase("list")) {
+            return "list";
+        } else if (input.toLowerCase().startsWith("mark")) {
+            return "mark";
+        } else if (input.toLowerCase().startsWith("unmark")) {
+            return "unmark";
+        } else if (input.toLowerCase().startsWith("todo")) {
+            return "todo";
+        } else if (input.toLowerCase().startsWith("deadline")) {
+            return "deadline";
+        } else if (input.toLowerCase().startsWith("event")) {
+            return "event";
+        } else if (input.toLowerCase().equals("bye")) {
+            return "bye";
+        } else {
+            return "invalid";
+        }
+    }
+
+    public static void printSandwichContent(String content, String dividerType) {
+
+        if (dividerType == "short") {
+            System.out.println(shortDivider);
+            System.out.println(content);
+            System.out.println(shortDivider);
+        } else {
+            System.out.println(divider);
+            System.out.println(content);
+            System.out.println(divider);
+        }
+    }
+
+    public static void addTask(Task task, int num) {
+        String content = "Added, now scram.\n" +
+                task.toString() + "\n" +
+                "Now you have " + num + " tasks in the list.";
+        printSandwichContent(content, "short");
     }
 }
