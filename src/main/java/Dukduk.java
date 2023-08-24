ee86868b3e210644ab7d57eadefa1a489f4987f7
wnchan <e0970469@u.nus.edu>
2023-08-24 16:29:25 +0800

    Level-1: Echo

--- src/main/java/Dukduk.java
+++ src/main/java/Dukduk.java
@@ -1,14 +1,30 @@
+import java.util.Scanner;
+
 public class Dukduk {
     public static void main(String[] args) {
-	printLine();
-	System.out.println(" Hello! I'm Dukduk");
-	System.out.println(" What can I do for you?");
-	printLine();
-	System.out.println(" Bye. Hope to see you again soon!");
-	printLine();	
+        printLine();
+        System.out.println(" Hello! I'm Dukduk");
+        System.out.println(" What can I do for you?");
+        printLine();
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print(" ");
+            String input = scanner.nextLine();
+            printLine();
+            System.out.println(" " + input);
+            printLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                printLine();
+                break;
+            }
+        }
+        scanner.close();
     }
 
     public static void printLine() {
-	    System.out.println("____________________________________________________________");
+        System.out.println("____________________________________________________________");
     }
 }
