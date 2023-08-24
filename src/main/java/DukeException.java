23a32a85ef790cbafdef050cbe40e4532bb0e51c
ElginTZM <63971687+ElginTZM@users.noreply.github.com>
2023-08-24 23:00:25 +0800

    Add DukeException class

---
+++ src/main/java/DukeException.java
@@ -0,0 +1,13 @@
+public class DukeException extends Exception {
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_RESET = "\u001B[0m";
+
+    public DukeException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%sError: %s%s", ANSI_RED, super.getMessage(), ANSI_RESET);
+    }
+}
