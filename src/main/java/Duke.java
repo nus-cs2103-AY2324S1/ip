bfa452c2bff9a6268b84d52c3a0305f03248b53c
Winston <leonardwinstoncx9@gmail.com>
2023-08-24 15:17:44 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,30 +1,38 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
-    public static String line = "____________________________________________________________\n";
+    public static String line = "\t____________________________________________________________\n";
+    public static ArrayList<String> strList = new ArrayList<>();
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
 
         String greeting =
                 line +
-                "Hello! I'm DukeBot\n" +
-                "What can I do for you?\n" +
+                "\tHello! I'm DukeBot\n" +
+                "\tWhat can I do for you?\n" +
                 line;
 
-        String exitMessage = "Bye. Hope to see you again\n" + line;
+        String exitMessage = line + "\tBye. Hope to see you again\n" + line;
         System.out.println(greeting);
 
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         String echo = myObj.nextLine();  // Read user input
 
         while (!echo.equalsIgnoreCase("bye")) {
-            System.out.println("");
-            System.out.println(line + echo + "\n" + line);
+
+            if (echo.equalsIgnoreCase("list")) {
+                int arrLength = strList.size();
+                System.out.println(line);
+                for (int i = 0; i < arrLength; i++) {
+                    int number = i + 1;
+                    System.out.println("\t" + number + ". " + strList.get(i));
+                }
+                System.out.println(line);
+            } else {
+                System.out.println(line + "\tadded: " + echo + "\n" + line);
+                strList.add(echo);
+            }
             echo = myObj.nextLine();  // Read user input
+
         }
         System.out.println(exitMessage);
     }
