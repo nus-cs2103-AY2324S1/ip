d1d19878f118fd98a570415ca2ef9edfa3074e56
Ken-Lai <weizhong9201@gmail.com>
2023-08-24 16:36:56 +0800

    Add ability to store text entered by user and display them back when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -5,6 +6,9 @@ public class Duke {
     private static final String INDENTATION = "    ";
     private static final String GREETING_TEXT = "Hello, I'm Tasket\n    What can I do for you?";
     private static final String GOODBYE_TEXT = "Bye. Hope to see you again soon!";
+
+    private static String prompt = "";
+    private static ArrayList<String> list = new ArrayList<>();
 
     public static void showSeparationLine() {
         System.out.println(INDENTATION + SEPARATION_LINE);
@@ -21,25 +25,53 @@ public class Duke {
         showSeparationLine();
     }
 
-    public static void echoInputText(String string) {
-        System.out.println(string);
+    public static void showAddedText(String text) {
+        System.out.println(INDENTATION + "added: " + text);
         showSeparationLine();
     }
 
+    public static void showLists() {
+        for (int i = 1; i <= list.size(); i++) {
+            System.out.printf("%s%d. %s\n", INDENTATION, i, list.get(i - 1));
+        }
+        showSeparationLine();
+    }
+
+    public static void echoInputText(String text) {
+        System.out.println(text);
+        showSeparationLine();
+    }
+
+    public static void parseText(String text) {
+        switch (text) {
+        case "list":
+            showLists();
+            break;
+
+        case "bye":
+            showGoodbyeText();
+            break;
+
+        default:
+            list.add(text);
+            showAddedText(text);
+            break;
+        }
+    }
+
     public static void main(String[] args) {
-        String prompt = "";
         Scanner sc = new Scanner(System.in);
 
         showGreetingText();
 
         while (!prompt.equals("bye")) {
-            prompt = sc.next();
+            System.out.println();
+            prompt = sc.nextLine();
             showSeparationLine();
 
-            echoInputText(prompt);
+            parseText(prompt);
         }
 
         sc.close();
-        showGoodbyeText();
     }
 }
