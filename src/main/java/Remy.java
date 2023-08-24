f511d1c075decb275fafe2ff5832f904dfa53b00
p-xp <panxinping10@gmail.com>
2023-08-24 17:45:06 +0800

    feat: implement Level-0

---
+++ src/main/java/Remy.java
@@ -0,0 +1,12 @@
+public class Remy {
+    public static void main(String[] args) {
+        String welcomeMessage = "____________________________________________________________\n" +
+                " Hello! I'm Remy.\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n";
+        String exitMessage = " Bye. Hope to see you again soon!\n" +
+        "____________________________________________________________\n";
+
+        System.out.println(welcomeMessage + exitMessage);
+    }
+}
