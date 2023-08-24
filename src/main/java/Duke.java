e4d5f5ba44f73a8437f7b1c375c49fe8c38de648
KamiliArsyad <arsyad.ik@gmail.com>
2023-08-24 17:07:20 +0800

    implement task and its status

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -37,6 +37,7 @@ public class Duke {
             this.model = model;
         }
 
+        // TODO: Implement a parser instead of hardcodoing if elses here
         public void handleInput() {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
@@ -49,6 +50,18 @@ public class Duke {
                 return;
             }
 
+            if (input.startsWith("mark")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                model.echo(storage.markAsDone(index));
+                return;
+            }
+
+            if (input.startsWith("unmark")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                model.echo(storage.unmarkAsDone(index));
+                return;
+            }
+
             model.echo(storage.save(input));
         }
 
@@ -58,29 +71,69 @@ public class Duke {
     }
 
     static class Storage {
-        private String[] storage = new String[100];
+        private final int SIZE = 100;
+        private final String[] storage = new String[SIZE];
+        private final boolean[] isDone = new boolean[SIZE];
+        private final Task[] tasks = new Task[SIZE];
         private int pointer = 0;
 
 
         public String save(String input) {
-            storage[pointer] = input;
+            Task task = new Task(input);
+            tasks[pointer] = task;
             pointer++;
             return "added: " + input;
+        }
+
+        public String markAsDone(int index) {
+            tasks[index].markAsDone();
+            return "Nice! I've meowrked this task as done:\n"
+                    + "    " + tasks[index];
+        }
+
+        public String unmarkAsDone(int index) {
+            tasks[index].unmarkAsDone();
+            return "Oh meow! I've marked this task as undone :( :\n"
+                    + "    " + tasks[index];
         }
 
         @Override
         public String toString() {
             StringBuilder sb = new StringBuilder();
             int count = 0;
-            for (String s : storage) {
-                if (s == null) {
+
+            for (Task task : tasks) {
+                if (task == null) {
                     break;
                 }
                 count++;
-                sb.append(count).append(". ").append(s).append("\n");
+                sb.append(count).append(". ").append(task).append("\n");
             }
 
             return sb.toString();
+        }
+    }
+
+    static class Task {
+        boolean isDone;
+        String description;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public void markAsDone() {
+            this.isDone = true;
+        }
+
+        public void unmarkAsDone() {
+            this.isDone = false;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + (isDone ? "X" : " ") + "] " + description;
         }
     }
 
