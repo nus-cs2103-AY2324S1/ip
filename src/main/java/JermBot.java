be50c4972b9c360b58dd4ab17f82094b22795627
Jeremy <jeremyyong0101@gmail.com>
2023-08-24 10:48:23 +0800

    Increment: Level-2

--- src/main/java/JermBot.java
+++ src/main/java/JermBot.java
@@ -5,15 +5,30 @@ public class JermBot {
         System.out.println("Hello! I'm JermBot");
         System.out.println("What can I do for you?");
 
+        String[] storage = new String[100];
+        int numOfItems = 0;
         Scanner sc = new Scanner(System.in);
-        String currStr = "";
-        while (!currStr.equals("bye")) {
-            if (currStr != "") {
-                System.out.println(currStr);
+        String currStr = sc.nextLine();
+        while (true) {
+            if (currStr.equals("bye")) {
+                break;
             }
+
+            if (currStr.equals("list")) {
+                for (int i = 0; i < numOfItems; i++) {
+                    System.out.print(i + 1);
+                    System.out.print(". " + storage[i] + "\n");
+                }
+            } else {
+                storage[numOfItems] = currStr;
+                numOfItems++;
+                System.out.println("added: " + currStr);
+            }
+
             currStr = sc.nextLine();
         }
 
         System.out.println("Bye. Hope to see you again soon!");
+        sc.close();
     }
 }
