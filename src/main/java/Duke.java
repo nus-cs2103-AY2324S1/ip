2163a4a390ef0fca8b9ce142ea1974aa007ba79d
yongning0310 <85162884+yongning0310@users.noreply.github.com>
2023-08-24 14:13:35 +0800

    added scanner to echo input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ██▄   ████▄    ▄     ▄▀  \n" +
@@ -9,6 +11,20 @@ public class Duke {
                 "                         ";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if ("bye".equals(input)) {
+                System.out.println("Bye. Hope to see you again soon!\n");
+                break; // This will exit the loop and end the program
+            } else {
+                System.out.println(input);
+            }
+        }
+
+        scanner.close();
+
     }
 }
