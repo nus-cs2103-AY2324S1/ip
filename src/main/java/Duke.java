f68b34308ad929deb71948eb250ab95bb180f564
Vedant Sinha <vedantsinha2003@gmail.com>
2023-08-24 21:08:04 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String[] taskList = new String[100];
+        int taskCount = 0;
+
         // Greeting message
         System.out.println("Hello! I'm Sivraj");
         System.out.println("What can I do for you?");
@@ -13,9 +17,18 @@ public class Duke {
                 // Farewell message
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else {
+            } else if (echo.equals("list")) {
                 System.out.println(" ----------------------------------------");
-                System.out.println("    " + echo);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("    " + (i + 1) + ". " + taskList[i]);
+                }
+                System.out.println(" -----------------------------------------");
+            } else {
+                taskList[taskCount] = echo;
+                taskCount++;
+
+                System.out.println(" ----------------------------------------");
+                System.out.println("    added: " + echo);
                 System.out.println(" ----------------------------------------");
             }
         }
