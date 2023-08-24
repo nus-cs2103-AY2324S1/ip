97e78d3be2133345d229ceebee60f58a15698f73
xyT <tongyu5336@gmail.com>
2023-08-24 19:54:08 +0800

    Level-0

---
+++ src/main/java/Tong.java
@@ -0,0 +1,15 @@
+public class Duke {
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        int length = 20; // Adjust the length of the line as needed
+        for (int i = 0; i < length; i++) {
+            System.out.print("-");
+        }
+        System.out.println("Hello! I'm TONG.\nWhat can I do for you?");
+    }
+}
