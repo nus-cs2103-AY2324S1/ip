916a1b37f8b987ebf5d4fd20e4fe03414ae31ee4
marcellaantania <marcellaantaniatan@gmail.com>
2023-08-24 13:39:45 +0800

    Fix typo

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,7 +1,7 @@
 public class Event extends Task {
     String from;
     String to;
-    public Task(String taskName, String from, String to) {
+    public Event(String taskName, String from, String to) {
         super(taskName);
         this.from = from;
         this.to = to;
