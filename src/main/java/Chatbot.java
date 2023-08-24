4afb4e8ac6cd143c44911523f54a1f2cf296c0df
andytoh1 <e0773488@u.nus.edu>
2023-08-24 08:05:07 +0800

    no message

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Chatbot {
     public static void main(String[] args) {
         String[] messageList = new String[100];
+        boolean[] messageDone = new boolean[100];
         int messageCount = 0;
         Scanner userInput = new Scanner(System.in);
         System.out.println("Hello! I'm Chatbot!");
@@ -11,12 +12,24 @@ public class Chatbot {
             String userMessage = userInput.nextLine();
             if (userMessage.equalsIgnoreCase("bye")) break;
             if (userMessage.equalsIgnoreCase("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < messageCount; i++) {
                     int listNumber = i + 1;
                     System.out.print(listNumber + ". ");
+                    if (messageDone[i]) System.out.print("[X] ");
+                    else System.out.print("[ ] ");
                     System.out.println(messageList[i]);
                 }
                 continue;
+            }
+            if (userMessage.length() >= 6) {
+                if (userMessage.substring(0, 4).equalsIgnoreCase("mark")) {
+                    int doneTask = Integer.parseInt(userMessage.substring(5));
+                    System.out.println("Well done! This task has been marked as done.");
+                    System.out.println("[X] " + messageList[doneTask - 1]);
+                    messageDone[doneTask - 1] = true;
+                    continue;
+                }
             }
             System.out.println("Added: " + userMessage);
             messageList[messageCount] = userMessage;
