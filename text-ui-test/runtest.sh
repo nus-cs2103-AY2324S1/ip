885bb01844c90734668ced7ab3d5233a3cf6c45f
ji-just-ji <wongjk9999@gmail.com>
2023-08-24 14:50:57 +0800

    Fix runtest.sh changes

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -9,7 +9,7 @@ fi
 # delete output from previous run
 if [ -e "./ACTUAL.TXT" ]
 then
-    rm EXPECTED.TXT
+    rm ACTUAL.TXT
 fi
 
 # compile the code into the bin folder, terminates if error occurred
@@ -19,15 +19,15 @@ then
     exit 1
 fi
 
-# run the program, feed commands from input.txt file and redirect the output to the EXPECTED.TXT
-java -classpath ../bin Peko < input.txt > EXPECTED.TXT
+# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ../bin Peko < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix EXPECTED.TXT EXPECTED-UNIX.TXT
+dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
 
 # compare the output to the expected output
-diff EXPECTED.TXT EXPECTED-UNIX.TXT
+diff ACTUAL.TXT EXPECTED-UNIX.TXT
 if [ $? -eq 0 ]
 then
     echo "Test result: PASSED"
