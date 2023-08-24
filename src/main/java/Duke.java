d175a96dc84ba70336ba541fbc7597c88af3d907
Andrew Janong <andrewjanong@gmail.com>
2023-08-24 13:41:00 +0800

    Mark as Done feature added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * A chatbot to keep track of your tasks
+ *
+ * @author Andrew Daniel Janong
+ */
 public class Duke {
     public static void main(String[] args) {
         List<Task> tasks = new ArrayList<>();
@@ -14,26 +19,41 @@ public class Duke {
                 "\t What can I do for you? \n\t" +
                 LINE);
 
-        while (true) {
-            String userInput = sc.nextLine();
-            if (userInput.toLowerCase().equals("bye")) {
-                System.out.println("\t" + LINE + "\n" +
-                        "\t Bye. Hope to see you again soon! \n\t" +
-                        LINE);
-                break;
-            } else if (userInput.toLowerCase().equals("list")) {
+        try {
+            while (true) {
+                String userInput = sc.nextLine();
+
                 System.out.println("\t" + LINE);
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println("\t " + (i+1) + ". " +  tasks.get(i));
+
+                if (userInput.toLowerCase().equals("bye")) {
+                    System.out.println("\t Bye. Hope to see you again soon!");
+                    break;
                 }
+                if (userInput.toLowerCase().equals("list")) {
+                    System.out.println("\t Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println("\t " + (i + 1) + "." + tasks.get(i));
+                    }
+                } else if (userInput.split(" ")[0].toLowerCase().equals("mark")) {
+                    System.out.println("\t Nice job! I've marked this task as done:");
+                    Task task = tasks.get(Integer.parseInt(userInput.split(" ")[1]) - 1);
+                    task.markAsDone();
+                    System.out.println("\t\t " + task);
+                } else if (userInput.split(" ")[0].toLowerCase().equals("unmark")) {
+                    System.out.println("\t What happened? I've marked this task as not done yet:");
+                    Task task = tasks.get(Integer.parseInt(userInput.split(" ")[1]) - 1);
+                    task.markAsNotDone();
+                    System.out.println("\t\t " + task);
+                } else {
+                    Task task = new Task(userInput);
+                    tasks.add(task);
+                    System.out.println("\t Task added: " + userInput);
+                }
+
                 System.out.println("\t" + LINE);
-            } else {
-                Task task = new Task(userInput);
-                tasks.add(task);
-                System.out.println("\t" + LINE + "\n" +
-                        "\t added: " + userInput + "\n\t" +
-                        LINE);
             }
+        } catch (Exception e) {
+            System.out.println("\t [ERROR] Sorry, an error occurred and ADJ has broken down :(");
         }
 
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,13 +1,62 @@
+/**
+ * Represents a task
+ *
+ * @author Andrew Daniel Janong
+ */
 public class Task {
-    private String taskName;
+    /**
+     * The task name or description
+     */
+    protected String taskName;
 
+    /**
+     * Marks whether the task is done or not
+     */
+    protected boolean isDone;
+
+    /**
+     * A public constructor for the task.
+     * A task is set to be not done when first constructed.
+     * @param taskName
+     */
     public Task(String taskName) {
         this.taskName = taskName;
+        this.isDone = false;
     }
 
+    /**
+     * Marks the task as done
+     */
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    /**
+     * Marks the task as not done
+     */
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    /**
+     * Gets the icon that shows whether the task is done or not
+     * @return The icon representing the progress of the task ([X] for done, [ ] for not done)
+     */
+    public String getStatusIcon() {
+        if (this.isDone) {
+            return "[X]";
+        } else {
+            return "[ ]";
+        }
+    }
+
+    /**
+     * Shows the string representation of a task by its status and name
+     * @return the String representing the task
+     */
     @Override
     public String toString() {
-        return this.taskName;
+        return this.getStatusIcon() + " " + this.taskName;
     }
 
 }
