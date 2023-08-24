8f639b10622b36fa74bed2f5ba3b12dcf81f2a7f
marcellaantania <marcellaantaniatan@gmail.com>
2023-08-24 17:05:07 +0800

    Update code to throw three types of thea-specific exceptions

--- src/main/java/Thea.java
+++ src/main/java/Thea.java
@@ -1,16 +1,19 @@
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
 
 public class Thea {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws WrongCommandException, EmptyDescriptionException, IndexOutOfBoundsException {
         Scanner input = new Scanner(System.in);
         ArrayList<Task> tasks =  new ArrayList<>();
         greet();
         String userInput = input.nextLine();
         while (true) {
             String[] commandWords = userInput.split(" ", 2);
+            ArrayList<String> commandWordsArray = new ArrayList<>();
+            Collections.addAll(commandWordsArray, commandWords);
             String command = commandWords[0];
             if (command.equals("bye")) {
                 exit();
@@ -19,26 +22,46 @@ public class Thea {
                 printList(tasks);
             } else if (command.equals("mark")) {
                 int index = Integer.parseInt(commandWords[1]) - 1;
-                tasks.get(index).markAsDone();
-                System.out.printf("Great job! ˊᗜˋ I've marked this task as done:\n  %s\n", tasks.get(index));
+                if (index > tasks.size() - 1) {
+                    throw new IndexOutOfBoundsException("There is currently no task " + (index + 1));
+                } else {
+                    tasks.get(index).markAsDone();
+                    System.out.printf("Great job! ˊᗜˋ I've marked this task as done:\n  %s\n", tasks.get(index));
+                }
             } else if (command.equals("unmark")) {
                 int index = Integer.parseInt(commandWords[1]) - 1;
-                tasks.get(index).unmarkAsDone();
-                System.out.printf("Okay, I've marked this task as not done yet:\n  %s\n", tasks.get(index));
+                if (index > tasks.size() - 1) {
+                    throw new IndexOutOfBoundsException("There is currently no task " + (index + 1));
+                } else {
+                    tasks.get(index).unmarkAsDone();
+                    System.out.printf("Okay, I've marked this task as not done yet:\n  %s\n", tasks.get(index));
+                }
             }
             else {
                 if (command.equals("todo")) {
-                    add(new ToDo(commandWords[1]), tasks);
-                }
-                else if (command.equals("deadline")) {
-                    String relevantData = commandWords[1];
-                    String[] nameAndTime = relevantData.split(" /by ");
-                    add(new Deadline(nameAndTime[0], nameAndTime[1]), tasks);
-                }
-                else if (command.equals("event")) {
-                    String relevantData = commandWords[1];
-                    String[] nameAndTime = relevantData.split(" /from | /to ");
-                    add(new Event(nameAndTime[0], nameAndTime[1], nameAndTime[2]), tasks);
+                    if (commandWordsArray.size() != 1) {
+                        add(new ToDo(commandWords[1]), tasks);
+                    } else {
+                        throw new EmptyDescriptionException("The description of a todo cannot be empty! '^'");
+                    }
+                } else if (command.equals("deadline")) {
+                    if (commandWordsArray.size() != 1) {
+                        String relevantData = commandWords[1];
+                        String[] nameAndTime = relevantData.split(" /by ");
+                        add(new Deadline(nameAndTime[0], nameAndTime[1]), tasks);
+                    } else {
+                        throw new EmptyDescriptionException("The description of a deadline cannot be empty! '^'");
+                    }
+                } else if (command.equals("event")) {
+                    if (commandWordsArray.size() != 1) {
+                        String relevantData = commandWords[1];
+                        String[] nameAndTime = relevantData.split(" /from | /to ");
+                        add(new Event(nameAndTime[0], nameAndTime[1], nameAndTime[2]), tasks);
+                    } else {
+                        throw new EmptyDescriptionException("The description of an event cannot be empty! '^'");
+                    }
+                } else {
+                    throw new WrongCommandException("Sorry, I don't understand what that means.. '^'");
                 }
             }
             userInput = input.nextLine();
