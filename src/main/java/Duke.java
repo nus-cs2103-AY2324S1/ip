ee580b78146058780b3919e86620fac9f6b81788
xCOLOURx <kylezcc@gmail.com>
2023-08-24 16:38:40 +0800

    Initial commit for lvl 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo =  " _   _  ___    _    ____\n" +
+                "| \\ | |/ _ \\  / \\  / ___|\n" +
+                "|  \\| | | | |/ _ \\| |\n" +
+                "| |\\  | |_| / ___ \\ |___\n" +
+                "|_| \\_|\\___/_/   \\_\\____|\n";
         System.out.println("Hello from\n" + logo);
+
+
+        String welcomeMessage = "____________________________________________________________\n" +
+                " Hello! I'm NOAC\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+
+        System.out.println(welcomeMessage);
+
     }
 }
