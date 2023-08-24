d31476098c3311a68a104598df4b2655101899f8
spatuly <a0259629l@gmail.com>
2023-08-24 16:58:03 +0800

    Level-0 v2 New Name

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,6 @@
 public class Duke {
     public static void main(String[] args) {
-        String greet = "Hello! I'm Duke\n" + "What can I do for you?\n";
+        String greet = "Hello! I'm Uke\n" + "What can I do for you?\n";
         String exit = "Bye. Hope to see you again soon!\n";
         System.out.println(greet + exit);
     }
