4062a72fe3fbe0d6d6cb9f37b8964b77171ab2b8
peasantbird <bertrand.wong.xh@gmail.com>
2023-08-24 17:01:50 +0800

    Change Duke.java to Bert.java

---
+++ src/main/java/Bert.java
@@ -0,0 +1,20 @@
+/**
+ * A chatbot named Bert
+ */
+public class Bert {
+    /**
+     * botName stores the name of the chatbot
+     */
+    private static final String botName = "Bert";
+
+    public static void main(String[] args) {
+        System.out.println(
+                "____________________________________________________________\n" +
+                "Hello! I'm " + Bert.botName + "\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n"
+        );
+    }
+}
