b3f279a2470113e0796618b1d47471a787525da0
NicholasTng010601 <nicholastng@hotmail.com>
2023-08-24 20:22:46 +0800

    Accidentally pushed level 7 to main branch. Created level 7 branch and added descriptions to storage class

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -9,6 +9,12 @@ public class Storage {
         this.filepath = filepath;
     }
 
+    /**
+     * Loads task list closed when bot was last used
+     *
+     * @return TaskList
+     * @throws IOException
+     */
     public ArrayList load() throws IOException {
         try {
             FileInputStream file = new FileInputStream(filepath);
@@ -22,6 +28,14 @@ public class Storage {
         }
     }
 
+    /**
+     * Saves task list into hard disk
+     *
+     * @param taskList
+     * Takes in Task list from current instance and save it into the hard disk
+     *
+     * @throws IOException
+     */
     public void save(ArrayList taskList) throws IOException{
         FileOutputStream file = new FileOutputStream(filepath);
         ObjectOutputStream output = new ObjectOutputStream(file);
