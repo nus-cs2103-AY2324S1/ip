6b3811963c7e480f237f2888c1b3085e54282a36
Tim-Siu <e1088140@u.nus.edu>
2023-08-24 11:50:54 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
     private static final String name = "Kevin";
     private static final int splitterLength = 50;
+
     private static void lineSplitter() {
         for (int i = 0; i < Duke.splitterLength; i++) {
             System.out.print("-");
@@ -18,8 +21,24 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!\n");
         lineSplitter();
     }
+
+    public static void echo() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+            lineSplitter();
+            System.out.println(input + "\n");
+            lineSplitter();
+        }
+        sc.close();
+    }
+
     public static void main(String[] args) {
         greet();
+        echo();
         bye();
     }
 }
