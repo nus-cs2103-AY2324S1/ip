3b5efb6752040054ee3c5d73551fe5258304d1d5
ph-nathan <px5099@gmail.com>
2023-08-24 11:05:04 +0800

    Add logic for marking and unmarking of tasks for Level 3: Mark as done

--- src/main/java/Pippi.java
+++ src/main/java/Pippi.java
@@ -2,49 +2,67 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Pippi {
-
-    public static void wrapText(String content) {
+    private String pippi =  "⣿⡿⠷⣶⣤⣄⡀⠀⠀⠀⢀⣤⣶⣿⣿⣿⣿⣶⣤⡀⠀⠀⠀⢀⣠⣤⣶⠾⢿⣿\n" +
+            "⣿⡇⠀⠀⣨⣿⣿⣷⠀⢠⣿⣿⠿⠋⢉⡉⠙⠿⣿⣿⣄⣤⣾⣿⣿⣅⠀⠀⢸⣿\n" +
+            "⢸⣧⠀⣰⣿⣿⣿⡏⢀⣿⣿⡏⢠⣾⣿⣿⣷⡄⢹⣿⣿⣿⣿⣿⣿⣿⣆⠀⣼⡇\n" +
+            "⠈⣿⣴⣿⣿⣿⣿⡇⠈⣿⣿⣧⣾⣿⣿⣿⣿⡇⢸⣿⣿⣿⣿⣿⣿⣿⣿⣦⣿⠁\n" +
+            "⠀⢹⣿⣿⣿⣿⣿⣿⣄⠙⢿⣿⣿⣿⣿⣿⠏⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀\n" +
+            "⠀⠀⢻⣿⣿⣿⣿⣿⣿⣶⣄⣈⠉⠉⣉⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀\n" +
+            "⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⡏⢹⣿⣿⣿⣿⡏⢹⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀\n" +
+            "⣴⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣧⣼⣿⣿⣿⣿⣧⣼⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣦\n" +
+            "⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⡉⠛⠛⢉⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿\n" +
+            "⠈⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠁\n" +
+            "⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⠿⠿⠿⠿⠿⠿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀";
+    private boolean inPokeball = false;
+    public void wrapText(String content) {
         String line = "_____________________________________\n";
-        System.out.println(line);
-        System.out.println(content);
-        System.out.println(line);
+        System.out.println(line + content + "\n" + line);
+    }
+    public void reply(String userMessage, ArrayList<Task> tasks) {
+        String[] input = userMessage.split(" ", 2);
+        String command = input[0];
+        if (command.equalsIgnoreCase("bye")) {
+            wrapText("Bye. Hope to see you again soon!");
+            this.inPokeball = true;
+        } else if (command.equalsIgnoreCase("list")) {
+            String all = "";
+            for (int i = 0; i < tasks.size(); i++) {
+                Task curr = tasks.get(i);
+                all = all + String.valueOf(i + 1) +"." + curr.toString() + "\n";
+            }
+            wrapText(all);
+        } else if (command.equalsIgnoreCase("mark")) {
+            int idx = Integer.parseInt(input[1]) - 1;
+            tasks.get(idx).mark();
+            wrapText("Nice I've marked this task as done: \n" +
+                    tasks.get(idx).toString());
+        } else if (command.equalsIgnoreCase("unmark")) {
+            int idx = Integer.parseInt(input[1]) - 1;
+            tasks.get(idx).unmark();
+            wrapText("OK, I've marked this task as not done yet: \n" +
+                    tasks.get(idx).toString());
+        } else {
+            tasks.add(new Task(userMessage));
+            wrapText("added " + userMessage);
+        }
+    }
+    public void start() {
+        wrapText(pippi + "\nHello trainer, I'm Pippi!\nWhat can I do for you?");
+        ArrayList<Task> tasks = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (!this.inPokeball) {
+            String userMessage = scanner.nextLine();
+            String[] input = userMessage.split(" ", 2);
+            reply(userMessage, tasks);
+        }
     }
     public static void main(String[] args) {
-        boolean inPokeball = false;
-        ArrayList<String> tasks = new ArrayList<String>();
-        String pippi =  "⣿⡿⠷⣶⣤⣄⡀⠀⠀⠀⢀⣤⣶⣿⣿⣿⣿⣶⣤⡀⠀⠀⠀⢀⣠⣤⣶⠾⢿⣿\n" +
-                        "⣿⡇⠀⠀⣨⣿⣿⣷⠀⢠⣿⣿⠿⠋⢉⡉⠙⠿⣿⣿⣄⣤⣾⣿⣿⣅⠀⠀⢸⣿\n" +
-                        "⢸⣧⠀⣰⣿⣿⣿⡏⢀⣿⣿⡏⢠⣾⣿⣿⣷⡄⢹⣿⣿⣿⣿⣿⣿⣿⣆⠀⣼⡇\n" +
-                        "⠈⣿⣴⣿⣿⣿⣿⡇⠈⣿⣿⣧⣾⣿⣿⣿⣿⡇⢸⣿⣿⣿⣿⣿⣿⣿⣿⣦⣿⠁\n" +
-                        "⠀⢹⣿⣿⣿⣿⣿⣿⣄⠙⢿⣿⣿⣿⣿⣿⠏⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀\n" +
-                        "⠀⠀⢻⣿⣿⣿⣿⣿⣿⣶⣄⣈⠉⠉⣉⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀\n" +
-                        "⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⡏⢹⣿⣿⣿⣿⡏⢹⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀\n" +
-                        "⣴⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣧⣼⣿⣿⣿⣿⣧⣼⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣦\n" +
-                        "⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⡉⠛⠛⢉⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿\n" +
-                        "⠈⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠁\n" +
-                        "⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⠿⠿⠿⠿⠿⠿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀\n" +
-                        "⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀";
-        Scanner scanner = new Scanner(System.in);
-        wrapText(pippi + "\nHello trainer, I'm Pippi!\nWhat can I do for you?");
-        while (!inPokeball) {
-            String userMessage = scanner.nextLine();
-            if (userMessage.equalsIgnoreCase("bye")) {
-                wrapText("Bye. Hope to see you again soon!");
-                inPokeball = true;
-            } else if (userMessage.equalsIgnoreCase("list")) {
-                String all = "";
-                for (int i = 0; i < tasks.size(); i++) {
-                    all = all + String.valueOf(i + 1) +". " + tasks.get(i) + "\n";
-                }
-                wrapText(all);
-
-            } else {
-                tasks.add(userMessage);
-                wrapText("added " + userMessage);
-            }
-        }
+        Pippi pippi = new Pippi();
+        pippi.start();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "[X] " : "[ ] ");
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return this.getStatusIcon() + this.getDescription();
+    }
+}
