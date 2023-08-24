d2abaaf350a945b17ba5ef188524752e49ca2fe1
ZENN\User <miljy123@gmail.com>
2023-08-24 19:55:46 +0800

    Add ability to mark tasks as done and not done

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -3,16 +3,17 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Buddy {
-    private static String name = "Buddy";
+    private static String name = "To Do Buddy";
 
     public static void main(String[] args) {
         String greeting = String.format("Hello! I'm %s\n", name);
-        String inquiry = "What can I do for you?\n";
+        String inquiry = "What would you like to do?\n";
         String exit = "Bye. Hope to see you again soon!\n";
 
         Scanner scanner = new Scanner(System.in);
-        List<String> list = new ArrayList();
         String command;
+        Task t;
+        List<Task> taskList = new ArrayList();
 
         System.out.println(greeting + inquiry);
 
@@ -23,16 +24,36 @@ public class Buddy {
                 break;
             }
             if (command.equals("list")) {
-                printText(list);
+                printList(taskList);
+            } else if (command.startsWith("mark") || command.startsWith("unmark")) {
+                String[] arrOfCmd = command.split(" ");
+                Integer taskNumber = Integer.valueOf(arrOfCmd[1]) - 1;
+                if (taskNumber < 0 || taskNumber > taskList.size()) {
+                    System.out.println("Invalid task number.");
+                } else {
+                    Task thisTask = taskList.get(taskNumber);
+                    if (command.startsWith("mark")) {
+                        thisTask.markTaskAsDone();
+                        System.out.println("Nice! I've marked this task as done:");
+                    }
+                    if (command.startsWith("unmark")) {
+                        thisTask.markTaskAsUndone();
+                        System.out.println("OK, I've marked this task as not done yet:");
+                    }
+                    System.out.println("  [" + thisTask.getStatusIcon() + "]" + thisTask.toString());
+                }
             } else {
-                list.add(command);
+                t = new Task(command);
+                taskList.add(t);
                 System.out.println("added: " + command);
             }
         }
     }
-    private static void printText(List<String> list) {
+    private static void printList(List<Task> list) {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < list.size(); i++) {
-            System.out.println((i + 1) + ". " + list.get(i));
+            Task task = list.get(i);
+            System.out.println((i + 1) + ".[" + task.getStatusIcon() + "] " + task);
         }
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -18,4 +18,9 @@ public class Task {
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
 }
