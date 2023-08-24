1b477e50bee2570b76826c73f99542c3e06ad7df
LHeng1 <limhengyi1@gmail.com>
2023-08-24 20:18:49 +0800

    Level-2-Add-List

--- src/main/java/Avalon.java
+++ src/main/java/Avalon.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 public class Avalon {
     public static void main(String[] args) {
+        String[] tasks = new String[100];
+        int taskCount = 0;
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(
                 "   ____________________________________________________________\n" +
                 "    Hello! I'm Arthur Pendragon.\n" +
@@ -8,23 +12,36 @@ public class Avalon {
                 "   ____________________________________________________________\n"
         );
 
-        Scanner scanner = new Scanner(System.in);
-        
         while (true){
             String userInput = scanner.nextLine();
-            if (userInput.equalsIgnoreCase("bye")){
+
+            if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println(
                         "   ____________________________________________________________\n" +
                                 "    Farewell. We will meet again!\n" +
                                 "   ____________________________________________________________"
                 );
                 break;
-            }
-
-            else {
+            } else if (userInput.equalsIgnoreCase("list")) {
+                if (taskCount == 0) {
+                    System.out.println(
+                            "   ____________________________________________________________\n" +
+                                    "    You haven't added anything, my sire.\n" +
+                                    "   ____________________________________________________________"
+                    );
+                } else {
+                    System.out.println("   ____________________________________________________________list");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println("    " + (i + 1) + ". " + tasks[i]);
+                    }
+                    System.out.println("   ____________________________________________________________");
+                }
+            } else {
+                tasks[taskCount] = userInput;
+                taskCount++;
                 System.out.println(
                         "   ____________________________________________________________\n" +
-                                "    " +
+                                "    added: " +
                                 userInput +
                                 "\n   ____________________________________________________________\n"
                 );
