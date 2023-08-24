f7738060af533ca23750a02ec2a63d4e33482b5c
oeggy03 <oeggy03>
2023-08-24 17:25:18 +0800

    Finished Level-0 Renamed the bot Implement an initial skeletal version of the it that simply greets the user and exits.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String Level0 = "____________________________________________________________\n" +
+                " Hello! I'm eggbot\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(Level0);
     }
 }
