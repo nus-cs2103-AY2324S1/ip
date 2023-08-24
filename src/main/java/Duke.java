914792eeae1b17a23f7a20206b3c1d4c0448be9e
eyelessrhyme7 <eyelessrhyme7@gmail.com>
2023-08-24 19:18:29 +0800

    Completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         /*
@@ -8,14 +10,27 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo + "Raman");*/
 
-        String greeting = "Hello, I'm Capt. Price! \nWhat can I do for you today, Sergeant?";
+        String greeting = "Hello, I'm Capt. Price! \n    What can I do for you today, Sergeant?";
         String horizontalBar = "-------------------------------------------------";
         String exitGreeting = "Over and out. See you next mission!";
 
-        System.out.println(horizontalBar);
-        System.out.println(greeting);
-        System.out.println(horizontalBar);
-        System.out.println(exitGreeting);
-        System.out.println(horizontalBar);
+        System.out.println(botMessage(greeting));
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+
+        while (!userInput.equalsIgnoreCase("bye")) {
+            System.out.println(botMessage(userInput));
+            userInput = scanner.nextLine();
+        }
+
+        System.out.println(botMessage(exitGreeting));
+
+    }
+
+    public static String botMessage(String message) {
+        String space = "    ";
+        String horizontalBar = "-------------------------------------------------";
+        return space + horizontalBar + "\n" + space + message + "\n" + space + horizontalBar;
     }
 }
