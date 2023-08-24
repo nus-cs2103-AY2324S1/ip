f76a6df8a04313ac96af33daf615695089dbcbbc
ZENN\User <miljy123@gmail.com>
2023-08-24 19:04:17 +0800

    Create Task.java class

---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markTaskAsDone() {
+        this.isDone = true;
+    }
+
+    public void markTaskAsUndone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+}
