97418aff406ee88e2b4664127b510aec59609ac6
Rubesh S <rubesh77@gmail.com>
2023-08-24 19:34:08 +0800

    Implement an initial skeletal version of the ChatBot. Duke Level-0, first commit

---
+++ src/main/java/ChatterBox.java
@@ -0,0 +1,18 @@
+
+public class ChatterBox {
+
+    public static void main(String[] args) {
+
+        linePrinter();
+        System.out.println("      Hello! I'm ChatterBox");
+        System.out.println("      What can I do for you?");
+        linePrinter();
+        System.out.println("      Bye. Hope to see you again soon!");
+        linePrinter();
+    }
+
+    private static void linePrinter() {
+        System.out.println("      ___________________________________________________________");
+    }
+
+}
