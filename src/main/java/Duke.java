c26bb3b4277a7bcd7875c784b51cf1d58074cc1e
Adammangzijun <117640074+adammangzijun@users.noreply.github.com>
2023-08-24 16:49:44 +0800

    Feature 0: added the base message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String space = "------------------------------------"; // for spacing purposes
+        String name = "Adam's Bot"; // name of bot
+
+        System.out.println(space);
+        System.out.println("Hello! I'm "+ name);
+        System.out.println("What can I do for you?");
+        System.out.println(space);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(space);
     }
 }
