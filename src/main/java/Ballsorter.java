cc9cb5d412bdc744872a0e7460bbd946f9112422
fuyiqiao <yiqiao_fu@hotmail.com>
2023-08-24 13:11:36 +0800

    A-Classes

--- src/main/java/Ballsorter.java
+++ src/main/java/Ballsorter.java
@@ -1,15 +1,12 @@
 import java.util.Scanner;
 
 public class Ballsorter {
+
     public static void main(String[] args) {
 
         String line = "____________________________________________________________";
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int numberOfTasks = 0;
-        String[] status = new String[100];
-        for (int i = 0; i < 100; i++) {
-            status[i] = " ";
-        }
 
         System.out.println(line);
         System.out.println("Hello! I'm Ballsorter\nWhat can I do for you?");
@@ -28,31 +25,27 @@ public class Ballsorter {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < numberOfTasks; i++) {
                     int temp = i + 1;
-                    System.out.println(temp + ". [" + status[i] + "] " + tasks[i]);
+                    System.out.println(temp + ". " + tasks[i].toString());
                 }
                 System.out.println(line);
 
             } else if (input.startsWith("mark")) {
 
                 int target = Integer.parseInt(input.substring(5)) - 1;
-                status[target] = "X";
-
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  [X] " + tasks[target]);
+                System.out.println(tasks[target].markDone());
                 System.out.println(line);
 
             } else if (input.startsWith("unmark")) {
 
                 int target = Integer.parseInt(input.substring(7)) - 1;
-                status[target] = " ";
-
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  [ ] " + tasks[target]);
+                System.out.println(tasks[target].markNotDone());
                 System.out.println(line);
 
             } else {
 
-                tasks[numberOfTasks] = input;
+                tasks[numberOfTasks] = new Task(input);
                 numberOfTasks++;
 
                 System.out.println("added: " + input);
