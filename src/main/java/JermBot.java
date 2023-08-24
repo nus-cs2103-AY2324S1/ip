56cb8b8d76779172e10520b5ea8e37e3bf0e25cb
Jeremy <jeremyyong0101@gmail.com>
2023-08-24 10:10:52 +0800

    Increment: Level-0

---
+++ src/main/java/JermBot.java
@@ -0,0 +1,7 @@
+public class JermBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm JermBot");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
