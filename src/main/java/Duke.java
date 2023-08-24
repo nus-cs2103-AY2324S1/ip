c8c189c6b0096bfa3f731847632216ccbff5bf26
Catherine Liang <catherineliang1101@gmail.com>
2023-08-24 13:55:12 +0800

    Added Level-2 changes: user inputs will now added to a list, inputting list will print out the list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Duke {
     private static final String DIVIDER = "      ____________________________________________________________";
@@ -11,6 +13,7 @@ public class Duke {
 
         // Check user input
         Scanner sc = new Scanner(System.in);
+        List<String> userList = new ArrayList<>();
 
         while (true) {
             String userInput = sc.nextLine();
@@ -22,8 +25,20 @@ public class Duke {
                 System.out.println(DIVIDER);
                 break;
 
+            } else if (userInput.equals("list")) {
+                // If user wants to check list
+                System.out.println(DIVIDER);
+                System.out.println("        Sure! Here are the tasks in your list:");
+                for (int i = 0; i < userList.size(); i++) {
+                    String str = String.format("            %d. %s", i + 1, userList.get(i));
+                    System.out.println(str);
+                }
+                System.out.println(DIVIDER);
+
             } else {
                 // Other inputs
+                userList.add(userInput);
+
                 System.out.println(DIVIDER);
                 System.out.println("        Added: " + userInput);
                 System.out.println(DIVIDER);
