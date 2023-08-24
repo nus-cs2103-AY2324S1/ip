a19886b2d0f0db9c504044d4308dd9da93a91419
Richard Hew <richardhewjialiang9@gmail.com>
2023-08-24 15:19:11 +0800

    Added the ability to mark tasks as done

--- src/main/java/Richie.java
+++ src/main/java/Richie.java
@@ -2,15 +2,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class Richie {
     static private String CHATBOT_NAME = "Richie";
-    static private ArrayList<String> itemArray = new ArrayList<>();
-    public static void addItem(String item) {
+    static private ArrayList<Task> itemArray = new ArrayList<>();
+    public static void addItem(Task item) {
         itemArray.add(item);
     }
     public static String listItems() {
         int length = itemArray.size();
         String result = "";
         for (int i = 0; i < length; i++) {
-            result += (i + 1) + "." + itemArray.get(i) + "\n";
+            result += (i + 1) + "." + itemArray.get(i).toString() + "\n";
         }
         return result;
     }
@@ -19,12 +19,21 @@ public class Richie {
         Scanner input = new Scanner(System.in);
         String message = input.nextLine();
         while (!message.equals("bye")) {
+            String[] stringArray = message.split(" ", 2);
             if (message.equals("list")) {
                 System.out.println(listItems());
                 message = input.nextLine();
                 continue;
+            } else if (stringArray[0].equals("mark")) {
+                int taskIndex = Integer.parseInt(stringArray[1]);
+                Task task = itemArray.get(taskIndex - 1);
+                task.markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n " + task);
+                message = input.nextLine();
+                continue;
             }
-            addItem(message);
+            Task task = new Task(message);
+            addItem(task);
             System.out.println("added : " + message);
             message = input.nextLine();
         }
