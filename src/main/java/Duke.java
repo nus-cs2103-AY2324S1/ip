ec79d492718e646acc4fdd640401230a0df3e0b0
eyelessrhyme7 <eyelessrhyme7@gmail.com>
2023-08-24 17:21:05 +0800

    Completed Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo + "Raman");*/
+
+        String greeting = "Hello, I'm Capt. Price! \nWhat can I do for you today, Sergeant?";
+        String horizontalBar = "-------------------------------------------------";
+        String exitGreeting = "Over and out. See you next mission!";
+
+        System.out.println(horizontalBar);
+        System.out.println(greeting);
+        System.out.println(horizontalBar);
+        System.out.println(exitGreeting);
+        System.out.println(horizontalBar);
     }
 }
