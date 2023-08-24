7ff37d08406c0a99195331a4d853bf95e745c133
andytoh1 <e0773488@u.nus.edu>
2023-08-24 09:14:25 +0800

    no message

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -24,27 +24,50 @@ public class Chatbot {
                 }
                 continue;
             }
-            if (userMessage.length() >= 6) {
+            try {
                 if (userMessage.substring(0, 4).equalsIgnoreCase("mark")) {
-                    int doneTask = Integer.parseInt(userMessage.substring(5));
-                    System.out.println("Well done! This task has been marked as done.");
-                    System.out.println("[" + messageType[doneTask] + "]" + "[X] " + messageList[doneTask - 1]);
-                    messageDone[doneTask - 1] = true;
-                    continue;
+                    try {
+                        int doneTask = Integer.parseInt(userMessage.substring(5));
+                        System.out.println("Well done! This task has been marked as done.");
+                        System.out.println("[" + messageType[doneTask] + "]" + "[X] " + messageList[doneTask - 1]);
+                        messageDone[doneTask - 1] = true;
+                        continue;
+                    } catch (Exception e) {
+                        System.out.println("Please use the format \"mark <task number>\"!");
+                        continue;
+                    }
                 } else if (userMessage.substring(0, 4).equalsIgnoreCase("todo")) {
-                    userMessage = userMessage.substring(5);
-                    messageType[messageCount] = 'T';
+                    try {
+                        userMessage = userMessage.substring(5);
+                        messageType[messageCount] = 'T';
+                    } catch (Exception e) {
+                        System.out.println("Please use the format \"todo <task description>\"!");
+                        continue;
+                    }
                 } else if (userMessage.substring(0, 8).equalsIgnoreCase("deadline")) {
-                    int index = userMessage.indexOf("/by");
-                    userMessage = userMessage.substring(9, index) + "(by: " + userMessage.substring(index + 4) + ")";
-                    messageType[messageCount] = 'D';
+                    try {
+                        int index = userMessage.indexOf("/by");
+                        userMessage = userMessage.substring(9, index) + "(by: " + userMessage.substring(index + 4) + ")";
+                        messageType[messageCount] = 'D';
+                    } catch (Exception e) {
+                        System.out.println("Please use the format \"deadline <task description> /by <time>\"!");
+                        continue;
+                    }
                 } else if (userMessage.substring(0, 5).equalsIgnoreCase("event")) {
-                    int index = userMessage.indexOf("/from");
-                    int index2 = userMessage.indexOf("/to");
-                    userMessage = userMessage.substring(6, index) + "(from: " + userMessage.substring(index + 6, index2)
-                        + "to: " + userMessage.substring(index2 + 4) + ")";
-                    messageType[messageCount] = 'E';
+                    try {
+                        int index = userMessage.indexOf("/from");
+                        int index2 = userMessage.indexOf("/to");
+                        userMessage = userMessage.substring(6, index) + "(from: " + userMessage.substring(index + 6, index2)
+                                + "to: " + userMessage.substring(index2 + 4) + ")";
+                        messageType[messageCount] = 'E';
+                    } catch (Exception e) {
+                        System.out.println("Please use the format \"event <task description> /from <time> /to <time>\"!");
+                        continue;
+                    }
                 }
+            } catch (Exception e) {
+                System.out.println("I'm sorry, but I don't know what that means!");
+                continue;
             }
             System.out.println("Added this task: " + "[" + messageType[messageCount] + "] " + userMessage);
             messageList[messageCount] = userMessage;
