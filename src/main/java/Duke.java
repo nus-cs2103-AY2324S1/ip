46c18dd8ea3a12f96b64e3dd2e4699078e8fff5c
jovkusuma <jovankusuma1@gmail.com>
2023-08-24 15:20:07 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String farewell = " Bye. Hope to see you again soon!";
+        String bye = " Bye. Hope to see you again soon!";
         String message = "____________________________________________________________\n"
-                + " Hello! I'm [YOUR CHATBOT NAME]\n"
+                + " Hello! I'm ChatBot\n"
                 + " What can I do for you?\n"
                 + "____________________________________________________________\n";
+        String[] tasksList = new String[100];
+        int count = 0;
 
         System.out.println(message);
 
@@ -14,14 +16,28 @@ public class Duke {
         while(true) {
             String input = scanner.nextLine();
             System.out.println("----------------------------------------------");
-            System.out.println(input);
-            System.out.println("----------------------------------------------");
-
             if(input.equalsIgnoreCase("bye")) {
                 System.out.println("----------------------------------------------");
-                System.out.println(farewell);
+                System.out.println(bye);
                 System.out.println("----------------------------------------------");
                 break;
+
+            } else if(input.equalsIgnoreCase("list")) {
+                System.out.println("----------------------------------------------");
+                for(int i = 0; i < count; i++) {
+                    if(count == 0) {
+                        System.out.println("You currently have no tasks!");
+                    } else {
+                        System.out.println((i + 1) + ". " + tasksList[i]);
+                    }
+                }
+                System.out.println("----------------------------------------------");
+
+            } else {
+                System.out.println("\t added: " + input);
+                tasksList[count] = input;
+                count++;
+                System.out.println("----------------------------------------------");
             }
         }
     }
