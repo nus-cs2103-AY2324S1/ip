226856f6e92deb714104f7ece4af35bc310431b8
hcs1203 <solomonrajhannah@gmail.com>
2023-08-24 18:01:30 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,9 +2,33 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String tasks[] = new String[100];
+        class Task {
+            protected String name;
+            protected boolean isDone = false;
 
+            public Task(String name) {
+                this.name = name;
+            }
+
+            public void mark(){
+                isDone = true;
+                System.out.println("Nice! I've marked this task as done:\n [X] " + name +"\n");
+                System.out.println("____________________________________________________________\n");
+            }
+
+            public void unmark(){
+                isDone = false;
+                System.out.println("OK, I've marked this task as not done yet:\n [ ] " + name + "\n");
+                System.out.println("____________________________________________________________\n");
+            }
+
+            public String printTask() {
+                return (isDone) ? "[X] " + name + "\n": "[ ] " + name + "\n";
+            }
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        Task tasks[] = new Task[100];
 
         System.out.println("____________________________________________________________\n" +
                 "Hello! I'm CarrotCake\n" +
@@ -19,17 +43,32 @@ public class Duke {
             if (input.toLowerCase().equals("list")) {
                 //Print tasks
                 for (int i = 0; i < count; i++) {
-                    System.out.print(Integer.toString(i+1) + ". " + tasks[i] + "\n");
+                    System.out.print(Integer.toString(i+1) + ". " + tasks[i].printTask());
                 }
                 System.out.println("____________________________________________________________\n");
                 input = scanner.nextLine();
                 continue;
             }
+            if (input.toLowerCase().startsWith("mark ")) {
+                String[] parts = input.split(" ");
+                int taskNumber = Integer.parseInt(parts[1]) - 1;
+                tasks[taskNumber].mark();
+                input = scanner.nextLine();
+                continue;
+            }
+            if (input.toLowerCase().startsWith("unmark ")) {
+                String[] parts = input.split(" ");
+                int taskNumber = Integer.parseInt(parts[1]) - 1;
+                tasks[taskNumber].unmark();
+                input = scanner.nextLine();
+                continue;
+            }
+
 
             System.out.println("added: " + input +
                     "\n____________________________________________________________\n");
 
-            tasks[count] = input;
+            tasks[count] = new Task(input);
             input = scanner.nextLine();
             count++;
         }
