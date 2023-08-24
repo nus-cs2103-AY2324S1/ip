1a627d36771499e66e649ca2fc7f0883cb86a84b
NgChunMan <chunman03@gmail.com>
2023-08-24 19:39:13 +0800

    Add: Level 6 (Delete)

--- src/main/java/Evo.java
+++ src/main/java/Evo.java
@@ -1,12 +1,10 @@
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Evo is a Personal Assistant Chatbot that helps a person to keep track of various things.
  * Tasks are represented by the nested static class Task.
  */
 public class Evo {
-
     /**
      * The main method of Evo program.
      *
@@ -31,10 +29,8 @@ public class Evo {
         // Initialise a scanner to receive text input from user
         Scanner scanner = new Scanner(System.in);
 
-        // taskIndex use to keep track the index number of the first empty slot in taskList
-        int taskIndex = 0;
-        // An array to store the Task objects
-        Task[] taskList = new Task[100];
+        // An ArrayList to store the Task objects
+        ArrayList<Task> taskList = new ArrayList<>();
 
         while (true) {
             try {
@@ -52,22 +48,27 @@ public class Evo {
                  * Then exit the current while loop and move to the next iteration.
                  */
                 if (Objects.equals(instruction, "list")) {
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < taskIndex; i++) {
-                        if (i == taskIndex - 1) {
-                            System.out.println(i + 1 + "." + taskList[i].toString() + "\n");
-                        } else {
-                            System.out.println(i + 1 + "." + taskList[i].toString());
+                    if (taskList.isEmpty()) {
+                        System.out.println("Here are the tasks in your list:\n");
+                        continue;
+                    } else {
+                        System.out.println("Here are the tasks in your list:");
+                        for (int i = 0; i < taskList.size(); i++) {
+                            if (i == taskList.size() - 1) {
+                                System.out.println(i + 1 + "." + taskList.get(i).toString() + "\n");
+                            } else {
+                                System.out.println(i + 1 + "." + taskList.get(i).toString());
+                            }
                         }
+                        continue;
                     }
-                    continue;
                 }
 
                 /**
                  * Split the text entered by user by "/" to differentiate the ToDo task with Deadline and Event object.
                  * If the text entered does not contain "/", then split the text entered by user by space and store it
                  * in a string array called actionType. Then, determine which action to be taken, whether is to mark a
-                 * task done, mark a task not done or add a ToDo task to the taskList.
+                 * task done, mark a task not done, add a ToDo task to the taskList or delete a task from the taskList.
                  * If the text entered contain "/", then split the text entered by user by "/" and store it in in a
                  * string array called typeAndDates. Then, determine which action to be taken, whether is to add a
                  * deadline task to the taskList or add an event task to the taskList.
@@ -80,20 +81,36 @@ public class Evo {
                         }
                         // Mark a task as done
                         int taskNumberInList = Integer.parseInt(actionType[1]) - 1;
-                        taskList[taskNumberInList].markAsDone();
+                        taskList.get(taskNumberInList).markAsDone();
 
                         System.out.println("Nice! I've marked this task as done:");
-                        System.out.println("  " + taskList[taskNumberInList].toString() + "\n");
+                        System.out.println("  " + taskList.get(taskNumberInList).toString() + "\n");
                     } else if (Objects.equals(actionType[0], "unmark")) {
                         if (actionType.length == 1) {
                             throw new MissingTaskToUnmarkException();
                         }
                         // Mark a task as not done
                         int taskNumberInList = Integer.parseInt(actionType[1]) - 1;
-                        taskList[taskNumberInList].markAsNotDone();
+                        taskList.get(taskNumberInList).markAsNotDone();
 
                         System.out.println("OK, I've marked this task as not done yet:");
-                        System.out.println("  " + taskList[taskNumberInList].toString() + "\n");
+                        System.out.println("  " + taskList.get(taskNumberInList).toString() + "\n");
+                    } else if (Objects.equals(actionType[0], "delete")) {
+                        if (taskList.isEmpty()) {
+                            throw new NoTaskException();
+                        }
+                        // Delete a task from taskList
+                        int taskNumberToDelete = Integer.parseInt(actionType[1]) - 1;
+                        Task deletedTask = taskList.get(taskNumberToDelete);
+                        taskList.remove(taskNumberToDelete);
+
+                        System.out.println("Noted. I've removed this task:");
+                        System.out.println("  " + deletedTask.toString());
+                        if (taskList.size() <= 1) {
+                            System.out.println("Now you have " + taskList.size() + " task in the list.\n");
+                        } else {
+                            System.out.println("Now you have " + taskList.size() + " tasks in the list.\n");
+                        }
                     } else if (Objects.equals(actionType[0], "todo")) {
                         // Add ToDo object
                         if (actionType.length == 1) {
@@ -109,12 +126,15 @@ public class Evo {
                         }
 
                         ToDo toDo = new ToDo(taskDescription);
-                        taskList[taskIndex] = toDo;
-                        taskIndex++;
+                        taskList.add(toDo);
 
                         System.out.println("Got it. I've added this task:");
                         System.out.println("  " + toDo.toString());
-                        System.out.println("Now you have " + taskIndex + " tasks in the list.\n");
+                        if (taskList.size() <= 1) {
+                            System.out.println("Now you have " + taskList.size() + " task in the list.\n");
+                        } else {
+                            System.out.println("Now you have " + taskList.size() + " tasks in the list.\n");
+                        }
                     } else if (Objects.equals(actionType[0], "deadline")) {
                         if (actionType.length == 1) {
                             throw new MissingDescriptionAndDeadlineException();
@@ -155,12 +175,15 @@ public class Evo {
                         }
                         // Create the deadline object with the taskDescription and taskBy
                         Deadline deadline = new Deadline(taskDescription, taskBy);
-                        taskList[taskIndex] = deadline;
-                        taskIndex++;
+                        taskList.add(deadline);
 
                         System.out.println("Got it. I've added this task:");
                         System.out.println("  " + deadline.toString());
-                        System.out.println("Now you have " + taskIndex + " tasks in the list.\n");
+                        if (taskList.size() <= 1) {
+                            System.out.println("Now you have " + taskList.size() + " task in the list.\n");
+                        } else {
+                            System.out.println("Now you have " + taskList.size() + " tasks in the list.\n");
+                        }
                     } else if (Objects.equals(actionType[0], "event")) {
                         // Add Event object to the taskList
                         String[] datesFrom = typeAndDates[1].split(" ");
@@ -188,12 +211,15 @@ public class Evo {
                         }
                         // Create the event object with the taskDescription and taskDuration
                         Event event = new Event(taskDescription, taskDuration);
-                        taskList[taskIndex] = event;
-                        taskIndex++;
+                        taskList.add(event);
 
                         System.out.println("Got it. I've added this task:");
                         System.out.println("  " + event.toString());
-                        System.out.println("Now you have " + taskIndex + " tasks in the list.\n");
+                        if (taskList.size() <= 1) {
+                            System.out.println("Now you have " + taskList.size() + " task in the list.\n");
+                        } else {
+                            System.out.println("Now you have " + taskList.size() + " tasks in the list.\n");
+                        }
                     }
                 }
             } catch (InvalidOperationException invalidOpExp) {
@@ -210,20 +236,23 @@ public class Evo {
                 // Catch the exception when user never specifies which task to be unmarked
                 System.out.println("Please specify the task to be unmarked.\n");
             } catch (MissingDescriptionAndDeadlineException missingDescAndDeadlineExp) {
-                // Catch the exception when the description and deadline of Deadline object are missing.
+                // Catch the exception when the description and deadline of Deadline object are missing
                 System.out.println("Description and deadline of this task are missing. " +
                         "Please specify the description and the deadline of this task.\n");
             } catch (MissingDescriptionAndDurationException missingDescAndDurationExp) {
-                // Catch the exception when the description and duration of Event object are missing.
+                // Catch the exception when the description and duration of Event object are missing
                 System.out.println("Description and duration of this event are missing. " +
                         "Please specify the description and the duration of this event.\n");
             } catch (MissingDeadlineException missingDeadlineException) {
-                // Catch the exception when the deadline of Deadline object is missing.
+                // Catch the exception when the deadline of Deadline object is missing
                 System.out.println("Deadline of this task is missing. Please specify the deadline of this task.\n");
             } catch (MissingDurationException missingDurationException) {
-                // Catch the exception when the duration of Event object is missing.
+                // Catch the exception when the duration of Event object is missing
                 System.out.println("Duration of this event is missing. " +
                         "Please specify the start date/time and end date/time.\n");
+            } catch (NoTaskException noTaskException) {
+                // Catch the exception when user tries to delete task from an empty taskList
+                System.out.println("This task cannot be deleted as there is no task in the list.\n");
             }
         }
     }
---
+++ src/main/java/NoTaskException.java
@@ -0,0 +1,13 @@
+/**
+ * This exception is thrown when user tries to delete a task from an empty taskList.
+ */
+public class NoTaskException extends Exception {
+
+    /**
+     * Constructs a NoTaskException object.
+     */
+    public NoTaskException() {
+        super();
+    }
+
+}
