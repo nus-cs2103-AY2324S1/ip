69a98b60931b86299b55c90c78f376aac0e515ee
lunaroddity <e0969600@u.nus.edu>
2023-08-24 13:32:23 +0800

    Add echo command, add exit when user types bye

--- src/main/java/Bareum.java
+++ src/main/java/Bareum.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
 public class Bareum {
+    static boolean active;
+    static void reply(String reply) {
+        String botName = "Bareum: ";
+        String fullReply = botName + reply;
+        System.out.println(fullReply);
+    }
 
     public static void main(String[] args) {
-        String line = "------------------------------------";
-        System.out.println(line);
-        System.out.println("Hello! I'm Bareum!");
-        System.out.println("\nWhat can I do for you? ^^");
-        System.out.println(line);
-        System.out.println("\nBye! Hope to see you again soon!");
-        System.out.println(line);
+        active = true;
+        String introduction = "Hello! I'm Bareum! What can I do for you? ^^\n";
+        String goodbye = "Bye! Hope to see you again soon!\n";
+
+        reply(introduction);
+        Scanner sc = new Scanner(System.in);
+        while (active) {
+            String command = sc.next();
+            if (command.equals("bye")) {
+                active = false;
+            } else {
+                reply(command);
+            }
+        }
+
+        reply(goodbye);
     }
 }
