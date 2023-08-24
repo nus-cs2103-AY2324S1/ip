651b5f031f33a066eb63fcb69a3dc976792a5ad7
LuoZYi <luozhongyi2002@gmail.com>
2023-08-24 16:58:32 +0800

    Level 1

--- Duke.java
+++ Duke.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class Duke {
     private String name = "Lakinta";
+    private Scanner scanner = new Scanner(System.in);
 
     public String greeting() {
         return "Hello! I'm " + name +
@@ -9,6 +11,20 @@ public class Duke {
     public String exit() {
         return "Bye. Hope to see you again soon!";
     }
+
+    public void echo() {
+        while (true) {
+            String response = scanner.nextLine();
+            if (response.equals("bye")) {
+                System.out.println(exit());
+                break;
+            } else {
+                System.out.println(response);
+            }
+        }
+        scanner.close();
+    }
+
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -18,6 +34,7 @@ public class Duke {
 //        System.out.println("Hello from\n" + logo);
         Duke myBot = new Duke();
         System.out.println(myBot.greeting());
-        System.out.println(myBot.exit());
+        myBot.echo();
+
     }
 }
