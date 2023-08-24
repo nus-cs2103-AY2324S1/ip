ce7de6070210390331da9054c496b2e653f45c83
ZD292 <ongzhendong@gmail.com>
2023-08-24 18:09:01 +0800

    commit level 2

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.*;
+
 public class Chatbot {
 
 	private String name;
@@ -13,6 +17,38 @@ public class Chatbot {
 				"What can I do for you?\n" +
 				"____________________________________________________________", this.name);
 		System.out.println(greeting);
+	}
+
+	public void run() {
+		Scanner newScan = new Scanner(System.in);
+		ArrayList<String> l1 = new ArrayList<>();
+		while (newScan.hasNextLine()) {
+			String action = newScan.nextLine();
+			if (action.equals("bye")) {
+				// exit
+				String echo = "____________________________________________________________\n" +
+						"Bye. Hope to see you again soon!\n" +
+						"____________________________________________________________";
+				System.out.println(echo);
+				System.exit(0);
+			} else if (!action.equals("list")) {
+				l1.add(action);
+				String echo = String.format("____________________________________________________________\n" +
+						"added: %s\n" +
+						"____________________________________________________________", action);
+				System.out.println(echo);
+			} else {
+				String lst = "";
+				for (int i = 0; i < l1.size(); i++) {
+					int idx = i + 1;
+					lst += idx + ". " + l1.get(i) + "\n";
+				}
+				String echo = String.format("____________________________________________________________\n"
+						+ "%s"
+						+ "____________________________________________________________", lst);
+				System.out.println(echo);
+			}
+		}
 	}
 
 }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -8,25 +7,8 @@ public class Duke {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
         Chatbot b1 = new Chatbot("Zhen Dong");
-        Scanner newScan = new Scanner(System.in);
         b1.greet();
-        while (newScan.hasNextLine()) {
-           String action = newScan.nextLine();
-           if (action.equals("bye")) {
-               // exit
-               String echo = String.format("____________________________________________________________\n" +
-                       "Bye. Hope to see you again soon!\n" +
-                       "____________________________________________________________", action);
-               System.out.println(echo);
-               System.exit(0);
-           } else {
-               String echo = String.format("____________________________________________________________\n" +
-                       "%s\n" +
-                       "____________________________________________________________", action);
-               System.out.println(echo);
-           }
-
-        }
+        b1.run();
 
     }
 }
