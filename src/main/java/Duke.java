d4181aa8ded208a15fdb472db673ee9e3a873286
Timothy Putra Prasetio <timothypp02@gmail.com>
2023-08-24 12:20:31 +0800

    Support multiple types of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,14 +22,17 @@ public class Duke {
             } else if (command.equals("list")) {
                 System.out.println(LINE);
                 System.out.println(" Here are the tasks in your list:");
+
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println(" " + (i + 1) + ". " + tasks[i]);
                 }
+
                 System.out.println(LINE);
             } else if (command.equals("mark")) {
                 int taskIndex = sc.nextInt() - 1;
                 Task t = tasks[taskIndex];
                 t.markAsDone();
+
                 System.out.println(LINE);
                 System.out.println(" Nice! I've marked this task as Done:");
                 System.out.println("  " + t);
@@ -38,15 +41,60 @@ public class Duke {
                 int taskIndex = sc.nextInt() - 1;
                 Task t = tasks[taskIndex];
                 t.markAsNotDone();
+
                 System.out.println(LINE);
                 System.out.println(" Nice! I've marked this task as Not Done:");
                 System.out.println("  " + t);
                 System.out.println(LINE);
+            } else if (command.equals("todo")) {
+                String todo = sc.nextLine().trim();
+                Task t = new Todo(todo);
+                tasks[taskCount++] = t;
+
+                System.out.println(LINE);
+                System.out.println(" Got it! I've added this task:");
+                System.out.println(t);
+                System.out.println(" Now you have " + taskCount + " tasks in the list.");
+                System.out.println(LINE);
+            } else if (command.equals("deadline")) {
+                String deadline = sc.nextLine();
+                String[] details = deadline.split("/");
+
+                String task = details[0].trim();
+                String by = details[1].replace("by", "").trim();
+
+                Task t = new Deadline(task, by);
+                tasks[taskCount++] = t;
+
+                System.out.println(LINE);
+                System.out.println(" Got it! I've added this task:");
+                System.out.println(t);
+                System.out.println(" Now you have " + taskCount + " tasks in the list.");
+                System.out.println(LINE);
+            } else if (command.equals("event")) {
+                String event = sc.nextLine();
+                String[] details = event.split("/");
+
+                String task = details[0].trim();
+                String from = details[1].replace("from", "").trim();
+                String to = details[2].replace("to", "").trim();
+
+                Task t = new Event(task, from, to);
+                tasks[taskCount++] = t;
+
+                System.out.println(LINE);
+                System.out.println(" Got it! I've added this task:");
+                System.out.println(t);
+                System.out.println(" Now you have " + taskCount + " tasks in the list.");
+                System.out.println(LINE);
             } else {
                 Task t = new Task(command);
                 tasks[taskCount++] = t;
+
                 System.out.println(LINE);
-                System.out.println(" Added: " + command);
+                System.out.println(" Got it! I've added this task:");
+                System.out.println(t);
+                System.out.println(" Now you have " + taskCount + " tasks in the list.");
                 System.out.println(LINE);
             }
         }
