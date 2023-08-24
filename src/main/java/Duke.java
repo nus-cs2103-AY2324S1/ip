3bff630f456d72b568cf38c969e8fd3f14ad26b0
Nidhish Krishnan <nid.nk45@gmail.com>
2023-08-24 17:40:54 +0800

    Added DukeException class to handle inputs that are not valid for the scannner

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,57 +8,78 @@ public class Duke {
         String s = sc.nextLine();
         ArrayList<SingleTask> taskList = new ArrayList<>();
         while (!s.equals("bye")) {
-            if (s.equals("list")) {
-                System.out.println("Here are your tasks ah boy:");
-                for (int i = 0; i < taskList.size(); i++) {
-                    SingleTask task = taskList.get(i);
-                    System.out.println((i + 1) + task.listString());
+            try {
+                if (s.equals("list")) {
+                    System.out.println("Here are your tasks ah boy:");
+                    for (int i = 0; i < taskList.size(); i++) {
+                        SingleTask task = taskList.get(i);
+                        System.out.println((i + 1) + task.listString());
+                    }
+                    s = sc.nextLine();
+                } else if (s.matches(".*\\bmark\\b.*")) {
+                    String[] parts = s.split(" ");
+                    int number = Integer.parseInt(parts[1]);
+                    SingleTask task = taskList.get(number - 1);
+                    task.mark();
+                    s = sc.nextLine();
+                } else if (s.matches(".*\\bunmark\\b.*")) {
+                    String[] parts = s.split(" ");
+                    int number = Integer.parseInt(parts[1]);
+                    SingleTask task = taskList.get(number - 1);
+                    task.unmark();
+                    s = sc.nextLine();
+                } else if (s.matches("(?i)^\\s*(todo|event|deadline)\\b.*")) {
+                    String[] parts = s.split(" ", 2);
+                    String TypeOfEvent = parts[0].toLowerCase();
+                    switch (TypeOfEvent) {
+                        case "todo":
+                            if (parts.length < 2) {
+                                throw new DukeException("Boy ah todo need description eh.");
+                            }
+                            String content = parts[1];
+                            SingleTask taskT = new ToDo(content);
+                            taskList.add(taskT);
+                            System.out.println(taskT.toString());
+                            System.out.println(String.format("Got %d task in list boy", taskList.size()));
+                            s = sc.nextLine();
+                            break;
+                        case "deadline":
+                            if (parts.length < 2) {
+                                throw new DukeException("Boy need to know when the deadline is eh.");
+                            }
+                            String[] part = parts[1].split("/by");
+                            if (part.length < 2) {
+                                throw new DukeException("BOY AH The deadline need to write a /by time!!");
+                            }
+                            SingleTask taskD = new Deadline(part[0], part[1]);
+                            taskList.add(taskD);
+                            System.out.println(taskD.toString());
+                            System.out.println(String.format("Got %d task in list boy", taskList.size()));
+                            s = sc.nextLine();
+                            break;
+                        case "event":
+                            if (parts.length < 2) {
+                                throw new DukeException("Boy need to know the event description eh.");
+                            }
+                            String[] strarray = parts[1].split("/");
+                            if (strarray.length < 3) {
+                                throw new DukeException("BOY!! The event command need /from and /to times.");
+                            }
+                            String from = strarray[1].replace("from", "").trim();
+                            String to = strarray[2].replace("to", "").trim();
+                            SingleTask taskE = new Event(strarray[0], from, to);
+                            taskList.add(taskE);
+                            System.out.println(taskE.toString());
+                            System.out.println(String.format("Got %d task in list boy", taskList.size()));
+                            s = sc.nextLine();
+                            break;
+                    }
+                } else {
+                    throw new DukeException("Boy idk what you saying eh must tell me todo or deadline or event :(");
                 }
-                s = sc.nextLine();
-            } else if (s.matches(".*\\bmark\\b.*")) {
-                String[] parts = s.split(" ");
-                int number = Integer.parseInt(parts[1]);
-                SingleTask task = taskList.get(number - 1);
-                task.mark();
-                s = sc.nextLine();
-            } else if (s.matches(".*\\bunmark\\b.*")) {
-                String[] parts = s.split(" ");
-                int number = Integer.parseInt(parts[1]);
-                SingleTask task = taskList.get(number - 1);
-                task.unmark();
-                s = sc.nextLine();
-            } else if(s.matches("(?i)^\\s*(todo|event|deadline)\\b.*")) {
-                String[] parts = s.split(" ", 2);
-                String TypeOfEvent = parts[0].toLowerCase();
-                switch (TypeOfEvent) {
-                    case "todo":
-                        String content = parts[1];
-                        SingleTask taskT = new ToDo(content);
-                        taskList.add(taskT);
-                        System.out.println(taskT.toString());
-                        System.out.println(String.format("Got %d task in list boy", taskList.size()));
-                        s = sc.nextLine();
-                        break;
-                    case "deadline":
-                        String[] part = parts[1].split("/by");
-                        SingleTask taskD = new Deadline(part[0], part[1]);
-                        taskList.add(taskD);
-                        System.out.println(taskD.toString());
-                        System.out.println(String.format("Got %d task in list boy", taskList.size()));
-                        s = sc.nextLine();
-                        break;
-                    case "event":
-                        String[] strarray = parts[1].split("/");
-                        String from = strarray[1].replace("from", "").trim();
-                        String to = strarray[2].replace("to", "").trim();
-                        SingleTask taskE = new Event(strarray[0], from, to);
-                        taskList.add(taskE);
-                        System.out.println(taskE.toString());
-                        System.out.println(String.format("Got %d task in list boy", taskList.size()));
-                        s = sc.nextLine();
-                        break;
-                }
-            } else {
+            }
+            catch (DukeException e) {
+                System.out.println(e.getMessage());
                 s = sc.nextLine();
             }
         }
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,10 @@
+public class DukeException extends Exception {
+    String message;
+    public DukeException(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
