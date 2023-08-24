437f0fbb55eeb56a270aca0ce9e5c2da5845a751
LHeng1 <limhengyi1@gmail.com>
2023-08-24 22:31:38 +0800

    Level-5-HandleErrors

--- src/main/java/Avalon.java
+++ src/main/java/Avalon.java
@@ -7,101 +7,109 @@ public class Avalon {
         //greet
         System.out.println(
                 "   ____________________________________________________________\n" +
-                "    Hello! I'm Arthur Pendragon.\n" +
-                "    What can I do for you?\n" +
-                "   ____________________________________________________________\n"
+                        "    Hello! I'm Arthur Pendragon.\n" +
+                        "    What can I do for you?\n" +
+                        "   ____________________________________________________________\n"
         );
 
-        while (true){
+        while (true) {
             String userInput = scanner.nextLine();
-            //exit
-            if (userInput.equalsIgnoreCase("bye")) {
-                System.out.println(
-                        "   ____________________________________________________________\n" +
-                                "    Farewell. We will meet again!\n" +
-                                "   ____________________________________________________________"
-                );
-                break;
-            //display list
-            } else if (userInput.equalsIgnoreCase("list")) {
-                if (taskCount == 0) {
+            try {
+                //exit
+                if (userInput.equalsIgnoreCase("bye")) {
                     System.out.println(
                             "   ____________________________________________________________\n" +
-                                    "    You haven't added anything, my sire.\n" +
+                                    "    Farewell. We will meet again!\n" +
                                     "   ____________________________________________________________"
                     );
-                } else {
-                    System.out.println("   ____________________________________________________________\n" +
-                                       "   Here are the tasks in your list:");
-                    for (int i = 0; i < taskCount; i++) {
-                        System.out.println("    " + (i + 1) + "." + tasks[i]);
+                    break;
+                //display list
+                } else if (userInput.equalsIgnoreCase("list")) {
+                    if (taskCount == 0) {
+                        System.out.println(
+                                "   ____________________________________________________________\n" +
+                                        "    You haven't added anything, my sire.\n" +
+                                        "   ____________________________________________________________"
+                        );
+                    } else {
+                        System.out.println("   ____________________________________________________________\n" +
+                                "   Here are the tasks in your list:");
+                        for (int i = 0; i < taskCount; i++) {
+                            System.out.println("    " + (i + 1) + "." + tasks[i]);
+                        }
+                        System.out.println("   ____________________________________________________________");
                     }
-                    System.out.println("   ____________________________________________________________");
-                }
 
-            //mark task
-            } else if (userInput.toLowerCase().startsWith("mark ")){
-                int taskIndex = Integer.parseInt(userInput.substring(5)) - 1;
-                if (taskIndex >= 0 && taskIndex < taskCount) {
-                    tasks[taskIndex].markDone();
+                //mark task
+                } else if (userInput.toLowerCase().startsWith("mark ")) {
+                    int taskIndex = Integer.parseInt(userInput.substring(5)) - 1;
+                    if (taskIndex >= 0 && taskIndex < taskCount) {
+                        tasks[taskIndex].markDone();
+                        System.out.println("   ____________________________________________________________\n" +
+                                "   Nice! I've marked this task as done:\n  " + "  " +
+                                tasks[taskIndex].getStatusIcon() + " " +
+                                tasks[taskIndex].description +
+                                "\n   ____________________________________________________________");
+                    } else {
+                        throw new IllegalArgumentException("Invalid task index.");
+                    }
+                //unmark task
+                } else if (userInput.toLowerCase().startsWith("unmark ")) {
+                    int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
+                    if (taskIndex >= 0 && taskIndex < taskCount) {
+                        tasks[taskIndex].markNotDone();
+                        System.out.println("   ____________________________________________________________\n" +
+                                "   Nice! I've marked this task as not done yet:\n  " + "  " +
+                                tasks[taskIndex].getStatusIcon() + " " +
+                                tasks[taskIndex].description +
+                                "\n   ____________________________________________________________");
+                    } else {
+                        throw new IllegalArgumentException("Invalid task index.");
+                    }
+                //create activity
+                } else if (userInput.toLowerCase().startsWith("todo ")) {
+                    String description = userInput.substring(5).trim();
+                    if (description.isEmpty()) {
+                        throw new IllegalArgumentException("The description of a todo cannot be empty.");
+                    }
+                    tasks[taskCount] = new ToDo(description);
+                    taskCount++;
                     System.out.println("   ____________________________________________________________\n" +
-                            "   Nice! I've marked this task as done:\n  " + "  " +
-                            tasks[taskIndex].getStatusIcon() + " " +
-                            tasks[taskIndex].description +
-                            "\n   ____________________________________________________________");
+                            "   Got it. I've added this task:\n  " + "  " + tasks[taskCount - 1]);
+                    System.out.println("   Now you have " + taskCount + " tasks in the list.");
+                    System.out.println("   ____________________________________________________________");
+                } else if (userInput.toLowerCase().startsWith("deadline ")) {
+                    String[] parts = userInput.substring(9).split(" /by ");
+                    if (parts.length != 2) {
+                        throw new IllegalArgumentException("Please provide a description and a deadline (use /by).");
+                    }
+                    String description = parts[0];
+                    String by = parts[1];
+                    tasks[taskCount] = new Deadline(description, by);
+                    taskCount++;
+                    System.out.println("   ____________________________________________________________\n" +
+                            "   Got it. I've added this task:\n  " + "  " + tasks[taskCount - 1]);
+                    System.out.println("   Now you have " + taskCount + " tasks in the list.");
+                    System.out.println("   ____________________________________________________________");
+                } else if (userInput.toLowerCase().startsWith("event ")) {
+                    String[] parts = userInput.substring(6).split(" /from | /to ");
+                    if (parts.length != 3) {
+                        throw new IllegalArgumentException("Please provide a description, a starting time, and an ending time (use /from and /to).");
+                    }
+                    String description = parts[0];
+                    String from = parts[1];
+                    String to = parts[2];
+                    tasks[taskCount] = new Event(description, from, to);
+                    taskCount++;
+                    System.out.println("   ____________________________________________________________\n" +
+                            "   Got it. I've added this task:\n  " + "  " + tasks[taskCount - 1]);
+                    System.out.println("   Now you have " + taskCount + " tasks in the list.");
+                    System.out.println("   ____________________________________________________________");
                 } else {
-                    System.out.println("   ____________________________________________________________\n" +
-                            "   Invalid task index." +
-                            "\n   ____________________________________________________________");
+                    throw new IllegalArgumentException("I'm sorry, but I don't know what that means :-(");
                 }
-            //unmark task
-            } else if (userInput.toLowerCase().startsWith("unmark ")) {
-                int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
-                if (taskIndex >= 0 && taskIndex < taskCount) {
-                    tasks[taskIndex].markNotDone();
-                    System.out.println("   ____________________________________________________________\n" +
-                            "   Nice! I've marked this task as not done yet:\n  " + "  " +
-                            tasks[taskIndex].getStatusIcon() + " " +
-                            tasks[taskIndex].description +
-                            "\n   ____________________________________________________________");
-                } else {
-                    System.out.println("   ____________________________________________________________\n" +
-                            "   Invalid task index." +
-                            "\n   ____________________________________________________________");
-                }
-            } else if (userInput.toLowerCase().startsWith("todo ")) {
-                String description = userInput.substring(5);
-                tasks[taskCount] = new ToDo(description);
-                taskCount++;
-                System.out.println("   ____________________________________________________________\n" +
-                        "   Got it. I've added this task:\n  " + "  " + tasks[taskCount - 1]);
-                System.out.println("   Now you have " + taskCount + " tasks in the list.");
-                System.out.println("   ____________________________________________________________");
-            } else if (userInput.toLowerCase().startsWith("deadline ")) {
-                String[] parts = userInput.substring(9).split(" /by ");
-                String description = parts[0];
-                String by = parts[1];
-                tasks[taskCount] = new Deadline(description, by);
-                taskCount++;
-                System.out.println("   ____________________________________________________________\n" +
-                        "   Got it. I've added this task:\n  " + "  " + tasks[taskCount - 1]);
-                System.out.println("   Now you have " + taskCount + " tasks in the list.");
-                System.out.println("   ____________________________________________________________");
-            } else if (userInput.toLowerCase().startsWith("event ")) {
-                String[] parts = userInput.substring(6).split(" /from | /to ");
-                String description = parts[0];
-                String from = parts[1];
-                String to = parts[2];
-                tasks[taskCount] = new Event(description, from, to);
-                taskCount++;
-                System.out.println("   ____________________________________________________________\n" +
-                        "   Got it. I've added this task:\n  " + "  " + tasks[taskCount - 1]);
-                System.out.println("   Now you have " + taskCount + " tasks in the list.");
-                System.out.println("   ____________________________________________________________");
-            } else {
-                System.out.println("   ____________________________________________________________");
-                System.out.println("    Error 404");
-                System.out.println("   ____________________________________________________________");
+            } catch(IllegalArgumentException e){
+                System.out.println("☹ OOPS!!! " + e.getMessage());
             }
         }
 
