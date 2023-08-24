517a3a274dc42f420aae2a2dc2dcccf1c0cec5f3
Qin Nanxin <nanxinqin@hotmail.com>
2023-08-24 19:02:00 +0800

    [Level 1. Echo] Improve the skeletal version of Edna so that it echos commands entered by the user, and exits when the user types the command bye.

--- src/main/java/Edna.java
+++ src/main/java/Edna.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Edna {
     public static void main(String[] args) {
-        String partition = "\n/----------------------------------------/\n";
+        Scanner sc = new Scanner(System.in);
+        String input;
+
+        String partition = "\n----------------------------------------\n";
+
         System.out.println(partition + "Hello! I'm Edna.");
-        System.out.println("What can I do for you?" + partition);
-        System.out.println("Bye. Hope to see you again soon!" + partition);
+        System.out.print("What can I do for you?" + partition);
+        input = sc.nextLine();
+
+        while(input.equals("bye") == false) {
+            System.out.print(input + partition);
+            input = sc.nextLine();
+        }
+
+        System.out.print("Bye. Hope to see you again soon!" + partition);
     }
 }
