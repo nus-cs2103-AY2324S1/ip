fad1461fc4e734bc027fc78be9f473c700a3d741
lambraydon <braydonlam2001@gmail.com>
2023-08-24 15:07:31 +0800

    Added isCompleted fields to Task.java. Added mark and unmark conditions in Ben.java

--- src/main/java/Ben.java
+++ src/main/java/Ben.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Ben {
     private static final String HORIZONTAL_LINE = "------------------------------------------";
@@ -16,7 +18,7 @@ public class Ben {
         System.out.println(HORIZONTAL_LINE + "\nBye. For now\n" + HORIZONTAL_LINE);
     }
 
-    public void echo(String message) {
+    public void add(String message) {
         Task task = new Task(message);
         tasks.add(task);
         System.out.println(HORIZONTAL_LINE + "\n" + "added: " + task + "\n" + HORIZONTAL_LINE);
@@ -34,6 +36,45 @@ public class Ben {
         System.out.println(HORIZONTAL_LINE + "\n" + message + HORIZONTAL_LINE);
     }
 
+    public void Mark(Task task) {
+        task.Mark();
+        System.out.println(HORIZONTAL_LINE + "\n" + "Nice! This task is completed\n" + task + "\n" + HORIZONTAL_LINE);
+    }
+
+    public void Unmark(Task task) {
+        task.Unmark();
+        System.out.println(HORIZONTAL_LINE + "\n" + "Okay! This task is not completed\n" + task + "\n" + HORIZONTAL_LINE);
+    }
+
+    public boolean isMarkCommand(String message) {
+        Pattern pattern = Pattern.compile("(unmark|mark)\\s*(-?\\d+)");
+        Matcher matcher = pattern.matcher(message.toLowerCase());
+
+        if (matcher.find()) {
+            // extract command
+            String command = matcher.group(1);
+
+            // extract task number
+            String TaskNumber = matcher.group(2);
+            int num = Integer.parseInt(TaskNumber) - 1;
+
+            // check whether number is valid
+            if (num < 0 || num >= tasks.size()) {
+                System.out.println(HORIZONTAL_LINE + "\n" + "Please input a valid task number" + "\n" + HORIZONTAL_LINE);
+                return true;
+            }
+
+            // if valid, mark or unmark the task
+            if (Objects.equals(command, "mark")) {
+                Mark(tasks.get(num));
+            } else {
+                Unmark(tasks.get(num));
+            }
+            return true;
+        }
+        return false;
+    }
+
     public void run() {
         greeting();
         while (isActive) {
@@ -43,7 +84,9 @@ public class Ben {
             } else if (Objects.equals(message.toLowerCase(), "list")) {
                 listToString();
             } else {
-                echo(message);
+                if (!isMarkCommand(message)) {
+                    add(message);
+                }
             }
         }
         bye();
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,28 @@
 public class Task {
     private String description;
+    private Boolean isCompleted;
 
     public Task(String description) {
         this.description = description;
+        this.isCompleted = false;
+    }
+
+    public void Mark() {
+        isCompleted = true;
+    }
+
+    public void Unmark() {
+        isCompleted = false;
     }
 
     @Override
     public String toString() {
-        return description;
+        String marking;
+        if (isCompleted) {
+            marking = "[X]";
+        } else {
+            marking = "[ ]";
+        }
+        return marking + " " + description;
     }
 }
