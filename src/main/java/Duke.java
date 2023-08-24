aa41e5ab4517caa45e4b1c829043906108d388bd
Chow Yuan Jing <zcyuanjing@gmail.com>
2023-08-24 18:34:21 +0800

    Fix Level-4, tasks with spaces work now

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -118,34 +118,43 @@ public class Duke {
     }
 
     private static void addTask(Command cmd, String input) {
-        String[] split = input.split(" ");
-        String taskType = split[0];
-        if (taskType.equals("todo")) {
-            if (split.length < 2) {
+        if (cmd.equals(Command.todo)) {
+            if (input.split(" ").length < 2) {
                 System.out.println("The description of a todo cannot be empty!\n" + lineSep);
             } else {
                 System.out.println("Got it. I've added this task:");
-                Todo todo = new Todo(split[1]);
+                Todo todo = new Todo(input.replaceFirst("todo ", ""));
                 tasks.add(todo);
                 System.out.println(todo);
             }
-        } else if (taskType.equals("deadline")) {
-            if (split.length < 3) {
+        } else if (cmd.equals(Command.deadline)) {
+            if (!input.matches(".*\\b /by \\b.*")) {
                 System.out.println("A deadline must contain a description and end specified with `/by`!\n"
                         + lineSep);
             } else {
                 System.out.println("Got it. I've added this task:");
-                Deadline deadline = new Deadline(split[1], split[3]);
+                String afterCommand = input.replaceFirst("deadline ", "");
+                String[] split = afterCommand.split(" /by ");
+                Deadline deadline = new Deadline(split[0], split[1]);
                 tasks.add(deadline);
                 System.out.println(deadline);
             }
-        } else if (taskType.equals("event")) {
-            if (split.length < 5) {
+        } else if (cmd.equals(Command.event)) {
+            if (!input.matches(".*\\b /by \\b.*") && !input.matches(".*\\b/to \\b.*")) {
                 System.out.println("An event must contain a description," +
                         " start and end specified with `/by` and `/to`!\n" + lineSep);
             } else {
                 System.out.println("Got it. I've added this task:");
-                Event event = new Event(split[1], split[3], split[5]);
+                String afterCommand = input.replaceFirst("deadline ", "");
+                // In case the user does /to before /by, split /by and /to and vice versa to get by and to
+                String by = afterCommand.split(" /by ")[1];
+                by = by.split(" /to ")[0];
+                String to = afterCommand.split(" /to ")[1];
+                to = to.split(" /by ")[0];
+                // Get index 0 to get before the /by and /to, so either way you'll get only the task
+                String task = input.split(" /to ")[0].split(" /by ")[0];
+                task = task.replaceFirst("event ", "");
+                Event event = new Event(task, by, to);
                 tasks.add(event);
                 System.out.println(event);
             }
