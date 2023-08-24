7d961f9438d7e4f4373c0357ac7dab9f4ab801fd
peasantbird <bertrand.wong.xh@gmail.com>
2023-08-24 17:40:11 +0800

    Add echo feature

--- src/main/java/Bert.java
+++ src/main/java/Bert.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 /**
- * A chatbot named Bert
+ * A chatbot named Bert that introduces itself,
+ * then echos commands entered by the user, until
+ * the user types 'bye' in which the program exits
  */
 public class Bert {
     /**
@@ -12,6 +16,19 @@ public class Bert {
                 "____________________________________________________________\n" +
                 "Hello! I'm " + Bert.botName + "\n" +
                 "What can I do for you?\n" +
+                "____________________________________________________________\n"
+        );
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        while (!s.equals("bye")) {
+            System.out.println(
+                    "____________________________________________________________\n" +
+                    s + "\n" +
+                    "____________________________________________________________\n"
+            );
+            s = sc.nextLine();
+        }
+        System.out.println(
                 "____________________________________________________________\n" +
                 "Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n"
