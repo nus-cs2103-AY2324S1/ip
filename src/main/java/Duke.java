c0fc4844287b3f0f1ec67a07ce38f57921dacaf0
Winston <leonardwinstoncx9@gmail.com>
2023-08-24 16:26:22 +0800

    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
     public static String line = "\t____________________________________________________________\n";
-    public static ArrayList<String> strList = new ArrayList<>();
+    public static ArrayList<Task> strList = new ArrayList<>();
     public static void main(String[] args) {
 
         String greeting =
@@ -24,12 +24,35 @@ public class Duke {
                 System.out.println(line);
                 for (int i = 0; i < arrLength; i++) {
                     int number = i + 1;
-                    System.out.println("\t" + number + ". " + strList.get(i));
+                    Task t = strList.get(i);
+                    String marked = t.getStatusIcon();
+                    String description = t.getDescription();
+                    System.out.println("\t" + number + "." + marked + " " + description);
                 }
                 System.out.println(line);
-            } else {
+            } else if (echo.startsWith("mark")) {
+                char lastCharacter = echo.charAt(echo.length() - 1);
+                int index = Character.getNumericValue(lastCharacter) - 1;
+                Task t = strList.get(index);
+                t.markTask();
+                System.out.println(line);
+                System.out.println("\tNice! I've marked this task as done:");
+                System.out.println("\t" + "\t" + t.getStatusIcon() + " " + t.getDescription());
+                System.out.println(line);
+            } else if (echo.startsWith("unmark")) {
+                char lastCharacter = echo.charAt(echo.length() - 1);
+                int index = Character.getNumericValue(lastCharacter) - 1;
+                Task t = strList.get(index);
+                t.unmarkTask();
+                System.out.println(line);
+                System.out.println("\tOK, I've marked this task as not done yet:");
+                System.out.println("\t" + "\t" + t.getStatusIcon() + " " + t.getDescription());
+                System.out.println(line);
+            }
+            else {
                 System.out.println(line + "\tadded: " + echo + "\n" + line);
-                strList.add(echo);
+                Task newTask = new Task(echo);
+                strList.add(newTask);
             }
             echo = myObj.nextLine();  // Read user input
 
