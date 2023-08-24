cffcb10de0395a50fa0b409ac39e162bc61eaf87
Eola-Z <sxplayer123@gmail.com>
2023-08-25 01:31:02 +0800

    Added the Echo functionality to the chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * The main class controlling the chatbot's actions.
  * This class should be able to keep track of tasks, and
@@ -10,6 +12,13 @@ public class Duke {
      */
     private static final String name = "JOHNATHAN CENATOR\n";
     private static final String horizontalLine = "-------------------------------\n";
+
+    /**
+     * An enum to track the status of the chatbot
+     * RUNNING, STOPPING, etc.
+     * [To add more if needed]
+     */
+    enum Status {RUNNING, STOPPING}
 
     /**
      * Sends a greeting message on startup of the chatbot.
@@ -26,13 +35,33 @@ public class Duke {
      * Sends a departing message on chatbot shutdown.
      */
     private static void exit() {
-        System.out.println("NOW GET OUTTA HERE!\n" +
+        System.out.println(horizontalLine +
+                "NOW GET OUTTA HERE!\n" +
                 "RESPECTFULLY,\n" +
                 name +
                 horizontalLine);
     }
+
+    /**
+     * Repeats the user's input
+     */
+    private static void echo(String input) {
+        System.out.println(horizontalLine + input + "\n" + horizontalLine);
+    }
     public static void main(String[] args) {
         greet();
+        Scanner textInput = new Scanner(System.in);
+        Status botStatus = Status.RUNNING;
+
+        while (botStatus == Status.RUNNING) {
+            String nextLine = textInput.nextLine();
+            if (nextLine.equals("bye")) {
+                botStatus = Status.STOPPING;
+                continue;
+            }
+            echo(nextLine);
+        }
+
         exit();
     }
 }
