ef0271d7f1a602de7d2f8a2aba63f3f42df232f4
redtailedfox <hongwei.hhw@gmail.com>
2023-08-24 16:59:05 +0800

    "Level-1"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+
+import java.util.Scanner;
 public class Duke {
 
     private static final String greet = "Hello! I'm ";
@@ -6,9 +8,22 @@ public class Duke {
 
     private static final String bye = "Bye. Hope to see you again soon!";
 
+    private static void respond(String s) {
+        if (s.equals("bye")) {
+            System.out.println(bye);
+        } else {
+            System.out.println(s);
+        }
+        
+    }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
         System.out.println(greet +  name);
         System.out.println(msg);
-        System.out.println(bye);
+
+        respond(s);
+
     }
 }
