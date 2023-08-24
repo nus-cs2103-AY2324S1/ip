0e0d3f5c75c252356c721c1ac4b4673749d22d7c
hyc17003 <e0970589@u.nus.edu>
2023-08-24 16:27:30 +0800

    Echo ChatterBot

--- src/main/java/ChatterBot.java
+++ src/main/java/ChatterBot.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class ChatterBot {
     public static void main(String[] args) {
+
         String logo = "ChatterBot";
-        System.out.println("Hello! I'm " + logo + "\nWhat can I do for you?"
-                + "\nBye. Hope to see you again soon!");
+        System.out.println("Hello! I'm " + logo + "\nWhat can I do for you?");
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String userMessage = scanner.nextLine();
+
+            if (userMessage.toLowerCase().equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(userMessage);
+            }
+        }
     }
 }
