af20120fa730895e8c3bbc4e3aada06e400c8d1d
AryanG01 <rajeevganju0@gmail.com>
2023-08-24 22:23:17 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Axela");
@@ -12,14 +14,23 @@ public class Duke {
         while (true) {
             String command = scanner.nextLine();
             System.out.println("____________________________________________________________");
-            System.out.println(" " + command);
-            System.out.println("____________________________________________________________");
 
             if (command.equalsIgnoreCase("bye")) {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (command.equalsIgnoreCase("list")) {
+                System.out.println(" Here are your tasks:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println(" added: " + command);
             }
+
+            System.out.println("____________________________________________________________");
         }
     }
 }
