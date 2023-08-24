3fe9fb3dbc306f02c0c245c890f6226ae8aa8e93
sopa301 <96387349+sopa301@users.noreply.github.com>
2023-08-24 19:20:15 +0800

    Minor optimisations

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Bot {
+    private static final Pattern markPattern = Pattern.compile("mark -?\\d+");
+    private static final Pattern unmarkPattern = Pattern.compile("unmark -?\\d+");
+    private static final Pattern deletePattern = Pattern.compile("delete -?\\d+");
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm the trash gremlin Caelus!\nWhat can I do for you?");
         ArrayList<Task> lst = new ArrayList<>();
-        Pattern markPattern = Pattern.compile("mark \\d+");
-        Pattern unmarkPattern = Pattern.compile("unmark \\d+");
-        Pattern deletePattern = Pattern.compile("delete \\d+");
         while (true) {
             try {
                 String str = sc.nextLine();
@@ -19,37 +19,13 @@ public class Bot {
                 } else if (str.equalsIgnoreCase("list")) {
                     displayList(lst);
                 } else if (str.startsWith("mark")) {
-                    if (!markPattern.matcher(str).matches()) {
-                        throw new InvalidIndexException();
-                    }
-                    int index = Integer.parseInt(str.substring(5)) - 1;
-                    if (index < 0 || index >= lst.size()) {
-                        throw new InvalidIndexException();
-                    }
-                    lst.get(index).mark();
-                    System.out.println("I'll mark this as done:\n" + lst.get(index).toString());
+                    markTask(str, lst);
                 } else if (str.startsWith("unmark")) {
-                    if (!unmarkPattern.matcher(str).matches()) {
-                        throw new InvalidIndexException();
-                    }
-                    int index = Integer.parseInt(str.substring(7)) - 1;
-                    if (index < 0 || index >= lst.size()) {
-                        throw new InvalidIndexException();
-                    }
-                    lst.get(index).unmark();
-                    System.out.println("I'll mark this as not done:\n" + lst.get(index).toString());
+                    unmarkTask(str, lst);
                 } else if (Task.isTaskCommand(str)) {
-                    addTask(Task.makeTask(str), lst);
+                    addTask(str, lst);
                 } else if (str.startsWith("delete")) {
-                    if (!deletePattern.matcher(str).matches()) {
-                        throw new InvalidIndexException();
-                    }
-                    int index = Integer.parseInt(str.substring(7)) - 1;
-                    if (index < 0 || index >= lst.size()) {
-                        throw new InvalidIndexException();
-                    }
-                    System.out.println("I've removed this task:\n" + lst.remove(index).toString());
-                    displayListLength(lst);
+                    deleteTask(str, lst);
                 } else {
                     throw new InvalidCommandException();
                 }
@@ -71,14 +47,48 @@ public class Bot {
             ctr++;
         }
         out.deleteCharAt(out.length() - 1);
-        System.out.println(out.toString());
+        System.out.println(out);
     }
     private static void displayListLength(ArrayList<Task> lst) {
         System.out.println("Now you have " + lst.size() + " task(s) in the list.");
     }
-    private static void addTask(Task newTask, ArrayList<Task> lst) {
+    private static void addTask(String str, ArrayList<Task> lst) throws InvalidTaskException {
+        Task newTask = Task.makeTask(str);
         System.out.println("Added:\n" + newTask.toString());
         lst.add(newTask);
+        displayListLength(lst);
+    }
+    private static void markTask(String str, ArrayList<Task> lst) throws InvalidIndexException {
+        if (!markPattern.matcher(str).matches()) {
+            throw new InvalidIndexException();
+        }
+        int index = Integer.parseInt(str.substring(5)) - 1;
+        if (index < 0 || index >= lst.size()) {
+            throw new InvalidIndexException();
+        }
+        lst.get(index).mark();
+        System.out.println("I'll mark this as done:\n" + lst.get(index).toString());
+    }
+    private static void unmarkTask(String str, ArrayList<Task> lst) throws InvalidIndexException {
+        if (!unmarkPattern.matcher(str).matches()) {
+            throw new InvalidIndexException();
+        }
+        int index = Integer.parseInt(str.substring(7)) - 1;
+        if (index < 0 || index >= lst.size()) {
+            throw new InvalidIndexException();
+        }
+        lst.get(index).unmark();
+        System.out.println("I'll mark this as not done:\n" + lst.get(index).toString());
+    }
+    private static void deleteTask(String str, ArrayList<Task> lst) throws InvalidIndexException {
+        if (!deletePattern.matcher(str).matches()) {
+            throw new InvalidIndexException();
+        }
+        int index = Integer.parseInt(str.substring(7)) - 1;
+        if (index < 0 || index >= lst.size()) {
+            throw new InvalidIndexException();
+        }
+        System.out.println("I've removed this task:\n" + lst.remove(index).toString());
         displayListLength(lst);
     }
 }
