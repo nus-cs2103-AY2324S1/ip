c9c78f57fc153628d3c139391943fb46ae95a027
LHeng1 <limhengyi1@gmail.com>
2023-08-24 20:55:39 +0800

    Level-3-MarkAsDone

--- src/main/java/Avalon.java
+++ src/main/java/Avalon.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Avalon {
     public static void main(String[] args) {
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
 
@@ -14,7 +14,7 @@ public class Avalon {
 
         while (true){
             String userInput = scanner.nextLine();
-
+            //exit
             if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println(
                         "   ____________________________________________________________\n" +
@@ -22,6 +22,7 @@ public class Avalon {
                                 "   ____________________________________________________________"
                 );
                 break;
+            //display list
             } else if (userInput.equalsIgnoreCase("list")) {
                 if (taskCount == 0) {
                     System.out.println(
@@ -30,14 +31,48 @@ public class Avalon {
                                     "   ____________________________________________________________"
                     );
                 } else {
-                    System.out.println("   ____________________________________________________________list");
+                    System.out.println("   ____________________________________________________________\n" +
+                                       "   Here are the tasks in your list:");
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println("    " + (i + 1) + ". " + tasks[i]);
+                        System.out.println("    " + (i + 1) + "." +
+                                            tasks[i].getStatusIcon() + tasks[i].description);
                     }
                     System.out.println("   ____________________________________________________________");
                 }
+
+            //mark task
+            } else if (userInput.toLowerCase().startsWith("mark ")){
+                int taskIndex = Integer.parseInt(userInput.substring(5)) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].markDone();
+                    System.out.println("   ____________________________________________________________\n" +
+                            "   Nice! I've marked this task as done:\n  " + "  " +
+                            tasks[taskIndex].getStatusIcon() + " " +
+                            tasks[taskIndex].description +
+                            "\n   ____________________________________________________________");
+                } else {
+                    System.out.println("   ____________________________________________________________\n" +
+                            "   Invalid task index." +
+                            "\n   ____________________________________________________________");
+                }
+            //unmark task
+            } else if (userInput.toLowerCase().startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].markNotDone();
+                    System.out.println("   ____________________________________________________________\n" +
+                            "   Nice! I've marked this task as not done yet:\n  " + "  " +
+                            tasks[taskIndex].getStatusIcon() + " " +
+                            tasks[taskIndex].description +
+                            "\n   ____________________________________________________________");
+                } else {
+                    System.out.println("   ____________________________________________________________\n" +
+                            "   Invalid task index." +
+                            "\n   ____________________________________________________________");
+                }
+            //add task
             } else {
-                tasks[taskCount] = userInput;
+                tasks[taskCount] = new Task(userInput);
                 taskCount++;
                 System.out.println(
                         "   ____________________________________________________________\n" +
@@ -47,5 +82,7 @@ public class Avalon {
                 );
             }
         }
+
+        scanner.close();
     }
 }
