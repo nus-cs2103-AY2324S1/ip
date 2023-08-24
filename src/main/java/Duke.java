16d4e82bc958ad1764ae3b2cdd07e651f78e3ea3
Taanish Bhardwaj <btaanish07@gmail.com>
2023-08-24 12:34:47 +0800

    Completing Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,33 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<>();
         String line = "____________________________________________________________\n";
         System.out.println(line + " Hello! I'm Alcazar\n" +
                 " What can I do for you?\n" +
                 line);
         String prompt = Duke.inputText();
         while (!prompt.equals("bye")) {
-            System.out.println(line + prompt + "\n" + line);
+            if(prompt.equals("list")) {
+                System.out.println(line + getTasks(tasks) + line);
+            } else {
+                tasks.add(prompt);
+                System.out.println(line + "added: " + prompt + "\n" + line);
+            }
             prompt = Duke.inputText();
         }
         System.out.println(line +
                 " Bye. Hope to see you again soon!\n" +
                 line);
+    }
+    public static String getTasks(ArrayList<? extends String> tasks) {
+        String listedTasks = "";
+        for(int i = 0; i < tasks.size(); i++) {
+            listedTasks += (i + 1) + ". " + tasks.get(i) + "\n";
+        }
+        return listedTasks;
     }
     public static String inputText() {
         Scanner sc =  new Scanner(System.in);
