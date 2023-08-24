40af116d34f0f57aa642644eb2615e05a85eee38
ZENN\User <miljy123@gmail.com>
2023-08-24 16:13:06 +0800

    Improved Buddy to echo user commands and exit on "bye"

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class Buddy {
     private static String name = "Buddy";
+
     public static void main(String[] args) {
         String greeting = String.format("Hello! I'm %s\n", name);
         String inquiry = "What can I do for you?\n";
         String exit = "Bye. Hope to see you again soon!\n";
-        System.out.println(greeting + inquiry + exit);
+
+        Scanner scanner = new Scanner(System.in);
+        String command;
+
+        System.out.println(greeting + inquiry);
+
+        while (true) {
+            command = scanner.next();
+            if (command.equals("bye")) {
+                System.out.println(exit);
+                break;
+            }
+            System.out.println(command);
+        }
     }
 }
