1d54df59f55480f26d52b4128c76259d8b5c9a2d
KT <tkt2001@hotmail.com>
2023-08-24 16:24:51 +0800

    l0 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
 public class Duke {
+    private static final String name = "Bot";
+    private static final String greeting = "Hello I'm " + name + "\n What can I do for you?";
+    private static final String end = "Bye bye";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(greeting);
+        System.out.println(end);
     }
 }
