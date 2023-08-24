3e748224c9ccc7ce8b129454424f0e8d694de1ff
spatuly <a0259629l@gmail.com>
2023-08-24 16:36:54 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,7 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greet = "Hello! I'm Duke\n" + "What can I do for you?\n";
+        String exit = "Bye. Hope to see you again soon!\n";
+        System.out.println(greet + exit);
     }
 }
