43a36e9d3bc33ac3187cb2c306fa37ecc5f2019f
Winston <leonardwinstoncx9@gmail.com>
2023-08-24 17:59:18 +0800

    ToDo, Event, Deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,16 @@
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
     public static String line = "\t____________________________________________________________\n";
     public static ArrayList<Task> strList = new ArrayList<>();
+
+    public static void printAddTask(Task t) {
+        System.out.println(line + "\tGot it. I've added this task: ");
+        System.out.println("\t\t" + t.toString());
+        System.out.println("\tNow you have " + strList.size() + " tasks in the list.");
+        System.out.println(line);
+    }
     public static void main(String[] args) {
 
         String greeting =
@@ -18,28 +26,28 @@ public class Duke {
         String echo = myObj.nextLine();  // Read user input
 
         while (!echo.equalsIgnoreCase("bye")) {
+            String[] words = echo.split(" ");
 
             if (echo.equalsIgnoreCase("list")) {
                 int arrLength = strList.size();
                 System.out.println(line);
+                System.out.println("\tHere are the tasks in your list:");
                 for (int i = 0; i < arrLength; i++) {
                     int number = i + 1;
                     Task t = strList.get(i);
-                    String marked = t.getStatusIcon();
-                    String description = t.getDescription();
-                    System.out.println("\t" + number + "." + marked + " " + description);
+                    System.out.println("\t" + number + "." + t.toString());
                 }
                 System.out.println(line);
-            } else if (echo.startsWith("mark")) {
+            } else if (words[0].equalsIgnoreCase("mark")) {
                 char lastCharacter = echo.charAt(echo.length() - 1);
                 int index = Character.getNumericValue(lastCharacter) - 1;
                 Task t = strList.get(index);
                 t.markTask();
                 System.out.println(line);
                 System.out.println("\tNice! I've marked this task as done:");
-                System.out.println("\t" + "\t" + t.getStatusIcon() + " " + t.getDescription());
+                System.out.println("\t" + "\t" + t.toString());
                 System.out.println(line);
-            } else if (echo.startsWith("unmark")) {
+            } else if (words[0].equalsIgnoreCase("unmark")) {
                 char lastCharacter = echo.charAt(echo.length() - 1);
                 int index = Character.getNumericValue(lastCharacter) - 1;
                 Task t = strList.get(index);
@@ -48,6 +56,30 @@ public class Duke {
                 System.out.println("\tOK, I've marked this task as not done yet:");
                 System.out.println("\t" + "\t" + t.getStatusIcon() + " " + t.getDescription());
                 System.out.println(line);
+            } else if (words[0].equalsIgnoreCase("todo")) {
+                String description = String.join(" ", Arrays.copyOfRange(words, 1, words.length));
+                Task newToDo = new ToDos(description);
+                strList.add(newToDo);
+                printAddTask(newToDo);
+            } else if (words[0].equalsIgnoreCase("deadline")) {
+                String description = String.join(" ", Arrays.copyOfRange(words, 1, words.length));
+                String[] parts = description.split("/");  // Split the input string by the delimiter "/"
+                String before = parts[0].trim();
+                String after = parts[1].trim();
+                String[] afterArray = after.split(" ");
+                Task newDeadline =  new Deadline(before, afterArray[1]);
+                strList.add(newDeadline);
+                printAddTask(newDeadline);
+            } else if (words[0].equalsIgnoreCase("event")) {
+                String description = String.join(" ", Arrays.copyOfRange(words, 1, words.length));
+                int fromIndex = description.indexOf("/from");  // Find the index of "/from"
+                int toIndex = description.indexOf("/to");  // Find the index of "/to"
+                String eventDescription = description.substring(0, fromIndex).trim();
+                String from = description.substring(fromIndex + "/from".length(), toIndex).trim();
+                String to = description.substring(toIndex + "/to".length()).trim();
+                Task newEvent = new Event(eventDescription, from, to);
+                strList.add(newEvent);
+                printAddTask(newEvent);
             }
             else {
                 System.out.println(line + "\tadded: " + echo + "\n" + line);
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -22,4 +22,9 @@ public class Task {
     public void unmarkTask() {
         this.isDone = false;
     }
+
+    @Override
+    public String toString() {
+        return this.getStatusIcon() + " " + this.description;
+    }
 }
