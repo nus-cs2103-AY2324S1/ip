151d80147ff4786acbe6237cea0b195b2304e0f6
nicrandomlee <leejpnicholas@gmail.com>
2023-08-24 23:06:40 +0800

    Level 3. Mark as Done Add the ability to mark tasks as done. Added the ability to change the status back to not done.

--- src/main/java/Cupid.java
+++ src/main/java/Cupid.java
@@ -2,9 +2,15 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Cupid {
+
+    public enum ChatFunction {
+        LIST,
+        MARK,
+        UNMARK
+    }
+
     public static void main(String[] args) {
-        String[] tasks = new String[100];
-        int taskCounter = 0;
+        ArrayList<Task> taskList = new ArrayList<>();
 
         System.out.println("____________________________________________________________");
         Scanner scanner = new Scanner(System.in);
@@ -13,20 +19,76 @@ public class Cupid {
 
         while (true) {
             String input = scanner.nextLine();
+            String[] inputArray = input.split(" ");
+
+            try {
+                ChatFunction function = ChatFunction.valueOf(inputArray[0].toUpperCase());
+                switch (function) {
+                    case LIST:
+                        for (int i=0; i<taskList.size(); i++) {
+                            String message = String.format("%d. [%s] %s", i+1, taskList.get(i).getStatusIcon(), taskList.get(i).getDescription());
+                            System.out.println(message);
+                        };
+                        continue;
+
+                    case MARK:
+                        if (inputArray.length < 2) {
+                            System.out.println("Invalid mark task provided.");
+                            break;
+                        }
+
+                        try {
+                            int targetTaskIdx = Integer.parseInt(inputArray[1]) -1;
+                            Task markTask = taskList.get(targetTaskIdx);
+                            markTask.markAsDone();
+                            System.out.println("Nice! I've marked this task as done:");
+                            String message = String.format("[%s] %s", markTask.getStatusIcon(), markTask.getDescription());
+                            System.out.println(message);
+                            break;
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid number provided.");
+                            break;
+                        } catch (IndexOutOfBoundsException e) {
+                            System.out.println("Invalid task number provided.");
+                            break;
+                        }
+
+                    case UNMARK:
+                        if (inputArray.length < 2) {
+                            System.out.println("Invalid unmark task provided.");
+                            break;
+                        }
+
+                        try {
+                            int targetTaskIdx = Integer.parseInt(inputArray[1]) -1;
+                            Task task = taskList.get(targetTaskIdx);
+                            task.markAsUndone();
+                            System.out.println("Ok! I've marked this task as not done yet:");
+                            String message = String.format("[%s] %s", task.getStatusIcon(), task.getDescription());
+                            System.out.println(message);
+                            break;
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid number provided.");
+                            break;
+                        } catch (IndexOutOfBoundsException e) {
+                            System.out.println("Invalid task number provided.");
+                            break;
+                        }
+
+                    default:
+                        break;
+                }
+            } catch (IllegalArgumentException e) {
+                // If task inserted not an ENUM
+                Task newTask = new Task(input);
+                taskList.add(newTask);
+                System.out.println("Added: " + input);
+            }
+
+
             if (input.toLowerCase().equals("bye")) {
                 break;
             }
-
-            if (input.toLowerCase().equals("list")) {
-                for (int i=1; i<taskCounter+1; i++) {
-                    System.out.println(i + ". " +  tasks[i-1]);
-                }
-                continue;
-            }
-
-            tasks[taskCounter] = input;
-            System.out.println("Added: " + input);
-            taskCounter++;
         }
 
 
