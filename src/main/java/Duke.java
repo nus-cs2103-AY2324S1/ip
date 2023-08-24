9661dc860645b48a7c9f5301bf924559d4ec85f9
spatuly <a0259629l@gmail.com>
2023-08-24 20:33:17 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,32 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
 
     static boolean allowNext = true;
+    static boolean addStr = true;
+    static ArrayList<String> storedStr = new ArrayList<>();
 
     public static void handleExit(String s) {
         String exit = "Bye. See you soon! :)\n";
-
 
         if (s.equals("bye")) {
             System.out.println(exit);
             allowNext = false;
         }
+    }
 
+    public static void handleList(String s) {
+        if (s.equals("list")) {
+            addStr = false;
+            int len = storedStr.size();
+            if (len > 0) {
+                for (int i = 1; i < len + 1; i++) {
+                    System.out.println(i + ". " + storedStr.get(i - 1));
+                }
+            } else {
+                System.out.println("Nothing found!");
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -23,9 +38,13 @@ public class Duke {
         while (allowNext) {
             String str = input.nextLine();
             handleExit(str);
+            handleList(str);
 
-            if (allowNext) {
-                System.out.println(str);
+            if (allowNext && addStr) {
+                storedStr.add(str);
+                System.out.println("added: " + str);
+            } else if (!addStr) {
+                addStr = true;
             }
         }
     }
