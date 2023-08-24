8286f55568f624c1e4c6374ae042afb3802d2da9
Winston <leonardwinstoncx9@gmail.com>
2023-08-24 14:39:01 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
+import java.util.Scanner;
 public class Duke {
+    public static String line = "____________________________________________________________\n";
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -8,13 +10,22 @@ public class Duke {
 //        System.out.println("Hello from\n" + logo);
 
         String greeting =
-                "____________________________________________________________\n" +
-                "Hello! I'm CHAT\n" +
+                line +
+                "Hello! I'm DukeBot\n" +
                 "What can I do for you?\n" +
-                "____________________________________________________________\n";
+                line;
 
-        String exitMessage = "Bye. Hope to see you again\n" +
-               "____________________________________________________________\n";
-        System.out.println(greeting + exitMessage);
+        String exitMessage = "Bye. Hope to see you again\n" + line;
+        System.out.println(greeting);
+
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        String echo = myObj.nextLine();  // Read user input
+
+        while (!echo.equalsIgnoreCase("bye")) {
+            System.out.println("");
+            System.out.println(line + echo + "\n" + line);
+            echo = myObj.nextLine();  // Read user input
+        }
+        System.out.println(exitMessage);
     }
 }
