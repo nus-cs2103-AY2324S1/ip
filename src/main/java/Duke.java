0cd22d2f2ff67fee20f47fba46b4a449284f3305
MathanC <ncmathan98@gmail.com>
2023-08-24 18:32:22 +0800

    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,18 +10,43 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<Task>();
         while (!str.equals("bye")){
             if(str.equals("list")){
                 System.out.println("____________________________________________________________");
                 for(int i=0;i<list.size();i++){
                     int j=i+1;
-                    System.out.println(j+". "+list.get(i)+"");
+                    System.out.println(j+"."+list.get(i)+"");
                 }
                 System.out.println("____________________________________________________________");
             }
+            else if(str.contains("unmark")){
+                System.out.println("____________________________________________________________");
+                String val= str.replaceAll("[^0-9]", "");
+                int index = Integer.parseInt(val);
+                index -=1;
+                list.get(index).setUndone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                int j=index+1;
+                System.out.println("\t"+list.get(index)+"\n");
+                System.out.println("____________________________________________________________");
+
+            }
+            else if(str.contains("mark")){
+                System.out.println("____________________________________________________________");
+                String val= str.replaceAll("[^0-9]", "");
+                int index = Integer.parseInt(val);
+                index -=1;
+                list.get(index).setDone();
+                System.out.println("Nice! I've marked this task as done:");
+                int j=index+1;
+                System.out.println("\t"+list.get(index)+"\n");
+                System.out.println("____________________________________________________________");
+
+            }
+
             else {
-                list.add(str);
+                list.add(new Task(str));
 
                 System.out.println("____________________________________________________________\n" +
                         str + "\n" +
@@ -33,4 +58,5 @@ public class Duke {
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________");
     }
+
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task{
+    private String task;
+    private boolean isDone;
+
+    public Task(String task){
+        this.task = task;
+        isDone=false;
+    }
+
+    public void setDone(){
+        isDone=true;
+    }
+    public void setUndone(){
+        isDone=false;
+    }
+    public boolean getDone(){
+        return isDone;
+    }
+
+    public String toString(){
+        if(isDone)
+            return "[X] "+ task;
+        return "[ ] "+ task;
+
+    }
+}
+
