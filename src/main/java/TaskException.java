9b06ce8efeb962640c58d6135b2688960eac9401
YangHeng Tang <yanghengtang@gmail.com>
2023-08-24 19:35:51 +0800

    Add TaskException.java

---
+++ src/main/java/TaskException.java
@@ -0,0 +1,7 @@
+public class TaskException extends Exception {
+    private static String HORIZONTAL_LINE = "____________________________________________________________\n";
+
+    public TaskException() {
+        super(HORIZONTAL_LINE + "You have entered an invalid task number.\n" + HORIZONTAL_LINE);
+    }
+}
