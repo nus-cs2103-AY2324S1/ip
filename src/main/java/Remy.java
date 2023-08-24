4b72f755de056e1ae73c791d5fda73eb2675d22f
p-xp <panxinping10@gmail.com>
2023-08-24 21:41:49 +0800

    feat: implement Level-2

--- src/main/java/Remy.java
+++ src/main/java/Remy.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Remy {
+
     public static void main(String[] args) {
         String welcomeMessage = "____________________________________________________________\n" +
                 " Hello! I'm Remy.\n" +
@@ -8,6 +10,7 @@ public class Remy {
         String exitMessage = " Bye. Hope to see you again soon!\n" +
         "____________________________________________________________\n";
 
+        ArrayList<String> taskList = new ArrayList(100);
         System.out.println(welcomeMessage);
         Scanner scanner = new Scanner(System.in);
         String input;
@@ -17,8 +20,19 @@ public class Remy {
             if (input.equals("bye")) {
                 System.out.println(exitMessage);
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println("__________");
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println(" " + (i + 1) + ". " + taskList.get(i));
+                }
+                System.out.println("__________");
+            } else {
+                taskList.add(input);
+                System.out.println("__________");
+                System.out.println(" added: " + input);
+                System.out.println("__________");
             }
-            System.out.println(input + "\n" + "--------");
+
 
         }
     }
