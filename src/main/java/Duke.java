12b399ed10f1eee6a413e2f5684377d47194ada8
TyrusLye <Lyetyrus@gmail.com>
2023-08-24 16:11:51 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,6 +28,24 @@ public class Duke {
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println((i + 1) + ". " + tasks.get(i));
                 }
+            } else if (command.startsWith("todo")) {
+                String description = command.substring(5).trim();
+                tasks.add(new Todo(description));
+                System.out.println("Got it. I've added this task:\n  " + tasks.get(tasks.size() - 1));
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+            } else if (command.startsWith("deadline")) {
+                String description = command.substring(9, command.indexOf("/by")).trim();
+                String by = command.substring(command.indexOf("/by") + 4).trim();
+                tasks.add(new Deadline(description, by));
+                System.out.println("Got it. I've added this task:\n  " + tasks.get(tasks.size() - 1));
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+            } else if (command.startsWith("event")) {
+                String description = command.substring(6, command.indexOf("/from")).trim();
+                String from = command.substring(command.indexOf("/from") + 6, command.indexOf("/to")).trim();
+                String to = command.substring(command.indexOf("/to") + 4).trim();
+                tasks.add(new Event(description, from, to));
+                System.out.println("Got it. I've added this task:\n  " + tasks.get(tasks.size() - 1));
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
             } else if (command.startsWith("mark")) {
                 int index = Integer.parseInt(command.split(" ")[1]) - 1;
                 if (index >= 0 && index < tasks.size()) {
@@ -64,8 +82,8 @@ class Task {
         this.description = description;
         this.isDone = false;
     }
-    public String getDescription() {
-        return description;
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // Return a tick or cross symbol cuz im lazy like that, or its easier. idk
     }
     public boolean isDone() {
         return isDone;
@@ -79,5 +97,47 @@ class Task {
     @Override
     public String toString() {
         return "[" + (isDone ? "X" : " ") + "] " + description;
+    }
+}
+
+class Todo extends Task {
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Deadline extends Task {
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
+
+
+class Event extends Task {
+    protected String from;
+    protected String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 }
