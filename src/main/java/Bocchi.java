b1b031bc7ddb801ec834f1be769693d79e47ee97
Jun Hong <junhongloww@gmail.com>
2023-08-24 18:47:32 +0800

    Add greeting and exit message

--- src/main/java/Bocchi.java
+++ src/main/java/Bocchi.java
@@ -1,7 +1,16 @@
 public class Bocchi {
+    private static final String LINE_BREAK = "___________________________________________________";
+
     /**
      * Main method.
      */
     public static void main(String[] args) {
+
+        System.out.println(LINE_BREAK);
+        System.out.println("Hello! I'm Bocchi");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE_BREAK);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE_BREAK);
     }
 }
