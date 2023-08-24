7cb94078725423b3f99c69215c175141ad86e5f2
tayian <90464339+tayian@users.noreply.github.com>
2023-08-24 21:58:29 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description; // getter
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void toggle() {
+        this.isDone = !this.isDone;
+    }
+}
