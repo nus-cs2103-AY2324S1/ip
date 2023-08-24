87e9f2bb29b37e2764557fcacc0b25f4562cba3b
Loo Mei Lin Zann <>
2023-08-24 11:28:33 +0800

    final changes and adding the @Override

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,4 @@
-public abstract class Task {
+public class Task {
     protected String description;
     protected boolean isDone;
 
--- src/main/java/TehOException.java
+++ src/main/java/TehOException.java
@@ -1,4 +1,5 @@
 public class TehOException extends Exception {
+    @Override
     public String toString() {
         return "☹ OOPS!!! Something is wrong with TehO!";
     }
