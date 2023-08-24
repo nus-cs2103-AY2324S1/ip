33533910ebbf6f990231b60580038c23e7b5551f
ElginTZM <63971687+ElginTZM@users.noreply.github.com>
2023-08-24 23:20:38 +0800

    Add exception handling for invalid inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -34,46 +34,100 @@ public class Duke {
         Duke.respond("Bye. Hope to see you again soon!");
     }
 
-    private static void listTasks(ArrayList<Task> tasks) {
+    private static void listTasks(ArrayList<Task> tasks) throws DukeException{
         ArrayList<String> output = new ArrayList<String>();
 
         for (int i = 0; i < tasks.size(); i++) {
             String taskOutput = String.format("%d. %s", i + 1, tasks.get(i));
             output.add(taskOutput);
         }
+
+        if (output.size() == 0) {
+            Duke.respond("There are no tasks in your list.");
+            return;
+        }
+
         Duke.respond(output);
     }
 
-    private static void markTaskAsDone(int taskIndex) {
-        if (taskIndex < 0 || taskIndex >= tasks.size()) {
-            Duke.respond("That task does not exist. Please type another command.");
-            return;
+    private static void markTaskAsDone(String args) throws DukeException {
+        if (tasks.size() == 0) {
+            throw new DukeException("There are no tasks added. Please add a task first.");
         }
 
-        Task targetTask = tasks.get(taskIndex);
-        targetTask.markAsDone();
+        String taskIndexString = args.split(" ")[0];
 
-        ArrayList<String> messages = new ArrayList<String>();
-        messages.add("Nice! I've marked this task as done:");
-        messages.add(String.format("  %s",targetTask.toString()));
+        try {
+            int taskIndex = Integer.parseInt(taskIndexString) - 1;
+            if (taskIndex < 0) {
+                throw new DukeException("Task number cannot be negative.\n     "
+                                        + "Please retry with a valid task number.");
+            }
 
-        Duke.respond(messages);
+            if (taskIndex >= tasks.size()) {
+                if (tasks.size() == 1) {
+                    throw new DukeException(String.format("Task %d does not exist.\n     "
+                                                          + "Use \"mark 1\" to mark the first task as done.", taskIndex + 1));
+                }
+
+                throw new DukeException(String.format("Task %d does not exist. Use a number between 1 and %d.",
+                                                      taskIndex + 1,
+                                                      tasks.size()));
+            }
+
+
+            Task targetTask = tasks.get(taskIndex);
+            targetTask.markAsDone();
+
+            ArrayList<String> messages = new ArrayList<String>();
+            messages.add("Nice! I've marked this task as done:");
+            messages.add(String.format("  %s",targetTask.toString()));
+
+            Duke.respond(messages);
+        } catch (NumberFormatException e) {
+            throw new DukeException(String.format("Task number provided \"%s\" is not a number.\n     "
+                                                  + "Please retry with a valid task number.", taskIndexString));
+        }
     }
 
-    private static void markTaskAsUndone(int taskIndex) {
-        if (taskIndex < 0 || taskIndex >= tasks.size()) {
-            Duke.respond("That task does not exist. Please type another command.");
-            return;
+    private static void markTaskAsUndone(String args) throws DukeException{
+        if (tasks.size() == 0) {
+            throw new DukeException("There are no tasks added. Please add a task first.");
         }
 
-        Task targetTask = tasks.get(taskIndex);
-        targetTask.markAsUndone();
+        String taskIndexString = args.split(" ")[0];
 
-        ArrayList<String> messages = new ArrayList<String>();
-        messages.add("OK, I've marked this task as not done yet:");
-        messages.add(String.format("  %s",targetTask.toString()));
+        try {
+            int taskIndex = Integer.parseInt(taskIndexString) - 1;
+            if (taskIndex < 0) {
+                throw new DukeException("Task number cannot be negative.\n     "
+                                        + "Please retry with a valid task number.");
+            }
 
-        Duke.respond(messages);
+            if (taskIndex >= tasks.size()) {
+                if (tasks.size() == 1) {
+                    throw new DukeException(String.format("Task %d does not exist.\n     "
+                                                          + "Use \"unmark 1\" to mark the first task as not done.", taskIndex + 1));
+                }
+
+                throw new DukeException(String.format("Task %d does not exist. Use a number between 1 and %d.",
+                                                      taskIndex + 1,
+                                                      tasks.size()));
+            }
+
+
+            Task targetTask = tasks.get(taskIndex);
+            targetTask.markAsDone();
+
+            ArrayList<String> messages = new ArrayList<String>();
+            messages.add("OK, I've marked this task as not done yet:");
+            messages.add(String.format("  %s",targetTask.toString()));
+
+            Duke.respond(messages);
+        } catch (NumberFormatException e) {
+            throw new DukeException(String.format("Task number provided \"%s\" is not a number.\n     "
+                                                  + "Please retry with a valid task number.", taskIndexString));
+        }
     }
 
     private static void respondWithAddedTask(Task newTask) {
@@ -84,10 +138,9 @@ public class Duke {
         Duke.respond(messages);
     }
 
-    private static void addTodo(String description) {
+    private static void addTodo(String description) throws DukeException{
         if (description.trim().equals("")) {
-            Duke.respond("Please specify a description.");
-            return;
+            throw new DukeException("No description specified. Please specify a description.");
         }
 
         Task newTodo = new Todo(description);
@@ -95,7 +148,7 @@ public class Duke {
         Duke.respondWithAddedTask(newTodo);
     }
 
-    private static void addDeadline(String args) {
+    private static void addDeadline(String args) throws DukeException {
         String[] splitArguments = args.split("/");
 
         String description = splitArguments[0];
@@ -108,13 +161,11 @@ public class Duke {
         }
 
         if (description.equals("")) {
-            Duke.respond("Please specify a description.");
-            return;
+            throw new DukeException("No description specified. Please specify a description.");
         }
 
         if (dueDateTime == null || dueDateTime.equals("")) {
-            Duke.respond("Please specify a due date/time.");
-            return;
+            throw new DukeException("No due date/time specified. Please specify a due date/time");
         }
 
         Task newDeadline = new Deadline(description, dueDateTime);
@@ -122,7 +173,7 @@ public class Duke {
         Duke.respondWithAddedTask(newDeadline);
     }
 
-    private static void addEvent(String args) {
+    private static void addEvent(String args) throws DukeException{
         String[] splitArguments = args.split("/");
 
         String description = splitArguments[0].trim();
@@ -140,18 +191,15 @@ public class Duke {
         }
 
         if (description.equals("")) {
-            Duke.respond("Please specify a description.");
-            return;
+            throw new DukeException("No description specified. Please specify a description.");
         }
         
         if (startDatetime == null || startDatetime.equals("")) {
-            Duke.respond("Please specify a start date/time.");
-            return;
+            throw new DukeException("No start date/time specified. Please specify a start date/time.");
         }
 
         if (endDatetime == null|| endDatetime.equals("")) {
-            Duke.respond("Please specify an end date/time.");
-            return;
+            throw new DukeException("No end date/time specified. Please specify an end date/time.");
         }
 
         Task newEvent = new Event(description, startDatetime, endDatetime);
@@ -176,30 +224,36 @@ public class Duke {
             String command = splitInput[0];
             String arguments = splitInput.length > 1 ? splitInput[1] : "";
 
-            switch(command) {
-            case "bye":
-                programRunning = false;
-                break;
-            case "list":
-                Duke.listTasks(tasks);
-                break;
-            case "mark":
-                Duke.markTaskAsDone(Integer.parseInt(arguments) - 1);
-                break;
-            case "unmark":
-                Duke.markTaskAsUndone(Integer.parseInt(arguments) - 1);
-                break;
-            case "todo":
-                Duke.addTodo(arguments);
-                break;
-            case "deadline":
-                Duke.addDeadline(arguments);
-                break;
-            case "event":
-                Duke.addEvent(arguments);
-                break;
-            default:
-                Duke.respond(String.format("Command \"%s\" does not exist. Please type another command.", command));
+            try {
+                switch(command) {
+                case "bye":
+                    programRunning = false;
+                    break;
+                case "list":
+                    Duke.listTasks(tasks);
+                    break;
+                case "mark":
+                    Duke.markTaskAsDone(arguments);
+                    break;
+                case "unmark":
+                    Duke.markTaskAsUndone(arguments);
+                    break;
+                case "todo":
+                    Duke.addTodo(arguments);
+                    break;
+                case "deadline":
+                    Duke.addDeadline(arguments);
+                    break;
+                case "event":
+                    Duke.addEvent(arguments);
+                    break;
+                default:
+                    throw new DukeException(String.format("Command \"%s\" does not exist!"
+                                                          + "Please type another command.", command));
+                }
+            } catch (DukeException e) {
+                Duke.respond(e);
+                continue;
             }
         }
 
