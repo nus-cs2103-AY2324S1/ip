f2860d0fd739b4f6e36b813d08ae352e8294aaa0
andytoh1 <e0773488@u.nus.edu>
2023-08-24 08:52:33 +0800

    no message

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -4,6 +4,7 @@ public class Chatbot {
     public static void main(String[] args) {
         String[] messageList = new String[100];
         boolean[] messageDone = new boolean[100];
+        char[] messageType = new char[100];
         int messageCount = 0;
         Scanner userInput = new Scanner(System.in);
         System.out.println("Hello! I'm Chatbot!");
@@ -16,6 +17,7 @@ public class Chatbot {
                 for (int i = 0; i < messageCount; i++) {
                     int listNumber = i + 1;
                     System.out.print(listNumber + ". ");
+                    System.out.print("[" + messageType[i] + "]");
                     if (messageDone[i]) System.out.print("[X] ");
                     else System.out.print("[ ] ");
                     System.out.println(messageList[i]);
@@ -26,12 +28,25 @@ public class Chatbot {
                 if (userMessage.substring(0, 4).equalsIgnoreCase("mark")) {
                     int doneTask = Integer.parseInt(userMessage.substring(5));
                     System.out.println("Well done! This task has been marked as done.");
-                    System.out.println("[X] " + messageList[doneTask - 1]);
+                    System.out.println("[" + messageType[doneTask] + "]" + "[X] " + messageList[doneTask - 1]);
                     messageDone[doneTask - 1] = true;
                     continue;
+                } else if (userMessage.substring(0, 4).equalsIgnoreCase("todo")) {
+                    userMessage = userMessage.substring(5);
+                    messageType[messageCount] = 'T';
+                } else if (userMessage.substring(0, 8).equalsIgnoreCase("deadline")) {
+                    int index = userMessage.indexOf("/by");
+                    userMessage = userMessage.substring(9, index) + "(by: " + userMessage.substring(index + 4) + ")";
+                    messageType[messageCount] = 'D';
+                } else if (userMessage.substring(0, 5).equalsIgnoreCase("event")) {
+                    int index = userMessage.indexOf("/from");
+                    int index2 = userMessage.indexOf("/to");
+                    userMessage = userMessage.substring(6, index) + "(from: " + userMessage.substring(index + 6, index2)
+                        + "to: " + userMessage.substring(index2 + 4) + ")";
+                    messageType[messageCount] = 'E';
                 }
             }
-            System.out.println("Added: " + userMessage);
+            System.out.println("Added this task: " + "[" + messageType[messageCount] + "] " + userMessage);
             messageList[messageCount] = userMessage;
             messageCount++;
         }
