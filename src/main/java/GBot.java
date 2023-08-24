294712380e56f3810bf59427f0ea882cdf90dfcb
gongg21 <gallen.ong21@gmail.com>
2023-08-25 00:20:26 +0800

    no message

---
+++ src/main/java/GBot.java
@@ -0,0 +1,8 @@
+public class GBot {
+    public static void main(String[] args) {
+        System.out.println("Hello I'm GBot\n");
+        System.out.println("What can I do for you today?\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+}
+
