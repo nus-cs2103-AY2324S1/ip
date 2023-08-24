d3810d2c4749022ecf9616405fad01c60c5fa846
ElginTZM <63971687+ElginTZM@users.noreply.github.com>
2023-08-24 16:51:08 +0800

    Add echo functionality to chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,52 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String LINE_SEPARATOR = "    ------------------------------------------------------------";
+
+    private static void respond(String message) {
+        System.out.println(LINE_SEPARATOR);
+        System.out.println("     " + message);
+        System.out.println(LINE_SEPARATOR);
+        System.out.print(">> ");
+    }
+
+    private static void respond(String[] messages) {
+        System.out.println(LINE_SEPARATOR);
+        for (int i = 0; i < messages.length; i++) {
+                System.out.println("     " + messages[i]);
+        }
+        System.out.println(LINE_SEPARATOR);
+        System.out.print(">> ");
+    }
+
+    
+    private static void greet() {
+        String[] messages = new String[] {"Hello! I'm A-CAT (Automated Chatbot Assistant for Tasks)", "What do you want to do today?"};
+        Duke.respond(messages);
+    }
+
+    private static void exit() {
+        Duke.respond("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
-        System.out.println("------------------------------------------------------------");
-        System.out.println(" Hello! I'm A-CAT (Automated Chatbot Assistant for Tasks)");
-        System.out.println(" What do you want to do today?");
-        System.out.println("------------------------------------------------------------");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("------------------------------------------------------------");
+        Duke.greet();
+
+        Scanner scanner = new Scanner(System.in);
+
+        Boolean programRunning = true;
+
+        while (programRunning) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                programRunning = false;
+            } else {
+                Duke.respond(input);
+            }
+        }
+
+        scanner.close();
+        Duke.exit();
     }
 }
