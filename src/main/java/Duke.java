d956743dcb81f1942378938f40df8ee2d39b5945
Ken-Lai <weizhong9201@gmail.com>
2023-08-24 16:11:24 +0800

    Echoes command from user and exits when bye is entered

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,45 @@
+import java.util.Scanner;
+
 public class Duke {
     private static final String SEPARATION_LINE  = "____________________________________________________________";
-    private static final String GREETING_TEXT = "Hello, I'm Tasket\nWhat can I do for you?";
+    private static final String INDENTATION = "    ";
+    private static final String GREETING_TEXT = "Hello, I'm Tasket\n    What can I do for you?";
     private static final String GOODBYE_TEXT = "Bye. Hope to see you again soon!";
 
+    public static void showSeparationLine() {
+        System.out.println(INDENTATION + SEPARATION_LINE);
+    }
+
     public static void showGreetingText() {
-        System.out.println(SEPARATION_LINE);
-        System.out.println(GREETING_TEXT);
-        System.out.println(SEPARATION_LINE);
+        showSeparationLine();
+        System.out.println(INDENTATION + GREETING_TEXT);
+        showSeparationLine();
     }
 
     public static void showGoodbyeText() {
-        System.out.println(GOODBYE_TEXT);
-        System.out.println(SEPARATION_LINE);
+        System.out.println(INDENTATION + GOODBYE_TEXT);
+        showSeparationLine();
+    }
+
+    public static void echoInputText(String string) {
+        System.out.println(string);
+        showSeparationLine();
     }
 
     public static void main(String[] args) {
+        String prompt = "";
+        Scanner sc = new Scanner(System.in);
+
         showGreetingText();
+
+        while (!prompt.equals("bye")) {
+            prompt = sc.next();
+            showSeparationLine();
+
+            echoInputText(prompt);
+        }
+
+        sc.close();
         showGoodbyeText();
     }
 }
