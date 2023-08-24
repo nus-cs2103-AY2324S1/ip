09cc7562ad2f5f29bd484720d2b038125a9116f6
Singa-Pirate <E0969244@u.nus.edu>
2023-08-24 19:23:07 +0800

    Level-3: mark as done

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ChatBot {
     static final String name = "4F5DA2";
     static final String line = "\t____________________________________________________________";
     static final Scanner scanner = new Scanner(System.in);
+    static final int maxNumberOfTasks = 100;
+
+    static ArrayList<Task> taskList = new ArrayList<>(maxNumberOfTasks);
 
     public static void greet() {
         System.out.println(line);
@@ -24,12 +28,47 @@ public class ChatBot {
         System.out.println(line);
     }
 
+    public static void addTask(String name) {
+        taskList.add(new Task(name));
+        output(String.format("\tadded: %s", name));
+    }
+
+    public static void markAs(boolean isDone, int index) {
+        Task task = taskList.get(index-1);
+        task.markAs(isDone);
+        output(String.format("\t%s\n\t[%s] %s",
+                isDone ? "Nice! I've marked this task as done:"
+                       : "OK, I've marked this task as not done yet:",
+                isDone ? "X" : " ",
+                task.getName()));
+    }
+
+    public static void listTasks() {
+        System.out.println(line);
+        for (int i = 0; i < taskList.size(); i++) {
+            Task task = taskList.get(i);
+            System.out.println(String.format("\t%d.[%s] %s",
+                    i+1,
+                    task.checkIsDone() ? "X" : " ",
+                    task.getName()));
+        }
+        System.out.println(line);
+    }
+
     public static void nextCommand() {
         String command = scanner.nextLine();
         if (command.equals("bye")) {
             exit();
+        } else if (command.equals("list")) {
+            listTasks();
+            nextCommand();
         } else {
-            output("\t" + command);
+            String[] words = command.split(" ");
+            if (words[0].equals("mark") || words[0].equals("unmark")) {
+                markAs(words[0].equals("mark"), Integer.parseInt(words[1]));
+            } else {
+                addTask(command);
+            }
             nextCommand();
         }
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    private String name;
+    private boolean isDone;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean checkIsDone() {
+        return isDone;
+    }
+
+    public void markAs(boolean isDone) {
+        this.isDone = isDone;
+    }
 }
