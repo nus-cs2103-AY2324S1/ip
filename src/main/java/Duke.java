d43e1bd978dfa70c7e185bb0952e169d12c31f74
FerdiHS <ferdi.fhs.fhs@gmail.com>
2023-08-24 14:55:51 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,12 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String message = "____________________________________________________________\n" +
+                            "Hello! I'm Not A ChatBot\n" + 
+                            "What can I do for you?\n" +
+                            "____________________________________________________________\n" + 
+                            "Bye. Hope to see you again soon!\n" +
+                            "____________________________________________________________";
+        System.out.println(message);
     }
 }
