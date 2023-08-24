3be31f30d34e02aa4f97163caa5f6930a020f4c7
yongning0310 <85162884+yongning0310@users.noreply.github.com>
2023-08-24 14:46:40 +0800

    created new class for task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,23 +14,45 @@ public class Duke {
         System.out.println("What can I do for you?\n");
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> inputs = new ArrayList<>();
+        ArrayList<Task> inputs = new ArrayList<>();
         while (true) {
             String input = scanner.nextLine();
             if ("bye".equals(input)) {
                 System.out.println("Bye. Hope to see you again soon!\n");
-                break; // This will exit the loop and end the program
+                break;
             } else {
                 if ("list".equals(input)) {
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 1; i < inputs.size()+1; i++) {
-                        System.out.println(i + ". " + inputs.get(i-1));
+                        Task task = inputs.get(i - 1);
+                        System.out.println(i + ". " + task.getStatusAndDescription());
+                    }
+                } else if (input.contains("mark")) {
+                    String[] parts = input.split(" "); // Splitting the string by space
+                    String lastPart = parts[parts.length - 1]; // Taking the last part
+                    try {
+                        int number = Integer.parseInt(lastPart); // Parsing the last part as an integer
+                        Task task = inputs.get(number-1);
+                        if (input.contains("unmark")) {
+                            System.out.println("OK, I've marked this task as not done yet:");
+                            task.markAsNotDone();
+                        }  else if (input.contains("mark")) {
+                            System.out.println("Nice! I've marked this task as done:");
+                            task.markAsDone();
+                        }
+                        System.out.println("  " + task.getStatusAndDescription());
+                    } catch (NumberFormatException e) {
+                        System.out.println("No number found at the end of the string.");
                     }
                 } else {
-                    inputs.add(input);
-                    System.out.println("added: " + input);
+                    Task newTask = new Task(input);
+                    inputs.add(newTask);
+                    System.out.println("added: " + newTask.getDescription());
                 }
             }
         }
+
+        // invalid inputs, invalid numbers, invalid commands
 
         scanner.close();
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String getStatusAndDescription() {
+        return String.format("[%s] %s", this.getStatusIcon(),this.description );
+    }
+
+    public String getDescription() {
+        return String.format(this.description );
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+}
