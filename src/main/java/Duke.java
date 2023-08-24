29c53d82794599dafbcf3a7946a309fb11fca4d7
Winston <leonardwinstoncx9@gmail.com>
2023-08-24 12:47:45 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+
+        String greeting =
+                "____________________________________________________________\n" +
+                " Hello! I'm CHAT\n" +
+                "____________________________________________________________\n" +
+                " What can I do for you?\n Bye. Hope to see you again\n" +
+               "____________________________________________________________\n";
+        System.out.println(greeting);
     }
 }
