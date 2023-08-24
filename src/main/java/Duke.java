e787142280e986a8e8e5999708fe1a96e421a654
scarletblanks <irisu.nita@gmail.com>
2023-08-24 17:13:28 +0800

    Level-0 Name Change

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo =
+                "___________  __________  __________  ||   \n"
+                +"|         | |         | |         |  ||      \n"
+                +"-----------  ---------- ----------   ||  \n"
+                + "    ||         ||          ||        ||       \n"
+                + "    ||         ||          ||        ||           \n"
+                + "    ||         ||          ||        ||           \n"
+                + "    ||         ||          ||        ||       \n"
+                + "    ||      __________  __________   ||      \n"
+                + "    ||      |         | |         |  ______ \n"
+                + "    ||      ----------  ----------   ______    \n";
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm \n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
