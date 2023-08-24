65f876dfc5284e4cb9fa5ba347ec43964cbecd00
andrefoo <76435197+andrefoo@users.noreply.github.com>
2023-08-24 15:03:57 +0800

    Level-1 Echo

--- src/main/java/Dre.java
+++ src/main/java/Dre.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Dre {
     public static void greet() {
         System.out.println("____________________________________________________________");
@@ -11,8 +13,18 @@ public class Dre {
         System.out.println("____________________________________________________________");
     }
 
+    public static void echo() {
+        Scanner sc = new Scanner(System.in);
+        String next = sc.nextLine();
+        while (!next.equals("bye")) {
+            System.out.println(next);
+            next = sc.nextLine();
+        }
+        exit();
+    }
+
     public static void main(String[] args) {
         greet();
-        exit();
+        echo();
     }
 }
