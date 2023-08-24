a2b11215bdddd8c8622e6edebee9531891ca1803
Goh Li Ting <e0970558@u.nus.edu>
2023-08-24 16:56:41 +0800

    Add ability to delete tasks

--- src/main/java/ChatBuddy.java
+++ src/main/java/ChatBuddy.java
@@ -1,31 +1,24 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ChatBuddy {
     // store a list of tasks
-    static Task[] list = new Task[100];
-    static int numOfTasks = 0;
+    private static ArrayList<Task> list = new ArrayList<>();
 
     /**
      * Add task to list array.
      *
      * @param task The task to add into the list of tasks.
-     * @throws ChatBuddyException if task list is full.
      */
-    private static void addTask(Task task) throws ChatBuddyException {
-        // check if task list is filled
-        if (numOfTasks >= list.length) {
-            throw new ChatBuddyException("The task list is full! No more tasks can be added!");
-        }
-
+    private static void addTask(Task task) {
         // add task to list
-        list[numOfTasks] = task;
-        numOfTasks++;
+        list.add(task);
 
         // display message
         printHorizontalLine();
         System.out.println("    Got it. I've added this task:");
         System.out.println("      " + task.toString());
-        System.out.println(String.format("    Now you have %d tasks in the list.", numOfTasks));
+        System.out.println(String.format("    Now you have %d tasks in the list.", list.size()));
         printHorizontalLine();
     }
 
@@ -116,12 +109,33 @@ public class ChatBuddy {
         addTask(event);
     }
 
+    /**
+     * Method to delete a task from the list.
+     *
+     * @param userInput The input provided by the user.
+     * @throws ChatBuddyException if the task number inputted by the user is not a valid task number.
+     */
+    private static void deleteTask(String userInput) throws ChatBuddyException {
+        String indexString = userInput.substring(7);
+        int taskIndex = Integer.parseInt(indexString) - 1;
+        if (taskIndex >= list.size()) {
+            throw new ChatBuddyException("Please input a valid task number. There are only " +
+                    list.size() + " tasks in the list.");
+        }
+        Task task = list.remove(taskIndex);
+        printHorizontalLine();
+        System.out.println("    Noted. I've removed this task:");
+        System.out.println("      " + task.toString());
+        System.out.println(String.format("    Now you have %d tasks in the list.", list.size()));
+        printHorizontalLine();
+    }
+
     /** Displays the list of tasks. */
     private static void printTaskList() {
         printHorizontalLine();
         System.out.println("    Here are the tasks in your list:");
-        for (int i = 0; i < list.length; i++) {
-            Task task = list[i];
+        for (int i = 0; i < list.size(); i++) {
+            Task task = list.get(i);
             if (task == null) {
                 // there are no more tasks
                 break;
@@ -154,20 +168,20 @@ public class ChatBuddy {
                 } else if (userInput.matches("mark [1-9][0-9]*")) {
                     String indexString = userInput.substring(5);
                     int taskIndex = Integer.parseInt(indexString) - 1;
-                    if (taskIndex >= numOfTasks) {
+                    if (taskIndex >= list.size()) {
                         throw new ChatBuddyException("Please input a valid task number. There are only " +
-                                numOfTasks + " tasks in the list.");
+                                list.size() + " tasks in the list.");
                     }
-                    Task task = list[taskIndex];
+                    Task task = list.get(taskIndex);
                     task.markAsDone();
                 } else if (userInput.matches("unmark [1-9][0-9]*")) {
                     String indexString = userInput.substring(7);
                     int taskIndex = Integer.parseInt(indexString) - 1;
-                    if (taskIndex >= numOfTasks) {
+                    if (taskIndex >= list.size()) {
                         throw new ChatBuddyException("Please input a valid task number. There are only " +
-                                numOfTasks + " in the list.");
+                                list.size() + " tasks in the list.");
                     }
-                    Task task = list[taskIndex];
+                    Task task = list.get(taskIndex);
                     task.markAsNotDone();
                 } else if (userInput.startsWith("todo")) {
                     addToDo(userInput);
@@ -175,6 +189,8 @@ public class ChatBuddy {
                     addDeadline(userInput);
                 } else if (userInput.startsWith("event")) {
                     addEvent(userInput);
+                } else if (userInput.matches("delete [1-9][0-9]*")) {
+                    deleteTask(userInput);
                 } else {
                     throw new ChatBuddyException("I'm sorry, but I don't know what that means :-(");
                 }
