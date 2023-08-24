c712c9a79710aaac1957fb856f46ad1739878a45
LIN <peterway6933@gmail.com>
2023-08-24 16:17:21 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Alex.java
@@ -0,0 +1,11 @@
+public class Alex {
+    public static void main(String[] args) {
+        String greeting = "_____________________________________________________________\n"
+                        + "Hello! I'm your personal task assistant, Alex\n"
+                        + "What can I do for you today?\n\n"
+                        + "_____________________________________________________________\n"
+                        + "Bye. Hope to see you again soon!\n"
+                        + "_____________________________________________________________\n";
+        System.out.println(greeting);
+    }
+}
