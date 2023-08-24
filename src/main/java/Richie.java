4fc07c1eb59059fef45006814ed7543501879328
Richard Hew <richardhewjialiang9@gmail.com>
2023-08-24 13:36:24 +0800

    Allows chatbot to echo user commands and detects the "bye" command to exit

--- src/main/java/Richie.java
+++ src/main/java/Richie.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 public class Richie {
     static private String CHATBOT_NAME = "Richie";
     public static void main(String[] args) {
         System.out.println("Hello! I'm " + CHATBOT_NAME + "\nWhat can I do for you?");
+        Scanner input = new Scanner(System.in);
+        String message = input.next();
+        while (!message.equals("bye")) {
+            System.out.println(message);
+            message = input.next();
+        }
         System.out.println("Bye. Hope to see you again soon!");
 
     }
