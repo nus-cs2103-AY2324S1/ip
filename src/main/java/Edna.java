5130bfd831bafc919575b2c39a7f84f9a089347c
Qin Nanxin <nanxinqin@hotmail.com>
2023-08-24 21:42:33 +0800

    [Level 2. Add, List] Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Edna.java
+++ src/main/java/Edna.java
@@ -1,21 +1,44 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Edna {
+    public static ArrayList<String> inputList = new ArrayList<String>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input;
 
-        String partition = "\n----------------------------------------\n";
+        String partition = "----------------------------------------";
 
-        System.out.println(partition + "Hello! I'm Edna.");
-        System.out.print("What can I do for you?" + partition);
+        System.out.println(partition);
+        System.out.println("Hello! I'm Edna.");
+        System.out.println("What can I do for you?");
+        System.out.println(partition);
+
         input = sc.nextLine();
 
         while(input.equals("bye") == false) {
-            System.out.print(input + partition);
+            if (input.equals("list")) {
+                 print();
+                 System.out.println(partition);
+            } else {
+                add(input);
+                System.out.println(partition);
+            }
             input = sc.nextLine();
         }
 
-        System.out.print("Bye. Hope to see you again soon!" + partition);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(partition);
+    }
+
+    public static void add(String input) {
+        inputList.add(input);
+        System.out.println("added: " + input);
+    }
+
+    public static void print() {
+        int num = 1;
+        for(String temp: inputList) {
+            System.out.println(num + ". " + temp);
+            num++;
+        }
     }
 }
