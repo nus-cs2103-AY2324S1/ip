a0cdce0825cbab51b97a51b68cbdba62aeab2426
wnchan <e0970469@u.nus.edu>
2023-08-24 17:10:35 +0800

    Level-3: Mark as Done

--- src/main/java/Dukduk.java
+++ src/main/java/Dukduk.java
@@ -7,7 +7,7 @@ public class Dukduk {
         System.out.println(" What can I do for you?");
         printLine();
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -23,12 +23,30 @@ public class Dukduk {
                 if (taskCount == 0) {
                     System.out.println(" No tasks added yet.");
                 } else {
+                    System.out.println(" Here are the tasks in your list:");
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                        System.out.println(" " + (i + 1) + ".[" + tasks[i].getStatusIcon()
+                                + "] " + tasks[i].getDescription());
                     }
                 }
+            } else if (input.startsWith("mark")) {
+                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].markAsDone();
+                    System.out.println(" Nice! I've marked this task as done:\n ["
+                            + tasks[taskIndex].getStatusIcon() + "] "
+                            + tasks[taskIndex].getDescription());
+                }
+            } else if (input.startsWith("unmark")) {
+                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].unmark();
+                    System.out.println(" OK, I've marked this task as not done yet:\n ["
+                            + tasks[taskIndex].getStatusIcon() + "] "
+                            + tasks[taskIndex].getDescription());
+                }
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println(" added: " + input);
             }
@@ -41,3 +59,4 @@ public class Dukduk {
         System.out.println("____________________________________________________________");
     }
 }
+
