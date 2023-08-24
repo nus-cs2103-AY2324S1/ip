ac8880ee77d6b73175774bc210526b3bbd85ce69
andrefoo <76435197+andrefoo@users.noreply.github.com>
2023-08-24 14:33:48 +0800

    Level-0 Rename, Greet, Exit

---
+++ src/main/java/Dre.java
@@ -0,0 +1,18 @@
+public class Dre {
+    public static void greet() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Dre");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void exit() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        greet();
+        exit();
+    }
+}
