678bee455ef90fe42d44eb82243afe4683a5f018
angkyakdifp <akdiangky@gmail.com>
2023-08-24 14:55:27 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,12 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String Introduction = "____________________________________________________________\n" +
+                        " Hello! I'm FootyCouch\n" +
+                        " What can I do for you?\n" +
+                        "____________________________________________________________\n" +
+                        " Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________";
+        System.out.println(Introduction);
     }
 }
