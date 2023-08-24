f88b6b32bb87fbb42eb9debc9cd5901b5b9f9f49
Tang-Moyan <e0817857@u.nus.edu>
2023-08-24 23:28:29 +0800

    Level 0 Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,13 @@
 public class Duke {
+    private static final String MyName = "Rio";
+    public static void Greet() {
+        System.out.println("Hello! I'm " + MyName);
+        System.out.println("What can I do for you?");
+    }
+
+    public  static void Exit() {
+        System.out.println(" Bye. Hope to see you again soon!");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
