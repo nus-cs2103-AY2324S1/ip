06ae568938319485767404dd8caea1783996f1df
Taanish Bhardwaj <btaanish07@gmail.com>
2023-08-24 11:43:57 +0800

    Completing Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "____________________________________________________________\n";
+        System.out.println(line + " Hello! I'm Alcazar\n" +
+                " What can I do for you?\n" +
+                line +
+                " Bye. Hope to see you again soon!\n" +
+                line);
+    }
+    public static String inputText() {
+        Scanner sc =  new Scanner(System.in);
+        String inp = sc.nextLine();
+        return inp;
     }
 }
