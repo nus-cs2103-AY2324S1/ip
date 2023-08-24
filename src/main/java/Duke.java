5617186079a95f4ada94008db3fedfe3152129e5
jovkusuma <jovankusuma1@gmail.com>
2023-08-24 20:02:39 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,31 +18,55 @@ public class Duke {
             String[] splitted = input.split(" ");
             System.out.println("----------------------------------------------");
             if(input.equalsIgnoreCase("bye")) {
-                System.out.println("----------------------------------------------");
                 System.out.println(bye);
                 System.out.println("----------------------------------------------");
                 break;
 
             } else if(input.equalsIgnoreCase("list")) {
-                System.out.println("----------------------------------------------");
                 System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < count; i++) {
-                    if (count == 0) {
-                        System.out.println("You currently have no tasks!");
-                    } else {
-                        System.out.println((i + 1) + "." + tasksList[i].toString());
-                    }
+                if (count == 0) {
+                    System.out.println("\t You currently have no tasks!");
                 }
+
+                for (int i = 0; i < count; i++) {
+                        System.out.println((i + 1) + "." + tasksList[i].toString());
+                }
+
                 System.out.println("----------------------------------------------");
-            } else if (splitted[0].equalsIgnoreCase("mark")) {
-                tasksList[Integer.parseInt(splitted[1]) - 1].markDone();
-            } else if (splitted[0].equalsIgnoreCase("unmark")) {
-                tasksList[Integer.parseInt(splitted[1]) - 1].unmarkDone();
+            } else if (input.startsWith("mark")) {
+                tasksList[Integer.parseInt(input.replace("mark ", "")) - 1].markDone();
+            } else if (input.startsWith("unmark")) {
+                tasksList[Integer.parseInt(input.replace("unmark ", "")) - 1].unmarkDone();
             } else {
-                System.out.println("\t added: " + input);
-                tasksList[count] = new Task(input);
+                //System.out.println("Got it. I've added this task:");
+                if(input.startsWith("todo")) {
+
+                    tasksList[count] = new Todo(input.replace("todo", ""));
+
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("\t" + tasksList[count].toString());
+                    System.out.println("Now you have " + (count + 1) + " tasks in the list");
+                    System.out.println("----------------------------------------------");
+                } else if(input.startsWith("event")) {
+                    //event project meeting /from Mon 2pm /to 4pm
+                    String[] s = input.replace("event ", "").split(" /from | /to");
+                    tasksList[count] = new Event(s[0], s[1], s[2]);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("\t" + tasksList[count].toString());
+                    System.out.println("Now you have " + (count + 1) + " tasks in the list");
+                    System.out.println("----------------------------------------------");
+
+                } else if (input.startsWith("deadline")) {
+                    //deadline return book /by Sunday
+                    String[] s = input.replace("deadline ", "").split(" /by ");
+                    tasksList[count] = new Deadline(s[0], s[1]);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("\t" + tasksList[count].toString());
+                    System.out.println("Now you have " + (count + 1) + " tasks in the list");
+                    System.out.println("----------------------------------------------");
+                }
                 count++;
-                System.out.println("----------------------------------------------");
+
             }
         }
     }
---
+++ src/main/java/Event.java
@@ -0,0 +1,14 @@
+public class Event extends Task {
+    protected String from;
+    protected String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + "to: " + to + ")";
+    }
+}
