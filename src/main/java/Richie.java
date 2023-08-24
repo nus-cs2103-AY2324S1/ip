41d1a8ce17fbf72d5034613b68b0938925fcfd5e
Richard Hew <richardhewjialiang9@gmail.com>
2023-08-24 13:14:51 +0800

    Renamed Chat bot from Duke to Richie and implemented an initial skeletal version of the chatbot (ie. greeting the user and exit)

---
+++ src/main/java/Richie.java
@@ -0,0 +1,8 @@
+public class Richie {
+    static private String CHATBOT_NAME = "Richie";
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm " + CHATBOT_NAME + "\nWhat can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+
+    }
+}
