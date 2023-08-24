a7b18aff1734013410d1f9cbb8ea51ac7e398d68
angkyakdifp <akdiangky@gmail.com>
2023-08-24 15:49:46 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Duke {
+    private void run() {
+        Scanner scanner = new Scanner(System.in);
+        String Input = scanner.nextLine();
+        while(!Input.equals("bye")) {
+            String output = "____________________________________________________________\n" +
+                    " " + Input + "\n" +
+                    "____________________________________________________________\n";
+            System.out.println(output);
+            Input = scanner.nextLine();
+        }
+    }
     public static void main(String[] args) {
+        Duke duke = new Duke();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -8,9 +21,12 @@ public class Duke {
         String Introduction = "____________________________________________________________\n" +
                         " Hello! I'm FootyCouch\n" +
                         " What can I do for you?\n" +
-                        "____________________________________________________________\n" +
+                        "____________________________________________________________\n";
+        String Exit =   "____________________________________________________________\n" +
                         " Bye. Hope to see you again soon!\n" +
                         "____________________________________________________________";
         System.out.println(Introduction);
+        duke.run();
+        System.out.println(Exit);
     }
 }
