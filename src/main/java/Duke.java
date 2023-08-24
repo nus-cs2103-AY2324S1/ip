803a54d5de297844b362c1bd7b4732f025a986dd
teeheehoo\jierongu <jiarongchoo@gmail.com>
2023-08-24 17:17:37 +0800

    added a task class with the ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<String> inputHistory = new ArrayList<>();
+        List<Task> inputHistory = new ArrayList<>();
 
         System.out.println("Hello! I'm Gideon");
         System.out.println("What can I do for you?");
@@ -16,11 +16,24 @@ public class Duke {
                 break;
             } else if (userInput.equalsIgnoreCase("list")) {
                 for(int i = 0; i < inputHistory.size(); i++) {
-                    System.out.println((i + 1) + ". " + inputHistory.get(i));
+                    System.out.println((i + 1) + ". " + inputHistory.get(i).getDescription());
                 }
+            } else if (userInput.startsWith("mark")) {
+                int id = Integer.parseInt(userInput.split(" ")[1]);
+                Task markedTask = inputHistory.get(id - 1);
+                markedTask.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(markedTask.getDescription());
+            } else if (userInput.startsWith("unmark")) {
+                int id = Integer.parseInt(userInput.split(" ")[1]);
+                Task unmarkedTask = inputHistory.get(id - 1);
+                unmarkedTask.markAsUnDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(unmarkedTask.getDescription());
             } else {
-                System.out.println("added: " + userInput);
-                inputHistory.add(userInput);
+                Task t = new Task(userInput);
+                inputHistory.add(t);
+                System.out.println("added: " + t.getDescription());
             }
         }
         scanner.close();
