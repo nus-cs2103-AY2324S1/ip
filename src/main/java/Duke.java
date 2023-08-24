73a159e353fcad09f1f072f9ff5a66201988d102
marioalvaro <marioalvaro131@gmail.com>
2023-08-24 16:45:25 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,12 +10,36 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         //System.out.println("Hello from\n" + logo);
 
-        String greet = "____________________________________________________________\n" +
-                " Hello! I'm Siri\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
+        String greet = "   ____________________________________________________________\n" +
+                "    Hello! I'm Siri\n" +
+                "    What can I do for you?\n" +
+                "   ____________________________________________________________\n";
         System.out.println(greet);
+
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+
+        boolean bye = false;
+        while (!bye) {
+            if (s == null) {
+                s = in.nextLine();
+            }
+
+            if (s.equals("bye")) {
+                bye = true;
+
+                String exit = "    ____________________________________________________________\n" +
+                        "     Bye. Hope to see you again soon!\n" +
+                        "    ____________________________________________________________";
+                System.out.println(exit);
+            } else {
+
+                String response = "    ____________________________________________________________\n" +
+                        "     " + s + "\n" +
+                        "    ____________________________________________________________\n";
+                System.out.println(response);
+                s = null;
+            }
+        }
     }
 }
