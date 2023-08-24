43a89002e7a6bfc310c02f6f44254dbfa2e9a369
hyc17003 <e0970589@u.nus.edu>
2023-08-24 15:56:19 +0800

    Add ChatterBot.java

---
+++ src/main/java/ChatterBot.java
@@ -0,0 +1,7 @@
+public class ChatterBot {
+    public static void main(String[] args) {
+        String logo = "ChatterBot";
+        System.out.println("Hello! I'm " + logo + "\nWhat can I do for you?"
+                + "\nBye. Hope to see you again soon!");
+    }
+}
