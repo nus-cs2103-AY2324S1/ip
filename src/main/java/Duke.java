2f05e5e007dce0a0c022005365078a5cfa0356bf
DESKTOP-N8BNBE3\Juanpa <juanpaabola+coding@gmail.com>
2023-08-24 14:56:24 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        TaskList taskList = new TaskList();
+        int total = 0;
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -16,9 +18,6 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println(line);
 
-        String[] lst = new String[100];
-        int total = 0;
-
         boolean running = true;
         while (running) {
             String command = sc.nextLine();
@@ -29,15 +28,33 @@ public class Duke {
                 running = false;
             } else if (command.equals("list")) {
                 System.out.println(line);
+                System.out.println("Here are your tasks:");
                 for (int count = 1; count <= total; count++) {
-                    System.out.println(count + ". " + lst[count - 1]);
+                    System.out.println(count + ". " + taskList.getTask(count - 1).toString());
                 }
+                System.out.println(line);
+            } else if (command.startsWith("mark")) {
+                String[] temp = command.split(" ", 2);
+                int ind = Integer.parseInt(temp[1]);
+                taskList.mark(ind-1);
+                System.out.println(line);
+                System.out.println("It is accomplished:");
+                System.out.println(taskList.getTask(ind-1).toString());
+                System.out.println(line);
+            } else if (command.startsWith("unmark")) {
+                String[] temp = command.split(" ", 2);
+                int ind = Integer.parseInt(temp[1]);
+                taskList.unmark(ind-1);
+                System.out.println(line);
+                System.out.println("It is unfinished:");
+                System.out.println(taskList.getTask(ind-1).toString());
                 System.out.println(line);
             } else {
                 System.out.println(line);
                 System.out.println("added: " + command);
                 System.out.println(line);
-                lst[total] = command;
+                Task newTask = new Task(command);
+                taskList.addTask(newTask);
                 total++;
             }
         }
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    private static List<Task> taskLists;
+
+    public TaskList() {
+        taskLists = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        taskLists.add(task);
+    }
+
+    public Task getTask(int index) {
+        return taskLists.get(index);
+    }
+
+    public void mark(int index) {
+        taskLists.get(index).markDone();
+    }
+
+    public void unmark(int index) {
+        taskLists.get(index).markUndone();
+    }
+}
