0761fcd2b9cccc4a8a212d2b314cbd261b64cb9c
heran lang <namedx9@gmail.com>
2023-08-24 10:58:51 +0800

    add different tasks

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,20 @@
+public class Deadline extends Task {
+
+    protected String by;
+
+    /**
+     * Constructs a Deadline object with the given description and deadline.
+     *
+     * @param description The description of the task.
+     * @param by          The deadline for the task.
+     */
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
---
+++ src/main/java/ToDo.java
@@ -0,0 +1,15 @@
+public class ToDo extends Task{
+    /**
+     * Constructs a ToDo object with the given description.
+     *
+     * @param description The description of the to-do task.
+     */
+    public ToDo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
