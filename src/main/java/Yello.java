97da718ce546a965553a0eceb1fe83f0c0c920fe
iantsaii <iantsai@Ians-MacBook-Pro.local>
2023-08-24 15:45:27 +0800

    Greet and Exit

--- src/main/java/Yello.java
+++ src/main/java/Yello.java
@@ -1,10 +1,8 @@
 public class Yello {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String LINE = "____________________________________________________________";
+        System.out.println(LINE + "\nHello! I'm Yello\n" +
+                "What can I do for you?\n" + LINE);
+        System.out.println("Bye. Hope to see you again soon!\n" + LINE);
     }
 }
