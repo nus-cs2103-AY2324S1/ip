c169e3d1479d9d0cc48783eb4d84dcd644b3d18a
Yucongkoo <yucongkoo0214@gmail.com>
2023-08-24 10:54:59 +0800

    Implementation of Level-0 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,29 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcomeMsg = "Hello from\n" + logo;
+        printOutput(welcomeMsg);
+
+        // greet the users
+        String greetings = "Hello! I'm Orion\n"
+                + "What can I do for you?\n";
+        printOutput(greetings);
+
+        printOutput("Bye. Hope to see you again soon!\n");
+    }
+
+    private static void drawLine() {
+        int lineLength = 60; // Adjust the length of the line as needed
+        char horizontalLineChar = '\u2500'; // Unicode character for a horizontal line
+
+        for (int i = 0; i < lineLength; i++) {
+            System.out.print(horizontalLineChar);
+        }
+        System.out.println();
+    }
+
+    private static void printOutput(String output) {
+        System.out.println(output);
+        drawLine();
     }
 }
