4d307d87d9dc2246865facccce42d567818c6357
jonyeokj <joyekajie@gmail.com>
2023-08-24 23:28:25 +0800

    Added isDone Functionality and Task.java

--- src/main/java/Martin.java
+++ src/main/java/Martin.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Martin {
-    private static List<String> addList = new ArrayList<>();
+    private static List<Task> tasks = new ArrayList<>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -16,8 +16,12 @@ public class Martin {
                 break;
             } else if (input.equalsIgnoreCase("list")) {
                 printTasks();
+            } else if (input.startsWith("mark")) {
+                markTask(input);
+            } else if (input.startsWith("unmark")) {
+                unmarkTask(input);
             } else {
-                addList.add(input);
+                tasks.add(new Task(input));
                 printMessage("added: " + input);
             }
         }
@@ -31,9 +35,39 @@ public class Martin {
 
     private static void printTasks() {
         System.out.println("    ____________________________________________________________");
-        for (int i = 0; i < addList.size(); i++) {
-            System.out.println("     " + (i + 1) + ". " + addList.get(i));
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println("     " + (i + 1) + ". " + tasks.get(i));
         }
         System.out.println("    ____________________________________________________________");
+    }
+
+    private static void markTask(String command) {
+        try {
+            int taskNo = Integer.parseInt(command.split(" ")[1]);
+            Task task = tasks.get(taskNo - 1);
+            if (task.isDone()) {
+                printMessage("Task \"" + task.getDescription() + "\" is already done.");
+            } else {
+                task.markAsDone();
+                printMessage("Nice! I've marked this task as done:\n       " + task);
+            }
+        } catch (Exception e) {
+            printMessage("Invalid task number.");
+        }
+    }
+
+    private static void unmarkTask(String command) {
+        try {
+            int taskNo = Integer.parseInt(command.split(" ")[1]);
+            Task task = tasks.get(taskNo - 1);
+            if (!task.isDone()) {
+                printMessage("Task \"" + task.getDescription() + "\" is not done yet.");
+            } else {
+                task.unmarkAsDone();
+                printMessage("OK, I've marked this task as not done yet:\n       " + task);
+            }
+        } catch (Exception e) {
+            printMessage("Invalid task number.");
+        }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void unmarkAsDone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + (isDone ? "X" : " ") + "] " + description;
+    }
+}
