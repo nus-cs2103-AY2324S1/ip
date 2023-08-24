d1753efd6252524d4f6a5a6d2e61cb7e22b80f78
tayian <90464339+tayian@users.noreply.github.com>
2023-08-25 02:49:31 +0800

    Update Task to support Subtypes

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -12,7 +12,11 @@ public class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
+        return (isDone ? "[X] " : "[ ] "); // mark done task with X
+    }
+
+    public String toString() {
+        return this.getStatusIcon() + this.description;
     }
 
     public void toggle() {
