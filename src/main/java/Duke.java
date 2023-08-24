20cbe4e4696035c0d38d78163ef32f6bd5a3b3ab
Jweewee <joshuawee555@gmail.com>
2023-08-24 22:39:47 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,42 +2,51 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 public class Duke {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    private List<Task> tasks = new ArrayList<>();
 
+    public static void main(String[] args) {
+        Duke wizzer = new Duke();
         String logo = "Wiz";
         List<Task> tasks = new ArrayList<>();
-
-
-
         System.out.println("Hello from " + logo +
-                "\nWhat can I do for you?\n" +
-                "Bye! Hope to see you again");
+                "\nWhat can I do for you?\n");
         System.out.println("--------------------------");
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
 
-        while(true) {
-            String command = scanner.nextLine();
+        while (!command.equals("bye")) {
             System.out.println("--------------------------");
-
+            wizzer.executeCommand(command);
             System.out.println("--------------------------");
-
-            if (command.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("--------------------------");
-                break;
-            } else if (command.equals("List")) {
-                System.out.println("Task List:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks.get(i).toString());
-                }
-            } else {
-                Task newTask = new Task(command);
-                tasks.add(newTask);
-                System.out.println("Added: " + newTask);
-            }
+            command = scanner.nextLine();
         }
+        scanner.close();
+        System.out.println("Bye. Hope to see you again soon!");
+    }
 
-
-
+    public void executeCommand(String command) {
+        String[] separateCommand = command.split(" ");
+        System.out.println("--------------------------");
+        System.out.println("--------------------------");
+        if (command.equals("list")) {
+            System.out.println("Task List:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println(" " + (i + 1) + ". " + tasks.get(i).toString());
+            }
+        } else if (command.startsWith("mark") || command.startsWith("unmark")) {
+            int taskNumber = Integer.parseInt(separateCommand[1]);
+            if (command.startsWith("mark")) {
+                tasks.get(taskNumber - 1).markAsDone();
+                System.out.println(" Marked as done: ");
+            } else if (command.startsWith("unmark")) {
+                tasks.get(taskNumber - 1).markAsUndone();
+                System.out.println(" Marked as not done yet: ");
+            }
+            System.out.println("   " + tasks.get(taskNumber - 1).toString());
+        } else {
+            Task currTask = new Task(separateCommand[0]);
+            tasks.add(currTask);
+            System.out.println("Added: " + currTask);
+        }
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,7 +1,17 @@
 public class Task {
     protected String description;
+    protected boolean isDone;
     public Task(String description) {
         this.description = description;
+    }
+    public void markAsDone() {
+        isDone = true;
+    }
+    public void markAsUndone() {
+        isDone = false;
+    }
+    public String getStatusIcon() {
+        return (isDone ? "X" : "");
     }
 
     @Override
