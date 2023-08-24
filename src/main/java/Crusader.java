abc03abb42d2b15c23056643e933e77fe09511a8
Kyriel Abad <abadkyriel@gmail.com>
2023-08-24 18:47:07 +0800

    Integrate Task Subclasses into Crusader.java

--- src/main/java/Crusader.java
+++ src/main/java/Crusader.java
@@ -89,6 +89,54 @@ public class Crusader {
         say("Bye!\nHave a great day!");
     }
 
+    /**
+     * Parses the prompt to generate a new Todo Task.
+     * @param prompt
+     */
+    private static void addTodo(String prompt) {
+        String name = prompt.substring(5);
+        Todo t = new Todo(name);
+        System.out.println("Adding the task:");
+        System.out.println(t.toString());
+        tasks.add(t);
+        System.out.printf("Now there are %d tasks in the list.\n", tasks.size());
+        addDivider();
+    }
+
+    /**
+     * Parses the prompt to generate a new Event Task.
+     * @param prompt
+     */
+    private static void addEvent(String prompt) {
+        int fromPosition = prompt.indexOf("/from");
+        int toPosition = prompt.indexOf("/to");
+        String name = prompt.substring(6, fromPosition - 1);
+        String from = prompt.substring(fromPosition + 6, toPosition - 1);
+        String to = prompt.substring(toPosition + 4);
+        Event e = new Event(name, from, to);
+        System.out.println("Adding the task:");
+        System.out.println(e.toString());
+        tasks.add(e);
+        System.out.printf("Now there are %d tasks in the list.\n", tasks.size());
+        addDivider();
+    }
+
+    /**
+     * Parses the prompt to generate a new Deadline Task.
+     * @param prompt
+     */
+    private static void addDeadline(String prompt) {
+        int byPosition = prompt.indexOf("/by");
+        String name = prompt.substring(9, byPosition - 1);
+        String by = prompt.substring(byPosition + 4);
+        Deadline d = new Deadline(name, by);
+        System.out.println("Adding the task:");
+        System.out.println(d.toString());
+        tasks.add(d);
+        System.out.printf("Now there are %d tasks in the list.\n", tasks.size());
+        addDivider();
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean notEnded = true;
@@ -115,6 +163,15 @@ public class Crusader {
                 case "unmark":
                     int j = Integer.parseInt(currentPrompt.split(" ")[1]);
                     unmark(j);
+                    break;
+                case "todo":
+                    addTodo(currentPrompt);
+                    break;
+                case "event":
+                    addEvent(currentPrompt);
+                    break;
+                case "deadline":
+                    addDeadline(currentPrompt);
                     break;
                 default:
                     say("added: " + currentPrompt);
