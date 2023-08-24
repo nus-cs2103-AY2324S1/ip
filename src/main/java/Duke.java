4788dded2b8feb58aa4845e346424dae0e3cb01a
DESKTOP-BD7R99C\Miguel <e1024867@u.nus.edu>
2023-08-24 22:07:19 +0800

    + Level 5 Modified Duke to make separate methods to greet and say bye to the user.
    + Changed the logic control flow of the messageHandler logic to use switch cases, as well as adding exceptions for invalid inputs.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,21 +10,13 @@ public class Duke {
     private DukeList list = new DukeList();
 
     /**
-     * Instantiates an instance of a Duke class.
-     */
-    public Duke() {}
-
-    /**
      * The main class for which the chatbot runs.
      * @param args The inputs given by the user in the command prompt.
      */
     public static void main(String[] args) {
         Duke lati = new Duke();
 
-        System.out.println("____________________________________________________________\n" +
-                " Hellooooooooooo! I'm Lati!\n" +
-                " What can I do for you? :3\n" +
-                "____________________________________________________________");
+        lati.greet();
 
         Scanner scan = new Scanner(System.in);
         String comd = scan.nextLine();
@@ -34,49 +26,100 @@ public class Duke {
             comd = scan.nextLine();
         }
 
+        lati.bye();
+    }
+
+    /**
+     * Sends a greeting message to the user.
+     */
+    public void greet() {
+        System.out.println("____________________________________________________________\n" +
+                " Hellooooooooooo! I'm Lati!\n" +
+                " What can I do for you? :3\n" +
+                "____________________________________________________________");
+    }
+
+    /**
+     * Sends a goodbye message to the user.
+     */
+    public void bye() {
         System.out.println(" Byeeeeee. Hope to see you again soon~~\n" +
                 "____________________________________________________________");
     }
 
     /**
-     * Helps to manage the user's inputs. "list" shows the current list, "mark / unmark"
+     * Helps to parse and manage the user's inputs. "list" shows the current list, "mark / unmark"
      * helps to mark or unmark the specific task in the list, and todo/deadline/event
      * adds todo/deadline/event tasks respectively.
      *
      * @param tally takes in the input string.
      */
     public void messageHandler(String tally) {
-        if (tally.equals("list")) {
-            list.display();
-        } else {
-            String comd = tally.split(" ")[0];
+        String comd = tally.split(" ")[0];
 
-            if (comd.equals("todo")) {
+        switch (comd) {
+            case "list":
+                list.display();
+                break;
+            case "todo":
                 Pattern todocmd = Pattern.compile("todo (.+)");
-                Matcher m = todocmd.matcher(tally);
-                m.matches();
-                list.store(m.group(1));
-            } else if (comd.equals("deadline")) {
+                Matcher mt = todocmd.matcher(tally);
+                if (this.groupRun(mt, 1)) {
+                    list.store(mt.group(1));
+                } else {
+                    throw new IllegalArgumentException("Whoops, wrong command! Type todo <task>");
+                }
+                break;
+            case "deadline":
                 Pattern deadlinecmd = Pattern.compile("deadline (.+) /by (.+)");
-                Matcher m = deadlinecmd.matcher(tally);
-                m.matches();
-                m.matches();
-                list.store(m.group(1), m.group(2));
-            } else if (comd.equals("event")) {
+                Matcher md = deadlinecmd.matcher(tally);
+                if (this.groupRun(md, 2)) {
+                    list.store(md.group(1), md.group(2));
+                } else {
+                    throw new IllegalArgumentException("Whoops, wrong command! Type deadline <task> /by <date>");
+                }
+                break;
+            case "event":
                 Pattern eventcmd = Pattern.compile("event (.+) /from (.+) /to (.+)");
-                Matcher m = eventcmd.matcher(tally);
-                m.matches();
-                m.matches();
-                m.matches();
-                list.store(m.group(1), m.group(2), m.group(3));
-            } else if (comd.equals("mark")) {
-                String index = tally.split(" ")[1];
-                list.mark(Integer.parseInt(index));
-            } else {
-                String index = tally.split(" ")[1];
-                list.unmark(Integer.parseInt(index));
-            }
+                Matcher ml = eventcmd.matcher(tally);
+                if (this.groupRun(ml, 3)) {
+                    list.store(ml.group(1), ml.group(2), ml.group(3));
+                } else {
+                    throw new IllegalArgumentException("Whoops, wrong command! Type event <task> /" +
+                            "from <date> /to <date>");
+                }
+                break;
+            case "mark":
+                try {
+                    String indexmark = tally.split(" ")[1];
+                    list.mark(Integer.parseInt(indexmark));
+                } catch (NullPointerException e) {
+                    throw new IllegalArgumentException("Ehh? What do you want to mark? Type mark <index>");
+                }
+                break;
+            case "unmark":
+                try {
+                    String indexunmark = tally.split(" ")[1];
+                    list.mark(Integer.parseInt(indexunmark));
+                } catch (NullPointerException e) {
+                    throw new IllegalArgumentException("Ehh? What do you want to mark? Type unmark <index>");
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Error, unknown command.");
         }
+
     }
+
+    /**
+     * Recursively runs the matches() method on a given matcher n times.
+     * @param m The matcher object
+     * @param n number of times it has to run
+     * @return the boolean value of all matches() run recursively.
+     */
+    private boolean groupRun(Matcher m, int n) {
+        return n == 0 ? false : n == 1 ? m.matches() : m.matches() && groupRun(m, n - 1);
+    }
+
 
 }
