7ee23026188c24a1ed6c4cb23e6e324207a886b5
josepholim <joseph.o.lim@gmail.com>
2023-08-24 12:31:36 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+    private Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.run();
+    }
+    public void run() {
+        System.out.println(HORIZONTAL_LINE + "\n"
+                + "Hello, I'm Je-O" + "\n"
+                + "What can I do for you?" + "\n"
+                + HORIZONTAL_LINE + "\n"
+                + "Bye. Hope to see you again soon!" + "\n"
+                + HORIZONTAL_LINE);
     }
 }
