320a8d46dd37076dc3163b3872ba5d101d10072c
iantsaii <iantsai@Ians-MacBook-Pro.local>
2023-08-24 17:04:45 +0800

    Add and List

--- src/main/java/Rocket.java
+++ src/main/java/Rocket.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Rocket {
@@ -9,7 +11,7 @@ public class Rocket {
         Scanner scanner = new Scanner(System.in);
 
         // Create list
-        String[] commandList = new String[100];
+        List<String> commandList = new ArrayList<>();
         System.out.println(LINE + "\n    Hello! I'm Rocket\n" +
                 "    What can I do for you?\n" + LINE);
         command = scanner.nextLine();
@@ -17,11 +19,16 @@ public class Rocket {
             if (command.equals("bye")) {
                 System.out.println(LINE + "\n    Bye. Hope to see you again soon!\n" + LINE);
                 break;
-            } else if (command.equals("list")){
-                System.out.println(LINE + "\n    list\n" + LINE);
+            } else if (command.equals("list")) {
+                System.out.println(LINE);
+                for (int i = 0; i < commandList.size(); i++) {
+                    System.out.println("    " + (i + 1) + ". " + commandList.get(i));
+                }
+                System.out.println(LINE);
                 command = scanner.nextLine();
             } else {
                 System.out.println(LINE + "\n    added: " + command + "\n" + LINE);
+                commandList.add(command);
                 command = scanner.nextLine();
             }
         }
