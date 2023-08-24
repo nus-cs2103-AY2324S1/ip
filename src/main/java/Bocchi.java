00c132f93009ea1720d5faf357c017b7523bd98f
Jun Hong <junhongloww@gmail.com>
2023-08-24 19:43:26 +0800

    Add echo functionality

--- src/main/java/Bocchi.java
+++ src/main/java/Bocchi.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 public class Bocchi {
     private static final String LINE_BREAK = "___________________________________________________";
 
@@ -19,12 +22,29 @@ public class Bocchi {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(LINE_BREAK);
     }
+
+    /**
+     * Echoes input message.
+     */
+    private static void echo(String message) {
+        System.out.println(LINE_BREAK);
+        System.out.println(message);
+        System.out.println(LINE_BREAK);
+    }
+
     /**
     /**
      * Main method.
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         greet();
+        String command = sc.nextLine();
+        while (!command.equals("bye")) {
+            echo(command);
+            command = sc.nextLine();
+        }
+        sc.close();
         exit();
     }
 }
