2a14b16e20419d192542a1e9d379bf0e8cc24b2a
kokrui <kokruiwong@gmail.com>
2023-08-24 15:43:08 +0800

    fix: Add space between event name and time info

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -54,6 +54,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(from: " + this.startTime + "to: " + this.endTime + ")";
+        return "[E]" + super.toString() + " (from: " + this.startTime + "to: " + this.endTime + ")";
     }
 }
