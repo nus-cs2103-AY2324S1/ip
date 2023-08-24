5d6c6b56f73e4dbdd4f92152790fd5958aec4f49
LHeng1 <limhengyi1@gmail.com>
2023-08-24 09:24:45 +0800

    Level-1

--- src/main/java/Avalon.java
+++ src/main/java/Avalon.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
 public class Avalon {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Arthur Pendragon.\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+        System.out.println(
+                "   ____________________________________________________________\n" +
+                "    Hello! I'm Arthur Pendragon.\n" +
+                "    What can I do for you?\n" +
+                "   ____________________________________________________________\n"
+        );
+
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true){
+            String userInput = scanner.nextLine();
+            if (userInput.equalsIgnoreCase("bye")){
+                System.out.println(
+                        "   ____________________________________________________________\n" +
+                                "    Farewell. We will meet again!\n" +
+                                "   ____________________________________________________________"
+                );
+                break;
+            }
+
+            else {
+                System.out.println(
+                        "   ____________________________________________________________\n" +
+                                "    " +
+                                userInput +
+                                "\n   ____________________________________________________________\n"
+                );
+            }
+        }
     }
 }
