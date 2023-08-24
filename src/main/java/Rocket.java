f5d3e52b78b2fdc4331c4109e979f8c530844ee1
iantsaii <iantsai@Ians-MacBook-Pro.local>
2023-08-24 16:26:48 +0800

    Echo back one command

--- src/main/java/Rocket.java
+++ src/main/java/Rocket.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Rocket {
     public static void main(String[] args) {
-        String LINE = "____________________________________________________________";
-        System.out.println(LINE + "\nHello! I'm Rocket\n" +
-                "What can I do for you?\n" + LINE);
-        System.out.println("Bye. Hope to see you again soon!\n" + LINE);
+        String LINE = "    ____________________________________________________________";
+        // Initialise string variable to store command
+        String command;
+        // Create scanner to read user input
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(LINE + "\n    Hello! I'm Rocket\n" +
+                "    What can I do for you?\n" + LINE);
+        command = scanner.nextLine();
+        System.out.println(LINE + "\n    " + command + "\n" + LINE);
+        System.out.println(LINE + "\n    Bye. Hope to see you again soon!\n" + LINE);
+
+
     }
 }
