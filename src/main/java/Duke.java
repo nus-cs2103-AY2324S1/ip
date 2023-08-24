600dbe3ba03c2d2af6a54992b05dac426d58d616
MathanC <ncmathan98@gmail.com>
2023-08-24 17:55:47 +0800

    no message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "Mathan";
+        System.out.println("____________________________________________________________\n" +
+                " Hello! I'm "+name+"\n" +
+                " What can I do for you?");
+        System.out.println("____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________");
     }
 }
