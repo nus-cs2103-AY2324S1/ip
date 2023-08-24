4d24671febcf1905cffd325928d24001e9815dd5
KT <tkt2001@hotmail.com>
2023-08-24 16:34:00 +0800

    l1 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     private static final String name = "Bot";
-    private static final String greeting = "Hello I'm " + name + "\n What can I do for you?";
+    private static final String greeting = "Hello I'm " + name + "\nWhat can I do for you?";
     private static final String end = "Bye bye";
 
     public static void main(String[] args) {
         System.out.println(greeting);
+        Scanner s = new Scanner(System.in);
+        while (s.hasNextLine()) {
+            String t = s.nextLine();
+            if (t.equals("bye")) {
+                exit();
+                return;
+            } else {
+                echo(t);
+            }
+        }
+    }
+
+    private static void echo(String s) {
+        System.out.println(s);
+    }
+
+    private static void exit() {
         System.out.println(end);
     }
 }
