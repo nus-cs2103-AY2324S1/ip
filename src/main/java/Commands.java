5f8d4a2d8cfe34e20da6dadce69ff87ebfaf4432
NicholasTng010601 <nicholastng@hotmail.com>
2023-08-24 15:24:24 +0800

    Edited text structure

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -1,4 +1,5 @@
 import Errors.DukeException;
+import Errors.InvalidTaskException;
 import Errors.InvalidTaskInput;
 import Tasks.Deadlines;
 import Tasks.Events;
@@ -21,17 +22,25 @@ public class Commands {
 
                         "____________________________________________________________\n" +
                          "Hello! I'm %s, a task planning bot that will help you record your tasks.\n" +
-                         "I currently can record 3 types of tasks. tasks to do, tasks with deadlines and events \n" +
-                         "Here is how you can record down your events.\n" +
-                         "To record tasks to do, simply begin your command with \"todo\" followed by a space and the task you need to do.\n" +
-                         "To record tasks with deadlines, simply begin your command with \"deadlines\" followed by the task and a / then the due date.\n" +
-                         "To record events, simply begin your command with \"events\" followed by the event, / and start time , then / with the end time" +
-                         "To view your list of events, type list. \n" + "To mark your events as done or undone, type in mark / unmark , followed by the index of the task \n"+
-                                "To delete tasks from the list, type delete task number, such as delete 2 \n" +
-                                        "Lastly, to exit the chatbot, type \"bye\" \n" +
-                                 "____________________________________________________________"
+                         "If you require help on the commands you can key in, type \"help\" \n" +
+                                 "____________________________________________________________\n"
                          , name);
         System.out.println(greet);
+    }
+
+    public static void commandList() {
+        String commands = (
+                "____________________________________________________________\n" +
+                "I currently can record 3 types of tasks. tasks to do, tasks with deadlines and events \n" +
+                "Here is how you can record down your events.\n" +
+                "To record tasks to do, simply begin your command with \"todo\" followed by a space and the task you need to do.\n" +
+                "To record tasks with deadlines, simply begin your command with \"deadlines\" followed by the task and a / then the due date.\n" +
+                "To record events, simply begin your command with \"events\" followed by the event, / and start time , then / with the end time" +
+                "To view your list of events, type list. \n" + "To mark your events as done or undone, type in mark / unmark , followed by the index of the task \n"+
+                "To delete tasks from the list, type delete task number, such as delete 2 \n" +
+                "Lastly, to exit the chatbot, type \"bye\" \n" + "____________________________________________________________\n"
+        );
+        System.out.println(commands);
     }
 
     // Level-0, Function to say goodbye
@@ -46,84 +55,97 @@ public class Commands {
     //  Level-1, Echo user input
     public static void echoUserInput(Task task) {
         System.out.println(task);
-        System.out.println("____________________________________________________________\n");
+        System.out.println("____________________________________________________________");
     }
 
     //    Level-2, Add, list
-    public static void addList(String input) throws InvalidTaskInput {
+    public static void addList(String input) throws InvalidTaskInput, InvalidTaskException {
         Task newTask = null;
 
         // check if input begins with todo, deadline or event to categorise task type
         String[] wordSeparator = input.split(" ");
         String firstWord = wordSeparator[0];
-        if (firstWord.equalsIgnoreCase("todo")) {
-            String taskToDo = input.substring(5);
 
-            // check that string is not empty after substring to remove todo;
-            if (taskToDo.isEmpty()) {
-                throw new InvalidTaskInput();
+        try {
+            if (firstWord.equalsIgnoreCase("todo")) {
+                String taskToDo = input.substring(5);
+
+                // check that string is not empty after substring to remove todo;
+                if (taskToDo.isEmpty()) {
+                    throw new InvalidTaskInput();
+                }
+                newTask = new ToDos(taskToDo);
+
             }
-            newTask = new ToDos(taskToDo);
+            if (firstWord.equalsIgnoreCase("deadline")) {
+                String[] parts = input.split("/");
 
+                /*
+                split input array into description and deadline.
+                "deadline return book /by Sunday" -> parts[0] = deadline return book  and parts[1] = by Sunday
+                using substring, deadline return book -> return book
+                throws exception if / not used properly, leading to error in either access in values or
+                fixing the deadline
+                */
+                if (parts.length != 2) {
+                    throw new InvalidTaskInput();
+                }
+
+                String description = parts[0].substring(9);
+                // check that string is not empty after substring to remove todo;
+                if (description.isEmpty()) {
+                    throw new InvalidTaskInput();
+                }
+
+                String deadline = parts[1];
+                newTask = new Deadlines(description, deadline);
+            }
+
+            if (firstWord.equalsIgnoreCase("event")) {
+                String[] parts = input.split("/");
+                /*
+                similar logic as above. additionally, splits input into start and end time.
+                Throws exception if / is not used properly, leading to error in start / end time.
+                 */
+
+                if (parts.length != 3) {
+                    throw new InvalidTaskInput();
+                }
+
+                String description = parts[0].substring(6);
+                String startTime = parts[1];
+                String endTime = parts[2];
+
+                // Ensure that start, end and description are all filled in
+                if (startTime.isEmpty() || endTime.isEmpty() || description.isEmpty()) {
+                    throw new InvalidTaskInput();
+                }
+
+                newTask = new Events(description, startTime, endTime);
+            }
+
+            if (newTask == null) throw new InvalidTaskException();
+            tasks.add(count++, newTask);
+            System.out.println("____________________________________________________________\n" +
+                    "I have added this task to your list\n" +
+                    String.format("[%s]", newTask.getTaskType()) + "[ ]" + newTask.getDescription() + "\n" +
+                    String.format("You now have %d tasks in your list.", count));
+            System.out.println("____________________________________________________________\n");
+
+        } catch (InvalidTaskInput inputException) {
+            System.out.println("____________________________________________________________\n");
+            System.out.println(inputException);
+            System.out.println("____________________________________________________________\n");
+
+        } catch (InvalidTaskException taskException) {
+            System.out.println("____________________________________________________________\n");
+            System.out.println(taskException);
+            System.out.println("____________________________________________________________\n");
         }
-        if (firstWord.equalsIgnoreCase("deadline")) {
-            String[] parts = input.split("/");
-
-            /*
-            split input array into description and deadline.
-            "deadline return book /by Sunday" -> parts[0] = deadline return book  and parts[1] = by Sunday
-            using substring, deadline return book -> return book
-            throws exception if / not used properly, leading to error in either access in values or
-            fixing the deadline
-            */
-            if (parts.length != 2) {
-                throw new InvalidTaskInput();
-            }
-
-            String description = parts[0].substring(9);
-            // check that string is not empty after substring to remove todo;
-            if (description.isEmpty()) {
-                throw new InvalidTaskInput();
-            }
-
-            String deadline = parts[1];
-            newTask = new Deadlines(description, deadline);
-        }
-
-        if (firstWord.equalsIgnoreCase("event")) {
-            String[] parts = input.split("/");
-            /*
-            similar logic as above. additionally, splits input into start and end time.
-            Throws exception if / is not used properly, leading to error in start / end time.
-             */
-
-            if (parts.length != 3) {
-                throw new InvalidTaskInput();
-            }
-
-            String description = parts[0].substring(6);
-            String startTime = parts[1];
-            String endTime = parts[2];
-
-            // Ensure that start, end and description are all filled in
-            if (startTime.isEmpty() || endTime.isEmpty() || description.isEmpty()) {
-                throw new InvalidTaskInput();
-            }
-
-            newTask = new Events(description, startTime, endTime);
-        }
-
-        if (newTask == null) newTask = new Task(input);
-        tasks.add(count++, newTask);
-        System.out.println("____________________________________________________________\n" +
-                "I have added this task to your list\n" +
-                String.format("[%s]",newTask.getTaskType()) + "[ ]" + newTask.getDescription() + "\n" +
-                String.format("You now have %d tasks in your list.", count));
-        System.out.println("____________________________________________________________\n");
     }
 
     public static void printList() {
-        System.out.println("____________________________________________________________");
+        System.out.println("____________________________________________________________\n");
         for (int i = 0; i < count; i++) {
             // for each task in task[], prints out a simple line describing the task
             Character taskType = tasks.get(i).getTaskType();
@@ -169,24 +191,32 @@ public class Commands {
     }
 
     //level 6. Delete tasks
-    public static void deleteTasks(int taskNumber) throws  DukeException{
-        if (count < taskNumber) throw new DukeException("Invalid input added");
-        Task removed = tasks.remove(taskNumber - 1);
-        count--;
-        System.out.println("____________________________________________________________\n");
-        System.out.println("Noted, I have deleted this task:");
-        System.out.println(String.format("  [%c][%s] %s ", removed.getTaskType(), removed.getStatusIcon(), removed));
-        System.out.println(String.format("You now have %d tasks in your list.", taskNumber));
-        System.out.println("____________________________________________________________\n");
+    public static void deleteTasks(int taskNumber) throws InvalidTaskInput {
+        try {
+            if (count <= taskNumber) throw new InvalidTaskInput();
+            Task removed = tasks.remove(taskNumber - 1);
+            count--;
+            System.out.println("____________________________________________________________\n");
+            System.out.println("Noted, I have deleted this task:");
+            System.out.println(String.format("  [%c][%s] %s ", removed.getTaskType(), removed.getStatusIcon(), removed));
+            System.out.println(String.format("You now have %d tasks in your list.", taskNumber));
+            System.out.println("____________________________________________________________");
+        } catch (InvalidTaskInput message) {
+            System.out.println("____________________________________________________________\n");
+            System.out.println(message);
+            System.out.println("____________________________________________________________\n");
+        }
     }
 
     // overall input handler which determines which function to run
-    public static void handleInput(String userInput) throws InvalidTaskInput, DukeException{
+    public static void handleInput(String userInput) throws InvalidTaskInput, DukeException, InvalidTaskException {
             if (userInput.equalsIgnoreCase("bye")) return;
             if (userInput.equalsIgnoreCase("list")) {
                 printList();
+            } else if (userInput.equalsIgnoreCase("help")) {
+                commandList();
             } else if (userInput.length() > 7 && userInput.substring(0,6).equalsIgnoreCase("delete")) {
-                // delete takes up first 5, 6 index takes up space, so 7 onwards should be numbers
+                // delete takes up first 5 indexes, index 6 takes up space, so 7 onwards should be numbers
                 String index = userInput.substring(7);
                 while (index.charAt(0) == ' ') index = index.substring(1);
                 while (index.charAt(index.length() - 1) == ' ') index = index.substring(0, index.length()-1);
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 import Errors.DukeException;
+import Errors.InvalidTaskException;
 import Errors.InvalidTaskInput;
 
 import java.util.Scanner;
 
 public class Duke {
-    public static void main (String[] args) throws DukeException, InvalidTaskInput {
+    public static void main (String[] args) throws DukeException, InvalidTaskInput, InvalidTaskException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -16,15 +17,22 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String userInput = "";
         do {
-            userInput = sc.nextLine();
+            try {
+                    userInput = sc.nextLine();
 
-            if (userInput.equals("")) {
-                throw new DukeException("OOPS, input field cannot be empty. Try entering a task or a command");
-            }
+                    if (userInput.equals("")) {
+                        throw new DukeException("OOPS, input field cannot be empty. Try entering a task or a command");
+                    }
 
-            Commands.handleInput(userInput);
-        } while (!userInput.toLowerCase().equals("bye"));
+                    Commands.handleInput(userInput);
+                } catch (DukeException error) {
+                    System.out.println("____________________________________________________________\n");
+                    System.out.println(error);
+                    System.out.println("____________________________________________________________\n");
+                }
+            } while (!userInput.toLowerCase().equals("bye"));
 
-        Commands.sayGoodBye();
-    }
+            Commands.sayGoodBye();
+        }
+
 }
