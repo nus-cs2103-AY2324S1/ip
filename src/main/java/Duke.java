9aad85b7f338672bf2c0914c173d8cbb914caf27
Ken-Lai <weizhong9201@gmail.com>
2023-08-24 15:41:04 +0800

    Change chatbot name and implement an initial skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
+    private static final String SEPARATION_LINE  = "____________________________________________________________";
+    private static final String GREETING_TEXT = "Hello, I'm Tasket\nWhat can I do for you?";
+    private static final String GOODBYE_TEXT = "Bye. Hope to see you again soon!";
+
+    public static void showGreetingText() {
+        System.out.println(SEPARATION_LINE);
+        System.out.println(GREETING_TEXT);
+        System.out.println(SEPARATION_LINE);
+    }
+
+    public static void showGoodbyeText() {
+        System.out.println(GOODBYE_TEXT);
+        System.out.println(SEPARATION_LINE);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        showGreetingText();
+        showGoodbyeText();
     }
 }
