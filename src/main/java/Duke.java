43564f11710b9c3069bc4111f44df22e35d02292
ElginTZM <63971687+ElginTZM@users.noreply.github.com>
2023-08-24 16:17:13 +0800

    Update chatbot name, greeting and exit message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("------------------------------------------------------------");
+        System.out.println(" Hello! I'm A-CAT (Automated Chatbot Assistant for Tasks)");
+        System.out.println(" What do you want to do today?");
+        System.out.println("------------------------------------------------------------");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("------------------------------------------------------------");
     }
 }
