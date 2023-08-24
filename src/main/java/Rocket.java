e25afd7afe6c1ac59faa7a2a89e0d22475c83138
iantsaii <iantsai@Ians-MacBook-Pro.local>
2023-08-24 15:46:27 +0800

    Renamed chatbot from Yello to Rocket

--- src/main/java/Rocket.java
+++ src/main/java/Rocket.java
@@ -1,7 +1,7 @@
-public class Yello {
+public class Rocket {
     public static void main(String[] args) {
         String LINE = "____________________________________________________________";
-        System.out.println(LINE + "\nHello! I'm Yello\n" +
+        System.out.println(LINE + "\nHello! I'm Rocket\n" +
                 "What can I do for you?\n" + LINE);
         System.out.println("Bye. Hope to see you again soon!\n" + LINE);
     }
