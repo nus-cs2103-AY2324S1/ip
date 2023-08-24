9fcd4c247c42e6f5b61378e690a39fce3a1c9ede
iantsaii <iantsai@Ians-MacBook-Pro.local>
2023-08-24 16:54:44 +0800

    Edited while loop

--- src/main/java/Rocket.java
+++ src/main/java/Rocket.java
@@ -7,18 +7,25 @@ public class Rocket {
         String command;
         // Create scanner to read user input
         Scanner scanner = new Scanner(System.in);
+
+        // Create list
+        String[] commandList = new String[100];
         System.out.println(LINE + "\n    Hello! I'm Rocket\n" +
                 "    What can I do for you?\n" + LINE);
         command = scanner.nextLine();
         while (true) {
-            if (!command.equals("bye")) {
-                System.out.println(LINE + "\n    " + command + "\n" + LINE);
+            if (command.equals("bye")) {
+                System.out.println(LINE + "\n    Bye. Hope to see you again soon!\n" + LINE);
+                break;
+            } else if (command.equals("list")){
+                System.out.println(LINE + "\n    list\n" + LINE);
                 command = scanner.nextLine();
             } else {
-                break;
+                System.out.println(LINE + "\n    added: " + command + "\n" + LINE);
+                command = scanner.nextLine();
             }
         }
-        System.out.println(LINE + "\n    Bye. Hope to see you again soon!\n" + LINE);
+
 
 
     }
