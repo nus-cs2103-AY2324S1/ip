97fabf3d11edd5e4e2adf5404529e290dccdcd0d
EricXiong420 <E0958551@u.nus.edu>
2023-08-25 00:29:38 +0800

    Makes the program echo user inputs

--- src/main/java/Hachi.java
+++ src/main/java/Hachi.java
@@ -1,15 +1,31 @@
-public class Duke {
+import java.util.Scanner;
+
+public class Hachi {
 
     public static void main(String[] args) {
         String name = "Hachi";
-        line();
-        System.out.println("Hello! I'm " + name + "\nWhat cam I do for you?");
-        line();
-        System.out.println("Bye. Hope to see you again soon!");
-        line();
+        printLine("Hello! I'm " + name + "\nWhat cam I do for you?");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                printLine("Bye. Hope to see you again soon!");
+                break;
+            }
+            else {
+                printLine(command);
+            }
+        }
+
     }
 
     public static void line() {
         System.out.println("____________________________________________________________");
+    }
+
+    public static void printLine(String s) {
+        line();
+        System.out.println(s);
+        line();
     }
 }
