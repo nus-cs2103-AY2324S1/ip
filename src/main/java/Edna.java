abfac8147f32ff0084b351ceb38e6510e2d725e5
Qin Nanxin <nanxinqin@hotmail.com>
2023-08-24 18:48:43 +0800

    [Level 0. Rename, Greet, Exit] (a) Changed chatbot name form Duke to Edna (b) Implement an initial skeletal version

---
+++ src/main/java/Edna.java
@@ -0,0 +1,8 @@
+public class Edna {
+    public static void main(String[] args) {
+        String partition = "\n/----------------------------------------/\n";
+        System.out.println(partition + "Hello! I'm Edna.");
+        System.out.println("What can I do for you?" + partition);
+        System.out.println("Bye. Hope to see you again soon!" + partition);
+    }
+}
