284f2d1a10cd2a9e7962cdbbd260e6277d6b991a
Aprup Kale <aprup.kale@gmail.com>
2023-08-24 08:43:27 +0800

    Update for Level 0 - Add greet and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        exit();
+    }
+
+    public static void greet() {
+        lines();
+        System.out.println("Hello! I'm Max");
+        System.out.println("What can I do for you?");
+        lines();
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+        lines();
+    }
+
+    public static void lines() {
+        System.out.println("");
+        for (int i = 0; i < 50; i++) {
+            System.out.print("-");
+        }
+        System.out.println("");
     }
 }
