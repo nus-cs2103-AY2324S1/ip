639a8c5c650862f3b8e55b9e55c91773c6c0f440
scarletblanks <irisu.nita@gmail.com>
2023-08-24 18:28:09 +0800

    Level 3. Mark as Done Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done. With OOP

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,10 +22,10 @@ public class Duke {
         System.out.println("____________________________________________________________");
 
         Scanner myScanner = new Scanner(System.in);
-        ArrayList<String> myList = new ArrayList<String>(); // Create an ArrayList object
+        ArrayList<Task> myList = new ArrayList<Task>(); // Create an ArrayList object
 
         while(true){
-            String inValue = myScanner.nextLine();
+            String inValue = myScanner.next();
             switch(inValue) {
                 case "bye":
                     System.out.println("____________________________________________________________");
@@ -36,18 +36,38 @@ public class Duke {
                 case "list":
                     System.out.println("____________________________________________________________");
                     int i = 1;
-                    for (String s: myList){
-                        System.out.println(String.valueOf(i) + ". " + s);
+                    for (Task t: myList){
+                        String prefix = t.getStatusIcon();
+                        System.out.println(String.valueOf(i) + "." + "[" + prefix + "]" + " " + t.get());
                         i++;
                     }
                     System.out.println("____________________________________________________________");
                     break;
 
+                case "mark":
+                    int number = myScanner.nextInt();
+                    myList.get(number-1).set();
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[X] " + myList.get(number-1).get());
+                    System.out.println("____________________________________________________________");
+                    break;
+
+                case "unmark":
+                    int numero = myScanner.nextInt();
+                    myList.get(numero-1).unset();
+                    System.out.println("____________________________________________________________");
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("[ ] " + myList.get(numero-1).get());
+                    System.out.println("____________________________________________________________");
+                    break;
+
                 default:
+                    inValue += myScanner.nextLine();
                     System.out.println("____________________________________________________________");
                     System.out.println("added: " + inValue);
                     System.out.println("____________________________________________________________");
-                    myList.add(inValue);
+                    myList.add(new Task(inValue));
                     break;
                     
             }
