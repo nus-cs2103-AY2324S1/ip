f14fc827236c3f744fb72ca4b9e8f9782bae5c58
hyc17003 <e0970589@u.nus.edu>
2023-08-24 18:15:37 +0800

    Mark as Done ChatterBot

--- src/main/java/ChatterBot.java
+++ src/main/java/ChatterBot.java
@@ -7,7 +7,7 @@ public class ChatterBot {
         String logo = "ChatterBot";
         System.out.println("Hello! I'm " + logo + "\nWhat can I do for you?");
 
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         while (true) {
             Scanner scanner = new Scanner(System.in);
@@ -17,13 +17,33 @@ public class ChatterBot {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (userMessage.toLowerCase().equals("list")) {
-                for (String str : list) {
-                    System.out.println(list.indexOf(str) + 1 + ". " + str);
+                System.out.println("Here are the tasks in your list:");
+                for (Task t : list) {
+                    System.out.println((list.indexOf(t) + 1) + ". "
+                            + "[" + t.getStatusIcon() + "] " + t.description);
                 }
+            } else if (userMessage.startsWith("mark") && isInteger(userMessage.substring(5))) {
+                String toMark = userMessage.substring(5);
+                list.get(Integer.parseInt(toMark) - 1).markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n" + "[X] " + list.get(Integer.parseInt(toMark) - 1).description);
+            } else if (userMessage.startsWith("unmark")) {
+                String toUnmark = userMessage.substring(7);
+                list.get(Integer.parseInt(toUnmark) - 1).markAsUndone();
+                System.out.println("OK, I've marked this task as not done yet:\n" + "[ ] " + list.get(Integer.parseInt(toUnmark) - 1).description);
             } else {
-                list.add(userMessage);
+                Task t = new Task(userMessage);
+                list.add(t);
                 System.out.println("added " + userMessage);
             }
+        }
+    }
+
+    public static boolean isInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 }
