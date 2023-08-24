e0cc227a637764aee0f9c0b98c8d84cf07d26ca9
xyT <tongyu5336@gmail.com>
2023-08-24 20:48:34 +0800

    Level-2 input accepted

--- src/main/java/Tong.java
+++ src/main/java/Tong.java
@@ -1,15 +1,32 @@
-public class Duke {
+import java.util.Scanner;
+
+public class Tong {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-        int length = 20; // Adjust the length of the line as needed
+        Scanner input = new Scanner(System.in);
+        String currentLine;
+
+        printHorizontalLine(35);
+        System.out.println("Hello! I'm TONG.");
+        System.out.println("Say something. Say anything.");
+        System.out.println("Sometimes you say 'bye'.");
+        printHorizontalLine(35);
+
+        currentLine = input.nextLine();
+
+        while (!currentLine.equalsIgnoreCase("bye")) {
+            System.out.println("TONG: " + currentLine);
+            currentLine = input.nextLine();
+        }
+
+        System.out.println("Bye.");
+        printHorizontalLine(35);
+        input.close();
+    }
+
+    public static void printHorizontalLine(int length) {
         for (int i = 0; i < length; i++) {
             System.out.print("-");
         }
-        System.out.println("Hello! I'm TONG.\nWhat can I do for you?");
+        System.out.println();
     }
 }
