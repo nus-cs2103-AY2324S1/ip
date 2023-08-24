a7f0f3619d7109e817f38c9f3d7793ef850e68de
marioalvaro <marioalvaro131@gmail.com>
2023-08-24 17:17:22 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,19 @@
+import java.sql.Array;
 import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        boolean bye = false;
+        String[] arrayList = new String[100];
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        //System.out.println("Hello from\n" + logo);
 
         String greet = "   ____________________________________________________________\n" +
                 "    Hello! I'm Siri\n" +
@@ -16,10 +21,7 @@ public class Duke {
                 "   ____________________________________________________________\n";
         System.out.println(greet);
 
-        Scanner in = new Scanner(System.in);
         String s = in.nextLine();
-
-        boolean bye = false;
         while (!bye) {
             if (s == null) {
                 s = in.nextLine();
@@ -33,10 +35,34 @@ public class Duke {
                         "    ____________________________________________________________";
                 System.out.println(exit);
             } else {
+                String response = "";
 
-                String response = "    ____________________________________________________________\n" +
-                        "     " + s + "\n" +
-                        "    ____________________________________________________________\n";
+                if (s.equals("list")) {
+                    String items = "";
+
+                    for (int i = 0; i < 100; i++) {
+                        if (arrayList[i] == null) {
+                            break;
+                        }
+                        items += "     " + (i+1) + ". " + arrayList[i] + "\n";
+                    }
+                    response = "    ____________________________________________________________\n" +
+                            items +
+                            "    ____________________________________________________________\n";
+
+                } else {
+                    for (int i = 0; i < 100; i++) {
+                        if (arrayList[i] == null) {
+                            arrayList[i] = s;
+                            break;
+                        }
+                    }
+
+                    response = "    ____________________________________________________________\n" +
+                            "     added: " + s + "\n" +
+                            "    ____________________________________________________________\n";
+                }
+
                 System.out.println(response);
                 s = null;
             }
