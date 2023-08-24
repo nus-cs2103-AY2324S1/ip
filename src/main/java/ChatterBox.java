7c84a9f76cbc0bb6d66b29e985f8be5c6d12e46c
Rubesh S <rubesh77@gmail.com>
2023-08-24 19:56:51 +0800

    Improve ChatBot to echo every input

--- src/main/java/ChatterBox.java
+++ src/main/java/ChatterBox.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class ChatterBox {
 
@@ -7,12 +8,31 @@ public class ChatterBox {
         System.out.println("      Hello! I'm ChatterBox");
         System.out.println("      What can I do for you?");
         linePrinter();
-        System.out.println("      Bye. Hope to see you again soon!");
-        linePrinter();
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                linePrinter(); 
+                System.out.println("      Bye. Hope to see you again soon!");
+                linePrinter();
+                break;
+
+            } else {
+                linePrinter();
+                tabPrinter(input);
+                linePrinter();
+
+            }
+        }
     }
 
     private static void linePrinter() {
         System.out.println("      ___________________________________________________________");
     }
 
+    private static void tabPrinter(String s) {
+        System.out.println("      " + s);
+    }
 }
