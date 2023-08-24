b2f45174d343edd6890e1bfec473be1b8fbc6e3f
qz1004 <e0967912@u.nus.edu>
2023-08-24 16:03:38 +0800

    added the ability to store whatever text entered by the user and display them back to the user when requested

--- src/main/java/Jo.java
+++ src/main/java/Jo.java
@@ -1,13 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Jo {
     public static void main(String[] args) {
 
         System.out.println("> Hello! I'm Jo.\n> What can I do for you?");
+        List<String> taskList = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         while (!input.equals("bye")) {
-            System.out.println("> " + input);
+            if (input.equals("list")) {
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println(i+1 + ". " + taskList.get(i));
+                }
+            } else {
+                taskList.add(input);
+                System.out.println("> added: " + input);
+            }
             input = scanner.nextLine();
         }
         System.out.println("> Bye. Hope to see you again soon!");
