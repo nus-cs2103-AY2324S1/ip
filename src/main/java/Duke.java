f594c49d4cf23886e19d4451faef646de83ca473
jannnice <>
2023-08-24 12:16:19 +0800

    Level 6. Delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Duke {
     public static final String horizontalLine = "    ____________________________________________________________\n";
-    public static Task[] taskArray = new Task[100];
+    public static ArrayList<Task> taskArray = new ArrayList<>();
     public static int numTask = 0;
     public static void greet() {
         System.out.println(horizontalLine
@@ -12,8 +13,9 @@ public class Duke {
                 + horizontalLine);
     }
     public static void exit() {
-        System.out.println("     Bye. Hope to see you again soon!\n"
-                + horizontalLine);
+        System.out.println(horizontalLine
+                           + "     Bye. Hope to see you again soon!\n"
+                           + horizontalLine);
     }
     public static void printCommand(String command){
         if (Objects.equals(command, "bye")) {
@@ -22,17 +24,29 @@ public class Duke {
 
             System.out.println(horizontalLine);
             int taskNum = Integer.parseInt(command.substring(7)) - 1;
-            taskArray[taskNum].markAsUndone();
+            // check
+            taskArray.get(taskNum).markAsUndone();
             System.out.println(horizontalLine);
 
         } else if (command.contains("mark")) {
 
             System.out.println(horizontalLine);
             int taskNum = Integer.parseInt(command.substring(5)) - 1;
-            taskArray[taskNum].markAsDone();
+            // check
+            taskArray.get(taskNum).markAsDone();
             System.out.println(horizontalLine);
 
-        } else {
+        } else if (command.contains("delete")) {
+            int deleteTask = Integer.parseInt(command.substring(7)) - 1;
+            Task removed = taskArray.get(deleteTask);
+            taskArray.remove(deleteTask);
+            numTask--;
+            System.out.println(horizontalLine+ "     Noted. I've removed this task:\n"
+                               + "     " + removed.printDesc()
+                               + "     Now you have " + numTask +" tasks in the list.\n"
+                               + horizontalLine);
+        }
+        else {
             try {
                 addTask(command);
             } catch (DukeException message) {
@@ -45,7 +59,7 @@ public class Duke {
 
             System.out.println(horizontalLine + "     Here are the tasks in your list:\n");
             for (int i = 0; i < numTask; i++) {
-                System.out.println("     " + (i + 1) + ". " + taskArray[i].printDesc());
+                System.out.println("     " + (i + 1) + ". " + taskArray.get(i).printDesc());
             }
             System.out.println(horizontalLine);
 
@@ -53,34 +67,37 @@ public class Duke {
             String[] taskDetails = task.split(" ", 2);
             if (Objects.equals(taskDetails[0], "todo")) {
                 try {
-                    taskArray[numTask] = new Todo(taskDetails[1]);
-                    taskArray[numTask].printMessage(numTask);
+                    taskArray.add(new Todo(taskDetails[1]));
+                    taskArray.get(numTask).printMessage(numTask);
                 } catch (ArrayIndexOutOfBoundsException e) {
                     System.out.println(horizontalLine + "     OOPS!!! The description of todo cannot be empty :(.\n"
                             + horizontalLine);
+                    numTask--;
                 }
                 numTask++;
             } else if (Objects.equals(taskDetails[0], "deadline")) {
                 try {
                     String[] deadlineDetails = taskDetails[1].split("/by", 2);
-                    taskArray[numTask] = new Deadline(deadlineDetails[0], deadlineDetails[1]);
-                    taskArray[numTask].printMessage(numTask);
+                    taskArray.add(new Deadline(deadlineDetails[0], deadlineDetails[1]));
+                    taskArray.get(numTask).printMessage(numTask);
                 } catch (ArrayIndexOutOfBoundsException e) {
                     System.out.println(horizontalLine
                                        + "     OOPS!!! The description of deadline is incomplete.\n"
                                        + horizontalLine);
+                    numTask--;
                 }
                 numTask++;
             } else if (Objects.equals(taskDetails[0], "event")) {
                 try {
                     String[] eventDetails = taskDetails[1].split("/", 3);
-                    taskArray[numTask] = new Event(eventDetails[0], eventDetails[1].substring(5),
-                            eventDetails[2].substring(3));
-                    taskArray[numTask].printMessage(numTask);
+                    taskArray.add(new Event(eventDetails[0], eventDetails[1].substring(5),
+                            eventDetails[2].substring(3)));
+                    taskArray.get(numTask).printMessage(numTask);
                 } catch (ArrayIndexOutOfBoundsException e) {
                     System.out.println(horizontalLine
                                        + "     OOPS!!! The description of event is incomplete :(.\n"
                                        + horizontalLine);
+                    numTask--;
                 }
                 numTask++;
             } else throw new DukeException("     OOPS!!! I'm sorry, but I don't know what that means :-(");
