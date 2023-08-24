4e3bb86578e399c7eced22a92b9e2d6b1d7ddbef
EricXiong420 <E0958551@u.nus.edu>
2023-08-24 17:56:45 +0800

    Level 0. Renamed Chatbot and wrote skeletal version that prints a hello and goodbye message

---
+++ src/main/java/Hachi.java
@@ -0,0 +1,15 @@
+public class Duke {
+
+    public static void main(String[] args) {
+        String name = "Hachi";
+        line();
+        System.out.println("Hello! I'm " + name + "\nWhat cam I do for you?");
+        line();
+        System.out.println("Bye. Hope to see you again soon!");
+        line();
+    }
+
+    public static void line() {
+        System.out.println("____________________________________________________________");
+    }
+}
