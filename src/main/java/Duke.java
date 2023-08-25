01712c5a0707e68bdcdea76a57ebb7dac354adaf
Chang Ji-xuan <changjixuan0410@gmail.com>
2023-08-26 00:34:43 +0800

    Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,6 @@ public class Duke {
     public static void main(String[] args) {
         String name = "Johnnythesnake";
 
-        System.out.println("Hello from\n" + name);
+        System.out.println("Hello I'm " + name + "\n" + "What can I do for you?\n" + "Bye. Hope to see you again soon!");
     }
 }
