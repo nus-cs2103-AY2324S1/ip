04b37b60e5a5439ee669e476637ad05cc56a2937
WinSheng1 <33998278+WinSheng1@users.noreply.github.com>
2023-08-25 19:01:54 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -11,11 +12,23 @@ public class Duke {
                 "Is there anything I can assist you with today?\n" +
                 horizontalLine);
 
+        ArrayList<String> allInputs = new ArrayList<>();
         String userInput;
 
         do {
             userInput = scanner.nextLine();
-            System.out.println(userInput + "\n" + horizontalLine);
+
+            if (userInput.equalsIgnoreCase("list")) {
+                for (int i = 0; i < allInputs.size(); i++) {
+                    System.out.println((i + 1) + ". " + allInputs.get(i));
+                }
+            } else {
+                    System.out.println("added: " + userInput + "\n" + horizontalLine);
+                    if (!userInput.equalsIgnoreCase("list")) {
+                        allInputs.add(userInput);
+                    }
+                }
+
         } while (!userInput.equalsIgnoreCase("bye"));
 
         System.out.println("Goodbye. Catch you later!" + "\n" + horizontalLine);
