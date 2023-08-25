e6b789bce8b3a89b02c391d297e91717c75428be
WinSheng1 <33998278+WinSheng1@users.noreply.github.com>
2023-08-25 19:52:10 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,22 +12,48 @@ public class Duke {
                 "Is there anything I can assist you with today?\n" +
                 horizontalLine);
 
-        ArrayList<String> allInputs = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         String userInput;
 
         do {
             userInput = scanner.nextLine();
 
             if (userInput.equalsIgnoreCase("list")) {
-                for (int i = 0; i < allInputs.size(); i++) {
-                    System.out.println((i + 1) + ". " + allInputs.get(i));
+                for (int i = 0; i < tasks.size(); i++) {
+                    Task task = tasks.get(i);
+                    System.out.println((i + 1) + ".[" + task.getStatusIcon() + "] " + task.description);
+                }
+            } else if (userInput.startsWith("mark ")) {
+                try {
+                    int taskNumber = Integer.parseInt(userInput.substring(5)) - 1;
+                    Task task = tasks.get(taskNumber);
+                        if (task.isDone) {
+                            System.out.println("Task " + (taskNumber + 1) + " is already done!\n" + horizontalLine);
+                        } else {
+                            task.isDone = true;
+                            System.out.println("Great job! Task " + (taskNumber + 1) + " is done!\n" + horizontalLine);
+                        }
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Invalid task number. Please try again. ");
+                }
+            } else if (userInput.startsWith("unmark ")) {
+                try {
+                    int taskNumber = Integer.parseInt(userInput.substring(7)) - 1;
+                    Task task = tasks.get(taskNumber);
+                        if (!task.isDone) {
+                            System.out.println("Task " + (taskNumber + 1) + " is still incomplete.\n" + horizontalLine);
+                        } else {
+                            task.isDone = false;
+                            System.out.println("No worries, Task " + (taskNumber + 1) + " is incomplete.\n" + horizontalLine);
+                        }
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Invalid task number. Please try again. ");
                 }
             } else {
-                    System.out.println("added: " + userInput + "\n" + horizontalLine);
-                    if (!userInput.equalsIgnoreCase("list")) {
-                        allInputs.add(userInput);
-                    }
-                }
+                    System.out.println("added task: " + userInput + "\n" + horizontalLine);
+                    Task task = new Task(userInput);
+                    tasks.add(task);
+            }
 
         } while (!userInput.equalsIgnoreCase("bye"));
 
