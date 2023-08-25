a3d51ea5de7da764f970bc5a895584e1bff35c53
JasonLCY-Temp <e0407197@u.nus.edu>
2023-08-25 14:27:49 +0800

    Update Deadline and Event classes to be functional

--- src/main/java/jeeves/task/Deadline.java
+++ src/main/java/jeeves/task/Deadline.java
@@ -1,4 +1,16 @@
 package jeeves.task;
 
-public class Deadline {
+public class Deadline extends Task {
+
+    protected String by;
+
+    public Deadline(String desc, String by) {
+        super(desc);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(this.id + ". [D]" + super.toString() + " (by: " + by + ")");
+    }
 }
--- src/main/java/jeeves/task/Event.java
+++ src/main/java/jeeves/task/Event.java
@@ -1,4 +1,18 @@
 package jeeves.task;
 
-public class Event {
+public class Event extends Task {
+
+    protected String from;
+    protected String to;
+
+    public Event(String desc, String from, String to) {
+        super(desc);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(this.id + ". [E]" + super.toString() + " (from: " + from + " to: " + to + ")");
+    }
 }
