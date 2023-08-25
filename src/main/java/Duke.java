1504d883e4cbdd0baf486ff1870a6faec82419ca
peiran <she_peiran@u.nus.edu>
2023-08-25 20:00:02 +0800

    update Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner io = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Husky\n" +
                 " What can I do for you?\n" +
@@ -14,8 +16,15 @@ public class Duke {
                         " Bye. Hope to see you again soon!\n" +
                         "____________________________________________________________\n");
                 break;
+            } else if (echo.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(String.format("%d. %s", i + 1, list.get(i)));
+                }
+                System.out.println("____________________________________________________________");
             } else {
-                System.out.println("____________________________________________________________\n" + echo + "\n" +
+                list.add(echo);
+                System.out.println("____________________________________________________________\n" + "added: " + echo + "\n" +
                         "____________________________________________________________");
             }
         }
