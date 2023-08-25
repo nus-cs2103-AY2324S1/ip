d9ed55fcb492a1611dafa452a0d3848248dca85f
Angelyxx <xuan2angel@gmail.com>
2023-08-25 23:56:11 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +8,32 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String chatbotName = "Bleep";
+        String header = "____________________________________________________________\n" +
+                "Hello! I'm " + chatbotName +
+                " \nWhat can I do for you?";
+        ArrayList<String> userEntries = new ArrayList<>();
+        System.out.println(header);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("____________________________________________________________\n");
+            String userInput = scanner.nextLine();
+            System.out.println("____________________________________________________________\n");
+            if (userInput.equals("bye")){
+                System.out.println("\tBye. Hope to see you again soon!");
+                break;
+            } else if (userInput.equals("list")) {
+                System.out.println("Here are your entries:");
+                for (int i = 0; i < userEntries.size(); i++) {
+                    System.out.println("\t" + (i + 1) + ". " + userEntries.get(i));
+                }
+            } else {
+                userEntries.add(userInput);
+                System.out.println("\t added: " + userInput);
+                //System.out.println();
+            }
+        }
     }
 }
