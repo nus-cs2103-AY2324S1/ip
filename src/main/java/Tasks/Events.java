7161df8cef8a00b83b40186818b133a0f2f72168
Xuan Yi <saltedfishxx@gmail.com>
2023-08-26 02:41:57 +0800

    Complete base Level-8: DateTime format

--- src/main/java/Tasks/Events.java
+++ src/main/java/Tasks/Events.java
@@ -1,4 +1,8 @@
 package Tasks;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Adapted from Partial Solution provided by https://nus-cs2103-ay2324s1.github.io/website/schedule/week2/project.html
  * A child class of Tasks to create tasks that start at a specific date/time and ends at a specific date/time.
@@ -6,10 +10,11 @@ package Tasks;
  */
 public class Events extends Task {
 
-    protected final String from;
-    protected final String to;
+    protected final LocalDateTime from;
+    protected final LocalDateTime to;
+    private final static DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    public Events(String description, Boolean isDone, String from, String to) {
+    public Events(String description, Boolean isDone, LocalDateTime from, LocalDateTime to) {
         super(description, isDone);
         this.from = from;
         this.to = to;
@@ -17,12 +22,14 @@ public class Events extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
+        return "[E]" + super.toString() + " (from: " + this.from.format(dateTimeFormat)
+                + " to: " + this.to.format(dateTimeFormat) + ")";
     }
 
     @Override
     public String getData() {
         String marked = isDone ? "1" : "0";
-        return "E | " + marked + " | " + this.taskDesc + " | " + this.from + " | " + this.to;
+        return "E | " + marked + " | " + this.taskDesc + " | " + this.from.format(dateTimeFormat)
+                + " | " + this.to.format(dateTimeFormat);
     }
 }
