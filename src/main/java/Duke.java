f5d98f2f20ccb9f3f786c17334e540eabce5d483
limzhenwy <zwlim38@gmail.com>
2023-08-26 17:01:32 +0800

    Erronous commit and push in the last push

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,7 +13,7 @@ public class Duke {
          */
         protected String description;
         /**
-         * Boolean for whether the task is completed or not.
+         * Whether the task is completed or not.
          */
         protected boolean isDone;
 
@@ -27,7 +27,7 @@ public class Duke {
         }
 
         /**
-         * String of the task's completion status.
+         * Task's completion status.
          * @return An "X" if the task is completed and a blank space otherwise
          */
         public String getStatusIcon() {
@@ -66,93 +66,195 @@ public class Duke {
      */
     public class Todo extends Task {
         /**
-         * String that signifies the specific type of task.
+         * Signifies the specific type of task.
          */
         protected String type = "T";
+
+        /**
+         * Constructor for the Todo task type.
+         * @param description String describing what the Todo task is about
+         */
         public Todo(String description) {
             super(description);
         }
+
+        /**
+         * Mark this Todo task as done.
+         */
         @Override
         public void mark() {
             this.isDone = true;
             System.out.println("Nice! I've marked this task as done:\n"
                     + this);
         }
+
+        /**
+         * Unmark this Todo task as not done yet.
+         */
         @Override
         public void unmark() {
             this.isDone = false;
             System.out.println("OK, I've marked this task as not done yet:\n"
                     + this);
         }
+
+        /**
+         * Description of this Todo task.
+         * @return String containing all relevant information about this Todo task
+         */
         @Override
         public String toString() {
             return "[" + this.type + "]" + "[" + this.getStatusIcon() + "]" + this.description;
         }
     }
+
+    /**
+     * Special kind of task that has a description and a deadline
+     */
     public class Deadline extends Task {
+        /**
+         * Signifies the specific type of task.
+         */
         protected String type = "D";
+        /**
+         * The date where the task should be completed by.
+         */
         protected String date;
+
+        /**
+         * Constructor for the Deadline class.
+         * @param description Describes the task
+         * @param date When the task should be completed by
+         */
         public Deadline(String description, String date) {
             super(description);
             this.date = date;
         }
+
+        /**
+         * Mark this Deadline task as done.
+         */
         @Override
         public void mark() {
             this.isDone = true;
             System.out.println("Nice! I've marked this task as done:\n"
             + this);
         }
+
+        /**
+         * Unmark this Deadline task as not done yet.
+         */
         @Override
         public void unmark() {
             this.isDone = false;
             System.out.println("OK, I've marked this task as not done yet:\n"
             + this);
         }
+
+        /**
+         * Description of this Deadline task.
+         * @return String containing the all the information of this Deadline task
+         */
         @Override
         public String toString() {
             return "[" + this.type + "]" + "[" + this.getStatusIcon() + "] " + this.description
                     + " (by:" + this.date + ")";
         }
     }
+
+    /**
+     * Special type of task that has a description, 'from' date and 'to' date.
+     */
     public class Event extends Task {
+        /**
+         * Signifies the 'Event' task.
+         */
         protected String type = "E";
+        /**
+         * Starting date of this task.
+         */
         protected String from;
+        /**
+         * Ending date of this task.
+         */
         protected String to;
+
+        /**
+         * Constructor for Event class.
+         * @param description Describes the event
+         * @param from Start of the event
+         * @param to End of the event
+         */
         public Event(String description, String from, String to) {
             super(description);
             this.from = from;
             this.to = to;
         }
+
+        /**
+         * Mark this Event task as done.
+         */
         @Override
         public void mark() {
             this.isDone = true;
             System.out.println("Nice! I've marked this task as done:\n"
                     + this);
         }
+
+        /**
+         * Unmark this Event task as not done yet.
+         */
         @Override
         public void unmark() {
             this.isDone = false;
             System.out.println("OK, I've marked this task as not done yet:\n"
                     + this);
         }
+
+        /**
+         * Description of this Event task.
+         * @return String containing all the relevant information of this Event task
+         */
         @Override
         public String toString() {
             return "[" + this.type + "]" + "[" + this.getStatusIcon() + "] " + this.description
                     + " (from: " + this.from + " to:" + this.to + ")";
         }
     }
+
+    /**
+     * Special exceptions that could be encountered by the chatbot.
+     */
     public static class DukeException extends Exception {
+        /**
+         * Constructor for the DukeException class.
+         * @param errorMessage Message about the error.
+         */
         public DukeException(String errorMessage) {
             super(errorMessage);
         }
+
+        /**
+         * Exception when no description is found.
+         */
         public static class EmptyDescription extends DukeException {
+            /**
+             * Constructor for the EmptyDescription class.
+             */
             public EmptyDescription() {
                 super("-------------------------------\n"
                         + "☹ OOPS!!! The description of a todo cannot be empty.\n"
                         + "-------------------------------\n");
             }
         }
+
+        /**
+         * Exception when an illegal argument is found.
+         */
         public static class WrongInput extends DukeException {
+            /**
+             * Constructor for the WrongInput class.
+             */
             public WrongInput() {
                 super("-------------------------------\n"
                       +  "☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n"
@@ -160,6 +262,11 @@ public class Duke {
             }
         }
     }
+
+    /**
+     * Creates a Todo task in taskArr.
+     * @param desc Description of the Todo task
+     */
     public void addTodo(String desc) {
         Todo curr = new Todo(desc);
         taskArr.add(curr);
@@ -169,6 +276,12 @@ public class Duke {
                 + totalTasks()
                 + "\n-------------------------------\n");
     }
+
+    /**
+     * Creates a Deadline task in taskArr.
+     * @param desc Description of the Deadline task
+     * @param date Date to complete the Deadline task by
+     */
     public void addDeadline(String desc, String date) {
         Deadline curr = new Deadline(desc, date);
         taskArr.add(curr);
@@ -178,6 +291,13 @@ public class Duke {
                 + totalTasks()
                 + "\n-------------------------------\n");
     }
+
+    /**
+     * Creates an Event task in taskArr.
+     * @param desc Description of the Event task
+     * @param from Date when the Event task starts
+     * @param to Date when the Event tasks end
+     */
     public void addEvent(String desc, String from, String to) {
         Event curr = new Event(desc, from, to);
         taskArr.add(curr);
@@ -187,6 +307,11 @@ public class Duke {
                 + totalTasks()
                 + "\n-------------------------------\n");
     }
+
+    /**
+     * Remove a task from taskArr.
+     * @param num Indicates the task number to be deleted
+     */
     public void delete(int num) {
         Task toRemove = taskArr.get(num);
         taskArr.remove(num);
@@ -196,6 +321,10 @@ public class Duke {
         + totalTasks()
         + "\n-------------------------------\n");
     }
+
+    /**
+     * Lists out all the tasks in taskArr.
+     */
     public void listOut() {
         int size = taskArr.size();
         System.out.println("-------------------------------\n"
@@ -205,6 +334,11 @@ public class Duke {
         }
         System.out.println("-------------------------------\n");
     }
+
+    /**
+     * Number of tasks in taskArr currently.
+     * @return String containing the number of tasks added to taskArr
+     */
     public String totalTasks() {
         int size = taskArr.size();
         return "\nNow you have " + size + " tasks in the list.";
@@ -225,8 +359,8 @@ public class Duke {
 
         System.out.println(greeting);
 
-        try {
-            while (bot.sc.hasNext()) {
+        while (bot.sc.hasNext()) {
+            try {
                 String str = bot.sc.next();
                 if (str.equals("bye")) {
                     System.out.println(exit);
@@ -260,8 +394,7 @@ public class Duke {
                             bot.delete(num - 1);
                         }
                     }
-                }
-                else {
+                } else {
                     // check for task type first
                     if (str.equals("todo")) {
                         String desc = bot.sc.nextLine();
@@ -310,13 +443,11 @@ public class Duke {
                         throw new DukeException.WrongInput();
                     }
                 }
+            } catch (DukeException.EmptyDescription e) {
+                    System.out.println(e.getMessage());
+            } catch (DukeException.WrongInput e) {
+                    System.out.println(e.getMessage());
             }
-        } catch (DukeException.EmptyDescription e) {
-            System.out.println(e.getMessage());
-        } catch (DukeException.WrongInput e) {
-            System.out.println(e.getMessage());
-        } finally {
-            bot.sc.close();
         }
     }
 }
