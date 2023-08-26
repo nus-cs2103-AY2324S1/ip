3f8e2fdbcdedda84bf34c7d609c891dd1acbdc3f
andrefoo <76435197+andrefoo@users.noreply.github.com>
2023-08-26 13:45:59 +0800

    Level 5: Error Handling

--- src/main/java/Dre.java
+++ src/main/java/Dre.java
@@ -22,25 +22,49 @@ public class Dre {
 
     public void add(String next) {
         if (next.startsWith("todo")) {
-            ToDo task = new ToDo(next.substring(5));
+            String desc = next.substring(5);
+            if (desc.isBlank()) {
+                System.out.println("OOPS!!! The description of a todo cannot be empty.");
+                return;
+            }
+            ToDo task = new ToDo(desc);
             list.add(task);
 
             System.out.println("Got it. I've added this task:");
             System.out.println(task.toString());
         } else if (next.startsWith("deadline")) {
-            Deadline task = new Deadline(next.substring(9, next.lastIndexOf('/') - 1), next.substring(next.lastIndexOf("/by") + 4));
+            String desc = next.substring(9, next.lastIndexOf('/') - 1);
+            String by = next.substring(next.lastIndexOf("/by") + 4);
+            if (desc.isBlank()) {
+                System.out.println("OOPS!!! The description of a deadline cannot be empty.");
+                return;
+            }
+            if (by.isBlank()) {
+                System.out.println("OOPS!!! The date of a deadline cannot be empty.");
+                return;
+            }
+            Deadline task = new Deadline(desc, by);
             list.add(task);
             System.out.println("Got it. I've added this task:");
-            System.out.println(task.toString());
+            System.out.println(task);
         } else if (next.startsWith("event")) {
-            Event task = new Event(next.substring(6, next.indexOf("/from") - 1),
-                                    next.substring(next.indexOf("/from") + 6, next.indexOf("/to") - 1),
-                                    next.substring(next.indexOf("/to") + 4));
+            String desc = next.substring(6, next.indexOf("/from") - 1);
+            String from = next.substring(next.indexOf("/from") + 6, next.indexOf("/to") - 1);
+            String to = next.substring(next.indexOf("/to") + 4);
+            if (desc.isBlank()) {
+                System.out.println("OOPS!!! The description of an event cannot be empty.");
+                return;
+            }
+            if (from.isBlank() || to.isBlank()) {
+                System.out.println("OOPS!!! The dates of an event cannot be empty.");
+                return;
+            }
+            Event task = new Event(desc, from, to);
             list.add(task);
             System.out.println("Got it. I've added this task:");
-            System.out.println(task.toString());
+            System.out.println(task);
         } else {
-            //throw error
+            System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
         }
         System.out.println("Now you have " + list.size() + " tasks in the list.");
     }
@@ -52,23 +76,46 @@ public class Dre {
     }
 
     public void mark(String next) {
+//        int start = next.lastIndexOf(' ');
+//        int taskIndex = Integer.parseInt(next.substring(start + 1));
+//        //error handling for non integer
+//        Task currTask = list.get(taskIndex - 1);
+//        currTask.done();
+//        System.out.println("Nice! I've marked this task as done:");
+//        System.out.println(currTask.toString());
         int start = next.lastIndexOf(' ');
-        int taskIndex = Integer.parseInt(next.substring(start + 1));
-        //error handling for non integer
-        Task currTask = list.get(taskIndex - 1);
-        currTask.done();
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.println(currTask.toString());
+        try {
+            int taskIndex = Integer.parseInt(next.substring(start + 1));
+
+            if (taskIndex <= 0 || taskIndex > list.size()) {
+                System.out.println("Invalid task index.");
+            } else {
+                Task currTask = list.get(taskIndex - 1);
+                currTask.done();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(currTask);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Task index must be an integer.");
+        }
     }
 
     public void unmark(String next) {
         int start = next.lastIndexOf(' ');
-        int taskIndex = Integer.parseInt(next.substring(start + 1));
-        //error handling for non integer
-        Task currTask = list.get(taskIndex - 1);
-        currTask.done();
-        System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println(currTask.toString());
+        try {
+            int taskIndex = Integer.parseInt(next.substring(start + 1));
+
+            if (taskIndex <= 0 || taskIndex > list.size()) {
+                System.out.println("Invalid task index.");
+            } else {
+                Task currTask = list.get(taskIndex - 1);
+                currTask.done();
+                System.out.println("Ok! I've marked this task as undone:");
+                System.out.println(currTask);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Task index must be an integer.");
+        }
     }
 
     public static void main(String[] args) {
@@ -83,8 +130,10 @@ public class Dre {
                 dre.mark(next);
             } else if (next.startsWith("unmark")) {
                 dre.unmark(next);
-            } else {
+            } else if (!next.trim().isEmpty()) {
                 dre.add(next);
+            } else {
+                System.out.println("Invalid input.");
             }
             next = sc.nextLine();
         }
