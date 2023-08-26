4e8e77aa3b0e2e520af73a50eee5a97c90c62f74
Chang Ji-xuan <changjixuan0410@gmail.com>
2023-08-26 21:03:49 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         String name = "Johnnythesnake";
@@ -6,18 +7,28 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a command: ");
-
+        ArrayList<String> commandList = new ArrayList<>();
         while (true) {
             System.out.print("Enter a command: ");
             String command = scanner.nextLine();
-            if (command.equals("bye")) {
+            if (command.equalsIgnoreCase("bye")) {
                 Exit exit = new Exit();
                 System.out.println(exit.exitMessage());
                 break;
+            } else if (command.equalsIgnoreCase("list")){
+                if (!commandList.isEmpty()) {
+                    for (int i = 1; i <= commandList.size(); i++) {
+                        System.out.println(i + " ." + commandList.get(i-1));
+                    }
+                } else {
+                    System.out.println("");
+                }
             } else {
-                System.out.println(command);
+                System.out.println("added: " + command);
+                commandList.add(command);
 
             }
+
         }
     }
 }
