37be297cf5a70e8e6ce890b622b01ae10ed7648a
leezhanpeng <zhanpenglee@outlook.com>
2023-08-27 02:55:27 +0800

    Make Robert understand Dates and Add clear and filter commands

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,26 @@
-public class Deadline extends Task {
-    protected String by;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String by) {
+public class Deadline extends Task {
+    protected LocalDate byDate;
+
+    public Deadline(String description, LocalDate by) {
         super(description);
-        this.by = by;
+        this.byDate = by;
     }
 
-     public Deadline(String description, String by, boolean isDone) {
+     public Deadline(String description, LocalDate by, boolean isDone) {
         super(description, isDone);
-        this.by = by;
+        this.byDate = by;
+    }
+
+    public boolean isDueOn(LocalDate date) {
+        return this.byDate.equals(date);
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), by);
+        return String.format("[D]%s (by: %s)", super.toString(),
+                this.byDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy")));
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,22 +1,31 @@
-public class Event extends Task {
-    protected String from;
-    protected String to;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String description, String from, String to) {
+public class Event extends Task {
+    protected LocalDate fromDate;
+    protected LocalDate toDate;
+
+    public Event(String description, LocalDate from, LocalDate to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.fromDate = from;
+        this.toDate = to;
     }
 
-    public Event(String description, String from, String to, boolean isDone) {
+    public Event(String description, LocalDate from, LocalDate to, boolean isDone) {
         super(description, isDone);
-        this.from = from;
-        this.to = to;
+        this.fromDate = from;
+        this.toDate = to;
+    }
+
+    public boolean isHappeningOn(LocalDate date) {
+        return !(this.toDate.isBefore(date) || this.fromDate.isAfter(date));
     }
 
     @Override
     public String toString() {
         return String.format("[E]%s (from: %s to: %s)",
-                super.toString(), from, to);
+                super.toString(),
+                this.fromDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy")),
+                this.toDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy")));
     }
 }
