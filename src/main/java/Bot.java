ffa88fa7399d87c97ce9439db9d125bc37dbfe42
Xihua Zhu <xihua12370@gmail.com>
2023-08-26 18:16:25 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,11 @@
+public class Bot {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n" +
+                " Hello! I'm [YOUR CHATBOT NAME]\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
+    }
+}
+}
