05c1586a135d1903d5aec704dbebcd05f4d1fbf4
imkwokyong <darrenkuan@gmail.com>
2023-08-27 04:16:08 +0800

    Add Save function for Level-7

---
+++ src/main/java/Storage.java
@@ -0,0 +1,77 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+class Storage {
+    private final String filePath;
+    public Storage(String filePath) {
+        this.filePath = filePath;
+    }
+
+    void createFile() throws IOException {
+        String[] temp = filePath.split("/");
+        String directoryPath = "";
+        for (int i = 0; i < temp.length - 1; i++) {
+            directoryPath += temp[i];
+            File directory = new File(directoryPath);
+            if (!directory.isDirectory()) {
+                directory.mkdir();
+            }
+            directoryPath += "/";
+        }
+        File file = new File(filePath);
+        file.createNewFile();
+    }
+
+    File loadFile() throws IOException {
+        File file = new File(filePath);
+        if (!file.exists()) {
+            createFile();
+        }
+        return file;
+    }
+
+    void writeFile(ArrayList<Task> tasks) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        for (Task task: tasks) {
+            fw.write(task.stringToFile() + "\n");
+        }
+        fw.close();
+    }
+    void appendFile(Task task) throws IOException {
+        FileWriter fw = new FileWriter(filePath, true);
+        fw.write(task.stringToFile() + "\n");
+        fw.close();
+    }
+
+    ArrayList<Task> createList() throws IOException {
+        ArrayList<Task> taskList = new ArrayList<>();
+        Scanner sc = new Scanner(loadFile());
+        while (sc.hasNext()) {
+            String[] temp = sc.nextLine().split(" \\| ");
+            for (String s: temp) {
+                System.out.print(s);
+            }
+            switch (temp[0].strip()) {
+            case "T":
+                taskList.add(new ToDo(temp[2].strip()));
+                break;
+            case "D":
+                taskList.add(new Deadline(temp[2].strip(), temp[3].strip()));
+                break;
+            case "E":
+                taskList.add(new Event(temp[2].strip(), temp[3].strip(), temp[4].strip()));
+                break;
+            default:
+                break;
+            }
+            if (temp[1].strip().equals("1")) {
+                taskList.get(taskList.size() - 1).markIsDone();
+            }
+        }
+        return taskList;
+    }
+
+}
