fe29b0f9781439417b12b42ab86139441d39cc45
QianChangru <changru_qian@u.nus.edu>
2023-08-26 21:20:10 +0800

    Finish Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
 
@@ -5,22 +6,37 @@ public class Duke {
 
         Scanner userInput = new Scanner(System.in);
 
-        System.out.println("    ____________________________________________________________");
-        System.out.println("    Hello! I'm Jarvis");
-        System.out.println("    What can I do for you?");
-        System.out.println("    ____________________________________________________________");
+        ArrayList<String> list = new ArrayList<>();
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Jarvis");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
 
         String command = userInput.nextLine();
 
         while (!command.equals("bye")) {
-            System.out.println("    ____________________________________________________________");
-            System.out.println("    " + command);
-            System.out.println("    ____________________________________________________________");
+            switch (command) {
+                case "list":
+                    System.out.println("____________________________________________________________");
+                    int counter = 0;
+                    while (counter != list.size()) {
+                        System.out.println(" " + counter + ". " + list.get(counter));
+                        counter++;
+                    }
+                    System.out.println("____________________________________________________________");
+                    break;
+                default:
+                    list.add(command);
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" added: " + command);
+                    System.out.println("____________________________________________________________");
+            }
             command = userInput.nextLine();
         }
 
-        System.out.println("    ____________________________________________________________");
-        System.out.println("    Bye. Hope to see you again soon!");
-        System.out.println("    ____________________________________________________________");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
