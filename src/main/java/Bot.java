c35740189d6b66b9be3d44f61f2e95cf3453a1be
Xihua Zhu <xihua12370@gmail.com>
2023-08-26 23:42:32 +0800

    Level 1. Echo

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
+
 public class Bot {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm [YOUR CHATBOT NAME]\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                "____________________________________________________________");
+        String str = sc.nextLine();
+        Storage<String> storage = new Storage<String>();
+        while (!str.equals("bye")) {
+            System.out.println("____________________________________________________________\n" +
+                    str + "\n" +
+                    "____________________________________________________________");
+            str = sc.nextLine();
+        }
+        System.out.println("____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+                "____________________________________________________________");
     }
-}
 }
