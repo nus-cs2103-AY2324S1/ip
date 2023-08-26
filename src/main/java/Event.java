52e9286ccc895ac561a960bdbf326dea69d76bc6
leezhanpeng <zhanpenglee@outlook.com>
2023-08-27 00:54:34 +0800

    Enable saving of tasks to hard disk

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -8,6 +8,12 @@ public class Event extends Task {
         this.to = to;
     }
 
+    public Event(String description, String from, String to, boolean isDone) {
+        super(description, isDone);
+        this.from = from;
+        this.to = to;
+    }
+
     @Override
     public String toString() {
         return String.format("[E]%s (from: %s to: %s)",
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -8,6 +8,11 @@ public class Task {
         this.isDone = false;
     }
 
+    public Task(String description, boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
