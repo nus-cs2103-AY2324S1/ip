d7765d363484e6b15781080cd8cac49b88590eaa
imkwokyong <darrenkuan@gmail.com>
2023-08-27 02:04:37 +0800

    Add constructor overloading for Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,9 +2,13 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
-    public Task(String description) {
+    public Task(String description, boolean isDone) {
         this.description = description;
-        this.isDone = false;
+        this.isDone = isDone;
+    }
+
+    public Task(String description) {
+        this(description, false);
     }
 
     public String getStatusIcon() {
