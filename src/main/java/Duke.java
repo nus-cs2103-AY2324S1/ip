ef182282245017b12b12a3d8a50241541d163607
majedah <majedah.alnaseri@gmail.com>
2023-08-26 16:06:11 +0800

    Complete Level 3 Implementation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Duke {
 
-    private static String[] currList = new String[100]; // array to store users' input
+    private static Task[] currList = new Task[100]; // array to store users' input
     private static int counter = 0; // current input
 
     public static void main(String[] args) {
@@ -23,19 +23,46 @@ public class Duke {
             String currInput = sc.nextLine();
             if (currInput.equals("bye")) {
                 System.out.println(">  ok thanks bye");
-                System.out.println(separator);
                 break;
             } else if (currInput.equals("list")) {
-                for (int i = 0; i < counter; i++) {
-                    System.out.println((i+1) + ") " + currList[i]);
+                if (counter == 0) {
+                    System.out.println(">  You have no tasks :)");
+                } else {
+                    System.out.println(">  Your tasks: ");
+                    for (int i = 0; i < counter; i++) {
+
+                        System.out.println((i + 1) + ") " + currList[i]);
+                    }
                 }
-                System.out.println(separator);
+
+            } else if (currInput.startsWith("mark")) {
+                int index = Integer.parseInt(currInput.split(" ")[1]) - 1;
+                if (index >= 0 && index < counter) {
+                    currList[index].setCompleted();
+                    System.out.println(">  ok, you have completed Task " + index + 1);
+                    System.out.println(currList[index]);
+                } else {
+                    System.out.println(">  Task " + (index + 1) + " was not found :(");
+                }
+            } else if (currInput.startsWith("unmark")) {
+                int index = Integer.parseInt(currInput.split(" ")[1])-1;
+                if (index >= 0 && index < counter) {
+                currList[index].setNotCompleted();
+                System.out.println(">  ok, you haven't completed Task " + index + 1);
+                System.out.println(currList[index]);
+                } else {
+                    System.out.println(">  Task " + (index + 1) + " was not found :(");
+                }
             } else {
-                currList[counter] = currInput;
-                counter++;
-                System.out.println(">  added: " + currInput);
-                System.out.println(separator);
+                if (currInput.isEmpty()) {
+                    System.out.println(">  empty tasks are not allowed :/");
+                } else {
+                    currList[counter] = new Task(currInput);
+                    counter++;
+                    System.out.println(">  added: " + currInput);
+                }
             }
+            System.out.println(separator);
         }
         sc.close();
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,37 @@
+public class Task {
+    protected String taskName;
+    protected boolean completed;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.completed = false;
+    }
+
+    public String getName() {
+        return taskName;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted();
+    }
+
+    public void setCompleted() {
+        completed = true;
+    }
+
+    public void setNotCompleted() {
+        completed = false;
+    }
+
+    @Override
+    public String toString() {
+        char mark;
+        if (completed) {
+            mark = 'X';
+        } else {
+            mark = ' ';
+        }
+
+        return "[" + mark + "] " + taskName;
+    }
+}
