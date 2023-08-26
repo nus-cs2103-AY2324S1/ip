b6d24aab755d6660344c1b79a7f91bdda55e1bdc
austi <austinhuang1203@gmail.com>
2023-08-26 16:28:23 +0800

    Implement Level-8
    Current state: No date time formatter, just accepts string
    
    Why problem:Not good as can key in anything
    
    This commit: Fixes this by formatting date for event and deadline
    
    This commit: Saves the tasklist to harddrive and loads it when you open ur program

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,19 +1,28 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 //this class represents an event
 public class Event extends Task{
-    String start;
-    String end;
+    LocalDate start;
+    LocalDate end;
     //default constructor to store start and end
     public Event(String name, String start, String end) {
         super(name);
-        this.start = start;
-        this.end = end;
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        try {
+            this.start = LocalDate.parse(start, inputFormatter);
+            this.end=LocalDate.parse(end, inputFormatter);
+        } catch (Exception e) {
+            System.out.println("Invalid date!");
+        }
     }
 
     //default display to represent the event object
     public String display() {
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
         if(done) {
-            return "[E][X] " + this.name + " (From: " + start + " To: " + end + ")";
+            return "[E][X] " + this.name + " (From: " + start.format(outputFormatter) + " To: " + end.format(outputFormatter) + ")";
         }
-        return "[E][] " + this.name + " (From: " + start + " To: " + end + ")";
+        return "[E][] " + this.name + " (From: " + start.format(outputFormatter) + " To: " + end.format(outputFormatter) + ")";
     }
 }
