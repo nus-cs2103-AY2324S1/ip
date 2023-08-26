50139be8acb89956c4f1072763b51e452800cb7a
austi <austinhuang1203@gmail.com>
2023-08-26 19:08:37 +0800

    Implement A-JUnit Current: No test cases Why problem: We need test cases this fix: Added test cases for 2 classes

---
+++ src/test/java/duke/EventTest.java
@@ -0,0 +1,54 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class EventTest {
+
+    @Test
+    public void testEventDisplayNotDone() {
+        Event event = new Event("Conference", "2023-08-25", "2023-08-27");
+
+        String expected = "[E][] Conference (From: Aug 25 2023 To: Aug 27 2023)";
+        String result = event.display();
+
+        assertEquals(expected, result, "Event display test (not done) failed: incorrect result");
+    }
+
+    @Test
+    public void testEventDisplayDone() {
+        Event event = new Event("Party", "2023-08-30", "2023-09-01");
+        event.done = true;
+
+        String expected = "[E][X] Party (From: Aug 30 2023 To: Sep 01 2023)";
+        String result = event.display();
+
+        assertEquals(expected, result, "Event display test (done) failed: incorrect result");
+    }
+
+    @Test
+    public void testEventDisplayWithDifferentDateFormat() {
+        Event event = new Event("Meeting", "2023-12-15", "2023-12-20");
+        event.done = true;
+
+        String expected = "[E][X] Meeting (From: Dec 15 2023 To: Dec 20 2023)";
+        String result = event.display();
+
+        assertEquals(expected, result, "Event display test (different date format) failed: incorrect result");
+    }
+
+    @Test
+    public void testEventDisplay2() {
+        Event event = new Event("Workshop", "2023-11-05", "2023-11-07");
+
+        String expected = "[E][] Workshop (From: Nov 05 2023 To: Nov 07 2023)";
+        String result = event.display();
+
+        assertEquals(expected, result, "Event display test failed: incorrect result");
+    }
+
+}
+
---
+++ src/test/java/duke/MarkPatternTest.java
@@ -0,0 +1,93 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MarkPatternTest {
+
+    @Test
+    public void testMarkPatternWithValidInput() {
+        MarkPattern markPattern = new MarkPattern();
+        int result = markPattern.mark("mark 5");
+        assertEquals(4, result, "Mark pattern test failed: incorrect result");
+    }
+
+    @Test
+    public void testMarkPatternWithDifferentInput() {
+        MarkPattern markPattern = new MarkPattern();
+        int result = markPattern.mark("mark 10");
+        assertEquals(9, result, "Mark pattern test failed: incorrect result");
+    }
+
+    @Test
+    public void testMarkPatternWithNoNumber() {
+        MarkPattern markPattern = new MarkPattern();
+        int result = markPattern.mark("mark");
+        assertEquals(-1, result, "Mark pattern test failed: incorrect result");
+    }
+
+    @Test
+    public void testMarkPatternWithSpaces() {
+        MarkPattern markPattern = new MarkPattern();
+        int result = markPattern.mark("  mark    7 ");
+        assertEquals(6, result, "Mark pattern test failed: incorrect result");
+    }
+
+    @Test
+    public void testUnmarkPatternWithValidInput() {
+        MarkPattern markPattern = new MarkPattern();
+        int result = markPattern.unmark("unmark 3");
+        assertEquals(2, result, "Unmark pattern test failed: incorrect result");
+    }
+
+    @Test
+    public void testUnmarkPatternWithDifferentInput() {
+        MarkPattern markPattern = new MarkPattern();
+        int result = markPattern.unmark("unmark 8");
+        assertEquals(7, result, "Unmark pattern test failed: incorrect result");
+    }
+
+    @Test
+    public void testUnmarkPatternWithNoNumber() {
+        MarkPattern markPattern = new MarkPattern();
+        int result = markPattern.unmark("unmark");
+        assertEquals(-1, result, "Unmark pattern test failed: incorrect result");
+    }
+
+    @Test
+    public void testUnmarkPatternWithSpaces() {
+        MarkPattern markPattern = new MarkPattern();
+        int result = markPattern.unmark("  unmark   5 ");
+        assertEquals(4, result, "Unmark pattern test failed: incorrect result");
+    }
+
+    @Test
+    public void testDeletePatternWithValidInput() {
+        MarkPattern markPattern = new MarkPattern();
+        int result = markPattern.del("delete 2");
+        assertEquals(1, result, "Delete pattern test failed: incorrect result");
+    }
+
+    @Test
+    public void testDeletePatternWithDifferentInput() {
+        MarkPattern markPattern = new MarkPattern();
+        int result = markPattern.del("delete 7");
+        assertEquals(6, result, "Delete pattern test failed: incorrect result");
+    }
+
+    @Test
+    public void testDeletePatternWithNoNumber() {
+        MarkPattern markPattern = new MarkPattern();
+        int result = markPattern.del("delete");
+        assertEquals(-1, result, "Delete pattern test failed: incorrect result");
+    }
+
+    @Test
+    public void testDeletePatternWithSpaces() {
+        MarkPattern markPattern = new MarkPattern();
+        int result = markPattern.del("  delete   3 ");
+        assertEquals(2, result, "Delete pattern test failed: incorrect result");
+    }
+}
+
+
