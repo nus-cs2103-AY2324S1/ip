e03ceedfcce23ca4694bc26db8276579d560c87b
Tim-Siu <e1088140@u.nus.edu>
2023-08-27 11:43:04 +0800

    Updated syntax to meet requirement

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -39,30 +39,30 @@ public class Duke {
 
 
         switch (taskType) {
-            case TODO:
-                try {
-                    return new ToDo(splittedInput[1]);
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    throw new NoCommandDetailException("OOPS!!! The description of a todo cannot be empty.");
-                }
-            case DEADLINE:
-                try {
-                    String[] deadlineParts = splitByDelimiter(splittedInput[1], " /by ");
-                    return new Deadline(deadlineParts[0], deadlineParts[1]);
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    throw new NoCommandDetailException("OOPS!!! The description of a deadline cannot be empty.");
-                }
-            case EVENT:
-                try {
-                    String[] eventParts = splitByDelimiter(splittedInput[1], " /from ");
-                    String eventName = eventParts[0];
-                    String[] eventTimes = splitByDelimiter(eventParts[1], " /to ");
-                    return new Event(eventName, eventTimes[0], eventTimes[1]);
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    throw new NoCommandDetailException("OOPS!!! The description of a event cannot be empty.");
-                }
-            default:
-                throw new CommandNotRecognizedException("I'm sorry, but I don't know what that means :-(");
+        case TODO:
+            try {
+                return new ToDo(splittedInput[1]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new NoCommandDetailException("OOPS!!! The description of a todo cannot be empty.");
+            }
+        case DEADLINE:
+            try {
+                String[] deadlineParts = splitByDelimiter(splittedInput[1], " /by ");
+                return new Deadline(deadlineParts[0], deadlineParts[1]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new NoCommandDetailException("OOPS!!! The description of a deadline cannot be empty.");
+            }
+        case EVENT:
+            try {
+                String[] eventParts = splitByDelimiter(splittedInput[1], " /from ");
+                String eventName = eventParts[0];
+                String[] eventTimes = splitByDelimiter(eventParts[1], " /to ");
+                return new Event(eventName, eventTimes[0], eventTimes[1]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new NoCommandDetailException("OOPS!!! The description of a event cannot be empty.");
+            }
+        default:
+            throw new CommandNotRecognizedException("I'm sorry, but I don't know what that means :-(");
         }
     }
 
@@ -81,43 +81,43 @@ public class Duke {
             String command = splittedInput[0].toLowerCase();
 
             switch (command) {
-                case "bye":
-                    sc.close();
-                    return;
+            case "bye":
+                sc.close();
+                return;
 
-                case "list":
-                    lineSplitter();
-                    for (int i = 0; i < list.size(); i++) {
-                        System.out.println((i + 1) + ". " + list.get(i));
-                    }
-                    lineSplitter();
-                    break;
+            case "list":
+                lineSplitter();
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
+                lineSplitter();
+                break;
 
-                case "mark":
-                    int markIndex = Integer.parseInt(splittedInput[1]) - 1;
-                    list.get(markIndex).setDone();
-                    lineSplitter();
-                    System.out.println("I've marked this task as done:\n  " + list.get(markIndex));
-                    lineSplitter();
-                    break;
+            case "mark":
+                int markIndex = Integer.parseInt(splittedInput[1]) - 1;
+                list.get(markIndex).setDone();
+                lineSplitter();
+                System.out.println("I've marked this task as done:\n  " + list.get(markIndex));
+                lineSplitter();
+                break;
 
-                case "delete":
-                    int deleteIndex = Integer.parseInt(splittedInput[1]) - 1;
-                    Task deletedTask = list.remove(deleteIndex);
-                    lineSplitter();
-                    System.out.println("Deleted this task:\n  " + deletedTask + "\n" + list.size() + " tasks in the list");
-                    lineSplitter();
-                    break;
-                case "unmark":
-                    int unmarkIndex = Integer.parseInt(splittedInput[1]) - 1;
-                    list.get(unmarkIndex).setUndone();
-                    lineSplitter();
-                    System.out.println("I've marked this task as undone:\n  " + list.get(unmarkIndex));
-                    lineSplitter();
-                    break;
+            case "delete":
+                int deleteIndex = Integer.parseInt(splittedInput[1]) - 1;
+                Task deletedTask = list.remove(deleteIndex);
+                lineSplitter();
+                System.out.println("Deleted this task:\n  " + deletedTask + "\n" + list.size() + " tasks in the list");
+                lineSplitter();
+                break;
+            case "unmark":
+                int unmarkIndex = Integer.parseInt(splittedInput[1]) - 1;
+                list.get(unmarkIndex).setUndone();
+                lineSplitter();
+                System.out.println("I've marked this task as undone:\n  " + list.get(unmarkIndex));
+                lineSplitter();
+                break;
 
-                default:
-                    try{
+            default:
+                try {
                     Task task = addTask(input);
                     if (task != null) {
                         list.add(task);
@@ -125,13 +125,14 @@ public class Duke {
                         System.out.println("added: " + task + " \n" + list.size() + " tasks in the list");
                         lineSplitter();
                     }
-                    break;} catch (CommandNotRecognizedException e) {
-                        System.out.println(e.getMessage());
-                        break;
-                    } catch (NoCommandDetailException e) {
-                        System.out.println(e.getMessage());
-                        break;
-                    }
+                    break;
+                } catch (CommandNotRecognizedException e) {
+                    System.out.println(e.getMessage());
+                    break;
+                } catch (NoCommandDetailException e) {
+                    System.out.println(e.getMessage());
+                    break;
+                }
             }
         }
     }
