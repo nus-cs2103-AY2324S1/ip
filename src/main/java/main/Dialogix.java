3343202d0fde6fd9fb45f300b74aeb2b1663576b
Long Nguyen Tan <longnguyentan2002@gmail.com>
2023-08-28 00:59:37 +0800

    Level 5. Handle Errors

--- src/main/java/main/Dialogix.java
+++ src/main/java/main/Dialogix.java
@@ -24,45 +24,50 @@ public class Dialogix {
         boolean continueDialog = true;
 
 
-        try {
-            System.out.print("User: ");
-            String userInput = scanner.nextLine();
 
-            if (userInput.equalsIgnoreCase("list")) {
-                System.out.println("Bot: Here are the tasks in your list:");
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.println((i + 1) + ". " + list.get(i).toString());
-                }
-            } else if (userInput.startsWith("todo")) {
-                String description = userInput.replaceFirst("todo\\s+", "");
-                list.add(new Todo(description));
-                System.out.println("Bot: Got it. I've added this task:\n  " + list.get(list.size() - 1).toString());
-            } else if (userInput.startsWith("deadline")) {
-                String[] parts = userInput.replaceFirst("deadline\\s+", "").split(" /by ");
-                if (parts.length == 2) {
-                    list.add(new Deadline(parts[0], parts[1]));
+
+
+        while (continueDialog) {
+            try {
+                System.out.print("User: ");
+                String userInput = scanner.nextLine();
+
+                if (userInput.equalsIgnoreCase("list")) {
+                    System.out.println("Bot: Here are the tasks in your list:");
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println((i + 1) + ". " + list.get(i).toString());
+                    }
+                } else if (userInput.startsWith("todo")) {
+                    String description = userInput.replaceFirst("todo\\s+", "");
+                    list.add(new Todo(description));
                     System.out.println("Bot: Got it. I've added this task:\n  " + list.get(list.size() - 1).toString());
+                } else if (userInput.startsWith("deadline")) {
+                    String[] parts = userInput.replaceFirst("deadline\\s+", "").split(" /by ");
+                    if (parts.length == 2) {
+                        list.add(new Deadline(parts[0], parts[1]));
+                        System.out.println("Bot: Got it. I've added this task:\n  " + list.get(list.size() - 1).toString());
+                    } else {
+                        System.out.println("Bot: Invalid input format for 'deadline'.");
+                    }
+                } else if (userInput.startsWith("event")) {
+                    String[] parts = userInput.replaceFirst("event\\s+", "").split(" /from | /to ");
+                    if (parts.length == 3) {
+                        list.add(new Event(parts[0], parts[1], parts[2]));
+                        System.out.println("Bot: Got it. I've added this task:\n  " + list.get(list.size() - 1).toString());
+                    } else {
+                        System.out.println("Bot: Invalid input format for 'event'.");
+                    }
+                } else if (userInput.equalsIgnoreCase("bye")) {
+                    System.out.println("Bot: Bye. Hope to see you again soon!");
+                    continueDialog = false;
                 } else {
-                    System.out.println("Bot: Invalid input format for 'deadline'.");
+                    throw new DialogixException("I'm sorry, but I don't know what that means :-(");
                 }
-            } else if (userInput.startsWith("event")) {
-                String[] parts = userInput.replaceFirst("event\\s+", "").split(" /from | /to ");
-                if (parts.length == 3) {
-                    list.add(new Event(parts[0], parts[1], parts[2]));
-                    System.out.println("Bot: Got it. I've added this task:\n  " + list.get(list.size() - 1).toString());
-                } else {
-                    System.out.println("Bot: Invalid input format for 'event'.");
-                }
-            } else if (userInput.equalsIgnoreCase("bye")) {
-                System.out.println("Bot: Bye. Hope to see you again soon!");
-                continueDialog = false;
-            } else {
-                throw new DialogixException("I'm sorry, but I don't know what that means :-(");
+            } catch (DialogixException e) {
+                System.out.println("Bot: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Bot: An error occurred: " + e.getMessage());
             }
-        } catch (DialogixException e) {
-            System.out.println("Bot: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Bot: An error occurred: " + e.getMessage());
         }
 
         System.out.println("____________________________________________________________");
