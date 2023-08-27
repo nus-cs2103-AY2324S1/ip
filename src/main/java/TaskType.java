612ae9a9afb48bae59c0f0ae487a179301a853c9
euchangxian <changxian2001@gmail.com>
2023-08-27 23:21:00 +0800

    Add TaskType enum

---
+++ src/main/java/TaskType.java
@@ -0,0 +1,14 @@
+public enum TaskType {
+    TODO("T"),
+    DEADLINE("D"),
+    EVENT("E");
+
+    private final String taskType;
+    TaskType(String taskType) {
+        this.taskType = taskType;
+    }
+
+    public String getTaskType() {
+        return taskType;
+    }
+}
