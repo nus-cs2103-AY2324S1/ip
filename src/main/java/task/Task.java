3b64be6f7c37958cc437c31e114df19cb8e9bfd3
Long Nguyen Tan <longnguyentan2002@gmail.com>
2023-08-28 00:18:17 +0800

    Level 3. Mark as Done

---
+++ src/main/java/task/Task.java
@@ -0,0 +1,31 @@
+package task;
+
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]");
+    }
+}
