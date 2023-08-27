6a51e727190420fd29ad0dd7c93d107b194dd75e
Bearypop <looyl2k@gmail.com>
2023-08-28 02:49:58 +0800

    Add Date feature

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,33 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String start;
-    protected String end;
+    protected LocalDate start;
+    protected LocalDate end;
 
     public Event(String description, String start, String end) {
         super(description);
-        this.start = start;
-        this.end = end;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        this.start = LocalDate.parse(start, formatter);
+        this.end = LocalDate.parse(end, formatter);
+    }
+
+    public Event(String description, String start, String end, boolean other) {
+        super(description);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd yyyy");
+        this.start = LocalDate.parse(start, formatter);
+        this.end = LocalDate.parse(end, formatter);
+    }
+
+    private String getDateString(LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd yyyy");
+        return date.format(formatter);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.start + " to: " + this.end + ")";
+        return "[E]" + super.toString() + " (from: " + this.getDateString(start) +
+                " to: " + this.getDateString(end) + ")";
     }
 }
 
