250ee6e5983c00511f075984237097cc611757a7
leezhanpeng <zhanpenglee@outlook.com>
2023-08-27 18:44:12 +0800

    Add JavaDoc to FindCommand

--- src/main/java/robert/command/FindCommand.java
+++ src/main/java/robert/command/FindCommand.java
@@ -6,15 +6,34 @@ import robert.task.Task;
 import robert.task.TaskList;
 import robert.ui.Ui;
 
-
+/**
+ * A Find extension of the <tt>Command</tt> class. Used to find tasks
+ * that are associated to a particular keyword.
+ *
+ * @author Lee Zhan Peng
+ */
 public class FindCommand extends Command {
 
+    /** The keyword that is used for querying */
     private final String keyword;
 
+    /**
+     * Constructs FindCommand using a date.
+     *
+     * @param keyword the keyword to be used for querying of tasks.
+     */
     public FindCommand(String keyword) {
         this.keyword = keyword;
     }
 
+    /**
+     * Executes the finding of associated tasks.
+     *
+     * @param tasks the list of tasks to be added onto.
+     * @param ui the ui that is responsible for the output of the CLI.
+     * @param storage the storage that loads stored tasks from hard disk.
+     * @throws RobertException as a mean of overriding the function.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws RobertException {
         int taskIndex = 0;
