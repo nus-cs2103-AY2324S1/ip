a7de9bc18102d6564375feba92633af7b01434d5
Kevin <e1053635@u.nus.edu>
2023-08-27 22:19:47 +0800

    Improve the skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String greet = "____________________________________________________________\n"
                 + "Hello! I'm Kevin\n"
                 + "What can I do  for you?\n"
-                + "____________________________________________________________\n"
+                + "____________________________________________________________\n";
+        String bye = "____________________________________________________________\n"
                 + "Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________";
 
         System.out.println(greet);
 
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.next();
+        while (true) {
+            if (!userInput.equals("bye")) {
+                String message = "____________________________________________________________\n"
+                        + userInput + "\n"
+                        + "____________________________________________________________\n";
+                System.out.println(message);
+            } else {
+                System.out.println(bye);
+                break;
+            }
+            userInput = scanner.next();
+        }
+        scanner.close();
     }
 }
