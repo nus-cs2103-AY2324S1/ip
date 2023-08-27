b6c6ab3bab63dfc926469d62cd2e5798c068364a
brendanneojw <e0726880@u.nus.edu>
2023-08-27 23:31:34 +0800

    Implement Level 8: Date and Time of various formats

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,27 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime by;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + convertDateToString(this.by) + ")";
     }
 
     @Override
     public String lineToWriteFile() {
-        return "D | " + super.lineToWriteFile() + " | " + this.by;
+        return "D | " + super.lineToWriteFile() + " | "
+                + convertDateToString(this.by);
+    }
+
+    public String convertDateToString(LocalDateTime date) {
+        return date.format(DateTimeFormatter.ofPattern("MMM dd yyyy hh:mma"));
     }
 }
