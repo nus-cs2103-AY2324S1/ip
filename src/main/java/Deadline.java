68d5fbdc018226c65d97b26a114213d4d789a533
jinyang628 <81862152+jinyang628@users.noreply.github.com>
2023-08-27 15:34:43 +0800

    Implement date recognition

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,5 +1,8 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
 
     /**
      * Class constructor specifying the description of the deadline task.
@@ -8,7 +11,7 @@ public class Deadline extends Task {
      */
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDate.parse(by);
     }
 
     /**
@@ -17,7 +20,7 @@ public class Deadline extends Task {
      */
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.by + ")";
+        return "[D]" + super.toString() + " (by: " + this.by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 
     public String saveTask() {
