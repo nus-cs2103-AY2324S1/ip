d8b83e8c28e545144aeb3975b527a456cf54ee38
brendanneojw <e0726880@u.nus.edu>
2023-08-27 20:36:19 +0800

    Implement Level 7: Save

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
+    private static final String dataPath = "./data/duke.txt";
     public static void main(String[] args) {
         printHorizontalLine();
         System.out.println("WEEWOOWEEWOO WELCOME! I'm Siren");
         System.out.println("What can I do for you?");
-        printHorizontalLine();
-        takeInput();
+
+        ArrayList<Task> taskArray;
+        try {
+            taskArray = loadFile();
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        takeInput(taskArray);
     }
 
     public static void printHorizontalLine() {
         System.out.println("____________________________________________________________");
     }
 
-    public static void takeInput() {
-        ArrayList<Task> taskArray = new ArrayList<>();
+    public static void takeInput(ArrayList<Task> taskArray) {
+        printHorizontalLine();
+
         Scanner sc = new Scanner(System.in);
         boolean isLoop = true;
 
@@ -32,6 +46,7 @@ public class Duke {
                     try {
                         int taskNum = Integer.parseInt(input[1]);
                         markTasks(input, taskArray, taskNum);
+                        rewriteFile(taskArray);
                     } catch (ArrayIndexOutOfBoundsException e) {
                         throw new DukeException("BEEPBEEP! You forgot to give a task number!");
                     }
@@ -44,6 +59,7 @@ public class Duke {
                     try {
                         taskArray.add(new Todo(input[1]));
                         printAdded(taskArray.size(), taskArray.get(taskArray.size() - 1));
+                        addLineToFile(taskArray);
                     } catch (ArrayIndexOutOfBoundsException e) {
                         throw new DukeException("BEEPBEEP! You forgot to give a description!");
                     }
@@ -53,6 +69,7 @@ public class Duke {
                     try {
                         String[] remainLine = input[1].split(" /", 2);
                         deadlineOrEventTask(input[0], remainLine, taskArray);
+                        addLineToFile(taskArray);
                     } catch (ArrayIndexOutOfBoundsException e) {
                         throw new DukeException("BEEPBEEP! You forgot to give a description or date/time!");
                     }
@@ -61,6 +78,7 @@ public class Duke {
                     try {
                         int taskNum = Integer.parseInt(input[1]);
                         deleteTasks(taskArray, taskNum);
+                        rewriteFile(taskArray);
                     } catch (ArrayIndexOutOfBoundsException e) {
                         throw new DukeException("BEEPBEEP! You forgot to give a task number!");
                     }
@@ -79,7 +97,7 @@ public class Duke {
 
     public static void listTasks(ArrayList<Task> taskArray) {
         if (taskArray.isEmpty()) {
-            System.out.println("HEYYYYYYYY! There's nothing to show here!");
+            System.out.println("HEYYYYYYYY! There's nothing to show in your list!");
         } else {
             System.out.println("WHEET WHEET WHEET! Here are the tasks in your list:");
             for (int i = 0; i < taskArray.size(); i++) {
@@ -121,15 +139,13 @@ public class Duke {
             throws DukeException {
         if (action.equals("deadline")) {
             if (!remainLine[1].contains("by")) {
-                System.out.println("BEEPBEEP! You forgot to give a \"/by date/time\" for the deadline!");
-                return;
+                throw new DukeException("BEEPBEEP! You forgot to give a \"/by date/time\" for the deadline!");
             }
             String dateTime = remainLine[1].substring(3);
             taskArray.add(new Deadline(remainLine[0],dateTime));
         } else {
             if (!remainLine[1].contains("from ")) {
-                System.out.println("BEEPBEEP! You forgot to give a \"/from date/time\" for the event");
-                return;
+                throw new DukeException("BEEPBEEP! You forgot to give a \"/from date/time\" for the event");
             }
             try {
                 String[] splitTo = remainLine[1].split("/to ", 2);
@@ -150,6 +166,103 @@ public class Duke {
             System.out.println("Now you have " + taskArray.size() + " tasks in the list.");
         } catch (IndexOutOfBoundsException e) {
             throw new DukeException("WARBLE WARBLE, this task number does not exist!");
+        }
+    }
+
+    public static boolean hasDirectory() {
+        File f = new File("./data");
+        if (f.exists() && f.isDirectory()) {
+            return true;
+        }
+        System.out.println("OH NO! I couldn't find a \"data\" directory in your project root directory, "
+                + "I'll be creating one for you!");
+        return false;
+    }
+
+    public static void createDirectory() {
+        File f = new File("./data");
+        if (!hasDirectory() && !f.mkdir()) {
+            return;
+        }
+    }
+
+    public static Scanner createOrGetFile() throws DukeException {
+        File f = new File(dataPath);
+        try {
+            if (!f.createNewFile()) {
+                printHorizontalLine();
+                System.out.println("BLINGBLING! You have a saved file! Displaying the contents to you (if any)!");
+            } else {
+                System.out.println("OH NO! I couldn't find a \"duke.txt\" file in your data directory, "
+                        + "I'll be creating one for you!");
+                printHorizontalLine();
+            }
+            return new Scanner(f);
+        } catch (IOException e) {
+            throw new DukeException("HEYHEY! Seems like I couldn't create the file for you. "
+                    + "Please manually add the file!");
+        }
+    }
+
+    public static ArrayList<Task> loadFile() {
+        createDirectory();
+
+        Scanner s = createOrGetFile();
+        ArrayList<Task> taskArray = new ArrayList<>();
+
+        while (s.hasNext()) {
+            String[] line = s.nextLine().trim().split(" \\| ", 3);
+
+            switch (line[0]) {
+            case "T":
+                taskArray.add(new Todo(line[2]));
+                break;
+            case "D":
+                String[] remainLine = line[2].split(" \\| ", 2);
+                taskArray.add(new Deadline(remainLine[0], remainLine[1]));
+                break;
+            case "E":
+                String[] remainingLine = line[2].split(" \\| ", 2);
+                String[] getDateTime = remainingLine[1].split(" to ");
+                taskArray.add(new Event(remainingLine[0], getDateTime[0], getDateTime[1]));
+                break;
+            }
+
+            if (line[1].equals("X")) {
+                taskArray.get(taskArray.size() - 1).markDone();
+            }
+        }
+        listTasks(taskArray);
+        return taskArray;
+    }
+
+    public static void addLineToFile(ArrayList<Task> taskArray) throws DukeException {
+        try {
+            FileWriter fw = new FileWriter(dataPath, true);
+            if (taskArray.size() == 1) {
+                fw.write(taskArray.get(0).lineToWriteFile());
+            } else {
+                fw.write("\n" + taskArray.get(taskArray.size() - 1).lineToWriteFile());
+            }
+            fw.close();
+        } catch (IOException e) {
+            throw new DukeException("UHOH! Something went wrong when attempting to write to file!");
+        }
+    }
+
+    public static void rewriteFile(ArrayList<Task> taskArray) throws DukeException {
+        try {
+            FileWriter fw = new FileWriter(dataPath);
+            for (int i = 0; i < taskArray.size(); i++) {
+                if (i != taskArray.size() - 1) {
+                    fw.write(taskArray.get(i).lineToWriteFile() + "\n");
+                } else {
+                    fw.write(taskArray.get(i).lineToWriteFile());
+                }
+            }
+            fw.close();
+        } catch (IOException e) {
+            throw new DukeException("UHOH! Something went wrong when attempting to write to file!");
         }
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -13,4 +13,9 @@ public class Event extends Task {
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
+
+    @Override
+    public String lineToWriteFile() {
+        return "E | " + super.lineToWriteFile() + " | " + this.from + " to " + this.to;
+    }
 }
