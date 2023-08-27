a4a7d3255abcedc186ee77fa80bc32f45d1dc19c
Long Nguyen Tan <longnguyentan2002@gmail.com>
2023-08-27 15:20:41 +0800

    Level 0 Update Rename, Greet, Exit

--- src/main/java/Dialogix.java
+++ src/main/java/Dialogix.java
@@ -1,12 +1,5 @@
-public class Duke {
+public class Dialogix {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-
         String welcomeMessage = "____________________________________________________________\n" +
                 " Hello! I'm [YOUR CHATBOT NAME]\n" +
                 " What can I do for you?\n" +
