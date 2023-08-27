fed068161ae0636ab48580891a52c033f35d924d
imkwokyong <darrenkuan@gmail.com>
2023-08-27 17:53:27 +0800

    Add Dates and Times functionality for Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,23 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 class Deadline extends Task {
-    protected String by;
+    protected LocalDateTime by;
 
     Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
     }
 
     @Override
     String stringToFile() {
-        return String.format("D | %s | %s", super.stringToFile(), by);
+        return String.format("D | %s | %s", super.stringToFile(),
+                by.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
     }
+
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), by);
+        return String.format("[D]%s (by: %s)", super.toString(),
+                by.format(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm")));
     }
 }
