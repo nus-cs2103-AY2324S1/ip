01e95b8afc7a8a33a2562daf8eb0a5709a2fdbe7
rayson <raysonyeap4c@gmail.com>
2023-08-28 00:48:33 +0800

    Delete unnecessary imports

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
