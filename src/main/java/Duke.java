1c538dda276cda9dd9744003f40d8ddbd454d888
Kevin <e1053635@u.nus.edu>
2023-08-27 22:21:45 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,12 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        List<String> textList = new ArrayList<>();
         String greet = "____________________________________________________________\n"
                 + "Hello! I'm Kevin\n"
                 + "What can I do  for you?\n"
@@ -13,18 +18,28 @@ public class Duke {
         System.out.println(greet);
 
         Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.next();
+        String userInput = scanner.nextLine();
         while (true) {
-            if (!userInput.equals("bye")) {
-                String message = "____________________________________________________________\n"
-                        + userInput + "\n"
-                        + "____________________________________________________________\n";
-                System.out.println(message);
-            } else {
+            if (userInput.equals("bye")) {
                 System.out.println(bye);
                 break;
+            } else if (userInput.equals("list")) {
+                int count = 1;
+                System.out.println("____________________________________________________________");
+                for (String str: textList) {
+                    String string = String.format("%d. %s", count, str);
+                    System.out.println(string);
+                    count++;
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                textList.add(userInput);
+                String message = "____________________________________________________________\n"
+                        + "added: " + userInput + "\n"
+                        + "____________________________________________________________\n";
+                System.out.println(message);
             }
-            userInput = scanner.next();
+            userInput = scanner.nextLine();
         }
         scanner.close();
     }
