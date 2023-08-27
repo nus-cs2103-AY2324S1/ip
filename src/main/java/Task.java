2071d570f150460fd2ecd66a3b20369aa8eec4d7
euchangxian <changxian2001@gmail.com>
2023-08-27 17:26:24 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private String taskName;
+    private boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        String taskStatus = isDone
+                ? "[X]"
+                : "[ ]";
+        return String.format("%s %s", taskStatus, taskName);
+    }
+}
