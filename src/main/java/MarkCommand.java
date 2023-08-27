9c4e3e5953c9b28c91a1732193452f808a48f415
aexolate <20797128+aexolate@users.noreply.github.com>
2023-08-27 17:32:30 +0800

    Add Parser, Command classes
    * Parser: deals with making sense of the user command
    * Added command types: AddCommand, DeleteCommand, ExitCommand,
      ListCommand, MarkCommand

---
+++ src/main/java/MarkCommand.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+
+public class MarkCommand extends Command {
+	private int taskIdx;
+	private boolean isMark;
+
+	public MarkCommand(int taskIdx, boolean isMark) {
+		this.taskIdx = taskIdx;
+		this.isMark = isMark;
+	}
+
+	@Override
+	public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
+		Task task = tasks.get(taskIdx);
+		if (isMark) {
+			ui.showMessage("Nice! I've marked this task as done:");
+			task.markAsDone();
+		} else {
+			ui.showMessage("OK, I've marked this task as not done yet:");
+			task.markAsUndone();
+		}
+		ui.showMessage(task.toString());
+
+		// Save the TaskList to Storage
+		try {
+			storage.write(tasks.serialize());
+		} catch (IOException ex) {
+			throw new DukeException("I/O Error: Failed to write to storage. "
+					+ ex.getMessage());
+		}
+	}
+}
