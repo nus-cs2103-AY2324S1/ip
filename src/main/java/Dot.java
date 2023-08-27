b9e5145a1edead2e06191e9d5ef40a0dece98a90
lamchenghou <lamchenghouu@gmail.com>
2023-08-27 11:34:50 +0800

    Move validation and parsing functionalities to Validation.java and Parser.java

--- src/main/java/Dot.java
+++ src/main/java/Dot.java
@@ -37,125 +37,45 @@ public class Dot {
                         dotTaskList.list();
                         break;
                     default:
-                        // TODO: abstract out validation
-                        // TODO: Refactor the below validation using String::match & regex
-                        // TODO: generalise the validation function for "<cmd> <args>" by using Consumer
-                        /* TODO: In a later increment, there will be a requirement to abstract out the Parser
-                                 However, it is unclear whether we should keep constructing Tasks in TaskList.
-                                 We allow for Dot to access the 4 classes that are <: Task
-                         */
                         // Note: To support more than 3 kinds of tasks, we can code a robust function and follow
                         // a standardised format.
-                        if (input.startsWith("mark") && (input.length() == 4 || input.charAt(4) == ' ')) {
-                            String[] substrings = input.split(" ");
-                            if (substrings.length == 2) {
-                                int position = Integer.parseInt(substrings[1]);
-                                dotTaskList.markTask(position - 1, true);
-                                dotTaskList.saveTaskListToStorage(new File(this.dataFilePathname));
-                            } else if (substrings.length == 1) {
-                                throw new DotException("No task number stated", TaskError.ERR_USING_MARK);
-                            } else {
-                                throw new DotException("Too many parameters", TaskError.ERR_USING_MARK);
-                            }
+                        if (Validation.isValidCommand(input, "mark")) {
+                            int position = Validation.intIfValidCommandSpaceNumber(input, TaskError.ERR_USING_MARK);
+                            dotTaskList.markTask(position - 1, true);
+                            dotTaskList.saveTaskListToStorage(new File(this.dataFilePathname));
                             break;
-                        } else if (input.startsWith("unmark") && (input.length() == 6 || input.charAt(6) == ' ')) {
-                            String[] substrings = input.split(" ");
-                            if (substrings.length == 2) {
-                                int position = Integer.parseInt(substrings[1]);
-                                dotTaskList.markTask(position - 1, false);
-                                dotTaskList.saveTaskListToStorage(new File(this.dataFilePathname));
-                            } else if (substrings.length == 1) {
-                                throw new DotException("No task number stated", TaskError.ERR_USING_UNMARK);
-                            } else {
-                                throw new DotException("Too many parameters", TaskError.ERR_USING_UNMARK);
-                            }
+                        } else if (Validation.isValidCommand(input, "unmark")) {
+                            int position = Validation.intIfValidCommandSpaceNumber(input, TaskError.ERR_USING_UNMARK);
+                            dotTaskList.markTask(position - 1, false);
+                            dotTaskList.saveTaskListToStorage(new File(this.dataFilePathname));
                             break;
-                        } else if (input.startsWith("todo") && (input.length() == 4 || input.charAt(4) == ' ')) {
-                            if (input.strip().length() <= 5) {
-                                throw new DotException("No task description given", TaskError.ERR_USING_TODO);
-                            }
-                            String restOfString = input.substring(5);
+                        } else if (Validation.isValidCommand(input, "todo")) {
+                            String restOfString = Validation.descIfValidCommandSpaceDesc(input, "todo",
+                                    "task description", TaskError.ERR_USING_TODO);
                             Task newTodoTask = new Todo(restOfString);
                             dotTaskList.addTask(newTodoTask);
                             dotTaskList.saveTaskListToStorage(new File(this.dataFilePathname));
                             break;
-                        } else if (input.startsWith("deadline") && (input.length() == 8 || input.charAt(8) == ' ')) {
-                            if (input.length() <= 9) {
-                                throw new DotException("No task description given", TaskError.ERR_USING_DEADLINE);
-                            }
-                            // We can assume that input is of format "deadline .+"
-                            // Case: "deadline /by"
-                            int indexOfSlash = input.indexOf(" /by");
-                            if (indexOfSlash == -1 || indexOfSlash == 8) {
-                                throw new DotException("No deadline given or is given without task description.",
-                                        TaskError.ERR_USING_DEADLINE);
-                            }
-
-                            // We can assume that input is now in the format "deadline .+ /by.*'
-                            String[] substrings = input.split(" /by");
-                            // RHS of || explanation: in the case /by is at the end of str,
-                            // then "" will not be an element of the resulting array
-                            if (substrings.length == 1) {
-                                throw new DotException("No deadline description given.", TaskError.ERR_USING_DEADLINE);
-                            } else if (substrings.length != 2) {
-                                // A side effect of this is that "deadline <desc> /by today /by"
-                                // will pass the check, and in a way, autocorrect
-                                throw new DotException("Too many instances of deadline descriptions.",
-                                        TaskError.ERR_USING_DEADLINE);
-                            }
-                            // Since supposedly filled spaces can appear as whitespace,
-                            // we need to run a check after the split
-                            // We will truncate "deadline" from the first element and strip it
-                            String description = substrings[0].substring(8).strip();
-                            if (description.isEmpty()) {
-                                throw new DotException("No task description given", TaskError.ERR_USING_DEADLINE);
-                            }
-                            // We will strip the second element, to see if deadline desc is given
-                            String deadline = substrings[1].strip();
-                            if (deadline.isEmpty()) {
-                                throw new DotException("No deadline description given", TaskError.ERR_USING_DEADLINE);
-                            }
-                            Task newDeadlineTask = new Deadline(description, deadline);
+                        } else if (Validation.isValidCommand(input, "deadline")) {
+                            String[] args = Validation.argsIfValidDeadlineFormat(input);
+                            Task newDeadlineTask = new Deadline(args[0], args[1]);
                             dotTaskList.addTask(newDeadlineTask);
                             dotTaskList.saveTaskListToStorage(new File(this.dataFilePathname));
                             break;
-                        } else if (input.startsWith("event")) {
-                            // .+ enforces at least one character, but disallows empty string
-                            // Regex below inspired by
-                            // https://stackoverflow.com/questions/2912894/how-to-match-any-
-                            // character-in-regular-expression
-                            String eventRegex = "event .+ /from .+ /to .+";
-                            if (!input.matches(eventRegex)) {
-                                throw new DotException("Wrong format for event.", TaskError.ERR_USING_EVENT);
-                            }
-                            int indexOfFirstSlash = input.indexOf("/from");
-                            int indexOfFSecondSlash = input.indexOf("/to", indexOfFirstSlash + 1);
-                            String description = input.substring(6, indexOfFirstSlash - 1);
-                            String start = input.substring(indexOfFirstSlash + 5, indexOfFSecondSlash).strip();
-                            String end = input.substring(indexOfFSecondSlash + 4);
-                            Task newEventTask = new Event(description, start, end);
+                        } else if (Validation.isValidCommand(input, "event")) {
+                            String[] args = Validation.argsIfValidEventFormat(input);
+                            Task newEventTask = new Event(args[0], args[1], args[2]);
                             dotTaskList.addTask(newEventTask);
                             dotTaskList.saveTaskListToStorage(new File(this.dataFilePathname));
                             break;
-                        } else if (input.startsWith("delete") && (input.length() == 6 || input.charAt(6) == ' ')) {
-                            String[] substrings = input.split(" ");
-                            if (substrings.length == 2) {
-                                int position = Integer.parseInt(substrings[1]);
-                                dotTaskList.deleteTask(position - 1);
-                                dotTaskList.saveTaskListToStorage(new File(this.dataFilePathname));
-                            } else if (substrings.length == 1) {
-                                throw new DotException("No task number stated", TaskError.ERR_DELETING_TASK);
-                            } else {
-                                throw new DotException("Too many parameters", TaskError.ERR_DELETING_TASK);
-                            }
+                        } else if (Validation.isValidCommand(input, "delete")) {
+                            int position = Validation.intIfValidCommandSpaceNumber(input, TaskError.ERR_DELETING_TASK);
+                            dotTaskList.deleteTask(position - 1);
+                            dotTaskList.saveTaskListToStorage(new File(this.dataFilePathname));
                             break;
-                        } else if (input.startsWith("whatsgoingon")
-                                && (input.length() == 12 || input.charAt(12) == ' ')) {
-                            input = input.strip();
-                            if (input.length() <= 12) {
-                                throw new DotException("No date given", TaskError.ERR_USING_WHATSGOINGON);
-                            }
-                            String restOfString = input.substring(13);
+                        } else if (Validation.isValidCommand(input, "whatsgoingon")) {
+                            String restOfString = Validation.descIfValidCommandSpaceDesc(input,
+                                    "whatsgoingon", "date", TaskError.ERR_USING_WHATSGOINGON);
                             if (!(Validation.isValidDate(restOfString))) {
                                 throw new DotException("Incorrect format for data, use dd/MM/yyyy",
                                         TaskError.ERR_USING_WHATSGOINGON);
@@ -172,9 +92,6 @@ public class Dot {
             } catch (DotException e) {
                 // For checked exception
                 e.handleError();
-            } catch (NumberFormatException e) {
-                // For runtime exceptions
-                TaskError.ERR_USING_MARK.printErrorMessage(e);
             }
         }
         Ui.goodbye();
--- src/main/java/validation/Validation.java
+++ src/main/java/validation/Validation.java
@@ -1,5 +1,8 @@
 package validation;
 
+import errors.DotException;
+import errors.TaskError;
+
 public class Validation {
 
     public static boolean isValidDate(String dateInput) {
@@ -7,4 +10,128 @@ public class Validation {
 
         return dateInput.matches(dateRegex);
     }
+
+    public static boolean isValidCommand(String input, String command) {
+        int commandLen = command.length();
+        return input.startsWith(command) &&
+                (input.length() == commandLen || input.charAt(commandLen) == ' ');
+    }
+    /**
+     * Validates whether input is in format: <command> <integer>.
+     * Assumes valid command.
+     * @param input from the user
+     * @param potentialError the TaskError that will handle potential exceptions
+     * @return the argument <integer>
+     * @throws DotException if input is invalid
+     */
+    public static int intIfValidCommandSpaceNumber(String input,
+                                                   TaskError potentialError) throws DotException {
+        // If position is returned, this value will definitely be overridden
+        int position = 1;
+
+        String[] substrings = input.split(" ");
+        if (substrings.length == 2) {
+            try {
+                position = Integer.parseInt(substrings[1]);
+            } catch (NumberFormatException e) {
+                throw new DotException("Invalid number given", potentialError);
+            }
+        } else if (substrings.length == 1) {
+            throw new DotException("No task number stated", potentialError);
+        } else {
+            throw new DotException("Too many parameters", potentialError);
+        }
+
+        return position;
+    }
+
+    /**
+     * Validates whether input is in format: <command> <description>
+     * We require command parameter because we are not relying on String::split
+     * Assumes that command is valid
+     * @param input from user
+     * @param command in format
+     * @param parameterDesc for error message
+     * @param potentialError the TaskError that will handle potential exceptions
+     * @return the argument <description>
+     * @throws DotException if input is invalid
+     */
+    public static String descIfValidCommandSpaceDesc(String input, String command, String parameterDesc,
+                                                     TaskError potentialError) throws DotException {
+        int commandLen = command.length();
+        if (input.strip().length() <= commandLen) {
+            throw new DotException(String.format("No %s given", parameterDesc),
+                    potentialError);
+        }
+        return input.substring(commandLen + 1);
+    }
+
+    /**
+     * Validates whether input is in format: deadline <desc> /by <deadline>
+     * This is capable of spotting format errors such as overflowing parameters
+     * @param input from user
+     * @return { description, deadline > if valid
+     * @throws DotException if input is invalid
+     */
+    public static String[] argsIfValidDeadlineFormat(String input) throws DotException {
+        if (input.length() <= 9) {
+            throw new DotException("No task description given", TaskError.ERR_USING_DEADLINE);
+        }
+        // We can assume that input is of format "deadline .+"
+        // Case: "deadline /by"
+        int indexOfSlash = input.indexOf(" /by");
+        if (indexOfSlash == -1 || indexOfSlash == 8) {
+            throw new DotException("No deadline given or is given without task description.",
+                    TaskError.ERR_USING_DEADLINE);
+        }
+
+        // We can assume that input is now in the format "deadline .+ /by.*'
+        String[] substrings = input.split(" /by");
+
+        if (substrings.length == 1) {
+            throw new DotException("No deadline description given.", TaskError.ERR_USING_DEADLINE);
+        } else if (substrings.length != 2) {
+            // A side effect of this is that "deadline <desc> /by today /by"
+            // will pass the check, and in a way, autocorrect
+            throw new DotException("Too many instances of deadline descriptions.",
+                    TaskError.ERR_USING_DEADLINE);
+        }
+        // Since supposedly filled spaces can appear as whitespace,
+        // we need to run a check after the split
+        // We will truncate "deadline" from the first element and strip it
+        String description = substrings[0].substring(8).strip();
+        if (description.isEmpty()) {
+            throw new DotException("No task description given", TaskError.ERR_USING_DEADLINE);
+        }
+        // We will strip the second element, to see if deadline desc is given
+        String deadline = substrings[1].strip();
+        if (deadline.isEmpty()) {
+            throw new DotException("No deadline description given", TaskError.ERR_USING_DEADLINE);
+        }
+        return new String[] { description, deadline };
+    }
+
+    /**
+     * Validates whether input is in format: event <desc> /from <start> /to <end>
+     * @param input from user
+     * @return { desc, start, end } if input is valid
+     * @throws DotException if input is invalid
+     */
+    public static String[] argsIfValidEventFormat(String input) throws DotException {
+        // .+ enforces at least one character, but disallows empty string
+        // Regex below inspired by
+        // https://stackoverflow.com/questions/2912894/how-to-match-any-
+        // character-in-regular-expression
+        String eventRegex = "event .+ /from .+ /to .+";
+        if (!input.matches(eventRegex)) {
+            throw new DotException("Wrong format for event.", TaskError.ERR_USING_EVENT);
+        }
+        int indexOfFirstSlash = input.indexOf("/from");
+        int indexOfFSecondSlash = input.indexOf("/to", indexOfFirstSlash + 1);
+        String description = input.substring(6, indexOfFirstSlash - 1);
+        String start = input.substring(indexOfFirstSlash + 5, indexOfFSecondSlash).strip();
+        String end = input.substring(indexOfFSecondSlash + 4);
+        return new String[] { description, start, end };
+    }
+
 }
