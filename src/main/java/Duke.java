2410b5b04b97470f829a1a361350b3b6607d707f
KamJiaYue <e0960550@u.nus.edu>
2023-08-28 01:23:06 +0800

    Complete Ui Implementation with manual testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,13 +3,11 @@ import java.util.Scanner;
 public class Duke {
 
     private TaskList taskList;
-    private Scanner scanner;
     private Ui ui;
 
     public Duke(String name) {
         this.taskList = new TaskList();
         this.ui = new Ui(name);
-        this.scanner = new Scanner(System.in);
     }
 
     public void startUp(String filePath) {
@@ -20,276 +18,59 @@ public class Duke {
 
 
     public void startService() throws DukeException {
-        String input = this.scanner.nextLine();
+
         boolean exceptionOccurs = false;
+
+        Commands cmd = this.ui.getNextUserInput();
 
         Ui.line();
 
-        Commands cmd = Commands.DEFAULT;
         Task task = null;
 
-        if (input.equals("bye")) {
-            cmd = Commands.BYE;
-        } else if (input.equals("list")) {
-            cmd = Commands.LIST;
-        } else if (input.startsWith("mark")) {
-            cmd = Commands.MARK;
-        } else if (input.startsWith("unmark")) {
-            cmd = Commands.UNMARK;
-        } else if (input.startsWith("delete")) {
-            cmd = Commands.DELETE;
-        } else if (input.startsWith("todo")) {
-            cmd = Commands.TODO;
-        } else if (input.startsWith("deadline")) {
-            cmd = Commands.DEADLINE;
-        } else if (input.startsWith("event")) {
-            cmd = Commands.EVENT;
-        }
-
-
-        switch (cmd) {
-
-        case BYE:
-            this.scanner.close();
-            this.ui.exit();
-            Ui.line();
-            return;
-        case LIST:
-            this.taskList.listOutEverything();
-            break;
-        case MARK:
-            try {
-                if (input.length() == 4) {
-                    exceptionOccurs = true;
+        try {
+            switch (cmd) {
+            case BYE:
+                this.ui.bye();
+                return;
+            case LIST:
+                this.taskList.listOutEverything();
+                break;
+            case MARK:
+                int markIndex = this.ui.mark();
+                if (this.taskList.isOutOfRange(markIndex)) {
                     throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be marked.");
                 }
-                if (input.charAt(4) != ' ') {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                }
-                if (input.length() == 5) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be marked.");
-                }
-
-                int index = 0;
-
-                try {
-                    index = Integer.parseInt(input.substring(5)) - 1;
-                } catch (NumberFormatException e) {
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be marked.");
-                }
-
-                if (this.taskList.isOutOfRange(index)) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be marked.");
-                }
-
-                this.taskList.mark(index);
-            } catch (DukeException e) {
-                System.out.println(e.getMessage());
-            }
-            break;
-        case UNMARK:
-            try {
-                if (input.length() == 6) {
-                    exceptionOccurs = true;
+                this.taskList.mark(markIndex);
+                break;
+            case UNMARK:
+                int unmarkIndex = this.ui.unmark();
+                if (taskList.isOutOfRange(unmarkIndex)) {
                     throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be unmarked.");
                 }
-                if (input.charAt(6) != ' ') {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                }
-                if (input.length() == 7) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be unmarked.");
-                }
-
-                int index = 0;
-
-                try {
-                    index = Integer.parseInt(input.substring(7)) - 1;
-                } catch (NumberFormatException e) {
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be unmarked.");
-                }
-
-                if (taskList.isOutOfRange(index)) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be unmarked.");
-                }
-
-                this.taskList.unmark(index);
-
-            } catch (DukeException e) {
-                System.out.println(e.getMessage());
-            }
-            break;
-        case DELETE:
-            try {
-
-                if (input.length() == 6) {
-                    exceptionOccurs = true;
+                this.taskList.unmark(unmarkIndex);
+                break;
+            case DELETE:
+                int deleteIndex = this.ui.delete();
+                if (taskList.isOutOfRange(deleteIndex)) {
                     throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be deleted.");
                 }
-                if (input.charAt(6) != ' ') {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                }
-                if (input.length() == 7) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be deleted.");
-                }
-
-                int index = 0;
-
-                try {
-                    index = Integer.parseInt(input.substring(7)) - 1;
-                } catch (NumberFormatException e) {
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be deleted.");
-                }
-
-                if (taskList.isOutOfRange(index)) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be deleted.");
-                }
-
-                this.taskList.remove(index);
-
-            } catch (DukeException e) {
-                System.out.println(e.getMessage());
-            }
-            break;
-        case TODO:
-            try {
-                if (input.length() == 4) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! The description of a todo cannot be empty.");
-                }
-                if (input.charAt(4) != ' ') {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                }
-                if (input.length() == 5) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! The description of a todo cannot be empty.");
-                }
-                task = new Todo(input.substring(5));
-            } catch (DukeException e) {
-                System.out.println(e.getMessage());
-            }
-            break;
-        case DEADLINE:
-            try {
-                if (input.length() == 8) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! The description of a deadline cannot be empty.");
-                }
-
-                if (input.charAt(8) != ' ') {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                }
-                if (input.length() == 9) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! The description of a deadline cannot be empty.");
-                }
-
-                if (!input.contains("/by")) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! The format of a deadline should be 'deadline YOUR_TASK /by YOUR_DEADLINE'.");
-                }
-
-                int slashIndex = input.indexOf("/by");
-
-                if (input.length() < slashIndex + 4) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! The format of a deadline should be 'deadline YOUR_TASK /by YOUR_DEADLINE'.");
-                }
-
-                String by = input.substring(slashIndex + 4);
-
-                if (slashIndex <= 9) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! The title of your deadline cannot be empty.");
-                }
-                task = new Deadline(input.substring(9, slashIndex - 1), by);
-            } catch (DukeException e) {
-                System.out.println(e.getMessage());
-            }
-            break;
-        case EVENT:
-            try {
-                if (input.length() == 5) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! The description of an event cannot be empty.");
-                }
-                if (input.charAt(5) != ' ') {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                }
-                if (input.length() == 6) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! The description of an event cannot be empty.");
-                }
-                if (!input.contains("/from") || !input.contains("/to")) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! The format of an event should be 'event YOUR_EVENT /from START_TIME /to END_TIME'.");
-                }
-
-                int fromIndex = input.indexOf("/from");
-                int toIndex = input.indexOf("/to");
-
-                if (input.charAt(fromIndex + 5) != ' ') {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! The format of an event should be 'event YOUR_EVENT /from START_TIME /to END_TIME'.");
-                }
-
-                if (fromIndex >= toIndex) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! The format of an event should be 'event YOUR_EVENT /from START_TIME /to END_TIME'.");
-                }
-
-                if (toIndex - 1 - (fromIndex + 6) < 1) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! The starting time of an event should not be empty.");
-                }
-                String from = input.substring(fromIndex + 6, toIndex - 1);
-
-                if (input.length() < toIndex + 3) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! The ending time of an event should not be empty.");
-                }
-
-                if (input.charAt(toIndex + 3) != ' ') {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! The format of an event should be 'event YOUR_EVENT /from START_TIME /to END_TIME'.");
-                }
-
-                String to = input.substring(toIndex + 4);
-
-                if (fromIndex - 1 - 6 < 1) {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! The title of an event should not be empty.");
-                }
-
-                if (input.charAt(fromIndex - 1) != ' ') {
-                    exceptionOccurs = true;
-                    throw new DukeException(Ui.i5 + "☹ OOPS!!! The format of an event should be 'event YOUR_EVENT /from START_TIME /to END_TIME'.");
-                }
-
-                task = new Event(input.substring(6, fromIndex - 1), from, to);
-            } catch (DukeException e) {
-                System.out.println(e.getMessage());
-            }
-            break;
-        default:
-            try {
-                exceptionOccurs = true;
+                this.taskList.remove(deleteIndex);
+                break;
+            case TODO:
+                task = this.ui.todo();
+                break;
+            case DEADLINE:
+                task = this.ui.deadline();
+                break;
+            case EVENT:
+                task = this.ui.event();
+                break;
+            default:
                 throw new DukeException(Ui.i5 + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-            } catch (DukeException e) {
-                System.out.println(e.getMessage());
             }
-            break;
+        } catch (DukeException e) {
+            exceptionOccurs = true;
+            System.out.println(e.getMessage());
         }
 
         if (!exceptionOccurs && task != null) {
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ui {
     public static final String i4 = "    ";
     public static final String i5 = Ui.i4 + " ";
@@ -5,9 +7,12 @@ public class Ui {
     public static final String line = Ui.i4 + "——————————————————————————————————————————————————————————————————";
 
     private String name;
+    private Scanner scanner;
+    private String input;
 
     public Ui(String name) {
         this.name = name;
+        this.scanner = new Scanner(System.in);
     }
 
     public static void line() {
@@ -23,5 +28,181 @@ public class Ui {
 
     public void exit() {
         System.out.println(Ui.i5 + "Bye. Hope to see you again soon!");
+    }
+    public void bye() {
+        Ui.line();
+        this.scanner.close();
+        this.exit();
+        Ui.line();
+    }
+
+    public Commands getNextUserInput() {
+        this.input = this.scanner.nextLine();
+
+        if (input.equals("bye")) {
+            return Commands.BYE;
+        } else if (input.equals("list")) {
+            return Commands.LIST;
+        } else if (input.startsWith("mark")) {
+            return Commands.MARK;
+        } else if (input.startsWith("unmark")) {
+            return Commands.UNMARK;
+        } else if (input.startsWith("delete")) {
+            return Commands.DELETE;
+        } else if (input.startsWith("todo")) {
+            return Commands.TODO;
+        } else if (input.startsWith("deadline")) {
+            return Commands.DEADLINE;
+        } else if (input.startsWith("event")) {
+            return Commands.EVENT;
+        }
+
+        return Commands.DEFAULT;
+
+    }
+
+    public int mark() throws DukeException {
+        if (this.input.length() == 4) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be marked.");
+        }
+        if (this.input.charAt(4) != ' ') {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
+        if (this.input.length() == 5) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be marked.");
+        }
+
+        try {
+            return Integer.parseInt(input.substring(5)) - 1;
+        } catch (NumberFormatException e) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be marked.");
+        }
+    }
+
+    public int unmark() throws DukeException {
+        if (this.input.length() == 6) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be unmarked.");
+        }
+        if (this.input.charAt(6) != ' ') {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
+        if (this.input.length() == 7) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be unmarked.");
+        }
+
+        try {
+            return Integer.parseInt(input.substring(7)) - 1;
+        } catch (NumberFormatException e) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be unmarked.");
+        }
+    }
+
+    public int delete() throws DukeException {
+        if (this.input.length() == 6) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be deleted.");
+        }
+        if (this.input.charAt(6) != ' ') {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
+        if (this.input.length() == 7) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be deleted.");
+        }
+
+        try {
+            return Integer.parseInt(input.substring(7)) - 1;
+        } catch (NumberFormatException e) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! Please specify a valid task number to be deleted.");
+        }
+    }
+
+    public Task todo() throws DukeException {
+        if (this.input.length() == 4) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! The description of a todo cannot be empty.");
+        }
+        if (this.input.charAt(4) != ' ') {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
+        if (this.input.length() == 5) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! The description of a todo cannot be empty.");
+        }
+
+        return new Todo(input.substring(5));
+    }
+
+    public Task deadline() throws DukeException {
+        if (this.input.length() == 8) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! The description of a deadline cannot be empty.");
+        }
+        if (this.input.charAt(8) != ' ') {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
+        if (this.input.length() == 9) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! The description of a deadline cannot be empty.");
+        }
+        if (!this.input.contains("/by")) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! The format of a deadline should be 'deadline YOUR_TASK /by YOUR_DEADLINE'.");
+        }
+
+        int slashIndex = this.input.indexOf("/by");
+
+        if (this.input.length() < slashIndex + 4) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! The format of a deadline should be 'deadline YOUR_TASK /by YOUR_DEADLINE'.");
+        }
+
+        String by = this.input.substring(slashIndex + 4);
+
+        if (slashIndex <= 9) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! The title of your deadline cannot be empty.");
+        }
+
+        return new Deadline(this.input.substring(9, slashIndex - 1), by);
+    }
+
+    public Task event() throws DukeException {
+        if (this.input.length() == 5) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! The description of an event cannot be empty.");
+        }
+        if (this.input.charAt(5) != ' ') {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
+        if (this.input.length() == 6) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! The description of an event cannot be empty.");
+        }
+        if (!this.input.contains("/from") || !this.input.contains("/to")) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! The format of an event should be 'event YOUR_EVENT /from START_TIME /to END_TIME'.");
+        }
+
+        int fromIndex = this.input.indexOf("/from");
+        int toIndex = this.input.indexOf("/to");
+
+        if (this.input.charAt(fromIndex + 5) != ' ') {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! The format of an event should be 'event YOUR_EVENT /from START_TIME /to END_TIME'.");
+        }
+        if (fromIndex >= toIndex) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! The format of an event should be 'event YOUR_EVENT /from START_TIME /to END_TIME'.");
+        }
+        if (toIndex - 1 - (fromIndex + 6) < 1) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! The starting time of an event should not be empty.");
+        }
+
+        String from = this.input.substring(fromIndex + 6, toIndex - 1);
+
+        if (this.input.length() < toIndex + 3) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! The ending time of an event should not be empty.");
+        }
+        if (this.input.charAt(toIndex + 3) != ' ') {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! The format of an event should be 'event YOUR_EVENT /from START_TIME /to END_TIME'.");
+        }
+
+        String to = this.input.substring(toIndex + 4);
+
+        if (fromIndex - 1 - 6 < 1) {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! The title of an event should not be empty.");
+        }
+        if (this.input.charAt(fromIndex - 1) != ' ') {
+            throw new DukeException(Ui.i5 + "☹ OOPS!!! The format of an event should be 'event YOUR_EVENT /from START_TIME /to END_TIME'.");
+        }
+
+        return new Event(this.input.substring(6, fromIndex - 1), from, to);
     }
 }
