80311f6f9a3bca7e0968a6b482bc92128c4f666b
Long Nguyen Tan <longnguyentan2002@gmail.com>
2023-08-27 15:36:44 +0800

    Level 2. Add, List

--- src/main/java/main/Dialogix.java
+++ src/main/java/main/Dialogix.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Dialogix {
@@ -11,22 +13,24 @@ public class Dialogix {
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
+        List<String> list = new ArrayList<>();
+
         boolean continueDialog = true;
         while (continueDialog) {
             System.out.print("User: ");
             String userInput = scanner.nextLine();
 
             if (userInput.equalsIgnoreCase("list")) {
-                System.out.println("Bot: You entered 'list'. Printing out list.");
-                // Print the list or perform related action
-            } else if (userInput.equalsIgnoreCase("blah")) {
-                System.out.println("Bot: You entered 'blah'. Printing out blah.");
-                // Print blah or perform related action
+                System.out.println("Bot: Here is the list.");
+                for (String str: list) {
+                    System.out.println(str);
+                }
             } else if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println("Bot: Bye. Hope to see you again soon!");
                 continueDialog = false;
             } else {
-                System.out.println("Bot: I'm not sure how to respond to that.");
+                list.add(userInput);
+                System.out.println("Bot: I have added " + userInput + " to the database.");
             }
         }
 
