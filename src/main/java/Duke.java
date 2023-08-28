bbe51b36635aae9361f8a3bb9ee78e54f84a0031
redtailedfox <hongwei.hhw@gmail.com>
2023-08-29 00:23:40 +0800

    added enums

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,9 +4,14 @@ import java.util.Scanner;
 public class Duke {
 
     private ArrayList<Task> l;
+    private boolean isRunning = true;
 
     private Duke() {
         this.l = new ArrayList<>();
+    }
+
+    private enum Command {
+        LIST, BYE, MARK, UNMARK, DEADLINE, TODO, EVENT, DELETE, UNSPECIFIED
     }
 
     private static final String greet = "Hello! I'm ";
@@ -14,6 +19,18 @@ public class Duke {
     private static final String msg = "What can I do for you?";
 
     private static final String bye = "Bye. Hope to see you again soon!";
+
+    private Command parseCommand(String s) {
+        if (s.equals("bye")) {return Command.BYE;}
+        if (s.equals("list")) {return Command.LIST;}
+        if (s.startsWith("mark")) {return Command.MARK;}
+        if (s.startsWith("unmark")) {return Command.UNMARK;}
+        if (s.startsWith("todo")) {return Command.TODO;}
+        if (s.startsWith("deadline")) {return Command.DEADLINE;}
+        if (s.startsWith("event")) {return Command.EVENT;}
+        if (s.startsWith("delete")) {return Command.DELETE;}
+        else return Command.UNSPECIFIED;
+    }
 
     private void hello() {
         System.out.println(greet + name);
@@ -23,13 +40,14 @@ public class Duke {
     private void start() {
         hello();
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNextLine()) {
+        while(sc.hasNextLine() && this.isRunning) {
             String s = sc.nextLine();
             respond(s);
         }
     }
 
     private void end() {
+        this.isRunning = false;
         System.out.println(bye);
     }
 
@@ -46,63 +64,83 @@ public class Duke {
     }
 
     private void respond(String s) {
-        if (s.equals("list")) {
-            System.out.println("Here are the tasks in your list:");
-            int count = 1;
-            for(Task ss : this.l) {
-                String res = String.format("%d.%s", count++, ss.toString());
-                System.out.println(res);
-            }
-        } else if (s.equals("bye")) {
-            end();
-        } else if (s.startsWith("mark")) {
-            int idx = Integer.parseInt(s.substring(5));
-            this.l.get(idx-1).mark();
-        } else if (s.startsWith("unmark")) {
-            int idx = Integer.parseInt(s.substring(7));
-            this.l.get(idx-1).unmark();
-        } else if (s.startsWith("deadline")) {
-            if (s.trim().length() <=8) {
-                throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
-            }
-            String t = s.substring(9);
-            String[] temp = t.split("/by");
-            if (temp.length < 2) {
-                throw new DukeException("OOPS!!! The description of a deadline must have a deadline.");
-            }
-            Deadline d = new Deadline(temp[0], temp[1].trim());
-            this.l.add(d);
-            added(d);
-        } else if (s.startsWith("todo")) {
-            if (s.trim().length() <=4) {
-                throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
-            }
-            String t = s.substring(5);
+        Command c = parseCommand(s);
+        try {
+            switch (c) {
+                case LIST:
+                    System.out.println("Here are the tasks in your list:");
+                    int count = 1;
+                    for (Task ss : this.l) {
+                        String res = String.format("%d.%s", count++, ss.toString());
+                        System.out.println(res);
+                    }
+                    break;
 
-            Todo temp = new Todo(t);
-            this.l.add(temp);
-            added(temp);
-        } else if (s.startsWith("event")) {
-            if (s.trim().length() <=5) {
-                throw new DukeException("OOPS!!! The description of a event cannot be empty.");
-            }
-            String t = s.substring(6);
-            String[] temp = t.split("/");
-            if (temp.length < 3) {
-                throw new DukeException("OOPS!!! The description of a event must have from and to.");
-            }
-            Events e = new Events(temp[0], temp[1].substring(4).trim(), temp[2].substring(2).trim());
-            this.l.add(e);
-            added(e);
-        } else if (s.startsWith("delete")) {
-            int idx = Integer.parseInt(s.substring(7));
-            Task t = this.l.get(idx-1);
-            this.l.remove(idx-1);
-            del(t);
+                case BYE:
+                    end();
+                    return;
 
-        }
-        else {
-            throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
+                case MARK:
+                    int idx = Integer.parseInt(s.substring(5));
+                    this.l.get(idx - 1).mark();
+                    break;
+
+                case UNMARK:
+                    int idx1 = Integer.parseInt(s.substring(7));
+                    this.l.get(idx1 - 1).unmark();
+                    break;
+
+                case DEADLINE:
+                    if (s.trim().length() <= 8) {
+                        throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+                    }
+                    String t = s.substring(9);
+                    String[] temp = t.split("/by");
+                    if (temp.length < 2) {
+                        throw new DukeException("OOPS!!! The description of a deadline must have a deadline.");
+                    }
+                    Deadline d = new Deadline(temp[0], temp[1].trim());
+                    this.l.add(d);
+                    added(d);
+                    break;
+
+                case TODO:
+                    if (s.trim().length() <= 4) {
+                        throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
+                    }
+                    String t1 = s.substring(5);
+
+                    Todo temp1 = new Todo(t1);
+                    this.l.add(temp1);
+                    added(temp1);
+                    break;
+
+                case EVENT:
+                    if (s.trim().length() <= 5) {
+                        throw new DukeException("OOPS!!! The description of a event cannot be empty.");
+                    }
+                    String t2 = s.substring(6);
+                    String[] temp2 = t2.split("/");
+                    if (temp2.length < 3) {
+                        throw new DukeException("OOPS!!! The description of a event must have from and to.");
+                    }
+                    Events e = new Events(temp2[0], temp2[1].substring(4).trim(), temp2[2].substring(2).trim());
+                    this.l.add(e);
+                    added(e);
+                    break;
+
+                case DELETE:
+                    int idx3 = Integer.parseInt(s.substring(7));
+                    Task t3 = this.l.get(idx3 - 1);
+                    this.l.remove(idx3 - 1);
+                    del(t3);
+                    break;
+
+                default:
+                    throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
+            }
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
         }
     }
 
