15b3abfa14c832442da9d4d46bc3fbcaf864efc6
ryantzr1 <dreisprung1821@gmail.com>
2023-08-28 14:50:05 +0800

    Add Date handling method

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,8 +1,15 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Represents an deadline task in the Duke application.
  */
 public class Deadline extends Task {
     protected String by;
+    protected LocalDate deadline;
 
     /**
      * Constructs a new Deadline task.
@@ -15,8 +22,23 @@ public class Deadline extends Task {
         this.by = by;
     }
 
+    private String toDate() {
+        //Solution below inspired by https://www.baeldung.com/java-datetimeformatter
+        List<String> validPatterns = Arrays.asList("M/d/yyyy", "MM-dd-yyyy", "yyyy/MM/dd");
+        for (String pattern : validPatterns) {
+            try {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+                deadline = LocalDate.parse(by.trim(), formatter);
+                return deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+            } catch (DateTimeParseException e) {
+                System.out.println("Unsupported date format: " + by);
+            }
+         }
+        throw new DateTimeParseException("Unsupported date format: " + by, by, 0);
+    }
+
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + toDate() + ")";
     }
 }
