e01740f9e321671f9ebd116ca51c4d239e5dcb30
teozern1 <75937968+teozern1@users.noreply.github.com>
2023-08-28 18:34:01 +0800

    Add Storage,TaskList classes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,124 +13,53 @@ import java.time.temporal.ChronoUnit;
 
 
 public class Duke {
-    static boolean exited = false;
     static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void exit() {
-        String goodbye = "Bye. Hope to see you again soon!";
-        System.out.println(goodbye);
+        Ui.exit();
         System.exit(0);
     }
 //    public static void echo(String input) {
 //        System.out.println(input);
 //    }
 
-    public static void readFile() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d yyyy");
-        try {
-            File eddieTaskList = new File("EddieTaskList.txt");
-            Scanner sc = new Scanner(eddieTaskList);
-            while (sc.hasNextLine()) {
-                String t = sc.nextLine();
-                String[] task = new String[5];
-                task = t.split(" , ");
 
-                if(task[0].equals("T")) {
-                    //System.out.println(task[2]);
-                    Todo todo = new Todo(task[2]);
-                    if (task[1].equals("x")) {
-                        todo.taskIsDone();
-                    }
-
-                    tasks.add(todo);
-                } if (task[0].equals("D")) {
-                    Deadline deadline = new Deadline(task[2], LocalDate.parse(task[3], formatter));
-//                    System.out.println(task[1]);
-                    if (task[1].equals("x")){
-                        deadline.taskIsDone();
-                    }
-                    tasks.add(deadline);
-                } if (task[0].equals("E")) {
-                    Event event = new Event(task[2], LocalDate.parse(task[3], formatter), LocalDate.parse(task[4], formatter));
-
-                    if (task[1].equals("x")) {
-                        event.taskIsDone();
-                    }
-
-                    tasks.add(event);
-                }
-            }
-
-            sc.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        }
-    }
 
     public static void add(Task t){
         String taskName = t.getName();
-        tasks.add(t);
-        System.out.println("Got it. I've added this task:\n "
-                + t.toString() + "\n"
-                + "Now you have " + tasks.size() + " tasks in the list.");
-        try {
-            writeToFile();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
+        TaskList.add(t);
+        Ui.addTask(t.toString(), TaskList.size());
+
+        Storage.write();
 
 
     }
 
     public static void clear() {
-        tasks.clear();
-        System.out.println("List Cleared!");
+        TaskList.clear();
+        Ui.clear();
 
-        try {
-            writeToFile();
-        } catch (IOException e) {
-            System.out.println("IOException when clearing tasks");
-        }
+        Storage.write();
     }
 
-    public static void writeToFile () throws IOException {
-        FileWriter eddieTaskList = new FileWriter("EddieTaskList.txt");
-        for (int i = 0; i < tasks.size(); i++) {
-            Task t = tasks.get(i);
 
-            if (t.getType() == "T") {
-                eddieTaskList.write("T , " + t.getStatus() + " , " + t.getName() + " , \n");
-            } else if (t.getType() == "D") {
-                eddieTaskList.write("D , " + t.getStatus() + " , " + t.getName() + " , " + t.getDeadline() + " , \n");
-            } else if (t.getType() == "E") {
-                eddieTaskList.write("E , " + t.getStatus() + " , " + t.getName() + " , " + t.getStartDate() + " , "
-                + t.getEndDate() + " , \n");
-            }
-        }
-        eddieTaskList.close();
-    }
 
     public static void list() {
-        int listSize = tasks.size();
+        int listSize = TaskList.size();
         for (int i = 0; i < listSize ; i++) {
             int num = i + 1;
-            Task taskToList = tasks.get(i);
-            System.out.println(num + ". " + taskToList.toString());
+            Task taskToList = TaskList.get(i);
+            Ui.listTask(num, taskToList.toString());
         }
     }
 
     public static void delete(int num) {
-        Task t = tasks.get(num - 1);
-        tasks.remove(num - 1);
-        System.out.println("Noted. I've removed this task: \n" +
-                t.toString() + "\n" +
-                "Now you have " + tasks.size() + " tasks in the list.");
+        Task t = TaskList.get(num - 1);
+        TaskList.delete(num - 1);
 
-        try {
-            writeToFile();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
+        Ui.removeTask(t.toString(), TaskList.size());
+        Storage.write();
+
     }
 
     public static void execute() throws DukeException {
@@ -146,23 +75,15 @@ public class Duke {
                 Duke.list();
             } else if (command.equals("mark")) {
                 int taskNum = sc.nextInt();
-                Task task = tasks.get(taskNum - 1);
+                Task task = TaskList.get(taskNum - 1);
                 task.taskIsDone();
-                try {
-                    writeToFile();
-                } catch (IOException e) {
-                    System.out.println("Cannot write to file (mark)");
-                }
+               Storage.write();
             } else if (command.equals("unmark")) {
                 int taskNum = sc.nextInt();
-                Task task = tasks.get(taskNum - 1);
+                Task task = TaskList.get(taskNum - 1);
                 task.taskNotDone();
 
-                try {
-                    writeToFile();
-                } catch (IOException e) {
-                    System.out.println("Cannot write to file (unmark)");
-                }
+                Storage.write();
             } else if (command.equals("delete")) {
                 int index = sc.nextInt();
                 delete(index);
@@ -229,14 +150,10 @@ public class Duke {
     }
     public static void main(String[] args) {
 
-        tasks.clear();
-        readFile();
+        TaskList.clear();
+        Storage.readFile();
 
-
-        String welcome = "Hello! I'm Eddie\n" +
-                "What can I do for you?";
-
-        System.out.println(welcome);
+        Ui.welcome();
 
         try {
             Duke.execute();
