390e105244dea9c8a32b9b333a8e4143c15242a3
mingyuanc <cmingyuan123@gmail.com>
2023-08-29 04:02:24 +0800

    add input field

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -3,22 +3,23 @@ public class Event extends Task {
     /**
      * The starting time of the event
      */
-    private String from;
+    private final String from;
 
     /**
      * The ending time of the event
      */
-    private String to;
+    private final String to;
 
     /**
      * constructor for Deadline
-     * 
-     * @param from - the starting time of the event
-     * @param to   - the ending time of the event
-     * @param task - the description of the task created
+     *
+     * @param from  - the starting time of the event
+     * @param to    - the ending time of the event
+     * @param task  - the description of the task created
+     * @param input - Input that generated the task
      */
-    public Event(String from, String to, String task) {
-        super(task);
+    public Event(String from, String to, String task, String input) {
+        super(task, input);
         this.from = from;
         this.to = to;
     }
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,11 +1,12 @@
 public class Todo extends Task {
     /**
      * Constructor for the Todo class
-     * 
-     * @param task - the description of the task created
+     *
+     * @param task  - the description of the task created
+     * @param input - Input that generated the task
      */
-    public Todo(String task) {
-        super(task);
+    public Todo(String task, String input) {
+        super(task, input);
     }
 
     @Override
