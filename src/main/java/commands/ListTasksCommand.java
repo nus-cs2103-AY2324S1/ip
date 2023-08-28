dbce4e1dc46f4f93f9515062265d0e3d98e83236
Woo Jia Hao <woojiahao1234@gmail.com>
2023-08-28 09:54:40 +0800

    Move list tasks behavior to TaskList

--- src/main/java/commands/ListTasksCommand.java
+++ src/main/java/commands/ListTasksCommand.java
@@ -11,17 +11,6 @@ public class ListTasksCommand implements ICommand {
   @Override
   public void execute(List<String> parts) {
     TaskList tasks = TaskList.getInstance();
-    if (tasks.size() == 0) {
-      PrintUtility.printText("You do not have any tasks, use todo, deadline, or event to add new " +
-          "ones!");
-      return;
-    }
-    List<String> formatted = IntStream
-        .range(0, tasks.size())
-        .mapToObj((j) -> String.format("%d. %s", j + 1, tasks.getTask(j)))
-        .collect(Collectors.toList());
-    formatted.add(0, "Here are the tasks in your list:");
-    String[] output = formatted.toArray(String[]::new);
-    PrintUtility.printText(output);
+    PrintUtility.printText(tasks.toString());
   }
 }
--- src/main/java/tasks/TaskList.java
+++ src/main/java/tasks/TaskList.java
@@ -1,5 +1,7 @@
 package tasks;
 
+import utility.PrintUtility;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -44,5 +46,18 @@ public class TaskList {
     if (index < 0 || index >= this.tasks.size()) {
       throw new IllegalArgumentException("Invalid index provided");
     }
+  }
+
+  @Override
+  public String toString() {
+    if (this.tasks.size() == 0) {
+      return "You do not have any tasks, use todo, deadline, or event to add new ones!";
+    }
+    List<String> formatted = IntStream
+        .range(0, tasks.size())
+        .mapToObj((j) -> String.format("%d. %s", j + 1, this.tasks.get(j)))
+        .collect(Collectors.toList());
+    formatted.add(0, "Here are the tasks in your list:");
+    return String.join("\n", formatted);
   }
 }
