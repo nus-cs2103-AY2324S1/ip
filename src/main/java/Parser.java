0363f91edcfc0b54b4ef7507d425b4d13f555914
Derek Tan <derek.tan.jing.xu@gmail.com>
2023-08-28 13:12:50 +0800

    Parser class: Add class skeleton

---
+++ src/main/java/Parser.java
@@ -0,0 +1,7 @@
+public class Parser {
+    private static final String DELIMITER_COMMAND = " ";
+    private static final String DELIMITER_DATE = " /";
+    public static String[] parseInput() {
+        return new String[]{};
+    }
+}
