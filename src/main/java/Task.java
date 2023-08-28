f5cba4609287c299daee97cb005951f34c75b1a4
Zjinnnn <zongjin.2@gmail.com>
2023-08-28 22:33:34 +0800

    feat: Implement level-7 increment

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,6 @@
-public class Task {
+import java.util.Arrays;
+
+public abstract class Task {
     private final String description;
     private boolean isDone;
 
@@ -7,13 +9,16 @@ public class Task {
         this.isDone = false;
     }
 
-    public String getStatusIcon() {
-        return (isDone ? "X" : " ");    // mark done task with X
+    public String getDescription() {
+        return this.description;
     }
 
-    @Override
-    public String toString() {
-        return String.format("[%s] %s", getStatusIcon(), description);
+    public boolean getIsDone() {
+        return this.isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");    // mark done task with X
     }
 
     public void markAsDone() {
@@ -22,5 +27,32 @@ public class Task {
 
     public void markAsUndone() {
         this.isDone = false;
+    }
+
+    public abstract String writeTaskToFile();
+
+    public static Task readTaskFromFile(String input) {
+        String[] args = input.split(" \\| ");
+        Task task;
+        switch (args[0]) {
+        case "T":
+            task = Todo.readTaskFromFile(args);
+            break;
+        case "D":
+            task = Deadline.readTaskFromFile(args);
+            break;
+        case "E":
+            task = Event.readTaskFromFile(args);
+            break;
+        default:
+            task = null;
+            break;
+        }
+        return task;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", getStatusIcon(), description);
     }
 }
