0cc6adbfa4c2fe3b2ddb4105334388c5844da0ea
JizhuoChen <88676760+JizhuoChen@users.noreply.github.com>
2023-08-28 23:27:54 +0800

    no message

--- src/main/java/Taskmanager.java
+++ src/main/java/Taskmanager.java
@@ -12,96 +12,107 @@ public class Taskmanager {
         String[] splt = input.split(" ");
         String keyword = splt[0];
         while (!keyword.equals("bye")) {
-            if (keyword.equals("list")) {
-                list();
-            } else if (keyword.equals("delete")) {
-                removeTask(Integer.parseInt(splt[1]));
-            } else if (keyword.equals("mark")) {
-                mark(Integer.parseInt(splt[1]));
-            } else if (keyword.equals("unmark")) {
-                unmark(Integer.parseInt(splt[1]));
-            } else {
-                try {
-                    if (keyword.equals("todo")) {
-                        String description = "";
-                        for (int i = 1; i < splt.length; ++i) {
-                            description = description + splt[i] + " ";
+            switch (keyword) {
+                case "list":
+                    list();
+                    break;
+                case "delete":
+                    removeTask(Integer.parseInt(splt[1]));
+                    break;
+                case "mark":
+                    mark(Integer.parseInt(splt[1]));
+                    break;
+                case "unmark":
+                    unmark(Integer.parseInt(splt[1]));
+                    break;
+                default:
+                    try {
+                        switch (keyword) {
+                            case "todo": {
+                                String description = "";
+                                for (int i = 1; i < splt.length; ++i) {
+                                    description = description + splt[i] + " ";
+                                }
+                                if (description.isEmpty()) {
+                                    throw new IllegalArgumentException("☹ OOPS!!! The description of a todo cannot be empty.");
+                                }
+                                addTask(new ToDos(description));
+                                break;
+                            }
+                            case "deadline": {
+                                int i = 1;
+                                if (i == splt.length) {
+                                    throw new IllegalArgumentException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                                }
+                                String temp = splt[i];
+                                String description = "";
+                                while (!temp.equals("/by")) {
+                                    description = description + temp + " ";
+                                    i += 1;
+                                    temp = splt[i];
+                                }
+                                i += 1;
+                                if (description.isEmpty()) {
+                                    throw new IllegalArgumentException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                                }
+                                String time = "";
+                                while (i < splt.length) {
+                                    time = time + splt[i] + " ";
+                                    i += 1;
+                                }
+                                if (time.isEmpty()) {
+                                    throw new IllegalArgumentException("☹ OOPS!!! The time/date of a deadline cannot be empty.");
+                                }
+                                addTask(new Deadlines(description, time));
+                                break;
+                            }
+                            case "event": {
+                                int i = 1;
+                                if (i == splt.length) {
+                                    throw new IllegalArgumentException("☹ OOPS!!! The description of an event cannot be empty.");
+                                }
+                                String temp = splt[i];
+                                String description = "";
+                                while (!temp.equals("/from")) {
+                                    description = description + temp + " ";
+                                    i += 1;
+                                    temp = splt[i];
+                                }
+                                i += 1;
+                                if (description.isEmpty()) {
+                                    throw new IllegalArgumentException("☹ OOPS!!! The description of an event cannot be empty.");
+                                }
+                                String start = "";
+                                if (i == splt.length) {
+                                    throw new IllegalArgumentException("☹ OOPS!!! The starting time of an event cannot be empty.");
+                                }
+                                temp = splt[i];
+                                while (!temp.equals("/to")) {
+                                    start = start + temp + " ";
+                                    i += 1;
+                                    temp = splt[i];
+                                }
+                                i += 1;
+                                if (start.isEmpty()) {
+                                    throw new IllegalArgumentException("☹ OOPS!!! The starting time of an event cannot be empty.");
+                                }
+                                String end = "";
+                                while (i < splt.length) {
+                                    end = end + splt[i] + " ";
+                                    i += 1;
+                                }
+                                if (end.isEmpty()) {
+                                    throw new IllegalArgumentException("☹ OOPS!!! The ending time of an event cannot be empty.");
+                                }
+                                addTask(new Events(description, start, end));
+                                break;
+                            }
+                            default:
+                                throw new IllegalArgumentException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                         }
-                        if (description.isEmpty()) {
-                            throw new IllegalArgumentException("☹ OOPS!!! The description of a todo cannot be empty.");
-                        }
-                        addTask(new ToDos(description));
-                    } else if (keyword.equals("deadline")) {
-                        int i = 1;
-                        if (i == splt.length) {
-                            throw new IllegalArgumentException("☹ OOPS!!! The description of a deadline cannot be empty.");
-                        }
-                        String temp = splt[i];
-                        String description = "";
-                        while (!temp.equals("/by")) {
-                            description = description + temp + " ";
-                            i += 1;
-                            temp = splt[i];
-                        }
-                        i += 1;
-                        if (description.isEmpty()) {
-                            throw new IllegalArgumentException("☹ OOPS!!! The description of a deadline cannot be empty.");
-                        }
-                        String time = "";
-                        while (i < splt.length) {
-                            time = time + splt[i] + " ";
-                            i += 1;
-                        }
-                        if (time.isEmpty()) {
-                            throw new IllegalArgumentException("☹ OOPS!!! The time/date of a deadline cannot be empty.");
-                        }
-                        addTask(new Deadlines(description, time));
-                    } else if (keyword.equals("event")) {
-                        int i = 1;
-                        if (i == splt.length) {
-                            throw new IllegalArgumentException("☹ OOPS!!! The description of an event cannot be empty.");
-                        }
-                        String temp = splt[i];
-                        String description = "";
-                        while (!temp.equals("/from")) {
-                            description = description + temp + " ";
-                            i += 1;
-                            temp = splt[i];
-                        }
-                        i += 1;
-                        if (description.isEmpty()) {
-                            throw new IllegalArgumentException("☹ OOPS!!! The description of an event cannot be empty.");
-                        }
-                        String start = "";
-                        if (i == splt.length) {
-                            throw new IllegalArgumentException("☹ OOPS!!! The starting time of an event cannot be empty.");
-                        }
-                        temp = splt[i];
-                        while (!temp.equals("/to")) {
-                            start = start + temp + " ";
-                            i += 1;
-                            temp = splt[i];
-                        }
-                        i += 1;
-                        if (start.isEmpty()) {
-                            throw new IllegalArgumentException("☹ OOPS!!! The starting time of an event cannot be empty.");
-                        }
-
-                        String end = "";
-                        while (i < splt.length) {
-                            end = end + splt[i] + " ";
-                            i += 1;
-                        }
-                        if (end.isEmpty()) {
-                            throw new IllegalArgumentException("☹ OOPS!!! The ending time of an event cannot be empty.");
-                        }
-                        addTask(new Events(description, start, end));
-                    } else {
-                        throw new IllegalArgumentException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    } catch (IllegalArgumentException e) {
+                        System.out.println(e.getMessage());
                     }
-                } catch (IllegalArgumentException e) {
-                    System.out.println(e.getMessage());
-                }
             }
             input = sn.nextLine();
             splt = input.split(" ");
