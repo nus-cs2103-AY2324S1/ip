67237346235d5540f41b915b4309d78a7751b698
seanpzk <seanpzk2@gmail.com>
2023-08-28 08:53:39 +0800

    Move printing job from Duke to ResponseGen

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,31 +13,31 @@ public class Duke {
     public static void listen(String input) throws InvalidUserInputException {
         if (input.equals("bye")) {
             botInUse=false;
-            System.out.println(responseGen.bye());
+            responseGen.bye();
         } else if (input.equals("list")) {
             String outputList = taskManager.outputNumberedList();
-            System.out.println(responseGen.list(outputList));
+            responseGen.list(outputList);
         } else if (input.contains("unmark")) {
             int a = Integer.parseInt(input.substring(7));
             Task t = taskManager.getTask(a-1);
             t.markAsUndone();
-            System.out.println(responseGen.unmarkTask(t));
+            responseGen.unmarkTask(t);
             updateSaveFile();
         } else if (input.contains("mark")) {
             int a = Integer.parseInt(input.substring(5));
             Task t = taskManager.getTask(a-1);
             t.markAsDone();
-            System.out.println(responseGen.markTask(t));
+            responseGen.markTask(t);
             updateSaveFile();
         } else if (input.contains("todo")) {
             try {
                 String toDoDescription = input.split("todo ")[1];
                 ToDo toDoTask = new ToDo(toDoDescription);
                 taskManager.addTask(toDoTask);
-                System.out.println(responseGen.toDoAdded(toDoTask, taskManager));
+                responseGen.toDoAdded(toDoTask, taskManager);
                 updateSaveFile();
             } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println(responseGen.toDoMissingContent());
+                responseGen.toDoMissingContent();
             }
         } else if (input.contains("deadline")) {
             try {
@@ -45,10 +45,10 @@ public class Duke {
                 String deadlineDescription = input.split("deadline ")[1].split(" /by")[0];
                 Deadline deadlineTask = new Deadline(deadlineDescription, by);
                 taskManager.addTask(deadlineTask);
-                System.out.println(responseGen.deadlineAdded(deadlineTask, taskManager));
+                responseGen.deadlineAdded(deadlineTask, taskManager);
                 updateSaveFile();
             } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println(responseGen.deadlineMissingContent());
+                responseGen.deadlineMissingContent();
             }
             } else if (input.contains("event")) {
             try {
@@ -57,27 +57,23 @@ public class Duke {
                 String eventDescription = input.split("event ")[1].split(" /from")[0];
                 Event eventTask = new Event(eventDescription, from, to);
                 taskManager.addTask(eventTask);
-                System.out.println(responseGen.eventAdded(eventTask, taskManager));
+                responseGen.eventAdded(eventTask, taskManager);
                 updateSaveFile();
             } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println(responseGen.eventMissingContent());
+                responseGen.eventMissingContent();
             }
         } else if (input.contains("delete")) {
             int a = Integer.parseInt(input.substring(7));
             Task toBeRemoved = taskManager.getTask(a-1);
             taskManager.deleteTask(a-1);
-            System.out.println(responseGen.taskDeleted(toBeRemoved, taskManager));
+            responseGen.taskDeleted(toBeRemoved, taskManager);
             updateSaveFile();
         } else {
             throw new InvalidUserInputException();
         }
     }
     public static void main(String[] args) {
-        
-        System.out.println(H_LINE
-                            + "Hello! I'm ChadBob.\n"
-                            + "What can I do for you?\n"
-                            + H_LINE) ;
+        responseGen.greetings();
         Scanner sc = new Scanner(System.in);
         while(botInUse) {
             fileManager.loadFileToTaskManager(taskManager);
@@ -85,9 +81,7 @@ public class Duke {
             try {
                 listen(input);
             } catch (InvalidUserInputException e) {
-                System.out.println(H_LINE
-                                    + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n"
-                                    + H_LINE);
+                responseGen.invalidInputRes();
             }
         }
 
