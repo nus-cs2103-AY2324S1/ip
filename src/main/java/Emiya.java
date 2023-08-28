db8745736638546a6bd0d91d5c91535b1e4b9506
seantehds <seanteh2001@gmail.com>
2023-08-28 21:37:41 +0800

    Fix a few bugs within Emiya with different numbers of tasks, as well as identation levels

--- src/main/java/Emiya.java
+++ src/main/java/Emiya.java
@@ -101,123 +101,132 @@ public class Emiya {
                 }
 
                 switch (typeOfTask) {
-                    case "mark":
-                        if (position != null) {
-                            if (position <= 0 || position > taskArrayList.size()) {
-                                throw new EmiyaException("Task does not exist! Please try with a different value");
-                            }
-                            taskArrayList.get(position-1).setMarked();
-                            System.out.println("-----------------------------------------\n" +
-                                    "Nice job! I have marked this task as done:\n" + taskArrayList.get(position-1) + "\n"
-                                    + "-----------------------------------------\n");
-                        } else {
-                            throw new EmiyaException("Unknown command received! Please try again!");
+                case "mark":
+                    if (position != null) {
+                        if (position <= 0 || position > taskArrayList.size()) {
+                            throw new EmiyaException("Task does not exist! Please try with a different value");
                         }
-                        break;
-                    case "unmark":
-                        if (position != null) {
-                            if (position <= 0 || position > taskArrayList.size()) {
-                                throw new EmiyaException("Task does not exist! Please try with a different value");
-                            }
-                            taskArrayList.get(position-1).setUnmarked();
-                            System.out.println("-----------------------------------------\n" +
-                                    "Oof, alright I have set this task as unmarked:\n" + taskArrayList.get(position-1) + "\n"
-                                    + "-----------------------------------------\n");
-                        } else {
-                            throw new EmiyaException("Unknown command received! Please try again!");
+                        taskArrayList.get(position-1).setMarked();
+                        System.out.println("-----------------------------------------\n" +
+                                "Nice job! I have marked this task as done:\n" + taskArrayList.get(position-1) + "\n"
+                                + "-----------------------------------------\n");
+                    } else {
+                        throw new EmiyaException("Unknown command received! Please try again!");
+                    }
+                    break;
+                case "unmark":
+                    if (position != null) {
+                        if (position <= 0 || position > taskArrayList.size()) {
+                            throw new EmiyaException("Task does not exist! Please try with a different value");
                         }
-                        break;
-                    case "delete":
-                        if (position != null) {
-                            if (position <= 0 || position > taskArrayList.size()) {
-                                throw new EmiyaException("Task does not exist! Please try with a different value");
-                            }
-                            Task task = taskArrayList.get(position-1);
-                            taskArrayList.remove(task);
-                            System.out.println("-----------------------------------------\n" +
+                        taskArrayList.get(position-1).setUnmarked();
+                        System.out.println("-----------------------------------------\n" +
+                                "Oof, alright I have set this task as unmarked:\n" + taskArrayList.get(position-1) + "\n"
+                                + "-----------------------------------------\n");
+                    } else {
+                        throw new EmiyaException("Unknown command received! Please try again!");
+                    }
+                    break;
+                case "delete":
+                    if (position != null) {
+                        if (position <= 0 || position > taskArrayList.size()) {
+                            throw new EmiyaException("Task does not exist! Please try with a different value");
+                        }
+                        Task task = taskArrayList.get(position-1);
+                        taskArrayList.remove(task);
+                        String deleteOutputMessage;
+                        if (taskArrayList.size() == 1) {
+                            deleteOutputMessage = "-----------------------------------------\n" +
+                                    "Sure, I shall now delete the following task:\n" + task + "\n"
+                                    + "Now you have " + taskArrayList.size() + " task in your list!\n"
+                                    + "-----------------------------------------\n";
+                        } else {
+                            deleteOutputMessage = "-----------------------------------------\n" +
                                     "Sure, I shall now delete the following task:\n" + task + "\n"
                                     + "Now you have " + taskArrayList.size() + " tasks in your list!\n"
-                                    + "-----------------------------------------\n");
-                        } else {
-                            throw new EmiyaException("Unknown command received! Please try again!");
-                        }
-                        break;
-                    case "todo":
-                        // need to be able to go through the rest of the string and add it inside
-                        if (taskDetails.length() < 1) {
-                            throw new EmiyaException("Oh no! Todo tasks cannot be empty! Please try again!");
-                        }
-                        ToDo todo = new ToDo(taskDetails);
-                        taskArrayList.add(todo);
-                        String todoOutputMessage = "";
-                        if (taskArrayList.size() == 1) {
-                            todoOutputMessage = "-----------------------------------------\n" +
-                                    "Sure! I have added this task to the list:\n" + todo + "\n"
-                                    + "Now you have " + taskArrayList.size() + " task in your list!\n"
-                                    + "-----------------------------------------\n";
-                        } else {
-                            todoOutputMessage = "-----------------------------------------\n" +
-                                    "Sure! I have added this task to the list:\n" + todo + "\n"
-                                    + "Now you have " + taskArrayList.size() + " tasks in your list!\n"
                                     + "-----------------------------------------\n";
                         }
-                        System.out.println(todoOutputMessage);
-                        break;
-                    case "deadline": // go through taskDetails and find /by
-                        if (taskDetails.length() < 1) {
-                            throw new EmiyaException("Oh no! Deadline tasks cannot be empty! Please try again!");
-                        }
-                        String[] deadlineDetails = taskDetails.split("/by", 2);
-                        if (deadlineDetails.length <= 1) {
-                            throw new EmiyaException("It seems like there's an error in your input! Did you remember to use /by in your input?");
-                        }
-                        Deadline deadline = new Deadline(deadlineDetails[0], deadlineDetails[1]);
-                        taskArrayList.add(deadline);
-                        String deadlineOutputMessage = "";
-                        if (taskArrayList.size() == 1) {
-                            deadlineOutputMessage = "-----------------------------------------\n" +
-                                    "Sure! I have added this task to the list:\n" + deadline + "\n"
-                                    + "Now you have " + taskArrayList.size() + " task in your list!\n"
-                                    + "-----------------------------------------\n";
-                        } else {
-                            deadlineOutputMessage = "-----------------------------------------\n" +
-                                    "Sure! I have added this task to the list:\n" + deadline + "\n"
-                                    + "Now you have " + taskArrayList.size() + " tasks in your list!\n"
-                                    + "-----------------------------------------\n";
-                        }
-                        System.out.println(deadlineOutputMessage);
-                        break;
-                    case "event": // need to go through taskDetails and find /from and /to
-                        if (taskDetails.length() <= 1) {
-                            throw new EmiyaException("Oh no! Event tasks cannot be empty! Please try again!");
-                        }
-                        String[] eventDetails = taskDetails.split("/from ", 2);
-                        if (eventDetails.length <= 1) {
-                            throw new EmiyaException("It seems like there's an error in your input! Did you remember to use /from in your input?");
-                        }
-                        String[] eventDurationDetails = eventDetails[1].split("/to ", 2);
-                        if (eventDurationDetails.length <= 1) {
-                            throw new EmiyaException("It seems like there's an error in your input! Did you remember to use /to in your input?");
-                        }
-                        Event event = new Event(eventDetails[0], eventDurationDetails[0], eventDurationDetails[1]);
-                        taskArrayList.add(event);
-                        String eventOutputMessage = "";
-                        if (taskArrayList.size() == 1) {
-                            eventOutputMessage = "-----------------------------------------\n" +
-                                    "Sure! I have added this task to the list:\n" + event + "\n"
-                                    + "Now you have " + taskArrayList.size() + " task in your list!\n"
-                                    + "-----------------------------------------\n";
-                        } else {
-                            eventOutputMessage = "-----------------------------------------\n" +
-                                    "Sure! I have added this task to the list:\n" + event + "\n"
-                                    + "Now you have " + taskArrayList.size() + " tasks in your list!\n"
-                                    + "-----------------------------------------\n";
-                        }
-
-                        System.out.println(eventOutputMessage);
-                        break;
-                    default:
+                        System.out.println(deleteOutputMessage);
+                    } else {
                         throw new EmiyaException("Unknown command received! Please try again!");
+                    }
+                    break;
+                case "todo":
+                    // need to be able to go through the rest of the string and add it inside
+                    if (taskDetails.length() < 1) {
+                        throw new EmiyaException("Oh no! Todo tasks cannot be empty! Please try again!");
+                    }
+                    ToDo todo = new ToDo(taskDetails);
+                    taskArrayList.add(todo);
+                    String todoOutputMessage;
+                    if (taskArrayList.size() == 1) {
+                        todoOutputMessage = "-----------------------------------------\n" +
+                                "Sure! I have added this task to the list:\n" + todo + "\n"
+                                + "Now you have " + taskArrayList.size() + " task in your list!\n"
+                                + "-----------------------------------------\n";
+                    } else {
+                        todoOutputMessage = "-----------------------------------------\n" +
+                                "Sure! I have added this task to the list:\n" + todo + "\n"
+                                + "Now you have " + taskArrayList.size() + " tasks in your list!\n"
+                                + "-----------------------------------------\n";
+                    }
+                    System.out.println(todoOutputMessage);
+                    break;
+                case "deadline": // go through taskDetails and find /by
+                    if (taskDetails.length() < 1) {
+                        throw new EmiyaException("Oh no! Deadline tasks cannot be empty! Please try again!");
+                    }
+                    String[] deadlineDetails = taskDetails.split("/by", 2);
+                    if (deadlineDetails.length <= 1) {
+                        throw new EmiyaException("It seems like there's an error in your input! Did you remember to use /by in your input?");
+                    }
+                    Deadline deadline = new Deadline(deadlineDetails[0], deadlineDetails[1]);
+                    taskArrayList.add(deadline);
+                    String deadlineOutputMessage;
+                    if (taskArrayList.size() == 1) {
+                        deadlineOutputMessage = "-----------------------------------------\n" +
+                                "Sure! I have added this task to the list:\n" + deadline + "\n"
+                                + "Now you have " + taskArrayList.size() + " task in your list!\n"
+                                + "-----------------------------------------\n";
+                    } else {
+                        deadlineOutputMessage = "-----------------------------------------\n" +
+                                "Sure! I have added this task to the list:\n" + deadline + "\n"
+                                + "Now you have " + taskArrayList.size() + " tasks in your list!\n"
+                                + "-----------------------------------------\n";
+                    }
+                    System.out.println(deadlineOutputMessage);
+                    break;
+                case "event": // need to go through taskDetails and find /from and /to
+                    if (taskDetails.length() <= 1) {
+                        throw new EmiyaException("Oh no! Event tasks cannot be empty! Please try again!");
+                    }
+                    String[] eventDetails = taskDetails.split("/from ", 2);
+                    if (eventDetails.length <= 1) {
+                        throw new EmiyaException("It seems like there's an error in your input! Did you remember to use /from in your input?");
+                    }
+                    String[] eventDurationDetails = eventDetails[1].split("/to ", 2);
+                    if (eventDurationDetails.length <= 1) {
+                        throw new EmiyaException("It seems like there's an error in your input! Did you remember to use /to in your input?");
+                    }
+                    Event event = new Event(eventDetails[0], eventDurationDetails[0], eventDurationDetails[1]);
+                    taskArrayList.add(event);
+                    String eventOutputMessage;
+                    if (taskArrayList.size() == 1) {
+                        eventOutputMessage = "-----------------------------------------\n" +
+                                "Sure! I have added this task to the list:\n" + event + "\n"
+                                + "Now you have " + taskArrayList.size() + " task in your list!\n"
+                                + "-----------------------------------------\n";
+                    } else {
+                        eventOutputMessage = "-----------------------------------------\n" +
+                                "Sure! I have added this task to the list:\n" + event + "\n"
+                                + "Now you have " + taskArrayList.size() + " tasks in your list!\n"
+                                + "-----------------------------------------\n";
+                    }
+
+                    System.out.println(eventOutputMessage);
+                    break;
+                default:
+                    throw new EmiyaException("Unknown command received! Please try again!");
                 }
             } catch (EmiyaException e) {
                 System.out.println(e.getMessage());
