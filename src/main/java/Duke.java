645bc46a203c1481dadc6a1e77f3db9a9c2c31f0
jordankanghm <jordankanghm@gmail.com>
2023-08-29 00:06:46 +0800

    Initial Commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,13 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
-    public static ArrayList<Task> list = new ArrayList<>();
+    // list exists so that duke.txt file does not have to be loaded so many times for mark,
+    // unmark and delete operations
+//    public static ArrayList<Task> list = loadDataFromFile();
     enum Day {
         Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
     }
@@ -11,6 +17,8 @@ public class Duke {
     }
     public static void main(String[] args) {
         printIntro();
+
+        ArrayList<Task> throwExceptionIfFileFormatIncorrect = loadDataFromFile();
 
         Scanner sc = new Scanner(System.in);
         // Get user command
@@ -31,7 +39,8 @@ public class Duke {
                     }
                     String taskName = cmd.split(" ", 2)[1];
                     Task todo = new ToDo(taskName);
-                    list.add(todo);
+//                    list.add(todo);
+                    appendTaskToFile(todo);
                     printAddTaskMessage(todo);
                 } else if (type.equals("deadline")) {
                     if (descriptionIsEmpty(cmd)) {
@@ -52,7 +61,8 @@ public class Duke {
 
                     // Assumes that deadlineDescription starts with a "by"
                     Task deadline = new Deadline(taskName, deadlineDescription.split(" ", 2)[1]);
-                    list.add(deadline);
+//                    list.add(deadline);
+                    appendTaskToFile(deadline);
                     printAddTaskMessage(deadline);
                 } else if (type.equals("event")) {
                     if (descriptionIsEmpty(cmd)) {
@@ -80,7 +90,8 @@ public class Duke {
 
                     // Assumes that starting and ending both start with "from" and "to" respectively
                     Task event = new Event(taskName, starting.split(" ", 2)[1].trim(), ending.split(" ", 2)[1].trim());
-                    list.add(event);
+//                    list.add(event);
+                    appendTaskToFile(event);
                     printAddTaskMessage(event);
                 } else if (type.equals("delete")) {
                     if (descriptionIsEmpty(cmd)) {
@@ -98,8 +109,9 @@ public class Duke {
                     if (!isValidTaskNumber(taskNumber)) {
                         throw new InvalidTaskNumberException(taskNumber);
                     }
-                    Task task = list.get(taskNumber - 1);
-                    list.remove(task);
+//                    Task task = list.get(taskNumber - 1);
+//                    list.remove(task);
+                    Task task = markOrDeleteDataToFile(taskNumber - 1, "delete");
                     printDeleteTaskMessage(task);
                 } else if (type.equals("mark")) {
                     if (descriptionIsEmpty(cmd)) {
@@ -118,8 +130,9 @@ public class Duke {
                         throw new InvalidTaskNumberException(taskNumber);
                     }
 
-                    Task task = list.get(taskNumber - 1);
-                    task.markTask();
+//                    Task task = list.get(taskNumber - 1);
+//                    task.markTask();
+                    Task task = markOrDeleteDataToFile(taskNumber - 1, "mark");
                     printMarkedTaskMessage(task);
                 } else if (type.equals("unmark")) {
                     if (descriptionIsEmpty(cmd)) {
@@ -138,8 +151,9 @@ public class Duke {
                         throw new InvalidTaskNumberException(taskNumber);
                     }
 
-                    Task task = list.get(taskNumber - 1);
-                    task.unMarkTask();
+//                    Task task = list.get(taskNumber - 1);
+//                    task.unMarkTask();
+                    Task task = markOrDeleteDataToFile(taskNumber - 1, "unmark");
                     printUnmarkedTaskMessage(task);
                 } else {  // If the inputted command is not valid, throw TaskTypeException
                     throw new TaskTypeException();
@@ -172,10 +186,119 @@ public class Duke {
         printExit();
     }
 
+    // Obtains the tasks from the duke.txt file and prints them one by one
     public static void printListItems() {
-        for (int i = 0; i < list.size(); i++) {
-            Task task = list.get(i);
-            System.out.println(i+1 + "." + task.getTaskType() + task.getStatusIcon() + " " + task.name);
+        ArrayList<Task> tasks = loadDataFromFile();
+        for (int i = 0; i < tasks.size(); i++) {
+            Task task = tasks.get(i);
+            System.out.println(i+1 + "." + task.getTaskType() + task.getStatusIcon() + " " + task.name +
+                                " " + task.getTimeInfo());
+        }
+    }
+
+//    public static void writeItemToFile(Task task) {
+//        String data =  list.size() + "." + task.getTaskType() + task.getStatusIcon() + " " + task.name + "\n";
+//        // Get the path to the duke.txt file
+//        Path filePath = Paths.get("./data/duke.txt");
+//        try {
+//            // Append data to the duke.txt file
+//            Files.write(filePath, data.getBytes(), StandardOpenOption.APPEND);
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        }
+//    }
+
+    // Removes all data from the duke.txt file
+    // Takes the task arraylist argument, serializes it and stores it in the duke.txt file
+    public static void saveDataToFile(ArrayList<Task> data) {
+        // Delete all data from duke.txt file
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("./data/duke.txt", false))) {
+            outputStream.writeObject(data);
+            System.out.println("Data saved to file.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Takes the task argument, serializes it and adds it to the end of the duke.txt file
+    public static void appendTaskToFile(Task task) {
+        ArrayList<Task> tasks = loadDataFromFile();
+
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("./data/duke.txt"))) {
+            tasks.add(task);
+            saveDataToFile(tasks);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Reads the data in the duke.txt file, unserializes it and returns an arraylist representing
+    // the saved tasks
+    public static ArrayList<Task> loadDataFromFile() {
+        ArrayList<Task> tasks = new ArrayList<>();
+
+        String folderPath = "./data/";
+        String filePath = "./data/duke.txt";
+
+        try {
+            // Check if the folder exists, create if not
+            Path folder = Paths.get(folderPath);
+            if (!Files.exists(folder)) {
+                Files.createDirectories(folder);
+                System.out.println("Folder created: " + folderPath);
+            }
+            // Check if the file exists, create if not
+            Path file = Paths.get(filePath);
+            if (!Files.exists(file)) {
+                Files.createFile(file);
+                System.out.println("File created: " + filePath);
+            }
+
+            // If the duke.txt file is empty, return an empty task arraylist
+            if (Files.size(file) == 0) {
+                return tasks;
+            }
+
+            // Throws StreamCorruptedException when data format is unserializable
+            ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filePath));
+            tasks = (ArrayList<Task>) inputStream.readObject();
+            System.out.println("Tasks loaded from file.");
+
+            // Throws exception if data in duke.txt is not an ArrayList<Task>
+            if (tasks instanceof ArrayList<?>) {
+                tasks = (ArrayList<Task>) tasks;
+                System.out.println("Tasks loaded from file.");
+            } else {
+                throw new InvalidDataFormatException();
+            }
+        } catch (InvalidDataFormatException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return tasks;
+    }
+
+    // Reads the duke.txt file, unserializes it and obtain the arraylist representing saved tasks.
+    // Depending on the purpose, information in the arraylist is updated, and the whole arraylist is
+    // serialized and saved again in the duke.txt file
+    public static Task markOrDeleteDataToFile(int index, String purpose) {
+        ArrayList<Task> tasks = loadDataFromFile();
+
+        if (purpose.equals("mark")) {
+            tasks.get(index).markTask();
+            saveDataToFile(tasks);
+            return tasks.get(index);
+        } else if (purpose.equals("unmark")) {
+            tasks.get(index).unMarkTask();
+            saveDataToFile(tasks);
+            return tasks.get(index);
+        } else {
+            Task deletedTask = tasks.get(index);
+            tasks.remove(index);
+            saveDataToFile(tasks);
+            return deletedTask;
         }
     }
 
@@ -188,19 +311,22 @@ public class Duke {
     }
 
     public static void printAddTaskMessage(Task task) {
+        int listSize = loadDataFromFile().size();
         String message = "____________________________________________________________\n" +
                 " Got it. I've added this task: \n   " +
                 task.getTaskType() + task.getStatusIcon() + " " + task.name + task.getTimeInfo() + "\n" +
-                " Now you have " + list.size() + " tasks in the list.\n" +
+                " Now you have " + listSize + " tasks in the list.\n" +
                 "____________________________________________________________";
         System.out.println(message);
     }
 
     public static void printDeleteTaskMessage(Task task){
+        int listSize = loadDataFromFile().size();
+
         String message = "____________________________________________________________\n" +
                 " Noted. I've removed this task: \n   " +
                 task.getTaskType() + task.getStatusIcon() + " " + task.name + task.getTimeInfo() + "\n" +
-                " Now you have " + list.size() + " tasks in the list.\n" +
+                " Now you have " + listSize + " tasks in the list.\n" +
                 "____________________________________________________________";
         System.out.println(message);
     }
@@ -215,7 +341,7 @@ public class Duke {
     public static void printMarkedTaskMessage(Task task) {
         String message = "____________________________________________________________\n" +
                 " Nice! I've marked this task as done:\n" +
-                "   " + task.getStatusIcon() + " " + task.name + "\n" +
+                "   " + task.getStatusIcon() + " " + task.name + task.getTimeInfo() + "\n" +
                 "____________________________________________________________";
         System.out.println(message);
     }
@@ -223,7 +349,7 @@ public class Duke {
     public static void printUnmarkedTaskMessage(Task task) {
         String message = "____________________________________________________________\n" +
                 " OK. I've marked this task as not done yet:\n" +
-                "   " + task.getStatusIcon() + " " + task.name + "\n" +
+                "   " + task.getStatusIcon() + " " + task.name + task.getTimeInfo() + "\n" +
                 "____________________________________________________________";
         System.out.println(message);
     }
@@ -402,6 +528,7 @@ public class Duke {
     }
 
     public static boolean isValidTaskNumber(int number) {
-        return number > 0 && number <= list.size();
+        int listSize = loadDataFromFile().size();
+        return number > 0 && number <= listSize;
     }
 }
