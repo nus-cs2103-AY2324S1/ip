839002df2b605e2aa70004427260675ab047b1ff
Derek Tan <derek.tan.jing.xu@gmail.com>
2023-08-28 20:44:41 +0800

    Duke class: Update to use TaskList class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,17 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.chrono.ChronoLocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static ArrayList<Task> tasks;
-    private static final String FILE_PATH = "./data/state.txt";
+    private static TaskList tasks;
     private static final String DATETIME_INPUT_FORMAT = "yyyy-MM-dd HHmm";
     public static final DateTimeFormatter dateTimeInputFormatter = DateTimeFormatter.ofPattern(DATETIME_INPUT_FORMAT);
-    private static final String DATE_INPUT_FORMAT = "yyyy-MM-dd";
-    public static final DateTimeFormatter dateInputFormatter = DateTimeFormatter.ofPattern(DATE_INPUT_FORMAT);
+
     public static void main(String[] args) {
-        Duke.tasks = new ArrayList<>();
+        Duke.tasks = new TaskList();
         Scanner in = new Scanner(System.in);
-        Duke.loadState();
 
         String name = "Derek";
         System.out.println("Hello! I'm " + name);
@@ -33,75 +25,49 @@ public class Duke {
 
                 if (command.equals(Command.MARK.getCommand())) {
                     int index = Integer.parseInt(parsedInput.get(1)) - 1;
-                    if (!Duke.isTaskIndexValid(index)) {
-                        throw new InvalidCommandException("☹ OOPS!!! The task index in invalid");
-                    }
-                    Task task = Duke.tasks.get(index);
-                    task.markAsDone();
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(task);
+                    tasks.mark(index);
                     continue;
                 }
                 if (command.equals(Command.UNMARK.getCommand())) {
                     int index = Integer.parseInt(parsedInput.get(1)) - 1;
-                    if (!Duke.isTaskIndexValid(index)) {
-                        throw new InvalidCommandException("☹ OOPS!!! The task index in invalid");
-                    }
-                    Task task = Duke.tasks.get(index);
-                    task.markAsUndone();
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println(task);
+                    tasks.unmark(index);
                     continue;
                 }
                 if (command.equals(Command.LIST.getCommand())) {
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < Duke.tasks.size(); i++) {
-                        System.out.println((i + 1) + "." + Duke.tasks.get(i));
-                    }
+                    tasks.printContents();
                     continue;
                 }
                 if (command.equals(Command.BYE.getCommand())) {
-                    Duke.saveState();
+                    Duke.tasks.saveState();
                     System.out.println("Bye. Hope to see you again soon!");
                     break;
                 }
                 if (command.equals(Command.TODO.getCommand())) {
                     ToDo newTodo = new ToDo(parsedInput.get(1));
-                    Duke.tasks.add(newTodo);
-                    Duke.printTaskAddedMessages(newTodo);
+                    tasks.add(newTodo);
                     continue;
                 }
                 if (command.equals(Command.DEADLINE.getCommand())) {
                     Deadline newDeadline = new Deadline(parsedInput.get(1),
                             LocalDateTime.parse(parsedInput.get(2), dateTimeInputFormatter));
-                    Duke.tasks.add(newDeadline);
-                    Duke.printTaskAddedMessages(newDeadline);
+                    tasks.add(newDeadline);
                     continue;
                 }
                 if (command.equals(Command.EVENT.getCommand())) {
                     Event newEvent = new Event(parsedInput.get(1),
                             LocalDateTime.parse(parsedInput.get(2), dateTimeInputFormatter),
                             LocalDateTime.parse(parsedInput.get(3), dateTimeInputFormatter));
-                    Duke.tasks.add(newEvent);
-                    Duke.printTaskAddedMessages(newEvent);
+                    tasks.add(newEvent);
                     continue;
                 }
                 if (command.equals(Command.DELETE.getCommand())) {
                     int index = Integer.parseInt(parsedInput.get(1)) - 1;
-                    if (!Duke.isTaskIndexValid(index)) {
-                        throw new InvalidCommandException("☹ OOPS!!! The task index in invalid");
-                    }
-                    Task task = Duke.tasks.remove(index);
-                    Duke.printTaskDeletedMessage(task);
+                    Duke.tasks.remove(index);
                     continue;
                 }
                 if (command.equals(Command.ON.getCommand())) {
                     LocalDate date = LocalDate.parse(parsedInput.get(1));
-                    for (int i = 0; i < Duke.tasks.size(); i++) {
-                        if (Duke.tasks.get(i).isOnDate(date)) {
-                            System.out.println((i + 1) + "." + Duke.tasks.get(i));
-                        }
-                    }
+                    tasks.printTasksOn(date);
                     continue;
                 }
                 throw new InvalidCommandException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
@@ -110,67 +76,5 @@ public class Duke {
             }
         }
         in.close();
-    }
-
-    private static void printTaskAddedMessages(Task task) {
-        System.out.println("Got it. I've added this task:");
-        System.out.println(task);
-        Duke.printTaskCount();
-    }
-
-    private static void printTaskDeletedMessage(Task task) {
-        System.out.println("Noted. I've removed this task:");
-        System.out.println(task);
-        Duke.printTaskCount();
-    }
-
-    private static void printTaskCount() {
-        int tasksCount = Duke.tasks.size();
-        System.out.println("Now you have " + tasksCount + (tasksCount == 1 ? " task" : " tasks") + " in the list.");
-    }
-
-    private static boolean isTaskIndexValid(int index) {
-        return index >= 0 && index < Duke.tasks.size();
-    }
-
-    private static void loadState() {
-        try {
-            File f = new File(Duke.FILE_PATH);
-            Scanner s = new Scanner(f);
-            while (s.hasNext()) {
-                String[] taskArray = s.nextLine().split(" / ");
-                Task task;
-
-                if (taskArray[0].equals(Command.TODO.getCommand())) {
-                    task = new ToDo(taskArray[2]);
-                } else if (taskArray[0].equals(Command.DEADLINE.getCommand())) {
-                    task = new Deadline(taskArray[2], LocalDateTime.parse(taskArray[3], dateTimeInputFormatter));
-                } else {
-                    task = new Event(taskArray[2], LocalDateTime.parse(taskArray[3], dateTimeInputFormatter),
-                            LocalDateTime.parse(taskArray[4], dateTimeInputFormatter));
-                }
-
-                if (taskArray[1].equals("1")) {
-                    task.markAsDone();
-                }
-                Duke.tasks.add(task);
-            }
-            System.out.println("Successfully loaded saved state");
-        } catch (FileNotFoundException e) {
-            System.out.println("File to save state cannot be found");
-        }
-    }
-
-    private static void saveState() {
-        try {
-            FileWriter fw = new FileWriter(Duke.FILE_PATH);
-            for (int i = 0; i < Duke.tasks.size(); i++) {
-                fw.write(Duke.tasks.get(i).toSaveStateString() + "\n");
-            }
-            fw.close();
-            System.out.println("Sucessfully saved state");
-        } catch (IOException e) {
-            System.out.println("Failed to save state");
-        }
     }
 }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,20 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TaskList {
     private ArrayList<Task> tasks = new ArrayList<>();
+    private static final String FILE_PATH = "./data/state.txt";
+    private static final String DATETIME_INPUT_FORMAT = "yyyy-MM-dd HHmm";
+    public static final DateTimeFormatter dateTimeInputFormatter = DateTimeFormatter.ofPattern(DATETIME_INPUT_FORMAT);
 
-    public TaskList() {}
+    public TaskList() {
+        loadState();
+    }
 
     public void add(Task task) {
         this.tasks.add(task);
+        this.printTaskAddedMessages(task);
     }
 
     public void remove(int index) throws InvalidCommandException {
         if (!isTaskValid(index)) {
             throw new InvalidCommandException("☹ OOPS!!! The task index in invalid");
         }
-        this.tasks.remove(index);
+        Task task = this.tasks.remove(index);
+        this.printTaskDeletedMessage(task);
     }
 
     public void printContents() {
@@ -52,7 +66,65 @@ public class TaskList {
         System.out.println(task);
     }
 
+    public void saveState() {
+        try {
+            FileWriter fw = new FileWriter(TaskList.FILE_PATH);
+            for (int i = 0; i < this.tasks.size(); i++) {
+                fw.write(this.tasks.get(i).toSaveStateString() + "\n");
+            }
+            fw.close();
+            System.out.println("Sucessfully saved state");
+        } catch (IOException e) {
+            System.out.println("Failed to save state");
+        }
+    }
+
+    private void loadState() {
+        try {
+            File f = new File(TaskList.FILE_PATH);
+            Scanner s = new Scanner(f);
+            while (s.hasNext()) {
+                String[] taskArray = s.nextLine().split(" / ");
+                Task task;
+
+                if (taskArray[0].equals(Command.TODO.getCommand())) {
+                    task = new ToDo(taskArray[2]);
+                } else if (taskArray[0].equals(Command.DEADLINE.getCommand())) {
+                    task = new Deadline(taskArray[2], LocalDateTime.parse(taskArray[3], dateTimeInputFormatter));
+                } else {
+                    task = new Event(taskArray[2], LocalDateTime.parse(taskArray[3], dateTimeInputFormatter),
+                            LocalDateTime.parse(taskArray[4], dateTimeInputFormatter));
+                }
+
+                if (taskArray[1].equals("1")) {
+                    task.markAsDone();
+                }
+                this.tasks.add(task);
+            }
+            System.out.println("Successfully loaded saved state");
+        } catch (FileNotFoundException e) {
+            System.out.println("File to save state cannot be found");
+        }
+    }
+
     private boolean isTaskValid(int index) {
         return index >= 0 && index < this.tasks.size();
+    }
+
+    private void printTaskAddedMessages(Task task) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println(task);
+        this.printTaskCount();
+    }
+
+    private void printTaskDeletedMessage(Task task) {
+        System.out.println("Noted. I've removed this task:");
+        System.out.println(task);
+        this.printTaskCount();
+    }
+
+    private void printTaskCount() {
+        int tasksCount = this.tasks.size();
+        System.out.println("Now you have " + tasksCount + (tasksCount == 1 ? " task" : " tasks") + " in the list.");
     }
 }
