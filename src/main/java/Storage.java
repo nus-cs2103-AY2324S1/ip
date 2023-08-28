a0d382a9bb53fc802a424e2c8e2b00641476d76a
Derek Tan <derek.tan.jing.xu@gmail.com>
2023-08-28 20:56:23 +0800

    Add Storage class

---
+++ src/main/java/Storage.java
@@ -0,0 +1,11 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Storage {
+    public static void saveData(String[] data, String filePath) throws IOException {}
+
+    public static <T> ArrayList<T> loadData(String filePath) {
+        return new ArrayList<T>();
+    }
+}
