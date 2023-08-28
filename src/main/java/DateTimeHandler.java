c65a2ea408c3b5943d4aa4d0b74bd9e752067455
ji-just-ji <wongjk9999@gmail.com>
2023-08-28 18:30:50 +0800

    Add try catch for DateTimeHandler

--- src/main/java/DateTimeHandler.java
+++ src/main/java/DateTimeHandler.java
@@ -11,16 +11,21 @@ public class DateTimeHandler {
     private int min;
     public DateTimeHandler(String s) {
 
-        String[] dateTime = s.split(" ");
-        String[] dateString = dateTime[0].split("/");
-        int timeString = Integer.parseInt(dateTime[1]);
-        day = Integer.parseInt(dateString[0]);
-        month = Integer.parseInt(dateString[1]);
-        year = Integer.parseInt(dateString[2]);
-        hour = timeString/100;
-        min = timeString%100;
-        date = LocalDateTime.of(year,month,day,hour,min);
-        dateTimeString = date.getMonth() +  " " + date.getDayOfMonth() + " " + date.getYear();
+        try {
+
+            String[] dateTime = s.split(" ");
+            String[] dateString = dateTime[0].split("/");
+            int timeString = Integer.parseInt(dateTime[1]);
+            day = Integer.parseInt(dateString[0]);
+            month = Integer.parseInt(dateString[1]);
+            year = Integer.parseInt(dateString[2]);
+            hour = timeString/100;
+            min = timeString%100;
+            date = LocalDateTime.of(year,month,day,hour,min);
+            dateTimeString = date.getMonth() +  " " + date.getDayOfMonth() + " " + date.getYear();
+        } catch (ArrayIndexOutOfBoundsException exception) {
+            
+        }
     }
 
     public LocalDateTime getDate() {
