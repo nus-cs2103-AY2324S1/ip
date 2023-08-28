bc1cdfffc8862783bbd78e6124679dab648d67b9
Derek Tan <derek.tan.jing.xu@gmail.com>
2023-08-28 20:57:14 +0800

    Storage class: Add implementation for saveData function

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -3,7 +3,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Storage {
-    public static void saveData(String[] data, String filePath) throws IOException {}
+    public static void saveData(String[] data, String filePath) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        for (int i = 0; i < data.length; i++) {
+            fw.write(data[i] + "\n");
+        }
+        fw.close();
+    }
 
     public static <T> ArrayList<T> loadData(String filePath) {
         return new ArrayList<T>();
