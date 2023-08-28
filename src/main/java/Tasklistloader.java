0fff40c39e33eda6cb5128e0fbaeca0bb7ac383c
SynapseProgramming <roald.ong@gmail.com>
2023-08-28 13:34:44 +0800

    Add in Tasklistloader class

---
+++ src/main/java/Tasklistloader.java
@@ -0,0 +1,38 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Tasklistloader {
+
+  private ArrayList<Task> taskList;
+  private ObjectMapper taskMapper;
+
+  public Tasklistloader(ArrayList<Task> tasks) {
+    this.taskList = tasks;
+    this.taskMapper = new ObjectMapper();
+  }
+
+
+  // given the original tasklist, save the tasks into a json file
+  public void saveTasks() {
+
+    Map<Integer, Task> tasks = new HashMap<>();
+    for (int i = 0; i < this.taskList.size(); i++) {
+      Integer index = i + 1;
+      Task currTask = taskList.get(i);
+      tasks.put(index, currTask);
+      try {
+        taskMapper.writeValue(new File("tasks.json"), tasks);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+  }
+}
