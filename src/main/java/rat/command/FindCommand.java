27d5711acb97e09ada2d0b04c223bba639ca2b1a
Keagan <keaganpzh@gmail.com>
2023-08-28 16:08:20 +0800

    Level-9: add JavaDoc for FindCommand.java

--- src/main/java/rat/command/FindCommand.java
+++ src/main/java/rat/command/FindCommand.java
@@ -2,15 +2,30 @@ package rat.command;
 
 import rat.tasks.RatTaskManager;
 
+/**
+ * This class encapsulates a Find command.
+ * A find command searches the task list for tasks that contain a keyword, and prints the matching tasks to the user.
+ */
 public class FindCommand extends RatCommand {
 
+    /**
+     * The keyword to search for.
+     */
     private String keyword;
 
+    /**
+     * Constructor for a FindCommand.
+     * @param ratTaskManager The RatTaskManager object used to store and process the user's tasks.
+     * @param input The user's input.
+     */
     public FindCommand(RatTaskManager ratTaskManager, String input) {
         super(ratTaskManager);
         this.keyword = input.substring(5);
     }
 
+    /**
+     * Executes the FindCommand.
+     */
     @Override
     public void execute() {
         this.ratTaskManager.printFoundTasks(this.keyword);
