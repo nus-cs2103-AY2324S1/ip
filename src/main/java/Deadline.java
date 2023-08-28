a12b00ad26b80ed847da4e2452b90ffdf8708e29
markgcera <>
2023-08-29 00:08:54 +0800

    Complete Level 8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,21 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDate by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDate.parse(by);
     }
 
     public String getBy() {
-        return this.by;
+        return this.by.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by:" + by + ")";
+        return "[D]" + super.toString() + "(by: " + by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,20 +1,27 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 public class Event extends Task {
 
-    protected String from;
-    protected String to;
+    protected LocalDate from;
+    protected LocalDate to;
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDate.parse(from);
+        this.to = LocalDate.parse(to);
     }
 
     public String getFromTo() {
-        return from + "-" + to;
+        return from.format(DateTimeFormatter.ofPattern("MMM d yyyy"))
+                + " to "
+                + to.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(from:" + from + "to:" + to + ")";
+        return "[E]" + super.toString() + "(from:"
+                + from.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + "to:"
+                + to.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
