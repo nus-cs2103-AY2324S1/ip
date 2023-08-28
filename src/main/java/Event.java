99de383e816b680d08bebb35c5c9e4f930123814
joeng03 <yinjoeng2003@gmail.com>
2023-08-28 17:23:49 +0800

    feat: Integrate Dates and Times into Deadline and Event

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,11 +1,23 @@
-public class Event extends Task {
-    protected String from = "";
-    protected String to = "";
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    public Event(String description, String from, String to) {
+public class Event extends Task {
+    protected LocalDate from = null;
+    protected LocalDate to = null;
+
+    public Event(String description, String from, String to) throws LinusException {
         super(description);
-        this.from = from;
-        this.to = to;
+        try {
+            this.from = LocalDate.parse(from);
+            this.to = LocalDate.parse(to);
+
+            if (this.from.isAfter(this.to)) {
+                throw new LinusException("☹ OOPS!!! Please specify the start date before/on the same day as the end date.");
+            }
+        } catch (DateTimeParseException e) {
+            throw new LinusException("☹ OOPS!!! Please specify the start and/or end dates in the correct format: yyyy-mm-dd");
+        }
     }
 
     @Override
@@ -15,6 +27,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return super.toString() + " (from: " + this.from + " to: " + this.to + ")";
+        return super.toString() + " (from: " + this.from.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " to: " + this.to.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
