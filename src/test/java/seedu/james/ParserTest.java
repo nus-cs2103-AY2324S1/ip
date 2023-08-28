f6d9601c11ebb0c2b6f8ccc7efc41acfc621429c
nreHieW <limweihern@gmail.com>
2023-08-28 15:52:44 +0800

    Add JUnit tests for Parser and TaskList

---
+++ src/test/java/seedu/james/ParserTest.java
@@ -0,0 +1,53 @@
+package seedu.james;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ParserTest {
+
+    @Test
+    public void checkTaskParsing() throws JamesException {
+        String[] taskInputs = {
+                "todo read book",
+                "deadline return book /by 2019-12-01 20:00",
+                "event project meeting /from 2022-08-06 14:00 /to 2022-08-06 18:00",
+                "todo join sports club",
+                "todo borrow book"
+        };
+        String[] taskOutputs = {
+                "[T][ ] read book",
+                "[D][ ] return book (by: Dec 01 2019 20:00)",
+                 "[E][ ] project meeting (from: Aug 06 2022 14:00 to: Aug 06 2022 18:00)",
+                "[T][ ] join sports club",
+                "[T][ ] borrow book"
+        };
+
+        Parser parser = new Parser();
+
+        for (int i = 0; i < taskInputs.length; i++) {
+            Task task = parser.parseTask(taskInputs[i]);
+            assertEquals(task.toString(), taskOutputs[i]);
+        }
+
+    }
+
+    @Test
+    public void checkParsing() {
+        String[] taskInputs = {
+                "todo read book",
+                "deadline return book /by 2019-12-01 20:00",
+                "event project meeting /from 2022-08-06 14:00 /to 2022-08-06 18:00",
+                "todo join sports club",
+                "todo borrow book"
+        };
+        TaskList taskList = new TaskList();
+        Parser parser = new Parser();
+
+        for (int i = 0; i < taskInputs.length; i++) {
+            parser.parse(taskList, taskInputs[i]);
+        }
+
+        assertEquals(taskList.size(), taskInputs.length);
+    }
+}
---
+++ src/test/java/seedu/james/TaskListTest.java
@@ -0,0 +1,26 @@
+package seedu.james;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class TaskListTest {
+
+    @Test
+    public void testAddTask() {
+        ToDoTask stub = new ToDoTask("stub");
+        TaskList taskList = new TaskList();
+        for (int i = 0; i < 10; i++) {
+            taskList.addTask(stub);
+        }
+        assertEquals(taskList.size(), 10);
+    }
+
+    @Test
+    public void testTaskString() {
+        ToDoTask stub = new ToDoTask("stub");
+        TaskList taskList = new TaskList();
+        for (int i = 0; i < 5; i++) {
+            taskList.addTask(stub);
+        }
+        String expected = "1.[T][ ] stub\n2.[T][ ] stub\n3.[T][ ] stub\n4.[T][ ] stub\n5.[T][ ] stub";
+        assertEquals(taskList.toString(), expected);
+    }
+}
