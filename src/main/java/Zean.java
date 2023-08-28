ef12adc73af418d03fa429066b042ccb0f8dc68f
zhonghan721 <e0957606@u.nus.edu>
2023-08-28 15:30:26 +0800

    Add TaskList.java
    There are several methods in Storage class that should be in TaskList class.
    
    Creating the TaskList class improves the OOP aspects of the code.

--- src/main/java/Zean.java
+++ src/main/java/Zean.java
@@ -1,5 +1,6 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * The main class for the chatbot.
@@ -8,16 +9,52 @@ import java.io.IOException;
  */
 public class Zean {
 
-    public static void main(String[] args) {
-        Ui.greet("Zean");
-        try {
-            Ui.takeInstructions();
-            Ui.exit();
-        } catch (FileNotFoundException e) {
-            System.out.println("\tOOPS! Something went wrong with the file.\n\tShutting down now...");
-        } catch (IOException e) {
-            System.out.println("\t OOPS! The file cannot be created.\n\tShutting down now...");
-        }
+    private Ui ui;
+    private TaskList tasks;
+    private Storage storage;
 
+    public Zean(String filePath) {
+        this.ui = new Ui();
+        try {
+            this.storage = new Storage(filePath);
+            this.tasks = new TaskList(this.storage);
+        } catch (FileNotFoundException e) {
+            this.ui.showError("\tOOPS! Something went wrong with the file.\n\tShutting down now...");
+        } catch (IOException e) {
+            this.ui.showError("\tOOPS! The file cannot be created.\n\tShutting down now...");
+        } catch (SecurityException e) {
+            this.ui.showError("\tOOPS! The file cannot be written due to invalid access.\n\tShutting down now...");
+        } catch (DukeException e) {
+            this.ui.showError(e.getMessage());
+        }
+    }
+
+    /**
+     * Handles the input provided and outputs corresponding messages.
+     */
+    public void run() {
+        this.ui.greet("Zean");
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = sc.nextLine();
+            this.ui.printDivider();
+            if (input.equals("bye")) {
+                break;
+            }
+            try {
+                Parser.parse(input, this.tasks);
+                this.ui.printDivider();
+            } catch (DukeException e) {
+                this.ui.showError(e.getMessage());
+            }
+        }
+        sc.close();
+        this.ui.exit();
+
+    }
+
+    public static void main(String[] args) {
+        new Zean("./data/zean.txt").run();
     }
 }
