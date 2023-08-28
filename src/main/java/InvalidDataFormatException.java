231e025b5d2fdfa63b1f96e21c1cd040f29bbcdf
jordankanghm <jordankanghm@gmail.com>
2023-08-29 00:46:13 +0800

    Understand Deadlines.
    Deadlines are treated as simply strings.
    
    We need to have some form of checking the string input to ensure that a proper date and time is inputted.
    
    Let's verify that the date and time inputted by the user is valid and store it in the Deadline object as a LocalDateTime object.
    
    By doing so, we can be sure that the user has inputted a valid deadline and code is more organised.

---
+++ src/main/java/InvalidDataFormatException.java
@@ -0,0 +1,7 @@
+public class InvalidDataFormatException extends DukeException{
+    public InvalidDataFormatException() {
+        super("____________________________________________________________\n" +
+                " ☹ OOPS!!! The saved data format is invalid!\n" +
+                "____________________________________________________________");
+    }
+}
