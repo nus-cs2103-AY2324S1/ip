8c724b35154f1fc2b070b8f6cc41285e866852e6
nreHieW <limweihern@gmail.com>
2023-08-28 10:31:09 +0800

    Include DateTime parsing

--- src/main/java/EventTask.java
+++ src/main/java/EventTask.java
@@ -1,15 +1,20 @@
-public class EventTask extends Task{
-    private String startTime;
-    private String endTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public EventTask(String description, String startTime, String endTime) {
+public class EventTask extends Task{
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    public EventTask(String description, String startTimeInput, String endTimeInput) {
         super(description);
-        this.startTime = startTime;
-        this.endTime = endTime;
+        this.startTime = LocalDateTime.parse(startTimeInput, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        this.endTime = LocalDateTime.parse(endTimeInput, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.startTime + " to: " + this.endTime + ")";
+        return "[E]" + super.toString() + " (from: "
+                + this.startTime.format(DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm"))
+                + " to: " + this.endTime.format(DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm")) + ")";
     }
 }
