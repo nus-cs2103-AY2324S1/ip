cf261e1d9739bf58e362d6d3d41efeefdc46039e
Ren Jing <e0958325@u.nus.edu>
2023-08-28 12:56:42 +0800

    Add JUnit Tests
    Added simple JUnit tests for ToDo and Deadline

--- src/test/java/ren/task/ToDoTest.java
+++ src/test/java/ren/task/ToDoTest.java
@@ -7,11 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ToDoTest {
     @Test
     public void getDescription() {
-        assertEquals(2, 2);
-    }
-
-    @Test
-    public void anotherDummyTest() {
-        assertEquals(4, 4);
+        assertEquals("read book", new ToDo("read book", false).getDescription());
     }
 }
