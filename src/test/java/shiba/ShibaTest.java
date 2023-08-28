c9fb66fb6447ea05c4d0a87fcd5457a47e070205
Shang Yi Ku <kushangyi2001@gmail.com>
2023-08-28 15:02:57 +0800

    Add gradle, with junit enabled

---
+++ src/test/java/shiba/ShibaTest.java
@@ -0,0 +1,12 @@
+package shiba;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ShibaTest {
+    @Test
+    public void InitTest() {
+        assertEquals(1 + 12, 13);
+    }
+}
