7808c61bd689b68c0cecf9082c87c0ce387681b0
JiHoon <healingdog2@gmail.com>
2023-08-28 21:53:36 +0800

    A-MoreOOP
    Move method for storing and saving tasks to a file to a separate class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,11 +20,6 @@ public class Duke {
     private TaskList tasks = new TaskList();
 
     /**
-     * File path to store tasks
-     */
-    private String filePath = "./data/duke.txt";
-
-    /**
      * The main entry point of the application.
      */
     public static void main(String[] args) {
@@ -39,7 +34,7 @@ public class Duke {
     public void run() {
         ui.showWelcome();
         boolean isExit = false;
-        readTask();
+        FileManager.readTask(tasks, ui);
 
         while (!isExit) {
             try {
@@ -47,90 +42,12 @@ public class Duke {
                 ui.showLine();
                 Command command = Parser.parse(userInput);
                 isExit = command.execute(this.tasks, ui);
-                writeTask();
+                FileManager.writeTask(tasks,ui);
             } catch (DukeException e) {
                 ui.showError(e.getMessage());
             } finally {
                 ui.showLine();
             }
-        }
-    }
-
-    /**
-     * Read the task from the file.
-     */
-    public void readTask() {
-        try {
-            File directory = new File("./data");
-            File file = new File(filePath);
-
-            if (!directory.exists()) {
-                directory.mkdir();
-            }
-
-            if (!file.exists()) {
-                try {
-                    file.createNewFile();
-                } catch (IOException e) {
-                    ui.showError("Error creating new file: " + e.getMessage());
-                }
-            }
-
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                Task task;
-
-                String line = scanner.nextLine();
-                String[] parts = line.split("\\|");
-                String taskType = parts[0];
-                boolean isDone = parts[1].equals("true");
-                String description = parts[2];
-
-                switch (taskType) {
-                case "T":
-                    task = new Todo(description);
-                    break;
-                case "D":
-                    String date = parts[3];
-                    task = new Deadline(description, date);
-                    break;
-                case "E":
-                    String from = parts[3];
-                    String to = parts[4];
-                    task = new Event(description, from, to);
-                    break;
-                default:
-                    continue;
-                }
-
-                if (isDone) {
-                    task.mark();
-                }
-
-                tasks.add(task);
-            }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            ui.showError(e.getMessage());
-        } catch (DukeException e) {
-            ui.showError(e.getMessage());
-        }
-    }
-
-
-    /**
-     * Write the task to the file.
-     */
-    public void writeTask() {
-        try {
-            FileWriter fw = new FileWriter(filePath);
-            for (int i = 0; i < tasks.size(); i++) {
-                Task task = tasks.get(i);
-                fw.write(task.getRaw() + "\n");
-            }
-            fw.close();
-        } catch (IOException e) {
-            ui.showError("An error occurred while writing to file.");
         }
     }
 }
---
+++ src/main/java/FileManager.java
@@ -0,0 +1,90 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * FileManager that takes care of reading and writing takes to a file.
+ */
+public  class FileManager {
+    private static String filePath = "./data/duke.txt";
+    
+    /**
+     * Read the task from the file.
+     */
+    public static void readTask(TaskList tasks, Ui ui) {
+        try {
+            File directory = new File("./data");
+            File file = new File(filePath);
+
+            if (!directory.exists()) {
+                directory.mkdir();
+            }
+
+            if (!file.exists()) {
+                try {
+                    file.createNewFile();
+                } catch (IOException e) {
+                    ui.showError("Error creating new file: " + e.getMessage());
+                }
+            }
+
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                Task task;
+
+                String line = scanner.nextLine();
+                String[] parts = line.split("\\|");
+                String taskType = parts[0];
+                boolean isDone = parts[1].equals("true");
+                String description = parts[2];
+
+                switch (taskType) {
+                case "T":
+                    task = new Todo(description);
+                    break;
+                case "D":
+                    String date = parts[3];
+                    task = new Deadline(description, date);
+                    break;
+                case "E":
+                    String from = parts[3];
+                    String to = parts[4];
+                    task = new Event(description, from, to);
+                    break;
+                default:
+                    continue;
+                }
+
+                if (isDone) {
+                    task.mark();
+                }
+
+                tasks.add(task);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            ui.showError(e.getMessage());
+        } catch (DukeException e) {
+            ui.showError(e.getMessage());
+        }
+    }
+
+
+    /**
+     * Write the task to the file.
+     */
+    public static void writeTask(TaskList tasks, Ui ui) {
+        try {
+            FileWriter fw = new FileWriter(filePath);
+            for (int i = 0; i < tasks.size(); i++) {
+                Task task = tasks.get(i);
+                fw.write(task.getRaw() + "\n");
+            }
+            fw.close();
+        } catch (IOException e) {
+            ui.showError("An error occurred while writing to file.");
+        }
+    }
+}
