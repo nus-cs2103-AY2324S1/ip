e3eee20de7828887e124e1dfae5476e22b1864f6
newway1814 <e1088145@u.nus.edu>
2023-08-29 01:07:03 +0800

    Completed Level-7

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,49 +1,51 @@
- public class Task {
-        protected String description;
-        protected boolean isDone;
+public class Task {
+    protected String description;
+    protected boolean isDone;
 
-        public Task(String description) {
-            this.description = description.strip();
-            this.isDone = false;
-        }
+    public Task(String description) {
+        this.description = description.strip();
+        this.isDone = false;
+    }
 
-        public Task() {
+    public Task() {
 
-        }
+    }
 
-        public String doneMessage() {
-            return "Nice! I've marked this task as done:";
-        }
+    public String doneMessage() {
+        return "Nice! I've marked this task as done:";
+    }
 
-        public String undoneMessage() {
-            return "OK, I've marked this task as not done yet:";
-        }
+    public String undoneMessage() {
+        return "OK, I've marked this task as not done yet:";
+    }
 
-        public String getStatusIcon() {
-            return (isDone ? "X" : " "); // mark done task with X
-        }
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
 
-        public String Box() {
-            return "[" + getStatusIcon() +"] ";
-        }
+    public String Box() {
+        return "[" + getStatusIcon() +"] ";
+    }
 
-        public void markAsDone() {
-            isDone = true;
-        }
-        public int getIndexOfMark(String str) {
-            return Integer.parseInt(str.substring(5));
-        }
+    public void markAsDone() {
+        isDone = true;
+    }
 
-        public int getIndexOfUnmark(String str) {
-            return Integer.parseInt(str.substring(7));
-        }
 
-        public void markUndone() {
-            isDone = false;
-        }
+    public void markUndone() {
+        isDone = false;
+    }
 
-     @Override
-     public String toString() {
-         return "[ ]" + this.Box() + this.description;
-     }
- }
+    @Override
+    public String toString() {
+        return "[ ]" + this.Box() + this.description;
+    }
+
+    public String type() {
+        return "";
+    }
+
+    public String getBy() {
+        return "";
+    }
+}
