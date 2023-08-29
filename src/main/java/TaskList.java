ea5f16076e90e9241766b9ceedc225b056bbdcac
Kailash201 <109354852+Kailash201@users.noreply.github.com>
2023-08-29 19:30:54 +0800

    Add more OOP classes

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    ArrayList<Task> tasks;
+
+    public TaskList(ArrayList<Task> tasks){
+        this.tasks = tasks;
+    }
+
+    public Task remTask(int n) {
+        return tasks.remove(n);
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public boolean isTaskListEmpty() {
+        return tasks.isEmpty();
+    }
+
+    public Task getTask (int n) {
+        return tasks.get(n);
+    }
+
+    public int getSize() {
+        return tasks.size();
+    }
+}
