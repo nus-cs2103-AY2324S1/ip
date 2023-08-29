f24cfbb0e73beed174484bc429ba8bb715ae9526
Woo Jia Hao <woojiahao1234@gmail.com>
2023-08-29 20:02:08 +0800

    Add test for option parsing

--- src/test/java/cyrus/parser/ParserTest.java
+++ src/test/java/cyrus/parser/ParserTest.java
@@ -3,27 +3,44 @@ package cyrus.parser;
 import cyrus.commands.CommandType;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ParserTest {
   private static final Parser parser = new Parser();
 
   @Test
   public void testParseWithEmptyLine() {
-    assertEquals(parser.parse(""), ParseInfo.EMPTY);
-    assertEquals(parser.parse("  "), ParseInfo.EMPTY);
+    assertEquals(ParseInfo.EMPTY, parser.parse(""));
+    assertEquals(ParseInfo.EMPTY, parser.parse("  "));
   }
 
   @Test
   public void testParseWithUnknownCommand() {
-    assertEquals(parser.parse("unknown hi this is unknown").commandType, CommandType.UNKNOWN);
+    assertEquals(CommandType.UNKNOWN, parser.parse("unknown hi this is unknown").commandType);
   }
 
   @Test
   public void testParseWithNoArgument() {
     assertEquals(
-        parser.parse("todo /option1 this /other other option!").getArgument(),
-        ""
+        "",
+        parser.parse("todo /option1 this /other other option!").getArgument()
     );
+  }
+
+  @Test
+  public void testParseWithOptions() {
+    HashMap<String, String> options =
+        parser.parse("todo /a this is a /b b /c hello! / this should be together").getOptions();
+    HashMap<String, String> expected = new HashMap<>();
+    expected.put("a", "this is a");
+    expected.put("b", "b");
+    expected.put("c", "hello! / this should be together");
+    for (var entry : expected.entrySet()) {
+      assertTrue(options.containsKey(entry.getKey()));
+      assertEquals(entry.getValue(), options.getOrDefault(entry.getKey(), "invalid"));
+    }
   }
 }
