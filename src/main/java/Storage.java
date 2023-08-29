9d73b8c061fcd574de8652dd1221c05275dd7de1
AlyssaPng <e0959598@u.nus.edu>
2023-08-30 00:20:23 +0800

    Add more classes for more OOP

---
+++ src/main/java/Storage.java
@@ -0,0 +1,105 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * The Storage class is responsible for loading tasks from the file and saving tasks in the file.
+ */
+public class Storage {
+
+    protected String filepath;
+
+    /**
+     * Constructor for Storage class.
+     *
+     * @param filepath The file path where the tasks are stored.
+     */
+    public Storage (String filepath) {
+        this.filepath = filepath;
+    }
+
+    /**
+     * Loads tasks from the file using the file path.
+     *
+     * @return An ArrayList of tasks loaded from the file.
+     * @throws DukeException If there is an issue loading tasks or parsing the file.
+     */
+    public ArrayList<Task> load() throws DukeException {
+        ArrayList<String> dataArray = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
+
+        try {
+            File file = new File(this.filepath);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String input = scanner.nextLine();
+                dataArray.add(input);
+            }
+
+            scanner.close();
+            String[] data = dataArray.toArray(new String[dataArray.size()]);
+
+            for (String item : data) {
+                taskList.add(loadData(item));
+            }
+        } catch (FileNotFoundException e) {
+            throw new DukeException("File Not Found: " + this.filepath);
+        }
+
+        return taskList;
+    }
+
+    /**
+     * Creates a Task from the string representation from the file.
+     *
+     * @param dataInput The string representation of the task.
+     * @return The created Task object.
+     * @throws DukeException If the string representation does not meet task requirements.
+     */
+    private static Task loadData(String dataInput) throws DukeException {
+        Task newTask;
+        String[] arr = dataInput.split("\\|");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].trim();
+        }
+
+
+        if (arr[0].equals("T")) {
+            if (arr.length < 3 || arr[2].isEmpty()) {
+                throw new EmptyTaskException("todo");
+            }
+
+            newTask = new ToDo(arr[2]);
+        } else if (arr[0].equals("D")) {
+            if (arr.length < 3 || arr[2].isEmpty()) {
+                throw new EmptyTaskException("deadline");
+            }
+            if (arr.length != 4|| arr[3].isEmpty()) {
+                throw new EmptyDateException("deadline");
+            }
+
+            newTask = new Deadline(arr[2], arr[3]);
+        } else {
+            if (arr.length < 3 || arr[2].isEmpty()) {
+                throw new EmptyTaskException("event");
+            }
+            if (arr.length < 4 || arr[3].isEmpty()) {
+                throw new EmptyDateException("event");
+            }
+            if (arr.length != 5 || arr[4].isEmpty()) {
+                throw new NoEndDateException();
+            }
+
+            newTask = new Event(arr[2], arr[3], arr[4]);
+        }
+
+        // mark as done
+        if (arr[1].equals("1")) {
+            newTask.markAsDone();
+        }
+
+        return newTask;
+    }
+}
---
+++ src/main/java/Ui.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+/**
+ * UI is a class that handles interactions with the user.
+ * It is responsible for displaying messages, and reading user input.
+ */
+public class Ui {
+    /**
+     * Constructor for UI class
+     */
+    public Ui() {
+
+    }
+
+    /**
+     * Display Welcome Message.
+     */
+    public void showWelcome() {
+        System.out.println("Hello! I'm Chatty\n" + "What can I do for you?");
+    }
+
+    /**
+     * Displays a given message to the user.
+     *
+     * @param msg The message to be displayed.
+     */
+    public void showMessage(String msg) {
+        System.out.println(msg);
+    }
+
+    /**
+     * Reads user's input.
+     *
+     * @return The user's input as a String.
+     */
+    public String readCommand() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        return userInput;
+    }
+
+    /**
+     * Displays an error message indicating that loading a file has failed.
+     */
+    public void showLoadingError() {
+        System.out.println("File not found");
+    }
+
+    /**
+     * Displays the list of tasks.
+     *
+     * @param tasks The current TaskList containing tasks to display.
+     */
+    public void showList(TaskList tasks) {
+        System.out.println(tasks.toString());
+    }
+
+    /**
+     * Displays a goodbye message to the user.
+     */
+    public void showBye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
