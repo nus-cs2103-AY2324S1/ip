3bfb23b4b804f71121bcf787d89f88aa38854611
chew01 <chewjh19@gmail.com>
2023-08-29 16:32:51 +0800

    Add save and load to disk methods to Storage class

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,4 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Storage {
     private final ArrayList<Task> tasks;
@@ -51,5 +56,55 @@ public class Storage {
             }
         }
         return builder.toString();
+    }
+
+    public void save() {
+        try {
+            FileWriter fw = new FileWriter("tasks.txt");
+            StringBuilder text = new StringBuilder();
+            for (Task t : tasks) {
+                text.append(String.format("%s\n", t.getSaveFormat()));
+            }
+            fw.write(text.toString());
+            fw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void load() {
+        try {
+            File f = new File("tasks.txt");
+            Scanner s = new Scanner(f);
+            while (s.hasNext()) {
+                String[] line = s.nextLine().trim().split(" \\| ");
+                boolean taskIsDone = line[1].equals("1");
+                switch (line[0]) {
+                    case "T":
+                        TodoTask newTodo = new TodoTask(line[2]);
+                        if (taskIsDone) {
+                            newTodo.complete();
+                        }
+                        this.tasks.add(newTodo);
+                        break;
+                    case "D":
+                        DeadlineTask newDeadline = new DeadlineTask(line[2], line[3]);
+                        if (taskIsDone) {
+                            newDeadline.complete();
+                        }
+                        this.tasks.add(newDeadline);
+                        break;
+                    case "E":
+                        EventTask newEvent = new EventTask(line[2], line[3], line[4]);
+                        if (taskIsDone) {
+                            newEvent.complete();
+                        }
+                        this.tasks.add(newEvent);
+                        break;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Task file not created yet.");
+        }
     }
 }
