36ba877bffa1b57ecd77088c1af88d1fdeee3edc
YangHeng Tang <yanghengtang@gmail.com>
2023-08-30 05:02:05 +0800

    Add UnknownCommand.java

---
+++ src/main/java/duke/command/UnknownCommand.java
@@ -0,0 +1,16 @@
+package duke.command;
+
+import duke.util.Storage;
+import duke.util.TaskList;
+import duke.util.Ui;
+
+public class UnknownCommand extends Command {
+    public UnknownCommand() {
+        super(false);
+    }
+
+    @Override
+    public void execute(TaskList list, Ui ui, Storage storage) {
+        ui.printCommandNotFound();
+    }
+}
