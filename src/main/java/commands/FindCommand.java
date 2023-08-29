52e48f7112211164bc7c6565f1e0844b388c6619
lamchenghou <lamchenghouu@gmail.com>
2023-08-29 12:44:58 +0800

    Add FindCommand.java

---
+++ src/main/java/commands/FindCommand.java
@@ -0,0 +1,34 @@
+package commands;
+
+import java.util.ArrayList;
+
+import errors.DotException;
+import tasks.TaskList;
+import ui.Ui;
+
+/**
+ * FindCommand allows the user to find tasks based on a query.
+ */
+public class FindCommand extends Command {
+
+    private final String query;
+
+    private final TaskList dotTaskList;
+
+    /**
+     * Constructor for FindCommand.
+     *
+     * @param query This is the query for the search.
+     * @param dotTaskList This is the TaskList to search from.
+     */
+    public FindCommand(String query, TaskList dotTaskList) {
+        this.query = query;
+        this.dotTaskList = dotTaskList;
+    }
+
+    @Override
+    public void execute() throws DotException {
+        ArrayList<String> queriedTasks = dotTaskList.getDisplayForQueriedTasks(this.query);
+        Ui.displayArrayList(queriedTasks);
+    }
+}
