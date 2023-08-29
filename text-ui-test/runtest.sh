9fa4fd57ea43191fb75dadf4c048f9d71868643d
Yu Han Zhang <yuhan@zhyuhan.me>
2023-08-29 22:01:25 +0800

    test: update expected output and test script

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,14 +13,20 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/**/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
+# remove existing duke cache
+if [ -e  ".duke-cache" ]
+then
+  rm .duke-cache
+fi
+
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin duke/Duke < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
