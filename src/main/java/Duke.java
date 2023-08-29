12650f1b8a2b0f75bc4f72e45906aaf8900235e5
MagnificentCreature <E0960127@u.nus.edu>
2023-08-30 01:00:48 +0800

    Formatting changes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -42,68 +42,68 @@ public class Duke {
             try {
                 Commands command = Commands.parseCommand(input);
                 switch (command) {
-                    case LIST:
-                        for (int i = 0; i < taskList.size(); i++) {
-                            printWithTab(tab + (i + 1) + ". " + taskList.get(i));
-                        }
+                case LIST:
+                    for (int i = 0; i < taskList.size(); i++) {
+                        printWithTab(tab + (i + 1) + ". " + taskList.get(i));
+                    }
+                    break;
+                case MARK:
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    if (index >= taskList.size() || index < 0) {
+                        printWithTab("Invalid index!");
                         break;
-                    case MARK:
-                        int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                        if (index >= taskList.size() || index < 0) {
-                            printWithTab("Invalid index!");
-                            break;
-                        }
-                        taskList.get(index).markAsDone();
-                        printWithTab("Nice! I've marked this task as done:");
-                        printWithTab(tab + taskList.get(index).toString());
+                    }
+                    taskList.get(index).markAsDone();
+                    printWithTab("Nice! I've marked this task as done:");
+                    printWithTab(tab + taskList.get(index).toString());
+                    break;
+                case UNMARK:
+                    index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    if (index >= taskList.size() || index < 0) {
+                        printWithTab("Invalid index!");
                         break;
-                    case UNMARK:
-                        index = Integer.parseInt(input.split(" ")[1]) - 1;
-                        if (index >= taskList.size() || index < 0) {
-                            printWithTab("Invalid index!");
-                            break;
-                        }
-                        taskList.get(index).markAsUndone();
-                        printWithTab("Ok! I've marked this task as not done yet:");
-                        printWithTab(tab + taskList.get(index).toString());
+                    }
+                    taskList.get(index).markAsUndone();
+                    printWithTab("Ok! I've marked this task as not done yet:");
+                    printWithTab(tab + taskList.get(index).toString());
+                    break;
+                case TODO:
+                    taskList.add(new Todo(Commands.extractTaskDescription(input)));
+                    printWithTab("Got it. I've added this task:");
+                    printWithTab(tab + taskList.get(taskList.size() - 1));
+                    printWithTab("Now you have " + taskList.size() + " tasks in the list.");
+                    break;
+                case DEADLINE:
+                    Deadline deadline = new Deadline(Commands.extractTaskDescription(input),
+                            Commands.extractDeadline(input));
+                    taskList.add(deadline);
+                    printWithTab("Got it. I've added this task:");
+                    printWithTab(tab + deadline);
+                    printWithTab("Now you have " + taskList.size() + " tasks in the list.");
+                    break;
+                case EVENT:
+                    Event event = new Event(Commands.extractTaskDescription(input),
+                            Commands.extractEventFrom(input), Commands.extractEventTo(input));
+                    taskList.add(event);
+                    printWithTab("Got it. I've added this task:");
+                    printWithTab(tab + event);
+                    printWithTab("Now you have " + taskList.size() + " tasks in the list.");
+                    break;
+                case DELETE:
+                    index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    if (index >= taskList.size() || index < 0) {
+                        printWithTab("Invalid index!");
                         break;
-                    case TODO:
-                        taskList.add(new Todo(Commands.extractTaskDescription(input)));
-                        printWithTab("Got it. I've added this task:");
-                        printWithTab(tab + taskList.get(taskList.size() - 1));
-                        printWithTab("Now you have " + taskList.size() + " tasks in the list.");
-                        break;
-                    case DEADLINE:
-                        Deadline deadline = new Deadline(Commands.extractTaskDescription(input),
-                                Commands.extractDeadline(input));
-                        taskList.add(deadline);
-                        printWithTab("Got it. I've added this task:");
-                        printWithTab(tab + deadline);
-                        printWithTab("Now you have " + taskList.size() + " tasks in the list.");
-                        break;
-                    case EVENT:
-                        Event event = new Event(Commands.extractTaskDescription(input),
-                                Commands.extractEventFrom(input), Commands.extractEventTo(input));
-                        taskList.add(event);
-                        printWithTab("Got it. I've added this task:");
-                        printWithTab(tab + event);
-                        printWithTab("Now you have " + taskList.size() + " tasks in the list.");
-                        break;
-                    case DELETE:
-                        index = Integer.parseInt(input.split(" ")[1]) - 1;
-                        if (index >= taskList.size() || index < 0) {
-                            printWithTab("Invalid index!");
-                            break;
-                        }
-                        Task task = taskList.get(index);
-                        taskList.remove(index);
-                        printWithTab("Noted. I've removed this task:");
-                        printWithTab(tab + task.toString()); 
-                        printWithTab("Now you have " + taskList.size() + " tasks in the list.");
-                        break;
-                    default:
-                        printWithTab("Unknown command!");
-                        break;
+                    }
+                    Task task = taskList.get(index);
+                    taskList.remove(index);
+                    printWithTab("Noted. I've removed this task:");
+                    printWithTab(tab + task.toString());
+                    printWithTab("Now you have " + taskList.size() + " tasks in the list.");
+                    break;
+                default:
+                    printWithTab("Unknown command!");
+                    break;
                 }
             } catch (UnknownCommandException | MissingDescriptionException | InvalidParametersException e) {
                 printWithTab(e.getMessage());
