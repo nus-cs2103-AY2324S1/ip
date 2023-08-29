9e0fc965d80364a6bc48b971dbaede702d42944f
Elgin Tan <ElginTZM@u.nus.edu>
2023-08-30 00:13:50 +0800

    Task Class: Add getDataString method

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,4 +1,5 @@
 public class Deadline extends Task {
+    private static String SYMBOL = "D";
     protected String dueDatetime;
 
     public Deadline(String description, String dueDatetime) {
@@ -7,7 +8,12 @@ public class Deadline extends Task {
     }
 
     @Override
+    public String getDataString() {
+        return String.join(" | ", Deadline.SYMBOL, super.isDone ? "1" : "0", super.getDescription(), this.dueDatetime);
+    }
+
+    @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), this.dueDatetime);
+        return String.format("[%s]%s (by: %s)", Deadline.SYMBOL, super.toString(), this.dueDatetime);
     }
 }
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,11 +1,17 @@
 public class Todo extends Task {
+    private static String SYMBOL = "T";
     
     public Todo (String description) {
         super(description);
     }
 
     @Override
+    public String getDataString() {
+        return String.join(" | ", Todo.SYMBOL, super.isDone ? "1" : "0", super.getDescription());
+    }
+
+    @Override
     public String toString() {
-        return String.format("[T]%s", super.toString());
+        return String.format("[%s]%s", Todo.SYMBOL, super.toString());
     }
 }
