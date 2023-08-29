a2032b6fe522744c125a48195e313305fd725703
Woo Jia Hao <woojiahao1234@gmail.com>
2023-08-29 19:51:12 +0800

    Add unit tests for parse input

--- src/test/java/cyrus/parser/ParserTest.java
+++ src/test/java/cyrus/parser/ParserTest.java
@@ -1,14 +1,29 @@
 package cyrus.parser;
 
-import org.junit.jupiter.api.Assertions;
+import cyrus.commands.CommandType;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserTest {
   private static final Parser parser = new Parser();
 
   @Test
   public void testParseWithEmptyLine() {
-    Assertions.assertEquals(parser.parse(""), ParseInfo.EMPTY);
-    Assertions.assertEquals(parser.parse("  "), ParseInfo.EMPTY);
+    assertEquals(parser.parse(""), ParseInfo.EMPTY);
+    assertEquals(parser.parse("  "), ParseInfo.EMPTY);
+  }
+
+  @Test
+  public void testParseWithUnknownCommand() {
+    assertEquals(parser.parse("unknown hi this is unknown").commandType, CommandType.UNKNOWN);
+  }
+
+  @Test
+  public void testParseWithNoArgument() {
+    assertEquals(
+        parser.parse("todo /option1 this /other other option!").getArgument(),
+        ""
+    );
   }
 }
