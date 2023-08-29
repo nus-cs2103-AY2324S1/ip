b87f0429315ba46a415668df0fe79ff911165a14
Gabriel4357 <gabrielseethor.jh@gmail.com>
2023-08-29 20:38:29 +0800

    Add JUnit Tests
    No testings done to test the methods and classes
    
    Having automated test cases makes it easier to spot
    bugs when the code changes.
    
    Let's,
    *add JUnit tests: ParserTest,
    TaskTest and TaskListTest

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ParserTest {
 
     @Test
-    public void wrongDate() {
+    public void wrongDateDeadLine() {
         try {
             Parser.parse("deadline dance /by 12-12-2001-T0800", new UI("Alfred"), new TaskList(),
                     new Storage("src/main/java/duke/data/duke.txt"));
@@ -18,12 +18,45 @@ public class ParserTest {
     }
 
     @Test
-    public void correctDate() throws DukeException {
+    public void correctDateDeadLine() throws DukeException {
         TaskList tasks = new TaskList();
         Parser.parse("deadline dance /by 2001-12-12T09:00:00", new UI("Alfred"), tasks ,
                 new Storage("src/main/java/duke/data/duke.txt"));
 
         assertEquals("dance (by: DECEMBER 12 2001, 09:00)", tasks.get(tasks.size() - 1).toString());
 
+    }
+
+    @Test
+    public void wrongCommand() {
+        try {
+            String wrongCommand = "jhfjd";
+            Parser.parse(wrongCommand, new UI("John"), new TaskList(),
+                    new Storage("src/main/java/duke/data/duke.txt"));
+        } catch (DukeException e){
+            assertEquals("I apologise, sir. But I do not understand what you mean.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void addEmptyTodoDescription(){
+        try {
+            Parser.parse("todo", new UI("John"), new TaskList(),
+                    new Storage("src/main/java/duke/data/duke.txt"));
+        } catch (DukeException e){
+            assertEquals("I apologise, sir. " +
+                    "But the description of todo cannot be empty", e.getMessage());
+        }
+    }
+
+    @Test
+    public void addEmptyDeadLineDescription() {
+        try {
+            Parser.parse("deadline", new UI("John"), new TaskList(),
+                    new Storage("src/main/java/duke/data/duke.txt"));
+        } catch (DukeException e){
+            assertEquals("I apologise, sir. " +
+                    "But the description and deadline cannot be empty", e.getMessage());
+        }
     }
 }
