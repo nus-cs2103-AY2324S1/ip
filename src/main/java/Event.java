c2cbcba29a804e6bcecfb51ed337837fcda995de
nicholascher <nicholascherkwokhui@gmail.com>
2023-08-29 14:55:19 +0800

    Update Date and Time to use LocalDate

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,9 +1,14 @@
+import java.io.IOException;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
-public class Event extends Task{
-    protected String from;
-    protected String to;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String description, String from, String to) {
+public class Event extends Task{
+    protected LocalDate from;
+    protected LocalDate to;
+
+    public Event(String description, LocalDate from, LocalDate to) {
         super(description);
         this.from = from;
         this.to = to;
@@ -11,7 +16,9 @@ public class Event extends Task{
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(from: " + from + "to: " + to + ")";
+        return "[E]" + super.toString() +
+                "(from: " + from.format(DateTimeFormatter.ofPattern("MMM d yyyy"))
+                + " to: " + to.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
     public String toStringFile() {
         return "E | " + super.toStringFile() + "/from " + from + "/to " + to;
@@ -27,20 +34,35 @@ public class Event extends Task{
             throw new DukeException("☹ OOPS!!! Please provide a valid start and end date");
         }
 
-        Event newTask = new Event(event[0], event[1], event[2]);
+        try {
+            LocalDate start = LocalDate.parse(event[1].stripTrailing());
+            LocalDate end = LocalDate.parse(event[2].stripTrailing());
+            if (start.isAfter(end)) {
+                throw new DukeException("☹ OOPS!!! Your start date has to be before your end date!");
+            }
+            Event newTask = new Event(event[0], start, end);
 
-        list.add(newTask);
-        System.out.println(line);
-        System.out.println("Got it. I've added the Event:\n\t" + newTask.toString());
-        System.out.println("Now you have " + list.size() + " tasks in the list.");
-        System.out.println(line);
+            list.add(newTask);
+            System.out.println(line);
+            System.out.println("Got it. I've added the Event:\n\t" + newTask.toString());
+            System.out.println("Now you have " + list.size() + " tasks in the list.");
+            System.out.println(line);
+        } catch (DateTimeParseException e) {
+            System.out.println("Your date should be formatted as YYYY-MM-DD");
+        }
+
 
     }
     public static void addSavedEvent(String description, ArrayList<Task> list, String isMarked) {
         String[] event = description.stripTrailing().split("/from |/to ");
-        Event newTask = new Event(event[0], event[1], event[2]);
-        list.add(newTask);
-        newTask.markFromRead(isMarked);
+        try {
+            Event newTask = new Event(event[0], LocalDate.parse(event[1]), LocalDate.parse(event[2]));
+            list.add(newTask);
+            newTask.markFromRead(isMarked);
+        } catch (DateTimeParseException e) {
+            System.out.println("Please enter a valid Date!");
+        }
+
     }
 
 }
