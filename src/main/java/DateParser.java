73b24673b73db15466a14611654b7f263d14cf17
Li <e0985877@u.nus.edu>
2023-08-30 00:42:10 +0800

    Implement Level8.Dates and Times

---
+++ src/main/java/DateParser.java
@@ -0,0 +1,22 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateParser {
+    public static LocalDateTime convertStringToDateTime(String datetime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        try {
+            return LocalDateTime.parse(datetime, formatter);
+        } catch (DateTimeParseException e) {
+            e.printStackTrace();
+            System.out.println("Invalid date format. Please enter in the format 2000-01-01 01:01");
+        }
+        return null;
+    }
+
+
+    public static String convertDateTimeToString(LocalDateTime datetime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        return datetime.format(formatter);
+    }
+}
--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,20 @@
+import java.time.LocalDateTime;
+
 public class Deadline extends Task{
-    private String by;
+    private LocalDateTime by;
 
     public Deadline(String name, String by) {
         super(name);
-        this.by = by;
+        this.by = DateParser.convertStringToDateTime(by);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.by + ")";
+        return "[D]" + super.toString() + " (by: " + DateParser.convertDateTimeToString(this.by) + ")";
     }
 
     public String convertTaskToString() {
-        return "D | " + (super.isDone() ? "1" : "0") + " | " + super.getName() + " | " + this.by;
+        return "D | " + (super.isDone() ? "1" : "0") + " | " + super.getName() + " | "
+                + DateParser.convertDateTimeToString(this.by) ;
     }
 }
