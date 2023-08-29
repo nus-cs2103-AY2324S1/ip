52914d9bed1cefd65a2abb9c28ae5b1e49f81d30
KamJiaYue <e0960550@u.nus.edu>
2023-08-29 21:57:56 +0800

    Correct coding style

--- src/main/java/seedu/duke/Task.java
+++ src/main/java/seedu/duke/Task.java
@@ -26,14 +26,14 @@ public abstract class Task {
 
     public void mark() {
         this.setDone();
-        System.out.println("     Nice! I've marked this task as done:");
-        System.out.println("       " + this);
+        System.out.println(Ui.i5 + "Nice! I've marked this task as done:");
+        System.out.println(Ui.i7 + this);
     }
 
     public void unmark() {
         this.setUndone();
-        System.out.println("     OK, I've marked this task as not done yet:");
-        System.out.println("       " + this);
+        System.out.println(Ui.i5 + "OK, I've marked this task as not done yet:");
+        System.out.println(Ui.i7 + this);
     }
 
     public String toStringForSave() {
