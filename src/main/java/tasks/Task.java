b373ed6eb90137e955a86cb3feda765daf0cd935
Chandan8186 <chandananandababu@gmail.com>
2023-08-29 23:50:22 +0800

    Add more javadocs
    - Add more javadocs

--- src/main/java/tasks/Task.java
+++ src/main/java/tasks/Task.java
@@ -5,30 +5,60 @@ package tasks;
  * All specific tasks will be its child classes.
  */
 public abstract class Task {
+
+    /** The task description */
     protected String desc;
+
+    /** Boolean indicating completion status */
     protected boolean isDone;
 
+    /** Method signature for abstract method getTextFormattedString */
     protected abstract String getTextFormattedString();
 
+    /**
+     * Constructor, initializes task description.
+     *
+     * @param desc Task Description.
+     */
     public Task(String desc) {
         this.desc = desc;
         this.isDone = false;
     }
 
+    /**
+     * Returns string representation of completion status.
+     *
+     * @return String representation of completion status.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
 
+    /**
+     * Marks task as done.
+     *
+     * @return True.
+     */
     protected boolean markAsDone() {
         this.isDone = true;
         return true;
     }
 
+    /**
+     * Marks task as not done.
+     *
+     * @return True.
+     */
     protected boolean unmark() {
         this.isDone = false;
         return true;
     }
 
+    /**
+     * Returns string representation of task.
+     *
+     * @return String representation of task.
+     */
     @Override
     public String toString() {
         String output = String.format("[%s] %s", getStatusIcon(), this.desc);
