79f541a6dc863df5d114ecc9be8b0bf650e9e787
tiif <pekyuan@gmail.com>
2023-08-29 23:21:35 +0800

    convert string to date object

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,10 +1,16 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
     protected String by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        //assume user always input date first, only consider with or without time
+        String parsed_by[] = by.split("\\s+");
+        String byDate = LocalDate.parse(parsed_by[0]).format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        this.by = parsed_by.length > 1 ? byDate + " " + parsed_by[1] : byDate;
     }
 
     @Override
