4a93413f9681f1b62787cf0f1e24daed1603703a
elhy1999 <ernestliu64@gmail.com>
2023-08-29 21:59:33 +0800

    Modify Deadline and Event classes to parse and store dates

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,12 +1,15 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    String startDatetime;
-    String endDatetime;
+    LocalDate startDatetime;
+    LocalDate endDatetime;
 
     Event(String name, String startDatetime, String endDatetime) {
         super(name);
-        this.startDatetime = startDatetime;
-        this.endDatetime = endDatetime;
+        this.startDatetime = LocalDate.parse(startDatetime);
+        this.endDatetime = LocalDate.parse(endDatetime);
     }
 
     @Override
@@ -15,17 +18,15 @@ public class Event extends Task {
     }
 
     String getStartDatetime() {
-        return this.startDatetime;
+        return this.startDatetime.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 
     String getEndDatetime() {
-        return this.endDatetime;
+        return this.endDatetime.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 
     @Override
     public String toString() {
         return "[E][" + (this.isDone() ? 'X' : ' ') + "] " + this.name + " (from: " + this.getStartDatetime() + " to: " + getEndDatetime() + ")";
     }
-
-
 }
