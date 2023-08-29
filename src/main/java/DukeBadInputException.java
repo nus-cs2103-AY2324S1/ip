0d79929379b3a4810528e996243f320b5cabb490
mingyuanc <cmingyuan123@gmail.com>
2023-08-29 17:44:41 +0800

    update name

--- src/main/java/DukeBadInputException.java
+++ src/main/java/DukeBadInputException.java
@@ -1,13 +1,13 @@
 /**
  * Signals when a user input is unable to be parsed or out of bound
  */
-public class BadInputException extends Exception {
+public class DukeBadInputException extends Exception {
     /**
      * Constructs an BadInputException with the specified detail message.
      *
      * @param e - description of the bad input
      */
-    public BadInputException(String e) {
+    public DukeBadInputException(String e) {
         super(e);
     }
 }
