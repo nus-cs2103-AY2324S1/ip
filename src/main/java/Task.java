f3a868d0274eb1decbfae1f6fa5bb35112e0369b
SynapseProgramming <roald.ong@gmail.com>
2023-08-29 09:02:14 +0800

    change done to boolean object type

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -14,7 +14,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 public class Task {
 
   private String description;
-  private boolean done;
+  private Boolean done;
 
   public Task(String desc) {
     this.description = desc;
