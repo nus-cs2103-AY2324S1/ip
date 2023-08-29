d85a274e6604db411782b2042e5bf4508e918862
Lingxi Xing <thelastoreointhejar@gmail.com>
2023-08-30 00:23:58 +0800

    feat: Add Parser class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Duke {
 
     // The task list is used to store the user's tasks.
-    private final TaskList list = new TaskList();
+    private final TaskList taskList = new TaskList();
 
     private final Storage storage;
 
@@ -21,48 +21,6 @@ public class Duke {
         new Duke("data/tasks.txt").run();
     }
 
-    /**
-     * The command enum is used to store the valid commands that LilBro can
-     * understand.
-     */
-    private static enum Command {
-        EXIT("bye"),
-        LIST_TASKS("list"),
-        ADD_TODO("todo"),
-        ADD_DEADLINE("deadline"),
-        ADD_EVENT("event"),
-        MARK_TASK("mark"),
-        UNMARK_TASK("unmark"),
-        DELETE_TASK("delete");
-
-        // The string representation of the command.
-        private final String value;
-
-        /**
-         * The constructor is used to create a new command with the given value.
-         * 
-         * @param value The string representation of the command.
-         */
-        private Command(String value) {
-            this.value = value;
-        }
-
-        /**
-         * The fromString method is used to get the command from the given string.
-         * 
-         * @param value The string representation of the command.
-         * @return The command if it exists, null otherwise.
-         */
-        public static Command fromString(String value) {
-            for (Command command : Command.values()) {
-                if (command.value.equalsIgnoreCase(value)) {
-                    return command;
-                }
-            }
-            return null;
-        }
-    }
-
     public Duke(String path) {
         Ui.greet();
         Ui.println("Checking for a save file...");
@@ -70,7 +28,7 @@ public class Duke {
         String fullPath = Path.of(projectRoot, path).toString();
         this.storage = new Storage(fullPath);
         try {
-            storage.load(this.list);
+            storage.load(this.taskList);
         } catch (DukeException e) {
             Ui.println(e.getMessage());
         }
@@ -79,15 +37,15 @@ public class Duke {
 
     public void run() {
         Scanner scanner = new Scanner(System.in);
+        Parser parser = new Parser(this.taskList);
         while (true) {
             String input = scanner.nextLine();
-            String[] parts = input.split(" ", 2);
-            Command command = Command.fromString(parts[0]);
-            String commandArgs = parts.length > 1 ? parts[1].trim() : "";
             try {
-                if (!this.executeCommand(command, commandArgs)) {
+                Command command = parser.parse(input);
+                if (command.getCommandType() == CommandType.EXIT) {
                     break;
                 }
+                command.execute();
             } catch (DukeException e) {
                 Ui.println(e.getMessage());
             }
@@ -95,75 +53,7 @@ public class Duke {
 
         scanner.close();
         Ui.println("Before you go, let me save your tasks...");
-        storage.save(this.list);
+        storage.save(this.taskList);
         Ui.bye();
     }
-
-    /**
-     * Executes the given command with the given arguments.
-     * 
-     * @param command The command to execute.
-     * @param args    The arguments to pass to the command.
-     * @throws DukeInvalidCommandException  If the command is invalid.
-     * @throws DukeInvalidArgumentException If the arguments are invalid.
-     * @return True if the command was executed successfully, false otherwise.
-     */
-    private boolean executeCommand(Command command, String args)
-            throws DukeInvalidCommandException, DukeInvalidArgumentException {
-        if (command == null) {
-            throw new DukeInvalidCommandException(
-                    "Not sure what you're blabbering on about, please use a valid command.");
-        }
-
-        switch (command) {
-        case EXIT:
-            return false;
-
-        case LIST_TASKS:
-            this.list.listTasks();
-            break;
-
-        case MARK_TASK:
-            try {
-                this.list.markTaskDone(Integer.parseInt(args));
-            } catch (NumberFormatException e) {
-                throw new DukeInvalidArgumentException("Stop trolling me bro. Please enter a numeric index.");
-            }
-            break;
-
-        case UNMARK_TASK:
-            try {
-                this.list.unmarkTaskDone(Integer.parseInt(args));
-            } catch (NumberFormatException e) {
-                throw new DukeInvalidArgumentException("Stop trolling me bro. Please enter a numeric index.");
-            }
-            break;
-
-        case ADD_TODO:
-            this.list.addTask(TaskType.TODO, args);
-            break;
-
-        case ADD_DEADLINE:
-            this.list.addTask(TaskType.DEADLINE, args);
-            break;
-
-        case ADD_EVENT:
-            this.list.addTask(TaskType.EVENT, args);
-            break;
-
-        case DELETE_TASK:
-            try {
-                this.list.deleteTask(Integer.parseInt(args));
-            } catch (NumberFormatException e) {
-                throw new DukeInvalidArgumentException("Stop trolling me bro. Please enter a numeric index.");
-            }
-            break;
-
-        default:
-            throw new DukeInvalidCommandException("I'm gonna be honest, no idea what you're saying.");
-        }
-
-        return true;
-    }
-
 }
