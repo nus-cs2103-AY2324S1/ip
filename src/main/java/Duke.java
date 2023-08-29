c3850eb7fee7fd26f6cb83be8c90b13d5a86178b
coderhuang559 <huangyixin1f@gmail.com>
2023-08-29 17:17:45 +0800

    Clean up
    Delete unused import.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,6 @@
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.time.temporal.ChronoUnit;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.BufferedReader;
