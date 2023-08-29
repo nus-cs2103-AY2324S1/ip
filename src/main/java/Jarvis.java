9eadc16c03d39032157e6bd0cca32eac43468b7b
Ting Yong Shen Amos <amostingyongquan@gmail.com>
2023-08-29 12:09:43 +0800

    Revert "Revert "Add Level 7. Save.""
    This reverts commit 8cfc1a6998e54913dce554928388c4da99f6dc4a.

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Jarvis {
     private static ArrayList<Task> taskList = new ArrayList<>(); // list of Tasks
@@ -84,6 +90,10 @@ public class Jarvis {
 
     // identifies which command has wrong formatting and prints feedback to user
     private static void printIncorrectFormat(String inputCommand, String validInputCommand) {
+        if (validInputCommand.equals("")) { // if input is empty
+            return;
+        }
+
         // since command is valid, check if formatting of the command is correct
         boolean markMatch = inputCommand.matches("mark \\d+");
         boolean uncheckMatch = inputCommand.matches("unmark \\d+");
@@ -91,7 +101,7 @@ public class Jarvis {
         boolean deadlineMatch = inputCommand.matches("deadline .+ /.+");
         boolean eventMatch = inputCommand.matches("event .+ /.+ /.+");
 
-        if (validInputCommand.equals("mark") && !markMatch) {
+        if (validInputCommand.equals("mark") && !markMatch) { // if mark command but wrong format
             try {
                 throw new WrongJarvisCommandFormatException("Apologies Sir, the format of the mark command you provided is incorrect.");
             } catch (WrongJarvisCommandFormatException e) {
@@ -102,7 +112,7 @@ public class Jarvis {
                 System.out.println(line);
                 return;
             }
-        } else if (validInputCommand.equals("uncheck") && !uncheckMatch) {
+        } else if (validInputCommand.equals("uncheck") && !uncheckMatch) { // if uncheck command but wrong format
             try {
                 throw new WrongJarvisCommandFormatException("Apologies Sir, the format of the uncheck command you provided is incorrect.");
             } catch (WrongJarvisCommandFormatException e) {
@@ -113,7 +123,7 @@ public class Jarvis {
                 System.out.println(line);
                 return;
             }
-        } else if (validInputCommand.equals("todo") && !todoMatch) {
+        } else if (validInputCommand.equals("todo") && !todoMatch) { // if todo command but wrong format
             try {
                 throw new WrongJarvisCommandFormatException("Apologies Sir, the format of the todo command you provided is incorrect.");
             } catch (WrongJarvisCommandFormatException e) {
@@ -124,7 +134,7 @@ public class Jarvis {
                 System.out.println(line);
                 return;
             }
-        } else if (validInputCommand.equals("deadline") && !deadlineMatch) {
+        } else if (validInputCommand.equals("deadline") && !deadlineMatch) { // if deadline command but wrong format
             try {
                 throw new WrongJarvisCommandFormatException("Apologies Sir, the format of the deadline command you provided is incorrect.");
             } catch (WrongJarvisCommandFormatException e) {
@@ -135,7 +145,7 @@ public class Jarvis {
                 System.out.println(line);
                 return;
             }
-        } else if (validInputCommand.equals("event") && !eventMatch) {
+        } else if (validInputCommand.equals("event") && !eventMatch) { // if event command but wrong format
             try {
                 throw new WrongJarvisCommandFormatException("Apologies Sir, the format of the event command you provided is incorrect.");
             } catch (WrongJarvisCommandFormatException e) {
@@ -198,32 +208,69 @@ public class Jarvis {
         return isValid;
     }
 
+    // delete contents in data.txt and return empty data.txt
+    private static File deleteContentsDataFile(File dataFile) {
+        try (FileWriter writer = new FileWriter(dataFile)) {
+            // Write an empty string to the file
+            writer.write("");
+        } catch (IOException e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
+        return dataFile;
+    }
+
+    // write tasks toString() to data.txt
+    private static void writeTaskListToDataFile(File dataFile) {
+        for (int i = 0; i < taskList.size(); i++) { // writing the string of each task to the data file
+            Task currentTask = taskList.get(i);
+            try (FileWriter dataFileWriter = new FileWriter(dataFile, true)) {
+                dataFileWriter.write(currentTask.toString() + "\n");
+            } catch (IOException e) {
+                System.err.println("An error occurred: " + e.getMessage());
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         addValidCommands(); // adding the list of valid commands to the validCommands arraylist
         printGreeting();
 
+        // getting the file path to the save file
+        String home = System.getProperty("user.home");
+        Path pathToSaveFile = Paths.get(home, "Desktop", "CS2103T", "IP", "data", "data.txt");
+        boolean isFileExists = Files.exists(pathToSaveFile);
+        File dataFile = new File(pathToSaveFile.toString());
+        try {
+            dataFile.createNewFile();
+        } catch (IOException e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
+
+        Pattern todoPattern = Pattern.compile("(todo) (.+)");
+        Pattern deadlinePattern = Pattern.compile("(deadline) (.+) /by (.+)");
+        Pattern eventPattern = Pattern.compile("(event) (.+) /from (.+) /to (.+)");
+        Pattern deletePattern = Pattern.compile("(delete) (\\d+)");
+
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String command = scanner.nextLine().trim();
             String nameOfValidCommand = isValidCommand(command);
-            if (nameOfValidCommand.equals("")) {continue;}
 
-            // if "list" is entered
-            if (command.matches("list")) {
+            Matcher todoMatcher = todoPattern.matcher(command);
+            Matcher deadlineMatcher = deadlinePattern.matcher(command);
+            Matcher eventMatcher = eventPattern.matcher(command);
+            Matcher deleteMatcher = deletePattern.matcher(command);
+
+            if (command.matches("list")) { // if "list" is entered
                 printList();
-                continue;
-            }
 
-            // if "bye" is entered
-            if (command.matches("bye")) {
+            } else if (command.matches("bye")) { // if "bye" is entered
                 printBye();
                 break;
-            }
 
-            // if "mark" or "uncheck" is entered
-            if (command.matches("mark \\d+") ||
-                    command.matches("uncheck \\d+")) {
+            } else if (command.matches("mark \\d+") ||
+                    command.matches("uncheck \\d+")) { // if "mark" or "uncheck" is entered
 
                 int taskNum = Integer.parseInt(command.substring(command.length() - 1));
                 if (!isValidTaskNumber(taskNum)) {continue;}
@@ -232,62 +279,49 @@ public class Jarvis {
                 if (command.matches("uncheck \\d+")) { // if "uncheck" is entered
                     currentTask.setDone(false);
                     printUncheck(currentTask);
-                    continue;
+
                 } else { // if "mark" is entered
                     currentTask.setDone(true);
                     printMark(currentTask);
-                    continue;
                 }
 
-            }
-
-            // if task command is entered
-            Pattern todoPattern = Pattern.compile("(todo) (.+)");
-            Pattern deadlinePattern = Pattern.compile("(deadline) (.+) /by (.+)");
-            Pattern eventPattern = Pattern.compile("(event) (.+) /from (.+) /to (.+)");
-            Matcher todoMatcher = todoPattern.matcher(command);
-            Matcher deadlineMatcher = deadlinePattern.matcher(command);
-            Matcher eventMatcher = eventPattern.matcher(command);
-            if (todoMatcher.matches() || deadlineMatcher.matches() || eventMatcher.matches()) {
+            } else if (todoMatcher.matches() ||
+                        deadlineMatcher.matches() ||
+                            eventMatcher.matches()) { // if task command is entered
+                Task newTask;
                 if (todoMatcher.matches()) { // if "todo" is entered
                     String taskDescription = todoMatcher.group(2);
-                    ToDo todo = new ToDo(taskDescription);
-                    taskList.add(todo);
-                    printTask(todo);
-                    continue;
+                    newTask = new ToDo(taskDescription);
+
                 } else if (deadlineMatcher.matches()) { // if "deadline" is entered
                     String taskDescription = deadlineMatcher.group(2);
                     String by = deadlineMatcher.group(3);
-                    Deadline deadline = new Deadline(taskDescription, by);
-                    taskList.add(deadline);
-                    printTask(deadline);
-                    continue;
+                    newTask = new Deadline(taskDescription, by);
+
                 } else { // if "event" is entered
                     String taskDescription = eventMatcher.group(2);
                     String from = eventMatcher.group(3);
                     String to = eventMatcher.group(4);
-                    Event event = new Event(taskDescription, from, to);
-                    taskList.add(event);
-                    printTask(event);
-                    continue;
-                }
-            }
+                    newTask = new Event(taskDescription, from, to);
 
-            // if delete is entered
-            Pattern deletePattern = Pattern.compile("(delete) (\\d+)");
-            Matcher deleteMatcher = deletePattern.matcher(command);
-            if (deleteMatcher.matches()) {
+                }
+                taskList.add(newTask);
+                printTask(newTask);
+
+            } else if (deleteMatcher.matches()) { // if delete is entered
                 int taskNum = Integer.parseInt(deleteMatcher.group(2));
                 if (isValidTaskNumber(taskNum)) {
                     Task deletedTask = taskList.get(taskNum - 1);
                     taskList.remove(taskNum - 1);
                     printDelete(deletedTask);
                 }
-                continue;
             }
 
-            // if none of the above commands go through
-            printIncorrectFormat(command, nameOfValidCommand);
+            else { // if none of the above commands go through
+                printIncorrectFormat(command, nameOfValidCommand);
+            }
+
+            writeTaskListToDataFile(deleteContentsDataFile(dataFile)); // write task list to data file after every iteration
         }
         scanner.close(); // closing the user input scanner
     }
