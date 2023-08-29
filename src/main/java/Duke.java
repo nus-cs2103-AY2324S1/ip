23ec65b4e960dd65f368677371d1533df04af2c3
Saumya Shah <68504323+LordSaumya@users.noreply.github.com>
2023-08-30 01:20:28 +0800

    Add UI class to replace methods in the main class.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,6 +18,7 @@ public class Duke {
         String divider = "\n____________________________________________________________\n";
 
         Scanner sc = new Scanner(System.in);
+        Ui ui = new Ui();
 
         ArrayList<Task> list = new ArrayList<Task>();
 
@@ -61,159 +62,133 @@ public class Duke {
                 File taskFile = new File("tasks.txt");
                 taskFile.createNewFile();
             } catch (Exception f) {
-                System.out.println("Error creating new file.");
+                ui.showError("Error creating new file.");
             }
         }
 
         String command = "";
 
-        System.out.println("Greetings, puny mortal. This is \n" + logo
-                + "\nThe Lord of Time. \nWhat foolish errand do you seek to accomplish with my immense powers?");
-        System.out.println(divider);
+        ui.startMessage();
 
         while (true) {
-            command = sc.nextLine();
+            command = ui.commandPrompt();
             if (command.equals("bye")) {
                 // End conversation
                 break;
             } else if (command.equals("list")) {
                 // List all tasks
-                System.out.println(divider);
-                System.out.println(
-                        "You have somehow found the audacity to conjure up this laughable list of inconsequential endeavours:\n");
-                for (int i = 1; i <= list.size(); i++) {
-                    System.out.println(i + ". " + list.get(i - 1));
-                }
-                System.out.println(divider);
+                ui.listTasks(list);
             } else if (command.startsWith("mark")) {
                 // Mark a task as done
-                System.out.println(divider);
+                ui.printDivider();
                 int taskNum = Integer.parseInt(command.substring(5));
                 try {
                     list.get(taskNum - 1).changeStatus(true);
-                    System.out.println(
-                            "Astonishingly enough, you have managed to triumph over this mind-bogglingly simple task:\n");
-                    System.out.println(list.get(taskNum - 1).toString());
+                    ui.markTaskMessage(list.get(taskNum - 1));
                 } catch (IndexOutOfBoundsException e) {
-                    System.out.println("You have no such task, mortal.");
+                    ui.showError("You have no such task, mortal.");
                 }
-                System.out.println(divider);
+                ui.printDivider();
             } else if (command.startsWith("unmark")) {
                 // Mark a task as not done
-                System.out.println(divider);
+                ui.printDivider();
                 int taskNum = Integer.parseInt(command.substring(7));
                 try {
                     list.get(taskNum - 1).changeStatus(false);
-                    System.out.println(
-                            "In a stunning twist, this task remains untouched by the hands of progress:\n");
-                    System.out.println(list.get(taskNum - 1).toString());
+                    ui.unmarkTaskMessage(list.get(taskNum - 1));
                 } catch (IndexOutOfBoundsException e) {
-                    System.out.println("You have no such task, mortal.");
+                    ui.showError("You have no such task, mortal.");
                 } finally {
-                    System.out.println(divider);
+                    ui.printDivider();
                 }
             } else if (command.startsWith("todo")) {
                 // Add a todo task
-                System.out.println(divider);
+                ui.printDivider();
                 try {
-                    System.out.println("This task has been reluctantly bestowed upon your ever-growing list:\n");
                     if (command.substring(5).equals("")) {
-                        throw new Exception("The description of a todo cannot be empty.");
+                        ui.showError("The description of a todo cannot be empty.");
                     }
                     list.add(new ToDo(command.substring(5)));
-                    System.out.println(list.get(list.size() - 1).toString());
-                    System.out
-                            .println("Congratulations, your pile of tasks has swelled to a whopping " + list.size()
-                                    + ".");
-                    ;
+                    ui.todoMessage(list.get(list.size() - 1));
+                    ui.taskListSizeMessage(list.size(), true);
                 } catch (Exception e) {
-                    System.out.println(e.getMessage());
+                    ui.showError(e.getMessage());
                 } finally {
-                    System.out.println(divider);
+                    ui.printDivider();
                 }
             } else if (command.startsWith("deadline")) {
                 // Add a deadline task
-                System.out.println(divider);
+                ui.printDivider();
                 try {
                     String[] deadlineTaskComponents = command.substring(9).split(" /by ");
 
                     if (deadlineTaskComponents[0].equals("")) {
-                        throw new Exception("The description of a deadline cannot be empty.");
+                        ui.showError("The description of a deadline cannot be empty.");
                     } else if (deadlineTaskComponents.length == 1) {
-                        throw new Exception("The deadline of a deadline cannot be empty.");
+                        ui.showError("The deadline of a deadline cannot be empty.");
                     } else if (deadlineTaskComponents.length > 2) {
-                        throw new Exception("The deadline of a deadline cannot contain more than one date.");
+                        ui.showError("The deadline of a deadline cannot contain more than one date.");
                     } else if (deadlineTaskComponents[1].equals("")) {
-                        throw new Exception("The deadline of a deadline cannot be empty.");
+                        ui.showError("The deadline of a deadline cannot be empty.");
                     }
 
-                    System.out.println(
-                            "With your constant mediocrity, it is entirely unlikely that you will be able to meet this deadline I have just added: \n");
                     list.add(new Deadline(deadlineTaskComponents[0], deadlineTaskComponents[1]));
-                    System.out.println(list.get(list.size() - 1).toString());
-                    System.out
-                            .println("Congratulations, your pile of tasks has swelled to a whopping " + list.size()
-                                    + ".");
+                    ui.deadlineMessage(list.get(list.size() - 1));
+                    ui.taskListSizeMessage(list.size(), true);
                 } catch (Exception e) {
-                    System.out.println(e.getMessage());
+                    ui.showError(e.getMessage());
                 } finally {
-                    System.out.println(divider);
+                    ui.printDivider();
                 }
             } else if (command.startsWith("event")) {
                 // Add an event task
-                System.out.println(divider);
+                ui.printDivider();
+
                 try {
                     String[] eventTaskComponents = command.substring(6).split(" /from ");
                     String eventDescription = eventTaskComponents[0];
                     if (eventDescription.equals("")) {
-                        throw new Exception("The description of an event cannot be empty.");
+                        ui.showError("The description of an event cannot be empty.");
                     } else if (eventTaskComponents.length == 1) {
-                        throw new Exception("The start time of an event cannot be empty.");
+                        ui.showError("The start time of an event cannot be empty.");
                     } else if (eventTaskComponents.length > 2) {
-                        throw new Exception("The start time of an event cannot contain more than one date.");
+                        ui.showError("The start time of an event cannot contain more than one date.");
                     }
 
                     String[] eventTaskDates = eventTaskComponents[1].split(" /to ");
 
                     if (eventTaskDates[1].equals("")) {
-                        throw new Exception("The end time of an event cannot be empty.");
+                        ui.showError("The end time of an event cannot be empty.");
                     } else if (eventTaskDates.length > 2) {
-                        throw new Exception("The end time of an event cannot contain more than one date.");
+                        ui.showError("The end time of an event cannot contain more than one date.");
                     }
 
-                    System.out.println(
-                            "Looks like I will have to slow time down myself if you wish to make it to this event I just added:\n");
                     list.add(new Event(eventDescription, eventTaskDates[0], eventTaskDates[1]));
-                    System.out.println(list.get(list.size() - 1).toString());
-                    System.out
-                            .println("Congratulations, your pile of tasks has swelled to a whopping " + list.size()
-                                    + ".");
+                    ui.eventMessage(list.get(list.size() - 1));
+                    ui.taskListSizeMessage(list.size(), true);
                 } catch (Exception e) {
-                    System.out.println(e.getMessage());
+                    ui.showError(e.getMessage());
                 } finally {
-                    System.out.println(divider);
+                    ui.printDivider();
                 }
             } else if (command.startsWith("delete")) {
                 // Delete a task
-                System.out.println(divider);
+                ui.printDivider();
                 int taskNum = Integer.parseInt(command.substring(7));
                 try {
-                    System.out.println(
-                            "One less annoyance to plague your feeble list. This task has been banished:\n");
-                    System.out.println(list.get(taskNum - 1).toString());
+                    ui.deleteMessage(list.get(taskNum - 1));
                     list.remove(taskNum - 1);
-                    System.out.println(
-                            "Congratulations, your pile of tasks has shrunk to a measly " + list.size() + ".");
+                    ui.taskListSizeMessage(list.size(), false);
                 } catch (IndexOutOfBoundsException e) {
-                    System.out.println("You have no such task, mortal.");
+                    ui.showError("You have no such task, mortal.");
                 } finally {
-                    System.out.println(divider);
+                    ui.printDivider();
                 }
             } else {
                 // Error message (unknown command)
-                System.out.println(divider);
-                System.out.println("Do not test my patience, mortal. Speak clearly.");
-                System.out.println(divider);
+                ui.printDivider();
+                ui.showError("Do not test my patience, mortal. Speak clearly.");
+                ui.printDivider();
             }
         }
 
@@ -228,9 +203,6 @@ public class Duke {
             System.out.println("Error writing to file.");
         }
 
-        System.out.println(divider);
-        System.out.println("Is that all? I have better things to do than to listen to lesser beings. Farewell.");
-        System.out.println(divider);
-        sc.close();
+        ui.endMessage();
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Ui {
+
+    String logo = " ___  __    ________  ________  ________   ________  ________      \r\n" + //
+            "|\\  \\|\\  \\ |\\   __  \\|\\   __  \\|\\   ___  \\|\\   __  \\|\\   ____\\     \r\n" + //
+            "\\ \\  \\/  /|\\ \\  \\|\\  \\ \\  \\|\\  \\ \\  \\\\ \\  \\ \\  \\|\\  \\ \\  \\___|_    \r\n" + //
+            " \\ \\   ___  \\ \\   _  _\\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\\\\\  \\ \\_____  \\   \r\n" + //
+            "  \\ \\  \\\\ \\  \\ \\  \\\\  \\\\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\\\\\  \\|____|\\  \\  \r\n" + //
+            "   \\ \\__\\\\ \\__\\ \\__\\\\ _\\\\ \\_______\\ \\__\\\\ \\__\\ \\_______\\____\\_\\  \\ \r\n" + //
+            "    \\|__| \\|__|\\|__|\\|__|\\|_______|\\|__| \\|__|\\|_______|\\_________\\\r\n" + //
+            "                                                       \\|_________|";
+
+    String divider = "\n____________________________________________________________\n";
+
+    Scanner sc = new Scanner(System.in);
+
+    public Ui() {
+    }
+
+    public void printDivider() {
+        System.out.println(divider);
+    }
+
+    public void startMessage() {
+        System.out.println("Greetings, puny mortal. This is \n" + logo
+                + "\nThe Lord of Time. \nWhat foolish errand do you seek to accomplish with my immense powers?");
+        printDivider();
+    }
+
+    public void showError(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
+    public String commandPrompt() {
+        return sc.nextLine();
+    }
+
+    public void listTasks(ArrayList<Task> taskList) {
+        printDivider();
+        System.out.println(
+                "You have somehow found the audacity to conjure up this laughable list of inconsequential endeavours:\n");
+        for (int i = 1; i <= taskList.size(); i++) {
+            System.out.println(i + ". " + taskList.get(i - 1));
+        }
+        printDivider();
+    }
+
+    public void markTaskMessage(Task task) {
+        System.out.println(
+                "Astonishingly enough, you have managed to triumph over this mind-bogglingly simple task:\n");
+        System.out.println(task.toString());
+    }
+
+    public void unmarkTaskMessage(Task task) {
+        System.out.println(
+                "You have somehow managed to fail this mind-bogglingly simple task:\n");
+        System.out.println(task.toString());
+    }
+
+    public void todoMessage(Task task) {
+        System.out.println("This task has been reluctantly bestowed upon your ever-growing list:\n");
+        System.out.println(task.toString());
+    }
+
+    public void deadlineMessage(Task task) {
+        System.out.println(
+                "With your constant mediocrity, it is entirely unlikely that you will be able to meet this deadline I have just added: \n");
+        System.out.println(task.toString());
+    }
+
+    public void eventMessage(Task task) {
+        System.out.println(
+                "Looks like I will have to slow time down myself if you wish to make it to this event I just added:\n");
+        System.out.println(task.toString());
+    }
+
+    public void deleteMessage(Task task) {
+        System.out.println(
+                "One less annoyance to plague your feeble list. This task has been banished:\n");
+        System.out.println(task.toString());
+    }
+
+    public void endMessage() {
+        printDivider();
+        System.out.println("Is that all? I have better things to do than to listen to lesser beings. Farewell.");
+        printDivider();
+    }
+
+    public void taskListSizeMessage(int size, boolean growing) {
+        if (growing) {
+            System.out.println("Congratulations, your pile of tasks has swelled to a whopping " + size + ".");
+        } else {
+            System.out.println("Your pile of tasks has shrunk to a measly " + size + ".");
+        }
+    }
+}
