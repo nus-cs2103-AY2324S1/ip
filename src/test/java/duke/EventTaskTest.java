82d01babb4a2b495ab247cfed489c15ac91b85ed
leesiyuan <lee.si.yuan.20@gmail.com>
2023-08-29 14:44:08 +0800

    Add junit Test
    Test EventTaskTest
    
    Added test for toString and markTaskAsDone methods.
    
    Test TodoTaskTest
    
    Added test for toString and markTaskAsDone methods.

---
+++ src/test/java/duke/EventTaskTest.java
@@ -0,0 +1,21 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+
+import duke.task.EventTask;
+
+public class EventTaskTest {
+    
+    @Test
+    public void testToString() {
+        EventTask eventTask = new EventTask("project meeting", "2020-08-25 14:00", "2020-08-25 16:00");
+        assert eventTask.toString().equals("[E][ ] project meeting (from: 2020-08-25 14:00 to: 2020-08-25 16:00)");
+    }
+
+    @Test
+    public void testMarkTaskAsDone() {
+        EventTask eventTask = new EventTask("project meeting", "2020-08-25 14:00", "2020-08-25 16:00");
+        eventTask.markAsDone();
+        assert eventTask.toString().equals("[E][X] project meeting (from: 2020-08-25 14:00 to: 2020-08-25 16:00)");
+    }
+}
