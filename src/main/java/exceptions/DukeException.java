f37dbdbdaadd7ace465055304d9b432687fa54af
shuenj <60507123+shuenj@users.noreply.github.com>
2023-08-29 14:39:08 +0800

    Add Javadocs for exception class

--- src/main/java/exceptions/DukeException.java
+++ src/main/java/exceptions/DukeException.java
@@ -1,5 +1,13 @@
 package exceptions;
+
+/**
+ * Represents a DukeException.
+ */
 public class DukeException extends Exception {
+    /**
+     * Constructor for DukeException.
+     * @param message
+     */
     public DukeException(String message) {
         super(message);
     }
