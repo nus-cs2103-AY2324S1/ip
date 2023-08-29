e53597da6d8eb9b796b4539d79b5dfac0bf2732c
YangHeng Tang <yanghengtang@gmail.com>
2023-08-30 05:01:13 +0800

    Add PrintListCommand.java

---
+++ src/main/java/duke/command/PrintListCommand.java
@@ -0,0 +1,16 @@
+package duke.command;
+
+import duke.util.Storage;
+import duke.util.TaskList;
+import duke.util.Ui;
+
+public class PrintListCommand extends Command {
+    public PrintListCommand() {
+        super(false);
+    }
+
+    @Override
+    public void execute(TaskList list, Ui ui, Storage storage) {
+        ui.printList(list);
+    }
+}
