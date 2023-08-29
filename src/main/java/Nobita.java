f7db58cc12e014042f8d2d3d0feb8715502a12fe
Seonlo99 <zheng_chenglong1999@hotmail.com>
2023-08-29 17:23:06 +0800

    Update syntax

--- src/main/java/Nobita.java
+++ src/main/java/Nobita.java
@@ -5,7 +5,6 @@ public class Nobita {
     private static ArrayList<Task> tasks = new ArrayList<>();
     private static int size = 0;
 
-
     public static void main(String[] args) {
         System.out.println("Hello! I'm Nobita");
         System.out.println("What can I do for you?");
@@ -21,56 +20,56 @@ public class Nobita {
 
             try {
                 switch (input[0].toLowerCase()) {
-                    case "bye":
-                        live = false;
-                        break;
-                    case "list":
-                        int listNum = 1;
-                        for (Task task: tasks) {
-                            printMessage(listNum + ". " + task);
-                            ++listNum;
-                        }
-                        break;
-                    case "mark": {
-                        int target = Integer.parseInt(input[1]) - 1;
-                        tasks.get(target).markComplete();
-                        break;
+                case "bye":
+                    live = false;
+                    break;
+                case "list":
+                    int listNum = 1;
+                    for (Task task: tasks) {
+                        printMessage(listNum + ". " + task);
+                        ++listNum;
                     }
-                    case "unmark": {
-                        int target = Integer.parseInt(input[1]) - 1;
-                        tasks.get(target).markIncomplete();
-                        break;
+                    break;
+                case "mark": {
+                    int target = Integer.parseInt(input[1]) - 1;
+                    tasks.get(target).markComplete();
+                    break;
+                }
+                case "unmark": {
+                    int target = Integer.parseInt(input[1]) - 1;
+                    tasks.get(target).markIncomplete();
+                    break;
+                }
+                case "todo":
+                    if (input.length < 2) {
+                        throwDescriptionException("todo");
                     }
-                    case "todo":
-                        if (input.length < 2) {
-                            throwDescriptionException("todo");
-                        }
-                        ToDo newToDo = new ToDo(input[1]);
-                        addTask(newToDo);
-                        break;
-                    case "deadline":
-                        if (input.length < 2) {
-                            throwDescriptionException("deadline");
-                        }
-                        String[] deadlineFields = input[1].split(" /by ");
-                        Deadline newDeadline = new Deadline(deadlineFields[0], deadlineFields[1]);
-                        addTask(newDeadline);
-                        break;
-                    case "event":
-                        if (input.length < 2) {
-                            throwDescriptionException("event");
-                        }
-                        String[] eventFields = input[1].split(" /from ");
-                        String[] fromAndTo = eventFields[1].split(" /to ");
-                        Event newEvent = new Event(eventFields[0], fromAndTo[0], fromAndTo[1]);
-                        addTask(newEvent);
-                        break;
-                    case "delete":
-                        int target = Integer.parseInt(input[1]) - 1;
-                        deleteTask(target);
-                        break;
-                    default:
-                        throw new NobitaException("I'm sorry, but I don't know what that means :-(");
+                    ToDo newToDo = new ToDo(input[1]);
+                    addTask(newToDo);
+                    break;
+                case "deadline":
+                    if (input.length < 2) {
+                        throwDescriptionException("deadline");
+                    }
+                    String[] deadlineFields = input[1].split(" /by ");
+                    Deadline newDeadline = new Deadline(deadlineFields[0], deadlineFields[1]);
+                    addTask(newDeadline);
+                    break;
+                case "event":
+                    if (input.length < 2) {
+                        throwDescriptionException("event");
+                    }
+                    String[] eventFields = input[1].split(" /from ");
+                    String[] fromAndTo = eventFields[1].split(" /to ");
+                    Event newEvent = new Event(eventFields[0], fromAndTo[0], fromAndTo[1]);
+                    addTask(newEvent);
+                    break;
+                case "delete":
+                    int target = Integer.parseInt(input[1]) - 1;
+                    deleteTask(target);
+                    break;
+                default:
+                    throw new NobitaException("I'm sorry, but I don't know what that means :-(");
                 }
             } catch (NobitaException e) {
                 printMessage(e.toString());
