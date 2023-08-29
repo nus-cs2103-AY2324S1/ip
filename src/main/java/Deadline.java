50ef758d8c1752839cf57c254e1b25715457e27c
freddychenyouren2 <freddychenyouren2@gmail.com>
2023-08-30 00:11:51 +0800

    Implement Level-8 features of Date and Time in tasks
    Deadline and Event tasks were initialized with String values for their timings
    
    Used Java library on Dates and Times to initialize Deadline and Event tasks with YYYY-MM-DD inputs
    Also handled cases where inputs are invalid dates or invalid formats with InvalidDateException.java
    
    Note: LoadTasks() Function now has poorer OOP Principles used due to optional initialization of tasks read from the  hard disk.
    This will be handled in the future.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,20 +1,27 @@
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Deadline extends Task {
 
-    protected String by;
+    private LocalDate by;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, LocalDate by) {
         super(description);
         this.by = by;
     }
 
+    private LocalDate getDeadlineDate() {
+        return this.by;
+    }
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        //return "[D]" + super.toString() + " (by: " + by + ")";
+        return String.format("[D]%s (by: %s)", super.toString(),
+                getDeadlineDate().format(DateTimeFormatter.ofPattern("MMM d yyyy")));
     }
 
-    public String getDueDate() {
+    public LocalDate getDueDate() {
         return this.by;
     }
 
@@ -25,20 +32,30 @@ public class Deadline extends Task {
      */
     public static void handleDeadlineTask(String userInput) throws IOException {
         String[] details = userInput.split("/by");
-        //details[0] contains "deadline" plus task description, need to erase "deadline". details[1] contains String deadline timing
+        //details[0] contains "deadline" plus task description, need to erase "deadline". details[1] contains deadline timing
         if (details.length == 2) {
             String taskDescription = details[0].trim().replaceFirst("deadline", "").trim();
             String deadline = details[1].trim();
-            Deadline deadlineTask = new Deadline(taskDescription, deadline);
-            Duke.saveTask(deadlineTask, true);
-            Duke.taskList.add(deadlineTask); //Deadline <: Task
 
-            //Print details in the console
-            System.out.println(Duke.HORIZONTAL_LINE);
-            System.out.println("     Got it. I've added this task:");
-            System.out.printf("       %s\n", deadlineTask.toString());
-            System.out.printf("     Now you have %d tasks in the list.\n", Duke.taskList.size());
-            System.out.println(Duke.HORIZONTAL_LINE);
+            //Check if input date is valid.
+            try {
+                if (Duke.isValidDate(deadline)) {
+                    Deadline deadlineTask = new Deadline(taskDescription, LocalDate.parse(deadline));
+                    Duke.saveTask(deadlineTask, true);
+                    Duke.taskList.add(deadlineTask); //Deadline <: Task
+
+                    //Print details in the console
+                    System.out.println(Duke.HORIZONTAL_LINE);
+                    System.out.println("     Got it. I've added this task:");
+                    System.out.printf("       %s\n", deadlineTask.toString());
+                    System.out.printf("     Now you have %d tasks in the list.\n", Duke.taskList.size());
+                    System.out.println(Duke.HORIZONTAL_LINE);
+                } else {
+                    throw new InvalidDateException();
+                }
+            } catch (InvalidDateException e) {
+                System.out.println(e.toString());
+            }
 
         } else {
             System.out.println(Duke.HORIZONTAL_LINE);
