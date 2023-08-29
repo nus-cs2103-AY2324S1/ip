ff425d10ffe31786e68f9db22951736b577ba304
chonguschonguschongus <chongzhenxi@gmail.com>
2023-08-29 21:47:58 +0800

    Add TimeFormatter to handle formatting of inputted dates

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,30 +1,42 @@
+import java.time.LocalDateTime;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Event extends Task {
     private String title;
-    private String start;
-    private String end;
+    private TimeFormatter start;
+    private TimeFormatter end;
 
     public Event(String response) {
         super(false);
         int toTrim = response.indexOf(" ");
         int info = response.indexOf("/");
         this.title = response.substring(toTrim + 1, info - 1);
-        String startingEnding = response.substring(info + 1);
-        int endTime = startingEnding.indexOf("/");
-        this.start = startingEnding.substring(5, endTime - 1);
-        this.end = startingEnding.substring(endTime + 4);
+        Pattern startPattern = Pattern.compile("/from (\\d+/\\d+/\\d+ \\d+)");
+        Pattern endPattern = Pattern.compile("/to (\\d+/\\d+/\\d+ \\d+)");
+        Matcher startMatcher = startPattern.matcher(response);
+        Matcher endMatcher = endPattern.matcher(response);
+        if (startMatcher.find() && endMatcher.find()) {
+            String startDateTime = startMatcher.group(1);
+            String endDateTime = endMatcher.group(1);
+            this.start = new TimeFormatter(startDateTime);
+            this.end = new TimeFormatter(endDateTime);
+        } else {
+            System.out.println("Incorrect format of date and time inputted");
+        }
     }
 
     @Override
     public String toFileString() {
         if (this.done == true) {
-            return "D | 1 | " + this.title + " | " + this.start + "-" + this.end;
+            return "D | 1 | " + this.title + " | " + this.start.formatDate() + " - " + this.end.formatDate();
         }
-        return "D | 0 | " + this.title + " | " +  this.start + "-" + this.end;
+        return "D | 0 | " + this.title + " | " +  this.start.formatDate() + " - " + this.end.formatDate();
     }
 
     @Override
     public String toString() {
-        String s = String.format("(from: %s to: %s)", start, end);
+        String s = String.format("| FROM: %s TO: %s |", start.formatDate(), end.formatDate());
         if (this.done == true) {
             return "[E] " + "[X] " + this.title + " " + s;
         }
