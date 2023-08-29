ec7214bb42f1ccfb119259a51fdb18b8927e2ae9
teozern1 <75937968+teozern1@users.noreply.github.com>
2023-08-29 16:18:37 +0800

    Add Junit Tests

---
+++ src/test/java/Eddie/DukeTest.java
@@ -0,0 +1,14 @@
+package Eddie;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class DukeTest {
+    @Test
+    public void addTest() {
+        Deadline d = new Deadline("Test", LocalDate.parse("2019-01-01"));
+        Duke.add(d);
+        assertEquals(TaskList.get(0), d);
+    }
+}
