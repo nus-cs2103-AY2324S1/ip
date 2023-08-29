3e465623a89bd7fd7d1db2c7072fcb4c8c9a449c
YangHeng Tang <yanghengtang@gmail.com>
2023-08-30 04:59:28 +0800

    Add Command.java abstract class

---
+++ src/main/java/duke/command/Command.java
@@ -0,0 +1,17 @@
+package duke.command;
+
+import duke.util.Storage;
+import duke.util.TaskList;
+import duke.util.Ui;
+
+public abstract class Command {
+    private boolean exit;
+
+    public Command(boolean exit) {
+        this.exit = exit;
+    }
+    public abstract void execute(TaskList tasks, Ui ui, Storage storage);
+    public boolean isExit() {
+        return this.exit;
+    }
+}
