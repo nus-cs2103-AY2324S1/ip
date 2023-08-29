123aabc14b14292444dbba1824a67be5222ed6ba
Elijah5399 <chia.elijah@gmail.com>
2023-08-29 18:38:04 +0800

    Implemented Date recognition for deadlines

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,23 +1,29 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * A Deadline is a task with an end date/time.
  */
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
 
     /**
      * Deadline constructor method.
      * @param description A description for the deadline task.
      * @param by When the deadline should be completed by.
      */
-    public Deadline(String description, String by) {
+    public Deadline(String description, LocalDate by) {
         super(description);
         this.by = by;
     }
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + this.getBy() + ")";
     }
     public String getBy() {
-        return this.by;
+        return by.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
+    }
+    public String getByForStorage() {
+        return by.toString();
     }
 }
