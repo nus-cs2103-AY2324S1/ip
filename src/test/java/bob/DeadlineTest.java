ae8385dda2c12dd479cfad0871a5f2ca2c6a6407
Jackie <jackieguo126@gmail.com>
2023-08-29 23:00:20 +0800

    junit

---
+++ src/test/java/bob/DeadlineTest.java
@@ -0,0 +1,30 @@
+package bob;
+
+import bob.exception.MissingDatesException;
+import bob.task.Deadline;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeadlineTest {
+
+    @Test
+    void testToString() {
+        try {
+            Deadline dl = new Deadline("submit homework /by 2022-01-01");
+            assertEquals("[D][ ] submit homework (by: Jan 01 2022)", dl.toString());
+        } catch (MissingDatesException e) {
+            System.out.println(e.message);
+        }
+    }
+
+    @Test
+    void toTxt() {
+        try {
+            Deadline dl = new Deadline("submit homework /by 2022-01-01");
+            assertEquals("deadline | 0 | submit homework | 2022-01-01", dl.toTxt());
+        } catch (MissingDatesException e) {
+            System.out.println(e.message);
+        }
+    }
+}
---
+++ src/test/java/bob/ParserTest.java
@@ -0,0 +1,16 @@
+package bob;
+
+import bob.command.AddCommand;
+import bob.command.Command;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+
+    @Test
+    void parse() {
+        Command c = new AddCommand("todo read");
+        assertEquals(c.getClass(), Parser.parse("todo read").getClass());
+    }
+}
