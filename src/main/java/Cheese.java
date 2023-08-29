321bcc5f4019e381ca20bf8125bb2293ab8f0d2f
LicongHuang <alexliconghuang@gmail.com>
2023-08-29 21:58:13 +0800

    Add save and load feature

--- src/main/java/Cheese.java
+++ src/main/java/Cheese.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Cheese {
     private List<Task> CheeseStack = new ArrayList<Task>();
+    private static final String FILE_PATH = "./data/cheese.txt";
 
     enum TaskType {
         TODO,
@@ -11,6 +19,7 @@ public class Cheese {
         EVENT
     }
     public void speak() {
+        loadTask();
         Scanner myObj = new Scanner(System.in);
         String echoedInput = "";
         System.out.println("\t-----------------------------------------");
@@ -21,11 +30,96 @@ public class Cheese {
             echoedInput = myObj.nextLine();
             commandResponse(echoedInput);
             if (echoedInput.equals("bye")) {
+                saveTasks();
+                myObj.close();
                 break;
+                
             }
         }
         System.out.println("\t-----------------------------------------");
     }
+
+    private void loadTask() {
+    try {
+      File file = new File(FILE_PATH);
+      if (file.exists()) {
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+          String line = scanner.nextLine();
+          String[] taskInfo = line.split("\\|");
+          char type = taskInfo[0].charAt(1);
+          boolean isDone = (taskInfo[0].charAt(4) == 'X');
+          String description = taskInfo[1].trim();
+
+          switch (type) {
+            case 'T':
+              Task todo = new Task(type, description);
+              if (isDone) {
+                todo.markAsDone();
+              }
+              CheeseStack.add(todo);
+              break;
+            case 'D':
+              String byPatternString = "\\(by: (.*?)\\)";
+              Pattern byPattern = Pattern.compile(byPatternString);
+              Matcher byMatcher = byPattern.matcher(description);
+              if (byMatcher.find()) {
+                String deadlineInfo = byMatcher.group(1);
+                String desc = description.split(" ")[0];
+                Task deadlineTask = new Task(type, desc, deadlineInfo);
+                if (isDone) {
+                  deadlineTask.markAsDone();
+                }
+                CheeseStack.add(deadlineTask);
+                break;
+              }
+            case 'E':
+              String fromPatternString = "from: (.*?) to:";  
+              String toPatternString = "to: (.*)\\)$";
+              Pattern fromPattern = Pattern.compile(fromPatternString);
+              Pattern toPattern = Pattern.compile(toPatternString);
+              
+              Matcher fromMatcher = fromPattern.matcher(taskInfo[1]);
+              Matcher toMatcher = toPattern.matcher(taskInfo[1]);
+
+              if (fromMatcher.find() && toMatcher.find()) {
+                String eventFrom = fromMatcher.group(1);
+                String eventTo = toMatcher.group(1);
+                String desc = description.split(" ")[0];
+                Task eventTask = new Task(type, desc, eventFrom, eventTo);
+                if (isDone) {
+                  eventTask.markAsDone();
+                }
+                CheeseStack.add(eventTask);
+              }
+              break;
+          }
+        }
+        scanner.close();
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("☹ OOPS!!! File not found.");
+    }
+  }
+
+  private void saveTasks() {
+    try {
+      File file = new File(FILE_PATH);
+      if (!file.exists()) {
+        file.getParentFile().mkdirs();
+        file.createNewFile();
+      }
+
+      FileWriter fileWriter = new FileWriter(file);
+      for (Task task : CheeseStack) {
+        fileWriter.write(task.toString() + System.lineSeparator());
+      }
+      fileWriter.close();
+
+    } catch (IOException e) {
+      System.out.println("☹ OOPS!!! Something went wrong: " + e.getMessage());
+    }
+  }
 
     private boolean isCommand(String input) {
         String[] inputSplit = input.split(" ");
@@ -65,6 +159,7 @@ public class Cheese {
                 case "deadline":
                     String[] deadlineInfo = taskDescription.split("/by", 2);
                     newTask = new Task('D', deadlineInfo[0].trim(), deadlineInfo[1].trim());
+                    System.out.println(deadlineInfo[0]);
                     break;
                 case "event":
                     String[] eventInfo = taskDescription.split("/from", 2);
@@ -178,13 +273,15 @@ public class Cheese {
         @Override
         public String toString() {
             StringBuilder sb = new StringBuilder();
-            sb.append("[").append(type).append("][").append(isDone ? "X" : " ").append("] ").append(description);
-            if (dateTime1 != null) {
-                sb.append(" (from: ").append(dateTime1);
-                if (dateTime2 != null) {
-                    sb.append(" to: ").append(dateTime2);
-                }
-                sb.append(")");
+            sb.append("[").append(type).append("][").append(isDone ? "X" : " ").append("]|").append(description);
+            if (dateTime2 == null) {
+                sb.append(" (by: ").append(dateTime1).append(")");
+            } else if (dateTime1 != null) {
+              sb.append(" (from: ").append(dateTime1);
+              if (dateTime2 != null) {
+                  sb.append(" to: ").append(dateTime2);
+              }
+              sb.append(")");
             }
             return sb.toString();
         }
---
+++ src/main/java/run_file.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+
+javac Cheese.java
+java Cheese
+
