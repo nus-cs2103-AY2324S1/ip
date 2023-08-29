fd1dcb4eda5bf786af2c22a87c9ba9e9aba8d5b4
longnguyentan <longnguyentan2002@gmail.com>
2023-08-29 21:34:33 +0800

    Use Java enums

--- src/main/java/task/Deadline.java
+++ src/main/java/task/Deadline.java
@@ -1,15 +1,21 @@
 package task;
 
+enum Priority {
+    HIGH, MEDIUM, LOW
+}
+
 public class Deadline extends Task {
     protected String by;
+    protected Priority priority;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        this.priority = null;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by + ") (priority: " + priority + ")";
     }
 }
