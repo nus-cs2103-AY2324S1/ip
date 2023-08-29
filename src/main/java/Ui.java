cd1f8cd7f232deb5daf124ce7bdcf86cfadd161d
Julius Bryan <67666355+juliusgambe@users.noreply.github.com>
2023-08-29 09:42:45 +0800

    Add Ui.java

---
+++ src/main/java/Ui.java
@@ -0,0 +1,40 @@
+public class Ui {
+
+    private final String chatbotName = "Gobble Gobble";
+
+
+    public void printWelcome() {
+        System.out.println(Duke.lineSeparator + "\n" + "Hello! I'm " + this.chatbotName + "\n" +
+                "What can I do for you?" + "\n" + Duke.lineSeparator);
+    }
+
+    public void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public void showLoadingError() {
+        System.out.println("Something went wrong while loading your list.");
+    }
+
+    public void mark(Task task) {
+        System.out.println(Duke.lineSeparator + "\n" + "Nice! I've marked this task as done:" + "\n" +
+                task + "\n" + Duke.lineSeparator);
+    }
+
+    public void unmark(Task task) {
+        System.out.println(Duke.lineSeparator + "\n" + "OK, I've marked this task as not done yet:" + "\n" +
+                task + "\n" + Duke.lineSeparator);
+    }
+
+    public void delete(Task task, int size) {
+        System.out.println(Duke.lineSeparator + "\n" + "Noted. I've removed this task:" + "\n"
+                + task.getDescription() + "\n" + "Now you have " + (size) + " tasks in the list." + "\n"
+                + Duke.lineSeparator);
+    }
+
+    public void addTask(Task task, int size) {
+        System.out.println(Duke.lineSeparator + "\n" + "Got it. I've added this task:" + "\n"
+                + task.toString() + "\n" + "Now you have " + (size) + " tasks in the list." + "\n"
+                + Duke.lineSeparator);
+    }
+}
