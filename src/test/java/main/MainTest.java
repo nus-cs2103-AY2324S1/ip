17eb4887361694dc7e3d4e64c86d11df672e6c78
larrywang0701 <e1075515@u.nus.edu>
2023-08-29 18:11:06 +0800

    A-JUnit

---
+++ src/test/java/main/MainTest.java
@@ -0,0 +1,35 @@
+package main;
+
+import command.CommandException;
+import org.junit.jupiter.api.Test;
+import task.EventTask;
+import task.Task;
+import task.TaskList;
+import util.DateTimeUtil;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+    @Test
+    public void dateTimeParseTest() throws CommandException {
+        try{
+            assertEquals(DateTimeUtil.parseDateTimeString("1970-01-01").toString(), "1970-01-01T00:00");
+            assertEquals(DateTimeUtil.parseDateTimeString("1970-01-01 05:01:03").toString(), "1970-01-01T05:01:03");
+        }
+        catch (Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void exceptionTest(){
+        // Event tasks: start time must <= end time
+        assertThrows(CommandException.class, ()->new EventTask("something", "2023-09-01 00:00:05", "2023-09-01 00:00:03"));
+        assertDoesNotThrow(()->new EventTask("something", "2023-09-01 00:00:03", "2023-09-01 00:00:05"));
+        TaskList testTaskList = new TaskList();
+        // No duplicate named tasks
+        assertDoesNotThrow(()->testTaskList.addTask(new Task("name")));
+        assertThrows(CommandException.class, ()->testTaskList.addTaskAndSay(new Task("name")));
+    }
+}
