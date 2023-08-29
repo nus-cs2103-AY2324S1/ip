666bd9aa2e0b7fee6142cb439dbaff4eaaf5d4a7
jonasongg <jonasong2@gmail.com>
2023-08-29 17:12:39 +0800

    add IllegalArgumentsExceptions for all tasks

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -22,12 +22,37 @@ public class Parser {
                 }
                 return new AddCommand(new Todo(splitInput[1]));
             case "deadline":
+                if (splitInput.length == 1) {
+                    throw new DukeIllegalArgumentsException("The description of a deadline cannot be empty\n");
+                }
+
                 String[] splitInputBy = splitInput[1].split(" /by ", 2);
+
+                if (splitInputBy.length == 1) {
+                    throw new DukeIllegalArgumentsException("A deadline must be specified! (after /by)\n");
+                }
+
                 return new AddCommand(
                         new Deadline(splitInputBy[0], LocalDateTime.parse(splitInputBy[1], dateTimeFormat)));
             case "event":
+                if (splitInput.length == 1) {
+                    throw new DukeIllegalArgumentsException("The description of an event cannot be empty\n");
+                }
+
                 String[] splitInputFrom = splitInput[1].split(" /from ", 2);
+
+                if (splitInputFrom.length == 1) {
+                    throw new DukeIllegalArgumentsException(
+                            "The start time of the event must be specified! (after /from)\n");
+                }
+
                 String[] splitInputTo = splitInputFrom[1].split(" /to ", 2);
+
+                if (splitInputTo.length == 1) {
+                    throw new DukeIllegalArgumentsException(
+                            "The end time of the event must be specified! (after /to)\n");
+                }
+
                 return new AddCommand(
                         new Event(splitInputFrom[0], LocalDateTime.parse(splitInputTo[0], dateTimeFormat),
                                 LocalDateTime.parse(splitInputTo[1], dateTimeFormat)));
