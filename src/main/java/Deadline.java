91f9a41eea5d8899645cf7356ba110f8827fda8e
raynertjx <rayner742@gmail.com>
2023-08-30 17:27:06 +0800

    Add error handling for invalid datetimes

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,28 @@
-public class Deadline extends Task {
-    String deadline;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String deadline) {
+public class Deadline extends Task {
+    LocalDateTime deadline;
+
+    public Deadline(String description, LocalDateTime deadline) {
         super(description);
+        this.validateDeadline(deadline);
         this.deadline = deadline;
     }
 
-    public Deadline(String description, boolean isDone, String deadline) {
+    public Deadline(String description, boolean isDone, LocalDateTime deadline) {
         super(description);
         this.isDone = isDone;
+        this.validateDeadline(deadline);
         this.deadline = deadline;
+    }
+
+    private void validateDeadline(LocalDateTime deadline) {
+        if (deadline.isBefore(LocalDateTime.now())) throw new IllegalArgumentException("Deadline must be in the future.");
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + String.format(" (by: %s)", deadline);
+        return "[D]" + super.toString() + String.format(" (by: %s)", super.generateDateString(this.deadline));
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,21 +1,30 @@
+import java.time.LocalDateTime;
+
 public class Event extends Task {
-    String from;
-    String to;
-    public Event(String description, String from, String to) {
+    LocalDateTime from;
+    LocalDateTime to;
+    public Event(String description, LocalDateTime from, LocalDateTime to) {
         super(description);
+        this.validateEventDuration(from, to);
         this.from = from;
         this.to = to;
     }
 
-    public Event(String description, boolean isDone, String from, String to) {
+    public Event(String description, boolean isDone, LocalDateTime from, LocalDateTime to) {
         super(description);
         this.isDone = isDone;
+        this.validateEventDuration(from, to);
         this.from = from;
         this.to = to;
+    }
+
+    private void validateEventDuration(LocalDateTime from, LocalDateTime to) {
+        if (from.isBefore(LocalDateTime.now()) || to.isBefore(LocalDateTime.now())) throw new IllegalArgumentException("Start and end of event must be in the future.");
+        if (from.isAfter(to)) throw new IllegalArgumentException("End of event must be after start of event.");
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + String.format(" (from: %s to: %s)", from, to);
+        return "[E]" + super.toString() + String.format(" (from: %s to: %s)", super.generateDateString(from), super.generateDateString(to));
     }
 }
