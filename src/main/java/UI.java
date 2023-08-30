2f4101fb9104dd84d3f7a042ee4f2804ed77da40
Derek Tan <derek.tan.jing.xu@gmail.com>
2023-08-30 09:10:28 +0800

    UI class: implement print task added, deleted, marked, unmarked functionality

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -12,13 +12,40 @@ public class UI {
 
     public void handleCommand(ArrayList<String> parsedInput, ArrayList<Task> taskList) {}
 
-    private void printWelcomeMessage() {
+    public void printWelcomeMessage() {
         String name = "Derek";
         System.out.println("Hello! I'm " + this.name);
         System.out.println("What can I do for you?");
     }
 
-    private void printGoodbyeMessage() {
+    public void printGoodbyeMessage() {
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public void printTaskAddedSuccessfullyMessage(Task task, int taskCount) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println(task);
+        this.printTaskCount(taskCount);
+    }
+
+
+    public void printTaskDeletedMessage(Task task, int taskCount) {
+        System.out.println("Noted. I've removed this task:");
+        System.out.println(task);
+        this.printTaskCount(taskCount);
+    }
+
+    public void printTaskMarkedMessage(Task task) {
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(task);
+    }
+
+    public void printTaskUnmarkedMessage(Task task) {
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(task);
+    }
+
+    private void printTaskCount(int taskCount) {
+        System.out.println("Now you have " + taskCount + (taskCount == 1 ? " task" : " tasks") + " in the list.");
     }
 }
