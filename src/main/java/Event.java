32d85e500887bd0ebe04c68b1257328d6ff6406c
MathanC <ncmathan98@gmail.com>
2023-08-30 21:12:36 +0800

    Dates

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,16 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
 
-    private String from;
-    private String to;
+    private LocalDate from;
+    private LocalDate to;
     public Event (String val, String from, String to){
         super(val);
-        this.from=from;
-        this.to = to;
+        this.from=LocalDate.parse(from,DateTimeFormatter.ofPattern("MMM dd yyyy"));
+        this.to = LocalDate.parse(to,DateTimeFormatter.ofPattern("MMM dd yyyy"));
     }
     public String toString(){
-        return "[E]"+super.toString() +" (from: "+from+" to: "+to+")";
+        return "[E]"+super.toString() +" (from: "+from.format(DateTimeFormatter.ofPattern("MMM dd yyyy"))+" to: "+to.format(DateTimeFormatter.ofPattern("MMM dd yyyy"))+")";
     }
 }
