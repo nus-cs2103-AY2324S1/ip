6caa35f84c564842d682cb9267fc3444623194d5
Woo Jia Hao <woojiahao1234@gmail.com>
2023-08-30 20:43:40 +0800

    Format tests

--- src/test/java/cyrus/commands/CommandTypeTest.java
+++ src/test/java/cyrus/commands/CommandTypeTest.java
@@ -7,24 +7,24 @@ import java.util.HashMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CommandTypeTest {
-  @Test
-  public void testFromString() {
-    var expected = new HashMap<String, CommandType>();
-    String[] testStrings = {
-        "bye", "todo", "deadline", "event", "delete", "list", "mark", "unmark", "nonsense"
-    };
-    expected.put("bye", CommandType.BYE);
-    expected.put("todo", CommandType.ADD_TODO);
-    expected.put("deadline", CommandType.ADD_DEADLINE);
-    expected.put("event", CommandType.ADD_EVENT);
-    expected.put("delete", CommandType.DELETE_TASK);
-    expected.put("list", CommandType.LIST_TASKS);
-    expected.put("mark", CommandType.MARK_TASK);
-    expected.put("unmark", CommandType.UNMARK_TASK);
-    expected.put("nonsense", CommandType.UNKNOWN);
+    @Test
+    public void testFromString() {
+        var expected = new HashMap<String, CommandType>();
+        String[] testStrings = {
+                "bye", "todo", "deadline", "event", "delete", "list", "mark", "unmark", "nonsense"
+        };
+        expected.put("bye", CommandType.BYE);
+        expected.put("todo", CommandType.ADD_TODO);
+        expected.put("deadline", CommandType.ADD_DEADLINE);
+        expected.put("event", CommandType.ADD_EVENT);
+        expected.put("delete", CommandType.DELETE_TASK);
+        expected.put("list", CommandType.LIST_TASKS);
+        expected.put("mark", CommandType.MARK_TASK);
+        expected.put("unmark", CommandType.UNMARK_TASK);
+        expected.put("nonsense", CommandType.UNKNOWN);
 
-    for (var test : testStrings) {
-      assertEquals(expected.get(test), CommandType.fromString(test));
+        for (var test : testStrings) {
+            assertEquals(expected.get(test), CommandType.fromString(test));
+        }
     }
-  }
 }
--- src/test/java/cyrus/storage/FileStorageTest.java
+++ src/test/java/cyrus/storage/FileStorageTest.java
@@ -22,66 +22,66 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FileStorageTest {
-  @Test
-  public void testLoadFileCreation() {
-    FileStorage storage = new FileStorage("test_data/test.json");
-    storage.load();
-    File testFile = new File("test_data/test.json");
-    assertTrue(testFile.exists());
-    testFile.deleteOnExit();
-  }
-
-  @Test
-  public void testLoadFileCreationNested() {
-    FileStorage storage = new FileStorage("test_data/nested/test.json");
-    storage.load();
-    File testFile = new File("test_data/nested/test.json");
-    assertTrue(testFile.exists());
-    testFile.deleteOnExit();
-  }
-
-  @Test
-  public void testLoadEmptyFile() throws IOException {
-    File testFile = new File("test_data/test.json");
-    testFile.getParentFile().mkdirs();
-    testFile.createNewFile();
-    FileStorage storage = new FileStorage("test_data/test.json");
-    var tasks = storage.load();
-    assertEquals(0, tasks.size());
-    testFile.deleteOnExit();
-  }
-
-  @Test
-  public void testLoadWithValidData() throws IOException {
-    List<Task> tasks = new ArrayList<>();
-    var formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-    tasks.add(new ToDo("todo task"));
-    tasks.add(new Deadline("deadline task", LocalDate.parse("16/08/2023", formatter)));
-    tasks.add(new Event("event task", LocalDate.parse("16/08/2023", formatter), LocalDate.parse(
-        "19/09/2023", formatter)));
-
-    var gson =
-        new GsonBuilder()
-            .excludeFieldsWithModifiers(Modifier.TRANSIENT)
-            .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
-            .create();
-
-    File testFile = new File("test_data/test.json");
-    testFile.getParentFile().mkdirs();
-    testFile.createNewFile();
-
-    try (BufferedWriter bw = new BufferedWriter(new FileWriter("test_data/test.json"))) {
-      gson.toJson(tasks, bw);
-    } catch (IOException e) {
-      return;
+    @Test
+    public void testLoadFileCreation() {
+        FileStorage storage = new FileStorage("test_data/test.json");
+        storage.load();
+        File testFile = new File("test_data/test.json");
+        assertTrue(testFile.exists());
+        testFile.deleteOnExit();
     }
 
-    FileStorage storage = new FileStorage("test_data/test.json");
-    List<Task> retrievedTasks = storage.load();
-    for (int i = 0; i < retrievedTasks.size(); i++) {
-      assertEquals(tasks.get(i).toString(), retrievedTasks.get(i).toString());
+    @Test
+    public void testLoadFileCreationNested() {
+        FileStorage storage = new FileStorage("test_data/nested/test.json");
+        storage.load();
+        File testFile = new File("test_data/nested/test.json");
+        assertTrue(testFile.exists());
+        testFile.deleteOnExit();
     }
 
-    testFile.deleteOnExit();
-  }
+    @Test
+    public void testLoadEmptyFile() throws IOException {
+        File testFile = new File("test_data/test.json");
+        testFile.getParentFile().mkdirs();
+        testFile.createNewFile();
+        FileStorage storage = new FileStorage("test_data/test.json");
+        var tasks = storage.load();
+        assertEquals(0, tasks.size());
+        testFile.deleteOnExit();
+    }
+
+    @Test
+    public void testLoadWithValidData() throws IOException {
+        List<Task> tasks = new ArrayList<>();
+        var formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        tasks.add(new ToDo("todo task"));
+        tasks.add(new Deadline("deadline task", LocalDate.parse("16/08/2023", formatter)));
+        tasks.add(new Event("event task", LocalDate.parse("16/08/2023", formatter), LocalDate.parse(
+                "19/09/2023", formatter)));
+
+        var gson =
+                new GsonBuilder()
+                        .excludeFieldsWithModifiers(Modifier.TRANSIENT)
+                        .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+                        .create();
+
+        File testFile = new File("test_data/test.json");
+        testFile.getParentFile().mkdirs();
+        testFile.createNewFile();
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("test_data/test.json"))) {
+            gson.toJson(tasks, bw);
+        } catch (IOException e) {
+            return;
+        }
+
+        FileStorage storage = new FileStorage("test_data/test.json");
+        List<Task> retrievedTasks = storage.load();
+        for (int i = 0; i < retrievedTasks.size(); i++) {
+            assertEquals(tasks.get(i).toString(), retrievedTasks.get(i).toString());
+        }
+
+        testFile.deleteOnExit();
+    }
 }
