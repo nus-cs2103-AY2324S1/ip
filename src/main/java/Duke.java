ff81b562bfb66ffb374ca339a95ebf676f6c39c8
Jie Xiang <n.jiexiang@gmail.com>
2023-08-31 00:19:04 +0800

    Incorporate Ui class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,10 +10,6 @@ import java.util.regex.Pattern;
  * A chatbot that helps you keep track of tasks.
  */
 public class Duke {
-    private static final String line = "____________________________________________________________\n";
-    private static final String greetingMessage = "Hello! I'm EnPassant\n"
-                                                + "What can I do for you?\n";
-    private static final String exitMessage = "Bye! Hope to see you again soon!\n";
     private static final String invalidIndexMessage = "Great heavens! You have entered an invalid index!\n";
     private static final String invalidInputMessage = "Great heavens! You have entered an invalid input!\n"
             + "Here are the valid commands:\n"
@@ -36,72 +32,14 @@ public class Duke {
             + "Usage: event <description> /from <start> /to <end>\n";
     private static final String invalidDeleteMessage = "Great heavens! The index of delete cannot be empty!\n"
             + "Usage: delete <index>\n";
-    private static final String markDoneMessage = "Great success! I have marked this task as done:\n";
-    private static final String markUndoneMessage = "Very nice! I have marked this task as not done yet:\n";
-    private static final String listMessage = "Here are the tasks in your list:\n";
     private static final String regexPattern = "\\b(bye|list|unmark|mark|todo|deadline|event|delete)\\s*" // match command
             + "([^/]*[^/\\s])?\\s*"                     // match chars that are not / after command, trimming whitespace
             + "(?:(/by|/from)\\s+([^/]*[^/\\s]))?\\s*"  // match /by or /from command and argument, trimming whitespace
             + "(?:(/to)\\s+([^/]*[^/\\s]))?\\s*";       // match /to command and argument, trimming whitespace
-    private static final String newTaskAddedMessage = "New task just dropped!\n";
-    private static final String taskDeletedMessage = "Task went on vacation, never came back.\n";
-    private static final String totalTaskCountMessage = "You now have %d tasks in the list!\n";
-
-
-    /**
-     * Helper method to print a message with horizontal lines above and below it.
-     *
-     * @param message The message to be printed.
-     */
-    private static void printWithLines(String message) {
-        System.out.print(line + message + line);
-    }
-
-    /**
-     * Helper method to print all the Tasks in the list with their respective indices,
-     * surrounded by horizontal lines.
-     *
-     * @param list The list containing the tasks.
-     */
-    private static void printList(ArrayList<Task> list) {
-        System.out.print(line + listMessage);
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println((i + 1) + ". " + list.get(i));
-        }
-        System.out.print(line);
-    }
-
-    /**
-     * Helper method to print the new task added along with its associated message,
-     * the new count of tasks, surrounded by horizontal lines.
-     *
-     * @param task The new task added.
-     * @param totalTasks The total count of tasks in the list.
-     */
-    private static void printTaskAdded(Task task, int totalTasks) {
-        System.out.print(line + newTaskAddedMessage);
-        System.out.print("  " + task + "\n");
-        System.out.printf(totalTaskCountMessage, totalTasks);
-        System.out.print(line);
-    }
-
-    /**
-     * Helper method to print the task deleted along with its associated message,
-     * the new count of tasks, surrounded by horizontal lines.
-     *
-     * @param task The deleted task.
-     * @param totalTasks The total count of tasks in the list.
-     */
-    private static void printTaskDeleted(Task task, int totalTasks) {
-        System.out.print(line + taskDeletedMessage);
-        System.out.print("  " + task + "\n");
-        System.out.printf(totalTaskCountMessage, totalTasks);
-        System.out.print(line);
-    }
 
     public static void main(String[] args) {
         Storage storage = new Storage("./data/duke.txt");
-        Scanner sc = new Scanner(System.in);
+        Ui ui = new Ui();
         Pattern pattern = Pattern.compile(regexPattern);
         ArrayList<Task> tasks;
         try {
@@ -110,68 +48,70 @@ public class Duke {
             System.out.println(e.getMessage());
             tasks = new ArrayList<>();
         }
-        printWithLines(greetingMessage);
+        ui.showGreeting();
 
         while (true) {
             try {
-                String str = sc.nextLine();
-                Matcher matcher = pattern.matcher(str);
+                String command = ui.readCommand();
+                Matcher matcher = pattern.matcher(command);
                 int index;
-                Task newTask;
+                Task newTask, currentTask;
 
                 if (!matcher.matches()) {
                     // no match means input is not valid
-                    throw new DukeException(line + invalidInputMessage + line);
+                    throw new DukeException(Ui.line + invalidInputMessage + Ui.line);
                 }
 
                 if (matcher.group(1).equals("bye")) {
-                    printWithLines(exitMessage);
+                    ui.showExitMessage();
                     break;
                 }
 
                 // match against the command given
                 switch (matcher.group(1)) {
                     case "list":
-                        printList(tasks);
+                        ui.showList(tasks);
                         break;
                     case "mark":
                         if (matcher.group(2) == null) {
-                            throw new DukeException(line + invalidMarkMessage + line);
+                            throw new DukeException(Ui.line + invalidMarkMessage + Ui.line);
                         }
 
                         index = Integer.parseInt(matcher.group(2));
 
                         if (index < 1 || index > tasks.size()) {
-                            throw new DukeException(line + invalidIndexMessage + line);
+                            throw new DukeException(Ui.line + invalidIndexMessage + Ui.line);
                         }
 
-                        tasks.get(index - 1).markAsDone();
-                        printWithLines(markDoneMessage + "  " + tasks.get(index - 1) + '\n');
+                        currentTask = tasks.get(index - 1);
+                        currentTask.markAsDone();
+                        ui.showTaskDone(currentTask);
                         storage.saveTasks(tasks);
                         break;
                     case "unmark":
                         if (matcher.group(2) == null) {
-                            throw new DukeException(line + invalidUnmarkMessage + line);
+                            throw new DukeException(Ui.line + invalidUnmarkMessage + Ui.line);
                         }
 
                         index = Integer.parseInt(matcher.group(2));
 
                         if (index < 1 || index > tasks.size()) {
-                            throw new DukeException(line + invalidIndexMessage + line);
+                            throw new DukeException(Ui.line + invalidIndexMessage + Ui.line);
                         }
 
-                        tasks.get(index - 1).markAsUndone();
-                        printWithLines(markUndoneMessage + "  " + tasks.get(index - 1) + '\n');
+                        currentTask = tasks.get(index - 1);
+                        currentTask.markAsUndone();
+                        ui.showTaskUndone(currentTask);
                         storage.saveTasks(tasks);
                         break;
                     case "todo":
                         if (matcher.group(2) == null) {
-                            throw new DukeException(line + invalidTodoMessage + line);
+                            throw new DukeException(Ui.line + invalidTodoMessage + Ui.line);
                         }
 
                         newTask = new Todo(matcher.group(2));
                         tasks.add(newTask);
-                        printTaskAdded(newTask, tasks.size());
+                        ui.showTaskAdded(newTask, tasks.size());
                         storage.saveTasks(tasks);
                         break;
                     case "deadline":
@@ -179,7 +119,7 @@ public class Duke {
                                 || matcher.group(3) == null
                                 || !matcher.group(3).equals("/by")
                                 || matcher.group(4) == null) {
-                            throw new DukeException(line + invalidDeadlineMessage + line);
+                            throw new DukeException(Ui.line + invalidDeadlineMessage + Ui.line);
                         }
 
                         LocalDateTime parsedDate;
@@ -187,12 +127,12 @@ public class Duke {
                             parsedDate = LocalDateTime.parse(matcher.group(4),
                                     DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
                         } catch (DateTimeParseException e) {
-                            throw new DukeException(line + invalidDeadlineMessage + line);
+                            throw new DukeException(Ui.line + invalidDeadlineMessage + Ui.line);
                         }
 
                         newTask = new Deadline(matcher.group(2), parsedDate);
                         tasks.add(newTask);
-                        printTaskAdded(newTask, tasks.size());
+                        ui.showTaskAdded(newTask, tasks.size());
                         storage.saveTasks(tasks);
                         break;
                     case "event":
@@ -203,31 +143,31 @@ public class Duke {
                                 || matcher.group(5) == null
                                 || !matcher.group(5).equals("/to")
                                 || matcher.group(6) == null) {
-                            throw new DukeException(line + invalidEventMessage + line);
+                            throw new DukeException(Ui.line + invalidEventMessage + Ui.line);
                         }
 
                         newTask = new Event(matcher.group(2), matcher.group(4), matcher.group(6));
                         tasks.add(newTask);
-                        printTaskAdded(newTask, tasks.size());
+                        ui.showTaskAdded(newTask, tasks.size());
                         storage.saveTasks(tasks);
                         break;
                     case "delete":
                         if (matcher.group(2) == null) {
-                            throw new DukeException(line + invalidDeleteMessage + line);
+                            throw new DukeException(Ui.line + invalidDeleteMessage + Ui.line);
                         }
 
                         index = Integer.parseInt(matcher.group(2));
 
                         if (index < 1 || index > tasks.size()) {
-                            throw new DukeException(line + invalidIndexMessage + line);
+                            throw new DukeException(Ui.line + invalidIndexMessage + Ui.line);
                         }
 
                         Task removed = tasks.remove(index - 1);
-                        printTaskDeleted(removed, tasks.size());
+                        ui.showTaskDeleted(removed, tasks.size());
                         storage.saveTasks(tasks);
                         break;
                     default:
-                        throw new DukeException(line + invalidInputMessage + line);
+                        throw new DukeException(Ui.line + invalidInputMessage + Ui.line);
                 }
             } catch (DukeException e) {
                 System.out.print(e.getMessage());
