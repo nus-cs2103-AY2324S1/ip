c450e72272b8ec712e01c8d8d8c8e883703b3828
raynertjx <rayner742@gmail.com>
2023-08-30 11:49:30 +0800

    Rename files from Duke to Bongo

--- src/main/java/DukeException.java
+++
@@ -1,8 +0,0 @@
-public class DukeException extends Exception {
-    public DukeException() {
-        super("OOPS!!! I'm sorry, but I don't know what that means :-(");
-    }
-    public DukeException(String message) {
-        super(message);
-    }
-}
