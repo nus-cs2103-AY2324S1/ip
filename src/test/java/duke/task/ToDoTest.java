70eeaf298e9da1f42a491a80241c81e2e401ed12
Jeffrey <jeffreyjianyujie@gmail.com>
2023-08-30 20:36:34 +0800

    Add JUnit ToDoTest

---
+++ src/test/java/duke/task/ToDoTest.java
@@ -0,0 +1,26 @@
+package duke.task;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class ToDoTest {
+
+    @Test
+    public void task_todoString_correctlyParsed() {
+        ToDo todo = new ToDo("Test Todo", false);
+        Assertions.assertEquals("[T][ ] Test Todo", todo.toString());
+    }
+
+    @Test
+    public void task_todoDataString_correctlyParsed() {
+        ToDo todo = new ToDo("Test Todo", true);
+        Assertions.assertEquals("T|1|Test Todo", todo.toDataString());
+    }
+
+    @Test
+    public void task_markToDo_marked() {
+        ToDo todo = new ToDo("Test Todo", false);
+        todo.markDone();
+        Assertions.assertEquals("[T][X] Test Todo", todo.toString());
+    }
+}
