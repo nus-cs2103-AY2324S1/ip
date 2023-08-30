f4ef71591f4fad985bd4c14371234fd46f3065ba
papataco14 <rithikvj@gmail.com>
2023-08-30 11:27:12 +0800

    Rename, Greet, Exit
    - Gave chatbot a new name
    - Implement an initial skeletal version that greets and exits

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "Chad";
+        String welcomeMessage = String.format("Hello! I'm %s!\nWhat do you want?", name);
+        String closingMessage = String.format("Bye. Hope to see you again soon!");
+        System.out.println(welcomeMessage);
+        System.out.println("______________________________________");
+        System.out.println(closingMessage);
+
     }
 }
