175f6f0fe2a6179b478751fe83d6dea7a08d3f68
weeweh <94617397+weeweh@users.noreply.github.com>
2023-08-31 00:39:47 +0800

    Add Find command

--- src/main/java/command/AddCommand.java
+++ src/main/java/command/AddCommand.java
@@ -3,6 +3,7 @@ package command;
 import java.time.LocalDate;
 
 import exception.DukeException;
+import exception.InvalidCommandException;
 import helper.Storage;
 import helper.Ui;
 import task.Deadline;
@@ -33,31 +34,37 @@ public class AddCommand extends Command {
      * a Task Object.
      * @param input
      * @return a Task
+     * @throws InvalidCommandException
      */
-    private Task parseTask(String input) {
-        if (input.startsWith("todo")) {
-            String description = input.split("todo", 2)[1].strip();
-            ToDo todo = new ToDo(description);
+    private Task parseTask(String input) throws InvalidCommandException {
+        try {
+            if (input.startsWith("todo")) {
+                String description = input.split("todo", 2)[1].strip();
+                ToDo todo = new ToDo(description);
 
-            return todo;
-        } else if (input.startsWith("deadline")) {
-            String[] parts = input.split("deadline", 2);
-            String description = parts[1].split("/by", 2)[0].strip();
-            String by = parts[1].split("/by", 2)[1].strip();
-            LocalDate date = LocalDate.parse(by);
-            Deadline deadline = new Deadline(description, date);
+                return todo;
+            } else if (input.startsWith("deadline")) {
+                String[] parts = input.split("deadline", 2);
+                String description = parts[1].split("/by", 2)[0].strip();
+                String by = parts[1].split("/by", 2)[1].strip();
+                LocalDate date = LocalDate.parse(by);
+                Deadline deadline = new Deadline(description, date);
 
-            return deadline;
-        } else {
-            String[] parts = input.split("event", 2);
-            String description = parts[1].split("/from", 2)[0].strip();
-            String time = parts[1].split("/from", 2)[1];
-            String from = time.split("/to", 2)[0].strip();
-            String to = time.split("/to", 2)[1].strip();
-            Event event = new Event(description, from, to);
+                return deadline;
+            } else {
+                String[] parts = input.split("event", 2);
+                String description = parts[1].split("/from", 2)[0].strip();
+                String time = parts[1].split("/from", 2)[1];
+                String from = time.split("/to", 2)[0].strip();
+                String to = time.split("/to", 2)[1].strip();
+                Event event = new Event(description, from, to);
 
-            return event;
+                return event;
+            }
+        } catch (Exception e) {
+            throw new InvalidCommandException("Wrong way to use the command bruv!");
         }
+
     }
 
     /**
@@ -68,7 +75,7 @@ public class AddCommand extends Command {
      * @throws DukeException
      */
     @Override
-    public void execute(TaskList list, Ui ui, Storage storage) {
+    public void execute(TaskList list, Ui ui, Storage storage) throws DukeException {
         String message = list.add(parseTask(this.task));
         ui.print(message);
     }
---
+++ src/main/java/command/FindCommand.java
@@ -0,0 +1,35 @@
+package command;
+
+import helper.Storage;
+import helper.Ui;
+import task.TaskList;
+
+public class FindCommand extends Command {
+    /** Command the user starts with to activate the FindCommand. */
+    public static final String COMMAND_WORD = "find";
+    /** Substring to find in description of Task. */
+    private String substring;
+
+    /**
+     * Constructs a FindCommand that finds a List of Tasks with description
+     * containing the substring.
+     * @param index
+     * @param substring
+     */
+    public FindCommand(int index, String substring) {
+        super(index);
+        this.substring = substring;
+    }
+
+    /**
+     * Prints out all Tasks that contain the substring specified by the User in its description.
+     * @param list
+     * @param ui
+     * @param storage
+     */
+    @Override
+    public void execute(TaskList list, Ui ui, Storage storage) {
+        String message = list.find(substring);
+        ui.print(message);
+    }
+}
