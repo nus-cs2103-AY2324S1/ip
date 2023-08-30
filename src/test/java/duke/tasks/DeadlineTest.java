0f6ead730cb11d94d965850462f0af595ee4c856
ivyy-poison <tan.yuhao.ivan@gmail.com>
2023-08-30 10:42:02 +0800

    Implement JUnit tests for ToDo and Deadline classes

---
+++ src/test/java/duke/tasks/DeadlineTest.java
@@ -0,0 +1,64 @@
+package duke.tasks;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+    
+    @Test
+    public void toStorageString_stringRepresentation_success() {
+        Deadline deadline = new Deadline ("read book", "23 Aug 2020 2359");
+        assertEquals("D | 0 | read book | 23 Aug 2020 2359", deadline.toStorageString());
+    }
+
+    @Test
+    public void toString_stringRepresentation_success() {
+        Deadline deadline = new Deadline ("read book", "23 Aug 2020 2359");
+        assertEquals("[D][ ] read book (by: 23-59-2020 23:59)", deadline.toString());
+    }
+
+    @Test
+    public void createDeadlineFromCommand_createDeadlineObject_success() throws Exception {
+        Deadline deadline = Deadline.createDeadlineFromCommand("deadline read book /by 23 Aug 2020 2359");
+        assertEquals("[D][ ] read book (by: 23-59-2020 23:59)", deadline.toString());
+    }
+
+    @Test
+    public void createDeadlineFromCommand_illegalArgument_exceptionThrown() {
+        try {
+            Deadline deadline = Deadline.createDeadlineFromCommand("deadline");
+        } catch (Exception e) {
+            assertEquals("The description of a deadline cannot be empty.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void createDeadlineFromCommand_illegalArgument_exceptionThrown2() {
+        try {
+            Deadline deadline = Deadline.createDeadlineFromCommand("deadline read book");
+        } catch (Exception e) {
+            assertEquals("The deadline command must contain a /by.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void createDeadlineFromCommand_illegalArgument_exceptionThrown3() {
+        try {
+            Deadline deadline = Deadline.createDeadlineFromCommand("deadline read book /by ");
+        } catch (Exception e) {
+            assertEquals("The deadline command must contain a description after /by.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void createDeadlineFromStorage_createDeadlineObject_success() {
+        Deadline deadline = Deadline.createDeadlineFromStorage("D | 0 | read book | 23 Aug 2020 2359");
+        assertEquals("[D][ ] read book (by: 23-59-2020 23:59)", deadline.toString());
+    }
+
+    @Test
+    public void createDeadlineFromStorage_createDoneDeadlineObject_success() {
+        Deadline deadline = Deadline.createDeadlineFromStorage("D | 1 | read book | 23 Aug 2020 2359");
+        assertEquals("[D][X] read book (by: 23-59-2020 23:59)", deadline.toString());
+    }
+
+}
---
+++ src/test/java/duke/tasks/ToDoTest.java
@@ -0,0 +1,47 @@
+package duke.tasks;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ToDoTest {
+
+    @Test
+    public void toStorageString_stringRepresentation_success() {
+        ToDo todo = new ToDo ("read book");
+        assertEquals("T | 0 | read book", todo.toStorageString());
+    }
+
+    @Test
+    public void toString_stringRepresentation_success() {
+        ToDo todo = new ToDo ("read book");
+        assertEquals("[T][ ] read book", todo.toString());
+    }
+
+    @Test
+    public void createToDoFromCommand_createToDoObject_success() throws Exception {
+        ToDo todo = ToDo.createToDoFromCommand("todo read book");
+        assertEquals("[T][ ] read book", todo.toString());
+    }
+
+    @Test
+    public void createToDoFromCommand_illegalArgument_exceptionThrown() {
+        try {
+            ToDo todo = ToDo.createToDoFromCommand("todo");
+        } catch (Exception e) {
+            assertEquals("The description of a todo cannot be empty.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void createToDoFromStorage_createToDoObject_success() {
+        ToDo todo = ToDo.createToDoFromStorage("T | 0 | read book");
+        assertEquals("[T][ ] read book", todo.toString());
+    }
+
+    @Test
+    public void createToDoFromStorage_createDoneToDoObject_success() {
+        ToDo todo = ToDo.createToDoFromStorage("T | 1 | read book");
+        assertEquals("[T][X] read book", todo.toString());
+    }
+
+}
