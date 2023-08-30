8fdc9a70956f02c4ea1d2625bbcf620704cfef0e
ji-just-ji <wongjk9999@gmail.com>
2023-08-31 00:05:56 +0800

    Add JavaDoc for Peko class

--- src/main/java/peko/Peko.java
+++ src/main/java/peko/Peko.java
@@ -7,9 +7,22 @@ public class Peko {
     private UserInputHandler userInputHandler;
     private UI ui;
     private StorageHandler storageHandler;
+
+    /**
+     * The main entry point of the program.
+     * This method initializes an instance of the Peko class and invokes its run method.
+     *
+     * @param args The command-line arguments passed to the program.
+     */
     public static void main(String[] args) {
         new Peko().run();
     }
+
+    /**
+     * Initiates the main execution loop of the program.
+     * This method displays an introduction, processes user input in a loop,
+     * saves data as needed, and displays an exit message upon completion.
+     */
     private void run() {
         Output.intro();
         while (true) {
@@ -22,6 +35,11 @@ public class Peko {
         Output.exit();
 
     }
+
+    /**
+     * Private constructor for the Peko class.
+     * Initializes necessary handlers for user input and data storage.
+     */
     private Peko() {
         userInputHandler = new UserInputHandler();
         storageHandler = new StorageHandler();
