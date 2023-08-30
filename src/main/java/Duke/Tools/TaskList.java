1e5d667977eafd7c673d539455559acfa22b26fe
rayson <raysonyeap4c@gmail.com>
2023-08-31 01:21:11 +0800

    Add JavaDoc for tools

--- src/main/java/Duke/Tools/TaskList.java
+++ src/main/java/Duke/Tools/TaskList.java
@@ -5,41 +5,76 @@ import Duke.Tasks.Task;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a list of tasks and provides methods to manipulate the list.
+ */
 public class TaskList {
 
-    private List<Task> tasks;
+    private final List<Task> tasks;
 
+    /**
+     * Constructs an empty TaskList.
+     */
     public TaskList() {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Constructs a TaskList with the given list of tasks.
+     *
+     * @param tasks The initial list of tasks.
+     */
     public TaskList(List<Task> tasks) {
         this.tasks = tasks;
     }
 
+    /**
+     * Adds a task to the task list.
+     *
+     * @param task The task to be added.
+     */
     public void add(Task task) {
         this.tasks.add(task);
     }
 
+    /**
+     * Removes a task from the task list at the specified index.
+     *
+     * @param index The index of the task to be removed.
+     */
     public void remove(int index) {
         this.tasks.remove(index);
     }
 
+    /**
+     * Returns the number of tasks in the task list.
+     *
+     * @return The number of tasks.
+     */
     public int size() {
         return this.tasks.size();
     }
 
+    /**
+     * Retrieves a task from the task list at the specified index.
+     *
+     * @param index The index of the task to be retrieved.
+     * @return The task at the specified index.
+     */
     public Task get(int index) {
         return this.tasks.get(index);
     }
 
+    /**
+     * Retrieves the formatted text representation of tasks for storage.
+     *
+     * @return The formatted text representation of tasks.
+     */
     public String retrieveForStorage() {
-
         StringBuilder textForStorage = new StringBuilder();
         for (Task task : tasks) {
             textForStorage.append(task.formatForStorage()).append("\n");
         }
         return textForStorage.toString();
     }
-
 }
