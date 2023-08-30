dc9135aa3f4fe0a20357528f3065e139c7fe6e67
AriellaCallista <arielgit>
2023-08-30 13:14:22 +0800

    Implement HelpCommand for invalid command words

--- src/main/java/HelpCommand.java
+++ src/main/java/HelpCommand.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class HelpCommand {
+public class HelpCommand extends Command {
+
+    public HelpCommand(String cmd, String arguments) {
+        super(cmd, arguments);
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        System.out.println(super.cmd + " is not a valid command!");
+    }
 }
