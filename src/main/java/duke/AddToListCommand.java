811c258e44bae8e9bf53193dc04f2d5158cbaf28
Yarn Meng <e0969262@u.nus.edu>
2023-08-30 11:51:14 +0800

    Add tests for Parser mark and unmark

--- src/main/java/duke/AddToListCommand.java
+++ src/main/java/duke/AddToListCommand.java
@@ -18,4 +18,15 @@ public class AddToListCommand extends Command {
                 + "\nNow you have " + list.getLength() + " tasks in the list.");
         storage.writeToSave(list);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof AddToListCommand)) {
+            return false;
+        }
+         //checked above
+        @SuppressWarnings({"unchecked"})
+        AddToListCommand c = (AddToListCommand) o;
+        return c.queries.equals(this.queries) && c.type.equals(this.type);
+    }
 }
--- src/main/java/duke/ExitCommand.java
+++ src/main/java/duke/ExitCommand.java
@@ -3,4 +3,9 @@ public class ExitCommand extends Command{
     @Override
     public void execute(ChatBotList list, Ui ui, Storage storage) throws ChatBotListException{
     }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof ExitCommand;
+    }
 }
