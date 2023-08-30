aa5525f6b5b502d76b727919a0845076e68722f0
raynertjx <rayner742@gmail.com>
2023-08-31 01:58:53 +0800

    Remove error handling and shift it to Command instances

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,52 +1,36 @@
 import java.util.Scanner;
 
 public class Parser {
+    public Parser() {
+    }
 
-    private void parse() {
-        label:
-        while (this.inputScanner.hasNext()) {
-            String[] input = inputScanner.nextLine().split(" ");
-            try {
-                switch (input[0]) {
-                    case "bye":
-                        this.bye();
-                        break label;
-                    case "list":
-                        this.listAllTasks();
-                        break;
-                    case "mark":
-                    case "unmark":
-                    case "delete":
-                        if (tasks.size() == 0) throw new BongoException("OOPS!!! There are currently no tasks.");
-                        if (input.length < 2) throw new BongoException("OOPS!!! Please include the task index.");
-                        int taskIndex = Integer.parseInt(input[1]) - 1;
-                        if (taskIndex < 0 || taskIndex >= tasks.size())
-                            throw new BongoException("OOPS!!! Task does not exist.");
-                        if (input[0].equals("mark")) {
-                            this.markTaskDone(taskIndex);
-                        } else if (input[0].equals("unmark")) {
-                            this.markTaskUndone(taskIndex);
-                        } else {
-                            this.deleteTask(taskIndex);
-                        }
-                        continue;
-                    case "todo":
-                        this.addTodo(input);
-                        break;
-                    case "deadline": {
-                        this.addDeadline(input);
-                        break;
-                    }
-                    case "event": {
-                        this.addEvent(input);
-                        break;
-                    }
-                    default:
-                        throw new BongoException();
+    ;
+
+    protected Command parse(String command) throws BongoException {
+        String[] input = command.split(" ");
+        switch (input[0]) {
+            case "bye":
+                return new ExitCommand();
+            case "list":
+                return new ListCommand();
+            case "mark":
+            case "unmark":
+            case "delete":
+                if (input.length <= 1) throw new BongoException("Please include the task index.");
+                int taskIndex = Integer.parseInt(input[1]) - 1;
+                if (input[0].equals("mark")) {
+                    return new MarkCommand(taskIndex);
+                } else if (input[0].equals("unmark")) {
+                    return new UnmarkCommand(taskIndex);
+                } else if (input[0].equals("delete")) {
+                    return new DeleteCommand(taskIndex);
                 }
-            } catch (BongoException e) {
-//                System.out.println(printError(e.getMessage()));
-            }
+            case "todo":
+            case "deadline":
+            case "event":
+                return new AddCommand(input);
+            default:
+                throw new BongoException();
         }
     }
 }
