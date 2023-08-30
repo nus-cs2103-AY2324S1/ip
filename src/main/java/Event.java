125a592dddb24c063abc92765800dab8804ff292
mingyu <wanmingyu0318@gmail.com>
2023-08-30 18:42:25 +0800

    Level-8 Change to Time and Date for deadline

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -23,6 +23,7 @@ public class Event extends Task {
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from:" + from + " to:" + to + ")";
+
     }
 
     @Override
