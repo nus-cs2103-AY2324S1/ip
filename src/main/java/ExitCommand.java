e8362aa49f2738fca2c91bfa69e3abd094f56ebb
Jie Xiang <n.jiexiang@gmail.com>
2023-08-31 00:50:12 +0800

    Implement ExitCommand

---
+++ src/main/java/ExitCommand.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ExitCommand extends Command {
+    @Override
+    public boolean isExit() {
+        return true;
+    }
+
+    @Override
+    public void execute(ArrayList<Task> tasks, Ui ui, Storage storage) {
+        ui.showExitMessage();
+    }
+}
