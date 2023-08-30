93082a585b031ca5e70601d00988fab3fa589842
AriellaCallista <arielgit>
2023-08-30 18:36:39 +0800

    Task: Implement getTask

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,6 @@
-public class Task {
+import java.time.LocalDate;
+
+public abstract class Task {
     protected String description;
     protected boolean isDone;
 
@@ -7,9 +9,9 @@ public class Task {
      *
      * @param description of task.
      */
-    public Task(String description) {
+    public Task(String description, boolean isDone) {
         this.description = description;
-        this.isDone = false;
+        this.isDone = isDone;
     }
 
     /**
@@ -41,6 +43,48 @@ public class Task {
      */
     public String toString() {
         return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+
+    /**
+     * Returns the formatted task string to be saved in the hard disk.
+     * @return formatted task string to be saved in the hard disk.
+     */
+    public String formatTask() {
+        return " | " + (isDone ? 1 : 0) + " | " + description;
+    }
+
+    /**
+     * Given a formatted task string, it returns the Task equivalent of that string.
+     * @param formattedTask task string that has been formatted in a specific manner from the harddisk.
+     * @return task equivalence of the formatted task string.
+     */
+    public static Task getTask(String formattedTask) {
+        char type = formattedTask.charAt(0);
+        boolean isDone = (formattedTask.charAt(4) == 1? true : false);
+        String description = formattedTask.substring(8);
+
+        switch (type) {
+        case 'T':
+            return new Todo(description, isDone);
+        case 'D':
+            int lastDescId = description.indexOf("|");
+            LocalDate by = LocalDate.parse(description.substring(lastDescId + 2));
+            description = description.substring(0, lastDescId - 1);
+            return new Deadline(description, by, isDone);
+        case 'E':
+            String temp = description;
+            lastDescId = description.indexOf('|');
+            description = description.substring(0, lastDescId - 1);
+            int lastFromId = temp.indexOf('|', lastDescId + 1);
+
+            LocalDate from = LocalDate.parse(temp.substring(lastDescId + 2, lastFromId - 1));
+
+
+            LocalDate to = LocalDate.parse(temp.substring(lastFromId + 2));
+            return new Event(description, from, to, isDone);
+        default:
+            return null;
+        }
     }
 
 
