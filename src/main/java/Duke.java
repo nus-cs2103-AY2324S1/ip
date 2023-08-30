fedfbdec231d900841404fa0a31d85ef5ddb4af9
Tang-Moyan <e0817857@u.nus.edu>
2023-08-30 09:57:26 +0800

    Added Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static void printOneLine() {
+        System.out.println("---------------------------");
+    }
+
     private static final String MyName = "Rio";
     public static void Greet() {
+        printOneLine();
         System.out.println("Hello! I'm " + MyName);
         System.out.println("What can I do for you?");
+        printOneLine();
     }
 
     public  static void Exit() {
+        printOneLine();
         System.out.println(" Bye. Hope to see you again soon!");
+        printOneLine();
     }
+
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,5 +27,26 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+
+        Greet();
+
+        boolean wantToExit = false;
+        Scanner getUserInput = new Scanner(System.in);
+        while (!(wantToExit)) {
+            String userInput = getUserInput.nextLine();
+            if (userInput.equals("bye")) {
+                wantToExit = true;
+                Exit();
+            } else {
+                printOneLine();
+                System.out.println(userInput);
+                printOneLine();
+            }
+        }
+
+
+
+
     }
 }
