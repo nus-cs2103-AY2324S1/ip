25122db521667f1de73f741d29d9d7efe902337d
Elgin <ElginTZM+Github@gmail.com>
2023-08-30 15:55:58 +0000

    Update Duke and ExitCommand class

--- src/main/java/ExitCommand.java
+++ src/main/java/ExitCommand.java
@@ -3,5 +3,6 @@ public class ExitCommand extends Command {
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         Ui.respond("Bye. Hope to see you again soon!");
+        Duke.stop();
     }
 }
