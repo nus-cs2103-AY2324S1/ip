fa8fa6235c8a62d3b68cfff99930beb558caf969
AriellaCallista <arielgit>
2023-08-30 18:38:53 +0800

    Refactor tasksList to be an array list of tasks

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 
 public class TaskList {
 
-    private ArrayList<String> tasksList = new ArrayList<>();
+    private ArrayList<Task> tasksList = new ArrayList<>();
 
     public TaskList(String tasks) {
         Scanner scanner = new Scanner(tasks);
         while(scanner.hasNextLine()) {
-            String task = scanner.nextLine();
-            tasksList.add(task);
+            String formattedTask = scanner.nextLine();
+            tasksList.add(Task.getTask(formattedTask));
         }
     }
 
@@ -21,8 +21,18 @@ public class TaskList {
         StringBuilder task = new StringBuilder();
         for (int i = 0; i < tasksList.size(); i++) {
             int id = i + 1;
-            task.append("   " + id + "." + tasksList.get(i).toString() + "\n");
+            task.append(id + "." + tasksList.get(i).toString() + "\n");
         }
         return("Here are the tasks in your list: \n" + task);
     }
+
+    public void add(Task task) {
+        tasksList.add(task);
+    }
+
+
+
+//    public void mark(int id) {
+//        tasksList
+//    }
 }
