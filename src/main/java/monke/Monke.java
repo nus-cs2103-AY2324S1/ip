73f50770d6e2b0708bb5aa085edf35261fb63fe3
jingjie88 <tanjingjie88@gmail.com>
2023-08-31 06:28:59 +0800

    Add Gradle

--- src/main/java/monke/Monke.java
+++ src/main/java/monke/Monke.java
@@ -1,16 +1,6 @@
 package monke;
 
 import monke.commands.Command;
-import monke.commands.ListCommand;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class Monke {
 
