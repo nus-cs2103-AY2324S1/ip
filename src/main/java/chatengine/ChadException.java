745a5f1c50b05a5156100ca6c632ae834605815d
papataco14 <rithikvj@gmail.com>
2023-08-30 22:34:22 +0800

    Added error handling
    - refactored code to allow for better error handling
    - added three types of exceptions common for chadbot: invalid commands, arguments, and format

---
+++ src/main/java/chatengine/ChadException.java
@@ -0,0 +1,25 @@
+package chatengine;
+
+public class ChadException extends Exception {
+    public ChadException(String message) {
+        super(message);
+    }
+
+    public static class InvalidCommandException extends ChadException {
+        public InvalidCommandException(String message) {
+            super(message);
+        }
+    }
+
+    public static class InvalidArgumentException extends ChadException {
+        public InvalidArgumentException(String message) {
+            super(message);
+        }
+    }
+
+    public static class InvalidFormatException extends ChadException {
+        public InvalidFormatException(String message) {
+            super(message);
+        }
+    }
+}
--- src/main/java/chatengine/ChatEngine.java
+++ src/main/java/chatengine/ChatEngine.java
@@ -8,7 +8,6 @@ public class ChatEngine {
     private final IOHandler ioHandler;
     private final TaskList taskList; // stores list of tasks
 
-
     public ChatEngine() {
         this.ioHandler = new ConsoleIO();
         this.taskList = new TaskList();
@@ -18,40 +17,71 @@ public class ChatEngine {
         ioHandler.greet();
         String input;
         while(!(input = ioHandler.readInput()).equals("bye")) {
-            processInput(input);
+            try {
+                processInput(input);
+            } catch (ChadException e) {
+                ioHandler.writeOutput("Error: " + e.getMessage());
+            }
         }
         ioHandler.sayGoodbye();
     }
 
-    private void processInput(String input) {
+    private void processInput(String input) throws ChadException {
+        if (input.trim().isEmpty()) {
+            throw new ChadException.InvalidArgumentException("Input cannot be empty.");
+        }
+
         String[] parts = input.split(" ", 2);
         String command = parts[0];
 
-        if ("mark".equalsIgnoreCase(command) && parts.length > 1) {
-            int index = Integer.parseInt(parts[1]) - 1;  // Convert to zero-based index
-            String response = taskList.markTaskAsDone(index);
-            ioHandler.writeOutput(response);
-        } else if ("unmark".equalsIgnoreCase(command) && parts.length > 1) {
-            int index = Integer.parseInt(parts[1]) - 1;  // Convert to zero-based index
-            String response = taskList.markTaskAsNotDone(index);
-            ioHandler.writeOutput(response);
-        } else if ("list".equalsIgnoreCase(command)) {
-            String taskListString = taskList.displayTasks();
-            ioHandler.writeOutput(taskListString);
-        } else if ("todo".equalsIgnoreCase(command)) {
-            taskList.addTodo(parts[1]);
-            ioHandler.writeOutput("Added new ToDo: " + parts[1]);
-        } else if ("deadline".equalsIgnoreCase(command)) {
-            String[] deadlineParts = parts[1].split(" /by ", 2);
-            taskList.addDeadline(deadlineParts[0], deadlineParts[1]);
-            ioHandler.writeOutput("Added new Deadline: " + deadlineParts[0] + " by " + deadlineParts[1]);
-        } else if ("event".equalsIgnoreCase(command)) {
-            String[] eventParts = parts[1].split(" /from | /to ", 3);
-            taskList.addEvent(eventParts[0], eventParts[1], eventParts[2]);
-            ioHandler.writeOutput("Added new Event: " + eventParts[0] + " from " + eventParts[1] + " to " + eventParts[2]);
-        } else {
-            // Handle unknown commands
-            ioHandler.writeOutput("Unknown command: " + command);
+        try {
+            switch (command.toLowerCase()) {
+                case "mark":
+                case "unmark":
+                    if (parts.length < 2) {
+                        throw new ChadException.InvalidArgumentException("Missing index for " + command);
+                    }
+                    int index = Integer.parseInt(parts[1]) - 1;
+                    String response = "mark".equalsIgnoreCase(command) ? taskList.markTaskAsDone(index) : taskList.markTaskAsNotDone(index);
+                    ioHandler.writeOutput(response);
+                    break;
+
+                case "list":
+                    ioHandler.writeOutput(taskList.displayTasks());
+                    break;
+
+                case "todo":
+                    if (parts.length < 2) {
+                        throw new ChadException.InvalidArgumentException("Missing task description for ToDo.");
+                    }
+                    taskList.addTodo(parts[1]);
+                    ioHandler.writeOutput("Added new ToDo: " + parts[1]);
+                    break;
+
+                case "deadline":
+                    if (parts.length < 2 || !parts[1].contains(" /by ")) {
+                        throw new ChadException.InvalidArgumentException("Invalid format for Deadline. Use: deadline {task} /by {date}");
+                    }
+                    String[] deadlineParts = parts[1].split(" /by ", 2);
+                    taskList.addDeadline(deadlineParts[0], deadlineParts[1]);
+                    ioHandler.writeOutput("Added new Deadline: " + deadlineParts[0] + " by " + deadlineParts[1]);
+                    break;
+
+                case "event":
+                    if (parts.length < 2 || !parts[1].contains(" /from ") || !parts[1].contains(" /to ")) {
+                        throw new ChadException.InvalidArgumentException("Invalid format for Event. Use: event {task} /from {start} /to {end}");
+                    }
+                    String[] eventParts = parts[1].split(" /from | /to ", 3);
+                    taskList.addEvent(eventParts[0], eventParts[1], eventParts[2]);
+                    ioHandler.writeOutput("Added new Event: " + eventParts[0] + " from " + eventParts[1] + " to " + eventParts[2]);
+                    break;
+
+                default:
+                    throw new ChadException.InvalidCommandException("Unknown command: " + command);
+            }
+        } catch (NumberFormatException e) {
+            throw new ChadException.InvalidFormatException("Invalid number format.");
         }
     }
+
 }
