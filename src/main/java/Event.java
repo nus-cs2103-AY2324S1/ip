0d9fc79265307934cdda7fb71d22f6061bac52a0
raynertjx <rayner742@gmail.com>
2023-08-31 01:56:15 +0800

    Change methods to throw BongoException

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -3,14 +3,14 @@ import java.time.LocalDateTime;
 public class Event extends Task {
     LocalDateTime from;
     LocalDateTime to;
-    public Event(String description, LocalDateTime from, LocalDateTime to) {
+    public Event(String description, LocalDateTime from, LocalDateTime to) throws BongoException {
         super(description);
         this.validateEventDuration(from, to);
         this.from = from;
         this.to = to;
     }
 
-    public Event(String description, boolean isDone, LocalDateTime from, LocalDateTime to) {
+    public Event(String description, boolean isDone, LocalDateTime from, LocalDateTime to) throws BongoException {
         super(description);
         this.isDone = isDone;
         this.validateEventDuration(from, to);
@@ -18,9 +18,9 @@ public class Event extends Task {
         this.to = to;
     }
 
-    private void validateEventDuration(LocalDateTime from, LocalDateTime to) {
-        if (from.isBefore(LocalDateTime.now()) || to.isBefore(LocalDateTime.now())) throw new IllegalArgumentException("Start and end of event must be in the future.");
-        if (from.isAfter(to)) throw new IllegalArgumentException("End of event must be after start of event.");
+    private void validateEventDuration(LocalDateTime from, LocalDateTime to) throws BongoException {
+        if (from.isBefore(LocalDateTime.now()) || to.isBefore(LocalDateTime.now())) throw new BongoException("Start and end of event must be in the future.");
+        if (from.isAfter(to)) throw new BongoException("End of event must be after start of event.");
     }
 
     @Override
