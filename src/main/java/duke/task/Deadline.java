ea8a0e6f7660ab4cebc282eaee4c5a4d1717cebc
Choonyan02 <choonyan02@gmail.com>
2023-08-30 23:30:44 +0800

    Add javadocs for Deadline and Task

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -3,15 +3,29 @@ package duke.task;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDate;
 
+/**
+ * Represents a task with a description, completion status, and a deadline date.
+ */
 public class Deadline extends Task {
   private LocalDate deadline = null;
 
+    /**
+     * Constructs a deadline task with the specified description and deadline date.
+     *
+     * @param description The description of the deadline task.
+     * @param by The deadline date in the format "yyyy-MM-dd".
+     */
   public Deadline(String description, String by) {
       super(description);
       DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
       this.deadline = LocalDate.parse(by, inputFormatter);
   }
 
+    /**
+     * Returns a string representation of the deadline task, including its completion status icon, description, and deadline date.
+     *
+     * @return A formatted string representing the deadline task.
+     */
   @Override
   public String toString() {
       DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -2,35 +2,69 @@ package duke.task;
 
 import java.io.Serializable;
 
+/**
+ * Represents a task with a description and completion status.
+ */
 public class Task implements Serializable {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructs a task with the specified description.
+     *
+     * @param description The description of the task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns an icon representing the completion status of the task.
+     *
+     * @return "X" if the task is done, " " (space) if the task is not done.
+     */
     public String getStatusIcon() {
         return (this.isDone ? "X" : " ");
     }
 
+    /**
+     * Returns the description of the task.
+     *
+     * @return The description of the task.
+     */
     public String getDesc() {
         return this.description;
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Marks the task as not done.
+     */
     public void markAsNotDone() {
         this.isDone = false;
     }
 
+    /**
+     * Checks if the task is done.
+     *
+     * @return true if the task is done, false otherwise.
+     */
     public boolean isDone() {
         return this.isDone;
     }
 
+    /**
+     * Returns a string representation of the task, including its completion status icon and description.
+     *
+     * @return A formatted string representing the task.
+     */
     public String toString() {
         return "[" + this.getStatusIcon() + "] " + this.description;
     }
