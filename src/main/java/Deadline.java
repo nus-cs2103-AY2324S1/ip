5759b029427373088caad7360534bf1ae2d35b17
Hanjoo Kim <hachookimhanjoo@gmail.com>
2023-08-30 12:41:24 +0800

    Add LocalDateTime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,15 +1,21 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime by;
     private String identifier;
 
-    public Deadline(String taskDescription, String by, boolean isDone) {
+    public Deadline(String taskDescription, LocalDateTime by, boolean isDone) {
         super(taskDescription, isDone);
         this.by = by;
         this.identifier = "[D]";
     }
 
     public String toString() {
-        return this.identifier + super.toString() + " (by: " + by + ")";
+        return this.identifier + super.toString() + " (by: " +
+                by.format(DateTimeFormatter.ofPattern("LLL dd yyyy Ka")) + ")";
     }
 }
