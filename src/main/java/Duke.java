f0163f5d8d76408b392a265fe8c70f8cccb456d3
Lin Wan Lei <111512031+LinWanLeii@users.noreply.github.com>
2023-08-30 21:42:58 +0800

    Update body

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,107 +13,111 @@ public class Duke {
         String message;
         ArrayList<Task> userList = new ArrayList<>();
         Scanner userInput = new Scanner(System.in);
+        message = userInput.nextLine();
 
-        do {
-            message = userInput.nextLine();
-            Task task = null;
-            try {
-                // Listing things out
-                if (message.equalsIgnoreCase("list")) {
-                    System.out.println(lineBreak);
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < userList.size(); i++) {
-                        int index = i + 1;
-                        System.out.println(index + ". " + userList.get(i));
-                    }
-                    System.out.println(lineBreak);
+        while (!message.equalsIgnoreCase("bye")) {
+            Task task = new Task(message);
+            // Listing things out
+            if (message.equalsIgnoreCase("list")) {
+                System.out.println(lineBreak);
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < userList.size(); i++) {
+                    int index = i + 1;
+                    System.out.println(index + ". " + userList.get(i));
                 }
-                //marking tasks
-                if (message.startsWith("mark")) {
-                    int taskIndex = Integer.parseInt(message.substring(5)) - 1;
-                    if (taskIndex >= 0 && taskIndex < userList.size()) {
-                        userList.get(taskIndex).markDone();
-                        System.out.println(lineBreak + "Nice! I've marked this task as done:");
-                        System.out.println("  " + userList.get(taskIndex) + lineBreak);
-                    } else {
-                        System.out.println(lineBreak + "Invalid Task Number" + lineBreak);
-                    }
-                    //userList.remove(task);
-                }
-                //un marking task
-                if (message.startsWith("unmark")) {
-                    int taskIndex = Integer.parseInt(message.substring(7)) - 1;
-                    if (taskIndex >= 0 && taskIndex < userList.size()) {
-                        userList.get(taskIndex).unmarkDone();
-                        System.out.println(lineBreak + "OK, I've marked this task as not done yet:");
-                        System.out.println("  " + userList.get(taskIndex) + lineBreak);
-                    } else {
-                        System.out.println(lineBreak + "Invalid Task Number" + lineBreak);
-                    }
-                    //userList.remove(task);
-                }
-                //deleting task
-                if (message.startsWith("delete")) {
-                    int taskIndex = Integer.parseInt(message.substring(7)) - 1;
-                    if (taskIndex >= 0 && taskIndex < userList.size()) {
-                        Task removing = userList.get(taskIndex);
-                        System.out.println(lineBreak + "Noted. I've removed this task:");
-                        System.out.println("  " + removing);
-                        userList.remove(removing);
-                        System.out.println("Now you have " + userList.size() + " tasks in the list" + lineBreak);
-                    } else {
-                        System.out.println(lineBreak + "Invalid Task Number" + lineBreak);
-                    }
-                    continue;
-                }
-                //taking in the different task
-                if (!message.equalsIgnoreCase("bye")) {
-                    if (message.equalsIgnoreCase("list")) continue;
-                    if (message.startsWith("mark")) continue;
-                    if (message.startsWith("unmark")) continue;
-                    if (message.startsWith("todo")) {
-                        String info = message.substring(5);
-                        if (info.isEmpty()) {
-                            throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
-                        }
-                        task = new Todo(info);
-                    }
-                    if (message.startsWith("deadline")) {
-                        String info = message.substring(9);
-                        String[] split = info.split("/by");
-                        if (split.length != 2) {
-                            throw new DukeException("☹ OOPS!!! The description of a deadline is invalid.");
-                        }
-                        task = new Deadline(split[0], split[1]);
-                    }
-                    if (message.startsWith("event")) {
-                        String info = message.substring(6);
-                        String[] split = info.split("/from | /to ");
-                        if (split.length != 3) {
-                            System.out.println("hi");
-                            throw new DukeException("☹ OOPS!!! The description of a event is invalid.");
-                        }
-                        task = new Event(split[0], split[1], split[2]);
-                    }
-                    if (task != null) {
-                        System.out.println(lineBreak + "Got it. I've added this task:");
-                        userList.add(task);
-                        int size = userList.size();
-                        System.out.println(task);
-                        System.out.println("Now you have " + size + " tasks in the list." + lineBreak);
-                        continue;
-                    }
-                    throw new DukeException(UNKNOWN_COMMAND);
+                System.out.println(lineBreak);
+                task.notTask();
+                task.isValid();
+            }
+            //marking tasks
+            if (message.startsWith("mark")) {
+                int taskIndex = Integer.parseInt(message.substring(5)) - 1;
+                if (taskIndex >= 0 && taskIndex < userList.size()) {
+                    userList.get(taskIndex).markDone();
+                    System.out.println(lineBreak + "Nice! I've marked this task as done:");
+                    System.out.println("  " + userList.get(taskIndex) + lineBreak);
                 } else {
-                    if (message.equalsIgnoreCase("bye")) break;
-                    throw new DukeException(UNKNOWN_COMMAND);
+                    System.out.println(lineBreak + "Invalid Task Number" + lineBreak);
+                }
+                task.notTask();
+                task.isValid();
+                //userList.remove(task);
+            }
+            //un marking task
+            if (message.startsWith("unmark")) {
+                int taskIndex = Integer.parseInt(message.substring(7)) - 1;
+                if (taskIndex >= 0 && taskIndex < userList.size()) {
+                    userList.get(taskIndex).unmarkDone();
+                    System.out.println(lineBreak + "OK, I've marked this task as not done yet:");
+                    System.out.println("  " + userList.get(taskIndex) + lineBreak);
+                } else {
+                    System.out.println(lineBreak + "Invalid Task Number" + lineBreak);
+                }
+                task.notTask();
+                task.isValid();
+                //userList.remove(task);
+            }
+            //deleting task
+            if (message.startsWith("delete")) {
+                int taskIndex = Integer.parseInt(message.substring(7)) - 1;
+                if (taskIndex >= 0 && taskIndex < userList.size()) {
+                    Task removing = userList.get(taskIndex);
+                    System.out.println(lineBreak + "Noted. I've removed this task:");
+                    System.out.println("  " + removing);
+                    userList.remove(removing);
+                    System.out.println("Now you have " + userList.size() + " tasks in the list" + lineBreak);
+                } else {
+                    System.out.println(lineBreak + "Invalid Task Number" + lineBreak);
+                }
+                task.notTask();
+                task.isValid();
+            }
+            // it is a task then
+            try {
+                if (message.startsWith("todo")) {
+                    String info = message.substring(5);
+                    if (info.isEmpty()) {
+                        task.hasError();
+                        throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                    }
+                    task = new Todo(info);
+                    task.isValid();
+                }
+                if (message.startsWith("deadline")) {
+                    String info = message.substring(9);
+                    String[] split = info.split("/by");
+                    if (split.length != 2) {
+                        task.hasError();
+                        throw new DukeException("☹ OOPS!!! The description of a deadline is invalid.");
+                    }
+                    task = new Deadline(split[0], split[1]);
+                    task.isValid();
+                }
+                if (message.startsWith("event")) {
+                    String info = message.substring(6);
+                    String[] split = info.split("/from | /to ");
+                    if (split.length != 3) {
+                        task.hasError();
+                        throw new DukeException("☹ OOPS!!! The description of a event is invalid.");
+                    }
+                    task = new Event(split[0], split[1], split[2]);
+                    task.isValid();
                 }
             } catch (DukeException e) {
-                System.out.println(lineBreak + e.getMessage() + lineBreak);
-            } catch (Exception e) {
-                System.out.println(lineBreak + "☹ OOPS!!! The description is invalid :(." + lineBreak);
+                System.out.printf(lineBreak + e.getMessage() + lineBreak);
             }
-        } while (!message.equalsIgnoreCase("bye"));
+            if (!task.isItValid()) {
+                System.out.println(lineBreak + UNKNOWN_COMMAND + lineBreak);
+            }
+            if (!task.isItErrored() && task.isItTask() && task.isItValid()) {
+                System.out.println(lineBreak + "Got it. I've added this task:");
+                userList.add(task);
+                int size = userList.size();
+                System.out.println(task);
+                System.out.println("Now you have " + size + " tasks in the list." + lineBreak);
+            }
+            message = userInput.nextLine();
+        }
         System.out.print(lineBreak + "Bye. Hope to see you again soon!" + lineBreak);
         userInput.close();
    }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,16 @@
 public class Task {
     private String name;
-    private Boolean marked;
+    private boolean marked;
+    private boolean hasError;
+    private boolean isTask;
+    private boolean isItValid;
     public String type;
     public Task(String name) {
         this.name = name;
         this.marked = false;
+        this.hasError = false;
+        this.isTask = true;
+        this.isItValid = false;
     }
 
     public void markDone() {
@@ -16,6 +22,12 @@ public class Task {
     public boolean isMarked() {
         return marked;
     }
+    public void hasError() { hasError = true; }
+    public boolean isItErrored() { return hasError; }
+    public void notTask() { isTask = false; }
+    public boolean isItTask() { return isTask; }
+    public void isValid() { isItValid = true; }
+    public boolean isItValid() { return isItValid; }
     @Override
     public String toString() {
         String s = "  [" + type + "]" + "[" + (marked ? "X" : " ") + "] " + name;
