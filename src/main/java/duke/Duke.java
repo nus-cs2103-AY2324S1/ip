55317858d8ecf2bffda5e2176956e43f41d82369
Yu Han Zhang <yuhan@zhyuhan.me>
2023-08-30 15:45:48 +0800

    refactor: extract input and output logic into package

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -3,51 +3,30 @@ package duke;
 import duke.commands.Command;
 import duke.commands.CommandResult;
 import duke.storage.Storage;
-
-import java.util.Scanner;
+import duke.ui.TextUi;
 
 public class Duke {
-    private static final int INDENT_SIZE = 4;
     private static final String NAME = "Jimmy";
     private static final String TASKS_CACHE_PATH = ".duke-cache";
     public static TaskList tasks;
 
-    private static String indent(String s) {
-        return String.format("%s%s", " ".repeat(INDENT_SIZE), s);
-    }
-
-    private static void whisper(String... strings) {
-        for (String s : strings) {
-            System.out.println(indent(s));
-        }
-    }
-
-    private static void say(String... strings) {
-        whisper(strings);
-        System.out.println();
-    }
-
-    private static void greet() {
-        say(String.format("Hello! I'm %s", NAME), "What can I do for you?");
-    }
-
     public static void main(String[] args) {
+        TextUi ui = new TextUi();
         Storage storage = new Storage(TASKS_CACHE_PATH);
         try {
             tasks = storage.load();
-            say(String.format("Loaded existing tasks from %s", TASKS_CACHE_PATH));
+            ui.say(String.format("Loaded existing tasks from %s", TASKS_CACHE_PATH));
         } catch (DukeException e) {
-            say(String.format("%s. Initializing empty task list...", e.getMessage()));
+            ui.say(String.format("%s. Initializing empty task list...", e.getMessage()));
             tasks = new TaskList();
         }
 
-        greet();
+        ui.greet(NAME);
 
         boolean shouldTerminate = false;
-        Scanner sc = new Scanner(System.in);
 
-        while (!shouldTerminate && sc.hasNextLine()) {
-            String input = sc.nextLine().trim();
+        while (!shouldTerminate) {
+            String input = ui.getInput();
 
             try {
                 Command command = Command.parse(input);
@@ -65,9 +44,9 @@ public class Duke {
                     storage.save(tasks);
                 }
 
-                say(result.response.toArray(new String[0]));
+                ui.say(result.response.toArray(new String[0]));
             } catch (DukeException e) {
-                say(e.getMessage());
+                ui.say(e.getMessage());
             }
         }
 
