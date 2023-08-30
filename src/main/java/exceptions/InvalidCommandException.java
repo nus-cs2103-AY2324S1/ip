8d5bec191aa15437886a834003bc787de9c08bb1
Evan Yan <103996156+evanyan13@users.noreply.github.com>
2023-08-30 11:24:13 +0800

    Update InvalidCommandException Message

--- src/main/java/exceptions/InvalidCommandException.java
+++ src/main/java/exceptions/InvalidCommandException.java
@@ -2,6 +2,6 @@ package exceptions;
 
 public class InvalidCommandException extends JarvisException {
     public InvalidCommandException(String message) {
-        super("Sorry Master, I'm not 100% sure what that means?");
+        super("Sorry Master, I'm not 100% sure what that means? Can you provide me with a more clear command please?");
     }
 }
