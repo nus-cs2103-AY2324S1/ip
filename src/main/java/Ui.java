7c97a0df738ed81e269e70bf93f25197cf362383
raynertjx <rayner742@gmail.com>
2023-08-31 01:57:01 +0800

    Adjust line spacing and add loading error messages

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -9,7 +9,7 @@ public class Ui {
         this.inputScanner = new Scanner(System.in);
     }
 
-    private String readCommand() {
+    protected String readCommand() {
         return this.inputScanner.nextLine();
     }
 
@@ -19,48 +19,63 @@ public class Ui {
                 "____________________________________________________________";
     }
 
-    private void showWelcome() {
+    protected void showLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    protected void showWelcome() {
         String welcomeMessage = " Hello! I'm Bongo!\n" +
                 " What can I do for you?\n";
         System.out.println(insertLines(welcomeMessage));
     }
 
-    private void showGoodbye() {
-        String goodbyeMessage = " Bye. Hope to see you again soon!\n";
-        System.out.println(insertLines(goodbyeMessage));
+    protected void showGoodbye() {
+        String goodbyeMessage = " Bye. Hope to see you again soon!";
+        System.out.println(goodbyeMessage);
+        this.inputScanner.close();
     }
 
-    private void showAllTasks(ArrayList<Task> tasks) {
+    protected void showAllTasks(ArrayList<Task> tasks) {
         StringBuilder allTasks = new StringBuilder();
         for (int i = 0; i < tasks.size(); i++) {
             allTasks.append(String.format(" %d. %s\n", i + 1, tasks.get(i)));
         }
         String tasksList = " Here are the tasks in your list:\n" +
                 allTasks;
-        System.out.println(insertLines(tasksList));
+        System.out.println(tasksList.trim());
     }
 
-    private void showAddedTask(Task newTask, int totalTasks) {
+    protected void showAddedTask(Task newTask, int totalTasks) {
         String addedTaskMessage = " Got it. I've added this task:\n" +
                 String.format("  %s\n", newTask) +
-                String.format(" Now you have %d tasks in the list.\n", totalTasks);
-        System.out.println(insertLines(addedTaskMessage));
+                String.format(" Now you have %d tasks in the list.", totalTasks);
+        System.out.println(addedTaskMessage);
     }
 
-    private void showTaskIsDone(Task task) {
-        String taskStatusMessage = " Nice! I've marked this task as done:\n";
-        System.out.println(insertLines(taskStatusMessage));
+    protected void showTaskIsDone(Task task) {
+        String taskStatusMessage = " Nice! I've marked this task as done:\n" + task;
+        System.out.println(taskStatusMessage.trim());
     }
 
-    private void showTaskIsUndone(Task task) {
-        String taskStatusMessage = " OK, I've marked this task as not done yet:\n";
-        System.out.println(insertLines(taskStatusMessage));
+    protected void showTaskIsUndone(Task task) {
+        String taskStatusMessage = " OK, I've marked this task as not done yet:\n" + task;
+        System.out.println(taskStatusMessage.trim());
     }
 
-    private void showDeleteTask(Task task, int tasksLeft) {
+    protected void showDeleteTask(Task task, int tasksLeft) {
         String taskDeleteMessage = " Noted. I've removed this task:\n" +
                 String.format("  %s\n", task) +
-                String.format(" Now you have %d tasks in the list.\n", tasksLeft);
-        System.out.println(insertLines(taskDeleteMessage));
+                String.format(" Now you have %d tasks in the list.", tasksLeft);
+        System.out.println(taskDeleteMessage);
+    }
+
+    protected void showError(String errorMessage) {
+        String finalErrorMessage = " Oh no! Bongo ran into an error :(\n" + String.format(" %s", errorMessage);
+        System.out.println(finalErrorMessage);
+    }
+
+    protected void showLoadingError() {
+        String loadingErrorMessage = "Oh no! Bongo couldn't find the files, so the following files were created:";
+        System.out.println(loadingErrorMessage);
     }
 }
