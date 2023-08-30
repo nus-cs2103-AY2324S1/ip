ebb963f7bc1f027ba5c26b1dc77556891462e1db
AriellaCallista <arielgit>
2023-08-30 12:40:47 +0800

    Implement parser class to parse commands

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Parser {
+public class Parser {
+
+
+    public static Command parse(String fullCommand) {
+        String[] strArr = fullCommand.split(" ", 2);
+        String cmdWord = strArr[0];
+        String arguments = strArr[1];
+
+        switch (cmdWord) {
+        case "todo": case "event": case "deadline":
+            return new AddCommand(cmdWord, arguments);
+        case "list":
+            return new ListCommand(cmdWord, arguments);
+        case "delete":
+            return new DeleteCommand(cmdWord, arguments);
+        case "bye":
+            return new ExitCommand(cmdWord, arguments);
+        default:
+            return new HelpCommand(cmdWord, arguments);
+        }
+    }
 }
