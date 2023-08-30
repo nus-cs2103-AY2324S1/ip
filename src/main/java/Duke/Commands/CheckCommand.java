bd6a56fc036fdf0c86325f038f7e78709cfdeb80
rayson <raysonyeap4c@gmail.com>
2023-08-31 00:50:22 +0800

    Add JavaDoc for Commands

--- src/main/java/Duke/Commands/CheckCommand.java
+++ src/main/java/Duke/Commands/CheckCommand.java
@@ -7,15 +7,31 @@ import Duke.Tools.Ui;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents a command to check tasks scheduled on a specific date and time.
+ */
 public class CheckCommand extends Command {
 
     private final DateTimeFormatter formatter;
 
+    /**
+     * Constructs a CheckCommand with the specified command and date-time formatter.
+     *
+     * @param fullCommand The full command string.
+     * @param formatter   The date-time formatter to parse date and time inputs.
+     */
     public CheckCommand(String fullCommand, DateTimeFormatter formatter) {
         super(fullCommand);
         this.formatter = formatter;
     }
 
+    /**
+     * Executes the check command, displaying tasks scheduled on the specified date and time.
+     *
+     * @param tasks   The list of tasks.
+     * @param ui      The user interface for displaying messages.
+     * @param storage The storage handler.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         String content = this.fullCommand.replaceAll("^\\s*check\\s*", "");
--- src/main/java/Duke/Commands/Command.java
+++ src/main/java/Duke/Commands/Command.java
@@ -4,20 +4,47 @@ import Duke.Tools.Storage;
 import Duke.Tools.TaskList;
 import Duke.Tools.Ui;
 
+/**
+ * Represents a command that can be executed on the task list.
+ */
 abstract public class Command {
 
     protected String fullCommand;
     protected boolean isExit;
 
+    /**
+     * Constructs a command with the given full command string.
+     *
+     * @param fullCommand The full command string.
+     */
     public Command(String fullCommand) {
         this.fullCommand = fullCommand;
         this.isExit = false;
     }
 
+    /**
+     * Constructs a command with no command string.
+     */
+    public Command() {
+        this.isExit = false;
+    }
+
+    /**
+     * Executes the command using the provided task list, user interface, and storage.
+     *
+     * @param tasks   The list of tasks.
+     * @param ui      The user interface for displaying messages.
+     * @param storage The storage handler for data persistence.
+     * @throws Exception Any exceptions that may occur during command execution.
+     */
     abstract public void execute(TaskList tasks, Ui ui, Storage storage) throws Exception;
 
+    /**
+     * Checks if the command is an exit command to determine whether bot should end.
+     *
+     * @return True if the command is an exit command, otherwise false.
+     */
     public boolean isExit() {
         return this.isExit;
     }
-
 }
