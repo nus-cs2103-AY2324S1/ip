fa9d0abdb86c23cff4d99743d38a0660651aa2df
Derek Tan <derek.tan.jing.xu@gmail.com>
2023-08-30 09:00:02 +0800

    UI class: implement print welcome message functionality

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -2,14 +2,21 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class UI {
+    private String name;
     private static final String DATETIME_INPUT_FORMAT = "yyyy-MM-dd HHmm";
     public static final DateTimeFormatter dateTimeInputFormatter = DateTimeFormatter.ofPattern(DATETIME_INPUT_FORMAT);
 
-    public UI() {}
+    public UI(String name) {
+        this.name = name;
+    }
 
     public void handleCommand(ArrayList<String> parsedInput, ArrayList<Task> taskList) {}
 
-    private static void printWelcomeMessage() {}
+    private void printWelcomeMessage() {
+        String name = "Derek";
+        System.out.println("Hello! I'm " + this.name);
+        System.out.println("What can I do for you?");
+    }
 
     private static void printGoodbyeMessage() {}
 }
