90478da2a04293f69db32e47e46ab685222199c9
raynertjx <rayner742@gmail.com>
2023-08-31 01:54:38 +0800

    Add utility class for date formatting

--- src/main/java/DateHelper.java
+++ src/main/java/DateHelper.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class DateHelper {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateHelper {
+    public static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+    public static LocalDateTime formatDateTime(String datetime) throws DateTimeParseException {
+        try {
+            return LocalDateTime.parse(datetime, formatter);
+        } catch (DateTimeParseException e) {
+            throw new DateTimeParseException(String.format("Please enter a valid datetime in the format of %s", "DD/MM/YYYY HHMM"), datetime, e.getErrorIndex());
+        }
+    }
 }
