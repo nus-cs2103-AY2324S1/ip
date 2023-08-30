293930e3419211b7d54bc92a6f6d8147f4fe38af
chew01 <chewjh19@gmail.com>
2023-08-30 12:39:22 +0800

    Add tests for all Task classes

--- src/test/java/ducky/task/DeadlineTaskTest.java
+++ src/test/java/ducky/task/DeadlineTaskTest.java
@@ -1,2 +1,49 @@
-package task;public class DeadlineTaskTest {
+package ducky.task;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTaskTest {
+
+    DeadlineTask deadlineTask1 = new DeadlineTask(
+            "test1",
+            LocalDate.parse("2019-01-01")
+    );
+    DeadlineTask deadlineTask2 = new DeadlineTask(
+            "test2",
+            LocalDate.parse("2023-08-30")
+    );
+
+    @BeforeEach
+    public void setDeadlineTask2ToCompleted() {
+        deadlineTask2.complete();
+    }
+
+    @Test
+    public void deadlineTask_toString() {
+        assertEquals(
+                deadlineTask1.toString(),
+                "[D][ ] test1 (by: Jan 01 2019)"
+                );
+        assertEquals(
+                deadlineTask2.toString(),
+                "[D][X] test2 (by: Aug 30 2023)"
+        );
+    }
+
+    @Test
+    public void deadlineTask_getSaveFormat() {
+        assertEquals(
+                deadlineTask1.getSaveFormat(),
+                "D | 0 | test1 | 2019-01-01"
+        );
+        assertEquals(
+                deadlineTask2.getSaveFormat(),
+                "D | 1 | test2 | 2023-08-30"
+        );
+    }
 }
