6065bfc8d6037c70919674fd468a06d2faf30e43
YangHeng Tang <yanghengtang@gmail.com>
2023-08-30 23:05:37 +0800

    Update TaskException.java

--- src/main/java/duke/task/TaskException.java
+++ src/main/java/duke/task/TaskException.java
@@ -1,9 +1,8 @@
 package duke.task;
 
 public class TaskException extends Exception {
-    private static String HORIZONTAL_LINE = "____________________________________________________________\n";
 
-    public TaskException() {
-        super(HORIZONTAL_LINE + "You have entered an invalid task number.\n" + HORIZONTAL_LINE);
+    public TaskException(String format) {
+        super("Oops! Invalid input for your task.\nValid Format: " + format);
     }
 }
