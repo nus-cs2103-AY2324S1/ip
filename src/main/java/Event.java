1b0cf49e96f93b0476ef98c6418adba212d8ec98
alientian <e1062165@u.nus.edu>
2023-08-31 01:56:42 +0800

    Add Dates

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,8 +1,11 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 public class Event extends  Task{
 
-    protected String from;
-    protected String to;
-    public Event(String description, String from, String to) {
+    protected LocalDate from;
+    protected LocalDate to;
+    public Event(String description, LocalDate from, LocalDate to) {
         super(description);
         this.from = from;
         this.to = to;
@@ -10,7 +13,9 @@ public class Event extends  Task{
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
+        return "[E]" + super.toString()
+                + " (from: " + this.from.format(DateTimeFormatter.ofPattern("MMM d yyyy"))
+                + " to: " + this.to.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 
     @Override
