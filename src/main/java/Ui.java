1bf7642e00fafb48fe19dc4af5e079cd288aef0a
AriellaCallista <arielgit>
2023-08-30 12:42:04 +0800

    Implement UI class

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Ui {
+import java.util.Scanner;
+
+public class Ui {
+
+    public void showWelcome() {
+        this.showLine();
+        System.out.println("Hello I'm Sana!\nWhat can I do for you today?");
+        this.showLine();
+    }
+
+    public String readCommand() {
+        Scanner myObj = new Scanner(System.in);
+        return myObj.nextLine();
+    }
+
+    public void showLine() {
+        System.out.println("_______________________________________");
+    }
+
+    public void showError(String msg) {
+        this.showLine();
+        System.out.println(msg);
+        this.showLine();
+    }
+
 }
