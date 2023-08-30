d7da0584b8ffb1c4513b9c8a6b73a45f071e7911
Singa-Pirate <E0969244@u.nus.edu>
2023-08-31 02:42:17 +0800

    A-MoreOOP

--- src/main/java/FilePermissionException.java
+++ src/main/java/FilePermissionException.java
@@ -10,6 +10,6 @@ public class FilePermissionException extends LocalFileException{
     public String toString() {
         return "\tOOPS, an error occurred when accessing / creating / writing a local file.\n" +
                 "\tThe file path is " + this.filePath +
-                "\n\tPlease check the relevant file permissions";
+                "\n\tFor this session, previous data cannot be read, and current data cannot be stored.";
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Ui {
+    static final String line = "\t____________________________________________________________";
+    static final Scanner scanner = new Scanner(System.in);
+
+    public void showLine() {
+        System.out.println(line);
+    }
+
+    public void output(String s) {
+        System.out.println(s.stripTrailing());
+        showLine();
+    }
+
+    public void greet() {
+        this.showLine();
+        System.out.println("\tWelcome back, human!");
+        System.out.println("\tI'm your personal ChatBot, " + ChatBot.name + ".");
+        System.out.println("\tWhat can I do for you today?");
+        this.showLine();
+    }
+
+    public void farewell() {
+        output("\tBye. Hope to see you again soon!");
+    }
+
+    public void showLoadingError(LocalFileException e) {
+        System.out.println(e.toString());
+    }
+
+    public String nextCommand() {
+        return scanner.nextLine();
+    }
+}
