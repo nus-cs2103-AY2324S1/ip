38bffbddb6437d6d1ce65f4ea3d8f510488e71d2
yiwen101 <e0968880@u.nus.edu>
2023-08-30 18:55:39 +0800

    unit testing

--- src/test/java/data/task/DeadlineTest.java
+++ src/test/java/data/task/DeadlineTest.java
@@ -2,12 +2,14 @@ package data.task;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.beans.Transient;
+
+import org.junit.jupiter.api.Test;
 
 import exception.InvalidInputException;
 
 
 public class DeadlineTest {
+    @Test
     public void toStringTest() throws InvalidInputException {
         Deadline deadline = new Deadline();
         deadline.setDescription("return book");
@@ -16,12 +18,17 @@ public class DeadlineTest {
         assertEquals("[D][ ] return book (by: Aug 23 2020)", deadline.toString());
     }
 
-
+    @Test
     public void setByTest() throws InvalidInputException {
         Deadline deadline = new Deadline();
         deadline.setDescription("return book");
         deadline.setBy("2020-08-23");
-        deadline.setBy("2020-08-23 23:59");
+        try {
+            deadline.setBy("2020-08-23 12:00");
+        } catch (InvalidInputException e) {
+            assertEquals("Invalid Input: Invalid date format, should be YYYY-MM-DD", e.getMessage());
+        }
+       
     }
 
     
---
+++ src/test/java/data/task/builder/DeadlineBuilderTest.java
@@ -0,0 +1,24 @@
+package data.task.builder;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import exception.DukeException;
+import data.task.Task;
+
+public class DeadlineBuilderTest {
+    @Test
+    public void buildFromStringTest() throws DukeException {
+        DeadlineBuilder deadlineBuilder = new DeadlineBuilder();
+        Task t = deadlineBuilder.buildFromString("deadline return book /by 2020-02-02");
+        assertEquals("[D][ ] return book (by: Feb 02 2020)", t.toString());
+        try {
+            deadlineBuilder.buildFromString("deadline return book /by 2020-02-02 12:00");
+        } catch (DukeException e) {
+            assertEquals("Invalid Input: Invalid date format, should be YYYY-MM-DD", e.getMessage());
+        }
+    } 
+}
+
