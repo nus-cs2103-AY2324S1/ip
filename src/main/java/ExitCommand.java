97f9c01bf2f35dc5c785d90a3e0e25f812e8ddef
songfangyl <e0960536@u.nus.edu>
2023-08-30 12:34:12 +0800

    Update DeleteCommand and EXPECTED.txt

--- src/main/java/ExitCommand.java
+++ src/main/java/ExitCommand.java
@@ -7,6 +7,6 @@ public class ExitCommand extends Command{
 
     @Override
     public void execute(TaskList taskList, Ui ui, Storage storage) throws DukeException{
-        return;
+        ui.showGoodByeMessage();
     }
 }
