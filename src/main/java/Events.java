4ae9f84b581615d482dd8d3c865bd1e6e19c11a9
sushiyade <68842066+sushiyade@users.noreply.github.com>
2023-08-30 15:55:15 +0800

    Add date time functionality

--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -1,6 +1,12 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Events extends Task{
-    String from, to;
-    public Events(String description, String from, String to) {
+
+    static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    LocalDateTime from, to;
+    public Events(String description, LocalDateTime from, LocalDateTime to) {
         super(description);
         this.from = from;
         this.to = to;
@@ -8,6 +14,7 @@ public class Events extends Task{
     @Override
     public String toString() {
         return "[E]" + "[" + this.getStatusIcon() + "]"
-                + super.toString() + " (from: " + from  + " to: " + to + ")";
+                + super.toString() + " (from: " + from.format(DATE_TIME_FORMATTER) +
+                " to: " + to.format(DATE_TIME_FORMATTER) + ")";
     }
 }
