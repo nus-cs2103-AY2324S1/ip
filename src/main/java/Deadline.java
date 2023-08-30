c33fa9691a0d2c5a071f562caca959cca70d72cf
vijay-shankaranand <shankaranand48@gmail.com>
2023-08-31 04:24:18 +0800

    add level-8 changes

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,25 +1,32 @@
-
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalTime;
 /**
  * Represents a deadline task that has a description and a due date.
  */
 public class Deadline extends Task {
 
     /** The due date of the deadline task. */
-    protected String by;
+    protected LocalDate endDate;
+    protected LocalTime endTime;
 
     /**
      * Constructs a deadline task with a description and a due date.
      *
      * @param description The description of the deadline task.
-     * @param by The due date of the deadline task.
+     * @param endDate The due date of the deadline task.
+     * @param endDate The due time of the deadline task.
      */
-    public Deadline(String description, String by) {
+    public Deadline(String description, String endDate, String endTime) {
         super(description);
-        this.by = by;
+        this.endDate = LocalDate.parse(endDate);
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("HHmm");
+       this.endTime = LocalTime.parse(endTime, inputFormatter);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " ( by: " + by + ")";
+        return "[D]" + super.toString() + " ( by: " +
+                this.endDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " " + this.endTime + " )";
     }
 }
