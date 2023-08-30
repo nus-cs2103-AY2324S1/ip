ce86b18c34c0c6c791fa0a285d1bc36ff5bfcfde
WangCheng0116 <e1088136@u.nus.edu>
2023-08-31 00:19:16 +0800

    Fix coding standard

---
+++ src/main/java/commands/AddEventCommand.java
@@ -0,0 +1,20 @@
+package commands;
+
+import storage.Storage;
+import tasklist.TaskList;
+import tasks.Task;
+import ui.Ui;
+
+public class AddEventCommand implements Command {
+  private Task task;
+
+  public AddEventCommand(Task task) {
+    this.task = task;
+  }
+
+  @Override
+  public void execute(TaskList tasks, Ui ui, Storage storage) {
+    tasks.add(task);
+    storage.addLine(task.toString());
+  }
+}
