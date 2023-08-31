e5017b852e088b29dd07454e82546a01005b17b9
zekone <hkw19991@gmail.com>
2023-09-01 02:03:18 +0800

    Add StringUtility.java

---
+++ src/main/java/utility/StringUtility.java
@@ -0,0 +1,33 @@
+package utility;
+
+public abstract class StringUtility {
+
+    /**
+     * Removes the first word from a string.
+     * 
+     * @param s The input string
+     * @return New string without first word.
+     */
+    public static String removeFirstWord(String s) {
+        int indexOfSpace = s.indexOf(' ');
+        if (indexOfSpace == -1) {
+            return "";
+        }
+        return s.substring(indexOfSpace + 1);
+    }
+
+    /**
+     * Extracts the first word from a string.
+     * 
+     * @param s The input string.
+     * @return The first word.
+     */
+    public static String getFirstWord(String s) {
+        int indexOfSpace = s.indexOf(' ');
+        if (indexOfSpace == -1) {
+            return s;
+        }
+        return s.substring(0, indexOfSpace);
+    }
+
+}
