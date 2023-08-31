94ee101c8a771544eddc3d82d52f5f426294ce2f
YangHeng Tang <yanghengtang@gmail.com>
2023-08-31 19:52:31 +0800

    Add Javadocs for Task.java

--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -1,5 +1,8 @@
 package duke.task;
 
+/**
+ * Represents a task with status indicating its completeness
+ */
 public class Task {
     protected String description;
     protected boolean isDone;
@@ -9,16 +12,27 @@ public class Task {
         this.isDone = false;
     }
 
+    /**
+     * Returns a string representation of the task.
+     *
+     * @return String representation of the task
+     */
     @Override
     public String toString() {
         String status = "[" + (this.isDone ? "X" : " ") + "]";
         return status + " " + this.description;
     }
 
+    /**
+     * Updates the task status to 'completed'
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Updates the task status to 'uncompleted'
+     */
     public void markAsNotDone() {
         this.isDone = false;
     }
