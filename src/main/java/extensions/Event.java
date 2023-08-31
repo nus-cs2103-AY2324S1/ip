16d4efa83e3f0399cf287a0f891be34a6e3beeac
J-hta-n <e0969121@u.nus.edu>
2023-08-31 19:06:15 +0800

    Add formatting for date and times (in deadlines and events)
    Program now requires users to input a valid dateTime format for parsing. A similar parsing process is used
    
    when reading from the saved task list file.

--- src/main/java/extensions/Event.java
+++ src/main/java/extensions/Event.java
@@ -1,30 +1,37 @@
 package extensions;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Represents a task with starting and ending dates/times.
  */
 public class Event extends Task {
-    String from;
-    String to;
-    public Event(String description, String from, String to) {
+    LocalDateTime fromDateTime;
+    LocalDateTime toDateTime;
+    public Event(String description, LocalDateTime fromDateTime, LocalDateTime toDateTime) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.fromDateTime = fromDateTime;
+        this.toDateTime = toDateTime;
+    }
+    public String getDateTimeFormat(LocalDateTime dateTime) {
+        return dateTime.format(DateTimeFormatter.ofPattern("dd MMM yyyy h:mm a"));
     }
     @Override
     public String getSaveFormat() {
         return String.format("E | %c | %s | %s | %s",
                 this.getDoneSymbol(),
                 this.description,
-                this.from,
-                this.to);
+                this.getDateTimeFormat(this.fromDateTime),
+                this.getDateTimeFormat(this.toDateTime));
     }
     @Override
     public String toString() {
         return String.format("[E][%c] %s (from: %s, to: %s)",
                 this.getDoneSymbol(),
                 this.description,
-                this.from,
-                this.to);
+                this.getDateTimeFormat(this.fromDateTime),
+                this.getDateTimeFormat(this.toDateTime));
     }
 
 }
