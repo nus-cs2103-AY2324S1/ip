e0be2b1d6f37c442cd893d0e51f02e88d44db2e6
Ravern Koh <ravernkoh@gmail.com>
2023-08-31 13:04:17 +0800

    Update text ui tests

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 
-# create bin directory if it doesn't exist
-if [ ! -d "../bin" ]
+# delete output from previous run
+if [ -e "text-ui-test/ACTUAL.TXT" ]
 then
-    mkdir ../bin
+    rm text-ui-test/ACTUAL.TXT
 fi
 
-# delete output from previous run
-if [ -e "./ACTUAL.TXT" ]
+# delete save file from previous run
+if [ -e "data/ekud.txt" ]
 then
-    rm ACTUAL.TXT
+    rm data/ekud.txt
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/ekud/*.java ../src/main/java/ekud/**/*.java
+if ! ./gradlew build
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Ekud < input.txt > ACTUAL.TXT
+java -jar build/libs/Ekud.jar < text-ui-test/input.txt > text-ui-test/ACTUAL.TXT
 
 # compare the output to the expected output
-diff ACTUAL.TXT EXPECTED.TXT
+diff text-ui-test/ACTUAL.TXT text-ui-test/EXPECTED.TXT
 if [ $? -eq 0 ]
 then
     echo "Test result: PASSED"
