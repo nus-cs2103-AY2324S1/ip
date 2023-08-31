a1bb3b9feb2529e15afa383d44aa97e6b6c46990
raynertjx <rayner742@gmail.com>
2023-08-31 17:12:45 +0800

    Add Javadoc comments for task package

--- src/main/java/bongo/task/Event.java
+++ src/main/java/bongo/task/Event.java
@@ -7,6 +7,14 @@ import bongo.helper.DateHelper;
 public class Event extends Task {
     LocalDateTime from;
     LocalDateTime to;
+
+    /**
+     * A constructor for a Event.
+     * @param description
+     * @param from
+     * @param to
+     * @throws BongoException
+     */
     public Event(String description, LocalDateTime from, LocalDateTime to) throws BongoException {
         super(description);
         this.validateEventDuration(from, to);
@@ -14,6 +22,14 @@ public class Event extends Task {
         this.to = to;
     }
 
+    /**
+     * A constructor for a Event, specifying whether it is done.
+     * @param description
+     * @param isDone
+     * @param from
+     * @param to
+     * @throws BongoException
+     */
     public Event(String description, boolean isDone, LocalDateTime from, LocalDateTime to) throws BongoException {
         super(description);
         this.isDone = isDone;
@@ -22,6 +38,12 @@ public class Event extends Task {
         this.to = to;
     }
 
+    /**
+     * Check if event to and from datetime provided is valid.
+     * @param from
+     * @param to
+     * @throws BongoException
+     */
     private void validateEventDuration(LocalDateTime from, LocalDateTime to) throws BongoException {
         if (from.isBefore(LocalDateTime.now()) || to.isBefore(LocalDateTime.now())) throw new BongoException("Start and end of event must be in the future.");
         if (from.isAfter(to)) throw new BongoException("End of event must be after start of event.");
