eb08150d8a746297799ddaa1f578151f453d5798
FerdiHS <ferdi.fhs.fhs@gmail.com>
2023-08-31 21:56:45 +0800

    Add Ui class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import exception.InvalidCommandException;
@@ -12,56 +13,75 @@ import task.Event;
 import task.Task;
 import task.TaskList;
 import task.ToDo;
+import ui.Ui;
 
 /**
  * Chatbot class
  */
 public class Duke {
+    // Attribute
+    Ui ui;
+    Storage storage;
+    TaskList tasks;
+
+    // Constructor
+
+    /**
+     * The constructor of Duke
+     * 
+     * @param directoryName the name of directory
+     * @param fileName the name of the file
+     */
+    public Duke(String directoryName, String fileName) {
+        this.ui = new Ui();
+        this.storage = new Storage(directoryName, fileName);
+        try {
+            this.tasks = new TaskList(storage.loadTasks());
+        } catch (IOException e) {
+            this.tasks = new TaskList(new ArrayList<>());
+            this.ui.printException(e);
+        }
+    }
+
+    // Method
+
     /**
      * The main method
      * 
      * @param args the input argument
      * @throws IOException
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        new Duke("./data", "storage.txt").run();
+    }
+
+    /**
+     * Runs the program
+     */
+    public void run() {
         Scanner scanner = new Scanner(System.in);
-        Storage storage = new Storage("./data", "storage.txt");
-        TaskList tasks = new TaskList(storage.loadTasks());
-        // ArrayList<Task> tasks = new ArrayList<Task>();
-        String intro = "    ____________________________________________________________\n" +
-                    "    Hello! I'm Not A ChatBot\n" + 
-                    "    What can I do for you?\n" +
-                    "    ____________________________________________________________";
-        String end = "    ____________________________________________________________\n" +
-                    "    Bye. Hope to see you again soon!\n" +
-                    "    ____________________________________________________________";
-        System.out.println(intro);
+        ui.printIntro();
         String message = scanner.nextLine();
         while(!message.equals("bye")) {
             try{
-                System.out.println("    ____________________________________________________________");
                 if(message.equals("list")) {
-                    System.out.println("    Here are the tasks in your list:");
-                    System.out.print(tasks);
+                    ui.printTasks(tasks);
                 } else if(message.split(" ")[0].equals("mark") && message.split(" ").length == 2 && isInt(message.split(" ")[1]) 
                         && Integer.parseInt(message.split(" ")[1]) <= tasks.size() && Integer.parseInt(message.split(" ")[1]) > 0) {
-                    tasks.getTask(Integer.parseInt(message.split(" ")[1]) - 1).mark();
-                    storage.updateTask(tasks);
-                    System.out.println("    Nice! I've marked this task as done:");
-                    System.out.println("    " + tasks.getTask(Integer.parseInt(message.split(" ")[1]) - 1));
+                    Task markTask = tasks.getTask(Integer.parseInt(message.split(" ")[1]) - 1);
+                    markTask.mark();
+                    ui.printMarkTask(markTask);
                 } else if(message.split(" ")[0].equals("unmark") && message.split(" ").length == 2 && isInt(message.split(" ")[1]) 
                         && Integer.parseInt(message.split(" ")[1]) <= tasks.size() && Integer.parseInt(message.split(" ")[1]) > 0) {
-                    tasks.getTask(Integer.parseInt(message.split(" ")[1]) - 1).unmark();
+                    Task unmarkTask = tasks.getTask(Integer.parseInt(message.split(" ")[1]) - 1);
+                    unmarkTask.unmark();
                     storage.updateTask(tasks);
-                    System.out.println("    OK, I've unmarked this task as not done yet:");
-                    System.out.println("    " + tasks.getTask(Integer.parseInt(message.split(" ")[1]) - 1));
+                    ui.printUnmarkTask(unmarkTask);
                 } else if(ToDo.isToDo(message)) {
                     Task newTask = new ToDo(message.substring(5));
                     tasks.addTask(newTask);
                     storage.addTask(newTask);
-                    System.out.println("    Got it. I've added this task:");
-                    System.out.println("    " + tasks.getTask(tasks.size() - 1));
-                    System.out.println("    Now you have " + tasks.size() + " tasks in the list.");
+                    ui.printAddTask(newTask, tasks.size());
                 } else if(Deadline.isDeadline(message)) {
                     String name = message.substring(9, message.indexOf("/by "));
                     String deadlineString = message.substring(message.indexOf("/by ") + 4);
@@ -69,9 +89,7 @@ public class Duke {
                     Task newTask = new Deadline(name, deadlineDate);
                     tasks.addTask(newTask);
                     storage.addTask(newTask);
-                    System.out.println("    Got it. I've added this task:");
-                    System.out.println("    " + tasks.getTask(tasks.size() - 1));
-                    System.out.println("    Now you have " + tasks.size() + " tasks in the list.");
+                    ui.printAddTask(newTask, tasks.size());
                 } else if(Event.isEvent(message)) {
                     String name = message.substring(6, message.indexOf("/from "));
                     int fromIndex = message.indexOf("/from ");
@@ -83,33 +101,28 @@ public class Duke {
                     Task newTask = new Event(name, fromDate, toDate);
                     tasks.addTask(newTask);
                     storage.addTask(newTask);
-                    System.out.println("    Got it. I've added this task:");
-                    System.out.println("    " + tasks.getTask(tasks.size() - 1));
-                    System.out.println("    Now you have " + tasks.size() + " tasks in the list.");
+                    ui.printAddTask(newTask, tasks.size());
                 } else if(message.split(" ")[0].equals("delete") && message.split(" ").length == 2 && isInt(message.split(" ")[1]) 
                         && Integer.parseInt(message.split(" ")[1]) <= tasks.size() && Integer.parseInt(message.split(" ")[1]) > 0) {
-                    Task removed = tasks.getTask(tasks.size() - 1);
+                    Task removedTask = tasks.getTask(tasks.size() - 1);
                     tasks.removeTask(Integer.parseInt(message.split(" ")[1]) - 1);
                     storage.updateTask(tasks);
-                    System.out.println("    Noted. I've removed this task:");
-                    System.out.println("    " + removed);
-                    System.out.println("    Now you have " + tasks.size() + " tasks in the list.");
+                    ui.printRemoveTask(removedTask, tasks.size());
                 } else {
                     throw new InvalidCommandException();
                 }
             } catch(InvalidToDoException e) {
-                System.out.println("    " + e.getMessage());
+                ui.printException(e);
             } catch(InvalidEventException e) {
-                System.out.println("    " + e.getMessage());
+                ui.printException(e);
             } catch(InvalidDeadlineException e) {
-                System.out.println("    " + e.getMessage());
+                ui.printException(e);
             } catch(InvalidCommandException e) {
-                System.out.println("    " + e.getMessage());
+                ui.printException(e);
             }
-            System.out.println("    ____________________________________________________________");
             message = scanner.nextLine();
         }
-        System.out.println(end);
+        ui.printEnd();
         scanner.close();
     }
 
---
+++ src/main/java/ui/Ui.java
@@ -0,0 +1,117 @@
+package ui;
+
+import java.util.Scanner;
+
+import task.Task;
+import task.TaskList;
+
+public class Ui {
+    // Attribute
+    private static String SPACE = "    ";
+    private static String DASH = "____________________________________________________________";
+    Scanner sc;
+    
+    // Constructor
+    /**
+     * The constructor of the Ui
+     */
+    public Ui() {
+        this.sc = new Scanner(System.in);
+    }
+
+    /**
+     * Prints the intro to user
+     */
+    public void printIntro() {
+        String intro = SPACE + DASH + "\n" +
+            SPACE + "Hello! I'm Not A ChatBot\n" + 
+            SPACE + "What can I do for you?\n" +
+            SPACE + DASH;
+        System.out.println(intro);
+    }
+
+    /**
+     * Prints the good bye message to user
+     */
+    public void printEnd() {
+        String end = SPACE + DASH + "\n" +
+            SPACE + "Bye. Hope to see you again soon!\n" +
+            SPACE + DASH;
+        System.out.println(end);
+    }
+
+    /**
+     * Prints if exception occur
+     * 
+     * @param e the exception
+     */
+    public void printException(Exception e) {
+        System.out.println(SPACE + DASH);
+        System.out.println(SPACE + e.getMessage());
+        System.out.println(SPACE + DASH);
+    }
+
+    /**
+     * Prints the list of tasks
+     * 
+     * @param tasks the list of tasks
+     */
+    public void printTasks(TaskList tasks) {
+        System.out.println(SPACE + DASH);
+        System.out.println("    Here are the tasks in your list:");
+        System.out.print(tasks);
+        System.out.println(SPACE + DASH);
+    }
+
+    /**
+     * Prints the output of marking task
+     * 
+     * @param task the task marked
+     */
+    public void printMarkTask(Task task) {
+        System.out.println(SPACE + DASH);
+        System.out.println(SPACE + "Nice! I've marked this task as done:");
+        System.out.println(SPACE + task);
+        System.out.println(SPACE + DASH);
+    }
+
+    /**
+     * Prints the output of unmarking task
+     * 
+     * @param task the task unmarked
+     */
+    public void printUnmarkTask(Task task) {
+        System.out.println(SPACE + DASH);
+        System.out.println(SPACE + "OK, I've unmarked this task as not done yet:");
+        System.out.println(SPACE + task);
+        System.out.println(SPACE + DASH);
+    }
+
+    /**
+     * Prints the output of adding task
+     * 
+     * @param task the new task
+     * @param size the number of tasks
+     */
+    public void printAddTask(Task task, int size) {
+        System.out.println(SPACE + DASH);
+        System.out.println(SPACE + "Got it. I've added this task:");
+        System.out.println(SPACE + task);
+        System.out.println(SPACE + "Now you have " + size + " tasks in the list.");
+        System.out.println(SPACE + DASH);
+    }
+
+    /**
+     * Prints the output of removing task
+     * 
+     * @param task the task removed
+     * @param size the number of tasks
+     */
+    public void printRemoveTask(Task task, int size) {
+        System.out.println(SPACE + DASH);
+        System.out.println("    Noted. I've removed this task:");
+        System.out.println("    " + task);
+        System.out.println("    Now you have " + size + " tasks in the list.");
+        System.out.println(SPACE + DASH);
+    }
+}
