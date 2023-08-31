2045c3cfd822ef9877bea3c41264951426d37a54
thaddeusong <67548448+bladedmistral@users.noreply.github.com>
2023-08-31 11:00:52 +0800

    Fix Task Constructors

---
+++ src/test/java/task/EventTest.java
@@ -0,0 +1,19 @@
+package task;
+
+import Duke.exception.DukeException;
+import Duke.task.Event;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EventTest {
+    @Test
+    public void TestEventConstructor() throws DukeException {
+        assertEquals(new Event("a/from 2020-02-10 10:15/to 2020-02-10 10:20").toString(), "[E][ ] a (from: 2020-02-10,10:15 to: 2020-02-10,10:20)");
+    }
+
+    @Test
+    public void TestEventToSaveFormat() throws DukeException {
+        assertEquals(new Event("a/from 2020-02-10 10:15/to 2020-02-10 10:20").toSaveFormat(), "event:2020-02-10 10:15|2020-02-10 10:20|a|false");
+    }
+}
