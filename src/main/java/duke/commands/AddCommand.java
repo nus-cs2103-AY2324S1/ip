92265cccd8f348398f060d68b0a420eed73764ce
ryam <ryanngohenrui@gmail.com>
2023-08-31 13:24:03 +0800

    Add Javadoc

--- src/main/java/duke/commands/AddCommand.java
+++ src/main/java/duke/commands/AddCommand.java
@@ -7,25 +7,52 @@ import duke.ui.Ui;
 import java.time.LocalDateTime;
 
 public class AddCommand extends Command {
-	private String description;
+	/**
+	 * The description of the task
+	 */
+	private final String description;
+	/**
+	 * The type of the task, either mark or unmark
+	 */
+	private final String type;
+	/**
+	 * The end time of the task
+	 */
 	private LocalDateTime to;
+	/**
+	 * The start time of the task
+	 */
 	private LocalDateTime from;
-	private String type;
 
-	// ToDo Constructor
+	/**
+	 * duke.tasks.Todo Constructor
+	 *
+	 * @param description
+	 */
 	public AddCommand(String description) {
 		this.description = description;
 		this.type = "todo";
 	}
 
-	// duke.tasks.Deadline Constructor
+	/**
+	 * duke.tasks.Deadline Constructor
+	 *
+	 * @param description the description of the task
+	 * @param to          the end time of the task
+	 */
 	public AddCommand(String description, LocalDateTime to) {
 		this.description = description;
 		this.to = to;
 		this.type = "deadline";
 	}
 
-	// duke.tasks.Event Constructor
+	/**
+	 * duke.tasks.Event Constructor
+	 *
+	 * @param description the description of the task
+	 * @param from        the start time of the task
+	 * @param to          the end time of the task
+	 */
 	public AddCommand(String description, LocalDateTime from, LocalDateTime to) {
 		this.description = description;
 		this.to = to;
@@ -33,6 +60,13 @@ public class AddCommand extends Command {
 		this.type = "event";
 	}
 
+	/**
+	 * Executes the command
+	 *
+	 * @param tasks   the task list
+	 * @param ui      the ui
+	 * @param storage the storage
+	 */
 	@Override
 	public void execute(TaskList tasks, Ui ui, Storage storage) {
 		Task task = null;
@@ -52,6 +86,11 @@ public class AddCommand extends Command {
 		storage.writeData(tasks.getAllTasks());
 	}
 
+	/**
+	 * Returns whether the program should exit
+	 *
+	 * @return whether the program should exit
+	 */
 	@Override
 	public boolean isExit() {
 		return false;
