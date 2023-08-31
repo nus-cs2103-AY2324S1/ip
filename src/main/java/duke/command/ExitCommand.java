b7ff26d4966442778219d743e41f31ad7b6c4960
YangHeng Tang <yanghengtang@gmail.com>
2023-08-31 19:42:54 +0800

    Add Javadocs for ExitCommand.java

--- src/main/java/duke/command/ExitCommand.java
+++ src/main/java/duke/command/ExitCommand.java
@@ -4,11 +4,21 @@ import duke.util.Storage;
 import duke.util.TaskList;
 import duke.util.Ui;
 
+/**
+ * Represents an Exit command to be executed.
+ */
 public class ExitCommand extends Command {
     public ExitCommand() {
         super(true);
     }
 
+    /**
+     * Executes the list of commands to exit the chatbot.
+     *
+     * @param list The given TaskList associated with the chatbot.
+     * @param ui The given Ui to show the status of the exit command.
+     * @param storage The given Storage that saves the TaskList locally.
+     */
     @Override
     public void execute(TaskList list, Ui ui, Storage storage) {
         storage.save(list);
