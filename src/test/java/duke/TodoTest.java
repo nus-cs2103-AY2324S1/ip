8b699675c735d01b7ae8243a7036bb10ade25a15
limzhenwy <zwlim38@gmail.com>
2023-08-31 22:18:17 +0800

    Add JUnit tests

---
+++ src/test/java/duke/TodoTest.java
@@ -0,0 +1,14 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoTest {
+    @Test
+    public void constructorTest() {
+        Todo read = new Todo("read");
+        assertEquals("T | 0 | read", read.toString());
+    }
+
+}
