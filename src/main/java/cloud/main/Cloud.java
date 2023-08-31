0b333602b1969714ac150ae0fc26de690b1b2f85
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-08-31 19:10:48 +0800

    feat(Cloud): Change default text to greeting/farewell

---
+++ src/main/java/cloud/main/Cloud.java
@@ -0,0 +1,18 @@
+package cloud.main;
+
+/**
+ * The chatbot's main class.
+ */
+public class Cloud {
+    public static void main(String[] args) {
+        Cloud.line();
+        System.out.println("Cloud, online.");
+        Cloud.line();
+        System.out.println("\\o");
+        Cloud.line();
+    }
+
+    public static void line() {
+        System.out.println("-".repeat(50));
+    }
+}
