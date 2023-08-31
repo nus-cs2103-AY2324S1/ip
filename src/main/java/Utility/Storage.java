8095bc8eacaafef4eedfe56c2959d3d174257375
justin soong <91204066+juzzztinsoong@users.noreply.github.com>
2023-08-31 19:17:23 +0800

    added OOP, packages already implemented

--- src/main/java/Utility/Storage.java
+++ src/main/java/Utility/Storage.java
@@ -10,7 +10,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
 import java.util.List;
 
 import Commands.Command;
