7a0f416acd58701b28e1be9e3fcf98ed7c2cf6b7
Jeffrey <jeffreyjianyujie@gmail.com>
2023-08-31 18:03:25 +0800

    JavaDoc for Duke, Ui

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -9,14 +9,29 @@ import duke.ui.Ui;
 
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Duke is a simple task management application that allows users to keep track
+ * of tasks, including to-dos, deadlines, and events.
+ *
+ * Duke uses a command-based approach, where users enter text commands to
+ * perform various operations on the task list.
+ */
 public class Duke {
 
+    /**
+     * The date and time input format used for parsing date and time strings.
+     */
     public static final DateTimeFormatter DATETIME_INPUT_FORMAT = DateTimeFormatter.ofPattern("dd-MM-yyyy HHmm");
 
     private Storage storage;
     private TaskList items;
     private Ui ui;
 
+    /**
+     * Constructs a Duke instance.
+     *
+     * @param filePath The file path where tasks are stored.
+     */
     public Duke(String filePath) {
         ui = new Ui();
         storage = new Storage(filePath);
@@ -28,6 +43,12 @@ public class Duke {
         }
     }
 
+    /**
+     * Runs the Duke application.
+     *
+     * This method initializes the user interface, loads tasks from storage,
+     * and enters a loop to process user commands.
+     */
     public void run() {
         ui.greet();
 
@@ -44,6 +65,11 @@ public class Duke {
         }
     }
 
+    /**
+     * The main method that starts the Duke application.
+     *
+     * @param args Command-line arguments (not used in this application).
+     */
     public static void main(String[] args) {
         new Duke("src/data/duke.txt").run();
     }
