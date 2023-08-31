1d9a846e920e3c35aa1bd70a187bd6bc3ae263f1
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-08-31 19:34:05 +0800

    feat(Cloud): Command handler, docs, bye command, echo unknown commands

--- src/main/java/cloud/main/Cloud.java
+++ src/main/java/cloud/main/Cloud.java
@@ -1,18 +1,63 @@
 package cloud.main;
 
+import java.util.Scanner;
+
+
+
 /**
  * The chatbot's main class.
  */
 public class Cloud {
-    public static void main(String[] args) {
-        Cloud.line();
-        System.out.println("Cloud, online.");
-        Cloud.line();
-        System.out.println("\\o");
-        Cloud.line();
+    /** The scanner used to read commands */
+    private static Scanner SCANNER = new Scanner(System.in);
+
+    /**
+     * Handles all String commands.
+     *
+     * @param command The command to handle.
+     */
+    private static void handle(String command) {
+        switch (command) {
+        case "bye":
+            Cloud.say("\\o");
+            System.exit(0);
+            break;
+        default:
+            Cloud.say(
+                String.format(
+                    "Unknown command: \"%s\"",
+                    command
+                )
+            );
+            break;
+        }
     }
 
-    public static void line() {
-        System.out.println("-".repeat(50));
+    /**
+     * Prints the specified text as output.
+     *
+     * @param text The text to print.
+     */
+    private static void say(String text) {
+        System.out.println(
+            String.format(
+                ">>> %s",
+                text
+            )
+        );
+    }
+
+    /**
+     * The chatbot's main method.
+     */
+    public static void main(String[] args) {
+        Cloud.say("Cloud online. Now listening for commands.");
+
+        while (true) {
+            System.out.println("");
+
+            String command = Cloud.SCANNER.nextLine();
+            Cloud.handle(command);
+        }
     }
 }
