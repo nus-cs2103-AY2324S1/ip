1874d81401e056640926a12e03b08ce29a257fbc
Tan Lip Wei <53000252+lipwei1808@users.noreply.github.com>
2023-09-01 01:07:07 +0800

    Refactor lines printing in UI class

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -4,99 +4,92 @@ import duke.tasks.Task;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class Ui {
-    private static String line = "    __________________________________________";
-    private Scanner s = new Scanner(System.in);
+    private static final String LINE = "    __________________________________________";
+    private final Scanner s = new Scanner(System.in);
 
     public String readCommand() {
         return s.nextLine();
     }
 
     public void showSuccessMark(boolean isMark, Task task) {
-        System.out.println(line);
-        if (isMark) {
-            System.out.println("    Nice! I've marked this task as done:");
-        } else {
-            System.out.println("    OK, I've marked this task as not done yet:");
-        }
-        System.out.println("      " + task);
-        System.out.println(line);
+        this.formatLines(() -> {
+            if (isMark) {
+                System.out.println("    Nice! I've marked this task as done:");
+            } else {
+                System.out.println("    OK, I've marked this task as not done yet:");
+            }
+            System.out.println("      " + task);
+        });
     }
 
     public void showAddTask(Task task, int totalTasks) {
-        System.out.println(line);
-        System.out.println("    Got it. I've added this task:");
-        System.out.println("      " + task);
-        System.out.println("    Now you have " + totalTasks + " tasks in the list.");
-        System.out.println(line);
+        this.formatLines(() -> {
+            System.out.println("    Got it. I've added this task:");
+            System.out.println("      " + task);
+            System.out.println("    Now you have " + totalTasks + " tasks in the list.");
+        });
     }
 
     public void showNoTasks() {
-        System.out.println(line);
-        System.out.println("    No tasks found!");
-        System.out.println(line);
+        this.formatLines(() -> System.out.println("    No tasks found!"));
     }
 
     public void showTasks(List<Task> tasks, boolean isFiltered) {
-        String header = isFiltered ? "Here are the matching tasks in your list:" : "Here are the tasks in your list:";
-        System.out.println(line);
-        System.out.println("    " + header);
-        for (int i = 0; i < tasks.size(); i++) {
-            Task task = tasks.get(i);
-            System.out.println(String.format("    %s.%s", i + 1, task));
-        }
-        System.out.println(line);
+        this.formatLines(() -> {
+            String header = isFiltered ? "Here are the matching tasks in your list:" : "Here are the tasks in your list:";
+            System.out.println("    " + header);
+            for (int i = 0; i < tasks.size(); i++) {
+                Task task = tasks.get(i);
+                System.out.println(String.format("    %s.%s", i + 1, task));
+            }
+        });
     }
 
     public void showSuccessDelete(Task t, int totalTasks) {
-        System.out.println(line);
-        System.out.println("    Noted. I've removed this task:");
-        System.out.println("    " + t);
-        System.out.println("    Now you have " + totalTasks + " tasks in the list.");
-        System.out.println(line);
+        this.formatLines(() -> {
+            System.out.println("    Noted. I've removed this task:");
+            System.out.println("    " + t);
+            System.out.println("    Now you have " + totalTasks + " tasks in the list.");
+        });
     }
 
     public void showError(String message) {
-        System.out.println(line);
-        System.out.println("    " + message);
-        System.out.println(line);
+        this.formatLines(() -> System.out.println("    " + message));
     }
 
     public void showUnknownCommand() {
-        System.out.println(line);
-        System.out.println("    Please enter something :-)");
-        System.out.println(line);
+        this.formatLines(() -> System.out.println("    Please enter something :-)"));
     }
 
     public void showSuccessLoadingStorage(String filepath) {
-        System.out.println(line);
-        System.out.println("    Data has been restored from " + filepath);
-        System.out.println(line);
+        this.formatLines(() -> System.out.println("    Data has been restored from " + filepath));
     }
 
     public void showErrorFileNotFound() {
-        System.out.println(line);
-        System.out.println("    Data file not found, creating a new one");
-        System.out.println(line);
+        this.formatLines(() -> System.out.println("    Data file not found, creating a new one"));
     }
 
     public void showErrorLoadingFile() {
-        System.out.println(line);
-        System.out.println("    Error creating new file, quitting program now...");
-        System.out.println(line);
+        this.formatLines(() -> System.out.println("    Error creating new file, quitting program now..."));
     }
 
     public void printGreetings() {
-        System.out.println(line);
-        System.out.println("    Hello I'm lynn the koala <3");
-        System.out.println("    What can I do for you?");
-        System.out.println(line);
+        this.formatLines(() -> {
+            System.out.println("    Hello I'm lynn the koala <3");
+            System.out.println("    What can I do for you?");
+        });
     }
 
     public void printExit() {
-        System.out.println(line);
-        System.out.println("    Bye. Hpoe to see you again soon!");
-        System.out.println(line);
+        this.formatLines(() -> System.out.println("    Bye. Hope to see you again soon!"));
+    }
+
+    private void formatLines(Runnable runnable) {
+        System.out.println(LINE);
+        runnable.run();
+        System.out.println(LINE);
     }
 }
