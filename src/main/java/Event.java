975d42e4481f59302a9581c6df03908c1343c726
adhigop13 <adhigop13@gmail.com>
2023-08-31 21:09:35 +0800

    Add LocalDateTime storage lvl8

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,18 +1,25 @@
-import java.util.Scanner;
+import java.time.LocalDateTime;
 
 public class Event extends Task {
     private String from;
     private String to;
 
+    private LocalDateTime fromDateTime;
+    private LocalDateTime toDateTime;
+
     public Event(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
+        this.fromDateTime = formatDateAndTime(from);
+        this.toDateTime = formatDateAndTime(to);
     }
     public Event(int status, String desciption, String from, String to) {
         super(desciption, status != 0);     //if 0, return false, else return true
         this.from = from;
         this.to = to;
+        this.fromDateTime = formatDateAndTime(from);
+        this.toDateTime = formatDateAndTime(to);
     }
 
     @Override
@@ -22,7 +29,7 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
+        return "[E]" + super.toString() + " (from: " + printDateTimeFormat(this.fromDateTime) + " to: " + printDateTimeFormat(this.toDateTime) + ")";
     }
 
 }
