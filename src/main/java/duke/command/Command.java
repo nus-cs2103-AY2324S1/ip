a3646e6560d6c37b89d3cef53d4dc045169f107e
YangHeng Tang <yanghengtang@gmail.com>
2023-08-31 19:41:50 +0800

    Add Javadocs for Command.java

--- src/main/java/duke/command/Command.java
+++ src/main/java/duke/command/Command.java
@@ -4,13 +4,30 @@ import duke.util.Storage;
 import duke.util.TaskList;
 import duke.util.Ui;
 
+/**
+ * Represents commands to be executed.
+ */
 public abstract class Command {
     private boolean exit;
 
     public Command(boolean exit) {
         this.exit = exit;
     }
+
+    /**
+     * Executes the list of commands associated with the Command object.
+     *
+     * @param tasks The given TaskList required for execution of some commands.
+     * @param ui The given Ui required for execution of some commands.
+     * @param storage The given Storage required for execution of some commands.
+     */
     public abstract void execute(TaskList tasks, Ui ui, Storage storage);
+
+    /**
+     * Returns a boolean value to indicate if Command object is an exit command.
+     *
+     * @return True if the Command object is an ExitCommand.
+     */
     public boolean isExit() {
         return this.exit;
     }
