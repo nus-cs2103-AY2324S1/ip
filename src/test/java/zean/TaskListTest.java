6ad212e6ec8bc6febd01a0bb1bdc422373ee9932
zhonghan721 <e0957606@u.nus.edu>
2023-08-31 18:17:15 +0800

    TaskListTest: Add more test cases

--- src/test/java/zean/TaskListTest.java
+++ src/test/java/zean/TaskListTest.java
@@ -89,4 +89,46 @@ public class TaskListTest {
                 new TaskList(new StorageStub()).add("read book"));
     }
 
+    @Test
+    public void printAddTaskTest2() {
+        assertEquals("\tGot it. I've added this task:\n\t  "
+                        + "[D][ ] some deadline task (by: Aug 31 2023)\n"
+                        + "\tNow you have 1 task in the list.",
+                new TaskList(new StorageStub()).add("some deadline task", "2023-08-31"));
+    }
+
+    @Test
+    public void printAddTaskTest3() {
+        assertEquals("\tGot it. I've added this task:\n\t  "
+                        + "[E][ ] some event task (from: Aug 31 2023 to: Sep 01 2023)\n"
+                        + "\tNow you have 1 task in the list.",
+                new TaskList(new StorageStub()).add("some event task", "2023-08-31", "2023-09-01"));
+    }
+
+    @Test
+    public void printDeleteTaskTest1() {
+        TaskList tasks = new TaskList(new StorageStub());
+        String add = tasks.add("read book");
+        assertEquals("\tNoted. I've removed this task.\n"
+                        + "\tNow you have 0 task in the list.",
+                tasks.deleteTask(1));
+    }
+
+    @Test
+    public void printDeleteTaskTest2() {
+        TaskList tasks = new TaskList(new StorageStub());
+        String add = tasks.add("some deadline task", "2023-08-31");
+        assertEquals("\tNoted. I've removed this task.\n"
+                        + "\tNow you have 0 task in the list.",
+                tasks.deleteTask(1));
+    }
+
+    @Test
+    public void printDeleteTaskTest3() {
+        TaskList tasks = new TaskList(new StorageStub());
+        String add = tasks.add("some event task", "2023-08-31", "2023-09-01");
+        assertEquals("\tNoted. I've removed this task.\n"
+                        + "\tNow you have 0 task in the list.",
+                tasks.deleteTask(1));
+    }
 }
