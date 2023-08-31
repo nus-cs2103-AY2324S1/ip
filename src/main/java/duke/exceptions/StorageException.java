b42968186a506f733db1485518d057b0348cf204
Tim-Siu <e1088140@u.nus.edu>
2023-08-31 09:41:06 +0800

    Adjust the exception message

--- src/main/java/duke/exceptions/StorageException.java
+++ src/main/java/duke/exceptions/StorageException.java
@@ -2,6 +2,6 @@ package duke.exceptions;
 
 public class StorageException extends ChatException {
     public StorageException(String message) {
-        super("Having trouble accessing the file: " + message + "\n");
+        super(message);
     }
 }
