296a79468fe03014fb863e6a330678278275b0e6
raynertjx <rayner742@gmail.com>
2023-08-31 16:25:22 +0800

    Add unit tests for todos

--- src/test/java/bongo/task/TodoTest.java
+++ src/test/java/bongo/task/TodoTest.java
@@ -1,2 +1,25 @@
-package bongo.task;public class TodoTest {
+package bongo.task;
+
+import bongo.helper.BongoException;
+import bongo.helper.Parser;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+public class TodoTest {
+    @Test
+    public void testTodoString() {
+        Todo sampleTodo = new Todo("make coffee");
+        String expectedOutput = "[T][ ] make coffee";
+        assertEquals(expectedOutput, sampleTodo.toString());
+    }
+
+    @Test
+    public void testEmptyTodoDescription() {
+        Exception exception = assertThrows(BongoException.class, () -> {
+            Parser.parse("todo");
+        });
+        String expectedMessage = "Please include the description of your todo.";
+        assertEquals(expectedMessage, exception.getMessage());
+    }
 }
