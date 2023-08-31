2d3d50a75f13d51f5ebfeffdc61f1c4af6f67ba0
tohpinren <tohpinren@gmail.com>
2023-08-31 12:56:01 +0800

    A-CodingStandard
    Project did not follow coding standards 100%.
    
    Coding Standards can make the code more readable.
    
    Refactor code using coding standards.

--- src/test/java/anto/TodoTest.java
+++ src/test/java/anto/TodoTest.java
@@ -1,8 +1,8 @@
 package anto;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class TodoTest {
     @Test
