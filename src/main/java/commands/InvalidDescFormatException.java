87ba1691373b7ff44db84cf12a146cc1eb8617a8
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-09-01 04:37:38 +0800

    Enable create exception with custom message

--- src/main/java/commands/InvalidDescFormatException.java
+++ src/main/java/commands/InvalidDescFormatException.java
@@ -8,7 +8,7 @@ public class InvalidDescFormatException extends CommandException{
     /**
      * Constructs a new InvalidDescFormatException with a default error message.
      */
-    public InvalidDescFormatException() {
-        super("The format of the command description if invalid!");
+    public InvalidDescFormatException(String msg) {
+        super(msg);
     }
 }
