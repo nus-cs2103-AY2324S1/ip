7e355c516a74644c3f73db88557aed6bcf42df08
fuyiqiao <>
2023-08-31 21:34:28 +0800

    Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,15 +1,21 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    String end;
-    public Deadline(String description, String end) {
+    LocalDateTime end;
+    DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd MMM, HH:mm");
+    DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    public Deadline(String description, LocalDateTime end) {
         super(description);
         this.end = end;
     }
     @Override
     public String toString() {
-        return "[D][" + this.getStatusIcon() + "] " + this.description + " (by: " + this.end + ")";
+        return "[D][" + this.getStatusIcon() + "] " + this.description
+                + " (by: " + this.end.format(outputFormatter) + ")";
     }
     public String toStorageString() {
         int stat = this.isDone ? 1 : 0;
-        return "D|" + stat + "|" + this.description + "|" + this.end;
+        return "D|" + stat + "|" + this.description + "|" + this.end.format(inputFormatter);
     }
 }
