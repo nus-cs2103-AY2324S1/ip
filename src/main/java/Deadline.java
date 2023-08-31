dde19fa19a8dad6385fe1e5cdfd2646bdf719cc6
xxiaoweii <e0967899@u.nus.edu>
2023-08-31 19:53:12 +0800

    Level 8 - Dates and Times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,15 +1,25 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
     private String deadlineDetails;
+    private LocalDateTime time;
 
     public Deadline(String task, String deadlineDetails) {
         super(task);
         this.deadlineDetails = deadlineDetails;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+        LocalDateTime dueDateTime = LocalDateTime.parse(deadlineDetails, formatter);
+        this.time = dueDateTime;
+
     }
 
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + deadlineDetails + ")";
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("HH:mm, MMM dd yyyy");
+        String timeToString = time.format(outputFormatter);
+        return "[D]" + super.toString() + " (by: " + timeToString + ")";
     }
 }
