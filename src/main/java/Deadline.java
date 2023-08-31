dad04a4ed73602684e0336391635e85c4a43a7f4
Darren159 <darren_low42@outlook.sg>
2023-08-31 16:14:55 +0800

    Add support for saving and loading tasks from hard disk

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -9,6 +9,11 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + this.by + ")";
+    }
+
+    @Override
+    public String toFileString() {
+        return "D" + super.toFileString() + " | " + this.by;
     }
 }
