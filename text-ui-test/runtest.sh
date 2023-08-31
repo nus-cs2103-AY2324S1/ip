72fea6bd2251ae7b05ff4d8f3844d0aa55569d28
Woo Jia Hao <woojiahao1234@gmail.com>
2023-08-31 21:47:38 +0800

    Fix runtest script to work with Gradle

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -12,19 +12,33 @@ then
     rm ACTUAL.TXT
 fi
 
+# Change to root directory to compile the jar
+cd ../
+
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! ./gradlew shadowJar
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
+# Temporarily move the existing save file to another name so that we can test and tear down
+mv data/data.json data/backup.json
+
 # run the program, feed cyrus.commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+# Execute the jar instead
+# Must run the jar from root directory
+java -jar build/libs/duke.jar < text-ui-test/input.txt > text-ui-test/ACTUAL.txt
+
+cd text-ui-test/
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
 dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+
+# Once done with test, restore original data file
+rm ../data/data.json
+mv ../data/backup.json ../data/data.json
 
 # compare the output to the expected output
 diff ACTUAL.TXT EXPECTED-UNIX.TXT
