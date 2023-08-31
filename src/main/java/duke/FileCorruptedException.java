adb07e7dc104a358dc5f50fadd21795bbb1d2186
chuababyy <altheachuajiaxuan@gmail.com>
2023-08-31 17:54:17 +0800

    Add new header comments

--- src/main/java/duke/FileCorruptedException.java
+++ src/main/java/duke/FileCorruptedException.java
@@ -1,6 +1,14 @@
 package duke;
 
+/**
+ * Represents the Exception which occurs when a file has been corrupted during loading.
+ * This exception is a subclass of {@link FileLoadException}.
+ */
 public class FileCorruptedException extends FileLoadException {
+    /**
+     * Constructs a new FileCorruptedException with the specified error message.
+     * @param message The error message indicating the cause of the exception.
+     */
     public FileCorruptedException(String message) {
         super(message);
     }
