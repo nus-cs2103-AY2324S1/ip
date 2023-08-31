4fe58216de9bbcc76259411f3863519d9b2329ec
owenyeo <owen_nly@hotmail.com>
2023-08-31 13:06:28 +0800

    Added InvalidIndexException Added saveTasks to save data on a text file. Added loadTasks for potential future use.

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /*
@@ -23,6 +29,9 @@ public class ChatBot {
 
     //String representing a border.
     private static final String BORDER = "____________________________________________________________\n";
+
+    //String representing the file directory.
+    private static final String DATA_SAVE_PATH = "src/main/java/data/chatBot.txt"; 
 
     //Enum Command to make code cleaner and allow for the use of 
     //switch case statements.
@@ -106,12 +115,14 @@ public class ChatBot {
                     throw new InvalidDescriptionException("What? Where's your label? Stop this.");
                 }
                 list.add(new ToDo(taskString));
+                saveTasks();
                 break;
 
             case ADD_DEADLINE:
                 try {
                     String[] deadlineParts = taskString.split("/by");
                     list.add(new Deadline(deadlineParts[0].trim(), deadlineParts[1].trim()));
+                    saveTasks();
                 } catch (IndexOutOfBoundsException e) {
                     throw new InvalidDescriptionException("Are you stupid? Can you follow instructions?");
                 }
@@ -123,6 +134,7 @@ public class ChatBot {
                     String eventLabel = eventParts[0];
                     String[] eventParts2 = eventParts[1].split("/to");
                     list.add(new Event(eventLabel.trim(), eventParts2[0].trim(), eventParts2[1].trim()));
+                    saveTasks();
                 } catch (IndexOutOfBoundsException e) {
                     throw new InvalidDescriptionException("Are you stupid? Can you follow instructions?");
                 }
@@ -166,14 +178,15 @@ public class ChatBot {
         System.out.println("Took you long enough. I've marked this task as done:");
         System.out.println(task.toString());
         System.out.println(BORDER);
+
+        saveTasks();
     }
 
-    /*
-     * To unmark a list item as undone.
-     * 
-     * @param int listNum item on the list to unmark.
-     * @return void
-     */
+     /**
+      * To unmark a list item as undone.
+      *
+      * @param listNum Index of the item on the list to unmark.
+      */
     public void unmark(int listNum) {
         int index = listNum - 1;
 
@@ -184,13 +197,15 @@ public class ChatBot {
         System.out.println("You incompetent child. I've unmarked the task. Please get it together.");
         System.out.println(task.toString());
         System.out.println(BORDER);
+
+        saveTasks();
         
     }
 
-    /*
-     * Deletes the item off the list and prints it out with a message.
+    /**
+     *  Deletes the item off the list and prints it out with a message.
      * 
-     * @return void
+     * @param listNum Index of the item of the list to delete.
      */
     public void delete(int listNum) {
         int index = listNum - 1;
@@ -201,19 +216,54 @@ public class ChatBot {
         System.out.println("I knew you couldn't finish it. Or maybe you did. I don't care. Deleted:\n");
         System.out.println(task.toString());
         System.out.println("Now you have an overwhelming " + (list.size() - 1) + " things to do.\n");
-
         System.out.println(BORDER);
 
         list.remove(index);
+        saveTasks();
     }
 
-    /* 
-     * Reads the input of the user, and executes the commands accordingly.
-     * If a command is unknown, throws an exception.
-     * 
-     * @param String message User input.
-     * @return void
-     */
+    private void loadTasks() {
+        try {
+            File file = new File(DATA_SAVE_PATH);
+            
+            if (file.exists()) {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line;
+
+                while ((line = br.readLine()) != null) {
+                    
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error loading tasks: " + e.getMessage());
+        }
+    }
+
+    private void saveTasks() {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter
+            (DATA_SAVE_PATH, false));
+
+            for (int i = 0; i < list.size(); i++) {
+                bw.write(list.get(i).toString());
+                bw.newLine();
+            }
+            bw.close();
+
+        } catch (IOException e) {
+            System.out.println("Error writing file:" + e.getMessage());
+        }
+    }
+
+     /**
+      * Reads the input of the user, and executes the commands accordingly.
+      * If a command is unknown, throws an exception.
+      *
+      * @param input
+      * @throws InvalidDescriptionException
+      * @throws InvalidCommandException
+      * @throws InvalidIndexException
+      */
     public void readInput(String input) throws 
         InvalidDescriptionException, InvalidCommandException, InvalidIndexException {
         //Split the input so that we can read integers.
@@ -237,6 +287,8 @@ public class ChatBot {
                     mark(Integer.parseInt(inputStrings[1]));
                 } catch (NumberFormatException e) {
                     throw new InvalidIndexException("Are you stupid? That's not a number.");
+                } catch (IndexOutOfBoundsException e) {
+                    throw new InvalidIndexException("That's not even a number on the list, idiot.");
                 }
                 break;
             case UNMARK:
@@ -244,6 +296,8 @@ public class ChatBot {
                     unmark(Integer.parseInt(inputStrings[1]));
                 } catch (NumberFormatException e) {
                     throw new InvalidIndexException("Are you stupid? That's not a number.");
+                } catch (IndexOutOfBoundsException e) {
+                    throw new InvalidIndexException("That's not even a number on the list, idiot.");
                 }
                 break;
 
@@ -264,7 +318,9 @@ public class ChatBot {
                     delete(Integer.parseInt(inputStrings[1]));
                 } catch (NumberFormatException e) {
                     throw new InvalidIndexException("Are you stupid? That's not a number.");
-                } 
+                } catch (IndexOutOfBoundsException e) {
+                    throw new InvalidIndexException("That's not even a number on the list, idiot.");
+                }
                 break;
 
             default:
--- src/main/java/InvalidIndexException.java
+++ src/main/java/InvalidIndexException.java
@@ -1,7 +1,11 @@
+/*
+ * Exception thrown when the index given for mark, unmark, or delete is wrong.
+ * 
+ * @author Owen Yeo
+ */
 public class InvalidIndexException extends ChatBotException {
 
     public InvalidIndexException(String e) {
         super(e);
     }
-    
 }
