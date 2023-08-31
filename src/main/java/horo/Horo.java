1770a13c307f9ee72802a2793f7d0c6042196bae
Xavier <xavieroh05@gmail.com>
2023-08-31 21:24:18 +0800

    Add javafx to gradle

--- src/main/java/horo/Horo.java
+++ src/main/java/horo/Horo.java
@@ -9,17 +9,84 @@ import horo.data.Event;
 import horo.data.TaskList;
 import horo.data.Todo;
 
-public class Horo {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Horo extends Application {
 
   private Storage storage;
   private TaskList taskList;
   private Ui ui;
   private static Scanner scanner = new Scanner(System.in);
 
+  private ScrollPane scrollPane;
+  private VBox dialogContainer;
+  private TextField userInput;
+  private Button sendButton;
+  private Scene scene;
+
+  public Horo() {
+  }
+
   public Horo(String filePath) {
     ui = new Ui();
     storage = new Storage(filePath);
     taskList = storage.load();
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    scrollPane = new ScrollPane();
+    dialogContainer = new VBox();
+    scrollPane.setContent(dialogContainer);
+
+    userInput = new TextField();
+    sendButton = new Button("Send");
+
+    AnchorPane mainLayout = new AnchorPane();
+    mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+    scene = new Scene(mainLayout);
+
+    stage.setScene(scene);
+    stage.show();
+
+    stage.setTitle("Duke");
+    stage.setResizable(false);
+    stage.setMinHeight(600.0);
+    stage.setMinWidth(400.0);
+
+    mainLayout.setPrefSize(400.0, 600.0);
+
+    scrollPane.setPrefSize(385, 535);
+    scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+    scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+    scrollPane.setVvalue(1.0);
+    scrollPane.setFitToWidth(true);
+
+    dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+    userInput.setPrefWidth(325.0);
+
+    sendButton.setPrefWidth(55.0);
+
+    AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+    AnchorPane.setBottomAnchor(sendButton, 1.0);
+    AnchorPane.setRightAnchor(sendButton, 1.0);
+
+    AnchorPane.setLeftAnchor(userInput, 1.0);
+    AnchorPane.setBottomAnchor(userInput, 1.0);
+
+    // new Horo("./data/tasks.txt").run();
   }
 
   public void run() {
@@ -94,7 +161,7 @@ public class Horo {
   }
 
   public static void main(String[] args) {
-    new Horo("./data/tasks.txt").run();
+    launch(args);
   }
 
   private static void exit() {
@@ -102,5 +169,4 @@ public class Horo {
     scanner.close();
     System.exit(0);
   }
-
 }
