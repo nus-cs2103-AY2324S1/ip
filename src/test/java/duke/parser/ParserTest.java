dc987dda632d7eb4ba5aecc19b766f7a3398b0dd
Jingya <e0968387@u.nus.edu>
2023-09-01 01:13:24 +0800

    Add certain classes for JUnit Testing

---
+++ src/test/java/duke/parser/ParserTest.java
@@ -0,0 +1,19 @@
+package duke.parser;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+    @Test
+    public void handleDeleteTask_nonsenseInput_noChangesMade(){
+
+
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void anotherDummyTest(){
+        assertEquals(4, 4);
+    }
+}
---
+++ src/test/java/duke/tasks/TaskListTest.java
@@ -0,0 +1,17 @@
+package duke.tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+    @Test
+    public void dummyTest(){
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void anotherDummyTest(){
+        assertEquals(4, 4);
+    }
+}
