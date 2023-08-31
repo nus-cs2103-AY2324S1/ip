28d4324ebb3904a388f357df3a08da20362dc2f8
Tim-Siu <e1088140@u.nus.edu>
2023-08-31 09:32:44 +0800

    Create the storage file when it doesn't exit

--- src/main/java/duke/storage/Storage.java
+++ src/main/java/duke/storage/Storage.java
@@ -17,7 +17,16 @@ public class Storage {
         TaskList tasks = new TaskList();
         File file = new File(filePath);
 
-        if (file.exists()) {
+        if (!file.exists()) {
+            try {
+                if (file.getParentFile() != null) {
+                    file.getParentFile().mkdirs();
+                }
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new StorageException("Unable to create new file at " + filePath);
+            }
+        } else {
             try (BufferedReader br = new BufferedReader(new FileReader(file))) {
                 String line;
                 while ((line = br.readLine()) != null) {
@@ -27,7 +36,7 @@ public class Storage {
                     }
                 }
             } catch (IOException e) {
-                throw new StorageException(filePath);
+                throw new StorageException("Unable to create new file at " + filePath);
             } catch (TimeParsingException e) {
                 throw new RuntimeException(e);
             }
@@ -60,40 +69,3 @@ public class Storage {
     }
 
 }
-//    public List<Task> load() throws StorageException {
-//        List<Task> tasks = new ArrayList<>();
-//        File file = new File(filePath);
-//
-//        if (file.exists()) {
-//            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-//                String line;
-//                while ((line = br.readLine()) != null) {
-//                    Task Duke.task = parseTask(line);
-//                    if (Duke.task != null) {
-//                        tasks.add(Duke.task);
-//                    }
-//                }
-//            } catch (IOException e) {
-//                throw new StorageException(filePath);
-//            } catch (TimeParsingException e) {
-//                throw new RuntimeException(e);
-//            }
-//        }
-//        return tasks;
-//    }
-//
-//
-//    private Task parseTask(String line) throws TimeParsingException {
-//        String[] parts = line.split(" \\| ");
-//        switch (parts[0]) {
-//        case "T":
-//            return new ToDo(parts[2], "1".equals(parts[1]));
-//        case "D":
-//            return new Deadline(parts[2], parts[3], "1".equals(parts[1]));
-//        case "E":
-//            String[] eventTimes = parts[3].split("--");
-//            return new Event(parts[2], eventTimes[0], eventTimes[1], "1".equals(parts[1]));
-//        default:
-//            return null;
-//        }
-//    }
