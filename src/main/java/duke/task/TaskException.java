c79fa5156a98b6e4904a5400eb5021922fd220bc
YangHeng Tang <yanghengtang@gmail.com>
2023-08-31 19:51:56 +0800

    Add Javadocs for TaskException.java

--- src/main/java/duke/task/TaskException.java
+++ src/main/java/duke/task/TaskException.java
@@ -1,5 +1,8 @@
 package duke.task;
 
+/**
+ * Represents an error that occurred during the initialisation of a Deadline object.
+ */
 public class TaskException extends Exception {
 
     public TaskException(String format) {
