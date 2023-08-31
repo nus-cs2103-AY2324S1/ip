d62e616c79f7e547b3069a8db13dd59ec11a3736
dhruvir29 <dhruvirathod29@gmail.com>
2023-08-31 16:59:49 +0800

    Optimisations done on Week 1 Code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,20 +4,23 @@ import java.util.ArrayList;
 
 
 public class Duke {
+    static List<Task> tasks;
+    static int count;
     public static void main(String[] args) {
         // an array list of tasks
-        List<Task> tasks = new ArrayList<>();
+         tasks = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         printHorizontalLine();
-        System.out.println("\t " + "Hello! I'm Buddy! \n" +
-                "\t " + "What can I do for you? ");
+        System.out.println("\t " + "Hey there amigo, excited to meet you! I'm Buddy, your friendly chat companion!\n" +
+                "\t " + "What can I do for you?");
         printHorizontalLine();
         int count = 0;
+
         while (count <=100 ) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
-                System.out.println("\t " + "Bye! Hope to see you again soon! ");
+                System.out.println("\t " + "Bye! Hope to see you again soon!");
                 printHorizontalLine();
                 break;
             }
@@ -50,7 +53,8 @@ public class Duke {
                     printHorizontalLine();
                     System.out.println("\tHere are the tasks in your list:");
                     if (tasks.size() == 0) {
-                         throw new DukeException("\tThis list is empty!");
+                         throw new DukeException("\t Seems like you have no tasks at the moment :) ");
+
                     }
                     for (int i = 1; i <= tasks.size(); i++) {
                         System.out.println("\t" + i + ". " + tasks.get(i - 1).toString());
@@ -62,12 +66,13 @@ public class Duke {
                 }
             }
 
+            // deleting task from list
             else if (input.startsWith("delete ")) {
                 try {
                     int pos = 0;
                     pos = Integer.parseInt(input.substring(7).trim());
-                    if (pos > tasks.size()) {
-                        throw new DukeException("\tThis number is out of bounds");
+                    if (pos > tasks.size() || pos == 0) {
+                        throw new DukeException("\tThis number is out of bounds! ");
                     }
                     Task element = tasks.get(pos - 1);
                     tasks.remove(pos - 1);
@@ -81,59 +86,10 @@ public class Duke {
                 }
             }
 
-
             // adding task to list
             else {
                 try {
-                    if (input.startsWith("todo")) {
-                        if (input.trim().length() <= 4) {
-                            throw new DukeException("\t☹ OOPS!!! The description of a todo cannot be empty");
-                        }
-                        Task task = new ToDo(input.substring(5));
-                        tasks.add(task);
-                        printHorizontalLine();
-                        System.out.println("\tOk you have added this task:");
-                        System.out.println("\t" + task.toString());
-                        System.out.println("\tNow you have " + tasks.size() + " tasks in the list");
-                        printHorizontalLine();
-                        count++;
-                    }
-
-                    else if (input.startsWith("deadline")) {
-                        if (input.trim().length() <= 8) {
-                            throw new DukeException("\t☹ OOPS!!! The description of a deadline cannot be empty");
-                        }
-                        int index = input.lastIndexOf("/by");
-                        Task task = new Deadline(input.substring(9, index - 1), input.substring(index + 4));
-                        tasks.add(task);
-                        printHorizontalLine();
-                        System.out.println("\tOk you have added this task:");
-                        System.out.println("\t" + task.toString());
-                        System.out.println("\tNow you have " + tasks.size() + " tasks in the list");
-                        printHorizontalLine();
-                        count++;
-                    }
-
-                    else if (input.startsWith("event")) {
-                        if (input.trim().length() <= 5) {
-                            throw new DukeException("\t☹ OOPS!!! The description of an event cannot be empty");
-                        }
-                        int indexFrom = input.lastIndexOf("/from");
-                        int indexTo = input.lastIndexOf("/to");
-                        Task task = new Event(input.substring(6, indexFrom - 1),
-                                input.substring(indexFrom + 6, indexTo - 1), input.substring(indexTo + 4));
-                        tasks.add(task);
-                        printHorizontalLine();
-                        System.out.println("\tOk I have added this task:");
-                        System.out.println("\t" + task.toString());
-                        System.out.println("\tNow you have " + tasks.size() + " tasks in the list");
-                        printHorizontalLine();
-                        count++;
-                    }
-
-                    else {
-                        throw new DukeException("\tHey bud! Sorry I don't quite know what you mean :-(");
-                    }
+                    addTaskToList(input);
                 }
                 catch (DukeException e){
                     e.printMessage();
@@ -143,8 +99,62 @@ public class Duke {
 
     }
 
+    public static void addTaskToList(String input) throws DukeException {
+        if (input.startsWith("todo")) {
+            if (input.trim().length() <= 4 ) {
+                throw new DukeException("\t Sorry! The description of a todo cannot be empty :(");
+            }
+            Task task = new ToDo(input.substring(5));
+            tasks.add(task);
+            printHorizontalLine();
+            System.out.println("\tNo problem! I have added this task:");
+            System.out.println("\t" + task.toString());
+            System.out.println("\tNow you have " + tasks.size() + " tasks in the list");
+            printHorizontalLine();
+            count++;
+        }
+
+        else if (input.startsWith("deadline")) {
+            if (input.trim().length() <= 8) {
+                throw new DukeException("\t Sorry! The description of a deadline cannot be empty :(");
+            }
+            int index = input.lastIndexOf("/by");
+            Task task = new Deadline(input.substring(9, index - 1), input.substring(index + 4));
+            tasks.add(task);
+            printHorizontalLine();
+            System.out.println("\tNo problem! I have added this task:");
+            System.out.println("\t" + task.toString());
+            System.out.println("\tNow you have " + tasks.size() + " tasks in the list");
+            printHorizontalLine();
+            count++;
+        }
+
+        else if (input.startsWith("event")) {
+            if (input.trim().length() <= 5) {
+                throw new DukeException("\t Sorry! The description of an event cannot be empty :(");
+            }
+            int indexFrom = input.lastIndexOf("/from");
+            int indexTo = input.lastIndexOf("/to");
+            Task task = new Event(input.substring(6, indexFrom - 1),
+                    input.substring(indexFrom + 6, indexTo - 1), input.substring(indexTo + 4));
+            tasks.add(task);
+            printHorizontalLine();
+            System.out.println("\tNo problem! I have added this task:");
+            System.out.println("\t" + task.toString());
+            System.out.println("\tNow you have " + tasks.size() + " tasks in the list");
+            printHorizontalLine();
+            count++;
+        }
+
+        else {
+            throw new DukeException("\tHey bud! Sorry I don't quite know what you mean :-(");
+        }
+
+    }
+
+
     public static void printHorizontalLine() {
-        System.out.println("    ________________________________________________");
+        System.out.println("    __________________________________________________________________");
     }
 
 
