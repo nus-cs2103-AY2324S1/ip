0957234a1691cb62af8819778f1681276d35e200
yytan25 <tan.yang.yi.2001@gmail.com>
2023-08-31 16:48:27 +0800

    Add Level-8 features

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,24 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+
 public class Deadline extends Task {
 
-    protected String due;
-    public Deadline(String details, String due) {
+    protected LocalDateTime due;
+    public Deadline(String details, LocalDateTime due) {
         super(details);
         this.due = due;
     }
 
-    public Deadline(String details, boolean isCompleted, String due) {
+    public Deadline(String details, boolean isCompleted, LocalDateTime due) {
         super(details, isCompleted);
         this.due = due;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.due + ")";
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("E, dd MMM yyyy");
+        return "[D]" + super.toString() + " (by: " +
+                dateFormatter.format(this.due.toLocalDate()) + " " +
+                this.due.toLocalTime().toString() + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,18 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+
 public class Event extends Task {
 
-    protected String start;
-    protected String end;
-    public Event(String details, String start, String end) {
+    protected LocalDateTime start;
+    protected LocalDateTime end;
+    public Event(String details, LocalDateTime start, LocalDateTime end) {
         super(details);
         this.start = start;
         this.end = end;
     }
 
-    public Event(String details, boolean isCompleted, String start, String end) {
+    public Event(String details, boolean isCompleted, LocalDateTime start,
+                 LocalDateTime end) {
         super(details, isCompleted);
         this.start = start;
         this.end = end;
@@ -16,7 +20,14 @@ public class Event extends Task {
 
     @Override
     public String toString() {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("dd MMM yyyy");
+        String startDate = dateFormatter.format(this.start.toLocalDate());
+        String endDate = dateFormatter.format(this.start.toLocalDate());
+        String startTime = this.start.toLocalTime().toString();
+        String endTime = this.end.toLocalTime().toString();
         return "[E]" + super.toString() + String.format(
-                " (from: %s to: %s)", this.start, this.end);
+                " (from: %s to: %s)",
+                startDate + " " + startTime,
+                endDate + " " + endTime);
     }
 }
