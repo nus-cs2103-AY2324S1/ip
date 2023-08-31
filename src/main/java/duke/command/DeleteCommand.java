ef5bc7c149376a1e0dda9360787a6b04a10bb07c
YangHeng Tang <yanghengtang@gmail.com>
2023-08-31 20:35:15 +0800

    Update DeleteCommand.java code quality

--- src/main/java/duke/command/DeleteCommand.java
+++ src/main/java/duke/command/DeleteCommand.java
@@ -14,8 +14,8 @@ public class DeleteCommand extends Command {
     }
 
     @Override
-    public void execute(TaskList list, Ui ui, Storage storage) {
-        Task task = list.remove(this.index);
-        ui.printDelete(task, list.size());
+    public void execute(TaskList taskList, Ui ui, Storage storage) {
+        Task task = taskList.remove(this.index);
+        ui.printDelete(task, taskList.size());
     }
 }
