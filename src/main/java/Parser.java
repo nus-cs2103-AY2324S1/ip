cf5a465c6631658c1a996cd5cd53d121f6f56cb1
xxiaoweii <e0967899@u.nus.edu>
2023-08-31 20:12:50 +0800

    Level 8 - Dates and Times

---
+++ src/main/java/Parser.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Parser {
+
+    public static LocalDateTime formatDate(String dateTimeString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+        LocalDateTime parseDate = LocalDateTime.parse(dateTimeString, formatter);
+        return parseDate;
+    }
+
+    public static String dateToString(LocalDateTime dateTime) {
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy h:mma");
+        String formattedDate = dateTime.format(outputFormatter);
+        return formattedDate;
+    }
+}
