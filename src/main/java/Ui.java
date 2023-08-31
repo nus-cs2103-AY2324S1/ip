873f5083390ce11356120a71db1025718d1b4014
rayyan35p <rayyanis0908@gmail.com>
2023-09-01 02:04:35 +0800

    Add Ui class

---
+++ src/main/java/Ui.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ui {
+    private Scanner scanner;
+    public Ui()
+    {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String getInput() {
+        String input = scanner.nextLine();
+        return input;
+    }
+
+    public String getFirstInput() {
+        System.out.println("Hello! I'm Bob");
+        System.out.println("What can I do for you?");
+        return this.getInput();
+    }
+}
