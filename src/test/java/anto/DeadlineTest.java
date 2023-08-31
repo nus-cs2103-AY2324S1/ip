4936263ad3630c2955c2660ee8a7393ee759e97a
tohpinren <tohpinren@gmail.com>
2023-08-31 11:36:15 +0800

    A-JUnit
    Project does not have JUnit tests.
    
    We should use JUnit to automate tests.
    
    Add JUnit tests to project

---
+++ src/test/java/anto/DeadlineTest.java
@@ -0,0 +1,33 @@
+package anto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class DeadlineTest {
+    @Test
+    public void createDeadlineTaskTest() {
+        try {
+            Deadline newTodoTask = new Deadline("Read book", "01-09-2023 23:59");
+            assertEquals("[D] [ ] Read book (by: 01 Sep 2023 23:59)", newTodoTask.toString());
+        } catch (DukeException e) {
+            fail("Expected exception was thrown");
+        }
+    }
+
+    @Test
+    public void markAndUnmarkTodoTaskTest() {
+        try {
+            Deadline newTodoTask = new Deadline("Read book", "01-09-2023 23:59");
+            newTodoTask.markAsDone();
+            assertEquals("X", newTodoTask.getStatusIcon());
+            assertEquals("[D] [X] Read book (by: 01 Sep 2023 23:59)", newTodoTask.toString());
+            newTodoTask.unmark();
+            assertEquals(" ", newTodoTask.getStatusIcon());
+            assertEquals("[D] [ ] Read book (by: 01 Sep 2023 23:59)", newTodoTask.toString());
+        } catch (DukeException e) {
+            fail("Expected exception was thrown");
+        }
+    }
+}
---
+++ src/test/java/anto/EventTest.java
@@ -0,0 +1,36 @@
+package anto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class EventTest {
+    @Test
+    public void createDeadlineTaskTest() {
+        try {
+            Event newTodoTask = new Event("Book meeting", "02-09-2023 20:00", "02-09-2023 21:00");
+            assertEquals("[E] [ ] Book meeting (from: 02 Sep 2023 20:00 to: 02 Sep 2023 21:00)",
+                    newTodoTask.toString());
+        } catch (DukeException e) {
+            fail("Expected exception was thrown");
+        }
+    }
+
+    @Test
+    public void markAndUnmarkTodoTaskTest() {
+        try {
+            Event newTodoTask = new Event("Book meeting", "02-09-2023 20:00", "02-09-2023 21:00");
+            newTodoTask.markAsDone();
+            assertEquals("X", newTodoTask.getStatusIcon());
+            assertEquals("[E] [X] Book meeting (from: 02 Sep 2023 20:00 to: 02 Sep 2023 21:00)",
+                    newTodoTask.toString());
+            newTodoTask.unmark();
+            assertEquals(" ", newTodoTask.getStatusIcon());
+            assertEquals("[E] [ ] Book meeting (from: 02 Sep 2023 20:00 to: 02 Sep 2023 21:00)",
+                    newTodoTask.toString());
+        } catch (DukeException e) {
+            fail("Expected exception was thrown");
+        }
+    }
+}
