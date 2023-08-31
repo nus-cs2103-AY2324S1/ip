ce15b5931318021f05d568700241eaf0c002d358
migfoo02 <migfoo02@gmail.com>
2023-08-31 22:28:28 +0800

    Revert "Add read and write function"
    This reverts commit be81397a497786685669addf1a3e1d822c8e8b0f.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -6,25 +6,6 @@ public class Deadline extends Task {
         this.endTime = endTime;
     }
 
-    public String getEndTime() {
-        return this.endTime;
-    }
-
-    public String writeTaskToFile() {
-        return String.format("%s | %s | %s | %s", "D",
-                this.isDone() ? 1 : 0,
-                this.getName(),
-                this.getEndTime());
-    }
-
-    public static Deadline readTaskFromFile(String[] args) {
-        Deadline newDeadlineTask = new Deadline(args[2], args[3]);
-        if (args[1].equals("1")) {
-            newDeadlineTask.markDone();
-        }
-        return newDeadlineTask;
-    }
-
     @Override
     public String toString() {
         return String.format("[D]%s (by: %s)", super.toString(), this.endTime);
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -23,28 +23,6 @@ public abstract class Task {
         return this.name;
     }
 
-    public abstract String writeTaskToFile();
-
-    public static Task readTaskFromFile(String input) {
-        String[] args = input.split(" \\| ");
-        Task task;
-        switch (args[0]) {
-            case "T":
-                task = Todo.readTaskFromFile(args);
-                break;
-            case "D":
-                task = Deadline.readTaskFromFile(args);
-                break;
-            case "E":
-                task = Event.readTaskFromFile(args);
-                break;
-            default:
-                task = null;
-                break;
-        }
-        return task;
-    }
-
     @Override
     public String toString() {
         String status = this.isDone ? "X" : " ";
