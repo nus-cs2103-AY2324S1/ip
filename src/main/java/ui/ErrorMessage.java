2b4c2c676bccc19284a3661ffec3a5427aad3e14
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-09-01 18:24:59 +0800

    Edit error message correspond to exception

--- src/main/java/ui/ErrorMessage.java
+++ src/main/java/ui/ErrorMessage.java
@@ -7,15 +7,11 @@ public class ErrorMessage {
     public static final Map<String, String> mapper = new HashMap<>();
 
     static {
-        mapper.put("InvalidCommandException",
+        mapper.put("InvalidCommandTypeException",
                 "Can't believe you're asking that! Grrr, what do you want now?");
-        mapper.put("EmptyDescException",
-                "Seriously? You want me to do something with an empty description?");
-        mapper.put("InvalidDescFormatException",
+        mapper.put("InvalidCommandFormatException",
                 "Are you trying to confuse me with this nonsense? Try again hooman!");
-        mapper.put("TaskStatusException",
-                "Grrr! Are you testing my patience? The task is already in that status!");
-        mapper.put("TaskListIndexOutOfBoundsException",
-                "Arf! Invalid task number? Seriously, can't you count? 💢");
+        mapper.put("CommandExecutionException",
+                "Argh, something went wrong! Bad hooman, what are you trying to do!?");
     }
 }
