1ed94e17c88d6a5f5d91aa9b34e83354280b276f
fry <E0968906@u.nus.edu>
2023-09-01 11:39:50 +0800

    Add enums for commands

---
+++ src/main/java/Command.java
@@ -0,0 +1,10 @@
+public enum Command {
+    MARK,
+    UNMARK,
+    LIST,
+    BYE,
+    TODO,
+    DEADLINE,
+    EVENT,
+    DELETE;
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,51 +7,55 @@ import exception.InvalidFormatException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import java.io.File;
+
 public class Duke {
-
-    public static void main(String[] args) throws DukeException {
+    private static String separator = "\n-----------------------------------------------------------------";
+    public static void main(String[] args) {
         String logo =" ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣶⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⠿⠛⠋⠉⠩⣄⠘⢿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⡏⠑⠒⠀⠀⣀⣀⠀⠀⢹⠈⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣷⡀⢀⣰⣿⡿⣿⣧⠀⠀⢡⣾⣧⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣯⣴⣿⠿⣄⣤⣾⡿⠟⠛⠛⠿⢿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣶⠿⠛⠋⠙⣿⣏⠀⠀⢻⣿⣡⣀⣀⠀⠀⠀⠀⢹⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⠿⠋⠁⠀⣀⣤⣶⣾⣿⣿⣤⣤⣾⣿⠉⠉⠙⠻⣿⠆⢀⣾⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡿⠋⠁⠀⣀⣴⣿⠿⠛⠉⠀⢀⣿⡿⠿⠟⢿⣆⠀⢀⣴⣯⣴⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠋⠀⠀⣠⣾⠟⠋⠀⠀⠀⠀⠀⣈⣿⣷⣤⣴⣾⣿⣈⣻⣿⡟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠀⣰⡿⠁⠀⣠⡾⠋⠁⠀⠀⢀⣠⣴⠶⠞⠛⠛⠋⠉⠉⠉⠉⠙⠛⠻⠷⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠰⣿⠁⠀⠀⣿⣄⣀⣠⣴⡾⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⣿⠿⣶⣄⠀⠀⠀⢀⣠⡄⠀⠀⠀⠀⠀⠀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠀⠈⠛⠶⠶⢾⣿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⣄⠉⠙⠻⠿⠟⢹⡇⠀⠀⠀⠀⠀⠀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠟⠁⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⠀⠀⠀⠀⠘⣿⣿⣦⣀⠀⠲⣾⣁⠀⠀⠀⠀⠀⡀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣦⡀⠀⠀⠀⣿⡿⣿⣿⣿⡆⠀⠉⠛⠛⠛⠛⢻⡏⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠀⣠⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⢀⡄⣸⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⠛⢦⡀⠰⣿⣿⣿⣽⣿⡇⠀⠀⠀⠀⠀⢠⡿⠀⠀ \n"
-                    + "⠀⠀⠀⠀⣀⣤⡾⢻⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⡏⠙⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠰⣽⣶⣄⠉⠻⣿⣿⣧⠀⠀⢀⣤⣾⠟⠁⠀⠀ \n"
-                    + "⢰⣶⡾⠛⠋⠉⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⢸⣸⡇⠀⣨⣿⣾⡋⠀⠀⠀⠀⢀⠀⠀⣿⡀⠀⠈⠛⢷⣄⠈⠛⣿⡆⠀⠘⣿⡀⠀⠀⠀⠀ \n"
-                    + "⠀⠙⠿⣦⣀⠀⠀⠀⠀⠀⠀⡾⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠋⠀⠙⢿⣦⣀⠀⠀⠘⣷⣄⣹⣧⠀⠀⠀⠈⢻⣦⠀⠈⠋⠀⠀⠘⣧⠀⠀⠀⠀ \n"
-                    + "⠀⠀⠀⠈⠛⠿⢶⡶⠃⠀⣰⠃⠀⠀⠀⠀⠀⢠⣿⠃⠀⠀⠀⠀⠀⠀⠉⠻⢷⣦⣤⣘⣿⡛⠛⠀⢀⣴⣶⣦⡹⣷⡀⠀⠀⠀⠀⠸⣧⠀⠀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⢠⡿⠃⠀⢀⡟⠀⠀⠀⠀⠀⠀⣼⣿⠀⠀⢀⣴⣿⣿⣷⡄⠀⠀⠈⠉⠉⠉⠉⠀⠀⢸⣿⣿⣿⣷⠻⣧⠀⠀⠀⠀⠀⢿⡆⠀⠀ \n"
-                    + "⠀⠀⠀⠀⢰⣿⠁⠀⠀⢸⠁⠀⠀⠀⠀⠈⠋⣿⠀⠀⠸⣿⣿⣿⣿⣿⡷⠀⠀⠀⠀⠀⠀⠀⠀  ⠈⠛⠿⠟⠃⠀⢻⣧⠀⠀⠀⠸⣧⠀⠀ \n"
-                    + "⠀⠀⠀⠀⣿⡇⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠉⠻⠿⠿⠋⠀⢠⡀⠀⠀⣀⣀⣀⣸⠇⠀⠀⠀⠀⠀⠈⢿⣧⠀⠀⠀⠀⣿⡀⠀ \n"
-                    + "⠀⠀⠀⢰⣿⠁⠀⠀⢰⡏⠀⠀⠀⠀⠀⠀⠀⠀⢿⡀⠀⠀⠀⠀⠀ ⠀⠀⠈⠛⠒⠛⠉⠈⠉⠀⠀⠀⠀⠀⠀⠀⠀⢸⡟⠀⠀⠀⠀⢸⡇⠀ \n"
-                    + "⠀⠀⠀⢸⣿⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⠁⠀⠀⠀⠀⢸⣿⠀ \n"
-                    + "⠀⠀⠀⢸⣿⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣤⣶⡿⠋⣿⠀⠀⠀⠀⠀⠀⣿⠀ \n"
-                    + "⠀⠀⠀⠘⣿⡄⠀⠀⢸⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣧⣄⣀⣀⣀⣠⣤⣶⣶⣾⣿⣿⣿⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⣠⠀⠀⢠⣿⡄ \n"
-                    + "⠀⠀⠀⠀⢻⣧⠀⠀⠸⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣟⠛⢻⡟⢻⡉⠉⣫⣀⠀⠀⠀⠉⠉⠛⠙⠿⣷⣿⡿⠁⢀⣰⡟⠀⠀⢸⣿⠀ \n"
-                    + "⠀⠀⠀⠀⠈⢻⣧⡀⠀⠹⣷⡀⠀⠀⠀⠀⢠⠀⠀⠀⠘⣿⣿⣿⢻⡿⠀⠈⠷⠟⠁⠘⢾⣿⣶⣤⣶⣾⡇⠀⣿⣿⣤⣶⣿⣿⠃⠀⠀⣾⡟⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠈⠻⣷⣄⡀⠹⣷⣄⠀⠀⠀⢸⣷⣤⡀⠀⠈⢻⣿⣯⣤⠀⠀⣠⡀⠀⢀⣼⣿⣿⣿⣿⣟⠁⠐⠿⣿⣿⣿⣿⠋⠀⢀⣾⠟⠀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠀⠈⠙⠿⣷⣮⣽⣷⣶⣤⣤⣿⣿⣿⣷⣶⣦⣭⣿⣿⣧⣠⠵⢯⡆⠚⣯⢿⠋⠛⠛⢫⣀⣠⣾⣿⢿⣿⣥⣤⠶⠛⠁⠀⠀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⢁⣾⣿⣿⣿⠿⠿⠿⠿⠻⢿⣿⣿⣷⣦⣤⣤⣀⣤⣤⣄⣶⣿⣿⡿⠟⠉⠀⠀⢻⡄⠀⠀⠀⠀⠀⠀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⡇⠀⠀⣀⡀⠈⢿⣧⠀⠉⠙⠛⠛⠛⠛⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⢿⡄⠀⠀⠀⠀⠀⠀ \n"
-                    + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣴⣿⣿⡏⠛⠉⢻⣿⣿⣿⣾⣿⣿⠀⢸⣿⠀⠀⠀⠀⠀⠙⠓⠢⠀⠀⠀⠀⠀⠸⠀⠀⠀⠘⣿⡄⠀⠀⠀⠀⠀ \n";
+                + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⠿⠛⠋⠉⠩⣄⠘⢿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
+                + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⡏⠑⠒⠀⠀⣀⣀⠀⠀⢹⠈⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
+                + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣷⡀⢀⣰⣿⡿⣿⣧⠀⠀⢡⣾⣧⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
+                + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣯⣴⣿⠿⣄⣤⣾⡿⠟⠛⠛⠿⢿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
+                + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣶⠿⠛⠋⠙⣿⣏⠀⠀⢻⣿⣡⣀⣀⠀⠀⠀⠀⢹⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
+                + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⠿⠋⠁⠀⣀⣤⣶⣾⣿⣿⣤⣤⣾⣿⠉⠉⠙⠻⣿⠆⢀⣾⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
+                + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡿⠋⠁⠀⣀⣴⣿⠿⠛⠉⠀⢀⣿⡿⠿⠟⢿⣆⠀⢀⣴⣯⣴⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
+                + "⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠋⠀⠀⣠⣾⠟⠋⠀⠀⠀⠀⠀⣈⣿⣷⣤⣴⣾⣿⣈⣻⣿⡟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
+                + "⠀⠀⠀⠀⠀⠀⠀⣰⡿⠁⠀⣠⡾⠋⠁⠀⠀⢀⣠⣴⠶⠞⠛⠛⠋⠉⠉⠉⠉⠙⠛⠻⠷⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
+                + "⠀⠀⠀⠀⠀⠀⠰⣿⠁⠀⠀⣿⣄⣀⣠⣴⡾⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⣿⠿⣶⣄⠀⠀⠀⢀⣠⡄⠀⠀⠀⠀⠀⠀⠀ \n"
+                + "⠀⠀⠀⠀⠀⠀⠀⠈⠛⠶⠶⢾⣿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⣄⠉⠙⠻⠿⠟⢹⡇⠀⠀⠀⠀⠀⠀⠀ \n"
+                + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠟⠁⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⠀⠀⠀⠀⠘⣿⣿⣦⣀⠀⠲⣾⣁⠀⠀⠀⠀⠀⡀⠀ \n"
+                + "⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣦⡀⠀⠀⠀⣿⡿⣿⣿⣿⡆⠀⠉⠛⠛⠛⠛⢻⡏⠀ \n"
+                + "⠀⠀⠀⠀⠀⠀⠀⣠⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⢀⡄⣸⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⠛⢦⡀⠰⣿⣿⣿⣽⣿⡇⠀⠀⠀⠀⠀⢠⡿⠀⠀ \n"
+                + "⠀⠀⠀⠀⣀⣤⡾⢻⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⡏⠙⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠰⣽⣶⣄⠉⠻⣿⣿⣧⠀⠀⢀⣤⣾⠟⠁⠀⠀ \n"
+                + "⢰⣶⡾⠛⠋⠉⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⢸⣸⡇⠀⣨⣿⣾⡋⠀⠀⠀⠀⢀⠀⠀⣿⡀⠀⠈⠛⢷⣄⠈⠛⣿⡆⠀⠘⣿⡀⠀⠀⠀⠀ \n"
+                + "⠀⠙⠿⣦⣀⠀⠀⠀⠀⠀⠀⡾⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠋⠀⠙⢿⣦⣀⠀⠀⠘⣷⣄⣹⣧⠀⠀⠀⠈⢻⣦⠀⠈⠋⠀⠀⠘⣧⠀⠀⠀⠀ \n"
+                + "⠀⠀⠀⠈⠛⠿⢶⡶⠃⠀⣰⠃⠀⠀⠀⠀⠀⢠⣿⠃⠀⠀⠀⠀⠀⠀⠉⠻⢷⣦⣤⣘⣿⡛⠛⠀⢀⣴⣶⣦⡹⣷⡀⠀⠀⠀⠀⠸⣧⠀⠀⠀ \n"
+                + "⠀⠀⠀⠀⠀⢠⡿⠃⠀⢀⡟⠀⠀⠀⠀⠀⠀⣼⣿⠀⠀⢀⣴⣿⣿⣷⡄⠀⠀⠈⠉⠉⠉⠉⠀⠀⢸⣿⣿⣿⣷⠻⣧⠀⠀⠀⠀⠀⢿⡆⠀⠀ \n"
+                + "⠀⠀⠀⠀⢰⣿⠁⠀⠀⢸⠁⠀⠀⠀⠀⠈⠋⣿⠀⠀⠸⣿⣿⣿⣿⣿⡷⠀⠀⠀⠀⠀⠀⠀⠀  ⠈⠛⠿⠟⠃⠀⢻⣧⠀⠀⠀⠸⣧⠀⠀ \n"
+                + "⠀⠀⠀⠀⣿⡇⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠉⠻⠿⠿⠋⠀⢠⡀⠀⠀⣀⣀⣀⣸⠇⠀⠀⠀⠀⠀⠈⢿⣧⠀⠀⠀⠀⣿⡀⠀ \n"
+                + "⠀⠀⠀⢰⣿⠁⠀⠀⢰⡏⠀⠀⠀⠀⠀⠀⠀⠀⢿⡀⠀⠀⠀⠀⠀ ⠀⠀⠈⠛⠒⠛⠉⠈⠉⠀⠀⠀⠀⠀⠀⠀⠀⢸⡟⠀⠀⠀⠀⢸⡇⠀ \n"
+                + "⠀⠀⠀⢸⣿⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⠁⠀⠀⠀⠀⢸⣿⠀ \n"
+                + "⠀⠀⠀⢸⣿⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣤⣶⡿⠋⣿⠀⠀⠀⠀⠀⠀⣿⠀ \n"
+                + "⠀⠀⠀⠘⣿⡄⠀⠀⢸⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣧⣄⣀⣀⣀⣠⣤⣶⣶⣾⣿⣿⣿⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⣠⠀⠀⢠⣿⡄ \n"
+                + "⠀⠀⠀⠀⢻⣧⠀⠀⠸⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣟⠛⢻⡟⢻⡉⠉⣫⣀⠀⠀⠀⠉⠉⠛⠙⠿⣷⣿⡿⠁⢀⣰⡟⠀⠀⢸⣿⠀ \n"
+                + "⠀⠀⠀⠀⠈⢻⣧⡀⠀⠹⣷⡀⠀⠀⠀⠀⢠⠀⠀⠀⠘⣿⣿⣿⢻⡿⠀⠈⠷⠟⠁⠘⢾⣿⣶⣤⣶⣾⡇⠀⣿⣿⣤⣶⣿⣿⠃⠀⠀⣾⡟⠀ \n"
+                + "⠀⠀⠀⠀⠀⠈⠻⣷⣄⡀⠹⣷⣄⠀⠀⠀⢸⣷⣤⡀⠀⠈⢻⣿⣯⣤⠀⠀⣠⡀⠀⢀⣼⣿⣿⣿⣿⣟⠁⠐⠿⣿⣿⣿⣿⠋⠀⢀⣾⠟⠀⠀ \n"
+                + "⠀⠀⠀⠀⠀⠀⠀⠈⠙⠿⣷⣮⣽⣷⣶⣤⣤⣿⣿⣿⣷⣶⣦⣭⣿⣿⣧⣠⠵⢯⡆⠚⣯⢿⠋⠛⠛⢫⣀⣠⣾⣿⢿⣿⣥⣤⠶⠛⠁⠀⠀⠀ \n"
+                + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⢁⣾⣿⣿⣿⠿⠿⠿⠿⠻⢿⣿⣿⣷⣦⣤⣤⣀⣤⣤⣄⣶⣿⣿⡿⠟⠉⠀⠀⢻⡄⠀⠀⠀⠀⠀⠀⠀ \n"
+                + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⡇⠀⠀⣀⡀⠈⢿⣧⠀⠉⠙⠛⠛⠛⠛⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⢿⡄⠀⠀⠀⠀⠀⠀ \n"
+                + "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣴⣿⣿⡏⠛⠉⢻⣿⣿⣿⣾⣿⣿⠀⢸⣿⠀⠀⠀⠀⠀⠙⠓⠢⠀⠀⠀⠀⠀⠸⠀⠀⠀⠘⣿⡄⠀⠀⠀⠀⠀ \n";
 
-        String msg = "----------------------------------------------------------------- \n"
-                + "Hello! I'm Paimon! \n"
-                + "What can I do for you? \n"
-                + "-----------------------------------------------------------------";
+        String msg = separator
+                + "\nHello! I'm Paimon!"
+                + "\nWhat can I do for you?"
+                + separator;
 
         System.out.println(logo + "\n" + msg);
 
         Scanner sc = new Scanner(System.in);
+
+        ArrayList<Task> ls = new ArrayList<>();
 
         boolean running = true;
 
@@ -64,109 +68,42 @@ public class Duke {
         String deadlineCommand = "deadline";
         String eventCommand = "event";
 
-        String command;
-
-        ArrayList<Task> ls = new ArrayList<>();
+        String input = sc.nextLine();
 
         while (running) {
-            command = sc.nextLine();
             try {
-                if (command.startsWith(markCommand)) {
-                    String indexStr = command.substring(markCommand.length() + 1);
-                    int num = Integer.valueOf(indexStr);
-                    Task t = ls.get(num - 1);
-                    t.markDone();
-                    System.out.println("Nice! I've marked this task as done: \n"
-                            + t.toString()
-                            + "\n-----------------------------------------------------------------");
-                } else if (command.startsWith(unmarkCommand)) {
-                    String indexStr = command.substring(unmarkCommand.length() + 1);
-                    int num = Integer.valueOf(indexStr);
-                    Task t = ls.get(num - 1);
-                    t.markUndone();
-                    System.out.println("OK, I've marked this task as not done yet: \n"
-                            + t.toString()
-                            + "\n-----------------------------------------------------------------");
-                } else if (command.equalsIgnoreCase(byeCommand)) {
+                String command = input.split(" ")[0];
+                Command c = Command.valueOf(command.toUpperCase());
+                switch (c) {
+                case MARK:
+                    Duke.markTask(input, ls);
+                    break;
+                case UNMARK:
+                    Duke.unmarkTask(input, ls);
+                    break;
+                case LIST:
+                    Duke.listItems(ls);
+                    break;
+                case BYE:
                     System.out.println("Bye Bye! See you soon :D"
-                            + "\n-----------------------------------------------------------------");
+                            + separator);
                     running = false;
-                } else if (command.equalsIgnoreCase(listCommand)) {
-                    int i = 1;
-                    System.out.println("Here are the tasks in your list: ");
-                    for (Task t : ls) {
-                        System.out.println(i + ". " + t.toString());
-                        i++;
-                    }
-                    System.out.println("-----------------------------------------------------------------");
-                } else if (command.startsWith(todoCommand)) {
-                    if (command.length() <= todoCommand.length() + 1) {
-                        throw new EmptyInputException("todo");
-                    }
-                    String des = command.substring(todoCommand.length() + 1);
-                    Todo t = new Todo(des);
-                    ls.add(t);
-                    System.out.println(
-                            "Got it. Task successfully added: \n"
-                                    + t.toString()
-                                    + "\nNow you have " + ls.size() + " tasks in the list"
-                                    + "\n-----------------------------------------------------------------");
-                } else if (command.startsWith(deadlineCommand)) {
-                    if (command.length() <= deadlineCommand.length() + 1) {
-                        throw new EmptyInputException("deadline");
-                    } else if (!command.contains("/by")) {
-                        throw new InvalidFormatException("deadline", "/by");
-                    }
-                    int byIndex = command.indexOf("/by") ;
-                    String des = command.substring(deadlineCommand.length() + 1, byIndex - 1);
-                    if (command.length() <= byIndex + 4) {
-                        throw new EmptyDateTimeException("deadline");
-                    }
-                    String by = command.substring(byIndex + 4);
-                    Deadline d = new Deadline(des, by);
-                    ls.add(d);
-                    System.out.println(
-                            "Got it. Task successfully added: \n"
-                                    + d.toString()
-                                    + "\nNow you have " + ls.size() + " tasks in the list"
-                                    + "\n-----------------------------------------------------------------");
-                } else if (command.startsWith(eventCommand)) {
-                    if (command.length() <= eventCommand.length() + 1) {
-                        throw new EmptyInputException("event");
-                    } else if (!command.contains("/from")) {
-                        throw new InvalidFormatException("event", "/from");
-                    } else if (!command.contains("/to")) {
-                        throw new InvalidFormatException("event", "/to");
-                    }
-                    int fromIndex = command.indexOf("/from");
-                    int toIndex = command.indexOf("/to");
-                    if (command.length() <= fromIndex + 6) {
-                        throw new EmptyDateTimeException("event");
-                    } else if (command.length() <= toIndex + 4) {
-                        throw new EmptyDateTimeException("event");
-                    }
-                    String des = command.substring(eventCommand.length() + 1, fromIndex - 1);
-                    String start = command.substring(fromIndex + 6, toIndex - 1);
-                    String end = command.substring(toIndex + 4);
-                    Event e = new Event(des, start, end);
-                    ls.add(e);
-                    System.out.println(
-                            "Got it. Task successfully added: \n"
-                                    + e.toString()
-                                    + "\nNow you have " + ls.size() + " tasks in the list"
-                                    + "\n-----------------------------------------------------------------");
-                } else if (command.startsWith(deleteCommand)) {
-                    String indexStr = command.substring(deleteCommand.length() + 1);
-                    int num = Integer.valueOf(indexStr);
-                    Task task = ls.get(num - 1);
-                    ls.remove(num - 1);
-                    System.out.println("Noted. I've removed this task: \n"
-                            + task.toString()
-                            + "\nNow you have " + ls.size() + " tasks in the list");
-                } else {
+                case TODO:
+                    Duke.todo(input, ls);
+                    break;
+                case DEADLINE:
+                    Duke.deadline(input, ls);
+                    break;
+                case EVENT:
+                    Duke.event(input, ls);
+                    break;
+                case DELETE:
+                    Duke.delete(input, ls);
+                    break;
+                default:
                     throw new InvalidCommandException(
                             "☹ OOPS!!! I'm sorry, but I don't know what that means :-("
-                                    + "\n-----------------------------------------------------------------");
+                                    + separator);
                 }
             } catch (EmptyInputException e) {
                 System.out.println(e.getMessage());
@@ -177,10 +114,115 @@ public class Duke {
             } catch (InvalidCommandException e) {
                 System.out.println(e.getMessage());
             } catch (Exception e) {
-                System.out.println("☹ OOPS!!! Something went wrong D:");
+                System.out.println("☹ OOPS!!! Something went wrong D:"
+                        + separator);
             }
         }
         sc.close();
+    }
+
+    private static void markTask(String input, ArrayList<Task> ls) {
+        String indexStr = input.split(" ")[1];
+        int num = Integer.valueOf(indexStr);
+        Task t = ls.get(num - 1 );
+        t.markDone();
+        System.out.println("Nice! I've marked this task as done: \n"
+                + t
+                + separator);
+    }
+
+    private static void unmarkTask(String input, ArrayList<Task> ls) {
+        String indexStr = input.split(" ")[1];
+        int num = Integer.valueOf(indexStr);
+        Task t = ls.get(num - 1 );
+        t.markUndone();
+        System.out.println("Nice! I've marked this task as done: \n"
+                + t
+                + separator);
+    }
+
+    private static void listItems(ArrayList<Task> ls) {
+        int i = 1;
+        System.out.println("Here are the tasks in your list: ");
+        for (Task t : ls) {
+            System.out.println(i + ". " + t.toString());
+            i++;
+        }
+        System.out.println("-----------------------------------------------------------------");
+    }
+
+    private static void todo(String input, ArrayList<Task> ls) throws EmptyInputException {
+        if (input.split(" ").length > 1) {
+            String des = input.split(" ")[1];
+            Todo t = new Todo(des);
+            ls.add(t);
+            System.out.println(
+                    "Got it. Task successfully added: \n"
+                            + t.toString()
+                            + "\nNow you have " + ls.size() + " tasks in the list"
+                            + separator);
+        } else {
+            throw new EmptyInputException("todo");
+        }
+    }
+
+    private static void deadline(String input, ArrayList<Task> ls)
+            throws EmptyInputException, InvalidFormatException, EmptyDateTimeException {
+        if (input.split(" ").length < 2) {
+            throw new EmptyInputException("deadline");
+        } else if (!input.contains("/by")) {
+            throw new InvalidFormatException("deadline", "/by");
+        } else if (input.length() <= input.indexOf("/by") + 4) {
+            throw new EmptyDateTimeException("deadline");
+        } else {
+            String tempDes = input.split(" ", 2)[1];
+            String des = tempDes.split(" /by " )[0];
+            String by = tempDes.split(" /by ")[1];
+            Deadline d = new Deadline(des, by);
+            ls.add(d);
+            System.out.println(
+                    "Got it. Task successfully added: \n"
+                            + d
+                            + "\nNow you have " + ls.size() + " tasks in the list"
+                            + separator);
+        }
+    }
+
+    private static void event(String input, ArrayList<Task> ls)
+            throws EmptyInputException, InvalidFormatException, EmptyDateTimeException {
+        if (input.split(" ").length < 2) {
+            throw new EmptyInputException("event");
+        } else if (!input.contains("/from")) {
+            throw new InvalidFormatException("event", "/from");
+        } else if (!input.contains("/to")) {
+            throw new InvalidFormatException("event", "/to");
+        } else if (input.length() <= input.indexOf("/from") + 6) {
+            throw new EmptyDateTimeException("event");
+        } else if (input.length() <= input.indexOf("/to") + 4) {
+            throw new EmptyDateTimeException("event");
+        } else {
+            String tempDes = input.split(" ", 2)[1];
+            String des = tempDes.split(" /from ")[0];
+            String start = tempDes.split(" /from ")[0].split(" /to ")[0];
+            String end = tempDes.split(" /to ")[1];
+            Event e = new Event(des, start, end);
+            ls.add(e);
+            System.out.println(
+                    "Got it. Task successfully added: \n"
+                            + e
+                            + "\nNow you have " + ls.size() + " tasks in the list"
+                            + separator);
+        }
+    }
+
+    private static void delete(String input, ArrayList<Task> ls) {
+        String indexStr = input.split(" ")[1];
+        int num = Integer.valueOf(indexStr);
+        Task task = ls.get(num - 1);
+        ls.remove(num - 1);
+        System.out.println("Noted. I've removed this task: \n"
+                + task.toString()
+                + "\nNow you have " + ls.size() + " tasks in the list");
     }
 }
 
