6f3880db675e1facaa7fbac5f591285c4ad8255a
aliciamichellew <aliciamichellew@gmail.com>
2023-09-01 14:36:27 +0800

    Add JavaDoc for ByeStrategy

--- src/main/java/kevin/evaluator/ByeStrategy.java
+++ src/main/java/kevin/evaluator/ByeStrategy.java
@@ -3,7 +3,15 @@ package kevin.evaluator;
 import kevin.taskList.TaskList;
 import java.util.ArrayList;
 
+/**
+ * A class responsible for the logic for BYE command.
+ */
 public class ByeStrategy extends BaseStrategy {
+    /**
+     * Constructor to initialize ByeStrategy.
+     * @param taskList This is the TaskList where the tasks are stored and operations are defined.
+     * @param arguments This is an ArrayList where all the needed arguments are stored.
+     */
     public ByeStrategy(TaskList taskList, ArrayList<String> arguments) {
         super(taskList, arguments);
     }
