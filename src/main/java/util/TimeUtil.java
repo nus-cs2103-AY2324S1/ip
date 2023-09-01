28fe2e26d5fbbd7800189915ab3c7da0d7cceb7d
euchangxian <changxian2001@gmail.com>
2023-09-02 00:12:31 +0800

    Fix TimeUtil::parseDateTimeString
    Fix parseDateTimeString() wrongly parsing yyyy-mm-dd, or inputs
    containing Date only, as invalid inputs.

--- src/main/java/util/TimeUtil.java
+++ src/main/java/util/TimeUtil.java
@@ -3,21 +3,25 @@ package util;
 
 import exception.TimeUtilException;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Locale;
 
 public class TimeUtil {
-    private static final DateTimeFormatter[] FORMATTERS = {
+    private static final DateTimeFormatter[] DATE_TIME_FORMATTERS = {
             DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"), // 2023-05-28 1800
-            DateTimeFormatter.ofPattern("yyyyMMdd HHmm"), // 20230528 1800
+            DateTimeFormatter.ofPattern("yyyyMMdd HHmm")  // 20230528 1800
+    };
+
+    private static final DateTimeFormatter[] DATE_ONLY_FORMATTERS = {
             DateTimeFormatter.ISO_LOCAL_DATE, // yyyy-mm-dd
             DateTimeFormatter.BASIC_ISO_DATE, // yyyymmdd
             DateTimeFormatter.ofPattern("d MMM yyyy", Locale.ENGLISH), // 1 Jan 2023
             DateTimeFormatter.ofPattern("d MMMM yyyy", Locale.ENGLISH), // 1 January 2023
             DateTimeFormatter.ofPattern("yyyy-MM"), // 2023-05
-            DateTimeFormatter.ofPattern("yyyyMM"), // 202305
+            DateTimeFormatter.ofPattern("yyyyMM")  // 202305
     };
 
     private static final DateTimeFormatter DISPLAY_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH");
@@ -29,9 +33,20 @@ public class TimeUtil {
         if (dateTime != null) {
             return dateTime;
         }
-        for (DateTimeFormatter formatter : FORMATTERS) {
+        // Attempt to parse with date-time formatters first
+        for (DateTimeFormatter formatter : DATE_TIME_FORMATTERS) {
             try {
                 return LocalDateTime.parse(input, formatter);
+            } catch (DateTimeParseException ignored) {
+                // Try next formatter.
+            }
+        }
+
+        // If not parsed yet, try date-only formatters and combine with current time.
+        for (DateTimeFormatter formatter : DATE_ONLY_FORMATTERS) {
+            try {
+                LocalDate parsedDate = LocalDate.parse(input, formatter);
+                return LocalDateTime.of(parsedDate, LocalDateTime.now().toLocalTime());
             } catch (DateTimeParseException ignored) {
                 // Try next formatter.
             }
