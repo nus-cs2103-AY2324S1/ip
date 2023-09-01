6c509799e06d509262478509d3a8f5980387bb2f
Aprup Kale <aprup.kale@gmail.com>
2023-09-01 17:45:59 +0800

    Fix coding standard errors

--- src/main/java/duke/exceptions/DukeParseException.java
+++ src/main/java/duke/exceptions/DukeParseException.java
@@ -1,6 +1,6 @@
 package duke.exceptions;
 
-public class DukeParseException extends DukeException{
+public class DukeParseException extends DukeException {
     public DukeParseException(String task) {
         super("OOPS!!! Missing parameters in " + task);
     }
