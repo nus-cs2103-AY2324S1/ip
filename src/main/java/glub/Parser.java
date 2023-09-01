9a48ae2c3e045d9dbfd6a4acf7be1fdd785c90b0
dylkaw <37286651+dylkaw@users.noreply.github.com>
2023-09-01 11:57:11 +0800

    Adjust case indentation to coding standard

--- src/main/java/glub/Parser.java
+++ src/main/java/glub/Parser.java
@@ -20,42 +20,42 @@ public class Parser {
             try {
                 String command = inputScanner.next();
                 switch (command) {
-                    case "bye":
-                        isListening = false;
-                        Ui.sayGoodbye();
-                        break;
-                    case "list":
-                        Ui.printListMsg(this.taskList);
-                        break;
-                    case "mark":
-                        taskList.mark(inputScanner.nextInt());
-                        break;
-                    case "unmark":
-                        taskList.unmark(inputScanner.nextInt());
-                        break;
-                    case "delete":
-                        taskList.deleteTask(inputScanner.nextInt());
-                        break;
-                    case "todo":
-                        String todo = inputScanner.nextLine();
-                        taskList.addTask(todo, TaskType.TODO, false);
-                        Ui.printAddMsg(taskList.getTaskList());
-                        storage.saveTasks(taskList.getTaskList());
-                        break;
-                    case "deadline":
-                        String deadline = inputScanner.nextLine();
-                        taskList.addTask(deadline, TaskType.DEADLINE, false);
-                        Ui.printAddMsg(taskList.getTaskList());
-                        storage.saveTasks(taskList.getTaskList());
-                        break;
-                    case "event":
-                        String event = inputScanner.nextLine();
-                        taskList.addTask(event, TaskType.EVENT, false);
-                        Ui.printAddMsg(taskList.getTaskList());
-                        storage.saveTasks(taskList.getTaskList());
-                        break;
-                    default:
-                        throw new GlubException("OOPS!! I'm sorry, but I don't know what that means :-(\n");
+                case "bye":
+                    isListening = false;
+                    Ui.sayGoodbye();
+                    break;
+                case "list":
+                    Ui.printListMsg(this.taskList);
+                    break;
+                case "mark":
+                    taskList.mark(inputScanner.nextInt());
+                    break;
+                case "unmark":
+                    taskList.unmark(inputScanner.nextInt());
+                    break;
+                case "delete":
+                    taskList.deleteTask(inputScanner.nextInt());
+                    break;
+                case "todo":
+                    String todo = inputScanner.nextLine();
+                    taskList.addTask(todo, TaskType.TODO, false);
+                    Ui.printAddMsg(taskList.getTaskList());
+                    storage.saveTasks(taskList.getTaskList());
+                    break;
+                case "deadline":
+                    String deadline = inputScanner.nextLine();
+                    taskList.addTask(deadline, TaskType.DEADLINE, false);
+                    Ui.printAddMsg(taskList.getTaskList());
+                    storage.saveTasks(taskList.getTaskList());
+                    break;
+                case "event":
+                    String event = inputScanner.nextLine();
+                    taskList.addTask(event, TaskType.EVENT, false);
+                    Ui.printAddMsg(taskList.getTaskList());
+                    storage.saveTasks(taskList.getTaskList());
+                    break;
+                default:
+                    throw new GlubException("OOPS!! I'm sorry, but I don't know what that means :-(\n");
                 }
             } catch (GlubException ex) {
                 Ui.printError(ex.getMessage());
