cfe193d9b44706bbff6e204935df2aefac13761b
marcu <marcussoh38@gmail.com>
2023-09-01 23:36:24 +0800

    Incr: coding style

--- src/main/java/Duke/UI/Ui.java
+++ src/main/java/Duke/UI/Ui.java
@@ -62,6 +62,7 @@ public class Ui {
 
         }
     }
+
     /**
      * Prints feedback to the user on what and how a Task got modified,
      * based on the user's command.
@@ -70,48 +71,48 @@ public class Ui {
      */
     public static void printResult(Commands command, Optional<Task> task, TaskList taskList) {
         switch (command) {
-            case TODO:
-            case DEADLINE:
-            case EVENT: {
-                task.ifPresent(t -> {
-                    System.out.println("\uD83D\uDE0A I've added a new task: " + t.toString());
-                    System.out.println("Now you have " + taskList.getSize() + " tasks!");
-                });
-                break;
-            }
-            case MARK: {
-                task.ifPresent(t -> System.out.println("Nice! I've marked this task as done: \n    " + t.toString()));
+        case TODO:
+        case DEADLINE:
+        case EVENT: {
+            task.ifPresent(t -> {
+                System.out.println("\uD83D\uDE0A I've added a new task: " + t.toString());
+                System.out.println("Now you have " + taskList.getSize() + " tasks!");
+            });
+            break;
+        }
+        case MARK: {
+            task.ifPresent(t -> System.out.println("Nice! I've marked this task as done: \n    " + t.toString()));
 
-                break;
-            }
-            case UNMARK: {
-                task.ifPresent(t -> System.out.println("Nice! I've marked this task as undone: \n    " + t.toString()));
+            break;
+        }
+        case UNMARK: {
+            task.ifPresent(t -> System.out.println("Nice! I've marked this task as undone: \n    " + t.toString()));
 
-                break;
+            break;
+        }
+        case DELETE: {
+            task.ifPresent(t -> System.out.println("\uD83D\uDE0A I've removed this task: " + t.toString()));
+            break;
+        }
+        case LIST: {
+            System.out.println(taskList);
+            break;
+        }
+        case FIND: {
+            if (taskList.getSize() == 0) {
+                System.out.println("Couldn't find any matching tasks!");
+            } else {
+                System.out.println("I found " + taskList.getSize() + " matching tasks:");
+                System.out.println(taskList.toString());
             }
-            case DELETE: {
-                task.ifPresent(t -> System.out.println("\uD83D\uDE0A I've removed this task: " + t.toString()));
-                break;
-            }
-            case LIST: {
-                System.out.println(taskList);
-                break;
-            }
-            case FIND: {
-                if (taskList.getSize() == 0) {
-                    System.out.println("Couldn't find any matching tasks!");
-                } else {
-                    System.out.println("I found " + taskList.getSize() + " matching tasks:");
-                    System.out.println(taskList.toString());
-                }
-                break;
-            }
-            case BYE: {
-                String exitMsg = "Bye! Hope to see you again soon.";
-                System.out.println(exitMsg);
+            break;
+        }
+        case BYE: {
+            String exitMsg = "Bye! Hope to see you again soon.";
+            System.out.println(exitMsg);
 
-                break;
-            }
+            break;
+        }
         }
     }
 
