324b2fcbb33e6d442f99909fae42f511afa79f73
Yu Han Zhang <yuhan@zhyuhan.me>
2023-09-01 23:04:34 +0800

    docs: document classes and methods for tasks

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -6,27 +6,56 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a list of tasks.
+ */
 public class TaskList implements Serializable {
     private static final long serialVersionUID = 5L;
 
     private final List<Task> tasks = new ArrayList<>();
 
+    /**
+     * Returns whether the task list is empty.
+     *
+     * @return true if the task list is empty
+     */
     public boolean isEmpty() {
         return tasks.isEmpty();
     }
 
+    /**
+     * Returns the size of the task list.
+     *
+     * @return the number of tasks in the task list
+     */
     public int size() {
         return tasks.size();
     }
 
+    /**
+     * Returns the task at the specified position.
+     *
+     * @param i the index of the task to return
+     * @return the task at the specified position
+     */
     public Task get(int i) {
         return tasks.get(i);
     }
 
+    /**
+     * Adds a task to the end of the task list.
+     *
+     * @param t the task to be added to the list
+     */
     public void add(Task t) {
         tasks.add(t);
     }
 
+    /**
+     * Removes the task at the specified position.
+     *
+     * @param i the index of the task to be removed
+     */
     public void remove(int i) {
         tasks.remove(i);
     }
--- src/main/java/duke/tasks/Task.java
+++ src/main/java/duke/tasks/Task.java
@@ -2,6 +2,9 @@ package duke.tasks;
 
 import java.io.Serializable;
 
+/**
+ * Represents a task with a description of what is to be done and its status.
+ */
 public class Task implements Serializable {
     private static final long serialVersionUID = 1L;
     protected String description;
@@ -12,6 +15,11 @@ public class Task implements Serializable {
         this.isDone = false;
     }
 
+    /**
+     * Returns whether the task has been done.
+     *
+     * @return true if the task has been done
+     */
     public boolean isDone() {
         return isDone;
     }
@@ -20,10 +28,16 @@ public class Task implements Serializable {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void markAsDone() {
         isDone = true;
     }
 
+    /**
+     * Marks the task as undone.
+     */
     public void markAsUndone() {
         isDone = false;
     }
