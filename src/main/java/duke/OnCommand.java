204d52bd2dee8ac295d3c6342d7a72ced419aec1
Derek Tan <derek.tan.jing.xu@gmail.com>
2023-09-01 16:48:47 +0800

    Add OnCommand class

---
+++ src/main/java/duke/OnCommand.java
@@ -0,0 +1,19 @@
+package duke;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class OnCommand extends Command{
+    public static final String COMMAND_ON = "on";
+
+    public OnCommand(ArrayList<String> params) {
+        super(params);
+    }
+
+    @Override
+    public void execute(TaskList tasks, UI ui, Storage storage) throws DukeException {
+        LocalDate date = LocalDate.parse(params.get(1));
+        ArrayList<Task> tasksOnDate = tasks.getTasksOn(date);
+        ui.printTasksOn(tasksOnDate);
+    }
+}
