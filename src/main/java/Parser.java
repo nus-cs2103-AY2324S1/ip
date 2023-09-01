d2fa711ddebaedd0c2cecdc39814f958d9eb316d
Chee Teng <>
2023-09-01 16:42:03 +0800

    Add Parser class

---
+++ src/main/java/Parser.java
@@ -0,0 +1,60 @@
+import java.time.LocalDate;
+
+public class Parser {
+
+    public static Command parse(String command) {
+        if (command.equals("list")) {
+            return new ListCommand();
+        } else if (command.startsWith("mark")) {
+            int index = command.length() - 1;
+            char c = command.charAt(index);
+            int number = c - 48 - 1;
+            return new MarkCommand(number);
+        } else if (command.startsWith("unmark")) {
+            int index = command.length() - 1;
+            char c = command.charAt(index);
+            int number = c - 48 - 1;
+            return new UnmarkCommand(number);
+        } else if (command.startsWith("todo")) {
+            try {
+                if (command.substring(4).isEmpty()) {
+                    throw new CheeException("OOPS!!! The description of a todo cannot be empty.");
+                }
+                String desc = command.substring(5);
+                Task instance = new Todo(desc);
+                return new AddCommand(instance);
+            } catch (CheeException e) {
+                System.out.println(e.getMessage());
+            }
+        } else if (command.startsWith("deadline")) {
+            int index = command.indexOf(47);
+            String description = command.substring(9, index - 1);
+            String time = command.substring(index + 4);
+            LocalDate day = LocalDate.parse(time);
+            Task instance = new Deadline(description, day);
+            return new AddCommand(instance);
+        } else if (command.startsWith("event")) {
+            int index1 = command.indexOf(47);
+            String description = command.substring(6, index1);
+            String duration = command.substring(index1 + 6);
+            int index2 = duration.indexOf(47);
+            String from = duration.substring(0, index2 - 1);
+            String to = duration.substring(index2 + 4);
+            LocalDate d1 = LocalDate.parse(from);
+            LocalDate d2 = LocalDate.parse(to);
+            Task instance = new Event(description, d1, d2);
+            return new AddCommand(instance);
+        } else if (command.startsWith("delete")) {
+            int index = command.length() - 1;
+            char c = command.charAt(index);
+            int number = c - 48 - 1;
+            return new DeleteCommand(number);
+        } else if (command.equals("bye")) {
+            return new ExitCommand();
+        }
+        return new InvalidCommand();
+    }
+
+
+}
+
