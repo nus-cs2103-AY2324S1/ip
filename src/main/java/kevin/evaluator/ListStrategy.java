e2094444f89fee67ce3e6fdaa3382b6ba1a60a9d
aliciamichellew <aliciamichellew@gmail.com>
2023-09-01 14:46:27 +0800

    Add JavaDoc for ListStrategy

--- src/main/java/kevin/evaluator/ListStrategy.java
+++ src/main/java/kevin/evaluator/ListStrategy.java
@@ -7,11 +7,27 @@ import kevin.exception.KevinException;
 
 import java.util.ArrayList;
 
+/**
+ * A class responsible for the logic for LIST command.
+ */
 public class ListStrategy extends BaseStrategy {
+    /**
+     * Constructor to initialize ListStrategy.
+     * @param taskList This is the TaskList where the tasks are stored and operations are defined.
+     * @param arguments This is an ArrayList where all the needed arguments are stored.
+     */
     public ListStrategy(TaskList taskList, ArrayList<String> arguments) {
         super(taskList, arguments);
     }
 
+    /**
+     * Handles the logic of the LIST command.
+     * @param logger This is the Logger that handles System.out.println.
+     * @param fileStorage This is the FileStorage that handles the storage in the local computer.
+     * @param isInFile This is the boolean to show whether the task is in the local computer's file.
+     * @return Returns a boolean that determines the continuation of the evaluation.
+     * @throws KevinException On the detection of errors.
+     */
     @Override
     public boolean evaluate(Logger logger, FileStorage fileStorage, boolean isInFile) throws KevinException {
         logger.log(taskList.list());
