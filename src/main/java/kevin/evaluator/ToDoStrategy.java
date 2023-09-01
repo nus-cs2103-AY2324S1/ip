b2807ead46837346183bff7ea24f56df0e7675d5
aliciamichellew <aliciamichellew@gmail.com>
2023-09-01 14:48:14 +0800

    Add JavaDoc for ToDoStrategy

--- src/main/java/kevin/evaluator/ToDoStrategy.java
+++ src/main/java/kevin/evaluator/ToDoStrategy.java
@@ -8,11 +8,27 @@ import kevin.exception.KevinException;
 
 import java.util.ArrayList;
 
+/**
+ * A class responsible for the logic for TODO command.
+ */
 public class ToDoStrategy extends BaseStrategy {
+    /**
+     * Constructor to initialize ToDoStrategy.
+     * @param taskList This is the TaskList where the tasks are stored and operations are defined.
+     * @param arguments This is an ArrayList where all the needed arguments are stored.
+     */
     public ToDoStrategy(TaskList taskList, ArrayList<String> arguments) {
         super(taskList, arguments);
     }
 
+    /**
+     * Handles the logic of the TODO command.
+     * @param logger This is the Logger that handles System.out.println.
+     * @param fileStorage This is the FileStorage that handles the storage in the local computer.
+     * @param isInFile This is the boolean to show whether the task is in the local computer's file.
+     * @return Returns a boolean that determines the continuation of the evaluation.
+     * @throws KevinException On the detection of errors.
+     */
     @Override
     public boolean evaluate(Logger logger, FileStorage fileStorage, boolean isInFile) throws KevinException {
         Boolean isDone = Boolean.getBoolean(this.arguments.get(0));
