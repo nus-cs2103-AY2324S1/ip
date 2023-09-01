82c616ad5001c9927f0b2c503e542373893256b8
Debbie Hii Wenxin <e0959749@u.nus.edu>
2023-09-01 15:14:31 +0800

    lelve 8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,16 +1,20 @@
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @JsonTypeName("Deadline")
 public class Deadline extends Task {
-    public String dateTime;
-    public Deadline(@JsonProperty("task") String task, @JsonProperty("dateTime") String dateTime) {
+    public LocalDate dateTime;
+    public Deadline(@JsonProperty("task") String task, @JsonProperty("dateTime") LocalDate dateTime) {
         super(task);
         this.dateTime=dateTime;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.dateTime + ")";
+        return "[D]" + super.toString() + " (by: " + this.dateTime.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
     }
 }
