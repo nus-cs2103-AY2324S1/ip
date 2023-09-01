5261c707c14032cc4127c39e65d677e40d7cdd84
antonTan96 <antonhz2021@gmail.com>
2023-09-01 19:18:11 +0800

    Add test cases

---
+++ src/test/java/CrackerPackage/Test1.java
@@ -0,0 +1,12 @@
+package CrackerPackage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test1 {
+    @Test
+    public void test1() {
+        assertEquals(1, 1);
+    }
+}
