9d93a8726ab7f8a97c3d8ccad03869b369efbd17
aliciamichellew <aliciamichellew@gmail.com>
2023-09-01 15:01:45 +0800

    Add JavaDoc for QueryObject

--- src/main/java/kevin/parser/QueryObject.java
+++ src/main/java/kevin/parser/QueryObject.java
@@ -2,23 +2,41 @@ package kevin.parser;
 
 import java.util.ArrayList;
 
+/**
+ * A class that contains the Command and arguments to be passed to the Evaluator.
+ */
 public class QueryObject {
     private final Commands commandType;
     private final ArrayList<String> args;
 
+    /**
+     * Constructor to initialize QueryObject
+     * @param commandType This is the Command for the query object.
+     * @param args This is the arguments needed to run the command.
+     */
     public QueryObject(Commands commandType, ArrayList<String> args) {
         this.commandType = commandType;
         this.args = args;
     }
 
+    /**
+     * Gets the command of the query object.
+     */
     public Commands getCommandType() {
         return commandType;
     }
 
+    /**
+     * Gets the arguments of the query object.
+     */
     public ArrayList<String> getArgs() {
         return args;
     }
 
+    /**
+     * {@inheritDoc}
+     * @return Returns a string representation of the object.
+     */
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder(this.commandType.name());
