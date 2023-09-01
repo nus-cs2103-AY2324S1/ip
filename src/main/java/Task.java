81243693b3dfd56d4dccf5f3f66bac40351526f0
Chee Teng <>
2023-09-01 11:54:20 +0800

    Add readFile method to load past data

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -32,6 +32,36 @@ public class Task {
         return s;
     }
 
+    public static Task readFile(String nextLine) {
+        String next = nextLine.substring(3);
+        Task t;
+        if(nextLine.startsWith("[T]")) {
+            String desc = next.substring(4);
+            t = new Todo(desc);
+        } else if (nextLine.startsWith("[D]")) {
+            int endDesc = next.indexOf("(by: ");
+            String desc = next.substring(4, endDesc);
+            int len = next.length();
+            String time = next.substring(5, len - 1);
+            t = new Deadline(desc, time);
+        } else {
+            int endDesc = next.indexOf("(from: ");
+            String desc = next.substring(4, endDesc);
+            int endFrom = next.indexOf("to: ");
+            String from = next.substring(endDesc - 6, endFrom);
+            String to = next.substring(endFrom - 3, next.length() - 2);
+            t = new Event(desc, from, to);
+        }
+
+        if (next.startsWith("[X]")) {
+            t.mark();
+        } else {
+            t.unmark();
+        }
+
+        return t;
+    }
+
     public String toString(){
         return "[" + getStatusIcon() + "]" + this.description;
     }
