a283688cdf3e67075305d959da8f061d09105670
zekone <hkw19991@gmail.com>
2023-09-01 17:09:33 +0800

    Add JUnit testing

---
+++ src/test/java/task/TaskListTest.java
@@ -0,0 +1,45 @@
+package task;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TaskListTest {
+
+    @Test
+    public void getTask_containsTask_success() {
+        TaskList list = new TaskList();
+        Task t1 = new Todo("test1");
+        Task t2 = new Todo("test2");
+        list.addTask(t1);
+        list.addTask(t2);
+        assertEquals(t1, list.getTask(0));
+        assertEquals(t2, list.getTask(1));
+        assertNotEquals(t2, list.getTask(0));
+    }
+
+    @Test
+    public void getTask_returnSameObject_success() {
+        TaskList list = new TaskList();
+        Task t1 = new Todo("test1");
+        Task t2 = new Todo("test2");
+        list.addTask(t1);
+        assertEquals(list.getTask(0), t1);
+        assertNotEquals(list.getTask(0), t2);
+    }
+
+    @Test
+    public void getTask_indexOOB_exception() {
+        TaskList list = new TaskList();
+        Task t1 = new Todo("test1");
+        list.addTask(t1);
+        try {
+            list.getTask(123);
+            fail();
+        } catch (Exception e) {
+            assertEquals("Index 123 out of bounds for length 1", e.getMessage());
+        }
+    }
+}
---
+++ src/test/java/utility/DateTimeParserTest.java
@@ -0,0 +1,36 @@
+package utility;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.time.LocalDateTime;
+
+public class DateTimeParserTest {
+
+    @Test
+    public void parse_corectFormat_success() {
+        assertEquals(LocalDateTime.of(2023, 9, 1, 10, 30), DateTimeParser.parse("1-9-2023 1030"));
+        assertEquals(LocalDateTime.of(2023, 9, 1, 10, 30), DateTimeParser.parse("1/9/2023 10:30"));
+        assertEquals(LocalDateTime.of(2023, 9, 1, 10, 30), DateTimeParser.parse("01/09/2023 10:30"));
+    }
+
+    @Test
+    public void parse_noTimeGiven_success() {
+        assertEquals(LocalDateTime.of(2023, 9, 1, 00, 00), DateTimeParser.parse("1-9-2023"));
+        assertEquals(LocalDateTime.of(2023, 9, 1, 00, 00), DateTimeParser.parse("1/9/2023"));
+    }
+
+    @Test
+    public void parse_wrongFormat_null() {
+        assertNull(DateTimeParser.parse("1/9-2023"));
+        assertNull(DateTimeParser.parse("1.9.2023"));
+        assertNull(DateTimeParser.parse("1/9/2023 2550"));
+        assertNull(DateTimeParser.parse("1/9/2023 2:10"));
+        assertNull(DateTimeParser.parse("1/19/2023 14:10"));
+        assertNull(DateTimeParser.parse("35/9/2023 14:10"));
+    }
+
+}
