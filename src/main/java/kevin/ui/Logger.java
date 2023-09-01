58fb2bb0883528e93ff90e86cd274f5ae141389e
aliciamichellew <aliciamichellew@gmail.com>
2023-09-01 15:51:30 +0800

    Add JavaDoc for Logger

--- src/main/java/kevin/ui/Logger.java
+++ src/main/java/kevin/ui/Logger.java
@@ -1,6 +1,13 @@
 package kevin.ui;
 
+/**
+ * A class responsible to output to the console.
+ */
 public class Logger {
+    /**
+     * Prints the text wrapped in horizontal lines.
+     * @param str
+     */
     public void log(String str) {
         System.out.println("\t" + HORIZONTAL_LINE);
         System.out.println("\t" + str);
@@ -17,11 +24,17 @@ public class Logger {
             "\t" +"| |\\  \\| |___\\ \\_/ /_| |_| |\\  |\n" +
             "\t" +"\\_| \\_/\\____/ \\___/ \\___/\\_| \\_/\n";
 
+    /**
+     * Prints the hello message when the program first runs.
+     */
     public void hello() {
         String welcomeMessage = "Hello! I'm" + BOT_NAME + "\n\t" + "What can I do for you?";
         this.log(welcomeMessage);
     }
 
+    /**
+     * Prints the bye message before the program ends.
+     */
     public void bye() {
         String goodbyeMessage = "Bye. Hope to see you again soon!";
         this.log(goodbyeMessage);
