964972ab8c0dcf238955b8823e749baa7ce04bb3
lunaroddity <e0969600@u.nus.edu>
2023-09-01 23:51:37 +0800

    Add save feature

--- src/main/java/EventTask.java
+++ src/main/java/EventTask.java
@@ -1,23 +1,62 @@
 public class EventTask extends Task {
     String startDateTime;
     String endDateTime;
-    public EventTask(String details) throws IllegalArgumentException {
-        super(details.split("/from ")[0]);
-        if (description.length() == 0) {
-            throw new IllegalArgumentException("Deadline description cannot be empty.");
-        }
 
-        String[] startTimeSplit = details.split("/from ")[1].split("/to");
-        String startDateTime = startTimeSplit[0];
-        String endDateTime = startTimeSplit[1];
+    private EventTask(boolean isDone, String description, String startDateTime,
+                     String endDateTime) {
+        super(isDone, description);
         this.startDateTime = startDateTime;
         this.endDateTime = endDateTime;
         // add exceptions for time
     }
 
+    public static EventTask makeEvent(String allDetails) throws IllegalArgumentException {
+        if (allDetails.length() == 0) {
+            throw new IllegalArgumentException("Event description cannot be empty.");
+        }
+
+        String[] descriptionStartEndTime = allDetails.split("/from");
+        if (descriptionStartEndTime.length <= 1) {
+            throw new IllegalArgumentException("Event start time cannot be empty.");
+        }
+
+        String description = descriptionStartEndTime[0];
+        String[] startEndTime = descriptionStartEndTime[1].split("/to");
+        if (startEndTime.length <= 1) {
+            throw new IllegalArgumentException("Event end time cannot be empty.");
+        }
+
+        String startDateTime = startEndTime[0];
+        String endDateTime = startEndTime[1];
+        return new EventTask(false, description, startDateTime, endDateTime);
+    }
+
+    public static EventTask makeEvent(String[] taskInputs) throws IllegalArgumentException {
+        if (taskInputs.length != 5) {
+            throw new IllegalArgumentException("Wrong number of arguments. " +
+                    "Current number of arguments: " + taskInputs.length);
+        }
+
+        if (taskInputs[2].length() == 0) {
+            throw new IllegalArgumentException("Event description cannot be empty.");
+        }
+
+        boolean isDone = taskInputs[1].equals("1");
+        String description = taskInputs[2];
+        String startDateTime = taskInputs[3];
+        String endDateTime = taskInputs[4];
+        return new EventTask(isDone, description, startDateTime, endDateTime);
+    }
+
     @Override
     public String toString() {
         return "[E][" + this.getStatusIcon() + "]" + this.description
-                + "(from: " + startDateTime + "to:" + endDateTime + ")";
+                + "(from:" + startDateTime + "to:" + endDateTime + ")";
+    }
+
+    @Override
+    public String toSavedString() {
+        String done = isDone ? "1" : "0";
+        return "E|" + done + "|" + this.description + "|" + this.startDateTime + "|" + this.endDateTime +"\n";
     }
 }
--- src/main/java/TodoTask.java
+++ src/main/java/TodoTask.java
@@ -1,13 +1,37 @@
 public class TodoTask extends Task {
-    public TodoTask(String description) throws IllegalArgumentException {
-        super(description);
+
+    private TodoTask(boolean isDone, String description) {
+        super(isDone, description);
+    }
+
+    static public TodoTask makeTodo(String description) throws IllegalArgumentException {
         if (description.length() == 0) {
             throw new IllegalArgumentException("Todo description cannot be empty.");
         }
+        return new TodoTask(false, description);
+    }
+
+    static public TodoTask makeTodo(String[] taskInputs) throws IllegalArgumentException {
+        if (taskInputs.length != 3) {
+            // make new exception for this
+            throw new IllegalArgumentException("Wrong number of arguments. " +
+                    "Current number of arguments: " + taskInputs.length);
+        }
+        if (taskInputs[2].length() == 0) {
+            throw new IllegalArgumentException("Todo description cannot be empty.");
+        }
+        boolean isDone = taskInputs[0].equals("1");
+        return new TodoTask(isDone, taskInputs[2]);
     }
 
     @Override
     public String toString() {
         return "[T][" + this.getStatusIcon() + "]" + this.description;
+    }
+
+    @Override
+    public String toSavedString() {
+        String done = isDone ? "1" : "0";
+        return "T|" + done + "|" + this.description + "\n";
     }
 }
