adc63cc676475f6cca04f327f02cd1b014c15b22
Timothy Putra Prasetio <timothypp02@gmail.com>
2023-09-02 01:52:37 +0800

    Add TaskList

--- src/main/java/anya/task/TaskList.java
+++ src/main/java/anya/task/TaskList.java
@@ -1,2 +1,39 @@
-package anya.task;public class TaskList {
+package anya.task;
+
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void add(Task task) {
+        this.tasks.add(task);
+    }
+
+    public void remove(Task task) {
+        this.tasks.remove(task);
+    }
+
+    public Task get(int index) {
+        return this.tasks.get(index);
+    }
+
+    public int size() {
+        return this.tasks.size();
+    }
+
+    public String list() {
+        StringBuilder list = new StringBuilder("Here are the tasks in your list:\n");
+        for (int i = 0; i < tasks.size(); i++) {
+            list.append(String.format((i + 1) + ". " + tasks.get(i)));
+        }
+        return list.toString();
+    }
 }
