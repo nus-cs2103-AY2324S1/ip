a97b7bc2a5f53769a087ca9ccaafec425c1b8437
junnengsoo <soojunneng01@gmail.com>
2023-09-01 18:53:56 +0800

    Add JUnit tests for TaskList and Task

---
+++ src/test/java/duke/tasks/TaskTest.java
@@ -0,0 +1,35 @@
+package duke.tasks;  //same package as the class being tested
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaskTest {
+    @Test
+    public void markDone_mark(){
+        Task task = new Task("Sleep");
+        task.markDone();
+        assertTrue(task.getIsDone());
+    }
+
+    @Test
+    public void unmarkDone_unmark(){
+        Task task = new Task("Sleep");
+        task.markDone();
+        task.unmarkDone();
+        assertFalse(task.getIsDone());
+    }
+
+    @Test
+    public void toFileFormat_notDone_correctFormat(){
+        Task task = new Task("Sleep");
+        assertEquals("0 | Sleep", task.toFileFormat());
+    }
+
+    @Test
+    public void toFileFormat_done_correctFormat(){
+        Task task = new Task("Sleep");
+        task.markDone();
+        assertEquals("1 | Sleep", task.toFileFormat());
+    }
+}
