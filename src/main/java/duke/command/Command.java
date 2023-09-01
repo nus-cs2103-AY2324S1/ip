265faa34aa3483b3f2114d6f6aadbeb76c4e038d
YangHeng Tang <yanghengtang@gmail.com>
2023-09-01 12:02:24 +0800

    Update JavaDoc for Command.java

--- src/main/java/duke/command/Command.java
+++ src/main/java/duke/command/Command.java
@@ -17,11 +17,11 @@ public abstract class Command {
     /**
      * Executes the list of commands associated with the Command object.
      *
-     * @param tasks The given TaskList required for execution of some commands.
+     * @param taskList The given TaskList required for execution of some commands.
      * @param ui The given Ui required for execution of some commands.
      * @param storage The given Storage required for execution of some commands.
      */
-    public abstract void execute(TaskList tasks, Ui ui, Storage storage);
+    public abstract void execute(TaskList taskList, Ui ui, Storage storage);
 
     /**
      * Returns a boolean value to indicate if Command object is an exit command.
