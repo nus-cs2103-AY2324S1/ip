282f81e35481af7dad7021dcf16e65646348aa33
Chee Teng <>
2023-09-01 12:52:10 +0800

    Update Event class to store and deal with LocalDate

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,23 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String from;
-    protected String to;
+    protected LocalDate from;
+    protected LocalDate to;
 
-    public Event(String description, String from, String to) {
+    public Event(String description, LocalDate from, LocalDate to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
     @Override
+    public String toWrite() {
+        return "[E]" + super.toWrite() + "(from: " + this.from.toString() + " to: " + this.to.toString() + ")";
+    }
+    @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from:" + this.from + "to:" + this.to + ")";
+        return "[E]" + super.toString() + "(from: " + this.from.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " to: " + this.to.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
