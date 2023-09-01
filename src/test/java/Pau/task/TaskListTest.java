fc2b8d78b1374081a3d3549a3d3474d9a2529209
zhiruo <e0967827@u.nus.edu>
2023-09-01 21:02:37 +0800

    Complete Level 9

--- src/test/java/Pau/task/TaskListTest.java
+++ src/test/java/Pau/task/TaskListTest.java
@@ -1,9 +1,7 @@
-package Pau.task;
+package pau.task;
 
-import Pau.exception.NoSuchTaskException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.ByteArrayOutputStream;
