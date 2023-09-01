da1b6a52a17d93accbeab2108f03603fedff4fb8
L1MJH <lolflgc@icloud.com>
2023-09-01 12:52:54 +0800

    Update Deadline.java

--- src/main/java/simon/task/Deadline.java
+++ src/main/java/simon/task/Deadline.java
@@ -6,11 +6,27 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+/**
+ * The {@code Deadline} class extends the {@code Task} class and represents a task
+ * that has a specific end date and time by which it should be completed.
+ */
 public class Deadline extends Task {
+    /** The end date and time for this deadline task. */
     public final LocalDateTime endDateTime;
+
+    /** Formatter to parse date and time input provided by the user. */
     private static final DateTimeFormatter INPUT_FORMATTER = DateTimeFormatter.ofPattern("d/M/yyyy[ HHmm]");
+
+    /** Formatter to format date and time for output display. */
     private static final DateTimeFormatter OUTPUT_FORMATTER = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mm a");
 
+    /**
+     * Constructs a new Deadline task.
+     *
+     * @param taskName The name or description of the task.
+     * @param endDate The end date and time string for this deadline.
+     * @throws SimonException If the provided date and time format is incorrect.
+     */
     public Deadline(String taskName, String endDate) throws SimonException {
         super(taskName);
         try {
@@ -25,6 +41,11 @@ public class Deadline extends Task {
         }
     }
 
+    /**
+     * Converts this Deadline task to a string format suitable for display.
+     *
+     * @return A string representation of this Deadline task.
+     */
     @Override
     public String toString() {
         return " [D]" + (super.isDone ? "[X] " : "[ ] ") + super.toString() + " (by: " + endDateTime.format(OUTPUT_FORMATTER) + ")";
