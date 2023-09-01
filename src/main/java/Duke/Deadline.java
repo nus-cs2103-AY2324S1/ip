63d516ee7532c35fb930f8825f408d44a168d720
glenngnng <glennng12@gmail.com>
2023-09-01 10:08:03 +0800

    Add JavaDocs for deadline Event todo

--- src/main/java/Duke/Deadline.java
+++ src/main/java/Duke/Deadline.java
@@ -3,6 +3,9 @@ package duke;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * A Deadline Task that contains a description and a deadline.
+ */
 public class Deadline extends Task{
 
     protected String by;
@@ -10,24 +13,43 @@ public class Deadline extends Task{
     protected DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
 
 
+    /**
+     * Returns a String that is formatted accordingly to the outputFormat, represents a date and time.
+     * @param s String that is in the specified inputFormat, represents a date and time.
+     * @return the string of date and time to be printed to the user.
+     */
     public String getDateTime(String s) {
         LocalDateTime localDateTime = LocalDateTime.parse(s, inputFormat);
         String dateTime = localDateTime.format(outputFormat);
         return dateTime;
     }
+
+    /**
+     * A Deadline task constructor.
+     * @param description The description of the Deadline task.
+     * @param by The deadline.
+     */
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
     }
 
-
+    /**
+     * Returns A string representation of the Deadline Task, including the symbol, status icon,
+     * description and deadline.
+     * @return a String to be printed to the user.
+     */
     @Override
     public String toString() {
         return "[D]" + super.getStatusIcon() + description + " (by: " + getDateTime(by) + ")";
     }
 
-
-    public static Deadline readFromFile(String[] segments) throws GmanException {
+    /**
+     * Reads a line in a .txt file that represents a Deadline task and converts it to a Deadline Task.
+     * @param segments A string array that is split by " | ", separating the line into useful chunks.
+     * @return A deadline task.
+     */
+    public static Deadline readFromFile(String[] segments){
         String symbol = segments[1];
         String description = segments[2];
         String by = segments[3];
@@ -38,6 +60,10 @@ public class Deadline extends Task{
         return toReturn;
     }
 
+    /**
+     * returns A String to be written into the .txt file.
+     * @return A formatted String for the Deadline task with " | " separators.
+     */
     @Override
     public String toWriteString() {
         String toWrite = "D" + " | " + super.toWriteString() + " | " + by;
--- src/main/java/Duke/Todo.java
+++ src/main/java/Duke/Todo.java
@@ -1,17 +1,33 @@
 package duke;
 
+/**
+ * A Todo Task that contains a description.
+ */
 public class Todo extends Task{
 
+    /**
+     * A Todo task constructor.
+     * @param description The description of the Todo task.
+     */
     public Todo(String description) {
         super(description);
     }
 
+    /**
+     * A string representation of the Todo Task containing the symbol, status icon and description.
+     * @return A string to be printed to the user.
+     */
     @Override
     public String toString() {
         return "[T]" + super.getStatusIcon() + description;
     }
-    
-    public static Todo readFromFile(String[] segments) throws GmanException {
+
+    /**
+     * Reads a line in a .txt file that represents a Todo task and converts it to a Todo Task.
+     * @param segments A string array that is split by " | ", separating the line into useful chunks.
+     * @return A deadline task.
+     */
+    public static Todo readFromFile(String[] segments) {
         String symbol = segments[1];
         String description = segments[2];
         Todo toReturn =  new Todo(description);
@@ -22,6 +38,10 @@ public class Todo extends Task{
     }
 
 
+    /**
+     * returns A String to be written into the .txt file.
+     * @return A formatted String for the Todo task and " | " separators.
+     */
     @Override
     public String toWriteString() {
         String toWrite = "T" + " | " + super.toWriteString();
