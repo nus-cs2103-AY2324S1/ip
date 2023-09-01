06cf98edb9744362eb0ae15fbcf769ce70e3b10b
KumChaiYin <kumchaiyin@gmail.com>
2023-09-01 15:25:12 +0800

    A-JUnit

---
+++ src/test/java/aichan/TaskListTest.java
@@ -0,0 +1,28 @@
+package aichan;
+
+import aichan.task.ToDo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+    @Test
+    public void testGetSize() {
+        TaskList tasks = new TaskList();
+        ToDo todo1 = new ToDo("return book");
+        ToDo todo2 = new ToDo("buy milk");
+        tasks.addTask(todo1);
+        tasks.addTask(todo2);
+        assertEquals(2, tasks.getSize());
+    }
+
+    @Test
+    public void testGetTask() {
+        TaskList tasks = new TaskList();
+        ToDo todo1 = new ToDo("return book");
+        ToDo todo2 = new ToDo("buy milk");
+        tasks.addTask(todo1);
+        tasks.addTask(todo2);
+        assertEquals(todo2, tasks.getTask(2));
+    }
+}
---
+++ src/test/java/aichan/task/EventTest.java
@@ -0,0 +1,35 @@
+package aichan.task;
+
+import aichan.AiChanException;
+import aichan.TaskList;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EventTest {
+    @Test
+    public void testToString() {
+        String[] arr = {"meeting", "13/12/2019 1200", "13/12/2019 1400"};
+        String result;
+        try {
+            Event event = new Event(arr);
+            result = event.toString();
+        } catch (AiChanException e) {
+            result = e.getMessage();
+        }
+        assertEquals("[E][ ] meeting (from: Dec 13 2019 1200 to: Dec 13 2019 1400)", result);
+    }
+
+    @Test
+    public void testToFileLine() {
+        String[] arr = {"meeting", "13/12/2019 1200", "13/12/2019 1400"};
+        String result;
+        try {
+            Event event = new Event(arr);
+            result = event.toFileLine();
+        } catch (AiChanException e) {
+            result = e.getMessage();
+        }
+        assertEquals("E | 0 | meeting | 13/12/2019 1200 - 13/12/2019 1400", result);
+    }
+}
