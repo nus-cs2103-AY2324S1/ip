ebc2736297cdb63be6d06323c91448d7451745c8
MagnificentCreature <E0960127@u.nus.edu>
2023-09-01 23:38:03 +0800

    Created Gradle files

---
+++ src/main/java/exceptions/MissingDescriptionException.java
@@ -0,0 +1,9 @@
+package exceptions;
+/**
+ * Represents the exception thrown when the user inputs a task without any textual description.
+ */
+public class MissingDescriptionException extends Exception{
+    public MissingDescriptionException(String task) {
+        super("OOPS!!! The description of a " + task + " cannot be empty.");
+    }
+}
