d5a02249d1d98f1e235f1a95e8727a3d5becd1a3
maypfv <maythandarhinn03@gmail.com>
2023-09-01 21:54:15 +0800

    Fix: Jar File Problems

--- src/main/java/blip/commands/DeleteCommand.java
+++ src/main/java/blip/commands/DeleteCommand.java
@@ -15,7 +15,7 @@ public class DeleteCommand extends Command {
     public void execute(TaskList taskList, BlipUI ui, BlipStorage storage) {
         // Task number does not exist.
         try {
-            if (this.index <= 0 || this.index >= taskList.size()) {
+            if (this.index < 0 || this.index >= taskList.size()) {
                 throw new WrongNumberException("!!! Wrong Task Number Error !!!\n");
             }
             Task taskToDelete = taskList.getTask(index);
