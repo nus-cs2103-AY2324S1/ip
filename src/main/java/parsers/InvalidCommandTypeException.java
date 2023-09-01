e57a8b64258d5162d7d4beed6646dbca2bed096b
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-09-01 18:23:53 +0800

    Add ParsingException subclass

---
+++ src/main/java/parsers/InvalidCommandTypeException.java
@@ -0,0 +1,15 @@
+package parsers;
+
+/**
+ * Represents an exception that is thrown when an error occurs due to an invalid command type.
+ */
+public class InvalidCommandTypeException extends InvalidParsingTypeException{
+    /**
+     * Initializes a new InvalidCommandTypeException with the specified error message.
+     *
+     * @param msg The error message describing the cause of the exception.
+     */
+    public InvalidCommandTypeException(String msg) {
+        super(msg);
+    }
+}
