3055e95f3bc77534cb8cc8d2512a5ed374be84a5
seantehds <seanteh2001@gmail.com>
2023-09-01 12:51:01 +0800

    Set up Gradle by merging with add-gradle branch

---
+++ src/test/java/emiya/datehandler/DateHandlerTest.java
@@ -0,0 +1,17 @@
+package emiya.datehandler;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DateHandlerTest {
+    @Test
+    public void dummyTest(){
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void anotherDummyTest(){
+        assertEquals(4, 4);
+    }
+}
