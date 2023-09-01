1fbfc188c3ecca0af74c1e65d357480b47eada2d
aslam341 <aslam.singapore@gmail.com>
2023-09-01 20:09:52 +0800

    Add JavaDoc to DukeException class

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -1,6 +1,16 @@
 package duke;
 
+/**
+ * The DukeException class is a custom exception class used to represent exceptions specific to the Duke application.
+ * It extends the standard Java Exception class and provides a constructor to set the exception message.
+ */
 public class DukeException extends Exception {
+
+    /**
+     * Constructs a DukeException with the specified error message.
+     *
+     * @param message The error message describing the exception.
+     */
     public DukeException(String message) {
         super(message);
     }
