b920c21510c291e71ba508a15b3bb46fbcac1ff4
aslam341 <aslam.singapore@gmail.com>
2023-09-01 17:37:33 +0800

    Add JavaDoc to Duke class

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -6,12 +6,20 @@ import duke.parser.Parser;
 import duke.tasks.TaskList;
 import duke.ui.Ui;
 
+/**
+ * The main class for the Duke application. Duke is a simple task manager.
+ */
 public class Duke {
 
     private Storage storage;
     private TaskList taskList;
     private Ui ui;
 
+    /**
+     * Constructs a Duke instance with a specified file path for data storage.
+     *
+     * @param filePath The file path where task data is loaded from and saved to.
+     */
     public Duke(String filePath) {
         ui = new Ui();
         try {
@@ -28,6 +36,9 @@ public class Duke {
         duke.run();
     }
 
+    /**
+     * Runs the Duke application, displaying a welcome message and processing user commands.
+     */
     public void run() {
         this.ui.showWelcome();
         String userInput = ui.readCommand();
