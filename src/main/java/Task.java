1b9edc5b4abec88ccd245dbb22ec5c5d64f3a708
Nicole Ng <nicol3slaw@gmail.com>
2023-09-01 11:54:19 +0800

    add save feature that save the tasks automatically

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,13 @@
 public class Task {
-    private String description;
-    private Boolean isDone;
+    protected String description;
+    protected Boolean isDone;
 
     public Task (String description) {
         this.description = description;
         this.isDone = false;
+    }
+    public String getTaskType() {
+        return ""; // Return an empty string for the base class
     }
 
     public void markDone() {
@@ -13,8 +16,47 @@ public class Task {
     public void markNotDone() {
         this.isDone = false;
     }
+    public String toFileString() {
+        char doneStatus = isDone ? '1' : '0';
+        return doneStatus + " | " + description;
+    }
+
+    public static Task createTaskFromData(String taskData) {
+        String[] taskParts = taskData.split("\\s*\\|\\s*");
+
+        if (taskParts.length >= 3) {
+            String taskType = taskParts[0].trim();
+            String doneStatus = taskParts[1].trim();
+            String description = taskParts[2].trim();
+
+            Task task;
+
+            if (taskType.equals("T")) {
+                task = Todo.createTodoFromData(taskData);
+            } else if (taskType.equals("D")) {
+                task = Deadline.createDeadlineFromData(taskData);
+            } else if (taskType.equals("E")) {
+                task = Event.createEventFromData(taskData);
+            } else {
+                System.out.println("Unrecognized task type: " + taskType);
+                return null;
+            }
+
+            if (doneStatus.equals("1")) {
+                task.markDone();
+            }
+
+            return task;
+        }
+
+        return null; // Incomplete data
+    }
+
+
+
     @Override
     public String toString() {
-        return "[" + (isDone? "X" : " ") + "] " + this.description;
+        String status = isDone ? "1" : "0";
+        return getTaskType() + " | " + status + " | " + description;
     }
 }
