0f4f4cc35996951e6d45436dffaf6351a003884b
Derek Tan <derek.tan.jing.xu@gmail.com>
2023-09-01 16:40:04 +0800

    Add DeleteCommand class

---
+++ src/main/java/duke/DeleteCommand.java
@@ -0,0 +1,18 @@
+package duke;
+
+import java.util.ArrayList;
+
+public class DeleteCommand extends Command {
+    public static final String DELETE_COMMAND = "delete";
+    public DeleteCommand(ArrayList<String> params) {
+        super(params);
+    }
+
+    @Override
+    public void execute(TaskList tasks, UI ui, Storage storage) throws DukeException {
+        int index = Integer.parseInt(params.get(1)) - 1;
+        Task task = tasks.remove(index);
+        ui.printTaskDeletedMessage(task, tasks.getTaskCount());
+        tasks.saveState(storage);
+    }
+}
