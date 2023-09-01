980f451e7ec7b61ecd80df5e0fc3152a2a574843
aslam341 <aslam.singapore@gmail.com>
2023-09-01 10:57:32 +0800

    Update Storage and Ui class

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Ui {
-    private static final String LINE_SEPARATOR = "____________________________________________________________";
+    public static final String LINE_SEPARATOR = "____________________________________________________________";
 
     Scanner scanner;
 
@@ -14,18 +14,18 @@ public class Ui {
     }
 
     public void showWelcome() {
-        System.out.println("Hello! I'm David\nWhat can I do for you?");
+        System.out.println(messageWithSeparator("Hello! I'm David\nWhat can I do for you?"));
     }
 
     public void showGoodbye() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(messageWithSeparator("Bye. Hope to see you again soon!"));
     }
 
-    public String messageWithSeparator(String message) {
+    public static String messageWithSeparator(String message) {
         return LINE_SEPARATOR+ "\n" + message + "\n" + LINE_SEPARATOR;
     }
 
-    public void printWithSeparator(String message) {
+    public static void printMessageWithSeparator(String message) {
         System.out.println(messageWithSeparator(message));
     }
 }
