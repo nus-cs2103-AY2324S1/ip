53591b1ca69445a9586fcaae993b806a98aa4007
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-09-01 17:36:54 +0800

    Add optional isDone param to constructor to task, deadline, event, todo

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -5,6 +5,11 @@ public class Deadline extends Task {
         this.deadline = deadline;
     }
 
+    public Deadline (String description, boolean isDone, String deadline) {
+        super(description, isDone);
+        this.deadline = deadline;
+    }
+
     @Override
     public String toString() {
         return "[D][" + this.getStatusIcon() + "] " +  this.description + " (by: " + this.deadline + ")";
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -6,6 +6,12 @@ public class Event extends Task {
         this.end = end;
     }
 
+    public Event(String description, boolean isDone, String start, String end) {
+        super(description, isDone);
+        this.start = start;
+        this.end = end;
+    }
+
     @Override
     public String toString() {
         return "[E][" + this.getStatusIcon() + "] " +  this.description + " (from: " + this.start + " to: " + this.end + ")";
