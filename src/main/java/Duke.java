baef718d45ce73a5c2a61f870ba3228963ac5a4c
RiyaMehta2211 <>
2023-09-01 21:48:49 +0800

    Merge branch-Level-8 with master

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Scanner;
 //This class has a chatbot which will generate a list based on a set of tasks
 //that have been input by the user. These tasks include deadlines, events and ToDos
+//The user input will be parsed to extract the date and time of the events and deadlines
 public class Duke {
     public static void main(String[] args) {
         String name = "Harry Potter";
