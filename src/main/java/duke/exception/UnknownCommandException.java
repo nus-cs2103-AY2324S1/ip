b4ff1fe436a10113add166ba0966f7f98339992c
junnengsoo <soojunneng01@gmail.com>
2023-09-01 17:37:26 +0800

    Add Gradle

--- src/main/java/duke/exception/UnknownCommandException.java
+++ src/main/java/duke/exception/UnknownCommandException.java
@@ -2,6 +2,6 @@ package duke.exception;
 
 public class UnknownCommandException extends Exception{
     public UnknownCommandException() {
-        super("What gibberish are you saying man?!");
+        super("What gibberish are you saying man?!\n");
     }
 }
