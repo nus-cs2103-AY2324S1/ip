2613cba34377416df7954ced2017d8889e1b1b65
aliciamichellew <aliciamichellew@gmail.com>
2023-09-01 14:47:23 +0800

    Add JavaDoc for MarkStrategy

--- src/main/java/kevin/evaluator/MarkStrategy.java
+++ src/main/java/kevin/evaluator/MarkStrategy.java
@@ -8,11 +8,27 @@ import kevin.exception.KevinException;
 
 import java.util.ArrayList;
 
+/**
+ * A class responsible for the logic for MARK command.
+ */
 public class MarkStrategy extends BaseStrategy {
+    /**
+     * Constructor to initialize MarkStrategy.
+     * @param taskList This is the TaskList where the tasks are stored and operations are defined.
+     * @param arguments This is an ArrayList where all the needed arguments are stored.
+     */
     public MarkStrategy(TaskList taskList, ArrayList<String> arguments) {
         super(taskList, arguments);
     }
 
+    /**
+     * Handles the logic of the MARK command.
+     * @param logger This is the Logger that handles System.out.println.
+     * @param fileStorage This is the FileStorage that handles the storage in the local computer.
+     * @param isInFile This is the boolean to show whether the task is in the local computer's file.
+     * @return Returns a boolean that determines the continuation of the evaluation.
+     * @throws KevinException On the detection of errors.
+     */
     @Override
     public boolean evaluate(Logger logger, FileStorage fileStorage, boolean isInFile) throws KevinException {
         int index = Integer.parseInt(this.arguments.get(1));
