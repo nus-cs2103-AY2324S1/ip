0b59fae70b21d9a312a4e58c7afe9383790e087a
L1MJH <lolflgc@icloud.com>
2023-09-01 12:51:06 +0800

    Add Javadoc for Parser

--- src/main/java/simon/command/Parser.java
+++ src/main/java/simon/command/Parser.java
@@ -6,12 +6,29 @@ import simon.task.ToDo;
 import simon.task.Deadline;
 import simon.task.Event;
 
+/**
+ * The {@code Parser} class is responsible for interpreting user input commands and
+ * converting them into actions that the Simon application can understand and execute.
+ * It serves as a bridge between user inputs and the internal representation of commands.
+ */
 public class Parser {
 
+    /**
+     * Enumeration of valid commands that the Simon application can process.
+     * Each command type corresponds to a specific action or operation
+     * that can be performed by the Simon application.
+     */
     public enum Command {
         TODO, DEADLINE, EVENT, LIST, MARK, UNMARK, DELETE, BYE, UNKNOWN
     }
 
+    /**
+     * Converts a given raw user input into its corresponding {@code Command} type.
+     * If the input does not match any known command, it defaults to {@code UNKNOWN}.
+     *
+     * @param input The raw user input command as a string.
+     * @return The {@code Command} enumeration representing the type of command.
+     */
     public static Command parseCommand(String input) {
         try {
             return Parser.Command.valueOf(input.toUpperCase());
@@ -20,6 +37,20 @@ public class Parser {
         }
     }
 
+    /**
+     * Analyzes the input data to construct a specific {@code Task} object based on
+     * the provided command type. This method can handle various task types like
+     * {@code ToDo}, {@code Deadline}, and {@code Event}.
+     *
+     * <p>
+     * The method throws a {@code SimonException} for invalid or incomplete user inputs.
+     * </p>
+     *
+     * @param inData The raw user input string containing task details.
+     * @param commandType The type of command the user input string represents.
+     * @return A {@code Task} object representing the task detailed in the user input.
+     * @throws SimonException If there is an error in interpreting the user input.
+     */
     public static Task parseAddTask(String inData, Command commandType) throws SimonException {
         Task task;
         switch (commandType) {
@@ -79,5 +110,4 @@ public class Parser {
         }
         return task;
     }
-
 }
