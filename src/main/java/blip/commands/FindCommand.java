cf8644ae5d09939d724a5e1c557d5c51e1b4ab28
maypfv <maythandarhinn03@gmail.com>
2023-09-02 01:25:47 +0800

    Add: JavaDoc for FindCommand class

--- src/main/java/blip/commands/FindCommand.java
+++ src/main/java/blip/commands/FindCommand.java
@@ -1,16 +1,32 @@
 package blip.commands;
 
-import java.time.LocalDateTime;
 import blip.ui.*;
 import blip.tasks.*;
 import blip.storage.*;
 
+/**
+ * Represents find command to find tasks that match description.
+ */
 public class FindCommand extends Command {
+    /**
+     * Description of task to find.
+     */
     String description;
+
+    /**
+     * Constructor of FindCommand.
+     * @param description The description of task to find.
+     */
     public FindCommand(String description) {
         this.description = description;
     }
 
+    /**
+     * Executes the finding of matching tasks in task list.
+     * @param tasks The Array List of tasks to find matching description
+     * @param ui The user interface of Blip
+     * @param storage The storage for Blip
+     */
     @Override
     public void execute(TaskList tasks, BlipUI ui, BlipStorage storage) {
         if (tasks.size() == 0) {
