6496d2e93583081a43ca61fe2d4b54657a198bc2
et-irl <e0958283@u.nus.edu>
2023-09-01 21:34:06 +0800

    Add JavaFx and Launcher for GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,9 +1,14 @@
 package duke;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 /**
  * Duke is a simple task management program that allows users to add, delete, mark, and list tasks.
  */
-public class Duke {
+public class Duke extends Application {
     private static TaskList taskList;
 
     /**
@@ -92,5 +97,15 @@ public class Duke {
         taskList = new TaskList(dataFilePath);
 
         Parser.start();
+    }
+
+    
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
