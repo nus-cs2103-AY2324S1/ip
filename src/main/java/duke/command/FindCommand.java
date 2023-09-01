4e4c49c22aa9254e75fb470ccb3d8d70f8af3e25
YangHeng Tang <yanghengtang@gmail.com>
2023-09-01 12:05:55 +0800

    Add JavaDoc for FindCommand.java

--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -4,6 +4,9 @@ import duke.util.Storage;
 import duke.util.TaskList;
 import duke.util.Ui;
 
+/**
+ * Represents a Find command to be executed.
+ */
 public class FindCommand extends Command {
     private String queryString;
 
@@ -12,9 +15,16 @@ public class FindCommand extends Command {
         this.queryString = queryString;
     }
 
+    /**
+     * Executes the list of commands to find a Task from a TaskList.
+     *
+     * @param taskList The given TaskList to be searched on.
+     * @param ui The given Ui to show the status of the finding of the task.
+     * @param storage The given Storage that saves the TaskList locally.
+     */
     @Override
-    public void execute(TaskList list, Ui ui, Storage storage) {
-        TaskList taskList = list.find(this.queryString);
-        ui.printFind(taskList);
+    public void execute(TaskList taskList, Ui ui, Storage storage) {
+        TaskList filteredTaskList = taskList.find(this.queryString);
+        ui.printFind(filteredTaskList);
     }
 }
