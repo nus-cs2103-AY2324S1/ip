3e9110538365f17535fa3470106cd8289723aa01
Derek Tan <derek.tan.jing.xu@gmail.com>
2023-09-01 16:45:40 +0800

    Add UnmarkCommand class

---
+++ src/main/java/duke/UnmarkCommand.java
@@ -0,0 +1,18 @@
+package duke;
+
+import java.util.ArrayList;
+
+public class UnmarkCommand extends Command{
+    public static final String COMMAND_UNMARK = "unmark";
+    public UnmarkCommand(ArrayList<String> params) {
+        super(params);
+    }
+
+    @Override
+    public void execute(TaskList tasks, UI ui, Storage storage) throws DukeException {
+        int index = Integer.parseInt(params.get(1)) - 1;
+        Task task = tasks.unmark(index);
+        ui.printTaskUnmarkedMessage(task);
+        tasks.saveState(storage);
+    }
+}
