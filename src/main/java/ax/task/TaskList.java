ab64aaf565e090cccd06dec00b8c35a80578c9d4
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2023-09-01 16:47:26 +0800

    add more javadocs

--- src/main/java/ax/task/TaskList.java
+++ src/main/java/ax/task/TaskList.java
@@ -1,12 +1,19 @@
 package ax.task;
 
-import ax.task.ListItem;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskList {
+    /**
+     * contains an ArrayList of ListItem to be used
+     */
     static List<ListItem> listItems = new ArrayList<ListItem>();
+
+    /**
+     * gets the list of items
+     * @return List<ListItem> containing all items
+     */
 
     public static List<ListItem> getListItems() {
         return listItems;
