afa2c4629414c429e3a96e90ddf1fbda0f894d4e
aliciamichellew <aliciamichellew@gmail.com>
2023-09-01 16:29:51 +0800

    Add JavaDoc for FindStrategy

--- src/main/java/kevin/evaluator/FindStrategy.java
+++ src/main/java/kevin/evaluator/FindStrategy.java
@@ -8,11 +8,27 @@ import kevin.ui.Logger;
 
 import java.util.ArrayList;
 
+/**
+ * A class responsible for the logic for FIND command.
+ */
 public class FindStrategy extends BaseStrategy {
+    /**
+     * Constructor to initialize FindStrategy.
+     * @param taskList This is the TaskList where the tasks are stored and operations are defined.
+     * @param arguments This is an ArrayList where all the needed arguments are stored.
+     */
     public FindStrategy(TaskList taskList, ArrayList<String> arguments) {
         super(taskList, arguments);
     }
 
+    /**
+     * Handles the logic of the FIND command.
+     * @param logger This is the Logger that handles System.out.println.
+     * @param fileStorage This is the FileStorage that handles the storage in the local computer.
+     * @param isInFile This is the boolean to show whether the task is in the local computer's file.
+     * @return Returns a boolean that determines the continuation of the evaluation.
+     * @throws KevinException On the detection of errors.
+     */
     @Override
     public boolean evaluate(Logger logger, FileStorage fileStorage, boolean isInFile) throws KevinException {
         String keyword = this.arguments.get(1);
