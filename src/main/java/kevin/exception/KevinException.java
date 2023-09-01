7adb62433f5c4d345f6b7143e4efb3bb5cabbfd6
aliciamichellew <aliciamichellew@gmail.com>
2023-09-01 14:54:08 +0800

    Add JavaDoc for KevinException

--- src/main/java/kevin/exception/KevinException.java
+++ src/main/java/kevin/exception/KevinException.java
@@ -1,10 +1,21 @@
 package kevin.exception;
 
+/**
+ * A class responsible for the exceptions for Kevin.
+ */
 public class KevinException extends Exception {
+    /**
+     * Constructor to intialize KevinException.
+     * @param errorMessage This is the message that will be displayed.
+     */
     public KevinException(String errorMessage) {
         super(errorMessage);
     }
 
+    /**
+     * {@inheritDoc}
+     * @return Returns the detail message string of this throwable.
+     */
     @Override
     public String getMessage() {
         return "☹ OOPS!!! " + super.getMessage();
