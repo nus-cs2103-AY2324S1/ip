7e6971cff199dca703fcdbfd1cbc5514914894d6
vijay-shankaranand <shankaranand48@gmail.com>
2023-09-01 14:43:40 +0800

    add A-JUnit changes

---
+++ src/test/java/JavAI/StorageTest.java
@@ -0,0 +1,55 @@
+package JavAI;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.io.*;
+import java.util.Scanner;
+
+public class StorageTest {
+
+    private Storage storage;
+    private final String filePath = "./src/main/txtFolder/JavAI.txt";
+
+    @BeforeEach
+    void setUp() {
+
+        storage = new Storage(filePath);
+    }
+
+    @Test
+    void testLoad() throws IOException {
+
+        File testFile = new File(filePath);
+        FileWriter writer = new FileWriter(testFile);
+        writer.write("[T][X] Sample Todo\n");
+        writer.write("[D][X] Sample Deadline ( by: Dec 31 2023 23:59 )\n");
+        writer.close();
+        ArrayList<Task> loadedTasks = storage.load();
+        assertEquals(2, loadedTasks.size());
+        assertTrue(loadedTasks.get(0) instanceof Todo);
+        assertTrue(loadedTasks.get(1) instanceof Deadline);
+    }
+
+    @Test
+    void testTaskListWriter() throws IOException {
+
+        ArrayList<Task> tasksToWrite = new ArrayList<>();
+        tasksToWrite.add(new Todo("Write JUnit Tests"));
+        tasksToWrite.add(new Deadline("Finish Project", "2023-12-31", "2359"));
+        storage.taskListWriter(tasksToWrite);
+        File testFile = new File(filePath);
+        Scanner scanner = new Scanner(testFile);
+        StringBuilder fileContent = new StringBuilder();
+        while (scanner.hasNextLine()) {
+            fileContent.append(scanner.nextLine()).append("\n");
+        }
+        scanner.close();
+
+        String expectedContent = "[T][ ] Write JUnit Tests\n" +
+                "[D][ ] Finish Project ( by: Dec 31 2023 23:59 )\n";
+        assertEquals(expectedContent, fileContent.toString());
+    }
+}
+
---
+++ src/test/java/JavAI/TaskListTest.java
@@ -0,0 +1,40 @@
+package JavAI;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TaskListTest {
+
+    private TaskList taskList;
+
+    @BeforeEach
+    void setUp() {
+        taskList = new TaskList();
+    }
+
+    @Test
+    void testAddTask() {
+        Task task = new Task("Sample Task");
+        taskList.add(task);
+        assertEquals(1, taskList.size());
+    }
+    @Test
+    void testDeleteTask() {
+        Task task1 = new Task("Task 1");
+        Task task2 = new Task("Task 2");
+        Task task3 = new Task("Task 3");
+
+        taskList.add(task1);
+        taskList.add(task2);
+        taskList.add(task3);
+
+        assertEquals(3, taskList.size());
+
+        taskList.delete(1);
+
+        assertEquals(2, taskList.size());
+        assertThrows(JavAIException.class, () -> taskList.get(2));
+    }
+
+}
