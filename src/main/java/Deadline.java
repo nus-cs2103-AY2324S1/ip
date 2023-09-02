c732b741e3886806cc780824881764a9cc91b3db
xyT <tongyu5336@gmail.com>
2023-09-02 12:15:50 +0800

    Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,24 @@
-public class Deadline extends Task {
-    protected String by;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
-    public Deadline(String description, String by) {
+public class Deadline extends Task {
+    protected LocalDateTime by;
+
+    public Deadline(String description, String dateString, String timeString) {
         super(description);
+        LocalDate date = LocalDate.parse(dateString);
+        LocalTime time = LocalTime.parse(timeString);
+        LocalDateTime by = LocalDateTime.of(date, time);
         this.by = by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.by + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy, HH:mm");
+        String byFormatted = this.by.format(formatter);
+        return "[D]" + super.toString() + " (by: " + byFormatted + ")";
     }
 }
