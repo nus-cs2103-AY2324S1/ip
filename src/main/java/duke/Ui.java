70aadb08eb0d77b20a3cbe8219cfd16323503094
lynnlow175 <lynnlow175@gmail.com>
2023-09-02 17:45:26 +0800

    A-JUnit

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -25,4 +25,53 @@ public class Ui {
         System.out.println("Got it. I've added this task:\n" + task + "\n"
                 + "Now you have " + taskList.size() + " tasks in the list.");
     }
+
+    public static void printEmptyList() {
+        System.out.println("There is currently no items in the list");
+    }
+
+    public static void printList(TaskList taskList) {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 1; i <= taskList.size(); i++) {
+            String task = String.valueOf(taskList.getTask(i));
+            System.out.println(i + ". " + task);
+        }
+    }
+
+    public static void printDone(Task task) {
+        System.out.println("Nice! I've marked this task as done:" + "\n" + task);
+    }
+
+    public static void printNotDone(Task task) {
+        System.out.println("OK, I've marked this task as not done yet:" + "\n" + task);
+    }
+
+    public static void OutofBounds() {
+        System.out.println("IndexOutOfBounds");
+    }
+
+    public static void NumberFormat() {
+        System.out.println("NumberFormatException");
+    }
+
+    public static void removeTask(String deleted, TaskList taskList) {
+        System.out.println("Noted. I've removed this task:\n" + deleted + "\n"
+                + "Now you have " + taskList.size() + " tasks in the list.");
+    }
+
+    public static void ToDoExcept() {
+        System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+    }
+
+    public static void DateFormatExcept() {
+        System.out.println("invalid date format");
+    }
+
+    public static void deadlineExcept() {
+        System.out.println("Wrong Deadline Format!");
+    }
+
+    public static void eventExcept() {
+        System.out.println("Wrong Event Format!");
+    }
 }
--- src/test/java/duke/StorageTest.java
+++ src/test/java/duke/StorageTest.java
@@ -1,4 +1,44 @@
 package duke;
 
-public class Storage {
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StorageTest {
+    private TaskList taskList;
+    @Test
+    public void test_Save() {
+        this.taskList = new TaskList();
+        String filepath = "./data/duke.txt";
+
+        for (int i = 0; i < 5; i++) {
+            taskList.add(new Todo("read book", "todo read book"));
+        }
+        Storage.save(taskList);
+        try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
+            for (int i = 0; i < 5; i++) {
+                assertEquals("todo read book", br.readLine());
+            }
+        } catch (IOException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void test_Invalid_File() {
+        TaskList taskList = new TaskList();
+        String filepath = "nonexistent.txt";
+        Storage.save(taskList);
+        try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
+            for (int i = 0; i < 5; i++) {
+                assertEquals("todo read book", br.readLine());
+            }
+        } catch (IOException e) {
+            // ignore
+        }
+    }
 }
