3f07d63b4eb250c7c615db789d904213cfad9e2f
euchangxian <changxian2001@gmail.com>
2023-09-02 16:34:06 +0800

    Add more specific Exceptions

---
+++ src/main/java/duke/exception/InvalidCommandInputException.java
@@ -0,0 +1,7 @@
+package duke.exception;
+
+public class InvalidCommandInputException extends DukeException {
+    public InvalidCommandInputException(String message) {
+        super(message);
+    }
+}
