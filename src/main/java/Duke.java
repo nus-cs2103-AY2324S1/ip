ac4f03fe12b3d952e321bc1704e892bbb5bacf51
dhruvir29 <dhruvirathod29@gmail.com>
2023-09-03 01:19:15 +0800

    Add class Ui

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,12 @@
 import java.time.DateTimeException;
-import java.util.List;
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 
 
 public class Duke {
 
     private static TaskList tasks;
+    private static Ui ui = new Ui();
 
     public static void main(String[] args) {
         Storage storage;
@@ -29,13 +23,8 @@ public class Duke {
         }
 
 
-
-
         Scanner scanner = new Scanner(System.in);
-        printHorizontalLine();
-        System.out.println("\t " + "Hey there amigo, excited to meet you! I'm Buddy, your friendly chat companion!\n" +
-                "\t " + "What can I do for you?");
-        printHorizontalLine();
+        ui.printWelcomeMessage();
         int count = 0;
 
         while (isRunning) {
@@ -43,9 +32,7 @@ public class Duke {
 
 
             if (input.equals("bye")) {
-                System.out.println("\t " + "Bye! Hope to see you again soon!");
-                printHorizontalLine();
-                isRunning = false;
+                ui.printGoodByeMessage();
                 break;
             }
 
@@ -54,10 +41,7 @@ public class Duke {
                 try {
                     int taskIndex = Integer.parseInt(input.substring(5));
                     tasks.markTaskAsDone(taskIndex - 1);
-                    printHorizontalLine();
-                    System.out.println("\tGreat! I've marked this task as done:");
-                    System.out.println("\t" + taskIndex + "." + tasks.getTask(taskIndex - 1).toString());
-                    printHorizontalLine();
+                    ui.printMarkTasksAsDone(taskIndex, tasks);
                     storage.writeTasksToFile(tasks);
                 }
                 catch (IOException i) {
@@ -71,10 +55,7 @@ public class Duke {
                 try {
                     int taskIndex = Integer.parseInt(input.substring(7));
                     tasks.markTaskAsNotDone(taskIndex - 1);
-                    printHorizontalLine();
-                    System.out.println("\tOk! I've marked this task as not done yet:");
-                    System.out.println("\t" + taskIndex + "." + tasks.getTask(taskIndex - 1).toString());
-                    printHorizontalLine();
+                    ui.printMarkTasksAsNotDone(taskIndex, tasks);
                     storage.writeTasksToFile(tasks);
                 }
                 catch (IOException i) {
@@ -86,16 +67,7 @@ public class Duke {
             //displaying list
             else if (input.strip().equals("list")) {
                 try {
-                    printHorizontalLine();
-                    System.out.println("\tHere are the tasks in your list:");
-                    if (tasks.getSize() == 0) {
-                        throw new DukeException("\t Seems like you have no tasks at the moment :) ");
-
-                    }
-                    for (int i = 1; i <= tasks.getSize(); i++) {
-                        System.out.println("\t" + i + ". " + tasks.getTask(i - 1).toString());
-                    }
-                    printHorizontalLine();
+                    ui.printListMessage(tasks);
                 }
                 catch (DukeException e) {
                     e.printMessage();
@@ -105,17 +77,13 @@ public class Duke {
             // deleting task from list
             else if (input.startsWith("delete ")) {
                 try {
-                    int pos = 0;
-                    pos = Integer.parseInt(input.substring(7).trim());
+                    int pos = Integer.parseInt(input.substring(7).trim());
                     if (pos > tasks.getSize() || pos == 0) {
                         throw new DukeException("\tThis number is out of bounds! ");
                     }
                     Task element = tasks.getTask(pos - 1);
                     tasks.deleteTask(pos - 1);
-                    printHorizontalLine();
-                    System.out.println("\tOkie I've removed this task:\n\t" + element.toString());
-                    System.out.println("\tNow you have " + tasks.getSize() + " tasks in the list.");
-                    printHorizontalLine();
+                    ui.printDeleteTasks(pos, tasks, element);
                     storage.writeTasksToFile(tasks);
                 }
                 catch (DukeException e) {
@@ -150,11 +118,7 @@ public class Duke {
             }
             Task task = new ToDo(input.substring(5));
             tasks.addTask(task);
-            printHorizontalLine();
-            System.out.println("\tNo problem! I have added this task:");
-            System.out.println("\t" + task.toString());
-            System.out.println("\tNow you have " + tasks.getSize() + " tasks in the list");
-            printHorizontalLine();
+            ui.printAddTaskToList(tasks, task);
         }
 
         else if (input.startsWith("deadline")) {
@@ -169,11 +133,7 @@ public class Duke {
                 int index = input.lastIndexOf("/by");
                 Task task = new Deadline(input.substring(9, index - 1), input.substring(index + 4));
                 tasks.addTask(task);
-                printHorizontalLine();
-                System.out.println("\tNo problem! I have added this task:");
-                System.out.println("\t" + task.toString());
-                System.out.println("\tNow you have " + tasks.getSize() + " tasks in the list");
-                printHorizontalLine();
+                ui.printAddTaskToList(tasks, task);
 
             }
             catch(DateTimeException d) {
@@ -201,11 +161,7 @@ public class Duke {
                 Task task = new Event(input.substring(6, indexFrom - 1),
                         input.substring(indexFrom + 6, indexTo - 1), input.substring(indexTo + 4));
                 tasks.addTask(task);
-                printHorizontalLine();
-                System.out.println("\tNo problem! I have added this task:");
-                System.out.println("\t" + task.toString());
-                System.out.println("\tNow you have " + tasks.getSize() + " tasks in the list");
-                printHorizontalLine();
+                ui.printAddTaskToList(tasks, task);
 
             }
             catch(DateTimeException d) {
@@ -218,46 +174,6 @@ public class Duke {
             throw new DukeException("\tHey bud! Sorry I don't quite know what you mean :-(");
         }
 
-    }
-
-//    private void saveTasks() throws IOException {
-//        Scanner scanner = new Scanner(new File("data/duke.txt"));
-//        while (scanner.hasNext()) {
-//            String[] split = scanner.nextLine().split("\\|");
-//            for (int i = 0; i < split.length; i++) {
-//                split[i] = split[i].strip();
-//            }
-//            String description = split[2];
-//
-//            switch (split[0]) {
-//                case "T":
-//                    tasks.add(new ToDo(description));
-//                    break;
-//                case "D":
-//                    String by = split[3];
-//                    tasks.add(new Deadline(description, by));
-//                    break;
-//                case "E":
-//                    String from = split[3];
-//                    String to = split[4];
-//                    tasks.add(new Event(description, split[3], split[4]));
-//                    break;
-//            }
-//        }
-//    }
-//
-//    private void writeTasksToFile() throws IOException {
-//        FileWriter fileWriter = new FileWriter("data/duke.txt");
-//        for (Task t: tasks) {
-//            fileWriter.write(t.toWriteString()+"\n");
-//        }
-//        fileWriter.close();
-//    }
-//
-
-
-    public static void printHorizontalLine() {
-        System.out.println("    __________________________________________________________________");
     }
 
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,66 @@
+public class Ui {
+
+    public void printWelcomeMessage() {
+        printHorizontalLine();
+        System.out.println("\t " + "Hey there amigo, excited to meet you! I'm Buddy, your friendly chat companion!\n" +
+                "\t " + "What can I do for you?");
+        printHorizontalLine();
+    }
+
+    public void printGoodByeMessage() {
+        printHorizontalLine();
+        System.out.println("\t " + "Bye! Hope to see you again soon!");
+        printHorizontalLine();
+    }
+
+    public void printMarkTasksAsDone(int index, TaskList tasks) {
+        printHorizontalLine();
+        System.out.println("\tGreat! I've marked this task as done:");
+        System.out.println("\t" + index + "." + tasks.getTask(index - 1).toString());
+        printHorizontalLine();
+        tasks.markTaskAsDone(index - 1);
+    }
+
+
+    public void printMarkTasksAsNotDone(int index, TaskList tasks) {
+        printHorizontalLine();
+        System.out.println("\tOk! I've marked this task as not done yet:");
+        System.out.println("\t" + index + "." + tasks.getTask(index - 1).toString());
+        printHorizontalLine();
+        tasks.markTaskAsDone(index - 1);
+    }
+
+    public void printListMessage(TaskList tasks) throws DukeException {
+            printHorizontalLine();
+            tasks.printTasks();
+            printHorizontalLine();
+    }
+
+    public void printAddedTask() {
+        System.out.println("\tNo problem! I have added this task:");
+    }
+
+    public void printHorizontalLine() {
+        System.out.println("    __________________________________________________________________");
+    }
+
+    public void printDeleteTasks(int pos, TaskList tasks, Task element) throws DukeException {
+        printHorizontalLine();
+        System.out.println("\tOkie I've removed this task:\n\t" + element.toString());
+        System.out.println("\tNow you have " + tasks.getSize() + " tasks in the list.");
+        printHorizontalLine();
+    }
+
+    public void printAddTaskToList(TaskList tasks, Task task) {
+        printHorizontalLine();
+        printAddedTask();
+        System.out.println("\t" + task.toString());
+        System.out.println("\tNow you have " + tasks.getSize() + " tasks in the list");
+        printHorizontalLine();
+    }
+
+
+
+
+
+}
