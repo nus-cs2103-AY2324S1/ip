368dc905068268b76d5c0cc55c11bad081137e0a
TiwKangXu <111041537+TiwKangXu@users.noreply.github.com>
2023-09-02 11:42:39 +0800

    Complete JavaFX setup

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -6,11 +6,15 @@ import duke.main.Parser;
 import duke.main.Storage;
 import duke.main.TaskList;
 import duke.main.Ui;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * A chatbot that helps to record tasks and store tasks.
  */
-public class Duke {
+public class Duke extends Application {
     private TaskList tasks;
     private Storage storage;
     private Parser parser;
@@ -27,6 +31,30 @@ public class Duke {
         this.storage = new Storage(tasks);
         this.parser = new Parser(tasks, storage);
         this.ui = new Ui();
+    }
+
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param stage the primary stage for this application, onto which
+     *                     the application scene can be set.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages.
+     * @throws Exception if something goes wrong
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 
     /**
