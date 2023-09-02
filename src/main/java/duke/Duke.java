50a332699155021f042f91bc131873469c780590
Chandan8186 <chandananandababu@gmail.com>
2023-09-02 22:58:11 +0800

    Implement rudimentary JavaFX gui

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -4,16 +4,30 @@ import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 import exceptions.DukeException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import tasks.Task;
 import tasks.TaskList;
 
 /**
  * Duke is a chatbot that allows users to manage tasks.
  */
-public class Duke {
+public class Duke extends Application {
 
     private static TaskList taskList = new TaskList();
     private static Storage storage;
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/userAvatar.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/dukeAvatar.png"));
 
     /**
      * Marks task as done.
@@ -138,6 +152,103 @@ public class Duke {
         storage.saveChanges(Duke.taskList);
         scanner.close();
         Ui.printOutro();
+    }
+
+    /**
+     * Iteration 2:
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    private void handleUserInput(VBox dialogContainer, TextField userInput) {
+        Label userText = new Label(userInput.getText());
+        Label dukeText = new Label(getResponse(userInput.getText()));
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, new ImageView(user)),
+                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
+        );
+        userInput.clear();
+    }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    private String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
+    /**
+     * Creates a label with the specified text and adds it to the dialog container.
+     *
+     * @param text String containing text to add.
+     * @return A label with the specified text that has word wrap enabled.
+     */
+    private Label getDialogLabel(String text) {
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+
+        return textToAdd;
+    }
+
+    @Override
+    public void start(Stage stage) {
+        //The container for the content of the chat to scroll.
+        ScrollPane scrollPane = new ScrollPane();
+        VBox dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        TextField userInput = new TextField();
+        Button sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        Scene scene = new Scene(mainLayout);
+
+        //Step 2. Formatting the window to look as expected
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        //Step 3. Add functionality to handle user input.
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput(dialogContainer, userInput);
+        });
+
+        userInput.setOnAction((event) -> {
+            handleUserInput(dialogContainer, userInput);
+        });
+
+        //Scroll down to the end every time dialogContainer's height changes.
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+
+        // FINAL STEP
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
