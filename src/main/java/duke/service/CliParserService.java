ea799b38088dd0a9625728776d1b51f3e2d8fca7
euchangxian <changxian2001@gmail.com>
2023-09-02 18:22:52 +0800

    Extract out parsing of input to its own method
    This is to allow for a more straightforward unit testing of
    CliParserService.

--- src/main/java/duke/service/CliParserService.java
+++ src/main/java/duke/service/CliParserService.java
@@ -18,24 +18,9 @@ public class CliParserService {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String line = scanner.nextLine().trim();
-            List<String> arguments = new ArrayList<>();
-
-            // Split the command and its primary argument
-            String[] splitBySpace = line.split(" ", 2);
-            String commandType = splitBySpace[0];
-
-            // If there's more than just the command, handle the arguments
-            if (splitBySpace.length > 1) {
-                // primaryArg refers to either task name, or the task index.
-                String primaryArg = splitBySpace[1].split("/")[0].trim();
-                arguments.add(primaryArg);
-
-                // Split the rest by slashes
-                String[] splitBySlash = splitBySpace[1].split("/");
-                for (int i = 1; i < splitBySlash.length; ++i) { // Start at 1 to skip the task name
-                    arguments.add(splitBySlash[i].trim());
-                }
-            }
+            ParseResult parseResult = parseCommandAndArguments(line);
+            String commandType = parseResult.getCommandType();
+            List<String> arguments = parseResult.getArguments();
             try {
                 Command command = commandFactory.createCommand(commandType, arguments);
                 if (command.isExit()) {
@@ -45,6 +30,46 @@ public class CliParserService {
             } catch (UnknownCommandException | InvalidCommandInputException e) {
                 uiService.printGenericMessage(e.getMessage());
             }
+        }
+    }
+
+    public ParseResult parseCommandAndArguments(String line) {
+        List<String> arguments = new ArrayList<>();
+
+        // Split the command and its primary argument
+        String[] splitBySpace = line.split(" ", 2);
+        String commandType = splitBySpace[0];
+
+        // If there's more than just the command, handle the arguments
+        if (splitBySpace.length > 1) {
+            // primaryArg refers to either task name, or the task index.
+            String primaryArg = splitBySpace[1].split("/")[0].trim();
+            arguments.add(primaryArg);
+
+            // Split the rest by slashes
+            String[] splitBySlash = splitBySpace[1].split("/");
+            for (int i = 1; i < splitBySlash.length; ++i) { // Start at 1 to skip the task name
+                arguments.add(splitBySlash[i].trim());
+            }
+        }
+        return new ParseResult(commandType, arguments);
+    }
+
+    static final class ParseResult {
+        private String commandType;
+        private List<String> arguments;
+
+        ParseResult(String commandType, List<String> arguments) {
+            this.commandType = commandType;
+            this.arguments = arguments;
+        }
+
+        public String getCommandType() {
+            return commandType;
+        }
+
+        public List<String> getArguments() {
+            return arguments;
         }
     }
 }
