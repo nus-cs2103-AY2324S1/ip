b5315780863aed9052df4e62b14aac00f32e9d4c
WZWren <115881359+WZWren@users.noreply.github.com>
2023-09-02 13:42:54 +0800

    Fix styling for exception package

--- src/main/java/trackerbot/exception/TrackerBotException.java
+++ src/main/java/trackerbot/exception/TrackerBotException.java
@@ -1,5 +1,8 @@
 package trackerbot.exception;
 
+/**
+ * Header comment here
+ */
 public class TrackerBotException extends Exception {
     public TrackerBotException(String errMsg) {
         super(errMsg);
