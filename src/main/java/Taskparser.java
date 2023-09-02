22fd0a66df831826045f681f3a1c7152c38ee7c3
saraozn <saraongzining@gmail.com>
2023-09-02 17:04:03 +0800

    feat: Save

---
+++ src/main/java/Taskparser.java
@@ -0,0 +1,42 @@
+public class Taskparser {
+    // Parse a string representation of a task and return the corresponding Task object
+    public static Task parseTask(String taskString) {
+        String[] parts = taskString.split(" \\| ");
+        String type = parts[0];
+        boolean isDone = Integer.parseInt(parts[1]) == 1;
+        String description = parts[2];
+
+        switch (type) {
+            case "T":
+                return new Todo(description);
+            case "D":
+                String deadline = parts[3];
+                return new Deadline(description, deadline);
+            case "E":
+                String from = parts[3];
+                String to = parts[4];
+                return new Event(description, from, to);
+            default:
+                throw new IllegalArgumentException("Invalid task type: " + type);
+        }
+    }
+
+    // Convert a Task object to a string representation
+    public static String taskToString(Task task) {
+        StringBuilder sb = new StringBuilder();
+
+        char type = task.toString().charAt(1);
+        sb.append(type).append(" | ");
+        sb.append(task.isDone ? "1" : "0").append(" | ");
+        sb.append(task.description);
+        if (task instanceof Deadline) {
+            Deadline deadlineTask = (Deadline) task;
+            sb.append(" | ").append(deadlineTask.by);
+        } else if (task instanceof Event) {
+            Event eventTask = (Event) task;
+            sb.append(" | ").append(eventTask.from).append(" | ").append(eventTask.to);
+        }
+
+        return sb.toString();
+    }
+}
