ffefbf56f3ff6f4d17ac40ef443a8ca8a745fb65
rayyan35p <rayyanis0908@gmail.com>
2023-09-03 00:19:24 +0800

    Add JUnit test for Task classes

---
+++ src/test/java/data/task/TaskListTest.java
@@ -0,0 +1,25 @@
+package data.task;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TaskListTest {
+      //Need to test this?
+//    @Test
+//    public void open_invalidFile_exceptionThrown() {
+//        FileNotFoundException thrown = assertThrows(FileNotFoundException.class,
+//                () -> new TaskList(new File("./data/duke.txt")).open(),
+//                "Expected open to throw but it didn't");
+//        assertTrue((thrown.getMessage().contains("")));
+//    }
+
+//    @Test
+//    public void testReadFromFile() {
+//        TaskList testTaskList = new TaskList(new File("/testFile.txt"));
+//    }
+}
