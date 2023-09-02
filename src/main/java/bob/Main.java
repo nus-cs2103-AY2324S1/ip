db4bc2613bbac66e0fc60f6912a0b0562118070f
Jackie <jackieguo126@gmail.com>
2023-09-02 18:54:41 +0800

    making MainWindow fx:root

--- src/main/java/bob/Main.java
+++ src/main/java/bob/Main.java
@@ -18,6 +18,7 @@ public class Main extends Application {
     public void start(Stage stage) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            fxmlLoader.setRoot(new AnchorPane());
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
