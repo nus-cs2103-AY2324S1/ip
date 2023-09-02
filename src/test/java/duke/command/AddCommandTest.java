66f4484ba3b6639556dcfc390c2e622ab01e2231
Debbie Hii Wenxin <e0959749@u.nus.edu>
2023-09-03 00:17:11 +0800

    Add test cases for Add and Delete Command classes.

---
+++ src/test/java/duke/command/AddCommandTest.java
@@ -0,0 +1,18 @@
+package duke.command;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AddCommandTest {
+    @Test
+    public void dummyTest() {
+
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void anotherDummyTest() {
+        assertEquals(4, 4);
+    }
+}
