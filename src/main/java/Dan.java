6fabf95ece8ef533f7f1b4bea73421b5aa2bec49
Song_Bingxi <bingxi.bobby.song@gmail.com>
2023-09-02 16:54:07 +0800

    added level 2

--- src/main/java/Dan.java
+++ src/main/java/Dan.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Dan {
@@ -9,24 +10,36 @@ public class Dan {
 
 
     private final static String greets = "\nDan: \n";
+
+    private static MyList strList = new MyList(100);
     private static void chat() {
         String text;
-        do {
+        while (true) {
             text = new Scanner(System.in).nextLine();
+            switch (text.toLowerCase()) {
+                case "bye" : break;
+
+                case "list" :
+                    System.out.println(
+                            greets +
+                            "你还要做的事情有：" + strList.toString()
+                    );
+            }
             System.out.println(greets + text);
-        } while (!text.toLowerCase().equals("bye"));
+        }
+//        !text.toLowerCase().equals("bye")
     }
     public static void hello() {
         System.out.println(
                 greets +
-                " Hello! I'm Dan!\n" +
-                " What can I do for you?\n"
+                " 我是小丹！\n" +
+                " 有什么可以要帮忙可以跟我说！\n"
         );
     }
     public static void goodbye() {
         System.out.println(
                 greets +
-                " Bye. Hope to see you again soon!\n"
+                " 拜拜啦！下次见！\n"
         );
     }
 }
