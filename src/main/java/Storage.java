c24c2079b65cbc45f56544119ad17df1ad3f29ca
hyc17003 <e0970589@u.nus.edu>
2023-09-02 20:51:30 +0800

    Make the code more OOP

---
+++ src/main/java/Storage.java
@@ -0,0 +1,56 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Storage {
+
+    private String filePath;
+    private static ArrayList<Task> list;
+
+    public Storage(String filePath, ArrayList<Task> list) {
+        this.filePath = filePath;
+        this.list = list;
+    }
+
+    public static void copyFileContents(String filePath) throws FileNotFoundException {
+        File f = new File(filePath);
+        Scanner s = new Scanner(f);
+        while (s.hasNext()) {
+            String line = s.nextLine();
+            if (line.contains("todo")) {
+                Todo todo = new Todo(line.substring(5));
+                list.add(todo);
+            } else if (line.contains("deadline")) {
+                int slashDeadline = line.indexOf("/");
+                String deadlineDescription = line.substring(9, slashDeadline).trim();
+                String deadlineBy = line.substring(slashDeadline + 3).trim();
+                Deadline deadline = new Deadline(deadlineDescription, deadlineBy);
+                list.add(deadline);
+            } else if (line.contains("event")) {
+                String[] eventSplit = line.split("/");
+                String eventDescription = eventSplit[0].substring(6);
+                String eventTo = eventSplit[1].substring(5);
+                String eventFrom = eventSplit[2].substring(3);
+                Event event = new Event(eventDescription, eventTo, eventFrom);
+                list.add(event);
+            }
+        }
+        s.close();
+    }
+
+    public static void writeToFile(String filePath, String textToWrite) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        fw.write(textToWrite);
+        fw.close();
+    }
+
+    public static void appendToFile(String filePath, String textToAppend) throws IOException {
+        FileWriter fw = new FileWriter(filePath, true);
+        fw.write(textToAppend + System.lineSeparator());
+        fw.close();
+    }
+
+}
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,28 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+    public class TaskList {
+
+    private static ArrayList<Task> list;
+
+    public TaskList(ArrayList<Task> list) {
+        this.list = list;
+    }
+
+    public static void initiateTaskList(Storage storage) {
+        try {
+            storage.copyFileContents("../data/ChatterBot.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
+
+    public static String convertToString(ArrayList<Task> list) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Task task : list) {
+            stringBuilder.append(task.forFile()).append(System.lineSeparator());
+        }
+        return stringBuilder.toString();
+    }
+
+}
