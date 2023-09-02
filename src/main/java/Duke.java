91413a691b7b0624e76b68545338011226ef2bb4
SynapseProgramming <roald.ong@gmail.com>
2023-09-02 19:44:20 +0800

    Remove unused headers

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,8 @@
 import exceptions.ParserException;
 import io.Parser;
 import io.Ui;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.NoSuchElementException;
 import storage.Storage;
-import tasks.Deadline;
-import tasks.Event;
 import tasks.Task;
 import tasks.TaskList;
 import tasks.Todo;
