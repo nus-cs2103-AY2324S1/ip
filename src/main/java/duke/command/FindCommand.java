6f2a1e7edb47cf60479419a4ae19aade8f7dc48d
Saumya Shah <68504323+LordSaumya@users.noreply.github.com>
2023-09-02 16:56:46 +0800

    Add JavaDoc to FindCommand class

--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -3,14 +3,28 @@ package duke.command;
 import duke.task.TaskList;
 import duke.ui.Ui;
 
+/**
+ * Represents a command to find tasks that match a search term.
+ */
 public class FindCommand extends Command {
     String searchTerm;
 
+    /**
+     * Initialises the find command with a search term.
+     * 
+     * @param searchTerm The search term to find tasks with.
+     */
     public FindCommand(String searchTerm) {
         super(CommandType.FIND);
         this.searchTerm = searchTerm;
     }
 
+    /**
+     * Executes the find command.
+     * 
+     * @param tasks The task list to find tasks from.
+     * @param ui The ui to display the matching tasks.
+     */
     public void execute(TaskList tasks, Ui ui) {
         TaskList matchingTasks = tasks.findTasks(searchTerm);
         ui.listTasks(matchingTasks, true);
