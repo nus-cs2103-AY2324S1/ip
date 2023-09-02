9fb72fce4f439436387e9e657f91b82155cee7c1
LIN <peterway6933@gmail.com>
2023-09-02 15:33:46 +0800

    print Dates and Times, print Task on given Date

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,38 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
+import java.time.Month;
+import java.time.LocalDate;
+
 public class Deadline extends Task{
     protected String by;
 
-    public Deadline(String description, String by) throws AlexException {
+    protected LocalDateTime parsedDateTime;
+
+    public Deadline(String description, String by) throws AlexException, DateTimeParseException {
         super(description);
         this.by = by;
+        this.parsedDateTime = LocalDateTime.parse(by, UserInputStorage.TIMEFORMATTER);
     }
 
     public String getBy() {
         return this.by;
     }
 
+    public LocalDate getDueDate() {
+        String dateString = this.by.substring(0, 10);
+        LocalDate date = LocalDate.parse(dateString);
+        return date;
+    }
+
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        int hour = parsedDateTime.getHour();
+        int minute = parsedDateTime.getMinute();
+        Month month = parsedDateTime.getMonth();
+        int day = parsedDateTime.getDayOfMonth();
+        int year = parsedDateTime.getYear();
+        return "[D]" + super.toString()
+                + " (by: " + hour +":"+ minute + " " + day + " " + month + " " + year + ")";
     }
 
 }
