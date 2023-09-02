c7764ce4c1016a31a4c02be343a2ff2cfbf9828e
nananakx-x <nanettekx.x3020@gmail.com>
2023-09-03 01:00:46 +0800

    Level 8. Dates and Times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,8 +1,12 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 /**
  * Represents a deadline task that has a description and a due date/time.
  */
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDateTime by;
 
     /**
      * Constructor with description and due date/time.
@@ -12,7 +16,23 @@ public class Deadline extends Task {
      */
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = parseDateTime(by);
+    }
+
+    // Convert string by into LocalDateTime by
+    private LocalDateTime parseDateTime(String dateTimeString) {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+            return LocalDateTime.parse(dateTimeString, formatter);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("☹ OOPS!!! Invalid date/time format. Please use dd/MM/yyyy HHmm");
+        }
+    }
+
+    // Format LocalDateTime by as a string
+    private String formatDateTime(LocalDateTime dateTime) {
+        //  "a" represents the AM/PM marker
+        return dateTime.format(DateTimeFormatter.ofPattern("dd MMM yyyy h:mm a"));
     }
 
     /**
@@ -22,6 +42,6 @@ public class Deadline extends Task {
      */
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + formatDateTime(by) + ")";
     }
 }
