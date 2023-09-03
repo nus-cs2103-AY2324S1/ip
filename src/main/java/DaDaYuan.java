5313878f6616fa7485883eac96dff15dd494ef55
DESKTOP-ITF4GUD\94866 <e0772448@u.nus.edu>
2023-09-03 10:15:03 +0800

    Echo

--- src/main/java/DaDaYuan.java
+++ src/main/java/DaDaYuan.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
+
 public class DaDaYuan {
+
     public static void main(String[] args) {
-        String logo = "Hello! I'm DaDaYuan"
-                + "What can I do for you?"
-                + "Bye. Hope to see you again soon!";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println("Hello! I'm DaDaYuan");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+
+        while (true) {
+            String msg = scanner.nextLine();
+
+            if (msg.equals("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+        }
+
+        scanner.close();
     }
 }
+
