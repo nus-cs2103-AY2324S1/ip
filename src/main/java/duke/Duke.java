8477d058b8732f9d4dea9710c954e08762cb5847
itssisi <e0969060@u.nus.edu>
2023-09-03 20:31:19 +0800

    Add a GUI implemented by JavaFX technology to the chatbot

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,15 +1,18 @@
 package duke;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Duke {
+    private final String FILE_PATH = "data/duke.txt";
     private Storage storage;
     private TaskList inputList;
     private Ui ui;
 
-    public Duke(String filePath) {
+    public Duke() {
         ui = new Ui();
-        storage = new Storage(filePath);
+        storage = new Storage(FILE_PATH);
         try {
             inputList = new TaskList(storage.load());
         } catch (Exception e) {
@@ -35,6 +38,28 @@ public class Duke {
     }
 
     public static void main(String[] args) {
-        new Duke("data/duke.txt").run();
+        new Duke().run();
+    }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    String getResponse(String input) {
+        // Create a stream to hold the output
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        // IMPORTANT: Save the old System.out!
+        PrintStream old = System.out;
+        // Tell Java to use your special stream
+        System.setOut(ps);
+        Parser.command(input, inputList);
+        storage.createFile(inputList);
+        // Put things back
+        System.out.flush();
+        System.setOut(old);
+        // Show what happened
+        System.out.println("Here: " + baos.toString());
+        return baos.toString();
     }
 }
