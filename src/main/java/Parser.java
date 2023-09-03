bf2270b0041b13f53b1ca261883cf04905575e43
Sebastian Tay <taysebastian95@gmail.com>
2023-09-03 13:03:26 +0800

    Fix coding standard for Parser class

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -55,7 +55,7 @@ public class Parser {
      * Returns the index of the task given in the command line argument args.
      *
      * @param args
-     * @return
+     * @return an integer corresponding to the index of the task that we want.
      * @throws NumberFormatException
      * @throws NoDescriptionException when the user did not input any additional arguments.
      * @throws ExcessiveArgumentException when the user input more arguments than required.
@@ -81,7 +81,7 @@ public class Parser {
      * Returns a new task based on the command line argument args.
      *
      * @param args
-     * @return
+     * @return a Task from the given args.
      * @throws NoDescriptionException
      */
     public static Task getTask(String args) throws NoDescriptionException {
@@ -91,50 +91,52 @@ public class Parser {
         String[] descriptions = null; //For multiple arguments
 
         switch (type) {
-            case "todo":
-                description = args.replaceFirst("todo ", "");
+        case "todo":
+            description = args.replaceFirst("todo ", "");
 
-                if (description.toLowerCase() == type) {
-                    throw new NoDescriptionException("");
-                }
+            if (description.toLowerCase() == type) {
+                throw new NoDescriptionException("");
+            }
 
-                newTask = new ToDo(description);
-                break;
+            newTask = new ToDo(description);
+            break;
 
-            case "deadline":
-                //Expected CL input: deadline <Description> /by <Due date in dd/MM/yyyy HHmm>
-                //TODO add error handling for no "/by" keyword
-                if (description.toLowerCase() == type) {
-                    throw new NoDescriptionException("Please input the right order: deadline <Description> /by <due date>");
-                }
+        case "deadline":
+            //Expected CL input: deadline <Description> /by <Due date in dd/MM/yyyy HHmm>
+            //TODO add error handling for no "/by" keyword
+            if (description.toLowerCase() == type) {
+                throw new NoDescriptionException(
+                        "Please input the right order: deadline <Description> /by <due date>");
+            }
 
-                description = args.replaceFirst("deadline ", "");
-                descriptions = description.split(" /by ");
+            description = args.replaceFirst("deadline ", "");
+            descriptions = description.split(" /by ");
 
-                if (descriptions.length < 2) {
-                    throw new IncorrectInputException("Please input the right order: deadline <Description> /by <due date>");
-                }
+            if (descriptions.length < 2) {
+                throw new IncorrectInputException(
+                        "Please input the right order: deadline <Description> /by <due date>");
+            }
 
-                newTask = new Deadline(descriptions[0], descriptions[1]);
-                break;
+            newTask = new Deadline(descriptions[0], descriptions[1]);
+            break;
 
-            case "event":
-                //Expected CL input: event <Description> /from <start> /to <end>
-                if (description.toLowerCase() == type) {
-                    throw new NoDescriptionException("");
-                }
+        case "event":
+            //Expected CL input: event <Description> /from <start> /to <end>
+            if (description.toLowerCase() == type) {
+                throw new NoDescriptionException("");
+            }
 
-                description = args.replaceFirst("event ", ""); //Remove type
+            description = args.replaceFirst("event ", ""); //Remove type
 
-                descriptions = description.split(" /from "); //Split remaining args into description + (from and to)
-                String from = descriptions[1].split(" /to ")[0]; //Split (from and to) into from and to
-                String to = descriptions[1].split(" /to ")[1];
+            descriptions = description.split(" /from "); //Split remaining args into description + (from and to)
+            String from = descriptions[1].split(" /to ")[0]; //Split (from and to) into from and to
+            String to = descriptions[1].split(" /to ")[1];
 
-                newTask = new Event(descriptions[0], from, to);
-                break;
+            newTask = new Event(descriptions[0], from, to);
+            break;
 
-            default:
-                break;
+        default:
+            break;
         }
 
         return newTask;
