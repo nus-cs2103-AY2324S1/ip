a6493ec72b897c74e4d9b2c8460f038d8581447a
LIN <peterway6933@gmail.com>
2023-09-03 15:18:18 +0800

    create a JAR File

--- src/test/java/Alex/ParserTest.java
+++ src/test/java/Alex/ParserTest.java
@@ -2,6 +2,7 @@ package Alex;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ParserTest {
     @Test
     public void parseTest() {
