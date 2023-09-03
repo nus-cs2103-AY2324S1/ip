d674becaa971a3962c0ada8e09a3fdaf1c9e8ed8
elhy1999 <ernestliu64@gmail.com>
2023-09-03 17:07:24 +0800

    Create FindCommand class

---
+++ src/main/java/taskmate/commands/FindCommand.java
@@ -0,0 +1,29 @@
+package taskmate.commands;
+
+import taskmate.tools.Storage;
+import taskmate.tools.TaskList;
+import taskmate.tools.Ui;
+import taskmate.tools.tasks.Task;
+
+import java.util.ArrayList;
+
+public class FindCommand extends Command {
+
+    String query;
+
+    public FindCommand(String query) {
+        this.query = query.toLowerCase();
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        ArrayList<Task> matchingTasks = new ArrayList<>();
+        for (Task t : tasks.getAllTasks()) {
+            boolean isMatchingTask = t.toString().toLowerCase().contains(this.query);
+            if (isMatchingTask) {
+                matchingTasks.add(t);
+            }
+        }
+        ui.printMatchingTasks(matchingTasks);
+    }
+}
