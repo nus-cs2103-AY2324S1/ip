d91d9263a6092eea613cac96d8363f290672c644
siqirua <lsq6918086@gmail.com>
2023-09-03 16:22:50 +0800

    Create a GUI for this project

---
+++ src/main/java/rua/Launcher.java
@@ -0,0 +1,12 @@
+package rua;
+
+import javafx.application.Application;
+
+/**
+ * A launcher class to workaround classpath issues.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Rua.class, args);
+    }
+}
