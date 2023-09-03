3d353477d799aed148ca8aa3cb6c17cfd40bb0fc
felibunnyy <felibunbun13@gmail.com>
2023-09-04 02:47:20 +0800

    modify logic for Duke, add class Todo Deadline Event that extends Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,31 +13,50 @@ public class Duke {
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 isRunning = false;
-                System.out.println("____________________________________________________________");
             } else if (userInput.equals("list")){
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(i + 1 + " .[" + tasks[i].getStatusIcon() + "] " + tasks[i]);
+                    System.out.println(i + 1 + "." + tasks[i]);
                 }
-                System.out.println("____________________________________________________________");
             } else if (userInput.startsWith("mark ")) {
                 System.out.println("Nice! I've marked this task as done:");
                 int taskIndex = Integer.parseInt(userInput.substring(5)) - 1;
                 tasks[taskIndex].switchCheck();
-                System.out.println("[" + tasks[taskIndex].getStatusIcon() + "] " + tasks[taskIndex]);
-                System.out.println("____________________________________________________________");
+                System.out.println(tasks[taskIndex].toString());
             } else if (userInput.startsWith("unmark ")) {
                 System.out.println("OK, I've marked this task as not done yet:");
                 int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
                 tasks[taskIndex].switchCheck();
-                System.out.println("[" + tasks[taskIndex].getStatusIcon() + "] " + tasks[taskIndex]);
-                System.out.println("____________________________________________________________");
-            } else {
-                System.out.println("added:" + userInput);
-                tasks[taskCount] = new Task(userInput);
+                System.out.println(tasks[taskIndex].toString());
+            } else if (userInput.startsWith("todo ")) {
+                Task newToDo = new Todo(userInput.substring(5));
+                System.out.println("Got it. I've added this task:");
+                tasks[taskCount] = new Todo(userInput);
                 taskCount++;
-                System.out.println("____________________________________________________________");
+                System.out.println(newToDo.toString());
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+            } else if (userInput.startsWith("deadline ")) {
+                System.out.println("Got it. I've added this task:");
+                String description = userInput.substring(9, userInput.indexOf("/by")).trim();
+                String by = userInput.substring(userInput.indexOf("/by") + 4).trim();
+                Task newDeadline = new Deadline(description, by);
+                tasks[taskCount] = newDeadline;
+                taskCount++;
+                System.out.println(newDeadline.toString());
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+            } else if (userInput.startsWith("event ")) {
+                System.out.println("Got it. I've added this task:");
+                String description = userInput.substring(6, userInput.indexOf("/from")).trim();
+                String from = userInput.substring(userInput.indexOf("/from") + 6, userInput.indexOf("/to")).trim();
+                String to = userInput.substring(userInput.indexOf("/to") + 4).trim();
+                Task newEvent = new Event(description, from, to);
+                tasks[taskCount] = newEvent;
+                taskCount++;
+                System.out.println(newEvent.toString());
+            } else {
+                System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
+            System.out.println("____________________________________________________________");
         }
         scanner.close();
     }
