1df1cc26d27ad7e4a1673d1dbbfd1a51e06afcef
Chen-Kuei <rubychen0427@gmail.com>
2023-09-03 15:32:10 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,26 @@
 import java.util.Scanner;
 public class Duke {
+    private static final int MAX_TASKS = 100;
+    private static String[] tasks = new String[MAX_TASKS];
+    private static int taskCount= 0;
+    private static void addTask(String task) {
+        if (taskCount < MAX_TASKS) {
+            tasks[taskCount++]  = task;
+            System.out.println("added: " + task);
+        } else {
+            System.out.println("Sorry, the task list is full.");
+        }
+    }
+    private static void listTasks() {
+        if (taskCount == 0) {
+            System.out.println("The task list is empty!");
+        } else {
+            for(int i = 0; i < taskCount; i++) {
+                System.out.println((i + 1) + ". " + tasks[i]);
+            }
+        }
+    }
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
         String greeting = "Hi, I'm BiuBiu.\nWhat can I do for you?";
         System.out.println(greeting);
         String exit = "Bye. Have a great day!";
@@ -16,8 +31,11 @@ public class Duke {
             if(userCommand.equalsIgnoreCase("bye")) {
                 System.out.println(exit);
                 break;
-            } else {
-                System.out.println(userCommand);
+            } else if (userCommand.equalsIgnoreCase("list")) {
+                listTasks();
+            }
+            else {
+                addTask(userCommand);
             }
         }
     }
