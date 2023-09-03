58c0bf1ac38ee7ef56a30f80b43a77be7c60b4ce
Timothy Putra Prasetio <timothypp02@gmail.com>
2023-09-03 16:50:30 +0800

    Add new Command FIND

--- src/main/java/anya/command/Command.java
+++ src/main/java/anya/command/Command.java
@@ -1,5 +1,5 @@
 package anya.command;
 
 public enum Command {
-    BYE, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, UNKNOWN
+    BYE, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, FIND, UNKNOWN
 }
