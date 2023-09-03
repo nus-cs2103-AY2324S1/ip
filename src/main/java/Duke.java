a811c91cdb0a07f85ba12c565fff9720f0336950
Chen-Kuei <rubychen0427@gmail.com>
2023-09-03 15:19:26 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -6,7 +7,18 @@ public class Duke {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
         String greeting = "Hi, I'm BiuBiu.\nWhat can I do for you?";
+        System.out.println(greeting);
         String exit = "Bye. Have a great day!";
-        System.out.println(greeting + "\n" + exit);
+
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String userCommand = scanner.nextLine();
+            if(userCommand.equalsIgnoreCase("bye")) {
+                System.out.println(exit);
+                break;
+            } else {
+                System.out.println(userCommand);
+            }
+        }
     }
 }
