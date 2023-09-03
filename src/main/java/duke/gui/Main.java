8c25f0c5ac1d89837d608176d0a14166558910de
imkwokyong <darrenkuan@gmail.com>
2023-09-03 16:40:39 +0800

    Add DialogBox.java, Main.java, MainWindow.java

---
+++ src/main/java/duke/gui/Main.java
@@ -0,0 +1,34 @@
+package duke.gui;
+
+import java.io.IOException;
+
+import duke.Duke;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * Main class that connects the GUI to Duke.
+ */
+public class Main extends Application {
+
+    private final Duke duke = new Duke();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setTitle("ForsakenX - An interactive Duke chatbot");
+            stage.setResizable(false);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
