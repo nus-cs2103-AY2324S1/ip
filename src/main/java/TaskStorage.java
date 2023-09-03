5657715258dcfc0af7b467dddf3b7f5411612cb7
lordidiot <0xlordidiot@gmail.com>
2023-09-03 17:30:27 +0800

    Fix wildcard import

--- src/main/java/TaskStorage.java
+++ src/main/java/TaskStorage.java
@@ -1,4 +1,8 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
