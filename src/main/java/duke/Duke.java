ffabe4470ce120bf6b3fc3bd9995e82357d9ffa5
SynapseProgramming <roald.ong@gmail.com>
2023-09-03 20:43:37 +0800

    Add in find function

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -3,6 +3,8 @@ package duke;
 import exceptions.ParserException;
 import io.Parser;
 import io.Ui;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 import storage.Storage;
 import tasks.Task;
@@ -11,170 +13,189 @@ import tasks.Todo;
 
 public class Duke {
 
-  private TaskList taskList;
-  private Parser parser;
-  private Ui ui;
-  private Storage storage;
+    private TaskList taskList;
+    private Parser parser;
+    private Ui ui;
+    private Storage storage;
 
-  public Duke() {
-    this.taskList = new TaskList();
-    this.parser = new Parser();
-    this.ui = new Ui();
-    this.storage = new Storage(this.taskList);
-  }
-
-
-  public void listTasks() {
-
-    if (taskList.isEmpty()) {
-      System.out.println("list is empty!");
-      return;
-    }
-
-    for (int i = 0; i < taskList.size(); i++) {
-
-      String index = Integer.toString(i + 1);
-      Task selectedTask = taskList.get(i);
-      System.out.println(index + " " + ui.displayTask(selectedTask));
-
-    }
-  }
-
-  public void unmarkTask() {
-    try {
-      // set current task as un-done
-      Task selectedTask = taskList.get(parser.getIndex());
-      selectedTask.setUnDone();
-      ui.displayAction("Marked selected task as un-done desu", selectedTask);
-    } catch (IndexOutOfBoundsException ex) {
-      System.out.println("Please enter a valid index!");
-    }
-  }
-
-  public void markTaskAsDone() {
-    try {
-      // set current task as done
-      Task selectedTask = taskList.get(parser.getIndex());
-      selectedTask.setDone();
-      ui.displayAction("Marked selected task as done", selectedTask);
-    } catch (IndexOutOfBoundsException ex) {
-      System.out.println("Please enter a valid index!");
-    }
-  }
-
-  public void addTodo() {
-    try {
-      Task curentTask = new Todo(parser.getTaskName());
-      taskList.add(curentTask);
-      System.out.println("added:\t" + ui.displayTask(curentTask));
-    } catch (StringIndexOutOfBoundsException ex) {
-
-      System.out.println("Please enter a name after the todo command!");
-    }
-  }
-
-  public void addDeadline() {
-
-    try {
-      Task curentTask = parser.parseDeadline();
-      taskList.add(curentTask);
-      System.out.println("added:\t" + ui.displayTask(curentTask));
-    } catch (ParserException ex) {
-      System.out.println(ex.getMessage());
+    public Duke() {
+        this.taskList = new TaskList();
+        this.parser = new Parser();
+        this.ui = new Ui();
+        this.storage = new Storage(this.taskList);
     }
 
 
-  }
+    public void listTasks() {
 
-  public void addEvent() {
-    try {
-      Task curentTask = parser.parseEvent();
-      taskList.add(curentTask);
-      System.out.println("added:\t" + ui.displayTask(curentTask));
-    } catch (ParserException ex) {
-      System.out.println(ex.getMessage());
-    }
-  }
-
-  public void deleteTask() {
-    if (taskList.isEmpty()) {
-      System.out.println("The list is empty!");
-      return;
-    }
-    try {
-      // remove the current task
-      Task selectedTask = taskList.get(parser.getIndex());
-      taskList.remove(parser.getIndex());
-      ui.displayAction("Deleting selected task!", selectedTask);
-    } catch (IndexOutOfBoundsException ex) {
-      System.out.println("Please enter a valid index!");
-    }
-  }
-
-  public void run() {
-
-    ui.displayGreetings();
-    storage.loadTasks();
-
-    label:
-    while (true) {
-
-      try {
-        parser.update();
-      } catch (NoSuchElementException ex) {
-        break;
-      }
-
-      // there is no input
-      if (parser.isInputThere()) {
-        break;
-      }
-
-      switch (parser.getCommandString()) {
-        case "bye":
-          break label;
-        case "list":
-          listTasks();
-          break;
-        case "mark": {
-          markTaskAsDone();
-          break;
+        if (taskList.isEmpty()) {
+            System.out.println("list is empty!");
+            return;
         }
-        case "unmark": {
-          unmarkTask();
-          break;
+
+        for (int i = 0; i < taskList.size(); i++) {
+
+            String index = Integer.toString(i + 1);
+            Task selectedTask = taskList.get(i);
+            System.out.println(index + " " + ui.displayTask(selectedTask));
+
         }
-        case "todo": {
-          addTodo();
-          break;
-        }
-        case "deadline": {
-          addDeadline();
-          break;
-        }
-        case "event": {
-          addEvent();
-          break;
-        }
-        case "delete": {
-          deleteTask();
-          break;
-        }
-        default:
-          System.out.println("Please enter a suitable task!");
-      }
     }
 
-    ui.displayGoodbye();
-    storage.saveTasks();
+    public void unmarkTask() {
+        try {
+            // set current task as un-done
+            Task selectedTask = taskList.get(parser.getIndex());
+            selectedTask.setUnDone();
+            ui.displayAction("Marked selected task as un-done desu", selectedTask);
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Please enter a valid index!");
+        }
+    }
+
+    public void markTaskAsDone() {
+        try {
+            // set current task as done
+            Task selectedTask = taskList.get(parser.getIndex());
+            selectedTask.setDone();
+            ui.displayAction("Marked selected task as done", selectedTask);
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Please enter a valid index!");
+        }
+    }
+
+    public void addTodo() {
+        try {
+            Task curentTask = new Todo(parser.getTaskName());
+            taskList.add(curentTask);
+            System.out.println("added:\t" + ui.displayTask(curentTask));
+        } catch (StringIndexOutOfBoundsException ex) {
+
+            System.out.println("Please enter a name after the todo command!");
+        }
+    }
+
+    public void addDeadline() {
+
+        try {
+            Task curentTask = parser.parseDeadline();
+            taskList.add(curentTask);
+            System.out.println("added:\t" + ui.displayTask(curentTask));
+        } catch (ParserException ex) {
+            System.out.println(ex.getMessage());
+        }
 
 
-  }
+    }
 
-  public static void main(String[] args) {
+    public void addEvent() {
+        try {
+            Task curentTask = parser.parseEvent();
+            taskList.add(curentTask);
+            System.out.println("added:\t" + ui.displayTask(curentTask));
+        } catch (ParserException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 
-    Duke duke = new Duke();
-    duke.run();
+    public void deleteTask() {
+        if (taskList.isEmpty()) {
+            System.out.println("The list is empty!");
+            return;
+        }
+        try {
+            // remove the current task
+            Task selectedTask = taskList.get(parser.getIndex());
+            taskList.remove(parser.getIndex());
+            ui.displayAction("Deleting selected task!", selectedTask);
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Please enter a valid index!");
+        }
+    }
 
-  }
+    public void findTask() {
+        System.out.println("finding task!");
+        String name = parser.getTaskName();
+        List<Task> tasks = taskList.findTasks(name);
+
+        System.out.println("Found:");
+        for (int i = 0; i < tasks.size(); i++) {
+
+            String index = Integer.toString(i + 1);
+            Task selectedTask = tasks.get(i);
+            System.out.println(index + " " + ui.displayTask(selectedTask));
+
+        }
+    }
+
+    public void run() {
+
+        ui.displayGreetings();
+        storage.loadTasks();
+
+        label:
+        while (true) {
+
+            try {
+                parser.update();
+            } catch (NoSuchElementException ex) {
+                break;
+            }
+
+            // there is no input
+            if (parser.isInputThere()) {
+                break;
+            }
+
+            switch (parser.getCommandString()) {
+                case "bye":
+                    break label;
+                case "list":
+                    listTasks();
+                    break;
+                case "mark": {
+                    markTaskAsDone();
+                    break;
+                }
+                case "unmark": {
+                    unmarkTask();
+                    break;
+                }
+                case "todo": {
+                    addTodo();
+                    break;
+                }
+                case "deadline": {
+                    addDeadline();
+                    break;
+                }
+                case "event": {
+                    addEvent();
+                    break;
+                }
+                case "delete": {
+                    deleteTask();
+                    break;
+                }
+                case "find": {
+                    findTask();
+                    break;
+                }
+                default:
+                    System.out.println("Please enter a suitable task!");
+            }
+        }
+
+        ui.displayGoodbye();
+        storage.saveTasks();
+
+
+    }
+
+    public static void main(String[] args) {
+
+        Duke duke = new Duke();
+        duke.run();
+
+    }
 }
--- src/main/java/tasks/TaskList.java
+++ src/main/java/tasks/TaskList.java
@@ -1,44 +1,56 @@
 package tasks;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import tasks.Task;
 
 
 public class TaskList {
 
-  public TaskList() {
-    tasks = new ArrayList<>();
-  }
+    public TaskList() {
+        tasks = new ArrayList<>();
+    }
 
-  @JsonIgnore
-  public int size() {
-    return tasks.size();
-  }
+    @JsonIgnore
+    public List<Task> findTasks(String description) {
+        List<Task> output;
+        output = tasks.stream().filter(task -> task.getDescription().contains(description)).collect(
+            Collectors.toList());
 
+        return output;
+    }
 
-  @JsonIgnore
-  public boolean isEmpty() {
-    return tasks.isEmpty();
-  }
-
-
-  @JsonIgnore
-  public void add(Task task) {
-    tasks.add(task);
-  }
-
-  @JsonIgnore
-  public Task get(int index) {
-    return tasks.get(index);
-  }
-
-  @JsonIgnore
-  public void remove(int index) {
-    tasks.remove(index);
-  }
+    @JsonIgnore
+    public int size() {
+        return tasks.size();
+    }
 
 
-  private ArrayList<Task> tasks;
+    @JsonIgnore
+    public boolean isEmpty() {
+        return tasks.isEmpty();
+    }
+
+
+    @JsonIgnore
+    public void add(Task task) {
+        tasks.add(task);
+    }
+
+    @JsonIgnore
+    public Task get(int index) {
+        return tasks.get(index);
+    }
+
+    @JsonIgnore
+    public void remove(int index) {
+        tasks.remove(index);
+    }
+
+
+    private ArrayList<Task> tasks;
 
 }
