5c08f4a7a7fb73816eb8cbefcd55526ae40190d3
DESKTOP-ITF4GUD\94866 <e0772448@u.nus.edu>
2023-09-03 09:50:59 +0800

    Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
-public class Duke {
+public class DaDaYuan {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "Hello! I'm DaDaYuan"
+                + "What can I do for you?"
+                + "Bye. Hope to see you again soon!"
         System.out.println("Hello from\n" + logo);
     }
 }
