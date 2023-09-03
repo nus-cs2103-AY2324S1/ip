a9658eca5560a956c8f79f9ccf4886e87a822c4e
He Yifan <e0968838@u.nus.edu>
2023-09-03 20:04:20 +0800

    Add Gradle

--- src/main/java/bellcurvegod/storage/Storage.java
+++ src/main/java/bellcurvegod/storage/Storage.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Storage {
