5af46ed6a9a73dcbb35389e5b7121dbf81e0bda7
nicrandomlee <leejpnicholas@gmail.com>
2023-09-03 23:10:56 +0800

    javadoc for save

--- src/main/java/Save.java
+++ src/main/java/Save.java
@@ -10,6 +10,12 @@ public class Save {
     private String saveFilePath;
     private static final String tempFilePath = "temp.txt";
 
+    /**
+     * Constructor for Save File
+     *
+     * @param  taskList  a task list to store tasks
+     * @param  saveFilePath file path of saved file
+     */
     public Save(TaskList taskList, String saveFilePath) throws IOException {
         this.taskList = taskList;
         this.saveFilePath = saveFilePath;
