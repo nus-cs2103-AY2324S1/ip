b639c767b6f6a3544b6a2ca5c4842a4dbe53b9a6
Sebastian Tay <taysebastian95@gmail.com>
2023-09-03 12:27:26 +0800

    Complete javaDocs for VedaExceptions package

--- src/main/java/VedaExceptions/ExcessiveArgumentException.java
+++ src/main/java/VedaExceptions/ExcessiveArgumentException.java
@@ -1,5 +1,11 @@
 package VedaExceptions;
 
+/**
+ * ExcessiveArgumentException is a form of IncorrectInputException that is thrown when the user provides more arguments
+ * than the command requires.
+ *
+ * @author Sebastian Tay
+ */
 public class ExcessiveArgumentException extends IncorrectInputException {
     public ExcessiveArgumentException(String message) {
         super(message);
--- src/main/java/VedaExceptions/IncorrectInputException.java
+++ src/main/java/VedaExceptions/IncorrectInputException.java
@@ -1,5 +1,10 @@
 package VedaExceptions;
 
+/**
+ * IncorrectInputException is a form of RuntimeException that is thrown when the user provide an invalid input.
+ *
+ * @author Sebastian Tay
+ */
 public class IncorrectInputException extends RuntimeException {
     public IncorrectInputException(String message) {
         super(message);
