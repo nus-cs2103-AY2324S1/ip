6000e3fc636060df4c49fdc025a08d3c125a7325
SynapseProgramming <roald.ong@gmail.com>
2023-09-03 14:20:56 +0800

    Add in javadoc comments for  Ui class

--- src/main/java/io/Ui.java
+++ src/main/java/io/Ui.java
@@ -7,7 +7,9 @@ import tasks.Todo;
 
 public class Ui {
 
-
+  /**
+   * Displays the greeting message to the user
+   */
   public void displayGreetings() {
     String greeting = "Hello! I'm KimochiUsagi (きもち　うさぎ)!\n";
     String info = "Ask the bunny a question!\n";
@@ -16,19 +18,35 @@ public class Ui {
   }
 
 
+  /**
+   * Displays a message and the display of a given Task one after the other.
+   *
+   * @param message The message to be displayed.
+   * @param task    The Task object whose display will be shown.
+   */
   public void displayAction(String message, Task task) {
     System.out.println(message);
     System.out.println(this.displayTask(task));
 
   }
 
+
+  /**
+   * Displays the goodbye text.
+   */
   public void displayGoodbye() {
     String goodbye = "Bye. See you again! (またね)";
     System.out.println(goodbye);
 
   }
 
-
+  /**
+   * Generates and returns the appropriate display string for the given Task object, based on its
+   * subtype.
+   *
+   * @param task The Task object for which to generate the display.
+   * @return The display string representing the given task.
+   */
   public String displayTask(Task task) {
     String answer = "";
 
