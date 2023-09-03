cd24c1fe1e1902dbb27bde693bcc959c38ed20aa
saraozn <saraongzining@gmail.com>
2023-09-03 12:04:40 +0800

    added storageTest class to test the methods load and save

---
+++ src/test/java/duke/StorageTest.java
@@ -0,0 +1,17 @@
+package duke; //if packages are matched, the test class can access package-private members of the target class
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class StorageTest {
+
+//    @Test
+//    public void storageTest1() throws FileNotFoundException {
+//        Storage store = new Storage("storageTest.txt");
+//        TaskList list = new TaskList(store.load());
+//
+//        assertEquals("1.[T][ ] read book", list.getTask(1).toString());
+//    }
+}
