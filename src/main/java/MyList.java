32d61ff9d6bde2f2f01acefd7f7d6fd8a6786d84
Song_Bingxi <bingxi.bobby.song@gmail.com>
2023-09-03 14:08:17 +0800

    modified MyList.java

--- src/main/java/MyList.java
+++ src/main/java/MyList.java
@@ -11,7 +11,7 @@ public class MyList extends ArrayList<Task> {
     public String toString() {
         StringBuffer rt = new StringBuffer();
         for (int i = 0; i < this.size(); i++) {
-            rt.append("  " + (i+1) + ". " + this.get(i).toStringAll() + "\n");
+            rt.append("  " + (i+1) + ". " + this.get(i).toString() + "\n");
         }
         return rt.toString();
     }
