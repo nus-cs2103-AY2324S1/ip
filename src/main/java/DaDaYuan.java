e749d9b586f20808ffc6a4866af0986dd5756d4b
DESKTOP-ITF4GUD\94866 <e0772448@u.nus.edu>
2023-09-03 10:11:19 +0800

    Correct missing colon

--- src/main/java/DaDaYuan.java
+++ src/main/java/DaDaYuan.java
@@ -2,7 +2,7 @@ public class DaDaYuan {
     public static void main(String[] args) {
         String logo = "Hello! I'm DaDaYuan"
                 + "What can I do for you?"
-                + "Bye. Hope to see you again soon!"
+                + "Bye. Hope to see you again soon!";
         System.out.println("Hello from\n" + logo);
     }
 }
