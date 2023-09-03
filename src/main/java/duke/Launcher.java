5853f59eb30e484eb4a374b1c3f9793bdc06a184
imkwokyong <darrenkuan@gmail.com>
2023-09-03 17:08:52 +0800

    Modify font style in FXML files

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -1,7 +1,11 @@
-package duke.gui;
+package duke;
 
+import duke.gui.Main;
 import javafx.application.Application;
 
+/**
+ * Launches the GUI.
+ */
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(Main.class, args);
