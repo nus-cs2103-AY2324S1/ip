a02cf9fe8f8176d6998348672f31d1c2af3ac3b6
felibunnyy <felibunbun13@gmail.com>
2023-09-04 01:42:51 +0800

    Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,30 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        boolean isRunning = true;
+        int taskCount = 0;
+        String[] tasks =  new String[100];
         System.out.println("Hello! I'm Chatty\nWhat can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+        while (isRunning) {
+            String userInput = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                isRunning = false;
+                System.out.println("____________________________________________________________");
+            } else if (userInput.equals("list")){
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(i + 1 + " . " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                System.out.println("added:" + userInput);
+                tasks[taskCount] = userInput;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+            }
+        }
     }
 }
