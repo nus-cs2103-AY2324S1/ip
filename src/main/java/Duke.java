8278b9078374aefed8d44203ae0ebab38826b7da
lordidiot <0xlordidiot@gmail.com>
2023-09-03 16:35:57 +0800

    Refactor and introduce parser class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,5 @@
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Scanner;
 
 public class Duke {
@@ -32,7 +27,8 @@ public class Duke {
             String userInput = scanner.nextLine();
 
             try {
-                running = Duke.handleInput(userInput, tasks);
+                Parser parser = Parser.from(userInput);
+                running = Duke.handleInput(parser, tasks);
                 taskStorage.storeTasks(tasks);
             } catch (DukeException e) {
                 System.out.printf("[!] %s\n", e.getMessage());
@@ -44,90 +40,76 @@ public class Duke {
         System.out.println("____________________________________________________________");
     }
 
-    private static boolean handleInput(String userInput, ArrayList<Task> tasks) throws DukeException {
-        if (userInput.equals("bye")) {
+    private static boolean handleInput(Parser parser, ArrayList<Task> tasks) throws DukeException {
+        switch (parser.getCommand()) {
+        case "bye":
             return false;
-        } else if (userInput.equals("list")) {
+        case "list":
             System.out.println("Pong: ");
             for (int i = 0; i < tasks.size(); i++) {
                 System.out.printf("    %d. %s\n", i+1, tasks.get(i));
             }
-        } else if (userInput.startsWith("mark")) {
-            String userIndex = userInput.substring(5);
-            int index = Integer.parseInt(userIndex) - 1;
+            break;
+        case "mark":
+            int index = parser.getArgAsInt() - 1;
             tasks.get(index).markDone();
             System.out.println("Pong: I've marked this task as done.");
             System.out.printf("    %s\n", tasks.get(index));
-        } else if (userInput.startsWith("unmark")) {
-            String userIndex = userInput.substring(7);
-            int index = Integer.parseInt(userIndex) - 1;
+            break;
+        case "unmark":
+            index = parser.getArgAsInt() - 1;
             tasks.get(index).unmarkDone();
             System.out.println("Pong: I've marked this task as not done.");
             System.out.printf("    %s\n", tasks.get(index));
-        } else if (userInput.startsWith("todo")) {
-            try {
-                tasks.add(new Todo(userInput.substring(5)));
-            } catch (StringIndexOutOfBoundsException e) {
-                throw new DukeException("Todo name cannot be empty.");
-            }
-            System.out.printf("Pong: [Added] %s\n", tasks.get(tasks.size() - 1));
-        } else if (userInput.startsWith("deadline")) {
-            String[] tokens;
-            LocalDateTime deadline;
-            try {
-                tokens = userInput.substring(9).split(" /by ");
-                 deadline = handleDateTime(tokens[1]);
-            } catch (StringIndexOutOfBoundsException e) {
-                throw new DukeException("Deadline name cannot be empty.");
-            } catch (ArrayIndexOutOfBoundsException e) {
-                throw new DukeException("Use /by to specify deadline date.");
-            } catch (ParseException e) {
-                throw new DukeException("Invalid DateTime format: " + e.getMessage());
-            }
-
-            tasks.add(new Deadline(tokens[0], deadline));
-            System.out.printf("Pong: [Added] %s\n", tasks.get(tasks.size() - 1));
-        } else if (userInput.startsWith("event")) {
-            String[] tokens;
-            LocalDateTime from, to;
-            try {
-                tokens = userInput.substring(6).split(" /from ");
-                String[] duration = tokens[1].split(" /to ");
-                from = handleDateTime(duration[0]);
-                to = handleDateTime(duration[1]);
-            } catch (StringIndexOutOfBoundsException e) {
-                throw new DukeException("Event name cannot be empty.");
-            } catch (ArrayIndexOutOfBoundsException e) {
-                throw new DukeException("Use /from and /to to specify event duration.");
-            } catch (ParseException e) {
-                throw new DukeException("Invalid DateTime format: " + e.getMessage());
-            }
-
-            tasks.add(new Event(tokens[0], from, to));
-            System.out.printf("Pong: [Added] %s\n", tasks.get(tasks.size() - 1));
-        } else if (userInput.startsWith("delete")) {
-            int deleteIndex = Integer.parseInt(userInput.substring(7));
-            Task task = tasks.remove(deleteIndex - 1);
+            break;
+        case "delete":
+            index = parser.getArgAsInt() - 1;
+            Task task = tasks.remove(index - 1);
             System.out.printf("Pong: [Deleted] %s\n", task);
+            break;
+        case "todo":
+            String todoName = parser.getArg();
+            if (todoName == null || todoName.equals("")) {
+                throw new DukeException("Todo name cannot be empty");
+            }
+            tasks.add(new Todo(todoName));
+            System.out.printf("Pong: [Added] %s\n", tasks.get(tasks.size() - 1));
+            break;
+        case "deadline":
+            String deadlineName = parser.getArg();
+            if (deadlineName == null || deadlineName.equals("")) {
+                throw new DukeException("Deadline name cannot be empty");
+            }
+
+            LocalDateTime deadline;
+            deadline = parser.getOptArgAsDateTime("by");
+
+            if (deadline == null) {
+                throw new DukeException("Use /by to specify deadline date");
+            }
+
+            tasks.add(new Deadline(deadlineName, deadline));
+            System.out.printf("Pong: [Added] %s\n", tasks.get(tasks.size() - 1));
+            break;
+        case "event":
+            String eventName = parser.getArg();
+            if (eventName == null || eventName.equals("")) {
+                throw new DukeException("Deadline name cannot be empty");
+            }
+
+            LocalDateTime from, to;
+            from = parser.getOptArgAsDateTime("from");
+            to = parser.getOptArgAsDateTime("to");
+
+            if (from == null || to == null) {
+                throw new DukeException("Use /from and /to to specify event duration");
+            }
+
+            tasks.add(new Event(eventName, from, to));
+            System.out.printf("Pong: [Added] %s\n", tasks.get(tasks.size() - 1));
+            break;
         }
 
         return true;
-    }
-
-    private static LocalDateTime handleDateTime(String dateTime) throws ParseException {
-        SimpleDateFormat dateFormat;
-
-        if (dateTime.length() == 10) {
-            dateFormat = new SimpleDateFormat("yyyy/MM/dd");
-        } else if (dateTime.length() == 15) {
-            dateFormat = new SimpleDateFormat("yyyy/MM/dd HHmm");
-        } else {
-            throw new ParseException("Invalid length", 0);
-        }
-
-        Date parsedDate = dateFormat.parse(dateTime);
-        Instant instant = parsedDate.toInstant();
-        LocalDateTime localDateTime = instant.atZone(ZoneId.systemDefault()).toLocalDateTime();
-        return localDateTime;
     }
 }
---
+++ src/main/java/Parser.java
@@ -0,0 +1,155 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+
+public class Parser {
+    private static final String DELIMITER_REGEX = " (?=/\\w+)";
+
+    private final String command;
+    private final String argument;
+
+    private final HashMap<String, String> optionalArgs;
+
+    private Parser(String command) {
+        this.command = command;
+        this.argument = null;
+        this.optionalArgs = null;
+    }
+
+    private Parser(String command, String argument, HashMap<String, String> optionalArgs) {
+        this.command = command;
+        this.argument = argument;
+        this.optionalArgs = optionalArgs;
+    }
+
+    /**
+     * Parses line of input and extracts task type, name and optional args.
+     * @param line User input.
+     * @return Parser instance with parsed information.
+     * @throws DukeException If input does not match expected format.
+     */
+    public static Parser from(String line) throws DukeException {
+        String taskName, taskType;
+        HashMap<String, String> optionalArgs;
+
+        int firstSpace = line.indexOf(' ');
+
+        // No arguments (bye/list/...)
+        if (firstSpace == -1) {
+            return new Parser(line);
+        }
+
+        taskType = line.substring(0, firstSpace);
+        String remainder = line.substring(firstSpace + 1);
+        String[] parts = remainder.split(DELIMITER_REGEX);
+
+        if (parts.length < 1 || parts[0].equals("")) {
+            throw new DukeException("Invalid command. Expected format: <task type> <task name> [...].");
+        }
+
+        taskName = parts[0];
+        optionalArgs = new HashMap<>();
+        for (int i = 1; i < parts.length; i++) {
+            String k, v;
+            String part = parts[i];
+            int space = part.indexOf(' ');
+
+            if (space == -1) {
+                k = part.substring(1);
+                v = "";
+            } else {
+                k = part.substring(1, space);
+                v = part.substring(space + 1);
+            }
+
+            optionalArgs.put(k, v);
+        }
+
+        return new Parser(taskType, taskName, optionalArgs);
+    }
+
+    /**
+     * Get command.
+     * @return Command name.
+     */
+    public String getCommand() {
+        return this.command;
+    }
+
+    /**
+     * Get first argument.
+     * @return First argument.
+     */
+    public String getArg() {
+        return this.argument;
+    }
+
+    /**
+     * Get first argument as integer.
+     * @return First argument cast to integer.
+     * @throws DukeException If argument cannot be parsed to integer.
+     */
+    public int getArgAsInt() throws DukeException {
+        if (this.argument == null) {
+            throw new DukeException("No integer argument provided");
+        }
+
+        try {
+            return Integer.parseInt(this.argument);
+        } catch (NumberFormatException e) {
+            throw new DukeException(String.format("Invalid integer argument: %s", this.argument));
+        }
+    }
+
+    /**
+     * Get optional argument value.
+     * @param argName Name of argument to get.
+     * @return Value of optional argument or null if not present.
+     */
+    public String getOptArg(String argName) {
+        if (this.optionalArgs == null || !this.optionalArgs.containsKey(argName)) {
+            return null;
+        }
+
+        return this.optionalArgs.get(argName);
+    }
+
+    /**
+     * Get optional argument value as LocalDateTime.
+     * @param argName Name of argument to get.
+     * @return Value of optional argument as LocalDateTime or null if not present.
+     * @throws DukeException If argument value cannot be parsed as LocalDateTime.
+     */
+    public LocalDateTime getOptArgAsDateTime(String argName) throws DukeException {
+        String arg = getOptArg(argName);
+
+        if (arg == null) {
+            return null;
+        }
+
+        SimpleDateFormat dateFormat;
+
+        if (arg.length() == 10) {
+            dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        } else if (arg.length() == 15) {
+            dateFormat = new SimpleDateFormat("yyyy/MM/dd HHmm");
+        } else {
+            throw new DukeException(String.format("Invalid date (%s), should be yyyy/MM/dd HHmm", arg));
+        }
+
+        Date parsedDate;
+        try {
+            parsedDate = dateFormat.parse(arg);
+        } catch (ParseException e) {
+            throw new DukeException(String.format("Invalid date (%s), should be yyyy/MM/dd HHmm", arg));
+        }
+
+        return parsedDate
+                .toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+    }
+}
