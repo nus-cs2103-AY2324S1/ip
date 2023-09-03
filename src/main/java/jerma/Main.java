e6574bf31ef2cb85135618fed6baa390fc34bbaa
Jason <jiacan.h01@gmail.com>
2023-09-03 19:16:38 +0800

    Integrate fxml

---
+++ src/main/java/jerma/Main.java
@@ -0,0 +1,32 @@
+package jerma;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    private Jerma jerma = new Jerma();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(
+                    Main.class.getResource("/views/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setJerma(jerma);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
