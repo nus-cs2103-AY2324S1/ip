cca78110cad213fc7f25dc2140abfccca869b957
yyyaohhh <yyyaohhh@gmail.com>
2023-09-03 16:28:14 +0800

    Add A-JavaDoc

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -9,32 +9,50 @@ import tasks.Event;
 import tasks.ToDo;
 import tasks.Task;
 
+/**
+ * The Storage class is responsible for reading and writing tasks to/from a file.
+ * It provides methods to write a list of tasks to a file and load tasks from a file.
+ */
 public class Storage {
     protected String filePath;
+
+    /**
+     * Constructs a new Storage instance with the specified file path.
+     *
+     * @param filePath The path to the file where tasks will be stored.
+     */
     public Storage(String filePath) {
         this.filePath = filePath;
     }
+
+    /**
+     * Writes a list of tasks to the file specified in the constructor.
+     *
+     * @param listOfTasks The list of tasks to be written to the file.
+     * @throws IOException If an I/O error occurs while writing to the file.
+     */
     public void writeToFile(TaskList listOfTasks) throws IOException {
         FileWriter fw = new FileWriter(filePath);
-        for(int i = 0; i < listOfTasks.tasks.size(); i++) {
-            if(listOfTasks.tasks.get(i) instanceof Event) {
+        for (int i = 0; i < listOfTasks.tasks.size(); i++) {
+            // Check the type of each task and write it to the file with appropriate formatting
+            if (listOfTasks.tasks.get(i) instanceof Event) {
                 Event x = (Event) listOfTasks.tasks.get(i);
-                if(listOfTasks.tasks.get(i).getStatus()) {
+                if (listOfTasks.tasks.get(i).getStatus()) {
                     fw.write("E | 0 |" + listOfTasks.printName(i) + "|" + x.getStart() + "-" + x.getEnd() + "\n");
                 } else {
                     fw.write("E | 1 |" + listOfTasks.printName(i) + "|" + x.getStart() + "-" + x.getEnd() + "\n");
                 }
             }
-            if(listOfTasks.tasks.get(i) instanceof ToDo) {
-                if(listOfTasks.tasks.get(i).getStatus()) {
+            if (listOfTasks.tasks.get(i) instanceof ToDo) {
+                if (listOfTasks.tasks.get(i).getStatus()) {
                     fw.write("T | 0 |" + listOfTasks.printName(i) + "\n");
                 } else {
                     fw.write("T | 1 |" + listOfTasks.printName(i) + "\n");
                 }
             }
-            if(listOfTasks.tasks.get(i) instanceof Deadline) {
+            if (listOfTasks.tasks.get(i) instanceof Deadline) {
                 Deadline x = (Deadline) listOfTasks.tasks.get(i);
-                if(listOfTasks.tasks.get(i).getStatus()) {
+                if (listOfTasks.tasks.get(i).getStatus()) {
                     fw.write("D | 0 |" + listOfTasks.printName(i) + "| " + x.getBy() + "\n");
                 } else {
                     fw.write("D | 1 |" + listOfTasks.printName(i) + "| " + x.getBy() + "\n");
@@ -43,17 +61,24 @@ public class Storage {
         }
         fw.close();
     }
+
+    /**
+     * Loads tasks from the file specified in the constructor and returns them as an ArrayList.
+     *
+     * @return An ArrayList of tasks loaded from the file.
+     * @throws FileNotFoundException If the specified file is not found.
+     */
     public ArrayList<Task> load() throws FileNotFoundException {
-        File f = new File(filePath); // create a File for the given file path
-        Scanner s = new Scanner(f); // create a Scanner using the File as the source
+        File f = new File(filePath);
+        Scanner s = new Scanner(f);
         ArrayList<Task> tasks = new ArrayList<>();
         while (s.hasNext()) {
             String line = s.nextLine();
             String[] parts = line.split("\\|");
-            if(parts[0].equals("T ")) {
+            if (parts[0].equals("T ")) {
                 ToDo temp = new ToDo(parts[2]);
                 tasks.add(temp);
-                if(parts[1].equals(" 0 ")) {
+                if (parts[1].equals(" 0 ")) {
                     temp.markDone();
                 }
             }
@@ -61,15 +86,15 @@ public class Storage {
                 String[] x = parts[3].split(" ");
                 Deadline temp = new Deadline(parts[2], x[1]);
                 tasks.add(temp);
-                if(parts[1].equals(" 0 ")) {
+                if (parts[1].equals(" 0 ")) {
                     temp.markDone();
                 }
             }
-            if(parts[0].equals("E ")) {
+            if (parts[0].equals("E ")) {
                 String[] duration = parts[3].split("-");
                 Event temp = new Event(parts[2], duration[0], duration[1]);
                 tasks.add(temp);
-                if(parts[1].equals(" 0 ")) {
+                if (parts[1].equals(" 0 ")) {
                     temp.markDone();
                 }
             }
@@ -77,3 +102,4 @@ public class Storage {
         return tasks;
     }
 }
+
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -3,41 +3,88 @@ import tasks.Deadline;
 import tasks.Event;
 import tasks.ToDo;
 import tasks.Task;
-
+/**
+ * The TaskList class represents a list of tasks and provides various methods for managing and interacting with those tasks.
+ */
 public class TaskList {
     protected ArrayList<Task> tasks;
 
+    /**
+     * Constructs a new TaskList with the provided list of tasks.
+     *
+     * @param tasks The list of tasks to initialize the TaskList with.
+     */
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
 
+    /**
+     * Adds a task to the task list.
+     *
+     * @param task The task to be added.
+     */
     public void addToList(Task task) {
         this.tasks.add(task);
     }
 
+    /**
+     * Marks a task as done by its index in the list.
+     *
+     * @param taskNum The index of the task to mark as done.
+     */
     public void markDone(int taskNum) {
         this.tasks.get(taskNum).markDone();
     }
 
+    /**
+     * Marks a task as undone by its index in the list.
+     *
+     * @param taskNum The index of the task to mark as undone.
+     */
     public void markUndone(int taskNum) {
         this.tasks.get(taskNum).markUndone();
     }
 
+    /**
+     * Returns the string representation of a task by its index.
+     *
+     * @param taskNum The index of the task to print.
+     * @return A string representation of the task.
+     */
     public String printTask(int taskNum) {
         return this.tasks.get(taskNum).toString();
     }
 
+    /**
+     * Returns the name of a task by its index.
+     *
+     * @param taskNum The index of the task to print the name of.
+     * @return The name of the task.
+     */
     public String printName(int taskNum) {
         return this.tasks.get(taskNum).getName();
     }
+
+    /**
+     * Prints the number of tasks in the list.
+     */
     public void numOfTask() {
         System.out.println("N... Now you have... " + this.tasks.size() + " tasks in the list. 0w0");
     }
+
+    /**
+     * Deletes a task by its index and prints its details before removal.
+     *
+     * @param taskNum The index of the task to delete.
+     */
     public void deleteTask(int taskNum) {
         System.out.println(this.printTask(taskNum));
         this.tasks.remove(taskNum);
     }
 
+    /**
+     * Prints the entire list of tasks with numbering for reference.
+     */
     public void printList() {
         for(int i = 0; i < tasks.size(); i++) {
             int x = i + 1;
