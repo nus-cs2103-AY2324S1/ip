68d0900121fc64d46bc59690a9951e4795f27aaa
ZENN\User <miljy123@gmail.com>
2023-09-03 15:21:34 +0800

    Update Buddy to understand dates

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,11 +1,20 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String start;
-    protected String end;
+    protected LocalDate start;
+    protected LocalDate end;
 
     public Event(String description, String start, String end, boolean isDone) {
         super(description, isDone);
-        this.start = start;
-        this.end = end;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        this.start = LocalDate.parse(start, formatter);
+        this.end = LocalDate.parse(end, formatter);
+    }
+
+    private String getDateString(LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        return date.format(formatter);
     }
 
     @Override
@@ -19,12 +28,13 @@ public class Event extends Task {
                 getTaskType(),
                 isDone ? 1 : 0,
                 this.description,
-                this.start,
-                this.end);
+                this.getDateString(start),
+                this.getDateString(end));
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + start + " to: " + end + ")";
+        return "[E]" + super.toString() + " (from: " + getDateString(start)
+                + " to: " + getDateString(end) + ")";
     }
 }
