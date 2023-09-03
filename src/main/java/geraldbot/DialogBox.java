358aac295fcd94fee8c85788c7edb0725ec170ef
geraldngjx <geraldngjx@gmail.com>
2023-09-04 01:22:03 +0800

    Add dialog boxes

---
+++ src/main/java/geraldbot/DialogBox.java
@@ -0,0 +1,30 @@
+package geraldbot;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class DialogBox extends HBox {
+
+    private Label text;
+    private ImageView displayPicture;
+
+    /**
+     * Constructs a DialogBox object with the specified label and image view.
+     *
+     * @param l The label to be displayed.
+     * @param iv The image view to be displayed.
+     */
+    public DialogBox(Label l, ImageView iv) {
+        text = l;
+        displayPicture = iv;
+
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+
+        this.setAlignment(Pos.TOP_RIGHT);
+        this.getChildren().addAll(text, displayPicture);
+    }
+}
