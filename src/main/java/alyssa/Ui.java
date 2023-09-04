a7013cbf99085af497e8b536f2791b955f7028da
Elijah5399 <chia.elijah@gmail.com>
2023-09-04 23:15:27 +0800

    Change constant variable names

--- src/main/java/alyssa/Ui.java
+++ src/main/java/alyssa/Ui.java
@@ -4,33 +4,33 @@ package alyssa;
  * This class helps Alyssa interact with the user.
  */
 public class Ui {
-    private static final String line = "____________________________________________________________";
-    private static final String logo = " ___  __    __   __  ____   ____    ___\n"
+    private static final String LINE = "____________________________________________________________";
+    private static final String LOGO = " ___  __    __   __  ____   ____    ___\n"
             + "|   | | |   \\ \\ / / |  __| |  __|  |   |\n"
             + "|   | | |    \\   /   \\ \\    \\ \\    |   |\n"
             + "|___| | |     | |     \\ \\    \\ \\   |___|\n"
             + "|   | | |___  | |     _\\ \\   _\\ \\  |   |\n"
             + "|   | |_____| |_|    |____| |____| |   |\n";
     protected void greet() {
-        System.out.println(logo);
-        System.out.println(line);
+        System.out.println(LOGO);
+        System.out.println(LINE);
         System.out.println("Hello! I'm Alyssa!");
         System.out.println("What can I do for you?");
-        System.out.println(line);
+        System.out.println(LINE);
     }
     protected void goodbye() {
-        System.out.println(line);
+        System.out.println(LINE);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(LINE);
     }
     protected void invalidTaskResponse() {
-        System.out.println(line);
+        System.out.println(LINE);
         System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
-        System.out.println(line);
+        System.out.println(LINE);
     }
     protected void printWithLines(String output) {
-        System.out.println(line);
+        System.out.println(LINE);
         System.out.println(output);
-        System.out.println(line);
+        System.out.println(LINE);
     }
 }
