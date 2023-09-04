b1006cf0a82513d23cc84ee87b99f5807e4f46f5
peiran18 <e0968798@u.nus.edu>
2023-09-04 08:52:50 +0800

    Add saving feature

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,9 +1,33 @@
 public class Deadline extends Task {
     protected String by;
+    private static String LINE = "-----------------------------------------\n";
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
     }
+
+    public static Deadline deadlineParse(String input) {
+        int indexBy = input.indexOf("/by");
+        if (input.length() < 1) {
+            new PotatoException(LINE + "Bruh you wanna do air or something?\n" + LINE);
+            return null;
+        } else {
+            return new Deadline(input.substring(0, indexBy),
+                    input.substring(indexBy + 4));
+        }
+    }
+
+    public static Deadline deadlineSavedParse(String input) {
+        int indexBy = input.indexOf("by:");
+        if (input.length() < 1) {
+            new PotatoException(LINE + "Bruh you wanna do air or something?\n" + LINE);
+            return null;
+        } else {
+            return new Deadline(input.substring(0, indexBy),
+                    input.substring(indexBy + 4));
+        }
+    }
+
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + by + ")";
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,11 +1,39 @@
 public class Event extends Task {
     protected String from;
     protected String to;
+    private static String LINE = "-----------------------------------------\n";
     public Event(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
     }
+
+    public static Event eventParse(String input) {
+        int indexFrom = input.indexOf("/from");
+        int indexTo = input.indexOf("/to");
+        if (input.length() < 1) {
+            new PotatoException(LINE + "Bruh you wanna do air or something?\n" + LINE);
+            return null;
+        } else {
+            return new Event(input.substring(0, indexFrom),
+                    input.substring(indexFrom + 6, indexTo),
+                    input.substring(indexTo + 4));
+        }
+    }
+
+    public static Event eventSavedParse(String input) {
+        int indexFrom = input.indexOf("from:");
+        int indexTo = input.indexOf("to:");
+        if (input.length() < 1) {
+            new PotatoException(LINE + "Bruh you wanna do air or something?\n" + LINE);
+            return null;
+        } else {
+            return new Event(input.substring(0, indexFrom),
+                    input.substring(indexFrom + 6, indexTo),
+                    input.substring(indexTo + 4));
+        }
+    }
+
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + "to: " + to + ")";
