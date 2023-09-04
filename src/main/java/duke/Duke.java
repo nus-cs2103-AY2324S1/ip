dc72ee080beb7c253477a0ddaee0a986e99f5c2a
JasonRay168 <e0985893@u.nus.edu>
2023-09-04 12:23:34 +0800

    Add JavaFX, Launcher, and update Gradle build file

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -6,10 +6,25 @@ import duke.utilities.Storage;
 import duke.utilities.TaskList;
 import duke.utilities.Ui;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /**
  * The main class for Duke Chatbot
  */
-public class Duke {
+public class Duke extends Application{
+
+	private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
 
     /** Variable to store task list */
     private Storage storage;
@@ -56,4 +71,27 @@ public class Duke {
     public static void main(String[] args) {
         new Duke("./tasks.txt").run();
     }
+
+	@Override
+	public void start(Stage stage) {
+		//Step 1. Setting up required components
+
+        //The container for the content of the chat to scroll.
+		scrollPane = new ScrollPane();
+		dialogContainer = new VBox();
+		scrollPane.setContent(dialogContainer);
+
+		userInput = new TextField();
+		sendButton = new Button("Send");
+
+		AnchorPane mainLayout = new AnchorPane();
+		mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+		scene = new Scene(mainLayout);
+
+		stage.setScene(scene);
+		stage.show();
+
+	   // more code to be added here later
+	}
 }
