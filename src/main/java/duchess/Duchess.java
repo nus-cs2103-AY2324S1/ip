27e94e0b91ccbabb2ef8160d169eb617ad3b9cfe
Evelyne Law <lawruixi@gmail.com>
2023-09-04 14:34:22 +0800

    added basic code for duchess gui

--- src/main/java/duchess/Duchess.java
+++ src/main/java/duchess/Duchess.java
@@ -2,10 +2,19 @@ package duchess;
 
 import java.util.Scanner;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 /**
  * The main class used to execute Duchess actions.
  */
-public class Duchess {
+public class Duchess extends Application {
     /**
      * Name for the Duchess AI.
      */
@@ -15,6 +24,16 @@ public class Duchess {
      * An ArrayList to store text in.
      */
     private static TaskList storedTasks = new TaskList();
+
+    /**
+     * JavaFX variables.
+     */
+    private static ScrollPane scrollPane;
+    private static VBox dialogContainer;
+    private static TextField userInput;
+    private static Button sendButton;
+    private static Scene scene;
+
     /**
      * Outputs the same string parameter that was passed in.
      *
@@ -250,5 +269,55 @@ public class Duchess {
         Storage.saveTasksToFile(Duchess.storedTasks);
 
         Ui.printFarewell();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        // Setting up the required componenttts
+        // The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+        
+        // Formatting the window to look as expected.
+        stage.setTitle("Duchess");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        // You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
