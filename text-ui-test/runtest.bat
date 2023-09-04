7681dfbdcc087790e83c2b3ada1c944255b308c1
tllshan <shantsou03@gmail.com>
2023-09-04 21:17:22 +0800

    Update runtest.bat for compatibility with package structure

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -3,19 +3,22 @@
 REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
+REM create classes directory if it doesn't exist
+if not exist ..\classes mkdir ..\classes
+
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
-REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+REM compile each Java file separately into the classes folder
+javac -d ..\classes -cp ..\src\main\java ..\src\main\java\chatterchicken\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
 )
 REM no error here, errorlevel == 0
 
-REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin ChatterChicken < input.txt > ACTUAL.TXT
+REM run the program, feed commands from input.txt file, and redirect the output to ACTUAL.TXT
+java -classpath ..\classes chatterchicken.ChatterChicken < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
