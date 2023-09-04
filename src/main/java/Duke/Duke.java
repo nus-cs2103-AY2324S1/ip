83a3cb3a93a01bee04b4b5a36e088aa0c9e4b0f4
MathanC <ncmathan98@gmail.com>
2023-09-05 00:33:16 +0800

    implement GUI

--- src/main/java/Duke/Duke.java
+++ src/main/java/Duke/Duke.java
@@ -1,9 +1,36 @@
 package Duke;
 
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 /**
  * Main class from which Duke program is run.
  */
 public class Duke {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+    private Scene scene;
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
+
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
@@ -19,10 +46,18 @@ public class Duke {
         new Duke("file.txt").run();
     }
 
+    public Duke(){
+        ui=new Ui();
+        storage=new Storage("file.txt");
+        tasks= new TaskList(storage.load());
+        parser=new Parser();
+    }
+
     /**
      * Public constructor for Duke
      * @param filename for the txt file to be referenced.
      */
+
     public Duke (String filename){
         ui=new Ui();
         storage=new Storage(filename);
@@ -42,6 +77,23 @@ public class Duke {
             isExit = parser.isExit();
         }
         ui.showExit();
+    }
+
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    public String getResponse(String input) {
+        parser.parse(input, tasks, storage, ui);
+        if (parser.isExit()) {
+            ui.showExit();
+        }
+        return ui.emptyBuffer();
+    }
+
+    public boolean hasEnded() {
+        return parser.isExit();
     }
 
 }
--- src/main/java/Duke/Ui.java
+++ src/main/java/Duke/Ui.java
@@ -7,11 +7,13 @@ import java.util.Scanner;
  */
 public class Ui {
     private Scanner scanner;
+    private String buffer;
 
     /**
      * Default constructor.
      */
     public Ui(){
+        buffer = "";
         scanner=new Scanner(System.in);
     }
 
@@ -32,6 +34,12 @@ public class Ui {
                 " What can I do for you?");
     }
 
+    public String getWelcome(){
+        return "____________________________________________________________\n" +
+                " Hello! I'm Mathan\n" +
+                " What can I do for you?";
+    }
+
     /**
      * Prints exit message.
      */
@@ -39,13 +47,18 @@ public class Ui {
         System.out.println("____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________");
+        buffer += "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
     }
 
     /**
      * Prints line spacer.
      */
     public void showLine(){
+
         System.out.println("____________________________________________________________");
+        buffer = buffer + "____________________________________________________________\n";
     }
 
     /**
@@ -54,6 +67,7 @@ public class Ui {
      */
     public void print(String str){
         System.out.println(str);
+        buffer += str + "\n";
     }
 
     /**
@@ -62,5 +76,12 @@ public class Ui {
      */
     public void handleError(DukeException e){
         System.out.println(e.getMessage());
+        buffer += e.getMessage() + "\n";
+    }
+
+    public String emptyBuffer(){
+        String tmp = buffer;
+        buffer = "";
+        return tmp;
     }
 }
