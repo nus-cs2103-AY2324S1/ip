8f5b25baee8d14aaa3d9373e9430e2fb63de289a
Jonyxzx <jonykhozhixiong@gmail.com>
2023-09-05 00:47:53 +0800

    Fix some wrapped text issue due to long text output

--- src/main/java/duke/ui/DialogBox.java
+++ src/main/java/duke/ui/DialogBox.java
@@ -9,10 +9,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 /**
  * This control represents a dialog box consisting of an ImageView to represent the speaker's face and a label
@@ -20,7 +21,7 @@ import javafx.scene.layout.HBox;
  */
 public class DialogBox extends HBox {
     @FXML
-    private Label dialog;
+    private Text dialogText;
     @FXML
     private ImageView displayPicture;
 
@@ -34,7 +35,7 @@ public class DialogBox extends HBox {
             e.printStackTrace();
         }
 
-        this.dialog.setText(text);
+        this.dialogText.setText(text);
         this.displayPicture.setImage(img);
     }
 
@@ -45,6 +46,8 @@ public class DialogBox extends HBox {
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
         Collections.reverse(tmp);
         getChildren().setAll(tmp);
+        Text t = (Text) getChildren().get(1);
+        t.setTextAlignment(TextAlignment.LEFT);
         setAlignment(Pos.TOP_LEFT);
     }
 
