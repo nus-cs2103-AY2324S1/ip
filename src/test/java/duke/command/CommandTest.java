c2263162f55b0613899546ac42b08b0d4b522cba
dlathyun <shyim0128@gmail.com>
2023-09-04 16:28:22 +0800

    Add JUnit tests

---
+++ src/test/java/duke/command/CommandTest.java
@@ -0,0 +1,14 @@
+package duke.command;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CommandTest {
+
+    @Test
+    public void byeCommand_shouldExit() {
+        Command command = new ByeCommand();
+        Assertions.assertTrue(command.isExitYet());
+    }
+}
