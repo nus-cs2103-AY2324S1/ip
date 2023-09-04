a3c5875f906c13330d6124bec7f7a5f4b8524779
eyelessrhyme7 <eyelessrhyme7@gmail.com>
2023-09-05 02:52:38 +0800

    Completed A-JUnit

---
+++ src/test/java/duke/ParserTest.java
@@ -0,0 +1,36 @@
+package duke;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ParserTest {
+    @Test
+    @DisplayName("Test assert ParserException in parseMark (when invalid Task number passed)")
+    public void parseMark_invalidTaskNo_throwsParserException() {
+        Exception parserException = assertThrows(duke.ParserException.class,
+                () -> new Parser().parseMark("mark 6", new TaskList()));
+        assertEquals("Please enter valid Task No. to mark!", parserException.getMessage());
+    }
+    @Test
+    @DisplayName("Test assert ParserException in parseMark (when non-int argument passed)")
+    public void parseMark_nonIntArgument_throwsParserException() {
+        Exception parserException = assertThrows(duke.ParserException.class,
+                () -> new Parser().parseMark("mark f", new TaskList()));
+        assertEquals("Please enter valid Task No. (INTEGER) to mark in the format: 'mark 4'",
+                parserException.getMessage());
+    }
+
+    @Test
+    @DisplayName("Test assert ParserException in parseMark (when non-int argument passed)")
+    public void parseDelete_nonIntArgument_throwsParserException() {
+        Exception parserException = assertThrows(duke.ParserException.class,
+                () -> new Parser().parseDelete("delete f", new TaskList()));
+        assertEquals("Please enter valid Task No. (INTEGER) to delete in the format: 'delete 4'",
+                parserException.getMessage());
+    }
+
+
+}
