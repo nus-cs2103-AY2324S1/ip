898fac89261e2e9687ef2fd66479581f31d0320b
marcellaantania <marcellaantaniatan@gmail.com>
2023-09-05 17:28:22 +0800

    Add test class with dummy tests

---
+++ src/test/java/thea/TheaTest.java
@@ -0,0 +1,17 @@
+package thea;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TheaTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void dummyTest1() {
+        assertEquals(4, 4);
+    }
+}
