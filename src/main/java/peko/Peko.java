478839513617e3ef520de1bd6fa5f405373cc34c
ji-just-ji <wongjk9999@gmail.com>
2023-09-05 16:22:15 +0800

    Add screen and button press

--- src/main/java/peko/Peko.java
+++ src/main/java/peko/Peko.java
@@ -1,10 +1,16 @@
 package peko;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Peko extends Application {
+
+
+public class Peko extends Application implements EventHandler<ActionEvent> {
     private UserInputHandler userInputHandler;
     private UI ui;
     private StorageHandler storageHandler;
@@ -28,13 +34,27 @@ public class Peko extends Application {
         storageHandler = new StorageHandler();
     }
 
+    private Button button;
     @Override
     public void start(Stage stage) throws Exception {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
 
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
+        stage.setTitle("PekoBot!");
+        button = new Button("Enter Peko!");
+        button.setOnAction(this);
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add((button));
+        Scene scene = new Scene(layout, 300, 250);
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.show();
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if (event.getSource() == button) {
+            System.out.println("Konpeko");
+        }
     }
 
 }
