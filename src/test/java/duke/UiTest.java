953b1a182ec28fa424df3102029ea804d62b295d
brendan <brenstack62@gmail.com>
2023-09-05 22:09:03 +0800

    Modify files to adhere to checkstyle

--- src/test/java/duke/UiTest.java
+++ src/test/java/duke/UiTest.java
@@ -1,12 +1,2 @@
-package duke;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class UiTest {
-    @Test
-    public void Test1() {
-        assertEquals(1, 1);
-    }
+package duke;public class UiTest {
 }
