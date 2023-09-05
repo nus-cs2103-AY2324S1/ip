1d6bdf77f489f69d02d5379d8e010c2d508ed11a
DESKTOP-ITF4GUD\94866 <e0772448@u.nus.edu>
2023-09-05 16:59:32 +0800

    Settles conflicts

--- src/main/java/duke/io/Saver.java
+++ src/main/java/duke/io/Saver.java
@@ -57,27 +57,6 @@ public class Saver {
             } catch (IOException e) {
                 System.out.println("Error occurred in creating directory");
             }
-=======
-import duke.task.Task;
-
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.List;
-
-public class Saver {
-
-    private static final String FILE_PATH = "./data/duke.txt";
-
-    public static void saveToFile(List<Task> tasks) {
-        try {
-            FileWriter fw = new FileWriter(FILE_PATH);
-            for (Task task : tasks) {
-                fw.write(task.toFileFormat() + "\n");
-            }
-            fw.close();
-        } catch (IOException e) {
-            System.out.println("Error writing to file: " + e.getMessage());
->>>>>>> e6dac5a50befda9d6de9219427da7ac43ff883ea
         }
     }
 }
--- src/main/java/duke/tasks/Task.java
+++ src/main/java/duke/tasks/Task.java
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-package duke.task;
 
-public abstract class Task {
-    protected String description;
-    protected boolean isDone;
-
-    public Task(String description) {
-        this.description = description;
-        this.isDone = false;
-    }
-
-    public void markAsDone() {
-        this.isDone = true;
-    }
-
-    public void unmark() {
-        this.isDone = false;
-=======
 package duke.tasks;
 
 public abstract class Task {
@@ -51,7 +33,6 @@ public abstract class Task {
     @Override
     public String toString() {
         return (isDone() ? DONE_FLAG : UNDONE_FLAG) + getDescription();
->>>>>>> branch-Level-7
     }
 }
 
