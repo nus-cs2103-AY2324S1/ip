530d490502efbc4a48cf4547f2925c6e1a1c31ec
pra-navi <pranavikiran@gmail.com>
2023-09-05 13:47:27 +0800

    View first iteration of GUI, no images and one-sided inputs

---
+++ src/main/java/sally/Launcher.java
@@ -0,0 +1,11 @@
+package sally;
+import javafx.application.Application;
+
+/**
+ * A launcher class to workaround classpath issues.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(sally.Sally.class, args);
+    }
+}
