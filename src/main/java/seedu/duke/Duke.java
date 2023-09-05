6a48472703687824b1881b7daa34cd2f7a391614
simbayippy <simbayippy@gmail.com>
2023-09-05 16:56:23 +0800

    Updated TaskList constructor

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -35,8 +35,10 @@ public class Duke {
 
 
     /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
+     * Returns response to the GUI.
+     *
+     * @param userInput users input
+     * @return the relevant response to user
      */
     public String getResponse(String userInput) {
         String toReturn = parser.parse(userInput, taskList);
