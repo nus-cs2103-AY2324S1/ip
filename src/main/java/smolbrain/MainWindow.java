0db9504a469691873481c58d86290ee313b0f4a5
pzl111 <phuazailian@gmail.com>
2023-09-05 18:33:48 +0800

    Complete UI design

--- src/main/java/smolbrain/MainWindow.java
+++ src/main/java/smolbrain/MainWindow.java
@@ -23,8 +23,8 @@ public class MainWindow extends AnchorPane {
 
     private Smolbrain smolbrain;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/bot.png"));
 
     @FXML
     public void initialize() {
