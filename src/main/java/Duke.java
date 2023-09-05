6de1d50787de9c3ba23ddbc47f60729990774ce2
Teng Xin Zhuan <>
2023-09-05 16:36:45 +0800

    Remove duplicates

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,249 +1,9 @@
 import java.util.ArrayList;
-<<<<<<< HEAD
 import java.util.Scanner;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
-=======
-import java.util.List;
-import java.util.Scanner;
->>>>>>> da6b9c34d25d0d3f35afcfaaddf63922612f8815
-
-class EmptyDescriptionException extends Exception {
-    public EmptyDescriptionException(String task) {
-        super("OOPS!!! The description of a " + task + " cannot be empty.");
-<<<<<<< HEAD
-=======
-    }
-}
-
-class WrongFormatException extends Exception {
-    public WrongFormatException(String message) {
-        super(message);
-    }
-}
-
-class UnknownCommandException extends Exception {
-    public UnknownCommandException() {
-        super("OOPS I'm sorry, but I don't know what that means :-P");
-    }
-}
-
-enum CommandType {
-    LIST, MARK, UNMARK, DELETE, TODO, DEADLINE, EVENT, UNKNOWN
-}
-
-class Duke {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String botName = "Aaronbot";
-        List<Task> tasks = new ArrayList<>();
-
-        System.out.println("Hello! I'm " + botName);
-        System.out.println("What can I do for you?");
-
-        while (true) {
-            String userInput = scanner.nextLine();
-
-            try {
-                processUserInput(userInput, tasks);
-            } catch (EmptyDescriptionException | UnknownCommandException | WrongFormatException e) {
-                System.out.println(e.getMessage());
-            }
-
-            if (userInput.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            }
-        }
-
-        scanner.close();
-    }
-
-    public static void processUserInput(String userInput, List<Task> tasks)
-            throws EmptyDescriptionException, UnknownCommandException, WrongFormatException {
-        String[] inputParts = userInput.split(" ");
-        String commandStr = inputParts[0];
-        CommandType commandType = getCommandType(commandStr);
-
-        switch (commandType) {
-            case LIST:
-                listTasks(tasks);
-                break;
-            case MARK:
-            case UNMARK:
-                int taskIndex = Integer.parseInt(inputParts[1]) - 1;
-                if (taskIndex >= 0 && taskIndex < tasks.size()) {
-                    Task task = tasks.get(taskIndex);
-                    if (commandType == CommandType.MARK) {
-                        task.markAsDone();
-                        System.out.println("Nice! I've marked this task as done:");
-                    } else {
-                        task.markAsNotDone();
-                        System.out.println("OK, I've marked this task as not done yet:");
-                    }
-                    System.out.println("  " + task);
-                } else {
-                    System.out.println("Invalid task index.");
-                }
-                break;
-            case DELETE:
-                int index = Integer.parseInt(inputParts[1]) - 1;
-                Task task = tasks.get(index);
-                tasks.remove(index);
-                System.out.println("Noted. I've removed this task: \n" + task + "\nNow you have " + tasks.size() + " tasks in the list.");
-                break;
-            case TODO:
-            case DEADLINE:
-            case EVENT:
-                handleTaskCreation(userInput, tasks, commandType);
-                break;
-            default:
-                throw new UnknownCommandException();
-        }
-    }
-
-    public static CommandType getCommandType(String commandStr) {
-        try {
-            return CommandType.valueOf(commandStr.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            return CommandType.UNKNOWN;
-        }
-    }
-
-    public static void listTasks(List<Task> tasks) {
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < tasks.size(); i++) {
-            System.out.println((i + 1) + ". " + tasks.get(i));
-        }
-    }
-
-    public static void handleTaskCreation(String userInput, List<Task> tasks, CommandType commandType)
-            throws EmptyDescriptionException, UnknownCommandException, WrongFormatException {
-        String[] inputParts = userInput.split(" ");
-        String command = inputParts[0];
-
-        if (commandType == CommandType.TODO) {
-            if (inputParts.length <= 1) {
-                throw new EmptyDescriptionException(command);
-            }
-            tasks.add(new Todo(userInput.substring(5)));
-        } else if (commandType == CommandType.DEADLINE) {
-            if (inputParts.length <= 1){
-                throw new EmptyDescriptionException(command);
-            } 
-            try {
-                String[] deadlineParts = userInput.split(" /by ");
-                String description = deadlineParts[0].substring(9);
-                String by = deadlineParts[1];
-                tasks.add(new Deadline(description, by));
-            } catch (Exception e) {
-                throw new WrongFormatException("☹ OOPS!!! deadlines need to be in this format, deadline return book /by Sunday");
-            }
-        } else if (commandType == CommandType.EVENT) {
-            if (inputParts.length <= 1){
-                throw new EmptyDescriptionException(command);
-            } 
-            try {
-                String[] eventParts = userInput.split(" /from | /to ");
-                String description = eventParts[0].substring(6);
-                String from = eventParts[1];
-                String to = eventParts[2];
-                tasks.add(new Event(description, from, to));
-            } catch (Exception e) {
-                throw new WrongFormatException("☹ OOPS!!! events need to be in this format, event project meeting /from Mon 2pm /to 4pm");
-            }
-        } else {
-            throw new UnknownCommandException();
-        }
-
-        System.out.println("Got it. I've added this task:");
-        System.out.println("  " + tasks.get(tasks.size() - 1));
-        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-    }
-}
-
-class Task {
-    protected String description;
-    protected boolean isDone;
-
-    public Task(String description) {
-        this.description = description;
-        this.isDone = false;
-    }
-
-    public void markAsDone() {
-        isDone = true;
-    }
-
-    public void markAsNotDone() {
-        isDone = false;
-    }
-
-    @Override
-    public String toString() {
-        return "[" + (isDone ? "X" : " ") + "] " + description;
-    }
-}
-
-class Todo extends Task {
-    public Todo(String description) {
-        super(description);
-    }
-
-    @Override
-    public String toString() {
-        return "[T]" + super.toString();
-    }
-}
-
-class Deadline extends Task {
-    private String by;
-
-    public Deadline(String description, String by) {
-        super(description);
-        this.by = by;
-    }
-
-    @Override
-    public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
-    }
-}
-
-class Event extends Task {
-    private String from;
-    private String to;
-
-    public Event(String description, String from, String to) {
-        super(description);
-        this.from = from;
-        this.to = to;
-    }
-
-    @Override
-    public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
->>>>>>> da6b9c34d25d0d3f35afcfaaddf63922612f8815
-    }
-}
-
-class WrongFormatException extends Exception {
-    public WrongFormatException(String message) {
-        super(message);
-    }
-}
-
-class UnknownCommandException extends Exception {
-    public UnknownCommandException() {
-        super("OOPS!!! I'm sorry, but I don't know what that means :-P");
-    }
-}
-
-enum CommandType {
-    LIST, MARK, UNMARK, DELETE, TODO, DEADLINE, EVENT, UNKNOWN
-}
 
 class Duke {
     public static void main(String[] args) {
@@ -366,7 +126,7 @@ class Duke {
                 String to = eventParts[2];
                 tasks.add(new Event(description, from, to));
             } catch (Exception e) {
-                throw new WrongFormatException("OOPS events need to be in this format, event project meeting /from Mon 2pm /to 4pm");
+                throw new WrongFormatException("OOPS!!! events need to be in this format, event project meeting /from Mon 2pm /to 4pm");
             }
         } else {
             throw new UnknownCommandException();
@@ -375,6 +135,28 @@ class Duke {
         System.out.println("  " + tasks.get(tasks.size() - 1));
         System.out.println("Now you have " + tasks.size() + " tasks in the list.");
     }
+}
+
+class EmptyDescriptionException extends Exception {
+    public EmptyDescriptionException(String task) {
+        super("OOPS!!! The description of a " + task + " cannot be empty.");
+    }
+}
+
+class WrongFormatException extends Exception {
+    public WrongFormatException(String message) {
+        super(message);
+    }
+}
+
+class UnknownCommandException extends Exception {
+    public UnknownCommandException() {
+        super("OOPS!!! I'm sorry, but I don't know what that means :-P");
+    }
+}
+
+enum CommandType {
+    LIST, MARK, UNMARK, DELETE, TODO, DEADLINE, EVENT, UNKNOWN
 }
 
 class Task {
@@ -450,8 +232,11 @@ class TaskList {
         this.tasks = new ArrayList<>();
 
         String[] lines = input.split("\n");
+
         for (String line : lines) {
-            System.out.print(line);
+            if (line.equals("")) {
+                continue;
+            }
             Task task = createTaskFromInput(line);
             if (task != null) {
                 tasks.add(task);
