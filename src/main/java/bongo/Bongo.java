f33236a2ce3eb258ca9665e05ce63f29e2586eb7
raynertjx <rayner742@gmail.com>
2023-09-05 22:17:01 +0800

    Finish Iteration 1

--- src/main/java/bongo/Bongo.java
+++ src/main/java/bongo/Bongo.java
@@ -1,6 +1,18 @@
 package bongo;
 
 import java.io.FileNotFoundException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import bongo.command.Command;
 import bongo.helper.BongoException;
@@ -12,15 +24,21 @@ import bongo.task.TaskList;
 /**
  * The Bongo class, the class for the BongoBot.
  */
-public class Bongo {
+public class Bongo extends Application {
+    private static final String FILEPATH = "data/bongo.txt";
     private final Storage storage;
     private final Ui ui;
     private TaskList tasks;
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
 
     /**
      * Initializes Bongo object with filepath.
      *
-     * @param filepath
+     * @param filepath Filepath of text file.
      */
     public Bongo(String filepath) {
         this.ui = new Ui();
@@ -34,6 +52,90 @@ public class Bongo {
         } catch (BongoException e) {
             ui.showError(e.getMessage());
         }
+    }
+
+    /**
+     * Empty constructor for Bongo.
+     */
+    public Bongo() {
+        this.ui = new Ui();
+        this.storage = new Storage(FILEPATH);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        //Step 1. Formatting the window to look as expected.
+
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        //Step 2. Formatting the window to look as expected
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        //Step 3. Add functionality to handle user input.
+        sendButton.setOnMouseClicked((event) -> {
+            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
+            userInput.clear();
+        });
+
+        userInput.setOnAction((event) -> {
+            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
+            userInput.clear();
+        });
+
+        //Scroll down to the end every time dialogContainer's height changes.
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+    }
+
+    /**
+     * Iteration 1:
+     * Creates a label with the specified text and adds it to the dialog container.
+     * @param text String containing text to add
+     * @return a label with the specified text that has word wrap enabled.
+     */
+    private Label getDialogLabel(String text) {
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+
+        return textToAdd;
     }
 
     /**
@@ -58,7 +160,7 @@ public class Bongo {
     }
 
     public static void main(String[] args) {
-        Bongo bongo = new Bongo("data/bongo.txt");
+        Bongo bongo = new Bongo(FILEPATH);
         bongo.run();
     }
 }
