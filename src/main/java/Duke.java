9ae2432c7978511f0b3d61f711316d130c96cc6e
tiif <pekyuan@gmail.com>
2023-09-06 23:23:13 +0800

    deleted some imports and comments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,8 @@
 import duke.*;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import javafx.scene.layout.Region;
-import javafx.scene.control.Label;
 
 /**
  * Main class for the duke application.
@@ -25,8 +13,6 @@ public class Duke {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
-    private Image user = new Image(this.getClass().getResourceAsStream("images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("images/DaDuke.png"));
 
     public Duke() {
         ui = new Ui();
@@ -97,10 +83,8 @@ public class Duke {
         boolean isExit = false;
         while (!isExit) {
             BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-            //todo: replace the input here with gui input
             String str = bf.readLine();
             String command = parser.parseCommand(str);
-            //regex detect all space
             if (command.equals("list")) {
                 ui.printTaskList(tasks);
             } else if (command.equals("mark")) {
