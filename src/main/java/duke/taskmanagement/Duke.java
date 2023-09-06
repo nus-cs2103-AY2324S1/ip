6b99d63b6ecdaa3dcb40c417c1d0f277c1116910
tiong jjyi <tiongjjyi@gmail.com>
2023-09-06 19:40:56 +0800

    add GUI (incomplete)

--- src/main/java/duke/taskmanagement/Duke.java
+++ src/main/java/duke/taskmanagement/Duke.java
@@ -1,4 +1,17 @@
 package duke.taskmanagement;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.layout.Region;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
@@ -14,7 +27,6 @@ public class Duke {
     private Ui ui = new Ui();
     private TaskList tasks;
     private Storage storage = new Storage(PATH);
-
     public void run() {
         ui.greet();
         tasks = new TaskList(this.ui, storage.loadData(), storage);
@@ -22,7 +34,21 @@ public class Duke {
         Parser parser = new Parser(this.ui, this.tasks);
         parser.readCmd(this.tasks, cmd);
     }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    public String getResponse(String input) {
+        ui.greet();
+        tasks = new TaskList(this.ui, storage.loadData(), storage);
+        String cmd = this.ui.readInCmd();
+        Parser parser = new Parser(this.ui, this.tasks);
+        parser.readCmd(this.tasks, input);
+        return "Duke heard: " + input;
+    }
     public static void main(String[] args) {
-        new Duke().run();
+        //new Duke().run();
+        Launcher.main(args);
     }
 }
