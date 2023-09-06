faf1bd72844dbf8dcbb71cab0331ab1156f2ad40
peiran18 <e0968798@u.nus.edu>
2023-09-06 22:37:22 +0800

    Add OOP

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,16 +1,21 @@
 public class Todo extends Task {
     private static String LINE = "-----------------------------------------\n";
-    public Todo(String description) {
-        super(description);
+    public Todo(String description, boolean isDone) {
+        super(description, isDone);
     }
 
-    public static Todo todoParse(String input) {
+    public static Todo todoParse(String input, String mark) {
         if (input.length() < 1) {
             new PotatoException(LINE + "Bruh you wanna do air or something?\n" + LINE);
             return null;
         } else {
-            return new Todo(input);
+            return new Todo(input, (mark.equals("1")));
         }
+    }
+
+    @Override
+    public String toSave() {
+        return "T | " + (isDone ? "1" : "0") + " | " + description;
     }
 
     @Override
