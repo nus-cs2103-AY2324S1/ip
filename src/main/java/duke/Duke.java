d2ad360186565a5b85761942b8c0912844ffee03
Saumya Shah <68504323+LordSaumya@users.noreply.github.com>
2023-09-07 01:46:35 +0800

    Change Duke to use GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -6,21 +6,11 @@ import duke.parser.Parser;
 import duke.storage.Storage;
 import duke.task.TaskList;
 import duke.ui.Ui;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
 
 /**
  * Initialises Duke and starts the interaction with the user.
  */
-public class Duke extends Application {
+public class Duke {
 
     private Storage storage;
 
@@ -30,12 +20,6 @@ public class Duke extends Application {
 
     private Parser parser;
 
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-
     /**
      * Initialises the application with the necessary components.
      */
@@ -44,6 +28,7 @@ public class Duke extends Application {
 
         storage = new Storage(ui);
         parser = new Parser(ui);
+
         try {
             list = new TaskList(storage.readFile());
         } catch (Exception e) {
@@ -52,79 +37,15 @@ public class Duke extends Application {
         }
     }
 
-    /**
-     * Runs the application.
-     * Starts the interaction with the user.
-     */
-    public void run() {
-        ui.showStartMessage();
 
-        Command command = new InvalidCommand();
-        while (command.type != Command.CommandType.BYE) {
-            command = parser.parseCommand(ui.readCommand());
-            command.execute(list, ui);
-        }
-
-        // Write to file
-        try {
-            storage.writeFile(list);
-        } catch (Exception e) {
-            ui.showError("Error writing to file.");
-        }
-    }
-
-    /**
-     * Starts the application by calling the run method.
+    /** 
+     * Parses the user input and executes the command.
      * 
-     * @param args Command line arguments.
-     */
-    public static void main(String[] args) {
-        new Duke().run();
-    }
-
-    @Override
-    public void start(Stage stage) {
-
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        scene = new Scene(mainLayout);
-        stage.setScene(scene);
-        stage.show();
+     * @param input The user input.
+     * @return The response to the user input.
+    */
+    public String getResponse(String input) {
+        Command command = parser.parseCommand(input.trim());
+        return command.execute(list, ui);
     }
 }
