977c9ce5e34a38cb09f3f2f184550a84bf80e92a
Teng Xin Zhuan <>
2023-09-06 15:23:38 +0800

    follow coding standard

--- src/main/java/seedu/duke/Task.java
+++ src/main/java/seedu/duke/Task.java
@@ -2,9 +2,9 @@
 package seedu.duke;
 
 /**
- * Task models the different type of tasks in a tasks list
+ * Task models the different type of tasks in a tasks list.
  *
- * @param discription is the name of the Task that needs to be done
+ * @param discription is the name of the Task that needs to be done.
  */
 class Task {
     protected String description;
