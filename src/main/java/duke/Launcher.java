06e23bb90e7a01eb7d96d69771f19610d16ea386
Tim-Siu <e1088140@u.nus.edu>
2023-09-06 17:24:30 +0800

    Basic GUI created with dummy response

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -3,10 +3,10 @@ package duke;
 import javafx.application.Application;
 
 /**
- * A launcher class to workaround classpath issues.
+ * A launcher class to launch the Duke application.
  */
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Duke.class, args);
+        Application.launch(Main.class, args);
     }
 }
