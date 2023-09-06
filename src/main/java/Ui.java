77cd6796dc4eca3539c1e86c1973e2e0ca441941
peiran18 <e0968798@u.nus.edu>
2023-09-06 23:00:10 +0800

    Fix minor bugs

---
+++ src/main/java/Ui.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Ui {
+    Scanner scanner;
+    public Ui() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void showWelcome() {
+        System.out.println("Hey GURL I'm Potato\n" + "How can I help ya?");
+    }
+
+    public void showGoodbye() {
+        System.out.println("You're cancelled. Leave.");
+    }
+
+    public void showLine() {
+        System.out.println("-----------------------------------------");
+    }
+
+    public void showLoadingError() {
+        System.out.println("Oops there's nothing to load! Let's start a new list!");
+    }
+
+    public void showError(String message) {
+        System.out.println(message);
+    }
+
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+}
