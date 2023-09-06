602e03f6fcdc008932be31f403078f42f80af121
Matthew Sung <SungMatt@SungMatt.com>
2023-09-06 20:08:11 +0800

    Adhere to style convention

--- src/test/java/mattbot/task/EventTest.java
+++ src/test/java/mattbot/task/EventTest.java
@@ -2,9 +2,9 @@ package mattbot.task; //same package as the class being tested
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
-
 import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
 
 public class EventTest {
 
