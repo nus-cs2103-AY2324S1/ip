f0cc53b08e95f413341d68027053c9c82f81831e
TyrusLye <Lyetyrus@gmail.com>
2023-09-07 01:24:26 +0800

    Level-7

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,21 +3,20 @@ import java.util.ArrayList;
 import java.io.*;
 
 public class Duke {
-    private static final String FILE_PATH = "src/main/java/task.txt";
+    private static final String FILE_PATH = "src/main/java/tasks.txt";
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Task> tasks = loadTasksFromFile();
+        ArrayList<Task> tasks = new ArrayList<>(100);
+        loadTasksFromFile(tasks);
         String logo = "██╗░░░██╗██████╗░██████╗░░█████╗░██╗\n"
-                    + "██║░░░██║██╔══██╗██╔══██╗██╔══██╗██║\n"
-                    + "██║░░░██║██████╔╝██████╦╝██║░░██║██║\n"
-                    + "██║░░░██║██╔══██╗██╔══██╗██║░░██║██║\n"
-                    + "╚██████╔╝██║░░██║██████╦╝╚█████╔╝██║\n"
-                    + "░╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝\n";
+                + "██║░░░██║██╔══██╗██╔══██╗██╔══██╗██║\n"
+                + "██║░░░██║██████╔╝██████╦╝██║░░██║██║\n"
+                + "██║░░░██║██╔══██╗██╔══██╗██║░░██║██║\n"
+                + "╚██████╔╝██║░░██║██████╦╝╚█████╔╝██║\n"
+                + "░╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-
-        //ArrayList<Task> tasks = new ArrayList<>(100);
 
         while (true) {
             String command = scanner.nextLine();
@@ -39,13 +38,11 @@ public class Duke {
                     tasks.add(new Todo(description));
                     System.out.println("Got it. I've added this task:\n  " + tasks.get(tasks.size() - 1));
                     System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-                    saveTasksToFile(tasks);
                 } else if (command.startsWith("deadline")) {
                     String description = command.substring(9, command.indexOf("/by")).trim();
                     String by = command.substring(command.indexOf("/by") + 4).trim();
                     tasks.add(new Deadline(description, by));
                     System.out.println("Got it. I've added this task:\n  " + tasks.get(tasks.size() - 1));
-                    saveTasksToFile(tasks);
                 } else if (command.startsWith("event")) {
                     String description = command.substring(6, command.indexOf("/from")).trim();
                     String from = command.substring(command.indexOf("/from") + 6, command.indexOf("/to")).trim();
@@ -53,7 +50,6 @@ public class Duke {
                     tasks.add(new Event(description, from, to));
                     System.out.println("Got it. I've added this task:\n  " + tasks.get(tasks.size() - 1));
                     System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-                    saveTasksToFile(tasks);
                 } else if (command.startsWith("mark")) {
                     int index = Integer.parseInt(command.split(" ")[1]) - 1;
                     if (index >= 0 && index < tasks.size()) {
@@ -62,7 +58,6 @@ public class Duke {
                     } else {
                         System.out.println("Invalid task index.");
                     }
-                    saveTasksToFile(tasks);
                 } else if (command.startsWith("unmark")) {
                     int index = Integer.parseInt(command.split(" ")[1]) - 1;
                     if (index >= 0 && index < tasks.size()) {
@@ -71,7 +66,6 @@ public class Duke {
                     } else {
                         System.out.println("Invalid task index.");
                     }
-                    saveTasksToFile(tasks);
                 }  else if (command.startsWith("delete")) {
                     int index = Integer.parseInt(command.split(" ")[1]) - 1;
                     if (index >= 0 && index < tasks.size()) {
@@ -81,11 +75,10 @@ public class Duke {
                     } else {
                         System.out.println("Invalid task index.");
                     }
-                    saveTasksToFile(tasks);
                 } else {
                     throw new DukeException("I'm sorry, but I don't know what that means :-(");
                 }
-
+                saveTasksToFile(tasks);
             }
             catch (DukeException e) {
                 System.out.println("☹ OOPS!!! " + e.getMessage());
@@ -97,49 +90,88 @@ public class Duke {
 
         scanner.close();
     }
-    private static void saveTasksToFile(ArrayList<Task> tasks) {
-        try (FileOutputStream fos = new FileOutputStream(FILE_PATH);
-             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-
-            oos.writeObject(tasks);
-            System.out.println("Tasks saved to file: " + FILE_PATH);
-        } catch (IOException e) {
-            System.out.println("Error saving tasks to file: " + e.getMessage());
+    private static void loadTasksFromFile(ArrayList<Task> tasks) {
+        try {
+            File file = new File(FILE_PATH);
+            if (file.exists()) {
+                Scanner fileScanner = new Scanner(file);
+                while (fileScanner.hasNextLine()) {
+                    String line = fileScanner.nextLine();
+                    Task task = createTaskFromLine(line);
+                    if (task != null) {
+                        tasks.add(task);
+                    }
+                }
+                fileScanner.close();
+            }
+        } catch (FileNotFoundException e) {
+            // Handle file not found exception
+            System.out.println("File not found: " + FILE_PATH);
         }
     }
 
-    private static ArrayList<Task> loadTasksFromFile() {
-        ArrayList<Task> tasks = new ArrayList<>();
+    private static Task createTaskFromLine(String line) {
+        String[] parts = line.split(" \\| ");
+        if (parts.length < 3) {
+            return null;
+        }
 
-        File file = new File(FILE_PATH);
+        String type = parts[0];
+        String status = parts[1];
+        String description = parts[2];
 
-        if (!file.exists()) {
-            try {
-                file.createNewFile(); // Create a blank file if it doesn't exist
-                System.out.println("Created new data file: " + FILE_PATH);
-            } catch (IOException e) {
-                System.out.println("Error creating data file: " + e.getMessage());
+        Task task = null;
+        switch (type) {
+            case "T":
+                task = new Todo(description);
+                break;
+            case "D":
+                if (parts.length >= 4) {
+                    String by = parts[3];
+                    task = new Deadline(description, by);
+                }
+                break;
+            case "E":
+                if (parts.length >= 5) {
+                    String from = parts[3];
+                    String to = parts[4];
+                    task = new Event(description, from, to);
+                }
+                break;
+        }
+
+        if (task != null) {
+            if (status.equals("1")) {
+                task.markDone();
+            } else {
+                task.markNotDone();
             }
         }
 
-        if (file.exists()) {
-            try (FileInputStream fis = new FileInputStream(FILE_PATH);
-                 ObjectInputStream ois = new ObjectInputStream(fis)) {
+        return task;
+    }
 
-                tasks = (ArrayList<Task>) ois.readObject();
-            } catch (IOException | ClassNotFoundException e) {
-                System.out.println("Error loading tasks from file: " + e.getMessage());
+    private static void saveTasksToFile(ArrayList<Task> tasks) {
+        try {
+            FileWriter fileWriter = new FileWriter(FILE_PATH);
+            for (Task task : tasks) {
+                fileWriter.write(task.toFileString() + System.lineSeparator());
             }
+            fileWriter.close();
+        } catch (IOException e) {
+            // Handle IO exception
+            System.out.println("Error saving tasks to file: " + e.getMessage());
         }
-
-        return tasks;
     }
 }
 
 class Task {
-    private String description;
-    private boolean isDone;
+    protected String description;
+    protected boolean isDone;
 
+    public String toFileString() {
+        return "";
+    }
     public Task(String description) {
         this.description = description;
         this.isDone = false;
@@ -162,12 +194,13 @@ class Task {
     }
 }
 
-class Todo extends Task implements Serializable{
-    public Todo() {
-        super("");
-    }
+class Todo extends Task {
     public Todo(String description) {
         super(description);
+    }
+    @Override
+    public String toFileString() {
+        return "T | " + (isDone ? "1" : "0") + " | " + description;
     }
 
     @Override
@@ -176,16 +209,17 @@ class Todo extends Task implements Serializable{
     }
 }
 
-class Deadline extends Task implements Serializable{
+class Deadline extends Task {
     protected String by;
-    public Deadline() {
-        super("");
-    }
+
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
     }
-
+    @Override
+    public String toFileString() {
+        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by;
+    }
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + by + ")";
@@ -193,18 +227,19 @@ class Deadline extends Task implements Serializable{
 }
 
 
-class Event extends Task implements Serializable{
+class Event extends Task {
     protected String from;
     protected String to;
-    public Event() {
-        super("");
-    }
+
     public Event(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
     }
-
+    @Override
+    public String toFileString() {
+        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + from + " | " + to;
+    }
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
