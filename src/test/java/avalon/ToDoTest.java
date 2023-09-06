d41a39c22da8e060264b66fd5931ecba1328b7c1
LHeng1 <limhengyi1@gmail.com>
2023-09-06 20:04:15 +0800

    Setup gradle to run unit tests using JUnit

--- src/test/java/avalon/ToDoTest.java
+++ src/test/java/avalon/ToDoTest.java
@@ -1,2 +1,17 @@
-package avalon;public class TodoTest {
+package avalon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ToDoTest {
+    @Test
+    public void dummyTest(){
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void anotherDummyTest(){
+        assertEquals(4, 4);
+    }
 }
