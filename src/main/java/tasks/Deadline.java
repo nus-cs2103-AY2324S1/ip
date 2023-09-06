34f13f2eac321c029984af6e934b5e1fe0f2a0d5
yyyaohhh <yyyaohhh@gmail.com>
2023-09-06 12:13:38 +0800

    Add A-Packages

---
+++ src/main/java/tasks/Deadline.java
@@ -0,0 +1,26 @@
+package tasks;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+public class Deadline extends Task {
+    protected LocalDate by;
+
+    public Deadline(String name, LocalDate by) {
+        super(name);
+        this.by = by;
+    }
+    public Deadline(String name, String by) {
+        super(name);
+        this.by = LocalDate.parse(by);
+    }
+
+    public LocalDate getBy() {
+        return this.by;
+    }
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " +
+                this.by.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
+    }
+}
---
+++ src/main/java/tasks/Event.java
@@ -0,0 +1,53 @@
+package tasks;
+/**
+ * The Event class represents an event, which is a type of task with a name, start date, and end date.
+ * It extends the Task class and provides methods to access the event's start and end dates,
+ * as well as a custom implementation of the toString method to represent the event in a specific format.
+ */
+public class Event extends Task {
+    protected String start;
+    protected String end;
+
+    /**
+     * Constructs a new Event with the specified name, start date, and end date.
+     *
+     * @param name  The name of the event.
+     * @param start The start date of the event.
+     * @param end   The end date of the event.
+     */
+    public Event(String name, String start, String end) {
+        super(name);
+        this.start = start;
+        this.end = end;
+    }
+
+    /**
+     * Gets the start date of the event.
+     *
+     * @return The start date of the event.
+     */
+    public String getStart() {
+        return this.start;
+    }
+
+    /**
+     * Gets the end date of the event.
+     *
+     * @return The end date of the event.
+     */
+    public String getEnd() {
+        return this.end;
+    }
+
+    /**
+     * Returns a string representation of the event in the format:
+     * [E][ ] name (from: start date to: end date)
+     *
+     * @return A string representation of the event.
+     */
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + start + " to: " + end + ")";
+    }
+}
+
