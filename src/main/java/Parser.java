482d896324b7420c6ca992d70483084e56694542
peiran18 <e0968798@u.nus.edu>
2023-09-06 22:58:03 +0800

    Add find feature

---
+++ src/main/java/Parser.java
@@ -0,0 +1,22 @@
+public class Parser {
+    public static Command parse(String input) {
+        if (input.equals("bye")) {
+            return new ExitCommand(input);
+
+        } else if (input.startsWith("mark ")) {
+            return new MarkCommand(input);
+
+        } else if (input.startsWith("unmark ")) {
+            return new UnmarkCommand(input);
+
+        } else if (input.startsWith("delete ")) {
+            return new DeleteCommand(input);
+
+        } else if (input.equals("list")) {
+            return new ListCommand();
+
+        } else {
+            return new AddCommand(input);
+        }
+    }
+}
