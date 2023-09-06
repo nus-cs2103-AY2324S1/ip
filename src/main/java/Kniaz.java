1e45212bfb08e377ba99c8069e053d098a746660
xavierpok <54708640+xavierpok@users.noreply.github.com>
2023-09-06 15:24:17 +0800

    Implement first stage of javafx

--- src/main/java/Kniaz.java
+++ src/main/java/Kniaz.java
@@ -1,11 +1,15 @@
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 import main.KniazSession;
 
 /**
  * Entry point for the Kniaz program
  */
-public class Kniaz {
+public class Kniaz extends Application{
 
     /**
      * Main method of Kniaz. Runs the session.
@@ -15,6 +19,18 @@ public class Kniaz {
         KniazSession.init().run();
     }
 
+    @Override
+    //@@author Jeffry Lium
+    //Reused from https://se-education.org/guides/tutorials/javaFxPart1.html
+    // with minor modifications
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
+    //@@author
 
 
 
---
+++ src/main/java/Launcher.java
@@ -0,0 +1,13 @@
+
+import javafx.application.Application;
+
+//@@author Jeffry Lium
+//Reused from https://se-education.org/guides/tutorials/javaFxPart1.html
+// with minor modifications
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Kniaz.class, args);
+    }
+}
+//@@author
+
