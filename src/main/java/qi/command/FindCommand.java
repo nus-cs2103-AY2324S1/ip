8037f03a9021aab0af6cc2585dc055c907c69d10
VN-Hao <111359733+VN-Hao@users.noreply.github.com>
2023-09-07 01:55:11 +0800

    Write documentation for FindCommand class

--- src/main/java/qi/command/FindCommand.java
+++ src/main/java/qi/command/FindCommand.java
@@ -1,19 +1,34 @@
 package qi.command;
 
-import qi.qiexception.QiException;
 import qi.storage.Storage;
 import qi.tasklist.TaskList;
 import qi.ui.Ui;
 
+/**
+ * Represents the process of find tasks that match with a key word.
+ */
 public class FindCommand extends Command {
     private String keyWord;
 
+    /**
+     * Takes in the key word to find task.
+     *
+     * @param keyWord Key word to find task.
+     */
     public FindCommand(String keyWord) {
         super(false);
         this.keyWord = keyWord;
     }
 
-    public void execute(TaskList tasks, Ui ui, Storage storage) throws QiException {
+    /**
+     * Finds the tasks containing the key word.
+     *
+     * @param tasks TaskList recording the current task in the list.
+     * @param ui Ui representing the current User Interface (UI).
+     * @param storage Storage updating data if necessary.
+     */
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
         ui.showMatching(tasks.matchingKeyWord(this.keyWord));
     }
 }
