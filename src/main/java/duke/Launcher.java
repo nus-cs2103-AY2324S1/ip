084dd84894458d33c8da5f80c9a6fc34467e8bc3
Saumya Shah <68504323+LordSaumya@users.noreply.github.com>
2023-09-07 01:48:23 +0800

    Add Launcher and Main classes to implement GUI

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -1,12 +1,16 @@
 package duke;
 
+
+// Solution below adapted from https://se-education.org/guides/tutorials/javaFxPart2.html
+
 import javafx.application.Application;
 
 /**
  * A launcher class to workaround classpath issues.
  */
-public class Launcher {
+
+public class Launcher { 
     public static void main(String[] args) {
-        Application.launch(Duke.class, args);
+        Application.launch(Main.class, args);
     }
 }
