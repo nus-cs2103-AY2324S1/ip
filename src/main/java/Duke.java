7be63aa6db3e5e4426494bb877af76c4bff8ebe3
tiif <pekyuan@gmail.com>
2023-09-06 22:33:42 +0800

    Before code refactoring

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,47 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import javafx.scene.layout.Region;
+import javafx.scene.control.Label;
 
-import duke.Ui;
+import duke.DukeException;
 import duke.Parser;
 import duke.Storage;
-import duke.TaskList;
-import duke.DukeException;
 import duke.Task;
+import duke.TaskList;
+import duke.Ui;
 
 /**
  * Main class for the duke application.
  */
 
 public class Duke {
+//    private ScrollPane scrollPane;
+//    private VBox dialogContainer;
+//    private TextField userInput;
+//    private Button sendButton;
+//    private Scene scene;
     private Parser parser;
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
-    public static void main(String[] args) throws IOException {
-        new Duke("./data/duke.txt").run();
-    }
+    private Image user = new Image(this.getClass().getResourceAsStream("images/DaUser.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("images/DaDuke.png"));
 
+    public Duke() {
+        //temporary to run launcher;
+    }
     public Duke(String filePath) {
         ui = new Ui();
         parser = new Parser();
@@ -33,7 +53,109 @@ public class Duke {
             tasks = new TaskList();
         }
     }
-    /** Main driver code for duke class.
+//    @Override public void start(Stage stage) {
+//        stage.setTitle("Duke");
+//        stage.setResizable(false);
+//        stage.setMinHeight(600.0);
+//        stage.setMinWidth(400.0);
+//
+//        scrollPane = new ScrollPane();
+//        dialogContainer = new VBox();
+//        scrollPane.setContent(dialogContainer);
+//        scrollPane.setPrefSize(385, 535);
+//        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+//        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+//        scrollPane.setVvalue(1.0);
+//        scrollPane.setFitToWidth(true);
+//
+//        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+//
+//        userInput = new TextField();
+//        sendButton = new Button("Send");
+//        userInput.setPrefWidth(325.0);
+//        sendButton.setPrefWidth(55.0);
+//
+//        AnchorPane mainLayout = new AnchorPane();
+//        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+//        mainLayout.setPrefSize(400.0, 600.0);
+//        AnchorPane.setTopAnchor(scrollPane, 1.0);
+//        AnchorPane.setBottomAnchor(sendButton, 1.0);
+//        AnchorPane.setRightAnchor(sendButton, 1.0);
+//        AnchorPane.setLeftAnchor(userInput, 1.0);
+//        AnchorPane.setBottomAnchor(userInput, 1.0);
+//
+//        scene = new Scene(mainLayout);
+//
+//        stage.setScene(scene);
+//        stage.show();
+//
+//        //step 3: add functionality to handle user input.
+//        sendButton.setOnMouseClicked((event) -> {
+//            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
+//            userInput.clear();
+//        });
+//        userInput.setOnAction((event) -> {
+//            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
+//            userInput.clear();
+//        });
+//
+//        dialogContainer.heightProperty().addListener((observable -> scrollPane.setVvalue(1.0)));
+//
+//        //Part 3. Add functionality to handle user input.
+//        sendButton.setOnMouseClicked((event) -> {
+//            handleUserInput();
+//        });
+//
+//        userInput.setOnAction((event) -> {
+//            handleUserInput();
+//        });
+//
+//    }
+//
+//     * Iteration 2:
+//     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+//     * the dialog container. Clears the user input after processing.
+//
+//    private void handleUserInput() {
+//        Label userText = new Label(userInput.getText());
+//        Label dukeText = new Label(getResponse(userInput.getText()));
+//        dialogContainer.getChildren().addAll(
+//                DialogBox.getUserDialog(userText, new ImageView(user)),
+//                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
+//        );
+//        userInput.clear();
+//    }
+//
+//
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
+//    /**
+//     * Iteration 1:
+//     * Creates a label with the specified text and adds it to the dialog container.
+//     * @param text String containing text to add
+//     * @return a label with the specified text that has word wrap enabled.
+//     */
+//    private Label getDialogLabel(String text) {
+//        Label textToAdd = new Label(text);
+//        textToAdd.setWrapText(true);
+//
+//        return textToAdd;
+//    }
+//
+//
+//    public static void main(String[] args) throws IOException {
+//        new Duke("./data/duke.txt").run();
+//    }
+
+    /**
+     * Main driver code for duke class.
      */
     public void run() throws IOException {
         ui.printWelcomeMessage();
@@ -68,7 +190,7 @@ public class Duke {
                 tasks.addTask(curr);
                 storage.store(tasks);
                 ui.printAddTask(curr, tasks.getSize());
-            } else if (command.equals("delete")){
+            } else if (command.equals("delete")) {
                 int index = parser.parseToIndex();
                 Task curr = tasks.getTask(index);
                 tasks.deleteTask(index);
