cf58c2ddf87ebc4e5d71569b981dd259bec3d79a
lordidiot <0xlordidiot@gmail.com>
2023-09-06 14:51:40 +0800

    Add JavaFX and prototype GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -8,6 +8,14 @@ import java.util.List;
  * Chatbot that manages a task list.
  */
 public class Duke {
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
     public static void main(String[] args) {
         boolean isRunning = true;
         TaskStorage taskStorage = new TaskStorage();
