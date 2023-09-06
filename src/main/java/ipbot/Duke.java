27f86087d359304119aac54a6e6e39bd88d23194
rocketninja7 <andrewyapp@gmail.com>
2023-09-06 12:25:57 +0800

    Conform to coding standards

--- src/main/java/ipbot/Duke.java
+++ src/main/java/ipbot/Duke.java
@@ -47,7 +47,7 @@ public class Duke {
             return;
         }
 
-        while(true) {
+        while (true) {
             ui.readCommand();
             ui.printDivider();
             Pair<String, Map<String, String>> commandResult = ui.processCommand();
@@ -67,68 +67,68 @@ public class Duke {
                         }
                         Map<String, String> commandArgs = commandResult.getSecond();
                         switch (commandName) {
-                            case LIST:
-                                if (!commandArgs.get("").isEmpty()) {
-                                    LocalDateTime queryDate = LocalDateTime.parse(
-                                            commandArgs.get("") + " 0000", QUERY_DATE_TIME_FORMATTER);
-                                    taskList.listTasks(ui, queryDate);
-                                }
-                                else{
-                                    taskList.listTasks(ui);
-                                }
-                                break;
-                            case MARK:
-                                int markIndex = Parser.checkIndexArg(commandArgs.get(""), taskList.getTasksSize());
-                                if (markIndex == -1) {
-                                    throw new CommandArgumentException("Invalid task to mark: " + commandArgs);
-                                }
-                                Pair<Task, Boolean> taskMark = taskList.markTask(markIndex);
-                                if (taskMark.getSecond()) {
-                                    ui.printMarkTask(taskMark.getFirst(), true);
-                                } else {
-                                    ui.printAlreadyMarkedTask(taskMark.getFirst(), true);
-                                }
-                                break;
-                            case UNMARK:
-                                int unmarkIndex = Parser.checkIndexArg(commandArgs.get(""), taskList.getTasksSize());
-                                if (unmarkIndex == -1) {
-                                    throw new CommandArgumentException("Invalid task to unmark: " + commandArgs);
-                                }
-                                Pair<Task, Boolean> taskUnmark = taskList.unmarkTask(unmarkIndex);
-                                if (taskUnmark.getSecond()) {
-                                    ui.printMarkTask(taskUnmark.getFirst(), false);
-                                } else {
-                                    ui.printAlreadyMarkedTask(taskUnmark.getFirst(), false);
-                                }
-                                break;
-                            case TODO:
-                                if (commandArgs.isEmpty()) {
-                                    throw new CommandArgumentException("Task description cannot be empty!");
-                                }
-                                ToDo toDo = taskList.addToDoWithArgs(commandArgs);
-                                ui.printAddedItem(toDo, "todo");
-                                break;
-                            case DEADLINE:
-                                Deadline deadline = taskList.addDeadlineWithArgs(commandArgs);
-                                ui.printAddedItem(deadline, "deadline");
-                                break;
-                            case EVENT:
-                                Event event = taskList.addEventWithArgs(commandArgs);
-                                ui.printAddedItem(event, "event");
-                                break;
-                            case DELETE:
-                                int deleteIndex = Parser.checkIndexArg(commandArgs.get(""), taskList.getTasksSize());
-                                if (deleteIndex == -1) {
-                                    throw new CommandArgumentException("Invalid task to delete: " + commandArgs);
-                                }
-                                Task task = taskList.deleteTask(deleteIndex);
-                                ui.printDeletedItem(task);
-                                break;
+                        case LIST:
+                            if (!commandArgs.get("").isEmpty()) {
+                                LocalDateTime queryDate = LocalDateTime.parse(
+                                        commandArgs.get("") + " 0000", QUERY_DATE_TIME_FORMATTER);
+                                taskList.listTasks(ui, queryDate);
+                            }
+                            else{
+                                taskList.listTasks(ui);
+                            }
+                            break;
+                        case MARK:
+                            int markIndex = Parser.checkIndexArg(commandArgs.get(""), taskList.getTasksSize());
+                            if (markIndex == -1) {
+                                throw new CommandArgumentException("Invalid task to mark: " + commandArgs);
+                            }
+                            Pair<Task, Boolean> taskMark = taskList.markTask(markIndex);
+                            if (taskMark.getSecond()) {
+                                ui.printMarkTask(taskMark.getFirst(), true);
+                            } else {
+                                ui.printAlreadyMarkedTask(taskMark.getFirst(), true);
+                            }
+                            break;
+                        case UNMARK:
+                            int unmarkIndex = Parser.checkIndexArg(commandArgs.get(""), taskList.getTasksSize());
+                            if (unmarkIndex == -1) {
+                                throw new CommandArgumentException("Invalid task to unmark: " + commandArgs);
+                            }
+                            Pair<Task, Boolean> taskUnmark = taskList.unmarkTask(unmarkIndex);
+                            if (taskUnmark.getSecond()) {
+                                ui.printMarkTask(taskUnmark.getFirst(), false);
+                            } else {
+                                ui.printAlreadyMarkedTask(taskUnmark.getFirst(), false);
+                            }
+                            break;
+                        case TODO:
+                            if (commandArgs.isEmpty()) {
+                                throw new CommandArgumentException("Task description cannot be empty!");
+                            }
+                            ToDo toDo = taskList.addToDoWithArgs(commandArgs);
+                            ui.printAddedItem(toDo, "todo");
+                            break;
+                        case DEADLINE:
+                            Deadline deadline = taskList.addDeadlineWithArgs(commandArgs);
+                            ui.printAddedItem(deadline, "deadline");
+                            break;
+                        case EVENT:
+                            Event event = taskList.addEventWithArgs(commandArgs);
+                            ui.printAddedItem(event, "event");
+                            break;
+                        case DELETE:
+                            int deleteIndex = Parser.checkIndexArg(commandArgs.get(""), taskList.getTasksSize());
+                            if (deleteIndex == -1) {
+                                throw new CommandArgumentException("Invalid task to delete: " + commandArgs);
+                            }
+                            Task task = taskList.deleteTask(deleteIndex);
+                            ui.printDeletedItem(task);
+                            break;
                         }
                     }
                 }
             }
-            catch(CommandArgumentException e){
+            catch (CommandArgumentException e) {
                 System.out.println(e.getMessage());
             }
             catch (DateTimeParseException e) {
--- src/main/java/ipbot/model/Task.java
+++ src/main/java/ipbot/model/Task.java
@@ -1,10 +1,9 @@
 package ipbot.model;
 
-import java.io.Serializable;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
-public class Task implements Serializable {
+public class Task {
     public static final DateTimeFormatter INPUT_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(
             "uuuu-MM-dd HHmm");
     public static final DateTimeFormatter DISPLAY_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(
