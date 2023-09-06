061eede8ac24b2542cbad78e72e6b1c7a6416595
aslam341 <aslam.singapore@gmail.com>
2023-09-06 20:00:01 +0800

    Update automated text ui testing

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,14 +13,14 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/duke/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin duke.Duke < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
@@ -31,8 +31,20 @@ diff ACTUAL.TXT EXPECTED-UNIX.TXT
 if [ $? -eq 0 ]
 then
     echo "Test result: PASSED"
-    exit 0
 else
     echo "Test result: FAILED"
+fi
+
+# Delete the data directory (assuming it's named "data")
+if [ -d "./data" ]
+then
+    rm -r ./data
+fi
+
+# Exit with an appropriate status code
+if [ $? -eq 0 ]
+then
+    exit 0
+else
     exit 1
 fi
