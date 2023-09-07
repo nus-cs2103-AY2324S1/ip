fb284471f4692ff4d930de9e04afe753d9e7aea3
Qin Nanxin <nanxinqin@hotmail.com>
2023-09-07 20:26:02 +0800

    Previous files corrupted -> Have to fork project again [Level-0] - Unable figure out why my computer cannot open the previous files - Attempt to clone work from GitHub  didn't work
    Level 0. Rename, Greet, Exit
    (a) Rename chatbot to "Edna-Duke".
    (b) Implement an initial skeletal version of the it that simply greets the user and exits.

---
+++ src/main/java/Duke.java
@@ -0,0 +1,15 @@
+public class Duke {
+    public static String partition = "------------------------------------------------------------";
+    public static void main(String[] args) {
+        System.out.println(partition);
+        System.out.println("Hello! I'm Edna-Duke.");
+        System.out.println("What can I do for you?");
+        System.out.println(partition);
+        exit();
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(partition);
+    }
+}
