c0ed63b4487a35d54ec8a16ad09b57e8b30d776f
LHeng1 <limhengyi1@gmail.com>
2023-09-08 01:17:10 +0800

    Add basic JUnit tests to 2 classes

---
+++ src/test/java/avalon/TaskParserTest.java
@@ -0,0 +1,28 @@
+package avalon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class TaskParserTest {
+
+    @Test
+    public void parse_deadlineCreation_success() {
+        String input = "D | 1 | return book | 2019-08-18 2000";
+        Task task = TaskParser.parse(input);
+
+        assertTrue(task instanceof Deadline);
+        assertTrue(task.isDone);
+        assertEquals("return book", task.description);
+    }
+
+    @Test
+    public void serialize_deadlineStringify_success() {
+        Task task = new Deadline("return book", "2019-08-18 2000");
+        String serialized = TaskParser.serialize(task);
+
+        assertEquals("D | 0 | return book | 2019-08-18 2000", serialized);
+    }
+}
