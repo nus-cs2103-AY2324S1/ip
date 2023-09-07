4bf04b6105f191ab7fa88b3288ff46814692f537
Jie Xiang <n.jiexiang@gmail.com>
2023-09-08 00:01:51 +0800

    Change Ui class methods to return string instead of printing

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -2,114 +2,81 @@ package duke;
 
 import duke.task.Task;
 
-import java.io.PrintStream;
-import java.util.Scanner;
-
 /**
  * This class handles the UI of the application.
  */
 public class Ui {
-    public static final String LINE = "____________________________________________________________\n";
-    private final Scanner in;
-    private final PrintStream out;
-
     /**
-     * Constructor for a duke.Ui instance.
-     */
-    public Ui() {
-        this.in = new Scanner(System.in);
-        this.out = System.out;
-    }
-
-    /**
-     * Print a message with horizontal lines above and below it.
-     *
-     * @param message The message to be printed.
-     */
-    public void printWithLines(String message) {
-        out.print(LINE + message + LINE);
-    }
-
-    /**
-     * Print all the duke.task.Task instances in the list with their respective indices,
-     * surrounded by horizontal lines.
+     * Returns a string of all the duke.task.Task instances in the list with their respective indices.
      *
      * @param list The list containing the tasks.
+     * @return The message string.
      */
-    public void showList(TaskList list) {
-        out.print(LINE + Messages.MESSAGE_LIST);
-        list.printTasks();
-        out.print(LINE);
+    public String showList(TaskList list) {
+        return Messages.MESSAGE_LIST + list.printTasks();
     }
 
     /**
-     * Print the new task added along with its associated message,
-     * the new count of tasks, surrounded by horizontal lines.
+     * Returns a string of the new task added along with its associated message and
+     * the new count of tasks.
      *
      * @param task       The new task added.
      * @param totalTasks The total count of tasks in the list.
+     * @return The message string.
      */
-    public void showTaskAdded(Task task, int totalTasks) {
-        out.print(LINE + Messages.MESSAGE_NEW_TASK_ADDED);
-        out.print("  " + task + "\n");
-        out.printf(Messages.MESSAGE_TOTAL_TASK_COUNT, totalTasks);
-        out.print(LINE);
+    public String showTaskAdded(Task task, int totalTasks) {
+        return String.format("%s  %s\n", Messages.MESSAGE_NEW_TASK_ADDED, task)
+                + String.format(Messages.MESSAGE_TOTAL_TASK_COUNT, totalTasks);
     }
 
     /**
-     * Print the task deleted along with its associated message,
-     * the new count of tasks, surrounded by horizontal lines.
+     * Returns a string of the task deleted along with its associated message and
+     * the new count of tasks.
      *
      * @param task       The deleted task.
      * @param totalTasks The total count of tasks in the list.
+     * @return The message string.
      */
-    public void showTaskDeleted(Task task, int totalTasks) {
-        out.print(LINE + Messages.MESSAGE_TASK_DELETED);
-        out.print("  " + task + "\n");
-        out.printf(Messages.MESSAGE_TOTAL_TASK_COUNT, totalTasks);
-        out.print(LINE);
+    public String showTaskDeleted(Task task, int totalTasks) {
+        return String.format("%s  %s\n", Messages.MESSAGE_TASK_DELETED, task)
+                + String.format(Messages.MESSAGE_TOTAL_TASK_COUNT, totalTasks);
     }
 
     /**
-     * Print the task marked as done along with its associated message,
-     * surrounded by horizontal lines.
+     * Returns a string of the task marked as done along with its associated message.
      *
      * @param task The done task.
+     * @return The message string.
      */
-    public void showTaskDone(Task task) {
-        printWithLines(Messages.MESSAGE_MARK_DONE + "  " + task + '\n');
+    public String showTaskDone(Task task) {
+        return Messages.MESSAGE_MARK_DONE + "  " + task + '\n';
     }
 
     /**
-     * Print the task marked as undone along with its associated message,
-     * surrounded by horizontal lines.
+     * Returns a string of the task marked as undone along with its associated message.
      *
      * @param task The undone task.
+     * @return The message string.
      */
-    public void showTaskUndone(Task task) {
-        printWithLines(Messages.MESSAGE_MARK_UNDONE + "  " + task + '\n');
+    public String showTaskUndone(Task task) {
+        return Messages.MESSAGE_MARK_UNDONE + "  " + task + '\n';
     }
 
     /**
-     * Print the greeting message.
-     */
-    public void showGreeting() {
-        printWithLines(Messages.MESSAGE_GREETING);
-    }
-
-    /**
-     * Print the exit message.
-     */
-    public void showExitMessage() {
-        printWithLines(Messages.MESSAGE_EXIT);
-    }
-
-    /**
-     * Reads the text entered by the user.
+     * Returns a String of the greeting message.
      *
-     * @return The line read.
+     * @return The message string.
      */
-    public String readCommand() {
-        return in.nextLine();
+    public static String showGreeting() {
+        return Messages.MESSAGE_GREETING;
+    }
+
+    /**
+     * Returns a String of the exit message.
+     *
+     * @return The message string.
+     */
+    public static String showExitMessage() {
+        return Messages.MESSAGE_EXIT;
     }
 }
