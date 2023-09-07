26ba4fead142b94b33426461a0ec8467e18b1810
Chen1x <chenxinan01@gmail.com>
2023-09-07 23:34:17 +0800

    Add level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,47 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+    private static String[] tasks = new String[100];
+    private static int taskAmount = 0;
+
+    private static void greet() {
         String greetings = "____________________________________________________________\n" +
                 " Hello! I'm Tackie\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n";
+        System.out.println(greetings);
+    }
 
+    private static void farewell() {
         String farewell = "____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________";
+        System.out.println(farewell);
+    }
 
-        System.out.println(greetings);
+    private static void list(){
+        for (int i = 0; i < Duke.taskAmount; i++) {
+            System.out.println(Duke.tasks[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Duke.greet();
 
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
         while (!userInput.equals("bye")) {
-            System.out.println("____________________________________________________________\n" +
-                    userInput +
-                    "\n____________________________________________________________");
+            if (userInput.equals("list")) {
+                Duke.list();
+            } else {
+                System.out.println("____________________________________________________________\n" +
+                        "Added: " + userInput +
+                        "\n____________________________________________________________");
+                tasks[Duke.taskAmount] = userInput;
+                Duke.taskAmount += 1;
+            }
             userInput = scanner.nextLine();
         }
-        System.out.println(farewell);
+        Duke.farewell();
     }
 }
