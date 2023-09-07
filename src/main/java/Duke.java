cb3764366cba7858f88f45b46a09c814e65f2a47
Qin Nanxin <nanxinqin@hotmail.com>
2023-09-07 20:29:49 +0800

    [Level 1. Echo] Improve the skeletal version of Duke so that it echos commands entered by the user, and exits when the user types the command bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.*;
+
 public class Duke {
     public static String partition = "------------------------------------------------------------";
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
         System.out.println(partition);
         System.out.println("Hello! I'm Edna-Duke.");
         System.out.println("What can I do for you?");
         System.out.println(partition);
+        
+        String input = sc.next();
+        while(!input.equals("bye")) {
+            System.out.println(input);
+            System.out.println(partition);
+            input = sc.next();
+        }
         exit();
     }
 
