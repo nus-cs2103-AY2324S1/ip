57f6d4d7b655ddabdca27fbddee7b47d8c10eb7f
peasantbird <bertrand.wong.xh@gmail.com>
2023-09-08 04:46:45 +0800

    Store dates as Localdate instead of String in deadline & event tasks

--- src/main/java/Bert.java
+++ src/main/java/Bert.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * A chatbot named Bert that keeps track of a task list.
@@ -120,7 +122,8 @@ public class Bert {
             throw new BertEmptyTaskException();
         }
         String[] inputs = substring.split(" /by ");
-        Deadline t = new Deadline(inputs[0], inputs[1]);
+        LocalDate ld = LocalDate.parse(inputs[1], DateTimeFormatter.ofPattern("yyyy-mm-dd"));
+        Deadline t = new Deadline(inputs[0], ld);
         al.add(t);
         System.out.println(
                 "____________________________________________________________\n" +
@@ -143,7 +146,9 @@ public class Bert {
         }
         String[] descriptionAndTimes = substring.split(" /from ");
         String[] times = descriptionAndTimes[1].split(" /to ");
-        Event t = new Event(descriptionAndTimes[0], times[0], times[1]);
+        LocalDate ld1 = LocalDate.parse(times[0], DateTimeFormatter.ofPattern("yyyy-mm-dd"));
+        LocalDate ld2 = LocalDate.parse(times[1], DateTimeFormatter.ofPattern("yyyy-mm-dd"));
+        Event t = new Event(descriptionAndTimes[0], ld1, ld2);
         al.add(t);
         System.out.println(
                 "____________________________________________________________\n" +
