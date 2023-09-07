a57ff803d7009f4d943a39bf46d4c34d1b4e90e2
RiyaMehta2211 <>
2023-09-07 22:38:21 +0800

    troubleshoot and set up GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,15 +2,26 @@ package duke;
 
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * The Duke class represents a chatbot that generates a list of tasks based on user input.
  * These tasks can include deadlines, events, and ToDos. The user input is parsed to extract
  * dates and times for events and deadlines.
  */
+public class Duke extends Application{
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
 
-public class Duke {
-    public static void main(String[] args) {
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
+    public Duke(){
         Storage storage = new Storage();
         Ui ui = new Ui();
         ui.printHello();
@@ -28,6 +39,9 @@ public class Duke {
         }
         scanner.close();
         ui.printBye();
+    }
+    public static void main(String[] args){
+        Duke duke = new Duke();
     }
 }
 
