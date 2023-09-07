6d8c2f90aaa3f0199ce84cf28ffcf706026fb579
scarletblanks <irisu.nita@gmail.com>
2023-09-07 19:25:23 +0800

    GUI added

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -1,2 +1,12 @@
+package duke;
+
+import javafx.application.Application;
+
+/**
+ * A launcher class to workaround classpath issues.
+ */
 public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Duke.class, args);
+    }
 }
