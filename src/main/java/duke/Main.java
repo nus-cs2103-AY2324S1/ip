d516477e3970a8b8e57a113dd53490044c8ca747
lynnlow175 <lynnlow175@gmail.com>
2023-09-08 23:32:52 +0800

    Level 10

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -1,31 +1,19 @@
 package duke;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
-import java.io.IOException;
-import java.util.Objects;
-
-
+/**
+ * A GUI for Duke using FXML.
+ */
 public class Main extends Application {
 
-    private final Duke duke = new Duke();
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
+    private Duke duke = new Duke();
 
     @Override
     public void start(Stage stage) {
--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -1,6 +1,5 @@
 package duke;
 
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -8,7 +7,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
  */
@@ -44,11 +42,6 @@ public class MainWindow extends AnchorPane {
     private void handleUserInput() {
         String input = userInput.getText();
         String response = duke.getResponse(input);
-
-        // Exit the program
-        if (response.equals("")) {
-            Platform.exit();
-        }
         dialogContainer.getChildren().addAll(
                 DialogBox.getUserDialog(input, userImage),
                 DialogBox.getDukeDialog(response, dukeImage)
