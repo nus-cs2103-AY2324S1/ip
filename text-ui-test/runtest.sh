072c48e918042360060a0ef4b5fd98ac59309991
Xenos F <git@xenosfio.com>
2023-09-08 13:47:01 +0800

    Fix test scripts to use package

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -24,7 +24,7 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Dude < input.txt > ACTUAL.TXT
+java -classpath ../bin dude.Dude < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
