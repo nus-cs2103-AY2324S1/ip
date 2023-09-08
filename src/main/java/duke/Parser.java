e0264df79eaf1a64ef7bba304b0201bebd99c1ec
Inez Kok <kfyinez@gmail.com>
2023-09-08 17:03:48 +0800

    Update names of method to coding standard

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -36,28 +36,28 @@ public class Parser {
             return new ListCommand();
         } else if (input.startsWith("mark")) {
             String number = input.replaceFirst("mark", "").trim();
-            validateMarkOrUnmarkorDelete(number, size);
+            checkRange(number, size);
 
             int index = Integer.parseInt(number);
             return new MarkDoneCommand(index);
         } else if (input.startsWith("unmark")) {
             String number = input.replaceFirst("unmark", "").trim();
-            validateMarkOrUnmarkorDelete(number, size);
+            checkRange(number, size);
 
             int index = Integer.parseInt(number);
             return new UnmarkDoneCommand(index);
         } else if (input.startsWith("todo")) {
             String description = input.replaceFirst("todo", "").trim();
-            validateToDo(description);
+            checkToDo(description);
 
             return new AddToDoCommand(description);
         } else if (input.startsWith("deadline")) {
             String[] deadlineString = input.replaceFirst("deadline", "")
                     .split("/", 2);
-            validateDeadline(deadlineString);
+            checkDeadline(deadlineString);
 
             String deadlineDate = deadlineString[1].replaceFirst("by", "").trim();
-            validateDate(deadlineDate);
+            checkDate(deadlineDate);
 
             String description = deadlineString[0].trim();
             LocalDate d = LocalDate.parse(deadlineDate);
@@ -65,12 +65,12 @@ public class Parser {
             return new AddDeadlineCommand(description, d);
         } else if (input.startsWith("event")) {
             String[] eventString = input.replaceFirst("event", "").split("/", 3);
-            validateEvent(eventString);
+            checkEvent(eventString);
 
             String start = eventString[1].replaceFirst("from", "").trim();
             String end = eventString[2].replaceFirst("to", "").trim();
-            validateDate(start);
-            validateDate(end);
+            checkDate(start);
+            checkDate(end);
 
             String description = eventString[0].trim();
             LocalDate d1 = LocalDate.parse(start);
@@ -79,13 +79,13 @@ public class Parser {
             return new AddEventCommand(description, d1, d2);
         } else if (input.startsWith("delete")) {
             String number = input.replaceFirst("delete", "").trim();
-            validateMarkOrUnmarkorDelete(number, size);
+            checkRange(number, size);
 
             int index = Integer.parseInt(number);
             return new DeleteCommand(index);
         } else if (input.startsWith("find")) {
             String keyword = input.replaceFirst("find", "").trim();
-            validateFind(keyword);
+            checkFind(keyword);
             return new FindCommand(keyword);
         } else {
             throw new DukeException("Boop Beep OOPS! I'm sorry, but I don't know what that means :(");
@@ -99,7 +99,7 @@ public class Parser {
      * @param size The size of the current task list.
      * @throws DukeException On input error.
      */
-    private static void validateMarkOrUnmarkorDelete(String number, int size) throws DukeException {
+    private static void checkRange(String number, int size) throws DukeException {
         if (number.isBlank()) {
             throw new DukeException("Boop Beep OOPS! Please make sure that the index of the task is not empty.");
         } else {
@@ -121,7 +121,7 @@ public class Parser {
      * @param description The string representation of the todo description.
      * @throws DukeException On input error.
      */
-    private static void validateToDo(String description) throws DukeException {
+    private static void checkToDo(String description) throws DukeException {
         if (description.isBlank()) {
             throw new DukeException("Boop Beep OOPS! The description of a todo cannot be empty.");
         }
@@ -133,7 +133,7 @@ public class Parser {
      * @param deadlineString The array of string representations of the parameters of a Deadline.
      * @throws DukeException On input error.
      */
-    private static void validateDeadline(String[] deadlineString) throws DukeException {
+    private static void checkDeadline(String[] deadlineString) throws DukeException {
         if (deadlineString.length != 2 || deadlineString[0].isBlank() || deadlineString[1].isBlank()) {
             throw new DukeException("Boop Beep OOPS! Please make sure that"
                     + " the description and date of the deadline is not empty.");
@@ -146,7 +146,7 @@ public class Parser {
      * @param eventString The array of string representations of the parameters of an Event.
      * @throws DukeException On input error.
      */
-    private static void validateEvent(String[] eventString) throws DukeException {
+    private static void checkEvent(String[] eventString) throws DukeException {
         if (eventString.length != 3 || eventString[0].isBlank() || eventString[1].isBlank()
                 || eventString[2].isBlank()) {
             throw new DukeException("Boop Beep OOPS! Please make sure that"
@@ -160,7 +160,7 @@ public class Parser {
      * @param date The string representation of the date.
      * @throws DukeException On format error of String date.
      */
-    private static void validateDate(String date) throws DukeException {
+    private static void checkDate(String date) throws DukeException {
         try {
             LocalDate d = LocalDate.parse(date);
         } catch (DateTimeParseException e) {
@@ -174,7 +174,7 @@ public class Parser {
      * @param keyword The keyword to be searched for.
      * @throws DukeException When keyword is blank.
      */
-    private static void validateFind(String keyword) throws DukeException {
+    private static void checkFind(String keyword) throws DukeException {
         if (keyword.isBlank()) {
             throw new DukeException("Boop Beep OOPS! Please check that you have entered a keyword.");
         }
