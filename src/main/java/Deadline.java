6c62c2df8d7d0315beaae4ab6c618e9f3bb10793
Kevin <e1053635@u.nus.edu>
2023-09-08 16:21:26 +0800

    Teach the chatbot how to understand dates and times.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,25 +1,36 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task{
-    private String day;
+    private LocalDate day;
 
 
-    private Deadline(String description, String day) {
+    private Deadline(String description, LocalDate day) {
         super(description);
         this.day = day;
     }
 
-    public Deadline(String description, String day, boolean isDone) {
+    public Deadline(String description, LocalDate day, boolean isDone) {
         super(description);
         this.day = day;
         this.isDone = isDone;
     }
 
-    public static Deadline createNewDeadlineTask(String description) {
+    public static Deadline createNewDeadlineTask(String description) throws DateTimeParseException{
         String[] splitMessage = description.split(" /by ");
-        return new Deadline(splitMessage[0], splitMessage[1]);
+        LocalDate date = LocalDate.parse(splitMessage[1]);
+        return new Deadline(splitMessage[0], date);
     }
 
     @Override
     public String toString() {
+        return "[D]" + super.toString() + " (by: " +
+                this.day.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
+    }
+
+    @Override
+    public String toFileString() {
         return "[D]" + super.toString() + " (by: " + this.day + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,17 @@
-public class Event extends Task {
-    private String startTime;
-    private String endTime;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    private Event(String description, String startTime, String endTime) {
+public class Event extends Task {
+    private LocalDate startTime;
+    private LocalDate endTime;
+
+    private Event(String description, LocalDate startTime, LocalDate endTime) {
         super(description);
         this.startTime = startTime;
         this.endTime = endTime;
     }
 
-    public Event(String description, String startTime, String endTime, boolean isDone) {
+    public Event(String description, LocalDate startTime, LocalDate endTime, boolean isDone) {
         super(description);
         this.startTime = startTime;
         this.endTime = endTime;
@@ -18,11 +21,19 @@ public class Event extends Task {
     public static Event createNewEventTask(String description) {
         String[] splitMessage1 = description.split(" /from ");
         String[] splitMessage2 = splitMessage1[1].split(" /to ");
-        return new Event(splitMessage1[0], splitMessage2[0], splitMessage2[1]);
+        return new Event(splitMessage1[0], LocalDate.parse(splitMessage2[0]), LocalDate.parse(splitMessage2[1]));
     }
 
     @Override
     public String toString() {
+        return "[E]" + super.toString() + " (from: " +
+                this.startTime.format(DateTimeFormatter.ofPattern("MMM d yyyy")) +
+                " to: " +
+                this.endTime.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
+    }
+
+    @Override
+    public String toFileString() {
         return "[E]" + super.toString() + " (from: " + this.startTime + " to: " + this.endTime + ")";
     }
 }
