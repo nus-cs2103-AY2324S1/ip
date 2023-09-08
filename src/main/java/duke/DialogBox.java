c3c6065be42d1df7c04787b448871d8b11b5bc9c
SelwynAng <selwynang.01@gmail.com>
2023-09-08 13:43:51 +0800

    Improve dialog box appearance

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -13,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
 
 /**
  * The DialogBox class represents a graphical dialog box that can display text and an image.
@@ -43,6 +44,7 @@ public class DialogBox extends HBox {
 
         dialog.setText(text);
         displayPicture.setImage(img);
+        displayPicture.setClip(new Circle(50, 50, 50));
     }
 
     /**
