527632fcd3f444e0f9ab647100574fdecfdba6b1
sopa301 <96387349+sopa301@users.noreply.github.com>
2023-09-08 22:19:27 +0800

    Remove old test code and text-based test from gradle.yaml
    Let's remove any text-based testing because we don't need it.

--- src/test/java/bot/utils/TaskListTest.java
+++ src/test/java/bot/utils/TaskListTest.java
@@ -5,11 +5,11 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.ArrayList;
 
-import bot.utils.tasks.Task;
 import org.junit.jupiter.api.Test;
 
 import bot.exceptions.InvalidIndexException;
 import bot.exceptions.InvalidTaskException;
+import bot.utils.tasks.Task;
 
 public class TaskListTest {
 
