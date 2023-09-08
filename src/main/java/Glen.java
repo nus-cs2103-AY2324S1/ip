8ccb6359876ebd1aac87f55ae0f6c0fa83329515
tayian <90464339+tayian@users.noreply.github.com>
2023-09-08 22:59:50 +0800

    Update Glen.java for more OOP

--- src/main/java/Glen.java
+++ src/main/java/Glen.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Glen {
     static final String HORLINE = "_____________________________________________________\n";
-    static ArrayList<Task> tasks = new ArrayList<Task>();
     private static Storage storage = new Storage("./data/tasks.txt");
+    static TaskList tasks = new TaskList();
     
     public static void main(String[] args) {
         tasks = storage.read();
@@ -15,11 +14,11 @@ public class Glen {
         String low = input.toLowerCase();
         while (!low.equals("bye")) {
             if (low.equals("list")) {
-                System.out.println(lst());
+                System.out.println(tasks.lst());
             } else {
                 int index = Math.max(0, low.indexOf(" "));
                 String firstWord = low.substring(0, index);
-                String end = "";
+                String end = ""; 
                 try {
                     end = input.substring(++index);
                 } catch (StringIndexOutOfBoundsException e) {}
@@ -30,7 +29,7 @@ public class Glen {
                             taskIndex = Integer.valueOf(end) - 1;
                         } catch (Exception NumberFormatException) {}
                         if (taskIndex >= 0 && taskIndex < tasks.size()) {
-                            toggle(firstWord, taskIndex);
+                            tasks.toggle(storage, firstWord, taskIndex);
                         } else {
                             System.out.println(HORLINE + "\u2639 OOPS!!! Please select a valid item to mark/unmark.\n" + HORLINE);
                         }   
@@ -41,7 +40,7 @@ public class Glen {
                             String string1 = end.substring(0, separatorIndex).trim();
                             String string2 = end.substring(separatorIndex + 3).trim();
 
-                            System.out.println(addDeadline(string1, string2));
+                            System.out.println(tasks.addDeadline(storage, string1, string2));
                         } catch (Exception StringIndexOutOfBoundsException) {
                             System.out.println(HORLINE + "\u2639 OOPS!!! Your request needs to be formatted as:\ndeadline <task name> /by <time>\n" + HORLINE);
                         }
@@ -54,7 +53,7 @@ public class Glen {
                             String string2 = end.substring(fromIndex + 6, toIndex).trim();
                             String string3 = end.substring(toIndex + 4).trim();
 
-                            System.out.println(addEvent(string1, string2, string3));
+                            System.out.println(tasks.addEvent(storage, string1, string2, string3));
                         } catch (Exception StringIndexOutOfBoundsException) {
                             System.out.println(HORLINE + "\u2639 OOPS!!! Your request needs to be formatted as:\nevent <event name> /from <start time> /to <end time>\n" + HORLINE);
                         }
@@ -63,7 +62,7 @@ public class Glen {
                         if (trimmed.equals("")) {
                             System.out.println(HORLINE + "\u2639 OOPS!!! The description of a todo cannot be empty.\n" + HORLINE);
                         } else {
-                            System.out.println(addTodo(trimmed));
+                            System.out.println(tasks.addTodo(storage, trimmed));
                         }
                     } else if (firstWord.equals("delete")) {
                         int taskIndex = -1;
@@ -71,7 +70,7 @@ public class Glen {
                             taskIndex = Integer.valueOf(end) - 1;
                         } catch (Exception NumberFormatException) {}
                         if (taskIndex >= 0 && taskIndex < tasks.size()) {
-                            System.out.println(del(taskIndex));
+                            System.out.println(tasks.del(storage, taskIndex));
                         } else {
                             System.out.println(HORLINE + "\u2639 OOPS!!! Please select a valid item to delete.\n" + HORLINE);
                         }   
@@ -97,78 +96,6 @@ public class Glen {
         String introText = "Hello, I'm Glen!\n" + 
                            "What can I do for you?\n";
         return HORLINE + logo + introText + HORLINE;
-    }
-
-    static String del(int taskIndex) {
-        String temp = tasks.get(taskIndex).toString();
-        tasks.remove(taskIndex);
-        return HORLINE + "Noted. I've removed this task:\n  " + temp + "\n" + "Now you have " + tasks.size() + " tasks in the list.\n" + HORLINE;
-    }
-
-    static String addDeadline(String inp, String by) {
-        Deadline newTask = new Deadline(inp, false, by);
-        String newTaskString = newTask.toFileString();
-        tasks.add(newTask);
-        storage.addTask(newTaskString);
-        return HORLINE + "Got it. I've added this task:\n  " + newTask.toString() + "\nNow you have " + tasks.size() + " tasks in the list.\n" + HORLINE;
-    }
-
-    static String addEvent(String inp, String from, String to) {
-        Event newTask = new Event(inp, false, from, to);
-        String newTaskString = newTask.toFileString();
-        tasks.add(newTask);
-        storage.addTask(newTaskString);
-        return HORLINE + "Got it. I've added this task:\n  " + newTask.toString() + "\nNow you have " + tasks.size() + " tasks in the list.\n" + HORLINE;
-    }
-
-    static String addTodo(String inp) {
-        Todo newTask = new Todo(inp, false);
-        String newTaskString = newTask.toFileString();
-        tasks.add(newTask);
-        storage.addTask(newTaskString);
-        return HORLINE + "Got it. I've added this task:\n  " + newTask.toString() + "\nNow you have " + tasks.size() + " tasks in the list.\n" + HORLINE;
-    }
-
-    static void toggle(String requestType, int taskIndex) {
-        if (taskIndex > tasks.size()) {
-            System.out.println("Invalid item selected. Please try again.");
-        } else {
-            Task task = tasks.get(taskIndex);
-            if (requestType.equals("mark") && task.getStatusIcon().equals("[X] ")) {
-                System.out.println(HORLINE + "Task is currently marked. Did you mean to unmark the task?\n" + HORLINE);
-            } else if (requestType.equals("unmark") && task.getStatusIcon().equals("[ ] ")) {
-                System.out.println(HORLINE + "Task is currently unmarked. Did you mean to mark the task?\n" + HORLINE);
-            } else {
-                task.toggle();
-                String updatedTaskString = "";
-                try {
-                    updatedTaskString = ((Deadline) task).toFileString();
-                } catch (ClassCastException e) {
-                    try {
-                        updatedTaskString = ((Todo) task).toFileString();
-                    } catch (ClassCastException f) {
-                        updatedTaskString = ((Event) task).toFileString();
-                    }
-                }
-                storage.updateTask(taskIndex, updatedTaskString);
-                if (task.getStatusIcon().equals("[X] ")) {
-                    System.out.println(HORLINE + "Nice! I've marked this task as done:");
-                    System.out.println("  [X] " + task.getDescription() + "\n" + HORLINE);
-                } else {
-                    System.out.println(HORLINE + "OK, I've marked this task as not done yet:");
-                    System.out.println("  [ ] " + task.getDescription() + "\n" + HORLINE);
-                }
-            }
-        }
-    }
-
-    static String lst() {
-        String temp = "Here are the tasks in your list:\n";
-        for(int i = 0; i < tasks.size(); i++) {
-            Task tempTask = tasks.get(i);
-            temp += String.valueOf(i + 1) + "." + tempTask.toString() + "\n";
-        }  
-        return HORLINE + temp + HORLINE;
     }
 
     static String exit() {
