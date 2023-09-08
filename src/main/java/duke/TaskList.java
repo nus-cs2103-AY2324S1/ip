4302710ca7a13005b6bd930d0ab30743b2774995
yuxunn <teeyuxun@gmail.com>
2023-09-08 19:38:30 +0800

    branch-Level-10

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -35,13 +35,16 @@ public class TaskList {
      * @param action String of name of action.
      */
 
-    public static void todo(String action) {
+    public static String todo(String action) {
         isDone[counter] = false;
         actions[counter] = action;
         type[counter] = "T";
         counter= counter + 1;
-        System.out.println("Got it. I've added this task:\n" + helper(action, "T", false));
-        System.out.println("Now you have " + counter + " tasks in the list.");
+        String response = "";
+        response = "Got it. I've added this task:\n" + helper(action, "T", false) + "\nNow you have " + counter + " tasks in the list.";
+        Storage.save("data/duke.txt", actions, type, isDone, dueString, startTime, endTime, counter);
+
+        return response;
     }
 
 
@@ -72,17 +75,17 @@ public class TaskList {
      * @param due String of when the task is due.
      * @param by LocalDateTime of formatted due.
      */
-    public static void deadline(String action, String due, LocalDateTime by) {
+    public static String deadline(String action, String due, LocalDateTime by) {
         String byString = due.trim();
         dueString[counter] = byString;
         actions[counter] = action;
-        // System.out.println(action + "hello");
         type[counter] = "D";
         isDone[counter] = false;
         counter = counter + 1;
-        System.out.println("Got it. I've added this task: \n" + helper(action, "D", false) + " (by" + " " + byString + ")");
-        System.out.println("Now you have " + counter + " tasks in the list.");
-
+        String response = "";
+        response = "Got it. I've added this task: \n" + helper(action, "D", false) + " (by" + " " + byString + ")" + "\n Now you have " + counter + " tasks in the list.";
+        Storage.save("data/duke.txt", actions, type, isDone, dueString, startTime, endTime, counter);
+        return response;
     }
 
     /**
@@ -91,7 +94,7 @@ public class TaskList {
      * @param start String indicating start time.
      * @param end String indicating the end time.
      */
-    public static void event(String action, String start, String end) {
+    public static String event(String action, String start, String end) {
         String from = start.trim();
         String to = end.trim();
         LocalDateTime startTimeEvent = helper2(from);
@@ -102,9 +105,37 @@ public class TaskList {
         isDone[counter] = false;
         type[counter] = "E";
         counter = counter + 1;
-        System.out.println("Got it. I've added this task:\n" + helper(action, "E", false) + " (from: " + from + " to: " + to + ")");
-        System.out.println("Now you have " + counter + " tasks in the list.");
+        String response = "";
+
+       response = "Got it. I've added this task:\n" + helper(action, "E", false) + " (from: " + from + " to: " + to + ")" + "\nNow you have " + counter + " tasks in the list.";
+        Storage.save("data/duke.txt", actions, type, isDone, dueString, startTime, endTime, counter);
+        return response;
     }
+
+
+    public static String list(TaskList taskList) {
+        StringBuilder response = new StringBuilder("Here are the tasks in your list:\n");
+
+        for (int i = 0; i < taskList.counter; i++) {
+            String DMYString = "";
+
+            if (taskList.type[i].equals("D")) {
+                DMYString = "by " + taskList.dueString[i];
+            } else if (taskList.type[i].equals("E")) {
+                DMYString = "from " + taskList.startTime[i] + " to " + taskList.endTime[i];
+            }
+
+            response.append((i + 1) + "." + taskList.helper(taskList.actions[i], taskList.type[i], taskList.isDone[i]) + " " + DMYString);
+
+            // Append a newline character after each task except the last one
+            if (i < taskList.counter - 1) {
+                response.append("\n");
+            }
+        }
+
+        return response.toString();
+    }
+
 
     /**
      * Helper method to format the string into the LocalDateTime format.
