eb75dbe43c95a919bdb7e8770dc5f2510d2dff4f
tayian <90464339+tayian@users.noreply.github.com>
2023-09-09 01:07:28 +0800

    Add JavaDoc for Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,28 +1,61 @@
+/**
+ * Represents a task.
+ */
 public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructor for Task.
+     * 
+     * @param description Description of the task.
+     * @param isDone      Whether the task is done.
+     */
     public Task(String description, boolean isDone) {
         this.description = description;
         this.isDone = isDone;
     }
 
+    /**
+     * Returns the description of the task.
+     * 
+     * @return Description of the task.
+     */
     public String getDescription() {
         return this.description; // getter
     }
 
+    /**
+     * Returns the isDone status of the task.
+     * 
+     * @return Whether the task is done.
+     */
     public String getStatusIcon() {
         return (isDone ? "[X] " : "[ ] "); // mark done task with X
     }
 
+    /**
+     * Returns the status and description of the task, to output to the user.
+     * 
+     * @return A String, properly formatting the status and description of the task.
+     */
     public String toString() {
         return this.getStatusIcon() + this.description;
     }
 
+    /**
+     * Toggle the isDone status of the task.
+     */
     public void toggle() {
         this.isDone = !this.isDone;
     }
 
+    /**
+     * Analyse line of text in tasks.txt and returns the corresponding task.
+     * 
+     * @param taskLine  Line of text in tasks.txt
+     * @return The corresponding task
+     */
     public static Task fromString(String taskLine) {
         if (taskLine.charAt(0) == 'D') {
             String[] parts = taskLine.split("\\|"); // Split the string by the | character
