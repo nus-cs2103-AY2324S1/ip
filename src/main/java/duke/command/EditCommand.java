baaca328c395dbb35ac1ffe37db47536df518d64
Rui Shan <ruishan.teo03@gmail.com>
2023-09-08 14:12:05 +0800

    Add edit task functionalities
    Tasks cannot be edited without being deleted.
    
    Users need to delete and create a new task to update list.
    
    Let's
    * Create new EditCommand to execute editing functionalities.
    * Relocate logic of parsing tasks so that new task can be created.
    * Update TaskList to include editTask method.

---
+++ src/main/java/duke/command/EditCommand.java
@@ -0,0 +1,40 @@
+package duke.command;
+
+import duke.DukeException;
+import duke.Storage;
+import duke.TaskList;
+import duke.ui.Ui;
+import duke.task.Task;
+
+/**
+ * Represents a command to edit a task from the task list in the Duke application.
+ */
+public class EditCommand extends Command {
+    private int index;
+    private Task task;
+
+    /**
+     * Constructs an EditCommand object with the specified task to be edit.
+     *
+     * @param index The index of the task to be deleted.
+     */
+    public EditCommand(int index, Task task) {
+        this.index = index;
+        this.task = task;
+    }
+
+    /**
+     * Executes the "edit" command, adding the stored task to the task list and updating storage.
+     *
+     * @param tasks   The task list that the command may operate on.
+     * @param ui      The user interface to interact with the user or display messages.
+     * @param storage The storage handler to manage data persistence.
+     * @throws DukeException If an error occurs while executing the command.
+     */
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
+        tasks.editTask(index, task, ui);
+        storage.saveFile(tasks);
+    }
+}
+
