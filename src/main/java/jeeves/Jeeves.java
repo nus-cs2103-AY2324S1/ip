db5aa6423c858f9f78ef52eff188303007571564
JasonLCY-Temp <e0407197@u.nus.edu>
2023-09-08 17:09:51 +0800

    Add functioning GUI for Jeeves

--- src/main/java/jeeves/Jeeves.java
+++ src/main/java/jeeves/Jeeves.java
@@ -2,8 +2,8 @@ package jeeves;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Scanner;
 
+import javafx.application.Application;
 import jeeves.exception.DeletedIdException;
 import jeeves.parser.Parser;
 import jeeves.storage.Storage;
@@ -22,22 +22,10 @@ public class Jeeves {
     private static final String RELATIVE_PATH_DATA_DIRECTORY = "data";
     private static final String RELATIVE_PATH_DATA_FILE = "data/JeevesData.txt";
 
-    private final Storage storage;
-    private final TaskList tasks;
-    private final Parser parser;
-    private final Ui ui;
-
-    /**
-     * Default constructor for a Jeeves object.
-     * Currently, requires no arguments to initialize.
-     * Creates a new Storage, TaskList, Parser and Ui object for usage during runtime.
-     */
-    public Jeeves() {
-        storage = new Storage(RELATIVE_PATH_DATA_DIRECTORY, RELATIVE_PATH_DATA_FILE);
-        tasks = new TaskList(storage.readTasklistFromFile());
-        parser = new Parser();
-        ui = new Ui();
-    }
+    private static final Storage storage = new Storage(RELATIVE_PATH_DATA_DIRECTORY, RELATIVE_PATH_DATA_FILE);
+    private static final TaskList tasks = new TaskList(storage.readTasklistFromFile());
+    private static final Parser parser = new Parser();
+    private static final Ui ui = new Ui();
 
     /**
      * Main process.
@@ -45,7 +33,7 @@ public class Jeeves {
      * @param args Optional command line arguments.
      */
     public static void main(String[] args) {
-        new Jeeves().run();
+        Application.launch(Ui.class);
     }
 
     /**
@@ -53,120 +41,122 @@ public class Jeeves {
      * Constantly loops and waits for a new line of user input.
      * Processes the input depending on what was entered.
      */
-    public void run() {
-        ui.printGreeting();
-
-        Scanner sc = new Scanner(System.in);
-
-        // Waits for user input and process it accordingly
-        while (true) {
-            // Reads the user input and parses the relevant tokens for use
-            ArrayList<String> tokens = parser.parseUserInput(sc.nextLine());
-            String currentCommand = tokens.get(0);
-            // Performs a different action depending on the input received
-            // Unless a specific pre-defined command is received, the program will
-            // print a default error message.
-            if (currentCommand.equals("list")) {
-                // Displays a different message if no task is being tracked
-                if (Task.getTaskCount() == 0) {
-                    System.out.println("I am not currently tracking anything for you Master");
-                } else {
-                    System.out.println("This is what I am tracking for you Master");
-                }
-
-                // Displays the current list of tasks tracked and their status
-                for (int i = 1; i <= Task.getTaskCount(); i++) {
-                    if (tasks.getTask(i) != null) {
-                        tasks.printTask(i);
-                    }
-                }
-                // Prints an empty line for output clarity
-                System.out.print("\n");
-            } else if (currentCommand.equals("mark")) {
-                // Get the id and try to process the marking
-                int id = Integer.parseInt(tokens.get(1));
-
-                try {
-                    if (tasks.getTask(id) == null) {
-                        // If the id to be marked belongs to a deleted task (null), throws the DeletedIdException
-                        throw new DeletedIdException();
-                    }
-
-                    tasks.markTask(id);
-                    System.out.println("Understood, I have marked the following task as done:");
-                    System.out.println("    ");
-                    tasks.printTask(id);
-                    System.out.println("\n");
-                } catch (DeletedIdException e) {
-                    System.out.println(e.getMessage());
-                }
-            } else if (currentCommand.equals("unmark")) {
-                int id = Integer.parseInt(tokens.get(1));
-
-                try {
-                    if (tasks.getTask(id) == null) {
-                        // If the id to be marked belongs to a deleted task (null), throws the DeletedIdException
-                        throw new DeletedIdException();
-                    }
-
-                    tasks.unmarkTask(id);
-                    System.out.println("Understood, I have marked the following task as not done:");
-                    System.out.println("    "
-                            + tasks.getTask(id).toString() + "\n");
-                } catch (DeletedIdException e) {
-                    System.out.println(e.getMessage());
-                }
-            } else if (currentCommand.equals("delete")) {
-                int id = Integer.parseInt(tokens.get(1));
-
-                try {
-                    if (tasks.getTask(id) == null) {
-                        // If the id to be marked belongs to a deleted task (null), throws the DeletedIdException
-                        throw new DeletedIdException();
-                    }
-
-                    System.out.println("Understood, I have deleted the following task:");
-                    System.out.println("    " + tasks.getTask(id).toString() + "\n");
-                    tasks.setTask(id, null);
-                } catch (DeletedIdException e) {
-                    System.out.println(e.getMessage());
-                }
-            } else if (currentCommand.equals("todo")) {
-                // Adds the To.do normally to the task list
-                Todo newTodo = new Todo(tokens.get(1));
-                tasks.addTaskAtIndex(Task.getTaskCount(), newTodo);
-                System.out.println("Task added:\n"
-                        + "    " + newTodo + "\n");
-
-            } else if (currentCommand.equals("deadline")) {
-                // Checks if the Date input is in the accepted format
-                LocalDate deadline = LocalDate.parse(tokens.get(2));
-
-                // Adds the 'Deadline' Task to the task list
-                Deadline newDeadline = new Deadline(tokens.get(1), deadline);
-                tasks.addTaskAtIndex(Task.getTaskCount(), newDeadline);
-                System.out.println("Deadline added:\n"
-                        + "    "
-                        + newDeadline + "\n");
-            } else if (currentCommand.equals("event")) {
-                // Adds the 'Event' Task to the task list
-                Event newEvent = new Event(tokens.get(1), tokens.get(2), tokens.get(3));
-                tasks.addTaskAtIndex(Task.getTaskCount(), newEvent);
-                System.out.println("Event added:\n"
-                        + "    " + newEvent + "\n");
-            } else if (currentCommand.equals("find")) {
-                tasks.searchFor(tokens.get(1));
-            } else if (currentCommand.equals("bye")) {
-                // Before the actual termination of the program, writes the current task list to the external file.
-                storage.writeTasklistToFile(tasks.getTaskListDataAsString());
-
-                // Displays the farewell message and terminates the application
-                ui.printFarewell();
-                System.exit(0);
+    public static String processInput(String inputLine) {
+        // Reads the user input and parses the relevant tokens for use
+        ArrayList<String> tokens = parser.parseUserInput(inputLine);
+        String currentCommand = tokens.get(0);
+        // Performs a different action depending on the input received
+        // Unless a specific pre-defined command is received, the program will
+        // print a default error message.
+        if (currentCommand.equals("list")) {
+            // Displays a different message if no task is being tracked
+            StringBuilder sb = new StringBuilder();
+            if (Task.getTaskCount() == 0) {
+                return "I am not currently tracking anything for you Master\n";
             } else {
-                // By default, informs the user that the command is not recognized.
-                ui.printInvalidCommand();
+                sb.append("This is what I am tracking for you Master\n");
             }
+
+            // Displays the current list of tasks tracked and their status
+            for (int i = 1; i <= Task.getTaskCount(); i++) {
+                if (tasks.getTask(i) != null) {
+                    sb.append(tasks.getTask(i).toString());
+                    sb.append("\n");
+                }
+            }
+            sb.append("\n");
+            return sb.toString();
+        } else if (currentCommand.equals("mark")) {
+            // Get the id and try to process the marking
+            int id = Integer.parseInt(tokens.get(1));
+
+            try {
+                if (tasks.getTask(id) == null) {
+                    // If the id to be marked belongs to a deleted task (null), throws the DeletedIdException
+                    throw new DeletedIdException();
+                }
+
+                tasks.markTask(id);
+                return "Understood, I have marked the following task as done:\n"
+                                + "    "
+                                + tasks.getTask(id).toString()
+                                + "\n";
+            } catch (DeletedIdException e) {
+                return e.getMessage();
+            }
+        } else if (currentCommand.equals("unmark")) {
+            int id = Integer.parseInt(tokens.get(1));
+
+            try {
+                if (tasks.getTask(id) == null) {
+                    // If the id to be marked belongs to a deleted task (null), throws the DeletedIdException
+                    throw new DeletedIdException();
+                }
+
+                tasks.unmarkTask(id);
+                return "Understood, I have marked the following task as not done:\n"
+                                + "    "
+                                + tasks.getTask(id).toString()
+                                + "\n";
+            } catch (DeletedIdException e) {
+                return e.getMessage();
+            }
+        } else if (currentCommand.equals("delete")) {
+            int id = Integer.parseInt(tokens.get(1));
+
+            try {
+                if (tasks.getTask(id) == null) {
+                    // If the id to be marked belongs to a deleted task (null), throws the DeletedIdException
+                    throw new DeletedIdException();
+                }
+
+                tasks.setTask(id, null);
+                return "Understood, I have deleted the following task:\n"
+                                + "    " 
+                                + tasks.getTask(id).toString()
+                                + "\n";
+            } catch (DeletedIdException e) {
+                return e.getMessage();
+            }
+        } else if (currentCommand.equals("todo")) {
+            // Adds the To.do normally to the task list
+            Todo newTodo = new Todo(tokens.get(1));
+            tasks.addTaskAtIndex(Task.getTaskCount(), newTodo);
+            return "Task added:\n"
+                            + "    "
+                            + newTodo
+                            + "\n";
+        } else if (currentCommand.equals("deadline")) {
+            // Checks if the Date input is in the accepted format
+            LocalDate deadline = LocalDate.parse(tokens.get(2));
+
+            // Adds the 'Deadline' Task to the task list
+            Deadline newDeadline = new Deadline(tokens.get(1), deadline);
+            tasks.addTaskAtIndex(Task.getTaskCount(), newDeadline);
+            return "Deadline added:\n"
+                            + "    "
+                            + newDeadline + "\n";
+        } else if (currentCommand.equals("event")) {
+            // Adds the 'Event' Task to the task list
+            Event newEvent = new Event(tokens.get(1), tokens.get(2), tokens.get(3));
+            tasks.addTaskAtIndex(Task.getTaskCount(), newEvent);
+            return "Event added:\n"
+                            + "    " 
+                            + newEvent
+                            + "\n";
+        } else if (currentCommand.equals("find")) {
+            tasks.searchFor(tokens.get(1));
+        } else if (currentCommand.equals("bye")) {
+            // Before the actual termination of the program, writes the current task list to the external file.
+            storage.writeTasklistToFile(tasks.getTaskListDataAsString());
+
+            // Displays the farewell message and terminates the application
+            return "I bid you farewell, Master\n";
         }
+        
+        // By default, informs the user that the command is not recognized.
+        return "Apologies Master, I am unable to understand that command.\n"
+                        + "I will improve myself to better serve you in the future.\n";
     }
+
 }
