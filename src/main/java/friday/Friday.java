ff104646bd7d5dcad1a8e777eaed9a9868cf2091
ryantzr1 <dreisprung1821@gmail.com>
2023-09-09 11:16:27 +0800

    Refactor to integrate parser
    This is to make the code more OOP and remove
    unnecessary code blocks.

--- src/main/java/friday/Friday.java
+++ src/main/java/friday/Friday.java
@@ -1,32 +1,20 @@
 package friday;
 
 import java.util.Scanner;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.scene.layout.Region;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 
 /**
  * Represents the main class for the Friday application.
  */
-public class Friday extends Application {
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image friday = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
-
+public class Friday {
     private TaskList taskList;
     private Storage storage;
     private Scanner input;
     private Ui ui;
     private Parser parser;
     private TextField userInput;
-
     private VBox dialogContainer;
 
     /**
@@ -44,87 +32,7 @@ public class Friday extends Application {
     }
 
     public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) {
-        // Step 1. Setting up required components
-        ScrollPane scrollPane = new ScrollPane();
-        scrollPane.setContent(dialogContainer);
-
-        Button sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput);
-
-        Scene scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        // Step 2. Formatting the window
-        stage.setTitle("Friday");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        // Step 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> {
-            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
-            handleUserInput();
-        });
-    }
-
-    /**
-     * Creates a label with the specified text and adds it to the dialog container.
-     * @param text String containing text to add
-     * @return a label with the specified text that has word wrap enabled.
-     */
-    private Label getDialogLabel(String text) {
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
-        return textToAdd;
-    }
-
-    /**
-     * Creates two dialog boxes, one echoing user input and the other containing Friday's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userInput.getText(), user),
-                DialogBox.getFridayDialog(userInput.getText(), friday)
-        );
-        userInput.clear();
+        Launcher.main(args);
     }
 
     /**
@@ -134,11 +42,9 @@ public class Friday extends Application {
      */
     public String getResponse(String input) {
         input = input.toLowerCase().trim();
-
         if (input.equals("hello") || input.equals("hi") || input.equals("hey")) {
             return "Hello! How can I assist you today?";
         }
-
 
         if (input.contains("thank")) {
             return "You're welcome!";
@@ -152,6 +58,6 @@ public class Friday extends Application {
             return "Goodbye! If you need anything else, just let me know.";
         }
 
-        return "I'm sorry, I didn't quite catch that. Can you rephrase or ask something else?";
+        return parser.processUserCommand(input, taskList, storage);
     }
 }
--- src/main/java/friday/Parser.java
+++ src/main/java/friday/Parser.java
@@ -18,90 +18,84 @@ public class Parser {
     }
 
     /**
-     * Processes user inputs and interacts with the TaskList to execute user commands.
-     * Continues to run until the user inputs the "bye" command.
+     * Processes user input and interacts with the TaskList to execute user commands.
+     * @param userInput The command input by the user.
+     * @param taskList The list of tasks.
+     * @param storage The storage object to save tasks.
      */
-    public void echo(Scanner input, TaskList taskList, Ui ui, Storage storage) {
-        while (input.hasNextLine()) {
-            try {
-                String userInput = input.nextLine();
-                if (userInput.equals("bye")) {
-                    ui.bye();
-                    break;
-                } else if (userInput.equals("list")) {
-                    ui.listTasks(taskList);
-                } else if (userInput.contains("unmark")) {
-                    int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
-                    ui.unmark();
-                    taskList.unmark(taskNumber - 1);
-                    saveTasks(taskList.toString(), storage);
-                } else if (userInput.contains("mark")) {
-                    int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
-                    ui.mark();
-                    taskList.mark(taskNumber - 1);
-                    saveTasks(taskList.toString(), storage);
-                } else if (userInput.contains("delete")) {
-                    int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
-                    taskList.delete(taskNumber - 1);
-                    saveTasks(taskList.toString(), storage);
-                } else if (userInput.contains("find")) {
-                    String[] findInput = userInput.split(" ", 2);
-                    if (findInput.length < 2 || findInput[1].trim().isEmpty()) {
-                        ui.showFindError();
-                    }
-                    TaskList result = taskList.findTasks(findInput[1]);
-                    ui.findTasks(result);
-                } else if (userInput.contains("todo")) {
-                    String[] todoInput = userInput.split(" ", 2);
-                    if (todoInput.length < 2 || todoInput[1].trim().isEmpty()) {
-                        ui.showTodoError();
-                    }
-                    Todo todo = new Todo(todoInput[1]);
-                    ui.addTask(todo);
-                    taskList.add(todo);
-                    ui.taskListMessage(taskList);
-                    saveTasks(taskList.toString(), storage);
-
-                } else if (userInput.contains("deadline")) {
-                    String[] commandAndDetails = userInput.split(" ", 2);
-                    if (commandAndDetails.length < 2 || !userInput.contains("/by")) {
-                        ui.showDeadlineFormatError();
-                    }
-                    String[] taskAndDate = commandAndDetails[1].split(" /by ", 2);
-                    if (taskAndDate.length < 2) {
-                        ui.showDeadlineMissingError();
-                    }
-                    String taskDescription = taskAndDate[0];
-                    String deadlineDate = taskAndDate[1];
-                    Deadline deadline = new Deadline(taskDescription, deadlineDate);
-                    ui.addTask(deadline);
-                    taskList.add(deadline);
-                    ui.taskListMessage(taskList);
-                    saveTasks(taskList.toString(), storage);
-                } else if (userInput.contains("event")) {
-                    String[] commandAndDetails = userInput.split(" ", 2);
-                    if (commandAndDetails.length < 2 || !userInput.contains("/from") || !userInput.contains("/to")) {
-                        ui.showEventFormatError();
-                    }
-                    String[] taskAndTimes = commandAndDetails[1].split(" /from | /to ", 3);
-                    if (taskAndTimes.length < 3) {
-                        ui.showEventMissingError();
-                    }
-                    String taskDescription = taskAndTimes[0];
-                    String startTime = taskAndTimes[1];
-                    String endTime = taskAndTimes[2];
-                    Event event = new Event(taskDescription, startTime, endTime);
-                    ui.addTask(event);
-                    taskList.add(event);
-                    ui.taskListMessage(taskList);
-                    saveTasks(taskList.toString(), storage);
-                } else {
-                    ui.showBadInputError();
+    public String processUserCommand(String userInput, TaskList taskList, Storage storage) {
+        try {
+            if (userInput.equals("bye")) {
+                return "Bye. Hope to see you again soon!";
+            } else if (userInput.equals("list")) {
+                return "Here are the tasks in your list:\n" + taskList.toString();
+            } else if (userInput.contains("unmark")) {
+                int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
+                taskList.unmark(taskNumber - 1);
+                saveTasks(taskList.toString(), storage);
+                return "Nice! I've marked this task as not done yet:";
+            } else if (userInput.contains("mark")) {
+                int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
+                taskList.mark(taskNumber - 1);
+                saveTasks(taskList.toString(), storage);
+                return "Nice! I've marked this task as done:";
+            } else if (userInput.contains("delete")) {
+                int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
+                taskList.delete(taskNumber - 1);
+                saveTasks(taskList.toString(), storage);
+                return "Task deleted successfully!";
+            } else if (userInput.contains("find")) {
+                String[] findInput = userInput.split(" ", 2);
+                if (findInput.length < 2 || findInput[1].trim().isEmpty()) {
+                    return "Oops! Please add a keyword to search for!";
                 }
-            } catch (FridayException e) {
-                ui.showLoadingError(e);
-                taskList = new TaskList();
+                TaskList result = taskList.findTasks(findInput[1]);
+                return "Here are the matching tasks in your list:\n" + result.toString();
+            } else if (userInput.contains("todo")) {
+                String[] todoInput = userInput.split(" ", 2);
+                if (todoInput.length < 2 || todoInput[1].trim().isEmpty()) {
+                    return "OOPS!!! The description of a todo cannot be empty.";
+                }
+                Todo todo = new Todo(todoInput[1]);
+                taskList.add(todo);
+                saveTasks(taskList.toString(), storage);
+                return "added: " + todo.toString();
+            } else if (userInput.contains("deadline")) {
+                String[] commandAndDetails = userInput.split(" ", 2);
+                if (commandAndDetails.length < 2 || !userInput.contains("/by")) {
+                    return "Incorrect format for 'deadline'. Here is a sample:\ndeadline return book /by Sunday";
+                }
+                String[] taskAndDate = commandAndDetails[1].split(" /by ", 2);
+                if (taskAndDate.length < 2) {
+                    return "Please provide both a task description and a deadline date.";
+                }
+                String taskDescription = taskAndDate[0];
+                String deadlineDate = taskAndDate[1];
+                Deadline deadline = new Deadline(taskDescription, deadlineDate);
+                taskList.add(deadline);
+                saveTasks(taskList.toString(), storage);
+                return "added: " + deadline.toString();
+            } else if (userInput.contains("event")) {
+                String[] commandAndDetails = userInput.split(" ", 2);
+                if (commandAndDetails.length < 2 || !userInput.contains("/from") || !userInput.contains("/to")) {
+                    return "Incorrect format for 'event'. Expected format: event TASK_DESCRIPTION /from START_TIME /to END_TIME";
+                }
+                String[] taskAndTimes = commandAndDetails[1].split(" /from | /to ", 3);
+                if (taskAndTimes.length < 3) {
+                    return "Please provide a task description, start time, and end time.";
+                }
+                String taskDescription = taskAndTimes[0];
+                String startTime = taskAndTimes[1];
+                String endTime = taskAndTimes[2];
+                Event event = new Event(taskDescription, startTime, endTime);
+                taskList.add(event);
+                saveTasks(taskList.toString(), storage);
+                return "added: " + event.toString();
+            } else {
+                return "OOPS!!! I'm sorry, but I don't know what that means :-(";
             }
+        } catch (Exception e) {
+            return "An error occurred: " + e.getMessage();
         }
     }
 }
