9419763a49aa6f430df118ac137c34e11e801f31
seanpzk <seanpzk2@gmail.com>
2023-09-09 15:27:05 +0800

    Implement switch case in Parser for better readability

--- src/main/java/duke/userio/Parser.java
+++ src/main/java/duke/userio/Parser.java
@@ -39,76 +39,94 @@ public class Parser {
      * @throws InvalidUserInputException User input does not generate any specific response.
      */
     public String listen(String input) throws InvalidUserInputException {
-        if (input.equals("bye")) {
-            botInUse = false;
-            return ui.bye();
-        } else if (input.equals("list")) {
-            String outputList = taskList.outputNumberedList();
-            return ui.list(outputList);
-        } else if (input.contains("unmark")) {
-            int a = Integer.parseInt(input.substring(7));
-            Task t = taskList.getTask(a - 1);
-            t.markAsUndone();
-            updateSaveFile();
-            return ui.unmarkTask(t);
-        } else if (input.contains("mark")) {
-            int a = Integer.parseInt(input.substring(5));
-            Task t = taskList.getTask(a - 1);
-            t.markAsDone();
-            updateSaveFile();
-            return ui.markTask(t);
-        } else if (input.contains("todo")) {
-            try {
-                String toDoDescription = input.split("todo ")[1];
-                ToDo toDoTask = new ToDo(toDoDescription);
-                taskList.addTask(toDoTask);
+        String response;
+        int taskIndexHolder;
+        String keyword = extractKeyword(input);
+        switch (keyword) {
+            case "bye":
+                botInUse = false;
+                response = ui.bye();
+                break;
+            case "list":
+                String outputList = taskList.outputNumberedList();
+                response = ui.list(outputList);
+                break;
+            case "unmark":
+                taskIndexHolder = Integer.parseInt(input.substring(7));
+                Task taskToBeUnmarked = taskList.getTask(taskIndexHolder - 1);
+                taskToBeUnmarked.markAsUndone();
                 updateSaveFile();
-                return ui.toDoAdded(toDoTask, taskList);
-            } catch (ArrayIndexOutOfBoundsException e) {
-                return ui.toDoMissingContent();
-            }
-        } else if (input.contains("deadline")) {
-            try {
-                String by = input.split("/by ")[1];
-                String deadlineDescription = input.split("deadline ")[1].split(" /by")[0];
-                Deadline deadlineTask = new Deadline(deadlineDescription, by);
-                taskList.addTask(deadlineTask);
+                response = ui.unmarkTask(taskToBeUnmarked);
+                break;
+            case "mark":
+                taskIndexHolder = Integer.parseInt(input.substring(5));
+                Task taskToBeMarked = taskList.getTask(taskIndexHolder - 1);
+                taskToBeMarked.markAsDone();
                 updateSaveFile();
-                return ui.deadlineAdded(deadlineTask, taskList);
-            } catch (ArrayIndexOutOfBoundsException e) {
-                return ui.deadlineMissingContent();
-            }
-        } else if (input.contains("event")) {
-            try {
-                String from = input.split("/from ")[1].split(" /to")[0];
-                String to = input.split("/to ")[1];
-                String eventDescription = input.split("event ")[1].split(" /from")[0];
-                Event eventTask = new Event(eventDescription, from, to);
-                taskList.addTask(eventTask);
-                updateSaveFile();
-                return ui.eventAdded(eventTask, taskList);
-            } catch (ArrayIndexOutOfBoundsException e) {
-                return ui.eventMissingContent();
-            }
-        } else if (input.contains("delete")) {
-            int a = Integer.parseInt(input.substring(7));
-            Task toBeRemoved = taskList.getTask(a - 1);
-            taskList.deleteTask(a - 1);
-            updateSaveFile();
-            return ui.taskDeleted(toBeRemoved, taskList);
-        } else if (input.contains("find")) {
-            String inputToFind = input.substring(6);
-            TaskList tempTL = new TaskList();
-            for (int i = 0; i < taskList.getSize(); i++) {
-                Task t = taskList.getTask(i);
-                if (t.containString(inputToFind)) {
-                    tempTL.addTask(t);
+                response = ui.markTask(taskToBeMarked);
+                break;
+            case "todo":
+                try {
+                    String toDoDescription = input.split("todo ")[1];
+                    ToDo toDoTask = new ToDo(toDoDescription);
+                    taskList.addTask(toDoTask);
+                    updateSaveFile();
+                    response = ui.toDoAdded(toDoTask, taskList);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    response = ui.toDoMissingContent();
                 }
-            }
-            return ui.findResponse(tempTL.outputNumberedList());
-        } else {
-            throw new InvalidUserInputException();
+                break;
+            case "deadline":
+                try {
+                    String by = input.split("/by ")[1];
+                    String deadlineDescription = input.split("deadline ")[1].split(" /by")[0];
+                    Deadline deadlineTask = new Deadline(deadlineDescription, by);
+                    taskList.addTask(deadlineTask);
+                    updateSaveFile();
+                    response = ui.deadlineAdded(deadlineTask, taskList);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    response = ui.deadlineMissingContent();
+                }
+                break;
+            case "event":
+                try {
+                    String from = input.split("/from ")[1].split(" /to")[0];
+                    String to = input.split("/to ")[1];
+                    String eventDescription = input.split("event ")[1].split(" /from")[0];
+                    Event eventTask = new Event(eventDescription, from, to);
+                    taskList.addTask(eventTask);
+                    updateSaveFile();
+                    response = ui.eventAdded(eventTask, taskList);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    response = ui.eventMissingContent();
+                }
+                break;
+            case "delete":
+                taskIndexHolder = Integer.parseInt(input.substring(7));
+                Task toBeRemoved = taskList.getTask(taskIndexHolder - 1);
+                taskList.deleteTask(taskIndexHolder - 1);
+                updateSaveFile();
+                response = ui.taskDeleted(toBeRemoved, taskList);
+                break;
+            case "find":
+                String inputToFind = input.substring(6);
+                TaskList tempTL = new TaskList();
+                for (int i = 0; i < taskList.getSize(); i++) {
+                    Task t = taskList.getTask(i);
+                    if (t.containString(inputToFind)) {
+                        tempTL.addTask(t);
+                    }
+                }
+                response = ui.findResponse(tempTL.outputNumberedList());
+                break;
+            default:
+                throw new InvalidUserInputException();
         }
+        return response;
+    }
+
+    private String extractKeyword(String input) {
+        return input.split(" ")[0];
     }
 
     /**
