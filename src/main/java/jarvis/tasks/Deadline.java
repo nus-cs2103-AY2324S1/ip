a85cddba248e412dde7f4cc26bf0194ab506fdfd
Evan Yan <103996156+evanyan13@users.noreply.github.com>
2023-09-09 19:34:34 +0800

    Edit syntax errors

--- src/main/java/jarvis/tasks/Deadline.java
+++ src/main/java/jarvis/tasks/Deadline.java
@@ -14,7 +14,7 @@ public class Deadline extends Task {
 
     public Deadline(String title, LocalDateTime dueDate, boolean isCompleted) {
         super(title, isCompleted);
-        this.dueDate = dueDate;
+        this.dueDateTime = dueDateTime;
     }
     
      /**
@@ -25,7 +25,7 @@ public class Deadline extends Task {
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(Ui.DATE_TIME_FORMAT);
-        String formattedDueDate = dueDate.format(formatter);
+        String formattedDueDate = dueDateTime.format(formatter);
         return "D | " + super.toString() + " | " + formattedDueDate;
     }
 }
