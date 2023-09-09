95556503cd1909685dce95cb0355ee9f0c896836
neyapraveen <neyapraveen@gmail.com>
2023-09-09 19:08:39 +0800

    Create Ui class

---
+++ src/main/java/Ui.java
@@ -0,0 +1,9 @@
+public class Ui {
+    public static String horizontalLine = "_".repeat(60) + "\n";
+    public static void greet() {
+        System.out.println("Hello! I'm Bot\n" + "What can I do for you?\n" + horizontalLine);
+    }
+    public static void sayBye() {
+        System.out.println(horizontalLine+ "Bye. Hope to see you again soon!\n" + horizontalLine);
+    }
+}
