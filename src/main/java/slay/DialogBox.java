702da25e1eab749d029c094e78e0b21f9e86a9ba
xyT <tongyu5336@gmail.com>
2023-09-10 13:05:08 +0800

    Level-10 round user image

--- src/main/java/slay/DialogBox.java
+++ src/main/java/slay/DialogBox.java
@@ -13,6 +13,9 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 
 /**
  * An example of a custom control using FXML.
@@ -22,8 +25,9 @@ import javafx.scene.layout.HBox;
 public class DialogBox extends HBox {
     @FXML
     private Label dialog;
+
     @FXML
-    private ImageView displayPicture;
+    private Circle circleFrame;
 
     private DialogBox(String text, Image img) {
         try {
@@ -36,7 +40,8 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
-        displayPicture.setImage(img);
+        circleFrame.setStroke(Color.ALICEBLUE);
+        circleFrame.setFill(new ImagePattern(img));
     }
 
     /**
