1025bf67c0c8b6829ed5d68369a993d2e11535d8
ChongWeiJie29 <chongweijie29@gmail.com>
2023-09-10 17:41:11 +0800

    Add to Level 10: GUI
    Change profile picture of bot and user.

--- src/main/java/dukepackage/Main.java
+++ src/main/java/dukepackage/Main.java
@@ -31,6 +31,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("ChampionSOS");
             fxmlLoader.<Gui>getController().setDuke(duke);
             stage.show();
         } catch (IOException e) {
