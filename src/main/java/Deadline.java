fe6a5b7f7889c8292756de0b61e0cecad7580336
Song_Bingxi <bingxi.bobby.song@gmail.com>
2023-09-10 14:49:22 +0800

    updated deadline.java with functions

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -12,9 +12,18 @@ public class Deadline extends Task {
         return "[D]" + super.toString() + " (by: " + deadline + ")";
     }
 
+    @Override
+    public String saveToString() {
+        return "deadline," + super.saveToString() + "," + deadline;
+    }
+
     // Constructor
     public Deadline(String description, String deadline) {
         super(description);
+        this.deadline = deadline;
+    }
+    public Deadline(String description, int mark, String deadline) {
+        super(description, mark != 0);
         this.deadline = deadline;
     }
 }
