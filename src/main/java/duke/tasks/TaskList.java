e4c4f10da52d70454568455499396fb64e4c0819
wjayee <wong.jayee@gmail.com>
2023-09-11 01:29:21 +0800

    Change code quality
    Certain code in some files are not of good quality
    
    Minor changes are made to improve the code quality of some methods
    
    It improves its readability and removes code that are error prone

--- src/main/java/duke/tasks/TaskList.java
+++ src/main/java/duke/tasks/TaskList.java
@@ -101,7 +101,7 @@ public class TaskList {
         for (int i = 0; i < tasks.size(); i++) {
             if (typeOfDes == 0) {
                 output.add(tasks.get(i).toString());
-            } else {
+            } else if (typeOfDes == 1) {
                 output.add(tasks.get(i).getTask());
             }
         }
@@ -113,8 +113,8 @@ public class TaskList {
      * @param input The user input.
      * @param command Type of command given by the user.
      * @return Dialogue for the bot to confirm status of the task.
-     * @throws DukeException Exceptions.InvalidInputException thrown if input
-     *     cannot be recognised. Exceptions.MissingTaskException thrown
+     * @throws DukeException InvalidInputException thrown if input
+     *     cannot be recognised. MissingTaskException thrown
      * @throws DukeException InvalidInputException thrown if input
      *     cannot be recognised. MissingTaskException thrown
      *     if task cannot be found in the task list.
@@ -129,6 +129,7 @@ public class TaskList {
 
             } else if (command.equals(Commands.MARK)) {
                 return task.markAsDone();
+
             } else {
                 throw new InvalidInputException("Invalid input");
             }
@@ -144,8 +145,8 @@ public class TaskList {
      * Deletes a task from the task list and returns a String as the dialogue.
      * @param input The user input.
      * @return Dialogue to confirm the deletion of the task from the list.
-     * @throws DukeException Exceptions.InvalidInputException thrown if input
-     *     cannot be recognised. Exceptions.MissingTaskException thrown
+     * @throws DukeException InvalidInputException thrown if input
+     *     cannot be recognised. MissingTaskException thrown
      *     if task cannot be found in the task list.
      */
     public String deleteTask(String input) throws DukeException {
