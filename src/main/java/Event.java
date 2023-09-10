df6fe03c1a771ef343b8085e25bd1fea3efcc861
felibunnyy <felibunbun13@gmail.com>
2023-09-10 22:10:47 +0800

    Level 8. Dates and Times

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,26 +1,43 @@
-public class Event extends Task {
-    protected String from;
-    protected String to;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 
-    public Event(String description, String from, String to) {
+public class Event extends Task {
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+
+    public Event(String description, String from, String to) throws DateTimeParseException {
         super(description);
-        this.from = from;
-        this.to = to;
+        //assumes from and to is either both in wrong format, or both in correct format
+        try {
+            this.from = DateTimeParser.toLocalDateTimeFromWrongFormat(from);
+            this.to = DateTimeParser.toLocalDateTimeFromWrongFormat(to);
+        } catch (DateTimeParseException e1) {
+            // If parsing in the first format fails, try the second format
+            this.from = DateTimeParser.toLocalDateTimeFromCorrectFormat(from);
+            this.to = DateTimeParser.toLocalDateTimeFromCorrectFormat(to);
+        }
     }
 
-    public Event(String description, String from, String to, boolean isDone) {
+    public Event(String description, String from, String to, boolean isDone) throws DateTimeParseException {
         super(description);
-        this.from = from;
-        this.to = to;
+        //assumes from and to is either both in wrong format, or both in correct format
+        try {
+            this.from = DateTimeParser.toLocalDateTimeFromWrongFormat(from);
+            this.to = DateTimeParser.toLocalDateTimeFromWrongFormat(to);
+        } catch (DateTimeParseException e1) {
+            // If parsing in the first format fails, try the second format
+            this.from = DateTimeParser.toLocalDateTimeFromCorrectFormat(from);
+            this.to = DateTimeParser.toLocalDateTimeFromCorrectFormat(to);
+        }
         this.isDone = isDone;
     }
 
     @Override
     public String parse() {
-        return "E | " + this.description + " | " + this.isDone + " | " + this.from + " | " + this.to;
+        return "E | " + this.description + " | " + this.isDone + " | " + DateTimeParser.fromLocalDateTimeToString(this.from) + " | " + DateTimeParser.fromLocalDateTimeToString(this.to);
     }
     @Override
     public String toString() {
-        return  "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
+        return  "[E]" + super.toString() + " (from: " + DateTimeParser.fromLocalDateTimeToString(this.from) + " to: " + DateTimeParser.fromLocalDateTimeToString(this.to) + ")";
     }
 }
