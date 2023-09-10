ba7c244ab18a1f1bce769150cd172fc1d0886b2c
felibunnyy <felibunbun13@gmail.com>
2023-09-10 17:33:54 +0800

    Level 7. Save

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -7,6 +7,18 @@ public class Event extends Task {
         this.from = from;
         this.to = to;
     }
+
+    public Event(String description, String from, String to, boolean isDone) {
+        super(description);
+        this.from = from;
+        this.to = to;
+        this.isDone = isDone;
+    }
+
+    @Override
+    public String parse() {
+        return "E | " + this.description + " | " + this.isDone + " | " + this.from + " | " + this.to;
+    }
     @Override
     public String toString() {
         return  "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,13 @@
-public class Task {
+public abstract class Task {
     protected String description;
     protected boolean isDone;
 
+    public abstract String parse();
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
+
 
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
