f9606909fb15922f6b116d47eb40b665c3a596fb
kokrui <kokruiwong@gmail.com>
2023-09-10 11:19:10 +0800

    refactor: Abstract away user interactions into Ui class

---
+++ src/main/java/Ui.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Ui {
+
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+
+    private final String name;
+    private final Scanner scanner;
+
+    public Ui(String name) {
+        this.name = name;
+        this.scanner = new Scanner(System.in);
+    }
+
+    private void printFormatted(String originalMessage) {
+        String indentedMessage = Arrays.stream(originalMessage.split("\n"))
+                .map(line -> "     " + line)
+                .collect(Collectors.joining("\n"));
+
+        String formattedMessage = String.format("    %s\n%s\n    %s",
+                HORIZONTAL_LINE, indentedMessage, HORIZONTAL_LINE);
+
+        System.out.println(formattedMessage);
+    }
+
+    public void startUserInputLoop(Consumer<String> parseInputAndDispatch) {
+        Stream.generate(scanner::nextLine)
+                .takeWhile(input -> !input.equals("bye"))
+                .forEach(parseInputAndDispatch);
+    }
+
+    public void greet() {
+        printFormatted("Hello! I'm " + this.name + "\nWhat can I do for you?");
+    }
+
+    public void exit() {
+        printFormatted("Bye. Hope to see you again soon!");
+    }
+
+
+    public void printTaskAddedMessage(Task task, int taskCount) {
+        printFormatted(String.format("Got it. I've added this task:\n  %s\n Now you have %d %s in the list.",
+                task.toString(), taskCount, (taskCount == 1 ? "task" : "tasks")));
+    }
+
+    public void printTaskDeletedMessage(Task task, int taskCount) {
+        printFormatted(String.format("Noted. I've removed this task:\n  %s\n Now you have %d %s in the list.",
+                task.toString(), taskCount, (taskCount == 1 ? "task" : "tasks")));
+    }
+
+    public void printTaskMarkedMessage(Task task) {
+        printFormatted(String.format("Nice! I've marked this task as done:\n  %s", task.toString()));
+    }
+
+    public void printTaskUnmarkedMessage(Task task) {
+        printFormatted(String.format("Nice! I've unmarked this task as done:\n  %s", task.toString()));
+    }
+
+    public void printTaskList(ArrayList<Task> tasks) {
+        printFormatted(IntStream.range(0, tasks.size())
+                .mapToObj(i -> (i + 1) + ". " + tasks.get(i))
+                .collect(Collectors.joining("\n")));
+    }
+
+    public void printErrorMessage(String errorMessage) {
+        printFormatted(String.format("Error: %s", errorMessage));
+    }
+
+
+}
