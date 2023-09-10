9fedd41cf40fca1464189600856710c95abfa3fe
yyyaohhh <yyyaohhh@gmail.com>
2023-09-10 19:57:06 +0800

    JavaFX part 4 error

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,11 +1,12 @@
-import Duke.Duke;
+import Duke.Main;
 import javafx.application.Application;
+
 public class Launcher {
 
     /**
      * A launcher class to workaround classpath issues.
      */
         public static void main(String[] args) {
-            Application.launch(Duke.class, args);
+            Application.launch(Main.class, args);
         }
 }
