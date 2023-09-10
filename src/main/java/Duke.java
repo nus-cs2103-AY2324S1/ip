34e03ea8f7476e7416065c29dc14fb4c6c5e883a
Chen1x <chenxinan01@gmail.com>
2023-09-10 22:10:28 +0800

    add Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,7 +24,7 @@ public class Duke {
         System.out.println("____________________________________________________________");
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < Duke.taskAmount; i++) {
-            System.out.println(Integer.toString(i + 1) + "." + Duke.tasks[i]);
+            System.out.println((i + 1) + "." + Duke.tasks[i]);
         }
         System.out.println("____________________________________________________________");
     }
@@ -47,14 +47,45 @@ public class Duke {
         System.out.println("____________________________________________________________");
     }
 
+    private static void printAddTaskInfo(Task task){
+        System.out.println("____________________________________________________________");
+        System.out.println("Got it. I've added this task:");
+        System.out.println(task);
+        System.out.println("Now you have " + Duke.taskAmount + " tasks in the list.");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void todo(String description){
+        Todo newTask = new Todo(description);
+        Duke.tasks[Duke.taskAmount] = newTask;
+        Duke.taskAmount += 1;
+        Duke.printAddTaskInfo(newTask);
+    }
+
+    private static void deadline(String description, String by){
+        Deadline newTask = new Deadline(description, by);
+        Duke.tasks[Duke.taskAmount] = newTask;
+        Duke.taskAmount += 1;
+        Duke.printAddTaskInfo(newTask);
+    }
+
+    private static void event(String description, String from, String to){
+        Event newTask = new Event(description, from, to);
+        Duke.tasks[Duke.taskAmount] = newTask;
+        Duke.taskAmount += 1;
+        Duke.printAddTaskInfo(newTask);
+    }
+
     public static void main(String[] args) {
         Duke.greet();
+
+        int taskIndex;
+        String description;
 
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
         while (!userInput.equals("bye")) {
             String[] inputParts = userInput.split(" ", 2);
-            int taskIndex;
             switch (inputParts[0]) {
             case "list":
                 Duke.list();
@@ -70,12 +101,31 @@ public class Duke {
                 Duke.unmark(taskIndex);
                 break;
 
+            case "todo":
+                description = inputParts[1];
+                Duke.todo(description);
+                break;
+
+            case "deadline":
+                inputParts = inputParts[1].split(" /by ", 2);
+                description = inputParts[0];
+                String by = inputParts[1];
+                Duke.deadline(description, by);
+                break;
+
+            case "event":
+                inputParts = inputParts[1].split(" /from ", 2);
+                description = inputParts[0];
+                inputParts = inputParts[1].split(" /to ", 2);
+                String from = inputParts[0];
+                String to = inputParts[1];
+                Duke.event(description, from, to);
+                break;
+
             default:
                 System.out.println("____________________________________________________________");
-                System.out.println("Added: " + userInput);
+                System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 System.out.println("____________________________________________________________");
-                Duke.tasks[Duke.taskAmount] = new Task(userInput);
-                Duke.taskAmount += 1;
             }
             userInput = scanner.nextLine();
         }
---
+++ src/main/java/Event.java
@@ -0,0 +1,16 @@
+public class Event extends Task {
+
+    protected String from;
+    protected String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+    }
+}
