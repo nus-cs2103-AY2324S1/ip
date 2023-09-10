910d3f02f386724c53743000b7d95bc13f97d98c
Evan Yan <103996156+evanyan13@users.noreply.github.com>
2023-09-10 23:13:50 +0800

    Enable shadowJar via Gradle

--- src/test/java/jarvis/tasks/TaskTest.java
+++ src/test/java/jarvis/tasks/TaskTest.java
@@ -1,16 +1,43 @@
-// package jarvis.tasks;
+package jarvis.tasks;
 
-// import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.time.LocalDateTime;
 
-// public class TaskTest {
-//     public void dummyTest() {
-//         assertEquals(2, 2);
-//     }
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-//     @Test
-//     public void anotherDummyTest() {
-//         assertEquals(3, 2);
-//     }
-// }
+public class TaskTest {
+    private Task task;
+
+    @BeforeEach
+    public void setUp() {
+        task = new Task("Sample Task", false);
+    }
+
+    @Test
+    public void getTitle_sampleTitle_success() {
+        assertEquals("Sample Task", task.getTitle());
+    }
+
+    @Test
+    public void markCompleted_markTestComplete_success() {
+        task.markCompleted();
+        assertTrue(task.isCompleted());
+    }
+
+    @Test
+    public void unmarkCompleted_unmarkTestComplete_success() {
+        task.unmarkCompleted();
+        assertFalse(task.isCompleted());
+    }
+
+    @Test
+    public void testSetAndGetDueDate() {
+        LocalDateTime dueDate = LocalDateTime.of(2023, 9, 30, 12, 0);
+        task.setDueDate(dueDate);
+        assertEquals(dueDate, task.getDueDate());
+    }
+}
