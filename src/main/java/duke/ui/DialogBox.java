856bdad2ab00c943cb1c5244e949cb0e5b985ef8
Aprup Kale <aprup.kale@gmail.com>
2023-09-11 11:15:43 +0800

    Fix list display issue in GUI

--- src/main/java/duke/ui/DialogBox.java
+++ src/main/java/duke/ui/DialogBox.java
@@ -6,10 +6,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -21,7 +21,7 @@ import java.util.Collections;
  */
 public class DialogBox extends HBox {
     @FXML
-    private Label dialog;
+    private Text dialog;
     @FXML
     private ImageView displayPicture;
 
