150deec7066d97753896b1cc7fb65d7b9d821148
teozern1 <75937968+teozern1@users.noreply.github.com>
2023-09-11 17:47:18 +0800

    Refactor Parser to use switch cases
    Using if else statements makes the code difficult to read, switch statements improve readability

--- src/main/java/Eddie/Parser.java
+++ src/main/java/Eddie/Parser.java
@@ -15,94 +15,91 @@ public class Parser {
     private static String execute(String s) throws DukeException {
         Scanner sc = new Scanner(s);
         while (true) {
-
             String command = sc.next();
 
+            switch (command) {
+                case "bye":
+                    return Duke.exit();
+                case "list":
+                    return Duke.list();
+                case "mark":
+                    int taskNum = sc.nextInt();
+                    Task task = TaskList.get(taskNum - 1);
 
-            if (command.equals("bye")) {
-                return Duke.exit();
-            } else if (command.equals("list")) {
-                return Duke.list();
-            } else if (command.equals("mark")) {
-                int taskNum = sc.nextInt();
-                Task task = TaskList.get(taskNum - 1);
-                task.taskIsDone();
-                Storage.write();
-                return Ui.mark(taskNum);
-            } else if (command.equals("unmark")) {
-                int taskNum = sc.nextInt();
-                Task task = TaskList.get(taskNum - 1);
-                task.taskNotDone();
+                    task.taskIsDone();
+                    Storage.write();
 
-                Storage.write();
-                return Ui.unmark(taskNum);
-            } else if (command.equals("delete")) {
-                int index = sc.nextInt();
-                return Duke.delete(index - 1);
-            } else if (command.equals("todo")) {
+                    return Ui.mark(taskNum);
+                case "unmark":
+                    taskNum = sc.nextInt();
+                    task = TaskList.get(taskNum - 1);
 
-                String restOfString = sc.nextLine();
-                if (restOfString.length() != 0) {
-                    String taskName = restOfString;
-                    Task taskToAdd = new Todo(taskName);
-                    return Duke.add(taskToAdd);
+                    task.taskNotDone();
 
-                } else {
-                    throw new EmptyDescriptionException();
-                }
+                    Storage.write();
+                    return Ui.unmark(taskNum);
+                case "delete":
+                    int index = sc.nextInt();
+                    return Duke.delete(index - 1);
+                case "todo":
+                    String restOfString = sc.nextLine();
+                    if (restOfString.length() != 0) {
+                        String taskName = restOfString;
+                        Task taskToAdd = new Todo(taskName);
+                        return Duke.add(taskToAdd);
 
-            } else if (command.equals("deadline")) {
-                String restOfString = sc.nextLine();
-
-                if (restOfString.length() != 0) {
-                    int slashIndex = restOfString.indexOf("/by");
-                    if (slashIndex == -1) {
-                        throw new MissingByDateException();
+                    } else {
+                        throw new EmptyDescriptionException();
                     }
-                    String taskName = restOfString.substring(0, slashIndex - 1);
-                    String date = restOfString.substring(slashIndex + 4);
-                    LocalDate d = LocalDate.parse(date);
-                    Task taskToAdd = new Deadline(taskName, d);
-                    return Duke.add(taskToAdd);
-                } else {
-                    throw new EmptyDescriptionException();
-                }
+                case "deadline":
+                    restOfString = sc.nextLine();
 
-            } else if (command.equals("event")) {
-
-                String restOfString = sc.nextLine();
-                if (restOfString.length() != 0) {
-                    int fromIndex = restOfString.indexOf("/from");
-                    int toIndex = restOfString.indexOf("/to");
-                    if (fromIndex == -1) {
-                        throw new MissingFromDateException();
-                    } else if (toIndex == -1) {
-                        throw new MissingToDateException();
+                    if (restOfString.length() != 0) {
+                        int slashIndex = restOfString.indexOf("/by");
+                        if (slashIndex == -1) {
+                            throw new MissingByDateException();
+                        }
+                        String taskName = restOfString.substring(0, slashIndex - 1);
+                        String date = restOfString.substring(slashIndex + 4);
+                        LocalDate d = LocalDate.parse(date);
+                        Task taskToAdd = new Deadline(taskName, d);
+                        return Duke.add(taskToAdd);
+                    } else {
+                        throw new EmptyDescriptionException();
                     }
+                case "event":
+                    restOfString = sc.nextLine();
+                    if (restOfString.length() != 0) {
+                        int fromIndex = restOfString.indexOf("/from");
+                        int toIndex = restOfString.indexOf("/to");
+                        if (fromIndex == -1) {
+                            throw new MissingFromDateException();
+                        } else if (toIndex == -1) {
+                            throw new MissingToDateException();
+                        }
 
-                    String taskName = restOfString.substring(0, fromIndex - 1);
-                    String fromDate = restOfString.substring(fromIndex + 6, toIndex - 1);
-                    String toDate = restOfString.substring(toIndex + 4);
+                        String taskName = restOfString.substring(0, fromIndex - 1);
+                        String fromDate = restOfString.substring(fromIndex + 6, toIndex - 1);
+                        String toDate = restOfString.substring(toIndex + 4);
 
-                    LocalDate from = LocalDate.parse(fromDate);
-                    LocalDate to = LocalDate.parse(toDate);
-                    Task taskToAdd = new Event(taskName, from, to);
-                    return Duke.add(taskToAdd);
-                } else {
-                    throw new EmptyDescriptionException();
-                }
-
-
-            } else if (command.equals("clear")){
-                return Duke.clear();
-            } else if (command.equals("find")) {
-                String restOfString = sc.next();
-                return Duke.find(restOfString);
-            } else {
-                throw new NoSuchCommandException();
+                        LocalDate from = LocalDate.parse(fromDate);
+                        LocalDate to = LocalDate.parse(toDate);
+                        Task taskToAdd = new Event(taskName, from, to);
+                        return Duke.add(taskToAdd);
+                    } else {
+                        throw new EmptyDescriptionException();
+                    }
+                case "clear":
+                    return Duke.clear();
+                case "find":
+                    restOfString = sc.next();
+                    return Duke.find(restOfString);
+                default:
+                    throw new NoSuchCommandException();
             }
         }
     }
+    
 
     /**
      * Method to be used to start parsing of user input.
