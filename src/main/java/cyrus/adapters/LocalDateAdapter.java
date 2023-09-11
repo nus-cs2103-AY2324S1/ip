5446a4f090213bb3fe92c8e6e8aa7c0b9445db40
Woo Jia Hao <woojiahao1234@gmail.com>
2023-09-11 17:02:50 +0800

    Improve the JavaDocs of LocalDateAdapter
    The JavaDocs of LocalDateAdapter currently is worded ambiguously.
    
    This creates confusion when reading them.
    
    Reworded the documentation to be clearer.

--- src/main/java/cyrus/adapters/LocalDateAdapter.java
+++ src/main/java/cyrus/adapters/LocalDateAdapter.java
@@ -14,12 +14,11 @@ import cyrus.utility.DateUtility;
  */
 public class LocalDateAdapter extends TypeAdapter<LocalDate> {
     /**
-     * When writing {@code LocalDateAdapter} to JSON, convert it to the input format of {@code dd
-     * /MM/yyy}.
+     * Converts {@code LocalDateAdapter} to the input format of {@code dd/MM/yyyy} when writing to JSON.
      *
-     * @param jsonWriter writes the value to the JSON
+     * @param jsonWriter writes the value to the JSON.
      * @param localDate  the Java object to write. May be null.
-     * @throws IOException if {@code jsonWriter} encounters an error with writing
+     * @throws IOException if {@code jsonWriter} encounters an error with writing.
      */
     @Override
     public void write(JsonWriter jsonWriter, LocalDate localDate) throws IOException {
@@ -27,12 +26,11 @@ public class LocalDateAdapter extends TypeAdapter<LocalDate> {
     }
 
     /**
-     * When attempting to deserialize to a {@code LocalDate}, use the same input format as {@code
-     * dd/MM/yyyy}.
+     * Deserializes {@code LocalDate} to the same input format as {@code dd/MM/yyyy}.
      *
-     * @param jsonReader reads the value from JSON
-     * @return {@code LocalDate} value
-     * @throws IOException if {@code jsonReader} encounters error with reading
+     * @param jsonReader reads the value from JSON.
+     * @return {@code LocalDate} value.
+     * @throws IOException if {@code jsonReader} encounters error with reading.
      */
     @Override
     public LocalDate read(JsonReader jsonReader) throws IOException {
