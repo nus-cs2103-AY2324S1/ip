b8a93b85f1ba86590e9a944fb7e710f21bc0edb7
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2023-09-12 01:58:14 +0800

    Add logic to return UNDO command

--- src/main/java/corgi/commands/CommandType.java
+++ src/main/java/corgi/commands/CommandType.java
@@ -56,6 +56,8 @@ public enum CommandType {
             return DATE;
         case "find":
             return FIND;
+        case "undo":
+            return UNDO;
         default:
             throw new InvalidCommandException();
         }
