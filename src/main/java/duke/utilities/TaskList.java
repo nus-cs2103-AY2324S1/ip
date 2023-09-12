fe744006b8d4afb51ba3b476360f2d17f99ff2c6
NicholasTng010601 <nicholastng@hotmail.com>
2023-09-12 19:06:35 +0800

    Created a contains function for tasklist

--- src/main/java/duke/utilities/TaskList.java
+++ src/main/java/duke/utilities/TaskList.java
@@ -6,61 +6,61 @@ import java.util.ArrayList;
 import duke.tasks.Task;
 
 /**
- * A list of tasks the user inputs
+ * A list of tasks the user inputs.
  */
 public class TaskList implements Serializable {
     protected ArrayList<Task> taskList;
 
     /**
-     * TaskList class constructor
-     * Instantiates an empty arraylist as the taskList
+     * TaskList class constructor.
+     * Instantiates an empty arraylist as the taskList.
      */
     public TaskList() {
         this.taskList = new ArrayList<>();
     }
 
     /**
-     * TaskList class constructor
-     * Instantiates taskList as the given arrayList
+     * TaskList class constructor.
+     * Instantiates taskList as the given arrayList.
      *
-     * @param taskList Instantiate taskList with given array:List
+     * @param taskList Instantiate taskList with given array:List.
      */
     public TaskList(ArrayList<Task> taskList) {
         this.taskList = taskList;
     }
 
     /**
-     * Gets the task at the specific index
+     * Gets the task at the specific index.
      *
-     * @param index The index of task
-     * @return task at the index
+     * @param index The index of task.
+     * @return task at the index.
      */
     public Task getTask(int index) {
         return this.taskList.get(index);
     }
 
     /**
-     * Gets the size of taskList
+     * Gets the size of taskList.
      *
-     * @return size of taskList
+     * @return size of taskList.
      */
     public int size() {
         return this.taskList.size();
     }
 
     /**
-     * Adds task to the back of the taskList
+     * Adds task to the back of the taskList.
      *
-     * @param task The task to be added to the list
+     * @param task The task to be added to the list.
      */
     public void addTask(Task task) {
         this.taskList.add(task);
     }
 
     /**
-     * Deletes task at the specific index
+     * Deletes task at the specific index.
      *
-     * @param index The index of task to be deleted
+     * @param index The index of task to be deleted.
      */
     public void deleteTask(int index) {
         this.taskList.remove(index);
@@ -75,28 +75,28 @@ public class TaskList implements Serializable {
     }
 
     /**
-     * Marks task as done
+     * Marks task as done.
      *
-     * @param index The index of task to be marked
+     * @param index The index of task to be marked.
      */
     public void markAsDone(int index) {
         this.taskList.get(index).markDone();
     }
 
     /**
-     * Marks task as not done
+     * Marks task as not done.
      *
-     * @param index The index of task to be marked
+     * @param index The index of task to be marked.
      */
     public void markAsNotDone(int index) {
         this.taskList.get(index).markNotDone();
     }
 
     /**
-     * Filters list according to keyword
+     * Filters list according to keyword.
      *
-     * @param keyword The keyword used to filter the list
-     * @return TaskList The new list with the words required added
+     * @param keyword The keyword used to filter the list.
+     * @return TaskList The new list with the words required added.
      */
     public TaskList filter(String keyword) {
         ArrayList<Task> filteredList = new ArrayList<>();
@@ -106,6 +106,16 @@ public class TaskList implements Serializable {
             }
         }
         return new TaskList(filteredList);
+    }
+
+    /**
+     * Checks if the task is present in the list.
+     *
+     * @param task The task to be checked.
+     * @return true if the task is in the list and false otherwise.
+     */
+    public boolean contains(Task task) {
+        return taskList.contains(task);
     }
 
 }
