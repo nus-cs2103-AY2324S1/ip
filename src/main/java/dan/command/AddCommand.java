d31bda62c34bb481f52db8cdaaa9883f99182172
Song_Bingxi <bingxi.bobby.song@gmail.com>
2023-09-12 14:56:08 +0800

    added AddCommand.java

---
+++ src/main/java/dan/command/AddCommand.java
@@ -0,0 +1,52 @@
+package dan.command;
+
+import dan.task.*;
+
+public class AddCommand extends Command {
+    /** Fields */
+    public static final String type = "add";
+    private String text;
+    private int id;
+
+    /** Constructor */
+    public AddCommand(String text, int i) {
+        super();
+        this.text = text;
+        this.id = id;
+    }
+
+    /** Methods */
+    @Override
+    public void op(TaskList tasks) throws IndexOutOfBoundsException {
+        String[] texts = text.split("/");
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = texts[i].trim();
+        }
+        Task newTask = null;
+        String description;
+        switch (id) {
+            case 1:
+                description = texts[0].substring(5);
+                newTask = new ToDo(description);
+                break;
+            case 2:
+                description = texts[0].substring(9);
+                String deadline = texts[1].substring(3);
+                newTask = new Deadline(description, deadline);
+                break;
+            case 3:
+                description = texts[0].substring(5);
+                String start = texts[1].substring(5);
+                String end = texts[2].substring(3);
+                newTask = new Event(description, start, end);
+                break;
+        }
+        tasks.add(newTask);
+        tasks.storageChanged = 1;
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+}
