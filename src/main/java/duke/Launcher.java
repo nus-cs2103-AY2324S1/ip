1ac004282b4488ef206ba778fed52d62ac2873ee
YangHeng Tang <yanghengtang@gmail.com>
2023-09-12 21:33:41 +0800

    Add JavaDoc for Launcher.java

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -2,6 +2,9 @@ package duke;
 
 import javafx.application.Application;
 
+/**
+ * Represents the launcher for this application.
+ */
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(Main.class, args);
