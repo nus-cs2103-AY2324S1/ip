c5c1d915e080e78dc536c53ecec35f020d00d929
xavierpok <54708640+xavierpok@users.noreply.github.com>
2023-09-12 22:22:09 +0800

    Revert "Revert "Adds tagging and searching for tagging""
    This reverts commit c358832dc915cb428d0aeb3d417670aa7e32a33c.
    
    There were a few errors, but now they're fixed

--- src/main/java/storage/TaskList.java
+++ src/main/java/storage/TaskList.java
@@ -61,6 +61,19 @@ public class TaskList extends ArrayList<Task> implements Serializable {
     }
 
 
+    /**
+     * Adds a tag to the task at the provided index
+     * @param index the index of the task
+     * @param tag the tag to add
+     * @return the task that was tagged
+     */
+    public Task addTag(int index, String tag) {
+        boolean addSucceeded = this.get(index).addTag(tag);
+        assert addSucceeded : String.format("Tried to add tag %s at %s, but failed!", tag, index);
+        return this.get(index);
+    }
+
+
 
 
 
