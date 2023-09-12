863943af246a6acf373e9a2b1370407547f9e578
thaddeusong <67548448+bladedmistral@users.noreply.github.com>
2023-09-12 18:46:42 +0800

    Add Assertions to TaskList

--- src/main/java/duke/task/TaskList.java
+++ src/main/java/duke/task/TaskList.java
@@ -9,17 +9,37 @@ import java.util.List;
 public class TaskList {
     private final List<Task> taskList = new ArrayList<>();
 
+    /**
+     * Adds a task to the task list.
+     * @param task The task to add.
+     */
     public void addTask(Task task) {
         taskList.add(task);
     }
 
+    /**
+     * Returns a task from the task list.
+     * @param index The index of the task.
+     * @return The task at the given index.
+     */
     public Task getTask(int index) {
+        assert index < taskList.size();
         return taskList.get(index);
     }
 
+    /**
+     * Removes a task from the task list.
+     * @param task The task to remove.
+     */
     public void removeTask(Task task) {
+        assert taskList.contains(task);
         taskList.remove(task);
     }
+
+    /**
+     * Returns the size of the task list.
+     * @return THe number of tasks in the list.
+     */
     public int getTaskCount() {
         return taskList.size();
     }
