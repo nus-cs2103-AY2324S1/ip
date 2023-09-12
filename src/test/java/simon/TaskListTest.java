6e3f003001189e8e56ec4bde68532111a46a06d4
L1MJH <lolflgc@icloud.com>
2023-09-12 16:21:59 +0800

    Update TaskListTest.java

--- src/test/java/simon/TaskListTest.java
+++ src/test/java/simon/TaskListTest.java
@@ -1,12 +1,13 @@
 package simon;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 import simon.task.Task;
 import simon.task.ToDo;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TaskListTest {
 
