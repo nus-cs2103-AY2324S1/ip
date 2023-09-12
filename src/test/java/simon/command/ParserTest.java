8bdd75c0cc67ec596d0952c1ed39e635a7efbd37
L1MJH <lolflgc@icloud.com>
2023-09-12 16:21:53 +0800

    Update ParserTest.java

--- src/test/java/simon/command/ParserTest.java
+++ src/test/java/simon/command/ParserTest.java
@@ -1,8 +1,9 @@
 package simon.command;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 import simon.SimonException;
 import simon.task.Task;
