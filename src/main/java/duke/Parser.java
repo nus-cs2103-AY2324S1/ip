238b73371c1bf440c714f3800f029fd5a6cfa1f3
jamz903 <jamietohhl@gmail.com>
2023-09-12 14:07:05 +0800

    Refactor Code and Abstract Functionality into DateParser Class
    This commit involves a significant code refactoring effort aimed at enhancing code quality and maintainability.
    
    Specifically, portions of logic in Parser has been abstracted out to DateParser, a dedicated class to process the Date objects of the Tasks.
    
    This change makes it easier to maintain and sets the stage for more efficient development and easier debugging in subsequent iterations.

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -17,15 +17,14 @@ public class Parser {
      * @throws DukeException    Exception to be thrown when the input cannot be read.
      */
     public String createTask(String promptText) throws DukeException {
-        String returnString = "";
+        String returnString;
         if (promptText.startsWith("todo")) {
             try {
                 Task todo = new Todo(promptText.substring(5));
                 String returnStatement = taskList.add(todo, true);
                 taskList.writeToFile();
                 returnString = returnStatement;
-            }
-            catch (StringIndexOutOfBoundsException s) {
+            } catch (StringIndexOutOfBoundsException s) {
                 throw new DukeException("OOPS!! The description of a todo cannot be empty.");
             }
         }
@@ -33,68 +32,32 @@ public class Parser {
             try {
                 String[] parts = promptText.split("/", 2);
                 String date = parts[1].substring(3);
-                if (DATE_PATTERN_DASH.matcher(date).matches()) {
-                    String[] dateParts = date.split("-");
+                if (DateParser.isEitherDate(date)) {
                     Task deadline = new Deadline(parts[0].substring(9),
-                            LocalDate.of(Integer.parseInt(dateParts[0]), Integer.parseInt(dateParts[1]), Integer.parseInt(dateParts[2])));
+                            DateParser.formatDate(date));
                     returnString = taskList.add(deadline, true);
-                }
-                else if (DATE_PATTERN_SLASH.matcher(date).matches()) {
-                    String[] dateParts = date.split("/");
-                    int day = dateParts[0].length() == 1 ? Integer.valueOf("0" + dateParts[0]) : Integer.valueOf(dateParts[0]);
-                    int month = dateParts[1].length() == 1 ? Integer.valueOf("0" + dateParts[1]) : Integer.valueOf(dateParts[1]);
-                    String[] yearParts = dateParts[2].split(" ");
-                    Task deadline = new Deadline(parts[0].substring(9),
-                            LocalDate.of(Integer.valueOf(yearParts[0]), month, day));
-                    returnString = taskList.add(deadline, true);
-                }
-                else {
+                } else {
                     Task deadline = new Deadline(parts[0].substring(9), parts[1].substring(2));
                     returnString = taskList.add(deadline, true);
                 }
                 taskList.writeToFile();
-            }
-            catch (StringIndexOutOfBoundsException s) {
+            } catch (StringIndexOutOfBoundsException s) {
                 throw new DukeException("OOPS!! The description of a deadline cannot be empty.");
             }
-        }
-        else {
+        } else {
             try {
-                String[] parts = promptText.split("/", 2);
-                String dateParts = parts[1].substring(5);
+                String[] taskDetails = promptText.split("/", 2);
+                String dateParts = taskDetails[1].substring(5);
                 int splitIndex = dateParts.indexOf("/to");
                 String startDate = dateParts.substring(0, splitIndex - 1);
                 String endDate = dateParts.substring(splitIndex + 4);
 
-                if (DATE_PATTERN_DASH.matcher(startDate).matches() &&
-                        DATE_PATTERN_DASH.matcher(endDate).matches()) {
-                    String[] startDateParts = startDate.split("-");
-                    String[] endDateParts = endDate.split("-");
-                    Task event = new Event(parts[0].substring(6),
-                            LocalDate.of(Integer.parseInt(startDateParts[0]),
-                            Integer.parseInt(startDateParts[1]), Integer.parseInt(startDateParts[2])),
-                            LocalDate.of(Integer.parseInt(endDateParts[0]), Integer.parseInt(endDateParts[1]),
-                                    Integer.parseInt(endDateParts[2])));
-                    returnString = taskList.add(event, true);
-                } else if (DATE_PATTERN_SLASH.matcher(startDate).matches() &&
-                        DATE_PATTERN_SLASH.matcher(endDate).matches()) {
-                    String[] startDateParts = startDate.split("/");
-                    String[] endDateParts = endDate.split("/");
-                    int startDay = startDateParts[0].length() == 1 ? Integer.valueOf("0" + startDateParts[0]) :
-                            Integer.valueOf(startDateParts[0]);
-                    int startMonth = startDateParts[1].length() == 1 ? Integer.valueOf("0" + startDateParts[1]) :
-                            Integer.valueOf(startDateParts[1]);
-                    int endDay = endDateParts[0].length() == 1 ? Integer.valueOf("0" + endDateParts[0]) :
-                            Integer.valueOf(endDateParts[0]);
-                    int endMonth = endDateParts[1].length() == 1 ? Integer.valueOf("0" + endDateParts[1]) :
-                            Integer.valueOf(endDateParts[1]);
-                    String[] startYearParts = startDateParts[2].split(" ");
-                    String[] endYearParts = endDateParts[2].split(" ");
-                    Task event = new Event(parts[0].substring(6), LocalDate.of(Integer.valueOf(startYearParts[0]),
-                            startMonth, startDay), LocalDate.of(Integer.valueOf(endYearParts[0]), endMonth, endDay));
+                if (DateParser.isEitherDate(startDate) && DateParser.isEitherDate(endDate)) {
+                    Task event = new Event(taskDetails[0].substring(6),
+                            DateParser.formatDate(startDate), DateParser.formatDate(endDate));
                     returnString = taskList.add(event, true);
                 } else {
-                    Task event = new Event(parts[0].substring(6), startDate, endDate);
+                    Task event = new Event(taskDetails[0].substring(6), startDate, endDate);
                     returnString = taskList.add(event, true);
                 }
                 taskList.writeToFile();
