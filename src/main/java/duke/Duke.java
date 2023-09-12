a2f13e1efb497af2a8bfae41e789adf4fdc884b1
Dick Ong <dickongwd@gmail.com>
2023-09-12 20:45:39 +0800

    Connect javafx GUI with chatbot response

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,73 +2,68 @@ package duke;
 
 import duke.command.Command;
 import duke.parser.Parser;
+import duke.storage.DukeStorageException;
 import duke.storage.Storage;
-import duke.ui.TextUi;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.layout.VBox;
+import javafx.application.Platform;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * The main class for the Duke chatbot.
  */
 public class Duke {
-    private final Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private final Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
+    private final Storage storage;
+    private final Parser parser;
+    private TaskList tasks;
 
-    public static void main(String[] args) {
-        TaskList tasks;
-        TextUi ui = new TextUi();
-        Storage storage = new Storage();
-        Parser parser = new Parser();
-
-        ui.showWelcomeMessage();
-
-        try {
-            tasks = storage.load();
-            if (tasks.size() == 0) {
-                ui.showMessage(String.format("No stored tasks found from %s", Storage.DEFAULT_STORAGE_PATH),
-                        "Starting from an empty task list.");
-                tasks = new TaskList();
-            } else {
-                ui.showMessage(String.format("Tasks loaded from %s", Storage.DEFAULT_STORAGE_PATH));
-            }
-        } catch (IOException e) {
-            ui.showMessage(String.format("Error loading tasks from %s", Storage.DEFAULT_STORAGE_PATH),
-                    "Starting from an empty task list.");
-            tasks = new TaskList();
-        }
-
-        Command command;
-        do {
-            String input = ui.getUserCommand();
-            command = parser.parseCommand(input);
-            command.setData(tasks);
-            String[] response = command.execute();
-            ui.showMessage(response);
-
-            try {
-                storage.save(tasks);
-            } catch (IOException e) {
-                ui.showMessage("Error saving tasks");
-            }
-        } while (!command.isBye());
+    /**
+     * The constructor for the Duke application.
+     * Sets up the relevant components required.
+     */
+    public Duke() {
+        storage = new Storage();
+        parser = new Parser();
     }
 
     /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
+     * Loads the tasks from storage.
+     *
+     * @return the number of tasks loaded from storage
+     * @throws DukeStorageException if an error occurs when loading tasks from storage
      */
-    public String getResponse(String input) {
-        return "Duke heard: " + input;
+    public int loadTasks() throws DukeStorageException {
+        try {
+            tasks = storage.load();
+            return tasks.size();
+        } catch (IOException e) {
+            tasks = new TaskList();
+            throw new DukeStorageException("Error reading from " + Storage.DEFAULT_STORAGE_PATH);
+        }
+    }
+
+    /**
+     * Returns Duke's response given the user input.
+     *
+     * @param input the input from the user
+     * @return the response from Duke
+     */
+    public String[] getResponse(String input) {
+        Command command = parser.parseCommand(input);
+        command.setData(tasks);
+        String[] response = command.execute();
+
+        if (command.isBye()) {
+            Platform.exit();
+        }
+
+        try {
+            storage.save(tasks);
+            return response;
+        } catch (IOException e) {
+            String[] newResponse = Arrays.copyOf(response, response.length + 1);
+            newResponse[newResponse.length - 1] = "Error saving tasks";
+            return newResponse;
+        }
     }
 }
--- src/main/java/duke/ui/MainWindow.java
+++ src/main/java/duke/ui/MainWindow.java
@@ -1,6 +1,8 @@
 package duke.ui;
 
 import duke.Duke;
+import duke.storage.DukeStorageException;
+import duke.storage.Storage;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -8,6 +10,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+
+import static duke.common.Messages.MESSAGE_FIRST_PROMPT;
+import static duke.common.Messages.MESSAGE_WELCOME;
 
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
@@ -32,6 +37,30 @@ public class MainWindow extends AnchorPane {
 
     public void setDuke(Duke d) {
         duke = d;
+
+        showMessage(MESSAGE_WELCOME, MESSAGE_FIRST_PROMPT);
+
+        try {
+            int tasksLoaded = duke.loadTasks();
+            if (tasksLoaded == 0) {
+                showMessage(String.format("No stored tasks found from %s", Storage.DEFAULT_STORAGE_PATH),
+                        "Starting from an empty task list.");
+            } else {
+                showMessage(String.format("Tasks loaded from %s", Storage.DEFAULT_STORAGE_PATH));
+            }
+        } catch (DukeStorageException e) {
+            showMessage(String.format("Error loading tasks from %s", Storage.DEFAULT_STORAGE_PATH),
+                    "Starting from an empty task list.");
+        }
+    }
+
+    /**
+     * Shows a response to the user via the javafx GUI.
+     *
+     * @param message the message to display, line by line
+     */
+    private void showMessage(String... message) {
+        dialogContainer.getChildren().add(DialogBox.getDukeDialog(String.join("\n", message), dukeImage));
     }
 
     /**
@@ -41,11 +70,9 @@ public class MainWindow extends AnchorPane {
     @FXML
     private void handleUserInput() {
         String input = userInput.getText();
-        String response = duke.getResponse(input);
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(input, userImage),
-                DialogBox.getDukeDialog(response, dukeImage)
-        );
+        String[] response = duke.getResponse(input);
+        dialogContainer.getChildren().add(DialogBox.getUserDialog(input, userImage));
+        showMessage(response);
         userInput.clear();
     }
 }
