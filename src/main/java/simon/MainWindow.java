fc670a57dd5ddae6fc37f0d150cdf13b4f502027
L1MJH <lolflgc@icloud.com>
2023-09-12 16:01:33 +0800

    Update MainWindow.java

--- src/main/java/simon/MainWindow.java
+++ src/main/java/simon/MainWindow.java
@@ -9,9 +9,11 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
 /**
- * Controller for MainWindow. Provides the layout for the other controls.
+ * Controller for MainWindow of the Simon application.
+ * Provides the layout for the other controls and manages user interactions.
  */
 public class MainWindow extends AnchorPane {
+
     @FXML
     private ScrollPane scrollPane;
     @FXML
@@ -21,28 +23,40 @@ public class MainWindow extends AnchorPane {
     @FXML
     private Button sendButton;
 
+    /** The main Simon application instance. */
     private Simon simon;
 
+    /** Image representing the user in the dialog box. */
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    /** Image representing Duke (Simon) in the dialog box. */
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
+    /**
+     * Initializes the UI components and displays the welcome message.
+     */
     @FXML
     public void initialize() {
         Ui ui = new Ui();
         Ui.showWelcome();
         dialogContainer.getChildren().addAll(
-                DialogBox.getDukeDialog(ui.getOutput() , dukeImage)
+                DialogBox.getDukeDialog(ui.getOutput(), dukeImage)
         );
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
     }
 
+    /**
+     * Sets the main Simon instance to this controller.
+     *
+     * @param d The Simon instance to be set.
+     */
     public void setDuke(Simon d) {
         simon = d;
     }
 
     /**
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
+     * Handles the user's input when a message is sent.
+     * It creates two dialog boxes - one for the user's message and the other for Simon's reply.
+     * The user's input is then cleared.
      */
     @FXML
     private void handleUserInput() {
