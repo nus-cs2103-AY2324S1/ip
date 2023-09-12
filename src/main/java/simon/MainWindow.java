5f71e12cdedc50c546523acb8854f007a353e2a8
L1MJH <lolflgc@icloud.com>
2023-09-12 15:41:55 +0800

    Update MainWindow.java

--- src/main/java/simon/MainWindow.java
+++ src/main/java/simon/MainWindow.java
@@ -7,7 +7,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-import simon.DialogBox;
 
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
