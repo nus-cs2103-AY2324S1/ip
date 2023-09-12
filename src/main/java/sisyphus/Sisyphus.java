5d34b62e69def031c8a0447a8fb7a8c4d8318ef1
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-09-12 18:55:22 +0800

    Add input and send button into scene

--- src/main/java/sisyphus/Sisyphus.java
+++ src/main/java/sisyphus/Sisyphus.java
@@ -2,9 +2,12 @@ package sisyphus;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import sisyphus.parser.Parser;
 import sisyphus.storage.Storage;
 import sisyphus.task.TaskList;
@@ -18,6 +21,13 @@ public class Sisyphus extends Application {
     private TaskList tasks;
     private Ui ui;
     private Parser parser;
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
 
     /**
      * Constructor for the Sisyphus chatBot.
@@ -53,11 +63,25 @@ public class Sisyphus extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+        //Step 1. Setting up required components
 
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        // more code to be added here later
     }
 
 }
