e8c63c8282fd4f86be32a186239c802ea725ae6c
YangHeng Tang <yanghengtang@gmail.com>
2023-09-13 00:35:01 +0800

    Update Parser.java code quality

--- src/main/java/duke/util/Parser.java
+++ src/main/java/duke/util/Parser.java
@@ -11,7 +11,14 @@ import duke.command.MarkCommand;
 import duke.command.PrintListCommand;
 import duke.command.UnknownCommand;
 import duke.command.UnmarkCommand;
-import duke.task.*;
+import duke.task.Deadline;
+import duke.task.DeadlineException;
+import duke.task.Event;
+import duke.task.EventException;
+import duke.task.Task;
+import duke.task.TaskException;
+import duke.task.ToDo;
+import duke.task.ToDoException;
 
 /**
  * Converts a given String into Tasks and Commands.
