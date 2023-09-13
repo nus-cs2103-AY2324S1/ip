52594ed723eae0c5cf49bafb1c8ef3c7680402a1
Seonlo99 <Zheng_chenglong1999@hotmail.com>
2023-09-13 16:18:58 +0800

    Add Assert
    $DeleteCommand, MarkCommand and UnmarkCommand uses a index to specify a task in tasklist.\n\nThe 3 classes assume the index to be within tasklist index.\n\nUsing Assert ensure the index is indeed within the tasklist index.

--- src/main/java/nobita/command/DeleteCommand.java
+++ src/main/java/nobita/command/DeleteCommand.java
@@ -40,9 +40,10 @@ public class DeleteCommand extends Command{
         if (tasks.checkIndexWithinRange(index)) {
             throw new NobitaException("Selected task number not in list");
         }
+        assert index > 0 && index <= tasks.getTasksSize() : "Index should be within tasks range";
         Task task = tasks.deleteTask(index);
         return "Noted. I've removed this task:\n" + task + "\nNow you have " +
-                tasks.getTotalTask() +" tasks in the list.";
+                tasks.getTasksSize() +" tasks in the list.";
     };
 
     /**
