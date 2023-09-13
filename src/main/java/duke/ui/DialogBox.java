bc7aa88489be2f73b10de94bad7105b4e5621274
Song-Mengfei <e1075530@u.nus.edu>
2023-09-13 17:28:08 +0800

    fix: change build.gradle
    The elder version cannot run on MacOS
    
    Let's
    *add the 'javafx.fxml' package in build.gradle
    *add the checkstyle function in build.gradle
    
    It is the easiest way for every one who get this repo could
    install the package

--- src/main/java/duke/ui/DialogBox.java
+++ src/main/java/duke/ui/DialogBox.java
@@ -1,10 +1,5 @@
 package duke.ui;
 
-import duke.ui.MainWindow;
-
-import java.io.IOException;
-import java.util.Collections;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -15,6 +10,9 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+
+import java.io.IOException;
+import java.util.Collections;
 
 /**
  * An example of a custom control using FXML.
