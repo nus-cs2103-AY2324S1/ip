c33ba6cee6ba2653f7ac4a09c142963a20f8ee6b
ylyma <amylingyuhui@gmail.com>
2023-09-13 22:06:39 +0800

    Change import order

--- src/test/java/kiera/task/TaskTest.java
+++ src/test/java/kiera/task/TaskTest.java
@@ -1,6 +1,9 @@
 package kiera.task;
-import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
     @Test
     public void markAsDone_userInput_success() {
