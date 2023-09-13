d1467026a4b96fa56a12b9c46dd2f74086b2c091
Song_Bingxi <bingxi.bobby.song@gmail.com>
2023-09-13 20:03:40 +0800

    modified AddCommand to apply Parser

--- src/main/java/dan/command/AddCommand.java
+++ src/main/java/dan/command/AddCommand.java
@@ -12,38 +12,15 @@ public class AddCommand extends Command {
     private Task newTask;
 
     /** Constructor */
-    public AddCommand(String text, int id) {
+    public AddCommand(Task t) {
         super();
-        this.text = text;
-        this.id = id;
+        this.newTask = t;
     }
 
     /** Methods */
     @Override
     public void op(TaskList tasks) throws DanException {
         try {
-            String[] texts = text.split("/");
-            for (int i = 0; i < texts.length; i++) {
-                texts[i] = texts[i].trim();
-            }
-            String description;
-            switch (id) {
-                case 1:
-                    description = texts[0].substring(5);
-                    newTask = new ToDo(description);
-                    break;
-                case 2:
-                    description = texts[0].substring(9);
-                    String deadline = texts[1].substring(3);
-                    newTask = new Deadline(description, deadline);
-                    break;
-                case 3:
-                    description = texts[0].substring(5);
-                    String start = texts[1].substring(5);
-                    String end = texts[2].substring(3);
-                    newTask = new Event(description, start, end);
-                    break;
-            }
             tasks.add(newTask);
             tasks.storageChanged = 1;
         } catch (IndexOutOfBoundsException e) {
@@ -51,13 +28,13 @@ public class AddCommand extends Command {
         }
     }
 
-    @Override
-    public String getType() {
-        return type;
-    }
 
     @Override
     public String toString() {
         return newTask.toString();
+    }
+
+    public static String getType() {
+        return type;
     }
 }
