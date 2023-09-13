cd198b99d9a7599edc1f457017ce846e9faad0e3
Song_Bingxi <bingxi.bobby.song@gmail.com>
2023-09-13 20:04:21 +0800

    Modified List

--- src/main/java/dan/command/ExitCommand.java
+++ src/main/java/dan/command/ExitCommand.java
@@ -15,8 +15,7 @@ public class ExitCommand extends Command {
     @Override
     public void op(TaskList tasks) {}
 
-    @Override
-    public String getType() {
+    public static String getType() {
         return type;
     }
 }
