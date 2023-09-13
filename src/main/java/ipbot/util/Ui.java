069c97bb776525aed0c8f5a158add0214d912d61
rocketninja7 <andrewyapp@gmail.com>
2023-09-13 15:14:05 +0800

    Remove dead code from Ui class

--- src/main/java/ipbot/util/Ui.java
+++ src/main/java/ipbot/util/Ui.java
@@ -1,70 +1,11 @@
 package ipbot.util;
 
-import ipbot.model.Pair;
 import ipbot.model.Task;
-
-import java.util.Map;
-import java.util.Scanner;
 
 /**
  * A class to handle all interactions with the user.
  */
 public class Ui {
-    private static final String HORIZONTAL_LINE = "____________________________________________________________";
-    private static final String NAME = "Ip Bot";
-    private Scanner scanner;
-    private String currentCommand;
-
-    /**
-     * Defines a Ui object to interact with standard input.
-     */
-    public Ui() {
-        scanner = new Scanner(System.in);
-    }
-
-    /**
-     * Prints the dividing line.
-     */
-    public void printDivider() {
-        System.out.println(HORIZONTAL_LINE);
-    }
-
-    /**
-     * Prints the welcome text.
-     */
-    public void printWelcome() {
-        printDivider();
-        System.out.println("Hello I'm " + NAME + "!");
-        System.out.println("While I may not be able to fight like Ip Man, I can assist you in other areas!");
-        System.out.println("What can I do for you?");
-        printDivider();
-    }
-
-    /**
-     * Prints the exit text.
-     */
-    public void printExit() {
-        System.out.println("Bye. Hope to see you again soon!");
-        printDivider();
-    }
-
-    /**
-     * Reads the command given by the user.
-     */
-    public void readCommand() {
-        this.currentCommand = scanner.nextLine().strip();
-    }
-
-    /**
-     * Process the previous command given by the user.
-     *
-     * @return The processed command given by the user.
-     * A Pair consisting of a command as a String, and the arguments as a Map.
-     */
-    public Pair<String, Map<String, String>> processCommand() {
-        return Parser.parseCommand(this.currentCommand);
-    }
-
     /**
      * Returns a task as a String in list format.
      *
