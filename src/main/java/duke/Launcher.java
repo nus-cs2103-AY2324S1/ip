f2f626086383b96a392a0c469d042bad76de3338
Jie Xiang <n.jiexiang@gmail.com>
2023-09-13 21:35:30 +0800

    Add javadoc for Launcher class

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -2,6 +2,9 @@ package duke;
 
 import javafx.application.Application;
 
+/**
+ * Launches the JavaFX GUI for the Duke chatbot.
+ */
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(Main.class, args);
