139b68fb24aaa38ccfa2a88d52c992ba865f6aab
Khaleelur <khaleelrrahman2002@gmail.com>
2023-09-13 18:58:12 +0800

    Remove unwanted imports

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Collections;
 
 /**
  * Class to handle the tasklist operations
