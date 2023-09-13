b4b987b4567abcc2ea36f659fcf5b35ce4fcde80
victorlaiyeeteng <vlai69863@gmail.com>
2023-09-13 17:51:46 +0800

    Refactor long functions

--- src/main/java/duke/duke/Duke.java
+++ src/main/java/duke/duke/Duke.java
@@ -43,78 +43,30 @@ public class Duke {
             String inputDetails = parser.getInputDetails();
             switch (command) {
             case "list":
-                return (taskList.showList());
+                return processList();
             case "mark": {
-                if (parser.checkInputDetailsAbsent()) {
-                    throw new DukeException("The task number to mark cannot be empty.");
-                }
-                int taskNumber = Integer.parseInt(inputDetails);
-                Task task = taskList.getTask(taskNumber);
-                return (task.setDone());
+                return processMark(parser, inputDetails);
             }
             case "unmark": {
-                if (parser.checkInputDetailsAbsent()) {
-                    throw new DukeException("The task number to unmark cannot be empty.");
-                }
-                int taskNumber = Integer.parseInt(inputDetails);
-                Task task = taskList.getTask(taskNumber);
-                return (task.unsetDone());
+                return processUnMark(parser, inputDetails);
             }
             case "todo": {
-                if (parser.checkInputDetailsAbsent()) {
-                    throw new DukeException("☹ OOPS!!! The description of a todo task cannot be empty.");
-                }
-                Task task = new Todo(inputDetails);
-                storage.saveTask(inputDetails);
-                return (taskList.addTask(task));
+                return processTodo(parser, inputDetails);
             }
             case "deadline": {
-                if (parser.checkInputDetailsAbsent()) {
-                    throw new DukeException("☹ OOPS!!! The info of a deadline task cannot be empty.");
-                }
-                String[] deadlineInfo = parser.parseInputDetailsDeadline();
-                String description = deadlineInfo[0];
-                String deadline = deadlineInfo[1];
-                Task task = new Deadline(description, deadline);
-                storage.saveTask(description, deadline);
-                return (taskList.addTask(task));
+                return processDeadline(parser);
             }
             case "event": {
-                if (parser.checkInputDetailsAbsent()) {
-                    throw new DukeException("☹ OOPS!!! The info of a event task cannot be empty.");
-                }
-                String[] eventInfo = parser.parseInputDetailsEvent();
-                String description = eventInfo[0];
-                String from = eventInfo[1];
-                String to = eventInfo[2];
-                Task task = new Event(description, from, to);
-                storage.saveTask(description, from, to);
-                return (taskList.addTask(task));
+                return processEvent(parser);
             }
             case "delete": {
-                if (parser.checkInputDetailsAbsent()) {
-                    throw new DukeException("The task number to delete cannot be empty.");
-                }
-                int taskNumber = Integer.parseInt(inputDetails);
-                storage.deleteTaskFromFile(taskNumber);
-                return (taskList.removeTask(taskNumber));
+                return processDelete(parser, inputDetails);
             }
             case "find": {
-                if (parser.checkInputDetailsAbsent()) {
-                    throw new DukeException("The keyword to find cannot be empty");
-                }
-                return (taskList.findTasks(inputDetails));
+                return processFind(parser, inputDetails);
             }
             case "reschedule": {
-                if (parser.checkInputDetailsAbsent()) {
-                    throw new DukeException("☹ OOPS!!! The info of task to reschedule cannot be empty.");
-                }
-                String[] rescheduleInfo = parser.parseRescheduleInfo();
-                int taskNumber = Integer.parseInt(rescheduleInfo[0]);
-                String rescheduleDetails = rescheduleInfo[1];
-                Task task = taskList.getTask(taskNumber);
-                storage.deleteTaskFromFile(taskNumber);
-                return (task.reschedule(rescheduleDetails, storage));
+                return processReschedule(parser);
             }
             default:
                 throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
@@ -122,6 +74,147 @@ public class Duke {
         } catch (Exception e) {
             return (ui.showMessage(e.getMessage()));
         }
+    }
+
+    /**
+     * Processes the list command.
+     * @return The list of tasks.
+     */
+    private String processList() {
+        return taskList.showList();
+    }
+
+    /**
+     * Process the mark command.
+     * @param parser Parser object that parses the whole command.
+     * @param inputDetails Details of the command instruction.
+     * @return Response after mark command executed.
+     * @throws DukeException When there is an invalid command input.
+     */
+    private String processMark(Parser parser, String inputDetails) throws DukeException {
+        if (parser.checkInputDetailsAbsent()) {
+            throw new DukeException("The task number to mark cannot be empty.");
+        }
+        int taskNumber = Integer.parseInt(inputDetails);
+        Task task = taskList.getTask(taskNumber);
+        return task.setDone();
+    }
+
+    /**
+     * Process the unmark command.
+     * @param parser Parser object that parses the whole command.
+     * @param inputDetails Details of the command instruction.
+     * @return Response after unmark command executed.
+     * @throws DukeException When there is an invalid command input.
+     */
+    private String processUnMark(Parser parser, String inputDetails) throws DukeException {
+        if (parser.checkInputDetailsAbsent()) {
+            throw new DukeException("The task number to unmark cannot be empty.");
+        }
+        int taskNumber = Integer.parseInt(inputDetails);
+        Task task = taskList.getTask(taskNumber);
+        return task.unsetDone();
+    }
+
+    /**
+     * Process the todo command.
+     * @param parser Parser object that parses the whole command.
+     * @param inputDetails Details of the command instruction.
+     * @return Response after todo command executed.
+     * @throws DukeException When there is an invalid command input.
+     */
+    private String processTodo(Parser parser, String inputDetails) throws DukeException {
+        if (parser.checkInputDetailsAbsent()) {
+            throw new DukeException("☹ OOPS!!! The description of a todo task cannot be empty.");
+        }
+        Task task = new Todo(inputDetails);
+        storage.saveTask(inputDetails);
+        return taskList.addTask(task);
+    }
+
+    /**
+     * Process the deadline command.
+     * @param parser Parser object that parses the whole command.
+     * @return Response after deadline command executed.
+     * @throws DukeException When there is an invalid command input.
+     */
+    private String processDeadline(Parser parser) throws DukeException {
+        if (parser.checkInputDetailsAbsent()) {
+            throw new DukeException("☹ OOPS!!! The info of a deadline task cannot be empty.");
+        }
+        String[] deadlineInfo = parser.parseInputDetailsDeadline();
+        String description = deadlineInfo[0];
+        String deadline = deadlineInfo[1];
+        Task task = new Deadline(description, deadline);
+        storage.saveTask(description, deadline);
+        return taskList.addTask(task);
+    }
+
+    /**
+     * Process the event command.
+     * @param parser Parser object that parses the whole command.
+     * @return Response after event command executed.
+     * @throws DukeException When there is an invalid command input.
+     */
+    private String processEvent(Parser parser) throws DukeException {
+        if (parser.checkInputDetailsAbsent()) {
+            throw new DukeException("☹ OOPS!!! The info of a event task cannot be empty.");
+        }
+        String[] eventInfo = parser.parseInputDetailsEvent();
+        String description = eventInfo[0];
+        String from = eventInfo[1];
+        String to = eventInfo[2];
+        Task task = new Event(description, from, to);
+        storage.saveTask(description, from, to);
+        return taskList.addTask(task);
+    }
+
+    /**
+     * Process the delete command.
+     * @param parser Parser object that parses the whole command.
+     * @param inputDetails Details of the command instruction.
+     * @return Response after delete command executed.
+     * @throws DukeException When there is an invalid command input.
+     */
+    private String processDelete(Parser parser, String inputDetails) throws DukeException {
+        if (parser.checkInputDetailsAbsent()) {
+            throw new DukeException("The task number to delete cannot be empty.");
+        }
+        int taskNumber = Integer.parseInt(inputDetails);
+        storage.deleteTaskFromFile(taskNumber);
+        return taskList.removeTask(taskNumber);
+    }
+
+    /**
+     * Process the find command.
+     * @param parser Parser object that parses the whole command.
+     * @param inputDetails Details of the command instruction.
+     * @return Response after find command executed.
+     * @throws DukeException When there is an invalid command input.
+     */
+    private String processFind(Parser parser, String inputDetails) throws DukeException {
+        if (parser.checkInputDetailsAbsent()) {
+            throw new DukeException("The keyword to find cannot be empty");
+        }
+        return taskList.findTasks(inputDetails);
+    }
+
+    /**
+     * Process the reschedule command.
+     * @param parser Parser object that parses the whole command.
+     * @return Response after reschedule command executed.
+     * @throws DukeException When there is an invalid command input.
+     */
+    private String processReschedule(Parser parser) throws DukeException {
+        if (parser.checkInputDetailsAbsent()) {
+            throw new DukeException("☹ OOPS!!! The info of task to reschedule cannot be empty.");
+        }
+        String[] rescheduleInfo = parser.parseRescheduleInfo();
+        int taskNumber = Integer.parseInt(rescheduleInfo[0]);
+        String rescheduleDetails = rescheduleInfo[1];
+        Task task = taskList.getTask(taskNumber);
+        storage.deleteTaskFromFile(taskNumber);
+        return (task.reschedule(rescheduleDetails, storage));
     }
 
 }
