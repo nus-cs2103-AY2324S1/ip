fcf4ea2bfe91c60e520b5a8412884bb435f4b968
Nam Do Hyun <e0638876@u.nus.edu>
2023-09-13 22:01:10 +0800

    Change ekuD image to a more reflective one
    ekuD image does not really represent its character right now.
    Change the image to something that is more representative.

--- src/main/java/brandon/chatbot/ui/MainWindow.java
+++ src/main/java/brandon/chatbot/ui/MainWindow.java
@@ -31,7 +31,7 @@ public class MainWindow extends AnchorPane {
     private Duke duke;
 
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/ekuD.png"));
 
     /**
      * Initializes the scene with welcome and a help message.
