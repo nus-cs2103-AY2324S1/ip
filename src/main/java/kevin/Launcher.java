ccdb0253560eec89a21d3e7d29e90b3d7f5c8926
aliciamichellew <aliciamichellew@gmail.com>
2023-09-14 21:20:02 +0800

    Implement fxml

--- src/main/java/kevin/Launcher.java
+++ src/main/java/kevin/Launcher.java
@@ -7,6 +7,6 @@ import javafx.application.Application;
  */
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Kevin.class, args);
+        Application.launch(Main.class, args);
     }
 }
