1f859050ef2b91abd84f94f74043547be7128c88
vivienherq <heruoqingvivien@gmail.com>
2023-09-14 12:11:42 +0800

    Add functionality to GUI for dude

--- src/main/java/dude/Dude.java
+++ src/main/java/dude/Dude.java
@@ -2,29 +2,13 @@ package dude;
 
 import java.io.FileNotFoundException;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
 import dude.command.Command;
 
 /**
  * Dude is a programme that allows users to manage their tasks.
  */
 
-public class Dude extends Application {
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-
+public class Dude {
     private TaskList taskList;
     private Storage storage;
     private Ui ui;
@@ -45,57 +29,6 @@ public class Dude extends Application {
             ui.showLoadingError();
             taskList = new TaskList();
         }
-    }
-
-    @Override
-    public void start(Stage stage) {
-        //Step 1: Setting up required components
-
-        //Container for the content of the chat to scroll
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        Scene scene = new Scene(mainLayout); // Setting the scene to be mainLayout
-
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
-
-        //Step 2: Formatting the window to look as expected
-        stage.setTitle("Dude");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385.0, 535.0);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(342.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
     }
 
     /**
