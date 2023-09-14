d901efc5ee92a83447d1c3783ac2ca93045868e2
awhb <awhangbin@gmail.com>
2023-09-14 17:50:54 +0800

    Week 5 - Duke BCD-Extension: C-FriendlierSyntax

--- src/main/java/chadbod/Command.java
+++ src/main/java/chadbod/Command.java
@@ -4,25 +4,27 @@ package chadbod;
  * Enumeration representing various commands that can be used in the ChadBod application.
  */
 public enum Command {
-    BYE("bye"),
-    LIST("list"),
-    MARK("mark"),
-    UNMARK("unmark"),
-    TODO("todo"),
-    DEADLINE("deadline"),
-    EVENT("event"),
-    DELETE("delete"),
-    FIND("find");
+    BYE("bye", "bb"),
+    LIST("list", "ls"),
+    MARK("mark", "mk"),
+    UNMARK("unmark", "umk"),
+    TODO("todo", "td"),
+    DEADLINE("deadline", "dl"),
+    EVENT("event", "ev"),
+    DELETE("delete", "del"),
+    FIND("find", "f");
 
     private final String value;
+    private final String alias;
 
     /**
      * Constructor for the Command enumeration.
      *
      * @param value The string representation of the command.
      */
-    Command(String value) {
+    Command(String value, String alias) {
         this.value = value;
+        this.alias = alias;
     }
 
     /**
@@ -31,6 +33,14 @@ public enum Command {
      * @return The string value of the command.
      */
     public String getValue() {
-        return value;
+        return this.value;
+    }
+    /**
+     * Get the string alias of the command.
+     *
+     * @return The string alias of the command.
+     */
+    public String getAlias() {
+        return this.alias;
     }
 }
