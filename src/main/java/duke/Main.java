98bcc9388d01f29905462e115a1d92f10c82208d
leesiyuan <lee.si.yuan.20@gmail.com>
2023-09-15 02:07:26 +0800

    Update Main.java
    Added Duke as the name of the application.

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -29,6 +29,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Duke");
             fxmlLoader.<Window>getController().setDuke(duke);
             stage.show();
         } catch (IOException e) {
