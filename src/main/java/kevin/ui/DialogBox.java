e980cf4f5703b22a4612fc2b4700b0a81e1e2ba6
aliciamichellew <aliciamichellew@gmail.com>
2023-09-14 20:45:31 +0800

    Make Gui until iteration 2

---
+++ src/main/java/kevin/ui/DialogBox.java
@@ -0,0 +1,28 @@
+package kevin.ui;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+/**
+ * Custom control for dialog box.
+ */
+public class DialogBox extends HBox {
+
+    private Label text;
+    private ImageView displayPicture;
+
+    public DialogBox(Label l, ImageView iv) {
+        text = l;
+        displayPicture = iv;
+
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+
+        this.setAlignment(Pos.TOP_RIGHT);
+        this.getChildren().addAll(text, displayPicture);
+    }
+}
+
