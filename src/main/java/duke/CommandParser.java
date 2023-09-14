56723ebec0d611b1fca1091eb4841669216ad14d
KamiliArsyad <arsyad.ik@gmail.com>
2023-09-14 22:13:12 +0800

    implement parser

---
+++ src/main/java/duke/CommandParser.java
@@ -0,0 +1,52 @@
+package duke;
+
+import duke.command.AddCommand;
+import duke.command.Command;
+import duke.command.DeleteCommand;
+import duke.command.ExitCommand;
+import duke.command.FindCommand;
+import duke.command.ListCommand;
+import duke.command.MarkCommand;
+
+/**
+ * Parses user input.
+ */
+public class CommandParser {
+
+    /**
+     * Parses user input into a command for execution.
+     * @param input full user input string
+     * @return the command based on the user input
+     */
+    public static Command parse(String input) {
+        if (input.equals("bye")) {
+            return new ExitCommand();
+        }
+
+        if (input.equals("list")) {
+            return new ListCommand();
+        }
+
+        if (input.startsWith("mark")) {
+            int index = Integer.parseInt(input.split(" ")[1]) - 1;
+            return new MarkCommand(index);
+        }
+
+        if (input.startsWith("unmark")) {
+            int index = Integer.parseInt(input.split(" ")[1]) - 1;
+            return new MarkCommand(index);
+        }
+
+        if (input.startsWith("find")) {
+            String keyword = input.split(" ")[1];
+            return new FindCommand(keyword);
+        }
+
+        if (input.startsWith("delete")) {
+            int index = Integer.parseInt(input.split(" ")[1]) - 1;
+            return new DeleteCommand(index);
+        }
+
+        return new AddCommand(input);
+    }
+}
--- src/main/java/duke/InputHandler.java
+++ src/main/java/duke/InputHandler.java
@@ -2,6 +2,7 @@ package duke;
 
 import java.util.Scanner;
 
+import duke.command.Command;
 import duke.task.TaskStorage;
 
 class InputHandler {
@@ -16,44 +17,13 @@ class InputHandler {
         this.taskStorage = taskStorage;
     }
 
-    // TODO: Implement a parser instead of hard coding if-elses here
     public void handleInput() {
         String input = scanner.nextLine();
-        if (input.equals("bye")) {
+        Command command = CommandParser.parse(input);
+        if (command.isExit()) {
             isExit = true;
-            return;
         }
-
-        if (input.equals("list")) {
-            System.out.println(taskStorage);
-            return;
-        }
-
-        if (input.startsWith("mark")) {
-            int index = Integer.parseInt(input.split(" ")[1]) - 1;
-            outputUI.echo(taskStorage.markAsDone(index));
-            return;
-        }
-
-        if (input.startsWith("unmark")) {
-            int index = Integer.parseInt(input.split(" ")[1]) - 1;
-            outputUI.echo(taskStorage.unmarkAsDone(index));
-            return;
-        }
-
-        if (input.startsWith("find")) {
-            String keyword = input.split(" ")[1];
-            outputUI.echo(taskStorage.find(keyword));
-            return;
-        }
-
-        if (input.startsWith("delete")) {
-            int index = Integer.parseInt(input.split(" ")[1]) - 1;
-            outputUI.echo(taskStorage.delete(index));
-            return;
-        }
-
-        outputUI.echo(taskStorage.save(input));
+        outputUI.echo(command.execute(taskStorage));
     }
 
     public boolean isExit() {
