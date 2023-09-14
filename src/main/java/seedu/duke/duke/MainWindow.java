04d703a4e9f2c031159ee2a00c4825ab0767c743
simbayippy <simbayippy@gmail.com>
2023-09-14 17:41:30 +0800

    Full commit message. Up to now, fully built the Duke chatbot, support various functions
    Moving forward, Will work on
    * improving UI
    * making chat box dynamic

--- src/main/java/seedu/duke/duke/MainWindow.java
+++ src/main/java/seedu/duke/duke/MainWindow.java
@@ -35,7 +35,8 @@ public class MainWindow extends AnchorPane {
      */
     @FXML
     public void initialize() {
-        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        //scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
         dialogContainer.getChildren().addAll(
                 DialogBox.getDukeDialog("Hello! I'm Duke\n"
                                 + "What can I do for you?\n"
