a7e105aa454618bfc471277ea91b89c10f743cf0
EricXiong420 <E0958551@u.nus.edu>
2023-09-15 02:17:33 +0800

    Add assertions

--- src/main/java/hachi/Command.java
+++ src/main/java/hachi/Command.java
@@ -3,14 +3,9 @@ package hachi;
 import exceptions.HachiException;
 
 /**
- * Parent command class.
+ * Parent abstract command class.
  */
 public abstract class Command {
-
-    /**
-     * Default command word.
-     */
-    private static final String COMMAND_WORD = "default_cmd";
 
     private String[] arguments;
 
@@ -18,17 +13,12 @@ public abstract class Command {
         this.arguments = arguments;
     }
 
-    /**
-     * Executes the command.
-     * @return The output for the given command.
-     */
-    public String execute() throws HachiException {
-        return "";
-    }
-
     public String[] getArguments() {
         return arguments;
     }
+
+    public abstract String execute() throws HachiException;
+
 
     protected abstract void checkArgumentLength(int argumentLength) throws HachiException;
 }
