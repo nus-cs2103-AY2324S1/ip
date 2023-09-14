437c685b8f58c3c789956c67e58ce5dfe31eae1a
aliciamichellew <aliciamichellew@gmail.com>
2023-09-14 13:34:13 +0800

    Setup JavaFX

--- src/main/java/kevin/Kevin.java
+++ src/main/java/kevin/Kevin.java
@@ -3,6 +3,10 @@ package kevin;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import kevin.evaluator.Evaluator;
 import kevin.exception.KevinException;
 import kevin.parser.FileParser;
@@ -15,7 +19,7 @@ import kevin.ui.Logger;
 /**
  * Kevin class, a task list chatbot.
  */
-public class Kevin {
+public class Kevin extends Application {
     public static void main(String[] args) {
         Logger logger = new Logger();
         Parser parser = new Parser();
@@ -49,6 +53,15 @@ public class Kevin {
             }
         }
         logger.bye();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
 
