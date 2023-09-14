68dd490844637767d1247c6f00e6d3c5d34774cd
Mohammed-Faizzzz <110959467+Mohammed-Faizzzz@users.noreply.github.com>
2023-09-14 17:43:56 +0800

    Add OOP: make Boolean field done private.
    Previously, Boolean field done was package private and classes Deadline, Event, and Todo modified the field directly.
    Making the field private and using the superclass method to modify it adheres to OOP principles

--- src/main/java/duke/Deadline.java
+++ src/main/java/duke/Deadline.java
@@ -23,14 +23,14 @@ public class Deadline extends Task {
 
     @Override
     public void mark() {
-        this.done = true;
+        super.mark();
         System.out.println(super.line() + "Okay, I have marked this task as completed!" + "\n" + this.toString());
         System.out.println(super.line());
     }
 
     @Override
     public void unMark() {
-        this.done = false;
+        super.unMark();
         System.out.println(super.line() + "Okay, I have marked this task as incomplete!" + "\n" + this.toString());
         System.out.println(super.line());
     }
