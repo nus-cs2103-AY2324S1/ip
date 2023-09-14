b7113ccae6e16b29eeccfa0732f1ad3a7e2defad
KamiliArsyad <arsyad.ik@gmail.com>
2023-09-15 00:11:46 +0800

    Fix Bug
    Create a long commit message by fixing a trivially short bug.

--- src/main/java/duke/CommandParser.java
+++ src/main/java/duke/CommandParser.java
@@ -1,12 +1,6 @@
 package duke;
 
-import duke.command.AddCommand;
-import duke.command.Command;
-import duke.command.DeleteCommand;
-import duke.command.ExitCommand;
-import duke.command.FindCommand;
-import duke.command.ListCommand;
-import duke.command.MarkCommand;
+import duke.command.*;
 
 /**
  * Parses user input.
@@ -34,7 +28,7 @@ public class CommandParser {
 
         if (input.startsWith("unmark")) {
             int index = Integer.parseInt(input.split(" ")[1]) - 1;
-            return new MarkCommand(index);
+            return new UnmarkCommand(index);
         }
 
         if (input.startsWith("find")) {
