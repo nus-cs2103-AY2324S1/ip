3c4bd07a17a866b89fb3157ab880986a3a9c34d4
Mohammed-Faizzzz <110959467+Mohammed-Faizzzz@users.noreply.github.com>
2023-09-14 17:53:58 +0800

    Delete dead code:
    Previous iterations of Duke used the Duke class as the main class, and used the startChat function that did not require an input.
    With the new iteration making use of the GUI, the previous code becomes redundant. Deleting this dead code improves code quality.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,13 +1,6 @@
 package duke;
 import java.io.FileNotFoundException;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.layout.VBox;
-
-
 /**
  * The Duke class represents a task management application that allows users to interact with tasks.
  * It provides methods to manage tasks, mark them as done, and perform various operations on tasks.
@@ -16,12 +9,6 @@ public class Duke {
     private final Ui ui;
     private TaskList tasks;
     private final String filePath = "./src/main/data/tasklist.txt";
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
     /**
      * Constructs a Duke instance with the specified file path to load task data from.
@@ -34,58 +21,6 @@ public class Duke {
         } catch (FileNotFoundException e) {
             tasks = new TaskList();
         }
-    }
-
-    /**
-     * The main method that starts the Duke application.
-     *
-     * @param args Command-line arguments.
-     */
-    public static void main(String[] args) {
-        new Duke().startChat();
-    }
-
-    /**
-     * Starts the chat interface for interacting with the Duke application.
-     * Manages user inputs and performs corresponding actions on tasks.
-     */
-    public void startChat() {
-        ui.greet();
-        Parser parser = new Parser();
-        String userInput = parser.getUserInput();
-        parser.setUserInput(userInput);
-        while (true) {
-            try {
-                if (parser.bye()) {
-                    break;
-                }
-                if (parser.list()) {
-                    tasks.printFileContents();
-                } else if (parser.mark()) {
-                    tasks.mark(userInput);
-                } else if (parser.unMark()) {
-                    tasks.unMark(userInput);
-                } else if (parser.delete()) {
-                    tasks.delete(userInput);
-                } else if (parser.todo()) {
-                    tasks.handleTodo(userInput);
-                } else if (parser.deadline()) {
-                    tasks.handleDeadline(userInput);
-                } else if (parser.event()) {
-                    tasks.handleEvent(userInput);
-                } else if (parser.find()) {
-                    tasks.handleFind(userInput);
-                } else {
-                    throw new DukeException("Error: Invalid Command!");
-                }
-            } catch (DukeException exception) {
-                System.out.println(Ui.line + exception.getMessage() + "\n" + Ui.line);
-            }
-            userInput = parser.getUserInput();
-            parser.setUserInput(userInput);
-        }
-        ui.goodbye();
-        parser.goodbye();
     }
 
     public String startChat(String userInput) {
@@ -121,7 +56,7 @@ public class Duke {
         }
         ui.goodbye();
         parser.goodbye();
-        return "yo";
+        return "";
     }
 
     /**
