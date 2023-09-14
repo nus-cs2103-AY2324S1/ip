735bfd5eb78e8ce551cf0ab6c2483785945f3b93
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-09-14 09:38:03 +0800

    devfeat(test): Set up runnable dummy tests

---
+++ src/test/java/com/cloud/chatbot/token/CommandManagerTest.java
@@ -0,0 +1,19 @@
+package com.cloud.chatbot.token;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+
+public class CommandManagerTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void anotherDummyTest() {
+        assertEquals(4, 4);
+    }
+}
