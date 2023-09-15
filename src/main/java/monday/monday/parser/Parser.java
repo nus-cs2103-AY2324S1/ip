1fcef7ad1285d9a701f3d14a014b1c1e30dcfd01
nubnubyas <e0970089@u.nus.edu>
2023-09-15 22:45:36 +0800

    Refactor and improve code structure in Parser.java
    This commit refactors the Parser class in the following ways:
    - Created separate private methods for each command's logic to prevent
    long methods.
    - Added a DEFAULT command to handle unrecognized commands gracefully.
    
    These changes make the code more organized, easier to understand, and
    better aligned with Java coding standards.

--- src/main/java/monday/monday/parser/Parser.java
+++ src/main/java/monday/monday/parser/Parser.java
@@ -7,13 +7,12 @@ import monday.task.Event;
 import monday.task.TaskList;
 import monday.task.ToDo;
 
-
 /**
  * Parser class is responsible for parsing user input and performing the corresponding actions.
  */
 public class Parser {
 
-    private TaskList taskList;
+    private final TaskList taskList;
 
     /**
      * An enumeration of available commands.
@@ -28,7 +27,8 @@ public class Parser {
         EVENT,
         DELETE,
         FIND,
-        HI
+        HI,
+        DEFAULT,
     }
 
     public Parser(TaskList taskList) {
@@ -48,8 +48,12 @@ public class Parser {
         String command = input[0];
         String content = input.length > 1 ? input[1] : null;
 
-        Command commandEnum = Command.valueOf(command.toUpperCase());
-
+        Command commandEnum;
+        try {
+            commandEnum = Command.valueOf(command.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            commandEnum = Command.DEFAULT;
+        }
 
         switch (commandEnum) {
             case HI:
@@ -58,89 +62,117 @@ public class Parser {
                 return Ui.exit();
             case LIST:
                 return taskList.toString();
-            case MARK: {
-                if (content == null) {
-                    throw new MondayException("Mark requires a index to mark the task as completed.");
-                }
-                int index = Integer.parseInt(content);
-
-                return taskList.mark(index);
-            }
-            case UNMARK: {
-                if (content == null) {
-                    throw new MondayException("UnMark requires a index to mark the task as uncompleted.");
-                }
-
-                int index = Integer.parseInt(content);
-
-                return taskList.unMark(index);
-            }
+            case MARK:
+                return markCommand(content);
+            case UNMARK:
+                return unMarkCommand(content);
             case TODO:
-                if (content == null) {
-                    throw new MondayException("The description of a todo cannot be empty.\n"
-                            + "Usage: todo (task)");
-                }
-
-                return taskList.addToList(new ToDo(content));
+                return toDoCommand(content);
             case DEADLINE:
-                try {
-                    if (content == null) {
-                        throw new MondayException("The description of a deadline cannot be empty.\n"
-                                + "Usage: deadline (task) /by (time)");
-                    }
-
-                    String[] taskDetails = content.split("/by", 2);
-                    String description = taskDetails[0];
-                    String date = taskDetails[1];
-
-                    return taskList.addToList(new Deadline(description.trim(), date.trim()));
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    throw new IllegalArgumentException("Invalid Format. "
-                            + "Usage: deadline (task) /by (time)");
-                }
+                return deadLineCommand(content);
             case EVENT:
-                try {
-                    if (content == null) {
-                        throw new MondayException("The description of a event cannot be empty.\n"
-                                + "Usage: event (task) /from (start time) /to (end time)");
-                    }
-
-                    String[] taskDetails = content.split("/from", 2);
-                    String description = taskDetails[0];
-                    String[] taskTiming = taskDetails[1].split("/to", 2);
-                    String start = taskTiming[0];
-                    String end = taskTiming[1];
-
-                    return taskList.addToList(new Event(description.trim(),
-                            start.trim(),
-                            end.trim()));
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    throw new IllegalArgumentException("Invalid Format. "
-                            + "Usage: event (task) /from (start time) /to (end time)");
-                }
+                return eventCommand(content);
             case DELETE:
-                if (content == null) {
-                    throw new MondayException("Delete requires a index to delete the task");
-                }
-                int index = Integer.parseInt(content);
-
-                return taskList.delete(index);
+                return deleteCommand(content);
             case FIND:
-                if (content == null) {
-                    throw new MondayException("Find requires a keyword to find the tasks");
-                }
-
-                String[] keywordDetails = content.split(" ");
-
-                if (keywordDetails.length > 1) {
-                    throw new IllegalArgumentException("Invalid Format. "
-                            + "Usage: find (keyword), there should only be one keyword.");
-                }
-
-                return taskList.find(keywordDetails[0]);
-            default:
+                return findCommand(content);
+            case DEFAULT:
                 throw new MondayException("Sorry, I do not understand what that means.\n"
                         + "Please provide a valid input/command. e.g todo read book");
+            default:
+                throw new MondayException("Sorry the ChatBot, please kindly wait for update.");
         }
+    }
+
+    private String markCommand(String content) throws MondayException {
+        if (content == null) {
+            throw new MondayException("Mark requires a index to mark the task as completed.");
+        }
+        int index = Integer.parseInt(content);
+
+        return taskList.mark(index);
+    }
+
+    private String unMarkCommand(String content) throws MondayException {
+        if (content == null) {
+            throw new MondayException("UnMark requires a index to mark the task as uncompleted.");
+        }
+
+        int index = Integer.parseInt(content);
+
+        return taskList.unMark(index);
+    }
+
+    private String toDoCommand(String content) throws MondayException {
+        if (content == null) {
+            throw new MondayException("The description of a todo cannot be empty.\n"
+                    + "Usage: todo (task)");
+        }
+
+        return taskList.addToList(new ToDo(content));
+    }
+
+    private String deadLineCommand(String content) throws MondayException {
+        try {
+            if (content == null) {
+                throw new MondayException("The description of a deadline cannot be empty.\n"
+                        + "Usage: deadline (task) /by (time)");
+            }
+
+            String[] taskDetails = content.split("/by", 2);
+            String description = taskDetails[0];
+            String date = taskDetails[1];
+
+            return taskList.addToList(new Deadline(description.trim(), date.trim()));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("Invalid Format. "
+                    + "Usage: deadline (task) /by (time)");
+        }
+    }
+
+    private String eventCommand(String content) throws MondayException {
+        try {
+            if (content == null) {
+                throw new MondayException("The description of a event cannot be empty.\n"
+                        + "Usage: event (task) /from (start time) /to (end time)");
+            }
+
+            String[] taskDetails = content.split("/from", 2);
+            String description = taskDetails[0];
+            String[] taskTiming = taskDetails[1].split("/to", 2);
+            String start = taskTiming[0];
+            String end = taskTiming[1];
+
+            return taskList.addToList(new Event(description.trim(),
+                    start.trim(),
+                    end.trim()));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("Invalid Format. "
+                    + "Usage: event (task) /from (start time) /to (end time)");
+        }
+    }
+
+    private String deleteCommand(String content) throws MondayException {
+        if (content == null) {
+            throw new MondayException("Delete requires a index to delete the task");
+        }
+        int index = Integer.parseInt(content);
+
+        return taskList.delete(index);
+    }
+
+    private String findCommand(String content) throws MondayException {
+        if (content == null) {
+            throw new MondayException("Find requires a keyword to find the tasks");
+        }
+
+        String[] keywordDetails = content.split(" ");
+
+        if (keywordDetails.length > 1) {
+            throw new IllegalArgumentException("Invalid Format. "
+                    + "Usage: find (keyword), there should only be one keyword.");
+        }
+
+        return taskList.find(keywordDetails[0]);
     }
 }
