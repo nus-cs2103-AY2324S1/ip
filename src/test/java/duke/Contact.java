ec026b13c138805e01c74c7f83db1f06bdbdae51
Mohammed-Faizzzz <110959467+Mohammed-Faizzzz@users.noreply.github.com>
2023-09-15 09:38:02 +0800

    Create Contact Class

---
+++ src/test/java/duke/Contact.java
@@ -0,0 +1,15 @@
+package duke;
+
+public class Contact {
+    private String name;
+    private int contactNum;
+
+    public Contact(String name) {
+        this.name = name;
+    }
+
+    public Contact(String name, int contactNum) {
+        this.name = name;
+        this.contactNum = contactNum;
+    }
+}
