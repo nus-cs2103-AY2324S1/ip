c8d0d66bf60317c979c64f4e27bbc2e308e38d3f
ji-just-ji <wongjk9999@gmail.com>
2023-09-15 11:58:47 +0800

    Create ArchiveHandler

---
+++ src/main/java/peko/memory/ArchiveHandler.java
@@ -0,0 +1,35 @@
+package peko.memory;
+
+import peko.tasks.Task;
+
+import java.io.*;
+
+public class ArchiveHandler {
+
+    private static File file = new File("src/main/Archive.txt");
+    private static PrintWriter printWriter;
+    public static void archive(Task task) {
+        fileManager();
+        String toStore = task.toStore() + "\n";
+        try {
+            Writer temp;
+            temp = new BufferedWriter(new FileWriter("src/main/Archive.txt", true));
+            temp.append(toStore);
+            temp.close();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private static void fileManager() {
+        try {
+            printWriter = new PrintWriter(file.getAbsoluteFile());
+            printWriter.write("");
+            printWriter.close();
+        } catch (FileNotFoundException e) {
+            File temp = new File(file.getParentFile(), "List.txt");
+            file = temp;
+        }
+    }
+
+}
