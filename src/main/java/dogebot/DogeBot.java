af262eb87bd79b3d96a160551ee4a4bcb9eff40f
kenvynKwek <kenvynkwek@gmail.com>
2023-09-16 13:53:32 +0800

    Add GUI Part 3: Interacting with the user
    Integrated JavaFX GUI to display user inputs into console and echo the command.

--- src/main/java/dogebot/DogeBot.java
+++ src/main/java/dogebot/DogeBot.java
@@ -3,13 +3,26 @@ package dogebot;
 import java.io.File;
 import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /**
  * The DogeBot program is an application that keeps track of tasks. Different types of tasks can be added, deleted,
  * marked and progress is saved to a text file.
  *
  * @author Kenvyn Kwek
  */
-public class DogeBot {
+public class DogeBot extends Application {
     protected static TaskList tasks;
     private static final String HOME = System.getProperty("user.home"); // get relative path
     private static final java.nio.file.Path PATH = java.nio.file.Paths.get(HOME, "OneDrive", "Desktop", "iP",
@@ -18,6 +31,13 @@ public class DogeBot {
     private Ui ui;
     private Parser userInput;
     private Storage storage;
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField commandInput;
+    private Button sendButton;
+    private Scene scene;
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/user_gigachad.png"));
+    private Image dogebot = new Image(this.getClass().getResourceAsStream("/images/dogebot.png"));
 
     /**
      * Initializes a DogeBot object with a filename.
@@ -41,6 +61,12 @@ public class DogeBot {
     }
 
     /**
+     * Empty constructor for Launcher.
+     */
+    public DogeBot() {
+    }
+
+    /**
      * Runs the DogeBot program.
      */
     public void run() {
@@ -52,7 +78,107 @@ public class DogeBot {
         storage.save(tasks);
     }
 
-    public static void main(String[] args) {
+    // public static void main(String[] args) {
+    //     new DogeBot("tasklist.txt").run();
+    // }
+
+    /**
+     * Main entry point for the start of JavaFX applications.
+     *
+     * @param stage the primary stage for this application, onto which the application scene can be set.
+     */
+    @Override
+    public void start(Stage stage) {
         new DogeBot("tasklist.txt").run();
+
+        // Step 1. Formatting the window to look as expected.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        commandInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, commandInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        // Step 2. Formatting the window to look as expected
+        stage.setTitle("DogeBot");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        commandInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(commandInput, 1.0);
+        AnchorPane.setBottomAnchor(commandInput, 1.0);
+
+        // Step 3. Add functionality to handle user input.
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+
+        commandInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+
+        // Scroll down to the end every time dialogContainer's height changes
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+    }
+
+    /**
+     * Iteration 1: Creates a label with the specified text and adds it to the dialog container.
+     * @param text String containing text to add.
+     * @return a label with the specified text that has word wrap enabled.
+     */
+    private Label getDialogLabel(String text) {
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+        return textToAdd;
+    }
+
+    /**
+     * Handles inputs with GUI.
+     */
+    private void handleUserInput() {
+        Label userText = new Label(commandInput.getText());
+        Label dogeBotText = new Label(getResponse(commandInput.getText()));
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, new ImageView(user)),
+                DialogBox.getDogeBotDialog(dogeBotText, new ImageView(dogebot))
+        );
+        commandInput.clear();
+    }
+
+    /**
+     * Gets Responses from DogeBot to be output in GUI.
+     *
+     * @param input User input.
+     * @return Output from DogeBot.
+     */
+    private String getResponse(String input) {
+        return "DogeBot heard: " + input;
     }
 }
