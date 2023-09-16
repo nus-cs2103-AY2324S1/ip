d46209ef8e6725af0d9aa098378e75d99b66db87
Anton <111056306+antonTan96@users.noreply.github.com>
2023-09-16 13:13:00 +0800

    Revert "Use java assertion feature"

--- src/main/java/crackerpackage/Storage.java
+++ src/main/java/crackerpackage/Storage.java
@@ -56,7 +56,6 @@ public class Storage {
         for (int i = 0; i < list.size(); i++) {
             StringBuilder taskString = new StringBuilder();
             Task task = list.getTask(i);
-            assert task != null;
             taskString.append(list.getTaskString(i).charAt(1));
             taskString.append('|');
             taskString.append(task.getStatus());
