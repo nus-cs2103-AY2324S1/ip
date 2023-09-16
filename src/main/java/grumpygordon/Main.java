5dd2c91b293cb08ab3285b6a952dba9beb22ccab
craigtonlian <108467437+craigtonlian@users.noreply.github.com>
2023-09-16 14:49:51 +0800

    Fix checkstyle error in Main

--- src/main/java/grumpygordon/Main.java
+++ src/main/java/grumpygordon/Main.java
@@ -8,7 +8,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
