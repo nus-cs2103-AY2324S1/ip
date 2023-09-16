a03917171ba10a566b510d4d9b4d8a73ff513b0d
DESKTOP-ITF4GUD\94866 <e0772448@u.nus.edu>
2023-09-16 16:53:22 +0800

    Fix bugs on datetime

--- src/main/java/duke/io/Storage.java
+++ src/main/java/duke/io/Storage.java
@@ -3,21 +3,24 @@ package duke.io;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.Files;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
-import duke.tasks.Task;
-import duke.tasks.Todo;
-import duke.tasks.Event;
-import duke.tasks.Deadline;
-import duke.tasks.TaskList;
-import duke.exceptions.UnknownCommandException;
-import duke.exceptions.FileIoException;
 import duke.exceptions.ErrorMessages;
+import duke.exceptions.FileIoException;
+import duke.exceptions.UnknownCommandException;
+import duke.parsers.TimeParser;
+import duke.tasks.Deadline;
+import duke.tasks.Event;
+import duke.tasks.Task;
+import duke.tasks.TaskList;
+import duke.tasks.Todo;
 
 public class Storage {
     private static final String TODO_FLAG = "[T]";
@@ -25,6 +28,9 @@ public class Storage {
     private static final String EVENT_FLAG = "[E]";
     private static final String IS_DONE_FLAG = "[X]";
     private static final String NOT_DONE_FLAG = "[ ]";
+
+    private static final String INVALID_TYPE_ERROR = "Type of event can only be [T], [D], or [E]";
+    private static final String INVALID_STATUS_ERROR = "IsDone flag of event can only be [ ], or [X]";
 
     private final String completeFilePath;
     private final Path parentDirectory;
@@ -71,18 +77,28 @@ public class Storage {
         boolean isTaskCompleted = taskData[1].equals(IS_DONE_FLAG);
 
         switch (taskData[0]) {
-            case TODO_FLAG:
-                addTask(tasks, new Todo(taskData[2].strip()), isTaskCompleted);
-                break;
-            case DEADLINE_FLAG:
-                addTask(tasks, new Deadline(taskData[2].trim(), taskData[3]), isTaskCompleted);
-                break;
-            case EVENT_FLAG:
-                addTask(tasks, new Event(taskData[2].strip(), taskData[3], taskData[4]), isTaskCompleted);
-                break;
-            default:
-                throw new UnknownCommandException("OOPS!!! Unrecognized task type: " + taskData[0]);
+        case TODO_FLAG:
+            addTask(tasks, new Todo(taskData[2].strip()), isTaskCompleted);
+            break;
+        case DEADLINE_FLAG:
+            LocalDateTime ddl = TimeParser.parseToLocalDateTime(taskData[3]);
+            addTask(tasks, new Deadline(taskData[2].trim(), ddl), isTaskCompleted);
+            break;
+        case EVENT_FLAG:
+            LocalDateTime from = TimeParser.parseToLocalDateTime(taskData[3]);
+            LocalDateTime to = TimeParser.parseToLocalDateTime(taskData[4]);
+            addTask(tasks, new Event(taskData[2].strip(), from, to), isTaskCompleted);
+            break;
+        default:
+            throw new UnknownCommandException("OOPS!!! Unrecognized task type: " + taskData[0]);
         }
+    }
+
+    private static void checkStorageFormat(String tag, String isDone) {
+        assert Objects.equals(tag, TODO_FLAG) || Objects.equals(tag, DEADLINE_FLAG)
+                || Objects.equals(tag, EVENT_FLAG) : INVALID_TYPE_ERROR;
+
+        assert Objects.equals(isDone, IS_DONE_FLAG) || Objects.equals(isDone, NOT_DONE_FLAG) : INVALID_STATUS_ERROR;
     }
 
     private static void addTask(TaskList list, Task task, boolean isDone) {
--- src/main/java/duke/parsers/TimeParser.java
+++ src/main/java/duke/parsers/TimeParser.java
@@ -12,22 +12,22 @@ import duke.exceptions.UnknownCommandException;
 import duke.exceptions.ErrorMessages;
 public class TimeParser {
     private static final String[] DATETIME_FORMATS = {
-            "MMM dd yyyy HHmm", "MMM dd yyyy HH:mm",
-            "yyyy-MM-dd'T'HH:mm", "dd/MM/yyyy HHmm",
-            "dd/MM/yyyy HH:mm", "yyyy/MM/dd HHmm",
-            "yyyy/MM/dd HH:mm", "yyyy/MM/dd'T'HHmm",
-            "yyyy-MM-dd HHmm", "yyyy-MM-dd HH:mm",
-            "dd MMM yyyy HHmm", "dd MMM yyyy HH:mm",
-            "MMM dd, yyyy HHmm", "MMM dd, yyyy HH:mm",
-            "dd-MM-yyyy HH:mm", "dd.MM.yyyy HH:mm",
-            "dd/MM/yy HH:mm", "yyyy-MM-dd'T'HH:mmZ",
-            "MMM dd yyyy hh:mm a", "yyyy-MM-dd HH:mm:ss"
+        "MMM dd yyyy HHmm", "MMM dd yyyy HH:mm",
+        "yyyy-MM-dd'T'HH:mm", "dd/MM/yyyy HHmm",
+        "dd/MM/yyyy HH:mm", "yyyy/MM/dd HHmm",
+        "yyyy/MM/dd HH:mm", "yyyy/MM/dd'T'HHmm",
+        "yyyy-MM-dd HHmm", "yyyy-MM-dd HH:mm",
+        "dd MMM yyyy HHmm", "dd MMM yyyy HH:mm",
+        "MMM dd, yyyy HHmm", "MMM dd, yyyy HH:mm",
+        "dd-MM-yyyy HH:mm", "dd.MM.yyyy HH:mm",
+        "dd/MM/yy HH:mm", "yyyy-MM-dd'T'HH:mmZ",
+        "MMM dd yyyy hh:mm a", "yyyy-MM-dd HH:mm:ss"
     };
 
     private static final String[] DATE_FORMATS = {
-            "MMM dd yyyy", "yyyy-MM-dd", "dd/MM/yyyy", "yyyy/MM/dd",
-            "dd MMM yyyy", "MMM dd, yyyy", "dd-mm-yyyy",
-            "dd/MM/yy", "yy-MM-dd", "dd.MM.yyyy"
+        "MMM dd yyyy", "yyyy-MM-dd", "dd/MM/yyyy", "yyyy/MM/dd",
+        "dd MMM yyyy", "MMM dd, yyyy", "dd-mm-yyyy",
+        "dd/MM/yy", "yy-MM-dd", "dd.MM.yyyy"
     };
     private static final List<DateTimeFormatter> ACCEPTABLE_DATE_FORMATTERS = Arrays.stream(DATE_FORMATS)
             .map(DateTimeFormatter::ofPattern)
