72b9842f1372e6a978de79fd2eb421bbee7134f3
cbj252 <cbj252@gmail.com>
2023-09-16 13:10:08 +0800

    Update tests

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -1,5 +1,6 @@
 package duke; //same package as the class being tested
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,13 +15,32 @@ public class ParserTest {
     }
 
     @Test
-    public void newTask_missingToFromEvent_nullReturned() {
+    public void parseCommandType_listCommandWithInvalidBack_exceptionThrown() {
+        try {
+            Parser.parseCommandType("list additionalInvalidWord");
+        } catch (Exception e) {
+            assertEquals("Invalid command", e.getMessage());
+        }
+    }
+
+    @Test
+    public void createNewTask_missingToFromEvent_worksNormally() {
         try {
             Task test = Parser.createNewTask("event 1 /from 2");
             assert test != null;
-            assertEquals("[E][ ] 1 (from: 2 to: )", test.toString());
+            assertEquals("[E][ ] 1 /from 2 (from:  to: )", test.toString());
         } catch (Exception e) {
             assertEquals("☹ OOPS!!! The title, from and to sections cannot be empty.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void createNewTask_invalidTask_returnsNull() {
+        try {
+            Task test = Parser.createNewTask("help");
+            assertNull(test);
+        } catch (Exception e) {
+            assertEquals("Unexpected exception.", e.getMessage());
         }
     }
 }
