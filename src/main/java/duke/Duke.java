4d80d90e3723ffffe13eb508d2c6caacb4389c1b
nicolengk <nicol3slaw@gmail.com>
2023-09-16 22:47:48 +0800

    Abstract Message class from Duke class This helps to make the run method from the Duke class shorter and more readerable as it currently contains more than 30 lines of code.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,9 +1,5 @@
 package duke;
 
-import java.util.List;
-
-import duke.Message;
-import duke.task.Task;
 import duke.utils.Parser;
 import duke.utils.Storage;
 import duke.utils.TaskList;
@@ -28,7 +24,6 @@ public class Duke {
             this.tasks = new TaskList(storage.loadTasksFromFile());
         } catch (DukeException e) {
             this.tasks = new TaskList();
-
         }
     }
 
@@ -36,48 +31,27 @@ public class Duke {
      * Runs the Duke chatbot.
      */
     public String run(String userInput) throws DukeException {
-        String msg = "";
         Parser parser = new Parser(userInput);
         String userCommand = parser.inputCommand();
 
         if (userCommand.equals("LIST")) {
-//            message = "Here are the tasks in your List:\n" + tasks.toString();
             message.listMessage(tasks);
         } else if (userCommand.equals("MARK")) {
-//            Task task = this.tasks.markTaskDone(parser.getTaskNumber());
-//            message = "Nice! I've marked this task as done:\n" + task;
             message.markMessage(tasks, parser.getTaskNumber());
         } else if (userCommand.equals("UNMARK")) {
-//            Task task = this.tasks.unmarkTask(parser.getTaskNumber());
-//            message = "OK, I've marked this task as not done yet:\n" + task;
             message.unmarkMessage(tasks, parser.getTaskNumber());
         } else if (userCommand.equals("TODO")) {
-//            Task task = this.tasks.addTodoTask(parser.getTodoDescription());
-//            message = "Got it. I've added this task:\n "
-//                    + task + "\nNow you have " + tasks.getSize() + " tasks in the list.";
             message.todoMessage(tasks, parser.getTodoDescription());
         } else if (userCommand.equals("DEADLINE")) {
             String[] deadlineDetails = parser.getDeadlineDescription();
-//            Task task = this.tasks.addDeadlineTask(descriptionAndDateTime[0],
-//                    parser.getDateTime(descriptionAndDateTime[1]));
-//            message = "Got it. I've added this task:\n "
-//                    + task + "\nNow you have " + tasks.getSize() + " tasks in the list.";
             message.deadlineMessage(tasks, deadlineDetails[0], parser.getDateTime(deadlineDetails[1]));
         } else if (userCommand.equals("EVENT")) {
             String[] eventDetails = parser.getEventDescription();
-//            Task task = this.tasks.addEventTask(deadlineDescription[0], parser.getDateTime(deadlineDescription[1]),
-//                parser.getDateTime(deadlineDescription[2]));
-//            message = "Got it. I've added this task:\n "
-//                    + task + "\nNow you have " + tasks.getSize() + " tasks in the list.";
             message.eventMessage(tasks, eventDetails[0], parser.getDateTime(eventDetails[1]),
                     parser.getDateTime(eventDetails[2]));
         } else if (userCommand.equals("DELETE")) {
-//            Task task = this.tasks.deleteTask(parser.getTaskNumber());
-//            message = "Now you have " + tasks.getSize() + " tasks in the list.";
             message.deleteMessage(tasks, parser.getTaskNumber());
         } else if (userCommand.equals("FIND")) {
-//            List<Task> matchingTasks = this.tasks.findTasksByKeyword(parser.getStringKeyword());
-//            message = "Here are the matching tasks in your list:\n" + matchingTasks;
             message.findMessage(tasks, parser.getStringKeyword());
         } else if (userCommand.equals("BYE")) {
             message.farewellMessage();
@@ -86,7 +60,7 @@ public class Duke {
         }
         this.storage.saveTasksToFile(this.tasks.getTasks());
 
-        return msg;
+        return message.getMessage();
     }
 
     /**
--- src/main/java/duke/Message.java
+++ src/main/java/duke/Message.java
@@ -1,58 +1,61 @@
 package duke;
 
 import duke.task.Task;
-import duke.task.Todo;
 import duke.utils.TaskList;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class Message {
+    private String message = "";
 
-
-    public String listMessage(TaskList tasks) {
-        return "Here are the tasks in your List:\n"+ tasks;
+    public void listMessage(TaskList tasks) {
+        message = "Here are the tasks in your List:\n"+ tasks;
     }
 
-    public String markMessage(TaskList tasks, int taskIndex) throws DukeException {
-        Task task = tasks.markTaskDone(taskIndex);
-        return "OK, I've marked this task as not done yet:\n" + tasks;
+    public void markMessage(TaskList tasks, int taskIndex) throws DukeException {
+        tasks.markTaskDone(taskIndex);
+        message = "OK, I've marked this task as not done yet:\n" + tasks;
     }
 
-    public String unmarkMessage(TaskList tasks, int taskIndex) throws DukeException {
-        Task task = tasks.unmarkTask(taskIndex);
-        return "OK, I've marked this task as not done yet:\n" + tasks;
+    public void unmarkMessage(TaskList tasks, int taskIndex) throws DukeException {
+        tasks.unmarkTask(taskIndex);
+        message = "OK, I've marked this task as not done yet:\n" + tasks;
     }
 
-    public String todoMessage(TaskList tasks, String description) throws DukeException {
+    public void todoMessage(TaskList tasks, String description) throws DukeException {
         Task todo = tasks.addTodoTask(description);
-        return "Got it. I've added this task:\n "
+        message = "Got it. I've added this task:\n "
                 + todo + "\nNow you have " + tasks.getSize() + " tasks in the list.";
     }
 
-    public String deadlineMessage(TaskList tasks, String description, LocalDateTime dateTime) throws DukeException {
+    public void deadlineMessage(TaskList tasks, String description, LocalDateTime dateTime) throws DukeException {
         Task deadline = tasks.addDeadlineTask(description, dateTime);
-        return "Got it. I've added this task:\n "
+        message = "Got it. I've added this task:\n "
                 + deadline + "\nNow you have " + tasks.getSize() + " tasks in the list.";
     }
 
-    public String eventMessage(TaskList tasks, String description, LocalDateTime from, LocalDateTime to) throws DukeException {
+    public void eventMessage(TaskList tasks, String description, LocalDateTime from, LocalDateTime to) throws DukeException {
         Task event = tasks.addEventTask(description, from, to);
-        return "Got it. I've added this task:\n "
+        message = "Got it. I've added this task:\n "
                 + event + "\nNow you have " + tasks.getSize() + " tasks in the list.";
     }
 
-    public String deleteMessage(TaskList tasks, int taskIndex) throws DukeException {
+    public void deleteMessage(TaskList tasks, int taskIndex) throws DukeException {
         Task task = tasks.deleteTask(taskIndex);
-        return "Now you have " + tasks.getSize() + " tasks in the list.";
+        message = "Now you have " + tasks.getSize() + " tasks in the list.";
     }
 
-    public String findMessage(TaskList tasks, String keyword) {
+    public void findMessage(TaskList tasks, String keyword) {
         List<Task> matchingTasks = tasks.findTasksByKeyword(keyword);
-        return "Here are the matching tasks in your list:\n" + matchingTasks;
+        message = "Here are the matching tasks in your list:\n" + matchingTasks;
     }
 
-    public String farewellMessage() {
-        return "Bye. Hope to see you again soon!";
+    public void farewellMessage() {
+        message = "Bye. Hope to see you again soon!";
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
