e911c9ab6d9de91e82e8cbb82d0ed775e15a84ef
Alicia Teng Hui Xin <teng.hxa@gmail.com>
2023-09-18 03:31:47 +0800

    Add Ui representative screenshot

--- src/main/java/sae/Main.java
+++ src/main/java/sae/Main.java
@@ -29,6 +29,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Sae");
             fxmlLoader.<MainWindow>getController().setSae(sae);
             stage.show();
         } catch (IOException e) {
