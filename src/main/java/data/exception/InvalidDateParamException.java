3a1e8ab6878febf3663fb03202a82a4b3407b0e8
McNaBry <mcnabry123@gmail.com>
2023-09-17 23:15:47 +0800

    remove multiline text block

--- src/main/java/data/exception/InvalidDateParamException.java
+++ src/main/java/data/exception/InvalidDateParamException.java
@@ -7,12 +7,11 @@ package data.exception;
  */
 public class InvalidDateParamException extends Exception {
     private static final String msg =
-            """
-            Oops, looks like your date is in an invalid format...
-            Here are some valid formats:
-            2023-10-20, 20-10-2023, 2023/10/20, 20/10/2023, Oct 10 2023, 10 Oct 2023
-            You can provide a timing as well: 2023-10-20 1800
-            """;
+            "Oops, looks like your date is in an invalid format...\n"
+            + "Here are some valid formats:\n"
+            + "2023-10-20, 20-10-2023, 2023/10/20, "
+            + "20/10/2023, Oct 10 2023, 10 Oct 2023\n"
+            + "You can provide a timing as well: 2023-10-20 1800";
 
     @Override
     public String toString() {
