9b6ee166d67f259bff225e4ee1e684a5f3e09f00
Mahidharah <mahidharah@gmail.com>
2023-09-18 00:07:37 +0800

    Style was all over the place
    Naming and javadocs did not follow perfectly follow style guidelines
    
    Incorporated checkstyle to aid with fixing style in code

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,12 +1,13 @@
 package duke;
 
+import java.util.Scanner;
+
 import duke.exceptions.DukeException;
 import duke.parsers.Parser;
-import duke.tasks.Task;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -15,8 +16,6 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.util.Scanner;
 
 /**
  * The chatbot Duke.
