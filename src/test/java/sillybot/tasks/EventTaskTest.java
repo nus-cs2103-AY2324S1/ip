e4b3eec34a6e38cca20be5babc547e2f49c21c0f
suryanshkushwaha <e0985892@u.nus.edu>
2023-09-19 00:55:23 +0800

    Minor fix

--- src/test/java/sillybot/tasks/EventTaskTest.java
+++ src/test/java/sillybot/tasks/EventTaskTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 
 public class EventTaskTest {
     @Test
-    void eventTask_encodesCorrectly_1() {
+    void eventTask_encodesCorrectly_one() {
         assertEquals("[E] [ ] project meeting (from: Jun 08 2033 to: Dec 08 2234)",
                 new EventTask("project meeting", "2033-06-08", "2234-12-08").toString());
     }
 
     @Test
-    void eventTask_encodesCorrectly_2() {
+    void eventTask_encodesCorrectly_two() {
         assertEquals("[E] [ ] attend Google Developers meeting (from: Dec 09 2008 to: Dec 10 2008)",
                 new EventTask("attend Google Developers meeting", "2008-12-09", "2008-12-10").toString());
     }
