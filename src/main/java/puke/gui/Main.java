c7af5b8b68409132086f6b3c0dceb6e6f73e647b
Andre Sim Jing Xiang <e0968908@u.nus.edu>
2023-09-18 23:39:47 +0800

    Add bot name in GUI window
    GUI window was previously empty.
    
    Added the name of the bot in the window and added a screenshot of
    the bot in preparation for creation of a User Guide.

--- src/main/java/puke/gui/Main.java
+++ src/main/java/puke/gui/Main.java
@@ -26,6 +26,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setPuke(puke);
+            stage.setTitle("PukeBot");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
