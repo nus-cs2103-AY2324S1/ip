b3cbbe2b2a4ad260737d1a610c7002520a1cf91e
Song_Bingxi <bingxi.bobby.song@gmail.com>
2023-09-18 09:05:49 +0800

    add JavaDoc to AddCommand

--- src/main/java/veneto/command/AddCommand.java
+++ src/main/java/veneto/command/AddCommand.java
@@ -1,39 +1,50 @@
 package veneto.command;
 
-import veneto.exceptions.DanException;
-import veneto.exceptions.DanOperateException;
+import veneto.exceptions.VenetoException;
+import veneto.exceptions.VenetoOperateException;
 import veneto.task.*;
 
 public class AddCommand extends Command {
-    /** Fields */
+    /* Fields */
     public static final String type = "add";
     private String text;
     private int id;
     private Task newTask;
 
-    /** Constructor */
+    /* Constructor */
+
     public AddCommand(Task t) {
         super();
         this.newTask = t;
     }
 
-    /** Methods */
+    /* Methods */
+    /**
+     * the AddCommand operates
+     * @param tasks the TaskList where the AddCommand put new task
+     */
     @Override
-    public void op(TaskList tasks) throws DanException {
+    public void op(TaskList tasks) throws VenetoException {
         try {
             tasks.add(newTask);
             tasks.storageChanged = 1;
         } catch (IndexOutOfBoundsException e) {
-            throw new DanOperateException("Add");
+            throw new VenetoOperateException("Add");
         }
     }
 
 
     @Override
+    /**
+     * @return the String representation of the task added to the TaskList
+     */
     public String toString() {
         return newTask.toString();
     }
 
+    /**
+     * @return the type of the Command
+     */
     public static String getType() {
         return type;
     }
