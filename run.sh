208140d911def4587692856a01c63457752c09a0
Evelyne Law <lawruixi@gmail.com>
2023-09-18 15:32:55 +0800

    add docs and update README for this project.

---
+++ run.sh
@@ -0,0 +1,5 @@
+cd src/main/java/duchess/
+javac Deadline.java Duchess.java DuchessException.java Event.java Parser.java Storage.java Task.java TaskList.java TaskStatus.java ToDo.java Ui.java Utility.java
+cd ..
+java duchess/Duchess
+cd ../../..
