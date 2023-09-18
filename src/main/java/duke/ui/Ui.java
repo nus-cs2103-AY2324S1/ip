d566f7f841e2ea5cacdcfad35071a44493d5c376
Khaleelur <khaleelrrahman2002@gmail.com>
2023-09-19 00:57:03 +0800

    Fix newline UI issues

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -4,7 +4,7 @@ package duke.ui;
  * Class to take care of UI
  */
 public class Ui {
-    private String greetMessage = "SUI, Hello! I'm Khaleelur!\n" + "What can I do for you?\n ";
+    private String greetMessage = "SUI, Hello! I'm Ronaldo!\n" + "What can I do for you, Messi?\n ";
     private String exitMessage = "SUI, Bye. Hope to see you again soon!\n";
     /**
      * Displays a greeting message.
