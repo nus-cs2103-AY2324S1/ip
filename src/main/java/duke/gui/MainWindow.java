717d201d0e284d38dba91e032bae81dc2f6ffb15
Yeo Boh Shin <110957811+YeoBohShin@users.noreply.github.com>
2023-09-18 16:59:25 +0800

    Update Ui.png

--- src/main/java/duke/gui/MainWindow.java
+++ src/main/java/duke/gui/MainWindow.java
@@ -30,6 +30,9 @@ public class MainWindow extends AnchorPane {
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        dialogContainer.getChildren().addAll(
+                DialogBox.getDukeDialog("Hello! I'm Aikent. How may I help you?", dukeImage)
+        );
     }
 
     public void setDuke(Duke d) {
