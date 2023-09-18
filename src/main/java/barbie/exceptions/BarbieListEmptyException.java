aaeab2e9284751fb8e7ddbcf2d32f38300e32634
Joey Lee <joeylee@comp.nus.edu>
2023-09-18 10:27:43 +0800

    Shorten longer methods

--- src/main/java/barbie/exceptions/BarbieListEmptyException.java
+++ src/main/java/barbie/exceptions/BarbieListEmptyException.java
@@ -1,6 +1,12 @@
 package barbie.exceptions;
 
+/**
+ * Represents the exception when there is an empty list accessed.
+ */
 public class BarbieListEmptyException extends BarbieException {
+    /**
+     * Constructs an instance of a BarbieListEmptyException.
+     */
     public BarbieListEmptyException() {
         super("Hmm.. your list looks empty. To add items, use the 'todo', "
                 + "'deadline' or 'party' commands!");
