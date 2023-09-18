d6ae3b7c68a4e13511b2bdd6e702d912e81e07d2
ong-wei-hong <weihong.ong@outlook.com>
2023-09-18 15:00:07 +0800

    checkStyle

--- src/test/java/duke/task/DeadlineTest.java
+++ src/test/java/duke/task/DeadlineTest.java
@@ -1,13 +1,13 @@
 package duke.task;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 public class DeadlineTest {
-	@Test
-	public void toStringTest() {
-		Task t = new Deadline("description", false, "2015-07-08 1914");
-		assertEquals(t.toString(), "  [D][ ] description (by: Jul 08 2015 07:14pm)");
-	}
+    @Test
+    public void toStringTest() {
+        Task t = new Deadline("description", false, "2015-07-08 1914");
+        assertEquals(t.toString(), "  [D][ ] description (by: Jul 08 2015 07:14pm)");
+    }
 }
--- src/test/java/duke/task/TodoTest.java
+++ src/test/java/duke/task/TodoTest.java
@@ -1,14 +1,13 @@
 package duke.task;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TodoTest {
-	@Test
-	public void toStringTest() {
-		Task t = new Todo("description", false);
-		assertEquals(t.toString(), "  [T][ ] description");
-	}
+import org.junit.jupiter.api.Test;
 
+public class TodoTest {
+    @Test
+    public void toStringTest() {
+        Task t = new Todo("description", false);
+        assertEquals(t.toString(), "  [T][ ] description");
+    }
 }
