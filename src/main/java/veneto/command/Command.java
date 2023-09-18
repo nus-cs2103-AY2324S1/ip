58e2c30ea6d4704edb80b8278d9791815d65d725
Song_Bingxi <bingxi.bobby.song@gmail.com>
2023-09-18 09:05:58 +0800

    add JavaDoc to Command

--- src/main/java/veneto/command/Command.java
+++ src/main/java/veneto/command/Command.java
@@ -3,7 +3,14 @@ package veneto.command;
 import veneto.task.TaskList;
 
 public abstract class Command {
-
+    /**
+     * the Command operates
+     * @param tasks the TaskList that the Command operates on
+     */
     public abstract void op(TaskList tasks);
+
+    /**
+     * @return the type of the Command
+     */
     public static String getType() {return null;}
 }
