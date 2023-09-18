2028dccc8b2256cb3f380a01aae951aba16ce1df
Song_Bingxi <bingxi.bobby.song@gmail.com>
2023-09-18 09:07:56 +0800

    add JavaDoc to TaskList

--- src/main/java/veneto/task/TaskList.java
+++ src/main/java/veneto/task/TaskList.java
@@ -3,16 +3,32 @@ package veneto.task;
 import java.util.ArrayList;
 
 public class TaskList extends ArrayList<Task> {
-    /** Fields */
+    /* Fields */
+    /**
+     * indicates if the storage is changed after a command operates
+     * will be modified when command operates.
+     */
     public int storageChanged = 0;
 
-    /** Constructors */
+    /* Constructors */
+    /**
+     * creates a new TaskList
+     */
     public TaskList() {}
+
+    /**
+     * creates a new TaskList
+     * @param initialCapacity the initial capacity of the list
+     */
     public TaskList(int initialCapacity) {
         super(initialCapacity);
     }
 
-    /** Methods */
+    /* Methods */
+    /**
+     * the string representation of the list
+     * @return all tasks in the list
+     */
     @Override
     public String toString() {
         StringBuffer rt = new StringBuffer();
@@ -21,5 +37,4 @@ public class TaskList extends ArrayList<Task> {
         }
         return rt.toString();
     }
-
 }
