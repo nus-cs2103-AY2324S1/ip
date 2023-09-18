0e728ce0d8063fe80c15b0fd3fef87f0ee0b530c
Song_Bingxi <bingxi.bobby.song@gmail.com>
2023-09-18 09:06:40 +0800

    add JavaDoc to ListCommand

--- src/main/java/veneto/command/ListCommand.java
+++ src/main/java/veneto/command/ListCommand.java
@@ -3,18 +3,25 @@ package veneto.command;
 import veneto.task.TaskList;
 
 public class ListCommand extends Command {
-    /** Fields */
+    /* Fields */
     public static final String type = "list";
 
-    /** Constructor */
+    /* Constructor */
     public ListCommand() {
         super();
     }
 
-    /** Methods */
+    /* Methods */
+    /**
+     * the ListCommand operates
+     * @param tasks the TaskList that the ListCommand shows
+     */
     @Override
     public void op(TaskList tasks) {}
 
+    /**
+     * @return the type of the Command
+     */
     public static String getType() {
         return type;
     }
