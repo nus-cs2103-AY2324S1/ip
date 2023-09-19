14efe525b12a9f4c4b2e7027da88f5e59e290b14
spatuly <a0259629l@gmail.com>
2023-09-19 22:42:51 +0800

    Update JUnit tests

--- src/test/java/uke/DeadlineTest.java
+++ src/test/java/uke/DeadlineTest.java
@@ -22,22 +22,22 @@ public class DeadlineTest {
     }
 
     @Test
-    public void testTodoStringConversion() {
+    public void testDeadlineStringConversion() {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
         LocalDateTime dueDate = LocalDateTime.parse("10/10/2023 10:10", dateTimeFormatter);
 
         Deadline test = new Deadline("Deadline Test", dueDate);
-        assertEquals("[D] [ ] Deadline Test (by: Oct 10 2023 10:10)", test.toString());
+        assertEquals("[D] [ ] Deadline Test (by Oct 10 2023 10:10)", test.toString());
 
         test.markAsDone();
-        assertEquals("[D] [X] Deadline Test (by: Oct 10 2023 10:10)", test.toString());
+        assertEquals("[D] [X] Deadline Test (by Oct 10 2023 10:10)", test.toString());
 
         test.markAsUndone();
-        assertEquals("[D] [ ] Deadline Test (by: Oct 10 2023 10:10)", test.toString());
+        assertEquals("[D] [ ] Deadline Test (by Oct 10 2023 10:10)", test.toString());
     }
 
     @Test
-    public void testTodoSavedStringConversion() {
+    public void testDeadlineSavedStringConversion() {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
         LocalDateTime dueDate = LocalDateTime.parse("11/11/2023 11:11", dateTimeFormatter);
 
