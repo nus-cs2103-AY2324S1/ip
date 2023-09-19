23ea300d35009432a56f7d147cf95db21aed1c45
dylkaw <37286651+dylkaw@users.noreply.github.com>
2023-09-19 15:28:18 +0800

    Add project name to title bar of window

--- src/main/java/glub/Main.java
+++ src/main/java/glub/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setGlub(glub);
+            stage.setTitle("glub");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
