71da17a201197dd0faab42edd5a30440cfdf2fd1
kurt <kurtyjlee@gmail.com>
2023-09-19 15:18:13 +0800

    change storageTest

--- src/test/java/cringebot/datafile/StorageTest.java
+++ src/test/java/cringebot/datafile/StorageTest.java
@@ -5,19 +5,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import cringebot.exceptions.CringeBotException;
+import cringebot.mocks.MockStorage;
 import cringebot.tasks.TaskList;
 
 public class StorageTest {
     @Test
     public void loadFileTest() throws CringeBotException {
-        Storage storage = new Storage("./src/test/resources/data/MockData.ser");
+        MockStorage storage = new MockStorage();
         storage.loadFromFile();
         assertEquals(0, storage.loadFromFile().size());
     }
 
     @Test
     public void storeFileTest() throws CringeBotException {
-        Storage storage = new Storage("./src/test/resources/data/MockData.ser");
+        MockStorage storage = new MockStorage();
         storage.writeToFile(new TaskList());
     }
 }
