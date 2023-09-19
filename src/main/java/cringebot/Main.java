bf71da42d6e36cacc6a0cd5ef405941b833718c0
kurt <kurtyjlee@gmail.com>
2023-09-19 18:22:58 +0800

    add better gui

--- src/main/java/cringebot/Main.java
+++ src/main/java/cringebot/Main.java
@@ -27,6 +27,7 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            scene.getStylesheets().add("./src/main/resources/view/mainWindow.css");
 
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setCringeBot(cringeBot);
