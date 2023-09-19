6cb1baab49fbedf8d10bc4020acfffa60076eb35
pra-navi <pranavikiran@gmail.com>
2023-09-19 19:34:21 +0800

    Update JUnit

--- src/test/java/sally/DeadlineTest.java
+++ src/test/java/sally/DeadlineTest.java
@@ -12,7 +12,7 @@ public class DeadlineTest {
     public void validDeadlineString() {
         LocalDateTime dateTime = LocalDateTime.of(2023, 9, 30, 14, 0);
         Deadline deadline = new Deadline("Submit report", dateTime);
-        String expectedOutput = "[D][ ] Submit report (by: Sep 30 2023, 2:00 PM)";
+        String expectedOutput = "[D][ ] (TBD) Submit report (by: Sep 30 2023, 2:00 PM)";
         assertEquals(expectedOutput, deadline.toString());
     }
 
@@ -21,7 +21,7 @@ public class DeadlineTest {
         LocalDateTime dateTime = LocalDateTime.of(2023, 9, 30, 14, 0);
         Deadline deadline = new Deadline("Submit report", dateTime);
         deadline.mark();
-        String expectedOutput = "[D][X] Submit report (by: Sep 30 2023, 2:00 PM)";
+        String expectedOutput = "[D][X] (TBD) Submit report (by: Sep 30 2023, 2:00 PM)";
         assertEquals(expectedOutput, deadline.toString());
     }
 
