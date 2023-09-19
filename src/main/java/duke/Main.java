30f7ee034df1df9b3f22115e7a816a836bd975c4
Li <e0985877@u.nus.edu>
2023-09-19 18:10:27 +0800

    Implement: add personality

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -13,7 +13,7 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
-    private Duke duke = new Duke("Duke", "data/duke.txt");
+    private Duke duke = new Duke("Maya", "data/duke.txt");
 
     @Override
     public void start(Stage stage) {
