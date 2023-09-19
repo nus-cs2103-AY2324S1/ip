510268385de04ae112ed6222df9d9178f3b02ac2
elhy1999 <ernestliu64@gmail.com>
2023-09-19 17:37:24 +0800

    Main.java: Add stage name 'TaskMate Gui'

--- src/main/java/taskmate/main/Main.java
+++ src/main/java/taskmate/main/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("TaskMate GUI");
             fxmlLoader.<MainWindow>getController().setTaskMate(taskMate);
             stage.show();
         } catch (IOException e) {
