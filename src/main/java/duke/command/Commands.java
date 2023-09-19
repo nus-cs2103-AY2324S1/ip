7e74ba198fee8fdbebdcba0bbe688e71ee884a61
tanshiyu <tanshiyu1999@gmail.com>
2023-09-19 22:57:07 +0800

    Added a new feature to direct people to a README
    Added a new command "help" to direct users to my github page, where the README.md file is located
    
    The README file will contain all of the instructions and guidance required for users to know what command they can use when utilizing Bob.
    
    The "help" command is chosen as it is something intuitive for users to type in when they are unfamiliar with the command required.

--- src/main/java/duke/command/Commands.java
+++ src/main/java/duke/command/Commands.java
@@ -4,7 +4,7 @@ package duke.command;
  * An enum of commands.
  */
 public enum Commands {
-    LIST, MARK, UNMARK, DEADLINE, TODO, EVENT, DELETE, FIND
+    LIST, MARK, UNMARK, DEADLINE, TODO, EVENT, DELETE, FIND, HELP
 }
 
 
