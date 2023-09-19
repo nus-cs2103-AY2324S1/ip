943dbad129055d6fa4b9cadf279be3d84beeff9a
Mohammed-Faizzzz <110959467+Mohammed-Faizzzz@users.noreply.github.com>
2023-09-19 16:51:43 +0800

    Make GUI Modifications

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -1,12 +1,15 @@
 package duke;
 
+import java.util.Objects;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+
+
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
  */
@@ -17,14 +20,13 @@ public class MainWindow extends AnchorPane {
     private VBox dialogContainer;
     @FXML
     private TextField userInput;
-    @FXML
-    private Button sendButton;
-
     private Duke duke;
-
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
-
+    private final Image userImage = new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/images/DaUser.png")));
+    private final Image dukeImage = new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/images/DaDuke.png")));
+    /**
+     * Initializes the GUI components.
+     * This method is typically called when the FXML controller is initialized.
+     */
     @FXML
     public void initialize() {
         Ui ui = new Ui();
