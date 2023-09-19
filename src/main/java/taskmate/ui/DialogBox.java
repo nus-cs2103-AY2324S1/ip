f4cafde97ec28d1ba7a4ed524cf510bcbc61a839
elhy1999 <ernestliu64@gmail.com>
2023-09-19 13:23:03 +0800

    Add margins to sides of images

--- src/main/java/taskmate/ui/DialogBox.java
+++ src/main/java/taskmate/ui/DialogBox.java
@@ -7,6 +7,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
@@ -39,6 +40,7 @@ public class DialogBox extends HBox {
         dialog.setText(text);
         dialog.setFont(Font.font("Courier New", 11));
         displayPicture.setImage(img);
+        HBox.setMargin(displayPicture, new Insets(0, 5, 0, 5));
     }
 
     /**
