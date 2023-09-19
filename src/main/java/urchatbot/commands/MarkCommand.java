545bd25fbce37285138088b286d7d5bfdf314556
jean-cq <mary84060117@gmail.com>
2023-09-20 03:17:04 +0800

    Update CodeQuality

--- src/main/java/urchatbot/commands/MarkCommand.java
+++ src/main/java/urchatbot/commands/MarkCommand.java
@@ -23,7 +23,8 @@ public class MarkCommand extends Command {
     }
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) throws URChatBotException {
-        if (tasks.getSize() < 1 || tasks.getSize() <= taskNumber) {
+        boolean isInvalidSize = tasks.getSize() < 1 || tasks.getSize() <= taskNumber;
+        if (isInvalidSize) {
             throw new URChatBotException("OOPS!!! No task to mark!");
         }
         Task markedTask = tasks.getTasks().get(taskNumber);
