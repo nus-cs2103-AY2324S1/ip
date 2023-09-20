b0c18be831c2244c82a390fc0441a58324828ea7
glenngnng <glennng12@gmail.com>
2023-09-20 18:09:43 +0800

    Name Gman GUI, change Ui screenshot

--- src/main/java/gman/Main.java
+++ src/main/java/gman/Main.java
@@ -25,6 +25,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setGman(gman);
+            stage.setTitle("Gman");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
