b78875783a7b8c39acd72b0f8e3eeb383de773e9
rionshocker <jaryl591@gmail.com>
2023-09-20 19:16:06 +0800

    Add additional test

--- src/test/java/duke/task/DeadlineTest.java
+++ src/test/java/duke/task/DeadlineTest.java
@@ -2,8 +2,6 @@ package duke.task;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeadlineTest {
@@ -16,5 +14,14 @@ public class DeadlineTest {
         testDeadline.changeStatus();
         assertEquals("[D][X] deadline (by: Oct 15 2019 18:00)", 
         testDeadline.toString(), "changeStatus() method works");
+    }
+
+    @Test
+    public void updateDeadlineTest() {
+        Deadline testDeadline = new Deadline("deadline", "2019-10-15 1800");
+        testDeadline.update("dEadline");
+        testDeadline.updateDate(testDeadline.convertToDateTime("2019-10-15 1700"), false);
+        assertEquals("[D][ ] dEadline (by: Oct 15 2019 17:00)", 
+        testDeadline.toString(), "updating method works");
     }
 }
