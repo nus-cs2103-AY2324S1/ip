cc12302d8cc140c8cfcdf52c42cb164c39c62599
jingyu987 <jingyu2987@gmail.com>
2023-09-21 00:57:44 +0800

    Improve code quality and fix storage path

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -1,4 +1,5 @@
 package storage;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -17,6 +18,7 @@ import tasks.TaskList;
 public class Storage {
     private File file;
 
+    private String filePath;
     /**
      * Constructs a Storage object with the specified file path.
      *
@@ -24,7 +26,13 @@ public class Storage {
      */
     public Storage(String filePath) {
         assert filePath != null;
+        this.filePath = filePath;
         this.file = new File(filePath);
+
+        File parentDirectory = file.getParentFile();
+        if (!parentDirectory.exists()) {
+            parentDirectory.mkdirs();
+        }
     }
 
     /**
@@ -43,28 +51,12 @@ public class Storage {
                     String taskType = parts[0];
                     boolean isDone = parts[1].equals("1");
                     String description = parts[2];
-
                     if (taskType.equals("T")) {
-                        Task task = Parser.parseTodo(description.trim());
-                        if (isDone) {
-                            task.markAsDone();
-                        }
-                        taskList.add(task);
+                        handleTodo(description, taskList, isDone);
                     } else if (taskType.equals("D")) {
-                        String by = parts[3];
-                        Task task = Parser.parseDeadline(description.trim(), by.trim());
-                        if (isDone) {
-                            task.markAsDone();
-                        }
-                        taskList.add(task);
+                        handleDeadline(description, taskList, isDone, parts);
                     } else if (taskType.equals("E")) {
-                        String from = parts[3];
-                        String to = parts[4];
-                        Task task = Parser.parseEvent(description.trim(), from.trim(), to.trim());
-                        if (isDone) {
-                            task.markAsDone();
-                        }
-                        taskList.add(task);
+                        handleEvent(description, taskList, isDone, parts);
                     }
                 }
             }
@@ -80,13 +72,62 @@ public class Storage {
     }
 
     /**
+     * Handles the todo task.
+     * @param description Description of the todo task.
+     * @param taskList Task list to be added to.
+     * @param isDone Boolean indicating whether the task is done.
+     */
+    private void handleTodo(String description, ArrayList<Task> taskList, boolean isDone) {
+        Task task = Parser.parseTodo(description.trim());
+        if (isDone) {
+            task.markAsDone();
+        }
+        taskList.add(task);
+    }
+
+    /**
+     * Handles the deadline task.
+     * @param description Description of the deadline task.
+     * @param taskList Task list to be added to.
+     * @param isDone Boolean indicating whether the task is done.
+     * @param parts An array containing parts of the input line, split by the '|' character.
+     */
+    private void handleDeadline(String description, ArrayList<Task> taskList, boolean isDone, String[] parts)
+            throws FishronException {
+        String by = parts[3];
+        Task task = Parser.parseDeadline(description.trim(), by.trim());
+        if (isDone) {
+            task.markAsDone();
+        }
+        taskList.add(task);
+    }
+
+    /**
+     * Handles the Event task.
+     * @param description Description of the Event task.
+     * @param taskList Task list to be added to.
+     * @param isDone Boolean indicating whether the task is done.
+     * @param parts An array containing parts of the input line, split by the '|' character.
+     */
+    private void handleEvent(String description, ArrayList<Task> taskList, boolean isDone, String[] parts)
+            throws FishronException {
+        String from = parts[3];
+        String to = parts[4];
+        Task task = Parser.parseEvent(description.trim(), from.trim(), to.trim());
+        if (isDone) {
+            task.markAsDone();
+        }
+        taskList.add(task);
+    }
+
+    /**
      * Saves the provided task list to the file.
      *
      * @param taskList The list of tasks to be saved.
      */
     public void saveTasksToFile(ArrayList<Task> taskList) {
         try {
-            PrintWriter printWriter = new PrintWriter("./data/fishron.txt");
+            PrintWriter printWriter = new PrintWriter(filePath);
             for (Task task : taskList) {
                 printWriter.println(task.toFileString());
             }
