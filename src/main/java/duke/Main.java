5ad94ee9b2839fce864ab5c532c69870002424b8
Jeffrey <jeffreyjianyujie@gmail.com>
2023-09-20 14:37:15 +0800

    set title as JEDuke

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.setTitle("JEDuke");
             stage.show();
 
         } catch (IOException e) {
