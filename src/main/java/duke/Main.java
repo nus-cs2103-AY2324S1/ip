5211dc8b4f83cb668a85daa2decf0b48b73fa678
alientian <e1062165@u.nus.edu>
2023-09-21 02:00:19 +0800

    Edit bugs to release jar

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -12,7 +12,7 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
-    private Duke duke = new Duke("./src/main/data/tasks.txt");
+    private Duke duke = new Duke("./dukeTasks.txt");
 
     @Override
     public void start(Stage stage) {
