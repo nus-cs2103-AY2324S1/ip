c325ca1faecafa244355812222a97f90041ba31e
tiong jjyi <tiongjjyi@gmail.com>
2023-09-21 02:19:32 +0800

    Code clean up

--- src/main/java/duke/taskmanagement/Storage.java
+++ src/main/java/duke/taskmanagement/Storage.java
@@ -1,7 +1,11 @@
 package duke.taskmanagement;
 
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
