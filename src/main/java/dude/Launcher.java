ef85dc135a5eab06496458723ce6f38cf09db495
Xenos F <git@xenosfio.com>
2023-09-21 00:57:21 +0800

    Add @@author tag for reused launcher code

--- src/main/java/dude/Launcher.java
+++ src/main/java/dude/Launcher.java
@@ -2,6 +2,10 @@ package dude;
 
 import javafx.application.Application;
 
+//@@author xenosf-reused
+// Reused from https://se-education.org/guides/tutorials/javaFxPart4.html
+// With minor alterations
+
 /**
  * A launcher class to workaround classpath issues.
  */
@@ -10,3 +14,4 @@ public class Launcher {
         Application.launch(Main.class, args);
     }
 }
+//@@author
