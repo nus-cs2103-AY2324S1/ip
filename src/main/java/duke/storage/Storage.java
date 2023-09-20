cf638ded79e42e24ef3e7304540ffa4d9c693041
freshcabbage123 <alan@Alans-MacBook-Air.local>
2023-09-20 12:23:03 +0800

    refactor: Refactor Storage based on soc-se-bot

--- src/main/java/duke/storage/Storage.java
+++ src/main/java/duke/storage/Storage.java
@@ -19,6 +19,7 @@ import duke.task.ToDo;
  */
 public class Storage {
     private final Path filePath;
+    private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 
     /**
      * Constructor for Storage.
@@ -34,72 +35,13 @@ public class Storage {
      * @throws IOException If an I/O error occurs.
      */
     public ArrayList<Task> load() throws IOException {
-        Path directoryPath = filePath.getParent();
-        if (!Files.exists(directoryPath)) {
-            Files.createDirectories(directoryPath);
-        }
-        // Check if file exists, if not create it
-        if (!Files.exists(filePath)) {
-            Files.createFile(filePath);
-        }
-
-        // Read file and load data
-        List<String> data;
-        try {
-            data = Files.readAllLines(filePath);
-        } catch (IOException e) {
-            System.out.println("An error occurred while reading the file: " + e.getMessage());
-            return new ArrayList<>();
-        }
+        ensureDirectoryAndFileExist();
+        List<String> data = readDataFromFile();
         ArrayList<Task> tasks = new ArrayList<>();
-
         for (String line : data) {
-            String[] parts = line.split("\\]", 3);
-            if (parts.length < 3) {
-                System.out.println("Invalid duke.task format found in Hard Disk");
-                continue;
-            }
-            char type = parts[0].charAt(parts[0].length() - 1);
-            char doneStatus = parts[1].charAt(1);
-            boolean isDone = doneStatus == 'X';
-            String description = parts[2].trim();
-            Task task;
-            DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
-            switch (type) {
-            case 'T':
-                task = new ToDo(description);
-                if (isDone) {
-                    task.markAsDone();
-                }
+            Task task = parseTaskFromLine(line);
+            if (task != null) {
                 tasks.add(task);
-                break;
-            case 'D':
-                String[] components = description.split(" \\(by: ");
-                String deadlineDescription = components[0].trim();
-                String deadlineDateTime = components[1].substring(0, components[1].length() - 1);
-                task = new Deadline(deadlineDescription, LocalDateTime.parse(deadlineDateTime, format));
-                if (isDone) {
-                    task.markAsDone();
-                }
-                tasks.add(task);
-                break;
-            case 'E':
-                String[] eventParts = description.split(" \\(from: ");
-                String eventDescription = eventParts[0].trim();
-                String[] eventDateTimes = eventParts[1].split(" to: ");
-                String eventStartDateTime = eventDateTimes[0];
-                String eventEndDateTime = eventDateTimes[1].substring(0, eventDateTimes[1].length() - 1);
-                task = new Event(eventDescription,
-                        LocalDateTime.parse(eventStartDateTime, format),
-                        LocalDateTime.parse(eventEndDateTime, format));
-                if (isDone) {
-                    task.markAsDone();
-                }
-                tasks.add(task);
-                break;
-            default:
-                System.out.println("Invalid duke.task type found in Hard Disk");
-                break;
             }
         }
         return tasks;
@@ -111,20 +53,121 @@ public class Storage {
      * @throws IOException If an I/O error occurs.
      */
     public void save(List<Task> tasks) throws IOException {
-        Path directoryPath = filePath.getParent();
-        if (!Files.exists(directoryPath)) {
-            Files.createDirectories(directoryPath);
-        }
-        // Check if file exists, if not create it
-        if (!Files.exists(filePath)) {
-            Files.createFile(filePath);
-        }
+        ensureDirectoryAndFileExist();
         // Write data to file
         List<String> data = new ArrayList<>();
         for (Task task : tasks) {
             data.add(task.toString());
         }
         Files.write(filePath, data);
+    }
+
+    /**
+     * Ensures that the directory and file exist.
+     * If not, create them.
+     * @throws IOException If an I/O error occurs.
+     */
+    private void ensureDirectoryAndFileExist() throws IOException {
+        Path directoryPath = filePath.getParent();
+        if (!Files.exists(directoryPath)) {
+            Files.createDirectories(directoryPath);
+        }
+        if (!Files.exists(filePath)) {
+            Files.createFile(filePath);
+        }
+    }
+
+    /**
+     * Parses the task from the line.
+     * @param line The line to parse.
+     * @return The task parsed from the line.
+     */
+    private Task parseTaskFromLine(String line) {
+        String[] parts = line.split("\\]", 3);
+        if (parts.length < 3) {
+            System.out.println("Invalid duke.task format found in Hard Disk");
+            return null;
+        }
+        char type = parts[0].charAt(parts[0].length() - 1);
+        boolean isDone = parts[1].charAt(1) == 'X';
+        String description = parts[2].trim();
+
+        switch (type) {
+            case 'T':
+                return createToDoTask(isDone, description);
+            case 'D':
+                return createDeadlineTask(isDone, description);
+            case 'E':
+                return createEventTask(isDone, description);
+            default:
+                System.out.println("Invalid duke.task type found in Hard Disk");
+                return null;
+        }
+    }
+
+    /**
+     * Creates a todo task.
+     * @param isDone Whether the task is done.
+     * @param description The description of the task.
+     * @return The todo task.
+     */
+    private Task createToDoTask(boolean isDone, String description) {
+        Task task = new ToDo(description);
+        if (isDone) {
+            task.markAsDone();
+        }
+        return task;
+    }
+
+    /**
+     * Creates a deadline task.
+     * @param isDone Whether the task is done.
+     * @param description The description of the task.
+     * @return The deadline task.
+     */
+    private Task createDeadlineTask(boolean isDone, String description) {
+        String[] components = description.split(" \\(by: ");
+        String deadlineDescription = components[0].trim();
+        String deadlineDateTime = components[1].substring(0, components[1].length() - 1);
+        Task task = new Deadline(deadlineDescription, LocalDateTime.parse(deadlineDateTime, FORMAT));
+        if (isDone) {
+            task.markAsDone();
+        }
+        return task;
+    }
+
+    /**
+     * Creates an event task.
+     * @param isDone Whether the task is done.
+     * @param description The description of the task.
+     * @return The event task.
+     */
+    private Task createEventTask(boolean isDone, String description) {
+        String[] eventParts = description.split(" \\(from: ");
+        String eventDescription = eventParts[0].trim();
+        String[] eventDateTimes = eventParts[1].split(" to: ");
+        String eventStartDateTime = eventDateTimes[0];
+        String eventEndDateTime = eventDateTimes[1].substring(0, eventDateTimes[1].length() - 1);
+        Task task = new Event(eventDescription,
+                LocalDateTime.parse(eventStartDateTime, FORMAT),
+                LocalDateTime.parse(eventEndDateTime, FORMAT));
+        if (isDone) {
+            task.markAsDone();
+        }
+        return task;
+    }
+
+    /**
+     * Reads the data from the file.
+     * @return The data read from the file.
+     */
+    private List<String> readDataFromFile() {
+        try {
+            return Files.readAllLines(filePath);
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file: " + e.getMessage());
+            return new ArrayList<>();
+        }
     }
 
     /**
