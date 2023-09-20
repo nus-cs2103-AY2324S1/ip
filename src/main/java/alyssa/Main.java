249f5612785ae7490a1508980939b9d252b37fb3
Elijah5399 <chia.elijah@gmail.com>
2023-09-20 22:25:12 +0800

    Add title to GUI window

--- src/main/java/alyssa/Main.java
+++ src/main/java/alyssa/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Alyssa");
             fxmlLoader.<MainWindow>getController().setAlyssa(alyssa);
             stage.show();
         } catch (IOException e) {
