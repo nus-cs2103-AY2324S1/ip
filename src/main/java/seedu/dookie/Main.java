9751694b2adc6be982ca4d58525fc5bb1763ff25
jordankanghm <jordankanghm@gmail.com>
2023-09-20 11:18:33 +0800

    Add title to GUI window

--- src/main/java/seedu/dookie/Main.java
+++ src/main/java/seedu/dookie/Main.java
@@ -24,6 +24,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Dookie");
             fxmlLoader.<MainWindow>getController().setDookie(dookie);
             stage.show();
 
