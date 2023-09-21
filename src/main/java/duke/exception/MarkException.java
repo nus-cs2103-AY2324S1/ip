9007e1728247fe78c902ad46b820943e2df0ccda
YangHeng Tang <yanghengtang@gmail.com>
2023-09-21 19:32:27 +0800

    Add MarkException.java

---
+++ src/main/java/duke/exception/MarkException.java
@@ -0,0 +1,10 @@
+package duke.exception;
+
+/**
+ * Represents an error that occurred during the initialisation of a mark command.
+ */
+public class MarkException extends CommandException {
+    public MarkException() {
+        super("mark TASK_INDEX");
+    }
+}
