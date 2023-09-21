29a38cc028eee2408406464c2dc5276c357e06d8
supermii2 <alvis.ng15@gmail.com>
2023-09-21 14:21:39 +0800

    Add more test cases for tasks

--- src/test/java/rock/client/ResponseTest.java
+++ src/test/java/rock/client/ResponseTest.java
@@ -1,2 +1,30 @@
-package rock.client.exceptions;public class ResponseTest {
+package rock.client;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ResponseTest {
+    @Test
+    public void testValidResponseMsg() {
+        final String TEST_STR = "TEST";
+        final Response TEST_RESPONSE = Response.createValidResponse(TEST_STR);
+        assertEquals(TEST_STR, TEST_RESPONSE.getMessage());
+    }
+    @Test
+    public void testValidResponseIsError() {
+        final Response TEST_RESPONSE = Response.createValidResponse("Irrelevant");
+        assertEquals(false, TEST_RESPONSE.isError());
+    }
+    @Test
+    public void testErrorResponseMsg() {
+        final String TEST_STR = "TEST";
+        final Response TEST_RESPONSE = Response.createErrorResponse(TEST_STR);
+        assertEquals(TEST_STR, TEST_RESPONSE.getMessage());
+    }
+    @Test
+    public void testErrorResponseIsError() {
+        final Response TEST_RESPONSE = Response.createErrorResponse("Irrelevant");
+        assertEquals(true, TEST_RESPONSE.isError());
+    }
 }
+
--- src/test/java/rock/tasks/TaskTest.java
+++ src/test/java/rock/tasks/TaskTest.java
@@ -2,10 +2,36 @@ package rock.tasks;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TaskTest {
-    static final TASK_DUMMY = new Task("TEST");
+    static String TASK_NAME = "TEST";
+    class TaskStub extends Task {
+        public TaskStub() {
+            super(TASK_NAME);
+        }
+        public LocalDate getDate() {
+            return LocalDate.now();
+        }
+    }
     @Test
+    public void getCompletedStatusDefault() {
+        final Task TASK_DUMMY = new TaskStub();
+        assertEquals(false, TASK_DUMMY.isCompleted());
+    }
 
+    @Test
+    public void setCompletionStatus() {
+        final Task TASK_DUMMY = new TaskStub();
+        TASK_DUMMY.setCompleted(true);
+        assertEquals(true, TASK_DUMMY.isCompleted());
+    }
+
+    @Test
+    public void getName() {
+        final Task TASK_DUMMY = new TaskStub();
+        assertEquals(TASK_NAME, TASK_DUMMY.getName());
+    }
 }
