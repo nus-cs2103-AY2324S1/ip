8ec3f1e8275628895430d9f887e3ba1ff5e36131
dlathyun <shyim0128@gmail.com>
2023-09-21 17:36:26 +0800

    Add test cases for checking whether string with white spaces will get parsed correctly in DateTimeParser.

--- src/test/java/duke/parser/DateTimeParserTest.java
+++ src/test/java/duke/parser/DateTimeParserTest.java
@@ -47,4 +47,19 @@ public class DateTimeParserTest {
         Assertions.assertTrue(actual.contains(expected));
     }
 
+    @Test
+    public void stringWithWhiteSpaces_forOutFormatted_shouldHaveCorrectFormat() throws KoraException {
+        DateTimeParser dtParser = new DateTimeParser();
+        String expected = "Sat, Aug 5 2023 12:11";
+        String actual = dtParser.outFormatted("          2023-08-05         12:11    ");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void stringWithWhiteSpaces_forSavedFormatted_shouldHaveCorrectFormat() throws KoraException {
+        DateTimeParser dtParser = new DateTimeParser();
+        String expected = "2023-08-05 12:11";
+        String actual = dtParser.saveFormatted("  2023-08-05       12:11  ");
+        Assertions.assertEquals(expected, actual);
+    }
 }
