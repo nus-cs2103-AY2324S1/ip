b208d2e8b00fc5bd7148959c4a38c83b703ea17e
VN-Hao <111359733+VN-Hao@users.noreply.github.com>
2023-09-21 22:31:28 +0800

    Improve overall GUI
    The avatar of the user and the texts do not change accordingly when
    the app's window resizes.
    
    Set the height and the width of the dialogBox to refer to the
    scrollPane's dimensions.
    
    This will make the GUI look more satisfying and flexible.

--- src/main/java/qi/MainWindow.java
+++ src/main/java/qi/MainWindow.java
@@ -7,6 +7,11 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.VBox;
 
 /**
@@ -29,11 +34,25 @@ public class MainWindow extends AnchorPane {
 
     @FXML
     public void initialize() {
+        dialogContainer.prefWidthProperty().bind(scrollPane.widthProperty().multiply(0.96));
+        dialogContainer.prefHeightProperty().bind(scrollPane.heightProperty());
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
     }
 
     public void setQi(Qi qi) {
         dialogContainer.getChildren().add(DialogBox.getQiDialog(qi.showWelcome(), qiImage));
+        BackgroundSize backgroundSize = new BackgroundSize(900,
+                700,
+                true,
+                true,
+                true,
+                false);
+        dialogContainer.setBackground(new Background(new BackgroundImage(
+                new Image(this.getClass().getResourceAsStream("/images/BackGround.png")),
+                BackgroundRepeat.REPEAT,
+                BackgroundRepeat.REPEAT,
+                BackgroundPosition.CENTER,
+                backgroundSize)));
         this.qi = qi;
     }
 
