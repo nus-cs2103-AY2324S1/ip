395e7b0b8592e40ca857b5ce6b9d0f56f26b3140
YangHeng Tang <yanghengtang@gmail.com>
2023-09-21 19:36:26 +0800

    Update ToDoException.java

--- src/main/java/duke/exception/ToDoException.java
+++ src/main/java/duke/exception/ToDoException.java
@@ -1,9 +1,9 @@
-package duke.task;
+package duke.exception;
 
 /**
  * Represents an error that occurred during the initialisation of a Deadline object.
  */
-public class ToDoException extends TaskException {
+public class ToDoException extends CommandException {
 
     public ToDoException() {
         super("todo (description)");
