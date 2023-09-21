fefa6967c98544be7ee334e73791575b7273052e
oeggy03 <oeggy03>
2023-09-21 22:10:05 +0800

    Update help command to include numbering
    Numbering of commands helps the user to read the commands more easily.

--- src/main/java/LogicHandlers/CommandHandlers/HelpCommandHandler.java
+++ src/main/java/LogicHandlers/CommandHandlers/HelpCommandHandler.java
@@ -9,17 +9,17 @@ public class HelpCommandHandler implements Command{
     private TaskArray tasks;
     private String help = " Hello! I'm EGGBOT!\n\n" +
             " >>> Adding of tasks:\n" +
-            " > ToDo task: 'todo [Task]'\n" +
-            " > Deadline task: 'deadline [Task /Deadline (dd-mm-yyyy hhmm)]'\n" +
-            " > Event task: 'event [Task /Start Date (dd-mm-yyyy hhmm)/End Date (dd-mm-yyyy hhmm)]'\n\n" +
+            " 1. ToDo task: 'todo [Task]'\n" +
+            " 2. Deadline task: 'deadline [Task /Deadline (dd-mm-yyyy hhmm)]'\n" +
+            " 3. Event task: 'event [Task /Start Date (dd-mm-yyyy hhmm)/End Date (dd-mm-yyyy hhmm)]'\n\n" +
             " >>> Other commands:\n" +
-            " > To view tasks, type 'list' \n" +
-            " > To mark a task as 'done', type 'mark [index]' \n" +
-            " > To mark a task as 'undone', type 'unmark [index]' \n" +
-            " > To delete a task, type 'delete [index]' \n" +
-            " > To find a task, type 'find <a word in the task name>' \n" +
-            " > To view a list of commands, type 'help' \n" +
-            " > To exit, type 'bye' or 'exit'";
+            " 4. To view tasks, type 'list' \n" +
+            " 5. To mark a task as 'done', type 'mark [index]' \n" +
+            " 6. To mark a task as 'undone', type 'unmark [index]' \n" +
+            " 7. To delete a task, type 'delete [index]' \n" +
+            " 8. To find a task, type 'find <a word in the task name>' \n" +
+            " 9. To view a list of commands, type 'help' \n" +
+            " 10. To exit, type 'bye' or 'exit'";
 
     /**
      * Constructor for HelpCommandHandler.
