6dc36eb367c3b13b11bf22df531c2a96fde5ddf7
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-09-22 07:37:44 +0800

    devfeat(test): Support UTC tests

--- src/test/java/com/cloud/chatbot/DateConverterTest.java
+++ src/test/java/com/cloud/chatbot/DateConverterTest.java
@@ -1,33 +1,45 @@
 package com.cloud.chatbot;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.Instant;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
 
 
-// Assuming the "user" is on a GMT+8 system
+// Assuming the "user" is on a GMT+8 or GMT/UTC system
 public class DateConverterTest {
     @Test
     public void timestampToInstant() {
         Instant instantExpected = Instant.parse("2023-01-31T20:56:00Z");
         Instant instantActual = DateConverter.timestampToInstant("1 2 23 456");
-        assertEquals(instantExpected, instantActual);
+        Instant instantActual2 = DateConverter.timestampToInstant("1 1 31 2056");
+        assertTrue(
+            Arrays.asList(instantActual, instantActual2)
+                    .contains(instantExpected)
+        );
     }
 
     @Test
     public void instantToPrettyTimestamp_nonZeroMinutes() {
         Instant instant = Instant.parse("2023-01-31T20:56:00Z");
         String timestamp = DateConverter.instantToPrettyTimestamp(instant);
-        assertEquals("1 Feb '23, 4:56am", timestamp);
+        assertTrue(
+            Arrays.asList("1 Feb '23, 4:56am", "31 Jan '23, 8:56pm")
+                    .contains(timestamp)
+        );
     }
 
     @Test
     public void instantToPrettyTimestamp_zeroMinutes() {
         Instant instant = Instant.parse("2023-09-14T13:00:00Z");
         String timestamp = DateConverter.instantToPrettyTimestamp(instant);
-        assertEquals("14 Sep '23, 9pm", timestamp);
+        assertTrue(
+            Arrays.asList("14 Sep '23, 9pm", "14 Sep '23, 1pm")
+                    .contains(timestamp)
+        );
     }
 }
