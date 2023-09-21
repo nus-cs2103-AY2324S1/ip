d0f04efbc589cd16cfcfbae459aa7554d1829176
Nixx162 <nicholas.jimmyalden@u.nus.edu>
2023-09-22 01:32:54 +0800

    style: Tweak the UI a little bit for better readability

--- src/main/java/pardiyem/ui/Main.java
+++ src/main/java/pardiyem/ui/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Pardiyem");
             fxmlLoader.<MainWindow>getController().setPardiyem(pardiyem);
             stage.show();
         } catch (IOException e) {
