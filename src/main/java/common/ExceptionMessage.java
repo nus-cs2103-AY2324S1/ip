bca712d66f2f7f60aef0a61925c8d462f7e4b1f2
zekone <hkw19991@gmail.com>
2023-09-21 17:56:01 +0800

    Edit messages

--- src/main/java/common/ExceptionMessage.java
+++ src/main/java/common/ExceptionMessage.java
@@ -9,6 +9,6 @@ public abstract class ExceptionMessage {
     public static final String INVALID_DATETIME = "The given date and time format is wrong!"
             + "Use [dd-mm-yyyy hhmm] or any other accepted formats";
     public static final String INVALID_INDEX = "The index given is invalid! Send 'list' to view the tasklist.";
-    public static final String INVALID_PRIORITY = "The priority given is invalid, please a number between 0 and 5.";
+    public static final String INVALID_PRIORITY = "The priority given is invalid, please a integer between 0 and 5.";
     public static final String MISSING_DESCRIPTION = "The description of a %s cannot be empty.";
 }
--- src/main/java/common/HelpMessage.java
+++ src/main/java/common/HelpMessage.java
@@ -9,19 +9,20 @@ public class HelpMessage {
 
     public static final String TODO = "Function: Add a to-do task to the tasklist.\n\n"
             + "Syntax: todo [Description] /priority(optional) [Priority]\n\n"
-            + "Priority = Any number from 0-5.\n\n"
+            + "Priority = Any integer from 0-5.\n\n"
             + "Example: todo Find food for Thorndike.\n\n"
             + "Example: todo Find food for Thorndike /priority 2.";
+            
     public static final String DEADLINE = "Function: Add a task with deadline to the tasklist.\n\n"
             + "Syntax: deadline [Description] /by [Date] /priority(optional) [Priority]\n\n"
-            + "Priority = Any number from 0-5.\n\n"
+            + "Priority = Any integer from 0-5.\n\n"
             + "Date = Any date and time. Time is optional and will default to 0000 if not specified.\n\n"
             + "Example: deadline Find food for Thorndike. /by 19-9-2023 1900.\n\n"
             + "Example: deadline Find food for Thorndike. /by 19-9-2023 1900. /priority 2\n\n";
 
     public static final String EVENT = "Function: Add a event to the tasklist.\n\n"
             + "Syntax: deadline [Description] /from [Date] /to [Date]\n\n"
-            + "Priority = Any number from 0-5.\n\n"
+            + "Priority = Any integer from 0-5.\n\n"
             + "Date = Any date and time. Time is optional and will default to 0000 if not specified.\n\n"
             + "Example: deadline Attend Thorndike's birthday party. /by 19-9-2023 1900 /to 20-9-2023 0800.\n\n"
             + "Example: deadline Attend Thorndike's birthday party. /by 19-9-2023 /to 20-9-2023. /priority 2\n\n";
@@ -51,7 +52,7 @@ public class HelpMessage {
     public static final String PRIORITY = "Function: Change the priority of a task.\n\n"
             + "Syntax: priority [Index] /set [Priority]\n\n"
             + "Index = The index of the task in the tasklist. Send 'list' to view the tasklist.\n\n"
-            + "Priority = Any number from 0-5.\n\n"
+            + "Priority = Any integer from 0-5.\n\n"
             + "Example: priority 3 /set 5";
 
     public static final String TOGGLE = "Function: Switch to dark/light mode.\n\n";
