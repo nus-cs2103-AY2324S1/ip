2f7e245e0f2b7f3f17e5ccf666f54c7c83f0c964
kiwibang <>
2023-09-21 16:35:36 +0800

    Add fixes to build.gradle

--- src/test/java/com/nyanbot/dukeexceptions/DukeEmptyTaskListExceptionTest.java
+++ src/test/java/com/nyanbot/dukeexceptions/DukeEmptyTaskListExceptionTest.java
@@ -1,15 +1,15 @@
 package com.nyanbot.dukeexceptions;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 public class DukeEmptyTaskListExceptionTest {
-        @Test
-        public void initTest() {
-            DukeEmptyTaskListException dummy = new DukeEmptyTaskListException();
-            String msg = dummy.toString();
-            System.out.println(msg);
-            assertEquals(msg, "com.nyanbot.dukeexceptions.DukeEmptyTaskListException: OOPS!!! The tasks list is empty!");
-        }
+    @Test
+    public void initTest() {
+        DukeEmptyTaskListException dummy = new DukeEmptyTaskListException();
+        String msg = dummy.toString();
+        System.out.println(msg);
+        assertEquals(msg, "com.nyanbot.dukeexceptions.DukeEmptyTaskListException: OOPS!!! The tasks list is empty!");
+    }
 }
