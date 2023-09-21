1828181bfcdd203391232c7cd88cead130901f0b
zekone <hkw19991@gmail.com>
2023-09-21 18:07:10 +0800

    Add test for deleteTask method of TaskList

--- src/test/java/task/TaskListTest.java
+++ src/test/java/task/TaskListTest.java
@@ -42,4 +42,37 @@ public class TaskListTest {
             assertEquals("Index 123 out of bounds for length 1", e.getMessage());
         }
     }
+
+    @Test
+    public void deleteTask_indexOob_exception() {
+        TaskList list = new TaskList();
+        try {
+            list.getTask(0);
+            fail();
+        } catch (Exception e) {
+            assertEquals("Index 0 out of bounds for length 0", e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void deleteTask_returnSameObject_success() {
+        TaskList list = new TaskList();
+        Task t1 = new Todo("test1");
+        Task t2 = new Todo("test2");
+        list.addTask(t1);
+        Task t3 = list.deleteTask(0);
+        assertEquals(t1, t3);
+        assertNotEquals(t2, t3);
+
+    }
+
+    @Test
+    public void deleteTask_removesTask_success() {
+        TaskList list = new TaskList();
+        Task t1 = new Todo("test1");
+        list.addTask(t1);
+        list.deleteTask(0);
+        assertEquals(0, list.size());
+    }
 }
