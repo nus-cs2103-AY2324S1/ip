991a9b462136c8c07cdf7dbb8beb6d7bf088eea5
Bearypop <looyl2k@gmail.com>
2023-09-21 14:47:15 +0800

    Add additional javadoc comments

--- src/main/java/duke/tasks/Task.java
+++ src/main/java/duke/tasks/Task.java
@@ -12,22 +12,40 @@ public class Task {
         this.done = false;
     }
 
+    /**
+     * Marks a task as done.
+     */
     public void markDone() {
         this.done = true;
     }
 
+    /**
+     * Marks a task as not done.
+     */
     public void markUndone() {
         this.done = false;
     }
 
+    /**
+     * Sets whether the task is done.
+     * @param done
+     */
     public void setDone(boolean done) {
         this.done = done;
     }
 
+    /**
+     * Returns a string representing the description of the task.
+     * @return description
+     */
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     * Returns a boolean representing whether the task is done.
+     * @return done
+     */
     public boolean getDone() {
         return this.done;
     }
