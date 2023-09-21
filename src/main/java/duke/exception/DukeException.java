ed3e20aa7c57a323a105a706360063b12a06f5fa
YangHeng Tang <yanghengtang@gmail.com>
2023-09-21 19:31:09 +0800

    Add DukeException.java

---
+++ src/main/java/duke/exception/DukeException.java
@@ -0,0 +1,11 @@
+package duke.exception;
+
+/**
+ * Represents an error that occurred during operation of Chad.
+ */
+public class DukeException extends Exception {
+
+    public DukeException(String message) {
+        super(message);
+    }
+}
