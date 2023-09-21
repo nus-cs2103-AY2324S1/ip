00051492b6d9cb5faef82965a377b215bdc0b900
dlathyun <shyim0128@gmail.com>
2023-09-21 17:20:46 +0800

    Edit DateTimeParser class such that it will remove white speaces when it parse the string.

--- src/main/java/duke/parser/DateTimeParser.java
+++ src/main/java/duke/parser/DateTimeParser.java
@@ -3,6 +3,7 @@ package duke.parser;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.Arrays;
 
 import duke.exception.KoraException;
 
@@ -17,18 +18,45 @@ public class DateTimeParser {
     }
 
     public String outFormatted(String dateTime) throws KoraException {
+        String dT = "";
+        String[] strArray = dateTime.trim().split(" ");
+        System.out.println(Arrays.toString(strArray));
+        if (strArray.length != 2) {
+            for (int i = 0; i < strArray.length; i++) {
+                if (!strArray[i].contains(" ") && !strArray[i].equals("")) {
+                    String str = strArray[i].trim();
+                    dT = dT + " " + str;
+                }
+            }
+            dT = dT.trim();
+        } else {
+            dT = dateTime.trim();
+        }
         try {
-            return LocalDateTime.parse(dateTime, saveFormatter).format(outFormatter);
+            return LocalDateTime.parse(dT, saveFormatter).format(outFormatter);
         } catch (DateTimeParseException e) {
-            throw new KoraException("Omo! The date format should be yyyy-MM-dd HH-mm!");
+            throw new KoraException("Omo! The date format should be yyyy-MM-dd HH-mm!" + dT + " " + Arrays.toString(strArray) + strArray[2].trim());
         }
     }
 
     public String saveFormatted(String dateTime) throws KoraException {
+        String dT = "";
+        String[] strArray = dateTime.trim().split(" ");
+        if (strArray.length != 2) {
+            for (int i = 0; i < strArray.length; i++) {
+                if (!strArray[i].contains(" ") && !strArray[i].equals("")) {
+                    String str = strArray[i].trim();
+                    dT = dT + " " + str;
+                }
+            }
+            dT = dT.trim();
+        } else {
+            dT = dateTime.trim();
+        }
         try {
-            return LocalDateTime.parse(dateTime, saveFormatter).format(saveFormatter);
+            return LocalDateTime.parse(dT, saveFormatter).format(saveFormatter);
         } catch (DateTimeParseException e) {
-            throw new KoraException("Omo! The date format should be yyyy-MM-dd HH-mm!");
+            throw new KoraException("Omo! The date format should be yyyy-MM-dd HH-mm!" + dT + " " + Arrays.toString(strArray) + strArray[2].trim());
         }
     }
 }
