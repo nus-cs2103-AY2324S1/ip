a3708461042f0f869a9d47aaade920ad49d78151
techjay-c <>
2023-09-21 10:58:58 +0800

    Remove unused imports in Deadline Test to comply with checkstyle

--- src/test/java/duke/DeadlineTest.java
+++ src/test/java/duke/DeadlineTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 import org.junit.jupiter.api.Test;
 
