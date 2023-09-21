6e72e54b1f2a0ca5e96ac3c7a3e65037fe53bc9d
Chen1x <chenxinan01@gmail.com>
2023-09-21 20:03:30 +0800

    Add A-JUnit

---
+++ src/test/java/jarvis/task/DeadlineTest.java
@@ -0,0 +1,34 @@
+package jarvis.task;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+
+    @Test
+    public void testToString() {
+        Deadline deadline = new Deadline("return book", LocalDateTime.of(2023, 9, 20, 12, 0));
+        assertEquals("[D][ ] return book (by: Sep 20 2023 1200)", deadline.toString());
+    }
+
+    @Test
+    public void testToSaveString() {
+        Deadline deadline = new Deadline("return book", false, LocalDateTime.of(2023, 9, 20, 12, 0));
+        assertEquals("D | 0 | return book | Sep 20 2023 1200", deadline.toSaveString());
+    }
+
+    @Test
+    public void testToSaveStringCompleted() {
+        Deadline deadline = new Deadline("return book", true, LocalDateTime.of(2023, 9, 20, 12, 0));
+        assertEquals("D | 1 | return book | Sep 20 2023 1200", deadline.toSaveString());
+    }
+
+    @Test
+    public void testGetByString() {
+        Deadline deadline = new Deadline("return book", LocalDateTime.of(2023, 9, 20, 12, 0));
+        assertEquals("Sep 20 2023 1200", deadline.getByString());
+    }
+}
