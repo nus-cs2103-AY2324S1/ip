03afb34f5f6037b581a4d5265f9d0305fea8496f
YangHeng Tang <yanghengtang@gmail.com>
2023-09-21 16:19:25 +0800

    Add ChadDialogBox.java

---
+++ src/main/java/duke/gui/ChadDialogBox.java
@@ -0,0 +1,39 @@
+package duke.gui;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+
+/**
+ * Represents controller for a dialog box consisting of an ImageView to represent the speaker's face and a label
+ * containing text from the speaker.
+ */
+public class ChadDialogBox extends HBox {
+    @FXML
+    private Text dialog;
+    @FXML
+    private ImageView displayPicture;
+
+    private ChadDialogBox(String text, Image img) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/ChadDialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        dialog.setText(text);
+        displayPicture.setImage(img);
+    }
+
+    public static ChadDialogBox getChadDialog(String text, Image img) {
+        return new ChadDialogBox(text, img);
+    }
+}
