00debc3ab637336227f9d5d8dbeb62a71100a1f5
ZD292 <ongzhendong@gmail.com>
2023-09-21 17:01:20 +0800

    add title

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -29,6 +29,7 @@ public class Main extends Application {
 			AnchorPane ap = fxmlLoader.load();
 			Scene scene = new Scene(ap);
 			stage.setScene(scene);
+			stage.setTitle("Duke ChatBot");
 			fxmlLoader.<MainWindow>getController().setDuke(duke);
 			stage.show();
 		} catch (IOException e) {
