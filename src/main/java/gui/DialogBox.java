575138e8067bf397d086d56b7504c965f8c084ed
Tan Yiming <tanyiming.nus@gmail.com>
2023-09-22 19:53:14 +0800

    Improve GUI for better visual effect

--- src/main/java/gui/DialogBox.java
+++ src/main/java/gui/DialogBox.java
@@ -9,11 +9,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 
 /**
  * An example of a custom control using FXML.
@@ -24,11 +23,11 @@ public class DialogBox extends HBox {
     // @@author tanyyyming-reused
     // Reused from https://se-education.org/guides/tutorials/javaFxPart4.html with minor modifications
     @FXML
-    private Text dialog;
+    private Label dialog;
     @FXML
     private ImageView displayPicture;
 
-    private DialogBox(String text, Image img) {
+    private DialogBox(String text, Image img, boolean isUser) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
             fxmlLoader.setController(this);
@@ -39,8 +38,13 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
-        dialog.setTextAlignment(TextAlignment.RIGHT);
         displayPicture.setImage(img);
+
+        if (isUser) {
+            dialog.getStyleClass().add("red-background");
+        } else {
+            dialog.getStyleClass().add("blue-background");
+        }
     }
 
     /**
@@ -50,16 +54,15 @@ public class DialogBox extends HBox {
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
         Collections.reverse(tmp);
         getChildren().setAll(tmp);
-        ((Text) getChildren().get(1)).setTextAlignment(TextAlignment.LEFT);
         setAlignment(Pos.TOP_LEFT);
     }
 
     public static DialogBox getUserDialog(String text, Image img) {
-        return new DialogBox(text, img);
+        return new DialogBox(text, img, true);
     }
 
     public static DialogBox getJarvisDialog(String text, Image img) {
-        var db = new DialogBox(text, img);
+        var db = new DialogBox(text, img, false);
         db.flip();
         return db;
     }
