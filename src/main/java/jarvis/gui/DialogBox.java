52d53226cbacf07d98dace7ca32549428fd18b52
Evan Yan <103996156+evanyan13@users.noreply.github.com>
2023-09-22 23:54:46 +0800

    Edit syntax error

--- src/main/java/jarvis/gui/DialogBox.java
+++ src/main/java/jarvis/gui/DialogBox.java
@@ -17,7 +17,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 /**
