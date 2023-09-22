e1bf80ea75aebcf162af34b276e2e64f2806557f
LIN <peterway6933@gmail.com>
2023-09-22 22:32:38 +0800

    added more exception handling

--- src/main/java/Alex/AlexException.java
+++ src/main/java/Alex/AlexException.java
@@ -4,6 +4,10 @@ package Alex;
  * An exception class that can be instantiated to represent exception related to Alex bot.
  */
 public class AlexException extends Exception {
+    /**
+     * Constructor for AlexException.
+     * @param message message to be wrapped in AlexException.
+     */
     public AlexException(String message) {
 
         super(message);
