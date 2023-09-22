5f6ba139cd1c533b714dfedaa96f05a5e7ada3cb
cbj252 <cbj252@gmail.com>
2023-09-22 18:49:09 +0800

    Make bye command close the GUI

--- src/main/java/duke/ui/MainWindow.java
+++ src/main/java/duke/ui/MainWindow.java
@@ -1,5 +1,7 @@
 package duke.ui;
 
+import javafx.animation.PauseTransition;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -7,6 +9,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
@@ -44,6 +47,14 @@ public class MainWindow extends AnchorPane {
     @FXML
     private void handleUserInput() {
         String input = userInput.getText();
+        //@@author bryanhce-reused
+        //Reused from @bryanhce
+        if (input.equals("bye")) {
+            PauseTransition delay = new PauseTransition(Duration.seconds(2));
+            delay.setOnFinished(e -> Platform.exit());
+            delay.play();
+        }
+        //@@author
         String response = mainProgram.run(input);
         dialogContainer.getChildren().addAll(
                 DialogBox.getUserDialog(input, userImage),
