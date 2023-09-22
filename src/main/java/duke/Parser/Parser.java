8e0686cf74d9dc47780ab85aa0968f52b18657a8
Fredy Lawrence <fredylawrence2003@gmail.com>
2023-09-22 16:20:16 +0800

    divideLongMethod

--- src/main/java/duke/Parser/Parser.java
+++ src/main/java/duke/Parser/Parser.java
@@ -35,101 +35,25 @@ public class Parser {
      */
     public static String parse(String input, Ui ui, Storage storage, TaskList tasks) throws DukeException {
         String command = input.split(" ")[0].toUpperCase();
-        Task targetTask;
         try {
             if (command.equals("BYE")) {
                 return ui.bye(storage, tasks);
             } else if (command.equals("LIST")) {
                 return ui.listing(tasks);
             } else if (command.equals("TODO")) {
-                if (ToDos.isTodo(input)) {
-                    targetTask = new ToDos(input.substring(5));
-                    tasks.addTask(targetTask);
-                    return ui.addTask(targetTask, tasks.getNumberOfTask());
-                } else {
-                    throw new DukeException("This todo is invalid.");
-                }
+                return parseTodo(input, ui, tasks);
             } else if (command.equals("DEADLINE")) {
-                if (Deadlines.isDeadline(input)) {
-                    targetTask = new Deadlines(
-                            input.substring(9, input.indexOf("/by ")),
-                            input.substring(input.indexOf("/by ") + 4)
-                    );
-                    tasks.addTask(targetTask);
-                    return ui.addTask(targetTask, tasks.getNumberOfTask());
-                } else {
-                    throw  new DukeException("This deadline is invalid.");
-                }
+                return parseDeadline(input, ui, tasks);
             } else if (command.equals("EVENT")) {
-                if (Events.isEvent(input)) {
-                    targetTask = new Events(
-                            input.substring(6, input.indexOf("/from ")),
-                            input.substring(input.indexOf("/from ") + 6, input.indexOf("/to ") - 1),
-                            input.substring(input.indexOf("/to ") + 4)
-                    );
-                    tasks.addTask((targetTask));
-                    return ui.addTask(targetTask, tasks.getNumberOfTask());
-                } else {
-                    throw new DukeException("This event is invalid.");
-                }
+                return parseEvent(input, ui, tasks);
             } else if (command.equals("PERIOD")) {
-                if (Periods.isPeriod(input)) {
-                    targetTask = new Periods(
-                            input.substring(7, input.indexOf("/between ")),
-                            input.substring(input.indexOf("/between ") + 8, input.indexOf("/and ") - 1),
-                            input.substring(input.indexOf("/and ") + 5)
-                    );
-                    tasks.addTask((targetTask));
-                    return ui.addTask(targetTask, tasks.getNumberOfTask());
-                } else {
-                    throw new DukeException("This period is invalid.");
-                }
+                return parsePeriod(input, ui, tasks);
             } else if (command.equals("MARK")) {
-                if (input.split(" ").length == 2 && isInteger(input.split(" ")[1])) {
-                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                    if (index < 0) {
-                        throw new DukeException("There is no task number " + (index + 1));
-                    } else if (index <= tasks.getNumberOfTask() - 1) {
-                        assert index > -1;
-                        tasks.getTask(index).changeMarkStatus(true);
-                        return ui.markAsDone(tasks.getTask(index));
-                    } else {
-                        throw new DukeException("You don't have that many tasks.");
-                    }
-                } else {
-                    throw new DukeException("You must specified the index of the task to mark.");
-                }
+                return parseMark(input, ui, tasks);
             } else if (command.equals("UNMARK")) {
-                if (input.split(" ").length == 2 && isInteger(input.split(" ")[1])) {
-                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                    if (index < 0) {
-                        throw new DukeException("There is no task number " + (index + 1));
-                    } else if (index <= tasks.getNumberOfTask() - 1) {
-                        assert index > -1;
-                        tasks.getTask(index).changeMarkStatus(false);
-                        return ui.markAsNotDone(tasks.getTask(index));
-                    } else {
-                        throw new DukeException("You don't have that many tasks.");
-                    }
-                } else {
-                    throw new DukeException("You must specified the index of the task to unmark.");
-                }
+                return parseUnmark(input, ui, tasks);
             } else if (command.equals("DELETE")) {
-                if (input.split(" ").length == 2 && isInteger(input.split(" ")[1])) {
-                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                    if (index < 0) {
-                        throw new DukeException("There is no task number " + (index + 1));
-                    } else if (index <= tasks.getNumberOfTask() - 1) {
-                        assert index > -1;
-                        targetTask = tasks.getTask(index);
-                        tasks.deleteTask(index);
-                        return ui.deleteTask(targetTask, tasks.getNumberOfTask());
-                    } else {
-                        throw new DukeException("You don't have that many tasks.");
-                    }
-                } else {
-                    throw new DukeException("You must specified the index of the task to delete.");
-                }
+                return parseDelete(input, ui, tasks);
             } else if (command.equals("FIND")) {
                 return ui.find(tasks, input.substring(5));
             } else {
@@ -137,6 +61,114 @@ public class Parser {
             }
         } catch (DukeException e) {
             return ui.error(e);
+        }
+    }
+
+    private static String parseTodo(String input, Ui ui, TaskList tasks) throws DukeException{
+        Task targetTask;
+        if (ToDos.isTodo(input)) {
+            targetTask= new ToDos(input.substring(5));
+            tasks.addTask(targetTask);
+            return ui.addTask(targetTask, tasks.getNumberOfTask());
+        } else {
+            throw new DukeException("This todo is invalid.");
+        }
+    }
+
+    private static String parseDeadline(String input, Ui ui, TaskList tasks) throws DukeException{
+        Task targetTask;
+        if (Deadlines.isDeadline(input)) {
+            targetTask = new Deadlines(
+                    input.substring(9, input.indexOf("/by ")),
+                    input.substring(input.indexOf("/by ") + 4)
+            );
+            tasks.addTask(targetTask);
+            return ui.addTask(targetTask, tasks.getNumberOfTask());
+        } else {
+            throw  new DukeException("This deadline is invalid.");
+        }
+    }
+
+    private static String parseEvent(String input, Ui ui, TaskList tasks) throws DukeException{
+        Task targetTask;
+        if (Events.isEvent(input)) {
+            targetTask = new Events(
+                    input.substring(6, input.indexOf("/from ")),
+                    input.substring(input.indexOf("/from ") + 6, input.indexOf("/to ") - 1),
+                    input.substring(input.indexOf("/to ") + 4)
+            );
+            tasks.addTask((targetTask));
+            return ui.addTask(targetTask, tasks.getNumberOfTask());
+        } else {
+            throw new DukeException("This event is invalid.");
+        }
+    }
+
+    private static String parsePeriod(String input, Ui ui, TaskList tasks) throws DukeException{
+        Task targetTask;
+        if (Periods.isPeriod(input)) {
+            targetTask = new Periods(
+                    input.substring(7, input.indexOf("/between ")),
+                    input.substring(input.indexOf("/between ") + 8, input.indexOf("/and ") - 1),
+                    input.substring(input.indexOf("/and ") + 5)
+            );
+            tasks.addTask((targetTask));
+            return ui.addTask(targetTask, tasks.getNumberOfTask());
+        } else {
+            throw new DukeException("This period is invalid.");
+        }
+    }
+
+    private static String parseMark(String input, Ui ui, TaskList tasks) throws DukeException{
+        if (input.split(" ").length == 2 && isInteger(input.split(" ")[1])) {
+            int index = Integer.parseInt(input.split(" ")[1]) - 1;
+            if (index < 0) {
+                throw new DukeException("There is no task number " + (index + 1));
+            } else if (index <= tasks.getNumberOfTask() - 1) {
+                assert index > -1;
+                tasks.getTask(index).changeMarkStatus(true);
+                return ui.markAsDone(tasks.getTask(index));
+            } else {
+                throw new DukeException("You don't have that many tasks.");
+            }
+        } else {
+            throw new DukeException("You must specified the index of the task to mark.");
+        }
+    }
+
+    private static String parseUnmark(String input, Ui ui, TaskList tasks) throws DukeException{
+        if (input.split(" ").length == 2 && isInteger(input.split(" ")[1])) {
+            int index = Integer.parseInt(input.split(" ")[1]) - 1;
+            if (index < 0) {
+                throw new DukeException("There is no task number " + (index + 1));
+            } else if (index <= tasks.getNumberOfTask() - 1) {
+                assert index > -1;
+                tasks.getTask(index).changeMarkStatus(false);
+                return ui.markAsNotDone(tasks.getTask(index));
+            } else {
+                throw new DukeException("You don't have that many tasks.");
+            }
+        } else {
+            throw new DukeException("You must specified the index of the task to unmark.");
+        }
+    }
+
+    private static String parseDelete(String input, Ui ui, TaskList tasks) throws DukeException{
+        Task targetTask;
+        if (input.split(" ").length == 2 && isInteger(input.split(" ")[1])) {
+            int index = Integer.parseInt(input.split(" ")[1]) - 1;
+            if (index < 0) {
+                throw new DukeException("There is no task number " + (index + 1));
+            } else if (index <= tasks.getNumberOfTask() - 1) {
+                assert index > -1;
+                targetTask = tasks.getTask(index);
+                tasks.deleteTask(index);
+                return ui.deleteTask(targetTask, tasks.getNumberOfTask());
+            } else {
+                throw new DukeException("You don't have that many tasks.");
+            }
+        } else {
+            throw new DukeException("You must specified the index of the task to delete.");
         }
     }
 }
