ea952824999479722f361a19cd49947b99c0b79c
AriellaCallista <arielgit>
2023-09-23 00:54:35 +0800

    Fix code quality

--- src/main/java/sana/Main.java
+++ src/main/java/sana/Main.java
@@ -1,7 +1,5 @@
 package sana;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -19,24 +17,6 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
-//            String filePath = "data/SanaTasks.txt";
-//            File folder = new File(String.valueOf(Paths.get("data/SanaTasks.txt").getParent()));
-//
-//            // Check if folder exists, if not create one
-//            if (!folder.exists()) {
-//                folder.mkdirs();
-//            }
-//
-//            // Check if file exists, if not create one
-//            File file = new File(filePath);
-//            if (!file.exists()) {
-//                try {
-//                    file.createNewFile();
-//                } catch (IOException e) {
-//                    System.out.println(e.getMessage());
-//                }
-//            }
-
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
