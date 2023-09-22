fd209ce61f2354566f3f67f4a640ee1a5cf65d45
AlyssaPng <e0959598@u.nus.edu>
2023-09-22 23:44:23 +0800

    Change Error Handling

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -83,21 +83,46 @@ public class TaskList {
         String[] inputArray = userInput.trim().split(" ", 2);
         if (inputArray.length != 2 || inputArray[1].isEmpty()) {
             throw new EmptyTaskException(inputArray[0]);
+        }
+
+        if (userInput.startsWith("todo")) {
+            newTask = createToDo(inputArray[1]);
+        } else if (userInput.startsWith("deadline")) {
+            newTask = createDeadline(inputArray[1]);
         } else {
-            if (inputArray[0].equals("todo")) {
-                newTask = new ToDo(inputArray[1]);
-            } else if (inputArray[0].equals("deadline")) {
-                String[] details = inputArray[1].split(" /by ");
-                if (details.length != 2 || details[1].isEmpty()) {
-                    throw new EmptyDateException(inputArray[0]);
-                }
-                newTask = new Deadline(details[0], getDate(details[1]));
-            } else {
-                assert inputArray[0].equals("event") : "The userInput should start with event";
-                newTask = createEvent(inputArray);
-            }
+            assert inputArray[0].equals("event") : "The userInput should start with event";
+            newTask = createEvent(inputArray[1]);
         }
         return newTask;
+    }
+
+    /**
+     * Creates a ToDo Task according to the user's input.
+     *
+     * @param todoDetails The description of the ToDo Task.
+     * @return the ToDo Task.
+     */
+    public Task createToDo(String todoDetails) {
+        return new ToDo(todoDetails);
+    }
+
+    /**
+     * Creates a Deadline Task according to the user's input.
+     *
+     * @param input the user's deadline input.
+     * @return the Deadline Task.
+     * @throws DukeException if the deadline inputs are wrong.
+     */
+    public Task createDeadline(String input) throws DukeException {
+        String[] details = input.split("/by ");
+        String description = details[0].trim();
+        if (description.equals("/by") || description.isEmpty()) {
+            throw new EmptyTaskException("deadline");
+        }
+        if (details.length != 2 || details[1].isEmpty()) {
+            throw new EmptyDateException("deadline");
+        }
+        return new Deadline(description, getDate(details[1].trim()));
     }
 
     /**
@@ -119,19 +144,25 @@ public class TaskList {
      * @return the corresponding Event Task.
      * @throws DukeException if the event details are wrongly formatted.
      */
-    public Task createEvent(String[] eventDetails) throws DukeException {
-        if (checkEventFormat(eventDetails[1])) {
+    public Task createEvent(String eventDetails) throws DukeException {
+        if (checkEventFormat(eventDetails)) {
             throw new DukeException("OOPS!! Please follow the format:\nevent DESCRIPTION /from FROM /to TO");
         }
-        String[] details = eventDetails[1].split(" /from ");
-        if (details.length != 2 || details[1].isEmpty()) {
-            throw new EmptyDateException(eventDetails[0]);
+        String[] details = eventDetails.trim().split("/from ");
+        if (details[0].equals("/from") || details[0].isEmpty()) {
+            throw new EmptyTaskException("event");
         }
-        String[] fromto = details[1].trim().split(" /to ");
+        if (details.length != 2 || details[1].isEmpty()) {
+            throw new EmptyDateException("event");
+        }
+        String[] fromto = details[1].trim().split("/to ");
+        if (fromto[0].equals("/to") || fromto[0].isEmpty()) {
+            throw new EmptyDateException("event");
+        }
         if (fromto.length != 2 || fromto[1].isEmpty()) {
             throw new NoEndDateException();
         }
-        return new Event(details[0], getDate(fromto[0]), getDate(fromto[1]));
+        return new Event(details[0], getDate(fromto[0].trim()), getDate(fromto[1].trim()));
     }
 
     /**
