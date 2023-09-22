f2c5b00ab03ef2d80af81cdde799fa5ba2922fbc
Goh Li Ting <e0970558@u.nus.edu>
2023-09-22 23:29:08 +0800

    Change ExitCommand to close the application

--- src/main/java/chatbuddy/command/ExitCommand.java
+++ src/main/java/chatbuddy/command/ExitCommand.java
@@ -4,6 +4,7 @@ import chatbuddy.ChatBuddyException;
 import chatbuddy.TaskList;
 import chatbuddy.storage.Storage;
 import chatbuddy.ui.Ui;
+import javafx.application.Platform;
 
 /** ExitCommand represents a command to exit the chatbot. */
 public class ExitCommand extends Command {
@@ -13,6 +14,7 @@ public class ExitCommand extends Command {
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) throws ChatBuddyException {
         storage.save(tasks);
+        Platform.exit();
         return ui.showExit();
     }
 }
