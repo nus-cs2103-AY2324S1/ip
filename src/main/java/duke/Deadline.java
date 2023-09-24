99395601f3d7c974d5f8c09543b22ad8725b9f6d
Vedant Sinha <vedantsinha2003@gmail.com>
2023-09-24 09:50:29 +0800

    Update deadline date format

--- src/main/java/duke/Deadline.java
+++ src/main/java/duke/Deadline.java
@@ -1,20 +1,25 @@
 package duke;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate byDate;
 
     public Deadline(String description, String by, char taskType) {
         super(description, taskType);
-        this.by = by;
+        this.byDate = LocalDate.parse(by, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 
     @Override
     public String toFileString() {
-        return super.toFileString() + " | " + by;
+        return super.toFileString() + " | " +
+                byDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 
     @Override
     public String toString() {
-        return super.toString() + " (by: " + by + ")";
+        return super.toString() + " (by: " +
+                byDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
     }
 }
