5e6f66eba2bac750e0f7142ab74816281930ec03
JasonRay168 <e0985893@u.nus.edu>
2023-09-24 15:50:53 +0800

    Add name of the app on top of main window

--- src/main/java/echobot/Main.java
+++ src/main/java/echobot/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("EchoBot");
             stage.setResizable(false);
             fxmlLoader.<MainWindow>getController().setEchoBot(echobot);
             fxmlLoader.<MainWindow>getController().greetUser();
