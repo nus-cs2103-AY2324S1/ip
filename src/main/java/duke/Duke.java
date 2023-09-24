e887e87654b99be58867931ca2aead0c392ddcb8
Vedant Sinha <vedantsinha2003@gmail.com>
2023-09-24 10:56:26 +0800

    Update Ui.java file

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -13,6 +13,7 @@ public class Duke {
     private static final String FILE_PATH = "./data/duke.txt";
     private static final ArrayList<Task> taskList = new ArrayList<>();
 
+
     public static void main(String[] args) {
 
         try {
@@ -22,20 +23,19 @@ public class Duke {
         }
 
         Scanner scanner = new Scanner(System.in);
+        Ui ui = new Ui();
 
         // Greeting message
-        System.out.println("Hello! I'm Sivraj");
-        System.out.println("What can I do for you?");
+        ui.displayWelcomeMessage();
 
         while (true) {
             String echo = scanner.nextLine();
-            String dashLine = "----------------------------------------";
 
             try {
 
                 if (echo.equals("bye")) {
                     // Farewell message
-                    System.out.println("Bye. Hope to see you again soon!");
+                    ui.displayByeMessage();
                     try {
                         appendTasksToFile(taskList, FILE_PATH);
                     } catch (IOException e) {
@@ -43,12 +43,7 @@ public class Duke {
                     }
                     break;
                 } else if (echo.equals("list")) {
-                    System.out.println(dashLine);
-                    System.out.println("    Here are the tasks in your list:");
-                    for (int i = 0; i < taskList.size(); i++) {
-                        System.out.println("     " + (i + 1) + "." + taskList.get(i));
-                    }
-                    System.out.println(dashLine);
+                    ui.displayListMessage(taskList);
                 } else if (echo.startsWith("todo")) {
                     if (echo.length() <= 5) {
                         throw new DukeException("The description of a todo cannot be empty.");
@@ -56,22 +51,14 @@ public class Duke {
                     String description = echo.substring(5).trim();
                     taskList.add(new ToDo(description, 'T'));
 
-                        System.out.println(dashLine);
-                        System.out.println("     Got it. I've added this task:");
-                        System.out.println("         " + taskList.get(taskList.size() - 1));
-                        System.out.println("     Now you have " + taskList.size() + " tasks in the list.");
-                        System.out.println(dashLine);
+                    ui.displayToDoMessage(taskList);
 
                 } else if (echo.startsWith("deadline")) {
                     int byIndex = echo.indexOf("/by");
                     String description = echo.substring(9, byIndex).trim();
                     String by = echo.substring(byIndex + 3).trim();
                     taskList.add(new Deadline(description, by, 'D'));
-                    System.out.println(dashLine);
-                    System.out.println("     Got it. I've added this task:");
-                    System.out.println("       " + taskList.get(taskList.size() - 1));
-                    System.out.println("     Now you have " + taskList.size() + " tasks in the list.");
-                    System.out.println(dashLine);
+                    ui.displayDeadlineMessage(taskList);
                 } else if (echo.startsWith("event")) {
                     int fromIndex = echo.indexOf("/from");
                     int toIndex = echo.indexOf("/to");
@@ -79,19 +66,12 @@ public class Duke {
                     String from = echo.substring(fromIndex + 5, toIndex).trim();
                     String to = echo.substring(toIndex + 3).trim();
                     taskList.add(new Event(description, from, to, 'E'));
-                    System.out.println(dashLine);
-                    System.out.println("     Got it. I've added this task:");
-                    System.out.println("       " + taskList.get(taskList.size() - 1));
-                    System.out.println("     Now you have " + taskList.size() + " tasks in the list.");
-                    System.out.println(dashLine);
+                    ui.displayEventMessage(taskList);
                 } else if (echo.startsWith("mark")) {
                     int taskIndex = Integer.parseInt(echo.substring(5).trim()) - 1;
                     if (taskIndex >= 0 && taskIndex < taskList.size()) {
                         taskList.get(taskIndex).markAsDone();
-                        System.out.println(dashLine);
-                        System.out.println("    Nice! I've marked this task as done:");
-                        System.out.println("       " + taskList.get(taskIndex));
-                        System.out.println(dashLine);
+                        ui.displayMarkMessage(taskList, taskIndex);
                     } else {
                         System.out.println("Invalid task index.");
                     }
@@ -99,10 +79,7 @@ public class Duke {
                     int taskIndex = Integer.parseInt(echo.substring(7).trim()) - 1;
                     if (taskIndex >= 0 && taskIndex < taskList.size()) {
                         taskList.get(taskIndex).markAsNotDone();
-                        System.out.println(dashLine);
-                        System.out.println("     OK, I've marked this task as not done yet:");
-                        System.out.println("       " + taskList.get(taskIndex));
-                        System.out.println(dashLine);
+                        ui.displayUnmarkMessage(taskList, taskIndex);
                     } else {
                         System.out.println("Invalid task index.");
                     }
@@ -110,11 +87,7 @@ public class Duke {
                     int taskIndex = Integer.parseInt(echo.substring(7).trim()) - 1;
                     if (taskIndex >= 0 && taskIndex < taskList.size()) {
                         Task removedTask = taskList.remove(taskIndex);
-                        System.out.println(dashLine);
-                        System.out.println("     Noted. I've removed this task:");
-                        System.out.println("       " + removedTask);
-                        System.out.println("     Now you have " + taskList.size() + " tasks in the list.");
-                        System.out.println(dashLine);
+                        ui.displayDeleteMessage(taskList, removedTask);
                     } else {
                         System.out.println("Invalid task index.");
                     }
@@ -122,9 +95,7 @@ public class Duke {
                     throw new DukeException("I'm sorry, but I don't know what that means :-(");
                 }
             } catch (DukeException e) {
-                System.out.println(dashLine);
-                System.out.println("    OOPS " + e.getMessage());
-                System.out.println(dashLine);
+                ui.displayErrorMessage(e);
             }
         }
 
--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -1,5 +1,78 @@
 package duke;
 
+import java.util.ArrayList;
+
 public class Ui {
+
+    public void displayWelcomeMessage() {
+        System.out.println("Hello! I'm Sivraj");
+        System.out.println("What can I do for you?");
+    }
+    String dashLine = "----------------------------------------";
+
+    public void displayToDoMessage(ArrayList<Task> taskList) {
+        System.out.println(dashLine);
+        System.out.println("     Got it. I've added this task:");
+        System.out.println("         " + taskList.get(taskList.size() - 1));
+        System.out.println("     Now you have " + taskList.size() + " tasks in the list.");
+        System.out.println(dashLine);
+    }
+
+    public void displayDeadlineMessage(ArrayList<Task> taskList) {
+        System.out.println(dashLine);
+        System.out.println("     Got it. I've added this task:");
+        System.out.println("       " + taskList.get(taskList.size() - 1));
+        System.out.println("     Now you have " + taskList.size() + " tasks in the list.");
+        System.out.println(dashLine);
+    }
+
+    public void displayEventMessage(ArrayList<Task> taskList) {
+        System.out.println(dashLine);
+        System.out.println("     Got it. I've added this task:");
+        System.out.println("       " + taskList.get(taskList.size() - 1));
+        System.out.println("     Now you have " + taskList.size() + " tasks in the list.");
+        System.out.println(dashLine);
+    }
+
+    public void displayMarkMessage(ArrayList<Task> taskList, int taskIndex) {
+        System.out.println(dashLine);
+        System.out.println("    Nice! I've marked this task as done:");
+        System.out.println("       " + taskList.get(taskIndex));
+        System.out.println(dashLine);
+    }
+
+    public void displayUnmarkMessage(ArrayList<Task> taskList, int taskIndex) {
+        System.out.println(dashLine);
+        System.out.println("     OK, I've marked this task as not done yet:");
+        System.out.println("       " + taskList.get(taskIndex));
+        System.out.println(dashLine);
+    }
+
+    public void displayListMessage(ArrayList<Task> taskList) {
+        System.out.println(dashLine);
+        System.out.println("    Here are the tasks in your list:");
+        for (int i = 0; i < taskList.size(); i++) {
+            System.out.println("     " + (i + 1) + "." + taskList.get(i));
+        }
+        System.out.println(dashLine);
+    }
+
+    public void displayDeleteMessage(ArrayList<Task> taskList, Task removedTask) {
+        System.out.println(dashLine);
+        System.out.println("     Noted. I've removed this task:");
+        System.out.println("       " + removedTask);
+        System.out.println("     Now you have " + taskList.size() + " tasks in the list.");
+        System.out.println(dashLine);
+    }
+
+    public void displayErrorMessage(DukeException e) {
+        System.out.println(dashLine);
+        System.out.println("    OOPS " + e.getMessage());
+        System.out.println(dashLine);
+    }
+
+    public void displayByeMessage() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
 }
 
