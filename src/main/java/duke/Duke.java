f270bbc90bcecbced61b9497ee139dabd9e0cac9
Qin Nanxin <nanxinqin@hotmail.com>
2023-09-25 20:57:07 +0800

    [Level 10. GUI] Initialize a GUI to the chatbot.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,3 +1,5 @@
+package duke;
+
 import java.util.Scanner;
 
 import duke.helper.DukeException;
@@ -5,6 +7,11 @@ import duke.helper.Parser;
 import duke.helper.Ui;
 import duke.storage.Storage;
 import duke.storage.TaskList;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class Duke {
     public static final Scanner SCANNER = new Scanner(System.in);
