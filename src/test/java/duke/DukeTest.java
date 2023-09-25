e808cd498ee9175f8c86318db9b837f5774d6a53
seraphimstreets <edisonsiowxiong@gmail.com>
2023-09-25 19:48:28 +0800

    remove junit test debug comment

--- src/test/java/duke/DukeTest.java
+++ src/test/java/duke/DukeTest.java
@@ -16,34 +16,26 @@ public class DukeTest {
 
 
     @Test
-    public void testFormatDeadline() {
-        try {
-            Deadline deadline = new Deadline("help", true,
-                    "/by 8/8/2020 1630");
-            String formattedDeadline = deadline.getFormattedDatetime(dtf.getOutFormatter());
-            assert(formattedDeadline.equals("(by: 2020-08-08 16:30)"));
-        } catch (DukeException e) {
-            System.out.println("Error occurred while converting item to string.");
-        }
+    public void testFormatDeadline() throws DukeException {
+        Deadline deadline = new Deadline("help", true, "/by 8/8/2020 1630");
+        String formattedDeadline = deadline.getFormattedDatetime(dtf.getOutFormatter());
+        assert(formattedDeadline.equals("(by: 2020-08-08 16:30)"));
     }
     @Test
-    public void testTaskListSize() {
+    public void testTaskListSize() throws DukeException {
         TaskList tl = new TaskList();
-        try {
-            Deadline deadline = new Deadline("help", true,
-                    "/by 8/8/2020 1630");
-            tl.addItem(deadline);
-            tl.addItem(deadline);
-            tl.addItem(deadline);
-            assertEquals(tl.getSize(), 3);
-            tl.removeItem(0);
-            assertEquals(tl.getSize(), 2);
-            tl.addItem(null);
-            assertEquals(tl.getSize(), 3);
 
-        } catch (DukeException e) {
-            System.out.println("Error occurred while adding items to Tasklist.");
-        }
+        Deadline deadline = new Deadline("help", true,
+                    "/by 8/8/2020 1630");
+        tl.addItem(deadline);
+        tl.addItem(deadline);
+        tl.addItem(deadline);
+        assertEquals(tl.getSize(), 3);
+        tl.removeItem(0);
+        assertEquals(tl.getSize(), 2);
+        tl.addItem(null);
+        assertEquals(tl.getSize(), 3);
+
     }
 
     @Test
@@ -61,43 +53,35 @@ public class DukeTest {
     }
 
     @Test
-    public void testHandleMarkCommand() {
-
-        try {
-            TaskList tl = new TaskList();
-            CommandHandler ch = new CommandHandler(tl, ui, dtf);
-            String userInput = "event build sandcastle /from 18/8/2020 1700 /to 18/8/2020 1800";
-            String[] splitStr = userInput.split("\\s+");
-            ch.handleEventCommand(splitStr);
-            String userInput2 = "mark 1";
-            String[] splitStr2 = userInput2.split("\\s+");
-            ch.handleMarkCommand(splitStr2);
-            boolean isMarked = tl.getItem(0).getIsCompleted();
-            assertEquals(isMarked, true);
-        } catch (DukeException e) {
-            System.out.println("Error occurred while adding items to Tasklist.");
-        }
+    public void testHandleMarkCommand() throws DukeException {
+        TaskList tl = new TaskList();
+        CommandHandler ch = new CommandHandler(tl, ui, dtf);
+        String userInput = "event build sandcastle /from 18/8/2020 1700 /to 18/8/2020 1800";
+        String[] splitStr = userInput.split("\\s+");
+        ch.handleEventCommand(splitStr);
+        String userInput2 = "mark 1";
+        String[] splitStr2 = userInput2.split("\\s+");
+        ch.handleMarkCommand(splitStr2);
+        boolean isMarked = tl.getItem(0).getIsCompleted();
+        assertEquals(isMarked, true);
     }
 
     @Test
-    public void testHandleUnmarkCommand() {
+    public void testHandleUnmarkCommand() throws DukeException {
 
-        try {
-            TaskList tl = new TaskList();
-            CommandHandler ch = new CommandHandler(tl, ui, dtf);
-            String userInput = "event build sandcastle /from 18/8/2020 1700 /to 18/8/2020 1800";
-            String[] splitStr = userInput.split("\\s+");
-            ch.handleEventCommand(splitStr);
-            String userInput2 = "mark 1";
-            String[] splitStr2 = userInput2.split("\\s+");
-            ch.handleMarkCommand(splitStr2);
-            String userInput3 = "unmark 1";
-            String[] splitStr3 = userInput3.split("\\s+");
-            ch.handleUnmarkCommand(splitStr3);
-            boolean isMarked = tl.getItem(0).getIsCompleted();
-            assertEquals(isMarked, false);
-        } catch (DukeException e) {
-            System.out.println("Error occurred while adding items to Tasklist.");
-        }
+        TaskList tl = new TaskList();
+        CommandHandler ch = new CommandHandler(tl, ui, dtf);
+        String userInput = "event build sandcastle /from 18/8/2020 1700 /to 18/8/2020 1800";
+        String[] splitStr = userInput.split("\\s+");
+        ch.handleEventCommand(splitStr);
+        String userInput2 = "mark 1";
+        String[] splitStr2 = userInput2.split("\\s+");
+        ch.handleMarkCommand(splitStr2);
+        String userInput3 = "unmark 1";
+        String[] splitStr3 = userInput3.split("\\s+");
+        ch.handleUnmarkCommand(splitStr3);
+        boolean isMarked = tl.getItem(0).getIsCompleted();
+        assertEquals(isMarked, false);
+
     }
 }
