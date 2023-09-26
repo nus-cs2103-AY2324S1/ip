766187e90d648ddedd6075cf6999e1e8a4129f5d
andytoh1 <e0773488@u.nus.edu>
2023-09-26 21:10:58 +0800

    Improve code quality
    Code quality can contribute greatly to readability of the code.
    
    In this change, multiple long and deeply-nested methods were
    broken into smaller components so as to avoid confusion down the line.

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -56,58 +56,51 @@ public class Chatbot extends Application {
 
     @Override
     public void start(Stage stage) {
-        //Step 1. Setting up required components
-
-        //The container for the content of the chat to scroll.
+        // Step 1. Setting up required components
         TaskList taskList = new TaskList();
         Ui ui = new Ui();
-
         Storage.load(taskList);
 
+        initializeUI(stage);
+        setStage(stage);
+        setEventHandlers(taskList);
+    }
+
+    private void initializeUI(Stage stage) {
         scrollPane = new ScrollPane();
         dialogContainer = new VBox();
         scrollPane.setContent(dialogContainer);
-
         userInput = new TextField();
         sendButton = new Button("Send");
-
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
+        mainLayout.setPrefSize(400.0, 600.0);
         scene = new Scene(mainLayout);
-
         stage.setScene(scene);
         stage.show();
+    }
 
+    private void setStage(Stage stage) {
         stage.setTitle("Chatbot");
         stage.setResizable(false);
         stage.setMinHeight(600.0);
         stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
         scrollPane.setPrefSize(385, 535);
         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
         scrollPane.setVvalue(1.0);
         scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
         dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
         userInput.setPrefWidth(325.0);
-
         sendButton.setPrefWidth(55.0);
-
         AnchorPane.setTopAnchor(scrollPane, 1.0);
-
         AnchorPane.setBottomAnchor(sendButton, 1.0);
         AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setLeftAnchor(userInput, 1.0);
         AnchorPane.setBottomAnchor(userInput, 1.0);
+    }
 
+    private void setEventHandlers(TaskList taskList) {
         sendButton.setOnMouseClicked((event) -> {
             dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
             userInput.clear();
@@ -127,10 +120,10 @@ public class Chatbot extends Application {
         userInput.setOnAction((event) -> {
             handleUserInput(taskList);
         });
-
-
-        // more code to be added here later
     }
+
+    // more code to be added here later
+
 
     private Label getDialogLabel(String text) {
         // You will need to import `javafx.scene.control.Label`.
