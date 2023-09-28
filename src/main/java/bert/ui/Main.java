1fb7aac4dfb76702085f47a1b12a706869faea10
peasantbird <bertrand.wong.xh@gmail.com>
2023-09-28 16:42:54 +0800

    Add a representative screenshot of Bert

--- src/main/java/bert/ui/Main.java
+++ src/main/java/bert/ui/Main.java
@@ -25,6 +25,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Bert");
             fxmlLoader.<MainWindow>getController().setBert(bert);
             stage.show();
         } catch (IOException e) {
