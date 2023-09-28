4ae78e214cb9b7288f8f338fa8913b73588b7e69
Song_Bingxi <bingxi.bobby.song@gmail.com>
2023-09-28 22:15:39 +0800

    modified Ui

--- src/main/java/veneto/ui/Ui.java
+++ src/main/java/veneto/ui/Ui.java
@@ -13,13 +13,13 @@ import java.util.Scanner;
 public class Ui {
     /* Fields */
     private final static String GREETS = "\nVeneto: \n";
-    private final static String[] COMMANDS = new String[] {
-            "toDo [TASK]",
-            "deadline [TASK] /by [DEADLINE(YYYY-MM-DD)]",
-            "event [TASK] /from [START_TIME(YYYY-MM-DD)] /to [END_TIME(YYYY-MM-DD)]",
-            "mark [TASK_ID]", "unmark [TASK_ID]",
-            "list", "bye"
-    };
+    private final static String COMMANDS =
+            "  toDo [TASK]\n" +
+            "  deadline [TASK] /by [DEADLINE(YYYY-MM-DD)]\n" +
+            "  event [TASK] /from [START_TIME(YYYY-MM-DD)] /to [END_TIME(YYYY-MM-DD)]\n" +
+            "  mark [TASK_ID]" + "unmark [TASK_ID]\n" +
+            "  find [KEYWORDS]\n" +
+            "  list" + "bye";
     private final static String LOGO =
             " ___      ___ __________  ___    ___  __________  __________  __________\n" +
             " \\  \\    /  /|   _______||   \\  |   ||   _______||___    ___||   ____   |\n" +
@@ -33,35 +33,28 @@ public class Ui {
      * shows which exception occurs and give hint to the user
      * @param e the DanException
      */
-    public static void showError(VenetoException e) {
+    public String showError(VenetoException e) {
         if (e instanceof VenetoOperateException) {
             switch (e.getMessage()) {
                 case "Unmarked":
-                    System.out.println(GREETS + " 这个没标记过哦！\n");
-                    return;
+                    return GREETS + " 这个没标记过哦！\n";
                 case "Marked":
-                    System.out.println(GREETS + " 这个已经做完了哦！\n");
-                    return;
+                    return GREETS + " 这个已经做完了哦！\n";
                 case "Add":
-                    System.out.println(GREETS + " 输入格式不对！");
-                    System.out.println(" 你可以跟我说：\n" + Arrays.toString(COMMANDS) + "\n");
-                    return;
+                    return GREETS + " 输入格式不对!\n 你可以跟我说：\n" + COMMANDS + "\n";
                 case "Not Found":
-                    System.out.println(GREETS + " 好像没有这样的任务要做哦\n");
-                    return;
+                    return GREETS + " 好像没有这样的任务要做哦\n";
             }
         } else if (e instanceof VenetoStorageException) {
             if (e.getMessage().equals("Storage File Destroyed")) {
-                System.out.println(GREETS + " 没找到内存哦 现在重新创建一个！\n");
+                return GREETS + " 没找到内存哦 现在重新创建一个！\n";
             } else {
-                System.out.println(GREETS + " 不该发生的发生了…");
+                return GREETS + " 不该发生的发生了…";
             }
         } else if (e.getMessage().equals("Invalid Command")) {
-            System.out.println(GREETS + " 输入格式不对！");
-            System.out.println(" 你可以跟我说：\n" + Arrays.toString(COMMANDS) + "\n");
-        } else {
-            System.out.println(GREETS + " ???\n");
+            return GREETS + " 输入格式不对！" + " 你可以跟我说：\n" + COMMANDS + "\n";
         }
+        return GREETS + " ???\n";
     }
 
     /**
@@ -69,75 +62,50 @@ public class Ui {
      * @param command the Command operated just now
      * @param tasks the TaskList of Veneto
      */
-    public void afterCommand(Command command, TaskList tasks) {
+    public String afterCommand(Command command, TaskList tasks) {
         switch (command.getType()) {
             case "exit":
-                goodbye();
-                break;
+                return goodbye();
             case "add":
-                System.out.println(
-                        GREETS + " 记下来了：\n " + command +
-                                "!\n 现在有" + tasks.size() + "项任务要做\n"
-                );
-                break;
+                return GREETS + " 记下来了：\n " + command +
+                        "!\n 现在有" + tasks.size() + "项任务要做\n";
             case "list":
-                System.out.println(
-                        GREETS + " 还有这些要做哦！\n" +
-                                tasks.toString()
-                );
-                break;
+                return GREETS + " 还有这些要做哦！\n" + tasks.toString();
             case "mark":
-                System.out.println(
-                        GREETS + " 我这就帮您标记好！\n " + command + "\n"
-                );
-                break;
+                return GREETS + " 我这就帮您标记好！\n " + command + "\n";
             case "unmark":
-                System.out.println(
-                        GREETS + " 啊 没关系 帮您擦掉标记了哦" + "\n " + command + "\n"
-                );
-                break;
+                return GREETS + " 啊 没关系 帮您擦掉标记了哦" + "\n " + command + "\n";
             case "delete":
-                System.out.println(
-                        GREETS +
-                                " 好啦，帮你擦掉了一条任务哦：\n " + command +
-                                "\n 现在还剩下" + tasks.size() + "项任务\n"
-                );
-                break;
+                return GREETS + " 好啦，帮你擦掉了一条任务哦：\n " + command +
+                                "\n 现在还剩下" + tasks.size() + "项任务\n";
             case "find":
-                System.out.println(
-                        GREETS + " 相关的任务都在这里了哦：\n" + command
-                );
-                break;
+                return GREETS + " 相关的任务都在这里了哦：\n" + command;
         }
+        return "Parser error";
     }
 
     /**
      * get input from user
      * @return the command translated from user input
      */
-    public Command getCommand() {
-        String text = new Scanner(System.in).nextLine();
-        return Parser.translateCommand(text);
+    public Command getCommand(String input) {
+        return Parser.translateCommand(input);
     }
 
     /**
      * greets when Veneto runs
      */
-    public void hello() {
-        System.out.println(
+    public String hello() {
+        return
                 LOGO + GREETS +
-                    " VV为您服务\n" +
-                    " 有什么可以要帮忙可以跟我说！\n"
-        );
+                    " Veneto为您服务哦\n" +
+                    " 有什么可以要帮忙可以跟我说！\n";
     }
 
     /**
      * says goodbye when session ends
      */
-    public void goodbye() {
-        System.out.println(
-                GREETS +
-                        " 拜拜啦 下次见\n"
-        );
+    public String goodbye() {
+        return GREETS + " 拜拜啦 下次见\n";
     }
 }
