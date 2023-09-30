7c315038f9b834a6f0e824c2bc74cfaa8a726811
Song_Bingxi <bingxi.bobby.song@gmail.com>
2023-09-30 09:39:43 +0800

    added constants STORAGE_DIR and STORAGE_PATH in Storage.java

--- src/main/java/veneto/Veneto.java
+++ src/main/java/veneto/Veneto.java
@@ -11,14 +11,14 @@ public class Veneto {
     private TaskList tasks;
     private Storage storage;
     private Ui ui;
-    private String filePath = "./src/main/data/veneto.txt";
+
 
     /* Constructor */
 
     public Veneto() {
         try {
             ui = new Ui();
-            storage = new Storage(filePath);
+            storage = new Storage();
             tasks = storage.load();
         } catch (VenetoException e) {
             ui.showError(e);
