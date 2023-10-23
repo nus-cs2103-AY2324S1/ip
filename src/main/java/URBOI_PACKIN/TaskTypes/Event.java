0efd428d4a1f349c9fb036f68fcdbd98e3cf78ee
TyrusLye <Lyetyrus@gmail.com>
2023-10-23 23:43:11 +0800

    A-Assertions

--- src/main/java/URBOI_PACKIN/TaskTypes/Event.java
+++ src/main/java/URBOI_PACKIN/TaskTypes/Event.java
@@ -24,4 +24,12 @@ public class Event extends Task {
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
 }
